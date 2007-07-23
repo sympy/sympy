b8dacaf445aabb9b3c647a2d0f39750c473d0975
inferno1386 <devnull@localhost>
2007-07-23 00:14:03 +0000

    Added test_demidovich to series module.

---
+++ sympy/modules/series/tests/test_demidovich.py
@@ -0,0 +1,111 @@
+from sympy import *
+
+"""
+(*) in problem number means that the number is relative to the book "Anti-demidovich,
+problemas resueltos, Ed. URSS"
+
+"""
+
+x = Symbol("x")
+h = Symbol("h")
+a = Symbol('a')
+
+def test_leadterm():
+    x = Symbol("x")
+    assert (3+2*x**(log(3)/log(2)-1)).leadterm(x)==(3,0)
+
+def sqrt3(x):
+    return x**Rational(1,3)
+
+def sqrt4(x):
+    return x**Rational(1,4)
+
+#def tan(x):
+#    return sin(x)/cos(x)
+
+def test_Limits_simple_0():
+    # XXX recursion depth exceeded
+    #assert Limit((2**(x+1)+3**(x+1))/(2**x+3**x),x,oo)==3  #175
+    pass
+
+def test_Limits_simple_1():
+    assert Limit((x+1)*(x+2)*(x+3)/x**3,x, oo)==1  #172
+    assert Limit(sqrt(x+1)-sqrt(x),x,oo)==0  #179
+    assert Limit((2*x-3)*(3*x+5)*(4*x-6)/(3*x**3+x-1),x,oo)==8  #Primjer 1
+    assert Limit(x/sqrt3(x**3+10),x,oo)==1  #Primjer 2
+    assert Limit((x+1)**2/(x**2+1),x,oo)==1  #181
+
+def test_Limits_simple_2():
+    assert Limit(1000*x/(x**2-1),x,oo)==0  #182
+    assert Limit((x**2-5*x+1)/(3*x+7),x,oo)==oo  #183
+    assert Limit((2*x**2-x+3)/(x**3-8*x+5),x,oo)==0  #184
+    assert Limit((2*x**2-3*x-4)/sqrt(x**4+1),x,oo)==2  #186
+    assert Limit((2*x+3)/(x+sqrt3(x)),x,oo)==2  #187
+    assert Limit(x**2/(10+x*sqrt(x)),x,oo)==oo  #188
+    assert Limit(sqrt3(x**2+1)/(x+1),x,oo)==0  #189
+    assert Limit(sqrt(x)/sqrt(x+sqrt(x+sqrt(x))),x,oo)==1  #190
+
+def test_Limits_simple_3():
+    # XXX Wrong limit
+    #assert Limit((x**2-(a+1)*x+a)/(x**3-a**3),x,a)==((a-1)/(3*a**2)).expand()  #196
+    assert Limit(((x+h)**3-x**3)/h,h,0)==3*x**2  #197
+    # XXX Wrong limit
+    #assert Limit((1/(1-x)-3/(1-x**3)),x,1)==-1  #198
+    assert Limit((sqrt(1+x)-1)/(sqrt3(1+x)-1),x,0)==Rational(3)/2  #Primer 4
+    assert Limit((sqrt(x)-1)/(x-1),x,1)==Rational(1)/2  #199
+    # XXX Errors
+    #assert Limit((sqrt(x)-8)/(sqrt3(x)-4),x,64)==3  #200
+    assert Limit((sqrt3(x)-1)/(sqrt4(x)-1),x,1)==Rational(4)/3  #201
+    assert Limit((sqrt3(x**2)-2*sqrt3(x)+1)/(x-1)**2,x,1)==Rational(1)/9  #202
+
+def test_Limits_simple_4():
+    assert Limit((sqrt(x)-sqrt(a))/(x-a),x,a)==1/(2*sqrt(a))  #Primer 5
+    assert Limit((sqrt(x)-1)/(sqrt3(x)-1),x,1)==Rational(3)/2  #205
+    assert Limit((sqrt(1+x)-sqrt(1-x))/x,x,0)==1  #207
+    assert Limit(sqrt(x**2-5*x+6)-x,x,oo)==-Rational(5)/2  #213
+    assert Limit(x*(sqrt(x**2+1)-x),x,oo)==Rational(1)/2  #214
+    # XXX Wrong limit
+    #assert Limit(x-sqrt3(x**3-1),x,oo)==0  #215
+    assert Limit(log(1+exp(x))/x,x,-oo)==0  #267a
+    assert Limit(log(1+exp(x))/x,x,oo)==1  #267b
+
+def test_f1():
+    a = Symbol("a")
+    m = Symbol("m")
+    n = Symbol("n")
+    assert Limit(sin(x)/x,x,2) == sin(2)/2 #216a
+    assert Limit(sin(x)/x,x,oo) == 0 #216b
+    assert Limit(sin(3*x)/x,x,0) == 3 #217
+    # XXX recursion depth exceeded
+    #assert Limit(sin(5*x)/sin(2*x),x,0) == Rational(5)/2 #218
+    assert Limit(sin(pi*x)/sin(3*pi*x),x,0) == Rational(1)/3 #219
+    assert Limit(x*sin(pi/x),x,oo) == pi #220
+    assert Limit((1-cos(x))/x**2,x,0) == Rational(1,2) #221
+    assert Limit((sin(x)-sin(a))/(x-a),x,a) == cos(a) #222, *176
+    # XXX Wrong limit
+    #assert Limit((cos(x)-cos(a))/(x-a),x,a) == -sin(a) #223
+    assert Limit((sin(x+h)-sin(x))/h,h,0) == cos(x) #225
+    assert Limit(x*sin(1/x),x,0) == 0 #227a
+    assert Limit(x*sin(1/x),x,oo) == 1 #227b
+    # XXX Wrong limit
+    #assert Limit((cos(m*x)-cos(n*x))/x**2,x,0) == ((n**2-m**2)/2).expand() #232
+    # XXX Errors
+    #assert Limit((tan(x)-sin(x))/x**3,x,0) == Rational(1,2) #233
+    assert Limit((x-sin(2*x))/(x+sin(3*x)),x,0) == -Rational(1,4) #237
+    # XXX recursion depth exceeded
+    #assert Limit((1-sqrt(cos(x)))/x**2,x,0) == Rational(1,4) #239
+    assert Limit((sqrt(1+sin(x))-sqrt(1-sin(x)))/x,x,0) == 1 #240
+
+    assert Limit((sin(2*x)/x)**(1+x),x,0) == 2 #Primer 7
+    assert Limit(((x+1)/(2*x+1))**(x**2),x,oo) == 0 #Primer 8
+    # XXX Wrong limit
+    #assert Limit(((x-1)/(x+1))**x,x,oo) == exp(-2) #Primer 9
+
+    assert Limit((1+h/x)**x,x,oo) == exp(h) #Primer 9
+
+def test_f2():
+    a = Symbol('a', real=True)
+    # XXX Recursion depth exceeded
+    #assert Limit( (sqrt(cos(x)) - sqrt3(cos(x))) / (sin(x)**2) , x, 0) == -Rational(1, 12) #*184
+    # XXX Requires inverse trig functions
+    #assert Limit(asin(a*x)/x, x, 0) == a
