e2f9361672fa5f32bc4e95a792888356d098e59e
ondrej.certik <devnull@localhost>
2007-07-24 20:47:01 +0000

    The Factorial in the core commented out

--- sympy/core/integer_sequences.py
+++ sympy/core/integer_sequences.py
@@ -9,19 +9,19 @@ class IntegerSequence(DefinedFunction):
 
     pass
 
-class Factorial(IntegerSequence):
+#class Factorial(IntegerSequence):
 
-    nofargs = 1
+#    nofargs = 1
 
-    def _eval_apply(self, arg):
-        if isinstance(arg, Basic.Zero): return Basic.One()
-        if isinstance(arg, Basic.Integer) and arg.is_positive:
-            r = arg.p
-            m = 1
-            while r:
-                m *= r
-                r -= 1
-            return Basic.Integer(m)
+#    def _eval_apply(self, arg):
+#        if isinstance(arg, Basic.Zero): return Basic.One()
+#        if isinstance(arg, Basic.Integer) and arg.is_positive:
+#            r = arg.p
+#            m = 1
+#            while r:
+#                m *= r
+#                r -= 1
+#            return Basic.Integer(m)
         # return Gamma(arg)
 
 class Binomial(IntegerSequence):
@@ -32,6 +32,7 @@ class Binomial(IntegerSequence):
     nofargs = 2
 
     def _eval_apply(self, z, k):
+        from sympy.modules.specfun.factorials import Factorial
         if isinstance(k, Basic.Integer):
             assert not k.is_negative,`k`
             l = []
@@ -40,6 +41,6 @@ class Binomial(IntegerSequence):
                 l.append(zk+n)
             return (Basic.Mul(*l)/Factorial()(k)).expand()
 
-Basic.singleton['factorial'] = Factorial
+#Basic.singleton['factorial'] = Factorial
 Basic.singleton['binomial'] = Binomial
 
