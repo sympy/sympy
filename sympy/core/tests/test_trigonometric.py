859f3b0f4eb1a9ad3fa5b951aae76e1f5440ebc5
ondrej.certik <devnull@localhost>
2007-07-26 19:04:01 +0000

    uncommenting tests, that work

--- sympy/core/tests/test_trigonometric.py
+++ sympy/core/tests/test_trigonometric.py
@@ -2,15 +2,15 @@ from sympy import *
 from decimal import Decimal
 
 x = Symbol('x')
-x = Symbol('y')
-x = Symbol('z')
+y = Symbol('y')
+z = Symbol('z')
 
 
 def feq(a,b):
     t = 1E-15
     return -t < a-b < t
 
-def _test_cos():
+def test_cos():
     assert cos(0) == 1
     assert cos(pi) == -1
     assert cos(-pi) == -1
@@ -22,31 +22,28 @@ def _test_cos():
     assert cos(pi/2) == 0
     assert cos((10**157+123)*pi/2) == 0
 
-    # XXX Most of these fail
-    assert cos(pi/3) == Rational(1, 2)
-    assert cos(2*pi/3) == -Rational(1, 2)
-    assert cos(-2*pi/3) == -Rational(1, 2)
-    assert cos(4*pi/3) == -Rational(1, 2)
-    assert cos(5*pi/3) == Rational(1, 2)
-    assert cos(7*pi/3) == Rational(1, 2)
-    assert cos(8*pi/3) == -Rational(1, 2)
+    #assert cos(pi/3) == Rational(1, 2)
+    #assert cos(2*pi/3) == -Rational(1, 2)
+    #assert cos(-2*pi/3) == -Rational(1, 2)
+    #assert cos(4*pi/3) == -Rational(1, 2)
+    #assert cos(5*pi/3) == Rational(1, 2)
+    #assert cos(7*pi/3) == Rational(1, 2)
+    #assert cos(8*pi/3) == -Rational(1, 2)
 
-    assert cos(pi/4) == Rational(1, 2)*sqrt(2)
-    assert cos(-pi/4) == Rational(1, 2)*sqrt(2)
-    assert cos(3*pi/4) == -Rational(1, 2)*sqrt(2)
-    assert cos(5*pi/4) == -Rational(1, 2)*sqrt(2)
-    assert cos(7*pi/4) == Rational(1, 2)*sqrt(2)
+    #assert cos(pi/4) == Rational(1, 2)*sqrt(2)
+    #assert cos(-pi/4) == Rational(1, 2)*sqrt(2)
+    #assert cos(3*pi/4) == -Rational(1, 2)*sqrt(2)
+    #assert cos(5*pi/4) == -Rational(1, 2)*sqrt(2)
+    #assert cos(7*pi/4) == Rational(1, 2)*sqrt(2)
 
-    assert cos(pi/6) == Rational(1, 2)*sqrt(3)
-    assert cos(5*pi/6) == -Rational(1, 2)*sqrt(3)
+    #assert cos(pi/6) == Rational(1, 2)*sqrt(3)
+    #assert cos(5*pi/6) == -Rational(1, 2)*sqrt(3)
 
-    # XXX No evalf for trig
     assert feq( float(cos(1)), 0.54030230586813977 )
     assert feq( float(cos(1) + cos(2)), 0.12415546932099736 )
     assert feq( float(cos(1)*cos(2)*cos(3)), 0.22259495730990297 )
     assert cos(1).evalf(precision=28) == Real("0.5403023058681397174009366074")
 
-    # XXX These all fail since expansion doesn't work
     assert cos(-x).expand() == cos(x)
     assert cos(-x-y).expand() == cos(x)*cos(y) - sin(x)*sin(y)
     assert cos(x+y-z).expand() == cos(x)*cos(y)*cos(z) + cos(x)*sin(y)*sin(z) - sin(x)*sin(y)*cos(z) + sin(x)*cos(y)*sin(z)
@@ -54,7 +51,7 @@ def _test_cos():
     assert cos(2*x).expand() == 2*cos(x)**2 - 1
     assert cos(-3*x).expand() == 4*cos(x)**3 - 3*cos(x)
 
-def _test_sin():
+def test_sin():
     assert sin(0) == 0
     assert sin(pi) == 0
     assert sin(-pi) == 0
@@ -67,25 +64,22 @@ def _test_sin():
     assert sin(5*pi/2) == 1
     assert sin(7*pi/2) == -1
 
-    # XXX Most of these fail
-    assert sin(pi/3) == Rational(1, 2)*sqrt(3)
-    assert sin(-2*pi/3) == -Rational(1, 2)*sqrt(3)
+    #assert sin(pi/3) == Rational(1, 2)*sqrt(3)
+    #assert sin(-2*pi/3) == -Rational(1, 2)*sqrt(3)
 
-    assert sin(pi/4) == Rational(1, 2)*sqrt(2)
-    assert sin(-pi/4) == -Rational(1, 2)*sqrt(2)
-    assert sin(17*pi/4) == Rational(1, 2)*sqrt(2)
-    assert sin(-3*pi/4) == -Rational(1, 2)*sqrt(2)
+    #assert sin(pi/4) == Rational(1, 2)*sqrt(2)
+    #assert sin(-pi/4) == -Rational(1, 2)*sqrt(2)
+    #assert sin(17*pi/4) == Rational(1, 2)*sqrt(2)
+    #assert sin(-3*pi/4) == -Rational(1, 2)*sqrt(2)
 
-    assert sin(pi/6) == Rational(1, 2)
-    assert sin(-pi/6) == -Rational(1, 2)
-    assert sin(7*pi/6) == -Rational(1, 2)
-    assert sin(-5*pi/6) == -Rational(1, 2)
+    #assert sin(pi/6) == Rational(1, 2)
+    #assert sin(-pi/6) == -Rational(1, 2)
+    #assert sin(7*pi/6) == -Rational(1, 2)
+    #assert sin(-5*pi/6) == -Rational(1, 2)
 
-    # XXX No evalf for trig
     assert feq( float(sin(1)), 0.8414709848078965 )
     assert sin(1).evalf(precision=28) == Real("0.8414709848078965066525023216")
 
-    # XXX These all fail since expansion doesn't work
     assert sin(-x).expand() == -sin(x)
     assert sin(-x-y).expand() == -sin(x)*cos(y) - cos(x)*sin(y)
     assert sin(x+y+z).expand() == sin(x)*cos(y)*cos(z) - sin(x)*sin(y)*sin(z) + cos(x)*sin(y)*cos(z) + cos(x)*cos(y)*sin(z)
