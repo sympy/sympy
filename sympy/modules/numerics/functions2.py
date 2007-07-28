19f19f6f7c0b99fffc349b73d359c984831e3e3a
fredrik.johansson <devnull@localhost>
2007-07-28 22:21:01 +0000

    tidied numerics gamma function code and fixed precision issue

--- sympy/modules/numerics/functions2.py
+++ sympy/modules/numerics/functions2.py
@@ -10,16 +10,18 @@ from utils_ import make_fixed
 from sympy import Rational
 
 
-#----------------------------------------------------------------------
-# Gamma function
-#
+#---------------------------------------------------------------------------#
+#                                                                           #
+#                              Gamma function                               #
+#                                                                           #
+#---------------------------------------------------------------------------#
 
 """
 We compute the gamma function using Spouge's approximation
 
     x! = (x+a)**(x+1/2) * exp(-x-a) * [c_0 + S(x) + eps]
 
-where S(x) is the sum c_k/(x+k) from k = 1 to a-1 and the coefficients
+where S(x) is the sum of c_k/(x+k) from k = 1 to a-1 and the coefficients
 are given by
 
   c_0 = sqrt(2*pi)
@@ -28,89 +30,146 @@ are given by
   c_k =  ----------- (a-k)**(k-1/2) exp(-k+a),  k = 1,2,...,a-1
           (k - 1)!
 
-If we choose a = int(1.26*n), the error eps is less than 10**-n
-for any x in the right complex half-plane (assuming a > 2). In practice,
-it seems that a can be chosen quite a bit lower (30-50%); this needs
-to be looked into.
+Due to an inequality proved by Spouge, if we choose a = int(1.26*n), the
+error eps is less than 10**-n for any x in the right complex half-plane
+(assuming a > 2). In practice, it seems that a can be chosen quite a bit
+lower still (30-50%); this possibility should be investigated.
 
 Reference:
 John L. Spouge, "Computation of the gamma, digamma, and trigamma
 functions", SIAM Journal on Numerical Analysis 31 (1994), no. 3, 931-944.
 """
 
+
+#----------------------------------------------------------------------
+#
+# We first implement a helper function for calculating the coefficients
+# c_k and caching them so that they can be re-used for multiple gamma
+# function evaluations
+#
+
 _spouge_cache = {}
 
-# Calculate coefficients for Spouge's approximation
-def _calc_spouge(a, prec):
-    c = [0] * a
+def _calc_spouge_coefficients(a, prec):
+    """
+    Calculate Spouge coefficients for approximation with parameter a.
+    Return a list of big integers representing the coefficients in
+    fixed-point form with a precision of prec bits.
+    """
+
     # We'll store the coefficients as fixed-point numbers but calculate
     # them as Floats for convenience. The initial terms are huge, so we
     # need to allocate extra bits to ensure full accuracy. The integer
-    # part of the largest term has ~= exp(a) or 2**(1.4*a) bits.
+    # part of the largest term has size ~= exp(a) or 2**(1.4*a)
     floatprec = prec + int(a*1.4)
     Float.store()
     Float.setprec(floatprec)
+
+    c = [0] * a
     b = exp(a-1)
     e = exp(1)
     c[0] = make_fixed(sqrt(2*pi_float()), prec)
     for k in range(1, a):
-        t = ((-1)**(k-1) * (a-k)**k)
-        u = t * b / sqrt(a-k)
-        c[k] = make_fixed(u, prec)
+        # print "%02f" % (100.0 * k / a), "% done"
+        c[k] = make_fixed(((-1)**(k-1) * (a-k)**k) * b / sqrt(a-k), prec)
+        # Divide off e and k instead of computing exp and k! from scratch
         b = b / (e * k)
-        # print "computing coefficients: %02f" % (100.0 * k / a), "% done"
+
     Float.revert()
     return c
 
 # Cached lookup of coefficients
-def _get_spouge(prec):
+def _get_spouge_coefficients(prec):
+
+    # This exact precision has been used before
     if prec in _spouge_cache:
         return _spouge_cache[prec]
+
     for p in _spouge_cache:
-        # Coefficients calculated for a slightly higher precision are ok too
+        # Coefficients calculated for a slightly higher precision are ok
+        # too. But if the difference is too big, we're better off
+        # starting from scratch
         if 0.8 <= float(p)/prec < 1:
             return _spouge_cache[p]
-    a = max(3, int(0.35*prec))  # ~= 1.26*n
-    coefs = _calc_spouge(a, prec)
+
+    # Here we estimate the value of a based on Spouge's inequality for
+    # the relative error
+    a = max(3, int(0.39*prec))  # ~= 1.26*n
+
+    # Compute and return
+    coefs = _calc_spouge_coefficients(a, prec)
     _spouge_cache[prec] = (prec, a, coefs)
     return _spouge_cache[prec]
 
+
+#----------------------------------------------------------------------
+# Helper functions for computing the sum part, S(x)
+#
+
+# Optimizing for different types of numbers makes the computation
+# several times faster in some common cases
+
+def _spouge_sum_float(x, prec, a, c):
+    xf = make_fixed(x, prec)
+    s = c[0]
+    for k in xrange(1, a):
+        s += (c[k] << prec) // (xf + (k << prec))
+    return Float((s, -prec))
+
+def _spouge_sum_rational(p, q, prec, a, c):
+    s = c[0]
+    for k in xrange(1, a):
+        s += c[k] * q // (p+q*k)
+    return Float((s, -prec))
+
+# to be implemented
+def _spouge_sum_complex():
+    pass
+
+
+#----------------------------------------------------------------------
+# Main function
+#
+
 def gamma(x):
     """
-    gamma(x) -- calculate the gamma function of the real number x
+    gamma(x) -- calculate the gamma function of the real number x.
 
     x must not be a negative integer or 0
     """
-    if not isinstance(x, (Float, Rational)):
+    Float.store()
+    Float._prec += 2
+
+    if not isinstance(x, (Float, Rational, int, long)):
         x = Float(x)
+
+    # For negative x (or positive x close to the pole at x = 0),
+    # we use the reflection formula
     if x < 0.25:
         if x == int(x):
             raise ZeroDivisionError, "gamma function pole"
-        # Use reflection formula
         Float._prec += 3
         p = pi_float()
         g = p / (sin(p*x) * gamma(1-x))
         Float._prec -= 3
         return +g
+
     x -= 1
-    prec, a, c = _get_spouge(Float.getprec() + 10)
-    # Perform summation in fixed-point mode
-    s = c[0]
-    # Optimize for rational numbers
+    prec, a, c = _get_spouge_coefficients(Float.getprec()+7)
+    # Sum the series
+    if isinstance(x, (int, long)):
+        x = Rational(x)
     if isinstance(x, Rational):
-        p = x.p
-        q = x.q
-        for k in xrange(1, a):
-            s += c[k] * q // (p+q*k)
+        s = _spouge_sum_rational(x.p, x.q, prec, a, c)
+        x = Float(x)
     else:
-        xf = make_fixed(x, prec)
-        for k in xrange(1, a):
-            s += (c[k] << prec) // (xf + (k << prec))
-    #print "moo", s
-    if isinstance(x, Rational):
-        # XXX: need to count bits to avoid loss of precision
-        x = Float(((x.p<<prec)//x.q, -prec))
-    Float._prec += 8
-    g = exp(log(x+a)*(x+Float(0.5))) * exp(-(x+a)) * Float((s, -prec))
-    Float._prec -= 8
+        x = Float(x)
+        s = _spouge_sum_float(x, prec, a, c)
+
+    # TODO: higher precision may be needed here when the precision
+    # and/or size of x are extremely large
+    Float._prec += 10
+    g = exp(log(x+a)*(x+Float(0.5))) * exp(-x-a) * s
+
+    Float.revert()
     return +g
--- sympy/modules/numerics/tests/test_functions2.py
+++ sympy/modules/numerics/tests/test_functions2.py
@@ -6,10 +6,8 @@ from sympy.modules.numerics import *
 import math
 
 def test_gamma():
-    assert gamma(1) == 1
-    assert gamma(2) == 1
-    assert gamma(3) == 2
-    assert gamma(10) == 362880
+    Float.store()
+    Float.setprec(53)
     assert gamma(0.25).ae('3.6256099082219083119')
     assert gamma(0.0001).ae('9999.4228832316241908')
     assert gamma(300).ae('1.0201917073881354535e612')
@@ -17,6 +15,16 @@ def test_gamma():
     assert gamma(-7.43).ae('0.00026524416464197007186')
     assert gamma(Rational(1,2)) == gamma(0.5)
     assert gamma(Rational(-7,3)).ae(gamma(Float(7)/3))
+    # Should always give exact factorials when they can
+    # be represented as Floats under the current working precision
+    fact = 1
+    for i in range(1, 18):
+        assert gamma(i) == fact
+        fact *= i
+    Float.setprec(1000)
+    for i in range(18, 105):
+        assert gamma(i) == fact
+        fact *= i
     Float.setdps(100)
     assert gamma(0.5).ae(sqrt(pi_float()))
     Float.revert()
