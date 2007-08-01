c579d08fe9515807d7c49606d0f3facf58c125ab
fredrik.johansson <devnull@localhost>
2007-08-01 01:49:05 +0000

    reimplemented sums_products.py, placed in /concrete/ (perhaps temporarily); all tests except one run

---
+++ sympy/concrete/sums_products.py
@@ -0,0 +1,206 @@
+from sympy import *
+from sympy.specfun import rising_factorial, factorial_, factorial_simplify
+from sympy.specfun.factorials import unfac
+from sympy.specfun import bernoulli2, bernoulli_poly
+from sympy.polynomials import factor, PolynomialException
+
+def ispoly(expr, var):
+    return False
+
+def getab(expr):
+    cls = expr.__class__
+    return cls(expr[0]), cls(*expr[1:])
+
+def indexsymbol(a):
+    if isinstance(a, Symbol):
+        return Symbol(a.name, integer=True)
+    else:
+        return Basic.sympify(a)
+
+class _BigOperator(Basic):
+
+    def __new__(cls, f, (i, a, b)):
+        self = Basic.__new__(cls)
+        assert isinstance(i, Symbol)
+        self.i = i
+        self.f = self.sympify(f)
+        self.a = indexsymbol(a)
+        self.b = indexsymbol(b)
+        return self.eval()
+
+
+class Sum(_BigOperator):
+    """
+    Symbolic summation with a variable number of terms
+
+    Sum(f, (i, a, b)) represents \sum_{i=a}^b f(i)
+    """
+
+    def __repr__(self):
+        return "Sum(%r, (%r, %r, %r))" % (self.f, self.i, self.a, self.b)
+
+    __str__ = __repr__
+
+    def reindex(self, a):
+        """Reindex the sum to start at a new lower index a."""
+        diff = self.a - a
+        b = self.b - diff
+        f = self.f.subs(self.i, self.i + diff)
+        return Sum(f, (self.i, a, b))
+
+    def split(self, n):
+        """Split into two sums, the first with n terms."""
+        f, i, a, b = self.f, self.i, self.a, self.b
+        return Sum(f, (i, a, a+n-1)) + Sum(f, (i, a+n, b))
+
+    def eval(self):
+        f, i, a, b = self.f, self.i, self.a, self.b
+
+        # Exploit the linearity of the sum
+        if not f.has(i):
+            return f*(b-a+1)
+        if isinstance(f, Mul):
+            L, R = getab(f)
+            if not L.has(i): return L*Sum(R, (i, a, b))
+            if not R.has(i): return R*Sum(L, (i, a, b))
+        if isinstance(f, Add):
+            L, R = getab(f)
+            lsum = Sum(L, (i,a,b))
+            rsum = Sum(R, (i,a,b))
+            if not isinstance(lsum, Sum) and not isinstance(rsum, Sum):
+                return lsum + rsum
+
+        # Polynomial terms with Faulhaber's formula
+        if f == i:
+            f = Pow(i, 1, evaluate=False) # TODO: match should handle this
+        p = Wild('p')
+        e = f.match(i**p)
+        if e != None:
+            c = p.subs_dict(e)
+            if c.is_integer and c >= 0:
+                s = (bernoulli_poly(c+1, b+1)-bernoulli_poly(c+1, a))/(c+1)
+                return s.expand()
+
+        # Geometric terms
+        if isinstance(f, Pow):
+            r, k = f[:]
+            if not r.has(i) and k == i:
+                # TODO: Pow should be able to simplify x**oo depending
+                # on whether |x| < 1 or |x| > 1 for non-rational x
+                if b == oo and isinstance(r, Rational) and abs(r) < 1:
+                    return r**a / (1-r)
+                else:
+                    return (r**a - r**(b+1)) / (1-r)
+
+        # Should nothing else works, use brute force if possible
+        if isinstance(a, Rational) and a.is_integer and \
+           isinstance(b, Rational) and b.is_integer:
+            s = 0
+            for j in range(a, b+1):
+                s += f.subs(i, j)
+            return s
+
+        return self
+
+    def euler_maclaurin(self, n=0):
+        """
+        Return n-th order Euler-Maclaurin approximation of self.
+        
+        The 0-th order approximation is simply the corresponding
+        integral
+        """
+        f, i, a, b = self.f, self.i, self.a, self.b
+        x = Symbol('x')
+        s = integrate(f.subs(i, x), x==[a,b])
+        if n > 0:
+            s += (f.subs(i, a) + f.subs(i, b))/2
+        for k in range(1, n):
+            g = f.diff(i, 2*k-1)
+            s += bernoulli2(2*k)/factorial_(2*k)*(g.subs(i,b)-g.subs(i,a))
+        return s
+
+
+class Product(_BigOperator):
+    """
+    Symbolic product with a variable number of factors
+
+    Product(f, (i, a, b)) represents \prod_{i=a}^b f(i)
+    """
+
+    def __repr__(self):
+        return "Product(%r, (%r, %r, %r))" % (self.f, self.i, self.a, self.b)
+
+    __str__ = __repr__
+
+    def eval(self):
+        # Perform factorial_simplify on any subproduct. It
+        # would be better to do this only once at the end, but this
+        # currently creates SymPy-unequal output for mathematically
+        # equal input (see Wallis product example
+        # in test_sums_products)
+        p = self._eval()
+        if isinstance(p, Product):
+            return self
+        else:
+            return factorial_simplify(p)
+        return p
+
+    def _eval(self):
+        f, i, a, b = self.f, self.i, self.a, self.b
+
+        if not f.has(i):
+            return f**(b-a+1)
+
+        if isinstance(f, Mul):
+            L, R = getab(f)
+            lp = Product(L, (i, a, b))
+            rp = Product(R, (i, a, b))
+            if not (isinstance(lp, Product) and isinstance(rp, Product)):
+                return lp * rp
+
+        if isinstance(f, Pow):
+            base, exp = f[:]
+            if not base.has(i):
+                s = Sum(exp, (i, a, b))
+                if not isinstance(s, Sum):
+                    return base ** s
+            elif not exp.has(i):
+                p = Product(base, (i, a, b))
+                if not isinstance(p, Product):
+                    return p ** exp
+
+        # Linear functions
+        if f == i:
+            return rising_factorial(a, b-a+1)
+        #if ispoly(f, i):
+        p = Wild('p')
+        q = Wild('q')
+        e = f.match(p+q*i)
+        if e != None:
+            pp = p.subs_dict(e)
+            qq = q.subs_dict(e)
+            if not pp.has(i) and not qq.has(i):
+                r = qq**(b-a+1) * unfac(b+pp/qq) / unfac(a+pp/qq-1)
+                return r
+
+        # Given a more complicated rational expression, try to factor
+        # it into linear functions
+        if isinstance(f, Add):
+            try:
+                num, den = fraction(together(f))
+                g = factor(num) / factor(den)
+                p = Product(g, (i, a, b))
+                if not isinstance(p, Product):
+                    return p
+            except PolynomialException:
+                pass
+
+        # Brute force
+        if isinstance(a, Rational) and a.is_integer and \
+           isinstance(b, Rational) and b.is_integer:
+            p = 1
+            for j in range(a, b+1):
+                p *= f.subs(i, j)
+            return p
+
+        return self
---
+++ sympy/concrete/tests/test_sums_products.py
@@ -0,0 +1,86 @@
+import sys
+sys.path.append(".")
+
+import py
+
+from sympy import *
+from sympy.concrete.sums_products import *
+from sympy.specfun import factorial_
+
+n = Symbol('n')
+a = Symbol('a')
+b = Symbol('b')
+
+def test_str():
+    assert str(Sum(cos(3*n), (n, a, b))) == "Sum(cos(3*n), (n, a, b))"
+
+def test_arithmetic_sums():
+    assert Sum(1, (n, a, b)) == b-a+1
+    assert Sum(1, (n, 1, 10)) == 10
+    assert Sum(2*n, (n, 0, 10**10)) == 100000000010000000000
+
+def test_polynomial_sums():
+    assert Sum(n**2, (n, 3, 8)) == 199
+    assert Sum(n, (n, a, b)) == \
+        ((a+b)*(b-a+1)/2).expand()
+    assert Sum(n**2, (n, 1, b)) == \
+        ((2*b**3+3*b**2+b)/6).expand()
+    assert Sum(n**3, (n, 1, b)) == \
+        ((b**4+2*b**3+b**2)/4).expand()
+    assert Sum(n**6, (n, 1, b)) == \
+        ((6*b**7+21*b**6+21*b**5-7*b**3+b)/42).expand()
+
+def test_geometric_sums():
+    assert Sum(pi**n, (n, 0, b)) == (1-pi**(b+1)) / (1-pi)
+    assert Sum(2 * 3**n, (n, 0, b)) == 3**(b+1) - 1
+    assert Sum(Rational(1,2)**n, (n, 1, oo)) == 1
+    assert Sum(2**n, (n, 0, b)) == 2**(b+1) - 1
+
+def test_composite_sums():
+    f = Rational(1,2)*(7 - 6*n + Rational(1,7)*n**3)
+    s = Sum(f, (n, a, b))
+    assert not isinstance(s, Sum)
+    A = 0
+    for i in range(-3, 5):
+        A += f.subs(n, i)
+    B = s.subs(a,-3).subs(b,4)
+    assert A == B
+
+def test_finite_sums():
+    assert Sum(cos(n), (n, -2, 1)) == cos(-2)+cos(-1)+cos(0)+cos(1)
+
+def test_euler_maclaurin():
+    z = Sum(1/n**3, (n, 1, oo))
+    A, B = getab(z.split(50))
+    apery = (A + B.euler_maclaurin(8)).evalf(25)
+    assert abs(apery - Real("1.202056903159594285399738162")) < Real("1e-20")
+
+def test_simple_products():
+    assert Product(2, (n, a, b)) == 2**(b-a+1)
+    assert Product(n, (n, 1, b)) == factorial_(b)
+    assert Product(n**3, (n, 1, b)) == factorial_(b)**3
+    assert Product(3**(2+n), (n, a, b)) \
+           == 3**(2*(1-a+b)+b/2+(b**2)/2+a/2-(a**2)/2)
+    assert Product(cos(n), (n, 3, 5)) == cos(3)*cos(4)*cos(5)
+    # If Product managed to evaluate this one, it most likely got it wrong!
+    assert isinstance(Product(n**n, (n, 1, b)), Product)
+
+def test_rational_products():
+    assert Product(1+1/n, (n, a, b)) == (1+b)/a
+    assert Product(n+1, (n, a, b)) == factorial_(1+b)/factorial_(a)
+    assert Product((n+1)/(n-1), (n, a, b)) == b*(1+b)/(a*(a-1))
+    assert Product(n/(n+1)/(n+2), (n, a, b)) \
+           == a*factorial_(a+1)/(b+1)/factorial_(b+2)
+    assert Product(n*(n+1)/(n-1)/(n-2), (n, a, b)) \
+           == b**2*(b-1)*(1+b)/(a-1)**2/(a*(a-2))
+
+def _test_wallis_product():
+    # Wallis product, given in two different forms to ensure that Product
+    # can factor simple rational expressions
+    A = Product(4*n**2 / (4*n**2-1), (n, 1, b))
+    B = Product((2*n)*(2*n)/(2*n-1)/(2*n+1), (n, 1, b))
+    R = 2**(-1-2*b)*factorial_(b)**2*4**b*pi/(factorial_(-Rational(1,2)+b)*factorial_(Rational(1,2)+b))
+    assert A == R
+    assert B == R
+    # This one should eventually also be doable (Euler's product formula for sin)
+    # assert Product(1+x/n**2, (n, 1, b)) == ...
