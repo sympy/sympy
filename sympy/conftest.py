6a67bfa7ea16141d01c49df78029ed01c8d8f7cd
ondrej.certik <devnull@localhost>
2007-09-06 08:30:05 +0000

    Updated documentation in conftest.py

--- sympy/conftest.py
+++ sympy/conftest.py
@@ -1,4 +1,8 @@
+# The py library is part of the "py.test" testing suite (python-codespeak-lib on
+# Debian), see http://codespeak.net/py/
+
 import py
+
 #this makes py.test put sympy directory into the sys.path, so that we can
 #"import sympy" from tests nicely
 rootdir = py.magic.autopath().dirpath()
