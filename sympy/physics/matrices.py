c2f3936c17216c25452ae228d9cb468d2cb984ec
kirill.smelkov <devnull@localhost>
2007-09-11 17:59:02 +0000

    Forgot to add files (Oh, those quilt/svn issues...)

---
+++ sympy/physics/matrices.py
@@ -0,0 +1,96 @@
+"""Known matrices related to physics"""
+
+from sympy import Matrix, I
+
+def msigma(i):
+    """Returns a Pauli matrix sigma_i. i=1,2,3
+    See also:
+    http://en.wikipedia.org/wiki/Pauli_matrices
+    """
+    if i==1:
+        mat=( (
+            (0, 1),
+            (1, 0)
+            ) )
+    elif i==2:
+        mat=( (
+            (0, -I),
+            (I, 0)
+            ) )
+    elif i==3:
+        mat=( (
+            (1, 0),
+            (0, -1)
+            ) )
+    else:
+        raise "Invalid Pauli index"
+    return Matrix(mat)
+
+def mgamma(mu,lower=False):
+    """Returns a Dirac gamma matrix gamma^mu in the standard
+    (Dirac) representation.
+
+    If you want gamma_mu, use gamma(mu, True).
+
+    We use a convention:
+
+    gamma^5 = I * gamma^0 * gamma^1 * gamma^2 * gamma^3
+    gamma_5 = I * gamma_0 * gamma_1 * gamma_2 * gamma_3 = - gamma^5
+
+    See also:
+
+    http://en.wikipedia.org/wiki/Gamma_matrices
+
+    """
+    if not mu in [0,1,2,3,5]:
+        raise "Invalid Dirac index"
+    if mu == 0:
+        mat = (
+                (1,0,0,0),
+                (0,1,0,0),
+                (0,0,-1,0),
+                (0,0,0,-1)
+                )
+    elif mu == 1:
+        mat = (
+                (0,0,0,1),
+                (0,0,1,0),
+                (0,-1,0,0),
+                (-1,0,0,0)
+                )
+    elif mu == 2:
+        mat = (
+                (0,0,0,-I),
+                (0,0,I,0),
+                (0,I,0,0),
+                (-I,0,0,0)
+                )
+    elif mu == 3:
+        mat = (
+                (0,0,1,0),
+                (0,0,0,-1),
+                (-1,0,0,0),
+                (0,1,0,0)
+                )
+    elif mu == 5:
+        mat = (
+                (0,0,1,0),
+                (0,0,0,1),
+                (1,0,0,0),
+                (0,1,0,0)
+                )
+    m= Matrix(mat)
+    if lower:
+        if mu in [1,2,3,5]:
+            m = - m
+    return m
+
+#Minkowski tensor using the convention (+,-,-,-) used in the Quantum Field
+#Theory
+minkowski_tensor = Matrix( (
+    (1,0,0,0),
+    (0,-1,0,0),
+    (0,0,-1,0),
+    (0,0,0,-1)
+    ))
+
---
+++ sympy/physics/paulialgebra.py
@@ -0,0 +1,65 @@
+from sympy import Symbol,I,Integer
+
+"""
+This module implements Pauli algebra by subclassing Symbol. Only aglebraic
+properties of Pauli matrices are used (we don't use the Matrix class).
+
+See the documentation to the class Pauli for examples.
+
+See also:
+    http://en.wikipedia.org/wiki/Pauli_matrices
+"""
+
+def delta(i,j):
+    if i==j:
+        return 1
+    else:
+        return 0
+
+def epsilon(i,j,k):
+    if (i,j,k) in [(1,2,3), (2,3,1), (3,1,2)]:
+        return 1
+    elif (i,j,k) in [(1,3,2), (3,2,1), (2,1,3)]:
+        return -1
+    else:
+        return 0
+
+class Pauli(Symbol):
+    """
+    >>> from sympy import *
+    >>> Pauli(1)
+    sigma1
+    >>> Pauli(1)*Pauli(2)
+    I*sigma3
+    >>> Pauli(1)*Pauli(1)
+    1
+    >>> Pauli(3)**4
+    1
+    >>> Pauli(1)*Pauli(2)*Pauli(3)
+    I
+
+    """
+
+    def __new__(cls, i):
+        if not i in [1,2,3]:
+            raise "Invalid Pauli index"
+        obj = Symbol.__new__(cls, "sigma%d"%i, commutative=False)
+        obj.i=i
+        return obj
+
+    # FIXME don't work for -I*Pauli(2)*Pauli(3)
+    def __mul__(self, other):
+        if isinstance(other, Pauli):
+            j=self.i
+            k=other.i
+            return delta(j,k) \
+                +I*epsilon(j,k,1)*Pauli(1) \
+                +I*epsilon(j,k,2)*Pauli(2) \
+                +I*epsilon(j,k,3)*Pauli(3)
+        return super(Pauli, self).__mul__(other)
+
+    def _eval_power(b, e):
+        if isinstance(e, Integer) and e.is_positive:
+            return super(Pauli, b).__pow__(int(e) % 2)
+
+
