77cca226c6d382db47cc68cf5dcd4728e8a6f6b7
pearu.peterson <devnull@localhost>
2007-09-19 14:44:04 +0000

    Impl dummy support to Symbol in sandbox.core

--- sympy/sandbox/core/symbol.py
+++ sympy/sandbox/core/symbol.py
@@ -1,13 +1,40 @@
 
+from utils import memoizer_immutable_args, memoizer_Symbol_new
 from basic import Atom
 from methods import ArithMeths, RelationalMeths
 
 class Symbol(ArithMeths, RelationalMeths, Atom, str):
 
-    def __new__(cls, name, **options):
+    """ Represents a symbol.
+
+    Symbol('x', dummy=True) returns a unique Symbol instance.
+    """
+
+    _dummy_count = 0
+    is_dummy = False
+    
+    @memoizer_Symbol_new
+    def __new__(cls, name, dummy=False, **options):
+        # when changing the Symbol signature then change memoizer_Symbol_new
+        # accordingly
         assert isinstance(name, str), `name`
-        assert not options, `options`
-        return str.__new__(cls, name)
+        obj = str.__new__(cls, name)
+        obj.is_dummy = dummy
+        if dummy:
+            Symbol._dummy_count += 1
+            obj.dummy_index = Symbol._dummy_count
+        return obj
 
     def torepr(self):
+        if self.is_dummy:
+            return '%s(%r, dummy=True)' % (self.__class__.__name__, str(self))
         return '%s(%r)' % (self.__class__.__name__, str(self))
+
+    def compare(self, other):
+        if self is other: return 0
+        c = cmp(self.__class__, other.__class__)
+        if c: return c
+        #
+        if self.is_dummy or other.is_dummy:
+            return cmp(id(self), id(other))
+        return str.__cmp__(self, other)
--- sympy/sandbox/core/utils.py
+++ sympy/sandbox/core/utils.py
@@ -1,22 +1,36 @@
 all_caches = {}
 
 def memoizer_immutable_args(name):
-  def make_memoized(func):
+    def make_memoized(func):
+        func._cache_it_cache = func_cache_it_cache = {}
+        def wrapper(*args):
+            try:
+                return func_cache_it_cache[args]
+            except KeyError:
+                pass
+            except TypeError, msg:
+                if 'dict objects are unhashable'==str(msg):
+                    return func(*args)
+                raise
+            func_cache_it_cache[args] = r = func(*args)
+            return r
+        all_caches[name] = func_cache_it_cache
+        return wrapper
+    return make_memoized
+
+def memoizer_Symbol_new(func):
     func._cache_it_cache = func_cache_it_cache = {}
-    def wrapper(*args):
+    def wrapper(cls, name, dummy=False, **options):
+        if dummy:
+            return func(cls, name, dummy=dummy, **options)
         try:
-            return func_cache_it_cache[args]
+            return func_cache_it_cache[name]
         except KeyError:
             pass
-        except TypeError, msg:
-            if 'dict objects are unhashable'==str(msg):
-                return func(*args)
-            raise
-        func_cache_it_cache[args] = r = func(*args)
+        func_cache_it_cache[name] = r = func(cls, name, dummy=dummy, **options)
         return r
-    all_caches[name] = func_cache_it_cache
+    all_caches['Symbol.__new__'] = func_cache_it_cache
     return wrapper
-  return make_memoized
 
 def clear_cache():
     """Clear all cached objects."""
