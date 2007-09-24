f18d020508b0505112b7a5e2dd8dafba7ff00e27
ondrej.certik <devnull@localhost>
2007-09-24 19:52:03 +0000

    polishing trigonometric.py imports

--- sympy/functions/elementary/trigonometric.py
+++ sympy/functions/elementary/trigonometric.py
@@ -1,6 +1,6 @@
 
 from sympy.core.basic import Basic, S, cache_it, cache_it_immutable
-from sympy.core.function import DefinedFunction, Apply, Lambda, SingleValuedFunction
+from sympy.core.function import Lambda, SingleValuedFunction
 
 ###############################################################################
 ########################## TRIGONOMETRIC FUNCTIONS ############################
