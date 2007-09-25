b10af2e4353e3c9353a730a2791dc48f6b30b06b
kirill.smelkov <devnull@localhost>
2007-09-25 10:35:04 +0000

    [1/2] lets use XFAIL/XPASS in tests
    It seems we continue breaking things so I have to take immediate action :)
    
    This patch patches py.test on the fly and adds 2 new outcome types to a test
    result: XFAIL and XPASS
    
    XFAIL means 'this testcase fails, but we know that.'
    XPASS means 'this testcase is known to fail but unexpectedly passed.'
    
    
    rationale: It is *bad* to just comment out tests when we change code.
    rationale: Even when working on design, if you don't want to solve every
    rationale: minor problem please mark tests as XFAIL
    
    rationale: py.test lacks this functionality and the reason I do it here is
    rationale: that we'll don't need to ask users to upgrade their py.test

---
+++ sympy/utilities/pytest.py
@@ -0,0 +1,85 @@
+"""py.test hacks to support XFAIL/XPASS"""
+
+# XXX this should be integrated into py.tes
+# XXX but we can't force everyone to install py-lib trunk
+
+# tested with py-lib 0.9.0
+from py.__.test.outcome import Outcome, Passed, Failed, Skipped
+from py.__.test.terminal.terminal import TerminalSession
+from time import time as now
+
+__all__ = ['XFAIL']
+
+class XFail(Outcome):
+    pass
+
+class XPass(Outcome):
+    pass
+
+TerminalSession.typemap[XFail] = 'f'
+TerminalSession.typemap[XPass] = 'X'
+
+TerminalSession.namemap[XFail] = 'XFAIL'
+TerminalSession.namemap[XPass] = '*** XPASS ***'
+
+
+def footer(self, colitems):
+    super(TerminalSession, self).footer(colitems) 
+    self.endtime = now()
+    self.out.line() 
+    self.skippedreasons()
+    self.failures()
+    self.xpasses()
+    self.summaryline()
+
+
+def xpasses(self):
+    texts = {}
+    for colitem, outcome in self.getitemoutcomepairs(XPass):
+        raisingtb = self.getlastvisible(outcome.excinfo.traceback) 
+        fn = raisingtb.frame.code.path
+        lineno = raisingtb.lineno
+        #d = texts.setdefault(outcome.excinfo.exconly(), {})
+        d = texts.setdefault(outcome.msg, {})
+        d[(fn,lineno)] = outcome 
+
+    if texts:
+        self.out.line()
+        self.out.sep('_', '*** XPASS ***')
+        for text, dict in texts.items():
+            #for (fn, lineno), outcome in dict.items(): 
+            #    self.out.line('Skipped in %s:%d' %(fn, lineno+1))
+            #self.out.line("reason: %s" % text) 
+            self.out.line("%s" % text) 
+            self.out.line()
+
+def summaryline(self): 
+    outlist = []
+    sum = 0
+    for typ in Passed, XPass, XFail, Failed, Skipped:
+        l = self.getitemoutcomepairs(typ)
+        if l:
+            outlist.append('%d %s' % (len(l), typ.__name__.lower()))
+        sum += len(l)
+    elapsed = self.endtime-self.starttime
+    status = "%s" % ", ".join(outlist)
+    self.out.sep('=', 'tests finished: %s in %4.2f seconds' %
+                     (status, elapsed))
+
+TerminalSession.footer  = footer
+TerminalSession.xpasses = xpasses
+TerminalSession.summaryline = summaryline
+
+def XFAIL(func):
+    """XFAIL decorator"""
+    def func_wrapper():
+        try:
+            func()
+        except:
+            raise XFail('XFAIL: %s' % func.func_name)
+        else:
+            raise XPass('XPASS: %s' % func.func_name)
+
+    return func_wrapper
+
+
