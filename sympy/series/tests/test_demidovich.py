697e4be14246340d96864e73168ff6ee0bc6a8e3
ondrej.certik <devnull@localhost>
2007-10-01 15:34:04 +0000

    tests in demidovich refactored

--- sympy/series/tests/test_demidovich.py
+++ sympy/series/tests/test_demidovich.py
@@ -1,4 +1,4 @@
-from sympy import *
+from sympy import limit, Symbol, oo, sqrt, Rational, log, exp, cos, sin, tan
 from sympy.utilities.pytest import XFAIL
 
 """
@@ -8,8 +8,6 @@ problemas resueltos, Ed. URSS"
 """
 
 x = Symbol("x", real=True, unbounded=True)
-h = Symbol("h")
-a = Symbol('a')
 
 def test_leadterm():
     assert (3+2*x**(log(3)/log(2)-1)).leadterm(x)==(3,0)
@@ -21,7 +19,6 @@ def sqrt4(x):
     return x**Rational(1,4)
 
 def test_Limits_simple_0():
-    # XXX recursion depth exceeded
     assert limit((2**(x+1)+3**(x+1))/(2**x+3**x),x,oo)==3  #175
 
 def test_Limits_simple_1():
@@ -42,11 +39,13 @@ def test_Limits_simple_2():
     assert limit(sqrt(x)/sqrt(x+sqrt(x+sqrt(x))),x,oo)==1  #190
 
 @XFAIL
-def test_Limits_simple_3():
-    # XXX Wrong limit
+def test_Limits_simple_3a():
+    a = Symbol('a', real=True)
     assert limit((x**2-(a+1)*x+a)/(x**3-a**3),x,a)==(a-1)/(3*a**2)  #196
+
+def test_Limits_simple_3b():
+    h = Symbol("h")
     assert limit(((x+h)**3-x**3)/h,h,0)==3*x**2  #197
-    # XXX Wrong limit
     assert limit((1/(1-x)-3/(1-x**3)),x,1)==-1  #198
     assert limit((sqrt(1+x)-1)/(sqrt3(1+x)-1),x,0)==Rational(3)/2  #Primer 4
     assert limit((sqrt(x)-1)/(x-1),x,1)==Rational(1)/2  #199
@@ -54,55 +53,58 @@ def test_Limits_simple_3():
     assert limit((sqrt3(x)-1)/(sqrt4(x)-1),x,1)==Rational(4)/3  #201
     assert limit((sqrt3(x**2)-2*sqrt3(x)+1)/(x-1)**2,x,1)==Rational(1)/9  #202
 
-@XFAIL
-def test_Limits_simple_4():
+def test_Limits_simple_4a():
+    a = Symbol('a', real=True)
     assert limit((sqrt(x)-sqrt(a))/(x-a),x,a)==1/(2*sqrt(a))  #Primer 5
     assert limit((sqrt(x)-1)/(sqrt3(x)-1),x,1)==Rational(3)/2  #205
     assert limit((sqrt(1+x)-sqrt(1-x))/x,x,0)==1  #207
     assert limit(sqrt(x**2-5*x+6)-x,x,oo)==-Rational(5)/2  #213
     assert limit(x*(sqrt(x**2+1)-x),x,oo)==Rational(1)/2  #214
-    # XXX Wrong limit
+
+@XFAIL
+def test_Limits_simple_4b():
     assert limit(x-sqrt3(x**3-1),x,oo)==0  #215
+
+def test_Limits_simple_4c():
     assert limit(log(1+exp(x))/x,x,-oo)==0  #267a
     assert limit(log(1+exp(x))/x,x,oo)==1  #267b
 
 @XFAIL
-def test_f1():
-    m = Symbol("m")
-    n = Symbol("n")
+def test_f1a():
+    h = Symbol("h")
     assert limit(sin(x)/x,x,2) == sin(2)/2 #216a
     assert limit(sin(x)/x,x,oo) == 0 #216b
-    assert limit(sin(3*x)/x,x,0) == 3 #217
-    # XXX recursion depth exceeded
     assert limit(sin(5*x)/sin(2*x),x,0) == Rational(5)/2 #218
     assert limit(sin(pi*x)/sin(3*pi*x),x,0) == Rational(1)/3 #219
     assert limit(x*sin(pi/x),x,oo) == pi #220
-    assert limit((1-cos(x))/x**2,x,0) == Rational(1,2) #221
     assert limit((sin(x)-sin(a))/(x-a),x,a) == cos(a) #222, *176
-    # XXX Wrong limit
     assert limit((cos(x)-cos(a))/(x-a),x,a) == -sin(a) #223
     assert limit((sin(x+h)-sin(x))/h,h,0) == cos(x) #225
     assert limit(x*sin(1/x),x,0) == 0 #227a
-    assert limit(x*sin(1/x),x,oo) == 1 #227b
-    # XXX Wrong limit
-    assert limit((cos(m*x)-cos(n*x))/x**2,x,0) == ((n**2-m**2)/2) #232
-    # XXX Errors
-    assert limit((tan(x)-sin(x))/x**3,x,0) == Rational(1,2) #233
     assert limit((x-sin(2*x))/(x+sin(3*x)),x,0) == -Rational(1,4) #237
-    # XXX recursion depth exceeded
     assert limit((1-sqrt(cos(x)))/x**2,x,0) == Rational(1,4) #239
+    assert limit((sin(2*x)/x)**(1+x),x,0) == 2 #Primer 7
+    assert limit(((x-1)/(x+1))**x,x,oo) == exp(-2) #Primer 9
+
+def test_f1b():
+    m = Symbol("m")
+    n = Symbol("n")
+    h = Symbol("h")
+    assert limit(sin(3*x)/x,x,0) == 3 #217
+    assert limit((1-cos(x))/x**2,x,0) == Rational(1,2) #221
+    assert limit(x*sin(1/x),x,oo) == 1 #227b
+    assert limit((cos(m*x)-cos(n*x))/x**2,x,0) == ((n**2-m**2)/2) #232
+    assert limit((tan(x)-sin(x))/x**3,x,0) == Rational(1,2) #233
     assert limit((sqrt(1+sin(x))-sqrt(1-sin(x)))/x,x,0) == 1 #240
 
-    assert limit((sin(2*x)/x)**(1+x),x,0) == 2 #Primer 7
     assert limit(((x+1)/(2*x+1))**(x**2),x,oo) == 0 #Primer 8
-    # XXX Wrong limit
-    assert limit(((x-1)/(x+1))**x,x,oo) == exp(-2) #Primer 9
     assert limit((1+h/x)**x,x,oo) == exp(h) #Primer 9
 
 @XFAIL
 def test_f2():
-    a = Symbol('a', real=True)
-    # XXX Recursion depth exceeded
     assert limit((sqrt(cos(x))-sqrt3(cos(x)))/(sin(x)**2),x,0) == -Rational(1,12) #*184
-    # XXX Requires inverse trig functions
+
+@XFAIL
+def test_f3():
+    a = Symbol('a', real=True)
     assert Limit(asin(a*x)/x, x, 0) == a
