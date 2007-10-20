99a857f5e03bd161724956f8f56c060911c72ce0
ondrej.certik <devnull@localhost>
2007-10-20 22:10:01 +0000

    sympy version updated

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -10,7 +10,7 @@ See the webpage for more information and documentation:
     http://code.google.com/p/sympy/
 """
 
-__version__ = "0.5.4-svn"
+__version__ = "0.5.5-svn"
 
 from sympy.core import *
 
