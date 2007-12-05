a4dd9ffe0a5921902a020833696e8e71b856e42e
Ondrej Certik <ondrej@certik.cz>
2007-12-06 00:40:06 +0100

    XFAIL the gtk test, so that sympy tests pass even without python-libxml2

--- sympy/printing/tests/test_gtk.py
+++ sympy/printing/tests/test_gtk.py
@@ -1,5 +1,9 @@
 from sympy import print_gtk, sin
+from sympy.utilities.pytest import XFAIL
 
+# this test fails if python-libxml2 isn't installed. We don't want to depend on
+# anything with SymPy
+@XFAIL
 def test1():
     from sympy.abc import x
     print_gtk(x**2, start_viewer=False)
