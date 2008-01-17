771573f990dc4bbbeaed96d36696a9e2be2a7fc7
Andrej "qwp0" Tokarčík <androsis@gmail.com>
2008-01-17 01:12:14 +0100

    Python printing implemented (#81)

---
+++ sympy/printing/python.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+from sympy.core import Basic
+from sympy import oo
+from printer import Printer
+
+class PythonPrinter(Printer):
+    """A printer which converts an expression into its Python interpretation."""
+
+    def __init__(self):
+        Printer.__init__(self)
+        self.symbols = []
+        self.functions = []
+
+    # Functions for printing exact type of expression
+    def _print_Add(self, expr):
+        for value in expr.args:
+            self._print(value)
+
+    def _print_Derivative(self, expr):
+        for arg in expr.args:
+            self._print(arg)
+
+    # procedure (!) for defining functions which have to be defined in print_python()
+    def _print_Function(self, expr, exp=None):
+        func = expr.func.__name__
+        args = ''.join([self._print(arg) for arg in expr.args])
+        import sympy
+        if not hasattr(sympy, func) and func not in self.functions:
+            self.functions.append(func)
+
+    def _print_Integral(self, expr):
+        for arg in expr._args:
+            self._print(arg)
+
+    def _print_Mul(self, expr):
+        terms = expr.as_coeff_terms()[1]
+        for term in terms:
+            self._print(term)
+
+    def _print_Pow(self, expr):
+        if isinstance(expr.exp, Basic.Rational) and expr.exp.q == 2:
+            self._print(expr.base)
+        else:
+            if isinstance(expr.base, Basic.Function) \
+              or isinstance(expr.base, Basic.Symbol):
+                self._print(expr.base)
+            if isinstance(expr.base, Basic.Function) \
+              or isinstance(expr.base, Basic.Symbol):
+                self._print(expr.exp)
+
+    def _print_Rational(self, expr):
+        if expr == oo:
+            return oo
+        if expr == 0:
+            return 0
+        return 'Rational(' + self._str(expr.p) + ', ' + self._str(expr.q) + ')'
+
+    def _print_Relational(self, expr):
+        self._print(expr.lhs) + self._print(expr.rhs)
+
+    # procedure (!) for defining symbols which have be defined in print_python()
+    def _print_Symbol(self, expr):
+        symbol = self._str(expr)
+        if symbol not in self.symbols:
+            self.symbols.append(symbol)
+
+    # Main function for printing
+    def doprint(self, expr):
+        return Printer.doprint(self, expr)
+
+    # Function definitions for clearer output
+    # (it's possible they don't have to be defined)
+    def _print_int(self, expr):
+        pass
+
+    def _print_module(self, expr):
+        raise Exception, 'Modules in the expression are unacceptable'
+
+    def _print_str(self, expr):
+        raise Exception, 'Strings in the expression are unacceptable'
+
+    def emptyPrinter(self, expr):
+        pass
+
+def python(expr):
+    """Return Python interpretation of passed expression
+    (can be passed to the exec() function without any modifications)"""
+
+    printer = PythonPrinter()
+    expr = printer.doprint(expr)
+
+    result = ''
+    # Returning found symbols and functions
+    for symbol in printer.symbols:
+        result += symbol + ' = Symbol(\'' + symbol + '\')\n'
+    for function in printer.functions:
+        result += function + ' = Function(\'' + function + '\')\n'
+
+    result += 'e = ' + printer._str(expr)
+    return result
+
+def print_python(expr):
+    """Print output of python() function"""
+    print python(expr)
---
+++ sympy/printing/tests/test_python.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+from sympy import Symbol, symbols, oo, limit, Rational, Integral, Derivative
+from sympy import log, exp, sqrt, pi, Function, sin
+
+from sympy.printing.python import python
+
+x, y = symbols('xy')
+th  = Symbol('theta')
+ph  = Symbol('phi')
+
+def test_python_basic():
+    # Simple numbers/symbols
+    assert python(-Rational(1)/2) == "e = Rational(-1, 2)"
+    assert python(-Rational(13)/22) == "e = Rational(-13, 22)"
+    assert python(oo) == "e = oo"
+
+    # Powers
+    assert python((x**2)) == "x = Symbol(\'x\')\ne = x**2"
+    assert python(1/x) == "x = Symbol('x')\ne = 1/x"
+    assert python(y*x**-2) == "y = Symbol('y')\nx = Symbol('x')\ne = y*x**(-2)"
+    assert python(x**Rational(-5, 2)) == "x = Symbol('x')\ne = x**(-5/2)"
+
+    # Sums of terms
+    assert python((x**2 + x + 1)) == "x = Symbol('x')\ne = 1 + x + x**2" 
+    assert python(1-x) == "x = Symbol('x')\ne = 1 - x"
+    assert python(1-2*x) == "x = Symbol('x')\ne = 1 - 2*x"
+    assert python(1-Rational(3,2)*y/x) == "y = Symbol('y')\nx = Symbol('x')\ne = 1 - 3/2*y/x"
+
+    # Multiplication
+    assert python(x/y) == "x = Symbol('x')\ny = Symbol('y')\ne = x/y"
+    assert python(-x/y) == "x = Symbol('x')\ny = Symbol('y')\ne = -x/y"
+    assert python((x+2)/y) == "y = Symbol('y')\nx = Symbol('x')\ne = 1/y*(2 + x)"
+    assert python((1+x)*y) == "y = Symbol('y')\nx = Symbol('x')\ne = y*(1 + x)"
+
+    # Check for proper placement of negative sign
+    assert python(-5*x/(x+10)) == "x = Symbol('x')\ne = -5*x/(10 + x)"
+    assert python(1 - Rational(3,2)*(x+1)) == "x = Symbol('x')\ne = (-1/2) - 3/2*x"
+
+def test_python_relational():
+    assert python(x == y) == "x = Symbol('x')\ny = Symbol('y')\ne = x == y"
+    assert python(x <= y) == "x = Symbol('x')\ny = Symbol('y')\ne = x <= y"
+    assert python(x > y) == "y = Symbol('y')\nx = Symbol('x')\ne = y < x"
+    assert python(x/(y+1) != y**2) == "x = Symbol('x')\ny = Symbol('y')\ne = x/(1 + y) != y**2"
+
+def test_python_functions():
+    # Simple
+    assert python((2*x + exp(x))) in "x = Symbol('x')\ne = 2*x + exp(x)"
+    assert python(sqrt(2)) == 'e = 2**(1/2)'
+    assert python(sqrt(2+pi)) == 'e = (2 + pi)**(1/2)'
+    assert python(abs(x)) == "x = Symbol('x')\ne = abs(x)"
+    assert python(abs(x/(x**2+1))) == "x = Symbol('x')\ne = abs(x/(1 + x**2))"
+
+    # Univariate/Multivariate functions
+    f = Function('f')
+    assert python(f(x)) == "x = Symbol('x')\nf = Function('f')\ne = f(x)"
+    assert python(f(x, y)) == "x = Symbol('x')\ny = Symbol('y')\nf = Function('f')\ne = f(x, y)"
+    assert python(f(x/(y+1), y)) == "x = Symbol('x')\ny = Symbol('y')\nf = Function('f')\ne = f(x/(1 + y), y)"
+
+    # Nesting of square roots
+    assert python(sqrt((sqrt(x+1))+1)) == "x = Symbol('x')\ne = (1 + (1 + x)**(1/2))**(1/2)"
+    # Function powers
+    assert python(sin(x)**2) == "x = Symbol('x')\ne = sin(x)**2"
+
+    # Conjugates
+    a, b = map(Symbol, 'ab')
+    #assert python( conjugate(a+b*I) ) == '_     _\na - I*b'
+    #assert python( conjugate(exp(a+b*I)) ) == ' _     _\n a - I*b\ne       '
+
+def test_python_derivatives():
+    # Simple
+    f_1 = Derivative(log(x), x, evaluate=False)
+    assert python(f_1) == "x = Symbol('x')\ne = D(log(x), x)"
+
+    f_2 = Derivative(log(x), x, evaluate=False) + x
+    assert python(f_2) == "x = Symbol('x')\ne = x + D(log(x), x)"
+
+    # Multiple symbols
+    f_3 = Derivative(log(x) + x**2, x, y, evaluate=False)
+    #assert python(f_3) == 
+
+    f_4 = Derivative(2*x*y, y, x, evaluate=False) + x**2
+    assert python(f_4) == "x = Symbol('x')\ny = Symbol('y')\ne = x**2 + D(2*x*y, y, x)"
+
+def test_python_integrals():
+    # Simple
+    f_1 = Integral(log(x), x)
+    assert python(f_1) == "x = Symbol('x')\ne = Integral(log(x), x)"
+
+    f_2 = Integral(x**2, x)
+    assert python(f_2) == "x = Symbol('x')\ne = Integral(x**2, x)"
+
+    # Double nesting of pow
+    f_3 = Integral(x**(2**x), x)
+    assert python(f_3) == "x = Symbol('x')\ne = Integral(x**(2**x), x)"
+
+    # Definite integrals
+    f_4 = Integral(x**2, (x,1,2))
+    assert python(f_4) == "x = Symbol('x')\ne = Integral(x**2, (x, (1, 2)))"
+
+    f_5 = Integral(x**2, (x,Rational(1,2),10))
+    assert python(f_5) == "x = Symbol('x')\ne = Integral(x**2, (x, (1/2, 10)))"
+
+    # Nested integrals
+    f_6 = Integral(x**2*y**2, x,y)
+    assert python(f_6) == "x = Symbol('x')\ny = Symbol('y')\ne = Integral(x**2*y**2, x, y)"
+
+
+# Not implemented yet
+#def test_python_matrix():
+#    p = python(Matrix([[x**2+1, 1], [y, x+y]]))
+#    s = ''
+#    assert p == s
+
+def test_python_limits():
+    assert python(limit(x, x, oo)) == 'e = oo'
+    assert python(limit(x**2, x, 0)) == 'e = 0'
