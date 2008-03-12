095e01ac6cbc7a46f64549ea23c3c4283ac02cd5
Kirill Smelkov <kirr@landau.phys.spbu.ru>
2008-03-12 09:36:31 +0300

    [5/5] __eq__/__ne__ returns True/False directly  (#135)
    Fix wrong test. Consider (even old behaviour):
    
    In [1]: Real('4') == Integer(4)
    Out[1]: True
    
    In [2]: x == x
    Out[2]: x = x
    
    In [3]: bool x == x
    Out[3]: True
    
    
    so if a==b and c==d then we expect a*c == b*d, right?
    
    <old behaviour>
    In [4]: Real('4') * x == Integer(4) * x
    Out[4]: 4*x = 4*x
    
    In [5]: bool _
    Out[5]: False
    
    This is at least surprising.
    
    <new behaviour>
    In [1]: Real('4') * x == Integer(4) * x
    Out[1]: True
    
    
    So we fix related test.

--- sympy/core/tests/test_complex.py
+++ sympy/core/tests/test_complex.py
@@ -84,7 +84,7 @@ def test_evalc():
 def test_pythoncomplex():
     x = Symbol("x")
     assert 4j*x == 4*x*I
-    assert 4j*x != 4.0*x*I
+    assert 4j*x == 4.0*x*I
     assert 4.1j*x != 4*x*I
 
 def test_rootcomplex():
