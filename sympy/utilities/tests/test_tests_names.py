131d2de6070e6023cdd21d737d9c1e0010d919c8
Ondrej Certik <ondrej@certik.cz>
2008-04-26 22:27:44 +0200

    A test for duplicate tests added.
    This is absolutely crucial, because of a bug in py.test that makes py.test
    ignoring all test files that have duplicate names.
    
    5 test files with duplicate names were discovered by this test and those will
    be renamed in the next patch.

---
+++ sympy/utilities/tests/test_tests_names.py
@@ -0,0 +1,21 @@
+from os import walk, sep, chdir, pardir
+from os.path import split, join, abspath, basename
+from glob import glob
+
+def test_no_duplicate_names():
+    message = "Test '%s' has a duplicate name, py.test will not run it!"
+    base_path = split(__file__)[0]
+    base_path += sep + pardir + sep + pardir # go to sympy/
+    base_path = abspath(base_path)
+    tests = {}
+    for root, dirs, files in walk(base_path):
+        for fname in glob(join(root, "test_*.py")):
+            tname = basename(fname)
+            if tname in tests:
+                tests[tname].append(fname)
+            else:
+                tests[tname] = [fname]
+    for fname in tests:
+        if len(tests[fname]) > 1:
+            print tests[fname]
+            assert False, message % (fname)
