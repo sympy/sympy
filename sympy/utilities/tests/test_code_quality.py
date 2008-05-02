f7bad436665efc1abe54fc0fb52e911ea1dfab4a
Ondrej Certik <ondrej@certik.cz>
2008-05-02 17:08:32 +0200

    Checks for not using tabs in indentation and also improves the line end check.
    Now we are checking that no line ends either by spaces or tabs and also that
    tabs are not used for indentation (they could be used in the middle of the line
    -- not sure if there is such a use case though).

--- sympy/utilities/tests/test_code_quality.py
+++ sympy/utilities/tests/test_code_quality.py
@@ -12,8 +12,22 @@ EXCLUDE = set([
     "%(sep)sthirdparty%(sep)s" % sepd,
 ])
 
-def test_no_trailing_whitespace():
-    message = "File contains trailing whitespace: %s, line %s."
+def get_whitespace(s):
+    """Returns all whitespace at the beginning of the line."""
+    for i in range(len(s)):
+        if not s[i].isspace():
+            return s[:i]
+    return ""
+
+
+def test_no_trailing_whitespace_and_no_tabs():
+    """
+    This test tests all files in sympy and makes sure no lines contains a
+    trailing whitespace at the end, and also that no line uses tabs instead of
+    spaces as the indentation.
+    """
+    message_space = "File contains trailing whitespace: %s, line %s."
+    message_tabs = "File contains tabs instead of spaces: %s, line %s."
     base_path = split(__file__)[0]
     base_path += sep + pardir + sep + pardir # go to sympy/
     base_path = abspath(base_path)
@@ -24,7 +38,11 @@ def test_no_trailing_whitespace():
             file = open(fname, "r")
             try:
                 for idx, line in enumerate(file):
-                    if line.endswith(" \n"):
-                        assert False, message % (fname, idx+1)
+                    #if line.endswith(" \n"):
+                    if line.rstrip()+"\n" != line and line.rstrip() != line:
+                        assert False, message_space % (fname, idx+1)
+                    w = get_whitespace(line)
+                    if w.expandtabs() != w:
+                        assert False, message_tabs % (fname, idx+1)
             finally:
                 file.close()
