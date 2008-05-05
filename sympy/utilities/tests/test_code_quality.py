c0ab9a34a955d86256b29f8f5d0733efc6c18907
Sebastian Krämer <basti.kr@gmail.com>
2008-05-05 23:08:20 +0200

    test_code_quality now checks examples and also looks for \r\n file endings.

--- sympy/utilities/tests/test_code_quality.py
+++ sympy/utilities/tests/test_code_quality.py
@@ -1,5 +1,5 @@
 from os import walk, sep, chdir, pardir
-from os.path import split, join, abspath
+from os.path import split, join, abspath, exists
 from glob import glob
 
 # System path separator (usually slash or backslash)
@@ -12,6 +12,11 @@ EXCLUDE = set([
     "%(sep)sthirdparty%(sep)s" % sepd,
 ])
 
+# Error messages
+message_space = "File contains trailing whitespace: %s, line %s."
+message_tabs = "File contains tabs instead of spaces: %s, line %s."
+message_carriage = "File contains carriage returns at end of line: %s, line %s"
+
 def get_whitespace(s):
     """Returns all whitespace at the beginning of the line."""
     for i in range(len(s)):
@@ -19,18 +24,11 @@ def get_whitespace(s):
             return s[:i]
     return ""
 
-
-def test_no_trailing_whitespace_and_no_tabs():
+def check_directory_tree(base_path):
     """
-    This test tests all files in sympy and makes sure no lines contains a
-    trailing whitespace at the end, and also that no line uses tabs instead of
-    spaces as the indentation.
+    Checks all files in the directory tree (with base_path as starting point)
+    for bad coding style.
     """
-    message_space = "File contains trailing whitespace: %s, line %s."
-    message_tabs = "File contains tabs instead of spaces: %s, line %s."
-    base_path = split(__file__)[0]
-    base_path += sep + pardir + sep + pardir # go to sympy/
-    base_path = abspath(base_path)
     for root, dirs, files in walk(base_path):
         for fname in glob(join(root, "*.py")):
             if filter(lambda ex: ex in fname, EXCLUDE):
@@ -41,8 +39,28 @@ def test_no_trailing_whitespace_and_no_tabs():
                     #if line.endswith(" \n"):
                     if line.rstrip()+"\n" != line and line.rstrip() != line:
                         assert False, message_space % (fname, idx+1)
+                    if "\r\n" in line:
+                        assert False, message_carriagereturn % (fname, idx+1)
                     w = get_whitespace(line)
                     if w.expandtabs() != w:
                         assert False, message_tabs % (fname, idx+1)
             finally:
                 file.close()
+
+
+def test_no_trailing_whitespace_and_no_tabs():
+    """
+    This test tests all files in sympy and makes sure no lines contains a
+    trailing whitespace at the end, and also that no line uses tabs instead of
+    spaces as the indentation.
+    """
+    path = split(__file__)[0]
+    path = path + sep + pardir + sep + pardir # go to sympy/
+    sympy_path = abspath(path)
+    examples_path = abspath(join(path + sep + pardir, "examples"))
+    paths = (sympy_path, examples_path)
+    for p in paths:
+        assert exists(p)
+        check_directory_tree(p)
+
+test_no_trailing_whitespace_and_no_tabs()
