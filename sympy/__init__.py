a13bbf9b6342d653ea4af6167f90d49dd5f98d93
Ondrej Certik <ondrej@certik.cz>
2008-05-24 18:59:32 +0200

    0.5.15

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -10,7 +10,7 @@ See the webpage for more information and documentation:
     http://code.google.com/p/sympy/
 """
 
-__version__ = "0.5.14-hg"
+__version__ = "0.5.15"
 
 #put path to pyglet into the search path, so that it can be imported
 import os.path
