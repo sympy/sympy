f008b85b74e2ff95dd956fe3817c0b93101dfb57
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-05-25 13:23:46 +0200

    Improves the performance of npartitions.
    By removing overhead in the inner sum and not grossly overestimating the
    required precision. Avoids use of mpmath's precision context in npartitions.
    Tests added for P(10^4) and P(10^5).
    
    Before:
    
    n / P(n) mod 10^10 / time
    
    10^1 / 0000000042 / 0.0061 seconds
    10^2 / 0190569292 / 0.0064 seconds
    10^3 / 2149727991 / 0.029 seconds
    10^4 / 6916435144 / 0.48 seconds
    10^5 / 9421098519 / 15 seconds
    10^6 / 7104673818 / 16.7 minutes
    10^7 / ---------- / 40 hours (estimated)
    10^8 / ---------- / 450 days (estimated)
    
    After:
    
    n / P(n) mod 10^10 / time
    
    10^1 / 0000000042 / 0.0019 seconds
    10^2 / 0190569292 / 0.0019 seconds
    10^3 / 2149727991 / 0.005 seconds
    10^4 / 6916435144 / 0.031 seconds
    10^5 / 9421098519 / 0.34 seconds
    10^6 / 7104673818 / 7 seconds
    10^7 / 7677288980 / 3.1 minutes
    10^8 / 9836637702 / 55 minutes
    
    P(10^8) has 11132 digits.
    
    Still much slower than Sage or Mathematica, but a step in the right
    direction. The speed could improve a lot with some work, perhaps by a
    factor 10 or more over the tested range. E.g. when computing P(10^8),
    perhaps 3/4 of the terms are small enough to calculate with regular
    floats, and with tighter bounds perhaps 1/3 of the terms could be
    discarded entirely.

--- sympy/ntheory/partitions_.py
+++ sympy/ntheory/partitions_.py
@@ -1,67 +1,80 @@
+from sympy.thirdparty.mpmath.lib import (fzero, fadd, fsub, fmul, fsqrt,
+    fdiv, fpi, pi_fixed, from_man_exp, from_int, from_rational, cosh_sinh,
+    fone, fcos, fshift, ftwo, fhalf, bitcount, to_int, to_str)
+from sympy.core.numbers import gcd
+import math
 
-def npartitions(n):
+def A(n, j, prec):
+    """Compute the inner sum in the HRR formula."""
+    if j == 1:
+        return fone
+    s = fzero
+    pi = pi_fixed(prec)
+    for h in xrange(1, j):
+        if gcd(h,j) != 1:
+            continue
+        # & with mask to compute fractional part of fixed-point number
+        one = 1 << prec
+        onemask = one - 1
+        half = one >> 1
+        g = 0
+        if j >= 3:
+            for k in xrange(1, j):
+                t = h*k*one//j
+                if t > 0: frac = t & onemask
+                else:     frac = -((-t) & onemask)
+                g += k*(frac - half)
+        g = ((g - 2*h*n*one)*pi//j) >> prec
+        s = fadd(s, fcos(from_man_exp(g, -prec), prec), prec)
+    return s
+
+def D(n, j, prec, sq23pi, sqrt8):
+    """
+    Compute the sinh term in the outer sum of the HRR formula.
+    The constants sqrt(2/3*pi) and sqrt(8) must be precomputed.
+    """
+    j = from_int(j)
+    pi = fpi(prec)
+    a = fdiv(sq23pi, j, prec)
+    b = fsub(from_int(n), from_rational(1,24,prec), prec)
+    c = fsqrt(b, prec)
+    ch, sh = cosh_sinh(fmul(a,c), prec)
+    D = fdiv(fsqrt(j,prec), fmul(fmul(sqrt8,b),pi), prec)
+    E = fsub(fmul(a,ch), fdiv(sh,c,prec), prec)
+    return fmul(D, E)
+
+def npartitions(n, verbose=False):
     """
     Calculate the partition function P(n), i.e. the number of ways that
     n can be written as a sum of positive integers.
 
-    P(n) is computed using a straightforward implementation of the
-    Hardy-Ramanujan-Rademacher formula, described e.g. at
-    http://mathworld.wolfram.com/PartitionFunctionP.html
+    P(n) is computed using the Hardy-Ramanujan-Rademacher formula,
+    described e.g. at http://mathworld.wolfram.com/PartitionFunctionP.html
 
-    The speed is decent up to n = 10**5 or so. The function has
-    been tested to give the correct result for n = 10**6.
+    The correctness of this implementation has been tested for 10**n
+    up to n = 8.
     """
-
     n = int(n)
-    if n < 0:
-        return 0
-    if n <= 5:
-        return [1, 1, 2, 3, 5, 7][n]
-
-    from sympy.core.numbers import gcd
-    from sympy.thirdparty.mpmath import mp, mpf, pi, sqrt, exp, log, cos
-
-    def frac(x):
-        return x - int(x)
-
-    def D(n, j):
-        a = sqrt(mpf(2)/3) * pi / j
-        b = mpf(n) - mpf(1)/24
-        c = sqrt(b)
-        expa = exp(a*c)
-        iexpa = mpf(1)/expa
-        ch = (expa + iexpa)*0.5
-        sh = (expa - iexpa)*0.5
-        return sqrt(j) / (2*sqrt(2)*b*pi) * (a*ch-sh/c)
-
-    def A(n, j):
-        if j == 1:
-            return mpf(1)
-        s = mpf(0)
-        for h in xrange(1, j):
-            if gcd(h,j) == 1:
-                s += cos((g(h,j)-2*h*n)*pi/j)
-        return s
-
-    def g(h, j):
-        if j < 3:
-            return mpf(0)
-        s = mpf(0)
-        for k in xrange(1, j):
-            s += k*(frac(h*mpf(k)/j)-0.5)
-        return s
-
-    # estimate number of digits in p(n)
-    pdigits = int((pi*sqrt(2.0*n/3)-log(4*n))/log(10)+1)
-
-    dps, mp.dps = mp.dps, pdigits*1.1 + 10
-
-    s = mpf(0)
-
-    M = max(6, int(0.24*sqrt(n)+4))
+    if n < 0: return 0
+    if n <= 5: return [1, 1, 2, 3, 5, 7][n]
+    # Estimate number of bits in p(n). This formula could be tidied
+    pbits = int((math.pi*(2*n/3.)**0.5-math.log(4*n))/math.log(10)+1)*\
+        math.log(10,2)
+    prec = p = int(pbits*1.1 + 100)
+    s = fzero
+    M = max(6, int(0.24*n**0.5+4))
+    sq23pi = fmul(fsqrt(from_rational(2,3,p), p), fpi(p), p)
+    sqrt8 = fsqrt(from_int(8), p)
     for q in xrange(1, M):
-        s += A(n,q) * D(n,q)
-    p = int(s + 0.5)
-    mp.dps = dps
+        a = A(n,q,p)
+        d = D(n,q,p, sq23pi, sqrt8)
+        s = fadd(s, fmul(a, d), prec)
+        if verbose:
+            print "step", q, "of", M, to_str(a, 10), to_str(d, 10)
+        # On average, the terms decrease rapidly in magnitude. Dynamically
+        # reducing the precision greatly improves performance.
+        p = bitcount(abs(to_int(d))) + 50
+    np = to_int(fadd(s, fhalf, prec))
+    return int(np)
 
-    return p
+__all__ = ['npartitions']
--- sympy/ntheory/tests/test_ntheory.py
+++ sympy/ntheory/tests/test_ntheory.py
@@ -115,6 +115,8 @@ def test_partitions():
     assert npartitions(200) == 3972999029388
     assert npartitions(1000) == 24061467864032622473692149727991
     assert npartitions(2000) == 4720819175619413888601432406799959512200344166
+    assert npartitions(10000) % 10**10 == 6916435144
+    assert npartitions(100000) % 10**10 == 9421098519
 
 def test_residue():
     assert n_order(2,13)==12
