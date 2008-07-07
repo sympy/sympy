9671b11f25574d70942708a9540e1db3aabdf3c7
Ondrej Certik <ondrej@certik.cz>
2008-07-07 20:47:32 +0200

    Release number updated in docs.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -40,9 +40,9 @@ copyright = '2008, SymPy Development Team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.5.15'
+version = '0.6.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.15'
+release = '0.6.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
