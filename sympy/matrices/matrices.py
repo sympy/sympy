d329909f097e7bdfb5a98f0f3d44c49d429b6b51
Ondrej Certik <ondrej@certik.cz>
2008-07-10 17:01:38 +0200

    Fixes the doctests for the Matrix.tolist() method.

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -235,11 +235,11 @@ class Matrix(object):
         >>> from sympy import *
         >>> m=Matrix(3, 3, range(9))
         >>> m
-        [1, 2, 3]
-        [4, 5, 6]
-        [7, 8, 9]
+        [0, 1, 2]
+        [3, 4, 5]
+        [6, 7, 8]
         >>> m.tolist()
-        [[1,2,3],[4,5,6],[7,8,9]]
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
         """
         ret = [0]*self.lines
         for i in xrange(self.lines):
