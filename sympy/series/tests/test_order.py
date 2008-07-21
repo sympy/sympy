d500a8610a72059bba81407ece2b40c75a2763e7
Ondrej Certik <ondrej@certik.cz>
2008-07-21 21:23:54 +0200

    test_order.py: the XFAILing test updated and made run.

--- sympy/series/tests/test_order.py
+++ sympy/series/tests/test_order.py
@@ -93,13 +93,11 @@ def test_add_1():
     assert Order(exp(1/x)+x) == Order(exp(1/x))
     assert Order(exp(1/x)+1/x**20) == Order(exp(1/x))
 
-@XFAIL
 def test_ln_args():
-    #sometimes runs, sometimes fails, probably a caching bug:
-    assert Order(ln(2*x)).expr == ln(x) # ln(2*x) -> ln(2)+ln(x
-    assert Order(ln(y*x)).expr == ln(x)+ln(y) # ln(x*y) -> ln(x)+ln(y
-    assert Order(ln(x**3)).expr == ln(x) # ln(x**3) -> 3*ln(x
-    assert Order(ln(2*x)).expr == ln(x) # ln(2*x) -> ln(2)+ln(x
+    assert O(log(x)) + O(log(2*x)) == O(log(x))
+    assert O(log(x)) + O(log(x**3)) == O(log(x))
+    # This works alone, but fails here due to a caching bug
+    #assert O(log(x*y)) + O(log(x)+log(y)) == O(log(x*y))
 
 def test_multivar_0():
     assert Order(x*y).expr == x*y
