5fa1cdf1fb95b3768727c3a09bfe10769a439cc3
Sebastian Krämer <basti.kr@gmail.com>
2008-07-24 19:53:49 +0200

    Lets LambdaPrinter inherit from StrPrinter.
    Many methods become redundant and can be erased. Therefor the whole printer gets much simpler.
    (Some tests for the LambdaPrinter have to be changed since the implementation of precedences in StrPrinter makes many brackets needless.)
    
    
    Signed-off-by: Kirill Smelkov <kirr@landau.phys.spbu.ru>
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

--- sympy/printing/tests/test_lambdarepr.py
+++ sympy/printing/tests/test_lambdarepr.py
@@ -3,10 +3,10 @@ from sympy.printing.lambdarepr import lambdarepr
 x,y,z = symbols("xyz")
 
 def test_basic():
-    assert lambdarepr(x*y)=="(x*y)"
-    assert lambdarepr(x+y) in ["(y+x)", "(x+y)"]
-    assert lambdarepr(x**y)=="(x)**(y)"
+    assert lambdarepr(x*y)=="x*y"
+    assert lambdarepr(x+y) in ["y + x", "x + y"]
+    assert lambdarepr(x**y)=="x**y"
 
 def test_matrix():
     A = Matrix([[x,y],[y*x,z**2]])
-    assert lambdarepr(A)=="Matrix([[    x,        y],[(x*y), (z)**(2)]])"
+    assert lambdarepr(A)=="Matrix([[  x,    y],[x*y, z**2]])"
