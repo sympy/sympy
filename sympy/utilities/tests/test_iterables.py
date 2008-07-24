696acc01214dd31e639174d23ae1e9a0530c1400
Kirill Smelkov <kirr@landau.phys.spbu.ru>
2008-07-25 01:04:05 +0400

    robustify tests for postorder_traversal and preorder_traversal

--- sympy/utilities/tests/test_iterables.py
+++ sympy/utilities/tests/test_iterables.py
@@ -8,13 +8,15 @@ def test_postorder_traversal():
     expr = z+w*(x+y)
     expected1 = [z, w, y, x, x + y, w*(x + y), z + w*(x + y)]
     expected2 = [z, w, x, y, x + y, w*(x + y), z + w*(x + y)]
-    assert list(postorder_traversal(expr)) in [expected1, expected2]
+    expected3 = [w, y, x, x + y, w*(x + y), z, z + w*(x + y)]
+    assert list(postorder_traversal(expr)) in [expected1, expected2, expected3]
 
 
 def test_preorder_traversal():
     expr = z+w*(x+y)
     expected1 = [z + w*(x + y), z, w*(x + y), w, x + y, y, x]
     expected2 = [z + w*(x + y), z, w*(x + y), w, x + y, x, y]
-    assert list(preorder_traversal(expr)) in [expected1, expected2]
+    expected3 = [z + w*(x + y), w*(x + y), w, x + y, y, x, z]
+    assert list(preorder_traversal(expr)) in [expected1, expected2, expected3]
 
 
