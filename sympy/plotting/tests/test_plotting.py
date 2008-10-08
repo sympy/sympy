a09d7fc5f256e806a7f9be6540f4b7638b276c84
Ondrej Certik <ondrej@certik.cz>
2008-10-08 17:36:02 +0200

    test_plotting: don't use classes with tests, just regular functions.
    This is the only place in SymPy that uses classes, so let's just use functions
    all over the place. This simplifies our own testsuite.
    
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>
    Signed-off-by: Riccardo Gori <goriccardo@gmail.com>

--- sympy/plotting/tests/test_plotting.py
+++ sympy/plotting/tests/test_plotting.py
@@ -11,60 +11,55 @@ except:
 from sympy import symbols, sin, cos
 x,y = symbols('xy')
 
-class TestPlotting:
-    def __init__(self):
-        global disabled
-        self.disabled = disabled
+def test_import():
+    from sympy import Plot
 
-    def test_import(self):
-        from sympy import Plot
+def test_plot_2d():
+    from sympy import Plot
+    p=Plot(x, [x, -5, 5, 4], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_2d(self):
-        from sympy import Plot
-        p=Plot(x, [x, -5, 5, 4], visible=False)
-        p.wait_for_calculations()
+def test_plot_2d_discontinuous():
+    from sympy import Plot
+    p=Plot(1/x, [x, -1, 1, 2], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_2d_discontinuous(self):
-        from sympy import Plot
-        p=Plot(1/x, [x, -1, 1, 2], visible=False)
-        p.wait_for_calculations()
+def test_plot_3d():
+    from sympy import Plot
+    p=Plot(x*y, [x, -5, 5, 5], [y, -5, 5, 5], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_3d(self):
-        from sympy import Plot
-        p=Plot(x*y, [x, -5, 5, 5], [y, -5, 5, 5], visible=False)
-        p.wait_for_calculations()
+def test_plot_3d_discontinuous():
+    from sympy import Plot
+    p=Plot(1/x, [x, -3, 3, 6], [y, -1, 1, 1], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_3d_discontinuous(self):
-        from sympy import Plot
-        p=Plot(1/x, [x, -3, 3, 6], [y, -1, 1, 1], visible=False)
-        p.wait_for_calculations()
+def test_plot_2d_polar():
+    from sympy import Plot
+    p=Plot(1/x, [x,-1,1,4], 'mode=polar', visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_2d_polar(self):
-        from sympy import Plot
-        p=Plot(1/x, [x,-1,1,4], 'mode=polar', visible=False)
-        p.wait_for_calculations()
+def test_plot_3d_cylinder():
+    from sympy import Plot
+    p=Plot(1/y, [x,0,6.282,4], [y,-1,1,4], 'mode=polar;style=solid', visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_3d_cylinder(self):
-        from sympy import Plot
-        p=Plot(1/y, [x,0,6.282,4], [y,-1,1,4], 'mode=polar;style=solid', visible=False)
-        p.wait_for_calculations()
+def test_plot_3d_spherical():
+    from sympy import Plot
+    p=Plot(1, [x,0,6.282,4], [y,0,3.141,4], 'mode=spherical;style=wireframe', visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_3d_spherical(self):
-        from sympy import Plot
-        p=Plot(1, [x,0,6.282,4], [y,0,3.141,4], 'mode=spherical;style=wireframe', visible=False)
-        p.wait_for_calculations()
+def test_plot_2d_parametric():
+    from sympy import Plot
+    p=Plot(sin(x), cos(x), [x, 0, 6.282, 4], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_2d_parametric(self):
-        from sympy import Plot
-        p=Plot(sin(x), cos(x), [x, 0, 6.282, 4], visible=False)
-        p.wait_for_calculations()
+def test_plot_3d_parametric():
+    from sympy import Plot
+    p=Plot(sin(x), cos(x), x/5.0, [x, 0, 6.282, 4], visible=False)
+    p.wait_for_calculations()
 
-    def test_plot_3d_parametric(self):
-        from sympy import Plot
-        p=Plot(sin(x), cos(x), x/5.0, [x, 0, 6.282, 4], visible=False)
-        p.wait_for_calculations()
-
-    def _test_plot_log(self):
-        from sympy import Plot
-        p=Plot(log(x), [x,0,6.282,4], 'mode=polar', visible=False)
-        p.wait_for_calculations()
+def _test_plot_log():
+    from sympy import Plot
+    p=Plot(log(x), [x,0,6.282,4], 'mode=polar', visible=False)
+    p.wait_for_calculations()
