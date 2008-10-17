b0a5b5863188685c995425380113f22b4d62fa78
Ondrej Certik <ondrej@certik.cz>
2008-10-17 15:48:22 +0200

    Tests for sympy.raises.
    This should have been committed with the bin/test patch, but I just forgot to
    "git add" the file.

---
+++ sympy/utilities/tests/test_pytest.py
@@ -0,0 +1,18 @@
+from sympy.utilities.pytest import raises
+
+def test_raises():
+    class My(Exception):
+        pass
+    raises(My, "raise My()")
+
+    try:
+        raises(My, "1+1")
+        assert False
+    except Exception, e:
+        assert e.message == "DID NOT RAISE"
+
+    try:
+        raises(My, "raise Exception('my text123')")
+        assert False
+    except Exception, e:
+        assert e.message == "my text123"
