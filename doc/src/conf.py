c18843c6a881ebf2df313ab317e7f2a37ba2c31d
Ondrej Certik <ondrej@certik.cz>
2008-11-12 11:31:10 +0100

    docs: sympy logo added.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -79,11 +79,13 @@ html_style = 'default.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
+
+html_logo = '_static/sympylogo.png'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
