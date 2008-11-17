a547818f368a4f757ce5bb35efabd7e95b17cd5b
Ondrej Certik <ondrej@certik.cz>
2008-11-17 14:20:56 +0100

    runtests.py: module docstring improved

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -3,7 +3,8 @@ This is our testing framework.
 
 Goals:
 
-* it should behave very similar (or identical) to py.test
+* it should be compatible with py.test and operate very similarly (or
+identically)
 * doesn't require any external dependencies
 * preferably all the functionality should be in this file only
 * no magic, just import the test file and execute the test functions, that's it
