77cd74d5d4cf8ed528c662ccd7b4f748bf9ba76e
Ondrej Certik <ondrej@certik.cz>
2008-11-19 21:56:04 +0100

    v0.6.3

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -42,7 +42,7 @@ copyright = '2008, SymPy Development Team'
 # The short X.Y version.
 version = '0.6.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.3.beta2'
+release = '0.6.3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
