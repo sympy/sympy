4afce86f01061b902987ed741b9eb0a20f7edae3
Ondrej Certik <ondrej@certik.cz>
2009-03-29 20:41:08 -0700

    v0.6.4.beta3

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -10,7 +10,7 @@ See the webpage for more information and documentation:
     http://code.google.com/p/sympy/
 """
 
-__version__ = "0.6.4.beta2"
+__version__ = "0.6.4.beta3"
 
 
 def __sympy_debug():
