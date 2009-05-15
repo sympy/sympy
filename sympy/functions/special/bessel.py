db0f02516137e0b0c763a18b658ad7373b50cef4
Ondrej Certik <ondrej@certik.cz>
2009-05-14 20:31:24 -0700

    Spherical Bessel function implemented
    Tests (and doctests) written, up to the order n=6.
    
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

---
+++ sympy/functions/special/bessel.py
@@ -0,0 +1,49 @@
+from sympy.functions.elementary.trigonometric import sin, cos
+
+def fn(n, z):
+    """
+    Coefficients for the spherical Bessel functions.
+
+    Those are only needed in the jn() function.
+
+    The coefficients are calculated from:
+
+    fn(0, z) = 1/z
+    fn(1, z) = 1/z**2
+    fn(n-1, z) + fn(n+1, z) == (2*n+1)/z * fn(n, z)
+    """
+    if n == 0:
+        return 1/z
+    elif n == 1:
+        return 1/z**2
+    elif n > 1:
+        return ((2*n-1)/z * fn(n-1, z)).expand() - fn(n-2, z)
+    elif n < 0:
+        return ((2*n+3)/z * fn(n+1, z)).expand() - fn(n+2, z)
+
+
+def jn(n, z):
+    """
+    Spherical Bessel function.
+
+    Examples:
+
+        >>> from sympy import Symbol
+        >>> z = Symbol("z")
+        >>> print jn(0, z)
+        sin(z)/z
+        >>> jn(1, z) == sin(z)/z**2 - cos(z)/z
+        True
+        >>> jn(3, z) ==(1/z - 15/z**3)*cos(z) + (15/z**4 - 6/z**2)*sin(z)
+        True
+
+    The spherical Bessel functions are calculated using the formula[0]:
+
+    jn(n, z) == fn(n, z) * sin(z) + (-1)**(n+1) * fn(-n-1, z) * cos(z)
+
+    where fn(n, z) are the coefficients, see fn()'s sourcecode for more
+    information.
+
+
+    """
+    return fn(n, z) * sin(z) + (-1)**(n+1) * fn(-n-1, z) * cos(z)
---
+++ sympy/functions/special/tests/test_bessel.py
@@ -0,0 +1,14 @@
+from sympy import jn, symbols, sin, cos
+
+def test_jn():
+    z = symbols("z")
+    assert jn(0, z) == sin(z)/z
+    assert jn(1, z) == sin(z)/z**2 - cos(z)/z
+    assert jn(2, z) == (3/z**3-1/z)*sin(z) - (3/z**2) * cos(z)
+    assert jn(3, z) == (15/z**4 - 6/z**2)*sin(z) + (1/z - 15/z**3)*cos(z)
+    assert jn(4, z) == (1/z + 105/z**5 - 45/z**3)*sin(z) + \
+                (-105/z**4 + 10/z**2)*cos(z)
+    assert jn(5, z) == (945/z**6 - 420/z**4 + 15/z**2)*sin(z) + \
+                (-1/z - 945/z**5 + 105/z**3)*cos(z)
+    assert jn(6, z) == (-1/z + 10395/z**7 - 4725/z**5 + 210/z**3)*sin(z) + \
+                (-10395/z**6 + 1260/z**4 - 21/z**2)*cos(z)
