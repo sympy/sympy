2587d8d9f9001e11190982d6746a6e6ac57ce264
Aaron Meurer <asmeurer@gmail.com>
2009-05-21 11:34:42 -0600

    Serious fix in exact engine
    The test was not even correct (I was taking a.diff instead of r[a].diff,
    so it would always pass.  I was also taking the wrong diffs anyway.  The
    tests worked because they were exact, but any non-exact equation that
    fit the form f(x,y)+g(x,y)*y'=0 would have returned a wrong result.
    This has now been fixed.  When I get homogeneous in, I will have tests
    of several functions of that form that are not exact.
    
    Also added note on future return of RootOf from solve.
    
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

--- sympy/solvers/solvers.py
+++ sympy/solvers/solvers.py
@@ -663,7 +663,7 @@ def solve_ODE_first_order(eq, f):
     y0 = Symbol('y0', dummy=True)
     r[a] = r[a].subs(f(x),y)
     r[b] = r[b].subs(f(x),y)
-    if r and a.diff(y) == b.diff(x) and r[b]!=0:
+    if r and simplify(r[a].diff(x)) == simplify(r[b].diff(y)) and r[b]!=0:
         tmpsol = integrate(r[a].subs(x,x0),(y,y0,y))+integrate(r[b],(x,x0,x))
         sol = 0
         assert tmpsol.is_Add
@@ -675,6 +675,7 @@ def solve_ODE_first_order(eq, f):
 
         try:
             # See if the equation can be solved explicitly for f
+            # This part of the code will change when solve returns RootOf.
             sol1 = solve(sol,y)
         except (NotImplementedError, AssertionError):
             return sol.subs(y,f(x))
