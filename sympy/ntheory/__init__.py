c4d8d65c3603374f655e7024d1fa25a1baae6a5a
Ondrej Certik <ondrej@certik.cz>
2009-07-10 17:04:51 -0600

    Fixes the imports to work in python2.4

--- sympy/ntheory/__init__.py
+++ sympy/ntheory/__init__.py
@@ -2,13 +2,13 @@
 Number theory module (primes, etc)
 """
 
-from .generate import nextprime, prevprime, prime, primepi, primerange,\
+from generate import nextprime, prevprime, prime, primepi, primerange,\
 randprime, Sieve, sieve
-from .primetest import isprime, mr, mr_safe
-from .factor_ import divisors, factorint, multiplicity, perfect_power,\
+from primetest import isprime, mr, mr_safe
+from factor_ import divisors, factorint, multiplicity, perfect_power,\
 pollard_pm1, pollard_rho, primefactors, totient, trailing
-from .partitions_ import npartitions
-from .residue import is_primitive_root, is_quad_residue, legendre_symbol,\
+from partitions_ import npartitions
+from residue import is_primitive_root, is_quad_residue, legendre_symbol,\
 n_order, totient_
-from .multinomial import binomial_coefficients, binomial_coefficients_list,\
+from multinomial import binomial_coefficients, binomial_coefficients_list,\
 multinomial_coefficients
