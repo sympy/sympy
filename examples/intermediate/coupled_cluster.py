f1568364bb2d41b9f9d570b519aa5dc2e9f676e0
Øyvind Jensen <jensen.oyvind@gmail.com>
2009-10-12 13:25:20 +0200

    Whitespace errors (tabs -> spaces)

--- examples/intermediate/coupled_cluster.py
+++ examples/intermediate/coupled_cluster.py
@@ -13,21 +13,21 @@ print "T. Daniel Crawford and Henry F. Schaefer III"
 print
 
 def get_CC_operators():
-	"""
-	Returns a tuple (T1,T2) of unique operators.
-	"""
-	i = symbols('i',below_fermi=True,dummy=True)
-	a = symbols('a',above_fermi=True,dummy=True)
-	t_ai = AntiSymmetricTensor('t',(a,),(i,))
-	ai = NO(Fd(a)*F(i))
-	i,j = symbols('ij',below_fermi=True,dummy=True)
-	a,b = symbols('ab',above_fermi=True,dummy=True)
-	t_abij = AntiSymmetricTensor('t',(a,b),(i,j))
-	abji = NO(Fd(a)*Fd(b)*F(j)*F(i))
+    """
+    Returns a tuple (T1,T2) of unique operators.
+    """
+    i = symbols('i',below_fermi=True,dummy=True)
+    a = symbols('a',above_fermi=True,dummy=True)
+    t_ai = AntiSymmetricTensor('t',(a,),(i,))
+    ai = NO(Fd(a)*F(i))
+    i,j = symbols('ij',below_fermi=True,dummy=True)
+    a,b = symbols('ab',above_fermi=True,dummy=True)
+    t_abij = AntiSymmetricTensor('t',(a,b),(i,j))
+    abji = NO(Fd(a)*Fd(b)*F(j)*F(i))
 
-	T1 = t_ai*ai
-	T2 = Number((1,4))*t_abij*abji
-	return (T1,T2)
+    T1 = t_ai*ai
+    T2 = Number((1,4))*t_abij*abji
+    return (T1,T2)
 
 print "Setting up hamiltonian"
 p,q,r,s = symbols('pqrs',dummy=True)
