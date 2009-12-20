60c95b3f6481a0ec118d075aedaa083c9f487e84
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2009-12-20 02:13:50 +0100

    setup.py: add test runners to scripts
    Signed-off-by: Vinzent Steinberg <Vinzent.Steinberg@gmail.com>

--- setup.py
+++ setup.py
@@ -216,7 +216,7 @@ setup(
       license = 'BSD',
       url = 'http://code.google.com/p/sympy',
       packages = ['sympy'] + modules + tests + pyglet_packages,
-      scripts = ['bin/isympy'],
+      scripts = ['bin/isympy', 'bin/test', 'bin/doctest'],
       ext_modules = [],
       package_data = { 'sympy.utilities.mathml' : ['data/*.xsl'] },
       data_files = [('share/man/man1', ['doc/man/isympy.1'])],
