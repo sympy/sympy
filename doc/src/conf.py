a84c89800d1dfd2e4f64578793d5f9bdae4ff463
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2009-12-20 22:16:47 +0100

    Start of 0.7.0 development cycle

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -40,9 +40,9 @@ copyright = '2008, 2009 SymPy Development Team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.6.6'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.6'
+release = '0.7.0-git'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
