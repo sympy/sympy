0305d07bc77327a9c6ad27b743294b46da116066
Chris Smith <smichr@gmail.com>
2010-03-03 20:00:57 +0545

    1848: code_quality refactored; no tabs in snippets

--- sympy/utilities/tests/test_code_quality.py
+++ sympy/utilities/tests/test_code_quality.py
@@ -3,15 +3,25 @@ from os.path import split, join, abspath, exists
 from glob import glob
 import re
 
-# System path separator (usually slash or backslash)
+# System path separator (usually slash or backslash) to be
+# used with excluded files, e.g.
+#     exclude = set([
+#                    "%(sep)sthirdparty%(sep)s" % sepd,
+#                   ])
 sepd = {"sep": sep}
 
-# Files having at least one of these in their path will be excluded
-# Example line:
-#    "%(sep)sprinting%(sep)spretty%(sep)s" % sepd,
-EXCLUDE = set([
-    "%(sep)sthirdparty%(sep)s" % sepd,
-])
+# path and sympy_path
+PATH = reduce(join, [split(__file__)[0], pardir, pardir]) # go to sympy/
+SYMPY_PATH = abspath(PATH)
+assert exists(SYMPY_PATH)
+
+# Tests can be executed when examples are not installed
+# (e.g. after "./setup.py install") so set the examples path
+# to null so it will be skipped by the checker if it is not
+# there.
+EXAMPLES_PATH = abspath(reduce(join, [PATH, pardir, "examples"]))
+if not exists(EXAMPLES_PATH):
+    EXAMPLES_PATH = ""
 
 # Error messages
 message_space = "File contains trailing whitespace: %s, line %s."
@@ -22,86 +32,85 @@ message_str_raise = "File contains string exception: %s, line %s"
 message_gen_raise = "File contains generic exception: %s, line %s"
 message_eof = "File does not end with a newline: %s, line %s"
 
-def get_whitespace(s):
-    """Returns all whitespace at the beginning of the line."""
-    for i in range(len(s)):
-        if not s[i].isspace():
-            return s[:i]
-    return ""
+def tab_in_leading(s):
+    """Returns True if there are tabs in the leading whitespace of a line,
+    including the whitespace of docstring code samples."""
+    n = len(s)-len(s.lstrip())
+    if not s[n:n+3] in ['...', '>>>']:
+        check = s[:n]
+    else:
+        smore = s[n+3:]
+        check = s[:n] + smore[:len(smore)-len(smore.lstrip())]
+    return not (check.expandtabs() == check)
 
-def check_directory_tree(base_path):
+def check_directory_tree(base_path, file_check, exclusions=set()):
     """
     Checks all files in the directory tree (with base_path as starting point)
-    for bad coding style.
+    with the file_check function provided, skipping files that contain
+    any of the strings in the set provided by exclusions.
     """
-    strRaise = re.compile(r'raise(\s+(\'|\")|\s*(\(\s*)+(\'|\"))')
-    genRaise = re.compile(r'raise(\s+Exception|\s*(\(\s*)+Exception)')
+    if not base_path:
+        return
     for root, dirs, files in walk(base_path):
         for fname in glob(join(root, "*.py")):
-            if filter(lambda ex: ex in fname, EXCLUDE):
+            if filter(lambda ex: ex in fname, exclusions):
                 continue
-            file = open(fname, "rb") # without "b" the lines from all systems will appear to be \n terminated
-            try:
-                for idx, line in enumerate(file):
-                    if line.endswith(" \n"):
-                        assert False, message_space % (fname, idx+1)
-                    if line.endswith("\r\n"):
-                        assert False, message_carriage % (fname, idx+1)
-                    w = get_whitespace(line)
-                    if w.expandtabs() != w:
-                        assert False, message_tabs % (fname, idx+1)
-                    if strRaise.search(line):
-                        assert False, message_str_raise % (fname, idx+1)
-                    if genRaise.search(line):
-                        assert False, message_gen_raise % (fname, idx+1)
-            finally:
-                # eof newline check
-                if not line.endswith('\n'):
-                    assert False, message_eof % (fname, idx+1)
-                file.close()
+            file_check(fname)
 
-
-def test_no_trailing_whitespace_and_no_tabs():
+def test_whitespace_and_exceptions():
     """
     This test tests all files in sympy and checks that:
       o no lines contains a trailing whitespace
-      o no line uses tabs instead of spaces as the indentation
+      o no lines end with \r\n
+      o no line uses tabs instead of spaces
       o that the file ends with a newline
+      o there are no general or string exceptions
     """
-    path = split(__file__)[0]
-    path = path + sep + pardir + sep + pardir # go to sympy/
-    sympy_path = abspath(path)
-    assert exists(sympy_path)
-    check_directory_tree(sympy_path)
-    examples_path = abspath(join(path + sep + pardir, "examples"))
-    # Remember that this test can be executed when examples are not installed
-    # (e.g. after "./setup.py install"), so don't raise an error if the
-    # examples are not found:
-    if exists(examples_path):
-        check_directory_tree(examples_path)
+    strRaise = re.compile(r'raise(\s+(\'|\")|\s*(\(\s*)+(\'|\"))')
+    genRaise = re.compile(r'raise(\s+Exception|\s*(\(\s*)+Exception)')
 
-def check_directory_tree_imports(p, exclude):
-    for root, dirs, files in walk(p):
-        for fname in glob(join(root, "*.py")):
-            if filter(lambda ex: ex in fname, exclude):
-                continue
-            file = open(fname, "r")
-            try:
-                for idx, line in enumerate(file):
-                    if re.match("^\s*(>>>)? from .* import .*\*",line):
-                        assert False, message_implicit % (fname, idx+1)
-            finally:
-                file.close()
+    def test(fname):
+        file = open(fname, "rb") # without "b" the lines from all systems will appear to be \n terminated
+        try:
+            line = None # to flag the case where there were no lines in file
+            for idx, line in enumerate(file):
+                if line.endswith(" \n"):
+                    assert False, message_space % (fname, idx+1)
+                if line.endswith("\r\n"):
+                    assert False, message_carriage % (fname, idx+1)
+                if tab_in_leading(line):
+                    assert False, message_tabs % (fname, idx+1)
+                if strRaise.search(line):
+                    assert False, message_str_raise % (fname, idx+1)
+                if genRaise.search(line):
+                    assert False, message_gen_raise % (fname, idx+1)
+        finally:
+            if line != None:
+                # eof newline check
+                if not line.endswith('\n'):
+                    assert False, message_eof % (fname, idx+1)
+            file.close()
+
+    exclude = set([
+        "%(sep)sthirdparty%(sep)s" % sepd,
+    ])
+    check_directory_tree(SYMPY_PATH, test, exclude)
+    check_directory_tree(EXAMPLES_PATH, test, exclude)
 
 def test_implicit_imports():
     """
     Tests that all files except __init__.py use explicit imports,
     even in the docstrings.
     """
-    path = split(abspath(__file__))[0]
-    path = path + sep + pardir + sep + pardir # go to sympy/
-    sympy_path = abspath(path)
-    examples_path = abspath(join(path + sep + pardir, "examples"))
+    def test(fname):
+        file = open(fname, "r")
+        try:
+            for idx, line in enumerate(file):
+                if re.match("^\s*(>>>)? from .* import .*\*",line):
+                    assert False, message_implicit % (fname, idx+1)
+        finally:
+            file.close()
+
     exclude = set([
         "%(sep)sthirdparty%(sep)s" % sepd,
         "%(sep)s__init__.py" % sepd,
@@ -109,10 +118,5 @@ def test_implicit_imports():
         "%(sep)smpmath%(sep)s" % sepd,
         "%(sep)splotting%(sep)s" % sepd,
     ])
-    assert exists(sympy_path)
-    check_directory_tree_imports(sympy_path, exclude)
-    # Remember that this test can be executed when examples are not installed
-    # (e.g. after "./setup.py install"), so don't raise an error if the
-    # examples are not found:
-    if exists(examples_path):
-        check_directory_tree_imports(examples_path, exclude)
+    check_directory_tree(SYMPY_PATH, test, exclude)
+    check_directory_tree(EXAMPLES_PATH, test, exclude)
