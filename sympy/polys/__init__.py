414d608f8ba6bfdd78ed2fe6dbf1ac875c554ccc
Mateusz Paprocki <mattpap@gmail.com>
2010-03-24 10:18:50 +0100

    Exposed refine_root() function to the top-level
    The new function is a wrapper for Poly.refine_root() and
    accepts a univariate polynomial as an expression or Poly
    class instance, e.g.:
    
    In [1]: refine_root(x**2 - 2, 1, 2, eps=S(1)/100)
    Out[1]:
    ⎛24  17⎞
    ⎜──, ──⎟
    ⎝17  12⎠
    
    Note that generators can't be specified for this function.

--- sympy/polys/__init__.py
+++ sympy/polys/__init__.py
@@ -15,7 +15,7 @@ from polytools import (
     sturm,
     sqf_norm, sqf_part, sqf_list, sqf,
     factor_list, factor,
-    intervals, nroots,
+    intervals, refine_root, nroots,
     cancel,
     reduced, groebner,
     symmetrize,
