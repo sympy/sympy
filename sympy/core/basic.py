302bc01d18ef072a899a4d6707dec08f3cb5bb76
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-03-28 10:29:39 +0200

    Fix Basic.__lt__() and friends for noncommutative symbols (issue 1878)
    All comparisons of for non-commutative symbols used to return False.  Now
    an Inequality or StrictInequality is returned instead.

--- sympy/core/basic.py
+++ sympy/core/basic.py
@@ -652,33 +652,31 @@ class Basic(AssumeMeths):
 
     @_sympifyit('other', False) # sympy >  other
     def __lt__(self, other):
-        #return sympify(other) > self
         dif = self - other
-        if not dif.is_negative is None:
+        if dif.is_negative != dif.is_nonnegative:
             return dif.is_negative
         return StrictInequality(self, other)
 
     @_sympifyit('other', True)  # sympy >  other
     def __gt__(self, other):
         dif = self - other
-        if not dif.is_negative is None:
+        if dif.is_positive !=  dif.is_nonpositive:
             return dif.is_positive
         return StrictInequality(other, self)
-        #return sympify(other) < self
 
     @_sympifyit('other', False) # sympy >  other
     def __le__(self, other):
         dif = self - other
-        if not dif.is_nonpositive is None:
+        if dif.is_nonpositive != dif.is_positive:
             return dif.is_nonpositive
         return Inequality(self, other)
 
     @_sympifyit('other', True)  # sympy >  other
     def __ge__(self, other):
         dif = self - other
-        if not dif.is_nonnegative is None:
+        if dif.is_nonnegative != dif.is_negative:
             return dif.is_nonnegative
-        return sympify(other) <= self
+        return Inequality(other, self)
 
 
     # ***************
