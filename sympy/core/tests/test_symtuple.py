2850a0b93487a54d6ea172539f9430fc7df093a4
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-06-28 11:57:34 +0200

    Fixed spacing in test_symptuple + Added test of SymTuple.subs()
    Signed-off-by: Andy R. Terrel <andy.terrel@gmail.com>

--- sympy/core/tests/test_symtuple.py
+++ sympy/core/tests/test_symtuple.py
@@ -9,8 +9,9 @@ def test_SymTuple():
     assert isinstance(st[:], SymTuple)
     assert st == SymTuple(1, 2, 3, 4)
     assert st.func(*st.args) == st
-    p,q,r,s = symbols('pqrs')
-    t2=(p,q,r,s)
+    p, q, r, s = symbols('p q r s')
+    t2 = (p, q, r, s)
     st2 = SymTuple(*t2)
     assert st2.atoms() == set(t2)
+    assert st == st2.subs({p:1, q:2, r:3, s:4})
 
