mattpap/sympy-polys

Removed RootsOf and improved significantly RootSum
RootsOf class was an artifact of previous design and is not
needed any more. Its functionality will be provided by roots().

RootSum now uses factorization to split the input polynomial
into irreducibles and return a sum of instances of RootSum
class, each with minimal arguments. Multiplicity of a root
is transformed into a multiplicative coefficient.

The order of arguments to RootSum was changed and now a
polynomial comes first and then an expression which will
be applied on each root. In case of an identity expression
one case skip it at all, as RootSum will construct a trivial
Lambda expression automatically.

Also printing of RootSum was improved. Added a pretty printer.

Example:

In [1]: RootSum(x - 1)
Out[1]: 1

In [2]: RootSum(x**3 + x + 3)
Out[2]:
       ⎛ 3        ⎞
RootSum⎝x  + x + 3⎠

In [3]: _.doit()
Out[3]:
      ⎛ 3           ⎞         ⎛ 3           ⎞         ⎛ 3           ⎞
RootOf⎝x  + x + 3, 0⎠ + RootOf⎝x  + x + 3, 1⎠ + RootOf⎝x  + x + 3, 2⎠

In [4]: RootSum(x**3 + x + 3, formal=False)
Out[4]:
      ⎛ 3           ⎞         ⎛ 3           ⎞         ⎛ 3           ⎞
RootOf⎝x  + x + 3, 0⎠ + RootOf⎝x  + x + 3, 1⎠ + RootOf⎝x  + x + 3, 2⎠

In [5]: RootSum((x - 7)*(x**3 + x + 3)**5, Lambda(x, x**2))
Out[5]:
              ⎛ 3           ⎛    2⎞⎞
49 + 5⋅RootSum⎝x  + x + 3, Λ⎝x, x ⎠⎠

In [6]: _.doit()
Out[6]:
       ⎛                     2                        2                        2⎞
       ⎜      ⎛ 3           ⎞          ⎛ 3           ⎞          ⎛ 3           ⎞ ⎟
49 + 5⋅⎝RootOf⎝x  + x + 3, 0⎠  + RootOf⎝x  + x + 3, 1⎠  + RootOf⎝x  + x + 3, 2⎠ ⎠

sympy/polys/rootoftools.py
@@ -1,6 +1,6 @@
 """Implementation of RootOf class and related tools. """
 
-from sympy import Basic, Integer, Real, I
+from sympy import Basic, Integer, Real, I, Add, Lambda
 
 from sympy.polys.polytools import Poly
 
@@ -313,4 +313,62 @@ class RootOf(Basic):
             mp.prec = _prec
 
         return Real._new(root.real._mpf_, prec) + I*Real._new(root.imag._mpf_, prec)
+
+class RootSum(Basic):
+    """Represents a sum of all roots of a univariate polynomial. """
+
+    __slots__ = ['poly', 'func', 'roots']
+
+    def __new__(cls, poly, func=None, formal=True):
+        """Construct new ``RootSum`` instance carrying all formal roots of ``poly``. """
+        poly = Poly(poly, greedy=False)
+
+        if not poly.is_univariate:
+            raise PolynomialError("only univariate polynomials are supported")
+
+        if func is None:
+            func = Lambda(poly.gen, poly.gen)
+        elif not hasattr(func, '__call__'):
+            raise TypeError("%s is not a callable object" % func)
+
+        (_, factors), terms = poly.factor_list(), []
+
+        for poly, k in factors:
+            roots = []
+
+            for i in xrange(0, poly.degree()):
+                root = RootOf(poly, i)
+
+                if formal and isinstance(root, RootOf):
+                    roots.append(root)
+                else:
+                    terms.append(k*func(root))
+
+            if formal and roots:
+                obj = Basic.__new__(cls)
+
+                obj.poly = poly
+                obj.func = func
+                obj.roots = roots
+
+                terms.append(k*obj)
+
+        return Add(*terms)
+
+    def _hashable_content(self):
+        return (self.expr, self.func)
+
+    @property
+    def expr(self):
+        return self.poly.as_basic()
+
+    @property
+    def args(self):
+        return [self.expr, self.func]
+
+    def doit(self, **hints):
+        if hints.get('roots', True):
+            return Add(*map(self.func, self.roots))
+        else:
+            return self
 

sympy/sympy

sympy/polys/rootoftools.py
@@ -1,6 +1,6 @@
 """Implementation of RootOf class and related tools. """
 
-from sympy import Basic, Integer, Real, I
+from sympy import Expr, Integer, Real, I, Add, Lambda
 
 from sympy.polys.polytools import Poly
 
@@ -81,7 +81,7 @@ def dup_minpoly_pow(f, p, q, K):
 _rootof_reals_cache = {}
 _rootof_complexes_cache = {}
 
-class RootOf(Basic):
+class RootOf(Expr):
     """Represents ``k``-th root of a univariate polynomial. """
 
     __slots__ = ['poly', 'index', 'pointer', 'conjugate']
@@ -125,7 +125,7 @@ class RootOf(Basic):
             if poly.length() == 2 and poly.TC():
                 return roots_binomial(poly)[index]
 
-        obj = Basic.__new__(cls)
+        obj = Expr.__new__(cls)
 
         obj.poly = poly
         obj.index = index
@@ -313,4 +313,62 @@ class RootOf(Basic):
             mp.prec = _prec
 
         return Real._new(root.real._mpf_, prec) + I*Real._new(root.imag._mpf_, prec)
+
+class RootSum(Expr):
+    """Represents a sum of all roots of a univariate polynomial. """
+
+    __slots__ = ['poly', 'func', 'roots']
+
+    def __new__(cls, poly, func=None, formal=True):
+        """Construct new ``RootSum`` instance carrying all formal roots of ``poly``. """
+        poly = Poly(poly, greedy=False)
+
+        if not poly.is_univariate:
+            raise PolynomialError("only univariate polynomials are supported")
+
+        if func is None:
+            func = Lambda(poly.gen, poly.gen)
+        elif not hasattr(func, '__call__'):
+            raise TypeError("%s is not a callable object" % func)
+
+        (_, factors), terms = poly.factor_list(), []
+
+        for poly, k in factors:
+            roots = []
+
+            for i in xrange(0, poly.degree()):
+                root = RootOf(poly, i)
+
+                if formal and isinstance(root, RootOf):
+                    roots.append(root)
+                else:
+                    terms.append(k*func(root))
+
+            if formal and roots:
+                obj = Expr.__new__(cls)
+
+                obj.poly = poly
+                obj.func = func
+                obj.roots = roots
+
+                terms.append(k*obj)
+
+        return Add(*terms)
+
+    def _hashable_content(self):
+        return (self.expr, self.func)
+
+    @property
+    def expr(self):
+        return self.poly.as_basic()
+
+    @property
+    def args(self):
+        return [self.expr, self.func]
+
+    def doit(self, **hints):
+        if hints.get('roots', True):
+            return Add(*map(self.func, self.roots))
+        else:
+            return self
 