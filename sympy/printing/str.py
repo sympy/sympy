dd7537278bdb7762c5b289f47330e38da7920e5d
Mateusz Paprocki <mattpap@gmail.com>
2010-07-24 13:57:21 +0200

    Fixed and improved doctests in densebasic.py

--- sympy/printing/str.py
+++ sympy/printing/str.py
@@ -343,6 +343,9 @@ class StrPrinter(Printer):
                            expr.rel_op,
                            self.parenthesize(expr.rhs, precedence(expr)))
 
+    def _print_DMP(self, expr):
+        return "%s(%s, %s)" % (expr.__class__.__name__, self._print(expr.rep), self._print(expr.dom))
+
     def _print_RootOf(self, expr):
         return "RootOf(%s, %d)" % (self._print_Add(expr.expr, order='lex'), expr.index)
 
