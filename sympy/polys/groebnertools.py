7de10c896a71637d6316aff4960e68e04cd82098
Mateusz Paprocki <mattpap@gmail.com>
2010-07-24 12:40:30 +0200

    Use sstr() in ExactQuotientFailed exception

--- sympy/polys/groebnertools.py
+++ sympy/polys/groebnertools.py
@@ -400,7 +400,7 @@ def sdp_quo(f, g, u, O, K):
     if not r:
         return q
     else:
-        raise ExactQuotientFailed('%s does not divide %s' % (g, f))
+        raise ExactQuotientFailed(f, g)
 
 def sdp_exquo(f, g, u, O, K):
     """Returns exact polynomial quotient in `K[x]`. """
