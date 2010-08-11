812b1a3496337aebbbd2e647736c17ee7d0e0ed2
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-08-12 00:01:28 +0200

    Fix failing doctest of fcode with double precision literals

--- sympy/printing/fcode.py
+++ sympy/printing/fcode.py
@@ -451,7 +451,7 @@ def fcode(expr, **settings):
        >>> from sympy import fcode, symbols, Rational, pi, sin
        >>> x, tau = symbols(["x", "tau"])
        >>> fcode((2*tau)**Rational(7,2))
-       '      8*sqrt(2.0)*tau**(7.0/2.0)'
+       '      8*sqrt(2.0d0)*tau**(7.0d0/2.0d0)'
        >>> fcode(sin(x), assign_to="s")
        '      s = sin(x)'
        >>> print fcode(pi)
