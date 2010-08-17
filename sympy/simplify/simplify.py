50f08622873e3356b949c7f8b403f18936d33891
Mateusz Paprocki <mattpap@gmail.com>
2010-08-17 20:18:21 +0200

    Disallow non-commutative expressions in polys
    In [1]: A, B = symbols('A,B', commutative=False)
    
    In [2]: sqf(A*B - B*A)
    (...)
    PolynomialError: non-commutative expressions are not supported

--- sympy/simplify/simplify.py
+++ sympy/simplify/simplify.py
@@ -1385,6 +1385,9 @@ def simplify(expr):
     if not isinstance(expr, Basic): # XXX: temporary hack
         return expr
 
+    if expr.is_commutative is False:
+        return together(powsimp(expr))
+
     expr = together(cancel(powsimp(expr)).expand())
 
     if not isinstance(expr, Basic): # XXX: temporary hack
