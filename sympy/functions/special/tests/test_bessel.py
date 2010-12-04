dd35d66552c74d56e0c3ce68510b252bd484cde4
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2010-12-04 20:38:00 +0100

    jn_zeros: add method='mpmath'
    This needs a more recent version of mpmath than currently shipped with sympy.
    So a "global" mpmath is used, if it is recent enough. Else the tests are skipped.

--- sympy/functions/special/tests/test_bessel.py
+++ sympy/functions/special/tests/test_bessel.py
@@ -48,3 +48,15 @@ def test_jn_zeros():
     assert eq(jn_zeros(3, 4), [6.987932, 10.417118, 13.698023, 16.923621])
     assert eq(jn_zeros(4, 4), [8.182561, 11.704907, 15.039664, 18.301255])
 
+def test_jn_zeros_mpmath():
+    try:
+        from mpmath import besseljzero
+    except ImportError:
+        return
+    zeros = lambda n, k: jn_zeros(n, k, method='mpmath')
+    assert eq(zeros(0, 4), [3.141592, 6.283185, 9.424777, 12.566370])
+    assert eq(zeros(1, 4), [4.493409, 7.725251, 10.904121, 14.066193])
+    assert eq(zeros(2, 4), [5.763459, 9.095011, 12.322940, 15.514603])
+    assert eq(zeros(3, 4), [6.987932, 10.417118, 13.698023, 16.923621])
+    assert eq(zeros(4, 4), [8.182561, 11.704907, 15.039664, 18.301255])
+
