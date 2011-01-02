17a912ee5216cd32c765502ab81c4ac227c9f315
Mateusz Paprocki <mattpap@gmail.com>
2011-01-03 00:39:19 +0100

    Allow parallel symmetric reduction of polynomials
    In [1]: symmetrize([x + y, x - y])
    Out[1]: [(x + y, 0), (x + y, -2⋅y)]
    
    In [2]: symmetrize([x + y, x - y], formal=True)
    Out[2]: ([(s₁, 0), (s₁, -2⋅y)], [(s₁, x + y), (s₂, x⋅y)])

--- sympy/polys/polyfuncs.py
+++ sympy/polys/polyfuncs.py
@@ -1,6 +1,7 @@
 """High--level polynomials manipulation functions. """
 
-from sympy.polys.polytools import poly_from_expr, Poly
+from sympy.polys.polytools import (
+    poly_from_expr, parallel_poly_from_expr, Poly)
 from sympy.polys.polyoptions import allowed_flags
 
 from sympy.polys.specialpolys import (
@@ -15,7 +16,7 @@ from sympy.utilities import (
 
 from sympy.core import S, Basic, Add, Mul
 
-def symmetrize(f, *gens, **args):
+def symmetrize(F, *gens, **args):
     """
     Rewrite a polynomial in terms of elementary symmetric polynomials.
 
@@ -29,82 +30,113 @@ def symmetrize(f, *gens, **args):
     (-2*x*y + (x + y)**2, 0)
 
     >>> symmetrize(x**2 + y**2, formal=True)
-    (-2*s2 + s1**2, 0, {s1: x + y, s2: x*y})
+    (-2*s2 + s1**2, 0, [(s1, x + y), (s2, x*y)])
 
     >>> symmetrize(x**2 - y**2)
     (-2*x*y + (x + y)**2, -2*y**2)
 
     >>> symmetrize(x**2 - y**2, formal=True)
-    (-2*s2 + s1**2, -2*y**2, {s1: x + y, s2: x*y})
+    (-2*s2 + s1**2, -2*y**2, [(s1, x + y), (s2, x*y)])
 
     """
     allowed_flags(args, ['formal', 'symbols'])
 
+    iterable = True
+
+    if not hasattr(F, '__iter__'):
+        iterable = False
+        F = [F]
+
     try:
-        f, opt = poly_from_expr(f, *gens, **args)
+        F, opt = parallel_poly_from_expr(F, *gens, **args)
     except PolificationFailed, exc:
-        if exc.expr.is_Number:
-            if exc.opt.formal:
-                return (exc.expr, S.Zero, {})
+        result = []
+
+        for expr in exc.exprs:
+            if expr.is_Number:
+                result.append((expr, S.Zero))
             else:
-                return (exc.expr, S.Zero)
+                raise ComputationFailed('symmetrize', len(F), exc)
         else:
-            raise ComputationFailed('symmetrize', 1, exc)
+            if not iterable:
+                result, = result
+
+            if not exc.opt.formal:
+                return result
+            else:
+                if iterable:
+                    return result, []
+                else:
+                    return result + ([],)
 
     polys, symbols = [], opt.symbols
-    gens, dom = f.gens, f.get_domain()
+    gens, dom = opt.gens, opt.domain
 
-    for i in xrange(0, len(f.gens)):
+    for i in xrange(0, len(gens)):
         poly = symmetric_poly(i+1, gens, polys=True)
         polys.append((symbols.next(), poly.set_domain(dom)))
 
     indices = range(0, len(gens) - 1)
     weights = range(len(gens), 0, -1)
 
-    symmetric = []
+    result = []
 
-    if not f.is_homogeneous:
-        symmetric.append(f.TC())
-        f -= f.TC()
+    for f in F:
+        symmetric = []
 
-    while f:
-        _height, _monom, _coeff = -1, None, None
+        if not f.is_homogeneous:
+            symmetric.append(f.TC())
+            f -= f.TC()
 
-        for i, (monom, coeff) in enumerate(f.terms()):
-            if all(monom[i] >= monom[i+1] for i in indices):
-                height = max([ n*m for n, m in zip(weights, monom) ])
+        while f:
+            _height, _monom, _coeff = -1, None, None
 
-                if height > _height:
-                    _height, _monom, _coeff = height, monom, coeff
+            for i, (monom, coeff) in enumerate(f.terms()):
+                if all(monom[i] >= monom[i+1] for i in indices):
+                    height = max([ n*m for n, m in zip(weights, monom) ])
 
-        if _height != -1:
-            monom, coeff = _monom, _coeff
-        else:
-            break
+                    if height > _height:
+                        _height, _monom, _coeff = height, monom, coeff
 
-        exponents = []
+            if _height != -1:
+                monom, coeff = _monom, _coeff
+            else:
+                break
 
-        for m1, m2 in zip(monom, monom[1:] + (0,)):
-            exponents.append(m1 - m2)
+            exponents = []
 
-        term = [ s**n for (s, _), n in zip(polys, exponents) ]
-        poly = [ p**n for (_, p), n in zip(polys, exponents) ]
+            for m1, m2 in zip(monom, monom[1:] + (0,)):
+                exponents.append(m1 - m2)
 
-        symmetric.append(Mul(coeff, *term))
+            term = [ s**n for (s, _), n in zip(polys, exponents) ]
+            poly = [ p**n for (_, p), n in zip(polys, exponents) ]
 
-        product = poly[0].mul(coeff)
+            symmetric.append(Mul(coeff, *term))
+            product = poly[0].mul(coeff)
 
-        for p in poly[1:]:
-            product = product.mul(p)
+            for p in poly[1:]:
+                product = product.mul(p)
 
-        f -= product
+            f -= product
+
+        result.append((Add(*symmetric), f.as_basic()))
 
     polys = [ (s, p.as_basic()) for s, p in polys ]
 
-    if opt.formal:
-        return (Add(*symmetric), f.as_basic(), dict(polys))
+    if not opt.formal:
+        for i, (sym, non_sym) in enumerate(result):
+            result[i] = (sym.subs(polys), non_sym)
+
+    if not iterable:
+        result, = result
+
+    if not opt.formal:
+        return result
     else:
-        return (Add(*symmetric).subs(polys), f.as_basic())
+        if iterable:
+            return result, polys
+        else:
+            return result + (polys,)
 
 def horner(f, *gens, **args):
     """
