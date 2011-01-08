92a560b2a6969bbfd7acfcfd53aafcd12e938dbc
Mateusz Paprocki <mattpap@gmail.com>
2011-01-08 21:34:53 +0100

    Allowed to use init_printing() in documentation
    By default init_printing() stores configuration in Printer, using
    set_global_settings() method. This is the best approach to make
    all kinds of printers work configured in interactive sessions.
    However, due to doctest's limitations, this approach fails in
    the case of documentation. For this purpose, no_global option
    was added to init_printing(). When set to True, it won't allow
    init_printing() to store any global settings. In this scenario
    only the chosen printer will be configured properly.

--- sympy/printing/pretty/stringpict.py
+++ sympy/printing/pretty/stringpict.py
@@ -238,7 +238,7 @@ class stringPict(object):
            break the expression in a form that can be printed
            on the terminal without being broken up.
          """
-        if not kwargs["wrap_line"]:
+        if kwargs["wrap_line"] is False:
             return "\n".join(self.picture)
 
         # Attempt to get a terminal width
