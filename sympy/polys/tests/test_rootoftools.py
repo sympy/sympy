927fcca26834b76ae1e312f58c8cc7956d4e2f49
Mateusz Paprocki <mattpap@gmail.com>
2011-01-15 12:59:20 +0100

    Made test_RootSum_independent work

--- sympy/polys/tests/test_rootoftools.py
+++ sympy/polys/tests/test_rootoftools.py
@@ -250,8 +250,12 @@ def test_RootSum_rational():
 
 def test_RootSum_independent():
     f = (x**3 - a)**2*(x**4 - b)**3
-    g = Lambda(x, 5*tan(x) + 7)
 
-    assert RootSum(f, g, x) == \
-        10*RootSum(x**3 - a, Lambda(x, tan(x)), x) + \
-        15*RootSum(x**4 - b, Lambda(x, tan(x)), x) + 126
+    g = Lambda(x, 5*tan(x) + 7)
+    h = Lambda(x, tan(x))
+
+    r0 = RootSum(x**3 - a, h, x)
+    r1 = RootSum(x**4 - b, h, x)
+
+    assert RootSum(f, g, x).as_ordered_terms() == [10*r0, 15*r1, 126]
+
