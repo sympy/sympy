bc557b55e51cb92f65a8d3ca8873cb243b61837e
Chris Smith <smichr@gmail.com>
2011-01-17 16:18:13 +0545

    2039 remove backslashes after parenthesis

--- sympy/core/mul.py
+++ sympy/core/mul.py
@@ -907,23 +907,23 @@ class Mul(AssocOp):
         # do quick tests to see if we can't succeed
 
         ok = True
-        if (\
+        if (
             # more non-commutative terms
             len(old_nc) > len(nc)):
             ok = False
-        elif (\
+        elif (
             # more commutative terms
             len(old_c) > len(c)):
             ok = False
-        elif (\
+        elif (
             # unmatched non-commutative bases
             set(_[0] for _ in  old_nc).difference(set(_[0] for _ in nc))):
             ok = False
-        elif (\
+        elif (
             # unmatched commutative terms
             set(old_c).difference(set(c))):
             ok = False
-        elif (\
+        elif (
             # differences in sign
             any(sign(c[b]) != sign(old_c[b]) for b in old_c)):
             ok = False
