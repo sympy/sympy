53822dd9b503488ac0fd0c3164ee4351c0e8fa35
Addison CUgini <ajcugini@gmail.com>
2011-02-07 09:20:02 -0800

    Modified the tests for qft

--- sympy/physics/quantum/tests/test_qft.py
+++ sympy/physics/quantum/tests/test_qft.py
@@ -1,9 +1,11 @@
 from sympy.physics.quantum.qft import *
 from sympy.physics.quantum.gate import *
+from sympy.physics.quantum.qubit import *
 from sympy.physics.quantum.represent import represent
+from sympy.physics.quantum.applyops import apply_operators
 from sympy.matrices.matrices import Matrix, eye
 from sympy.core.symbol import Symbol
-from sympy import exp, I, pi
+from sympy import exp, I, pi, sqrt
 
 
 def test_RkGate():
@@ -27,17 +29,10 @@ def test_quantum_fourier():
     assert IQFT(0,3).decompose() == HadamardGate(2)*CGate((1,), RkGate(2,-2))*CGate((0,),RkGate(2,-3))\
     *HadamardGate(1)*CGate((0,), RkGate(1,-2))*HadamardGate(0)*SwapGate(0,2)
     
-    assert represent(QFT(0,3)*IQFT(0,3), ZGate(0), nqubits=3)\
-     == eye(8)
+    assert represent(QFT(0,3), ZGate(0), nqubits=3)\
+     == Matrix([[exp(2*pi*I/8)**(i*j%8)/sqrt(8) for i in range(8)] for j in range(8)])
      
     assert QFT(0,4).decompose() #non-trivial decomposition
-    assert apply_operators(QFT(0,3).decompose()*Qubit((0,0,0))).expand() ==\
-     apply_operators(HadamardGate(0)*HadamardGate(1)*HadamardGate(2)*Qubit((0,0,0))).expand()
+    assert apply_operators(QFT(0,3).decompose()*Qubit(0,0,0)).expand() ==\
+     apply_operators(HadamardGate(0)*HadamardGate(1)*HadamardGate(2)*Qubit(0,0,0)).expand()
      
-     
-def test_Fourier():
-    assert QFT(0,3).decompose() == SwapGate(0,2)*HadamardGate(0)\
-    *RkGate(1,0,2)*HadamardGate(1)*RkGate(2,0,3)*RkGate(2,1,2)*HadamardGate(2)
-    assert QFT(0,3).input_number == 2
-    assert IQFT(0,3).decompose() == HadamardGate(2)*IRkGate(2,1,2)\
-    *IRkGate(2,0,3)*HadamardGate(1)*IRkGate(1,0,2)*HadamardGate(0)*SwapGate(0,2)
