93fc7eb493a91f6a707c7871903cdbd8491ac7f5
Brian Granger <ellisonbg@gmail.com>
2011-02-07 22:17:50 -0800

    Fixing whitespace in test_gate.

--- sympy/physics/quantum/tests/test_gate.py
+++ sympy/physics/quantum/tests/test_gate.py
@@ -134,7 +134,7 @@ def test_CNOTGate():
 # def test_gateSort():
 #     assert gate_sort(XGate(1)*HadamardGate(0)**2*CNOTGate(0,1)*XGate(1)*XGate(0))\
 #      == HadamardGate(0)**2*XGate(1)*CNOTGate(0,1)*XGate(0)*XGate(1)
-# 
+#
 # def test_gate_simp():
 #      assert gate_simp(HadamardGate(0)*XGate(1)*HadamardGate(0)**2*CNOTGate(0,1)\
 #      *XGate(1)**3*XGate(0)*ZGate(3)**2*PhaseGate(4)**3) == HadamardGate(0)*\
