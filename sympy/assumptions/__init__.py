fe9fdc09856c6616d590a1605ca2c7c0bfa9966e
Priit Laes <plaes@plaes.org>
2011-02-12 14:16:33 +0200

    Use __all__ to define exports under assumptions

--- sympy/assumptions/__init__.py
+++ sympy/assumptions/__init__.py
@@ -1,3 +1,8 @@
 from assume import Assume, global_assumptions, Predicate, AssumptionsContext
 from ask import Q, ask, register_handler, remove_handler
 from refine import refine
+
+__all__ = [
+    'Assume', 'AssumptionsContext', 'Predicate', 'Q',
+    'ask', 'global_assumptions', 'refine', 'register_handler', 'remove_handler'
+    ]
