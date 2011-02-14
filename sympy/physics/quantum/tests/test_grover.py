a5429b258ac9699e1595e05c16bbd32d5fdcc14c
rayman <rayman@CoolGuyJr.(none)>
2011-02-13 23:54:06 -0800

    Fixed most of the changes after Prof. Granger' review.    -Changed implementation to use IntQubits    -Changed function name to _create_computational_basis    -Moved docstrings

--- sympy/physics/quantum/tests/test_grover.py
+++ sympy/physics/quantum/tests/test_grover.py
@@ -1,64 +1,65 @@
 from sympy import sqrt, power
 from sympy.physics.quantum.applyops import apply_operators
-from sympy.physics.quantum.qubit import Qubit
+from sympy.physics.quantum.qubit import Qubit, IntQubit
 from sympy.physics.quantum.grover import *
 import sympy.physics.quantum.grover
 
 gpath = sympy.physics.quantum.grover
 
 def return_one_on_two(qubits):
-    qubitTwo = gpath._create_zeroes(qubits.nqubits - 2) + '10'
-    return 1 if qubits == Qubit(qubitTwo) else 0
+    return True if qubits == IntQubit(2, qubits.nqubits) else False
 
 def return_one_on_one(qubits):
-    qubitOne = gpath._create_zeroes(qubits.nqubits - 2) + '01'
-    return 1 if qubits == Qubit(qubitOne) else 0
+    return True if qubits == IntQubit(1, qubits.nqubits) else False
 
-def test_create_basis_states():
-    first_half_state = Qubit('00')/2 + Qubit('01')/2
-    second_half_state = Qubit('10')/2 + Qubit('11')/2
+def test_create_computational_basis():
+    nbits = 2
+    first_half_state = IntQubit(0, nbits)/2 + IntQubit(1, nbits)/2
+    second_half_state = IntQubit(2, nbits)/2 + IntQubit(3, nbits)/2
     super_state = first_half_state + second_half_state
-    assert super_state == gpath._create_basis_states(2)
+    assert super_state == gpath._create_computational_basis(nbits)
 
-    first_q = (1/sqrt(8))*Qubit('000') + (1/sqrt(8))*Qubit('001')
-    second_q = (1/sqrt(8))*Qubit('010') + (1/sqrt(8))*Qubit('011')  
-    third_q = (1/sqrt(8))*Qubit('100') + (1/sqrt(8))*Qubit('101')  
-    fourth_q = (1/sqrt(8))*Qubit('110') + (1/sqrt(8))*Qubit('111')  
+    nbits = 3
+    first_q = (1/sqrt(8))*IntQubit(0, nbits) + (1/sqrt(8))*IntQubit(1, nbits)
+    second_q = (1/sqrt(8))*IntQubit(2, nbits) + (1/sqrt(8))*IntQubit(3, nbits)  
+    third_q = (1/sqrt(8))*IntQubit(4, nbits) + (1/sqrt(8))*IntQubit(5, nbits)  
+    fourth_q = (1/sqrt(8))*IntQubit(6, nbits) + (1/sqrt(8))*IntQubit(7, nbits)  
     super_state = first_q + second_q + third_q + fourth_q
-    assert super_state == gpath._create_basis_states(3)
+    assert super_state == gpath._create_computational_basis(nbits)
 
 def test_OracleGate():
-    v = OracleGate(1, lambda qubits: 1 if qubits == Qubit('0') else 0)
-    assert apply_operators(v*Qubit('0')) == -Qubit('0')
-    assert apply_operators(v*Qubit('1')) == Qubit('1')
+    v = OracleGate(1, lambda qubits: True if qubits == IntQubit(0) else False)
+    assert apply_operators(v*IntQubit(0)) == -IntQubit(0)
+    assert apply_operators(v*IntQubit(1)) == IntQubit(1)
 
+    nbits = 2
     v = OracleGate(2, return_one_on_two)
-    assert apply_operators(v*Qubit('00')) == Qubit('00')
-    assert apply_operators(v*Qubit('01')) == Qubit('01')  
-    assert apply_operators(v*Qubit('10')) == -Qubit('10')
-    assert apply_operators(v*Qubit('11')) == Qubit('11')
+    assert apply_operators(v*IntQubit(0, nbits)) == IntQubit(0, nbits)
+    assert apply_operators(v*IntQubit(1, nbits)) == IntQubit(1, nbits)  
+    assert apply_operators(v*IntQubit(2, nbits)) == -IntQubit(2, nbits)
+    assert apply_operators(v*IntQubit(3, nbits)) == IntQubit(3, nbits)
 
 def test_WGate():
     numqubits = 2
-    basis_states = gpath._create_basis_states(numqubits)
+    basis_states = gpath._create_computational_basis(numqubits)
     w = WGate(numqubits)
     assert apply_operators(w*basis_states) == basis_states
 
-    qubit_one = Qubit('01')
+    qubit_one = IntQubit(1, numqubits)
     expected = ((2/sqrt(pow(2, numqubits)))*basis_states) - qubit_one
     assert apply_operators(w*qubit_one) == expected
 
 def test_grover_iteration_1():
     numqubits = 2
-    basis_states = gpath._create_basis_states(numqubits)
+    basis_states = gpath._create_computational_basis(numqubits)
     v = OracleGate(numqubits, return_one_on_one)
     iterated = gpath.grover_iteration(basis_states, v)
-    expected = Qubit('01')
+    expected = IntQubit(1, numqubits)
     assert apply_operators(iterated) == expected
 
 def test_grover_iteration_2():
     numqubits = 4
-    basis_states = gpath._create_basis_states(numqubits)
+    basis_states = gpath._create_computational_basis(numqubits)
     v = OracleGate(numqubits, return_one_on_two)
     # Should return Qubit('0010') after (pi/4)sqrt(pow(2, n))
     # In this case, after around pi times (3 or 4)
