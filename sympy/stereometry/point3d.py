fc9cbcdb80175d6b271280f2668ddaa958b92eed
Kuleshov Alexander <kuleshovmail@gmail.com>
2011-03-20 12:51:13 +0000

    indentation fixed

--- sympy/stereometry/point3d.py
+++ sympy/stereometry/point3d.py
@@ -1,16 +1,17 @@
 from volumetricEntity import Geometry3dEntity
 
 class Point3d(Geometry3dEntity):
- """
-    A point in Euclidean N-space defined by a sequence of values. Can be
-    constructed from a sequence of points.
+    """
+        A point in Euclidean N-space defined by a sequence of values. Can be
+        constructed from a sequence of points.
 
-    Examples:
-    ======
-        >>> from sympy.sterometry import Point3d
-        >>> Point3d(10,10,20)
-        Point3d(10, 10, 20)
- """
+        Examples:
+        ======
+            >>> from sympy.sterometry import Point3d
+            >>> Point3d(10,10,20)
+            Point3d(10, 10, 20)
+    """
+    
     def __new__(cls, *args, **kwargs):
         if isinstance(args[0], (tuple, list, set)):
             coords = tuple([sympify(x) for x in args[0]])
