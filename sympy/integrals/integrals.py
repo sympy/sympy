e101e2312d447d4289a4c6c044cabb1966c877a7
hector1618 <hector1618@gmail.com>
2011-03-25 19:08:00 +0530

    Issue 1394. Added integration by parts with conditions
    Modified code integrals.py so as to enable it to do integration by parts ( with one condition ) when everything else which is currently present fails to solve the integration.

--- sympy/integrals/integrals.py
+++ sympy/integrals/integrals.py
@@ -572,7 +572,22 @@ class Integral(Expr):
             if h is not None:
                 parts.append(coeff * h)
             else:
-                return None
+                # integral(f1*f2,x) = f1 * integrate(f2, x) - integrate(f1.diff(x) * integrate(f2,x), x)
+                # We will use the above technique to solve integration.
+                # Since we are putting the following condition
+                #         len (args_m) >= len(Mul.make_args(f1.diff(x) * f2_inte))
+                # the process is terminating even though its recursive.
+                args_m = Mul.make_args(f)
+                if len(args_m)>1:
+                    f1 = args_m[0]
+                    f2 = Mul(*args_m[1:])
+                    f2_inte = integrate(f2,x)
+                    if len (args_m) >= len(Mul.make_args(f1.diff(x) * f2_inte)):
+                        return f1 * f2_inte - integrate(f1.diff(x) * f2_inte ,x)
+                    else :
+                        return None
+                else:
+                    return None
 
         return Add(*parts)
 
