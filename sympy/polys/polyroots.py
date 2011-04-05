5b255d2dc1f4ffb9897545e191dbf94a091d569f
Chris Smith <smichr@gmail.com>
2011-04-05 12:02:54 +0545

    1170: sorting with compare_pretty orders Rationals
        The _compare_pretty routine was modified to consider the
        case when p3 may not appear as a key and to properly sort
        rationals. In master:
    
        >>> sorted([S.One, S.Half, S.NegativeOne, S(2)], cmp=Basic.compare_pretty)
        [1, 1/2, -1, 2]
    
        After this commit:
    
        >>> sorted([S.One, S.Half, S.NegativeOne, S(2)], cmp=Basic.compare_pretty)
        [-1, 1/2, 1, 2]
    
        Just using the sorted fuction would give the same results, but whenever
        the Basic.compare_pretty is being used to order powers nicely the ordering
        of rationals would be off. Here's a case where you might want to use
        the compare_pretty:
    
        >>> sorted([x**3,2*x**2])
        [x**3, 2*x**2]
        >>> sorted([x**3,2*x**2], cmp=Basic.compare_pretty)
        [2*x**2, x**3]
    
        So the latter result is desired, but if Rationals were involved, they would
        come out mis-ordered in a mathematical sense.

--- sympy/polys/polyroots.py
+++ sympy/polys/polyroots.py
@@ -297,11 +297,11 @@ def roots(f, *gens, **flags):
     >>> from sympy.abc import x, y
 
     >>> roots(x**2 - 1, x)
-    {1: 1, -1: 1}
+    {-1: 1, 1: 1}
 
     >>> p = Poly(x**2-1, x)
     >>> roots(p)
-    {1: 1, -1: 1}
+    {-1: 1, 1: 1}
 
     >>> p = Poly(x**2-y, x, y)
 
@@ -312,7 +312,7 @@ def roots(f, *gens, **flags):
     {y**(1/2): 1, -y**(1/2): 1}
 
     >>> roots([1, 0, -1])
-    {1: 1, -1: 1}
+    {-1: 1, 1: 1}
 
     """
     flags = dict(flags)
