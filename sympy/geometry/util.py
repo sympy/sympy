9c7496b231321279fb2ccad9607897ec5e95b5f0
Gary Kerr <gary.kerr@blueyonder.co.uk>
2011-04-08 00:16:45 +0100

    Update docstring in geometry/util.py and geometry/exceptions.py
    Using numpy standard.
    
    Fixed some whitespace around operators and brackets.

--- sympy/geometry/util.py
+++ sympy/geometry/util.py
@@ -1,37 +1,58 @@
+"""Utility functions for geometrical entities.
+
+Contains
+--------
+intersection
+convex_hull
+are_similar
+
+"""
+
 def intersection(*entities):
-    """
-    Finds the intersection between a list GeometryEntity instances. Returns a
-    list of all the intersections, Will raise a NotImplementedError exception
-    if unable to calculate the intersection.
+    """The intersection of a collection of GeometryEntity instances.
 
-    Examples:
-    =========
-        >>> from sympy.geometry import Point, Line, Circle, intersection
-        >>> p1,p2,p3 = Point(0,0), Point(1,1), Point(-1, 5)
-        >>> l1, l2 = Line(p1, p2), Line(p3, p2)
-        >>> c = Circle(p2, 1)
-        >>> intersection(l1, p2)
-        [Point(1, 1)]
-        >>> intersection(l1, l2)
-        [Point(1, 1)]
-        >>> intersection(c, p2)
-        []
-        >>> intersection(c, Point(1, 0))
-        [Point(1, 0)]
-        >>> intersection(c, l2)
-        [Point(1 - 5**(1/2)/5, 1 + 2*5**(1/2)/5), Point(1 + 5**(1/2)/5, 1 - 2*5**(1/2)/5)]
+    Parameters
+    ----------
+    entities : sequence of GeometryEntity
 
-    Notes:
-    ======
-        - The intersection of any geometrical entity with itself should return
-          a list with one item: the entity in question.
-        - An intersection requires two or more entities. If only a single
-          entity is given then one will receive an empty intersection list.
-        - It is possible for intersection() to miss intersections that one
-          knows exists because the required quantities were not fully
-          simplified internally.
-        - Reals should be converted to Rationals, e.g. Rational(str(real_num))
-          or else failures due to floating point issues may result.
+    Returns
+    -------
+    intersection : list of GeometryEntity
+
+    Raises
+    ------
+    NotImplementedError
+        When unable to calculate intersection.
+
+    Notes
+    -----
+    The intersection of any geometrical entity with itself should return
+    a list with one item: the entity in question.
+    An intersection requires two or more entities. If only a single
+    entity is given then the function will return an empty list.
+    It is possible for `intersection` to miss intersections that one
+    knows exists because the required quantities were not fully
+    simplified internally.
+    Reals should be converted to Rationals, e.g. Rational(str(real_num))
+    or else failures due to floating point issues may result.
+
+    Examples
+    --------
+    >>> from sympy.geometry import Point, Line, Circle, intersection
+    >>> p1, p2, p3 = Point(0, 0), Point(1, 1), Point(-1, 5)
+    >>> l1, l2 = Line(p1, p2), Line(p3, p2)
+    >>> c = Circle(p2, 1)
+    >>> intersection(l1, p2)
+    [Point(1, 1)]
+    >>> intersection(l1, l2)
+    [Point(1, 1)]
+    >>> intersection(c, p2)
+    []
+    >>> intersection(c, Point(1, 0))
+    [Point(1, 0)]
+    >>> intersection(c, l2)
+    [Point(1 - 5**(1/2)/5, 1 + 2*5**(1/2)/5), Point(1 + 5**(1/2)/5, 1 - 2*5**(1/2)/5)]
+
     """
     from entity import GeometryEntity
 
@@ -43,29 +64,41 @@ def intersection(*entities):
     for entity in entities[2:]:
         newres = []
         for x in res:
-            newres.extend( GeometryEntity.do_intersection(x, entity) )
+            newres.extend(GeometryEntity.do_intersection(x, entity))
         res = newres
     return res
 
 
 def convex_hull(*args):
-    """
-    Returns a Polygon representing the convex hull of a set of 2D points.
+    """The convex hull of a collection of 2-dimensional points.
 
-    Notes:
-    ======
-        This can only be performed on a set of non-symbolic points.
+    Parameters
+    ----------
+    args : a collection of Points
 
-    Example:
-    ========
-        >>> from sympy.geometry import Point, convex_hull
-        >>> points = [ Point(x) for x in [(1,1), (1,2), (3,1), (-5,2), (15,4)] ]
-        >>> convex_hull(points)
-        Polygon(Point(-5, 2), Point(1, 1), Point(3, 1), Point(15, 4))
+    Returns
+    -------
+    convex_hull : Polygon
 
-    Description of method used:
-    ===========================
-        See http://en.wikipedia.org/wiki/Graham_scan.
+    Notes
+    -----
+    This can only be performed on a set of non-symbolic points.
+
+    See Also
+    --------
+    Point
+
+    References
+    ----------
+    http://en.wikipedia.org/wiki/Graham_scan
+
+    Examples
+    --------
+    >>> from sympy.geometry import Point, convex_hull
+    >>> points = [Point(x) for x in [(1, 1), (1, 2), (3, 1), (-5, 2), (15, 4)]]
+    >>> convex_hull(points)
+    Polygon(Point(-5, 2), Point(1, 1), Point(3, 1), Point(15, 4))
+
     """
     from point import Point
     from line import Segment
@@ -85,10 +118,11 @@ def convex_hull(*args):
         return Segment(p[0], p[1])
 
     def orientation(p, q, r):
-        '''Return positive if p-q-r are clockwise, neg if ccw, zero if colinear.'''
-        return (q[1]-p[1])*(r[0]-p[0]) - (q[0]-p[0])*(r[1]-p[1])
+        '''Return positive if p-q-r are clockwise, neg if ccw, zero if
+        collinear.'''
+        return (q[1] - p[1])*(r[0] - p[0]) - (q[0] - p[0])*(r[1] - p[1])
 
-    '''scan to find upper and lower convex hulls of a set of 2d points.'''
+    # scan to find upper and lower convex hulls of a set of 2d points.
     U = []
     L = []
     p.sort()
@@ -108,13 +142,40 @@ def convex_hull(*args):
 
 
 def are_similar(e1, e2):
-    """
-    Returns True if e1 and e2 are similar (one can be uniformly scaled to
-    the other) or False otherwise.
+    """Are two geometrical entities similar.
 
-    Notes:
-    ======
-        - If the two objects are equal then they are always similar.
+    Can one geometrical entity be uniformly scaled to the other?
+
+    Parameters
+    ----------
+    e1 : GeometryEntity
+    e2 : GeometryEntity
+
+    Returns
+    -------
+    are_similar : boolean
+
+    Raises
+    ------
+    GeometryError
+        When `e1` and `e2` cannot be compared.
+
+    Notes
+    -----
+    If the two objects are equal then they are similar.
+
+    Examples
+    --------
+    >>> from sympy import Point, Circle, Triangle, are_similar
+    >>> c1, c2 = Circle(Point(0, 0), 4), Circle(Point(1, 4), 3)
+    >>> t1 = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+    >>> t2 = Triangle(Point(0, 0), Point(2, 0), Point(0, 2))
+    >>> t3 = Triangle(Point(0, 0), Point(3, 0), Point(0, 1))
+    >>> are_similar(t1, t2)
+    True
+    >>> are_similar(t1, t3)
+    False
+
     """
     if e1 == e2:
         return True
