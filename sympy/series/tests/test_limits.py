061afd84ef1da7529ae67a0dee284bcb67d4bacf
hector <hector1618@gmail.com>
2011-04-15 05:25:53 +0530

    Modifined tests in sympy/series/tests/tests_limits.py
    Defined new tests and modified existing tests accourding to modified defination of limits.
    i.e. limit exists if and only if limit_right == limit_left.

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -22,18 +22,17 @@ def test_basic1():
     assert limit(x - x**2, x, oo) == -oo
     assert limit((1 + x)**(1 + sqrt(2)),x,0) == 1
     assert limit((1 + cos(x))**oo, x, 0) == oo
-    assert limit((1 + x)**oo, x, 0) == oo
+    assert limit((1 + x)**oo, x, 0, dir = '+') == oo
     assert limit((1 + x)**oo, x, 0, dir='-') == 0
     assert limit((1 + x + y)**oo, x, 0, dir='-') == (1 + y)**(oo)
     assert limit(y/x/log(x), x, 0) == -y*oo
-    assert limit(cos(x + y)/x, x, 0) == cos(y)*oo
     raises(NotImplementedError, 'limit(Sum(1/x, (x, 1, y)) - log(y), y, oo)')
     assert limit(Sum(1/x, (x, 1, y)) - 1/y, y, oo) == Sum(1/x, (x, 1, oo))
     assert limit(gamma(1/x + 3), x, oo) == 2
 
     # approaching 0
     # from dir="+"
-    assert limit(1 + 1/x, x, 0) == oo
+    assert limit(1 + 1/x, x, 0, dir= '+') == oo
     # from dir='-'
     # Add
     assert limit(1 + 1/x, x, 0, dir='-') == -oo
@@ -65,6 +64,13 @@ def test_basic4():
     assert limit(2*x**8 + y*x**(-3), x, -2) == 512 - y/8
     assert limit(sqrt(x + 1) - sqrt(x), x, oo)==0
     assert integrate(1/(x**3+1),(x,0,oo)) == 2*pi*sqrt(3)/9
+
+@XFAIL
+def test_changed_definition():
+    assert limit(abs(x)/x,x,0) == 1
+    assert limit((1 + x)**oo, x, 0) == oo
+    assert limit(cos(x + y)/x, x, 0) == 1
+    assert limit(1 + 1/x, x, 0) == oo
 
 def test_issue786():
     assert limit(x*y + x*z, z, 2) == x*y+2*x
