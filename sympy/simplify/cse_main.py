80d259755e089fd96824ad3ab9055a16c825432f
Chris Smith <smichr@gmail.com>
2011-04-14 10:27:04 +0545

    cse watches for exact Mul and Add sub-expressions
        Adds and Muls that were not repeated subexpressions may still
        contain subexpresions that are repeated, e.g. x+y+z and x+y
        have x+y in common, but the cse process misses these.
    
        Now all such expressions are stored and processed after the
        main processing. set intersection of the args of all Adds are
        tested for repeated subexpressions. For Muls, intersection of
        the commutative args and longest sub-expression for the non-commutative
        args are sought.
    
        Since cse is working with exact sub-expressions, a subexpression
        like x + y will not be removed from x + 2*y, nor will x*y be removed
        from x*y**2.
    
        An insert function was added which has a dual purpose of checking to
        see if subtree has already been recorded and to keep the to_eliminate
        list sorted.

--- sympy/simplify/cse_main.py
+++ sympy/simplify/cse_main.py
@@ -1,9 +1,10 @@
 """ Tools for doing common subexpression elimination.
 """
 import bisect
+import difflib
 
-from sympy import Basic
-from sympy.utilities.iterables import preorder_traversal, numbered_symbols
+from sympy import Basic, Mul, Add
+from sympy.utilities.iterables import preorder_traversal, numbered_symbols, any
 
 import cse_opts
 
@@ -98,6 +99,8 @@ def cse(exprs, symbols=None, optimizations=None):
         # an actual iterator.
         symbols = iter(symbols)
     seen_subexp = set()
+    muls = set()
+    adds = set()
     to_eliminate = []
     to_eliminate_ops_count = []
 
@@ -113,22 +116,103 @@ def cse(exprs, symbols=None, optimizations=None):
     exprs = [preprocess_for_cse(e, optimizations) for e in exprs]
 
     # Find all of the repeated subexpressions.
+    def insert(subtree):
+        '''This helper will insert the subtree into to_eliminate while
+        maintaining the ordering by op count and will skip the insertion
+        if subtree is already present.'''
+        ops_count = subtree.count_ops()
+        index_to_insert = bisect.bisect(to_eliminate_ops_count, ops_count)
+        # all i up to this index have op count <= the current op count
+        # so check that subtree is not yet present from this index down
+        # (if necessary) to zero.
+        for i in xrange(index_to_insert - 1, -1, -1):
+            if to_eliminate_ops_count[i] == ops_count and \
+               subtree == to_eliminate[i]:
+                return # already have it
+        to_eliminate_ops_count.insert(index_to_insert, ops_count)
+        to_eliminate.insert(index_to_insert, subtree)
+
     for expr in exprs:
         pt = preorder_traversal(expr)
         for subtree in pt:
             if subtree.is_Atom:
                 # Exclude atoms, since there is no point in renaming them.
                 continue
-            elif subtree in seen_subexp:
-                if subtree not in to_eliminate:
-                    ops_count = subtree.count_ops()
-                    index_to_insert = bisect.bisect(to_eliminate_ops_count, ops_count)
 
-                    to_eliminate_ops_count.insert(index_to_insert, ops_count)
-                    to_eliminate.insert(index_to_insert, subtree)
+            if subtree in seen_subexp:
+                insert(subtree)
                 pt.skip()
+                continue
+
+            if subtree.is_Mul:
+                muls.add(subtree)
+            elif subtree.is_Add:
+                adds.add(subtree)
+
+            seen_subexp.add(subtree)
+
+    # process adds - any adds that weren't repeated might contain
+    # subpatterns that are repeated, e.g. x+y+z and x+y have x+y in common
+    adds = [set(a.args) for a in adds]
+    for i in xrange(len(adds)):
+        for j in xrange(i + 1, len(adds)):
+            com = adds[i].intersection(adds[j])
+            if len(com) > 1:
+                insert(Add(*com))
+
+                # remove this set of symbols so it doesn't appear again
+                adds[i] = adds[i].difference(com)
+                adds[j] = adds[j].difference(com)
+                for k in xrange(j + 1, len(adds)):
+                    if not com.difference(adds[k]):
+                        adds[k] = adds[k].difference(com)
+
+    # process muls - any muls that weren't repeated might contain
+    # subpatterns that are repeated, e.g. x*y*z and x*y have x*y in common
+
+    # use SequenceMatcher on the nc part to find the longest common expression
+    # in common between the two nc parts
+    sm = difflib.SequenceMatcher()
+
+    muls = [a.args_cnc() for a in muls]
+    for i in xrange(len(muls)):
+        if muls[i][1]:
+            sm.set_seq1(muls[i][1])
+        for j in xrange(i + 1, len(muls)):
+            # the commutative part in common
+            ccom = muls[i][0].intersection(muls[j][0])
+
+            # the non-commutative part in common
+            if muls[i][1] and muls[j][1]:
+                # see if there is any chance of an nc match
+                ncom = set(muls[i][1]).intersection(set(muls[j][1]))
+                if len(ccom) + len(ncom) < 2:
+                    continue
+
+                # now work harder to find the match
+                sm.set_seq2(muls[j][1])
+                i1, _, n = sm.find_longest_match(0, len(muls[i][1]),
+                                                 0, len(muls[j][1]))
+                ncom = muls[i][1][i1:i1 + n]
             else:
-                seen_subexp.add(subtree)
+                ncom = []
+
+            com = list(ccom) + ncom
+            if len(com) < 2:
+                continue
+
+            insert(Mul(*com))
+
+            # remove ccom from all if there was no ncom; to update the nc part
+            # would require finding the subexpr and then replacing it with a
+            # dummy to keep bounding nc symbols from being identified as a
+            # subexpr, e.g. removing B*C from A*B*C*D might allow A*D to be
+            # identified as a subexpr which would not be right.
+            if not ncom:
+                muls[i][0] = muls[i][0].difference(ccom)
+                for k in xrange(j, len(muls)):
+                    if not ccom.difference(muls[k][0]):
+                        muls[k][0] = muls[k][0].difference(ccom)
 
     # Substitute symbols for all of the repeated subexpressions.
     replacements = []
@@ -140,8 +224,6 @@ def cse(exprs, symbols=None, optimizations=None):
         for j, expr in enumerate(reduced_exprs):
             reduced_exprs[j] = expr.subs(subtree, sym)
         # Make the substitution in all of the subsequent substitutions.
-        # WARNING: modifying iterated list in-place! I think it's fine,
-        # but there might be clearer alternatives.
         for j in range(i+1, len(to_eliminate)):
             to_eliminate[j] = to_eliminate[j].subs(subtree, sym)
 
