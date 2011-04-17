b65d248bba9ec0de117f65bfc7c154d2fb36d510
Tom Bachmann <e_mc_h2@web.de>
2011-04-17 10:24:35 +0100

    C5) Remove XFAIL from things that now work.
    Apparently issue 835 has been fixed as a side effect.

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -256,7 +256,6 @@ def test_issue1447():
             else:
                 assert None
 
-@XFAIL
 def test_issue835():
     assert limit((1 + x**log(3))**(1/x), x, 0) == 1
     assert limit((5**(1/x) + 3**(1/x))**x, x, 0) == 5
