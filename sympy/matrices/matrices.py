c22c9d4b71f8aedc374b930bbfadc8453a13161b
Sherjil Ozair <sherjilozair@gmail.com>
2011-04-20 14:01:37 +0530

    Modifies is_diagonal to remove check for square.
    Non-square matrices can also be diagonal now. For square diagonal matrices, user will have to check for diagonal and square both.

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -1330,8 +1330,6 @@ class Matrix(object):
 
         See also: .is_lower(), is_upper() .is_diagonalizable()
         """
-        if not self.is_square:
-            return False
         for i in range(self.cols):
             for j in range(self.rows):
                 if i != j and self[i, j] != 0:
