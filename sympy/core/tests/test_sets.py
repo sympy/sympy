mattpap/sympy-polys

Added as_relational() method to Interval class
The new method rewrites an interval as a conjunction of
inequalities (or, in particular, as an inequality or an
equation, or a boolean), e.g.:

In [1]: a = Symbol('a', real=True)

In [2]: Interval(-2, 3).as_relational(a)
Out[2]: a ≤ 3 ∧ -2 ≤ a

In [3]: Interval(-oo, 3).as_relational(a)
Out[3]: a ≤ 3

In [4]: Interval(3, 3).as_relational(a)
Out[4]: a = 3

In [5]: Interval(-oo, oo).as_relational(a)
Out[5]: True

sympy/core/tests/test_sets.py
@@ -1,6 +1,6 @@
 from sympy import (
-    Symbol, Set, Union, Interval, oo, S,
-    Inequality, max_, min_, raises, And, Or
+    Symbol, Set, Union, Interval, oo, S, Inequality,
+    max_, min_, raises, And, Or, Eq, Le, Lt
 )
 from sympy.mpmath import mpi
 
@@ -183,3 +183,36 @@ def test_union_contains():
 def test_is_number():
     assert Interval(0, 1).is_number is False
     assert Set().is_number is False
+
+def test_Interval_is_point():
+    assert Interval(3, 3).is_point == True
+    assert Interval(3, 4).is_point == False
+
+def test_Interval_is_left_unbounded():
+    assert Interval(-oo, 3).is_left_unbounded == True
+    assert Interval(3, 4).is_left_unbounded == False
+
+def test_Interval_is_right_unbounded():
+    assert Interval(3, oo).is_right_unbounded == True
+    assert Interval(3, 4).is_right_unbounded == False
+
+def test_Interval_as_relational():
+    x = Symbol('x', real=True)
+
+    assert Interval(3, 3).as_relational(x) == Eq(x, 3)
+
+    assert Interval(-1, 2, False, False).as_relational(x) == And(Le(-1, x), Le(x, 2))
+    assert Interval(-1, 2, True, False).as_relational(x) == And(Lt(-1, x), Le(x, 2))
+    assert Interval(-1, 2, False, True).as_relational(x) == And(Le(-1, x), Lt(x, 2))
+    assert Interval(-1, 2, True, True).as_relational(x) == And(Lt(-1, x), Lt(x, 2))
+
+    assert Interval(-oo, 2, right_open=False).as_relational(x) == Le(x, 2)
+    assert Interval(-oo, 2, right_open=True).as_relational(x) == Lt(x, 2)
+
+    assert Interval(-2, oo, left_open=False).as_relational(x) == Le(-2, x)
+    assert Interval(-2, oo, left_open=True).as_relational(x) == Lt(-2, x)
+
+    assert Interval(-oo, oo).as_relational(x) == True
+
+    raises(ValueError, "Interval(-1, 2).as_relational(Symbol('x'))")
+

sympy/sympy

sympy/core/tests/test_sets.py
@@ -1,8 +1,7 @@
 from sympy import (
     Symbol, Set, Union, Interval, oo, S,
-    Inequality, Max, Min, And, Or
+    Inequality, Max, Min, And, Or, Eq, Le, Lt
 )
-from sympy.utilities.pytest import raises
 from sympy.mpmath import mpi
 
 from sympy.utilities.pytest import raises
@@ -186,3 +185,35 @@ def test_union_contains():
 def test_is_number():
     assert Interval(0, 1).is_number is False
     assert Set().is_number is False
+
+def test_Interval_is_point():
+    assert Interval(3, 3).is_point == True
+    assert Interval(3, 4).is_point == False
+
+def test_Interval_is_left_unbounded():
+    assert Interval(-oo, 3).is_left_unbounded == True
+    assert Interval(3, 4).is_left_unbounded == False
+
+def test_Interval_is_right_unbounded():
+    assert Interval(3, oo).is_right_unbounded == True
+    assert Interval(3, 4).is_right_unbounded == False
+
+def test_Interval_as_relational():
+    x = Symbol('x', real=True)
+
+    assert Interval(3, 3).as_relational(x) == Eq(x, 3)
+
+    assert Interval(-1, 2, False, False).as_relational(x) == And(Le(-1, x), Le(x, 2))
+    assert Interval(-1, 2, True, False).as_relational(x) == And(Lt(-1, x), Le(x, 2))
+    assert Interval(-1, 2, False, True).as_relational(x) == And(Le(-1, x), Lt(x, 2))
+    assert Interval(-1, 2, True, True).as_relational(x) == And(Lt(-1, x), Lt(x, 2))
+
+    assert Interval(-oo, 2, right_open=False).as_relational(x) == Le(x, 2)
+    assert Interval(-oo, 2, right_open=True).as_relational(x) == Lt(x, 2)
+
+    assert Interval(-2, oo, left_open=False).as_relational(x) == Le(-2, x)
+    assert Interval(-2, oo, left_open=True).as_relational(x) == Lt(-2, x)
+
+    assert Interval(-oo, oo).as_relational(x) == True
+
+    raises(ValueError, "Interval(-1, 2).as_relational(Symbol('x'))")