104d0e751f6839aab77dda2ff558340d900721df
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-04-23 10:03:45 +0530

    Fixed behaviour of atanh at infinities.

--- sympy/functions/elementary/hyperbolic.py
+++ sympy/functions/elementary/hyperbolic.py
@@ -703,6 +703,10 @@ class atanh(Function):
                 return S.Infinity
             elif arg is S.NegativeOne:
                 return S.NegativeInfinity
+            elif arg is S.Infinity:
+                return -S.ImaginaryUnit * C.atan(arg)
+            elif arg is S.NegativeInfinity:
+                return S.ImaginaryUnit * C.atan(-arg)
             elif arg.is_negative:
                 return -cls(-arg)
         else:
