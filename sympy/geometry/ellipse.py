mattpap/sympy-polys

Translated all old-style raise statements

sympy/geometry/ellipse.py
@@ -53,8 +53,8 @@ class Ellipse(GeometryEntity):
         eccentricity = sympify(eccentricity)
 
         if len(filter(None, (hradius, vradius, eccentricity))) != 2:
-            raise ValueError, 'Exactly two arguments between "hradius", '\
-                '"vradius", and "eccentricity" must be not None."'
+            raise ValueError('Exactly two arguments between "hradius", '\
+                '"vradius", and "eccentricity" must be not None."')
 
         if eccentricity is not None:
             if hradius is None:
@@ -431,3 +431,4 @@ class Circle(Ellipse):
             return list(set([Point(x,y), Point(x,-y), Point(-x,y), Point(-x,-y)]))
 
         return Ellipse.intersection(self, o)
+

sympy/sympy

sympy/geometry/ellipse.py
@@ -108,8 +108,8 @@ class Ellipse(GeometryEntity):
             center = Point(center)
 
         if len(filter(None, (hradius, vradius, eccentricity))) != 2:
-            raise ValueError, 'Exactly two arguments of "hradius", '\
-                '"vradius", and "eccentricity" must not be None."'
+            raise ValueError('Exactly two arguments of "hradius", '\
+                '"vradius", and "eccentricity" must not be None."')
 
         if eccentricity is not None:
             if hradius is None: