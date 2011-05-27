21e9193f6e437b4cd3265ed5219a573e8f72cb20
Jeremias Yehdegho <j.yehdegho@gmail.com>
2011-05-27 16:49:10 +0200

    Fix test_groebner

--- sympy/polys/tests/test_polytools.py
+++ sympy/polys/tests/test_polytools.py
@@ -2302,9 +2302,9 @@ def test_groebner():
     F = [x*y - 2*y, 2*y**2 - x**2]
 
     assert groebner(F, order='grevlex') == \
-        [-2*x**2 + x**3, -x**2 + 2*y**2, -2*y + x*y]
+        [y**3 - 2*y, x**2 - 2*y**2, x*y - 2*y]
     assert groebner(F, order='grevlex', field=True) == \
-        [-2*x**2 + x**3, -x**2/2 + y**2, -2*y + x*y]
+        [y**3 - 2*y, x**2 - 2*y**2, x*y - 2*y]
 
     assert groebner([1], x) == [1]
 
