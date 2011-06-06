0d0cd4eb3b33ac1f016e96aed5b33824e4ea96c7
Gilbert Gede <gilbertgede@gmail.com>
2011-06-05 17:42:55 -0700

    Added tests for DynamicSymbol

--- sympy/physics/classical/tests/test_kinematics.py
+++ sympy/physics/classical/tests/test_kinematics.py
@@ -277,6 +277,27 @@ def test_express3():
     assert C.x == express((cos(q3)*B.x - sin(q3)*B.z), C)
     assert C.z == express((sin(q3)*B.x + cos(q3)*B.z), C)
 
+def test_dynamicsymbol():
+    x = DynamicSymbol('x')
+    xd = DynamicSymbol('xd')
+    xdd = DynamicSymbol('xdd')
+    y = DynamicSymbol('y')
+    yd = DynamicSymbol('yd')
+    ydd = DynamicSymbol('ydd')
+    t = Symbol('t')
+    assert diff(x,t) == xd
+    assert diff(2 * x + 4, t) == 2 * xd
+    assert diff(2 * x + 4 + y, t) == 2 * xd + yd
+    assert diff(2 * x + 4 + y * x, t) == 2 * xd + x * yd + xd * y
+    assert diff(2 * x + 4 + y * x, x) == 2 + y
+    assert (diff(4 * x**2 + 3 * x + x * y, t) == 3 * xd + x * yd + xd * y +
+            8 * x * xd)
+    assert (diff(4 * x**2 + 3 * xd + x * y, t) ==  3 * xdd + x * yd + xd * y +
+            8 * x * xd)
+    assert diff(4 * x**2 + 3 * xd + x * y, xd) == 3
+    assert diff(4 * x**2 + 3 * xd + x * y, xdd) == 0
+
+
 """
 def test_ang_vel():
     A2 = N.orientnew('A2', 'Simple', q4, 2)
