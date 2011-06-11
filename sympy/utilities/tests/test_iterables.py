b725df4068c2e40afcde1222abff010461d6935d
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-06-11 22:34:14 +0530

    utilities/iterables: Added partitions function.
    Helped-by : Chris Smith <smichr@gmail.com>
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/utilities/tests/test_iterables.py
+++ sympy/utilities/tests/test_iterables.py
@@ -1,8 +1,8 @@
 from sympy import symbols, Integral, Tuple, Dummy, Basic
 from sympy.utilities.iterables import (postorder_traversal, preorder_traversal,
     flatten, group, take, subsets, variations, cartes, numbered_symbols,
-    dict_merge, prefixes, postfixes, sift, topological_sort, rotate_left, rotate_right,
-    multiset_partitions)
+    dict_merge, prefixes, postfixes, sift, topological_sort, rotate_left,
+    rotate_right, multiset_partitions, partitions)
 from sympy.core.singleton import S
 from sympy.functions.elementary.piecewise import Piecewise, ExprCondPair
 from sympy.utilities.pytest import raises
@@ -240,3 +240,17 @@ def test_multiset_partitions():
     assert list(multiset_partitions([1,2,3,4], 2)) == [[[1, 2, 3], [4]], [[1, 3], \
     [2, 4]], [[1], [2, 3, 4]], [[1, 2], [3, 4]], [[1, 2, 4], [3]], \
     [[1, 4], [2, 3]], [[1, 3, 4], [2]]]
+
+def test_partitions():
+    assert [p.copy() for p in partitions(6, k=2)] == [{2: 3}, \
+    {1: 2, 2: 2}, {1: 4, 2: 1}, {1: 6}]
+
+    assert [p.copy() for p in partitions(6, k=3)] == [{3: 2}, \
+    {1: 1, 2: 1, 3: 1}, {1: 3, 3: 1}, {2: 3}, {1: 2, 2: 2}, \
+    {1: 4, 2: 1}, {1: 6}]
+
+    assert [p.copy() for p in partitions(6, k=2, m=2)] == []
+
+    assert [p.copy() for p in partitions(8, k=4, m=3)] == [{4: 2},\
+    {1: 1, 3: 1, 4: 1}, {2: 2, 4: 1}, {2: 1, 3: 2}]
+
