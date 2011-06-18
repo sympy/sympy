9efa0bdbed3c998113361473c657089e0a43cd52
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-06-19 04:51:51 +0530

    combinatorics/graycode: Added tests for graycode.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

---
+++ sympy/combinatorics/tests/test_graycode.py
@@ -0,0 +1,20 @@
+from sympy.combinatorics.graycode import GrayCode, unrank_gray_code
+
+def test_graycode():
+    a = GrayCode(4)
+    assert len(list(a.generate_bitlist())) == 16
+    assert list(a.generate_bitlist(['0', '0', '1', '0'])) == \
+    [['0', '0', '1', '0'], ['0', '1', '1', '0'], ['0', '1', '1', '1'], \
+    ['0', '1', '0', '1'], ['0', '1', '0', '0'], ['1', '1', '0', '0'], \
+    ['1', '1', '0', '1'], ['1', '1', '1', '1'], ['1', '1', '1', '0'], \
+    ['1', '0', '1', '0'], ['1', '0', '1', '1'], ['1', '0', '0', '1'], \
+    ['1', '0', '0', '0']]
+
+    a = GrayCode(5, start = ['1','0','0','1','0'])
+    assert a.rank == 14
+    a = GrayCode(6, start = ['1','0','1','0','0','0'])
+    assert a.rank == 6
+
+    assert unrank_gray_code(4, 6)._current == \
+          ['0', '1', '1', '0', '0', '0']
+    assert unrank_gray_code(4, 6).rank == 4
