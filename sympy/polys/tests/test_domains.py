ee69051a190a9b62900e1f65c7d6f0bb5015c998
Mateusz Paprocki <mattpap@gmail.com>
2011-06-21 01:54:16 +0200

    Improved modular integer and added tests (#2504)
    In [1]: GF = FF(3)
    
    In [2]: GF(2)
    Out[2]: 2 mod 3
    
    In [3]: GF(GF(2))
    Out[3]: 2 mod 3
    
    In [4]: _ + 1
    Out[4]: 0 mod 3
    
    In [5]: _ + 1
    Out[5]: 1 mod 3
    
    In [6]: 2*_
    Out[6]: 2 mod 3
    
    In [7]: 2*_
    Out[7]: 1 mod 3

--- sympy/polys/tests/test_domains.py
+++ sympy/polys/tests/test_domains.py
@@ -7,11 +7,14 @@ from sympy.polys.domains import (
     ZZ, QQ, RR, PythonRationalType as Q, ZZ_sympy, QQ_sympy,
     RR_mpmath, RR_sympy, PolynomialRing, FractionField, EX)
 
+from sympy.polys.domains.modularinteger import ModularIntegerFactory
+
 from sympy.polys.polyerrors import (
     UnificationFailed,
     GeneratorsNeeded,
     GeneratorsError,
     CoercionFailed,
+    NotInvertible,
     DomainError)
 
 from sympy.polys.polyclasses import DMP, DMF
@@ -629,3 +632,106 @@ def test_RealDomain_from_sympy():
     raises(CoercionFailed, "RR.convert(x)")
     raises(CoercionFailed, "RR.convert(oo)")
     raises(CoercionFailed, "RR.convert(-oo)")
+
+def test_ModularInteger():
+    GF = ModularIntegerFactory(3)
+
+    a = GF(0)
+    assert isinstance(a, GF) and a == 0
+    a = GF(1)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)
+    assert isinstance(a, GF) and a == 2
+    a = GF(3)
+    assert isinstance(a, GF) and a == 0
+    a = GF(4)
+    assert isinstance(a, GF) and a == 1
+
+    a = GF(GF(0))
+    assert isinstance(a, GF) and a == 0
+    a = GF(GF(1))
+    assert isinstance(a, GF) and a == 1
+    a = GF(GF(2))
+    assert isinstance(a, GF) and a == 2
+    a = GF(GF(3))
+    assert isinstance(a, GF) and a == 0
+    a = GF(GF(4))
+    assert isinstance(a, GF) and a == 1
+
+    a = -GF(1)
+    assert isinstance(a, GF) and a == 2
+    a = -GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = 2 + GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2) + 2
+    assert isinstance(a, GF) and a == 1
+    a = GF(2) + GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2) + GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = 3 - GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(3) - 2
+    assert isinstance(a, GF) and a == 1
+    a = GF(3) - GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(3) - GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = 2*GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)*2
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)*GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)*GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = 2/GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)/2
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)/GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)/GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = 1 % GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(1) % 2
+    assert isinstance(a, GF) and a == 1
+    a = GF(1) % GF(2)
+    assert isinstance(a, GF) and a == 1
+    a = GF(1) % GF(2)
+    assert isinstance(a, GF) and a == 1
+
+    a = GF(2)**0
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)**1
+    assert isinstance(a, GF) and a == 2
+    a = GF(2)**2
+    assert isinstance(a, GF) and a == 1
+
+    assert bool(GF(3)) is False
+    assert bool(GF(4)) is True
+
+    GF = ModularIntegerFactory(5)
+
+    a = GF(1)**(-1)
+    assert isinstance(a, GF) and a == 1
+    a = GF(2)**(-1)
+    assert isinstance(a, GF) and a == 3
+    a = GF(3)**(-1)
+    assert isinstance(a, GF) and a == 2
+    a = GF(4)**(-1)
+    assert isinstance(a, GF) and a == 4
+
+    raises(NotInvertible, "GF(0)**(-1)")
+    raises(NotInvertible, "GF(5)**(-1)")
+
+    raises(ValueError, "ModularIntegerFactory(0)")
+    raises(ValueError, "ModularIntegerFactory(2.1)")
+    raises(TypeError, "ModularIntegerFactory(3, QQ)")
