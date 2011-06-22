c11a433f91b08f716d06a84b74a03422cd883812
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2011-06-22 23:15:33 +0200

    implement Levi-Civita symbol
    This replaces the already present Eijk and should provide a more general
    and probably faster implementation.

--- sympy/functions/special/tensor_functions.py
+++ sympy/functions/special/tensor_functions.py
@@ -1,5 +1,7 @@
+import operator
+from functools import reduce
 from sympy.core.function import Function
-from sympy.core import sympify, S
+from sympy.core import sympify, S, Integer
 
 ###############################################################################
 ###################### Kronecker Delta, Levi-Civita etc. ######################
@@ -38,3 +40,47 @@ class Eijk(Function):
             return S.NegativeOne
         elif i==j or j==k or k==i:
             return S.Zero
+
+def prod(a):
+    return reduce(operator.mul, a, 1)
+
+def eval_levicivita(*args):
+    """Evaluate Levi-Civita symbol."""
+    from sympy import factorial
+    n = len(args)
+    return prod(
+        prod(args[j] - args[i] for j in xrange(i + 1, n))
+        / factorial(i) for i in xrange(n))
+    # converting factorial(i) to int is slightly faster
+
+class LeviCivita(Function):
+    """Represent the Levi-Civita symbol.
+
+    For even permutations of indices it returns 1, for odd permutations -1, and
+    for everything else (a repeated index) it returns 0.
+
+    Thus it represents an alternating pseudotensor.
+
+    >>> from sympy import LeviCivita, symbols
+    >>> LeviCivita(1,2,3)
+    1
+    >>> LeviCivita(1,3,2)
+    -1
+    >>> LeviCivita(1,2,2)
+    0
+    >>> i,j,k = symbols('i j k')
+    >>> LeviCivita(i,j,k)
+    LeviCivita(i, j, k)
+    >>> LeviCivita(i,j,i)
+    0
+    """
+    @classmethod
+    def eval(cls, *args):
+        if all(isinstance(a, (int, Integer)) for a in args):
+            return eval_levicivita(*args)
+        if len(set(args)) < len(args):
+            return S.Zero
+
+    def doit(self):
+        return eval_levicivita(*self.args)
+
--- sympy/functions/special/tests/test_tensor_functions.py
+++ sympy/functions/special/tests/test_tensor_functions.py
@@ -1,4 +1,4 @@
-from sympy import symbols, Dij, Eijk
+from sympy import symbols, Dij, Eijk, LeviCivita
 
 x, y = symbols('x,y')
 
@@ -16,3 +16,18 @@ def test_Eijk():
     assert Eijk(1, 3, 1) == 0
     assert Eijk(1, x, x) == 0
     assert Eijk(x, y, x) == 0
+
+def test_levicivita():
+    assert LeviCivita(1, 2, 3) == 1
+    assert LeviCivita(1, 3, 2) == -1
+    assert LeviCivita(1, 2, 2) == 0
+    i,j,k = symbols('i j k')
+    assert LeviCivita(i, j, k) == LeviCivita(i,j,k, evaluate=False)
+    assert LeviCivita(i, j, i) == 0
+    assert LeviCivita(1, i, i) == 0
+    assert LeviCivita(i, j, k).doit() == (j - i)*(k - i)*(k - j)/2
+    assert LeviCivita(1, 2, 3, 1) == 0
+    assert LeviCivita(4, 5, 1, 2, 3) == 1
+    assert LeviCivita(4, 5, 2, 1, 3) == -1
+
+
