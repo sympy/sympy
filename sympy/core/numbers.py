24f1afc51b7c80e87d8b55bfbc84ea0bc3ea5118
Vladimir Perić <vlada.peric@gmail.com>
2011-07-12 11:29:02 +0200

    Define __round__ method for Floats
    In Python 2, the builtin round() method always returned a float. In
    Python 3, this has been changed: round(x) now delegates to x.__round__.
    Our Float class doesn't have a __round__ method, so add one. It simply
    casts to float before calling the builtin (based on the same code in
    mpmath).

--- sympy/core/numbers.py
+++ sympy/core/numbers.py
@@ -448,6 +448,9 @@ class Float(Number):
     def __hash__(self):
         return super(Float, self).__hash__()
 
+    def __round__(self, *args):
+        return round(float(self), *args)
+
     def epsilon_eq(self, other, epsilon="10e-16"):
         return abs(self - other) < Float(epsilon)
 
