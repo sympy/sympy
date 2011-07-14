65027c8e32064993b11b2d89ab46a99481d37f87
Nikhil Sarda <diff.operator@gmail.com>
2011-07-13 20:55:57 -0400

    combinatorics/prufer: Added conversion to and from tree and prufer code representation.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/prufer.py
+++ sympy/combinatorics/prufer.py
@@ -3,17 +3,36 @@ from sympy.core import Basic
 class Prufer(Basic):
     """
     The Prufer correspondence is an algorithm that describes the
-    bijection between labelled trees and the Prufer code.
+    bijection between labeled trees and the Prufer code. A Prufer
+    code of a labeled tree is unique upto isomorphism and has
+    a length of n - 2.
+    Prufer sequences were first used by Heinz Prufer to give a
+    proof of Cayley's formula.
     """
     _prufer_repr = None
+    _tree_repr = None
+    _nodes = None
 
     @property
     def prufer_repr(self):
+        if self._prufer_repr is None:
+            self._prufer_repr = self.to_prufer(self.tree_repr, self.nodes)
         return self._prufer_repr
 
-    def __new__(cls, *args, **kw_args):
+    @property
+    def tree_repr(self):
+        if self._tree_repr is None:
+            self._tree_repr = self.to_tree(self.prufer_repr)
+        return self._tree_repr
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @staticmethod
+    def to_prufer(tree, n):
         """
-        The constructor for the Prufer object.
+        Convert to Prufer code.
 
         Examples:
         >>> from sympy.combinatorics.prufer import Prufer
@@ -21,9 +40,6 @@ class Prufer(Basic):
         >>> a.prufer_repr
         [0, 0]
         """
-        ret_obj = Basic.__new__(cls, *args, **kw_args)
-        tree = args[0]
-        n = args[1]
         d = [0] * n
         L = [0] * n
         for edge in tree:
@@ -45,5 +61,50 @@ class Prufer(Basic):
             d[x] -= 1
             d[y] -= 1
             tree.remove(e)
-        ret_obj._prufer_repr = L[:2]
+        return L[:2]
+
+    @staticmethod
+    def to_tree(prufer):
+        """
+        Converts to tree representation.
+
+        Examples:
+        >>> from sympy.combinatorics.prufer import Prufer
+        >>> a = Prufer([0, 2], 4)
+        >>> a.tree_repr
+        [[3, 0], [1, 2], [2, 0]]
+        """
+        tree = []
+        prufer.append(0)
+        n = len(prufer) + 1
+        d = [1] * n
+        for i in xrange(0, n - 2):
+            d[prufer[i]] += 1
+        for i in xrange(0, n - 1):
+            x = n - 1
+            while d[x] != 1:
+                x -= 1
+            y = prufer[i]
+            d[x] -= 1
+            d[y] -= 1
+            tree.append([x, y])
+        return tree
+
+    def __new__(cls, *args, **kw_args):
+        """
+        The constructor for the Prufer object.
+
+        Examples:
+        >>> from sympy.combinatorics.prufer import Prufer
+        >>> a = Prufer([[0, 1], [0, 2], [0, 3]], 4)
+        >>> a.prufer_repr
+        [0, 0]
+        """
+        ret_obj = Basic.__new__(cls, *args, **kw_args)
+        if isinstance(args[0][0], list):
+            ret_obj._tree_repr = args[0]
+            ret_obj._nodes = args[1]
+        else:
+            ret_obj._prufer_repr = args[0]
+            ret_obj._nodes = len(ret_obj._prufer_repr) + 2
         return ret_obj
