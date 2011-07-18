a25484df6609b8105e3026d8ebafa91c4bfd815a
Vladimir Perić <vlada.peric@gmail.com>
2011-07-18 11:09:32 +0200

    polys/polytools: Use iterable() in __new__
    In Python 3, strings are iterable. This introduces an error when
    creating a Poly() object. Use iterable() from compatibility to avoid
    this, which ignores strings and dicts by default. As a dict can be used
    to make a Poly(), explicitly exclude only strings.

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -70,6 +70,8 @@ from sympy.polys.constructor import construct_domain
 
 from sympy.polys import polyoptions as options
 
+from sympy.core.compatibility import iterable
+
 class Poly(Expr):
     """Generic class for representing polynomial expressions. """
 
@@ -84,7 +86,7 @@ class Poly(Expr):
         if 'order' in opt:
             raise NotImplementedError("'order' keyword is not implemented yet")
 
-        if hasattr(rep, '__iter__'):
+        if iterable(rep, exclude=str):
             if isinstance(rep, dict):
                 return cls._from_dict(rep, opt)
             else:
