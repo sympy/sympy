a1f8a6b8087713c04eb143e01201d8ac13355a8e
Mateusz Paprocki <mattpap@gmail.com>
2011-07-25 04:50:03 +0200

    Fixed handling of corner cases in dmp_cancel()
    In [1]: Poly(0, x).cancel(Poly(0, x))
    Out[1]: (1, Poly(0, x, domain='ZZ'), Poly(0, x, domain='ZZ'))
    
    In [2]: Poly(0, x).cancel(Poly(x, x))
    Out[2]: (1, Poly(0, x, domain='ZZ'), Poly(1, x, domain='ZZ'))
    
    In [3]: Poly(x, x).cancel(Poly(0, x))
    Out[3]: (1, Poly(1, x, domain='ZZ'), Poly(0, x, domain='ZZ'))
    
    In [4]: Poly(0, x).cancel(Poly(x/2, x))
    Out[4]: (2, Poly(0, x, domain='QQ'), Poly(1, x, domain='QQ'))
    
    In [5]: Poly(x/2, x).cancel(Poly(0, x))
    Out[5]: (1/2, Poly(1, x, domain='QQ'), Poly(0, x, domain='QQ'))

--- sympy/polys/tests/test_euclidtools.py
+++ sympy/polys/tests/test_euclidtools.py
@@ -610,6 +610,15 @@ def test_dup_cancel():
     assert dup_cancel(f, g, ZZ) == (f, g)
     assert dup_cancel(F, G, ZZ) == (f, g)
 
+    assert dup_cancel([], [], ZZ) == ([], [])
+    assert dup_cancel([], [], ZZ, include=False) == (ZZ(1), ZZ(1), [], [])
+
+    assert dup_cancel([ZZ(1), ZZ(0)], [], ZZ) == ([ZZ(1)], [])
+    assert dup_cancel([ZZ(1), ZZ(0)], [], ZZ, include=False) == (ZZ(1), ZZ(1), [ZZ(1)], [])
+
+    assert dup_cancel([], [ZZ(1), ZZ(0)], ZZ) == ([], [ZZ(1)])
+    assert dup_cancel([], [ZZ(1), ZZ(0)], ZZ, include=False) == (ZZ(1), ZZ(1), [], [ZZ(1)])
+
 def test_dmp_cancel():
     f = ZZ.map([[2], [0], [-2]])
     g = ZZ.map([[1], [-2], [1]])
@@ -619,3 +628,12 @@ def test_dmp_cancel():
 
     assert dmp_cancel(f, g, 1, ZZ) == (p, q)
     assert dmp_cancel(f, g, 1, ZZ, include=False) == (ZZ(1), ZZ(1), p, q)
+
+    assert dmp_cancel([[]], [[]], 1, ZZ) == ([[]], [[]])
+    assert dmp_cancel([[]], [[]], 1, ZZ, include=False) == (ZZ(1), ZZ(1), [[]], [[]])
+
+    assert dmp_cancel([[ZZ(1), ZZ(0)]], [[]], 1, ZZ) == ([[ZZ(1)]], [[]])
+    assert dmp_cancel([[ZZ(1), ZZ(0)]], [[]], 1, ZZ, include=False) == (ZZ(1), ZZ(1), [[ZZ(1)]], [[]])
+
+    assert dmp_cancel([[]], [[ZZ(1), ZZ(0)]], 1, ZZ) == ([[]], [[ZZ(1)]])
+    assert dmp_cancel([[]], [[ZZ(1), ZZ(0)]], 1, ZZ, include=False) == (ZZ(1), ZZ(1), [[]], [[ZZ(1)]])
