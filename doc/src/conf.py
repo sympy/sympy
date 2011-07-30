fe4befc8275c88bc23790538bd58a7113019284c
Aaron Meurer <asmeurer@gmail.com>
2011-07-29 22:22:17 -0600

    Start of the 0.7.2 release cycle

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -48,7 +48,7 @@ copyright = '2008, 2009, 2010, 2011 SymPy Development Team'
 # The short X.Y version.
 version = '0.7.1'
 # The full version, including alpha/beta/rc tags.
-release = '0.7.1'
+release = '0.7.1-git'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
