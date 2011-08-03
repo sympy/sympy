e14007d36e4c4cc336c0d348ac2565fe152fa449
hector <hector1618@gmail.com>
2011-08-03 14:11:12 +0530

    Stripped whitespace

--- sympy/functions/combinatorial/numbers.py
+++ sympy/functions/combinatorial/numbers.py
@@ -67,17 +67,17 @@ class fibonacci(Function):
     @staticmethod
     def _fib(n):
         """
-        The governing formula for this function is 
+        The governing formula for this function is
             F_{m + n} = F_{m} * F_{n + 1} + F_{m - 1} * F_{n}
 
         Any number N uniquely be written as N = m + n, where m is non-negative and n = 2^k and N < 2^(k + 1).
-        We generate only those Fibonacci numbers which will be used to calculate F_m and F_n and hence the algorithm runs in O(ln(n)) time. 
+        We generate only those Fibonacci numbers which will be used to calculate F_m and F_n and hence the algorithm runs in O(ln(n)) time.
 
         References and further reading
         ==============================
             * The Art of Computer Programming Vol.1 Fundamental Algorithms by Donald E. Knuth.
               1.2.8 Fibonacci Numbers
- 
+
         """
         a, b, p, q = 1, 0, 0, 1
         while n:
@@ -86,7 +86,7 @@ class fibonacci(Function):
                 n -= 1
             else:
                 p, q = p*p + q*q, q*q + 2*p*q
-                n //= 2 
+                n //= 2
         return b
 
     @staticmethod
