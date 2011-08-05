007bf869c1fccdbe81412c6df82250b20fa8f306
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-08-04 23:44:15 -0400

    combinatorics/partitions: Added integer partition as a class and added facility to compare instances of integer partitions.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/__init__.py
+++ sympy/combinatorics/__init__.py
@@ -1,4 +1,4 @@
 from sympy.combinatorics.partitions import Partition
 from sympy.combinatorics.permutations import Permutation
 from sympy.combinatorics.prufer import Prufer
-
+from sympy.combinatorics.partitions import Partition, IntegerPartition
