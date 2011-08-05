96a218f87a7f34e42b745dd08f07b6f908e9387a
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-08-04 23:45:09 -0400

    combinatorics/partitions: Added a routine to generate a random partition.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/partitions.py
+++ sympy/combinatorics/partitions.py
@@ -1,5 +1,7 @@
 from sympy.core import Basic
 
+import random
+
 class Partition(Basic):
     """
     This class represents an abstract partition.
@@ -181,3 +183,121 @@ class Partition(Basic):
         Examples:
         """
         raise NotImplementedError()
+
+class IntegerPartition(Partition):
+    """
+    This class represents an abstract partition.
+    A partition is a set of disjoint sets whose
+    union equals a given set.
+    """
+
+    def next(self):
+        """
+        Generates the next partition.
+
+        Examples:
+        """
+        raise NotImplementedError()
+
+    def previous(self):
+        """
+        Generates the previous partition.
+
+        Examples:
+        """
+        raise NotImplementedError()
+
+    @property
+    def size(self):
+        """
+        Gets the size of the partition.
+
+        Examples:
+        """
+        return len(self.args[0])
+
+    @property
+    def partition_set(self):
+        """
+        Gets the set of the partition.
+
+        Examples:
+        """
+        return self.args[1]
+
+    @property
+    def partition_array(self):
+        """
+        Gets the array of partitions from the
+        partition object
+
+        Examples:
+        """
+        return self.args[0]
+
+    def __new__(cls, *args, **kw_args):
+        """
+        Generates a new partition object.
+        It also verifies if the arguments passed are
+        valid and if it is found that they are not then
+        an exception is raised.
+
+        Examples:
+        """
+        partition = args[0]
+        integer_rep = args[1]
+
+        if not isinstance(partition, list) or sum(partition) != integer_rep:
+            raise ValueError("The partition is not valid")
+
+        obj = Basic.__new__(cls, *args, **kw_args)
+        return obj
+
+    def _compare(self, other):
+        """
+        Compares two partitions.
+
+        The basis for comparison of two integer partitions is the
+        majorizing concept.
+
+        Examples:
+        >>> from sympy.combinatorics.partitions import IntegerPartition
+        >>> a = IntegerPartition([1,1,1,1,1], 5)
+        >>> b = IntegerPartition([1,1,1,2], 5)
+        >>> a.compare(b)
+        -1
+        >>> a < b
+        True
+        """
+        k = min(self.size, other.size)
+        val_self = 0
+        val_other = 0
+        for i in xrange(k):
+            val_self += self.partition_array[i]
+            val_other += other.partition_array[i]
+            if val_self > val_other:
+                return 1
+            elif val_self < val_other:
+                return -1
+        return 1
+
+    @property
+    def rank(self):
+        """
+        Gets the rank of a partition.
+
+        Examples:
+        """
+        raise NotImplementedError()
+
+def random_integer_partition(n):
+    """
+    Generates a random integer partition.
+    """
+    partition = []
+    while(n > 0):
+        k = random.randint(1, n)
+        partition.append(k)
+        n -= k
+    list.sort(partition)
+    return partition
