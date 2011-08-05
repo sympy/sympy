377e8c0a1484579a0804973c279ce76d0b6f0015
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-08-04 23:41:39 -0400

    combinatorics/partitions: First commit. Added init file.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/__init__.py
+++ sympy/combinatorics/__init__.py
@@ -1,4 +1,6 @@
-from sympy.combinatorics.permutations import Permutation
+from sympy.combinatorics.permutations import Permutation, josephus
 from sympy.combinatorics.prufer import Prufer
 from sympy.combinatorics.generators import cyclic, alternating, symmetric, dihedral
 from sympy.combinatorics.subsets import Subset
+from sympy.combinatorics.partitions import Partition
+
