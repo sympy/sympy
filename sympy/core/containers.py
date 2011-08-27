448f6cd2c418d6484050609f8c903a6e953e9314
Chris Smith <smichr@gmail.com>
2011-08-27 10:19:31 +0545

    edit docstring and sympify query args

--- sympy/core/containers.py
+++ sympy/core/containers.py
@@ -1,9 +1,9 @@
-"""Module for Sympy containers
+"""Module for SymPy containers
 
-    (Sympy objects that store other Sympy objects)
+    (SymPy objects that store other SymPy objects)
 
     The containers implemented in this module are subclassed to Basic.
-    They are supposed to work seamlessly within the Sympy framework.
+    They are supposed to work seamlessly within the SymPy framework.
 """
 
 from basic import Basic
@@ -15,7 +15,7 @@ class Tuple(Basic):
     Wrapper around the builtin tuple object
 
     The Tuple is a subclass of Basic, so that it works well in the
-    Sympy framework.  The wrapped tuple is available as self.args, but
+    SymPy framework.  The wrapped tuple is available as self.args, but
     you can also access elements or slices with [:] syntax.
 
     >>> from sympy import symbols
@@ -114,36 +114,47 @@ class Dict(Basic):
     """
     Wrapper around the builtin dict object
 
-    The Dict is a subclass of Basic.
-    This means so that it works well in the Sympy framework and that it
-    is immutable. With the exception of changing and setting values it behaves
-    identically to the python dict.
+    The Tuple is a subclass of Basic, so that it works well in the
+    SymPy framework.  Because it is immutable, it may be included
+    in sets, but its values must all be given at instantiation and
+    cannot be changed afterwards.  Otherwise it behaves identically
+    to the Python dict.
 
+    >>> from sympy import S
     >>> from sympy.core.containers import Dict
 
     >>> D = Dict({1:'one', 2:'two'})
-    >>> D[1]
-    one
     >>> for k, v in D: print k, v
     1 one
     2 two
+
+    The args are sympified so the 1 and 2 are Integers and the values
+    are Symbols. Queries automatialy sympify args so the following work:
+    >>> 1 in D
+    True
+    >>> D.has('one') # searches keys and values
+    True
+    >>> 'one' in D # not in the keys
+    False
+    >>> D[1]
+    one
 
     """
 
     def __new__(cls, *args):
         if len(args)==1 and args[0].__class__ is dict:
-            items = [Tuple(k, v) for k, v in args[0].items()]
-        elif iterable(args) and all(len(arg)==2 for arg in args):
+            items = [Tuple(k, v) for k, v in args[0].iteritems()]
+        elif iterable(args) and all(len(arg) == 2 for arg in args):
             items = [Tuple(k, v) for k, v in args]
         else:
-            raise TypeError('Pass Dict args as Dict((k1, v1), (k2, v2), ...)')
+            raise TypeError('Pass Dict args as Dict((k1, v1), ...) or Dict({k1: v1, ...})')
         obj = Basic.__new__(cls, *items)
-        obj._dict = dict(items) # In case Tuple decides it wants to Sympify
+        obj._dict = dict(items) # In case Tuple decides it wants to sympify
         return obj
 
     def __getitem__(self, key):
         """x.__getitem__(y) <==> x[y]"""
-        return self._dict[key]
+        return self._dict[sympify(key)]
 
     def __setitem__(self, key, value):
         raise NotImplementedError("SymPy Dicts are Immutable")
@@ -185,13 +196,12 @@ class Dict(Basic):
 
     def get(self, key, default):
         '''D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.'''
-        return self._dict.get(key, default)
+        return self._dict.get(sympify(key), default)
 
     def has_key(self, key):
         '''D.has_key(k) -> True if D has a key k, else False'''
-        return self._dict.has_key(key)
+        return self._dict.has_key(sympify(key))
 
     def __contains__(self, key):
         '''D.__contains__(k) -> True if D has a key k, else False'''
-        return self.has_key(key)
-
+        return self.has_key(sympify(key))
