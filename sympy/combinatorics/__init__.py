5ff8d17b0c13c5f0ce3831e613a3460587ca3ee4
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2011-09-04 16:36:57 +0800

    fixed implicit imports errors.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/__init__.py
+++ sympy/combinatorics/__init__.py
@@ -1,6 +1,4 @@
-from sympy.combinatorics.permutations import Permutation, josephus
 from sympy.combinatorics.prufer import Prufer
 from sympy.combinatorics.generators import cyclic, alternating, symmetric, dihedral
 from sympy.combinatorics.subsets import Subset
-from sympy.combinatorics.partitions import Partition, IntegerPartition
-
+from sympy.combinatorics.partitions import (Partition, IntegerPartition, RGS_rank, RGS_unrank, RGS_enum, from_RGS)
