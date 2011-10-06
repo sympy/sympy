3281fb2be040f5db6d92fb00fc79db6d47ab9df2
Miha Marolt <tloramus@gmail.com>
2011-10-06 21:31:30 +0200

    Don't use numpydoc to list class methods.
    In Sympy documentation class methods are listed with :members: option to the
    autoclass::,
    
    	.. autoclass:: ClassName
    	   :members:,
    
    so there is no need to use numpydoc extension for this.

--- doc/ext/docscrape_sphinx.py
+++ doc/ext/docscrape_sphinx.py
@@ -190,8 +190,7 @@ class SphinxDocString(NumpyDocString):
         out += self._str_section('Notes')
         out += self._str_references()
         out += self._str_examples()
-        for param_list in ('Attributes', 'Methods'):
-            out += self._str_member_list(param_list)
+        out += self._str_member_list('Attributes')
         out = self._str_indent(out,indent)
         return '\n'.join(out)
 
