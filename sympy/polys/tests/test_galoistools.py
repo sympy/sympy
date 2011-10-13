ec1238effc664e10baac171c0ad3f9b71e356b14
Chris Smith <smichr@gmail.com>
2011-10-13 21:07:27 +0545

    merge csolve_primepower into csolve_prime
        The results of gf_solve and csolve_primefactor are now sorted, too.

--- sympy/polys/tests/test_galoistools.py
+++ sympy/polys/tests/test_galoistools.py
@@ -23,7 +23,7 @@ from sympy.polys.galoistools import (
     gf_edf_zassenhaus, gf_edf_shoup,
     gf_berlekamp, gf_zassenhaus, gf_shoup,
     gf_factor_sqf, gf_factor,
-    gf_value, linear_congruence, csolve_prime, csolve_primepower, gf_csolve
+    gf_value, linear_congruence, csolve_prime, gf_csolve
 )
 
 from sympy.polys.polyerrors import (
@@ -786,15 +786,15 @@ def test_gf_csolve():
     assert linear_congruence(0, 5, 5) == [0, 1, 2, 3, 4]
     assert linear_congruence(3, 12, 15) == [4, 9, 14]
     assert linear_congruence(6, 0, 18) == [0, 3, 6, 9, 12, 15]
-
+    # with power = 1
     assert csolve_prime([1, 3, 2, 17], 7) == [3]
     assert csolve_prime([1, 3, 1, 5], 5) == [0, 1]
     assert csolve_prime([3, 6, 9, 3], 3) == [0, 1, 2]
+    # with power > 1
+    assert csolve_prime([1, 1, 223], 3, 4) == [4, 13, 22, 31, 40, 49, 58, 67, 76]
+    assert csolve_prime([3, 5, 2, 25], 5, 3) == [16, 50, 99]
+    assert csolve_prime([3, 2, 2, 49], 7, 3) == [147, 190, 234]
 
-    assert csolve_primepower([1, 1, 223], 3, 4) == [76, 49, 22, 67, 40, 13, 58, 31, 4]
-    assert csolve_primepower([3, 5, 2, 25], 5, 3) == [99, 16, 50]
-    assert csolve_primepower([3, 2, 2, 49], 7, 3) == [234, 190, 147]
-
-    assert gf_csolve([1, 1, 7], 189) == [76, 49, 13, 175, 139, 112]
+    assert gf_csolve([1, 1, 7], 189) == [13, 49, 76, 112, 139, 175]
     assert gf_csolve([1, 3, 4, 1, 30], 60) ==  [10, 30]
     assert gf_csolve([1, 1, 7], 15) == []
