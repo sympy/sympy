44ee00109730ae42aa5ad210aed0960d3c3e02ea
rayman <rayman@CoolGuyJr>
2011-10-21 16:49:56 -0700

    Thinking about defining a class to contain identities and a set of identities.  Still need to refactor the code in interative_identity_search.

--- sympy/physics/quantum/identitysearch.py
+++ sympy/physics/quantum/identitysearch.py
@@ -1,11 +1,30 @@
+from sets import Set
+
 from sympy import Mul
 from sympy.matrices import Matrix, eye
 from sympy.physics.quantum.gate import X, Y, Z, H, S, T, CNOT, IdentityGate, gate_simp
 from sympy.physics.quantum.represent import represent
 
+# Possible inherit Python List
+class GateIdentity():
+    
+# Save the identities to a set
+class GateIdentitySet(Set):
+    def write_to_file(filename, append=False):
+        '''Write gate identities to a specified file.'''
+
+        # File is truncated unless given a flag to not truncate.
+        identities = open(filename, 'a') if append else open(filename, 'w+')
+        identities.write(identity+'\n')
+        identities.close()
+
 # Number of qubits in the spaces (to be operated on).
 def default_numqubits():
     return 2
+
+# Number base the counter will use or number of gates possible in given space.
+def base(gate_list):
+    return len(gate_list)
 
 # Dynamically generate and concatenate a list of all possible sympy gate objects in given space.
 def construct_gate_list(numqubits):
@@ -21,10 +40,6 @@ def construct_gate_list(numqubits):
 
     return gate_list
 
-# Number base the counter will use or number of gates possible in given space.
-def base(gate_list):
-    return len(gate_list)
-
 # Dynamically generate and concatenate a list of all possible scipy.sparse gate matrices in given space.
 def construct matrix_list(numqubits):
     xs = [represent(X(i), nqubits=numqubits, format='scipy.sparse') for i in xrange(numqubits)]
@@ -39,13 +54,9 @@ def construct matrix_list(numqubits):
 
     return matrix_list
 
-# Create the first instance of the number list
-def initial_num(base):
-    return [i for i in range(base)]
-
-# Default number list has 2 gates
-def default_num():
-    return initial_num(default_numqubits)
+# Initial number list - start identity search with 2 gates
+def initial_num():
+    return [0, 1]
 
 def count(base, number):
     # Uses a list of numbers (as digits) to count in a specified base.
@@ -89,27 +100,6 @@ def is_scalar_matrix(matrix):
             return True
     return False
 
-# Initialize the log file.
-filename = '/home/matt/Documents/gate_identities/' + str(numqubits) + '-qubit_identities.txt'
-f = open(filename, 'a')
-f.close()
-
-# Save the identities to a set
-
-def write_identity(name, identity):
-    # Writes gate identity to specified file.
-    identities = open(name, 'a')
-    identities.write(identity+'\n')
-    identities.close()
-
-def check_identity(name, identity):
-    # Checks if gate identity is (not) already in specified file.
-    identities = open(name, 'r')
-    id_list = identities.readlines()
-    if identity+'\n' in id_list:
-        return False
-    return True
-
 def iterative_identity_search(numqubits):
     # Begin searching for gate identities in given space.
 
@@ -117,15 +107,19 @@ def iterative_identity_search(numqubits):
     gate_list = construct_gate_list(numqubits)
     matrix_list = construct_matrix_list(numqubits)
 
+    num = initial_num()
+
     # Iteratively search for identities by increasing the
     # number of gates in the list
     while True:
         if num[0] is 0:
             matrices = number_to_matrices(num[1:], matrix_list)
             cached = matrix_mul(matrices)
+
         gates = number_to_gates(num, gate_list)
+
         if len(gate_simp(Mul(*gates)).args) == len(gates):
-            circuit = number_to_matrices(num[0])*cached
+            circuit = number_to_matrices(num[0], matrix_list)*cached
             if is_scalar_matrix(circuit) and check_identity(filename, str(gate_simp(Mul(*gates)).args)):
                 write_identity(filename, str(gate_simp(Mul(*gates)).args))
                 print num
@@ -133,3 +127,6 @@ def iterative_identity_search(numqubits):
                 print circuit
                 print ''
         count(base, num)
+
+def random_identity_search(numqubits):
+
