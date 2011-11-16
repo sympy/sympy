0f8b47de35d5fec209e86e1e138be68f3e628167
Ronan Lamy <Ronan.Lamy@normalesup.org>
2011-11-16 14:18:35 +0000

    pytest: clear the cache after each test

--- sympy/conftest.py
+++ sympy/conftest.py
@@ -1,7 +1,12 @@
 import sys
 sys._running_pytest = True
 
+from sympy.core.cache import clear_cache
+
 def pytest_terminal_summary(terminalreporter):
     if (terminalreporter.stats.get('error', None) or
             terminalreporter.stats.get('failed', None)):
         terminalreporter.write_sep(' ', 'DO *NOT* COMMIT!', red=True, bold=True)
+
+def pytest_runtest_teardown():
+    clear_cache()
