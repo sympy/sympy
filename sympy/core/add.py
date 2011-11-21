5ef22bb8efde751001d516f6653f683a00cd66b1
Matt Habel <habelinc@gmail.com>
2011-11-21 17:08:09 -0500

    Revert "Fixed errors reported by pyflakes in galgebra module. One is commented because I have no clue of the original intentions"
    This reverts commit 013fecfd025419ef39e4439eb1ae5a2ac14610a6.

--- sympy/core/add.py
+++ sympy/core/add.py
@@ -200,7 +200,13 @@ class Add(AssocOp):
 
     @classmethod
     def class_key(cls):
-        """Nice order of classes"""
+        """
+        Nice order of classes
+        
+        **Example**
+            >>> (7 + 9*x).class_key()
+            (3, 1, Add)
+        """
         return 3, 1, cls.__name__
 
     @cacheit
@@ -211,9 +217,10 @@ class Add(AssocOp):
             y is a tuple in the form of (num * var, num * var, ....) where num is the coefficients of all things multipled by var
 
         **Examples**
-            >>> from sympy.abc import x, y
             >>> (7*x + 8*y*x + 19*x + 17*y + 7).as_coeff_add()
             (7, (8*x*y, 17*y, 26*x))
+            >>> (7*x + 8*y + 19*x + 17*y + 7 + 9).as_coeff_add()
+            (16, (25*y, 26*x))
         """
         if deps:
             l1 = []
@@ -476,6 +483,13 @@ class Add(AssocOp):
     def as_real_imag(self, deep=True):
         """
         returns a tuple represeting a complex numbers
+            
+            **Examples**
+
+            >>> (7 + 9*I).as_real_imag()
+            (7, 9)
+            >>> Add(10*I + 7).as_real_imag()
+            (7, 10)
         """
         sargs, terms = self.args, []
         re_part, im_part = [], []
