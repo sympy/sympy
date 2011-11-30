f29daece85fa1245d36e6795acb46c3d97a9422b
Matt Habel <habelinc@gmail.com>
2011-11-29 21:32:17 -0800

    import functions from combinatorial

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -63,7 +63,7 @@ from integrals import *
 from tensor import *
 from parsing import *
 # Adds about .04-.05 seconds of import time
-# from combinatorics import * 
+#from combinatorics import * 
 # This module is slow to import:
 #from physics import units
 from plotting import Plot, textplot
