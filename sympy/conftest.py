4bd930b8bc6410a9966327c8e73e0b1849c71157
Ronan Lamy <Ronan.Lamy@normalesup.org>
2011-12-02 06:35:21 +0000

    Add more info to pytest header

--- sympy/conftest.py
+++ sympy/conftest.py
@@ -3,6 +3,15 @@ sys._running_pytest = True
 
 from sympy.core.cache import clear_cache
 
+def pytest_report_header(config):
+    from sympy.utilities.misc import ARCH
+    s = "architecture: %s\n" % ARCH
+    from sympy.core.cache import USE_CACHE
+    s += "cache:        %s\n" % USE_CACHE
+    from sympy.polys.domains import GROUND_TYPES
+    s += "ground types: %s\n" % GROUND_TYPES
+    return s
+
 def pytest_terminal_summary(terminalreporter):
     if (terminalreporter.stats.get('error', None) or
             terminalreporter.stats.get('failed', None)):
