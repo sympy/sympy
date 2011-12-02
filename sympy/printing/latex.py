4ad4305f968fc30d6e23af68ae764f06ab5e2006
Tiffany Zhu <bubble.wubble.303@gmail.com>
2011-12-01 16:39:07 -0800

    2861; add even more documentation

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -1027,54 +1027,75 @@ class LatexPrinter(Printer):
 def latex(expr, **settings):
     r"""
     Convert the given expression to LaTeX representation.
-    
-    Valid setting keywords:
-      
-    	order: Any of the supported monomial orderings (currently "lex", "grlex", or "grevlex"), "old", and "none". This parameter does nothing for Mul objects. Setting order to "old" uses the compatibility ordering for Add defined in Printer. For very large expressions, set the 'order' keyword to 'none' if speed is a concern.
-      
-      mode: Specifies how the generated code will be delimited. 'mode' can be one of 'plain', 'inline', 'equation' or 'equation*'.  If 'mode' is set to 'plain', then the resulting code will not be delimited at all (this is the default). If 'mode' is set to 'inline' then inline LaTeX $ $ will be used.  If 'mode' is set to 'equation' or 'equation*', the resulting code will be enclosed in the 'equation' or 'equation*' environment (remember to import 'amsmath' for 'equation*'), unless the 'itex' option is set. In the latter case, the $$ $$ syntax is used. 
-      
-      itex: Specifies if itex-specific syntax is used, including emitting $$ $$.
-      
-      fold_frac_powers: Emit "^{p/q}" instead of "^{\frac{p}{q}}" for fractional powers.
-      
-      fold_func_brackets: Fold function brackets where applicable.
-      
-      mul_symbol: The symbol to use for multiplication. Can be one of None, "ldot", "dot", or "times".
-      
-      inv_trig_style: How inverse trig functions should be displayed. Can be one of "abbreviated", "full", or "power".
-      
-      mat_str: Which matrix environment string to emit. "smallmatrix", "bmatrix", etc. # This doesn't currently work
-      
-      mat_delim: The delimiter to wrap around matrices. Can be one of "[", "(". # This doesn't currently work.
-      
-      symbol_names: Dictionary of symbols and the custom strings they should be emitted as. 
-      Example: 
-      >>> latex(x**2, symbol_names={x:'x_i'})
-      'x_i^{2}'
-
-    Usage examples:
       
     >>> from sympy import latex, Rational
     >>> from sympy.abc import x, y, mu, tau
-
+    
     >>> latex((2*tau)**Rational(7,2))
     '8 \\sqrt{2} \\tau^{\\frac{7}{2}}'
 
+    order: Any of the supported monomial orderings (currently "lex", "grlex", or "grevlex"), "old", and "none". This parameter does nothing for Mul objects. Setting order to "old" uses the compatibility ordering for Add defined in Printer. For very large expressions, set the 'order' keyword to 'none' if speed is a concern.
+
+
+    mode: Specifies how the generated code will be delimited. 'mode' can be one of 'plain', 'inline', 'equation' or 'equation*'.  If 'mode' is set to 'plain', then the resulting code will not be delimited at all (this is the default). If 'mode' is set to 'inline' then inline LaTeX $ $ will be used.  If 'mode' is set to 'equation' or 'equation*', the resulting code will be enclosed in the 'equation' or 'equation*' environment (remember to import 'amsmath' for 'equation*'), unless the 'itex' option is set. In the latter case, the $$ $$ syntax is used.
+    
     >>> latex((2*mu)**Rational(7,2), mode='plain')
     '8 \\sqrt{2} \\mu^{\\frac{7}{2}}'
-
+    
     >>> latex((2*tau)**Rational(7,2), mode='inline')
     '$8 \\sqrt{2} \\tau^{\\frac{7}{2}}$'
-
+    
     >>> latex((2*mu)**Rational(7,2), mode='equation*')
     '\\begin{equation*}8 \\sqrt{2} \\mu^{\\frac{7}{2}}\\end{equation*}'
-
+    
     >>> latex((2*mu)**Rational(7,2), mode='equation')
     '\\begin{equation}8 \\sqrt{2} \\mu^{\\frac{7}{2}}\\end{equation}'
 
+    itex: Specifies if itex-specific syntax is used, including emitting $$ $$.
+    
     >>> latex((2*mu)**Rational(7,2), mode='equation', itex=True)
     '$$8 \\sqrt{2} \\mu^{\\frac{7}{2}}$$'
+
+    fold_frac_powers: Emit "^{p/q}" instead of "^{\frac{p}{q}}" for fractional powers.
+    
+    >>> latex((2*tau)**Rational(7,2), fold_frac_powers=True)
+    8 \sqrt{2} \tau^{7/2}
+
+    fold_func_brackets: Fold function brackets where applicable.
+    
+    >>> latex((2*tau)**sin(Rational(7,2)))
+    \left(2 \tau\right)^{\sin{\left (\frac{7}{2} \right )}}
+    >>> latex((2*tau)**sin(Rational(7,2)), fold_func_brackets = True)
+    \left(2 \tau\right)^{\sin {\frac{7}{2}}}
+
+    mul_symbol: The symbol to use for multiplication. Can be one of None, "ldot", "dot", or "times".
+      
+    >>> latex((2*tau)**sin(Rational(7,2)), mul_symbol="times")
+    \left(2 \times \tau\right)^{\sin{\left (\frac{7}{2} \right )}}
+
+    inv_trig_style: How inverse trig functions should be displayed. Can be one of "abbreviated", "full", or "power". Defaults to "abbreviated".
+      
+    >>> latex((2*tau)**asin(Rational(7,2)))
+    \left(2 \tau\right)^{\operatorname{asin}{\left (\frac{7}{2} \right )}}
+    >>> latex((2*tau)**asin(Rational(7,2)), inv_trig_style="full")
+    \left(2 \tau\right)^{\arcsin{\left (\frac{7}{2} \right )}}
+    >>> latex((2*tau)**asin(Rational(7,2)), inv_trig_style="power")
+    \left(2 \tau\right)^{\sin^{-1}{\left (\frac{7}{2} \right )}}
+
+    mat_str: Which matrix environment string to emit. "smallmatrix", "bmatrix", etc. This paramater currently doesn't work. Defaults to "bmatrix".
+      
+    >>> latex([[1, 2], [3, 4]], mat_str = "bmatrix")
+    \begin{bmatrix}\begin{bmatrix}1, & 2\end{bmatrix}, & \begin{bmatrix}3, & 4\end{bmatrix}\end{bmatrix}
+    
+    mat_delim: The delimiter to wrap around matrices. Can be one of "[", "(". # This doesn't currently work.
+
+    >>> latex([[1, 2], [3, 4]], mat_delim = "(")
+    \begin{bmatrix}\begin{bmatrix}1, & 2\end{bmatrix}, & \begin{bmatrix}3, & 4\end{bmatrix}\end{bmatrix}
+      
+    symbol_names: Dictionary of symbols and the custom strings they should be emitted as.
+    
+    >>> latex(x**2, symbol_names={x:'x_i'})
+    'x_i^{2}'
 
     Besides all Basic based expressions, you can recursively
     convert Python containers (lists, tuples and dicts) and
@@ -1082,12 +1103,6 @@ def latex(expr, **settings):
 
     >>> latex([2/x, y], mode='inline')
     '$\\begin{bmatrix}\\frac{2}{x}, & y\\end{bmatrix}$'
-
-    You can print symbols with custom strings by passing a dictionary
-    to the argument symbol_names.
-
-    >>> latex(x**2, symbol_names={x:'x_i'})
-    'x_i^{2}'
 
     """
 
