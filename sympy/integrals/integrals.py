69397a134b08e247ccd04e32f25bde5e1d9ad73e
David Ju <Sgtmook314@gmail.com>
2011-12-19 18:36:27 -0800

    Doctest Coverage for Integral module
    Improved the Doctest Coverage for Integral module. Several functions which are defined within other functions are not covered with doctests.

--- sympy/integrals/integrals.py
+++ sympy/integrals/integrals.py
@@ -171,6 +171,19 @@ class Integral(Expr):
 
         This is a very naive and quick test, not intended to check for special
         patterns like Integral(sin(m*x)*cos(n*x), (x, 0, 2*pi)) == 0.
+
+        Examples
+        ========
+
+            >>> from sympy import Integral
+            >>> from sympy.abc import x, y, z
+            >>> Integral(1, (x, 1, 1)).is_zero
+            True
+            >>> Integral(0, (x, y, z)).is_zero
+            True
+            >>> Integral(1, (x, 1, 2)).is_zero
+            False
+
         """
         if (self.function.is_zero or
             any(len(xab) == 3 and xab[1] == xab[2] for xab in self.limits)):
@@ -285,6 +298,17 @@ class Integral(Expr):
 
         The mapping must be uniquely invertible (e.g. a linear or linear
         fractional transformation).
+
+        Examples
+        ========
+
+            >>> from sympy.abc import a, b, c
+            >>> from sympy import Integral
+            >>> Integral(a*b + 2+c, (c, -1, 1/2)).transform(a, c*2)
+            Integral(a*b + c + 2, (c, -1, 0))
+            >>> Integral(a**2+1, (a, -1, 2)).transform(a, 1+2*a)
+            Integral(2*(2*a + 1)**2 + 2, (a, -1, 1/2))
+
         """
         if x not in self.variables:
             return self
@@ -300,8 +324,10 @@ class Integral(Expr):
         function = function.subs(x, mapping) * mapping.diff(x)
 
         def calc_limit(a, b):
-            """replace x with a, using subs if possible, otherwise limit
-            where sign of b is considered"""
+            """
+            replace x with a, using subs if possible, otherwise limit
+            where sign of b is considered
+            """
             wok = inverse_mapping.subs(x, a)
             if wok is S.NaN or wok.is_bounded is False and a.is_bounded:
                 return limit(sign(b)*inverse_mapping, x, a)
--- sympy/integrals/trigonometry.py
+++ sympy/integrals/trigonometry.py
@@ -107,10 +107,10 @@ def trigintegrate(f, x):
                 # C   = (1-S )  = sum(i, (-) * B(k,i) * S  )
                 if m > 0 :
                     for i in range(0,m/2+1):
-                        res += (-1)**i * binomial(m/2,i) * sin_pow_integrate(n+2*i, x)
+                        res += (-1)**i * binomial(m/2,i) * _sin_pow_integrate(n+2*i, x)
 
                 elif m == 0:
-                    res=sin_pow_integrate(n,x)
+                    res=_sin_pow_integrate(n,x)
                 else:
                     # m < 0 , |n| > |m|
                     #  /                                                           /
@@ -140,7 +140,7 @@ def trigintegrate(f, x):
                     #       n                                        2
                     #    sin (x) term is expanded here interms of cos (x), and then integrated.
                     for i in range(0,n/2+1):
-                        res += (-1)**i * binomial(n/2,i) * cos_pow_integrate(m+2*i, x)
+                        res += (-1)**i * binomial(n/2,i) * _cos_pow_integrate(m+2*i, x)
 
                 elif n == 0 :
                     ##  /
@@ -150,7 +150,7 @@ def trigintegrate(f, x):
                     #  |    m
                     #  | cos (x)
                     # /
-                    res= cos_pow_integrate(m,x)
+                    res= _cos_pow_integrate(m,x)
                 else:
                     # n < 0 , |m| > |n|
                     #  /                                                         /
@@ -176,7 +176,7 @@ def trigintegrate(f, x):
                         res=Rational(-1,m+1)*cos(x)**(m+1)*sin(x)**(n-1) + Rational(n-1,m+1)*sympy.integrals.integrate(cos(x)**(m+2)*sin(x)**(n-2),x)
             return res.subs(x, a*x) / a
 
-def sin_pow_integrate(n,x):
+def _sin_pow_integrate(n,x):
     if n > 0 :
         if n == 1:
             #Recursion break
@@ -193,7 +193,7 @@ def sin_pow_integrate(n,x):
         #
         #
 
-        return Rational(-1,n)*cos(x)*sin(x)**(n-1)+Rational(n-1,n)*sin_pow_integrate(n-2,x)
+        return Rational(-1,n)*cos(x)*sin(x)**(n-1)+Rational(n-1,n)*_sin_pow_integrate(n-2,x)
 
     if n < 0:
         if n == -1:
@@ -211,14 +211,14 @@ def sin_pow_integrate(n,x):
         #/                                                 /
         #
         #
-        return Rational(1,n+1)*cos(x)*sin(x)**(n+1) + Rational(n+2,n+1) * sin_pow_integrate(n+2,x)
+        return Rational(1,n+1)*cos(x)*sin(x)**(n+1) + Rational(n+2,n+1) * _sin_pow_integrate(n+2,x)
 
     else:
         #n == 0
         #Recursion break.
         return x
 
-def cos_pow_integrate(n,x):
+def _cos_pow_integrate(n,x):
     if n > 0 :
         if n==1:
             #Recursion break.
@@ -235,7 +235,7 @@ def cos_pow_integrate(n,x):
         #
         #
 
-        return Rational(1,n)*sin(x)*cos(x)**(n-1)+Rational(n-1,n)*cos_pow_integrate(n-2,x)
+        return Rational(1,n)*sin(x)*cos(x)**(n-1)+Rational(n-1,n)*_cos_pow_integrate(n-2,x)
 
     if n < 0:
         if n == -1:
@@ -254,7 +254,7 @@ def cos_pow_integrate(n,x):
         #
 
 
-        return Rational(-1,n+1)*sin(x)*cos(x)**(n+1) + Rational(n+2,n+1) * cos_pow_integrate(n+2,x)
+        return Rational(-1,n+1)*sin(x)*cos(x)**(n+1) + Rational(n+2,n+1) * _cos_pow_integrate(n+2,x)
     else :
         # n == 0
         #Recursion Break.
