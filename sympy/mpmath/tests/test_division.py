58a75694eb0b1f63a5ad9c3ccfb9b52cb426a42a
Kendhia <kendhia@gmail.com>
2011-12-29 23:09:26 +0100

    Update sympy/mpmath/tests/test_division.py

--- sympy/mpmath/tests/test_division.py
+++ sympy/mpmath/tests/test_division.py
@@ -3,7 +3,7 @@ from sympy.mpmath import mpf, mp
 
 from random import randint, choice, seed
 
-from sympy.utilities.pytest import XFAIL, raises
+from sympy.utilities.pytest import XFAIL
 
 all_modes = [round_floor, round_ceiling, round_down, round_up, round_nearest]
 
@@ -142,12 +142,5 @@ def test_mod():
 def test_div_negative_rnd_bug():
     mp.dps = 15
     assert (-3) / mpf('0.1531879017645047') == mpf('-19.583791966887116')
-    assert mpf('-2.6342475750861301') / mpf('0.35126216427941814') == mpf('-7.4993775104985909')
-    
-@XFAIL
-def test_issue_1756():
-    x = Symbol('x')
-    f = Function('f')
-    g = Function('g')
-   assert 1/g(x) == O(1/f(x))    
+    assert mpf('-2.6342475750861301') / mpf('0.35126216427941814') == mpf('-7.4993775104985909') 
    
