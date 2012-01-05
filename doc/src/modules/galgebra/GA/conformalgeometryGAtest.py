f10f4514484c20600de5d1be333d639d3c696d74
Swapnil Agarwal <swapnilag29@gmail.com>
2012-01-06 02:20:55 +0530

    Refactor documentation of files in GA

--- doc/src/modules/galgebra/GA/conformalgeometryGAtest.py
+++ doc/src/modules/galgebra/GA/conformalgeometryGAtest.py
@@ -1,46 +1,49 @@
-        print '\nExample: Conformal representations of circles, lines, spheres, and planes'
+    >>> print '\nExample: Conformal representations of circles, lines, spheres, and planes'
 
-        metric = '1 0 0 0 0,0 1 0 0 0,0 0 1 0 0,0 0 0 0 2,0 0 0 2 0'
+    Example: Conformal representations of circles, lines, spheres, and planes
 
-        MV.setup('e0 e1 e2 n nbar',metric,debug=0)
-        MV.set_str_format(1)
-        e = n+nbar
-        #conformal representation of points
+    >>> metric = '1 0 0 0 0,0 1 0 0 0,0 0 1 0 0,0 0 0 0 2,0 0 0 2 0'
+    >>> MV.setup('e0 e1 e2 n nbar',metric,debug=0)
+    >>> MV.set_str_format(1)
+    >>> e = n+nbar
 
-        A = make_vector(e0)    # point a = (1,0,0)  A = F(a)
-        B = make_vector(e1)    # point b = (0,1,0)  B = F(b)
-        C = make_vector(-1*e0) # point c = (-1,0,0) C = F(c)
-        D = make_vector(e2)    # point d = (0,0,1)  D = F(d)
-        X = make_vector('x',3)
+    #conformal representation of points
+    >>> A = make_vector(e0)    # point a = (1,0,0)  A = F(a)
+    >>> C = make_vector(-1*e0) # point c = (-1,0,0) C = F(c)
+    >>> D = make_vector(e2)    # point d = (0,0,1)  D = F(d)
+    >>> X = make_vector('x',3)
 
-        print 'a = e0, b = e1, c = -e0, and d = e2'
-        print 'A = F(a) = 1/2*(a*a*n+2*a-nbar), etc.'
-        print 'Circle through a, b, and c'
-        print 'Circle: A^B^C^X = 0 =',(A^B^C^X)
-        print 'Line through a and b'
-        print 'Line  : A^B^n^X = 0 =',(A^B^n^X)
-        print 'Sphere through a, b, c, and d'
-        print 'Sphere: A^B^C^D^X = 0 =',(A^B^C^D^X)
-        print 'Plane through a, b, and d'
-        print 'Plane : A^B^n^D^X = 0 =',(A^B^n^D^X)
+    >>> print 'a = e0, b = e1, c = -e0, and d = e2'
+    a = e0, b = e1, c = -e0, and d = e2
 
-Example: Conformal representations of circles, lines, spheres, and planes
-a = e0, b = e1, c = -e0, and d = e2
-A = F(a) = 1/2*(a*a*n+2*a-nbar), etc.
-Circle through a, b, and c
-Circle: A^B^C^X = 0 = {-x2}e0^e1^e2^n
-+{x2}e0^e1^e2^nbar
-+{-1/2 + 1/2*x0**2 + 1/2*x1**2 + 1/2*x2**2}e0^e1^n^nbar
+    >>> print 'A = F(a) = 1/2*(a*a*n+2*a-nbar), etc.'
+    A = F(a) = 1/2*(a*a*n+2*a-nbar), etc.
 
-Line through a and b
-Line  : A^B^n^X = 0 = {-x2}e0^e1^e2^n
-+{-1/2 + x0/2 + x1/2}e0^e1^n^nbar
-+{x2/2}e0^e2^n^nbar
-+{-x2/2}e1^e2^n^nbar
+    >>> print 'Circle through a, b, and c'
+    Circle through a, b, and c
 
-Sphere through a, b, c, and d
-Sphere: A^B^C^D^X = 0 = {1/2 - 1/2*x0**2 - 1/2*x1**2 - 1/2*x2**2}e0^e1^e2^n^nbar
+    >>> print 'Circle: A^B^C^X = 0 =',(A^B^C^X)
+    Circle: A^B^C^X = 0 = {-x2}e0^e1^e2^n
+    +{x2}e0^e1^e2^nbar
+    +{-1/2 + 1/2*x0**2 + 1/2*x1**2 + 1/2*x2**2}e0^e1^n^nbar
 
-Plane through a, b, and d
-Plane : A^B^n^D^X = 0 = {1/2 - x0/2 - x1/2 - x2/2}e0^e1^e2^n^nbar
+    >>> print 'Line through a and b'
+    Line through a and b
+    +{-1/2 + x0/2 + x1/2}e0^e1^n^nbar
+    +{x2/2}e0^e2^n^nbar
+    +{-x2/2}e1^e2^n^nbar
 
+    >>> print 'Line  : A^B^n^X = 0 =',(A^B^n^X)
+    Line  : A^B^n^X = 0 = {-x2}e0^e1^e2^n
+
+    >>> print 'Sphere through a, b, c, and d'
+    Sphere through a, b, c, and d
+
+    >>> print 'Sphere: A^B^C^D^X = 0 =',(A^B^C^D^X)
+    Sphere: A^B^C^D^X = 0 = {1/2 - 1/2*x0**2 - 1/2*x1**2 - 1/2*x2**2}e0^e1^e2^n^nbar
+
+    >>> print 'Plane through a, b, and d'
+    Plane through a, b, and d
+
+    >>> print 'Plane : A^B^n^D^X = 0 =',(A^B^n^D^X)
+    Plane : A^B^n^D^X = 0 = {1/2 - x0/2 - x1/2 - x2/2}e0^e1^e2^n^nbar
--- doc/src/modules/galgebra/GA/hyperbolicGAtest.py
+++ doc/src/modules/galgebra/GA/hyperbolicGAtest.py
@@ -1,141 +1,164 @@
-        print 'Example: non-euclidian distance calculation'
+    >>> print 'Example: non-euclidian distance calculation'
+    Example: non-euclidian distance calculation
 
-        metric = '0 # #,# 0 #,# # 1'
-        MV.setup('X Y e',metric)
-        MV.set_str_format(1)
-        L = X^Y^e
-        B = L*e
-        Bsq = (B*B)()
-        print 'L = X^Y^e is a non-euclidian line'
-        print 'B = L*e =',B
-        BeBr =B*e*B.rev()
-        print 'B*e*B.rev() =',BeBr
-        print 'B^2 =',Bsq
-        print 'L^2 =',(L*L)()
-        make_symbols('s c Binv M S C alpha')
-        Bhat = Binv*B # Normalize translation generator
-        R = c+s*Bhat # Rotor R = exp(alpha*Bhat/2)
-        print 's = sinh(alpha/2) and c = cosh(alpha/2)'
-        print 'R = exp(alpha*B/(2*|B|)) =',R
-        Z = R*X*R.rev()
-        Z.expand()
-        Z.collect([Binv,s,c,XdotY])
-        print 'R*X*R.rev() =',Z
-        W = Z|Y
-        W.expand()
-        W.collect([s*Binv])
-        print '(R*X*rev(R)).Y =',W
-        M = 1/Bsq
-        W.subs(Binv**2,M)
-        W.simplify()
-        Bmag = sympy.sqrt(XdotY**2-2*XdotY*Xdote*Ydote)
-        W.collect([Binv*c*s,XdotY])
+    >>> metric = '0 # #,# 0 #,# # 1'
+    >>> MV.setup('X Y e',metric)
+    >>> MV.set_str_format(1)
+    >>> L = X^Y^e
+    >>> B = L*e
+    >>> Bsq = (B*B)()
 
-        W.subs(2*XdotY**2-4*XdotY*Xdote*Ydote,2/(Binv**2))
-        W.subs(2*c*s,S)
-        W.subs(c**2,(C+1)/2)
-        W.subs(s**2,(C-1)/2)
-        W.simplify()
-        W.subs(1/Binv,Bmag)
-        W = W().expand()
-        print '(R*X*R.rev()).Y =',W
-        nl = '\n'
+    >>> print 'L = X^Y^e is a non-euclidian line'
+    L = X^Y^e is a non-euclidian line
 
-        Wd = collect(W,[C,S],exact=True,evaluate=False)
-        print 'Wd =',Wd
-        Wd_1 = Wd[ONE]
-        Wd_C = Wd[C]
-        Wd_S = Wd[S]
-        print '|B| =',Bmag
-        Wd_1 = Wd_1.subs(Bmag,1/Binv)
-        Wd_C = Wd_C.subs(Bmag,1/Binv)
-        Wd_S = Wd_S.subs(Bmag,1/Binv)
-        print 'Wd[ONE] =',Wd_1
-        print 'Wd[C] =',Wd_C
-        print 'Wd[S] =',Wd_S
+    >>> print 'B = L*e =',B
+    B = L*e = X^Y
+    +{-(Y.e)}X^e
+    +{(X.e)}Y^e
 
+    >>> BeBr =B*e*B.rev()
 
-        lhs = Wd_1+Wd_C*C
-        rhs = -Wd_S*S
-        lhs = lhs**2
-        rhs = rhs**2
-        W = (lhs-rhs).expand()
-        W = (W.subs(1/Binv**2,Bmag**2)).expand()
-        print 'W =',W
-        W = (W.subs(S**2,C**2-1)).expand()
-        print 'W =',W
-        W = collect(W,[C,C**2],evaluate=False)
-        print 'W =',W
-        
-        a = W[C**2]
-        b = W[C]
-        c = W[ONE]
+    >>> print 'B*e*B.rev() =',BeBr
+    B*e*B.rev() = {2*(X.Y)*(X.e)*(Y.e) - (X.Y)**2}e
 
-        print 'a =',a
-        print 'b =',b
-        print 'c =',c
+    >>> print 'B^2 =',Bsq
+    B^2 = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2
 
-        D = (b**2-4*a*c).expand()
-        print 'Setting to 0 and solving for C gives:'
-        print 'Descriminant D = b^2-4*a*c =',D
-        C = (-b/(2*a)).expand()
-        print 'C = cosh(alpha) = -b/(2*a) =',C
+    >>> print 'L^2 =',(L*L)()
+    L^2 = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2
 
-Example: non-euclidian distance calculation
-L = X^Y^e is a non-euclidian line
-B = L*e = X^Y
-+{-(Y.e)}X^e
-+{(X.e)}Y^e
+    >>> make_symbols('s c Binv M S C alpha')
+    >>> Bhat = Binv*B # Normalize translation generator
+    >>> R = c+s*Bhat # Rotor R = exp(alpha*Bhat/2)
 
-B*e*B.rev() = {2*(X.Y)*(X.e)*(Y.e) - (X.Y)**2}e
+    >>> print 's = sinh(alpha/2) and c = cosh(alpha/2)'
+    s = sinh(alpha/2) and c = cosh(alpha/2)
 
-B^2 = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2
-L^2 = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2
-s = sinh(alpha/2) and c = cosh(alpha/2)
-R = exp(alpha*B/(2*|B|)) = {c}1
-+{Binv*s}X^Y
-+{-(Y.e)*Binv*s}X^e
-+{(X.e)*Binv*s}Y^e
+    >>> print 'R = exp(alpha*B/(2*|B|)) =',R
+    R = exp(alpha*B/(2*|B|)) = {c}1
+    +{Binv*s}X^Y
+    +{-(Y.e)*Binv*s}X^e
+    +{(X.e)*Binv*s}Y^e
 
-R*X*R.rev() = {Binv*(2*(X.Y)*c*s - 2*(X.e)*(Y.e)*c*s) + Binv**2*((X.Y)**2*s**2 
-               - 2*(X.Y)*(X.e)*(Y.e)*s**2) + c**2}X
-             +{2*Binv*c*s*(X.e)**2}Y
-             +{Binv**2*(-2*(X.e)*(X.Y)**2*s**2 + 4*(X.Y)*(Y.e)*(X.e)**2*s**2)
-               - 2*(X.Y)*(X.e)*Binv*c*s}e
+    >>> Z = R*X*R.rev()
+    >>> Z.expand()
+    >>> Z.collect([Binv,s,c,XdotY])
 
-(R*X*rev(R)).Y = {Binv*s*(-4*(X.Y)*(X.e)*(Y.e)*c + 2*c*(X.Y)**2) 
-                 + Binv**2*s**2*(-4*(X.e)*(Y.e)*(X.Y)**2 +
-                 4*(X.Y)*(X.e)**2*(Y.e)**2 + (X.Y)**3) + (X.Y)*c**2}1
+    >>> print 'R*X*R.rev() =',Z
+    R*X*R.rev() = {Binv*(2*(X.Y)*c*s - 2*(X.e)*(Y.e)*c*s) + Binv**2*((X.Y)**2*s**2 
+                   - 2*(X.Y)*(X.e)*(Y.e)*s**2) + c**2}X
+                 +{2*Binv*c*s*(X.e)**2}Y
+                 +{Binv**2*(-2*(X.e)*(X.Y)**2*s**2 + 4*(X.Y)*(Y.e)*(X.e)**2*s**2)
+                   - 2*(X.Y)*(X.e)*Binv*c*s}e
 
-(R*X*R.rev()).Y = S*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2) 
-                  + (X.Y)*Binv*C*(-2*(X.Y)*(X.e)*(Y.e) + 
-                  (X.Y)**2)**(1/2) + (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) 
-                  + (X.Y)**2)**(1/2) - 
-                  (X.e)*(Y.e)*Binv*C*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)
-Wd = {1: (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2),
-      S: (-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2),
-      C: (X.Y)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2) - 
-         (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)}
-|B| = (-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)
+    >>> W = Z|Y
+    >>> W.expand()
+    >>> W.collect([s*Binv])
 
-Wd[ONE] = (X.e)*(Y.e)
-Wd[C] = (X.Y) - (X.e)*(Y.e)
-Wd[S] = 1/Binv
+    >>> print '(R*X*rev(R)).Y =',W
+    (R*X*rev(R)).Y = {Binv*s*(-4*(X.Y)*(X.e)*(Y.e)*c + 2*c*(X.Y)**2) 
+                     + Binv**2*s**2*(-4*(X.e)*(Y.e)*(X.Y)**2 +
+                     4*(X.Y)*(X.e)**2*(Y.e)**2 + (X.Y)**3) + (X.Y)*c**2}1
 
-W = 2*(X.Y)*(X.e)*(Y.e)*C + (X.Y)**2*C**2 + (X.e)**2*(Y.e)**2 
-    - (X.Y)**2*S**2 + (X.e)**2*(Y.e)**2*C**2 - 2*C*(X.e)**2*(Y.e)**2 
-    - 2*(X.Y)*(X.e)*(Y.e)*C**2 + 2*(X.Y)*(X.e)*(Y.e)*S**2
-W = -2*(X.Y)*(X.e)*(Y.e) + 2*(X.Y)*(X.e)*(Y.e)*C + (X.Y)**2 
-    + (X.e)**2*(Y.e)**2 + (X.e)**2*(Y.e)**2*C**2 -
-     2*C*(X.e)**2*(Y.e)**2
-W = {1: -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2 + (X.e)**2*(Y.e)**2,
-     C**2: (X.e)**2*(Y.e)**2,
-     C: 2*(X.Y)*(X.e)*(Y.e) - 2*(X.e)**2*(Y.e)**2}
+    >>> M = 1/Bsq
+    >>> W.subs(Binv**2,M)
+    >>> W.simplify()
+    >>> Bmag = sympy.sqrt(XdotY**2-2*XdotY*Xdote*Ydote)
+    >>> W.collect([Binv*c*s,XdotY])
 
-a = (X.e)**2*(Y.e)**2
-b = 2*(X.Y)*(X.e)*(Y.e) - 2*(X.e)**2*(Y.e)**2
-c = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2 + (X.e)**2*(Y.e)**2
+    >>> W.subs(2*XdotY**2-4*XdotY*Xdote*Ydote,2/(Binv**2))
+    >>> W.subs(2*c*s,S)
+    >>> W.subs(c**2,(C+1)/2)
+    >>> W.subs(s**2,(C-1)/2)
+    >>> W.simplify()
+    >>> W.subs(1/Binv,Bmag)
+    >>> W = W().expand()
 
-Setting to 0 and solving for C gives:
-Descriminant D = b^2-4*a*c = 0
-C = cosh(alpha) = -b/(2*a) = 1 - (X.Y)/((X.e)*(Y.e))
+    >>> print '(R*X*R.rev()).Y =',W
+    (R*X*R.rev()).Y = S*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2) 
+                      + (X.Y)*Binv*C*(-2*(X.Y)*(X.e)*(Y.e) + 
+                      (X.Y)**2)**(1/2) + (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) 
+                      + (X.Y)**2)**(1/2) - 
+                      (X.e)*(Y.e)*Binv*C*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)
+
+    >>> nl = '\n'
+
+    >>> Wd = collect(W,[C,S],exact=True,evaluate=False)
+
+    >>> print 'Wd =',Wd
+    Wd = {1: (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2),
+          S: (-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2),
+          C: (X.Y)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2) - 
+             (X.e)*(Y.e)*Binv*(-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)}
+
+    >>> Wd_1 = Wd[ONE]
+    >>> Wd_C = Wd[C]
+    >>> Wd_S = Wd[S]
+
+    >>> print '|B| =',Bmag
+    |B| = (-2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2)**(1/2)
+
+    >>> Wd_1 = Wd_1.subs(Bmag,1/Binv)
+    >>> Wd_C = Wd_C.subs(Bmag,1/Binv)
+    >>> Wd_S = Wd_S.subs(Bmag,1/Binv)
+
+    >>> print 'Wd[ONE] =',Wd_1
+    Wd[ONE] = (X.e)*(Y.e)
+
+    >>> print 'Wd[C] =',Wd_C
+    Wd[C] = (X.Y) - (X.e)*(Y.e)
+
+    >>> print 'Wd[S] =',Wd_S
+    Wd[S] = 1/Binv
+
+    >>> lhs = Wd_1+Wd_C*C
+    >>> rhs = -Wd_S*S
+    >>> lhs = lhs**2
+    >>> rhs = rhs**2
+    >>> W = (lhs-rhs).expand()
+    >>> W = (W.subs(1/Binv**2,Bmag**2)).expand()
+
+    >>> print 'W =',W
+    W = 2*(X.Y)*(X.e)*(Y.e)*C + (X.Y)**2*C**2 + (X.e)**2*(Y.e)**2 
+        - (X.Y)**2*S**2 + (X.e)**2*(Y.e)**2*C**2 - 2*C*(X.e)**2*(Y.e)**2 
+        - 2*(X.Y)*(X.e)*(Y.e)*C**2 + 2*(X.Y)*(X.e)*(Y.e)*S**2
+
+    >>> W = (W.subs(S**2,C**2-1)).expand()
+
+    >>> print 'W =',W
+    W = -2*(X.Y)*(X.e)*(Y.e) + 2*(X.Y)*(X.e)*(Y.e)*C + (X.Y)**2 
+        + (X.e)**2*(Y.e)**2 + (X.e)**2*(Y.e)**2*C**2 -
+         2*C*(X.e)**2*(Y.e)**2
+
+    >>> W = collect(W,[C,C**2],evaluate=False)
+
+    >>> print 'W =',W
+    W = {1: -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2 + (X.e)**2*(Y.e)**2,
+         C**2: (X.e)**2*(Y.e)**2,
+         C: 2*(X.Y)*(X.e)*(Y.e) - 2*(X.e)**2*(Y.e)**2}
+
+    >>> a = W[C**2]
+    >>> b = W[C]
+    >>> c = W[ONE]
+
+    >>> print 'a =',a
+    a = (X.e)**2*(Y.e)**2
+
+    >>> print 'b =',b
+    b = 2*(X.Y)*(X.e)*(Y.e) - 2*(X.e)**2*(Y.e)**2
+
+    >>> print 'c =',c
+    c = -2*(X.Y)*(X.e)*(Y.e) + (X.Y)**2 + (X.e)**2*(Y.e)**2
+
+    >>> D = (b**2-4*a*c).expand()
+
+    >>> print 'Setting to 0 and solving for C gives:'
+    Setting to 0 and solving for C gives:
+
+    >>> print 'Descriminant D = b^2-4*a*c =',D
+    Descriminant D = b^2-4*a*c = 0
+
+    >>> C = (-b/(2*a)).expand()
+
+    >>> print 'C = cosh(alpha) = -b/(2*a) =',C
+    C = cosh(alpha) = -b/(2*a) = 1 - (X.Y)/((X.e)*(Y.e))
