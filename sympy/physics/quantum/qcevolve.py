8a24b3ca44c32d142bcfb3c5d6d8986f4f1c24e1
rayman <rayman@CoolGuy.(none)>
2012-01-16 20:56:11 -0800

    Renamed classes in qcevolve because names were getting long. Thinking of adding a new class to represent quantum circuits as linear programs.

--- sympy/physics/quantum/qcevolve.py
+++ sympy/physics/quantum/qcevolve.py
@@ -14,12 +14,17 @@ __all__ = [
     'GQuantumCircuit'
 ]
 
-class GQuantumCircuit(GenomeBase):
-    """A representation of quantum circuits for genetic algorithms.
+class GQCBase(Basic, GenomeBase):
+    """A base representation of quantum circuits for genetic algorithms.
 
-    Specifically, GQuantumCircuit is used for genetic programming,
+    Specifically, GQCBase is used for genetic programming,
     a specialization of genetic algorithms that evolve computer
     programs, or generally, algorithms.  Each quantum circuit
     represents a quantum algorithm.
+    """
+    pass
+
+class GQCLinear(GQuantumCircuitBase):
+    """A linear program representation of quantum circuits.
     """
     pass
