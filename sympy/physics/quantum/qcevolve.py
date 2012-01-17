212204d4ace037a9cccc25994ee68190dce92c95
rayman <rayman@CoolGuy.(none)>
2012-01-16 21:58:36 -0800

    Fixed a couple of bugs due to name changes and inheritance.

--- sympy/physics/quantum/qcevolve.py
+++ sympy/physics/quantum/qcevolve.py
@@ -8,10 +8,11 @@ http://pyevolve.sourceforge.net/index.html
 
 """
 
+from sympy import Basic
 from pyevolve.GenomeBase import GenomeBase
 
 __all__ = [
-    'GQuantumCircuit'
+    'GQCBase'
 ]
 
 class GQCBase(Basic, GenomeBase):
@@ -21,10 +22,20 @@ class GQCBase(Basic, GenomeBase):
     a specialization of genetic algorithms that evolve computer
     programs, or generally, algorithms.  Each quantum circuit
     represents a quantum algorithm.
+
+    TODO:
+    * Initializer operator
+    * Mutator operator
+    * Crossover operator
     """
     pass
 
-class GQCLinear(GQuantumCircuitBase):
+class GQCLinear(GQCBase):
     """A linear program representation of quantum circuits.
+
+    TODO:
+    * Initializer operator
+    * Mutator operator
+    * Crossover operator
     """
     pass
