22176ffe3a4111aa9ecf65e3025e085deb471198
Chris Smith <smichr@gmail.com>
2012-01-17 13:23:21 +0545

    add molar mass unit (mmu) to units and add note about it

--- sympy/physics/tests/test_units.py
+++ sympy/physics/tests/test_units.py
@@ -6,7 +6,8 @@ from sympy.physics.units import (au, amu, charge, day, find_unit,
 def test_units():
     assert (5*m/s * day) / km == 432
     assert foot / meter == Rational('0.3048')
-    assert str(u.g/(1*u.amu).n(1)` == '6.e+23'
+    # amu is a pure mass so mass/mass gives a number, not an amount (mol)
+    assert str(u.g/(u.amu).n(1)` == '6.e+23'
 
     # Light from the sun needs about 8.3 minutes to reach earth
     t = (1*au / speed_of_light).evalf() / minute
