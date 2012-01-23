da402115e44afaec870a66a86e7d13f875b8a790
Chris Smith <smichr@gmail.com>
2012-01-23 11:29:13 +0545

    cover latex_ex print_Mul

--- sympy/galgebra/tests/test_GA.py
+++ sympy/galgebra/tests/test_GA.py
@@ -299,6 +299,7 @@ def test__print_Mul_Add():
     l = LatexPrinter()
     assert l._print_Mul(n*m) == 'm n'
     assert l._print_Mul(-2*m) == '- 2 m'
+    assert l._print_Mul(2*m) == '2 m'
     assert l._print_Add(-5 + 4*z) == '-5 + 4 z'
     assert l._print_Add(-5 - 4*z) == '-5 - 4 z'
     assert l._print_Add(n - 2) == '-2 + n'
