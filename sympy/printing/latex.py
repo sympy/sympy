1dd1efce9a8ef33d647ddb08d8d03ade6dceddc8
Chris Smith <smichr@gmail.com>
2012-01-23 13:05:27 +0545

    cover matAdd printing

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -897,20 +897,26 @@ class LatexPrinter(Printer):
             return "%s^T"%self._print(mat)
 
     def _print_MatAdd(self, expr):
-        terms = expr.args
-        tex = self._print(terms[0])
+        c, terms = expr.as_coeff_Add()
+        tex = []
+        if c < 0:
+            tex.append("-")
+            tex.append(self._print(-c))
 
-        for term in terms[1:]:
+        for term in Add.make_args(terms):
             coeff, M = term.as_coeff_Mul()
 
             if coeff < 0:
-                tex += " -"
+                tex.append("-")
+                coeff = -coeff
             else:
-                tex += " +"
+                tex.append("+")
 
-            tex += " " + self._print(M)
+            if coeff != 1:
+                tex.append(self._print(coeff))
+            tex.append(self._print(M))
 
-        return tex
+        return " ".join(tex)
 
     def _print_MatMul(self, expr):
         coeff, tail = expr.as_coeff_Mul()
@@ -1191,4 +1197,3 @@ def latex(expr, **settings):
 def print_latex(expr, **settings):
     """Prints LaTeX representation of the given expression."""
     print latex(expr, **settings)
-
