830637b9dafa1336b0acf40454c0ba1c63b743bb
Chris Smith <smichr@gmail.com>
2012-01-26 02:43:00 +0545

    round() added to miscellaneous functions; Float mods
        Float doesn't allow prec > 15 for inexact floats.
    
            Unless a float is binary-exact (denominator a power of 2) it cannot
            be requested to have more than 15 digits of precision. A user-friendly
            error message results if the attempt is made.
    
            For floats with less than 16 significant digits, there is no need
            to enter them as strings since the Float value will be exact to
            15 digits. And creating floats to more precision than 15 is disallowed
            unless the float is an exact float. So the user that wants a high
            precision float should enter it as a string with the necessary digits
            or as a short string with the precision. The latter will be shorter:
    
            Float('.100 000 000 000 000 0') vs
            Float('.1', 16) or Float(Float('.1'), 16)
    
            (Spaces are now allowed in the strings sent to Float, too.) Having
            to enter the precision acts as a check on the digits entered. To
            have all significant figures be counted automatically, send the
            null string ('') for the precision.
    
        Float normalizes in _new
    
            All Floats that are created should have the correct normalization
            for the given precision, but sometimes this is not True. So a
            renormalization should not be necessary in __eq__; but it did
            need to be made in Float._new and needs to be made in some other
            routines (which are part of the next commit).
    
            For example, in Float._as_mpf_val if self._prec == prec this
            normalization should not be necessary, but there are some
            values being generated in solve (via polys) that come back
            and do not pass the test that self._mpf_ is the same as
            the normalized mpf -- even though the precisions were the
            same. The test that produces such numbers is the one testing
            issue 2236; here is an example of discrepant mpfs:
    
            orig = (0, 93132257461547851L, 30, 57)
            >>> 93132257461547851L*2**30
            99999999999999999396020224L
            norm = (0, 5820766091346741L, 34, 53)
            >>> 5820766091346741L*2**34
            100000000000000004764729344L
    
        A work-around for mpmath is included: if mpmath sees a
        tuple with 0 mantissa it assumes the number is 0 but it
        may not be (e.g. inf has a large negative mantissa).
    
        The round function, like Python's own, always returns a Float.
    
        evalf.txt and gotchas.txt have been updated to reflect
        changes.

--- sympy/concrete/summations.py
+++ sympy/concrete/summations.py
@@ -209,7 +209,7 @@ class Sum(Expr):
             -log(2) + 7/20 + log(5)
             >>> from sympy import sstr
             >>> print sstr((s.evalf(), e.evalf()), full_prec=True)
-            (1.26629073187416, 0.0175000000000000)
+            (1.26629073187415, 0.0175000000000000)
 
         The endpoints may be symbolic:
 
