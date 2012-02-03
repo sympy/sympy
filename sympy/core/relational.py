2a7edf7d359b8dcb5da41a977777640e14d15bdd
Sean Vig <sean.v.775@gmail.com>
2012-02-03 08:50:16 -0600

    Fix Sphinx errors in relational.py
    Improves docstring in relational.py, changes ``Than`` to ``*Than`` and
    improves the linking of items within the docstring.

--- sympy/core/relational.py
+++ sympy/core/relational.py
@@ -258,15 +258,16 @@ class GreaterThan(_Greater):
     Extended Summary
     ================
 
-    The ``Than`` classes represent inequal relationships, where the left-hand side
-    is generally bigger or smaller than the right-hand side.  For example, the
-    GreaterThan class represents an inequal relationship where the left-hand
-    side is at least as big as the right side, if not bigger.  In mathematical
-    notation:
+    The ``*Than`` classes represent inequal relationships, where the left-hand
+    side is generally bigger or smaller than the right-hand side.  For example,
+    the GreaterThan class represents an inequal relationship where the
+    left-hand side is at least as big as the right side, if not bigger.  In
+    mathematical notation:
 
     lhs >= rhs
 
-    In total, there are four ``Than`` classes, to represent the four inequalities:
+    In total, there are four ``*Than`` classes, to represent the four
+    inequalities:
 
     +-----------------+--------+
     |Class Name       | Symbol |
@@ -294,10 +295,10 @@ class GreaterThan(_Greater):
     |StrictLessThan(lhs, rhs)    |   lhs <  rhs    |
     +----------------------------+-----------------+
 
-    In addition to the normal .lhs and .rhs of Relations, ``Than`` inequality
+    In addition to the normal .lhs and .rhs of Relations, ``*Than`` inequality
     objects also have the .lts and .gts properties, which represent the "less
-    than side" and "greater than side" of the operator.  Use of .lts and .gts in
-    an algorithm rather than .lhs and .rhs as an assumption of inequality
+    than side" and "greater than side" of the operator.  Use of .lts and .gts
+    in an algorithm rather than .lhs and .rhs as an assumption of inequality
     direction will make more explicit the intent of a certain section of code,
     and will make it similarly more robust to client code changes:
 
@@ -344,7 +345,7 @@ class GreaterThan(_Greater):
     >>> e1 == e2
     True
 
-    However, it is also perfectly valid to instantiate a ``Than`` class less
+    However, it is also perfectly valid to instantiate a ``*Than`` class less
     succinctly and less conviently:
 
     >>> rels = Rel(x, 1, '>='), Relational(x, 1, '>='), GreaterThan(x, 1)
@@ -426,7 +427,7 @@ class GreaterThan(_Greater):
     >>> e #doctest: +SKIP
     y < z
 
-    Due to an implementation detail or decision of Python[1], there is no way
+    Due to an implementation detail or decision of Python [1]_, there is no way
     for SymPy to reliably create that as a chained inequality.  To create a
     chained inequality, the only method currently available is to make use of
     And:
@@ -438,7 +439,7 @@ class GreaterThan(_Greater):
     And(x < y, y < z)
 
     Note that this is different than chaining an equality directly via use of
-    parenthesis (this is currently an open bug in SymPy[2]):
+    parenthesis (this is currently an open bug in SymPy [2]_):
 
     >>> e = (x < y) < z
     >>> type( e )
@@ -451,20 +452,18 @@ class GreaterThan(_Greater):
     point.  For the time being (circa Jan 2012), use And to create chained
     inequalities.
 
-    .. [1]
-
-    This implementation detail is that Python provides no reliable
-    method to determine that a chained inequality is being built.  Chained
-    comparison operators are evaluated pairwise, using "and" logic (see
-    http://docs.python.org/reference/expressions.html#notin).  This is done in
-    an efficient way, so that each object being compared is only evaluated once
-    and the comparison can short-circuit.  For example, ``1 > 2 > 3`` is
-    evaluated by Python as ``(1 > 2) and (2 > 3)``.  The ``and`` operator
-    coerces each side into a bool, returning the object itself when it
-    short-circuits.  Currently, the bool of the ``Than`` operators will give True
-    or False arbitrarily.  Thus, if we were to compute ``x > y > z``, with
-    ``x``, ``y``, and ``z`` being Symbols, Python converts the statement
-    (roughly) into these steps:
+    .. [1] This implementation detail is that Python provides no reliable
+       method to determine that a chained inequality is being built.  Chained
+       comparison operators are evaluated pairwise, using "and" logic (see
+       http://docs.python.org/reference/expressions.html#notin).  This is done
+       in an efficient way, so that each object being compared is only
+       evaluated once and the comparison can short-circuit.  For example, ``1
+       > 2 > 3`` is evaluated by Python as ``(1 > 2) and (2 > 3)``.  The
+       ``and`` operator coerces each side into a bool, returning the object
+       itself when it short-circuits.  Currently, the bool of the --Than
+       operators will give True or False arbitrarily.  Thus, if we were to
+       compute ``x > y > z``, with ``x``, ``y``, and ``z`` being Symbols,
+       Python converts the statement (roughly) into these steps:
 
     (1) x > y > z
     (2) (x > y) and (y > z)
@@ -481,19 +480,18 @@ class GreaterThan(_Greater):
 
         In Python, there is no way to override the ``and`` operator, or to
         control how it short circuits, so it is impossible to make something
-        like ``x > y > z`` work.  There is an open PEP to change this
-        http://www.python.org/dev/peps/pep-0335/, but until that is implemented,
-        this cannot be made to work.
+        like ``x > y > z`` work.  There is an open PEP to change this,
+        :pep:`335`, but until that is implemented, this cannot be made to work.
 
     .. [2] For more information, see these two bug reports:
 
-    "Separate boolean and symbolic relationals"
-    `Issue 1887 <http://code.google.com/p/sympy/issues/detail?id=1887>`
+       "Separate boolean and symbolic relationals"
+       `Issue 1887 <http://code.google.com/p/sympy/issues/detail?id=1887>`_
 
-    "It right 0 < x < 1 ?"
-    `Issue 2960 <http://code.google.com/p/sympy/issues/detail?id=2960>`
+       "It right 0 < x < 1 ?"
+       `Issue 2960 <http://code.google.com/p/sympy/issues/detail?id=2960>`_
 
-"""
+    """
 
     rel_op = '>='
 
