e52e9ecb14ed05ca73ac0580e6a30b9e8d00c221
Saptarshi Mandal <sapta.iitkgp@gmail.com>
2012-03-22 17:58:18 -0400

    Lots of refactoring and code cleanup. Fixed some docstrings.
    Signed-off-by: Saptarshi Mandal <sapta.iitkgp@gmail.com>

--- sympy/combinatorics/__init__.py
+++ sympy/combinatorics/__init__.py
@@ -1,4 +1,4 @@
 from sympy.combinatorics.permutations import Permutation
 from sympy.combinatorics.prufer import Prufer
-from sympy.combinatorics.partitions import (Partition, IntegerPartition, RGS_rank, RGS_unrank, RGS_enum,
-                                            partition_from_rgs)
+from sympy.combinatorics.partitions import (Partition, IntegerPartition, RGS_rank, RGS_unrank, RGS_enum)
+
