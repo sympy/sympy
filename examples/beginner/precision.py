0af063ef397fa9f44eca6d9f4bf0acefa9e15ee8
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2012-03-22 20:59:11 +0530

    Tweaked examples/beginner/ to fix some errors, improve readability
        differentiation.py   : Modified the examples to make it more
                               illustrative, added descriptions.
        expansion.py         : Added descriptions.
        limits_examples.py   : Adjusted spacing in the import statement
                               to follow the code conventions.
        precision.py         : Added descriptions.

--- examples/beginner/precision.py
+++ examples/beginner/precision.py
@@ -6,9 +6,14 @@ Demonstrates SymPy's arbitrary precision abilities
 """
 
 import sympy
-from sympy import pprint
+from sympy import pprint, Symbol
 
 def main():
+    x = Pow(2, 50, evaluate=False)
+    y = Pow(10, -50, evaluate=False)
+    print
+    print Mul(x, y, evaluate=False)
+    print
     e = sympy.Rational(2)**50/sympy.Rational(10)**50
     pprint(e)
 
