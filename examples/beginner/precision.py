b57f4f64692572317a28a6352223866715afda68
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2012-03-23 13:27:52 +0530

    Modified code to let pprint do the type setting

--- examples/beginner/precision.py
+++ examples/beginner/precision.py
@@ -11,9 +11,8 @@ from sympy import pprint, Symbol
 def main():
     x = Symbol('2')
     y = Symbol('10')
-    pprint(x**50)
-    print '__'
-    pprint(y**50)
+    print
+    pprint((x/y)**50)
     print
     e = sympy.Rational(2)**50/sympy.Rational(10)**50
     pprint(e)
