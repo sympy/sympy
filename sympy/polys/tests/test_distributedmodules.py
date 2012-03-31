2d058240c06d43d71faaaec0f3b083dadd45d56e
Tom Bachmann <e_mc_h2@web.de>
2012-03-31 13:49:41 +0100

    [polys] Add simple tests for standard bases wrt local orders.

--- sympy/polys/tests/test_distributedmodules.py
+++ sympy/polys/tests/test_distributedmodules.py
@@ -9,7 +9,7 @@ from sympy.polys.distributedmodules import (
 )
 
 from sympy.polys.monomialtools import (
-    lex, grlex
+    lex, grlex, InverseOrder
 )
 
 from sympy.polys.domains import QQ
@@ -141,5 +141,24 @@ def test_nontrivial():
     assert not contains([x**3+y**3, y**3+z**3, z**3+x**3, x**2*y + x**2*z + y**2*z],
                         x**2 + y**2)
 
-# TODO test non-global orderings
+    # compare local order
+    assert not contains([x*(1+x+y), y*(1+z)], x)
+    assert not contains([x*(1+x+y), y*(1+z)], x + y)
+
+def test_local():
+    igrlex = InverseOrder(grlex)
+    gens = [x, y, z]
+    def contains(I, f):
+        S = [sdm_from_vector([g], igrlex, QQ, gens=gens) for g in I]
+        G = sdm_groebner(S, sdm_nf_mora, igrlex, QQ)
+        return sdm_nf_mora(sdm_from_vector([f], lex, QQ, gens=gens),
+                           G, lex, QQ) == sdm_zero()
+    assert contains([x, y], x)
+    assert contains([x, y], x + y)
+    assert not contains([x, y], 1)
+    assert not contains([x, y], z)
+    assert contains([x**2 + y, x**2 + x], x - y)
+    assert not contains([x+y+z, x*y+x*z+y*z, x*y*z], x**2)
+    assert contains([x*(1+x+y), y*(1+z)], x)
+    assert contains([x*(1+x+y), y*(1+z)], x + y)
 
