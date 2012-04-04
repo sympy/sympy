a6e53c7b81876370fd3e21ad1a48590a78cdfaf4
Manoj Babu <manoj.babu2378@gmail.com>
2012-04-04 14:44:10 +0530

    Improved trigsimp for sin(x + y) and sinh(x + y)
    Added a backslash  in test_simplify.py as the line is too long
    added to tan(x+y) , tanh(x+y) assert

--- sympy/simplify/tests/test_simplify.py
+++ sympy/simplify/tests/test_simplify.py
@@ -70,6 +70,20 @@ def test_trigsimp1():
     assert trigsimp(y*tan(x)**2/sin(x)**2) == y/cos(x)**2
     assert trigsimp(cot(x)/cos(x)) == 1/sin(x)
 
+    assert trigsimp(sin(x + y) + sin(x - y)) == 2*sin(x)*cos(y)
+    assert trigsimp(sin(x + y) - sin(x - y)) == 2*sin(y)*cos(x)
+    assert trigsimp(cos(x + y) + cos(x - y)) == 2*cos(x)*cos(y)
+    assert trigsimp(cos(x + y) - cos(x - y)) == -2*sin(x)*sin(y)
+    assert ratsimp(trigsimp(tan(x + y) - tan(x)/(1 - tan(x)*tan(y)))) == \
+        -tan(y)/(tan(x)*tan(y) -1)
+
+    assert trigsimp(sinh(x + y) + sinh(x - y)) == 2*sinh(x)*cosh(y)
+    assert trigsimp(sinh(x + y) - sinh(x - y)) == 2*sinh(y)*cosh(x)
+    assert trigsimp(cosh(x + y) + cosh(x - y)) == 2*cosh(x)*cosh(y)
+    assert trigsimp(cosh(x + y) - cosh(x - y)) == 2*sinh(x)*sinh(y)
+    assert ratsimp(trigsimp(tanh(x + y) - tanh(x)/(1 + tanh(x)*tanh(y)))) == \
+        tanh(y)/(tanh(x)*tanh(y) + 1)
+
     assert trigsimp(cos(0.12345)**2 + sin(0.12345)**2) == 1
     e = 2*sin(x)**2 + 2*cos(x)**2
     assert trigsimp(log(e), deep=True) == log(2)
