30efbde4515b086ebb07399511335a8d15db4ad6
Chris Smith <smichr@gmail.com>
2012-04-13 02:32:52 +0545

    used signed dummy when computing leading_term in limit
        This allows powers like sqrt(1/x) to simplify to 1/sqrt(x) and
        be detected as x-based powers.

--- sympy/series/limits.py
+++ sympy/series/limits.py
@@ -125,8 +125,10 @@ def limit(e, z, z0, dir="+"):
                      for m in Mul.make_args(a))
                      for a in Add.make_args(w)))
                 if all(ok(w) for w in (n, d)):
-                    inve = (n/d).subs(z, 1/z)
-                    return limit(inve.as_leading_term(z), z, S.Zero, "+" if z0 is S.Infinity else "-")
+                    u = C.Dummy(positive=(z0 is S.Infinity))
+                    inve = (n/d).subs(z, 1/u)
+                    return limit(inve.as_leading_term(u), u,
+                        S.Zero, "+" if z0 is S.Infinity else "-")
 
             # weed out the z-independent terms
             i, d = e.as_independent(z)
