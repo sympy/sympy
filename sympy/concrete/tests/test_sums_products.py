e841e74d16674afac48c71d1551085731528f33b
arpit goyal <agmps18@gmail.com>
2012-04-14 14:56:46 +0530

    code editted, test for n_c objects added

--- sympy/concrete/tests/test_sums_products.py
+++ sympy/concrete/tests/test_sums_products.py
@@ -298,11 +298,12 @@ def test_is_zero():
         assert func(x, (x, 1, 1)).is_zero is None
 
 def test_is_commutative():
-    assert Sum(1, (x, 1, 1)).is_commutative is True
-    assert Sum(1, (x, 1, x)).is_commutative is True
-    assert Sum(0, (x, y, z)).is_commutative is True
-    assert Sum(x, (y, 1, 2)).is_commutative is True
+    assert Sum(1, (z, 1, 1)).is_commutative is True
+    assert Sum(z, (a, 1, z)).is_commutative is True
+    x=Symbol('x',commutative=False)
+    y=Symbol('y',commutative=False)	
     assert Sum(x, (x, 1, 2)).is_commutative is True
+    assert Sum(y, (x, 1, 2)).is_commutative is False
     assert Sum(x*y, (x, 1, 2), (y, 1, 3)).is_commutative is True
 def test_is_number():
     assert Sum(1, (x, 1, 1)).is_number is True
