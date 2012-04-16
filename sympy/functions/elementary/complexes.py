5a0c66d317d29d2bb936799f0f99717c28121f9b
Chris Smith <smichr@gmail.com>
2012-04-16 09:42:53 +0545

    sign(nonzero real)**even -> 1

--- sympy/functions/elementary/complexes.py
+++ sympy/functions/elementary/complexes.py
@@ -244,6 +244,15 @@ class sign(Function):
     def _eval_is_zero(self):
         return (self.args[0] is S.Zero)
 
+    def _eval_power(self, other):
+        if (
+            self.args[0].is_real and
+            self.args[0].is_nonzero and
+            other.is_integer and
+            other.is_even
+            ):
+            return S.One
+
     def _sage_(self):
         import sage.all as sage
         return sage.sgn(self.args[0]._sage_())
@@ -346,7 +355,7 @@ class Abs(Function):
     def _eval_conjugate(self):
         return self
 
-    def _eval_power(self,other):
+    def _eval_power(self, other):
         if self.args[0].is_real and other.is_integer:
             if other.is_even:
                 return self.args[0]**other
