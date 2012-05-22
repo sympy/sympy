02c9e8017d47ec12e170f9981d6b668dcb0963f5
Guru Devanla <grdvnl@gmail.com>
2012-05-22 09:40:02 -0500

    added test case to latex printing

--- sympy/physics/quantum/tests/test_density.py
+++ sympy/physics/quantum/tests/test_density.py
@@ -1,19 +1,38 @@
-from sympy.physics.quantum.densityOp import Density, matrix_to_density
+from sympy import pprint, latex
+from sympy.physics.quantum.density import Density
+from sympy.physics.quantum.state import Ket, Bra
 from sympy.physics.quantum.qubit import Qubit
 from sympy.physics.quantum.qapply import qapply
 from sympy.physics.quantum.gate import HadamardGate
 from sympy.physics.quantum.represent import represent
 from sympy.physics.quantum.dagger import Dagger
+from sympy.physics.quantum.operator import *
 from sympy.functions import sqrt
 
+def test_eval_args():
+    pass
 
-    
-def test_representDensity():
-    mat = represent(Qubit('01'), nqubits=2)
-    assert represent(Density([Qubit('01'),1]), nqubits=2) == mat*Dagger(mat)    
-    
-def test_apply_operators_density():    
-    assert apply_operators(Density([Qubit('00'),1]).operate_on(HadamardGate(0)\
-           *HadamardGate(1)))\
-        == Density([apply_operators(HadamardGate(0)*HadamardGate(1)*Qubit('00')),1])
+def test_doit():
+    pass
 
+def test_represent():
+    pass
+
+def test_entropy():
+    pass
+
+def entropy():
+    pass
+
+def test_reduced_density():
+    pass
+
+def test_entropy_of_entanglement():
+    pass
+
+def test_latex():
+    d = Density([Ket(0),0.5],[Ket(1),0.5])
+    result =  (r'\rho\left(\begin{pmatrix}{\left|0\right\rangle }, & '
+           r'0.5\end{pmatrix},\begin{pmatrix}{\left|1\right\rangle }, & '
+           r'0.5\end{pmatrix}\right)')
+    assert latex(d) == result
