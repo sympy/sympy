c7f44294cddcb9dd2b0877072feb2dc6934abbd7
Guru Devanla <grdvnl@gmail.com>
2012-05-30 17:36:44 -0500

    Added doc strings, notebook examples for density

--- sympy/physics/quantum/density.py
+++ sympy/physics/quantum/density.py
@@ -12,8 +12,6 @@ from matrixutils import numpy_ndarray, scipy_sparse_matrix, to_numpy
 class Density(HermitianOperator):
     """Density operator for representing mixed states.
 
-    Density operator used to represent mixed states.
-
     TODO: Density operator support for Qubits
 
     Parameters
@@ -22,10 +20,10 @@ class Density(HermitianOperator):
     values : tuples/lists
     Each tuple/list should be of form (state, prob) or [state,prob]
 
-    Examples:
+    Examples
     =========
 
-    Create a density operator with 2 states represented by Kets:
+    Create a density operator with 2 states represented by Kets.
 
     >>> from sympy.physics.quantum.state import Ket
     >>> from sympy.physics.quantum.density import Density
@@ -33,18 +31,7 @@ class Density(HermitianOperator):
     >>> d
     'Density'((|0>, 0.5),(|1>, 0.5))
 
-    We can obtain all states and probs:
-    >>> d.states()
-    (|0>, |1>)
-    >>> d.states()[1]
-    |1>
-
-    >>> d.probs()
-    (0.5, 0.5)
-    >>> d.probs()[1]
-    0.500000000000000
-
-   """
+    """
 
     @classmethod
     def _eval_args(cls, args):
@@ -61,30 +48,110 @@ class Density(HermitianOperator):
         return args
 
     def states(self):
-        """Return list of all states"""
+        """Return list of all states.
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.states()
+        (|0>, |1>)
+
+        """
         return Tuple(*[arg[0] for arg in self.args])
 
     def probs(self):
-        """Return list of all probabilities"""
+        """Return list of all probabilities.
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.probs()
+        (0.5, 0.5)
+
+        """
         return Tuple(*[arg[1] for arg in self.args])
 
     def get_state(self, index):
-        """Return specfic state by index"""
+        """Return specfic state by index.
+
+        Parameters
+        ==========
+
+        index : index of state to be returned
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.states()[1]
+        |1>
+
+        """
         state = self.args[index][0]
         return state
 
     def get_prob(self, index):
-        """Return probability of specific state by index"""
+        """Return probability of specific state by index.
+
+        Parameters
+        ===========
+
+        index : index of states whose probability is returned.
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.probs()[1]
+        0.500000000000000
+
+        """
         prob = self.args[index][1]
         return prob
 
     def apply_op(self, op):
-        """op operates on each individual state"""
+        """op will operate on each individual state.
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> from sympy.physics.quantum.operator import Operator
+        >>> A = Operator('A')
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.apply_op(A)
+        'Density'((A*|0>, 0.5),(A*|1>, 0.5))
+
+        """
         new_args = [(op*state, prob) for (state, prob) in self.args]
         return Density(*new_args)
 
     def doit(self, **hints):
-        """Expands the density operator into an outer product format"""
+        """Expand the density operator into an outer product format.
+
+        Examples
+        =========
+
+        >>> from sympy.physics.quantum.state import Ket
+        >>> from sympy.physics.quantum.density import Density
+        >>> from sympy.physics.quantum.operator import Operator
+        >>> A = Operator('A')
+        >>> d = Density([Ket(0), 0.5], [Ket(1),0.5])
+        >>> d.doit()
+        0.5*|0><0| + 0.5*|1><1|
+
+        """
         terms = []
         for (state, prob) in self.args:
             terms.append(prob*(state*Dagger(state)))
