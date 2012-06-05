462a5e12973f96dc17789b52015fa997ecf73aa0
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-06-06 02:09:17 +0300

    Add some tests for Diagram.
    The tests also attempt to cover some subtle aspects of how the diagram
    treats its objects and morphism.

--- sympy/categories/tests/test_baseclasses.py
+++ sympy/categories/tests/test_baseclasses.py
@@ -1,4 +1,6 @@
-from sympy.categories import Object, Morphism
+from sympy.categories import Object, Morphism, Diagram
+from sympy.utilities.pytest import XFAIL
+from sympy import FiniteSet, EmptySet
 
 def test_object():
     A = Object("A")
@@ -74,3 +76,70 @@ def test_morphism():
     assert Morphism(A, B, "") != Morphism(A, B, "")
 
     assert hash(f) == hash(Morphism(A, B, "f"))
+
+@XFAIL
+def test_diagram():
+    A = Object("A")
+    B = Object("B")
+    C = Object("C")
+
+    f = Morphism(A, B, "f")
+    g = Morphism(B, C, "g")
+    id_A = Morphism(A, A, "1_A")
+    id_B = Morphism(B, B, "1_B")
+
+    empty = EmptySet()
+
+    # Test the addition of identities.
+    d1 = Diagram()
+    d1.add_premise(f)
+
+    assert d1.list_objects() == FiniteSet(A, B)
+    assert d1.hom(A, B) == (FiniteSet(f), empty)
+    assert d1.hom(A, A) == (FiniteSet(Morphism(A, A, "")), empty)
+    assert d1.hom(B, B) == (FiniteSet(Morphism(B, B, "")), empty)
+
+    # Test the addition of composites.
+    d2 = Diagram()
+    d2.add_premise(f)
+    d2.add_premise(g)
+    homAC = d2.hom(A, C)[0]
+
+    assert d2.list_objects() == FiniteSet(A, B, C)
+    assert g * f in d2.premises.keys()
+
+    # Test equality, inequality and hash.
+    d11 = Diagram()
+    d11.add_premise(f)
+
+    assert d1 == d11
+    assert d1 != d2
+    assert hash(d1) == hash(d11)
+
+    d11 = Diagram()
+    d11.add_premise(f, "unique")
+
+    assert d1 != d11
+
+    # Make sure that (re-)adding composites (with new properties)
+    # works as expected.
+    d = Diagram()
+    d.add_premise(f)
+    d.add_premise(g)
+    d.add_conclusion(g * f, "unique")
+
+    assert d.conclusions[g * f] == FiniteSet("unique")
+
+    # Check how the properties of composite morphisms are computed.
+    d = Diagram()
+    d.add_premise(f, "unique", "isomorphism")
+    d.add_premise(g, "unique")
+
+    assert d.premises[g * f] == FiniteSet("unique")
+
+    # Check that conclusion morphisms with new objects are not allowed.
+    d = Diagram()
+    d.add_premise(f)
+    d.add_conclusion(g)
+
+    assert d.conclusions == {}
