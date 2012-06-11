f9ae06cf438ed198bb770dfd8aab001b31637cf3
Tom Bachmann <e_mc_h2@web.de>
2012-06-11 14:09:45 +0100

    [docs/polys] Clean up polys docstrings.
    Note that these changes are based on visual inspection of the compiled docs,
    and not on a detailed study of the source code. The aim of this commit is
    cosmetical.
    No docstrings were changed that are not included in the sphinx doc.

--- sympy/polys/domains/field.py
+++ sympy/polys/domains/field.py
@@ -9,31 +9,31 @@ class Field(Ring):
     has_Field = True
 
     def get_ring(self):
-        """Returns a ring associated with `self`. """
+        """Returns a ring associated with ``self``. """
         raise DomainError('there is no ring associated with %s' % self)
 
     def get_field(self):
-        """Returns a field associated with `self`. """
+        """Returns a field associated with ``self``. """
         return self
 
     def exquo(self, a, b):
-        """Exact quotient of `a` and `b`, implies `__div__`.  """
+        """Exact quotient of ``a`` and ``b``, implies ``__div__``.  """
         return a / b
 
     def quo(self, a, b):
-        """Quotient of `a` and `b`, implies `__div__`. """
+        """Quotient of ``a`` and ``b``, implies ``__div__``. """
         return a / b
 
     def rem(self, a, b):
-        """Remainder of `a` and `b`, implies nothing.  """
+        """Remainder of ``a`` and ``b``, implies nothing.  """
         return self.zero
 
     def div(self, a, b):
-        """Division of `a` and `b`, implies `__div__`. """
+        """Division of ``a`` and ``b``, implies ``__div__``. """
         return a / b, self.zero
 
     def gcd(self, a, b):
-        """Returns GCD of `a` and `b` that is consistent with the core
+        """Returns GCD of ``a`` and ``b`` that is consistent with the core
         implementation. In addition, this allows the primitive of an
         expression to be cleared of Rationals.
 
@@ -59,7 +59,7 @@ class Field(Ring):
         return self.convert(p, ring)/q
 
     def lcm(self, a, b):
-        """Returns LCM of `a` and `b` that is consistent with the core
+        """Returns LCM of ``a`` and ``b`` that is consistent with the core
         implementation.
 
         >>> from sympy.polys.domains import QQ
@@ -82,7 +82,7 @@ class Field(Ring):
         return self.convert(p, ring)/q
 
     def revert(self, a):
-        """Returns `a**(-1)` if possible. """
+        """Returns ``a**(-1)`` if possible. """
         if a:
             return 1/a
         else:
