0a6dcb29af7df6fcb3d81bb39d1813c4535e00d6
Stefan Krastanov <krastanov.stefan@gmail.com>
2012-06-25 14:20:54 +0200

    Print correctly Subs instances in LaTeX.
    Use the substack command.

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -377,6 +377,14 @@ class LatexPrinter(Printer):
         else:
             return r"%s %s" % (tex, self._print(expr.expr))
 
+    def _print_Subs(self, subs):
+        expr, old, new = subs.args
+        latex_expr = self._print(expr)
+        latex_old = (self._print(e) for e in old)
+        latex_new = (self._print(e) for e in new)
+        latex_subs = r'\\ '.join(e[0] + '=' + e[1] for e in zip(latex_old, latex_new))
+        return r'\left. %s \right|_{\substack{ %s }}' % (latex_expr, latex_subs)
+
     def _print_Integral(self, expr):
         tex, symbols = "", []
 
