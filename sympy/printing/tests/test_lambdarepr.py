be7bd00ef615778cf97313d8bb0809c5db968ea4
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-07-01 02:11:18 +0300

    LambdaPrinter: Sort .args before printing.
    This avoids the dependency on the order in which the elements get into
    .args.  This commit also fixes test_lambdarepr.py correspondingly.

--- sympy/printing/tests/test_lambdarepr.py
+++ sympy/printing/tests/test_lambdarepr.py
@@ -37,8 +37,8 @@ def test_piecewise():
     )
     l = lambdarepr(p)
     eval(h + l)
-    assert l == "((1) if (x < 1) else (((2) if (x < 2) else (((0) if (True) "\
-        "else None)))))"
+    assert l == "((0) if (True) else (((1) if (x < 1) else (((2) if (x < 2) "\
+           "else None)))))"
 
     p = Piecewise(
         (1, x < 1),
@@ -55,8 +55,8 @@ def test_piecewise():
     )
     l = lambdarepr(p)
     eval(h + l)
-    assert l == "((x) if (x < 1) else (((x**2) if (((x <= 4) "\
-        "and (x > 3))) else (((0) if (True) else None)))))"
+    assert l == "((0) if (True) else (((x) if (x < 1) else (((x**2) "\
+           "if (((x <= 4) and (x > 3))) else None)))))"
 
     p = Piecewise(
         (x**2, x < 0),
@@ -66,8 +66,8 @@ def test_piecewise():
     )
     l = lambdarepr(p)
     eval(h + l)
-    assert l == "((x**2) if (x < 0) else (((x) if (((x >= 0) and (x < 1))) "\
-        "else (((-x + 2) if (x >= 1) else (((0) if (True) else None)))))))"
+    assert l == "((0) if (True) else (((x) if (((x >= 0) and (x < 1))) "\
+           "else (((x**2) if (x < 0) else (((-x + 2) if (x >= 1) else None)))))))"
 
     p = Piecewise(
         (x**2, x < 0),
@@ -76,8 +76,8 @@ def test_piecewise():
     )
     l = lambdarepr(p)
     eval(h + l)
-    assert l == "((x**2) if (x < 0) else (((x) if (((x >= 0) "\
-        "and (x < 1))) else (((-x + 2) if (x >= 1) else None)))))"
+    assert l == "((x) if (((x >= 0) and (x < 1))) else (((x**2) "\
+           "if (x < 0) else (((-x + 2) if (x >= 1) else None)))))"
 
     p = Piecewise(
         (1, x >= 1),
