5b682b8da378c100283f3a9d8640b0ffe86e5ad6
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-07-01 15:40:41 +0300

    LatexPrinter: Always sort the arguments of Min and Max.

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -507,11 +507,13 @@ class LatexPrinter(Printer):
         return tex
 
     def _print_Min(self, expr, exp=None):
-        texargs = [r"%s" % self._print(symbol) for symbol in expr.args]
+        args = sorted(expr.args, key=default_sort_key)
+        texargs = [r"%s" % self._print(symbol) for symbol in args]
         return r"\min\left(%s\right)" % ", ".join(texargs)
 
     def _print_Max(self, expr, exp=None):
-        texargs = [r"%s" % self._print(symbol) for symbol in expr.args]
+        args = sorted(expr.args, key=default_sort_key)
+        texargs = [r"%s" % self._print(symbol) for symbol in args]
         return r"\max\left(%s\right)" % ", ".join(texargs)
 
     def _print_floor(self, expr, exp=None):
