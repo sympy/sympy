ff5223f99267fc3565a357e8ee6da87cec732b01
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-07-04 01:52:58 +0300

    DiagramGrid: Rearrange the tests a bit.
    This commit assures a slightly better logical grouping of the tests.

--- sympy/categories/tests/test_drawing.py
+++ sympy/categories/tests/test_drawing.py
@@ -245,6 +245,39 @@ def test_DiagramGrid():
     assert grid[2, 2] == A4
     assert grid[2, 3] == A8
 
+    # A line diagram.
+    A = Object("A")
+    B = Object("B")
+    C = Object("C")
+    D = Object("D")
+    E = Object("E")
+
+    f = NamedMorphism(A, B, "f")
+    g = NamedMorphism(B, C, "g")
+    h = NamedMorphism(C, D, "h")
+    i = NamedMorphism(D, E, "i")
+    d = Diagram([f, g, h, i])
+    grid = DiagramGrid(d, layout="sequential")
+
+    assert grid.width == 5
+    assert grid.height == 1
+    assert grid[0, 0] == A
+    assert grid[0, 1] == B
+    assert grid[0, 2] == C
+    assert grid[0, 3] == D
+    assert grid[0, 4] == E
+
+    # Test the transposed version.
+    grid = DiagramGrid(d, layout="sequential", transpose=True)
+
+    assert grid.width == 1
+    assert grid.height == 5
+    assert grid[0, 0] == A
+    assert grid[1, 0] == B
+    assert grid[2, 0] == C
+    assert grid[3, 0] == D
+    assert grid[4, 0] == E
+
     # A pullback.
     m1 = NamedMorphism(A, B, "m1")
     m2 = NamedMorphism(A, C, "m2")
@@ -355,35 +388,3 @@ def test_DiagramGrid():
     assert grid[2, 4] is None
     assert grid[2, 5] == E
 
-    # A line diagram.
-    A = Object("A")
-    B = Object("B")
-    C = Object("C")
-    D = Object("D")
-    E = Object("E")
-
-    f = NamedMorphism(A, B, "f")
-    g = NamedMorphism(B, C, "g")
-    h = NamedMorphism(C, D, "h")
-    i = NamedMorphism(D, E, "i")
-    d = Diagram([f, g, h, i])
-    grid = DiagramGrid(d, layout="sequential")
-
-    assert grid.width == 5
-    assert grid.height == 1
-    assert grid[0, 0] == A
-    assert grid[0, 1] == B
-    assert grid[0, 2] == C
-    assert grid[0, 3] == D
-    assert grid[0, 4] == E
-
-    # Test the transposed version.
-    grid = DiagramGrid(d, layout="sequential", transpose=True)
-
-    assert grid.width == 1
-    assert grid.height == 5
-    assert grid[0, 0] == A
-    assert grid[1, 0] == B
-    assert grid[2, 0] == C
-    assert grid[3, 0] == D
-    assert grid[4, 0] == E
