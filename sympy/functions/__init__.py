25198bd5421cd7404a90338fa9392190578bea22
Raoul Bourquin <raoulb@bluewin.ch>
2012-07-07 12:24:46 +0200

    Skeleton for elliptic integrals

--- sympy/functions/__init__.py
+++ sympy/functions/__init__.py
@@ -39,5 +39,7 @@ from sympy.functions.special.polynomials import (legendre, assoc_legendre,
         hermite, chebyshevt, chebyshevu, chebyshevu_root, chebyshevt_root,
         laguerre, assoc_laguerre, gegenbauer, jacobi, jacobi_normalized)
 from sympy.functions.special.spherical_harmonics import Ylm, Zlm
+from sympy.functions.special.elliptic_integrals import (ellipticeinc,
+		ellipticfinc, ellipticpiinc, elliptick, elliptice, ellipticpi)
 
 ln = log
