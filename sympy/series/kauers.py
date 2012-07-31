350595bc94863e57d9db1e91ba1debe112756ca3
Saurabh <saurabh.jhaa@gmail.com>
2012-07-31 17:10:06 +0530

    kauers: triedto address the comments but not sure if the new variable names are appropriate. However, all assertions are complete

--- sympy/series/kauers.py
+++ sympy/series/kauers.py
@@ -1,33 +1,32 @@
 from sympy import expand
-"""
-Takes as input the expression and the variable used in constructing the expression and returns the diffrence between function's value when the input is incremented to 1 and the original function value
 
-More specifically, it takes as input the function f(x) expression and x used in function expression and returns f(x + 1) - f(x) 
+def delta(expression, variable, increment = 1):
 
-If you want an increment other than 1, supply it as a third argument to the Delta
+    """
+    Takes as input the expression and the variable used in constructing the expression
+    and returns the diffrence between function's value when the input is incremented
+    to 1 and the original function value. More specifically, it takes as input the
+    function f(x) expression and x used in function expression as variable
+    and returns f(x + 1) - f(x). 
 
-Examples
-=========
+    If you want an increment other than 1, supply it as a third argument to the Delta
 
->>> Delta(x**2, x) 
-2*x + 1
->>> Delta(y**3 + 2*y**2 + 3*y + 4, y) 
-3*y**2 + 7*y + 6
->>> Delta(x**2 + 3*x + 8, x, 2) 
-4*x + 10
->>>Delta(z**3 + 8*z, z, 3)
-9*z**2 + 27*z + 51
+    Examples
+    =========
 
-"""
+    >>> Delta(x**2, x) 
+    2*x + 1
+    >>> Delta(y**3 + 2*y**2 + 3*y + 4, y) 
+    3*y**2 + 7*y + 6
+    >>> Delta(x**2 + 3*x + 8, x, 2) 
+    4*x + 10
+    >>>Delta(z**3 + 8*z, z, 3)
+    9*z**2 + 27*z + 51
 
-def Delta(expr, n, d = 1):
-    expr =(expr).expand()
-    expr2 = expr.subs(n, n + d)
-    expr2 = (expr2).expand()
-    expr = expr2 - expr
-    expr = expr.expand()
-    return expr
-
-
-
-
+    """
+    expression = expression.expand()
+    expression2 = expression.subs(variable, variable + increment)
+    expression2 = expression2.expand()
+    expression = expression2 - expression
+    expression = expression.expand()
+    return expression
