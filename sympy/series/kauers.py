776810c0c249fe063dca01ae0298dc7a6f544cf8
Saurabh <saurabh.jhaa@gmail.com>
2012-08-03 22:11:53 +0530

    kauers:addressed previous comments and added a new function dominant_term() along with it's tests

--- sympy/series/kauers.py
+++ sympy/series/kauers.py
@@ -1,32 +1,51 @@
 from sympy import expand
+from sympy import diff
 
-def delta(expression, variable, increment = 1):
-
+def finite_diff(expression, variable, increment = 1):
     """
     Takes as input the expression and the variable used in constructing the expression
     and returns the diffrence between function's value when the input is incremented
     to 1 and the original function value. More specifically, it takes as input the
-    function f(x) expression and x used in function expression as variable
-    and returns f(x + 1) - f(x). 
-
-    If you want an increment other than 1, supply it as a third argument to the Delta
+    function f(x) expression and x used in function expression as variable and returns
+    f(x + 1)-f(x).If you want an increment other than 1, supply it as a
+    third argument to the finite_diff
 
     Examples
     =========
-
-    >>> Delta(x**2, x) 
+    >>> from sympy.abc import x, y, z
+    >>> from sympy.series.kauers import finite_diff
+    >>> finite_diff(x**2, x)
     2*x + 1
-    >>> Delta(y**3 + 2*y**2 + 3*y + 4, y) 
+    >>> finite_diff(y**3 + 2*y**2 + 3*y + 4, y)
     3*y**2 + 7*y + 6
-    >>> Delta(x**2 + 3*x + 8, x, 2) 
+    >>> finite_diff(x**2 + 3*x + 8, x, 2)
     4*x + 10
-    >>>Delta(z**3 + 8*z, z, 3)
+    >>> finite_diff(z**3 + 8*z, z, 3)
     9*z**2 + 27*z + 51
-
     """
     expression = expression.expand()
     expression2 = expression.subs(variable, variable + increment)
     expression2 = expression2.expand()
-    expression = expression2 - expression
-    expression = expression.expand()
-    return expression
+    return expression2 - expression
+
+def dominant_term(expr, n):
+     """
+     Takes as input a polynomial expression along with it's variable and
+     returns the dominant terms of the expression
+
+     Examples
+     =========
+     >>> from sympy.abc import n, x, z
+     >>> from sympy.series.kauers import dominant_term
+     >>> dominant_term(3*n**2 + 4*n + 8, n)
+     n**2
+     >>> dominant_term(4*x**5 + 6*x**3 + 2*x**2 + 5, x)
+     x**5
+     >>> dominant_term(z**3 + 3*z**2 +4*z + 8, z)
+     z**3
+     """
+     counter = 1
+     while (expr != 0):
+         expr = diff(expr, n)
+         counter = counter + 1
+     return n**(counter - 2)
