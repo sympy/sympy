1f68c2cd0311f3b92969524b25061eaa74177c00
Raoul Bourquin <raoulb@bluewin.ch>
2012-08-06 17:15:15 +0200

    Test low-level implementation of Gegenbauer polynomials

--- sympy/polys/tests/test_orthopolys.py
+++ sympy/polys/tests/test_orthopolys.py
@@ -1,9 +1,10 @@
 """Tests for efficient functions for generating orthogonal polynomials. """
 
-from sympy import Poly, Rational as Q
+from sympy import Poly, S, Rational as Q
 from sympy.utilities.pytest import raises
 
 from sympy.polys.orthopolys import (
+    gegenbauer_poly,
     chebyshevt_poly,
     chebyshevu_poly,
     hermite_poly,
@@ -12,6 +13,19 @@ from sympy.polys.orthopolys import (
 )
 
 from sympy.abc import x, a
+
+def test_gegenbauer_poly():
+    raises(ValueError, lambda: gegenbauer_poly(-1, a, x))
+
+    assert gegenbauer_poly(1, a, x, polys=True) == Poly(2*a*x, x, domain='ZZ(a)')
+
+    assert gegenbauer_poly(0, a, x) == 1
+    assert gegenbauer_poly(1, a, x) == 2*a*x
+    assert gegenbauer_poly(2, a, x) == -a + x**2*(2*a**2 + 2*a)
+    assert gegenbauer_poly(3, a, x) == x**3*(4*a**3/3 + 4*a**2 + 8*a/3) + x*(-2*a**2 - 2*a)
+
+    assert gegenbauer_poly(1, S.Half).dummy_eq(x)
+    assert gegenbauer_poly(1, a , polys=True) == Poly(2*a*x, x, domain='ZZ(a)')
 
 def test_chebyshevt_poly():
     raises(ValueError, lambda: chebyshevt_poly(-1, x))
