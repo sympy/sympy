2a97f80b99a47220fcba28d87cfef27c36bcac39
Raoul Bourquin <raoulb@bluewin.ch>
2012-08-25 18:03:19 +0200

    Fix doctests

--- sympy/functions/special/spherical_harmonics.py
+++ sympy/functions/special/spherical_harmonics.py
@@ -26,7 +26,7 @@ class Ynm(Function):
     Examples
     ========
 
-    >>> from sympy import Ynm, S, conjugate, diff
+    >>> from sympy import Ynm, Symbol
     >>> from sympy.abc import n,m
     >>> theta = Symbol("theta")
     >>> phi = Symbol("phi")
@@ -36,10 +36,20 @@ class Ynm(Function):
 
     Several symmetries are known, for the order
 
+    >>> from sympy import Ynm, Symbol
+    >>> from sympy.abc import n,m
+    >>> theta = Symbol("theta")
+    >>> phi = Symbol("phi")
+
     >>> Ynm(n, -m, theta, phi)
     (-1)**m*exp(-2*I*m*phi)*Ynm(n, m, theta, phi)
 
     as well as for the angles
+
+    >>> from sympy import Ynm, Symbol, simplify
+    >>> from sympy.abc import n,m
+    >>> theta = Symbol("theta")
+    >>> phi = Symbol("phi")
 
     >>> Ynm(n, m, -theta, phi)
     Ynm(n, m, theta, phi)
@@ -80,6 +90,11 @@ class Ynm(Function):
     We can differentiate the functions with respect
     to both angles
 
+    >>> from sympy import Ynm, Symbol, diff
+    >>> from sympy.abc import n,m
+    >>> theta = Symbol("theta")
+    >>> phi = Symbol("phi")
+
     >>> diff(Ynm(n, m, theta, phi), theta)
     m*cot(theta)*Ynm(n, m, theta, phi) + sqrt((-m + n)*(m + n + 1))*exp(-I*phi)*Ynm(n, m + 1, theta, phi)
 
@@ -88,13 +103,21 @@ class Ynm(Function):
 
     Further we can compute the complex conjugation
 
+    >>> from sympy import Ynm, Symbol, conjugate
+    >>> from sympy.abc import n,m
+    >>> theta = Symbol("theta")
+    >>> phi = Symbol("phi")
+
     >>> conjugate(Ynm(n, m, theta, phi))
     (-1)**(2*m)*exp(-2*I*m*phi)*Ynm(n, m, theta, phi)
 
     To get back the well known expressions in spherical
     coordinates we use full expansion
 
-    >>> from sympy import expand_func
+    >>> from sympy import Ynm, Symbol, expand_func
+    >>> from sympy.abc import n,m
+    >>> theta = Symbol("theta")
+    >>> phi = Symbol("phi")
 
     >>> expand_func(Ynm(n, m, theta, phi))
     sqrt((2*n + 1)*(-m + n)!/(m + n)!)*exp(I*m*phi)*assoc_legendre(n, m, cos(theta))/(2*sqrt(pi))
