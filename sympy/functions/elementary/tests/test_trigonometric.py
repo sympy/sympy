1d219e34b4e71d150cf5295c89a81400f1a4384c
zanzibar7 <tcr-public2@psu.edu>
2012-08-27 17:05:18 -0400

    changed / to // in one place for compatibility

--- sympy/functions/elementary/tests/test_trigonometric.py
+++ sympy/functions/elementary/tests/test_trigonometric.py
@@ -829,7 +829,7 @@ def test_sincos_rewrite_sqrt():
     for p in [1,3,5,17,3*5*17]:
         for t in [ 1, 8]:
             n=t*p
-            for i in xrange(1,(n+1)/2+1):
+            for i in xrange(1,(n+1)//2+1):
                 if 1 == gcd(i,n):
                     x = i*pi/n
                     s1 = sin(x).rewrite(sqrt)
