92142befb50fc0429717208e928c63cc77971c21
Chris Smith <smichr@gmail.com>
2012-08-31 09:10:47 +0545

    combinatorics: more classes imported in init

--- sympy/combinatorics/__init__.py
+++ sympy/combinatorics/__init__.py
@@ -4,4 +4,10 @@ from sympy.combinatorics.generators import cyclic, alternating, symmetric, dihed
 from sympy.combinatorics.subsets import Subset
 from sympy.combinatorics.partitions import (Partition, IntegerPartition,
     RGS_rank, RGS_unrank, RGS_enum)
-from sympy.combinatorics.polyhedron import Polyhedron
+from sympy.combinatorics.polyhedron import (Polyhedron, tetrahedron, cube,
+    octahedron, dodecahedron, icosahedron)
+from sympy.combinatorics.perm_groups import PermutationGroup
+from sympy.combinatorics.group_constructs import DirectProduct
+from sympy.combinatorics.graycode import GrayCode
+from sympy.combinatorics.named_groups import (SymmetricGroup, DihedralGroup,
+    CyclicGroup, AlternatingGroup, AbelianGroup, RubikGroup)
