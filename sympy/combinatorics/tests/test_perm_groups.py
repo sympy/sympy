445be4ad9f6e280e6f1aab88a94d61e9a4da7370
Chris Smith <smichr@gmail.com>
2012-09-06 16:27:17 +0545

    perm_groups: clean up coset_decomposition -> coset_factor

--- sympy/combinatorics/tests/test_perm_groups.py
+++ sympy/combinatorics/tests/test_perm_groups.py
@@ -178,24 +178,24 @@ def test_coset_rank():
     assert G.coset_rank(gens[0]) == 6
     assert G.coset_unrank(6) == gens[0]
 
-def test_coset_decomposition():
+def test_coset_factor():
     a = Permutation([2,0,1,3,4,5])
     b = Permutation([2,1,3,4,5,0])
     g = PermutationGroup([a, b])
     assert g.order() == 360
     rep = g.coset_repr()
     d = Permutation([1,0,2,3,4,5])
-    assert not g.coset_decomposition(d.array_form)
+    assert not g.coset_factor(d.array_form)
     assert not g.has_element(d)
     c = Permutation([1,0,2,3,5,4])
-    v = g.coset_decomposition(c)
+    v = g.coset_factor(c)
     assert _af_muln(*v) == [1,0,2,3,5,4]
     assert g.has_element(c)
 
     a = Permutation([0,2,1])
     g = PermutationGroup([a])
     c = Permutation([2,1,0])
-    assert not g.coset_decomposition(c)
+    assert not g.coset_factor(c)
     assert g.coset_rank(c) == None
 
 def test_orbits():
