43f42de78dd77a057246eac612b61ba4fc0ea05e
Chris Smith <smichr@gmail.com>
2012-10-06 00:52:47 +0545

    give simplify method same interface as function

--- sympy/core/expr.py
+++ sympy/core/expr.py
@@ -2721,10 +2721,12 @@ class Expr(Basic, EvalfMixin):
         from sympy.integrals import integrate
         return integrate(self, *args, **kwargs)
 
-    def simplify(self):
+    def simplify(self, ratio=1.7, measure=None):
         """See the simplify function in sympy.simplify"""
         from sympy.simplify import simplify
-        return simplify(self)
+        from sympy.core.function import count_ops
+        measure = measure or count_ops
+        return simplify(self, ratio, measure)
 
     def nsimplify(self, constants=[], tolerance=None, full=False):
         """See the nsimplify function in sympy.simplify"""
