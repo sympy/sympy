0f523d836cc7f62c508845b2006fbc5ea84e43d8
Matthew Rocklin <mrocklin@gmail.com>
2012-10-05 12:25:30 -0500

    add test for rmid

--- sympy/rr/rl.py
+++ sympy/rr/rl.py
@@ -4,8 +4,13 @@ def rmid(isid):
     def ident_remove(expr):
         """ Remove identities """
         ids = map(isid, expr.args)
-        if sum(ids) > 0 and len(expr.args) > 1:
-            return expr.__class__(*[arg for arg, x in zip(expr.args, ids) if x])
-        else:
+        if sum(ids) == 0:           # No identities. Common case
             return expr
+        elif sum(ids) != len(ids):  # there is at least one non-identity
+            return expr.__class__(*[arg for arg, x in zip(expr.args, ids)
+                                        if  not x])
+        else:
+            first_id = (arg for arg, x in zip(expr.args, ids) if x).next()
+            return expr.__class__(first_id)
+
     return ident_remove
