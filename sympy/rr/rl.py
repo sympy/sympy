110afed09bd882a422ff11669a63e5c82d4eb9be
Matthew Rocklin <mrocklin@gmail.com>
2012-10-10 07:17:46 -0500

    expand glom docstring

--- sympy/rr/rl.py
+++ sympy/rr/rl.py
@@ -67,6 +67,14 @@ def glom(key, count, combine):
 
     >>> rl(Add(x, -x, 3*x, 2, 3, evaluate=False))
     3*x + 5
+
+    # Wait, how are key, count and combine supposed to work?
+    >>> key(2*x)
+    x
+    >>> count(2*x)
+    2
+    >>> combine(2, x)
+    2*x
     """
     def conglomerate(expr):
         """ Conglomerate together identical args x + x -> 2x """
