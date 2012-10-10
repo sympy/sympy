15593403c1db3c7ab23dd9f20a738813f9a95ba5
Matthew Rocklin <mrocklin@gmail.com>
2012-10-10 17:42:05 -0500

    docstring for rr module

--- sympy/rr/__init__.py
+++ sympy/rr/__init__.py
@@ -1,4 +1,26 @@
-# Rewrite Rule engine
+""" Rewrite Rules
+
+A rule is a function that transforms one expression into another
+
+    Rule :: Expr -> Expr
+
+A strategy is a function that says how a rule should be applied to a syntax
+tree. In general strategies take rules and produce a new rule
+
+    Strategy :: [Rules], Other-stuff -> Rule
+
+This allows developers to separate a mathematical transformation from the
+algorithmic details of applying that transformation. The goal is to separate
+the work of mathematical programming from algorithmic programming.
+
+Submodules
+
+rr.rl         - some fundamental rules
+rr.strat_pure - strategies that have nothing to do with SymPy
+rr.traverse   - strategies that traverse a SymPy AST
+rr.strat      - some conglomerate strategies that do depend on SymPy
+"""
+
 import rl
 import traverse
 from rl import rm_id, unpack, flatten, sort, glom, distribute
