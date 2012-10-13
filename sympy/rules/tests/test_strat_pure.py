575195345c648b28ba759e0f8592bdbce1c158c4
Matthew Rocklin <mrocklin@gmail.com>
2012-10-12 22:46:28 -0500

    test strat_pure

--- sympy/rules/tests/test_strat_pure.py
+++ sympy/rules/tests/test_strat_pure.py
@@ -1,4 +1,5 @@
-from sympy.rules.strat_pure import null_safe
+from sympy.rules.strat_pure import (null_safe, exhaust, memoize, condition,
+        chain, try_safe)
 
 def test_null_safe():
     def rl(expr):
@@ -9,3 +10,39 @@ def test_null_safe():
 
     assert      rl(3) == None
     assert safe_rl(3) == 3
+
+def posdec(x):
+    if x > 0:
+        return x-1
+    else:
+        return x
+def test_exhaust():
+    sink = exhaust(posdec)
+    assert sink(5) == 0
+    assert sink(10) == 0
+
+def test_memoize():
+    rl = memoize(posdec)
+    assert rl(5) == posdec(5)
+    assert rl(5) == posdec(5)
+    assert rl(-2) == posdec(-2)
+
+def test_condition():
+    rl = condition(lambda x: x%2 == 0, posdec)
+    assert rl(5) == 5
+    assert rl(4) == 3
+
+def test_chain():
+    rl = chain(posdec, posdec)
+    assert rl(5) == 3
+    assert rl(1) == 0
+
+def test_try_safe():
+    def rl(expr):
+        assert False
+    safe_rl = try_safe(rl)
+    assert safe_rl(1) == 1
+
+def do_one():
+    rl = do_one(posdec, posdec)
+    assert rl(5) == 4
