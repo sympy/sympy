50d728985a6ae9ccd0e3713dff876aa5b2a0db24
Matthew Hoff <mhoff@calpoly.edu>
2012-10-25 13:45:07 -0700

    Removed unecessary code

--- sympy/physics/quantum/sho1d.py
+++ sympy/physics/quantum/sho1d.py
@@ -1,20 +1,12 @@
 """Simple Harmonic Oscillator 1-Dimension"""
 
-<<<<<<< HEAD
+
 #%load_ext sympyprinting
-=======
-%load_ext sympyprinting
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
 
 from sympy import *
 from sympy.physics.quantum import *
 from sympy.physics.quantum.qexpr import *
 #from sympy.physics.quantum.cartesian import*
-<<<<<<< HEAD
-
-=======
-#from sympy.printing.pretty.stringpict import prettyForm
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
 
 #--------------------------------------------------------------------
 
@@ -22,7 +14,6 @@ class RaisingOp(Operator):
 	
 	@classmethod
 	def default_args(self):
-<<<<<<< HEAD
 		args = QExpr._eval_args(args)
 		if len(args) == 1:
 			return("a",)
@@ -63,75 +54,18 @@ class RaisingOp(Operator):
 	def _print_label_pretty(self,printer, *args):
 		return self._print_sequence(
 			self.label, self._pretty, self._label_separator, printer, *args
-=======
-		return("a",)
-	
-	def _eval_adjoint(self):
-		return Dagger(self)
-		
-	# Printing
-	
-	_label_separator = ','
-	
-	def _print_sequence(self, seq, printer, *args):
-		result = []
-		for item in seq:
-			result.append(printer._print(item, *args))
-		return sep.join(result)
-		
-	def _print_sequence_pretty(self, seq, printer, *args):
-		pform = printer._print(seq[0], *args)
-		for item in seq[1]:
-			pform = prettyForm(*pform.right((sep)))
-			pform = prettyForm(*pform.right((printer._print(item, *args))))
-		return pform
-	
-	def _print_label(self, printer, *args):
-		return self._print_sequence(
-			self.label, self._label_separator, printer, *args
-		)
-	
-	def _print_label_repr(self, printer, *args):
-		return self._print_sequence(
-			self.label, ',', printer, *args
-		)
-		
-	def _print_label_pretty(self, printer, *args):
-		return self._print_sequence_pretty(
-			self.label, self._label_separator, printer, *args
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
 		)
 		
 	def _print_label_latex(self, printer, *args):
 		return self._print_sequence(
-<<<<<<< HEAD
 			self.label, self._latex, self._label_separator, printer, *args
 		)
-	
-	
-	
-=======
-			self.label, self._label_separator, printer, *args
-		)
-		
-	def _print_contents(self, printer, *args):
-		return self._printer_label(printer, *args)
-		
-	def _print_contents_repr(self, printer, *args):
-		return self._print_label_repr(printer, *args)
-		
-	def _print_contents_pretty(self, printer, *args):
-		return self._print_label_pretty(printer, *args)
-		
-	def _print_contents_latex(self, printer, *args):
-		return self._print_label_latex(printer, *args)
 
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
 class LoweringOp(Operator):
 
 	@classmethod
 	def default_args(self):
-<<<<<<< HEAD
+
 		args = QExpr._eval_args(args)
 		if len(args) == 1:
 			return("a",)
@@ -143,42 +77,22 @@ class LoweringOp(Operator):
 		
 	def _eval_commutator_RaisingOp(self, other):
 		return 1
-=======
-		return ("a",)
-		
-	def _eval_adjoint(self):
-		return Dagger(self)
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
+
 
 
 #--------------------------------------------------------------------
 
-		
-<<<<<<< HEAD
-	
-class SHOState(State):
-	pass
 
-
-class SHOKet(SHOState, Ket):
-=======
-class SHOBase(StateBase):
-	pass
-
-
-	
 class SHOState(SHOBase):
 	pass
 
 
-class SHOKet(SHOState, KetBase):
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
+class SHOKet(SHOState, Ket):
 	
 	@classmethod
 	def dual_class(self):
 		return SHOBra
-		
-<<<<<<< HEAD
+
 	def _eval_innerproduct_SHOBra(self, bra, **hints):
 		result = KroneckerDelta(self, bra)
 		return result
@@ -188,11 +102,4 @@ class SHOBra(SHOState, Bra):
 	@classmethod
 	def dual_class(self):
 		return SHOKet
-	
-=======
-class SHOBra(SHOState, BraBase):
 
-	@classmethod
-	def dual_class(self):
-		return SHOKet
->>>>>>> a9a035523a4ce79aa10c68494d2655b7ef5ae39e
