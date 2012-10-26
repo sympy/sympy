aefd2de82602bc37c1d5b1d8c1ab15acdfdc1fc9
Matthew Rocklin <mrocklin@gmail.com>
2012-10-26 07:09:53 -0700

    add Hadamard + Matrix product test

--- sympy/matrices/expressions/tests/test_hadamard.py
+++ sympy/matrices/expressions/tests/test_hadamard.py
@@ -5,9 +5,11 @@ from sympy.utilities.pytest import raises
 
 
 def test_HadamardProduct():
-    n, m = symbols('n,m')
+    n, m, k = symbols('n,m,k')
+    Z = MatrixSymbol('Z', n, n)
     A = MatrixSymbol('A', n, m)
     B = MatrixSymbol('B', n, m)
+    C = MatrixSymbol('C', m, k)
 
     assert HadamardProduct(A, B, A).shape == A.shape
 
@@ -17,3 +19,6 @@ def test_HadamardProduct():
     raises(TypeError,  lambda: 5 - A)
 
     assert HadamardProduct(A, 2*B, -A)[1, 1] == -2 * A[1, 1]**2 * B[1, 1]
+
+    mix = HadamardProduct(Z*A, B)*C
+    assert mix.shape == (n, k)
