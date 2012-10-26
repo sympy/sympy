de2fcafe26be4422dd675ee88ef48647cab1e23e
Matthew Rocklin <mrocklin@gmail.com>
2012-10-26 07:07:55 -0700

    fix test_args test for HadamardProduct

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1633,7 +1633,7 @@ def test_sympy__matrices__expressions__hadamard__HadamardProduct():
     from sympy.matrices.expressions.hadamard import HadamardProduct
     from sympy.matrices.expressions import MatrixSymbol
     X = MatrixSymbol('X', x, y)
-    Y = MatrixSymbol('Y', y, x)
+    Y = MatrixSymbol('Y', x, y)
     assert _test_args(HadamardProduct(X, Y))
 
 def test_sympy__matrices__expressions__matpow__MatPow():
