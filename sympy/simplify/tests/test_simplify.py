914a06a4d361d33bda20d1fc3285a70d57fe4e91
Chris Smith <smichr@gmail.com>
2012-10-26 12:28:42 +0545

    pep8 E261

--- sympy/simplify/tests/test_simplify.py
+++ sympy/simplify/tests/test_simplify.py
@@ -354,7 +354,7 @@ def test_simplify_complex():
     cosAsExp = cos(x)._eval_rewrite_as_exp(x)
     tanAsExp = tan(x)._eval_rewrite_as_exp(x)
     assert simplify(cosAsExp*tanAsExp).expand() == (
-        sin(x))._eval_rewrite_as_exp(x).expand() # issue 1242
+        sin(x))._eval_rewrite_as_exp(x).expand()  # issue 1242
 
 def test_simplify_ratio():
     # roots of x**3-3*x+5
