195ece1df9c7c77b258dd308aa430ab645e063e3
Matthew Rocklin <mrocklin@gmail.com>
2012-10-31 12:20:39 -0500

    remove SymPy dependence from traverse
    All dependence is now in util.py

--- sympy/rules/traverse.py
+++ sympy/rules/traverse.py
@@ -1,21 +1,20 @@
-# Strategies to traverse a SymPy Tree
-from sympy import Basic
-from util import new
+# Strategies to traverse a Tree
+from util import new, is_leaf, children
 
 def top_down(rule):
     """ Apply a rule down an AST running it on the top nodes first """
     def top_down_rl(expr):
         newexpr = rule(expr)
-        if not isinstance(newexpr, Basic) or newexpr.is_Atom:
+        if is_leaf(newexpr):
             return newexpr
-        return new(newexpr.__class__, *map(top_down_rl, newexpr.args))
+        return new(type(newexpr), *map(top_down_rl, children(newexpr)))
     return top_down_rl
 
 def bottom_up(rule):
     """ Apply a rule down an AST running it on the bottom nodes first """
     def bottom_up_rl(expr):
-        if not isinstance(expr, Basic) or expr.is_Atom:
+        if is_leaf(expr):
             return rule(expr)
         else:
-            return rule(new(expr.__class__, *map(bottom_up_rl, expr.args)))
+            return rule(new(type(expr), *map(bottom_up_rl, children(expr))))
     return bottom_up_rl
