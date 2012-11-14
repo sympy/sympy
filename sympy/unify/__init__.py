a0183027744a563493e816c52a6b5249d31a422c
Matthew Rocklin <mrocklin@gmail.com>
2012-11-14 12:01:39 -0600

    add blogpost link in docstring

--- sympy/unify/__init__.py
+++ sympy/unify/__init__.py
@@ -1,6 +1,9 @@
 """ Unification in SymPy
 
-See sympy.unify.core for details"""
+See sympy.unify.core docstring for algorithmic details
+
+See http://matthewrocklin.com/blog/work/2012/11/01/Unification/ for discussion
+"""
 
 import core
 import usympy
