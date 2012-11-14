9c591903dfb1728cc6dfac1e1144fe3fe12827b2
Matthew Rocklin <mrocklin@gmail.com>
2012-11-14 10:20:59 -0600

    default imports

--- sympy/unify/__init__.py
+++ sympy/unify/__init__.py
@@ -1,3 +1,9 @@
+""" Unification in SymPy
+
+See sympy.unify.core for details"""
+
 import core
 import usympy
 import rewrite
+from usympy import unify, patternify, rebuild
+from rewrite import rewriterule
