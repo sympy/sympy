e20f4fd1bd10bcbeab446a59e8df4e381078dc95
Matthew Rocklin <mrocklin@gmail.com>
2012-11-14 07:58:11 -0600

    add CompositeComputation and unique function

--- sympy/computations/core.py
+++ sympy/computations/core.py
@@ -1,3 +1,12 @@
+import itertools
+
+def unique(seq):
+    seen = set()
+    for item in seq:
+        if item not in seen:
+            seen.add(item)
+            yield item
+
 class Computation(object):
 
     def inputs(self):
@@ -7,7 +16,17 @@ class Computation(object):
         raise NotImplementedError()
 
     def edges(self):
-        import itertools
         inedges  = ((i, self) for i in self.inputs())
         outedges = ((self, o) for o in self.outputs())
         return itertools.chain(inedges, outedges)
+
+class CompositeComputation(Computation):
+
+    def inputs(self):
+        return unique(itertools.chain(*[c.inputs() for c in self.computations]))
+
+    def outputs(self):
+        return unique(itertools.chain(*[c.outputs() for c in self.computations]))
+
+    def edges(self):
+        return itertools.chain(*[c.edges() for c in self.computations])
--- sympy/computations/tests/test_core.py
+++ sympy/computations/tests/test_core.py
@@ -1,7 +1,10 @@
-from sympy.computations.core import Computation
+from sympy.computations.core import Computation, unique
 
 def test_Computation():
     C = Computation()
     assert hasattr(C, 'inputs')
     assert hasattr(C, 'outputs')
     assert hasattr(C, 'edges')
+
+def test_unique():
+    assert tuple(unique((1, 3, 1, 2))) == (1, 3, 2)
