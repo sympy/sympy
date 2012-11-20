8f8f1919790838bbc0b2dff61d71c4b9fef6056c
Matthew Rocklin <mrocklin@gmail.com>
2012-11-19 19:53:01 -0600

    remove submodule imports

--- sympy/unify/__init__.py
+++ sympy/unify/__init__.py
@@ -5,8 +5,5 @@ See sympy.unify.core docstring for algorithmic details
 See http://matthewrocklin.com/blog/work/2012/11/01/Unification/ for discussion
 """
 
-import core
-import usympy
-import rewrite
 from usympy import unify, patternify, rebuild
 from rewrite import rewriterule
