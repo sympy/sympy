8059fa4d0dec72514a5bc7d108f017086d2732e5
nthorne <notrupertthorne@gmail.com>
2012-12-03 22:52:29 +0100

    Changed to HTML entity codes for wikipedia reference, since closing parenthesis was not parsed properly by linkcheck.

--- sympy/physics/quantum/operator.py
+++ sympy/physics/quantum/operator.py
@@ -90,7 +90,7 @@ class Operator(QExpr):
     References
     ==========
 
-    .. [1] http://en.wikipedia.org/wiki/Operator_(physics)
+    .. [1] http://en.wikipedia.org/wiki/Operator_&#40;physics&#41;
     .. [2] http://en.wikipedia.org/wiki/Observable
     """
 
