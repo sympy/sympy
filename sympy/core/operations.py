3827bb56d4bade1cf915cd74ba1a66de00761b96
Chris Smith <smichr@gmail.com>
2012-12-05 08:32:57 +0545

    3539b: do not let Mul become more complex

--- sympy/core/operations.py
+++ sympy/core/operations.py
@@ -190,7 +190,7 @@ class AssocOp(Basic):
                 return None
             newpattern = self.func(*wild_part)
             newexpr = self._combine_inverse(expr, exact)
-            if expr.is_Add and newexpr.count_ops() > expr.count_ops():
+            if (expr.is_Add or expr.is_Mul) and newexpr.count_ops() > expr.count_ops():
                 return None
             return newpattern.matches(newexpr, repl_dict)
 
--- sympy/core/tests/test_match.py
+++ sympy/core/tests/test_match.py
@@ -614,3 +614,5 @@ def test_issue_3539():
     a = Wild('a')
     x = Symbol('x')
     assert (x - 2).match(a - x) is None
+    assert (6/x).match(a*x) is None
+    assert (6/x**2).match(a/x) == {a: 6/x}
