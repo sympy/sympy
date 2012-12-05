420aeb4144412696a03750155d25bf66fbc40422
Matthew Rocklin <mrocklin@gmail.com>
2012-12-05 08:15:37 -0600

    fix permutation doctest
    Needed Matrix call to make the MatMul explicit

--- sympy/matrices/expressions/permutation.py
+++ sympy/matrices/expressions/permutation.py
@@ -17,7 +17,7 @@ class PermutationMatrix(MatrixExpr):
     [3, 4, 5]
     [6, 7, 8]
 
-    >>> P*Matrix(3, 3, range(9))
+    >>> Matrix(P*Matrix(3, 3, range(9)))
     [3, 4, 5]
     [6, 7, 8]
     [0, 1, 2]
