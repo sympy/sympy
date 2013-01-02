ab32873ada20106c70ecee6208160aa22c52bc10
Raoul Bourquin <raoulb@bluewin.ch>
2013-01-02 02:37:38 +0100

    Series expansion for "_erfs" helper function at point I*oo
    It does the job:
    
    >>> gruntz((I*erfi(I*x) - I*erfi(I*(x - exp(-exp(x)))))*exp(x**2)*exp(exp(x)), x, oo)
    -2/sqrt(pi)
    
    >>> gruntz((I*erfi(x) - I*erfi(I*(x - exp(-exp(x)))))*exp(x**2)*exp(exp(x)), x, oo)
    oo*I
    
    >>> gruntz((erfi(x) - I*erfi(I*(x - exp(-exp(x)))))*exp(x**2)*exp(exp(x)), x, oo)
    oo
    
    The first one is just example 8.23 from Gruntz' thesis. With a "rewrite(erf)"
    this reduces to "erf" of real argument and limit point oo, hence can be easily
    done with the old code.

--- sympy/functions/special/error_functions.py
+++ sympy/functions/special/error_functions.py
@@ -1725,15 +1725,30 @@ class _erfs(Function):
     nargs = 1
 
     def _eval_aseries(self, n, args0, x, logx):
-        if args0[0] != S.Infinity:
-            return super(_erfs, self)._eval_aseries(n, args0, x, logx)
+        point = args0[0]
 
-        z = self.args[0]
-        l = [ 1/sqrt(S.Pi) * C.factorial(2*k)*(-S(
-            4))**(-k)/C.factorial(k) * (1/z)**(2*k + 1) for k in xrange(0, n) ]
-        o = C.Order(1/z**(2*n + 1), x)
-        # It is very inefficient to first add the order and then do the nseries
-        return (Add(*l))._eval_nseries(x, n, logx) + o
+        # Expansion at oo
+        if point is S.Infinity:
+            z = self.args[0]
+            l = [ 1/sqrt(S.Pi) * C.factorial(2*k)*(-S(
+                4))**(-k)/C.factorial(k) * (1/z)**(2*k + 1) for k in xrange(0, n) ]
+            o = C.Order(1/z**(2*n + 1), x)
+            # It is very inefficient to first add the order and then do the nseries
+            return (Add(*l))._eval_nseries(x, n, logx) + o
+
+        # Expansion at I*oo
+        t = point.extract_multiplicatively(S.ImaginaryUnit)
+        if t is S.Infinity:
+            z = self.args[0]
+            # TODO: is the series really correct?
+            l = [ 1/sqrt(S.Pi) * C.factorial(2*k)*(-S(
+                4))**(-k)/C.factorial(k) * (1/z)**(2*k + 1) for k in xrange(0, n) ]
+            o = C.Order(1/z**(2*n + 1), x)
+            # It is very inefficient to first add the order and then do the nseries
+            return (Add(*l))._eval_nseries(x, n, logx) + o
+
+        # All other points are not handled
+        return super(_erfs, self)._eval_aseries(n, args0, x, logx)
 
     def fdiff(self, argindex=1):
         if argindex == 1:
