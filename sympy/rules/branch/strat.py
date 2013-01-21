3130f005c61b0e5e933e78394fd9f01410eb64c2
Matthew Rocklin <mrocklin@gmail.com>
2013-01-21 08:57:36 -0800

    add yieldify imports

--- sympy/rules/branch/strat.py
+++ sympy/rules/branch/strat.py
@@ -1,5 +1,5 @@
 from strat_pure import (exhaust, multiplex, debug, notempty, condition, chain,
-        onaction, sfilter)
+        onaction, sfilter, yieldify)
 from traverse import top_down
 
 def canon(*rules):
