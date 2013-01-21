19fe9ef901fb3354ff5ee06910393db882bfedfc
Matthew Rocklin <mrocklin@gmail.com>
2013-01-20 16:28:08 -0800

    density -> distribution in simplify

--- sympy/stats/tests/test_simplify.py
+++ sympy/stats/tests/test_simplify.py
@@ -1,6 +1,7 @@
 from sympy.stats.simplify import statsimp
 from sympy.stats import Normal, ChiSquared
-from sympy.stats.crv_types import ChiSquaredDensity, NormalDensity, Normal
+from sympy.stats.crv_types import (ChiSquaredDistribution, NormalDistribution,
+        Normal)
 from sympy.stats.rv import Density, pspace
 from sympy import Symbol, simplify
 
@@ -17,21 +18,21 @@ def rebuild(x):
 def test_chisquared():
     X = Normal('x', 0, 1)
     dens = list(statsimp(Density(X**2)))[0]
-    assert isinstance(pspace(dens).density, ChiSquaredDensity)
+    assert isinstance(pspace(dens).density, ChiSquaredDistribution)
 
 def test_chisquared_two_degrees():
     X = Normal('X', 0, 1)
     Y = Normal('Y', 0, 1)
     dens = list(statsimp(Density(X**2 + Y**2)))[0].expr.pspace.density
 
-    assert isinstance(dens, ChiSquaredDensity)
+    assert isinstance(dens, ChiSquaredDistribution)
     assert rebuild(dens.k) == 2
 
 def _unifies(a, b):
     assert tuple(unify, a, b)
 
 def test_unify():
-    assert tuple(unify(NormalDensity(0, 1), NormalDensity(0, 1)))
+    assert tuple(unify(NormalDistribution(0, 1), NormalDistribution(0, 1)))
     assert tuple(unify(Normal(x, 0, 1), Normal(x, 0, 1)))
     assert tuple(unify(Normal(y, 0, 1), Normal(x, 0, 1), wilds=[x]))
     assert list(unify(Normal(x, 0, 1), Normal(y, 0, 1),
