9435bf8d545c1def1abe08153e2736a093c7a18e
Matthew Rocklin <mrocklin@gmail.com>
2013-01-21 10:55:35 -0800

    Testing statsimp
    chisquared example works
    testing expression simplification separately from densities
    changed strategies in statsimp

--- sympy/stats/simplify.py
+++ sympy/stats/simplify.py
@@ -2,12 +2,14 @@ from sympy.stats import Normal, ChiSquared
 from sympy.stats.crv import ContinuousDistribution
 from sympy.stats.rv import Density, RandomSymbol
 from sympy.unify import rewriterule
-from sympy.rules.branch import multiplex, exhaust, chain, yieldify
-from sympy.rules import rebuild
-from sympy import Symbol
+from sympy.rules.branch import multiplex, exhaust, chain, yieldify, debug
+from sympy.rules.branch.traverse import top_down
+from sympy.rules import rebuild, flatten
+from sympy import Symbol, Dummy
 
 x, y, z, n, m = map(Symbol, 'xyznm')
 
+z = Dummy('z')
 
 # Equivalences of random expressions under density. E.g.
 # Density(Normal(x, 0, 1)) == Density(StandardNormal(y))
@@ -15,7 +17,9 @@ x, y, z, n, m = map(Symbol, 'xyznm')
 expression_equivalences = [
     (Normal(x, 0, 1)**2, ChiSquared(x, 1), [x]),
     (Normal(x, 0, 1)**2 + z, ChiSquared(x, 1) + z, [x, z]),
-    (ChiSquared(x, m) + ChiSquared(y, n), ChiSquared(x, n+m), [x, y, n, m])
+    (ChiSquared(x, m) + ChiSquared(y, n), ChiSquared(x, n+m), [x, y, n, m]),
+    (ChiSquared(x, m) + ChiSquared(y, n) + z, ChiSquared(x, n+m) + z,
+        [x, y, n, m, z])
 ]
 
 def unpack_Density(d):
@@ -29,4 +33,4 @@ expression_rrs = [rewriterule(Density(src), Density(tgt), wilds)
 
 rrs = expression_rrs + [unpack_Density]
 
-statsimp = chain(exhaust(multiplex(*rrs)), yieldify(rebuild))
+statsimp = exhaust(multiplex(yieldify(rebuild), *rrs))
--- sympy/stats/tests/test_simplify.py
+++ sympy/stats/tests/test_simplify.py
@@ -1,36 +1,43 @@
 from sympy.stats.simplify import (statsimp, rrs, expression_rrs,
-        unpack_Density, rebuild, yieldify)
+        unpack_Density, expression_equivalences)
 from sympy.stats import Normal, ChiSquared
 from sympy.stats.crv_types import (ChiSquaredDistribution, NormalDistribution,
         Normal)
 from sympy.stats.rv import Density, pspace
 from sympy import Symbol, simplify
-from sympy.rules.branch import exhaust, multiplex, chain
+from sympy.rules.branch import exhaust, multiplex, chain, yieldify
+from sympy.rules import rebuild
 
-from sympy.unify import unify
+from sympy.unify import unify, rewriterule
 
-exprstatsimp = chain(exhaust(multiplex(*expression_rrs)), yieldify(rebuild))
+expr_rrs = map(rewriterule, *zip(*expression_equivalences))
+exprsimp = exhaust(multiplex(yieldify(rebuild), *expr_rrs))
 
 x, y = map(Symbol, 'xy')
 
 def test_chisquared():
     X = Normal('x', 0, 1)
-    dens = list(exprstatsimp(Density(X**2)))[0]
-    assert isinstance(pspace(dens).density, ChiSquaredDistribution)
+    assert next(exprsimp(X**2)).pspace.density == ChiSquaredDistribution(1)
+    assert next(statsimp(Density(X**2))) == ChiSquaredDistribution(1)
 
 def test_chisquared_two_degrees():
     X = Normal('X', 0, 1)
     Y = Normal('Y', 0, 1)
-    dens = list(exprstatsimp(Density(X**2 + Y**2)))[0].expr.pspace.density
-    assert dens == ChiSquaredDistribution(2)
+    assert next(exprsimp(X**2 + Y**2)).pspace.density == ChiSquaredDistribution(2)
+    assert next(statsimp(Density(X**2 + Y**2))) == ChiSquaredDistribution(2)
+
+def test_chisquared_three_degrees():
+    X = Normal('X', 0, 1)
+    Y = Normal('Y', 0, 1)
+    Z = Normal('Z', 0, 1)
+    assert next(exprsimp(X**2 + Y**2 + Z**2)).pspace.density ==\
+            ChiSquaredDistribution(3)
+    assert next(statsimp(Density(X**2 + Y**2 + Z**2))) == \
+            ChiSquaredDistribution(3)
 
 def test_unpack_Density():
     assert next(unpack_Density(Density(Normal('x', 0, 1)))) == \
             NormalDistribution(0, 1)
-
-def test_statsimp():
-    expr = Normal('X', 0, 1)**2 + Normal('Y', 0, 1)**2
-    assert set(statsimp(Density(expr))) == set([ChiSquaredDistribution(2)])
 
 def test_unify():
     assert tuple(unify(NormalDistribution(0, 1), NormalDistribution(0, 1)))
