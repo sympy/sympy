fa9c0fbe1abcfd960a781330d416d6dc4e2eabc1
ashuven63 <ashuven63@gmail.com>
2013-01-23 20:31:37 +0530

    Reviewed products.py

--- sympy/concrete/products.py
+++ sympy/concrete/products.py
@@ -115,9 +115,7 @@ class Product(Expr):
             i, a, b = limit
             dif = b - a
             if dif.is_Integer and dif < 0:
-                #a, b = b, a
-		return 1
-
+                return 1                
             g = self._eval_product(f, (i, a, b))
             if g is None:
                 return Product(powsimp(f), *self.limits[index:])
