a56a2f21af7a86f34b58cdf6d301df5538b95983
Chris Smith <smichr@gmail.com>
2013-01-27 05:45:43 +0545

    fix kernS for powers

--- sympy/core/sympify.py
+++ sympy/core/sympify.py
@@ -303,9 +303,36 @@ def _sympify(a):
 def kernS(s):
     """Use a hack to try keep autosimplification from joining Integer or
     minus sign into an Add of a Mul; this modification doesn't
-    prevent the 2-arg Mul from becoming an Add, however. If use of the
-    hack fails, the un-hacked string will be passed to sympify...and you
-    get what you get.
+    prevent the 2-arg Mul from becoming an Add, however.
+
+    Examples
+    ========
+
+    >>> from sympy.core.sympify import kernS
+    >>> from sympy.abc import x, y, z
+
+    The 2-arg Mul allows a leading Integer to be distributed and kernS does
+    not prevent that:
+
+    >>> 2*(x + y) == kernS('2*(x + y)') == 2*x + 2*y
+    True
+
+    But a Mul with more than 2 args need not have the leading Integer
+    distributed and the kernS version of S will keep that distribution
+    from occuring:
+
+    >>> 2*(x  + y)*z  # the first two arguments undergo the distribution
+    z*(2*x + 2*y)
+    >>> 2*z*(x + y)  # in this form, the Add is not multiplied by an Integer
+    2*z*(x + y)
+    >>> kernS('2*(x + y)*z')  # kernS will stop the distribution in this case
+    2*z*(x + y)
+
+    >>> kernS('E**-(x)')
+    exp(-x)
+
+    If use of the hack fails, the un-hacked string will be passed to sympify...
+    and you get what you get.
 
     XXX This hack should not be necessary once issue 1497 has been resolved.
     """
@@ -314,12 +341,44 @@ def kernS(s):
 
     hit = False
     if '(' in s:
+        if s.count('(') != s.count(")"):
+            raise SympifyError('unmatch laft parentheses')
+
         kern = '_kern'
         while kern in s:
             kern += "_"
+        lparen = 'kern_2'
+        while lparen in s:
+            lparen += "_"
         olds = s
-        s = re.sub(r'(\d *\*|-) *\(', r'\1%s*(' % kern, s)
+        s = re.sub(r'(\d *\*|-) *\(', r'\1(%s*%s' % (kern, lparen), s)
+
         hit = kern in s
+        if hit:
+            # close parentheses after kerns; if this fails there is
+            # either an error in this parsing or in the original string
+            i = 0
+            close = []
+            while True:
+                i = s.find(kern,  i)
+                if i == -1:
+                    break
+                count = 1  # for the one before the kern
+                for j in range(i + 1, len(s)):
+                    c = s[j]
+                    if c == "(":
+                        count += 1
+                    elif c == ")":
+                        count -= 1
+                    else:
+                        continue
+                    if count == 0:
+                        close.append(j)
+                        i += len(kern)  # continue from the last kern
+                        break
+            for i in reversed(close):
+                s = ')'.join([s[:i], s[i:]])
+            s = s.replace(lparen, "(")
 
     for i in range(2):
         try:
