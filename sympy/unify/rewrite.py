2e4995c45941624543b94277f977409e3388bae7
Matthew Rocklin <mrocklin@gmail.com>
2013-02-01 17:40:50 -0800

    docstring for rewriterule

--- sympy/unify/rewrite.py
+++ sympy/unify/rewrite.py
@@ -5,12 +5,31 @@ from sympy.unify.usympy import rebuild
 from sympy.rules.tools import subs
 from sympy import Expr
 
-def rewriterule(p1, p2, variables=(), condition=None):
+def rewriterule(source, target, variables=(), condition=None):
+    """ Rewrite rule
+
+    Transform expressions that match source into expressions that match target
+    treating all `variables` as wilds.
+
+    >>> from sympy.abc import x, y, z
+    >>> from sympy.unify.rewrite import rewriterule
+    >>> rl = rewriterule(x + y, x**y, [x, y])
+    >>> list(rl(z + 3))
+    [3**z, z**3]
+
+    Use ``condition`` to specify additional requirements.  Inputs are taken in
+    the same order as is found in variables.
+
+    >>> rl = rewriterule(x + y, x**y, [x, y], lambda x, y: x.is_integer)
+    >>> list(rl(z + 3))
+    [3**z]
+    """
+
     def rewrite_rl(expr):
-        for match in unify(p1, expr, {}, variables=variables):
+        for match in unify(source, expr, {}, variables=variables):
             if (condition is None or
                 condition(*[match.get(var, var) for var in variables]) == True):
-                expr2 = subs(match)(p2)
+                expr2 = subs(match)(target)
                 if isinstance(expr2, Expr):
                     expr2 = rebuild(expr2)
                 yield expr2
