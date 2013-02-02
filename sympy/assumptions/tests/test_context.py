e2732bd44103c96633266405b8df4d832080fb18
Matthew Rocklin <mrocklin@gmail.com>
2013-02-02 14:19:29 -0800

    test nested context managers

--- sympy/assumptions/tests/test_context.py
+++ sympy/assumptions/tests/test_context.py
@@ -1,8 +1,23 @@
 from sympy.assumptions import ask, Q
 from sympy.assumptions.assume import assume
-from sympy.abc import x
+from sympy.abc import x, y
 
-def test_context_manager():
+def test_assume():
     with assume(Q.integer(x)):
         assert ask(Q.integer(x))
     assert not ask(Q.integer(x))
+
+def test_assume_nested():
+    with assume(Q.integer(x)):
+        assert ask(Q.integer(x))
+        assert not ask(Q.integer(y))
+        assert not ask(Q.integer(x + y))
+        with assume(Q.integer(y)):
+            assert ask(Q.integer(x))
+            assert ask(Q.integer(y))
+            assert ask(Q.integer(x + y))
+        assert ask(Q.integer(x))
+        assert not ask(Q.integer(y))
+        assert not ask(Q.integer(x + y))
+    assert not ask(Q.integer(x))
+    assert not ask(Q.integer(y))
