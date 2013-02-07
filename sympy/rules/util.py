1f6d9179563920e29acd7e5a16eb208b7f62330e
Matthew Rocklin <mrocklin@gmail.com>
2013-02-06 23:02:19 -0800

    RuleDB

--- sympy/rules/util.py
+++ sympy/rules/util.py
@@ -14,3 +14,16 @@ def children(x):
 
 def count(tup):
     return dict((k, len(v)) for k,v in groupby((lambda x: x), tup).items())
+
+class RuleDB(object):
+    def __init__(self):
+        self.coll = list()
+
+    def insert(self, tup, val):
+        self.coll.append((count(tup), val))
+
+    def query(self, indata):
+        inkey = count(indata)
+        for collkey, collval in self.coll:
+            if all(inkey.get(k, 0) >= v for k, v in collkey.items()):
+                yield collval
