1fecf60dd2838343134844ffb4fddbafc0e8d88c
Matthew Rocklin <mrocklin@gmail.com>
2013-02-13 10:54:56 -0800

    Block docstring

--- sympy/matrices/expressions/block.py
+++ sympy/matrices/expressions/block.py
@@ -1,6 +1,24 @@
 from sympy.matrices.expressions.matexpr  import MatrixExpr
 
 class Block(MatrixExpr):
+    """ A block of a Matrix Expression
+
+    Examples
+
+    >>> from sympy import Block, ImmutableMatrix
+    >>> M = ImmutableMatrix(4, 4, range(16))
+    >>> print M
+    [ 0,  1,  2,  3]
+    [ 4,  5,  6,  7]
+    [ 8,  9, 10, 11]
+    [12, 13, 14, 15]
+
+    >>> B = Block(M, (0, 2), (2, 4))
+    >>> print ImmutableMatrix(B)
+    [2, 3]
+    [6, 7]
+    """
+
     parent = property(lambda self: self.args[0])
     rowbounds = property(lambda self: self.args[1])
     colbounds = property(lambda self: self.args[2])
