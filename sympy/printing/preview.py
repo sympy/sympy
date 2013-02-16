616e5070eb553dfeee35109e7af9fed6a392cb45
Thomas Hisch <t.hisch@gmail.com>
2013-02-16 22:18:29 +0100

    unskip strinio doctest

--- sympy/printing/preview.py
+++ sympy/printing/preview.py
@@ -67,7 +67,8 @@ def preview(expr, output='png', viewer=None, euler=True, packages=(),
     There is also support for writing to a StringIO like object, which needs
     to be passed to the 'outputbuffer' argument.
 
-    >>> obj = StringIO() # doctest: +SKIP
+    >>> import StringIO
+    >>> obj = StringIO()
     >>> preview(x + y, output='png', viewer='StringIO',
     ...         outputbuffer=obj) # doctest: +SKIP
 
