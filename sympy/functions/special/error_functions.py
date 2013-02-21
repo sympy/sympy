0050ced4f55b965e14c3a72cf6bce5d6f2a8d822
Ramana Venkata <idlike2dream@gmail.com>
2013-02-22 04:20:30 +0530

    Error fix

--- sympy/functions/special/error_functions.py
+++ sympy/functions/special/error_functions.py
@@ -754,8 +754,7 @@ class erfinv(Function):
             return z.args[0]
 
         nz = z.extract_multiplicatively(-1)
-        if nz is not None and ((nz.func is erf) and x.is_real):
-            x = nz.args[0]
+        if nz is not None and ((nz.func is erf) and (nz.args[0]).is_real):
             return -nz.args[0]
 
     def _eval_rewrite_as_erfcinv(self, z):
