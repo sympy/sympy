2398ebe7a29e133e287d6716bc6733eabc768f64
Matthew Rocklin <mrocklin@gmail.com>
2013-02-21 11:03:27 -0800

    RandomSymbol.is_positive

--- sympy/stats/rv.py
+++ sympy/stats/rv.py
@@ -233,6 +233,9 @@ class RandomSymbol(Expr):
     symbol = property(lambda self: self.args[1])
     name   = property(lambda self: self.symbol.name)
 
+    is_positive = property(lambda self: self.symbol.is_positive)
+    is_integer = property(lambda self: self.symbol.is_integer)
+
     @property
     def is_commutative(self):
         return self.symbol.is_commutative
