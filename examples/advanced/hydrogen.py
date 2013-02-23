3468f798d37165f40ed3c8769471a8f6a1d51d38
Ananya <ananyaha93@gmail.com>
2013-02-23 20:22:42 +0530

    Issue 3658 Cleaner Code

--- examples/advanced/hydrogen.py
+++ examples/advanced/hydrogen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 This example shows how to work with the Hydrogen radial wavefunctions.
 """
@@ -16,17 +18,11 @@ def main():
 
     print "Normalization:"
     i = Integral(R_nl(1, 0, 1, r)**2 * r**2, (r, 0, oo))
-    print pretty(i),
-    print " = "+str( i.doit())
+    print pretty(i)," = ",i.doit
     i = Integral(R_nl(2, 0, 1, r)**2 * r**2, (r, 0, oo))
-    print pretty(i),
-    print " = "+str( i.doit())
+    print pretty(i)," = ",i.doit()
     i = Integral(R_nl(2, 1, 1, r)**2 * r**2, (r, 0, oo))
-    print pretty(i),
-    print " = "+str( i.doit())
+    print pretty(i)," = ",i.doit()
 
 if __name__ == '__main__':
     main()
-
-
-
