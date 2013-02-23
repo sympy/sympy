e8451aa4d80b6b493d974ee43c65a5adb80c787b
Ananya <ananyaha93@gmail.com>
2013-02-23 12:26:14 +0530

    Issue 3658 : Removed call to Eq as it was unnecessary

--- examples/advanced/hydrogen.py
+++ examples/advanced/hydrogen.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 This example shows how to work with the Hydrogen radial wavefunctions.
 """
@@ -18,13 +16,11 @@ def main():
 
     print "Normalization:"
     i = Integral(R_nl(1, 0, 1, r)**2 * r**2, (r, 0, oo))
-    pprint(Eq(i, i.doit()))
-
+    pprint(str(-i.doit())+" + "+str(i)+" =0")
     i = Integral(R_nl(2, 0, 1, r)**2 * r**2, (r, 0, oo))
-    pprint(Eq(i, i.doit()))
-
+    pprint(str(-i.doit())+" + "+str(i)+" =0")
     i = Integral(R_nl(2, 1, 1, r)**2 * r**2, (r, 0, oo))
-    pprint(Eq(i, i.doit()))
+    pprint(str(-i.doit())+" + "+str(i)+" =0")
 
 if __name__ == '__main__':
     main()
