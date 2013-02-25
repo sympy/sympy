53ae824991006c130da2b1e27576e59dd56fb4b9
Matthew Hoff <mhoff@calpoly.edu>
2013-02-24 22:37:47 -0800

    added get_final_state

--- sympy/physics/quantum/mappinggate.py
+++ sympy/physics/quantum/mappinggate.py
@@ -1,10 +1,13 @@
 """Mapping Quantum Gates"""
 
 from sympy import Integer
+from sympy.core.containers import Dict
+from sympy.core.basic import Basic
 from sympy.physics.quantum import Dagger
 from sympy.physics.quantum.gate import Gate
 from sympy.physics.quantum.qubit import Qubit, IntQubit
-from sympy.physics.quantum.matrixutils import matrix_zeros
+from sympy.physics.quantum.matrixutils import matrix_eye
+from sympy.physics.quantum.qexpr import split_qexpr_parts
 
 #-----------------------------------------------------------------------------------
 
@@ -22,44 +25,53 @@ class MappingGate(Gate):
 
     @classmethod
     def _eval_args(cls, args):
-        if len(args) % 2 == 1:
-            raise ValueError('Need even number of arguments') 
-        values = matrix_zeros(1, len(args))
-        for i in range(len(args)):
-            values[i] = Qubit(str(args[i]))
-        args = values
-        for i in range(len(args) - 1):
-            if args[i].nqubits != args[i + 1].nqubits:
+        temp = {}
+        for arg in args:
+            i = Qubit(arg[0])
+            f = Qubit(arg[1])
+            #parts = split_qexpr_parts(f)
+            #f = Qubit(parts[1])
+            #scalar = parts[0]
+            if i.nqubits != f.nqubits:
                 raise ValueError('Number of qubits for each state do not match')
-        return args
+            temp[i] = f
+            #temp[Dagger(i)] = conjugate(scalar)*Dagger(f)
+        new_args = Dict(temp)
+        print new_args
+        return new_args
 
     @classmethod
     def _eval_hilbert_space(cls, args):
         pass
 
     @property
-    def size(self, *args):
+    def size(self):
         """Total number of initial and final state pairs"""
         return int(len(self.args)/2)
 
     @property
-    def mat(self, *args):
+    def nqubits(self):
         """Gives the dimension of the matrix representation"""
-        return 2**self.args[0].nqubits
+        return self.args[0].nqubits
 
-    def _apply_operator_Qubit(self, qubits, *args):
-        for i in range(self.size):
-            if qubits == self.args[2*i]:
-                return self.args[2*i + 1]
-            elif qubits.nqubits != self.args[2*i].nqubits:
-                raise ValueError('Initial state not specified')
+    def get_final_state(self, qubit):
+        i = Qubit(qubit)
+        f = self.args.get(i, None)
+        if f is None:
+            return i
+        else:
+            return f
+
+    def _apply_operator_Qubit(self, qubit):
+        return self.get_final_state(qubit)
 
     def _eval_rewrite_as_op(self, *args):
-        product = matrix_zeros(1, len(self.args))
-        for i in range(self.size):
-            value = self.args[2*i + 1]*Dagger(self.args[2*i])
-            product[i] = value
-        return sum(product)
+        terms = []
+        for i in range(2**self.nqubits):
+            init = Qubit(IntQubit(i))
+            fin = self.get_final_state(init)
+            term[i] = fin*Dagger(init)
+        return Add(*terms)
 
     def _represent_default_basis(self, **options):
         return self._represent_ZGate(None, **options)
@@ -67,20 +79,12 @@ class MappingGate(Gate):
     def _represent_ZGate(self, basis, *args, **options):
         format = options.get('format','sympy')
         spmatrix = options.get('spmatrix', 'csr')
-        matrix = matrix_zeros(self.mat, self.mat, **options)
-        for i in range(self.size):
-            for j in range(self.size):
-                for k in range(self.size):
-                    if IntQubit(j).as_int() == IntQubit(self.args[2*i + 1]).as_int():
-                        if IntQubit(k).as_int() == IntQubit(self.args[2*i]).as_int():
-                            if format == 'scipy.sparse':
-                                matrix[j, k] = matrix[j, k] + float(Integer(1))
-                            else:
-                                matrix[j, k] = matrix[j, k] + Integer(1)
-                        else:
-                            matrix[j, k] = matrix[j, k]
-                    else:
-                        matrix[j, k] = matrix[j, k]
+        matrix = matrix_eye(2**self.nqubits, **options)
+        for i, f in self.args.items():
+            col = IntQubit(i).as_int()
+            row = IntQubit(f).as_int()
+            matrix[col, col] = Integer(0)
+            matrix[row, col] = f
         if format == 'scipy.sparse':
             matrix = matrix.tocsr()
         return matrix
