92eba8168676c617841fd7863aa6c06b426400df
Mateusz Paprocki <mattpap@gmail.com>
2013-02-28 17:35:39 +0100

    doc: Added support for ipython_console_highlighting
    To update, copy this file:
    
    https://raw.github.com/ipython/ipython/master/docs/sphinxext/ipython_console_highlighting.py
    
    to doc/ext.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -22,7 +22,7 @@ sys.path = ['../sympy', 'ext'] + sys.path
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax',
-              'numpydoc', 'sympylive', 'tikz']
+              'numpydoc', 'sympylive', 'tikz', 'ipython_console_highlighting']
 
 # Use this to use pngmath instead
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.pngmath', ]
