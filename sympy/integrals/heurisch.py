311faadca5c310a36651a0b3102af7356f7a7a56
chetna <cheta.gup@gmail.com>
2013-03-18 05:19:01 +0530

    Update heurisch.py
    Reduced the number of permutations of mapping by not generating cases where mapping[-1] != x

--- sympy/integrals/heurisch.py
+++ sympy/integrals/heurisch.py
@@ -257,16 +257,20 @@ def heurisch(f, x, rewrite=False, hints=None, mappings=None, retries=3, degree_o
         def _sort_key(arg):
             return default_sort_key(arg[0].as_independent(x)[1])
         mapping = sorted(mapping.items(), key=_sort_key, reverse=True)
+    #optimizing the number of permutations 
+	for i in mapping:
+	    if(i[0]==x):
+	       adding_tuple=(x,i[1])
+	       mapping.remove(i)
         mappings = permutations(mapping)
 
     def _substitute(expr):
         return expr.subs(mapping)
 
     for mapping in mappings:
-        # TODO: optimize this by not generating permutations where mapping[-1] != x.
-        if mapping[-1][0] != x:
-            continue
-
+        # solved !!-> TODO: optimize this by not generating permutations where mapping[-1] != x.
+	mapping = list(mapping)
+	mapping.append(adding_tuple)
         mapping = list(mapping)
 
         diffs = [ _substitute(cancel(g.diff(x))) for g in terms ]
