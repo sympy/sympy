77da6b7c93920e9854fad2f63912b4b96449dbf9
Sergey B Kirpichev <skirpichev@gmail.com>
2013-03-18 11:30:57 +0400

    some formatting fixes in recurr.py

--- sympy/solvers/recurr.py
+++ sympy/solvers/recurr.py
@@ -11,22 +11,22 @@
 
    Given a recurrence relation:
 
-      a_{k}(n) y(n+k) + a_{k-1}(n) y(n+k-1) + ... + a_{0}(n) y(n) = f(n)
+   .. math:: a_{k}(n) y(n+k) + a_{k-1}(n) y(n+k-1) + ... + a_{0}(n) y(n) = f(n)
 
-   where k > 0 and a_{i}(n) are polynomials in n. To use rsolve_X we need
-   to put all coefficients in to a list L of k+1 elements the following
+   where `k > 0` and `a_{i}(n)` are polynomials in `n`. To use rsolve_X we need
+   to put all coefficients in to a list L of `k+1` elements the following
    way:
 
       L = [ a_{0}(n), ..., a_{k-1}(n), a_{k}(n) ]
 
-   where L[i], for i=0..k, maps to a_{i}(n) y(n+i) (y(n+i) is implicit).
+   where ``L[i]``, for `i=0, \dots, k`, maps to `a_{i}(n) y(n+i)` (`y(n+i)` is implicit).
 
    For example if we would like to compute m-th Bernoulli polynomial up to
    a constant (example was taken from rsolve_poly docstring), then we would
-   use b(n+1) - b(n) == m*n**(m-1) recurrence, which has solution b(n) =
-   B_m + C.
+   use `b(n+1) - b(n) == m n^{m-1}` recurrence, which has solution `b(n) =
+   B_m + C`.
 
-   Then L = [-1, 1] and f(n) = m*n**(m-1) and finally for m=4:
+   Then L = [-1, 1] and `f(n) = m n^(m-1)` and finally for m=4:
 
     >>> from sympy import Symbol, bernoulli, rsolve_poly
     >>> n = Symbol('n', integer=True)
@@ -64,15 +64,16 @@ from sympy.utilities.iterables import numbered_symbols
 
 
 def rsolve_poly(coeffs, f, n, **hints):
-    """Given linear recurrence operator L of order 'k' with polynomial
-       coefficients and inhomogeneous equation Ly = f, where 'f' is a
-       polynomial, we seek for all polynomial solutions over field K
-       of characteristic zero.
+    """Given linear recurrence operator `\operatorname{L}` of order
+       `k` with polynomial coefficients and inhomogeneous equation
+       `\operatorname{L} y = f`, where 'f' is a polynomial, we seek
+       for all polynomial solutions over field K of characteristic zero.
 
        The algorithm performs two basic steps:
 
            (1) Compute degree N of the general polynomial solution.
-           (2) Find all polynomials of degree N or less of Ly = f.
+           (2) Find all polynomials of degree N or less
+               of `\operatorname{L} y = f`.
 
        There are two methods for computing the polynomial solutions.
        If the degree bound is relatively small, i.e. it's smaller than
@@ -82,7 +83,7 @@ def rsolve_poly(coeffs, f, n, **hints):
 
        In the other case, the algorithm performs transformation of the
        initial equation to an equivalent one, for which the system of
-       algebraic equations has only 'r' indeterminates. This method is
+       algebraic equations has only `r` indeterminates. This method is
        quite sophisticated (in comparison with the naive one) and was
        invented together by Abramov, Bronstein and Petkovsek.
 
@@ -90,8 +91,8 @@ def rsolve_poly(coeffs, f, n, **hints):
        the case of linear q-difference and differential equations.
 
        Lets say that we would like to compute m-th Bernoulli polynomial
-       up to a constant. For this we can use b(n+1) - b(n) == m*n**(m-1)
-       recurrence, which has solution b(n) = B_m + C. For example:
+       up to a constant. For this we can use `b(n+1) - b(n) == m n^{m-1}`
+       recurrence, which has solution `b(n) = B_m + C`. For example:
 
        >>> from sympy import Symbol, rsolve_poly
        >>> n = Symbol('n', integer=True)
@@ -339,10 +340,10 @@ def rsolve_poly(coeffs, f, n, **hints):
 
 
 def rsolve_ratio(coeffs, f, n, **hints):
-    """Given linear recurrence operator L of order 'k' with polynomial
-       coefficients and inhomogeneous equation Ly = f, where 'f' is a
-       polynomial, we seek for all rational solutions over field K of
-       characteristic zero.
+    """Given linear recurrence operator `\operatorname{L}` of order `k`
+       with polynomial coefficients and inhomogeneous equation
+       `\operatorname{L} y = f`, where `f` is a polynomial, we seek
+       for all rational solutions over field K of characteristic zero.
 
        This procedure accepts only polynomials, however if you are
        interested in solving recurrence with rational coefficients
@@ -351,11 +352,12 @@ def rsolve_ratio(coeffs, f, n, **hints):
 
        The algorithm performs two basic steps:
 
-           (1) Compute polynomial v(n) which can be used as universal
-               denominator of any rational solution of equation Ly = f.
+           (1) Compute polynomial `v(n)` which can be used as universal
+               denominator of any rational solution of equation
+               `\operatorname{L} y = f`.
 
            (2) Construct new linear difference equation by substitution
-               y(n) = u(n)/v(n) and solve it for u(n) finding all its
+               `y(n) = u(n)/v(n)` and solve it for `u(n)` finding all its
                polynomial solutions. Return None if none were found.
 
        Algorithm implemented here is a revised version of the original
@@ -443,9 +445,10 @@ def rsolve_ratio(coeffs, f, n, **hints):
 
 
 def rsolve_hyper(coeffs, f, n, **hints):
-    """Given linear recurrence operator L of order 'k' with polynomial
-       coefficients and inhomogeneous equation Ly = f we seek for all
-       hypergeometric solutions over field K of characteristic zero.
+    """Given linear recurrence operator `\operatorname{L}` of order `k`
+       with polynomial coefficients and inhomogeneous equation
+       `\operatorname{L} y = f` we seek for all hypergeometric solutions
+       over field `K` of characteristic zero.
 
        The inhomogeneous part can be either hypergeometric or a sum
        of a fixed number of pairwise dissimilar hypergeometric terms.
@@ -453,16 +456,16 @@ def rsolve_hyper(coeffs, f, n, **hints):
        The algorithm performs three basic steps:
 
            (1) Group together similar hypergeometric terms in the
-               inhomogeneous part of Ly = f, and find particular
-               solution using Abramov's algorithm.
+               inhomogeneous part of `\operatorname{L} y = f`, and find
+               particular solution using Abramov's algorithm.
 
-           (2) Compute generating set of L and find basis in it,
-               so that all solutions are linearly independent.
+           (2) Compute generating set of `\operatorname{L}` and find basis
+               in it, so that all solutions are linearly independent.
 
            (3) Form final solution with the number of arbitrary
-               constants equal to dimension of basis of L.
+               constants equal to dimension of basis of `\operatorname{L}`.
 
-       Term a(n) is hypergeometric if it is annihilated by first order
+       Term `a(n)` is hypergeometric if it is annihilated by first order
        linear difference equations with polynomial coefficients or, in
        simpler words, if consecutive term ratio is a rational function.
 
@@ -639,9 +642,9 @@ def rsolve(f, y, init=None):
 
     Given k-th order linear recurrence Ly = f, or equivalently:
 
-     a_{k}(n) y(n+k) + a_{k-1}(n) y(n+k-1) + ... + a_{0}(n) y(n) = f
+    .. math:: a_{k}(n) y(n+k) + a_{k-1}(n) y(n+k-1) + ... + a_{0}(n) y(n) = f(n)
 
-    where a_{i}(n), for i=0..k, are polynomials or rational functions
+    where `a_{i}(n)`, for i=0..k, are polynomials or rational functions
     in n, and f is a hypergeometric function or a sum of a fixed number
     of pairwise dissimilar hypergeometric terms in n, finds all solutions
     or returns None, if none were found.
@@ -655,11 +658,11 @@ def rsolve(f, y, init=None):
 
       L = [ v_0, v_1, ..., v_m ]
 
-    where L[i] = v_i, for i=0..m, maps to y(n_i).
+    where ``L[i] = v_i``, for i=0..m, maps to `y(n_i)`.
 
     As an example lets consider the following recurrence:
 
-     (n - 1) y(n + 2) - (n**2 + 3 n - 2) y(n + 1) + 2 n (n + 1) y(n) == 0
+    .. math:: (n - 1) y(n + 2) - (n^2 + 3 n - 2) y(n + 1) + 2 n (n + 1) y(n) = 0
 
     >>> from sympy import Function, rsolve
     >>> from sympy.abc import n
