602f65c283a0e939abd3903bd06fdc3c32e5af04
Matthew Rocklin <mrocklin@gmail.com>
2013-03-26 14:35:50 -0500

    g(y) -> g in compute_density
    g was already in terms of y

--- sympy/stats/crv.py
+++ sympy/stats/crv.py
@@ -387,7 +387,7 @@ class SingleContinuousPSpace(ContinuousPSpace, SinglePSpace):
         if not gs:
             raise ValueError("Can not solve %s for %s"%(expr, self.value))
         fx = self.compute_density(self.value)
-        fy = sum(fx(g(y)) * abs(g(y).diff(y)) for g in gs)
+        fy = sum(fx(g) * abs(g.diff(y)) for g in gs)
         return Lambda(y, fy)
 
 
