d0e323a03b25389f1a8044f345e12ac5e0241f9e
Alan Bromborsky <abrombo@verizon.net>
2013-03-30 18:03:37 -0400

    Documented blacklists.

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -542,7 +542,23 @@ def _doctest(*paths, **kwargs):
         "doc/src/modules/plotting.rst",  # generates live plots
         "sympy/statistics",                # prints a deprecation
         "doc/src/modules/statistics.rst",  # warning (the module is deprecated)
+<<<<<<< HEAD
         "sympy/utilities/compilef.py"  # needs tcc
+=======
+        "sympy/utilities/compilef.py",  # needs tcc
+        "sympy/utilities/autowrap.py",  # needs installed compiler
+        "sympy/galgebra/GA.py",  # needs numpy
+        "sympy/galgebra/latex_ex.py",  # needs numpy
+        "sympy/conftest.py",  # needs py.test
+        "sympy/utilities/benchmarking.py",  # needs py.test
+        "examples/advanced/autowrap_integrators.py",  # needs numpy
+        "examples/advanced/autowrap_ufuncify.py",  # needs numpy
+        "examples/intermediate/mplot2d.py",
+        # needs numpy and matplotlib
+        "examples/intermediate/mplot3d.py",
+        # needs numpy and matplotlib
+        "examples/intermediate/sample.py",  # needs numpy
+>>>>>>> Documented blacklists.
     ])
 
     if import_module('numpy') is None:
