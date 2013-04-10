8684dc414b5121532037f18e5aeba7d49a363b7c
Matthew Rocklin <mrocklin@gmail.com>
2013-04-09 22:16:30 -0500

    pow_of_abs logpy test

--- sympy/logpy/tests/test_core.py
+++ sympy/logpy/tests/test_core.py
@@ -3,26 +3,31 @@ from logpy import Relation, facts
 from sympy import Basic, Q, Dummy, Symbol, Abs
 from functools import partial
 
-x = Dummy('x')
+x, k = map(Dummy, 'xk')
 
 reduces = Relation('reduces')
 
 facts(reduces, *[
         (Basic(5), Basic(3), True),
-        (Abs(x), x, Q.positive(x))
+        (Abs(x), x, Q.positive(x)),
+        (Abs(x)**k, x**k, Q.real(x) & Q.even(k)),
 
     ])
 
-vars = [x]
+vars = [x, k]
 
 refine = partial(refine_one, vars=vars, reduces=reduces)
 
-
+y = Symbol('y')
+z = Symbol('z')
 
 def test_basic():
     assert refine(Basic(5)) == Basic(3)
 
 def test_exprs_simple():
-    y = Symbol('y')
     assert refine(Abs(y)) == Abs(y)
     assert refine(Abs(y), Q.positive(y)) == y
+
+def test_exprs_pow_of_abs():
+    assert refine(Abs(y)**4, Q.real(y)) == y**4
+    assert refine(Abs(y*z)**4, Q.real(y) & Q.real(z)) == (y*z)**4
