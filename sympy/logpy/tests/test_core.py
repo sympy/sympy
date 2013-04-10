1334bd9899ff99a5cc029596dd397c39f5d893a0
Matthew Rocklin <mrocklin@gmail.com>
2013-04-09 22:21:42 -0500

    refine_deep with top_down

--- sympy/logpy/tests/test_core.py
+++ sympy/logpy/tests/test_core.py
@@ -2,6 +2,9 @@ from sympy.logpy import refine_one
 from logpy import Relation, facts
 from sympy import Basic, Q, Dummy, Symbol, Abs
 from functools import partial
+from sympy.assumptions import assuming
+
+from sympy.strategies.traverse import top_down
 
 x, k = map(Dummy, 'xk')
 
@@ -18,6 +21,9 @@ vars = [x, k]
 
 refine = partial(refine_one, vars=vars, reduces=reduces)
 
+refine_deep = top_down(refine)
+
+
 y = Symbol('y')
 z = Symbol('z')
 
@@ -31,3 +37,8 @@ def test_exprs_simple():
 def test_exprs_pow_of_abs():
     assert refine(Abs(y)**4, Q.real(y)) == y**4
     assert refine(Abs(y*z)**4, Q.real(y) & Q.real(z)) == (y*z)**4
+
+def test_pow_of_abs_deep():
+    with assuming(Q.real(y)):
+        assert refine_deep(2*Abs(y)**4) == 2*y**4
+        assert refine_deep(Abs(2*y)**4) == (2*y)**4
