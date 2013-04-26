d12e8e42a700b3cdb3b631fb376090f31b2b58f2
rathmann <rathmann.os@gmail.com>
2013-04-25 20:24:42 -0700

    Added multiset_partitions_baseline() - a (simplified) version of smichr's set-partition-based multiset partitions code for use as a sanity test and comparison.
    Added a test using this baseline version.
    
    Added a larger test case to the subrange test - some of the code paths
    are only exercised with large multisets.
    
    	modified:   sympy/utilities/tests/test_enumerative.py

--- sympy/utilities/tests/test_enumerative.py
+++ sympy/utilities/tests/test_enumerative.py
@@ -1,7 +1,6 @@
 from itertools import izip
 # Todo - use izip_longest when we deprecate 2.5
 
-from sympy.utilities.iterables import  multiset_partitions
 from sympy.utilities.enumerative import (
     factoring_visitor,
     list_visitor,
@@ -10,6 +9,8 @@ from sympy.utilities.enumerative import (
     PartComponent,
     part_key
     )
+from sympy.utilities.iterables import  multiset_partitions, _set_partitions
+from sympy.utilities.pytest import slow
 
 # first some functions only useful as test scaffolding - these provide
 # straightforward, but slow reference implementations against which to
@@ -34,9 +35,75 @@ def part_range_filter(partition_iterator, lb, ub):
         if lpart >= lb and lpart < ub:
             yield state
 
-def multiset_partitions_slow(multiplicities):
-    # todo cheesey version of smichr's code
-    pass
+def multiset_partitions_baseline(multiplicities, components):
+    """Enumerates partitions of a multiset
+
+    Parameters
+    ==========
+
+    multiplicities
+         list of integer multiplicities of the components of the multiset.
+
+    components
+         the components (elements) themselves
+
+    Returns
+    =======
+
+    Set of partitions.  Each partition is tuple of parts, and each
+    part is a tuple of components (with repeats to indicate
+    multiplicity)
+
+    Notes
+    =====
+
+    Multiset partitions can be created as equivalence classes of set
+    partitions, and this function does just that.  This approach is
+    slow and memory intensive compared to the more advanced algorithms
+    available, but the code is simple and easy to understand.  Hence
+    this routine is strictly for testing -- to provide a
+    straightforward baseline against which to regress the production
+    versions.  (This code is a simplified version of an earlier
+    production implementation.)
+    """
+
+    canon = []                  # list of components with repeats
+    for ct, elem in izip(multiplicities, components):
+        canon.extend([elem]*ct)
+
+    # accumulate the multiset partitions in a set to eliminate dups
+    cache = set()
+    n = len(canon)
+    for nc, q in _set_partitions(n):
+        rv = [[] for i in xrange(nc)]
+        for i in xrange(n):
+            rv[q[i]].append(canon[i])
+        canonical = tuple(
+            sorted([tuple(p) for p in rv]))
+        cache.add(canonical)
+    return cache
+
+
+def compare_multiset_w_baseline(multiplicities):
+    """
+    Enumerates the partitions of multiset with AOCP algorithm and
+    baseline implementation, and compare the results.
+
+    """
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    bl_partitions = multiset_partitions_baseline(multiplicities, letters)
+
+    # The partitions returned by the different algorithms may have
+    # their parts in different orders.  Also, they generate partitions
+    # in different orders.  Hence the sorting, and set comparison.
+
+    aocp_partitions = set()
+    for state in multiset_partitions_taocp(multiplicities):
+        p1 = tuple(sorted(
+                [tuple(p) for p in list_visitor(state, letters)]))
+        aocp_partitions.add(p1)
+
+    assert bl_partitions == aocp_partitions
 
 def compare_multiset_states(s1, s2):
     """compare for equality two instances of multiset partition states
@@ -53,21 +120,34 @@ def compare_multiset_states(s1, s2):
             return True
     return False
 
-def test_multiset_versions():
+def test_multiset_partitions_taocp():
+    """Compares the output of multiset_partitions_taocp with a baseline
+    (set partition based) implementation."""
+
+    # Test cases should not be too large, since the baseline
+    # implementation is fairly slow.
+    multiplicities = [2,2]
+    compare_multiset_w_baseline(multiplicities)
+
+    multiplicities = [4,3,1]
+    compare_multiset_w_baseline(multiplicities)
+
+def test_multiset_partitions_versions():
     """Compares Knuth-based versions of multiset_partitions"""
     multiplicities = [5,2,2,1]
     m = MultisetPartitionTraverser()
     for s1, s2 in izip(m.enum_all(multiplicities),
-                               multiset_partitions_taocp(multiplicities)):
+                       multiset_partitions_taocp(multiplicities)):
         assert compare_multiset_states(s1, s2)
 
-def test_subrange():
-    """Compare filter-based and more optimized subrange implementations"""
-    multiplicities = [4,4,2,1] # mississippi
+def subrange_exercise(mult, lb, ub):
+    """Compare filter-based and more optimized subrange implementations
 
+    Helper for tests, called with both small and larger multisets.
+    """
     m = MultisetPartitionTraverser()
-    assert m.count_partitions(multiplicities) == \
-        m.count_partitions_slow(multiplicities)
+    assert m.count_partitions(mult) == \
+        m.count_partitions_slow(mult)
 
     # Note - multiple traversals from the same
     # MultisetPartitionTraverser object cannot execute at the same
@@ -77,12 +157,27 @@ def test_subrange():
     md = MultisetPartitionTraverser()
 
     #  Several paths to compute just the size two partitions
-    a_it = ma.enum_range(multiplicities, 1, 2)
-    b_it = part_range_filter(multiset_partitions_taocp(multiplicities),1,2)
-    c_it = part_range_filter(mc.enum_small(multiplicities, 2), 1, 10)
-    d_it = part_range_filter(md.enum_large(multiplicities, 1), 0, 2)
+    a_it = ma.enum_range(mult, lb, ub)
+    b_it = part_range_filter(multiset_partitions_taocp(mult), lb, ub)
+    c_it = part_range_filter(mc.enum_small(mult, ub), lb, sum(mult))
+    d_it = part_range_filter(md.enum_large(mult, lb), 0, ub)
 
     for sa, sb, sc, sd in izip(a_it, b_it, c_it, d_it):
         assert compare_multiset_states(sa, sb)
         assert compare_multiset_states(sa, sc)
         assert compare_multiset_states(sa, sd)
+
+def test_subrange():
+    # Quick, but doesn't hit some of the corner cases
+    mult = [4,4,2,1] # mississippi
+    lb = 1
+    ub = 2
+    subrange_exercise(mult, lb, ub)
+
+@slow
+def test_subrange_large():
+    # takes a second or so, depending on cpu, Python version, etc.
+    mult = [6,3,2,1]
+    lb = 4
+    ub = 7
+    subrange_exercise(mult, lb, ub)
