57c4f13ea63ea976003da55b37cc0d9689354d0e
Mateusz Paprocki <mattpap@gmail.com>
2013-05-04 01:53:25 +0200

    polys: DRY in sympy/polys/domains/__init__.py

--- sympy/polys/domains/__init__.py
+++ sympy/polys/domains/__init__.py
@@ -4,67 +4,67 @@ __all__ = []
 
 import domain
 __all__.extend(domain.__all__)
-from domain import Domain
+from domain import *
 
 import finitefield
 __all__.extend(finitefield.__all__)
-from finitefield import FiniteField
+from finitefield import *
 
 import integerring
 __all__.extend(integerring.__all__)
-from integerring import IntegerRing
+from integerring import *
 
 import rationalfield
 __all__.extend(rationalfield.__all__)
-from rationalfield import RationalField
+from rationalfield import *
 
 import realfield
 __all__.extend(realfield.__all__)
-from realfield import RealField
+from realfield import *
 
 import complexfield
 __all__.extend(complexfield.__all__)
-from complexfield import ComplexField
+from complexfield import *
 
 import pythonfinitefield
 __all__.extend(pythonfinitefield.__all__)
-from pythonfinitefield import PythonFiniteField
+from pythonfinitefield import *
 
 import gmpyfinitefield
 __all__.extend(gmpyfinitefield.__all__)
-from gmpyfinitefield import GMPYFiniteField
+from gmpyfinitefield import *
 
 import pythonintegerring
 __all__.extend(pythonintegerring.__all__)
-from pythonintegerring import PythonIntegerRing
+from pythonintegerring import *
 
 import gmpyintegerring
 __all__.extend(gmpyintegerring.__all__)
-from gmpyintegerring import GMPYIntegerRing
+from gmpyintegerring import *
 
 import pythonrationalfield
 __all__.extend(pythonrationalfield.__all__)
-from pythonrationalfield import PythonRationalField
+from pythonrationalfield import *
 
 import gmpyrationalfield
 __all__.extend(gmpyrationalfield.__all__)
-from gmpyrationalfield import GMPYRationalField
+from gmpyrationalfield import *
 
 import algebraicfield
 __all__.extend(algebraicfield.__all__)
-from algebraicfield import AlgebraicField
+from algebraicfield import *
 
 import polynomialring
 __all__.extend(polynomialring.__all__)
-from polynomialring import PolynomialRing
+from polynomialring import *
 
 import fractionfield
 __all__.extend(fractionfield.__all__)
-from fractionfield import FractionField
+from fractionfield import *
 
 import expressiondomain
 __all__.extend(expressiondomain.__all__)
-from expressiondomain import ExpressionDomain
+from expressiondomain import *
 
 FF_python = PythonFiniteField
 FF_gmpy = GMPYFiniteField
