6e61390ecec1cddcd6d31777d0474915e7d8fc90
hm <hacman0@gmail.com>
2013-05-12 13:01:13 -0400

    add in alphabets file

---
+++ sympy/core/alphabets.py
@@ -0,0 +1,5 @@
+greeks = frozenset(['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta',
+                    'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu',
+                    'xi', 'omicron', 'pi', 'rho', 'sigma', 'tau', 'upsilon',
+                    'phi', 'chi', 'psi', 'omega'])
+
