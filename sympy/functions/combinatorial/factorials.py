264ec61322b52ee8b68055bbf639f7bdc951990b
Raoul Bourquin <raoulb@bluewin.ch>
2013-05-19 00:08:38 +0200

    Fix depredcated "in" for binomial expansion.

--- sympy/functions/combinatorial/factorials.py
+++ sympy/functions/combinatorial/factorials.py
@@ -560,7 +560,7 @@ class binomial(CombinatorialFunction):
             return binomial(*self.args)
 
         k = self.args[1]
-        if k.is_Add and n in k:
+        if k.is_Add and k.has(n):
             k = n - k
 
         if k.is_Integer:
