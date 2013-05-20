2e5cb74ca3a10f203cfe51016cc8f3491cd423c2
Katja Sophie Hotz <katja.sophie.hotz@student.tuwien.ac.at>
2013-05-21 01:05:20 +0200

    add tests for minpoly over fraction fields

--- sympy/polys/tests/test_numberfields.py
+++ sympy/polys/tests/test_numberfields.py
@@ -19,6 +19,7 @@ from sympy.polys.numberfields import (
 from sympy.polys.polyerrors import (
     IsomorphismFailed,
     NotAlgebraic,
+    GeneratorsError,
 )
 
 from sympy.polys.polyclasses import DMP
@@ -28,7 +29,7 @@ from sympy.solvers import solve
 
 from sympy.utilities.pytest import skip
 
-from sympy.abc import x, y
+from sympy.abc import x, y, z
 
 Q = Rational
 
@@ -673,3 +674,32 @@ def test_isolate():
     assert isolate(-sqrt(2), eps=S(1)/100) == (-S(17)/12, -S(24)/17)
 
     raises(NotImplementedError, lambda: isolate(I))
+
+def test_minpoly_fraction_field():
+    assert minimal_polynomial(1/x, y) == -x*y + 1
+    assert minimal_polynomial(1 / (x + 1), y) == (x + 1)*y - 1
+
+    assert minimal_polynomial(sqrt(x), y) == y**2 - x
+    assert minimal_polynomial(sqrt(x + 1), y) == y**2 - x - 1
+    assert minimal_polynomial(sqrt(x) / x, y) == x*y**2 - 1
+    assert minimal_polynomial(sqrt(2) * sqrt(x), y) == y**2 - 2 * x
+    assert minimal_polynomial(sqrt(2) + sqrt(x), y) == \
+        y**4 + (-2*x - 4)*y**2 + x**2 - 4*x + 4
+
+    assert minimal_polynomial(x**Rational(1,3), y) == y**3 - x
+    assert minimal_polynomial(x**Rational(1,3) + sqrt(x), y) == \
+        y**6 - 3*x*y**4 - 2*x*y**3 + 3*x**2*y**2 - 6*x**2*y - x**3 + x**2
+
+    assert minimal_polynomial(sqrt(x) / z, y) == z**2*y**2 - x
+    assert minimal_polynomial(sqrt(x) / (z + 1), y) == (z**2 + 2*z + 1)*y**2 - x
+
+    assert minimal_polynomial(1/x, y, polys=True) == Poly(-x*y + 1, y)
+    assert minimal_polynomial(1 / (x + 1), y, polys=True) == \
+        Poly((x + 1)*y - 1, y)
+    assert minimal_polynomial(sqrt(x), y, polys=True) == Poly(y**2 - x, y)
+    assert minimal_polynomial(sqrt(x) / z, y, polys=True) == \
+        Poly(z**2*y**2 - x, y)
+
+    raises(NotAlgebraic, lambda: minimal_polynomial(exp(x), y))
+    raises(GeneratorsError, lambda: minimal_polynomial(sqrt(x), x))
+    raises(NotImplementedError, lambda: minimal_polynomial(sqrt(x), x, compose=False))
