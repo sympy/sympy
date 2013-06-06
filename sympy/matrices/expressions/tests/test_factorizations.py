44df82c4dab1b15a2b12322a0eb58a133404f35b
Matthew Rocklin <mrocklin@gmail.com>
2013-06-06 14:42:31 -0500

    remove commented refine factorization tests

--- sympy/matrices/expressions/tests/test_factorizations.py
+++ sympy/matrices/expressions/tests/test_factorizations.py
@@ -9,18 +9,15 @@ def test_LU():
     assert L.shape == U.shape == X.shape
     assert ask(Q.lower_triangular(L))
     assert ask(Q.upper_triangular(U))
-    # assert refine(L*U) == X
 
 def test_Cholesky():
     L = LofCholesky(X)
-    # assert refine(L*L.T) == X
 
 def test_QR():
     Q_, R = qr(X)
     assert Q_.shape == R.shape == X.shape
     assert ask(Q.orthogonal(Q_))
     assert ask(Q.upper_triangular(R))
-    # assert refine(Q*R) == X
 
 def test_svd():
     U, S, V = svd(X)
@@ -28,4 +25,3 @@ def test_svd():
     assert ask(Q.orthogonal(U))
     assert ask(Q.orthogonal(V))
     assert ask(Q.diagonal(S))
-    # assert refine(U*S*V) == X
