30db4f11149edbe4ecd2e624d8283954abd47c95
Björn Dahlgren <bjodah@gmail.com>
2013-06-11 07:35:15 +0200

    Added include_dirs argument for numpy in generated setup.py for Cython

--- sympy/utilities/autowrap.py
+++ sympy/utilities/autowrap.py
@@ -211,9 +211,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy as np
+
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension(%(args)s, extra_compile_args=['-std=c99'])]
+    ext_modules = [Extension(%(args)s, include_dirs=[np.get_include()], extra_compile_args=['-std=c99'])]
         )
 """
 
