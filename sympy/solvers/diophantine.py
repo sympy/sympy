64764d8eebea76eef16cbbd4655e4988a366ee8a
Thilina Rathnayake <thilinarmtb@gmail.com>
2013-06-14 09:33:27 +0530

    Tests updated and now the method works for any number of variables

--- sympy/solvers/diophantine.py
+++ sympy/solvers/diophantine.py
@@ -1,6 +1,8 @@
 from sympy import degree_list
 from sympy import igcd
-from sympy import symbols, Wild
+from sympy import symbols
+from sympy import Add
+from sympy import Integer
 
 
 def diop_solve(eq):
@@ -8,49 +10,95 @@ def diop_solve(eq):
     coeff, var, t = classify_diop(eq)
 
     if t == "linear":
-        return diop_linear(coeff, var)
+        return diop_linear(var, coeff)
 
 
 def classify_diop(eq):
 
+    var = list(eq.free_symbols)
+    var.sort()
+
     if max(degree_list(eq)) == 1:
-        var = list(eq.free_symbols)
         diop_type = "linear"
-
-        pattern = 0
-
-        for i, v in enumerate(var):
-            a = Wild("a" + str(i+1), exclude=var)
-            pattern = pattern + a * v
-
-        b = Wild('b', exclude=var)
-        pattern = pattern - b
-
-    return eq.match(pattern), var, diop_type
+    else:
+        raise NotImplementedError("Still not implemented")
 
 
-def diop_linear(coeff, var):
+    coeff = dict([reversed(t.as_independent(*var)) for t in eq.args])
+    for v in coeff:
+        if not isinstance(coeff[v], Integer):
+            raise TypeError("Coefficients should be Integers")
+
+    return coeff, var, diop_type,
+
+
+def diop_linear(var, coeff):
+
+    x = var[0]; y = var[1]
+    a = coeff[x]; b = coeff[y]
+
+    if len(var) == len(coeff):
+        c = 0
+    else:
+        c = -coeff[Integer(1)]
 
     if len(var) == 2:
-        a1 = Wild('a1', exclude=var)
-        a2 = Wild('a2', exclude=var)
-        b = Wild('b', exclude=var)
+        sol_x, sol_y = base_solution(c, a, b, True)
+        return {x: sol_x, y: sol_y}
 
-        t = symbols('t', type = int)
-        d, x0, y0 = extended_euclid(coeff[a1], coeff[a2])
+    elif len(var) > 2:
+        X = []; Y = []
 
-        if d == igcd(coeff[b], d):
-            return {var[0]:(coeff[b]/d) * (x0 + coeff[a2]*t), var[1]:(coeff[b]/d) * (y0 - coeff[a1]*t)}
+        for v in var[2:]:
+            sol_x, sol_y  = base_solution(-coeff[v], a, b)
+            X.append(sol_x*v); Y.append(sol_y*v)
+
+        sol_x, sol_y = base_solution(c, a, b, True)
+        X.append(sol_x); Y.append(sol_y)
+
+        l = []
+        if None not in X and None not in Y:
+            l.append((x, Add(*X))); l.append((y, Add(*Y)))
+            for v in var[2:]:
+                l.append((v, v))
         else:
-            print "Not Solvable"
+            for v in var:
+                l.append((v, None))
+
+        return dict(l)
+
+
+def base_solution(c, a, b, param=False):
+
+    t = symbols("t", type = Integer)
+
+    if c == 0:
+        if param:
+            return (b*t , -a*t)
+        else:
+            return (0, 0)
+    else:
+        x0, y0, d = extended_euclid(abs(a), abs(b))
+
+        x0 = x0 * (a / abs(a))
+        y0 = y0 * (b / abs(b))
+
+        if d == igcd(c, d):
+            a = a / d; b = b / d; c = c / d;
+            if param:
+                return (c*(x0 + b*t), c*(y0 - a*t))
+            else:
+                return (c * x0, c * y0)
+        else:
+            return (None, None)
 
 
 def extended_euclid(a, b):
 
     if b == 0:
-        return (a, 1, 0)
+        return (1, 0, a)
 
-    d, x0, y0 = extended_euclid(b, a%b)
+    x0, y0, d = extended_euclid(b, a%b)
     x, y = y0, x0 - (a//b) * y0
 
-    return d, x, y
+    return x, y, d
--- sympy/solvers/tests/test_diophantine.py
+++ sympy/solvers/tests/test_diophantine.py
@@ -1,7 +1,25 @@
 from sympy.solvers.diophantine import diop_solve
 from sympy import symbols
+from sympy import Integer
 
-def test_diop_solve():
-    x, y, t = symbols("x, y, t", type = int)
+
+def test_linear():
+    x, y, z, w, t = symbols("x, y, z, w, t", type = Integer)
 
     assert diop_solve(2*x + 3*y - 5) == {x: 15*t - 5, y: -10*t + 5}
+    assert diop_solve(3*y + 2*x - 5) == {x: 15*t - 5, y: -10*t + 5}
+    assert diop_solve(2*x - 3*y - 5) == {x: -15*t - 5, y: -10*t - 5}
+    assert diop_solve(-2*x - 3*y - 5) == {x: -15*t + 5, y: 10*t - 5}
+    assert diop_solve(7*x + 5*y) == {x: 5*t, y: -7*t}
+    assert diop_solve(2*x + 4*y) == {x: 4*t, y: -2*t}
+    assert diop_solve(4*x + 6*y - 4) == {x: 6*t - 2, y: -4*t + 2}
+    assert diop_solve(4*x + 6*y - 3) == {x: None, y: None}
+    assert diop_solve(4*x + 3*y -4*z + 5) == \
+           {x: -15*t + 4*z - 5, y: 20*t - 4*z + 5, z: z}
+    assert diop_solve(4*x + 2*y + 8*z - 5) == {x: None, y: None, z: None}
+    assert diop_solve(5*x + 7*y - 2*z - 6) == \
+           {x: 42*t + 6*z + 18, y: -30*t - 4*z - 12, z: z}
+    assert diop_solve(3*x - 6*y + 12*z - 9) == \
+           {x: -6*t - 4*z + 3, y: -3*t, z: z}
+    assert diop_solve(x + 3*y - 4*z + w -6) == \
+           {w: 6*t, x: -6*t - 3*y + 4*z + 6, y: y, z: z}
