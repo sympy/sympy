7d1cf0d4880d816d4b5fab530e0f2df78c83e1a9
Oliver Lee <oliverzlee@gmail.com>
2013-06-25 01:12:43 -0700

    Add test for output types of Vector, Dyadic products.
    Test the output types for dot, cross, and outer products of Vectors and
    Dyadics.

--- sympy/physics/mechanics/tests/test_essential.py
+++ sympy/physics/mechanics/tests/test_essential.py
@@ -1,9 +1,9 @@
-from sympy import cos, Matrix, sin, symbols, simplify, pi, Function, \
-     zeros
+from sympy import cos, Matrix, sin, symbols, pi, S, Function, zeros
 from sympy.abc import x, y, z
-from sympy.physics.mechanics import Vector, ReferenceFrame, dot, \
-     dynamicsymbols, CoordinateSym, express
+from sympy.physics.mechanics import Vector, ReferenceFrame, dot, dynamicsymbols
+from sympy.physics.mechanics import Dyadic, CoordinateSym, express
 from sympy.physics.mechanics.essential import MechanicsLatexPrinter
+from sympy.utilities.pytest import raises
 
 Vector.simp = True
 A = ReferenceFrame('A')
@@ -345,3 +345,73 @@ def test_dyadic_simplify():
 def test_latex_printer():
     r = Function('r')('t')
     assert MechanicsLatexPrinter().doprint(r**2) == "r^{2}"
+
+
+def test_output_type():
+    A = ReferenceFrame('A')
+    v = A.x + A.y
+    d = v | v
+    zerov = Vector([])
+    zerod = Dyadic([])
+
+    # dot products
+    assert isinstance(d & d, Dyadic)
+    assert isinstance(d & zerod, Dyadic)
+    assert isinstance(zerod & d, Dyadic)
+    assert isinstance(d & v, Vector)
+    assert isinstance(v & d, Vector)
+    assert isinstance(d & zerov, Vector)
+    assert isinstance(zerov & d, Vector)
+    raises(TypeError, lambda: d & S(0))
+    raises(TypeError, lambda: S(0) & d)
+    raises(TypeError, lambda: d & 0)
+    raises(TypeError, lambda: 0 & d)
+    assert not isinstance(v & v, (Vector, Dyadic))
+    assert not isinstance(v & zerov, (Vector, Dyadic))
+    assert not isinstance(zerov & v, (Vector, Dyadic))
+    raises(TypeError, lambda: v & S(0))
+    raises(TypeError, lambda: S(0) & v)
+    raises(TypeError, lambda: v & 0)
+    raises(TypeError, lambda: 0 & v)
+
+    # cross products
+    raises(TypeError, lambda: d ^ d)
+    raises(TypeError, lambda: d ^ zerod)
+    raises(TypeError, lambda: zerod ^ d)
+    assert isinstance(d ^ v, Dyadic)
+    assert isinstance(v ^ d, Dyadic)
+    assert isinstance(d ^ zerov, Dyadic)
+    assert isinstance(zerov ^ d, Dyadic)
+    assert isinstance(zerov ^ d, Dyadic)
+    raises(TypeError, lambda: d ^ S(0))
+    raises(TypeError, lambda: S(0) ^ d)
+    raises(TypeError, lambda: d ^ 0)
+    raises(TypeError, lambda: 0 ^ d)
+    assert isinstance(v ^ v, Vector)
+    assert isinstance(v ^ zerov, Vector)
+    assert isinstance(zerov ^ v, Vector)
+    raises(TypeError, lambda: v ^ S(0))
+    raises(TypeError, lambda: S(0) ^ v)
+    raises(TypeError, lambda: v ^ 0)
+    raises(TypeError, lambda: 0 ^ v)
+
+    # outer products
+    raises(TypeError, lambda: d | d)
+    raises(TypeError, lambda: d | zerod)
+    raises(TypeError, lambda: zerod | d)
+    raises(TypeError, lambda: d | v)
+    raises(TypeError, lambda: v | d)
+    raises(TypeError, lambda: d | zerov)
+    raises(TypeError, lambda: zerov | d)
+    raises(TypeError, lambda: zerov | d)
+    raises(TypeError, lambda: d | S(0))
+    raises(TypeError, lambda: S(0) | d)
+    raises(TypeError, lambda: d | 0)
+    raises(TypeError, lambda: 0 | d)
+    assert isinstance(v | v, Dyadic)
+    assert isinstance(v | zerov, Dyadic)
+    assert isinstance(zerov | v, Dyadic)
+    raises(TypeError, lambda: v | S(0))
+    raises(TypeError, lambda: S(0) | v)
+    raises(TypeError, lambda: v | 0)
+    raises(TypeError, lambda: 0 | v)
