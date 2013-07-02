9fa79b3dc4d3d6cf19afd6656986f0cf60d40342
Aaron Meurer <asmeurer@gmail.com>
2013-07-02 18:30:19 -0500

    Add fuzzy_or

--- sympy/core/logic.py
+++ sympy/core/logic.py
@@ -75,6 +75,25 @@ def fuzzy_not(v):
     else:
         return not v
 
+def fuzzy_or(*args):
+    """
+    Or in fuzzy logic. Returns True (any True), False (all False), or None
+
+    See the docstrings of fuzzy_and and fuzzy_not for more info.  fuzzy_or is
+    related to the two by the standard De Morgan's law.
+
+    >>> from sympy.core.logic import fuzzy_or
+    >>> fuzzy_or(True, False)
+    True
+    >>> fuzzy_or(True, None)
+    True
+    >>> fuzzy_or(False, False)
+    False
+    >>> print fuzzy_or(False, None)
+    None
+
+    """
+    return fuzzy_not(fuzzy_and(*(fuzzy_not(i) for i in args)))
 
 class Logic(object):
     """Logical expression"""
