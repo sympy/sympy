a7d5393e7cfbbba8dbb39e73bb75794ed2848f70
Matthew Rocklin <mrocklin@gmail.com>
2013-07-04 16:30:16 -0500

    docstring for conditioning on random symbol

--- sympy/stats/rv.py
+++ sympy/stats/rv.py
@@ -450,7 +450,7 @@ def rs_swap(a, b):
 
 
 def given(expr, condition=None, **kwargs):
-    """
+    """ Conditional Random Expression
     From a random expression and a condition on that expression creates a new
     probability space from the condition and returns the same expression on that
     conditional probability space.
@@ -463,6 +463,25 @@ def given(expr, condition=None, **kwargs):
     >>> Y = given(X, X>3)
     >>> density(Y).dict
     {4: 1/3, 5: 1/3, 6: 1/3}
+
+    Following convention, if the condition is a random symbol then that symbol
+    is considered fixed.
+
+    >>> from sympy.stats import Normal
+    >>> from sympy import pprint
+    >>> from sympy.abc import z
+
+    >>> X = Normal('X', 0, 1)
+    >>> Y = Normal('Y', 0, 1)
+    >>> pprint(density(X + Y, Y)(z), use_unicode=False)
+                    2
+           -(-Y + z)
+           -----------
+      ___       2
+    \/ 2 *e
+    ------------------
+             ____
+         2*\/ pi
     """
 
     if not random_symbols(condition) or pspace_independent(expr, condition):
