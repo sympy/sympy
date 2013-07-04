7c7a4de51d137bc4a3e78da3852a3f6889f688eb
Julien Rioux <julien.rioux@gmail.com>
2013-07-04 23:37:54 +0200

    code style: sympy/parsing

--- sympy/parsing/tests/test_implicit_multiplication_application.py
+++ sympy/parsing/tests/test_implicit_multiplication_application.py
@@ -62,7 +62,6 @@ def test_implicit_application():
            lambda: parse_expr('sin**2(x)', transformations=transformations2))
 
 
-
 def test_function_exponentiation():
     cases = {
         'sin**2(x)': 'sin(x)**2',
