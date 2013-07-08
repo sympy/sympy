181d1e630e248c46917a18e9e9fc1cf0990dff6f
Aaron Meurer <asmeurer@gmail.com>
2013-07-07 22:38:49 -0500

    Small updates to mailmap_update.py

--- bin/mailmap_update.py
+++ bin/mailmap_update.py
@@ -3,12 +3,26 @@
 """
 A tool to help keep .mailmap and AUTHORS up-to-date.
 """
+# TODO:
+# - Check doc/src/aboutus.rst
+# - Make it easier to update .mailmap or AUTHORS with the correct entries.
 
 import os
+import sys
 
 from fabric.api import local, env
-from fabric.colors import yellow
+from fabric.colors import yellow, blue, green
 from fabric.utils import warn
+
+mailmap_update_path = os.path.abspath(__file__)
+mailmap_update_dir = os.path.dirname(mailmap_update_path)
+sympy_top = os.path.split(mailmap_update_dir)[0]
+sympy_dir = os.path.join(sympy_top, 'sympy')
+
+if os.path.isdir(sympy_dir):
+    sys.path.insert(0, sympy_top)
+
+from sympy.utilities.misc import filldedent
 
 try:
     # Only works in newer versions of fabric
@@ -33,6 +47,13 @@ authors_skip = ["Kirill Smelkov <kirr@landau.phys.spbu.ru>"]
 
 predate_git = 0
 
+print blue(filldedent("""Read the text at the top of AUTHORS and the text at
+the top of .mailmap for information on how to fix the below errors.  If
+someone is missing from AUTHORS, add them where they would have been if they
+were added after their first pull request was merged ( checkout the merge
+commit from the first pull request and see who is at the end of the AUTHORS
+file at that commit."""))
+
 print
 print yellow("People who are in AUTHORS but not in git:")
 print
@@ -51,10 +72,10 @@ print
 for name in sorted(set(git_people) - set(authors) - set(authors_skip)):
     print name
 
-authors_count = AUTHORS[AUTHORS.find(firstauthor):].strip().count("\n")
+# + 1 because the last newline is stripped by strip()
+authors_count = AUTHORS[AUTHORS.find(firstauthor):].strip().count("\n") + 1
 adjusted_authors_count = (
     authors_count
-    + 1 # Final line won't have newline due to above strip()
     - predate_git
     + len(authors_skip)
     )
@@ -66,4 +87,9 @@ print yellow("There are {git_count} people in git, and {adjusted_authors_count} 
     adjusted_authors_count=adjusted_authors_count))
 
 if git_count != adjusted_authors_count:
-    warn("These two are not the same!")
+    warn("These two numbers are not the same!")
+else:
+    print
+    print green(filldedent("""Congratulations. The AUTHORS and .mailmap files
+appear to be up to date. You should now verify that doc/src/aboutus has %s
+people.""" % authors_count))
