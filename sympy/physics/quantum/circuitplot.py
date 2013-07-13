db770aef25771968455c88b3df19299b30a0c3fa
Rick Muller <rpmuller@gmail.com>
2013-07-13 08:33:50 -0600

    Outdenting labler

--- sympy/physics/quantum/circuitplot.py
+++ sympy/physics/quantum/circuitplot.py
@@ -36,10 +36,7 @@ if not np or not matplotlib:
 
     def circuit_plot(*args, **kwargs):
         raise ImportError('numpy or matplotlib not available.')
-
-    def labler(*args, **kwargs): return []
 else:
-
     pyplot = matplotlib.pyplot
     Line2D = matplotlib.lines.Line2D
     Circle = matplotlib.patches.Circle
@@ -243,20 +240,20 @@ else:
         """
         return CircuitPlot(c, nqubits, **kwargs)
 
-    def labler(n,symbol='q'):
-        """Autogenerate labels for wires of quantum circuits.
+def labler(n,symbol='q'):
+    """Autogenerate labels for wires of quantum circuits.
 
-        Parameters
-        ==========
-        n : int
-            number of qubits in the circuit
-        symbol : string
-            A character string to precede all gate labels. E.g. 'q_0', 'q_1', etc.
+    Parameters
+    ==========
+    n : int
+      number of qubits in the circuit
+    symbol : string
+      A character string to precede all gate labels. E.g. 'q_0', 'q_1', etc.
 
-        >>> from sympy.physics.quantum.circuitplot import labler
-        >>> labler(2)
-        ['q_1', 'q_0']
-        >>> labler(3,'j')
-        ['j_2', 'j_1', 'j_0']
-        """
-        return ['%s_%d' % (symbol,n-i-1) for i in range(n)]
+    >>> from sympy.physics.quantum.circuitplot import labler
+    >>> labler(2)
+    ['q_1', 'q_0']
+    >>> labler(3,'j')
+    ['j_2', 'j_1', 'j_0']
+    """
+    return ['%s_%d' % (symbol,n-i-1) for i in range(n)]
