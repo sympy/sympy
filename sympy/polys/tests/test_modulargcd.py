5e04ac606b2638033080667ce2e3309f602704d8
Katja Sophie Hotz <katja.sophie.hotz@student.tuwien.ac.at>
2013-07-13 11:33:12 +0200

    polys: add tests for modgcd_bivariate()

--- sympy/polys/tests/test_modulargcd.py
+++ sympy/polys/tests/test_modulargcd.py
@@ -2,7 +2,8 @@ from sympy.polys.rings import ring
 from sympy.polys.domains import ZZ
 from sympy.polys.modulargcd import (
     modgcd_univariate,
-    modgcd_bivariate)
+    modgcd_bivariate,
+    modgcd_multivariate)
 
 def test_modgcd_univariate_integers():
     R, x = ring("x", ZZ)
@@ -115,3 +116,84 @@ def test_modgcd_bivariate_integers():
     f = 2*x**2 + 2*x*y - 3*x - 3*y
     g = 4*x*y - 2*x + 4*y**2 - 2*y
     assert modgcd_bivariate(f, g) == (x + y, 2*x - 3, 4*y - 2)
+
+
+def test_modgcd_multivariate_integers():
+    R, x, y = ring("x,y", ZZ)
+
+    f, g = R.zero, R.zero
+    assert modgcd_multivariate(f, g) == (0, 0, 0)
+
+    f, g = 2*x**2 + 4*x + 2, x + 1
+    assert modgcd_multivariate(f, g) == (x + 1, 2*x + 2, 1)
+
+    f, g = x + 1, 2*x**2 + 4*x + 2
+    assert modgcd_multivariate(f, g) == (x + 1, 1, 2*x + 2)
+
+    f = 2*x**2 + 2*x*y - 3*x - 3*y
+    g = 4*x*y - 2*x + 4*y**2 - 2*y
+    assert modgcd_multivariate(f, g) == (x + y, 2*x - 3, 4*y - 2)
+
+    f, g = x*y**2 + 2*x*y + x, x*y**3 + x
+    assert modgcd_multivariate(f, g) == (x*y + x, y + 1, y**2 - y + 1)
+
+    R, x, y, z, u = ring("x,y,z,u", ZZ)
+
+    f, g = u**2 + 2*u + 1, 2*u + 2
+    assert modgcd_multivariate(f, g) == (u + 1, u + 1, 2)
+
+    f, g = z**2*u**2 + 2*z**2*u + z**2 + z*u + z, u**2 + 2*u + 1
+    h, cff, cfg = u + 1, z**2*u + z**2 + z, u + 1
+
+    assert modgcd_multivariate(f, g) == (h, cff, cfg)
+    assert modgcd_multivariate(g, f) == (h, cfg, cff)
+
+    R, x, y, z = ring("x,y,z", ZZ)
+
+    f, g = x - y*z, x - y*z
+    assert modgcd_multivariate(f, g) == (x - y*z, 1, 1)
+
+    f, g, h = R.fateman_poly_F_1()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    R, x, y, z, u, v = ring("x,y,z,u,v", ZZ)
+
+    f, g, h = R.fateman_poly_F_1()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    R, x, y, z, u, v, a, b = ring("x,y,z,u,v,a,b", ZZ)
+
+    f, g, h = R.fateman_poly_F_1()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    R, x, y, z, u, v, a, b, c, d = ring("x,y,z,u,v,a,b,c,d", ZZ)
+
+    f, g, h = R.fateman_poly_F_1()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    R, x, y, z = ring("x,y,z", ZZ)
+
+    f, g, h = R.fateman_poly_F_2()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    f, g, h = R.fateman_poly_F_3()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
+
+    R, x, y, z, t = ring("x,y,z,t", ZZ)
+
+    f, g, h = R.fateman_poly_F_3()
+    H, cff, cfg = modgcd_multivariate(f, g)
+
+    assert H == h and H*cff == f and H*cfg == g
