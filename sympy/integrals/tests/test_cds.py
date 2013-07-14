94596cd5a49fb68c2514eea3e7bd99d4e6f4816b
chetna <cheta.gup@gmail.com>
2013-07-14 20:16:10 +0530

    Test Indication in CDS(test/test_cds.py):
    This PR adds the test to check for the nitpicks
    in the coupled differntial system

--- sympy/integrals/tests/test_cds.py
+++ sympy/integrals/tests/test_cds.py
@@ -10,12 +10,21 @@ from sympy.integrals.risch import (gcdex_diophantine, frac_in, as_poly_1t,
     integrate_nonlinear_no_specials, integer_powers, DifferentialExtension,
     risch_integrate, DecrementLevel, NonElementaryIntegral)
 from sympy.integrals.cds import (cds_cancel_prim, cds_cancel_exp,
-    cds_cancel_tan)
+    cds_cancel_tan, coupled_DE_System)
 from sympy.utilities.pytest import raises
 
 from sympy.abc import x, t, nu, z, a, y
 t0, t1, t2 = symbols('t:3')
 i = Symbol('i')
+
+def test_cds_cancel_prim():
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t)]})
+    b1 = Poly(2*x*t**3, DE.t)
+    b2 = Poly(t + x, DE.t)
+    c1 = Poly(t**2 + x**2, DE.t)
+    c2 = Poly(2*t*x, DE.t)
+    n = 4
+    print cds_cancel_prim(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
 
 def test_cds_cancel_exp():
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)]})
@@ -23,8 +32,21 @@ def test_cds_cancel_exp():
     b2 = Poly(4*t**2 + 2*t, DE.t)
     c1 = Poly(-t**2 + 2*t, DE.t)
     c2 = Poly(2*x*t, DE.t)
-    n = 2
+    n = 3
     print cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    b1 = Poly(t**3 + t**2 + 1, DE.t)
+    b2 = Poly(t**4 + t**2 + 2*t + x**3, DE.t)
+    c1 = Poly(-t**2 + 2*t + x**2, DE.t)
+    c2 = Poly(2*x*t, DE.t)
+    n = 2
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)]})
+    print cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    b1 = Poly(t**2, DE.t)
+    b2 = Poly(2*t**2, DE.t)
+    c1 = Poly(-t**2 + 2*t , DE.t)
+    c2 = Poly(2*t, DE.t)
+    n = 2
+#    print cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
 
 
 def test_cds_cancel_tan():
@@ -34,4 +56,20 @@ def test_cds_cancel_tan():
     c1 = Poly(-t**2 + 2*t - 8*x**2 + 1, DE.t)
     c2 = Poly(2*(1 - 2*x), DE.t)
     n = 2
+#    print cds_cancel_tan(b0, b2, c1, c2, DE, n)
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)]})
+    b0 = Poly(0, DE.t)
+    b2 = Poly(4*x, DE.t)
+    c1 = Poly(8*x**2*1/(t**2 + 1) - t**2*1/(t**2 + 1) + 2*t*1/(t**2 + 1) + 1/(t**2 + 1), 1/(t**2 + 1))
+    c2 = Poly(-4*x*1/(t**2 + 1) + 2*1/(t**2 + 1), 1/(t**2 + 1))
+    n = 2
     print cds_cancel_tan(b0, b2, c1, c2, DE, n)
+
+def test_coupled_DE_System():
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)]})
+    b1 = Poly(5*t + 2*t**3 + x**2, DE.t)
+    b2 = Poly(4*t**2 + x, DE.t)
+    c1 = Poly(-t**2 + 2*t - 8*x**2 + 1, DE.t)
+    c2 = Poly(2*(1 - 2*x), DE.t)
+    n = 4
+    print coupled_DE_System(b1, b2, c1, c2, DE)
