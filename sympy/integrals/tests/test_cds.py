661a42dff50845ff5edb5d437c784538f4aea9cd
chetna <cheta.gup@gmail.com>
2013-07-15 19:23:16 +0530

    Test Cases for coupled differntial system
    Here i have just added the tests on which i have tried the correctness
    and error in the code for cds. Tests are just to check the valid working
    of cancel cases for primitive, hypertangent and hyperexponential
    differntial extension. It also add the L_K and E_k to the DE,
     which would be required to suffice the conditions of the structure
    theorem
    
    The current code would seem to completely fail for hypertangent cases
    as we have no support for T_K and A_K in prde.py
    I have added the tests on which i have tried running the code
    Though the outputs are not as expected(which tend to mostly fail because
    of parametric log derivative problem returning None) but the code has been
    tested interms of the implmentation wrt to cds.py
    Improvments forth-coming commits

--- sympy/integrals/tests/test_cds.py
+++ sympy/integrals/tests/test_cds.py
@@ -18,29 +18,31 @@ t0, t1, t2 = symbols('t:3')
 i = Symbol('i')
 
 def test_cds_cancel_prim():
-    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t)]})
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t)],
+        'L_K': [1], 'E_K': [], 'L_args': [x], 'E_args': []})
     b1 = Poly(2*x*t**3, DE.t)
     b2 = Poly(t + x, DE.t)
     c1 = Poly(t**2 + x**2, DE.t)
     c2 = Poly(2*t*x, DE.t)
     n = 4
-    print cds_cancel_prim(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    cds_cancel_prim(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
 
 def test_cds_cancel_exp():
-    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)]})
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)],
+        'L_K': [1], 'E_K': [], 'L_args': [x], 'E_args': []})
     b1 = Poly(2*t**3, DE.t)
     b2 = Poly(4*t**2 + 2*t, DE.t)
     c1 = Poly(-t**2 + 2*t, DE.t)
     c2 = Poly(2*x*t, DE.t)
     n = 3
-    print cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
     b1 = Poly(t**3 + t**2 + 1, DE.t)
     b2 = Poly(t**4 + t**2 + 2*t + x**3, DE.t)
     c1 = Poly(-t**2 + 2*t + x**2, DE.t)
     c2 = Poly(2*x*t, DE.t)
     n = 2
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)]})
-    print cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    cds_cancel_exp(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
     b1 = Poly(t**2, DE.t)
     b2 = Poly(2*t**2, DE.t)
     c1 = Poly(-t**2 + 2*t , DE.t)
@@ -57,19 +59,25 @@ def test_cds_cancel_tan():
     c2 = Poly(2*(1 - 2*x), DE.t)
     n = 2
 #    print cds_cancel_tan(b0, b2, c1, c2, DE, n)
-    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)]})
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)],
+       'L_K': [], 'E_K': [], 'L_args': [], 'E_args': []})
     b0 = Poly(0, DE.t)
     b2 = Poly(4*x, DE.t)
     c1 = Poly(8*x**2*1/(t**2 + 1) - t**2*1/(t**2 + 1) + 2*t*1/(t**2 + 1) + 1/(t**2 + 1), 1/(t**2 + 1))
     c2 = Poly(-4*x*1/(t**2 + 1) + 2*1/(t**2 + 1), 1/(t**2 + 1))
     n = 2
-    print cds_cancel_tan(b0, b2, c1, c2, DE, n)
+    cds_cancel_tan(b0, b2, c1, c2, DE, n)
 
 def test_coupled_DE_System():
-    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)]})
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)],
+       'L_K': [1], 'E_K': [], 'L_args': [x], 'E_args': []})
     b1 = Poly(5*t + 2*t**3 + x**2, DE.t)
     b2 = Poly(4*t**2 + x, DE.t)
     c1 = Poly(-t**2 + 2*t - 8*x**2 + 1, DE.t)
     c2 = Poly(2*(1 - 2*x), DE.t)
     n = 4
-    print coupled_DE_System(b1, b2, c1, c2, DE)
+    coupled_DE_System(b1, b2, c1, c2, DE)
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t1), Poly(1/(x + 1), t2)],
+       'L_K': [1, 2], 'E_K': [], 'L_args': [x, x + 1], 'E_args': []})
+    coupled_DE_System(Poly(t, t), Poly(1, t),Poly(t**2 - 3*t + 1, t), Poly(-t**2 + t + 1, t), DE)
+
