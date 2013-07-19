0cd1d08bcdc4cd692060d619d417f5901efc27ed
chetna <cheta.gup@gmail.com>
2013-07-20 02:32:39 +0530

    Folowing Changes:
    Checks for unused variables
    Removes unwanted return statements
    Returns NonElementaryIntegral instead of None

--- sympy/integrals/cds.py
+++ sympy/integrals/cds.py
@@ -12,8 +12,6 @@ in K, it decides whether the system of equations
 
 Hence returning (y1, y2) if a solution exist, None otherwise
 """
-from __future__ import with_statement
-
 from sympy import sqrt
 
 from sympy.core import Dummy, ilcm, Add, Mul, Pow, S
@@ -26,13 +24,13 @@ from sympy.integrals.risch import (NonElementaryIntegralException,
 from sympy.integrals.rde import (weak_normalizer,
     bound_degree, spde, solve_poly_rde, normal_denom, special_denom)
 from sympy.integrals.prde import (real_imag, is_log_deriv_k_t_radical_in_field)
-def cds_cancel_prim(a, b1, b2, c1, c2, DE, n):
+def cds_cancel_primitive(a, b1, b2, c1, c2, DE, n):
     """
     Cancellation - primitive case
 
     Given a derivation D on k[t], n either an integer or positive
     infinity, a in Const(k), b1, b2 in k and c1, c2 in k[t] with
-    Dt in k, sqrt(a) not in k(t) and b1!=0 or b2=0, return either
+    Dt in k, sqrt(a) not in k(t) and b1 != 0 or b2 != 0, return either
     "no solution" in which case the system as follows
 
          /     \       /        \    /  \        /    \
@@ -56,24 +54,24 @@ def cds_cancel_prim(a, b1, b2, c1, c2, DE, n):
         n1, m1, u1 = A1
         n2, m2, u2 = A2
         u = u1 + u2*sqrt(a)
-        if is_deriv(u1*c1 + a*z2*c2) and is_deriv(z2*c1 + z1*c2):
+        if is_deriv(u1*c1 + a*u2*c2) and is_deriv(u2*c1 + u1*c2):
             if p1.degree() <= n and p2.degree() <= n:
-                denom = (z1**2 - a*z2**2).as_expr()
-                return ((u1*p1 - a*z2*p2).as_expr()/denom, (z1*p2 - z2*p1).as_expr()/denom())
+                denom = u1**2 - a*u2**2
+                return ((u1*p1 - a*u2*p2).to_field().mul_ground(1/denom),\
+                    (u1*p2 - a*u2*p1).to_field().mul_ground(1/denom))
             else:
-                return None
+                raise NonElementaryIntegralException
     if c1 == 0 and c2 == 0:
         return (0, 0)
     if n < max(c1.degree(), c2.degree()):
-        return None
+        raise NonElementaryIntegralException
     q1, q2 = (0, 0)
-    while c1 is not 0 or c2 is not 0:
+    while c1 or c2:
         m = max(c1.degree(), c2.degree())
         if n < m:
-            return None
-        A = coupled_DE_System(b1, b2, Poly(c1.nth(m),DE.t), Poly(c2.nth(m),DE.t), DE)
-        if A is None:
-            return None
+            raise NonElementaryIntegralException
+            
+        A = coupled_DE_system(b1, b2, Poly(c1.nth(m),DE.t), Poly(c2.nth(m),DE.t), DE)
         (s1, s2) = A
         q1 = q1 + s1*t**m
         q2 = q2 + s2*t**m
@@ -115,25 +113,23 @@ def cds_cancel_exp(a, b1, b2, c1, c2, DE, n):
          n2, m2, u2 = A2
          m =  m1
          u = u1 + u2*sqrt(a)
-         if is_deriv(u1*c1 + a*z2*c2) and is_deriv(z2*c1 + z1*c2):
-             q1 = ((z1*p1 - a*z2*p2)*t**(-m)).as_expr()/(z1**2 - a*z2**2).as_expr()
-             q2 = ((z1*p2 - z2*p1)*t**(-m)).as_expr()/(z1**2 - a*z2**2).as_expr()
+         if is_deriv(u1*c1 + a*u2*c2) and is_deriv(u2*c1 + u1*c2):
+             q1 = ((u1*p1 - a*u2*p2)*t**(-m)).as_expr()/(u1**2 - a*u2**2).as_expr()
+             q2 = ((u1*p2 - u2*p1)*t**(-m)).as_expr()/(u1**2 - a*u2**2).as_expr()
              if q1 in k[t] and q2 in k[t] and q1.degree()<=n and p2.degree()<=n:
                  return (q1, q2)
              else:
-                 return None
+                 raise NonElementaryIntegralException
     if c1 == 0 and c2 == 0:
         return (0, 0)
     if n < max(c1.degree(), c2.degree()):
-        return None
+        raise NonElementaryIntegralException
     q1, q2 = (0, 0)
-    while c1 is not 0 and c2 is not 0:
+    while c1 or c2:
          m = max(c1.degree(), c2.degree())
          if n <  m:
-             return None
-         A = coupled_DE_System(b1, b2, Poly(c1.nth(m),DE.t), Poly(c2.nth(m),DE.t), DE)
-         if A is None:
-             return None
+             raise NonElementaryIntegralException
+         A = coupled_DE_system(b1, b2, Poly(c1.nth(m),DE.t), Poly(c2.nth(m),DE.t), DE)
          (s1, s2) = A
          q1 = q1 + s1*t**m
          q2 = q2 + s2*t**m
@@ -164,15 +160,15 @@ def cds_cancel_tan(b0, b2, c1, c2, DE, n):
     t = DE.t
     if n == 0:
       if c1 in k and c2 in k:
-          A = coupled_DE_System(b0, b2, c1, c2)
+          A = coupled_DE_system(b0, b2, c1, c2)
       else:
-          return None
+          raise NonElementaryIntegralException
     p = t - sqrt(-1)
-    eta = DE.d.exquo(Poly(DE.t**2 + 1, DE.t))
+    eta = DE.d.exquo(Poly(t**2 + 1, t))
     #u1 + u2*I = c1(I) + c2(I)*I
     c1_ = c1.eval(sqrt(-1))
     c2_ = c2.eval(sqrt(-1))
-    ca, cd = frac_in(c1_ + c2_*sqrt(-1), DE.t)
+    ca, cd = frac_in(c1_ + c2_*sqrt(-1), t)
 
     ca_dict = ca.as_poly(sqrt(-1)).as_dict()
     ca_r = [value if key[0] % 2 == 0 else 0 for key, value in ca_dict.items()]
@@ -189,24 +185,20 @@ def cds_cancel_tan(b0, b2, c1, c2, DE, n):
     u1d = cd_r*cd_r + cd_i*cd_i
     u2a = ca_i*cd_r - ca_r*cd_i
     u2d = u1d
-    u1 = (u1a.as_expr()/u1d.as_expr()).as_poly(DE.t)
-    u2 = (u2a.as_expr()/u1d.as_expr()).as_poly(DE.t)
-    A = coupled_DE_System(b0, b2, u1, u2, DE)
-    if A is None:
-        return None
+    u1 = (u1a.as_expr()/u1d.as_expr()).as_poly(t)
+    u2 = (u2a.as_expr()/u1d.as_expr()).as_poly(t)
+    A = coupled_DE_system(b0, b2, u1, u2, DE)
     (s1, s2) = A
-    c = c1 - z1 + n*eta*(s1*t + s2) + (c2 - z2 + n*eta*(s2*t - s1))*sqrt(-1)
+    c = c1 - u1 + n*eta*(s1*t + s2) + (c2 - u2 + n*eta*(s2*t - s1))*sqrt(-1)
     c = (c.as_expr()/p.as_expr()).as_poly(DE.t)
     d1a, d2a, d1d = real_imag(c)
-    d1 = (d1a.as_expr()/d1d.as_expr()).as_poly(DE.t)
-    d2 = (d2a.as_expr()/d1d.as_expr()).as_poly(DE.t)
+    d1 = (d1a.as_expr()/d1d.as_expr()).as_poly(t)
+    d2 = (d2a.as_expr()/d1d.as_expr()).as_poly(t)
     B = cds_cancel_tan(b0, b2 + eta, d1, d2, DE, n-1)
-    if B is None:
-        return None
     h1, h2 = B
     return (h1*t + h2 + s1, h2*t - h1 + s2)
 
-def coupled_DE_System(b1, b2, c1, c2, DE):
+def coupled_DE_system(b1, b2, c1, c2, DE):
     """
     """
     from sympy.integrals.rde import (no_cancel_b_large,
@@ -225,24 +217,20 @@ def coupled_DE_System(b1, b2, c1, c2, DE):
     n = bound_degree(A, B, C, DE)
     b, c, m, alpha, beta = spde(A, B, C, n, DE)
     # non cancellation cases solve for q
-    if not b.is_zero and (DE.case == 'base' or b.degree(DE.t) > max(0, DE.d.degree(DE.t) - 1)):
-        try:
-            q = no_cancel_b_large(b, c, n, DE)
-            return Poly(cancel((alpha*q + beta)/m) , DE.t)
-        except NonElementaryIntegralException:
-            return None
-    elif (b.is_zero or b.degree(DE.t) < DE.d.degree(DE.t) - 1) and (DE.case == 'base' or DE.d.degree(DE.t) >= 2):
-        try:
-            q = no_cancel_b_small(b, c, n, DE)
-            return Poly(cancel((alpha*q + beta)/m) , DE.t)
-        except NonElementaryIntegralException:
-            return None
-    elif DE.d.degree(DE.t) >= 2 and b.degree(DE.t) == DE.d.degree(DE.t) - 1 and n > -b.as_poly(DE.t).LC()/DE.d.as_poly(DE.t).LC():
-        try:
-            q = no_cancel_equal(b, c, n, DE)
-            return Poly(cancel((alpha*q + beta)/m) , DE.t)
-        except NonElementaryIntegralException:
-            return None
+    if not b.is_zero and (DE.case == 'base' or \
+      b.degree(DE.t) > max(0, DE.d.degree(DE.t) - 1)):
+        q = no_cancel_b_large(b, c, n, DE)
+        return Poly(cancel((alpha*q + beta)/m) , DE.t)
+
+    elif (b.is_zero or b.degree(DE.t) < DE.d.degree(DE.t) - 1) \
+      and (DE.case == 'base' or DE.d.degree(DE.t) >= 2):
+        q = no_cancel_b_small(b, c, n, DE)
+        return Poly(cancel((alpha*q + beta)/m) , DE.t)
+
+    elif DE.d.degree(DE.t) >= 2 and b.degree(DE.t) == DE.d.degree(DE.t)- 1 \
+      and n > -b.as_poly(DE.t).LC()/DE.d.as_poly(DE.t).LC():
+        q = no_cancel_equal(b, c, n, DE)
+        return Poly(cancel((alpha*q + beta)/m) , DE.t)
     # Does not fall in non cancellation
     # Hence cancellation cases
     case = DE.case
@@ -253,4 +241,6 @@ def coupled_DE_System(b1, b2, c1, c2, DE):
         (q1, q2) = cds_cancel_exp(a, b1, b2, c1, c2, DE, n)
     if case == 'tan':
         (q1, q2) = cds_cancel_tan(a, b1, b2, c1, c2, DE, n)
+    print "here"
+    return (q1, q2)
     return Poly(cancel(((q1 + sqrt(-1)*q2)*alpha + beta)/m), DE.t)
--- sympy/integrals/tests/test_cds.py
+++ sympy/integrals/tests/test_cds.py
@@ -9,15 +9,15 @@ from sympy.integrals.risch import (gcdex_diophantine, frac_in, as_poly_1t,
     integrate_hyperexponential, integrate_hypertangent_polynomial,
     integrate_nonlinear_no_specials, integer_powers, DifferentialExtension,
     risch_integrate, DecrementLevel, NonElementaryIntegral)
-from sympy.integrals.cds import (cds_cancel_prim, cds_cancel_exp,
-    cds_cancel_tan, coupled_DE_System)
+from sympy.integrals.cds import (cds_cancel_primitive, cds_cancel_exp,
+    cds_cancel_tan, coupled_DE_system)
 from sympy.utilities.pytest import raises
 
 from sympy.abc import x, t, nu, z, a, y
 t0, t1, t2 = symbols('t:3')
 i = Symbol('i')
 
-def test_cds_cancel_prim():
+def test_cds_cancel_primitive():
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t)],
         'L_K': [1], 'E_K': [], 'L_args': [x], 'E_args': []})
     b1 = Poly(2*x*t**3, DE.t)
@@ -25,7 +25,7 @@ def test_cds_cancel_prim():
     c1 = Poly(t**2 + x**2, DE.t)
     c2 = Poly(2*t*x, DE.t)
     n = 4
-    cds_cancel_prim(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
+    cds_cancel_primitive(Poly(sqrt(-1), DE.t), b1, b2, c1, c2, DE, n)
 
 def test_cds_cancel_exp():
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)],
@@ -58,7 +58,7 @@ def test_cds_cancel_tan():
     c1 = Poly(-t**2 + 2*t - 8*x**2 + 1, DE.t)
     c2 = Poly(2*(1 - 2*x), DE.t)
     n = 2
-    cds_cancel_tan(b0, b2, c1, c2, DE, n)
+#    cds_cancel_tan(b0, b2, c1, c2, DE, n)
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)],
        'L_K': [], 'E_K': [], 'L_args': [], 'E_args': []})
     b0 = Poly(0, DE.t)
@@ -68,15 +68,40 @@ def test_cds_cancel_tan():
     n = 2
     cds_cancel_tan(b0, b2, c1, c2, DE, n)
 
-def test_coupled_DE_System():
-    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)],
+def test_coupled_DE_system():
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t, t)],
+       'L_K': [], 'E_K': [1], 'L_args': [], 'E_args': [x]})
+    g1 = Poly(2*t + 1, t)
+    g2 = Poly(2*t**2 + 2*t + 1)
+    assert coupled_DE_system(Poly(t + 1, t), Poly(t - 1, t), g1, g2, DE) == \
+       (Poly(t, t), Poly(t + 1, t))
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t)],
        'L_K': [1], 'E_K': [], 'L_args': [x], 'E_args': []})
+    g1 = Poly(t + 1 + x*t - x*t**3, t)
+    g2 = Poly(2 + 2*t + 3*x*t**3 + 2*x*t)
+    assert coupled_DE_system(Poly(t**2 + 1, t), Poly(t**2, t), g1, g2, DE) == \
+       Poly((-x/(1 + I) + 3*I*x/(1 + I))*t, t)
+
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t1), Poly(1/(x + 1), t2)],
+       'L_K': [1, 2], 'E_K': [], 'L_args': [x, x + 1], 'E_args': []})
+    g1 = Poly(t + 1 +x*t - x*t**3, t)
+    g2 = Poly(2 + 2*t + 3*x*t**3 + 2*x*t)
+    assert coupled_DE_system(Poly(t**2 + 1, t), Poly(t**2, t), g1, g2, DE) == \
+       Poly((-x/(1 + I) + 3*I*x/(1 + I))*t, t)
+
+
+    g1 = Poly(t**3 - 3*t**2*x - 4*t**2 - t*x - t - x - 1 + 1/x, t)
+    g2 = Poly(2*t**3 + 3*t**2 + t**2*x - t*x**2 - 2*t*x + 2*t + 2 + 2/x, t)
+    assert coupled_DE_system(Poly(t**2 + 1, t), Poly(2*t + t*x, t), g1, g2, DE) == \
+       (Poly(t - x, t), Poly(2*t + 1, t))
+    DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(t**2 + 1, t)],
+       'L_K': [], 'E_K': [], 'L_args': [], 'E_args': []})
     b1 = Poly(5*t + 2*t**3 + x**2, DE.t)
     b2 = Poly(4*t**2 + x, DE.t)
     c1 = Poly(-t**2 + 2*t - 8*x**2 + 1, DE.t)
     c2 = Poly(2*(1 - 2*x), DE.t)
     n = 4
-    coupled_DE_System(b1, b2, c1, c2, DE)
+    coupled_DE_system(b1, b2, c1, c2, DE)
     DE = DifferentialExtension(extension={'D': [Poly(1, x), Poly(1/x, t1), Poly(1/(x + 1), t2)],
        'L_K': [1, 2], 'E_K': [], 'L_args': [x, x + 1], 'E_args': []})
-    coupled_DE_System(Poly(t, t), Poly(1, t),Poly(t**2 - 3*t + 1, t), Poly(-t**2 + t + 1, t), DE)
+    coupled_DE_system(Poly(t, t), Poly(1, t),Poly(t**2 - 3*t + 1, t), Poly(-t**2 + t + 1, t), DE)
