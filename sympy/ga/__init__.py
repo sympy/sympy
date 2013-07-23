8ce15c16cf53896f0277970a6443afd8d9baaab6
Julien Rioux <julien.rioux@gmail.com>
2013-07-23 15:56:08 +0200

    Remove duplicated stuff.

--- sympy/ga/__init__.py
+++ sympy/ga/__init__.py
@@ -2,11 +2,12 @@
 A module for geometric algebra
 
 """
-from ga import MV, Com, DD, Format, GAeval, Nga, ReciprocalFrame, ScalarFunction, \
-    cross, define_precedence, dual, ga_print_off, ga_print_on, inv, proj, \
-    refl, rot, rotor
+from ga import MV, Com, DD, Format, Nga, ReciprocalFrame, ScalarFunction, \
+    cross, dual, ga_print_off, ga_print_on, inv, proj, refl, rot, rotor
 
 from ga_debug import oprint, ostr
+
+from ga_precedence import define_precedence, GAeval
 
 from ga_print import enhance_print, Get_Program, Print_Function, latex, xdvi
 
