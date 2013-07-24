5c1e4d4b0c8011f619a98243f3dcaa1dd986247c
Sergey B Kirpichev <skirpichev@gmail.com>
2013-07-24 22:06:45 +0400

    XFAIL in gruntz for 2073

--- sympy/series/tests/test_gruntz.py
+++ sympy/series/tests/test_gruntz.py
@@ -413,3 +413,19 @@ def test_issue3744():
     n = Symbol('n', integer=True, positive=True)
     r = (n + 1)*x**(n + 1)/(x**(n + 1) - 1) - x/(x - 1)
     assert gruntz(r, x, 1).simplify() == n/2
+
+
+@XFAIL
+def test_issue_2073():
+    n = Symbol('n')
+    r = Symbol('r', positive=True)
+    c = Symbol('c')
+    p = Symbol('p', positive=True)
+    m = Symbol('m', negative=True)
+    expr = ((2*n*(n - r + 1)/(n + r*(n - r + 1)))**c + \
+        (r - 1)*(n*(n - r + 2)/(n + r*(n - r + 1)))**c - n)/(n**c - n)
+    expr = expr.subs(c, c + 1)
+    assert gruntz(expr.subs(c, m), n, oo) == 1
+    # fail:
+    assert gruntz(expr.subs(c, p), n, oo).simplify() == \
+        (2**(p + 1) + r - 1)/(r + 1)**(p + 1)
