ccb475ed7b5045b2fa0fb98a1f5e22af52632b9a
Rick Muller <rpmuller@gmail.com>
2013-07-30 10:31:19 -0600

    Disabling rc, and cleaning up whitespace

--- sympy/physics/quantum/circuitplot.py
+++ sympy/physics/quantum/circuitplot.py
@@ -47,6 +47,9 @@ else:
     Line2D = matplotlib.lines.Line2D
     Circle = matplotlib.patches.Circle
 
+    #from matplotlib import rc
+    #rc('text',usetex=True)
+
     class CircuitPlot(object):
         """A class for managing a circuit plot."""
 
