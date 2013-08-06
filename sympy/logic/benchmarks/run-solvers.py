sympy/sympy

commit e2a415797d4adb81551c5331b1adfdb4a6ecbe94

Update run-solvers.py
Fixed a syntax error in line 27

sympy/logic/benchmarks/run-solvers.py
@@ -23,8 +23,8 @@ for test in INPUT:
         assert satisfiable(theory, algorithm=alg)
         end = time.time()
         results[test][alg] = end - start
-        print("Test %d in time %.2f seconds for algorithm %s." % ()
-            test, end - start, alg)
+        print("Test %d in time %.2f seconds for algorithm %s." % 
+            (test, end - start, alg))
 
 print("problem," + ','.join(ALGORITHMS))
 

commit 0bdb41a9e1820abf7d068e58f3cdac9d2c8f7221

Fix a white space error
This was introduced in e2a415797d4adb81551c5331b1adfdb4a6ecbe94.

sympy/logic/benchmarks/run-solvers.py
@@ -23,7 +23,7 @@ for test in INPUT:
         assert satisfiable(theory, algorithm=alg)
         end = time.time()
         results[test][alg] = end - start
-        print("Test %d in time %.2f seconds for algorithm %s." % 
+        print("Test %d in time %.2f seconds for algorithm %s." %
             (test, end - start, alg))
 
 print("problem," + ','.join(ALGORITHMS))