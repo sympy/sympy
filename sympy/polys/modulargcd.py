9bcedccf052beaf46b4515173fef263114530ed5
Katja Sophie Hotz <katja.sophie.hotz@student.tuwien.ac.at>
2013-08-08 16:23:37 +0200

    polys: add documentation in modulargcd

--- sympy/polys/modulargcd.py
+++ sympy/polys/modulargcd.py
@@ -638,8 +638,13 @@ def _interpolate_multivariate(evalpoints, hpeval, ring, i, p, ground=False):
     r"""
     Reconstruct a polynomial `h_p` in `\mathbb{Z}_p[x_0, \ldots, x_{k-1}]`
     from a list of evaluation points in `\mathbb{Z}_p` and a list of
-    polynomials in `\mathbb{Z}_p[x_0, \ldots, x_{k-2}]`, which are the images
-    of `h_p` evaluated in the variable `x_{k-1}`.
+    polynomials in
+    `\mathbb{Z}_p[x_0, \ldots, x_{i-1}, x_{i+1}, \ldots, x_{k-1}]`, which
+    are the images of `h_p` evaluated in the variable `x_i`.
+
+    It is also possible to reconstruct a parameter of the ground domain,
+    i.e. if `h_p` is a polynomial over `\mathbb{Z}_p[x_0, \ldots, x_{k-1}]`.
+    In this case, one has to set ``ground=True``.
 
     Parameters
     ==========
@@ -647,18 +652,25 @@ def _interpolate_multivariate(evalpoints, hpeval, ring, i, p, ground=False):
     evalpoints : list of Integer objects
         list of evaluation points in `\mathbb{Z}_p`
     hpeval : list of PolyElement objects
-        list of polynomials in `\mathbb{Z}_p[x_0, \ldots, x_{k-2}]`,
-        images of `h_p` evaluated in the variable `x_{k-1}`
+        list of polynomials in (resp. over)
+        `\mathbb{Z}_p[x_0, \ldots, x_{i-1}, x_{i+1}, \ldots, x_{k-1}]`,
+        images of `h_p` evaluated in the variable `x_i`
     ring : PolyRing
         `h_p` will be an element of this ring
+    i : Integer
+        index of the variable which has to be reconstructed
     p : Integer
         prime number, modulus of `h_p`
+    ground : Boolean
+        indicates whether `x_i` is in the ground domain, default is
+        ``False``
 
     Returns
     =======
 
     hp : PolyElement
-        interpolated polynomial in `\mathbb{Z}_p[x_0, \ldots, x_{k-1}]`
+        interpolated polynomial in (resp. over)
+        `\mathbb{Z}_p[x_0, \ldots, x_{k-1}]`
 
     """
     hp = ring.zero
@@ -1200,12 +1212,52 @@ def modgcd_multivariate(f, g):
 
 
 def _gf_div(f, g, p):
+    r"""
+    Compute `\frac f g` modulo `p` for two univariate polynomials over
+    `\mathbb Z_p`.
+    """
     ring = f.ring
     densequo, denserem = gf_div(f.to_dense(), g.to_dense(), p, ring.domain)
     return ring.from_dense(densequo), ring.from_dense(denserem)
 
 
 def _rational_function_reconstruction(c, p, m):
+    r"""
+    Reconstruct a rational function `\frac a b` in `\mathbb Z_p(t)` from
+
+    .. math::
+
+        c = \frac a b \; \mathrm{mod} \, m,
+
+    where `c` and `m` are polynomials in `\mathbb Z_p[t]` and `m` has
+    positive degree.
+
+    The algorithm is based on the Euclidean Algorithm. In general, `m` is
+    not irreducible, so it is possible that `b` is not invertible modulo
+    `m`. In that case ``None`` is returned.
+
+    Parameters
+    ==========
+
+    c : PolyElement
+        univariate polynomial in `\mathbb Z[t]`
+    p : Integer
+        prime number
+    m : PolyElement
+        modulus, not necessarily irreducible
+
+    Returns
+    =======
+
+    frac : FracElement
+        either `\frac a b` in `\mathbb Z(t)` or ``None``
+
+    References
+    ==========
+
+    1. [Hoeij04]_
+
+    """
     ring = c.ring
     domain = ring.domain
     M = m.degree()
@@ -1236,6 +1288,51 @@ def _rational_function_reconstruction(c, p, m):
 
 
 def _rational_reconstruction_func_coeffs(hm, p, m, ring, k):
+    r"""
+    Reconstruct every coefficient `c_h` of a polynomial `h` in
+    `\mathbb Z_p(t_k)[t_1, \ldots, t_{k-1}][x, z]` from the corresponding
+    coefficient `c_{h_m}` of a polynomial `h_m` in
+    `\mathbb Z_p[t_1, \ldots, t_k][x, z] \cong \mathbb Z_p[t_k][t_1, \ldots, t_{k-1}][x, z]`
+    such that
+
+    .. math::
+
+        c_{h_m} = c_h \; \mathrm{mod} \, m,
+
+    where `m \in \mathbb Z_p[t]`.
+
+    The reconstruction is based on the Euclidean Algorithm. In general, `m`
+    is not irreducible, so it is possible that this fails for some
+    coefficient. In that case ``None`` is returned.
+
+    Parameters
+    ==========
+
+    hm : PolyElement
+        polynomial in `\mathbb Z[t_1, \ldots, t_k][x, z]`
+    p : Integer
+        prime number, modulus of `\mathbb Z_p`
+    m : PolyElement
+        modulus, polynomial in `\mathbb Z[t]`, not necessarily irreducible
+    ring : PolyRing
+        `\mathbb Z(t_k)[t_1, \ldots, t_{k-1}][x, z]`, `h` will be an
+        element of this ring
+    k : Integer
+        index of the parameter `t_k` which will be reconstructed
+
+    Returns
+    =======
+
+    h : PolyElement
+        reconstructed polynomial in
+        `\mathbb Z(t_k)[t_1, \ldots, t_{k-1}][x, z]` or ``None``
+
+    See also
+    ========
+
+    _rational_function_reconstruction
+
+    """
     h = ring.zero
 
     for monom, coeff in hm.iterterms():
@@ -1261,15 +1358,43 @@ def _rational_reconstruction_func_coeffs(hm, p, m, ring, k):
 
 
 def _gf_gcdex(f, g, p):
+    r"""
+    Extended Euclidean Algorithm for two univariate polynomials over
+    `\mathbb Z_p`.
+
+    Returns polynomials `s, t` and `h`, such that `h` is the GCD of `f` and
+    `g` and `sf + tg = h \; \mathrm{mod} \, p`.
+
+    """
     ring = f.ring
     s, t, h = gf_gcdex(f.to_dense(), g.to_dense(), p, ring.domain)
     return ring.from_dense(s), ring.from_dense(t), ring.from_dense(h)
 
 
 def _trunc(f, minpoly, p):
+    r"""
+    Compute the reduced representation of a polynomial `f` in
+    `\mathbb Z_p[z] / (\check m_{\alpha}(z))[x]`
 
+    Parameters
+    ==========
 
+    f : PolyElement
+        polynomial in `\mathbb Z[x, z]`
+    minpoly : PolyElement
+        polynomial `\check m_{\alpha} \in \mathbb Z[z]`, not necessarily
+        irreducible
+    p : Integer
+        prime number, modulus of `\mathbb Z_p`
 
+    Returns
+    =======
+
+    ftrunc : PolyElement
+        polynomial in `\mathbb Z[x, z]`, reduced modulo
+        `\check m_{\alpha}(z)` and `p`
+
+    """
     ring = f.ring
     minpoly = minpoly.set_ring(ring)
     p_ = ring.ground_new(p)
@@ -1278,6 +1403,33 @@ def _trunc(f, minpoly, p):
 
 
 def _euclidean_algorithm(f, g, minpoly, p):
+    r"""
+    Compute the monic GCD of two univariate polynomials in
+    `\mathbb{Z}_p[z]/(\check m_{\alpha}(z))[x]` with the Euclidean
+    Algorithm.
+
+    In general, `\check m_{\alpha}(z)` is not irreducible, so it is possible
+    that some leading coefficient is not invertible modulo
+    `\check m_{\alpha}(z)`. In that case ``None`` is returned.
+
+    Parameters
+    ==========
+
+    f, g : PolyElement
+        polynomials in `\mathbb Z[x, z]`
+    minpoly : PolyElement
+        polynomial in `\mathbb Z[z]`, not necessarily irreducible
+    p : Integer
+        prime number, modulus of `\mathbb Z_p`
+
+    Returns
+    =======
+
+    h : PolyElement
+        GCD of `f` and `g` in `\mathbb Z[z, x]` or ``None``, coefficients
+        are in `\left[ -\frac{p-1} 2, \frac{p-1} 2 \right]`
+
+    """
     ring = f.ring
 
     f = _trunc(f, minpoly, p)
@@ -1307,6 +1459,38 @@ def _euclidean_algorithm(f, g, minpoly, p):
 
 
 def _trial_division(f, h, minpoly, p=None):
+    r"""
+    Check if `h` divides `f` in
+    `\mathbb K[t_1, \ldots, t_k][z]/(m_{\alpha}(z))`, where `\mathbb K` is
+    either `\mathbb Q` or `\mathbb Z_p`.
+
+    This algorithm is based on pseudo division and does not use any
+    fractions. By default `\mathbb K` is `\mathbb Q`, if a prime number `p`
+    is given, `\mathbb Z_p` is chosen instead.
+
+    Parameters
+    ==========
+
+    f, h : PolyElement
+        polynomials in `\mathbb Z[t_1, \ldots, t_k][x, z]`
+    minpoly : PolyElement
+        polynomial `m_{\alpha}(z)` in `\mathbb Z[t_1, \ldots, t_k][z]`
+    p : Integer or None
+        if `p` is given, `\mathbb K` is set to `\mathbb Z_p` instead of
+        `\mathbb Q`, default is ``None``
+
+    Returns
+    =======
+
+    rem : PolyElement
+        remainder of `\frac f h`
+
+    References
+    ==========
+
+    1. [Hoeij02]_
+
+    """
     ring = f.ring
     domain = ring.domain
 
@@ -1345,6 +1529,10 @@ def _trial_division(f, h, minpoly, p=None):
 
 
 def _evaluate_ground(f, i, a):
+    r"""
+    Evaluate a polynomial `f` at `a` in the `i`-th variable of the ground
+    domain.
+    """
     ring = f.ring.clone(domain=f.ring.domain.ring.drop(i))
     fa = ring.zero
 
@@ -1355,6 +1543,47 @@ def _evaluate_ground(f, i, a):
 
 
 def _func_field_modgcd_p(f, g, minpoly, p):
+    r"""
+    Compute the GCD of two polynomials `f` and `g` in
+    `\mathbb Z_p(t_1, \ldots, t_k)[z]/(\check m_\alpha(z))[x]`.
+
+    The algorithm reduces the problem step by step by evaluating the
+    polynomials `f` and `g` at `t_k = a` for suitable `a \in \mathbb Z_p`
+    and then calls itself recursively to compute the GCD in
+    `\mathbb Z_p(t_1, \ldots, t_{k-1})[z]/(\check m_\alpha(z))[x]`. If these
+    recursive calls are successful, the GCD over `k` variables is
+    interpolated, otherwise the algorithm returns ``None``. After
+    interpolation, Rational Function Reconstruction is used to obtain the
+    correct coefficients. If this fails, a new evaluation point has to be
+    chosen, otherwise the desired polynomial is obtained by clearing
+    denominators. The result is verified with a fraction free trial
+    division.
+
+    Parameters
+    ==========
+
+    f, g : PolyElement
+        polynomials in `\mathbb Z[t_1, \ldots, t_k][x, z]`
+    minpoly : PolyElement
+        polynomial in `\mathbb Z[t_1, \ldots, t_k][z]`, not necessarily
+        irreducible
+    p : Integer
+        prime number, modulus of `\mathbb Z_p`
+
+    Returns
+    =======
+
+    h : PolyElement
+        primitive associate in `\mathbb Z[t_1, \ldots, t_k][x, z]` of the
+        GCD of the polynomials `f` and `g`  or ``None``, coefficients are
+        in `\left[ -\frac{p-1} 2, \frac{p-1} 2 \right]`
+
+    References
+    ==========
+
+    1. [Hoeij04]_
+
+    """
     ring = f.ring
     domain = ring.domain # Z[t_1, ..., t_k]
 
@@ -1480,6 +1709,41 @@ def _func_field_modgcd_p(f, g, minpoly, p):
 
 
 def _integer_rational_reconstruction(c, m, domain):
+    r"""
+    Reconstruct a rational number `\frac a b` from
+
+    .. math::
+
+        c = \frac a b \; \mathrm{mod} \, m,
+
+    where `c` and `m` are integers.
+
+    The algorithm is based on the Euclidean Algorithm. In general, `m` is
+    not a prime number, so it is possible that `b` is not invertible modulo
+    `m`. In that case ``None`` is returned.
+
+    Parameters
+    ==========
+
+    c : Integer
+        `c = \frac a b \; \mathrm{mod} \, m`
+    m : Integer
+        modulus, not necessarily prime
+    domain : IntegerRing
+        `a, b, c` are elements of ``domain``
+
+    Returns
+    =======
+
+    frac : Rational
+        either `\frac a b` in `\mathbb Q` or ``None``
+
+    References
+    ==========
+
+    1. [Wang81]_
+
+    """
     if c < 0:
         c += m
 
@@ -1509,6 +1773,46 @@ def _integer_rational_reconstruction(c, m, domain):
 
 
 def _rational_reconstruction_int_coeffs(hm, m, ring):
+    r"""
+    Reconstruct every rational coefficient `c_h` of a polynomial `h` in
+    `\mathbb Q[t_1, \ldots, t_k][x, z]` from the corresponding integer
+    coefficient `c_{h_m}` of a polynomial `h_m` in
+    `\mathbb Z[t_1, \ldots, t_k][x, z]` such that
+
+    .. math::
+
+        c_{h_m} = c_h \; \mathrm{mod} \, m,
+
+    where `m \in \mathbb Z`.
+
+    The reconstruction is based on the Euclidean Algorithm. In general,
+    `m` is not a prime number, so it is possible that this fails for some
+    coefficient. In that case ``None`` is returned.
+
+    Parameters
+    ==========
+
+    hm : PolyElement
+        polynomial in `\mathbb Z[t_1, \ldots, t_k][x, z]`
+    m : Integer
+        modulus, not necessarily prime
+    ring : PolyRing
+        `\mathbb Q[t_1, \ldots, t_k][x, z]`, `h` will be an element of this
+        ring
+
+    Returns
+    =======
+
+    h : PolyElement
+        reconstructed polynomial in `\mathbb Q[t_1, \ldots, t_k][x, z]` or
+        ``None``
+
+    See also
+    ========
+
+    _integer_rational_reconstruction
+
+    """
     h = ring.zero
 
     if isinstance(ring.domain, PolynomialRing):
@@ -1530,6 +1834,71 @@ def _rational_reconstruction_int_coeffs(hm, m, ring):
 
 
 def _func_field_modgcd_m(f, g, minpoly):
+    r"""
+    Compute the GCD of two polynomials in
+    `\mathbb Q(t_1, \ldots, t_k)[z]/(m_{\alpha}(z))[x]` using a modular
+    algorithm.
+
+    The algorithm computes the GCD of two polynomials `f` and `g` by
+    calculating the GCD in
+    `\mathbb Z_p(t_1, \ldots, t_k)[z] / (\check m_{\alpha}(z))[x]` for
+    suitable primes `p` and the primitive associate `\check m_{\alpha}(z)`
+    of `m_{\alpha}(z)`. Then the coefficients are reconstructed with the
+    Chinese Remainder Theorem and Rational Reconstruction. To compute the
+    GCD over `\mathbb Z_p(t_1, \ldots, t_k)[z] / (\check m_{\alpha})[x]`,
+    the recursive subroutine ``_func_field_modgcd_p`` is used. To verify the
+    result in `\mathbb Q(t_1, \ldots, t_k)[z] / (m_{\alpha}(z))[x]`, a
+    fraction free trial division is used.
+
+    Parameters
+    ==========
+
+    f, g : PolyElement
+        polynomials in `\mathbb Z[t_1, \ldots, t_k][x, z]`
+    minpoly : PolyElement
+        irreducible polynomial in `\mathbb Z[t_1, \ldots, t_k][z]`
+
+    Returns
+    =======
+
+    h : PolyElement
+        the primitive associate in `\mathbb Z[t_1, \ldots, t_k][x, z]` of
+        the GCD of `f` and `g`
+
+    Examples
+    ========
+
+    >>> from sympy.polys.modulargcd import _func_field_modgcd_m
+    >>> from sympy.polys import ring, ZZ
+
+    >>> R, x, z = ring('x, z', ZZ)
+    >>> minpoly = (z**2 - 2).drop(0)
+
+    >>> f = x**2 + 2*x*z + 2
+    >>> g = x + z
+    >>> _func_field_modgcd_m(f, g, minpoly)
+    x + z
+
+    >>> D, t = ring('t', ZZ)
+    >>> R, x, z = ring('x, z', D)
+    >>> minpoly = (z**2-3).drop(0)
+
+    >>> f = x**2 + (t + 1)*x*z + 3*t
+    >>> g = x*z + 3*t
+    >>> _func_field_modgcd_m(f, g, minpoly)
+    x + t*z
+
+    References
+    ==========
+
+    1. [Hoeij04]_
+
+    See also
+    ========
+
+    _func_field_modgcd_p
+
+    """
     ring = f.ring
     domain = ring.domain
 
@@ -1621,6 +1990,30 @@ def _func_field_modgcd_m(f, g, minpoly):
 
 
 def _to_ZZ_poly(f, ring):
+    r"""
+    Compute an associate of a polynomial
+    `f \in \mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]` in
+    `\mathbb Z[x_1, \ldots, x_{n-1}][z] / (\check m_{\alpha}(z))[x_0]`,
+    where `\check m_{\alpha}(z) \in \mathbb Z[z]` is the primitive associate
+    of the minimal polynomial `m_{\alpha}(z)` of `\alpha` over
+    `\mathbb Q`.
+
+    Parameters
+    ==========
+
+    f : PolyElement
+        polynomial in `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]`
+    ring : PolyRing
+        `\mathbb Z[x_1, \ldots, x_{n-1}][x_0, z]`
+
+    Returns
+    =======
+
+    f_ : PolyElement
+        associate of `f` in
+        `\mathbb Z[x_1, \ldots, x_{n-1}][x_0, z]`
+
+    """
     f_ = ring.zero
 
     if isinstance(ring.domain, PolynomialRing):
@@ -1655,6 +2048,29 @@ def _to_ZZ_poly(f, ring):
 
 
 def _to_ANP_poly(f, ring):
+    r"""
+    Convert a polynomial
+    `f \in \mathbb Z[x_1, \ldots, x_{n-1}][z]/(\check m_{\alpha}(z))[x_0]`
+    to a polynomial in `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]`,
+    where `\check m_{\alpha}(z) \in \mathbb Z[z]` is the primitive associate
+    of the minimal polynomial `m_{\alpha}(z)` of `\alpha` over
+    `\mathbb Q`.
+
+    Parameters
+    ==========
+
+    f : PolyElement
+        polynomial in `\mathbb Z[x_1, \ldots, x_{n-1}][x_0, z]`
+    ring : PolyRing
+        `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]`
+
+    Returns
+    =======
+
+    f_ : PolyElement
+        polynomial in `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]`
+
+    """
     domain = ring.domain
     f_ = ring.zero
 
@@ -1683,6 +2099,10 @@ def _to_ANP_poly(f, ring):
 
 
 def _minpoly_from_dense(minpoly, ring):
+    r"""
+    Change representation of the minimal polynomial from ``DMP`` to
+    ``PolyElement`` for a given ring.
+    """
     minpoly_ = ring.zero
 
     for monom, coeff in minpoly.terms():
@@ -1692,6 +2112,11 @@ def _minpoly_from_dense(minpoly, ring):
 
 
 def _primitive_in_x0(f):
+    r"""
+    Compute the content in `x_0` and the primitive part of a polynomial `f`
+    in
+    `\mathbb Q(\alpha)[x_0, x_1, \ldots, x_{n-1}] \cong \mathbb Q(\alpha)[x_1, \ldots, x_{n-1}][x_0]`.
+    """
     fring = f.ring
     ring = fring.drop_to_ground(*xrange(1, fring.ngens))
     dom = ring.domain.ring
@@ -1708,6 +2133,109 @@ def _primitive_in_x0(f):
 
 # TODO: add support for algebraic function fields
 def func_field_modgcd(f, g):
+    r"""
+    Compute the GCD of two polynomials `f` and `g` in
+    `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]` using a modular algorithm.
+
+    The algorithm first computes the primitive associate
+    `\check m_{\alpha}(z)` of the minimal polynomial `m_{\alpha}` in
+    `\mathbb{Z}[z]` and the primitive associates of `f` and `g` in
+    `\mathbb{Z}[x_1, \ldots, x_{n-1}][z]/(\check m_{\alpha})[x_0]`. Then it
+    computes the GCD in
+    `\mathbb Q(x_1, \ldots, x_{n-1})[z]/(m_{\alpha}(z))[x_0]`.
+    This is done by calculating the GCD in
+    `\mathbb{Z}_p(x_1, \ldots, x_{n-1})[z]/(\check m_{\alpha}(z))[x_0]` for
+    suitable primes `p` and then reconstructing the coefficients with the
+    Chinese Remainder Theorem and Rational Reconstuction. The GCD over
+    `\mathbb{Z}_p(x_1, \ldots, x_{n-1})[z]/(\check m_{\alpha}(z))[x_0]` is
+    computed with a recursive subroutine, which evaluates the polynomials at
+    `x_{n-1} = a` for suitable evaluation points `a \in \mathbb Z_p` and
+    then calls itself recursively until the ground domain does no longer
+    contain any parameters. For
+    `\mathbb{Z}_p[z]/(\check m_{\alpha}(z))[x_0]` the Euclidean Algorithm is
+    used. The results of those recursive calls are then interpolated and
+    Rational Function Reconstruction is used to obtain the correct
+    coefficients. The results, both in
+    `\mathbb Q(x_1, \ldots, x_{n-1})[z]/(m_{\alpha}(z))[x_0]` and
+    `\mathbb{Z}_p(x_1, \ldots, x_{n-1})[z]/(\check m_{\alpha}(z))[x_0]`, are
+    verified by a fraction free trial division.
+
+    Apart from the above GCD computation some GCDs in
+    `\mathbb Q(\alpha)[x_1, \ldots, x_{n-1}]` have to be calculated,
+    because treating the polynomials as univariate ones can result in
+    a spurious content of the GCD. For this ``func_field_modgcd`` is
+    called recursively.
+
+    Parameters
+    ==========
+
+    f, g : PolyElement
+        polynomials in `\mathbb Q(\alpha)[x_0, \ldots, x_{n-1}]`
+
+    Returns
+    =======
+
+    h : PolyElement
+        monic GCD of the polynomials `f` and `g`
+    cff : PolyElement
+        cofactor of `f`, i.e. `\frac f h`
+    cfg : PolyElement
+        cofactor of `g`, i.e. `\frac g h`
+
+    Examples
+    ========
+
+    >>> from sympy.polys.modulargcd import func_field_modgcd
+    >>> from sympy.polys import AlgebraicField, QQ, ring
+    >>> from sympy import sqrt
+
+    >>> A = AlgebraicField(QQ, sqrt(2))
+    >>> R, x = ring('x', A)
+
+    >>> f = x**2 - 2
+    >>> g = x + sqrt(2)
+
+    >>> h, cff, cfg = func_field_modgcd(f, g)
+
+    >>> h == x + sqrt(2)
+    True
+    >>> cff * h == f
+    True
+    >>> cfg * h == g
+    True
+
+    >>> R, x, y = ring('x, y', A)
+
+    >>> f = x**2 + 2*sqrt(2)*x*y + 2*y**2
+    >>> g = x + sqrt(2)*y
+
+    >>> h, cff, cfg = func_field_modgcd(f, g)
+
+    >>> h == x + sqrt(2)*y
+    True
+    >>> cff * h == f
+    True
+    >>> cfg * h == g
+    True
+
+    >>> f = x + sqrt(2)*y
+    >>> g = x + y
+
+    >>> h, cff, cfg = func_field_modgcd(f, g)
+
+    >>> h == R.one
+    True
+    >>> cff * h == f
+    True
+    >>> cfg * h == g
+    True
+
+    References
+    ==========
+
+    1. [Hoeij04]_
+
+    """
     ring = f.ring
     domain = ring.domain
     n = ring.ngens
