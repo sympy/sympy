adbd7b492b5adb8cbf0fe533160e3bf8bef34b03
David P. Sanders <dpsanders@gmail.com>
2013-08-11 09:03:08 -0500

    Modified latex printing of Lambda to use \mapsto

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -600,7 +600,7 @@ class LatexPrinter(Printer):
             symbols = self._print(tuple(symbols))
 
         args = (symbols, self._print(expr))
-        tex = r"\Lambda {\left (%s \right )}" % ", ".join(args)
+        tex = r"%s \mapsto %s" % (symbols, self._print(expr))
 
         return tex
 
