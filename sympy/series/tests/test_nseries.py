21205a04e9cd410eef414ccfd3e9c090fe7ed8e7
Sergey B Kirpichev <skirpichev@gmail.com>
2013-08-17 23:52:32 +0400

    Fix and UnXFAIL tests in test_expbug4_failing(); see PR #757

--- sympy/series/tests/test_nseries.py
+++ sympy/series/tests/test_nseries.py
@@ -238,12 +238,8 @@ def test_expbug4():
     assert exp(
         log(sin(2*x)/x)*(1 + x)).series(x, 0, 2) == 2 + 2*x*log(2) + O(x**2)
 
-
-@XFAIL
-def test_expbug4_failing():
-    x = Symbol("x", real=True)
-    assert exp(log(2) + O(x)).nseries(x, 0, 2) == 2 + O(x**2, x)
-    assert ((2 + O(x))**(1 + x)).nseries(x, 0, 2) == 2 + O(x**2, x)
+    assert exp(log(2) + O(x)).nseries(x, 0, 2) == 2 + O(x)
+    assert ((2 + O(x))**(1 + x)).nseries(x, 0, 2) == 2 + O(x)
 
 
 def test_logbug4():
