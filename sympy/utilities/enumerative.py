91893de72c7bc4160fd8121348dc4346e7620c18
rathmann <rathmann.os@gmail.com>
2013-08-17 20:55:05 -0700

    More 3/2 adustments: generators don't have .next() in python3, use next(obj) instead.
    	modified:   sympy/utilities/enumerative.py

--- sympy/utilities/enumerative.py
+++ sympy/utilities/enumerative.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 from sympy.core.compatibility import xrange
 
 #
@@ -344,10 +344,10 @@ def list_visitor(state, components):
     >>> from sympy.utilities.enumerative import list_visitor
     >>> from sympy.utilities.enumerative import multiset_partitions_taocp
     >>> states = multiset_partitions_taocp([1, 2, 1])
-    >>> s = states.next()
+    >>> s = next(states)
     >>> list_visitor(s, 'abc')  # for multiset 'a b b c'
     [['a', 'b', 'b', 'c']]
-    >>> s = states.next()
+    >>> s = next(states)
     >>> list_visitor(s, [1, 2, 3])  # for multiset '1 2 2 3
     [[1, 2, 2], [3]]
     """
