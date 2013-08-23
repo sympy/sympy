716badfaba922c0d94624e81cd6ad022d27ef84c
Jason K. Moore <moorepants@gmail.com>
2013-08-23 09:44:19 -0400

    Calling python instead of the test script.

--- bin/test_buildbot.sh
+++ bin/test_buildbot.sh
@@ -3,16 +3,33 @@
 # We are using the NiPy buildbot to run our time intensive tests: with all
 # optional dependencies, without caching, and tests marked as slow.
 
-DIR=`dirname $0`
+# Exit on error
+set -e
+# Echo each command
+set -x
 
 # run all tests (all install dependencies should be installed)
-$DIR/test
+python - <<'EOF'
+import sympy
+if not sympy.test():
+    raise Exception('Tests failed')
+EOF
+# run only the slow tests, time out after 30 minutes
+python - <<'EOF'
+import sympy
+if not sympy.test(slow=True, timeout=1800):
+    raise Exception('Tests failed')
+EOF
 
 # run all the tests without the cache
-$DIR/test --no-cache
-
-# run only the slow tests, time out after 30 minutes
-$DIR/test --slow --timeout=1800
-
-# run slow tests without the cache?
-#test --no-cache --slow --timeout=1800
+export SYMPY_USE_CACHE=no
+python - <<'EOF'
+import sympy
+if not sympy.test():
+    raise Exception('Tests failed')
+EOF
+python - <<'EOF'
+import sympy
+if not sympy.test(slow=True, timeout=1800):
+    raise Exception('Tests failed')
+EOF
