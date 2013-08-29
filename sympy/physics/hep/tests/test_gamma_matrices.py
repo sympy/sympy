f4d0612dc6bd277da1e5bd805df463e5be6cb42e
Francesco Bonazzi <franz.bonazzi@gmail.com>
2013-08-29 20:19:01 +0200

    test cases now have dimensional parameter, so they can later be re-used for dimensional regularization tests.

--- sympy/physics/hep/tests/test_gamma_matrices.py
+++ sympy/physics/hep/tests/test_gamma_matrices.py
@@ -16,66 +16,70 @@ def test_kahane_algorithm():
     # kahane_simplify only works in four dimensions:
     D = 4
 
-    t = (G(a1)*G(mu11)*G(a2)*G(mu21)*G(-a1)*G(mu31)*G(-a2))
-    assert kahane_simplify(t) == -4*G(mu11)*G(mu31)*G(mu21) - 4*G(mu31)*G(mu11)*G(mu21)
+    # Some examples taken from Kahane's paper, 4 dim only:
+    if D == 4:
+        t = (G(a1)*G(mu11)*G(a2)*G(mu21)*G(-a1)*G(mu31)*G(-a2))
+        assert kahane_simplify(t) == -4*G(mu11)*G(mu31)*G(mu21) - 4*G(mu31)*G(mu11)*G(mu21)
+
+        t = (G(a1)*G(mu11)*G(mu12)*\
+                              G(a2)*G(mu21)*\
+                              G(a3)*G(mu31)*G(mu32)*\
+                              G(a4)*G(mu41)*\
+                              G(-a2)*G(mu51)*G(mu52)*\
+                              G(-a1)*G(mu61)*\
+                              G(-a3)*G(mu71)*G(mu72)*\
+                              G(-a4))
+        assert kahane_simplify(t) == \
+            16*G(mu31)*G(mu32)*G(mu72)*G(mu71)*G(mu11)*G(mu52)*G(mu51)*G(mu12)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu31)*G(mu32)*G(mu72)*G(mu71)*G(mu12)*G(mu51)*G(mu52)*G(mu11)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu71)*G(mu72)*G(mu32)*G(mu31)*G(mu11)*G(mu52)*G(mu51)*G(mu12)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu71)*G(mu72)*G(mu32)*G(mu31)*G(mu12)*G(mu51)*G(mu52)*G(mu11)*G(mu61)*G(mu21)*G(mu41)
+
+    # Fully Lorentz-contracted expressions, these return scalars:
 
     t = (G(mu)*G(-mu))
-    assert kahane_simplify(t) == 4
+    assert kahane_simplify(t) == D
 
     t = (G(mu)*G(nu)*G(-mu)*G(-nu))
-    assert kahane_simplify(t) == -8
+    assert kahane_simplify(t) == 2*D - D**2  # -8
 
     t = (G(mu)*G(nu)*G(-nu)*G(-mu))
-    assert kahane_simplify(t) == 16
+    assert kahane_simplify(t) == D**2  # 16
 
     t = (G(mu)*G(nu)*G(-rho)*G(-nu)*G(-mu)*G(rho))
-    assert kahane_simplify(t) == 16
+    assert kahane_simplify(t) == 4*D - 4*D**2 + D**3  # 16
 
     t = (G(mu)*G(nu)*G(rho)*G(-rho)*G(-nu)*G(-mu))
-    assert kahane_simplify(t) == 64
-
-    t = (G(a1)*G(mu11)*G(mu12)*\
-                          G(a2)*G(mu21)*\
-                          G(a3)*G(mu31)*G(mu32)*\
-                          G(a4)*G(mu41)*\
-                          G(-a2)*G(mu51)*G(mu52)*\
-                          G(-a1)*G(mu61)*\
-                          G(-a3)*G(mu71)*G(mu72)*\
-                          G(-a4))
-    assert kahane_simplify(t) == \
-        16*G(mu31)*G(mu32)*G(mu72)*G(mu71)*G(mu11)*G(mu52)*G(mu51)*G(mu12)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu31)*G(mu32)*G(mu72)*G(mu71)*G(mu12)*G(mu51)*G(mu52)*G(mu11)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu71)*G(mu72)*G(mu32)*G(mu31)*G(mu11)*G(mu52)*G(mu51)*G(mu12)*G(mu61)*G(mu21)*G(mu41) + 16*G(mu71)*G(mu72)*G(mu32)*G(mu31)*G(mu12)*G(mu51)*G(mu52)*G(mu11)*G(mu61)*G(mu21)*G(mu41)
-
-    t = (G(mu)*G(nu)*G(rho)*G(sigma)*G(-mu))
-    assert kahane_simplify(t) == -2*G(sigma)*G(rho)*G(nu)
-
-    t = (G(mu)*G(nu)*G(-mu))
-    assert kahane_simplify (t) == -2*G(nu)
-
-    t = (G(mu)*G(nu)*G(rho)*G(-mu))
-    assert kahane_simplify(t) == 2*G(nu)*G(rho) + 2*G(rho)*G(nu)
+    assert kahane_simplify(t) == D**3  # 64
 
     t = (G(a1)*G(a2)*G(a3)*G(a4)*G(-a3)*G(-a1)*G(-a2)*G(-a4))
-    assert kahane_simplify(t) == -32
+    assert kahane_simplify(t) == -8*D + 16*D**2 - 8*D**3 + D**4  # -32
 
     t = (G(-mu)*G(-nu)*G(-rho)*G(-sigma)*G(nu)*G(mu)*G(sigma)*G(rho))
-    assert kahane_simplify(t) == 64
+    assert kahane_simplify(t) == -16*D + 24*D**2 - 8*D**3 + D**4  # 64
 
     t = (G(-mu)*G(nu)*G(-rho)*G(sigma)*G(rho)*G(-nu)*G(mu)*G(-sigma))
-    assert kahane_simplify(t) == -32
+    assert kahane_simplify(t) == 8*D - 12*D**2 + 6*D**3 - D**4  # -32
 
     t = (G(a1)*G(a2)*G(a3)*G(a4)*G(a5)*G(-a3)*G(-a2)*G(-a1)*G(-a5)*G(-a4))
-    assert kahane_simplify(t) == 256
+    assert kahane_simplify(t) == 64*D - 112*D**2 + 60*D**3 - 12*D**4 + D**5  # 256
 
     t = (G(a1)*G(a2)*G(a3)*G(a4)*G(a5)*G(-a3)*G(-a1)*G(-a2)*G(-a4)*G(-a5))
-    assert kahane_simplify(t) == -128
+    assert kahane_simplify(t) == 64*D - 120*D**2 + 72*D**3 - 16*D**4 + D**5  # -128
 
     t = (G(a1)*G(a2)*G(a3)*G(a4)*G(a5)*G(a6)*G(-a3)*G(-a2)*G(-a1)*G(-a6)*G(-a5)*G(-a4))
-    assert kahane_simplify(t) == -128
+    assert kahane_simplify(t) == 416*D - 816*D**2 + 528*D**3 - 144*D**4 + 18*D**5 - D**6  # -128
 
     t = (G(a1)*G(a2)*G(a3)*G(a4)*G(a5)*G(a6)*G(-a2)*G(-a3)*G(-a1)*G(-a6)*G(-a4)*G(-a5))
-    assert kahane_simplify(t) == -128
+    assert kahane_simplify(t) == 416*D - 848*D**2 + 584*D**3 - 172*D**4 + 22*D**5 - D**6  # -128
 
-    # more complex expressions:
+    # Expressions with free indices:
+
+    t = (G(mu)*G(nu)*G(rho)*G(sigma)*G(-mu))
+    assert kahane_simplify(t).equals(-2*G(sigma)*G(rho)*G(nu) + (4-D)*G(nu)*G(rho)*G(sigma))
+
+    t = (G(mu)*G(nu)*G(-mu))
+    assert kahane_simplify (t) == (2-D)*G(nu)
+
+    t = (G(mu)*G(nu)*G(rho)*G(-mu))
+    assert kahane_simplify(t) == 2*G(nu)*G(rho) + 2*G(rho)*G(nu) - (4-D)*G(nu)*G(rho)
 
     t = 2*G(m2)*G(m0)*G(m1)*G(-m0)*G(-m1)
     st = kahane_simplify(t)
@@ -130,7 +134,7 @@ def test_kahane_algorithm():
     else:
         assert st.equals(result1)
 
-    # and a few very simple cases:
+    # and a few very simple cases, with no contracted indices:
 
     t = G(m0)
     st = kahane_simplify(t)
