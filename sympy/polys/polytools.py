08c35834f02700cd018bcb9f34162f3dd377525c
akritas <akritas@uth.gr>
2013-08-31 11:48:57 +0300

    refs to intervals

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -2939,7 +2939,17 @@ class Poly(Expr):
 
     def intervals(f, all=False, eps=None, inf=None, sup=None, fast=False, sqf=False):
         """
-        Compute isolating intervals for roots of ``f``.
+        Compute isolating intervals for roots of ``f``. 
+
+        For real roots the Vincent-Akritas-Strzebonski (VAS) continued fractions method is used.
+
+        References:
+        ===========
+           1. Alkiviadis G. Akritas and Adam W. Strzebonski: A Comparative Study of Two Real Root
+           Isolation Methods . Nonlinear Analysis: Modelling and Control, Vol. 10, No. 4, 297-304, 2005.
+           2. Alkiviadis G. Akritas, Adam W. Strzebonski and Panagiotis S. Vigklas: Improving the
+           Performance of the Continued Fractions Method Using new Bounds of Positive Roots. Nonlinear
+           Analysis: Modelling and Control, Vol. 13, No. 3, 265-279, 2008.
 
         Examples
         ========
