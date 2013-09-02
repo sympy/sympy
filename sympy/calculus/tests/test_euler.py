7de03edfc700072ce560e6da31aaaae697b52cf6
Sergey B Kirpichev <skirpichev@gmail.com>
2013-09-02 10:29:27 +0400

    Add tests

---
+++ sympy/calculus/tests/test_euler.py
@@ -0,0 +1,30 @@
+from sympy import (Symbol, Function, Derivative, Eq, cos, sin)
+from sympy.calculus.euler import euler_equations
+
+def test_euler_pendulum():
+    x = Function('x')
+    t = Symbol('t')
+    L = (x(t).diff(t))**2/2 + cos(x(t))
+    assert euler_equations(L, x(t), t) == \
+        set([Eq(-sin(x(t)) - Derivative(x(t), t, t), 0)])
+
+
+def test_euler_henonheiles():
+    x = Function('x')
+    y = Function('y')
+    t = Symbol('t')
+    L = sum((z(t).diff(t))**2/2 - z(t)**2/2 for z in [x, y])
+    L += -x(t)**2*y(t) + y(t)**3/3
+    assert euler_equations(L, [x(t), y(t)], t) == \
+        set([Eq(-x(t)**2 + y(t)**2 - y(t) - Derivative(y(t), t, t), 0),
+             Eq(-2*x(t)*y(t) - x(t) - Derivative(x(t), t, t), 0)])
+
+
+def test_euler_sineg():
+    psi = Function('psi')
+    t = Symbol('t')
+    x = Symbol('x')
+    L = (psi(t, x).diff(t))**2/2 - (psi(t, x).diff(x))**2/2 + cos(psi(t, x))
+    assert euler_equations(L, psi(t, x), [t, x]) == \
+        set([Eq(-sin(psi(t, x)) - Derivative(psi(t, x), t, t) + \
+             Derivative(psi(t, x), x, x), 0)])
