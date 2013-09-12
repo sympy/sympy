fcaab7524994033c79aca3795cf237d38b1bfefe
srjoglekar246 <srjoglekar246@gmail.com>
2013-09-13 01:33:24 +0530

    Added tests

--- sympy/physics/mechanics/tests/test_functions.py
+++ sympy/physics/mechanics/tests/test_functions.py
@@ -6,7 +6,8 @@ from sympy.physics.mechanics import (angular_momentum, cross, dot,
                                      inertia_of_point_mass,
                                      kinematic_equations, kinetic_energy,
                                      linear_momentum, outer, partial_velocity,
-                                     potential_energy)
+                                     potential_energy, get_motion_pos,
+                                     get_motion_vel, get_motion_acc)
 from sympy.utilities.pytest import raises
 
 Vector.simp = True
@@ -331,6 +332,60 @@ def test_express():
             cos(q2)*cos(q3)*A.z), C)
     assert C.x == express((cos(q3)*B.x - sin(q3)*B.z), C)
     assert C.z == express((sin(q3)*B.x + cos(q3)*B.z), C)
+
+
+def test_get_motion_methods():
+    #Initialization
+    F = ReferenceFrame('F')
+    t = dynamicsymbols._t
+    s1, s2, s3 = symbols('s1 s2 s3')
+    S1, S2, S3 = symbols('S1 S2 S3')
+    S4, S5, S6 = symbols('S4 S5 S6')
+    t1, t2 = symbols('t1 t2')
+    a, b, c = dynamicsymbols('a b c')
+    ad, bd, cd = dynamicsymbols('a b c', 1)
+    a2d, b2d, c2d = dynamicsymbols('a b c', 2)
+    v0 = S1*F.x + S2*F.y + S3*F.z
+    v01 = S4*F.x + S5*F.y + S6*F.z
+    v1 = s1*F.x + s2*F.y + s3*F.z
+    v2 = a*F.x + b*F.y + c*F.z
+    v2d = ad*F.x + bd*F.y + cd*F.z
+    v2dd = a2d*F.x + b2d*F.y + c2d*F.z
+    #Test get_motion_pos
+    assert get_motion_pos(0, F) == [0, 0, 0]
+    assert get_motion_pos(v1, F) == [0, 0, v1]
+    assert get_motion_pos(v2, F) == [v2dd, v2d, v2]
+    #Test get_motion_vel
+    assert get_motion_vel(frame = F) == [0, 0, 0]
+    assert get_motion_vel(v1, frame = F) == [0, v1, v1 * t]
+    assert get_motion_vel(v1, v0, t1, F) == [0, v1, v0 + v1*(t - t1)]
+    assert get_motion_vel(v1, v2, t1, F) == \
+           [0, v1, v1*t - v1*t1 + v2.subs(t, t1)]
+    assert get_motion_vel(v2, v0, t1, F) == \
+           [v2d, v2,
+            (S1 + Integral(a, t) - Integral(a, t))*F.x + \
+            (S2 + Integral(b, t) - Integral(b, t))*F.y +
+            (S3 + Integral(c, t) - Integral(c, t))*F.z]
+    #Test get_motion_acc
+    assert get_motion_acc(frame = F) == [0, 0, 0]
+    assert get_motion_acc(v1, frame = F) == [v1, v1 * t, v1 * t**2/2]
+    assert get_motion_acc(v1, v0, v2, t1, t2, F) == \
+           [v1, (v0 + v1*t - v1*t1),
+            -v0*t2 + v1*t**2/2 + v1*t1*t2 - \
+            v1*t2**2/2 + t*(v0 - v1*t1) + \
+            v2.subs(t, t2)]
+    assert get_motion_acc(v1, v0, v01, t1, t2, F) == \
+           [v1, v0 + v1*t - v1*t1,
+            -v0*t2 + v01 + v1*t**2/2 + \
+            v1*t1*t2 - v1*t2**2/2 + \
+            t*(v0 - v1*t1)]
+    assert get_motion_acc(a*F.x, S1*F.x,
+                          S2*F.x, t1, t2, F) == [a*F.x, (S1 + Integral(a, t) - \
+                                                         Integral(a, t))*F.x,
+                                                 (S2 + Integral(S1 + Integral(a, t) -\
+                                                                Integral(a, t), t) -\
+                                                  Integral(S1 - Integral(a, t) + \
+                                                           Integral(a, t), t))*F.x]
 
 
 def test_inertia():
