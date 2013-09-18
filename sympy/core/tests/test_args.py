69309d24e197e4760370953a38fcd72a55d2ceca
Mary Clark <meclark256@gmail.com>
2013-09-18 22:03:18 +0100

    Fixing a typo in test_args

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1668,7 +1668,7 @@ def test_sympy__liealgebras__cartan_type__Standard_Cartan():
     assert _test_args(Standard_Cartan("A", 2))
 
 @XFAIL
-def test_sympy__Liealgebras__weyl_group__WeylGroup():
+def test_sympy__liealgebras__weyl_group__WeylGroup():
     from sympy.liealgebras.weyl_group import WeylGroup
     assert _test_args(WeylGroup("B4"))
 
