92b3c788bbe16c22f17687bd18cab42c6cc762cd
Sergey B Kirpichev <skirpichev@gmail.com>
2013-10-04 14:58:54 +0400

    Add test for issue 3400

--- sympy/series/tests/test_residues.py
+++ sympy/series/tests/test_residues.py
@@ -64,3 +64,7 @@ def test_NotImplemented():
 def test_bug():
     assert residue(2**(z)*(s + z)*(1 - s - z)/z**2, z, 0) == \
         1 + s*log(2) - s**2*log(2) - 2*s
+
+
+def test_issue_3400():
+    assert residue(1/(exp(z) - 1), z, 0) == 1
