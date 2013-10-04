be4e471f8136aa5f53628302c2e5d60d2f15d777
Sergey B Kirpichev <skirpichev@gmail.com>
2013-10-04 16:12:01 +0400

    Add test for issue 2555

--- sympy/series/tests/test_residues.py
+++ sympy/series/tests/test_residues.py
@@ -64,3 +64,7 @@ def test_NotImplemented():
 def test_bug():
     assert residue(2**(z)*(s + z)*(1 - s - z)/z**2, z, 0) == \
         1 + s*log(2) - s**2*log(2) - 2*s
+
+
+def test_issue_2555():
+    assert residue(1/(x**2 + a**2)**2, x, a*I) == -I/(4*a**3)
