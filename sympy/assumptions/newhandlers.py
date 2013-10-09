6a2d1db539c5fbe3305767c0fb5c856e45d41e9d
Aaron Meurer <asmeurer@gmail.com>
2013-10-08 20:09:00 -0600

    Add a start to a handler system for newask
    I haven't integrated it into newask yet.

---
+++ sympy/assumptions/newhandlers.py
@@ -0,0 +1,105 @@
+from __future__ import print_function, division
+
+from collections import MutableMapping, defaultdict
+
+from sympy.core import Add, Mul
+from sympy.matrices.expressions import MatMul
+
+from sympy.assumptions.ask import Q
+from sympy.logic.boolalg import Equivalent, Implies, And, Or
+
+# APIs here may be subject to change
+
+class Handler(object):
+    def get_relevant_fact(self, key):
+        return True
+
+class ArgHandler(Handler):
+    """
+    A handler relationships among args
+
+    This handles cases when there is some relationship between
+    Q.assumption(expr) and Q.assumption(arg) for arg in expr.args.
+
+    Subclasses should override the get_relationship method.
+    """
+
+    def __init__(self, predicate):
+        self.predicate = predicate
+
+    def get_relevant_fact(self, key):
+        expr = key.args[0]
+        if key.func == self.predicate: # TODO: isinstance doesn't work here
+            return self.get_relationship(key, map(self.predicate, expr.args))
+        return True
+
+class EquivalentAnyArgs(ArgHandler):
+    """
+    Q.assumption(expr) iff any(Q.assumption(arg) for arg in expr.args)
+    """
+    def get_relationship(self, key, mapped_args):
+        return Equivalent(key, Or(*mapped_args))
+
+class EquivalentAllArgs(ArgHandler):
+    """
+    Q.assumption(expr) iff all(Q.assumption(arg) for arg in expr.args)
+    """
+    def get_relationship(self, key, mapped_args):
+        return Equivalent(key, And(*mapped_args))
+
+class AllArgsImplies(ArgHandler):
+    """
+    all(Q.assumption(arg) for arg in expr.args) implies Q.assumption(expr)
+    (but the reverse implication does not hold)
+    """
+    def get_relationship(self, key, mapped_args):
+        return Implies(And(*mapped_args), key)
+
+# TODO: Create a handler registry system
+
+class class_handler_registry(MutableMapping):
+    """
+    Register handlers against classes
+
+    ``registry[C] = handler`` registers ``handler`` for class
+    ``C``. ``registry[C]`` returns a set of handlers for class ``C``, or any
+    of its superclasses.
+    """
+    def __init__(self):
+        self.d = defaultdict(set)
+        super(class_handler_registry, self).__init__()
+
+    def __setitem__(self, key, item):
+        self.d[key].add(item)
+
+    def __getitem__(self, key):
+        ret = set()
+        for k in self.d:
+            if issubclass(key, k):
+                ret.update(self.d[k])
+        return ret
+
+    def __delitem__(self, key):
+        del self.d[key]
+
+    def __iter__(self):
+        return self.d.__iter__()
+
+    def __len__(self):
+        return len(self.d)
+
+    def __repr__(self):
+        return str(self.d)
+
+handler_registry = class_handler_registry()
+
+def register_handler(klass, handler, registry=handler_registry):
+    registry[klass].add(handler)
+
+for handler, key, klass in [
+    (EquivalentAnyArgs, Q.zero, Mul),
+    (EquivalentAllArgs, Q.invertible, MatMul),
+    (AllArgsImplies, Q.positive, Add),
+    ]:
+
+    register_handler(klass, handler(key))
---
+++ sympy/assumptions/tests/test_newhandlers.py
@@ -0,0 +1,48 @@
+from sympy import Mul, Basic, Q, Expr, And, symbols, Equivalent, Implies, Or
+
+from sympy.assumptions.newhandlers import (handler_registry, AllArgsImplies,
+    EquivalentAnyArgs, EquivalentAllArgs, class_handler_registry, ArgHandler)
+
+x, y = symbols('x y')
+
+def test_class_handler_registry():
+    my_handler_registry = class_handler_registry()
+
+    # The predicate doesn't matter here, so just use is_true
+    all_args_implies_is_true = AllArgsImplies(Q.is_true)
+    equiv_all_args_is_true = EquivalentAllArgs(Q.is_true)
+
+    my_handler_registry[Mul] = all_args_implies_is_true
+    my_handler_registry[Expr] = equiv_all_args_is_true
+
+    assert my_handler_registry[Basic] == set()
+    assert my_handler_registry[Expr] == set([equiv_all_args_is_true])
+    assert my_handler_registry[Mul] == set([equiv_all_args_is_true, all_args_implies_is_true])
+
+def test_ArgHandler():
+    class AndArgHandler(ArgHandler):
+        def get_relationship(self, key, mapped_args):
+            return And(key, *mapped_args)
+
+    handler = AndArgHandler(Q.zero)
+    assert handler.get_relevant_fact(Q.zero(x*y)) == And(Q.zero(x*y),
+    Q.zero(x), Q.zero(y))
+    assert handler.get_relevant_fact(Q.positive(x*y)) == True
+
+def test_EquivalentAllArgs():
+    handler = EquivalentAllArgs(Q.invertible)
+    assert handler.get_relevant_fact(Q.invertible(x*y)) == Equivalent(Q.invertible(x*y),
+    And(Q.invertible(x), Q.invertible(y)))
+    assert handler.get_relevant_fact(Q.zero(x*y)) == True
+
+def test_EquivalentAnyArgs():
+    handler = EquivalentAnyArgs(Q.zero)
+    assert handler.get_relevant_fact(Q.zero(x*y)) == Equivalent(Q.zero(x*y),
+    Or(Q.zero(x), Q.zero(y)))
+    assert handler.get_relevant_fact(Q.integer(x*y)) == True
+
+def test_AllArgsImplies():
+    handler = AllArgsImplies(Q.positive)
+    assert handler.get_relevant_fact(Q.positive(x + y)) == \
+    Implies(And(Q.positive(x), Q.positive(y)), Q.positive(x + y))
+    assert handler.get_relevant_fact(Q.zero(x + y)) == True
