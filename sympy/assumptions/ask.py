da75224551226e4951d6c60c92fd1a769b1146c8
Aaron Meurer <asmeurer@gmail.com>
2013-10-18 13:23:39 -0600

    Add a docstring for Q.positive

--- sympy/assumptions/ask.py
+++ sympy/assumptions/ask.py
@@ -173,7 +173,41 @@ class Q:
     True
 
     """)
-    positive = Predicate('positive')
+    positive = Predicate('positive', doc=r"""
+    Positive real number predicate.
+
+    ``Q.positive(x)`` is true iff ``x`` is real and `x > 0`, that is if ``x``
+    is in the interval `(0, \infty)`.  In particular, infinity is not
+    positive.
+
+    A few important facts about negative numbers:
+
+    - Note that ``Q.nonpositive`` and ``~Q.positive`` are *not* the same
+      thing. ``~Q.positive(x)`` simply means that ``x`` is not positive,
+      whereas ``Q.nonpositive(x)`` means that ``x`` is real and not
+      positive, i.e., ``Q.positive(x)`` is logically equivalent to
+      ``Q.negative(x) | Q.zero(x)``.  So for example, ``~Q.positive(I)`` is
+      true, whereas ``Q.nonpositive(I)`` is false.
+
+    - See the docstring of ``Q.real`` for more information about related facts.
+
+    Example
+    =======
+
+    >>> from sympy import Q, ask, symbols, I
+    >>> x = symbols('x')
+    >>> ask(Q.positive(x), Q.real(x) & ~Q.negative(x) & ~Q.zero(x))
+    True
+    >>> ask(Q.positive(1))
+    True
+
+    >>> ask(Q.nonpositive(I))
+    False
+    >>> ask(~Q.positive(I))
+    True
+
+
+    """)
     nonpositive = Predicate('nonpositive')
     nonnegative = Predicate('nonnegative')
     zero = Predicate('zero')
