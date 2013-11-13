f5df50c2fdd0ef214b0c9928f7c9433dabf93e01
Sergey B Kirpichev <skirpichev@gmail.com>
2013-11-13 11:34:22 +0400

    Revert cbrt printing

--- sympy/printing/str.py
+++ sympy/printing/str.py
@@ -473,8 +473,6 @@ class StrPrinter(Printer):
 
         if expr.exp is S.Half and not rational:
             return "sqrt(%s)" % self._print(expr.base)
-        elif expr.exp == Rational(1, 3) and not rational:
-            return "cbrt(%s)" % self._print(expr.base)
 
         if expr.is_commutative:
             if -expr.exp is S.Half and not rational:
