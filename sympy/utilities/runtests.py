sympy/sympy

commit 68ddfe80195811b8a1098e1945b28f959c41db45

Blacklist numeric-computation.rst tests if numpy or theano modules are not imported

sympy/utilities/runtests.py
@@ -564,7 +564,8 @@ def _doctest(*paths, **kwargs):
             "examples/advanced/autowrap_ufuncify.py",
             "examples/intermediate/sample.py",
             "examples/intermediate/mplot2d.py",
-            "examples/intermediate/mplot3d.py"
+            "examples/intermediate/mplot3d.py",
+            "doc/src/modules/numeric-computation.rst"
         ])
     else:
         if import_module('matplotlib') is None:
@@ -579,6 +580,9 @@ def _doctest(*paths, **kwargs):
 
     if import_module('pyglet') is None:
         blacklist.extend(["sympy/plotting/pygletplot"])
+
+    if import_module('theano') is None:
+        blacklist.extend("doc/src/modules/numeric-computation.rst")
 
     # disabled because of doctest failures in asmeurer's bot
     blacklist.extend([

commit b9bcae5a9561c23e1ff070f6c9feefadc8da537f

Fix doctest blacklist, broken by 68ddfe8

sympy/utilities/runtests.py
@@ -605,7 +605,7 @@ def _doctest(*paths, **kwargs):
         blacklist.extend(["sympy/plotting/pygletplot"])
 
     if import_module('theano') is None:
-        blacklist.extend("doc/src/modules/numeric-computation.rst")
+        blacklist.extend(["doc/src/modules/numeric-computation.rst"])
 
     # disabled because of doctest failures in asmeurer's bot
     blacklist.extend([