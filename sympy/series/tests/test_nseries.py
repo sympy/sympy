7c49f3629b276e381acb4010f3c8b5535864617c
Sergey B Kirpichev <skirpichev@gmail.com>
2013-11-29 17:27:32 +0400

    Fix test_issue_5654

--- sympy/series/tests/test_nseries.py
+++ sympy/series/tests/test_nseries.py
@@ -502,9 +502,9 @@ def test_issue_5183():
 
 def test_issue_5654():
     a = Symbol('a')
-    assert (1/(x**2+a**2)**2).nseries(x, x0=I*a, n=0) == \
+    assert (1/(x**2+a**2)**2).subs(x, x + I*a).nseries(x, n=0) == \
         -I/(4*a**3*x) - 1/(4*a**2*x**2) + O(1, x)
-    assert (1/(x**2+a**2)**2).nseries(x, x0=I*a, n=1) == \
+    assert (1/(x**2+a**2)**2).subs(x, x + I*a).nseries(x, n=1) == \
         3/(16*a**4) - I/(4*a**3*x) - 1/(4*a**2*x**2) + O(x)
 
 
