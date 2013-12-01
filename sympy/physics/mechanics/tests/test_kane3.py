b6ff9c943d1f785468d996f64e2842e29f9c48e6
Julien Rioux <julien.rioux@gmail.com>
2013-12-01 22:03:12 +0100

    Fix slow tests in python3.

--- sympy/physics/mechanics/tests/test_kane3.py
+++ sympy/physics/mechanics/tests/test_kane3.py
@@ -284,5 +284,4 @@ def test_bicycle():
     for i in range(6):
         eps = 1.e-12
         error = Res.subs(v, i) - A.subs(v, i)
-        to_test = error.applyfunc(lambda x: x < eps)
-        assert min(to_test)
+        assert all(bool(abs(x) < eps) for x in error)
