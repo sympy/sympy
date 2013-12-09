b0c947850ea8060ee9ac35b7523cb03508402c4f
Frifon <artem200@yandex.ru>
2013-12-09 10:10:10 -0500

    Test

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-from sympy import Poly, roots, Symbol
+# from sympy import Poly, roots, Symbol
 from sympy.utilities.solution import reset_solution, last_solution
+from sympy import symbols
+from sympy.solvers.solvers import solve
+from sympy.polys import Poly
 
-x = Symbol('x')
 reset_solution()
-print roots(Poly(6 * x ** 3 - 11 * x ** 2 - 2 * x + 8, x))
+x, y = symbols('x,y')
+# print solve([x + 5*y - 2, -3*x + 6*y - 15], [x, y])
+print solve(Poly((x - 2) * (x**2 + 3) * (x - 6), x))
 R = last_solution()
 for i in R:
 	print i
