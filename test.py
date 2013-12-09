6cf20d2bab43c043a5ea6feea1c5b2f5fb7cd567
Frifon <artem200@yandex.ru>
2013-12-09 10:18:51 -0500

    Refresh all

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from sympy.polys import Poly
 
 reset_solution()
 x, y = symbols('x,y')
-# print solve([x + 5*y - 2, -3*x + 6*y - 15], [x, y])
-print solve(Poly((x - 2) * (x**2 + 3) * (x - 6), x))
+print solve([x + 5*y - 2, -3*x + 6*y - 15], [x, y])
+# print solve(Poly((x - 2) * (x**2 + 3) * (x - 6), x))
 R = last_solution()
 for i in R:
 	print i
