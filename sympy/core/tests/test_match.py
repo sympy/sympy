b1e00638211b99a9a88d6df09c616c90dd6ed9ec
Chris Smith <smichr@gmail.com>
2013-12-16 19:50:42 -0600

    restore setting of nargs and remove inline comment

--- sympy/core/tests/test_match.py
+++ sympy/core/tests/test_match.py
@@ -216,7 +216,7 @@ def test_derivative1():
     x, y = map(Symbol, 'xy')
     p, q = map(Wild, 'pq')
 
-    f = Function('f')
+    f = Function('f', nargs=1)
     fd = Derivative(f(x), x)
 
     assert fd.match(p) == {p: fd}
