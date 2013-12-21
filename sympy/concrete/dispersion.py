054d524e7f958b956282d3292c5625cd9306ba5d
raoul <raoulb@bluewin.ch>
2013-12-21 02:39:00 +0100

    An even better implementation of the dispersion algorithm

--- sympy/concrete/dispersion.py
+++ sympy/concrete/dispersion.py
@@ -8,7 +8,7 @@ def dispersionset(p, q=None):
     r"""Compute the 'dispersion set' of two polynomials.
 
     For two polynomials `f(x)` and `g(x)` with `deg f > 0`
-    and `deg g > 0` the dispersion set `J(f,g)` is defined as:
+    and `deg g > 0` the dispersion set `J(f, g)` is defined as:
 
     .. math::
         J(f, g) & := \{a \in \mathbb{N}_0 | gcd(f(x), g(x+a)) \neq 1\} \\
@@ -37,21 +37,18 @@ def dispersionset(p, q=None):
     if not p.is_univariate or not q.is_univariate:
         raise ValueError("Polynomials need to be univariate")
 
+    # The generator
+    if not p.gen == q.gen:
+        raise ValueError("Polynomials must have the same generator")
+    gen = p.gen
+
     # We define the dispersion of constant polynomials to be zero
     if p.degree() < 1 or q.degree() < 1:
         return set([0])
 
-    # The generator
-    if not p.gens[0] == q.gens[0]:
-        raise ValueError("Polynomials must have the same generator")
-    gen = p.gens[0]
-
     # Factor p and q over the rationals
     fp = factor_list(p, gen)
-    if same:
-        fq = fp
-    else:
-        fq = factor_list(q, gen)
+    fq = factor_list(q, gen) if not same else fp
 
     # Iterate over all pairs of factors
     J = set([])
@@ -59,35 +56,38 @@ def dispersionset(p, q=None):
         for t, unused in fq[1]:
             m = s.degree()
             n = t.degree()
-            if not m == n:
-                D = []
-            else:
-                a = s.coeff_monomial(gen**n)
-                b = s.coeff_monomial(gen**(n-1))
-                c = t.coeff_monomial(gen**n)
-                d = t.coeff_monomial(gen**(n-1))
-                j = (b*c-a*d) / (a*c*n)
-                if j < 0:
-                    D = []
-                else:
-                    if (c*s - a*t.shift(j)).is_zero:
-                        D = [j]
-                    else:
-                        D = []
-            J = J.union(D)
+            if n != m:
+                continue
+            an = s.LC()
+            bn = t.LC()
+            if not (an - bn).is_zero:
+                continue
+            # Note that the roles of `s` and `t` below are switched
+            # w.r.t. the original paper. This is for consistency
+            # with the description in the book of W. Koepf.
+            anm1 = s.coeff_monomial(gen**(m-1))
+            bnm1 = t.coeff_monomial(gen**(n-1))
+            alpha = (anm1 - bnm1) / S(n*bn)
+            if not alpha.is_integer:
+                continue
+            if alpha < 0 or alpha in J:
+                continue
+            if n > 1 and not (s - t.shift(alpha)).is_zero:
+                continue
+            J.add(alpha)
 
     return J
 
 
 def dispersion(p, q=None):
-    r"""Compute the 'dispersion' of a polynomial.
+    r"""Compute the 'dispersion' of polynomials.
 
     For two polynomials `f(x)` and `g(x)` with `deg f > 0`
-    and `deg g > 0` the dispersion `dis(f,g)` is defined as:
+    and `deg g > 0` the dispersion `dis(f, g)` is defined as:
 
     .. math::
-        dis(f, g) & := max\{ J(f,g) \union \{0\}\} \\
-                  &  = max\{ \{a \in \mathbb{N} | gcd(f(x), g(x+a)) \neq 1\} \union \{0\}\}
+        dis(f, g) & := max\{ J(f,g) \union \{0\} \} \\
+                  &  = max\{ \{a \in \mathbb{N} | gcd(f(x), g(x+a)) \neq 1\} \union \{0\} \}
 
     and for a single polynomial `dis(f) := dis(f, f)`.
 
