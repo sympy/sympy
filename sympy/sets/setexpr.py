44e90f02762d5f1b7638610eb0ecba6b6dac0b74
Matthew Rocklin <mrocklin@gmail.com>
2014-01-01 10:13:33 -0800

    pull out simplify functionality to _simplify functions

--- sympy/sets/setexpr.py
+++ sympy/sets/setexpr.py
@@ -17,6 +17,7 @@ def setexpr(inp):
 
 
 class SetExpr(Expr):
+    """ An expression that can take on values of a set """
     set = property(lambda self: self.args[0])
 
     def _latex(self, printer):
@@ -34,64 +35,64 @@ class SetExpr(Expr):
         return printer._print_seq((x, bar, xx, inn, set), '', '', ' ')
 
 
-def simplify(setexpr):
+def simplify(inp):
     """ Collapse expression containing SetExprs to single SetExpr """
-    if not setexpr.has(SetExpr):
-        return setexpr
+    if not inp.has(SetExpr):
+        return inp
 
     # Recurse downwards
     # e.g. simplify(x + y) -> simplify(simplify(x) + simplify(y))
-    setexpr = setexpr.func(*map(simplify, setexpr.args))
+    inp = inp.func(*map(simplify, inp.args))
 
     # Handle cases like exp(...) or sin(...)
-    if isinstance(setexpr, Function):
-        return SetExpr(imageset(type(setexpr), setexpr.args[0].set))
+    op, args = inp.func, inp.args
 
-    elif isinstance(setexpr, (Add, Mul)):
-        groups = sift(setexpr.args, lambda x: isinstance(x, SetExpr))
+    # If operation is commutative and we have non-SetExprs then
+    if (isinstance(inp, (Add, Mul))
+            and not all(isinstance(arg, SetExpr) for arg in inp.args)):
+        groups = sift(inp.args, lambda x: isinstance(x, SetExpr))
         setexprs, others = groups[True], groups[False]
-        op = type(setexpr)
-        se = join(op, setexprs)  # call out to many-setexpr function
+        se = simplify(op(*setexprs))  # call out to many-setexpr function
         return SetExpr(imageset(x, op(x, *others), se.set))
 
-    elif isinstance(setexpr, Pow) and isinstance(setexpr.base, SetExpr):
-        return SetExpr(imageset(x, Pow(x, setexpr.exp), setexpr.base.set))
-
-    return setexpr
-
-
-def join(op, setexprs):
-    ''' Join many setexprs into one
-
-    Relies on many functions named join_foo below
-    '''
-    assert all(isinstance(se, SetExpr) for se in setexprs)
-
+    args2 = [arg.set if isinstance(arg, SetExpr) else arg for arg in args]
     for key, func in join_list:  # Multiple Dispatch
         if (issubclass(op, key[0])
-            and len(key[1:]) == len(setexprs)
-            and all(isinstance(se.set, typ)
-                    for se, typ in zip(setexprs, key[1:]))):
-            return func(op, *setexprs)
+            and len(key[1:]) == len(args2)
+            and all(isinstance(se, typ)
+                    for se, typ in zip(args2, key[1:]))):
+            return func(op, *args)
 
-    # Two args is a common case for join_foo functions.  Lets reduce with join.
-    if len(setexprs) > 2:
-        result = setexprs[0]
-        for se in setexprs[1:]:
-            result = join(op, [result, se])
+    # Two args is a common case for _simplify_foo functions.  Lets reduce with join.
+    if len(args) > 2:
+        result = args[0]
+        for se in args[1:]:
+            result = simplify(op(result, se))
         return result
 
-    return op(*setexprs)
+    return op(*args)
 
 
-def join_Add_Intervals(_, a, b):
+def _simplify_Function(func, a):
+    """ f(SetExpr(set)) -> SetExpr(imageset(f, set)) """
+    return SetExpr(imageset(func, a.set))
+
+
+def _simplify_Pow(_, base, exp):
+    """ SetExpr(set)**expr -> SetExpr(imageset(x, x**expr, set)) """
+    return SetExpr(imageset(x, Pow(x, exp), base.set))
+
+
+def _simplify_Add_Intervals(_, a, b):
+    """ SetExpr([x, y]) + SetExpr([m, n]) -> SetExpr([x + m, y + n]) """
     return SetExpr(Interval(a.set.start + b.set.start,
                             a.set.end + b.set.end,
                             a.set.left_open or b.set.left_open,
                             a.set.right_open or b.set.right_open))
 
 
-def join_Mul_Intervals(_, a, b):
+def _simplify_Mul_Intervals(_, a, b):
+    """ SetExpr([x, y]) * SetExpr([m, n]) -> SetExpr([...]) """
     start = Min(*[x * y for x in (a.set.start, a.set.end)
                         for y in (b.set.start, b.set.end)])
     end =   Max(*[x * y for x in (a.set.start, a.set.end)
@@ -100,13 +101,15 @@ def join_Mul_Intervals(_, a, b):
     return SetExpr(Interval(start, end))
 
 
-def join_FiniteSet(op, a, b):
+def _simplify_FiniteSet(op, a, b):
     if isinstance(b.set, FiniteSet):
         a, b = b, a
     return SetExpr(Union(*[simplify(op(x, b)).set for x in a.set]))
 
 
-join_list = [[(Add, Interval, Interval),    join_Add_Intervals],
-             [(Mul, Interval, Interval),    join_Mul_Intervals],
-             [((Add, Mul), FiniteSet, Set), join_FiniteSet],
-             [((Add, Mul), Set, FiniteSet), join_FiniteSet]]
+join_list = [[(Function, Set),              _simplify_Function],
+             [(Pow, Set, Expr),             _simplify_Pow],
+             [(Add, Interval, Interval),    _simplify_Add_Intervals],
+             [(Mul, Interval, Interval),    _simplify_Mul_Intervals],
+             [((Add, Mul), FiniteSet, Set), _simplify_FiniteSet],
+             [((Add, Mul), Set, FiniteSet), _simplify_FiniteSet]]
