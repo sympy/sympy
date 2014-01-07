3b2f9f9dff74a97da3197ee1df12d77b8a43fb96
zanzibar7 <tcr-public2@psu.edu>
2014-01-07 17:50:38 -0500

    requested revisions

--- sympy/concrete/expr_with_limits.py
+++ sympy/concrete/expr_with_limits.py
@@ -64,14 +64,13 @@ class ExprWithLimits(Expr):
         # Any embedded piecewise functions need to be brought out to the
         # top level so that integration can go into piecewise mode at the
         # earliest possible moment.
-
-        if hasattr(function,'func') and function.func is C.Equality:
+        function = sympify(function)
+        if hasattr(function, 'func') and function.func is C.Equality:
             lhs = function.lhs
             rhs = function.rhs
-            return C.Equality(cls(lhs,*symbols,**assumptions), \
-                cls(rhs,*symbols,**assumptions))
-
-        function = piecewise_fold(sympify(function))
+            return C.Equality(cls(lhs, *symbols, **assumptions), \
+                cls(rhs, *symbols, **assumptions))
+        function = piecewise_fold(function)
 
         if function is S.NaN:
             return S.NaN
@@ -338,14 +337,13 @@ class AddWithLimits(ExprWithLimits):
         #
         # This constructor only differs from ExprWithLimits
         # in the application of the orientation variable.  Perhaps merge?
-
-        if hasattr(function,'func') and function.func is C.Equality:
+        function = sympify(function)
+        if hasattr(function, 'func') and function.func is C.Equality:
             lhs = function.lhs
             rhs = function.rhs
-            return C.Equality(cls(lhs,*symbols,**assumptions), \
-                cls(rhs,*symbols,**assumptions))
-
-        function = piecewise_fold(sympify(function))
+            return C.Equality(cls(lhs, *symbols, **assumptions), \
+                cls(rhs, *symbols, **assumptions))
+        function = piecewise_fold(function)
 
         if function is S.NaN:
             return S.NaN
