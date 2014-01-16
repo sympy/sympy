6c59649bc7870b4ee92efdca2ac3227104afb639
Anurag Sharma <anurags92@gmail.com>
2014-01-16 20:24:13 +0530

    new_beta_function

---
+++ sympy/functions/special/beta_functions.py
@@ -0,0 +1,44 @@
+from __future__ import print_function, division
+
+from sympy.core import Add, S, C, sympify, oo, pi
+from sympy.core.function import Function, ArgumentIndexError
+from .zeta_functions import zeta
+from .error_functions import erf
+from sympy.core import Dummy, Rational
+from sympy.functions.elementary.exponential import log
+from sympy.functions.elementary.integers import floor
+from sympy.functions.elementary.miscellaneous import sqrt
+from sympy.functions.combinatorial.numbers import bernoulli
+from sympy.functions.combinatorial.factorials import rf
+from sympy.functions.combinatorial.numbers import harmonic
+from sympy.core.compatibility import xrange
+
+###############################################################################
+############################ COMPLETE BETA  FUNCTION ##########################
+###############################################################################
+class beta(Function):
+    """Beta function or Euler's first integral is closely associated with gamma function.
+
+    References
+    ==========
+
+    .. [1] http://en.wikipedia.org/wiki/Beta_function
+    """
+    nargs = 2
+    unbranched = True
+
+    def fdiff(self, argindex=2):
+        x, y = self.args
+        if argindex == 1:
+            # Diff wrt x
+            return beta(x, y)*(digamma(x) - digamma(x+y)) 
+        elif argindex == 2:
+            # Diff wrt y
+            return beta(x, y)*(digamma(y) - digamma(x+y)) 
+        else:
+            raise ArgumentIndexError(self, argindex)
+
+	@classmethod
+    def eval(cls, x, y):
+        return gamma(x)*gamma(y) / gamma(x + y) 
+        
---
+++ sympy/functions/special/tests/test_beta_functions.py
@@ -0,0 +1,24 @@
+from sympy import (
+    Symbol, gamma, I, oo, nan, zoo, factorial, sqrt, Rational, log,
+    polygamma, EulerGamma, pi, uppergamma, S, expand_func, loggamma, sin,
+    cos, O, cancel, lowergamma, exp, erf, beta, exp_polar, harmonic, zeta,
+    factorial)
+from sympy.core.function import ArgumentIndexError
+from sympy.utilities.randtest import (test_derivative_numerically as td,
+                                      random_complex_number as randcplx,
+                                      test_numerically as tn)
+from sympy.utilities.pytest import raises
+
+x = Symbol('x')
+y = Symbol('y')
+n = Symbol('n', integer=True)
+
+
+def test_beta():
+    x, y = Symbol('x'), Symbol('y')
+    assert beta(x, y) == gamma(x)*gamma(y)/gamma(x + y)
+    assert beta(x, y) == beta(y, x)  # Symmetric
+    assert beta(x, y) == beta(x,y+1) + beta(x+1,y)
+
+    assert diff(beta(x, y), x) == beta(x, y)*(digamma(x) - digamma(x+y))
+    assert diff(beta(x, y), y) == beta(x, y)*(digamma(y) - digamma(x+y))
