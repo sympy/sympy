a7100bc4a407e70e90f86b0aeb6647f50136d1f0
Anurag Sharma <anurags92@gmail.com>
2014-01-18 03:33:54 +0530

    Modified import headers and argindex

--- sympy/functions/special/beta_functions.py
+++ sympy/functions/special/beta_functions.py
@@ -2,17 +2,7 @@ from __future__ import print_function, division
 
 from sympy.core import Add, S, C, sympify, oo, pi
 from sympy.core.function import Function, ArgumentIndexError
-from .zeta_functions import zeta
-from .error_functions import erf
-from sympy.core import Dummy, Rational
-from sympy.functions.elementary.exponential import log
-from sympy.functions.elementary.integers import floor
-from sympy.functions.elementary.miscellaneous import sqrt
-from sympy.functions.combinatorial.numbers import bernoulli
-from sympy.functions.combinatorial.factorials import rf
-from sympy.functions.combinatorial.numbers import harmonic
-from sympy.core.compatibility import xrange
-from sympy.functions.special.gamma_functions import gamma
+from sympy.functions.special.gamma_functions import gamma, digamma
 
 ###############################################################################
 ############################ COMPLETE BETA  FUNCTION ##########################
@@ -28,19 +18,23 @@ class beta(Function):
     nargs = 2
     unbranched = True
 
-    def fdiff(self, argindex=2):
-        x, y = self.args
+    def fdiff(self, argindex):
+      x, y = self.args 
         if argindex == 1:
-            # Diff wrt x
-            return beta(x, y)*(digamma(x) - digamma(x+y)) 
+          # Diff wrt x
+          return beta(x, y)*(digamma(x) - digamma(x + y)) 
         elif argindex == 2:
-            # Diff wrt y
-            return beta(x, y)*(digamma(y) - digamma(x+y)) 
+          # Diff wrt y
+          return beta(x, y)*(digamma(y) - digamma(x + y)) 
         else:
-            raise ArgumentIndexError(self, argindex)
+          raise ArgumentIndexError(self, argindex)
 
     @classmethod
     def eval(cls, x, y):
-        return gamma(x)*gamma(y) / gamma(x + y) 
+      return gamma(x)*gamma(y) / gamma(x + y)
+
+    def _eval_expand_func(self, **hints):
+      x, y = self.args
+      return gamma(x)*gamma(y) / gamma(x + y) 
          
         
