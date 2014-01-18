d94fa39a513cb5dce31c665327bf46d948b26c94
raoul <raoulb@bluewin.ch>
2014-01-18 01:11:55 +0100

    -> Remove unused import in beta

--- sympy/functions/special/beta_functions.py
+++ sympy/functions/special/beta_functions.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, division
 
-from sympy.core import Add, S, C, sympify, oo, pi
 from sympy.core.function import Function, ArgumentIndexError
 from sympy.functions.special.gamma_functions import gamma, digamma
 from sympy.core.function import diff
@@ -8,6 +7,7 @@ from sympy.core.function import diff
 ###############################################################################
 ############################ COMPLETE BETA  FUNCTION ##########################
 ###############################################################################
+
 class beta(Function):
     """Beta function or Euler's first integral is closely associated with gamma function.
 
@@ -20,13 +20,13 @@ class beta(Function):
     unbranched = True
 
     def fdiff(self, argindex):
-        x, y = self.args 
+        x, y = self.args
         if argindex == 1:
             # Diff wrt x
-            return beta(x, y)*(digamma(x) - digamma(x + y)) 
+            return beta(x, y)*(digamma(x) - digamma(x + y))
         elif argindex == 2:
             # Diff wrt y
-            return beta(x, y)*(digamma(y) - digamma(x + y)) 
+            return beta(x, y)*(digamma(y) - digamma(x + y))
         else:
             raise ArgumentIndexError(self, argindex)
 
@@ -36,6 +36,4 @@ class beta(Function):
 
     def _eval_expand_func(self, **hints):
         x, y = self.args
-        return gamma(x)*gamma(y) / gamma(x + y) 
-         
-        
+        return gamma(x)*gamma(y) / gamma(x + y)
