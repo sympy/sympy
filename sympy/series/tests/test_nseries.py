c77eb8b58733ff3762e6d8a15c1765eb15272f94
Avichal Dayal <avichal.dayal@gmail.com>
2014-01-21 18:05:03 +0530

    Added test for issue 2753
    Also changed the test in test_nseries which caused failure
    It now does not include logx term in big O notation.

--- sympy/series/tests/test_nseries.py
+++ sympy/series/tests/test_nseries.py
@@ -73,7 +73,7 @@ def test_power_x_x1():
 
 def test_power_x_x2():
     assert (x**x).nseries(x, n=4) == \
-        1 + x*log(x) + x**2*log(x)**2/2 + x**3*log(x)**3/6 + O(x**4*log(x)**4)
+        1 + x*log(x) + x**2*log(x)**2/2 + x**3*log(x)**3/6 + O(x**4)
 
 
 def test_log_singular1():
