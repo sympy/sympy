5aa5c4292558458922fe3c72769e872f862a0cfb
akshayah3 <akshaynukala95@gmail.com>
2014-01-27 18:52:21 +0530

    Rolling Back Commit

--- sympy/functions/special/error_functions.py
+++ sympy/functions/special/error_functions.py
@@ -98,6 +98,13 @@ class erf(Function):
         else:
             raise ArgumentIndexError(self, argindex)
 
+
+    def inverse(self, argindex=1):
+        """
+        Returns the inverse of this function.
+        """
+        return erfinv
+
     @classmethod
     def eval(cls, arg):
         if arg.is_Number:
@@ -280,6 +287,12 @@ class erfc(Function):
             return -2*C.exp(-self.args[0]**2)/sqrt(S.Pi)
         else:
             raise ArgumentIndexError(self, argindex)
+
+    def inverse(self, argindex=1):
+        """
+        Returns the inverse of this function.
+        """
+        return erfcinv
 
     @classmethod
     def eval(cls, arg):
@@ -736,6 +749,12 @@ class erfinv(Function):
         else :
             raise ArgumentIndexError(self, argindex)
 
+    def inverse(self, argindex=1):
+        """
+        Returns the inverse of this function.
+        """
+        return erf
+
     @classmethod
     def eval(cls, z):
         if z is S.NaN:
@@ -807,6 +826,12 @@ class erfcinv (Function):
             return -sqrt(S.Pi)*C.exp(self.func(self.args[0])**2)*S.Half
         else:
             raise ArgumentIndexError(self, argindex)
+
+    def inverse(self, argindex=1):
+        """
+        Returns the inverse of this function.
+        """
+        return erfc
 
     @classmethod
     def eval(cls, z):
