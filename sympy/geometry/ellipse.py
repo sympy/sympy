0752ad5eea8354d6f54bde7269941944066b4095
akshayah3 <akshaynukala95@gmail.com>
2014-02-06 07:52:37 +0530

    Contains method of a line.

--- sympy/geometry/ellipse.py
+++ sympy/geometry/ellipse.py
@@ -584,25 +584,24 @@ class Ellipse(GeometryEntity):
 
         """
 
-        if isinstance(p, Point):
-            if p.x.is_imaginary or p.y.is_imaginary:
-                raise ValueError('Enter a Real Point.')
-            else:
-                if p in self:
-                    return False
-
-                if len(self.foci) == 2:
-                    # if the combined distance from the foci to p (h1 + h2) is less
-                    # than the combined distance from the foci to the minor axis
-                    # (which is the same as the major axis length) then p is inside
-                    # the ellipse
-                    h1, h2 = [f.distance(p) for f in self.foci]
-                    test = 2*self.major - (h1 + h2)
-                else:
-                    test = self.radius - self.center.distance(p)
-            return fuzzy_bool(test.is_positive)
+        p = Point(p)
+        if p.x.is_imaginary or p.y.is_imaginary:
+            raise ValueError('Enter a Real Point.')
         else:
-            raise ValueError('Provide a Point as an argument.')
+            if p in self:
+                return False
+
+            if len(self.foci) == 2:
+                # if the combined distance from the foci to p (h1 + h2) is less
+                # than the combined distance from the foci to the minor axis
+                # (which is the same as the major axis length) then p is inside
+                # the ellipse
+                h1, h2 = [f.distance(p) for f in self.foci]
+                test = 2*self.major - (h1 + h2)
+            else:
+                test = self.radius - self.center.distance(p)
+        return fuzzy_bool(test.is_positive)
+
 
 
     @doctest_depends_on(modules=('pyglet',))
