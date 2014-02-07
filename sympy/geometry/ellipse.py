ebae49e37da9bc0e51aa84941769c22fa5b58d20
akshayah3 <akshaynukala95@gmail.com>
2014-02-07 16:53:15 +0530

    Eliminated imaginary points.

--- sympy/geometry/ellipse.py
+++ sympy/geometry/ellipse.py
@@ -825,7 +825,7 @@ class Ellipse(GeometryEntity):
             points = solve((seq, eq), (x, y))
             # complicated expressions may not be decidably real so evaluate to
             # check whether they are real or not
-            points = [Point(i)for i in points if (i[0].is_real and i[1].is_real is True)]
+            points = [Point(i).n(prec) if prec is not None else Point(i)for i in points if (i[0].is_real and i[1].is_real is True)]
         slopes = [norm.subs(zip((x, y), pt.args)) for pt in points]
         if prec is not None:
             slopes = [i.n(prec) if i not in (-oo, oo, zoo) else i for i in slopes]
