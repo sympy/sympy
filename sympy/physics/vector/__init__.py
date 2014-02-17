fc411dc5e46c85700ae111e4cbab256bb63308f2
srjoglekar246 <srjoglekar246@gmail.com>
2014-02-17 20:24:07 +0530

    Added files from previous PR

--- sympy/physics/vector/__init__.py
+++ sympy/physics/vector/__init__.py
@@ -34,3 +34,8 @@ __all__.extend(functions.__all__)
 from . import printing
 from .printing import *
 __all__.extend(printing.__all__)
+
+from . import fieldfunctions
+from .fieldfunctions import *
+__all__.extend(fieldfunctions.__all__)
+
