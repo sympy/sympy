c3b1ccf14a1354dd894dd52270fa77579a13becd
srjoglekar246 <srjoglekar246@gmail.com>
2014-02-17 20:36:37 +0530

    Added separate method to Vector

--- sympy/physics/vector/vector.py
+++ sympy/physics/vector/vector.py
@@ -444,6 +444,31 @@ class Vector(object):
     __rand__ = __and__
     __rmul__ = __mul__
 
+    def separate(self):
+        """
+        The constituents of this vector in different reference frames,
+        as per its definition.
+
+        Returns a dict mapping each ReferenceFrame to the corresponding
+        constituent Vector.
+
+        Examples
+        ========
+
+        >>> from sympy.physics.vector import ReferenceFrame
+        >>> R1 = ReferenceFrame('R1')
+        >>> R2 = ReferenceFrame('R2')
+        >>> v = 3*R1.x + 2*R1.y + R2.x
+        >>> v.separate()
+        {R1: 3*R1.x + 2*R1.y, R2: R2.x}
+
+        """
+
+        components = {}
+        for x in self.args:
+            components[x[1]] = Vector([x])
+        return components
+
     def dot(self, other):
         return self & other
     dot.__doc__ = __and__.__doc__
