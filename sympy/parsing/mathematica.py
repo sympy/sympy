e1302ddff972380e12989f6cfb1091ce15b967e0
adityashah30 <adityashah30@gmail.com>
2014-02-22 08:57:36 +0530

    Formatted the code to fit every line in 72 characters per line and changed the position of the comments to above the code

--- sympy/parsing/mathematica.py
+++ sympy/parsing/mathematica.py
@@ -14,33 +14,56 @@ def parse(s):
 
     #Begin rules
     rules = (
-        (r"\A(\d+)([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",  # Arithmetic operation between a constant and a function
-        lambda m: m.group(1) + translateFunction(m.group(2)) + parse(m.group(3)) ),
 
-        (r"\A(\w+\[[^\]]+[^\[]*\])([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",  # Arithmetic operation between two functions
-        lambda m: parse(m.group(1)) + translateFunction(m.group(2)) + parse(m.group(3)) ),
+        # Arithmetic operation between a constant and a function
 
-        (r"\A(\w+)\[([^\]]+[^\[]*)\]\Z",  # Function call
+        (r"\A(\d+)([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",
+        lambda m: m.group(1) + translateFunction(
+                m.group(2)) + parse(m.group(3)) ),
+
+        # Arithmetic operation between two functions
+
+        (r"\A(\w+\[[^\]]+[^\[]*\])([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",
+        lambda m: parse(m.group(1)) + translateFunction(
+                m.group(2)) + parse(m.group(3)) ),
+
+        # Function call
+
+        (r"\A(\w+)\[([^\]]+[^\[]*)\]\Z",
         lambda m: translateFunction(
-            m.group(1)) + "(" + parse(m.group(2)) + ")" ),
+                m.group(1)) + "(" + parse(m.group(2)) + ")" ),
 
-        (r"\((.+)\)\((.+)\)",  # Parenthesized implied multiplication
-        lambda m: "(" + parse(m.group(1)) + ")*(" + parse(m.group(2)) + ")" ),
+        # Parenthesized implied multiplication
 
-        (r"\A\((.+)\)\Z",  # Parenthesized expression
+        (r"\((.+)\)\((.+)\)",
+        lambda m: "(" + parse(m.group(1)) + ")*(" + parse(
+                m.group(2)) + ")" ),
+
+         # Parenthesized expression
+
+        (r"\A\((.+)\)\Z",
         lambda m: "(" + parse(m.group(1)) + ")" ),
 
-        (r"\A(.*[\w\.])\((.+)\)\Z",  # Implied multiplication - a(b)
+        # Implied multiplication - a(b)
+
+        (r"\A(.*[\w\.])\((.+)\)\Z",
         lambda m: parse(m.group(1)) + "*(" + parse(m.group(2)) + ")" ),
 
-        (r"\A\((.+)\)([\w\.].*)\Z",  # Implied multiplication - (a)b
+        # Implied multiplication - (a)b
+
+        (r"\A\((.+)\)([\w\.].*)\Z",
         lambda m: "(" + parse(m.group(1)) + ")*" + parse(m.group(2)) ),
 
-        (r"\A([\d\.]+)([a-zA-Z].*)\Z",  # Implied multiplicatin - 2a
+        # Implied multiplicatin - 2a
+
+        (r"\A([\d\.]+)([a-zA-Z].*)\Z",
         lambda m: parse(m.group(1)) + "*" + parse(m.group(2)) ),
 
-        (r"\A([^=]+)([\^\-\*/\+=]=?)(.+)\Z",  # Infix operator
-        lambda m: parse(m.group(1)) + translateOperator(m.group(2)) + parse(m.group(3)) ))
+        # Infix operator
+
+        (r"\A([^=]+)([\^\-\*/\+=]=?)(.+)\Z",
+        lambda m: parse(m.group(1)) + translateOperator(
+                m.group(2)) + parse(m.group(3)) ) )
     #End rules
 
     for rule, action in rules:
