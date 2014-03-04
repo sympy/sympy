cf159392a68242c1cffa3ce3e9df954fdfec4d96
Sudhanshu Mishra <mrsud94@gmail.com>
2014-03-05 04:06:39 +0530

    Added test for half integers for which racah was raising error earlier

--- sympy/physics/tests/test_clebsch_gordan.py
+++ sympy/physics/tests/test_clebsch_gordan.py
@@ -125,3 +125,4 @@ def test_racah():
     assert racah(3,3,3,3,3,3) == Rational(-1,14)
     assert racah(2,2,2,2,2,2) == Rational(-3,70)
     assert racah(7,8,7,1,7,7, prec=4).is_Float
+    assert racah(5.5,7.5,9.5,6.5,8,9)
