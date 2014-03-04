65580c6a3effdfe3aa818801086a5caa55de5870
Sudhanshu Mishra <mrsud94@gmail.com>
2014-03-05 05:08:31 +0530

    Some changes in test_racah

--- sympy/physics/tests/test_clebsch_gordan.py
+++ sympy/physics/tests/test_clebsch_gordan.py
@@ -125,4 +125,5 @@ def test_racah():
     assert racah(3,3,3,3,3,3) == Rational(-1,14)
     assert racah(2,2,2,2,2,2) == Rational(-3,70)
     assert racah(7,8,7,1,7,7, prec=4).is_Float
-    assert racah(5.5,7.5,9.5,6.5,8,9)
+    assert racah(5.5,7.5,9.5,6.5,8,9) == -719*sqrt(598)/1158924
+    assert abs(racah(5.5,7.5,9.5,6.5,8,9, prec=4) - (-0.01517)) < S('1e-4')
