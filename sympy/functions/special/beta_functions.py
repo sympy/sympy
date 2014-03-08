86dd302121cec55526460c1730f7d2b977a09ca1
Anurag Sharma <anurags92@gmail.com>
2014-03-08 14:10:56 +0530

    \math{B} in place of Betae

--- sympy/functions/special/beta_functions.py
+++ sympy/functions/special/beta_functions.py
@@ -13,21 +13,21 @@ class beta(Function):
     Legendre:
 
     .. math::
-        \Beta(x,y) := \int^{1}_{0} t^{x-1} (1-t)^{y-1}
+        \mathrm{B}(x,y) := \int^{1}_{0} t^{x-1} (1-t)^{y-1}
 
     Beta function or Euler's first integral is closely associated with gamma function.
     The Beta function often used in probability theory and mathematical statistics.
     It satisfies properties like:
 
     .. math::
-        \Beta(a,1) = \frac{1}{a}
-        \Beta(a,b) = \Beta(b,a)
-        \Beta(a,b) = \frac{\Gamma(a)*\Gamma(b)}{\Gamma(a+b)}
+        \mathrm{B}(a,1) = \frac{1}{a}
+        \mathrm{B}(a,b) = \mathrm{B}(b,a)
+        \mathrm{B}(a,b) = \frac{\Gamma(a)*\Gamma(b)}{\Gamma(a+b)}
 
     Therefore for integral values of a and b
 
     .. math::
-        \Beta(a,b) = \frac{(a-1)!*(b-1)!}{(a+b-1)!}
+        \mathrm{B} = \frac{(a-1)!*(b-1)!}{(a+b-1)!}
 
     Examples
     ========
--- sympy/geometry/polygon.py
+++ sympy/geometry/polygon.py
@@ -172,18 +172,18 @@ class Polygon(GeometryEntity):
         # sides of the polygon...but for now we use the n**2 algorithm
         # and check if any side intersects with any preceding side
         hit = _symbol('hit')
-        if not rv.is_convex:
+        if not rv.is_convex():
             sides = rv.sides
             for i, si in enumerate(sides):
-                pts = si[0], si[1]
+                pts = si.p1, si.p2
                 ai = si.arbitrary_point(hit)
                 for j in xrange(i):
                     sj = sides[j]
-                    if sj[0] not in pts and sj[1] not in pts:
+                    if sj.p1 not in pts and sj.p2 not in pts:
                         aj = si.arbitrary_point(hit)
-                        tx = (solve(ai[0] - aj[0]) or [S.Zero])[0]
+                        tx = (solve(ai.x - aj.x) or [S.Zero])[0]
                         if tx.is_number and 0 <= tx <= 1:
-                            ty = (solve(ai[1] - aj[1]) or [S.Zero])[0]
+                            ty = (solve(ai.y - aj.y) or [S.Zero])[0]
                             if (tx or ty) and ty.is_number and 0 <= ty <= 1:
                                 raise GeometryError(
                                     "Polygon has intersecting sides.")
