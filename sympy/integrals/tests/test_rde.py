f640dc8441e040e6dc80e570d8e3232ca50f8847
Anurag Sharma <anurags92@gmail.com>
2014-03-10 02:06:11 +0530

    Taste cases added for testing order_at for different degrees.

--- sympy/integrals/tests/test_rde.py
+++ sympy/integrals/tests/test_rde.py
@@ -15,12 +15,21 @@ t0, t1, t2, k = symbols('t:3 k')
 def test_order_at():
     a = Poly(t**4, t)
     b = Poly((t**2 + 1)**3*t, t)
+    c = Poly((t**2 + 1)**6*t, t)
+    d = Poly((t**2 + 1)**10*t**10, t)
+    e = Poly((t**2 + 1)**100*t**37, t)
     p1 = Poly(t, t)
     p2 = Poly(1 + t**2, t)
     assert order_at(a, p1, t) == 4
     assert order_at(b, p1, t) == 1
+    assert order_at(c, p1, t) == 1
+    assert order_at(d, p1, t) == 10
+    assert order_at(e, p1, t) == 37
     assert order_at(a, p2, t) == 0
     assert order_at(b, p2, t) == 3
+    assert order_at(c, p2, t) == 6
+    assert order_at(d, p1, t) == 10
+    assert order_at(e, p2, t) == 100
     assert order_at(Poly(0, t), Poly(t, t), t) == oo
     assert order_at_oo(Poly(t**2 - 1, t), Poly(t + 1), t) == \
         order_at_oo(Poly(t - 1, t), Poly(1, t), t) == -1
