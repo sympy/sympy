4740a32f02871ac9215dd4313637256a5df6af9b
Soumya Dipta Biswas <sdb1323@gmail.com>
2014-03-17 04:53:17 +0530

    Add support for Equivalent with multiple arguments

--- sympy/logic/tests/test_boolalg.py
+++ sympy/logic/tests/test_boolalg.py
@@ -290,10 +290,12 @@ def test_De_Morgan():
 
 
 def test_eliminate_implications():
-
+    from sympy.abc import A, B, C, D
     assert eliminate_implications(Implies(A, B, evaluate=False)) == (~A) | B
     assert eliminate_implications(
         A >> (C >> Not(B))) == Or(Or(Not(B), Not(C)), Not(A))
+    assert eliminate_implications(Equivalent(A, B, C, D)) == \
+        (~A | B) & (~B | C) & (~C | D) & (~D | A)
 
 
 def test_conjuncts():
