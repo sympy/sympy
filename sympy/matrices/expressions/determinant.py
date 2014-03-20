3111c158eb86e41dad77d33beb56a247c59162ca
ruba <pruba16@gmail.com>
2014-03-20 19:22:21 +0530

    Integral : Improve the functionality of Determinant class
    Original Determinant method which takes a matrix as its parameter will produce just the string Determinant. So Here I have modified the code to produce the steps of calculating an nxn matrix. So the output of the Determinant method will be a string which represent the way of multiplication to get the determinant. So it will be more useful than the previous one.
    
    Example:
    
    >>>from sympy import * ;
    >>>A=MatrixSymbol('A',1,1);
    >>>Determinant(A);
    A[0][0]
    
    >>>A=MatrixSymbol('A',2,2);
    >>>Determinant(A);
    A[0][0]*A[1][1] - A[0][1]*A[1][0]
    
    >>>A=MatrixSymbol('B',3,3);
    >>>Determinant(A);
    B[0][0](B[1][1]*B[2][2] - B[1][2]*B[2][1])-B[0][1](B[1][0]*B[2][2] - B[1][2]*B[2][0])+B[0][2](B[1][0]*B[2][1] - B[1][1]*B[2][0])

--- sympy/matrices/expressions/determinant.py
+++ sympy/matrices/expressions/determinant.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 
 from sympy import Basic, Expr, S, Q
 from .matexpr import ShapeError
-
+from  .matexpr import MatrixExpr
 
 class Determinant(Expr):
     """Matrix Determinant
@@ -25,7 +25,61 @@ class Determinant(Expr):
         if not mat.is_square:
             raise ShapeError("Det of a non-square matrix")
 
-        return Basic.__new__(cls, mat)
+        n=mat.rows;
+
+        matrix=[[0 for x in range(n)] for x in range(n)]
+
+        for i in range(0,n):
+            for j in range(0,n):
+                matrix[i][j]=str(mat) +"["+str(i)+"]["+str(j)+"]";
+
+        return Determinant.det_sub(matrix);
+
+    @classmethod
+    def det_sub(self,matrix):
+        n=len(matrix);
+        det_mat="";
+
+        if(n==1):
+            det_mat=matrix[0][0];
+        elif(n==2):
+            det_mat=matrix[0][0]+"*" + matrix[1][1]+" - " + matrix[0][1]+"*" + matrix[1][0];
+        else:
+            sign="+";
+
+            for i in range(0,n):
+                if(i==0):
+                    det_mat=matrix[0][0]+"("+Determinant.det_sub(Determinant.getM(matrix,0,0))+")";
+                else:
+                    if(i%2==0):
+                        sign="+";
+                    else:
+                        sign="-";
+                    det_mat=det_mat+sign+matrix[0][i]+"("+Determinant.det_sub(Determinant.getM(matrix,0,i))+")";
+
+        return det_mat;
+
+    @classmethod
+    def getM(self,matrix,p,q):
+        n=len(matrix);
+
+        matrixA=[[0 for x in range(n-1)] for x in range(n-1)]
+
+        countR=0;
+        countC=0;
+
+        for i in range(1,n):
+                countC=0;
+                for j in range(0,n):
+                    if(j==q):
+                        continue;
+                    matrixA[countR][countC]=matrix[i][j];
+                    countC+=1;
+                countR+=1;
+
+        return matrixA;
+
+
 
     @property
     def arg(self):
