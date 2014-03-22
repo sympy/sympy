464eb0465fae0460904113d590ded847ee70279d
Sergey B Kirpichev <skirpichev@gmail.com>
2014-03-22 20:44:08 +0400

    change git --version requirements

--- bin/mailmap_update.py
+++ bin/mailmap_update.py
@@ -39,8 +39,8 @@ git_people = unicode(local(git_command, capture=True), 'utf-8').strip().split("\
 from distutils.version import LooseVersion
 
 git_ver = local('git --version', capture=True)[12:]
-if LooseVersion(git_ver) < LooseVersion('1.8.5'):
-    print(yellow("Please use a newer git version >= 1.8.5"))
+if LooseVersion(git_ver) < LooseVersion('1.8.4.2'):
+    print(yellow("Please use a newer git version >= 1.8.4.2"))
 
 with open(os.path.realpath(os.path.join(__file__, os.path.pardir,
     os.path.pardir, "AUTHORS"))) as fd:
