b9b6b8a9337888fdcfcf15af1dedc758d0662dd0
Matthew Rocklin <mrocklin@gmail.com>
2014-03-30 09:04:18 -0700

    add nexted evalute=False test

---
+++ sympy/core/tests/test_evaluate.py
@@ -0,0 +1,17 @@
+from sympy.abc import x, y
+from sympy.core.evaluate import evaluate
+from sympy.core import Mul, Add
+
+def test_add():
+    with evaluate(False):
+        expr = x + x
+        assert isinstance(expr, Add)
+        assert expr.args == (x, x)
+    assert isinstance(x + x, Mul)
+
+
+def test_nested():
+    with evaluate(False):
+        expr = (x + x) + (y + y)
+        assert expr.args == ((x + x), (y + y))
+        assert expr.args[0].args == (x, x)
