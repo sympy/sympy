29201cbd49d39ced9e082e96a34c6b6f745d943b
Aaron Meurer <asmeurer@gmail.com>
2014-04-19 16:51:23 -0500

    Fix missing import

--- sympy/assumptions/satask.py
+++ sympy/assumptions/satask.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division
 
-from sympy.core import oo
+from sympy.core import oo, Tuple
 
 from sympy.assumptions.assume import global_assumptions, AppliedPredicate
 from sympy.logic.inference import satisfiable
