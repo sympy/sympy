e06fc8883a65dee275bc3f7bc7d6e1fa81efedfc
Christopher Smith <smichr@gmail.com>
2014-04-21 11:10:58 -0500

    watch out for negative args of log in evalf

--- sympy/core/tests/test_evalf.py
+++ sympy/core/tests/test_evalf.py
@@ -125,6 +125,7 @@ def test_evalf_complex_cancellation():
 def test_evalf_logs():
     assert NS("log(3+pi*I)", 15) == '1.46877619736226 + 0.808448792630022*I'
     assert NS("log(pi*I)", 15) == '1.14472988584940 + 1.57079632679490*I'
+    assert NS('log(-1 + 0.00001)', 2) == '-1.0e-5 + 3.1*I'
 
 
 def test_evalf_trig():
