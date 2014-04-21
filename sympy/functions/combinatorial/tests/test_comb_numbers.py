9f88de932d6a6be803245f1a81ae179232f49f51
raoul <raoulb@bluewin.ch>
2014-04-21 16:35:39 +0200

    Tests for expansion of harmonic numbers at rational arguments

--- sympy/functions/combinatorial/tests/test_comb_numbers.py
+++ sympy/functions/combinatorial/tests/test_comb_numbers.py
@@ -1,9 +1,10 @@
 import string
 
-from sympy import (bernoulli, Symbol, symbols, Dummy, Sum, harmonic, Rational, oo,
-    zoo, pi, I, bell, fibonacci, lucas, euler, catalan, binomial, gamma, sqrt,
-    hyper, log, digamma, trigamma, polygamma, diff, Expr, sympify, expand_func,
-    EulerGamma, factorial)
+from sympy import (bernoulli, Symbol, symbols, Dummy, S, Sum, Rational,
+                   oo, zoo, pi, I, simplify, expand_func, harmonic,
+                   bell, fibonacci, lucas, euler, catalan, binomial, gamma,
+                   sqrt, hyper, log, digamma, trigamma, polygamma, diff,
+                   EulerGamma, factorial, sin, cos, cot, cancel)
 
 from sympy.utilities.pytest import XFAIL, raises
 
@@ -86,6 +87,73 @@ def test_harmonic():
     assert harmonic(10, 0) == 10
     assert harmonic(oo, 1) == oo
     assert harmonic(oo, 2) == (pi**2)/6
+
+def test_harmonic_rational():
+    ne = S(6)
+    no = S(5)
+    pe = S(8)
+    po = S(9)
+    qe = S(10)
+    qo = S(13)
+
+    Heee = harmonic(ne + pe/qe)
+    Aeee = (-log(10) + 2*(-1/S(4) + sqrt(5)/4)*log(sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 - 1/S(4))*log(sqrt(sqrt(5)/8 + 5/S(8)))
+             + pi*(1/S(4) + sqrt(5)/4)/(2*sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 13944145/S(4720968))
+
+    Heeo = harmonic(ne + pe/qo)
+    Aeeo = (-log(26) + 2*log(sin(3*pi/13))*cos(4*pi/13) + 2*log(sin(2*pi/13))*cos(32*pi/13)
+             + 2*log(sin(5*pi/13))*cos(80*pi/13) - 2*log(sin(6*pi/13))*cos(5*pi/13)
+             - 2*log(sin(4*pi/13))*cos(pi/13) + pi*cot(5*pi/13)/2 - 2*log(sin(pi/13))*cos(3*pi/13)
+             + 2422020029/S(702257080))
+
+    Heoe = harmonic(ne + po/qe)
+    Aeoe = (-log(20) + 2*(1/S(4) + sqrt(5)/4)*log(-1/S(4) + sqrt(5)/4)
+             + 2*(-1/S(4) + sqrt(5)/4)*log(sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 - 1/S(4))*log(sqrt(sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 + 1/S(4))*log(1/S(4) + sqrt(5)/4)
+             + 11818877030/S(4286604231) - pi*sqrt(sqrt(5)/8 + 5/S(8))/(-sqrt(5)/2 + 1/S(2)) )
+
+
+    Heoo = harmonic(ne + po/qo)
+    Aeoo = (-log(26) + 2*log(sin(3*pi/13))*cos(54*pi/13) + 2*log(sin(4*pi/13))*cos(6*pi/13)
+             + 2*log(sin(6*pi/13))*cos(108*pi/13) - 2*log(sin(5*pi/13))*cos(pi/13)
+             - 2*log(sin(pi/13))*cos(5*pi/13) + pi*cot(4*pi/13)/2
+             - 2*log(sin(2*pi/13))*cos(3*pi/13) + 11669332571/S(3628714320))
+
+    Hoee = harmonic(no + pe/qe)
+    Aoee = (-log(10) + 2*(-1/S(4) + sqrt(5)/4)*log(sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 - 1/S(4))*log(sqrt(sqrt(5)/8 + 5/S(8)))
+             + pi*(1/S(4) + sqrt(5)/4)/(2*sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 779405/S(277704))
+
+    Hoeo = harmonic(no + pe/qo)
+    Aoeo = (-log(26) + 2*log(sin(3*pi/13))*cos(4*pi/13) + 2*log(sin(2*pi/13))*cos(32*pi/13)
+             + 2*log(sin(5*pi/13))*cos(80*pi/13) - 2*log(sin(6*pi/13))*cos(5*pi/13)
+             - 2*log(sin(4*pi/13))*cos(pi/13) + pi*cot(5*pi/13)/2
+             - 2*log(sin(pi/13))*cos(3*pi/13) + 53857323/S(16331560))
+
+    Hooe = harmonic(no + po/qe)
+    Aooe = (-log(20) + 2*(1/S(4) + sqrt(5)/4)*log(-1/S(4) + sqrt(5)/4)
+             + 2*(-1/S(4) + sqrt(5)/4)*log(sqrt(-sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 - 1/S(4))*log(sqrt(sqrt(5)/8 + 5/S(8)))
+             + 2*(-sqrt(5)/4 + 1/S(4))*log(1/S(4) + sqrt(5)/4)
+             + 486853480/S(186374097) - pi*sqrt(sqrt(5)/8 + 5/S(8))/(2*(-sqrt(5)/4 + 1/S(4))))
+
+    Hooo = harmonic(no + po/qo)
+    Aooo = (-log(26) + 2*log(sin(3*pi/13))*cos(54*pi/13) + 2*log(sin(4*pi/13))*cos(6*pi/13)
+             + 2*log(sin(6*pi/13))*cos(108*pi/13) - 2*log(sin(5*pi/13))*cos(pi/13)
+             - 2*log(sin(pi/13))*cos(5*pi/13) + pi*cot(4*pi/13)/2
+             - 2*log(sin(2*pi/13))*cos(3*pi/13) + 383693479/S(125128080))
+
+    H = [Heee, Heeo, Heoe, Heoo, Hoee, Hoeo, Hooe, Hooo]
+    A = [Aeee, Aeeo, Aeoe, Aeoo, Aoee, Aoeo, Aooe, Aooo]
+
+    for h, a in zip(H, A):
+        e = expand_func(h).doit()
+        assert cancel(e/a) == 1
+        assert h.n() == a.n()
 
 
 def test_harmonic_rewrite_polygamma():
