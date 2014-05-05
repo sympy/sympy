7d07d886cf3720a8c8c952817f60046622509d66
Peter Brady <ptb@lanl.gov>
2014-05-05 14:33:47 -0600

    Implemented Python 3 functools.lru_cache as a class decorator compatible with py2 and 3

--- sympy/core/cache.py
+++ sympy/core/cache.py
@@ -1,66 +1,112 @@
 """ Caching facility for SymPy """
 from __future__ import print_function, division
-from collections import deque
-from weakref import WeakValueDictionary,WeakKeyDictionary
+from sympy.core.decorators import wraps
+import types
+from collections import namedtuple
+try:
+    from _thread import RLock
+except:
+    class RLock:
+        'Dummy reentrant lock for builds without threads'
+        def __enter__(self): pass
+        def __exit__(self, exctype, excinst, exctb): pass
 # TODO: refactor CACHE & friends into class?
 
-# global cache registry:
-CACHE = []  # [] of
-            # (item, {} or tuple of {})
+class _cache(list):
+    """ List of cached functions """
 
-from sympy.core.decorators import wraps
+    def print_cache(self):
+        """print cache info"""
 
-def print_cache():
-    """print cache content"""
-
-    for item, cache in CACHE:
-        item = str(item)
-        head = '='*len(item)
-
-        print(head)
-        print(item)
-        print(head)
-
-        if not isinstance(cache, tuple):
-            cache = (cache,)
-            shown = False
-        else:
-            shown = True
-
-        for i, kv in enumerate(cache):
-            if shown:
-                print('\n*** %i ***\n' % i)
-
-            if hasattr(kv,'items'):
-                for k, v in list(kv.items()):
-                    print('  %s :\t%s' % (k, v))
+        for item in self:
+            name = item.__name__
+            myfunc = item
+            while hasattr(myfunc,'__wrapped__'):
+                if hasattr(myfunc,'cache_info'):
+                    info = myfunc.cache_info()
+                    break
+                else:
+                    myfunc = myfunc.__wrapped__
             else:
-                for k in kv:
-                    print('  %s  ' % k )
+                info = None
 
+            print(name,info)
 
-def clear_cache():
-    """clear cache content"""
-    for item, cache in CACHE:
-        if not isinstance(cache, tuple):
-            cache = (cache,)
+    def clear_cache(self):
+        """clear cache content"""
+        for item in CACHE:
+            myfunc = item
+            while hasattr(myfunc,'__wrapped__'):
+                if hasattr(myfunc,'cache_clear'):
+                    myfunc.cache_clear()
+                    break
+                else:
+                    myfunc = myfunc.__wrapped__
 
-        for kv in cache:
-            kv.clear()
+# global cache registry:
+CACHE = _cache()
+# make clear and print methods available
+print_cache = CACHE.print_cache
+clear_cache = CACHE.clear_cache
+
 
 ########################################
 
+# lru_cache class compatible with python 2 and 3.  Original function based
+# implementation is part of the functools standard library for Python 3
+_CacheInfo = namedtuple("CacheInfo", ["hits", "misses", "fails","maxsize", "currsize"])
 
-def __cacheit_nocache(func):
-    return func
+class _HashedSeq(list):
+    """ This class guarantees that hash() will be called no more than once
+        per element.  This is important because the lru_cache() will hash
+        the key multiple times on a cache miss.
 
+    """
+
+    __slots__ = 'hashvalue'
+
+    def __init__(self, tup, hash=hash):
+        self[:] = tup
+        self.hashvalue = hash(tup)
+
+    def __hash__(self):
+        return self.hashvalue
 
 # from sympy.assumptions.assume import global_assumptions  # circular import
 from sympy.core.evaluate import global_evaluate
-_globals = (global_evaluate,)
+_globals = global_evaluate
 
+def _make_key(args, kwds, typed,
+             kwd_mark = (object(),),
+             fasttypes = {int, str, frozenset, type(None)},
+             sorted=sorted, tuple=tuple, type=type, len=len):
+    """Make a cache key from optionally typed positional and keyword arguments
 
-def __cacheit(func):
+    The key is constructed in a way that is flat as possible rather than
+    as a nested structure that would take more memory.
+
+    If there is only a single argument and its data type is known to cache
+    its hash value, then that argument is returned without a wrapper.  This
+    saves space and improves lookup speed.
+
+    """
+    key = args
+    if kwds:
+        sorted_items = sorted(kwds.items())
+        key += kwd_mark
+        for item in sorted_items:
+            key += item
+    if typed:
+        key += tuple(type(v) for v in args)
+        if kwds:
+            key += tuple(type(v) for k, v in sorted_items)
+    if _globals:
+        key += tuple(g for g in _globals)
+    elif len(key) == 1 and type(key[0]) in fasttypes:
+        return key[0]
+    return _HashedSeq(key)
+
+class __cacheit(object):
     """caching decorator.
 
        important: the result of cached function must be *immutable*
@@ -81,58 +127,116 @@ def __cacheit(func):
        to force cacheit to check returned results mutability and consistency,
        set environment variable SYMPY_USE_CACHE to 'debug'
     """
-    # Many of the functions which we want to cache have arguments and return
-    # values which are not weak-referenceable.  To get around this, the 
-    # arguments of the function are used as the key in weakValueDict while 
-    # the return values are stored as values in a weakKeyDict.  The dicts 
-    # are linked via a dummy object which is weak-referenceable and hashable.
-    # Messy...
-    func._cache_it_arg2obj = func_cache_it_arg2obj = WeakValueDictionary()
-    func._cache_it_obj2ret = func_cache_it_obj2ret = WeakKeyDictionary()
-    # weak references will be deleted when values are removed from deque
-    func._cache_it_cache = func_cache_it_cache = deque(maxlen=2000)
 
-    CACHE.append((func, (func_cache_it_arg2obj,
-                         func_cache_it_obj2ret,
-                         func_cache_it_cache)))
+    def __init__(self,func):
+        wraps(func)(self)
+        if not hasattr(self,'__wrapped__'):
+            setattr(self,'__wrapped__',func)
+        # Users should only access the lru_cache through its public API:
+        #       cache_info, cache_clear, and f.__wrapped__
+        # The internals of the lru_cache are encapsulated for thread safety and
+        # to allow the implementation to change (including a possible C version).
 
-    # dummy class
-    class Object():
-        pass
+        # Constants shared by all lru cache instances:
+        self._make_key = _make_key         # build a key from the function arguments
+        self._PREV, self._NEXT, self._KEY, self._RESULT = 0, 1, 2, 3   # names for the link fields
+        self._maxsize = 2000
+        self._cache = {}
+        self._hits = self._misses = self._fails = 0
+        self._full = False
+        self._lock = RLock()         # because linkedlist updates aren't threadsafe
+        self._root = []              # root of the circular doubly linked list
+        self._root[:] = [self._root, self._root, None, None]     # initialize by pointing to self
+        # append to global cache list
+        CACHE.append(self)
 
-    @wraps(func)
-    def wrapper(*args, **kw_args):
-        """
-        Assemble the args and kw_args to compute the hash.
-        """
-        k = [(x, type(x)) for x in args]
-        if kw_args:
-            keys = sorted(kw_args)
-            k.extend([(x, kw_args[x], type(kw_args[x])) for x in keys])
-        if _globals:
-            k.extend([tuple(g) for g in _globals])
-        k = tuple(k)
+    def _cache_get(self,key):
+        return self._cache.get(key)
+
+    def __call__(self,*args,**kwargs):
+        # Size limited caching that tracks accesses by recency
         try:
-            hash(k)
-        except TypeError: # k is unhashable
-            return func(*args,**kw_args)
-        
-        # check if we are cached
-        try:
-            obj = func_cache_it_arg2obj[k]
-            ret = func_cache_it_obj2ret[obj]
-        except KeyError: # not cached
-            ret = func(*args, **kw_args)
-            obj = Object()
-            func_cache_it_arg2obj[k] = obj
-            func_cache_it_obj2ret[obj] = ret
+            key = self._make_key(args, kwargs, typed=True)
+            if key is None:
+                raise TypeError
+        except TypeError:
+            # args,kwargs not hashable. mark as fail and return result
+            self._fails += 1
+            return self.__wrapped__(*args, **kwargs)
+        PREV,NEXT,KEY,RESULT = self._PREV,self._NEXT,self._KEY,self._RESULT
 
-        # move 'obj' to front of deque
-        func_cache_it_cache.append(obj)
-        return ret
+        with self._lock:
+            link = self._cache_get(key)
+            if link is not None:
+                # Move the link to the front of the circular queue
+                link_prev, link_next, _key, result = link
+                link_prev[NEXT] = link_next
+                link_next[PREV] = link_prev
+                last = self._root[PREV]
+                last[NEXT] = self._root[PREV] = link
+                link[PREV] = last
+                link[NEXT] = self._root
+                self._hits += 1
+                return result
+        result = self.__wrapped__(*args, **kwargs)
+        with self._lock:
+            if key in self._cache:
+                # Getting here means that this same key was added to the
+                # cache while the lock was released.  Since the link
+                # update is already done, we need only return the
+                # computed result and update the count of misses.
+                pass
+            elif self._full:
+                # Use the old self._root to store the new key and result.
+                oldroot = self._root
+                oldroot[KEY] = key
+                oldroot[RESULT] = result
+                # Empty the oldest link and make it the new self._root.
+                # Keep a reference to the old key and old result to
+                # prevent their ref counts from going to zero during the
+                # update. That will prevent potentially arbitrary object
+                # clean-up code (i.e. __del__) from running while we're
+                # still adjusting the links.
+                self._root = oldroot[NEXT]
+                oldkey = self._root[KEY]
+                oldresult = self._root[RESULT]
+                self._root[KEY] = self._root[RESULT] = None
+                # Now update the cache dictionary.
+                del self._cache[oldkey]
+                # Save the potentially reentrant cache[key] assignment
+                # for last, after the self._root and links have been put in
+                # a consistent state.
+                self._cache[key] = oldroot
+            else:
+                # Put result in a new link at the front of the queue.
+                last = self._root[PREV]
+                link = [last, self._root, key, result]
+                last[NEXT] = self._root[PREV] = self._cache[key] = link
+                self._full = (len(self._cache) >= self._maxsize)
+            self._misses += 1
+        return result
 
-    return wrapper
 
+    def cache_info(self):
+        """ Report cache statistics """
+        with self._lock:
+            return _CacheInfo(self._hits,self._misses,self._fails,
+                              self._maxsize,len(self._cache))
+
+    def cache_clear(self):
+        """ Clear the cache and cache statistics """
+        with self._lock:
+            self._cache.clear()
+            self._root[:] = [self._root, self._root, None, None]
+            self._hits = self._misses = self._fails = 0
+            self._full = False
+
+    def __get__(self,instance,cls):
+        """ Ensure proper bound method object creation """
+        if instance is None:
+            return self
+        else:
+            return types.MethodType(self,instance)
 
 def __cacheit_debug(func):
     """cacheit + code to check cache consistency"""
