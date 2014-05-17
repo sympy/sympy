cf392f15a4dc083181e675b82c60c11b2014fdeb
raoul <raoulb@bluewin.ch>
2014-05-17 02:40:30 +0200

    Mathieu Cosine Function

--- sympy/functions/special/mathieu_functions.py
+++ sympy/functions/special/mathieu_functions.py
@@ -67,3 +67,63 @@ class mathieus(Function):
     def _eval_conjugate(self):
         a, q, z = self.args
         return self.func(a.conjugate(), q.conjugate(), z.conjugate())
+
+
+class mathieuc(Function):
+    r"""
+    The Mathieu Cosine function. This function is one solution
+    of the Mathieu differential equation:
+
+    .. math ::
+        y(x)^{\prime\prime} + (a - 2 q \cos(2 x) y(x) = 0
+
+    The other solution is the Mathieu Sine function.
+
+    Examples
+    ========
+
+    >>> from sympy import I, oo, mathieus
+    >>> from sympy.abc import a, q, z
+
+    >>> mathieuc(a, q, z)
+    mathieuc(a, q, z)
+
+    >>> mathieuc(a, 0, z)
+    cos(sqrt(a)*z)
+
+    See Also
+    ========
+
+    mathieus: Mathieu sine function
+    mathieusprime: Derivative of Mathieu sine function
+    mathieucprime: Derivative of Mathieu cosine function
+
+    References
+    ==========
+
+    .. [1] http://en.wikipedia.org/wiki/Mathieu_function
+    .. [2] http://dlmf.nist.gov/28
+    .. [3] http://mathworld.wolfram.com/MathieuFunction.html
+    .. [4] http://functions.wolfram.com/MathieuandSpheroidalFunctions/MathieuC/
+    """
+
+    unbranched = True
+
+    def fdiff(self, argindex=1):
+        if argindex == 3:
+            a, q, z = self.args
+            return mathieucprime(a, q, z)
+        else:
+            raise ArgumentIndexError(self, argindex)
+
+    @classmethod
+    def eval(cls, a, q, z):
+        if q.is_Number and q is S.Zero:
+            return cos(sqrt(a)*z)
+        # Try to pull out factors of -1
+        if z.could_extract_minus_sign():
+            return cls(a, q, -z)
+
+    def _eval_conjugate(self):
+        a, q, z = self.args
+        return self.func(a.conjugate(), q.conjugate(), z.conjugate())
