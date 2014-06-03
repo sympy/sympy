878cc7c2e73310946be11efba0ce31cdc7da78d1
Jim Crist <crist042@umn.edu>
2014-06-03 18:27:09 -0500

    Generalized Linearization Routine
    The following changes were made to the Linearizer Class:
    
    1.) Now accepts if q, u, or any of the f_# generalized terms
    are empty matrices. Linearization proceeds as it should.
    
    2.) Broke the linearization process into a couple "private"
    class methods. The ones done so far only need to be run once
    upon initialization. This should save calculation time if the
    user intends to do multiple linearizations about different
    operating points.

--- sympy/physics/mechanics/linearize.py
+++ sympy/physics/mechanics/linearize.py
@@ -36,6 +36,130 @@ class Linearizer(object):
         self.qd = self.q.diff(t)
         self.ud = self.u.diff(t)
 
+        # Calculates here only need to be run once:
+        self._form_permutation_matrices()
+        self._form_block_matrices()
+        self._form_coefficient_matrices()
+
+    def _form_permutation_matrices(self):
+        """ Forms the permutation matrices for this choice of independent
+        coordinates and speeds """
+
+        # Extract dimension variables
+        n = len(self.q)
+        o = len(self.u)
+        l = len(self.f_c)
+        m = len(self.f_a)
+        # Compute permutation matrices
+        if n != 0:
+            self.Pq = permutation_matrix(self.q, Matrix([self.q_i, self.q_d]))
+            if l > 0:
+                self.Pqi = self.Pq[:, :-l]
+                self.Pqd = self.Pq[:, -l:]
+            else:
+                self.Pqi = self.Pq
+                self.Pqd = Matrix([])
+        if o != 0:
+            self.Pu = permutation_matrix(self.u, Matrix([self.u_i, self.u_d]))
+            if m > 0:
+                self.Pui = self.Pu[:, :-m]
+                self.Pud = self.Pu[:, -m:]
+            else:
+                self.Pui = self.Pu
+                self.Pud = Matrix([])
+        # Compute combination permutation matrix for computing A and B
+        P_col1 = Matrix([self.Pqi, zeros(o, n - l)])
+        P_col2 = Matrix([zeros(n, o - m), self.Pui])
+        if P_col1:
+            if P_col2:
+                self.P_prime = P_col1.row_join(P_col2)
+            else:
+                self.P_prime = P_col1
+        else:
+            self.P_prime = P_col2
+
+    def _form_coefficient_matrices(self):
+        """ Form the coefficient matrices. These only need to be solved for once"""
+
+        # Dimension terms
+        n = len(self.q)
+        o = len(self.u)
+        l = len(self.f_c)
+        m = len(self.f_a)
+        # Build up the coefficient matrices C_0, C_1, and C_2
+        # If there are configuration constraints (l > 0), form C_0 as normal.
+        # If not, C_0 is I_(nxn). Note that this works even if n=0
+        if l > 0:
+            f_c_jac_q = self.f_c.jacobian(self.q)
+            self.C_0 = (eye(n) - self.Pqd * (f_c_jac_q * self.Pqd).inv() * f_c_jac_q) * self.Pqi
+        else:
+            self.C_0 = eye(n)
+        # If there are motion constraints (m > 0), form C_1 and C_2 as normal.
+        # If not, C_1 is 0, and C_2 is I_(oxo). Note that this works even if
+        # o = 0.
+        if m > 0:
+            f_v_jac_u = self.f_v.jacobian(self.u)
+            temp = self.Pud * (f_v_jac_u * self.Pud).inv()
+            if n != 0:
+                f_v_jac_q = self.f_v.jacobian(self.q)
+                self.C_1 = -temp * f_v_jac_q
+            else:
+                self.C_1 = 0
+            self.C_2 = (eye(o) - temp * f_v_jac_u) * self.Pui
+        else:
+            self.C_1 = 0
+            self.C_2 = eye(o)
+
+    def _form_block_matrices(self):
+        """ Form the block matrices. These only need to be solved for once """
+
+        # Dimension terms
+        n = len(self.q)
+        o = len(self.u)
+        s = len(self.r)
+        l = len(self.f_c)
+        m = len(self.f_a)
+        # Block Matrix Definitions. These are only defined if under certain
+        # conditions. If undefined, an empty matrix is used instead
+        if n != 0:
+            self.M_qq = self.f_0.jacobian(self.qd)
+            self.A_qq = -(self.f_0 + self.f_1).jacobian(self.q)
+        else:
+            self.M_qq = Matrix([])
+            self.A_qq = Matrix([])
+        if o != 0 and m != 0:
+            self.M_uuc = self.f_a.jacobian(self.ud)
+            self.A_uuc = -self.f_a.jacobian(self.u)
+        else:
+            self.M_uuc = Matrix([])
+            self.A_uuc = Matrix([])
+        if o != 0 and o != m:
+            self.M_uud = self.f_2.jacobian(self.ud)
+            self.A_uud = -self.f_3.jacobian(self.u)
+        else:
+            self.M_uud = Matrix([])
+            self.A_uud = Matrix([])
+        if n != 0 and m != 0:
+            self.M_uqc = self.f_a.jacobian(self.qd)
+            self.A_uqc = -self.f_a.jacobian(self.q)
+        else:
+            self.M_uqc = Matrix([])
+            self.A_uqc = Matrix([])
+        if n != 0 and o != m:
+            self.M_uqd = self.f_2.jacobian(self.qd)
+            self.A_uqd = -(self.f_2 + self.f_3).jacobian(self.q)
+        else:
+            self.M_uqd = Matrix([])
+            self.A_uqd = Matrix([])
+        if o != 0 and n != 0:
+            self.A_qu = -self.f_1.jacobian(self.u)
+        else:
+            self.A_qu = Matrix([])
+        if s != 0 and o != m:
+            self.B_u = -self.f_3.jacobian(self.r)
+        else:
+            self.B_u = Matrix([])
+                
     def linearize(self, q_trim=None, u_trim=None, qd_trim=None, ud_trim=None, r_trim=None, A_and_B=False):
         """ Linearize the system about the trim conditions. Note that
         q_trim, u_trim, qd_trim, ud_trim must satisfy the equations of motion.
@@ -69,117 +193,105 @@ class Linearizer(object):
         dtrim = qd_trim
         dtrim.update(ud_trim)
 
-        # Rename terms to shorten expressions
-        q = self.q
-        u = self.u
-        qd = self.qd
-        ud = self.ud
-        r = self.r
-        q_i = self.q_i
-        q_d = self.q_d
-        u_i = self.u_i
-        u_d = self.u_d
-        f_0 = self.f_0
-        f_1 = self.f_1
-        f_2 = self.f_2
-        f_3 = self.f_3
-        f_a = self.f_a
-        f_v = self.f_v
-        f_c = self.f_c
-
         # Dimension terms
-        n = len(q)
-        o = len(u)
-        s = len(r)
-        l = len(f_c)
-        m = len(f_a)
+        n = len(self.q)
+        o = len(self.u)
+        s = len(self.r)
+        l = len(self.f_c)
+        m = len(self.f_a)
 
-        # Compute permutation matrices
-        Pq = permutation_matrix(q, Matrix([q_i, q_d]))
-        if l > 0:
-            Pqi = Pq[:, :-l]
-            Pqd = Pq[:, -l:]
-        else:
-            Pqi = Pq
-            Pqd = Matrix([])
-        Pu = permutation_matrix(u, Matrix([u_i, u_d]))
-        if m > 0:
-            Pui = Pu[:, :-m]
-            Pud = Pu[:, -m:]
-        else:
-            Pui = Pu
-            Pud = Matrix([])
-
-        # Block Matrix Definitions
-        M_qq = f_0.jacobian(qd)
-        M_uqd = f_2.jacobian(qd)
-        M_uud = f_2.jacobian(ud)
-        A_qq = -(f_0 + f_1).jacobian(q)
-        A_qu = -f_1.jacobian(u)
-        A_uqd = -(f_2 + f_3).jacobian(q)
-        A_uud = -f_3.jacobian(u)
-        # Only form these if there are dependent speeds (m > 0).
-        if m > 0:
-            M_uqc = f_a.jacobian(qd)
-            M_uuc = f_a.jacobian(ud)
-            A_uqc = -f_a.jacobian(q)
-            A_uuc = -f_a.jacobian(u)
+        # Rename terms to shorten expressions
+        M_qq = self.M_qq
+        A_qq = self.A_qq
+        M_uuc = self.M_uuc
+        A_uuc = self.A_uuc
+        M_uud = self.M_uud
+        A_uud = self.A_uud
+        M_uqc = self.M_uqc
+        A_uqc = self.A_uqc
+        M_uqd = self.M_uqd
+        A_uqd = self.A_uqd
+        A_qu = self.A_qu
+        B_u = self.B_u
+        C_0 = self.C_0
+        C_1 = self.C_1
+        C_2 = self.C_2
 
         # Build up Mass Matrix
         #     |M_qq    0_nxo|
         # M = |M_uqc   M_uuc|
         #     |M_uqd   M_uud|
-        row1 = M_qq.row_join(zeros(n, o))
-        # The second row only exists if there are motion constraints
-        if m > 0:
-            row2 = M_uqc.row_join(M_uuc)
+        if o != 0:
+            col2 = Matrix([zeros(n, o), M_uuc, M_uud])
+        if n != 0:
+            col1 = Matrix([M_qq, M_uqc, M_uqd])
+            if o != 0:
+                M = col1.row_join(col2)
+            else:
+                M = col1
         else:
-            row2 = Matrix([])
-        row3 = M_uqd.row_join(M_uud)
-        M = Matrix([row1, row2, row3])
+            M = col2
         M_eq = _subs_keep_derivs(M.subs(dtrim), trim)
         M_eq.simplify()
-
-        # Build up the coefficient matrices C_0, C_1, and C_2
-        # If there are configuration constraints (l > 0), form C_0 as normal.
-        # If not, C_0 is I_(nxn)
-        if l > 0:
-            f_c_jac_q = f_c.jacobian(q)
-            C_0 = (eye(n) - Pqd * (f_c_jac_q * Pqd).inv() * f_c_jac_q) * Pqi
-        else:
-            C_0 = eye(n)
-        # If there are motion constraints (m > 0), form C_1 and C_2 as normal.
-        # If not, C_1 is 0, and C_2 is I_(oxo)
-        if m > 0:
-            f_v_jac_q = f_v.jacobian(q)
-            f_v_jac_u = f_v.jacobian(u)
-            temp = Pud * (f_v_jac_u * Pud).inv()
-            C_1 = -temp * f_v_jac_q
-            C_2 = (eye(o) - temp * f_v_jac_u) * Pui
-        else:
-            C_1 = 0
-            C_2 = eye(o)
 
         # Build up state coefficient matrix A
         #     |(A_qq + A_qu*C_1)*C_0       A_qu*C_2|
         # A = |(A_uqc + A_uuc*C_1)*C_0    A_uuc*C_2|
         #     |(A_uqd + A_uud*C_1)*C_0    A_uud*C_2|
-        row1 = ((A_qq + A_qu * C_1) * C_0).row_join(A_qu * C_2)
-        # The second row only exists if there are motion constraints
-        if m > 0:
-            row2 = ((A_uqc + A_uuc * C_1) * C_0).row_join(A_uuc * C_2)
+        # Col 1 is only defined if n != 0
+        if n != 0:
+            r1c1 = A_qq
+            if o != 0:
+                r1c1 += A_qu*C_1
+            r1c1 = r1c1 * C_0
+            if m != 0:
+                r2c1 = A_uqc
+                if o != 0:
+                    r2c1 += A_uuc*C_1
+                r2c1 = r2c1 * C_0
+            else:
+                r2c1 = Matrix([])
+            if o != m:
+                r3c1 = A_uqd
+                if o != 0:
+                    r3c1 += A_uud*C_1
+                r3c1 = r3c1 * C_0
+            else:
+                r3c1 = Matrix([])
+            col1 = Matrix([r1c1, r2c1, r3c1])
         else:
-            row2 = Matrix([])
-        row3 = ((A_uqd + A_uud * C_1) * C_0).row_join(A_uud * C_2)
-        Amat = Matrix([row1, row2, row3])
+            col1 = Matrix([])
+        # Col 2 is only defined if o != 0
+        if o != 0:
+            if n != 0:
+                r1c2 = A_qu * C_2
+            else:
+                r1c2 = Matrix([])
+            if m != 0:
+                r2c2 = A_uuc * C_2
+            else:
+                r2c2 = Matrix([])
+            if o != m:
+                r3c2 = A_uud * C_2
+            else:
+                r3c2 = Matrix([])
+            col2 = Matrix([r1c2, r2c2, r3c2])
+        else:
+            col2 = Matrix([])
+        if col1:
+            if col2:
+                Amat = col1.row_join(col2)
+            else:
+                Amat = col1
+        else:
+            Amat = col2
         Amat_eq = _subs_keep_derivs(Amat.subs(dtrim), trim)
         Amat_eq.simplify()
 
         # Build up the B matrix if there are forcing variables
         #     |0_(n + m)xs|
         # B = |B_u        |
-        if s > 0:
-            B_u = -f_3.jacobian(r)
+        if s != 0 and o != m:
             Bmat = zeros(n + m, s).col_join(B_u)
             Bmat_eq = _subs_keep_derivs(Bmat.subs(dtrim), trim)
         else:
@@ -188,14 +300,11 @@ class Linearizer(object):
         # kwarg A_and_B indicates to return  A, B for forming the equation
         # dx = [A]x + [B]r, where x = [q_ind, u_ind]^T,
         if A_and_B:
-            P_row1 = Pqi.row_join(zeros(n, o - m))
-            P_row2 = zeros(o, n - l).row_join(Pui)
-            P = P_row1.col_join(P_row2)
             Minv = M_eq.inv()
-            A_cont = P.T*Minv*Amat_eq
+            A_cont = self.P_prime.T*Minv*Amat_eq
             A_cont.simplify()
             if Bmat_eq:
-                B_cont = P.T*Minv*Bmat_eq
+                B_cont = self.P_prime.T*Minv*Bmat_eq
                 B_cont.simplify()
             else:
                 B_cont = Bmat_eq
