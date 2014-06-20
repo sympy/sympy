e7d5da351a331af1ffbede836d1282c1d06e7074
KDMOE <kundankumar18581@gmail.com>
2014-06-20 15:38:53 +0530

    Added a mathod and a test case

--- sympy/solvers/tests/test_ode.py
+++ sympy/solvers/tests/test_ode.py
@@ -3,7 +3,7 @@ from __future__ import division
 
 from sympy import (acos, acosh, asinh, atan, cos, Derivative, diff, dsolve,
     Dummy, Eq, erf, erfi, exp, Function, I, Integral, LambertW, log, O, pi,
-    Rational, RootOf, S, simplify, sin, sqrt, Symbol, tan, asin,
+    Rational, RootOf, S, simplify, sin, sqrt, Symbol, tan, asin, sinh, cosh,
     Piecewise, symbols, Poly)
 from sympy.solvers.ode import (_undetermined_coefficients_match, checkodesol,
     classify_ode, classify_sysode, constant_renumber, constantsimp,
@@ -187,9 +187,9 @@ def test_nonlinear_2eq_order1():
     x, y, z = symbols('x, y, z', function=True)
     t = Symbol('t')
     eq1 = (Eq(diff(x(t),t),x(t)*y(t)**3), Eq(diff(y(t),t),y(t)**5))
-    sol1 = [[C1*exp(I/(-1/(4*C2 + 4*t))**(1/4)), C1*exp(-I/(-1/(4*C2 + 4*t))**(1/4)), \
-    C1*exp(-1/(-1/(4*C2 + 4*t))**(1/4)), C1*exp((-1/(4*C2 + 4*t))**(-1/4))], [-(-1/(4*C2 + 4*t))**(1/4), \
-    (-1/(4*C2 + 4*t))**(1/4), -I*(-1/(4*C2 + 4*t))**(1/4), I*(-1/(4*C2 + 4*t))**(1/4)]]
+    sol1 = [[C1*exp(I/(-1/(4*C2 + 4*t))**(S(1)/4)), C1*exp(-I/(-1/(4*C2 + 4*t))**(S(1)/4)), \
+    C1*exp(-1/(-1/(4*C2 + 4*t))**(S(1)/4)), C1*exp((-1/(4*C2 + 4*t))**(-S(1)/4))], [-(-1/(4*C2 + 4*t))**(S(1)/4), \
+    (-1/(4*C2 + 4*t))**(S(1)/4), -I*(-1/(4*C2 + 4*t))**(S(1)/4), I*(-1/(4*C2 + 4*t))**(S(1)/4)]]
     assert dsolve(eq1) == sol1
 
     eq2 = (Eq(diff(x(t),t), exp(3*x(t))*y(t)**3),Eq(diff(y(t),t), y(t)**5))
@@ -199,13 +199,17 @@ def test_nonlinear_2eq_order1():
     assert dsolve(eq2) == sol2
 
     eq3 = (Eq(diff(x(t),t), y(t)*x(t)), Eq(diff(y(t),t), x(t)**3))
-    sol3 = [[6**(2/3)/(6*(sinh(sqrt(C1*(C2 + t)**2)/2)/sqrt(C1))**(2/3)), \
-    6**(2/3)/(6*(-sinh(sqrt(C1*(C2 + t)**2)/2)/sqrt(C1))**(2/3))], [sqrt(C1 + C1/sinh(sqrt(C1*(C2 + t)**2)/2)**2)/3]]
+    sol3 = [[6**(S(2)/3)/(6*(sinh(sqrt(C1*(C2 + t)**2)/2)/sqrt(C1))**(S(2)/3)), \
+    6**(S(2)/3)/(6*(-sinh(sqrt(C1*(C2 + t)**2)/2)/sqrt(C1))**(S(2)/3))], [sqrt(C1 + C1/sinh(sqrt(C1*(C2 + t)**2)/2)**2)/3]]
     assert dsolve(eq3) == sol3
 
     eq4 = (Eq(diff(x(t),t),x(t)*y(t)*sin(t)**2), Eq(diff(y(t),t),y(t)**2*sin(t)**2))
     sol4 = [[-2*exp(C1)/(C2*exp(C1) + t - sin(2*t)/2)], [-2/(C1 + t - sin(2*t)/2)]]
     assert dsolve(eq4) == sol4
+
+    eq5 = (Eq(x(t),t*diff(x(t),t)+diff(x(t),t)*diff(y(t),t)), Eq(y(t),t*diff(y(t),t)+diff(y(t),t)**2))
+    sol5 = [[C1*C2 + C1*t], [C2**2 + C2*t]]
+    assert dsolve(eq5) == sol5
 
 
 def test_checkodesol():
