d2fce08604b960e110feaa4cb2869cc6439fdd24
Sudhanshu Mishra <mrsud94@gmail.com>
2014-06-22 23:05:11 +0530

    Correct implicit import

--- sympy/physics/optics/__init__.py
+++ sympy/physics/optics/__init__.py
@@ -16,5 +16,8 @@ from .waves import TWave
 __all__.extend(waves.__all__)
 
 from . import gaussopt
-from .gaussopt import *
+from .gaussopt import RayTransferMatrix, FreeSpace, FlatRefraction,\
+    CurvedRefraction, FlatMirror, CurvedMirror, ThinLens, GeometricRay,\
+    BeamParameter, waist2rayleigh, rayleigh2waist, geometric_conj_ab,\
+    geometric_conj_af, geometric_conj_bf, gaussian_conj, conjugate_gauss_beams
 __all__.extend(gaussopt.__all__)
