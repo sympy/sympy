585deabeb178143c193dbf0a625ec86bc0f5894a
Sudhanshu Mishra <mrsud94@gmail.com>
2014-06-28 07:38:45 +0530

    Add deprecation warning for sympy.physics.gaussopt

---
+++ sympy/physics/gaussopt.py
@@ -0,0 +1,12 @@
+from sympy.physics.optics.gaussopt import RayTransferMatrix, FreeSpace,\
+    FlatRefraction, CurvedRefraction, FlatMirror, CurvedMirror, ThinLens,\
+    GeometricRay, BeamParameter, waist2rayleigh, rayleigh2waist, geometric_conj_ab,\
+    geometric_conj_af, geometric_conj_bf, gaussian_conj, conjugate_gauss_beams
+
+from sympy.utilities.exceptions import SymPyDeprecationWarning
+
+
+SymPyDeprecationWarning(feature="Module sympy.physics.gaussopt",
+        useinstead="sympy.physics.optics.gaussopt",
+        deprecated_since_version="0.7.6").warn()
+
