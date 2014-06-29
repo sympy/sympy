75b70eabd037528ff71af195472c76185862204c
Sergey B Kirpichev <skirpichev@gmail.com>
2014-06-29 18:57:23 +0400

    Add DLMF reference for cos

--- sympy/functions/elementary/trigonometric.py
+++ sympy/functions/elementary/trigonometric.py
@@ -412,7 +412,8 @@ class cos(TrigonometricFunction):
     ==========
 
     .. [1] http://en.wikipedia.org/wiki/Trigonometric_functions
-    .. [2] http://functions.wolfram.com/ElementaryFunctions/Cos
+    .. [2] http://dlmf.nist.gov/4.14
+    .. [3] http://functions.wolfram.com/ElementaryFunctions/Cos
 
     """
 
