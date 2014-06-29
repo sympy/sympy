9c7d6fe558a0281d3c6185f2d630d807dcdbe836
Sergey B Kirpichev <skirpichev@gmail.com>
2014-06-29 18:56:38 +0400

    Add DLMF reference for sin

--- sympy/functions/elementary/trigonometric.py
+++ sympy/functions/elementary/trigonometric.py
@@ -186,7 +186,8 @@ class sin(TrigonometricFunction):
     ==========
 
     .. [1] http://en.wikipedia.org/wiki/Trigonometric_functions
-    .. [2] http://functions.wolfram.com/ElementaryFunctions/Sin
+    .. [2] http://dlmf.nist.gov/4.14
+    .. [3] http://functions.wolfram.com/ElementaryFunctions/Sin
 
     """
 
