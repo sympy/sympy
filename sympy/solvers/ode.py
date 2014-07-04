8a6979e508e19185a393e8962a900412f2e8ebbf
KDMOE <kundankumar18581@gmail.com>
2014-07-04 19:24:16 +0530

    Removed a repeated condition
    Condition for number of equation being equal to number of function was repeated. This commit contain removal of one of them.

--- sympy/solvers/ode.py
+++ sympy/solvers/ode.py
@@ -548,8 +548,6 @@ def dsolve(eq, func=None, hint="default", simplify=True,
             if eq[i].coeff(diff(func[i],t,ode_order(eq[i], func[i]))).is_negative:
                 eq[i] = -eq[i]
         match['eq'] = eq
-        if len(func) != len(eq) and (func != [None]):
-            raise ValueError("Number of functions given is less than number of equations %s" % func)
         if len(set(order.values()))!=1:
             raise ValueError("It solves only those systems of equations whose orders are equal")
         match['order'] = list(order.values())[0]
