3201f112157ee2c33a0253e85058f27e7176b31c
Chris Smith <smichr@gmail.com>
2014-07-18 20:10:12 -0500

    modify 4 minute test so it takes about 2 seconds
    Before original commit:
    >>> series(cot(x**4 + x**5), x, 0, 1)
    O(x)
    
    After original commit
    >>> series(cot(x**4 + x**5), x, 0, 1)
    x**(-4) - 1/x**3 + x**(-2) - 1/x + O(x)
    
    This simpler test failed in master before the original
    changes but succeeds afterwards demonstrating that the
    change worked. It also takes orders of magnitude less time
    to compute.
    
    Since the time that the correction was made, another correction
    was made that includes the "+1" term; that is now included in the
    modified test, too.

--- sympy/functions/elementary/tests/test_trigonometric.py
+++ sympy/functions/elementary/tests/test_trigonometric.py
@@ -476,11 +476,9 @@ def test_cot():
 def test_cot_series():
     assert cot(x).series(x, 0, 9) == \
         1/x - x/3 - x**3/45 - 2*x**5/945 - x**7/4725 + O(x**9)
-    # issue 6210:
-    assert cot(x**20 + x**21 + x**22).series(x, 0, 4) == \
-        x**(-20) - 1/x**19 + x**(-17) - 1/x**16 + x**(-14) - 1/x**13 + \
-        x**(-11) - 1/x**10 + x**(-8) - 1/x**7 + x**(-5) - 1/x**4 + \
-        x**(-2) - 1/x + x - x**2 + O(x**4)
+    # issue 6210
+    assert cot(x**4 + x**5).series(x, 0, 1) == \
+        x**(-4) - 1/x**3 + x**(-2) - 1/x + 1 + O(x)
 
 
 def test_cot_rewrite():
