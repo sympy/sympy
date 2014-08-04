f6fa0b34bfa5846e1d930f4ebf4c6811c8b44602
Sudhanshu Mishra <mrsud94@gmail.com>
2014-08-04 13:43:26 +0530

    Fix limiting condition issue and add tests

--- sympy/physics/optics/utils.py
+++ sympy/physics/optics/utils.py
@@ -17,7 +17,7 @@ __all__ = ['refraction_angle',
            'lens_formula'
            ]
 
-from sympy import Symbol, sympify, sqrt, Matrix, acos
+from sympy import Symbol, sympify, sqrt, Matrix, acos, oo, Limit
 from sympy.geometry.line3d import Ray3D
 from sympy.geometry.util import intersection
 from sympy.geometry.plane import Plane
@@ -321,11 +321,102 @@ def mirror_formula(focal_length=None, u=None, v=None):
     focal_length = sympify(focal_length)
     u = sympify(u)
     v = sympify(v)
-
+    if u == oo:
+        _u = Symbol('u')
+    if v == oo:
+        _v = Symbol('v')
+    if focal_length == oo:
+        _f = Symbol('f')
     if focal_length is None:
+        if u == oo and v == oo:
+            return Limit(Limit(_v*_u/(_v + _u), _u, oo), _v, oo).doit()
+        if u == oo:
+            return Limit(v*_u/(v + _u), _u, oo).doit()
+        if v == oo:
+            return Limit(_v*u/(_v + u), _v, oo).doit()
         return v*u/(v + u)
     if u is None:
+        if v == oo and focal_length == oo:
+            return Limit(Limit(_v*_f/(_v - _f), _v, oo), _f, oo).doit()
+        if v == oo:
+            return Limit(_v*focal_length/(_v - focal_length), _v, oo).doit()
+        if focal_length == oo:
+            return Limit(v*_f/(v - _f), _f, oo).doit()
         return v*focal_length/(v - focal_length)
     if v is None:
+        if u == oo and focal_length == oo:
+            return Limit(Limit(_u*_f/(_u - _f), _u, oo), _f, oo).doit()
+        if u == oo:
+            return Limit(_u*focal_length/(_u - focal_length), _u, oo).doit()
+        if focal_length == oo:
+            return Limit(u*_f/(u - _f), _f, oo).doit()
         return u*focal_length/(u - focal_length)
 
+
+def lens_formula(focal_length=None, u=None, v=None):
+    """
+    This function provides one of the three parameters
+    when two of them are supplied.
+    This is valid only for paraxial rays.
+
+    Parameters
+    ==========
+
+    focal_length : sympifiable
+        Focal length of the mirror.
+    u : sympifiable
+        Distance of object from the optical center on
+        the principal axis.
+    v : sympifiable
+        Distance of the image from the optical center
+        on the principal axis.
+
+    Examples
+    ========
+
+    >>> from sympy.physics.optics import lens_formula
+    >>> from sympy.abc import f, u, v
+    >>> lens_formula(focal_length=f, u=u)
+    f*u/(f + u)
+    >>> lens_formula(focal_length=f, v=v)
+    f*v/(f - v)
+    >>> lens_formula(u=u, v=v)
+    u*v/(u - v)
+
+    """
+    if focal_length and u and v:
+        raise ValueError("Please provide only two parameters")
+
+    focal_length = sympify(focal_length)
+    u = sympify(u)
+    v = sympify(v)
+    if u == oo:
+        _u = Symbol('u')
+    if v == oo:
+        _v = Symbol('v')
+    if focal_length == oo:
+        _f = Symbol('f')
+    if focal_length is None:
+        if u == oo and v == oo:
+            return Limit(Limit(_v*_u/(_u - _v), _u, oo), _v, oo).doit()
+        if u == oo:
+            return Limit(v*_u/(_u - v), _u, oo).doit()
+        if v == oo:
+            return Limit(_v*u/(u - _v), _v, oo).doit()
+        return v*u/(u - v)
+    if u is None:
+        if v == oo and focal_length == oo:
+            return Limit(Limit(_v*_f/(_f - _v), _v, oo), _f, oo).doit()
+        if v == oo:
+            return Limit(_v*focal_length/(focal_length - _v), _v, oo).doit()
+        if focal_length == oo:
+            return Limit(v*_f/(_f - v), _f, oo).doit()
+        return v*focal_length/(focal_length - v)
+    if v is None:
+        if u == oo and focal_length == oo:
+            return Limit(Limit(_u*_f/(_u + _f), _u, oo), _f, oo).doit()
+        if u == oo:
+            return Limit(_u*focal_length/(_u + focal_length), _u, oo).doit()
+        if focal_length == oo:
+            return Limit(u*_f/(u + _f), _f, oo).doit()
+        return u*focal_length/(u + focal_length)
