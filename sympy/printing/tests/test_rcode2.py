52c43e5bcf6316f9306d5a1f02cb0afda9e21514
Markus Müller <markus.mueller.1.g@googlemail.com>
2014-08-04 19:17:40 +0200

    finished the translation of the tests of the ccode to the rcode module

--- sympy/printing/tests/test_rcode2.py
+++ sympy/printing/tests/test_rcode2.py
@@ -4,20 +4,24 @@ from sympy.utilities.pytest import raises
 from sympy.printing.rcode import RCodePrinter
 from sympy.utilities.lambdify import implemented_function
 from sympy.tensor import IndexedBase, Idx
+from sympy.printing.fcode import fcode, FCodePrinter
 
 # import test
 from sympy import rcode
 
-x, y, z = symbols('x,y,z')
-g = Function('g')
-
-
-
-#mm
-def test_rcode_constants_other():
-    assert rcode(GoldenRatio) == "GoldenRatio = 1.61803398874989;\nGoldenRatio"
-    #assert rcode(2*GoldenRatio) == "double const GoldenRatio = 1.61803398874989;\n2*GoldenRatio"
-    #assert rcode(
-    #    2*Catalan) == "double const Catalan = 0.915965594177219;\n2*Catalan"
-    #assert rcode(2*EulerGamma) == "double const EulerGamma = 0.577215664901533;\n2*EulerGamma"
-
+i, m = symbols('i m', integer=True, cls=Dummy)
+x = IndexedBase('x')
+y = IndexedBase('y')
+i = Idx(i, m)
+print((x[i].base.label))
+print((x[i].indices))
+print(m.dummy_index)
+print(i.label.dummy_index)
+code = rcode(x[i], assign_to=y[i])
+print(code)
+expected = (
+        'for (i_%(icount)i in 1:m_%(mcount)i){\n'
+    '   y[i_%(icount)i] = x[i_%(icount)i];\n'
+    '}'
+) % {'icount': i.label.dummy_index, 'mcount': m.dummy_index}
+print(expected)
