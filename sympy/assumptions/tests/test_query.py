5a981ff756d57c8a2127f61ba2f9d489608dceb1
Sergey B Kirpichev <skirpichev@gmail.com>
2014-08-28 23:39:11 +0400

    fix test

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -2108,7 +2108,7 @@ def test_issue_7246():
     assert ask(Q.positive(acos(x)), Q.nonnegative(x + 1) & Q.nonpositive(x - 1)) is True
     assert ask(Q.positive(acos(x)), Q.nonnegative(x - 1)) is None
 
-    assert ask(Q.positive(acot(x)), Q.positive(x)) is True
+    assert ask(Q.positive(acot(x)), Q.positive(x) & ~Q.infinity(x)) is True
     assert ask(Q.positive(acot(x)), Q.real(x)) is True
     assert ask(Q.positive(acot(x)), Q.imaginary(x)) is False
     assert ask(Q.positive(acot(x))) is None
