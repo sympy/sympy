4788a9b4bc3eefb5be1f0f753bbc4378acde26df
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-09-09 00:02:29 +0100

    printing: MatPow should have Mul precedence

--- sympy/printing/precedence.py
+++ sympy/printing/precedence.py
@@ -37,6 +37,7 @@ PRECEDENCE_VALUES = {
     "NegativeInfinity": PRECEDENCE["Add"],
     "MatAdd": PRECEDENCE["Add"],
     "MatMul": PRECEDENCE["Mul"],
+    "MatPow": PRECEDENCE["Pow"],
     "HadamardProduct": PRECEDENCE["Mul"]
 }
 
