40ca09c09d7615743bf2e17ceb38d86d47a7a89d
Ondřej Čertík <ondrej.certik@gmail.com>
2014-09-13 20:05:06 -0600

    Add necessary SymPy definitions for Rubi
    This is just the initial attempt. More needs to be added.

---
+++ sympy/integrals/rubi/rubi_definitions.py
@@ -0,0 +1,23 @@
+def eq(a, b):
+    return (a-b).simplify() == 0
+
+def gt(a, b):
+    return (a-b).simplify().n() > 0
+
+def lt(a, b):
+    return (a-b).simplify().n() < 0
+
+def ge(a, b):
+    return eq(a, b) or gt(a, b)
+
+def le(a, b):
+    return eq(a, b) or lt(a, b)
+
+def integer(a):
+    return a.is_integer
+
+def subst(a, x, y):
+    return a.subs(x, y)
+
+def Int(a, x):
+    return a.integrate(x)
