e39f773e1dbdfcd5095631bdf6d43cb48f552bce
Ondřej Čertík <ondrej.certik@gmail.com>
2014-09-17 09:39:55 -0600

    Add documentation to the routines

--- sympy/integrals/rubi/tests/test_trig.py
+++ sympy/integrals/rubi/tests/test_trig.py
@@ -1,5 +1,20 @@
 from sympy import S, Symbol, sin, cos
-from sympy.integrals.rubi.SineIntegrationRules import intsin5
+from sympy.integrals.rubi.SineIntegrationRules import intsin5, intsin12
+
+# The various functions in SineIntegrationRules return the antiderivatives of
+# the following expressions:
+#   intsin5(a,b,c,n,x)
+#       (c*sin(a+b*x))**n
+#   intsin6(a,b,c,d,n,x)
+#       (a+b*sin(c+d*x))**n
+#   intsin9(a,b,c,d,e,f,m,n,x)
+#       (a+b*sin(e+f*x))**m*(c+d*sin(e+f*x))**n
+#   intsin(a,b,c,d,e,f,A,B,m,n,x)
+#       x*(a+b*sin(e+f*x))**m*(A+B*sin(e+f*x))*(c+d*sin(e+f*x))**n
+#   intsin12(a,b,c,d,e,f,A,B,C,m,n,x)
+#       (a+b*sin(e+f*x))**m*(c+d*sin(e+f*x))**n*(A+B*sin(e+f*x)+C*sin(e+f*x)**2)
+# where the parameters (a,b,c,d,e,f,A,B,C) and exponents (m,n) are arbitrary
+# numeric or symbolic expressions, including zeros and ones.
 
 def test_intsin5():
     x = Symbol("x")
