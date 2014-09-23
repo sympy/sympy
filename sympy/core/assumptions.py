994bdc15a736121c3f88ab76f402e169b755db9d
Chris Smith <smichr@gmail.com>
2014-09-23 06:40:41 -0500

    modify assumptions
    It is currently possible to create a meaningless set of assumptions:
    
        neither
            Dummy(zero=True, finite=False) nor
            Dummy(zero=True, infinite=True)
        raise an error.
    
        If we assume finite == !infinite in assumptions, we
        create an alias: finite=False is the same as infinite=True
        and vice versa. We don't want to do that. What we want is
        behavior like integer and noninteger:
    
        Dummy(integer=0) is not the same thing as Dummy(noninteger=1).
        A noninteger is commutative, complex, hermitian, noninteger,
        nonzero, and real while integer=0 is only commutative -- nothing
        else is known about it (other than that it is not 0).
    
        /!\ In general, it is much easier to comprehend what one means
        by using only true attributes to desccribe it. If something is
        not zero but still complex, say nonzero=True instead of zero=False.
    
        So assumptions.py has been modified to make the following true:
    
        ```
        finite   : 0 < abs(x) < oo
        finite  : 0 <= abs(x) < oo
        infinite : abs(x) == oo
        ```
    
        The same is also true for zero and nonzero. If nonzero is True you
        are talking about a complex number that is not zero. But if zero is
        False you are only talking about a commutative object that is not
        zero.
    
        In addition, we now use is_infinite = True not is_finite = False
        in the few places where this is defined. The former is an affirmation
        of what the object *is* not an indication of what it is not.
    
    In addition, nonzero is also made to be complex and algebraic,
    transcendental and irrational numbers are now marked as finite.

--- sympy/core/assumptions.py
+++ sympy/core/assumptions.py
@@ -15,23 +15,23 @@ being integer, is also real, complex, etc.
 
 Here follows a list of possible assumption names:
 
-    - commutative   - object commutes with any other object with
-                        respect to multiplication operation.
-    - real          - object can have only values from the set
-                        of real numbers
-    - integer       - object can have only values from the set
-                        of integers
-    - algebraic     - object can have only values from the set
-                        of algebraic numbers
+    - commutative    - object commutes with any other object with
+                       respect to multiplication operation.
+    - real           - object can have only values from the set
+                       of real numbers
+    - integer        - object can have only values from the set
+                       of integers
+    - algebraic      - object can have only values from the set
+                       of algebraic numbers
     - transcendental - object can have only values from the set
-                        of transcendental numbers
-    - finite        - object absolute value is bounded
-    - positive      - object can have only positive values
-    - negative      - object can have only negative values
-    - nonpositive      - object can have only nonpositive values
-    - nonnegative      - object can have only nonnegative values
-    - irrational    - object value cannot be represented exactly by Rational
-    - infinite      - object value is arbitrarily large
+                       of transcendental numbers
+    - finite         - object absolute value is bounded
+    - positive       - object can have only positive values
+    - negative       - object can have only negative values
+    - nonpositive    - object can have only nonpositive values
+    - nonnegative    - object can have only nonnegative values
+    - irrational     - object value cannot be represented exactly by Rational
+    - infinite       - object value is arbitrarily large
 
 Implementation note: assumption values are stored in
 ._assumptions dictionary or are returned by getter methods (with
@@ -40,9 +40,9 @@ property decorators) or are attributes of objects/classes.
 Examples
 ========
 
-    >>> from sympy import Symbol
-    >>> Symbol('x', real = True)
-    x
+>>> from sympy import Symbol
+>>> Symbol('x', real = True)
+x
 
 """
 from __future__ import print_function, division
@@ -51,21 +51,22 @@ from sympy.core.facts import FactRules, FactKB
 from sympy.core.core import BasicMeta
 from sympy.core.compatibility import integer_types, with_metaclass
 
+
 # This are the rules under which our assumptions function
 #
 # References
 # ----------
 #
-# negative,     -- http://en.wikipedia.org/wiki/Negative_number
+# negative,      -- http://en.wikipedia.org/wiki/Negative_number
 # nonnegative
 #
-# even, odd     -- http://en.wikipedia.org/wiki/Parity_(mathematics)
-# imaginary     -- http://en.wikipedia.org/wiki/Imaginary_number
-# composite     -- http://en.wikipedia.org/wiki/Composite_number
-# finite        -- http://en.wikipedia.org/wiki/Finite
-#                  https://docs.python.org/3/library/math.html#math.isfinite
-#                  http://docs.scipy.org/doc/numpy/reference/generated/numpy.isfinite.html
-# irrational    -- http://en.wikipedia.org/wiki/Irrational_number
+# even, odd      -- http://en.wikipedia.org/wiki/Parity_(mathematics)
+# imaginary      -- http://en.wikipedia.org/wiki/Imaginary_number
+# composite      -- http://en.wikipedia.org/wiki/Composite_number
+# finite         -- http://en.wikipedia.org/wiki/Finite
+#                   https://docs.python.org/3/library/math.html#math.isfinite
+#                   http://docs.scipy.org/doc/numpy/reference/generated/numpy.isfinite.html
+# irrational     -- http://en.wikipedia.org/wiki/Irrational_number
 # transcendental -- http://en.wikipedia.org/wiki/Transcendental_number
 # ...
 
@@ -74,7 +75,7 @@ _assume_rules = FactRules([
     'integer        ->  rational',
     'rational       ->  real',
     'rational       ->  algebraic',
-    'algebraic      ->  complex',
+    'algebraic      ->  complex & finite',
     'real           ->  complex',
     'real           ->  hermitian',
     'imaginary      ->  complex',
@@ -85,7 +86,7 @@ _assume_rules = FactRules([
     'even           ==  integer & !odd',
 
     'real           ==  negative | zero | positive',
-    'transcendental ==  complex & !algebraic',
+    'transcendental ==  complex & !algebraic & finite',
 
     'negative       ==  nonpositive & nonzero',
     'positive       ==  nonnegative & nonzero',
@@ -99,13 +100,16 @@ _assume_rules = FactRules([
     'prime          ->  integer & positive',
     'composite      ==  integer & positive & !prime',
 
-    'irrational     ==  real & !rational',
+    'irrational     ==  real & !rational & finite',
 
     'imaginary      ->  !real',
 
-    '!finite        ==  infinite',
     'noninteger     ==  real & !integer',
-    '!zero        ==  nonzero',
+
+    'nonzero        ==  !zero & complex',
+
+    'finite         ==  complex & !infinite',
+    'infinite       ==  complex & !finite',
 ])
 
 _assume_defined = _assume_rules.defined_facts.copy()
