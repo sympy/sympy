b5f5904d9a7430c9af2a12589b5c955d2272390e
Sergey B Kirpichev <skirpichev@gmail.com>
2014-10-21 16:22:46 +0400

    Add test for issue 7602
    fixes #7602

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -321,6 +321,8 @@ def test_linear_systemLU():
 
 
 def test_solve_transcendental():
+    from sympy.abc import a, b
+
     assert solve(exp(x) - 3, x) == [log(3)]
     assert set(solve((a*x + b)*(exp(x) - 3), x)) == set([-b/a, log(3)])
     assert solve(cos(x) - y, x) == [-acos(y) + 2*pi, acos(y)]
@@ -407,6 +409,11 @@ def test_solve_transcendental():
 
     # issue 7245
     assert solve(sin(sqrt(x))) == [0, pi**2]
+
+    # issue 7602
+    a, b = symbols('a, b', real=True, negative=False)
+    assert str(solve(Eq(a, 0.5 - cos(pi*b)/2), b)) == \
+        '[-0.318309886183791*acos(-2.0*a + 1.0) + 2.0, 0.318309886183791*acos(-2.0*a + 1.0)]'
 
 
 def test_solve_for_functions_derivatives():
