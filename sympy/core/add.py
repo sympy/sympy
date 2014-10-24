12e4989860216fb9a88ec2dada4aaac83dd00b13
Aaron Meurer <asmeurer@gmail.com>
2014-10-24 13:49:25 -0700

    Remove redundant conditions from Add.flatten
    is_real now implies is_finite.

--- sympy/core/add.py
+++ sympy/core/add.py
@@ -216,11 +216,11 @@ class Add(Expr, AssocOp):
         # oo, -oo
         if coeff is S.Infinity:
             newseq = [f for f in newseq if not
-                      (f.is_nonnegative or f.is_real and f.is_finite)]
+                      (f.is_nonnegative or f.is_real)]
 
         elif coeff is S.NegativeInfinity:
             newseq = [f for f in newseq if not
-                      (f.is_nonpositive or f.is_real and f.is_finite)]
+                      (f.is_nonpositive or f.is_real)]
 
         if coeff is S.ComplexInfinity:
             # zoo might be
