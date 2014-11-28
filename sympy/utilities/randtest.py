3025699a74dbefa4799978776bac7efcedd93884
Chris Smith <smichr@gmail.com>
2014-11-28 13:16:07 -0600

    add numerically_zero to randtest.py
    It was initially tempting to compute an approximation for the error in
    the expression by computing a value at n=2 and another at n=4 and taking
    the difference, but the following shows how that can be unreliable:
    with
    >>> eq = -x - 1 + (-x**2 + 1)/(-x + 1) and
    >>> r = {x: 148683004974611/S(50000000000000) -
    ...         144151475495133*I/1000000000000000}
    >>> for maxn in range(1,100):
    ...   a, b = [z.n(i,maxn=maxn) for i in (2,4)]
    ...   if abs(b) > abs(b-a): print maxn
    ...
    10
    11
    12
    >>>
    
    So at those values of maxn it looks like the computed value is larger than
    the error in the computation. Unless there is a sound way to compute the
    error in the expression (quickly) then this, at least, is *not* a way to do it.

--- sympy/utilities/randtest.py
+++ sympy/utilities/randtest.py
@@ -5,7 +5,7 @@ from __future__ import print_function, division
 from random import uniform
 import random
 
-from sympy import I, nsimplify, Tuple, Symbol
+from sympy import I, nsimplify, Tuple, Symbol, sympify
 from sympy.core.compatibility import is_sequence, as_int
 
 
@@ -38,6 +38,62 @@ def comp(z1, z2, tol):
         return diff/az1 <= tol
     else:
         return diff <= tol
+
+
+def numerically_zero(f, number=10, prec=10, a=2, b=-1, c=3, d=1, maxfail=100):
+    """
+    Test numerically if ``|f| < 10**-prec`` by replacing all symbols (``number``
+    times) with random complex numbers and evaluating the result. If a value
+    is ever computed that fails the test, False will be returned; otherwise
+    None will be returned. Failures to compute a finite result will not be
+    counted and will be allowed ``maxfail`` times before an error message
+    is raised.
+
+    Examples
+    ========
+
+    >>> from sympy import sin, cos, pi, S, Add
+    >>> from sympy.abc import x
+    >>> from sympy.utilities.randtest import numerically_zero as is_zero
+    >>> is_zero(sin(x)**2 + cos(x)**2)
+    False
+    >>> is_zero(sin(x)**2 + cos(x)**2 - 1)
+
+    Better detection of nonzero values is obtained by using larger values
+    of ``prec``:
+
+    >>> eps = lambda e: Add(pi, 10**S(e), -pi, evaluate=False)
+    >>> is_zero(eps(-11))
+    >>> is_zero(eps(-11), prec=12)
+    False
+
+    See Also
+    ========
+    random_complex_number
+    """
+    from sympy.core.function import _coeff_isneg
+    tol = sympify(10)**-prec
+    f = sympify(f)
+    if not f.args:
+        if f.is_number and abs(f) <= tol:
+            return True
+        return False
+    s = f.free_symbols
+    did = 0
+    for i in range(number + maxfail):
+        reps = dict(zip(s,
+            [random_complex_number(a, b, c, d, rational=True) for i in s]))
+        check = f.xreplace(reps).n(2, maxn=prec)
+        if not check.is_finite == True:
+            # reject attempts where nan or infinite quantites were calculated
+            continue
+        if abs(check) > tol:
+            return False
+        did += 1
+        if not s or did == number:
+            break
+    else:
+        raise NotImplementedError('unable to find good values for calculation')
 
 
 def verify_numerically(f, g, z=None, tol=1.0e-6, a=2, b=-1, c=3, d=1):
