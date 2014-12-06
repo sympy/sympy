9ade047d4b11e00c2f996c51d7ab5d6586614dd2
Sergey B Kirpichev <skirpichev@gmail.com>
2014-12-06 14:51:03 +0300

    Add examples for summation with symbolic indices
    fixes #8549

--- sympy/concrete/summations.py
+++ sympy/concrete/summations.py
@@ -73,7 +73,7 @@ class Sum(AddWithLimits,ExprWithIntLimits):
     ========
 
     >>> from sympy.abc import i, k, m, n, x
-    >>> from sympy import Sum, factorial, oo
+    >>> from sympy import Sum, factorial, oo, IndexedBase, Function
     >>> Sum(k,(k,1,m))
     Sum(k, (k, 1, m))
     >>> Sum(k,(k,1,m)).doit()
@@ -88,6 +88,18 @@ class Sum(AddWithLimits,ExprWithIntLimits):
     Piecewise((1/(-x + 1), Abs(x) < 1), (Sum(x**k, (k, 0, oo)), True))
     >>> Sum(x**k/factorial(k),(k,0,oo)).doit()
     exp(x)
+
+    Here are examples to do summation with symbolic indices.  You
+    can use either Function of IndexedBase classes:
+
+    >>> f = Function('f')
+    >>> Sum(f(n), (n, 0, 3)).doit()
+    f(0) + f(1) + f(2) + f(3)
+    >>> Sum(f(n), (n, 0, oo)).doit()
+    Sum(f(n), (n, 0, oo))
+    >>> f = IndexedBase('f')
+    >>> Sum(f[n]**2, (n, 0, 3)).doit()
+    f[0]**2 + f[1]**2 + f[2]**2 + f[3]**2
 
     An example showing that the symbolic result of a summation is still
     valid for seemingly nonsensical values of the limits. Then the Karr
