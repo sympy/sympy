2f1e5630185dbfcf6e2b2978c628f169021dd122
Chris Smith <smichr@gmail.com>
2014-12-27 04:43:05 -0600

    minor geometry test mods

--- sympy/geometry/tests/test_geometry.py
+++ sympy/geometry/tests/test_geometry.py
@@ -1664,9 +1664,8 @@ def test_idiff():
     assert ans == idiff(circ, [y], x, 3).simplify()
     assert idiff(circ, y, x, 3).simplify() == ans
     explicit  = 12*x/sqrt(-x**2 + 4)**5
-    assert ans.subs(y, solve(circ, y)[0]).simplify() == \
-        explicit
-    assert explicit in [sol.diff(x, 3).simplify() for sol in solve(circ, y)]
+    assert ans.subs(y, solve(circ, y)[0]).equals(explicit)
+    assert True in [sol.diff(x, 3).equals(explicit) for sol in solve(circ, y)]
     assert idiff(x + t + y, [y, t], x) == -Derivative(t, x) - 1
 
 
