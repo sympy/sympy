e3da035e14d04ba9a6011986c2373de25a0772dc
Sergey B Kirpichev <skirpichev@gmail.com>
2015-01-11 21:03:31 +0300

    Fix trivial series expansion (for constants)
    Closes: #8805

--- sympy/series/tests/test_series.py
+++ sympy/series/tests/test_series.py
@@ -148,3 +148,7 @@ def test_exp_product_positive_factors():
     assert series(exp(x), x, n=8) == 1 + a*b + a**2*b**2/2 + \
         a**3*b**3/6 + a**4*b**4/24 + a**5*b**5/120 + a**6*b**6/720 + \
         a**7*b**7/5040 + O(a**8*b**8, a, b)
+
+
+def test_issue_8805():
+    assert series(1, n=8) == 1
