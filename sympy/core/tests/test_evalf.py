2e763c458caf34eb065e929f29e2473a7876f1aa
Chris Smith <smichr@gmail.com>
2015-01-21 13:02:51 -0600

    watch for all rationals in calc_part
    The number reresented with n, c, p in calc_part is [1, -1][n]*c*2**p.
    with n,c,p all integers. A non-integer will have p < 0 so that test was
    added to the existing test. Th c!=1 and p!=0 test is for a computed
    integer; something that was a pure integer would have c=1 or p=0.

--- sympy/core/tests/test_evalf.py
+++ sympy/core/tests/test_evalf.py
@@ -1,8 +1,9 @@
 from sympy import (Abs, Add, atan, ceiling, cos, E, Eq, exp, factorial,
                    fibonacci, floor, Function, GoldenRatio, I, Integral,
                    integrate, log, Mul, N, oo, pi, Pow, product, Product,
-                   Rational, S, Sum, sin, sqrt, sstr, sympify)
-from sympy.core.evalf import complex_accuracy, PrecisionExhausted, scaled_zero
+                   Rational, S, Sum, sin, sqrt, sstr, sympify, Symbol)
+from sympy.core.evalf import (complex_accuracy, PrecisionExhausted,
+    scaled_zero, get_integer_part)
 from sympy.core.compatibility import long
 from mpmath import inf, ninf
 from sympy.abc import n, x, y
@@ -455,3 +456,16 @@ def test_issue_8821_highprec_from_str():
     assert Abs(sin(p)) < 1e-15
     p = N(s, 64)
     assert Abs(sin(p)) < 1e-64
+
+
+def test_issue_8853():
+    p = Symbol('x', even=True, positive=True)
+    assert floor(-p - S.Half).is_even == False
+    assert floor(-p + S.Half).is_even == True
+    assert ceiling(p - S.Half).is_even == True
+    assert ceiling(p + S.Half).is_even == False
+
+    assert get_integer_part(S.Half, -1, {}, True) == (0, 0)
+    assert get_integer_part(S.Half, 1, {}, True) == (1, 0)
+    assert get_integer_part(-S.Half, -1, {}, True) == (-1, 0)
+    assert get_integer_part(-S.Half, 1, {}, True) == (0, 0)
