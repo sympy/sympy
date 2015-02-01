0952fcf0bde1802e794851e7e76dc4f235bf7f5b
Chai Wah Wu <cwwuieee@gmail.com>
2015-02-01 09:14:31 -0500

    trigonometric: generate simpler expressions for trigfunctions(k*pi/n)
    For n = 12, 15, 40, and some other values, generate expressions of
    sin, cos, tan, cot(k*pi/n) with smaller number of nested sqrt.
    For example currently cos(pi/12), tan(pi/8), and cot(pi/12) return
    sqrt(sqrt(3)/4 + 1/2) , sqrt(-sqrt(2)/4 + 1/2)/sqrt(sqrt(2)/4 + 1/2)
    and sqrt(sqrt(3)/4 + 1/2)/sqrt(-sqrt(3)/4 + 1/2) resp.
    The current patch returns sqrt(2)/4 + sqrt(6)/4, sqrt(2)*(-sqrt(2)/2 + 1)
    and sqrt(3) + 2 resp.
    
    * simplify tan(k*pi/2n) and cot(k*pi/2n) for values of n such that
      explicit expression exists.
    * added additional tests to test_trigonometric and changed tests in numbers,
      geometry and pretty
    * Since tan(pi/15) and cot(pi/15) are evaluated by current patch, the tests
    for them are moved from test_tan_rewrite to test_tan and from
    test_cot_rewrite to test_cot.  Also the tests are changed somewhat since
    even though cos(pi/15)*tan(pi/15)  and sin(pi/15) return mathematically
    equivalent equations, they do not cancel out under simplify.

--- sympy/printing/pretty/tests/test_pretty.py
+++ sympy/printing/pretty/tests/test_pretty.py
@@ -4297,8 +4297,8 @@ atan2⎜───────, ╲╱ x ⎟\n\
 def test_pretty_geometry():
     e = Segment((0, 1), (0, 2))
     assert pretty(e) == 'Segment(Point(0, 1), Point(0, 2))'
-    e = Ray((1, 1), angle=4.05*pi)
-    assert pretty(e) == 'Ray(Point(1, 1), Point(2, tan(pi/20) + 1))'
+    e = Ray((1, 1), angle=4.02*pi)
+    assert pretty(e) == 'Ray(Point(1, 1), Point(2, tan(pi/50) + 1))'
 
 
 def test_expint():
