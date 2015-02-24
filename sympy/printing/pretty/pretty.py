1bd9fc0b0de2777c00de866129bb69db3622d344
Swachhand Lokhande <swachhandl95@gmail.com>
2015-02-24 19:23:12 +0530

    pretty printing: improved `Trace`
    Defined a function _print_Trace in sympy.printing.pretty.pretty.PrettyPrinter
    so that this function is used to print the Trace rather than the
    emptyPrinter.
    
    Example:
    
    Before:
    
    In [70]: X = ImmutableMatrix([[1, 2], [3, 4]])
    
    In [71]: w = Trace(X) + Trace(2*X)
    
    In [72]: pprint(w)
    Trace(Matrix([ + Trace(Matrix([
       [1, 2],          [2, 4],
      [3, 4]]))        [6, 8]]))
    
    After:
    
    In [70]: X = ImmutableMatrix([[1, 2], [3, 4]])
    
    In [71]: w = Trace(X) + Trace(2*X)
    
    In [72]: pprint(w)
      ⎛⎡1  2⎤⎞ +   ⎛⎡2  4⎤⎞
    tr⎜⎢    ⎥⎟   tr⎜⎢    ⎥⎟
      ⎝⎣3  4⎦⎠     ⎝⎣6  8⎦⎠
    
    The position of the operators in between the Traces are off position.
    Still need to figure out how to fix that.

--- sympy/printing/pretty/pretty.py
+++ sympy/printing/pretty/pretty.py
@@ -639,6 +639,14 @@ class PrettyPrinter(Printer):
         D = self._print_matrix_contents(e)
         D = prettyForm(*D.parens('[', ']'))
         return D
+
+    def _print_Trace(self, e):
+        mat = e._args[0]
+        D = self._print_MatrixBase(mat)
+        D = prettyForm(*D.parens('(', ')'))
+        D = prettyForm(*D.left('\n'*(D.height()//2) + 'tr'))
+        return D
+
     _print_ImmutableMatrix = _print_MatrixBase
     _print_Matrix = _print_MatrixBase
 
