60410dba9bfb60d8e009dddebda945d4f088adc7
Sartaj Singh <singhsartaj94@gmail.com>
2015-02-28 01:46:27 +0530

    Implemented fourier sine/cosine series expansion
    * Added module fourier in sympy.series
    * Added SeqFormula class, that knows how to generate coefficients based on
    a formula
    * Added FourierSeries Class,
      This Class represents a fourier series expansion.
      Contains methods for getting the general representation of series
      and for getting the first n terms.
    * Added fourier_series function.
      It is a wrapper that returns the first n terms of a series or an
      infinite generator of terms

--- sympy/series/fourier.py
+++ sympy/series/fourier.py
@@ -1,14 +1,36 @@
 """Fouries series expansion"""
 
-from sympy.core import oo, Symbol
+from sympy import oo, pi
+from sympy.core.compatibility import range
+from sympy.core.sympify import sympify
+from sympy.core.symbol import Symbol
+from sympy.core.expr import Expr
+from sympy.core.add import Add
+from sympy.core.compatibility import integer_types
+from sympy.integrals import integrate
+from sympy.functions.elementary.trigonometric import cos, sin
+from sympy.concrete.summations import Sum
 
 class SeqFormula(object):
     """
-    Creates a generator of coefficients based on a formula
+    Generates coefficients of a sequence based on a formula
+
+    Examples
+    ========
+
+    >>> from sympy.abc import x, n
+    >>> from sympy import oo
+    >>> from sympy.series.fourier import SeqFormula
+    >>> s = SeqFormula(x*(x+1), x, (0, oo))
+    >>> s.coeff(n)
+    n*(n + 1)
+    >>> s.coeff(10)
+    110
+
     """
     def __init__(self, formula, x, bounds=None):
-        self._formula = formula
-        self.x = x
+        self._formula = sympify(formula)
+        self.x = sympify(x)
         self._coeff = {}
         if not bounds:
             bounds = (0, oo)
@@ -16,9 +38,13 @@ class SeqFormula(object):
         self._current = bounds[0]
 
     def coeff(self, index):
-        if index > self._upper or index < self._lower:
-            raise IndexError("index %d not in (%d, %d)" % (index, self._lower, self._upper))
-        if self._coeff.has_key(index):
+        """
+        returns the nth coefficient
+        """
+        if isinstance(index, integer_types):
+            if index > self._upper or index < self._lower:
+                raise IndexError("index %d not in (%d, %d)" % (index, self._lower, self._upper))
+        if index in self._coeff:
             return self._coeff[index]
         self._coeff[index] = self._eval_coeff(index)
         return self._coeff[index]
@@ -36,3 +62,106 @@ class SeqFormula(object):
         self._coeff[self._current] = val
         self._current += 1
         return val
+    __next__ = next
+
+class FourierSeries(Expr):
+    """Represents Fourier sine/cosine series
+
+    Examples
+    ========
+
+    >>> from sympy.abc import x
+    >>> from sympy import pi
+    >>> from sympy.series.fourier import FourierSeries
+    >>> fseries = FourierSeries(x, x, (-pi, pi))
+    >>> fseries.as_series()
+    Sum(-2*(-1)**n*sin(n*x)/n, (n, 1, oo))
+    >>> fseries.as_nseries(n=3)
+    2*sin(x) - sin(2*x) + 2*sin(3*x)/3
+
+    """
+    def __new__(cls, f, x, bounds=(-pi, pi)):
+        f = sympify(f)
+        x = sympify(x)
+
+        lower_bound, upper_bound = bounds
+        L = upper_bound - lower_bound
+        int_tuple = (x, lower_bound, upper_bound)
+
+        n = Symbol('n', integer=True, positive=True)
+        a0 = 2 * (integrate(f, int_tuple) / L)
+
+        cos_term = cos(2*n*pi*x / L)
+        seq_an = SeqFormula((2*integrate(f * cos_term, int_tuple) / L), n, (1, oo))
+
+        sin_term = sin(2*n*pi*x / L)
+        seq_bn = SeqFormula((2*integrate(f * sin_term, int_tuple) / L), n, (1, oo))
+
+        obj = Expr.__new__(cls, f, x)
+        obj.x, obj.n, obj.bounds, obj.L = x, n, bounds, L
+        obj.seq_an, obj.seq_bn = seq_an, seq_bn
+        obj.a0, obj.an, obj.bn = a0, seq_an.coeff(n), seq_bn.coeff(n)
+        return obj
+
+    def free_symbols(self):
+        return self.x.free_symbols
+
+    def __getitem__(self, index):
+        if isinstance(index, integer_types):
+            if index < 0:
+                raise IndexError("Index should be in (0, Inf)")
+            return self._eval_n_term(index)
+        elif isinstance(index, slice):
+            return [self._eval_n_term(i) for i in range(index.start, index.stop, index.step or 1)]
+
+    def as_series(self):
+        """
+        Represents series as:
+        (a0 / 2) + Sum(an*cos(n*x), (n,1,oo)) + Sum(bn*sin(n*x), (n,1,oo))
+        """
+        n = self.n
+        cos_term = cos(2*n*pi*self.x / self.L)
+        sin_term = sin(2*n*pi*self.x / self.L)
+        an = self.seq_an.coeff(n).simplify()
+        bn = self.seq_bn.coeff(n).simplify()
+        fseries = self.a0 / 2
+        if an != 0:
+            fseries += Sum(an * cos_term, (n, 1, oo))
+        if bn != 0:
+            fseries += Sum(bn * sin_term, (n, 1, oo))
+        return fseries
+
+    def _eval_n_term(self, n):
+        """
+        evaluates the nth term
+        """
+        cos_term = cos(2*n*pi*self.x / self.L)
+        sin_term = sin(2*n*pi*self.x / self.L)
+        if n == 0:
+            return self.a0 / 2
+        an = self.seq_an.coeff(n)
+        bn = self.seq_bn.coeff(n)
+        return Add(*[an*cos_term, bn*sin_term])
+
+    def as_nseries(self, n=6):
+        """
+        Gives the terms till n = num
+        """
+        return Add(*self[0:n + 1])
+
+def fourier_series(f, x, bounds=(-pi, pi), n=6):
+    """
+    Gives fourier series expansion
+    if n is given, n terms will be returned
+    if n is None, returns an infinite generator of terms
+    """
+    fseries = FourierSeries(f, x, bounds)
+    if not n is None:
+        return fseries.as_nseries(n)
+    else:
+        def yield_fourier_series(fseries):
+            i = 0
+            while True:
+                yield fseries[i]
+                i += 1
+        return yield_fourier_series(fseries)
--- sympy/series/tests/test_fourier.py
+++ sympy/series/tests/test_fourier.py
@@ -1,6 +1,6 @@
-from sympy import integrate, pi, sin, cos
-from sympy.series.fourier import SeqFormula
-from sympy.abc import x, y, z
+from sympy import pi, sin, cos, Piecewise, Rational
+from sympy.series.fourier import SeqFormula, FourierSeries, fourier_series
+from sympy.abc import x, n
 from sympy.utilities.pytest import raises
 from sympy.core.compatibility import range
 
@@ -8,15 +8,46 @@ def test_SeqFormula_generator():
     seq = SeqFormula(x*(x+1), x, (0, 5))
     assert [val for val in seq] == [n*(n+1) for n in range(6)]
 
-    seq2 = SeqFormula(integrate(x * cos(y*x), (x, -pi, pi)), y, (0, 5))
-    assert [val for val in seq2] == [integrate(x * cos(n*x), (x, -pi, pi)) for n in range(6)]
-
-    seq3 = SeqFormula(integrate(x * sin(y*x), (x, -pi, pi)), y, (0, 5))
-    assert [val for val in seq3] == [integrate(x * sin(n*x), (x, -pi, pi)) for n in range(6)]
+    seq2 = SeqFormula('x*(x+1)', 'x', (0, 5))
+    assert [val for val in seq2] == [n*(n+1) for n in range(6)]
 
 def test_SeqFormula_coeff():
-    seq = SeqFormula(sin(x), x, (0, 5))
-    assert seq.coeff(3) == sin(3)
+    seq = SeqFormula(x*(x+1), x, (0, 5))
+    assert seq.coeff(3) == 12
 
     raises(IndexError, lambda: seq.coeff(10))
     raises(IndexError, lambda: seq.coeff(-1))
+
+def test_FourierSeries_even_function():
+    f = FourierSeries(x**2, x)
+    assert f.as_nseries(n=3) == -4*cos(x) + cos(2*x) - 4*cos(3*x)/9 \
+            + pi**2/3
+    assert f[0] == pi**2/3
+    assert f[1] == -4*cos(x)
+
+def test_FourierSeries_odd_function():
+    f = FourierSeries(x, x)
+    assert f.as_nseries(n=3) == 2*sin(x) - sin(2*x) + 2*sin(3*x)/3
+    assert f[0] == 0
+    assert f[1] == 2*sin(x)
+
+def test_FourierSeries_simple_functions():
+    pe = Piecewise((0, x <= 0), (x, True))
+    f = FourierSeries(pe, x, (-2, 2))
+    assert f.as_nseries(n=3) == 2*sin(pi*x/2)/pi - sin(pi*x)/pi \
+            + 2*sin(3*pi*x/2)/(3*pi) - 4*cos(pi*x/2)/pi**2 \
+            - 4*cos(3*pi*x/2)/(9*pi**2) + Rational(1,2)
+    assert f[0] == Rational(1,2)
+    assert f[1] == 2*sin(pi*x/2)/pi - 4*cos(pi*x/2)/pi**2
+
+def test_fourier_series_nterms():
+    f = FourierSeries(x, x)
+    fn = fourier_series(x, x, n=3)
+    assert f.as_nseries(n=3) == fn
+
+def test_fourier_series_generator():
+    f = FourierSeries(x, x)
+    fn = fourier_series(x, x, n=None)
+    assert next(fn) == f[0]
+    assert next(fn) == f[1]
+    assert next(fn) == f[2]
