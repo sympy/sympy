c75b6733cee4bccd5457f24e19e43561100fb04a
Chris Smith <smichr@gmail.com>
2015-02-26 21:36:10 -0600

    8155: add test for issue

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -1755,7 +1755,7 @@ def test_issue_8828():
     assert p == q == r
 
 
-def test_issue_2840():
+def test_issue_2840_8155():
     assert solve(sin(3*x) + sin(6*x)) == [
         0, -pi, pi, 2*pi, -2*I*log(-(-1)**(S(1)/9)), -2*I*log(-(-1)**(S(2)/9)),
         -2*I*log((-1)**(S(7)/9)), -2*I*log((-1)**(S(8)/9)), -2*I*log(-S(1)/2 -
@@ -1767,3 +1767,6 @@ def test_issue_2840():
         I*cos(pi/18)), -2*I*log(sin(pi/18) + I*cos(pi/18)),
         -2*I*log(exp(-2*I*pi/9)), -2*I*log(exp(-I*pi/9)),
         -2*I*log(exp(I*pi/9)), -2*I*log(exp(2*I*pi/9))]
+    assert solve(2*sin(x) - 2*sin(2*x)) == [
+        0, -pi, pi, -2*I*log(-sqrt(3)/2 - I/2), -2*I*log(-sqrt(3)/2 + I/2),
+        -2*I*log(sqrt(3)/2 - I/2), -2*I*log(sqrt(3)/2 + I/2)]
