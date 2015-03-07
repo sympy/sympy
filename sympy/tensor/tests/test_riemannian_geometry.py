ac4ecefc072e7285e491b0d75bb150d3c427c2fb
KsenijaM <ksuxa3009ru@gmail.com>
2015-03-07 22:13:46 +0700

    Added tests

--- sympy/tensor/tests/test_riemannian_geometry.py
+++ sympy/tensor/tests/test_riemannian_geometry.py
@@ -1,4 +1,5 @@
 from sympy.tensor import *
+from sympy.matrices import Matrix
 from sympy import symbols, cos, sin
 
 
@@ -30,7 +31,7 @@ def test_christoffel_1_gtnsr():
     x1, x2 = symbols('x1, x2')
     var_list = [x1, x2]
     g = Arraypy([2, 2, 1])
-    g = g.To_tensor((-1, -1))
+    g = g.to_tensor((-1, -1))
     g[1, 1] = cos(x2)**2
     g[1, 2] = 0
     g[2, 1] = 0
@@ -45,7 +46,7 @@ def test_christoffel_1_gtnsr():
     res_arr[2, 2, 1] = 0
     res_arr[2, 1, 2] = 0
     res_arr[2, 2, 2] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1)
+    res_ten = res_arr.to_tensor((-1, -1, -1))
 
     print('test_christoffel_1_gtnsr_t  <=== actual test code')
     assert christoffel_1(g, var_list) == res_ten
@@ -75,7 +76,7 @@ def test_christoffel_1_gm():
     res_arr[1, 1, 0] = 0
     res_arr[1, 0, 1] = 0
     res_arr[1, 1, 1] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1)
+    res_ten = res_arr.to_tensor((-1, -1, -1))
 
     print('test_christoffel_1_gm_t  <=== actual test code')
     assert christoffel_1(g, var_list) == res_ten
@@ -95,13 +96,13 @@ def test_christoffel_2_gtnsr():
     x1, x2 = symbols('x1, x2')
     var_list = [x1, x2]
     g = Arraypy([2, 2, 1])
-    g = g.To_tensor((-1, -1))
+    g = g.to_tensor((-1, -1))
     g[1, 1] = cos(x2)**2
     g[1, 2] = 0
     g[2, 1] = 0
     g[2, 2] = 1
 
-    res_arr = Arraypy([3, 2, 0])
+    res_arr = Arraypy([3, 2, 1])
     res_arr[1, 1, 1] = 0
     res_arr[1, 1, 2] = sin(x2) * cos(x2)
     res_arr[1, 2, 1] = -sin(x2) / cos(x2)
@@ -110,7 +111,7 @@ def test_christoffel_2_gtnsr():
     res_arr[2, 2, 1] = 0
     res_arr[2, 1, 2] = 0
     res_arr[2, 2, 2] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1)
+    res_ten = res_arr.to_tensor((-1, -1, -1))
 
     print('test_christoffel_2_gtnsr_t  <=== actual test code')
     assert christoffel_2(g, var_list) == res_ten
@@ -140,7 +141,7 @@ def test_christoffel_2_gm():
     res_arr[1, 1, 0] = 0
     res_arr[1, 0, 1] = 0
     res_arr[1, 1, 1] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1)
+    res_ten = res_arr.to_tensor((-1, -1, -1))
 
     print('test_christoffel_2_gm_t  <=== actual test code')
     assert christoffel_2(g, var_list) == res_ten
@@ -167,16 +168,16 @@ def test_covar_der():
     res_arr[0, 1] = x1 * x2**3 * sin(x2) * cos(x2) + 1
     res_arr[1, 0] = -x1 * x2**3 * sin(x2) / cos(x2) + 3 * x1 * x2**2
     res_arr[1, 1] = sin(x2)
-    res_ten = res_arr.to_tensor(-1, 1)
+    res_ten = res_arr.to_tensor((-1, 1))
 
     print('test_covar_der_t  <=== actual test code')
     assert covar_der(X, g, var_list) == res_ten
     assert isinstance(covar_der(X, g, var_list), Tensor)
-    assert covar_der(X, g, var_list).type_pq == (-1, 1)
+    assert covar_der(X, g, var_list).type_pq == (1, 1)
 
     assert covar_der(X, g, var_list, 't') == res_ten
     assert isinstance(covar_der(X, g, var_list, 't'), Tensor)
-    assert covar_der(X, g, var_list).type_pq == (-1, 1)
+    assert covar_der(X, g, var_list, 't').type_pq == (1, 1)
 
     print('test_covar_der_a  <=== actual test code')
     assert covar_der(X, g, var_list, 'a') == res_arr
@@ -200,11 +201,11 @@ def test_covar_der_XY():
     print('test_covar_der_XY_t  <=== actual test code')
     assert covar_der_XY(X, Y, g, var_list) == res_ten
     assert isinstance(covar_der_XY(X, Y, g, var_list), Tensor)
-    assert covar_der_XY(X, Y, g, var_list).type_pq == (1)
+    assert covar_der_XY(X, Y, g, var_list).type_pq == (1, 0)
 
     assert covar_der_XY(X, Y, g, var_list, 't') == res_ten
     assert isinstance(covar_der_XY(X, Y, g, var_list, 't'), Tensor)
-    assert covar_der_XY(X, Y, g, var_list).type_pq == (1)
+    assert covar_der_XY(X, Y, g, var_list).type_pq == (1, 0)
 
     print('test_covar_der_XY_a  <=== actual test code')
     assert covar_der_XY(X, Y, g, var_list, 'a') == res_arr
@@ -215,7 +216,7 @@ def test_riemann_gtnsr():
     x1, x2 = symbols('x1, x2')
     var_list = [x1, x2]
     g = Arraypy([2, 2, 1])
-    g = g.To_tensor((-1, -1))
+    g = g.to_tensor((-1, -1))
     g[1, 1] = cos(x2)**2
     g[1, 2] = 0
     g[2, 1] = 0
@@ -238,7 +239,7 @@ def test_riemann_gtnsr():
     res_arr[2, 1, 2, 1] = -1
     res_arr[2, 2, 1, 2] = 0
     res_arr[2, 2, 2, 2] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1, 1)
+    res_ten = res_arr.to_tensor((-1, -1, -1, 1))
 
     print('test_riemann_gtnsr_t  <=== actual test code')
     assert riemann(g, var_list) == res_ten
@@ -259,24 +260,24 @@ def test_riemann_gm():
     var_list = [x1, x2]
     g = Matrix([[cos(x2)**2, 0], [0, 1]])
 
-    res_arr = Arraypy([4, 2, 1])
+    res_arr = Arraypy([4, 2, 0])
+    res_arr[0, 0, 0, 0] = 0
+    res_arr[0, 0, 0, 1] = 0
+    res_arr[0, 0, 1, 1] = 0
+    res_arr[0, 1, 1, 1] = 0
     res_arr[1, 1, 1, 1] = 0
-    res_arr[1, 1, 1, 2] = 0
-    res_arr[1, 1, 2, 1] = 0
-    res_arr[1, 1, 2, 2] = 0
-    res_arr[1, 2, 1, 1] = 0
-    res_arr[1, 2, 2, 1] = 1
-    res_arr[1, 2, 2, 2] = 0
-    res_arr[1, 2, 1, 2] = -cos(x2)**2
-    res_arr[2, 1, 1, 1] = 0
-    res_arr[2, 1, 1, 2] = cos(x2)**2
-    res_arr[2, 2, 1, 1] = 0
-    res_arr[2, 2, 2, 1] = 0
-    res_arr[2, 1, 2, 2] = 0
-    res_arr[2, 1, 2, 1] = -1
-    res_arr[2, 2, 1, 2] = 0
-    res_arr[2, 2, 2, 2] = 0
-    res_ten = res_arr.to_tensor(-1, -1, -1, 1)
+    res_arr[1, 1, 1, 0] = 0
+    res_arr[1, 1, 0, 0] = 0
+    res_arr[1, 0, 0, 0] = 0
+    res_arr[1, 0, 1, 0] = -1
+    res_arr[0, 1, 0, 1] = -cos(x2)**2
+    res_arr[1, 1, 0, 1] = 0
+    res_arr[0, 0, 1, 0] = 0
+    res_arr[1, 0, 1, 1] = 0
+    res_arr[0, 1, 0, 0] = 0
+    res_arr[0, 1, 1, 0] = 1
+    res_arr[1, 0, 0, 1] = cos(x2)**2
+    res_ten = res_arr.to_tensor((-1, -1, -1, 1))
 
     print('test_riemann_gm_t  <=== actual test code')
     assert riemann(g, var_list) == res_ten
@@ -313,23 +314,23 @@ def test_ricci_riemtnsr1():
     riemann_arr[2, 1, 2, 1] = -1
     riemann_arr[2, 2, 1, 2] = 0
     riemann_arr[2, 2, 2, 2] = 0
-    riemann_ten = riemann_arr.to_tensor(-1, -1, -1, 1)
+    riemann_ten = riemann_arr.to_tensor((-1, -1, -1, 1))
 
     res_arr = Arraypy([2, 2, 1])
     res_arr[1, 1] = cos(x2)**2
     res_arr[1, 2] = 0
     res_arr[2, 2] = 0
     res_arr[2, 2] = 1
-    res_ten = res_ten.to_tensor(-1, -1)
+    res_ten = res_arr.to_tensor((-1, -1))
 
     print('test_ricci_riemtnsr1_t  <=== actual test code')
     assert ricci(riemann_ten, var_list) == res_ten
     assert isinstance(ricci(riemann_ten, var_list), Tensor)
-    assert ricci(riemann_arr, var_list).type_pq == (-1, -1)
+    assert ricci(riemann_ten, var_list).type_pq == (0, 2)
 
-    assert ricci(g, 't') == res_ten
+    assert ricci(riemann_ten, var_list, 't') == res_ten
     assert isinstance(ricci(riemann_ten, var_list, 't'), Tensor)
-    assert ricci().type_pq == (-1, -1)
+    assert ricci(riemann_ten, var_list, 't').type_pq == (0, 2)
 
     print('test_ricci_riemtnsr1_a  <=== actual test code')
     assert ricci(riemann_ten, var_list, 'a') == res_arr
@@ -340,7 +341,7 @@ def test_ricci_riemtnsr0():
     x1, x2 = symbols('x1, x2')
     var_list = [x1, x2]
 
-    riemann_arr = Arraypy([4, 2, 1])
+    riemann_arr = Arraypy([4, 2, 0])
     riemann_arr[0, 0, 0, 0] = 0
     riemann_arr[0, 0, 0, 1] = 0
     riemann_arr[0, 0, 1, 1] = 0
@@ -357,27 +358,27 @@ def test_ricci_riemtnsr0():
     riemann_arr[0, 1, 0, 0] = 0
     riemann_arr[0, 1, 1, 0] = 1
     riemann_arr[1, 0, 0, 1] = cos(x2)**2
-    riemann_ten = riemann_arr.to_tensor(-1, -1, -1, 1)
+    riemann_ten = riemann_arr.to_tensor((-1, -1, -1, 1))
 
-    res_arr = Arraypy([2, 2, 1])
+    res_arr = Arraypy([2, 2, 0])
     res_arr[0, 0] = cos(x2)**2
     res_arr[0, 1] = 0
     res_arr[1, 1] = 0
     res_arr[1, 1] = 1
-    res_ten = res_ten.to_tensor(-1, -1)
+    res_ten = res_arr.to_tensor((-1, -1))
 
     print('test_ricci_riemtnsr0_t  <=== actual test code')
     assert ricci(riemann_ten, var_list) == res_ten
     assert isinstance(ricci(riemann_ten, var_list), Tensor)
-    assert ricci(riemann_arr, var_list).type_pq == (-1, -1)
+    assert ricci(riemann_ten, var_list).type_pq == (0, 2)
 
-    assert ricci(g, 't') == res_ten
-    assert isinstance(ricci(g, 't'), Tensor)
-    assert ricci().type_pq == (-1, -1)
+    assert ricci(riemann_ten, var_list, 't') == res_ten
+    assert isinstance(ricci(riemann_ten, var_list, 't'), Tensor)
+    assert ricci(riemann_ten, var_list, 't').type_pq == (0, 2)
 
     print('test_ricci_riemtnsr0_a  <=== actual test code')
-    assert ricci(g, 'a') == res_arr
-    assert isinstance(ricci(g, 'a'), Arraypy)
+    assert ricci(riemann_ten, var_list, 'a') == res_arr
+    assert isinstance(ricci(riemann_ten, var_list, 'a'), Arraypy)
 
 
 def test_scal_curv():
@@ -386,30 +387,26 @@ def test_scal_curv():
 
     g = Matrix([[cos(x2)**2, 0], [0, 1]])
 
-    g_ten = Arraypy([2, 2, 1])
-    g_ten = g_ten.To_tensor((-1, -1))
+    g_ten = Arraypy([2, 2, 1]).to_tensor((-1, -1))
     g_ten[1, 1] = cos(x2)**2
     g_ten[1, 2] = 0
     g_ten[2, 1] = 0
     g_ten[2, 2] = 1
 
-    g_ten0 = Arraypy([2, 2, 1])
-    g_ten0 = g_ten0.To_tensor((-1, -1))
+    g_ten0 = Arraypy([2, 2, 0]).to_tensor((-1, -1))
     g_ten0[0, 0] = cos(x2)**2
     g_ten0[0, 1] = 0
     g_ten0[1, 0] = 0
     g_ten0[1, 1] = 1
 
     ricci = Matrix([[cos(x2)**2, 0], [0, 1]])
-    ricci_ten = Arraypy([2, 2, 1])
-    ricci_ten = g_ten.To_tensor((-1, -1))
+    ricci_ten = Arraypy([2, 2, 1]).to_tensor((-1, -1))
     ricci_ten[1, 1] = cos(x2)**2
     ricci_ten[1, 2] = 0
     ricci_ten[2, 1] = 0
     ricci_ten[2, 2] = 1
 
-    ricci_ten0 = Arraypy([2, 2, 1])
-    ricci_ten0 = g_ten0.To_tensor((-1, -1))
+    ricci_ten0 = Arraypy([2, 2, 0]).to_tensor((-1, -1))
     ricci_ten0[0, 0] = cos(x2)**2
     ricci_ten0[0, 1] = 0
     ricci_ten0[1, 0] = 0
@@ -433,21 +430,19 @@ def test_k_sigma():
     X = [1, 2]
     Y = [3, 4]
 
-    g_ten = Arraypy([2, 2, 1])
-    g_ten = g_ten.To_tensor((-1, -1))
+    g_ten = Arraypy([2, 2, 1]).to_tensor((-1, -1))
     g_ten[1, 1] = cos(x2)**2
     g_ten[1, 2] = 0
     g_ten[2, 1] = 0
     g_ten[2, 2] = 1
 
-    g_ten0 = Arraypy([2, 2, 1])
-    g_ten0 = g_ten0.To_tensor((-1, -1))
+    g_ten0 = Arraypy([2, 2, 0]).to_tensor((-1, -1))
     g_ten0[0, 0] = cos(x2)**2
     g_ten0[0, 1] = 0
     g_ten0[1, 0] = 0
     g_ten0[1, 1] = 1
 
-    riemann_arr = Arraypy([4, 2, 1])
+    riemann_arr = Arraypy([4, 2, 0])
     riemann_arr[0, 0, 0, 0] = 0
     riemann_arr[0, 0, 0, 1] = 0
     riemann_arr[0, 0, 1, 1] = 0
@@ -464,7 +459,7 @@ def test_k_sigma():
     riemann_arr[0, 1, 0, 0] = 0
     riemann_arr[0, 1, 1, 0] = 1
     riemann_arr[1, 0, 0, 1] = cos(x2)**2
-    riemann_ten = riemann_arr.to_tensor(-1, -1, -1, 1)
+    riemann_ten = riemann_arr.to_tensor((-1, -1, -1, 1))
 
     assert k_sigma(X, Y, riemann_ten, g, var_list) == 1
     assert k_sigma(X, Y, riemann_ten, g_ten, var_list) == 1
