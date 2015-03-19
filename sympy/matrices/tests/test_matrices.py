8940392c04f96c3140e7fb0e9419721b57fa27e5
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-03-19 01:06:27 +0000

    matrices: convert empty matrices to immutable correctly
    Previously `ones(3, 0).as_immutable` gave a `0x0` instead of a
    `3x0` matrix.  The `0x3` case was already working: add a new test
    next to the existing one.

--- sympy/matrices/tests/test_matrices.py
+++ sympy/matrices/tests/test_matrices.py
@@ -230,6 +230,7 @@ def test_tolist():
 def test_as_mutable():
     assert zeros(0, 3).as_mutable() == zeros(0, 3)
     assert zeros(0, 3).as_immutable() == ImmutableMatrix(zeros(0, 3))
+    assert zeros(3, 0).as_immutable() == ImmutableMatrix(zeros(3, 0))
 
 
 def test_determinant():
