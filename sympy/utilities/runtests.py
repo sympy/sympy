116db94a711801b52fce4717e4fe13e79c14aede
Peter Schmidt <peter@peterjs.com>
2015-03-31 12:16:35 +1100

    Another false positive on unexpectedly fast - should only be unexpectedly fast if we actually ran the slow test. Example usages:
    sympy.test('sympy/polys/tests/test_polyroots.py', slow_threshold=1)
    ...
    ________________________________ slowest tests _________________________________
    test_issue_8438 - Took 1.914 seconds
    test_roots0 - Took 2.411 seconds
    test_roots_quartic - Took 9.863 seconds
    ============ tests finished: 18 passed, 1 skipped, in 18.58 seconds ============
    
    sympy.test('sympy/polys/tests/test_polyroots.py', slow_threshold=1, slow=True)
    ...
    ________________________________ slowest tests _________________________________
    test_nroots1 - Took 14.311 seconds
    ================== tests finished: 1 passed, in 14.37 seconds ==================
    
    # Hack a return statement into the test_nroots1:
    sympy.test('sympy/polys/tests/test_polyroots.py', slow_threshold=1, slow=True)
    ...
    ___________________________ unexpectedly fast tests ____________________________
    test_nroots1 - Took 0.000 seconds
    ================== tests finished: 1 passed, in 0.06 seconds ===================

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -1139,7 +1139,7 @@ class SymPyTests(object):
             taken = time.time() - start
             if taken > self._slow_threshold:
                 self._reporter.slow_test_functions.append((f.__name__, taken))
-            if getattr(f, '_slow', False):
+            if getattr(f, '_slow', False) and slow:
                 if taken < self._fast_threshold:
                     self._reporter.fast_test_functions.append((f.__name__, taken))
         self._reporter.leaving_filename()
