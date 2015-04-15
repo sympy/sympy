e92cb375f6916ce78dc68ad00bd7581117dff70b
Sudhanshu Mishra <mrsud94@gmail.com>
2015-04-15 06:15:27 +0530

    Fixed power simplification of order. Now O(1)**O(1) returns O(1)

--- sympy/series/order.py
+++ sympy/series/order.py
@@ -294,6 +294,8 @@ class Order(Expr):
     def _eval_power(b, e):
         if e.is_Number and e.is_nonnegative:
             return b.func(b.expr ** e, *b.args[1:])
+        if e == O(1):
+            return b
         return
 
     def as_expr_variables(self, order_symbols):
--- sympy/series/tests/test_order.py
+++ sympy/series/tests/test_order.py
@@ -409,3 +409,4 @@ def test_order_subs_limits():
 
 def test_issue_9192():
     assert O(1)*O(1) == O(1)
+    assert O(1)**O(1) == O(1)
