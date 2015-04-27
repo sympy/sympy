3a6e338c8b277cff243a0dbcf60b6af2a1d4076e
MadelineWu <wumadeline@berkeley.edu>
2015-04-26 22:23:31 -0700

    Fixed issue #9202

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -317,6 +317,10 @@ class LatexPrinter(Printer):
 
         def convert(expr):
             if not expr.is_Mul:
+                if denom == 1:
+                    return r"\left(%s\right)" % str(self._print(expr))
+                # print(numer)
+                # print(denom)
                 return str(self._print(expr))
             else:
                 _tex = last_term_tex = ""
@@ -634,7 +638,6 @@ class LatexPrinter(Printer):
         symbols. For multi-letter function names that LaTeX does not know
         about, (e.g., Li, sech) use \operatorname{} so that the function name
         is rendered in Roman font and LaTeX handles spacing properly.
-
         expr is the expression involving the function
         exp is an exponent
         '''
@@ -1815,12 +1818,9 @@ def translate(s):
     r'''
     Check for a modifier ending the string.  If present, convert the
     modifier to latex and translate the rest recursively.
-
     Given a description of a Greek letter or other special character,
     return the appropriate latex.
-
     Let everything else pass as given.
-
     >>> from sympy.printing.latex import translate
     >>> translate('alphahatdotprime')
     "{\\dot{\\hat{\\alpha}}}'"
@@ -1841,19 +1841,15 @@ def translate(s):
 def latex(expr, **settings):
     r"""
     Convert the given expression to LaTeX representation.
-
     >>> from sympy import latex, pi, sin, asin, Integral, Matrix, Rational
     >>> from sympy.abc import x, y, mu, r, tau
-
     >>> print(latex((2*tau)**Rational(7,2)))
     8 \sqrt{2} \tau^{\frac{7}{2}}
-
     order: Any of the supported monomial orderings (currently "lex", "grlex", or
     "grevlex"), "old", and "none". This parameter does nothing for Mul objects.
     Setting order to "old" uses the compatibility ordering for Add defined in
     Printer. For very large expressions, set the 'order' keyword to 'none' if
     speed is a concern.
-
     mode: Specifies how the generated code will be delimited. 'mode' can be one
     of 'plain', 'inline', 'equation' or 'equation*'.  If 'mode' is set to
     'plain', then the resulting code will not be delimited at all (this is the
@@ -1862,104 +1858,73 @@ def latex(expr, **settings):
     enclosed in the 'equation' or 'equation*' environment (remember to import
     'amsmath' for 'equation*'), unless the 'itex' option is set. In the latter
     case, the ``$$ $$`` syntax is used.
-
     >>> print(latex((2*mu)**Rational(7,2), mode='plain'))
     8 \sqrt{2} \mu^{\frac{7}{2}}
-
     >>> print(latex((2*tau)**Rational(7,2), mode='inline'))
     $8 \sqrt{2} \tau^{\frac{7}{2}}$
-
     >>> print(latex((2*mu)**Rational(7,2), mode='equation*'))
     \begin{equation*}8 \sqrt{2} \mu^{\frac{7}{2}}\end{equation*}
-
     >>> print(latex((2*mu)**Rational(7,2), mode='equation'))
     \begin{equation}8 \sqrt{2} \mu^{\frac{7}{2}}\end{equation}
-
     itex: Specifies if itex-specific syntax is used, including emitting ``$$ $$``.
-
     >>> print(latex((2*mu)**Rational(7,2), mode='equation', itex=True))
     $$8 \sqrt{2} \mu^{\frac{7}{2}}$$
-
     fold_frac_powers: Emit "^{p/q}" instead of "^{\frac{p}{q}}" for fractional
     powers.
-
     >>> print(latex((2*tau)**Rational(7,2), fold_frac_powers=True))
     8 \sqrt{2} \tau^{7/2}
-
     fold_func_brackets: Fold function brackets where applicable.
-
     >>> print(latex((2*tau)**sin(Rational(7,2))))
     \left(2 \tau\right)^{\sin{\left (\frac{7}{2} \right )}}
     >>> print(latex((2*tau)**sin(Rational(7,2)), fold_func_brackets = True))
     \left(2 \tau\right)^{\sin {\frac{7}{2}}}
-
     fold_short_frac: Emit "p / q" instead of "\frac{p}{q}" when the
     denominator is simple enough (at most two terms and no powers).
     The default value is `True` for inline mode, False otherwise.
-
     >>> print(latex(3*x**2/y))
     \frac{3 x^{2}}{y}
     >>> print(latex(3*x**2/y, fold_short_frac=True))
     3 x^{2} / y
-
     long_frac_ratio: The allowed ratio of the width of the numerator to the
     width of the denominator before we start breaking off long fractions.
     The default value is 2.
-
     >>> print(latex(Integral(r, r)/2/pi, long_frac_ratio=2))
     \frac{\int r\, dr}{2 \pi}
     >>> print(latex(Integral(r, r)/2/pi, long_frac_ratio=0))
     \frac{1}{2 \pi} \int r\, dr
-
     mul_symbol: The symbol to use for multiplication. Can be one of None,
     "ldot", "dot", or "times".
-
     >>> print(latex((2*tau)**sin(Rational(7,2)), mul_symbol="times"))
     \left(2 \times \tau\right)^{\sin{\left (\frac{7}{2} \right )}}
-
     inv_trig_style: How inverse trig functions should be displayed. Can be one
     of "abbreviated", "full", or "power". Defaults to "abbreviated".
-
     >>> print(latex(asin(Rational(7,2))))
     \operatorname{asin}{\left (\frac{7}{2} \right )}
     >>> print(latex(asin(Rational(7,2)), inv_trig_style="full"))
     \arcsin{\left (\frac{7}{2} \right )}
     >>> print(latex(asin(Rational(7,2)), inv_trig_style="power"))
     \sin^{-1}{\left (\frac{7}{2} \right )}
-
     mat_str: Which matrix environment string to emit. "smallmatrix", "matrix",
     "array", etc. Defaults to "smallmatrix" for inline mode, "matrix" for
     matrices of no more than 10 columns, and "array" otherwise.
-
     >>> print(latex(Matrix(2, 1, [x, y])))
     \left[\begin{matrix}x\\y\end{matrix}\right]
-
     >>> print(latex(Matrix(2, 1, [x, y]), mat_str = "array"))
     \left[\begin{array}{c}x\\y\end{array}\right]
-
     mat_delim: The delimiter to wrap around matrices. Can be one of "[", "(",
     or the empty string. Defaults to "[".
-
     >>> print(latex(Matrix(2, 1, [x, y]), mat_delim="("))
     \left(\begin{matrix}x\\y\end{matrix}\right)
-
     symbol_names: Dictionary of symbols and the custom strings they should be
     emitted as.
-
     >>> print(latex(x**2, symbol_names={x:'x_i'}))
     x_i^{2}
-
     ``latex`` also supports the builtin container types list, tuple, and
     dictionary.
-
     >>> print(latex([2/x, y], mode='inline'))
     $\left [ 2 / x, \quad y\right ]$
-
     """
-
     return LatexPrinter(settings).doprint(expr)
-
-
 def print_latex(expr, **settings):
     """Prints LaTeX representation of the given expression."""
     print(latex(expr, **settings))
