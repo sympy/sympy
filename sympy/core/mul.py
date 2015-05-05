sympy/sympy

commit 41557f563821d9d77c71ed0edb23e80c00467b4c

simplify Mul._eval_is_negative routine

sympy/core/mul.py
@@ -1124,39 +1124,7 @@ class Mul(Expr, AssocOp):
             return False
 
     def _eval_is_negative(self):
-        """Return True if self is negative, False if not, and None if it
-        cannot be determined.
-
-        This algorithm is non-recursive and works by keeping track of the
-        sign which changes when a negative or nonpositive is encountered.
-        Whether a nonpositive or nonnegative is seen is also tracked since
-        the presence of these makes it impossible to return True, but
-        possible to return False if the end result is nonnegative. e.g.
-
-            pos * neg * nonpositive -> pos or zero -> False is returned
-            pos * neg * nonnegative -> neg or zero -> None is returned
-        """
-
-        sign = 1
-        saw_NON = False
-        for t in self.args:
-            if t.is_positive:
-                continue
-            elif t.is_negative:
-                sign = -sign
-            elif t.is_zero:
-                return False
-            elif t.is_nonpositive:
-                sign = -sign
-                saw_NON = True
-            elif t.is_nonnegative:
-                saw_NON = True
-            else:
-                return
-        if sign == -1 and saw_NON is False:
-            return True
-        if sign > 0:
-            return False
+        return (-self).is_positive
 
     def _eval_is_odd(self):
         is_integer = self.is_integer

commit 9b8681219a84da72759e306541020e234925ce53

no-cache: Fix infinite recursion in Mul._eval_is_* methods
When SYMPY_USE_CACHE='no', an infinite recursion can be triggered with
Mul._eval_is_imaginary/antihermitian/negative. You can see a lot of
errors from this in the test suite.

The original bugs were introduced in:
41557f563821d9d77c71ed0edb23e80c00467b4c
ff71d9f451c1c14df2a095895624ba2bf67fa61e
9faa909cb5a133f8cb65128eed2bb3fc62d8268d

This was hard to fix because these 'simplification' changes had
side-effects. It was not a simple matter of reverting the changes.

There were 2 main side-effects:
 - It handled is_positive and is_negative better because it somehow
   accounted for checking finite-ness when one of the args was zero.
   I explicitly handled this case in this fix.
 - It handled the positive=False case in is_negative. I also explicitly
   handled this case in is_negative and the symmetric case in
   is_positive.

I needed to partially regress a test_arit test back to what it was on
this commit: 9faa909cb5a133f8cb65128eed2bb3fc62d8268d

I don't know enough about hermitian/antihermitian properties to know if
this regression is correct or not. This needs further investigation.

sympy/core/mul.py
@@ -1058,11 +1058,31 @@ class Mul(Expr, AssocOp):
             return real  # doesn't matter what zero is
 
     def _eval_is_imaginary(self):
-        z = self.is_zero
-        if z:
-            return False
-        elif z is False:
-            return (S.ImaginaryUnit*self).is_real
+        real = True
+        zero = one_neither = False
+
+        for t in self.args:
+            if t.is_imaginary:
+                real = not real
+            elif t.is_real:
+                if zero is False:
+                    zero = fuzzy_not(t.is_nonzero)
+                    if zero:
+                        return False
+            elif t.is_real is False:
+                if one_neither:
+                    return  # complex terms might cancel
+                one_neither = True
+            else:
+                return
+
+        if zero is False:
+            if one_neither:  # N = a+I*b or I*b
+                if real:
+                    return  # r*N is like N: could be either
+                return False  # neither I*N values is imaginary
+            if not real:
+                return True
 
     def _eval_is_hermitian(self):
         real = True
@@ -1095,11 +1115,34 @@ class Mul(Expr, AssocOp):
             return real
 
     def _eval_is_antihermitian(self):
-        z = self.is_zero
-        if z:
-            return False
-        elif z is False:
-            return (S.ImaginaryUnit*self).is_hermitian
+        imag = False
+        one_nc = zero = one_neither = False
+
+        for t in self.args:
+            if not t.is_commutative:
+                if one_nc:
+                    return
+                one_nc = True
+
+            if t.is_antihermitian:
+                imag = not imag
+            elif t.is_hermitian:
+                if zero is False:
+                    zero = fuzzy_not(t.is_nonzero)
+                    if zero:
+                        return True
+            elif t.is_hermitian is False:
+                if one_neither:
+                    return
+                one_neither = True
+            else:
+                return
+
+        if one_neither:
+            if imag:
+                return zero
+        elif zero is False or imag:
+            return imag
 
     def _eval_is_irrational(self):
         for t in self.args:
@@ -1130,27 +1173,79 @@ class Mul(Expr, AssocOp):
 
         sign = 1
         saw_NON = False
+        not_real = 0
         for t in self.args:
             if t.is_positive:
                 continue
             elif t.is_negative:
                 sign = -sign
             elif t.is_zero:
-                return False
+                if all(a.is_finite for a in self.args):
+                    return False
+                return
             elif t.is_nonpositive:
                 sign = -sign
                 saw_NON = True
             elif t.is_nonnegative:
                 saw_NON = True
+            elif t.is_positive is False:
+                sign = -sign
+                saw_NON = True
+                not_real += 1
+            elif t.is_negative is False:
+                saw_NON = True
+                not_real += 1
             else:
                 return
-        if sign == 1 and saw_NON is False:
+        if sign == 1 and saw_NON is False and not_real == 0:
             return True
-        if sign < 0:
+        if sign < 0 and not_real <= 1:
             return False
 
     def _eval_is_negative(self):
-        return (-self).is_positive
+        """Return True if self is negative, False if not, and None if it
+        cannot be determined.
+
+        This algorithm is non-recursive and works by keeping track of the
+        sign which changes when a negative or nonpositive is encountered.
+        Whether a nonpositive or nonnegative is seen is also tracked since
+        the presence of these makes it impossible to return True, but
+        possible to return False if the end result is nonnegative. e.g.
+
+            pos * neg * nonpositive -> pos or zero -> False is returned
+            pos * neg * nonnegative -> neg or zero -> None is returned
+        """
+
+        sign = 1
+        saw_NON = False
+        not_real = 0
+        for t in self.args:
+            if t.is_positive:
+                continue
+            elif t.is_negative:
+                sign = -sign
+            elif t.is_zero:
+                if all(a.is_finite for a in self.args):
+                    return False
+                return
+            elif t.is_nonpositive:
+                sign = -sign
+                saw_NON = True
+            elif t.is_nonnegative:
+                saw_NON = True
+            elif t.is_positive is False:
+                sign = -sign
+                saw_NON = True
+                not_real += 1
+            elif t.is_negative is False:
+                saw_NON = True
+                not_real += 1
+            else:
+                return
+        if sign == -1 and saw_NON is False and not_real == 0:
+            return True
+        if sign > 0 and not_real <= 1:
+            return False
 
     def _eval_is_odd(self):
         is_integer = self.is_integer

sympy/core/tests/test_arit.py
@@ -1248,7 +1248,7 @@ def test_Mul_hermitian_antihermitian():
     assert e3.is_antihermitian is None
     assert e4.is_antihermitian is None
     assert e5.is_antihermitian is None
-    assert e6.is_antihermitian is None
+    assert e6.is_antihermitian is False
 
 
 def test_Add_is_comparable():