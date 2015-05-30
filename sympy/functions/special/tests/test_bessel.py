cd8ff4e6f9219e757d06bf81b4274f3f4aef18e2
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-05-31 00:20:21 +0100

    expr: never multiplicatively/additively extract from a NaN
    You can multiplicatively extract nan from anything... but the docs
    for these functions note "in a nice way".  Consider:
    ````
    (5*I).extract_multiplicatively(I)    # 5
    (S.One).extract_multiplicatively(I)  # None
    ````
    Notable, latter does not give `1/I`.  However:
    ````
    nan.extract_multiplicatively(I)   # was nan, now None
    ````
    
    The fix is to always return None when self is NaN.
    
    This fixes an infinite recursion in Bessel functions with NaN
    arguments: test added.
    
    Other inconsistencies that are fixed and tested:
    ````
    nan.extract_multiplicatively(nan)   # was 1
    nan.extract_additively(1)           # was an error
    nan.extract_additively(nan)         # was 0
    ````

--- sympy/functions/special/tests/test_bessel.py
+++ sympy/functions/special/tests/test_bessel.py
@@ -271,6 +271,12 @@ def test_bessel_eval():
     assert besselj(3, I*z) == -I*besseli(3, z)
 
 
+def test_bessel_nan():
+    # FIXME: could have these return NaN; for now just fix infinite recursion
+    for f in [besselj, bessely, besseli, besselk, hankel1, hankel2, yn, jn]:
+        assert f(1, S.NaN) == f(1, S.NaN, evaluate=False)
+
+
 def test_conjugate():
     from sympy import conjugate, I, Symbol
     n, z, x = Symbol('n'), Symbol('z', real=False), Symbol('x', real=True)
