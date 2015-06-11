sympy/sympy

commit d1a454a58a9a9bc15980d731ff0f6cbef33c3d77

add _monotonic_sign and use in Add._eval_is_(pos, neg, npos, nneg)

sympy/core/add.py
@@ -11,7 +11,6 @@ from .cache import cacheit
 from .numbers import ilcm, igcd
 from .expr import Expr
 
-
 # Key for sorting commutative args in canonical order
 _args_sortkey = cmp_to_key(Basic.compare)
 def _addsort(args):
@@ -493,8 +492,20 @@ class Add(Expr, AssocOp):
         return False
 
     def _eval_is_positive(self):
+        from sympy.core.exprtools import _monotonic_sign
         if self.is_number:
             return super(Add, self)._eval_is_positive()
+        c, a = self.as_coeff_Add()
+        if not c.is_zero:
+            v = _monotonic_sign(a)
+            if v is not None:
+                s = v + c
+                if s.is_positive and a.is_nonnegative:
+                    return True
+                if len(self.free_symbols) == 1:
+                    v = _monotonic_sign(self)
+                    if v is not None and v.is_positive:
+                        return True
         pos = nonneg = nonpos = unknown_sign = False
         saw_INF = set()
         args = [a for a in self.args if not a.is_zero]
@@ -534,9 +545,51 @@ class Add(Expr, AssocOp):
         elif not pos and not nonneg:
             return False
 
+    def _eval_is_nonnegative(self):
+        from sympy.core.exprtools import _monotonic_sign
+        if not self.is_number:
+            c, a = self.as_coeff_Add()
+            if not c.is_zero and a.is_nonnegative:
+                v = _monotonic_sign(a)
+                if v is not None:
+                    s = v + c
+                    if s.is_nonnegative:
+                        return True
+                    if len(self.free_symbols) == 1:
+                        v = _monotonic_sign(self)
+                        if v is not None and v.is_nonnegative:
+                            return True
+
+    def _eval_is_nonpositive(self):
+        from sympy.core.exprtools import _monotonic_sign
+        if not self.is_number:
+            c, a = self.as_coeff_Add()
+            if not c.is_zero and a.is_nonpositive:
+                v = _monotonic_sign(a)
+                if v is not None:
+                    s = v + c
+                    if s.is_nonpositive:
+                        return True
+                    if len(self.free_symbols) == 1:
+                        v = _monotonic_sign(self)
+                        if v is not None and v.is_nonpositive:
+                            return True
+
     def _eval_is_negative(self):
+        from sympy.core.exprtools import _monotonic_sign
         if self.is_number:
             return super(Add, self)._eval_is_negative()
+        c, a = self.as_coeff_Add()
+        if not c.is_zero:
+            v = _monotonic_sign(a)
+            if v is not None:
+                s = v + c
+                if s.is_negative and a.is_nonpositive:
+                    return True
+                if len(self.free_symbols) == 1:
+                    v = _monotonic_sign(self)
+                    if v is not None and v.is_negative:
+                        return True
         neg = nonpos = nonneg = unknown_sign = False
         saw_INF = set()
         args = [a for a in self.args if not a.is_zero]

sympy/core/assumptions.py
@@ -154,6 +154,7 @@ from sympy.core.facts import FactRules, FactKB
 from sympy.core.core import BasicMeta
 from sympy.core.compatibility import integer_types, with_metaclass
 
+
 from random import shuffle
 
 

sympy/core/exprtools.py
@@ -16,13 +16,191 @@ from sympy.core.coreerrors import NonCommutativeExpression
 from sympy.core.containers import Tuple, Dict
 from sympy.utilities import default_sort_key
 from sympy.utilities.iterables import (common_prefix, common_suffix,
-        variations, ordered)
+        variations, ordered, cartes)
 
 from collections import defaultdict
 
 
+_eps = Dummy(positive=True)
+
+
 def _isnumber(i):
     return isinstance(i, (SYMPY_INTS, float)) or i.is_Number
+
+
+def _monotonic_sign(self):
+    """Return the value closest to 0 that ``self`` may have if all symbols
+    are signed and the result is uniformly the same sign for all values of symbols.
+    If a symbol is only signed but not known to be an
+    integer or the result is 0 then a symbol representative of the sign of self
+    will be returned. Otherwise, None is returned if a) the sign could be positive
+    or negative or b) self is not in one of the following forms:
+
+    - L(x, y, ...) + A: a function linear in all symbols x, y, ... with an
+      additive constant; if A is zero then the function can be a monomial whose
+      sign is monotonic over the range of the variables, e.g. (x + 1)**3 if x is
+      nonnegative.
+    - A/L(x, y, ...) + B: the inverse of a function linear in all symbols x, y, ...
+      that does not have a sign change from positive to negative for any set
+      of values for the variables.
+    - M(x, y, ...) + A: a monomial M whose factors are all signed and a constant, A.
+    - A/M(x, y, ...) + B: the inverse of a monomial and constants A and B.
+    - P(x): a univariate polynomial
+
+    Examples
+    ========
+
+    >>> from sympy.core.exprtools import _monotonic_sign as F
+    >>> from sympy import Dummy, S
+    >>> nn = Dummy(integer=True, nonnegative=True)
+    >>> p = Dummy(integer=True, positive=True)
+    >>> p2 = Dummy(integer=True, positive=True)
+    >>> F(nn + 1)
+    1
+    >>> F(p - 1)
+    _nneg
+    >>> F(nn*p + 1)
+    1
+    >>> F(p2*p + 1)
+    2
+    >>> F(nn - 1)  # could be negative, zero or positive
+    """
+    if not self.is_real:
+        return
+
+    if (-self).is_Symbol:
+        rv = _monotonic_sign(-self)
+        return rv if rv is None else -rv
+
+    if self.is_Symbol:
+        s = self
+        if s.is_prime:
+            if s.is_odd:
+                return S(3)
+            else:
+                return S(2)
+        elif s.is_positive:
+            if s.is_even:
+                return S(2)
+            elif s.is_integer:
+                return S.One
+            else:
+                return _eps
+        elif s.is_negative:
+            if s.is_even:
+                return S(-2)
+            elif s.is_integer:
+                return S.NegativeOne
+            else:
+                return -_eps
+        if s.is_zero or s.is_nonpositive or s.is_nonnegative:
+            return S.Zero
+        return None
+
+    # univariate polynomial
+    free = self.free_symbols
+    if len(free) == 1:
+        if self.is_polynomial():
+            from sympy.polys.polytools import real_roots
+            from sympy.polys.polyroots import roots
+            from sympy.polys.polyerrors import PolynomialError
+            x = free.pop()
+            x0 = _monotonic_sign(x)
+            if x0 == _eps or x0 == -_eps:
+                x0 = S.Zero
+            if x0 is not None:
+                d = self.diff(x)
+                if d.is_number:
+                    roots = []
+                else:
+                    try:
+                        roots = real_roots(d)
+                    except (PolynomialError, NotImplementedError):
+                        roots = [r for r in roots(d, x) if r.is_real]
+                y = self.subs(x, x0)
+                if x.is_nonnegative and all(r <= x0 for r in roots):
+                    if y.is_nonnegative and d.is_positive:
+                        if y:
+                            return y if y.is_positive else Dummy('pos', positive=True)
+                        else:
+                            return Dummy('nneg', nonnegative=True)
+                    if y.is_nonpositive and d.is_negative:
+                        if y:
+                            return y if y.is_negative else Dummy('neg', negative=True)
+                        else:
+                            return Dummy('npos', nonpositive=True)
+                elif x.is_nonpositive and all(r >= x0 for r in roots):
+                    if y.is_nonnegative and d.is_negative:
+                        if y:
+                            return Dummy('pos', positive=True)
+                        else:
+                            return Dummy('nneg', nonnegative=True)
+                    if y.is_nonpositive and d.is_positive:
+                        if y:
+                            return Dummy('neg', negative=True)
+                        else:
+                            return Dummy('npos', nonpositive=True)
+        else:
+            n, d = self.as_numer_denom()
+            den = None
+            if n.is_number:
+                den = _monotonic_sign(d)
+            elif not d.is_number:
+                if _monotonic_sign(n) is not None:
+                    den = _monotonic_sign(d)
+            if den is not None and (den.is_positive or den.is_negative):
+                v = n*den
+                if v.is_positive:
+                    return Dummy('pos', positive=True)
+                elif v.is_nonnegative:
+                    return Dummy('nneg', nonnegative=True)
+                elif v.is_negative:
+                    return Dummy('neg', negative=True)
+                elif v.is_nonpositive:
+                    return Dummy('npos', nonpositive=True)
+        return None
+
+    # multivariate
+    c, a = self.as_coeff_Add()
+    v = None
+    if not a.is_polynomial():
+        # F/A or A/F where A is a number and F is a signed, rational monomial
+        n, d = a.as_numer_denom()
+        if not (n.is_number or d.is_number):
+            return
+        if (
+                a.is_Mul or a.is_Pow) and \
+                a.is_rational and \
+                all(p.exp.is_Integer for p in a.atoms(Pow) if p.is_Pow) and \
+                (a.is_positive or a.is_negative):
+            v = S(1)
+            for ai in Mul.make_args(a):
+                if ai.is_number:
+                    v *= ai
+                    continue
+                reps = {}
+                for x in ai.free_symbols:
+                    reps[x] = _monotonic_sign(x)
+                    if reps[x] is None:
+                        return
+                v *= ai.subs(reps)
+    elif c:
+        # signed linear expression
+        if not any(p for p in a.atoms(Pow) if not p.is_number) and (a.is_nonpositive or a.is_nonnegative):
+            free = list(a.free_symbols)
+            p = {}
+            for i in free:
+                v = _monotonic_sign(i)
+                if v is None:
+                    return
+                p[i] = v or (_eps if i.is_nonnegative else -_eps)
+            v = a.xreplace(p)
+    if v is not None:
+        rv = v + c
+        if v.is_nonnegative and rv.is_positive:
+            return rv.subs(_eps, 0)
+        if v.is_nonpositive and rv.is_negative:
+            return rv.subs(_eps, 0)
 
 
 def decompose_power(expr):

sympy/core/tests/test_assumptions.py
@@ -879,3 +879,24 @@ def test_issue_8075():
 def test_issue_8642():
     x = Symbol('x', real=True, integer=False)
     assert (x*2).is_integer is None
+
+
+def test_issues_8632_8633_8638_8675_8992():
+    p = Dummy(integer=True, positive=True)
+    nn = Dummy(integer=True, nonnegative=True)
+    assert (p - S.Half).is_positive
+    assert (p - 1).is_nonnegative
+    assert (nn + 1).is_positive
+    assert (-p + 1).is_nonpositive
+    assert (-nn - 1).is_negative
+    prime = Dummy(prime=True)
+    assert (prime - 2).is_nonnegative
+    assert (prime - 3).is_nonnegative is None
+    even = Dummy(positive=True, even=True)
+    assert (even - 2).is_nonnegative
+
+    p = Dummy(positive=True)
+    assert (p/(p + 1) - 1).is_negative
+    assert ((p + 2)**3 - S.Half).is_positive
+    n = Dummy(negative=True)
+    assert (n - 3).is_nonpositive

sympy/core/tests/test_exprtools.py
@@ -2,14 +2,15 @@
 
 from sympy import (S, Add, sin, Mul, Symbol, oo, Integral, sqrt, Tuple, I,
                    Interval, O, symbols, simplify, collect, Sum, Basic, Dict,
-                   root, exp, cos)
-from sympy.abc import a, b, t, x, y, z
+                   root, exp, cos, sin, oo, Dummy, log)
 from sympy.core.exprtools import (decompose_power, Factors, Term, _gcd_terms,
-                                  gcd_terms, factor_terms, factor_nc)
+                                  gcd_terms, factor_terms, factor_nc,
+                                  _monotonic_sign)
 from sympy.core.mul import _keep_coeff as _keep_coeff
 from sympy.simplify.cse_opts import sub_pre
-
 from sympy.utilities.pytest import raises
+
+from sympy.abc import a, b, t, x, y, z
 
 
 def test_decompose_power():
@@ -354,3 +355,55 @@ def test_issue_7903():
     a = symbols(r'a', real=True)
     t = exp(I*cos(a)) + exp(-I*sin(a))
     assert t.simplify()
+
+
+def test_monotonic_sign():
+    F = _monotonic_sign
+    x = symbols('x')
+    assert F(x) is None
+    assert F(-x) is None
+    assert F(Dummy(prime=True)) == 2
+    assert F(Dummy(prime=True, odd=True)) == 3
+    assert F(Dummy(positive=True, integer=True)) == 1
+    assert F(Dummy(positive=True, even=True)) == 2
+    assert F(Dummy(negative=True, integer=True)) == -1
+    assert F(Dummy(negative=True, even=True)) == -2
+    assert F(Dummy(zero=True)) == 0
+    assert F(Dummy(nonnegative=True)) == 0
+    assert F(Dummy(nonpositive=True)) == 0
+
+    assert F(Dummy(positive=True) + 1).is_positive
+    assert F(Dummy(positive=True, integer=True) - 1).is_nonnegative
+    assert F(Dummy(positive=True) - 1) is None
+    assert F(Dummy(negative=True) + 1) is None
+    assert F(Dummy(negative=True, integer=True) - 1).is_nonpositive
+    assert F(Dummy(negative=True) - 1).is_negative
+    assert F(-Dummy(positive=True) + 1) is None
+    assert F(-Dummy(positive=True, integer=True) - 1).is_negative
+    assert F(-Dummy(positive=True) - 1).is_negative
+    assert F(-Dummy(negative=True) + 1).is_positive
+    assert F(-Dummy(negative=True, integer=True) - 1).is_nonnegative
+    assert F(-Dummy(negative=True) - 1) is None
+    x = Dummy(negative=True)
+    assert F(x**3).is_nonpositive
+    assert F(x**3 + log(2)*x - 1).is_negative
+    x = Dummy(positive=True)
+    assert F(-x**3).is_nonpositive
+
+    p = Dummy(positive=True)
+    assert F(1/p).is_positive
+    assert F(p/(p + 1)).is_positive
+    p = Dummy(nonnegative=True)
+    assert F(p/(p + 1)).is_nonnegative
+    p = Dummy(positive=True)
+    assert F(-1/p).is_negative
+    p = Dummy(nonpositive=True)
+    assert F(p/(-p + 1)).is_nonpositive
+
+    p = Dummy(positive=True, integer=True)
+    q = Dummy(positive=True, integer=True)
+    assert F(-2/p/q).is_negative
+    assert F(-2/(p - 1)/q) is None
+
+    assert F((p - 1)*q + 1).is_positive
+    assert F(-(p - 1)*q - 1).is_negative

sympy/functions/combinatorial/numbers.py
@@ -948,7 +948,7 @@ class genocchi(Function):
 
     def _eval_rewrite_as_bernoulli(self, n):
         if n.is_integer and n.is_nonnegative:
-            return 2 * (1 - S(2) ** n) * bernoulli(n)
+            return (1 - S(2) ** n) * bernoulli(n) * 2
 
     def _eval_is_integer(self):
         if self.args[0].is_integer and self.args[0].is_positive:
@@ -984,8 +984,9 @@ class genocchi(Function):
 
     def _eval_is_prime(self):
         n = self.args[0]
-        if (not n.is_integer) or (not n.is_positive):
-            return None
+        # only G_6 = -3 and G_8 = 17 are prime,
+        # but SymPy does not consider negatives as prime
+        # so only n=8 is tested
         return (n - 8).is_zero
 
 

sympy/functions/combinatorial/tests/test_comb_factorials.py
@@ -181,8 +181,7 @@ def test_factorial2():
     assert factorial2(tt - 1).is_integer
     assert factorial2(tte - 1).is_integer
     assert factorial2(tpe - 3).is_integer
-    # This should work, but it doesn't due to ...
-    # assert factorial2(tto - 4).is_integer
+    assert factorial2(tto - 4).is_integer
     assert factorial2(tto - 2).is_integer
     assert factorial2(tf).is_integer is None
     assert factorial2(tfe).is_integer is None
@@ -197,11 +196,9 @@ def test_factorial2():
     assert factorial2(n).is_positive is None
     assert factorial2(tt - 1).is_positive is True
     assert factorial2(tte - 1).is_positive is True
-    # This should work, but it doesn't due to ...
-    # assert factorial2(tpe - 3).is_positive is True
+    assert factorial2(tpe - 3).is_positive is True
     assert factorial2(tpe - 1).is_positive is True
-    # This should work, but it doesn't due to ...
-    # assert factorial2(tto - 2).is_positive is True
+    assert factorial2(tto - 2).is_positive is True
     assert factorial2(tto - 1).is_positive is True
     assert factorial2(tf).is_positive is None
     assert factorial2(tfe).is_positive is None

sympy/functions/combinatorial/tests/test_comb_numbers.py
@@ -324,12 +324,14 @@ def test_genocchi():
     m = Symbol('m', integer=True)
     n = Symbol('n', integer=True, positive=True)
     assert genocchi(m) == genocchi(m)
-    assert genocchi(n).rewrite(bernoulli) == 2 * (1 - 2 ** n) * bernoulli(n)
+    assert genocchi(n).rewrite(bernoulli) == (1 - 2 ** n) * bernoulli(n) * 2
     assert genocchi(2 * n).is_odd
     assert genocchi(4 * n).is_positive
-    # This should work for 4 * n - 2, but fails due to some variation of issue
-    # 8632 ((4*n-2).is_positive returns None)
+    # these are the only 2 prime Genocchi numbers
+    assert genocchi(6, evaluate=False).is_prime == S(-3).is_prime
+    assert genocchi(8, evaluate=False).is_prime
     assert genocchi(4 * n + 2).is_negative
+    assert genocchi(4 * n - 2).is_negative
 
 
 def test_nC_nP_nT():

commit 8f7ddee60b1812965bc69e4754aea175d4eb41e4

Fix infinite recursion in Add._is_* methods
When SYMPY_USE_CACHE='no', an infinite recursion can be triggered with
Add._eval_is_positive/negative/nonnegative/nonpositive. You can see a
lot of errors from this in the test suite.

The original bug was introduced in:
d1a454a58a9a9bc15980d731ff0f6cbef33c3d77

The fix simply checks whether 's' is equal to self before calling the
recursive function.

To test this, run:
./bin/test -C test_assumptions -k test_issue_4149
./bin/test -C test_assumptions -k test_issues_8632_8633_8638_8675_8992

sympy/core/add.py
@@ -500,7 +500,7 @@ class Add(Expr, AssocOp):
             v = _monotonic_sign(a)
             if v is not None:
                 s = v + c
-                if s.is_positive and a.is_nonnegative:
+                if self != s and s.is_positive and a.is_nonnegative:
                     return True
                 if len(self.free_symbols) == 1:
                     v = _monotonic_sign(self)
@@ -553,7 +553,7 @@ class Add(Expr, AssocOp):
                 v = _monotonic_sign(a)
                 if v is not None:
                     s = v + c
-                    if s.is_nonnegative:
+                    if self != s and s.is_nonnegative:
                         return True
                     if len(self.free_symbols) == 1:
                         v = _monotonic_sign(self)
@@ -568,7 +568,7 @@ class Add(Expr, AssocOp):
                 v = _monotonic_sign(a)
                 if v is not None:
                     s = v + c
-                    if s.is_nonpositive:
+                    if self != s and s.is_nonpositive:
                         return True
                     if len(self.free_symbols) == 1:
                         v = _monotonic_sign(self)
@@ -584,7 +584,7 @@ class Add(Expr, AssocOp):
             v = _monotonic_sign(a)
             if v is not None:
                 s = v + c
-                if s.is_negative and a.is_nonpositive:
+                if self != s and s.is_negative and a.is_nonpositive:
                     return True
                 if len(self.free_symbols) == 1:
                     v = _monotonic_sign(self)