1330147111ebc682cf8ae362daa94746d8a5a6f3
WANG Longqi <iqgnol@gmail.com>
2015-06-13 18:31:11 +0800

    change the test case

--- sympy/core/tests/test_evalf.py
+++ sympy/core/tests/test_evalf.py
@@ -128,7 +128,7 @@ def test_evalf_logs():
     assert NS("log(3+pi*I)", 15) == '1.46877619736226 + 0.808448792630022*I'
     assert NS("log(pi*I)", 15) == '1.14472988584940 + 1.57079632679490*I'
     assert NS('log(-1 + 0.00001)', 2) == '-1.0e-5 + 3.1*I'
-    assert NS('log(2,4,evaluate=False)', 2) == '0.50'
+    assert NS('log(100, 10, evaluate=False)', 15) == '2.00000000000000'
 
 
 def test_evalf_trig():
