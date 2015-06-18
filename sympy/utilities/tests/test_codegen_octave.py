41dbc8ec964ad6651151c94fb5d4df4dab5ee46d
Peter Brady <petertbrady@gmail.com>
2015-06-17 22:08:14 -0600

    Added tests for Octave

--- sympy/utilities/tests/test_codegen_octave.py
+++ sympy/utilities/tests/test_codegen_octave.py
@@ -565,3 +565,25 @@ def test_m_not_supported():
         "end\n"
     )
     assert source == expected
+
+def test_global_vars_octave():
+    x, y, z, t = symbols("x y z t")
+    result = codegen(('f', x*y), "Octave", header=False, empty=False,
+                     global_vars=(y,))
+    source = result[0][1]
+    expected = (
+        "function out1 = f(x)\n"
+        "  out1 = x.*y;\n"
+        "end\n"
+        )
+    assert source == expected
+
+    result = codegen(('f', x*y+z), "Octave", header=False, empty=False,
+                     argument_sequence=(x, y), global_vars=(z, t))
+    source = result[0][1]
+    expected = (
+        "function out1 = f(x, y)\n"
+        "  out1 = x.*y + z;\n"
+        "end\n"
+    )
+    assert source == expected
