c94310480813647c0e70c1f592d507ba596d7a61
Amit Kumar <dtu.amit@gmail.com>
2015-07-03 08:26:27 +0530

    Fix Issue 9536 statement

--- sympy/sets/tests/test_sets.py
+++ sympy/sets/tests/test_sets.py
@@ -857,4 +857,4 @@ def test_SymmetricDifference():
 def test_issue_9536():
     from sympy.functions.elementary.exponential import log
     a = Symbol('a', real=True)
-    assert FiniteSet(log(a)).intersect(S.Reals) == Intersection((-oo, oo), {log(a)})
+    assert FiniteSet(log(a)).intersect(S.Reals) == Intersection(S.Reals, FiniteSet(log(a)))
