0c86cc5f8bf469c0137a0bd8d05c034050666fca
Francesco Bonazzi <franz.bonazzi@gmail.com>
2015-07-25 10:42:46 +0200

    Idx can be index of Product now.

--- sympy/concrete/tests/test_sums_products.py
+++ sympy/concrete/tests/test_sums_products.py
@@ -3,7 +3,7 @@ from sympy import (
     factorial, Function, harmonic, I, Integral, KroneckerDelta, log,
     nan, Ne, Or, oo, pi, Piecewise, Product, product, Rational, S, simplify,
     sqrt, Sum, summation, Symbol, symbols, sympify, zeta, gamma, Le, Indexed,
-    Idx, IndexedBase)
+    Idx, IndexedBase, prod)
 from sympy.abc import a, b, c, d, f, k, m, x, y, z
 from sympy.concrete.summations import telescopic
 from sympy.utilities.pytest import XFAIL, raises
@@ -837,14 +837,21 @@ def test_indexed_idx_sum():
     i = symbols('i', cls=Idx)
     r = Indexed('r', i)
     assert Sum(r, (i, 0, 3)).doit() == sum([r.xreplace({i: j}) for j in range(4)])
+    assert Product(r, (i, 0, 3)).doit() == prod([r.xreplace({i: j}) for j in range(4)])
 
     j = symbols('j', integer=True)
     assert Sum(r, (i, j, j+2)).doit() == sum([r.xreplace({i: Idx(j+k)}) for k in range(3)])
+    assert Product(r, (i, j, j+2)).doit() == prod([r.xreplace({i: Idx(j+k)}) for k in range(3)])
 
     k = Idx('k', range=(1, 3))
     A = IndexedBase('A')
     assert Sum(A[k], k).doit() == sum([A[Idx(j, (1, 3))] for j in range(1, 4)])
+    assert Product(A[k], k).doit() == prod([A[Idx(j, (1, 3))] for j in range(1, 4)])
 
     raises(ValueError, lambda: Sum(A[k], (k, 1, 4)))
     raises(ValueError, lambda: Sum(A[k], (k, 0, 3)))
     raises(ValueError, lambda: Sum(A[k], (k, 2, oo)))
+
+    raises(ValueError, lambda: Product(A[k], (k, 1, 4)))
+    raises(ValueError, lambda: Product(A[k], (k, 0, 3)))
+    raises(ValueError, lambda: Product(A[k], (k, 2, oo)))
