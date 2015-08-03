6277e246174771c5f5bcba0db58d0d31cbea74d3
Björn Dahlgren <bjodah@gmail.com>
2015-08-04 01:17:51 +0200

    Add tests for refine with flipped Eq (issue #9697)

--- sympy/assumptions/tests/test_refine.py
+++ sympy/assumptions/tests/test_refine.py
@@ -74,6 +74,7 @@ def test_Relational():
     assert refine(x >= 0,  Q.is_true(y >= 0)) == (x >= 0)
     assert not refine(Eq(x, 0), ~Q.is_true(Eq(x, 0)))
     assert refine(Eq(x, 0),  Q.is_true(Eq(x, 0)))
+    assert refine(Eq(x, 0),  Q.is_true(Eq(0, x))) == True
     assert refine(Eq(x, 0),  Q.is_true(Eq(y, 0))) == Eq(x, 0)
     assert not refine(Ne(x, 0), ~Q.is_true(Ne(x, 0)))
     assert refine(Ne(x, 0),  Q.is_true(Ne(x, 0)))
@@ -99,7 +100,11 @@ def test_Piecewise():
         Piecewise((1, x >= 0), (3, True))
     assert refine(Piecewise((1, Eq(x, 0)), (3, True)), Q.is_true(Eq(x, 0)))\
         == 1
+    assert refine(Piecewise((1, Eq(x, 0)), (3, True)), Q.is_true(Eq(0, x)))\
+        == 1
     assert refine(Piecewise((1, Eq(x, 0)), (3, True)), ~Q.is_true(Eq(x, 0)))\
+        == 3
+    assert refine(Piecewise((1, Eq(x, 0)), (3, True)), ~Q.is_true(Eq(0, x)))\
         == 3
     assert refine(Piecewise((1, Eq(x, 0)), (3, True)), Q.is_true(Eq(y, 0)))\
         == Piecewise((1, Eq(x, 0)), (3, True))
