6920e7f37d7763e1717d0423b9390ec78c3093d2
Sudhanshu Mishra <mrsud94@gmail.com>
2015-08-12 15:50:06 +0530

    Add the feature for all_models using pycosat

--- sympy/logic/algorithms/pycosat_wrapper.py
+++ sympy/logic/algorithms/pycosat_wrapper.py
@@ -7,16 +7,27 @@ from sympy import default_sort_key
 from sympy.logic.boolalg import conjuncts, to_cnf, to_int_repr, _find_predicates
 
 
-def pycosat_satisfiable(expr):
+def pycosat_satisfiable(expr, all_models):
+    import pycosat
     symbols = sorted(_find_predicates(expr), key=default_sort_key)
     symbols_int_repr = range(1, len(symbols) + 1)
     clauses = conjuncts(to_cnf(expr))
     clauses_int_repr = to_int_repr(clauses, symbols)
 
-    import pycosat
-    r = pycosat.solve(map(list, clauses_int_repr))
-    result = (r != "UNSAT")
-
-    if not result:
-        return result
-    return dict((symbols[abs(lit) - 1], lit > 0) for lit in r)
+    if not all_models:
+        r = pycosat.solve(map(list, clauses_int_repr))
+        result = (r != "UNSAT")
+        if not result:
+            return result
+        return dict((symbols[abs(lit) - 1], lit > 0) for lit in r)
+    else:
+        r = pycosat.itersolve(map(list, clauses_int_repr))
+        result = (r != "UNSAT")
+        if not result:
+            return result
+        sols = []
+        # Make solutions sympy compatible
+        for sol in r:
+            sols.append(dict((symbols[abs(lit) - 1], lit > 0) for lit in sol))
+        # Return generator object
+        return (sol for sol in sols)
