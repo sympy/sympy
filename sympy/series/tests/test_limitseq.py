0b10527cebc2a743445aec79ab5acb4ab173e01a
Sartaj Singh <singhsartaj94@gmail.com>
2015-08-13 04:45:26 +0530

    implement _eval_difference_delta for Add

--- sympy/series/tests/test_limitseq.py
+++ sympy/series/tests/test_limitseq.py
@@ -29,3 +29,13 @@ def test_difference_delta__Sum():
 
     e = Sum(1/k, (k, 1, n), (m, 1, n))
     assert dd(e, n) == harmonic(n)
+
+
+def test_difference_delta__Add():
+    e = n + n*(n + 1)
+    assert dd(e, n) == 2*n + 3
+    assert dd(e, n, 2) == 4*n + 8
+
+    e = n + Sum(1/k, (k, 1, n))
+    assert dd(e, n) == 1 + 1/(n + 1)
+    assert dd(e, n, 5) == 5 + Sum(1/k, (k, n + 1, n + 5))
