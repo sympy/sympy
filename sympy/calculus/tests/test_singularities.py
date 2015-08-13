aa6090b69f64721391dec38de04e8d01d23c48bf
AMiT Kumar <dtu.amit@gmail.com>
2015-08-13 11:14:50 +0530

    Add tests for differential calculus methods

--- sympy/calculus/tests/test_singularities.py
+++ sympy/calculus/tests/test_singularities.py
@@ -1,7 +1,13 @@
 from sympy import Symbol, exp, log
-from sympy.calculus.singularities import singularities
+from sympy.calculus.singularities import (singularities, is_increasing,
+                                          is_strictly_increasing, is_decreasing,
+                                          is_strictly_decreasing)
+from sympy.sets import Interval
+from sympy import oo, S
 
 from sympy.utilities.pytest import XFAIL
+
+x = Symbol('x')
 
 
 def test_singularities():
@@ -19,26 +25,29 @@ def test_singularities_non_rational():
     assert singularities(log((x - 2)**2), x) == (2)
 
 
-@XFAIL
 def test_is_increasing():
-    pass
+    assert is_increasing(x**3 - 3*x**2 + 4*x, S.Reals)
+    assert is_increasing(-x**2, Interval(-oo, 0))
+    assert not is_increasing(-x**2, Interval(0, oo))
+    assert not is_increasing(4*x**3 - 6*x**2 - 72*x + 30, Interval(-2, 3))
 
 
-@XFAIL
 def test_is_strictly_increasing():
-    pass
+    assert is_strictly_increasing(4*x**3 - 6*x**2 - 72*x + 30, Interval.Ropen(-oo, -2))
+    assert is_strictly_increasing(4*x**3 - 6*x**2 - 72*x + 30, Interval.Lopen(3, oo))
+    assert not is_strictly_increasing(4*x**3 - 6*x**2 - 72*x + 30, Interval.open(-2, 3))
+    assert not is_strictly_increasing(-x**2, Interval(0, oo))
 
 
-@XFAIL
 def test_is_decreasing():
-    pass
+    assert is_decreasing(1/(x**2 - 3*x), Interval.open(1.5, 3))
+    assert is_decreasing(1/(x**2 - 3*x), Interval.Lopen(3, oo))
+    assert not is_decreasing(1/(x**2 - 3*x), Interval.Ropen(-oo, S(3)/2))
+    assert not is_decreasing(-x**2, Interval(-oo, 0))
 
 
-@XFAIL
 def test_is_strictly_decreasing():
-    pass
-
-
-@XFAIL
-def is_monotonic():
-    pass
+    assert is_decreasing(1/(x**2 - 3*x), Interval.open(1.5, 3))
+    assert is_decreasing(1/(x**2 - 3*x), Interval.Lopen(3, oo))
+    assert not is_decreasing(1/(x**2 - 3*x), Interval.Ropen(-oo, S(3)/2))
+    assert not is_decreasing(-x**2, Interval(-oo, 0))
