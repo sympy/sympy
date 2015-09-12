494fddf30f7cc6ed7900bbbcb2ba5a7c56fedbe8
Sean Vig <sean.v.775@gmail.com>
2015-09-12 12:25:34 -0500

    Fix typo in cg.py
    Reported by shinya.kat@gmail.com on the mailing list

--- sympy/physics/quantum/tests/test_cg.py
+++ sympy/physics/quantum/tests/test_cg.py
@@ -171,6 +171,7 @@ def test_cg_simp_sum():
 def test_doit():
     assert Wigner3j(1/2, -1/2, 1/2, 1/2, 0, 0).doit() == -sqrt(2)/2
     assert Wigner6j(1, 2, 3, 2, 1, 2).doit() == sqrt(21)/105
+    assert Wigner6j(3, 1, 2, 2, 2, 1).doit() == sqrt(21) / 105
     assert Wigner9j(
         2, 1, 1, S(3)/2, S(1)/2, 1, S(1)/2, S(1)/2, 0).doit() == sqrt(2)/12
     assert CG(1/2, 1/2, 1/2, -1/2, 1, 0).doit() == sqrt(2)/2
