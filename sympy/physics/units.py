9d4d092fe1f602b0f7b5454d637bac5b28c8cbdb
newman101 <jremes@outlook.com>
2015-10-13 09:18:00 +0100

    Draconic year added to units.py

--- sympy/physics/units.py
+++ sympy/physics/units.py
@@ -226,6 +226,7 @@ sidereal_year = sidereal_years = Rational('31558149.540')*s
 tropical_year = tropical_years = Rational('365.24219')*day
 common_year = common_years = Rational('365')*day
 julian_year = julian_years = Rational('365.25')*day
+draconic_year = draconic_years = Rational('346.62')*day
 
 year = years = tropical_year
 
