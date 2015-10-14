2dc1606eec010bc9bcee8aa774d6c46d58082186
Dzhelil Rufat <drufat@caltech.edu>
2015-10-14 16:24:24 -0700

    Add unit tests.

--- sympy/printing/tests/test_numpy.py
+++ sympy/printing/tests/test_numpy.py
@@ -1,6 +1,7 @@
-from sympy import Piecewise
+from sympy import Piecewise, lambdify, Equality, Unequality
 from sympy.abc import x
 from sympy.printing.lambdarepr import NumPyPrinter
+import numpy as np
 
 def test_numpy_piecewise_regression():
     """
@@ -10,3 +11,17 @@ def test_numpy_piecewise_regression():
     """
     p = Piecewise((1, x < 0), (0, True))
     assert NumPyPrinter().doprint(p) == 'select([x < 0,True], [1,0], default=nan)'
+
+
+def test_equality():
+    e = Equality(x, 1)
+
+    f = lambdify((x,), e)
+    x_ = np.array([0, 1, 2])
+    assert np.array_equal(f(x_), [False, True, False])
+
+    e = Unequality(x, 1)
+
+    f = lambdify((x,), e)
+    x_ = np.array([0, 1, 2])
+    assert np.array_equal(f(x_), [True, False, True])
