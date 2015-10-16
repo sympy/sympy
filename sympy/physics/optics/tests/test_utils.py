d844c9aa07a25cbd76242ff94586bafbb7da8f67
newman101 <jremes@outlook.com>
2015-10-16 15:12:08 +0100

    Removed extra EOF from test_utils.py

--- sympy/physics/optics/tests/test_utils.py
+++ sympy/physics/optics/tests/test_utils.py
@@ -110,4 +110,3 @@ def test_hyperfocal_distance():
     f, N, c = symbols('f, N, c')
     assert hyperfocal_distance(f=f, N=N, c=c) == f**2/(N*c)
     assert round(hyperfocal_distance(f=0.5, N=8, c=0.0033), 2) == 9.47
-
