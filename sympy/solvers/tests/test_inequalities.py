c146823d03efa81bccc9031f1c8ac566dcba8691
Chris Smith <smichr@gmail.com>
2015-10-24 22:04:41 -0500

    9451: rework inequality solving
    Issues 9451 and 9954 stem from some problems with how inequalities
    functions are used.
    
    The primary entry point is reduce_inequalities. This function should
    dispatch to the appropiate helper.
    
    -- issue 9954 stemmed from the fact that the change to a real variable
       was happening in solve_univariate_inequality. This has been removed.
    
    -- issue 9451 stemmed from the fact that the expression was dispatched
       to reduce_abs_inequalities rather than solve_univariate_inequality.
    
    Changes were also made to reduce_inequalities to make sure that errors
    are raised only for non-real symbols of interest. Any rational that
    auto-evaluates to a bool is handled there, too, rather than in helper
    functions.

--- sympy/solvers/tests/test_inequalities.py
+++ sympy/solvers/tests/test_inequalities.py
@@ -185,6 +185,7 @@ def test_reduce_abs_inequalities():
 
     nr = Symbol('nr', real=False)
     raises(TypeError, lambda: reduce_inequalities(abs(nr - 5) < 3))
+    assert reduce_inequalities(e, symbols=[x, nr]) == ans
 
 
 def test_reduce_inequalities_general():
@@ -286,27 +287,40 @@ def test_solve_univariate_inequality():
 
 def test_issue_9954():
     assert isolve(x**2 >= 0, x, relational=False) == S.Reals
-    assert isolve(x**2 >= 0, x, relational=True) == S.Reals.as_relational(x)
+    assert isolve(x**2 >= 0, x, relational=True) == \
+        S.Reals.as_relational(x)
     assert isolve(x**2 < 0, x, relational=False) == S.EmptySet
-    assert isolve(x**2 < 0, x, relational=True) == S.EmptySet.as_relational(x)
+    assert isolve(x**2 < 0, x, relational=True) == \
+        S.EmptySet.as_relational(x)
+    assert reduce_inequalities(x**2 >= 0) == S.true
+    assert reduce_inequalities(x**2 < 0) == S.false
 
 
-@slow
-def test_slow_general_univariate():
-    r = RootOf(x**5 - x**2 + 1, 0)
-    assert solve(sqrt(x) + 1/root(x, 3) > 1) == \
-        Or(And(S(0) < x, x < r**6), And(r**6 < x, x < oo))
+def test_radical_univariate():
+    assert str(solve(sqrt(x) + 1/root(x, 3) - 2 > 0
+        ).as_set().n(3)) == "(0, 0.373) U (1.0, +inf)"
+    assert str(solve(sqrt(x - 2) + 1/(x - 3) - 3 > 0
+        ).as_set().n(3)) == '(3.0, 3.57) U (10.2, +inf)'
+    assert str(solve(sqrt(x - 1) - 3 + 1/root(x - 2, 3) > 0
+        ).as_set().n(3)) == '(2.0, 2.14) U (6.79, +inf)'
 
 
 def test_issue_8545():
-    from sympy import refine
     eq = 1 - x - abs(1 - x)
     ans = And(Lt(1, x), Lt(x, oo))
     assert reduce_abs_inequality(eq, '<', x) == ans
     eq = 1 - x - sqrt((1 - x)**2)
-    assert reduce_inequalities(refine(eq) < 0) == ans
+    assert reduce_inequalities(eq < 0) == ans
 
 
 def test_issue_8974():
     assert isolve(-oo < x, x) == And(-oo < x, x < oo)
     assert isolve(oo > x, x) == And(-oo < x, x < oo)
+
+
+def test_issue_9451():
+    assert solve(abs(1/x) < 1).as_set() == Union(Interval.Ropen(-oo, -1), Interval.Lopen(1, oo))
+    assert solve(1/abs(x) + 1/(x - 2) < 2) == Or(
+        And(-oo < x, x < -sqrt(2) + 1),
+        And(-sqrt(5)/2 + S(3)/2 < x, x < 2),
+        And(sqrt(5)/2 + S(3)/2 < x, x < oo))
