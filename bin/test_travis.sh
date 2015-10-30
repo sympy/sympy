9d6aada63603e566a2bf2eee740474a7a66f2353
Aaron Meurer <asmeurer@gmail.com>
2015-10-30 12:04:12 -0500

    Merge several builds together in Travis
    I also reorganized the test_travis.sh script so that it can run multiple
    things for different environment variables at once.

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -13,10 +13,15 @@ if [[ "${TEST_SPHINX}" == "true" ]]; then
     cd _build/latex
     export LATEXOPTIONS="-interaction=nonstopmode"
     make all
-elif [[ "${TEST_SAGE}" == "true" ]]; then
+fi
+
+if [[ "${TEST_SAGE}" == "true" ]]; then
     sage -v
     sage -python bin/test sympy/external/tests/test_sage.py
-elif [[ "${TEST_ASCII}" == "true" ]]; then
+fi
+
+if [[ "${TEST_ASCII}" == "true" ]]; then
+    export OLD_LANG=$LANG
     export LANG=c
     mkdir empty
     cd empty
@@ -26,52 +31,70 @@ sympy.test('print')
 EOF
     cd ..
     bin/doctest
-else
-    # We change directories to make sure that we test the installed version of
-    # sympy.
-    mkdir empty
-    cd empty
+    export LANG=$OLD_LANG
+fi
 
-    if [[ "${TEST_DOCTESTS}" == "true" ]]; then
-        cat << EOF | python
+# We change directories to make sure that we test the installed version of
+# sympy.
+mkdir empty
+cd empty
+
+if [[ "${TEST_DOCTESTS}" == "true" ]]; then
+    cat << EOF | python
 import sympy
 if not sympy.doctest():
     raise Exception('Tests failed')
 EOF
-        cd ..
-        bin/doctest doc/
-    elif [[ "${TEST_SLOW}" == "true" ]]; then
-        cat << EOF | python
+    cd ..
+    bin/doctest doc/
+fi
+
+if [[ "${TEST_SLOW}" == "true" ]]; then
+    cat << EOF | python
 import sympy
 if not sympy.test(split='${SPLIT}', slow=True):
     # Travis times out if no activity is seen for 10 minutes. It also times
     # out if the whole tests run for more than 50 minutes.
     raise Exception('Tests failed')
 EOF
-    elif [[ "${TEST_THEANO}" == "true" ]]; then
-        cat << EOF | python
+fi
+
+if [[ "${TEST_THEANO}" == "true" ]]; then
+    cat << EOF | python
 import sympy
 if not sympy.test('*theano*'):
     raise Exception('Tests failed')
 EOF
-    elif [[ "${TEST_GMPY}" == "true" ]] && [[ "${TEST_MATPLOTLIB}" == "true" ]]; then
-        cat << EOF | python
+fi
+
+if [[ "${TEST_GMPY}" == "true" ]]; then
+    cat << EOF | python
 import sympy
-if not (sympy.test('sympy/polys/', 'sympy/plotting') and
-        sympy.doctest('sympy/polys/', 'sympy/plotting')):
+if not sympy.test('sympy/polys/') and sympy.doctest('sympy/polys/'):
     raise Exception('Tests failed')
 EOF
-    elif [[ "${TEST_AUTOWRAP}" == "true" ]]; then
-        cat << EOF | python
+fi
+
+if [[ "${TEST_MATPLOTLIB}" == "true" ]]; then
+    cat << EOF | python
+import sympy
+if not sympy.test('sympy/plotting') and sympy.doctest('sympy/plotting'):
+    raise Exception('Tests failed')
+EOF
+fi
+
+if [[ "${TEST_AUTOWRAP}" == "true" ]]; then
+    cat << EOF | python
 import sympy
 if not sympy.test('sympy/external/tests/test_autowrap.py'):
     raise Exception('Tests failed')
 EOF
-    else
-        cat << EOF | python
+fi
+
+if [[ "${TEST_SYMPY}" == "true" ]]; then
+    cat << EOF | python
 import sympy
-if not sympy.test(split='${SPLIT}'):
-    raise Exception('Tests failed')
+    if not sympy.test(split='${SPLIT}'):
+       raise Exception('Tests failed')
 EOF
-        fi
 fi
