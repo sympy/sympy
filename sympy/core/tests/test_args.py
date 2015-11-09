6f0017c8da4bfcb32e22ccc5c5ebcf6d18db2956
Aaron Meurer <asmeurer@gmail.com>
2015-11-09 17:09:37 -0600

    Test that objects are recreatable in test_args
    I haven't fixed any of the new test failures yet.

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -80,7 +80,11 @@ def test_all_classes_are_tested():
 
 
 def _test_args(obj):
-    return all(isinstance(arg, Basic) for arg in obj.args)
+    all_basic = all(isinstance(arg, Basic) for arg in obj.args)
+    # Ideally obj.func(*obj.args) would always recreate the object, but for
+    # now, we only require it for objects with non-empty .args
+    recreatable = not obj.args or obj.func(*obj.args) == obj
+    return all_basic and recreatable
 
 
 def test_sympy__assumptions__assume__AppliedPredicate():
