872f6570992c1e53ae0e579410739847f3442fc3
Aaron Meurer <asmeurer@gmail.com>
2015-11-18 17:05:56 -0600

    Fix some simple vector printing test failures
    They were caused by expression re-ordering due to the recent change in the
    args structure of BaseScalar.

--- sympy/vector/tests/test_printing.py
+++ sympy/vector/tests/test_printing.py
@@ -83,8 +83,8 @@ pretty_s = u(
 #This is the pretty form for ((a**2 + b)*N.i + 3*(C.y - c)*N.k) | N.k
 upretty_d_7 = u(
 """\
-⎛ 2    ⎞ (N_i|N_k) + (3⋅C_y - 3⋅c) (N_k|N_k)\n\
-⎝a  + b⎠                                    \
+⎛ 2    ⎞ (N_i|N_k) + (-3⋅c + 3⋅C_y) (N_k|N_k)\n\
+⎝a  + b⎠                                     \
 """)
 pretty_d_7 = u(
 """\
@@ -144,7 +144,7 @@ def test_latex_printing():
     assert latex(v[2]) == '- \\mathbf{\\hat{i}_{N}}'
     assert latex(v[5]) == ('(a)\\mathbf{\\hat{i}_{N}} + ' +
                            '(- b)\\mathbf{\\hat{j}_{N}}')
-    assert latex(v[6]) == ('(\\mathbf{{x}_{N}} + a^{2})\\mathbf{\\' +
+    assert latex(v[6]) == ('(a^{2} + \\mathbf{{x}_{N}})\\mathbf{\\' +
                            'hat{i}_{N}} + \\mathbf{\\hat{k}_{N}}')
     assert latex(v[8]) == ('\\mathbf{\\hat{j}_{N}} + (\\mathbf{{x}_' +
                            '{C}}^{2} - \\int f{\\left (b \\right )}\\,' +
