aef67eb9ce409800d2889b26094b10966a489c4c
Chris Smith <smichr@gmail.com>
2015-11-18 20:37:37 -0600

    10156: do not use has when testing overlap of variables in Sum
    Also modify polytools/_symbolic_factor_list to only use
    _eval_factor *once* per arg.

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -5611,16 +5611,14 @@ def _symbolic_factor_list(expr, opt, method):
     """Helper function for :func:`_symbolic_factor`. """
     coeff, factors = S.One, []
 
-    args = list(Mul.make_args(expr))
+    args = [i._eval_factor() if hasattr(i, '_eval_factor') else i for i in Mul.make_args(expr)]
     for arg in args:
         if arg.is_Number:
             coeff *= arg
             continue
-        if hasattr(arg, '_eval_factor'):
-            arg = arg._eval_factor()
-            if arg.is_Mul:
-                args.extend(arg.args)
-                continue
+        if arg.is_Mul:
+            args.extend(arg.args)
+            continue
         if arg.is_Pow:
             base, exp = arg.args
             if base.is_Number:
