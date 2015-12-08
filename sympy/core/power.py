3d6c2e55deaa153ae1bf7f7755f905be93ad6528
Gaurav Dhingra <axyd0000@gmail.com>
2015-12-08 12:15:49 +0530

    explanation for oo**e for non-real `e` added

--- sympy/core/power.py
+++ sympy/core/power.py
@@ -124,14 +124,17 @@ class Pow(Expr):
     | (-oo)**oo    | nan     |                                               |
     | (-oo)**-oo   |         |                                               |
     +--------------+---------+-----------------------------------------------+
-    | oo**I        | nan     |                                               |
-    | (-oo)**I     |         |                                               |
+    | oo**I        | nan     | oo**e could probably be best thought of as    |
+    | (-oo)**I     |         | the limit of x**e for real x tending to oo.   |
+    |              |         | If e is I , then the limit does not exist and |
+    |              |         | nan is used to indicate that.                 |
     +------------------------------------------------------------------------+
-    | oo**(1+I)    | zoo     |                                               |
-    | (-oo)**(1+I) |         |                                               |
+    | oo**(1+I)    | zoo     | If the real part of e is positive, then the   |
+    | (-oo)**(1+I) |         | limit of |x**e| is oo; so the limit of x**e   |
+    |              |         | oo. So limit is zoo.                          |
     +------------------------------------------------------------------------+
-    | oo**(-1+I)   | 0       |                                               |
-    | (-oo)**(-1+I)|         |                                               |
+    | oo**(-1+I)   | 0       | If the real part of e is negative, then the   |
+    | (-oo)**(-1+I)|         | limit is 0.                                   |
     +------------------------------------------------------------------------+
 
     Because symbolic computations are more flexible that floating point
