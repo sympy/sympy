a5431a628166c9370d75bc2ddc3a9967b77b8676
Meghana <meghana.madhyastha@gmail.com>
2015-12-19 15:44:50 +0530

    fixing the helper function argument in autowrap

--- sympy/utilities/autowrap.py
+++ sympy/utilities/autowrap.py
@@ -505,6 +505,15 @@ def autowrap(
     code_generator = get_code_generator(language, "autowrap")
     CodeWrapperClass = _get_code_wrapper_class(backend)
     code_wrapper = CodeWrapperClass(code_generator, tempdir, flags, verbose)
+
+    helps = []
+    for name_h, expr_h, args_h in helpers:
+        helps.append(make_routine(name_h, expr_h, args_h))
+
+    for name_h, expr_h, args_h in helpers:
+        if expr.has(expr_h):
+            name_h = binary_function(name_h, expr_h, backend = 'dummy')
+            expr = expr.subs(expr_h, name_h(*args_h))
     try:
         routine = make_routine('autofunc', expr, args)
     except CodeGenArgumentListError as e:
@@ -517,10 +526,6 @@ def autowrap(
                 raise
             new_args.append(missing.name)
         routine = make_routine('autofunc', expr, args + new_args)
-
-    helps = []
-    for name, expr, args in helpers:
-        helps.append(make_routine(name, expr, args))
 
     return code_wrapper.wrap_code(routine, helpers=helps)
 
