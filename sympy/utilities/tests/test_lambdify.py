729909d194ca5cb23baea6c83f88d13936eb1421
Shekhar Prasad Rajak <shekharrajak@live.com>
2015-12-30 15:34:07 +0530

    minor edit

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -606,8 +606,8 @@ def test_special_printers():
 
     expr = sympy.sqrt(sympy.sqrt(2) + sympy.sqrt(3)) + sympy.S(1)/2
 
-    func0 = lambdify((), expr, modules="mpmath", printer=intervalrepr())
-    func1 = lambdify((), expr, modules="mpmath", printer=IntervalPrinter())
+    func0 = lambdify((), expr, modules="mpmath", printer=intervalrepr)
+    func1 = lambdify((), expr, modules="mpmath", printer=IntervalPrinter)
     func2 = lambdify((), expr, modules="mpmath", printer=IntervalPrinter())
 
     mpi = type(mpmath.mpi(1, 2))
