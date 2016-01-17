ab7eab7bc2011854358bb2ac38669f3faa271843
Chris Smith <smichr@gmail.com>
2016-01-16 23:55:28 -0600

    bbp_pi modifications
    - allow an arbitrary number of digits to be computed
    - make sure that leading 0s are shown; this requires
      that the number be truncated before the formatting
      occurs.

--- sympy/ntheory/bbp_pi.py
+++ sympy/ntheory/bbp_pi.py
@@ -33,7 +33,7 @@ for increasing n and an effective fit was found between n and the
 working precision value.
 
 2. The while loop to evaluate whether the series has converged has be
-replaced with a fixed for loop, that option was selected because in a
+replaced with a fixed for-loop, that option was selected because in a
 very large number of cases the loop converged to a point where no
 difference can be detected in less than 15 iterations. (done for more
 accurate memory and time banking).
@@ -51,31 +51,45 @@ import math
 from sympy.core.compatibility import range
 
 
-def _series(j, n):
+def _series(j, n, prec=14):
 
     # Left sum from the bbp algorithm
     s = 0
-    D = _dn(n)
+    D = _dn(n, prec)
+    D4 = 4 * D
+    k = 0
+    d = 8 * k + j
     for k in range(n + 1):
-        r = 8*k + j
-        s += (pow(16, n - k, r) << 4 * D) // r
+        s += (pow(16, n - k, d) << D4) // d
+        d += 8
 
-    # Right sum. should iterate to infinty, but now just iterates to the point where
-    # one iterations change is beyond the resolution of the data type used
+    # Right sum iterates to infinity for full precision, but we
+    # stop at the point where one iteration is beyond the precision
+    # specified.
 
     t = 0
-    for k in range(n + 1, n + 15):
-        xp = int(16**(n - k) * 16**D)
-        t += xp // (8 * k + j)
+    k = n + 1
+    e = 4*(D + n - k)
+    d = 8 * k + j
+    while True:
+        dt = (1 << e) // d
+        if not dt:
+            break
+        t += dt
+        # k += 1
+        e -= 4
+        d += 8
     total = s + t
 
     return total
 
 
-def pi_hex_digits(n):
-    """Returns a string containing 14 digits after the nth value of pi in hex
-       The decimal has been taken out of the number, so
-       n = 0[0] = 3 # First digit of pi in hex, 3
+def pi_hex_digits(n, prec=14):
+    """Returns a string containing ``prec`` (default 14) digits
+    starting at the nth digit of pi in hex. Counting of digits
+    starts at 0 and the decimal is not counted, so for n = 0 the
+    returned value starts with 3; n = 1 corresponds to the first
+    digit past the decimal point (which in hex is 2).
 
     Examples
     ========
@@ -83,8 +97,13 @@ def pi_hex_digits(n):
     >>> from sympy.ntheory.bbp_pi import pi_hex_digits
     >>> pi_hex_digits(0)
     '3243f6a8885a30'
-    >>> pi_hex_digits(10)
-    '5a308d313198a2'
+    >>> pi_hex_digits(13)
+    '08d313198a2e03'
+
+    References
+    ==========
+
+    .. [1] http://www.numberworld.org/digits/Pi/
     """
 
     # main of implementation arrays holding formulae coefficients
@@ -93,18 +112,16 @@ def pi_hex_digits(n):
     j = [1, 4, 5, 6]
 
     #formulae
-    x = + (a[0]*_series(j[0], n)
-         - a[1]*_series(j[1], n)
-         - a[2]*_series(j[2], n)
-         - a[3]*_series(j[3], n)) & (16**(_dn(n)) - 1)
+    D = _dn(n, prec)
+    x = + (a[0]*_series(j[0], n, prec)
+         - a[1]*_series(j[1], n, prec)
+         - a[2]*_series(j[2], n, prec)
+         - a[3]*_series(j[3], n, prec)) & (16**D - 1)
 
-    s = ("%014x" % x)
-    #s is constrained between 0 and 14
-    return s[:14]
+    s = ("%0" + "%ix" % prec) % (x // 16**(D - prec))
+    return s
 
 
-def _dn(n):
+def _dn(n, prec):
     # controller for n dependence on precision
-    if (n < 1000):
-        return 16
-    return int(math.log10(n//1000) + 18)
+    return int(math.log(n + prec)/math.log(16) + prec + 2)
