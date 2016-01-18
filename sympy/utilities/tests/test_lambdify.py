9e8cb2d72af967e2f040e27d04cc3a4463269e41
Shekhar Prasad Rajak <shekharrajak@live.com>
2016-01-18 13:00:23 +0530

    import Min

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -9,6 +9,7 @@ from sympy.utilities.lambdify import implemented_function
 from sympy.utilities.pytest import skip
 from sympy.utilities.decorator import conserve_mpmath_dps
 from sympy.external import import_module
+from sympy.functions.elementary.miscellaneous import Max, Min
 import math
 import sympy
 
