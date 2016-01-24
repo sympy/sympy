d376d579ad0db42c1c68a875ce058027603785b9
AMiT Kumar <dtu.amit@gmail.com>
2016-01-24 20:33:50 +0530

    Remove useless import

--- sympy/geometry/point.py
+++ sympy/geometry/point.py
@@ -10,7 +10,7 @@ Point3D
 
 from __future__ import division, print_function
 
-from sympy.core import S, sympify, Pow
+from sympy.core import S, sympify
 from sympy.core.compatibility import iterable, range
 from sympy.core.containers import Tuple
 from sympy.simplify import nsimplify, simplify
