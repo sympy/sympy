3d6f52acd74518e37afaf1f1f276879dab5babeb
Ashutosh Saboo <ashutosh.saboo96@gmail.com>
2016-01-28 20:14:00 +0530

    Updated Test File for Issue No 10388 | ashutosh.saboo96@gmail.com

--- sympy/functions/combinatorial/tests/test_comb_factorials.py
+++ sympy/functions/combinatorial/tests/test_comb_factorials.py
@@ -5,6 +5,7 @@ from sympy.functions.combinatorial.factorials import subfactorial
 from sympy.functions.special.gamma_functions import uppergamma
 from sympy.utilities.pytest import XFAIL
 
+#Solves and Fixes Issue #10388 - This is the updated test for the same solved issue
 
 def test_rf_eval_apply():
     x, y = symbols('x,y')
@@ -194,7 +195,7 @@ def test_factorial2():
     nt = Symbol('nt', nonnegative=True)
     nf = Symbol('nf', nonnegative=False)
     nn = Symbol('nn')
-
+    #Solves and Fixes Issue #10388 - This is the updated test for the same solved issue
     raises(ValueError, lambda: factorial2(S(5)/2))
     assert factorial2(n).is_integer is None
     assert factorial2(tt - 1).is_integer
