163604d1bdf1e8c2c8f6bceed2c681a0f70f0b3f
Shekhar Prasad Rajak <shekharrajak@live.com>
2016-01-30 14:27:46 +0530

    another testcase added

--- sympy/solvers/tests/test_solveset.py
+++ sympy/solvers/tests/test_solveset.py
@@ -1054,9 +1054,11 @@ def test_issue_10426():
     _n =Dummy('_n')
     _x =Dummy('_x')
     _y =Dummy('_y')
-    assert solveset(sin(x + a1) - sin(x), a1, domain=S.Reals) == imageset(Lambda(_n, 2*_n*pi), S.Integers)
-    #assert solveset(sin(x + a) - sin(x), a) == \
-    #ConditionSet(a, Eq(-sin(x) + sin(a + x), 0), ComplexRegion(Lambda((_x, _y), _x + _y*I), Interval(-oo, oo)))
+    assert solveset(sin(x + a) - sin(x), a) == \
+    ConditionSet(a, Eq(-sin(x) + sin(a + x), 0), S.Complexes)
+    #assert solveset(sin(x + a1) - sin(x), a1, domain=S.Reals) == imageset(Lambda(_n, 2*_n*pi), S.Integers)
+    # assert solveset(sin(x + a) - sin(x), a) == \
+    # ConditionSet(a, Eq(-sin(x) + sin(a + x), 0), ComplexRegion(Lambda((_x, _y), _x + _y*I), Interval(-oo, oo)))
 
 
 def test_issue_9913():
