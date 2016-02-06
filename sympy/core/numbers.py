284c86ab3342eaf31a88a841d1753166119ca24f
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2016-02-06 13:02:41 +0530

    Special case Rational * 1 and 1 * Rational
    Avoids a call to igcd which takes a lot of time if the numerator and denominator of the Rational are large.

--- sympy/core/numbers.py
+++ sympy/core/numbers.py
@@ -1388,7 +1388,11 @@ class Rational(Number):
 
     @_sympifyit('other', NotImplemented)
     def __mul__(self, other):
-        if isinstance(other, Rational):
+        if self is S.One:
+            return other
+        elif other is S.One:
+            return self
+        elif isinstance(other, Rational):
             return Rational(self.p*other.p, self.q*other.q)
         elif isinstance(other, Float):
             return other*self
