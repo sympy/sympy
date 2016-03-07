1d21440a605738d26ae59f8810b14ba6beff84d2
Sanya Khurana <sanya@monica.in>
2016-03-07 14:59:52 +0530

    Fixing simpify (evaluate = False) for Unary Operators - Issue #10560
    `>>> sympify('4*-3', evaluate=False)`
    `(-3)*4`

--- sympy/parsing/sympy_parser.py
+++ sympy/parsing/sympy_parser.py
@@ -931,6 +931,12 @@ class EvaluateFalseTransformer(ast.NodeTransformer):
         if node.op.__class__ in self.operators:
             sympy_class = self.operators[node.op.__class__]
             right = self.visit(node.right)
+            left = self.visit(node.left)
+
+            if isinstance(node.left, ast.UnaryOp) and (isinstance(node.right, ast.UnaryOp) == 0) and sympy_class in ('Mul'):
+                temp = right
+                right = left
+                left = temp
 
             if isinstance(node.op, ast.Sub):
                 right = ast.UnaryOp(op=ast.USub(), operand=right)
@@ -945,7 +951,7 @@ class EvaluateFalseTransformer(ast.NodeTransformer):
 
             new_node = ast.Call(
                 func=ast.Name(id=sympy_class, ctx=ast.Load()),
-                args=[self.visit(node.left), right],
+                args=[left, right],
                 keywords=[ast.keyword(arg='evaluate', value=ast.Name(id='False', ctx=ast.Load()))],
                 starargs=None,
                 kwargs=None
