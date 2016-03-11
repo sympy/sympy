17087daf8a2ad5c277ecbddda6cd19507c861cb0
Akshay Siramdas <akshaysiramdas@gmail.com>
2016-03-11 12:42:09 +0530

    TODO part of integrals/trigonometry.py

--- sympy/integrals/trigonometry.py
+++ sympy/integrals/trigonometry.py
@@ -23,10 +23,11 @@ def _integer_instance(n):
 def _pat_sincos(x):
     a = Wild('a', exclude=[x])
     b = Wild('b', exclude=[x])
+    c = Wild('c')
     n, m = [Wild(s, exclude=[x], properties=[_integer_instance])
                 for s in 'nm']
-    pat = sin(a*x)**n * cos(b*x)**m
-    return pat, a, b, n, m
+    pat = c*sin(a*x)**n * cos(b*x)**m
+    return pat, a, b, c, n, m
 
 _u = Dummy('u')
 
@@ -59,7 +60,7 @@ def trigintegrate(f, x, conds='piecewise'):
     sympy.integrals.integrals.Integral
     """
     from sympy.integrals.integrals import integrate
-    pat, a, b, n, m = _pat_sincos(x)
+    pat, a, b, c, n, m = _pat_sincos(x)
 
     f = f.rewrite('sincos')
     M = f.match(pat)
@@ -74,12 +75,13 @@ def trigintegrate(f, x, conds='piecewise'):
 
     a = M[a]
     b = M[b]
+    c = M[c]
     
     if m == 1 and n == 1:
         if a == b:
-            return 1/2 * -cos(2*a*x)/2*a
+            return c/2 * -cos(2*a*x)/2*a
         else:
-            sol = (-cos((a+b)*x)/(2*(a+b))) - (cos((a-b)*x)/(2*(a-b)))
+            sol = c * ((-cos((a+b)*x)/(2*(a+b))) - (cos((a-b)*x)/(2*(a-b))))
             return sol   
 
     if n.is_odd or m.is_odd:
