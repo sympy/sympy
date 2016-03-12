e154ea006ec3e69e46d85ecc00977a865e613d2a
Abhishek Verma <iamvermaabhishek@gmail.com>
2016-03-12 19:24:21 +0530

    "Removed Trailing whitespace from line 7 in sympy/integrals/tests/test_heurisch.py"

--- sympy/integrals/tests/test_heurisch.py
+++ sympy/integrals/tests/test_heurisch.py
@@ -4,7 +4,7 @@ from sympy import Rational, sqrt, symbols, sin, exp, log, sinh, cosh, cos, pi, \
     besselj, besselk, bessely, jn
 from sympy.integrals.heurisch import components, heurisch, heurisch_wrapper
 from sympy.utilities.pytest import XFAIL, skip, slow, ON_TRAVIS
-from sympy.integrals.integrals import integrate 
+from sympy.integrals.integrals import integrate
 x, y, z, nu = symbols('x,y,z,nu')
 f = Function('f')
 
