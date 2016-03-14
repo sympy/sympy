e376f8b205cf4e18b63ef017ab210187abb6fc75
Aravind Reddy <aravindreddy255@gmail.com>
2016-03-14 23:50:21 +0530

    Fixed #10701: is_nilpotent() modified

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -2080,6 +2080,8 @@ class MatrixBase(object):
         >>> a.is_nilpotent()
         False
         """
+        if not self:
+            return True
         if not self.is_square:
             raise NonSquareMatrixError(
                 "Nilpotency is valid only for square matrices")
