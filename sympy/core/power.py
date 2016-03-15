dd9a01da64270e85c6695cc7305d13f52ae4e466
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-03-15 14:41:48 +0000

    Improved power substitutions Fixes issue 10829
    This commit makes `(4**x).subs(2**x, y)` -> `2**y`

--- sympy/core/power.py
+++ sympy/core/power.py
@@ -565,6 +565,12 @@ class Pow(Expr):
         if old == self.base:
             return new**self.exp._subs(old, new)
 
+        # issue 10829: (4**x - 3*y + 2).subs(2**x, y) -> y**2 - 3*y + 2
+        if old.func is self.func and self.exp == old.exp:
+            l = log(self.base, old.base)
+            if l.is_Number:
+                return Pow(new, l)
+
         if old.func is self.func and self.base == old.base:
             if self.exp.is_Add is False:
                 ct1 = self.exp.as_independent(Symbol, as_Add=False)
