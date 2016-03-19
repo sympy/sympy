21def0f310e66f11ef46f43c8b7b078d225904ea
Jatin Yadav <jatinyadav25@gmail.com>
2016-03-19 18:48:30 +0530

    Update generate.py
    Updated algorithm description

--- sympy/ntheory/generate.py
+++ sympy/ntheory/generate.py
@@ -231,19 +231,51 @@ def primepi(n):
 
         Algorithm Description:
 
-        Let phi(a,b) denote the number of integers<=a
-        which remain after sieving from primes <= b
+        In sieve method, we remove all multiples of prime p
+        except p itself.
 
-        Clearly pi(n) = phi(n,sqrt(n))
-        Following recursion is used and implemented as dp:
-        phi(a,b) = phi(a,b-1) , if b is not a prime
-        phi(a,b) = phi(a,b-1)-phi(a/b,b-1) + phi(p-1,p-1), if b is prime
+        Let phi(i,j) be the number of integers 2<=k<=i
+        which remain after sieving from primes less than
+        or equal to j.
+        Clearly, pi(n) = phi(n,sqrt(n))
+
+        If j is not a prime,
+        phi(i,j) = phi(i,j-1)
+
+        if j is a prime,
+        We remove all numbers(except j) whose
+        smallest prime factor is j.
+
+        Let x=(j*a) be such a number, where 2<=a<=i/j
+        Now, after sieving from primes less than equal to j-1,
+        a must remain
+        (because x, and hence a has no prime factor <= j-1)
+        Clearly, there are phi(i/j,j-1) such a which remain on
+        sieving from primes <= j-1
+
+        Now, if a is a prime less than equal to j-1 ,
+        x= j*a, has smallest prime factor = a.
+        So, it has already been removed(by sieving from a).
+        So, we don't need to remove it again.
+        (Note: there will be pi(j-1) such x)
+
+        Thus, number of x, that will be removed are:
+        phi(i/j,j-1) - phi(j-1,j-1)
+        (Note that pi(j-1) has been written as phi(j-1,j-1))
+
+        => phi(i,j) = phi(i,j-1) - phi(i/j,j-1) + phi(j-1,j-1)
+
+        So,following recursion is used and implemented as dp:
+
+        phi(a,b) = phi(a,b-1), if b is not a prime
+        phi(a,b) = phi(a,b-1)-phi(a/b,b-1) + phi(b-1,b-1), if b is prime
 
         Clearly a is always of the form floor(n/k),
         which can take at most 2*sqrt(n) values.
         Two arrays arr1,arr2 are maintained
         arr1[i] = phi(i,j),
         arr2[i] = phi(n//i,j)
+
         Finally the answer is arr2[1]
 
         Examples
@@ -273,14 +305,14 @@ def primepi(n):
     arr2 = [0]*(lim+1)
     for i in range(1, lim+1):
         arr1[i] = i-1
-        arr2[i] = 0 if i==0 else n//i - 1
+        arr2[i] = n//i - 1
     for i in range(2, lim+1):
-        #Presently, arr1[k]=phi(k,i-1),
-        #arr2[k] = phi(n//k,i-1)
+        # Presently, arr1[k]=phi(k,i-1),
+        # arr2[k] = phi(n//k,i-1)
         if arr1[i] == arr1[i-1]:
             continue
         p = arr1[i-1]
-        for j in range(1,min(n//(i*i) , lim) + 1):
+        for j in range(1,min(n//(i*i), lim) + 1):
             st = i*j
             if st <= lim:
                 arr2[j] -= arr2[st] - p
