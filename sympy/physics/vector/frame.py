cb997480c7241cf0db8040ad890c0a3a702b7dc2
Jason K. Moore <moorepants@gmail.com>
2016-03-24 16:21:04 -0700

    Added a partial velocity method to ReferenceFrame.

--- sympy/physics/vector/frame.py
+++ sympy/physics/vector/frame.py
@@ -733,6 +733,46 @@ class ReferenceFrame(object):
         """The basis Vector for the ReferenceFrame, in the z direction. """
         return self._z
 
+    def partial_velocity(self, frame, *gen_speeds):
+        """Returns the partial angular velocities of this frame in the given
+        frame with respect to one or more provided generalized speeds.
+
+        Parameters
+        ==========
+        frame : ReferenceFrame
+            The frame with which the angular velocity is defined in.
+        gen_speeds : functions of time
+            The generalized speeds.
+
+        Returns
+        =======
+        partial_velocities : tuple of Vector
+            The partial angular velocity vectors corresponding to the provided
+            generalized speeds.
+
+        Examples
+        ========
+
+        >>> from sympy.physics.vector import ReferenceFrame, dynamicsymbols
+        >>> N = ReferenceFrame('N')
+        >>> A = ReferenceFrame('A')
+        >>> u1, u2 = dynamicsymbols('u1, u2')
+        >>> A.set_ang_vel(N, u1 * A.x + u2 * N.y)
+        >>> A.partial_velocity(N, u1)
+        A.x
+        >>> A.partial_velocity(N, u1, u2)
+        (A.x, N.y)
+
+        """
+
+        partials = [self.ang_vel_in(frame).diff(speed, frame, var_in_dcm=False)
+                    for speed in gen_speeds]
+
+        if len(partials) == 1:
+            return partials[0]
+        else:
+            return tuple(partials)
+
 
 def _check_frame(other):
     from .vector import VectorTypeError
