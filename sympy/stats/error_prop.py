d066c8e05cb6bf5b9a1c0461a2f828d756186fab
Anthony Scopatz <scopatz@gmail.com>
2016-05-18 16:46:18 -0400

    added covariance computation

--- sympy/stats/error_prop.py
+++ sympy/stats/error_prop.py
@@ -1,12 +1,14 @@
 """Tools for arithmetic error propogation."""
 from __future__ import print_function, division
-from itertools import repeat
+from itertools import repeat, combinations
 
-from sympy import Symbol, Add, Mul, simplify, Pow, exp
+from sympy import S, Symbol, Add, Mul, simplify, Pow, exp
 from sympy.stats.symbolic_probability import RandomSymbol, Variance, Covariance
 
+_arg0_or_var = lambda var: var.args[0] if len(var.args) > 0 else var
 
-def variance_prop(expr, consts=()):
+
+def variance_prop(expr, consts=(), include_covar=False):
     """Symbolically propagates variance (`\sigma^2`) for expressions.
     This is computed as as seen in [1]_.
 
@@ -18,6 +20,8 @@ def variance_prop(expr, consts=()):
         Represents symbols that are known constants in the expr,
         and thus have zero variance. All symbols not in consts are
         assumed to be variant.
+    include_covar : bool, optional
+        Flag for whether or not to include covariances, default=False.
 
     Returns
     =======
@@ -50,93 +54,36 @@ def variance_prop(expr, consts=()):
     args = expr.args
     if len(args) == 0:
         if expr in consts:
-            return 0
+            return S(0)
         elif isinstance(expr, RandomSymbol):
             return Variance(expr)
         elif isinstance(expr, Symbol):
             return Variance(RandomSymbol(expr))
         else:
-            return 0
-    var_args = list(map(variance_prop, args, repeat(consts)))
+            return S(0)
+    var_args = list(map(variance_prop, args, repeat(consts),
+                        repeat(include_covar)))
     if isinstance(expr, Add):
-        return Add(*var_args)
+        var_expr = Add(*var_args)
+        if include_covar:
+            terms = [2 * Covariance(_arg0_or_var(x), _arg0_or_var(y)) \
+                     for x, y in combinations(var_args, 2)]
+            var_expr += Add(*terms)
     elif isinstance(expr, Mul):
         terms = [v/a**2 for a, v in zip(args, var_args)]
-        return simplify(expr**2 * Add(*terms))
+        var_expr = simplify(expr**2 * Add(*terms))
+        if include_covar:
+            terms = [2*Covariance(_arg0_or_var(x), _arg0_or_var(y))/(a*b) \
+                     for (a, b), (x, y) in zip(combinations(args, 2),
+                                               combinations(var_args, 2))]
+            var_expr += Add(*terms)
     elif isinstance(expr, Pow):
         b = args[1]
         v = var_args[0] * (expr * b / args[0])**2
-        return simplify(v)
+        var_expr = simplify(v)
     elif isinstance(expr, exp):
-        return simplify(var_args[0] * expr**2)
+        var_expr = simplify(var_args[0] * expr**2)
     else:
         # unknown how to proceed, return variance of whole expr.
-        return Variance(expr)
-
-
-def covariance_prop(expr, consts=()):
-    """Symbolically propagates covariance (`\sigma_{AB}`) for expressions.
-    This is computed as as seen in [1]_.
-
-    Parameters
-    ==========
-    expr : Expr
-        A sympy expression to compute the covariance for.
-    consts : sequence of Symbols, optional
-        Represents symbols that are known constants in the expr,
-        and thus have zero variance. All symbols not in consts are
-        assumed to be variant.
-
-    Returns
-    =======
-    var_expr : Expr
-        An expression for the total variance of the expr.
-        The variance for the original symbols (e.g. x) are represented
-        via instance of the Variance symbol (e.g. Variance(x)).
-
-    Examples
-    ========
-
-    >>> from sympy import symbols, exp
-    >>> from sympy.stats.error_prop import covariance_prop
-    >>> x, y = symbols('x y')
-
-    >>> covariance_prop(x + y)
-    Variance(x) + Variance(y)
-
-    >>> variance_prop(x * y)
-    Variance(x)*y**2 + Variance(y)*x**2
-
-    >>> variance_prop(exp(2*x))
-    4*Variance(x)*exp(4*x)
-
-    References
-    ==========
-    .. [1] https://en.wikipedia.org/wiki/Propagation_of_uncertainty
-
-    """
-    args = expr.args
-    if len(args) == 0:
-        if expr in consts:
-            return 0
-        elif isinstance(expr, RandomSymbol):
-            return expr
-        elif isinstance(expr, Symbol):
-            return RandomSymbol(expr)
-        else:
-            return 0
-    covar_args = list(map(covariance_prop, args, repeat(consts)))
-    if isinstance(expr, Add):
-        return Add(*var_args)
-    elif isinstance(expr, Mul):
-        terms = [v/a**2 for a, v in zip(args, var_args)]
-        return simplify(expr**2 * Add(*terms))
-    elif isinstance(expr, Pow):
-        b = args[1]
-        v = var_args[0] * (expr * b / args[0])**2
-        return simplify(v)
-    elif isinstance(expr, exp):
-        return simplify(var_args[0] * expr**2)
-    else:
-        # unknown how to proceed, return variance of whole expr.
-        return Variance(expr)
+        var_expr = Variance(expr)
+    return var_expr
--- sympy/stats/tests/test_error_prop.py
+++ sympy/stats/tests/test_error_prop.py
@@ -1,5 +1,6 @@
 from sympy import symbols, exp, Function
-from sympy.stats.symbolic_probability import RandomSymbol, Variance
+from sympy.stats.symbolic_probability import (RandomSymbol, Variance,
+        Covariance)
 from sympy.stats.error_prop import variance_prop
 
 
@@ -26,4 +27,32 @@ def test_variance_prop():
         }
     for inp, out in cases.items():
         obs = variance_prop(inp, consts=consts)
+        assert out == obs
+
+def test_variance_prop_with_covar():
+    x, y, z = symbols('x y z')
+    phi, t = consts = symbols('phi t')
+    a = RandomSymbol(x)
+    var_x = Variance(a)
+    b = RandomSymbol(y)
+    var_y = Variance(b)
+    c = RandomSymbol(z)
+    var_z = Variance(c)
+    covar_x_y = Covariance(a, b)
+    covar_x_z = Covariance(a, c)
+    covar_y_z = Covariance(b, c)
+    cases = {
+        x + y: var_x + var_y + 2*covar_x_y,
+        a + y: var_x + var_y + 2*covar_x_y,
+        x + y + z: var_x + var_y + var_z + \
+                   2*covar_x_y + 2*covar_x_z + 2*covar_y_z,
+        2*x: 4*var_x,
+        x*y: var_x*y**2 + var_y*x**2 + 2*covar_x_y/(x*y),
+        1/x: var_x/x**4,
+        exp(x): var_x*exp(2*x),
+        exp(2*x): 4*var_x*exp(4*x),
+        exp(-x*t): t**2*var_x*exp(-2*t*x),
+        }
+    for inp, out in cases.items():
+        obs = variance_prop(inp, consts=consts, include_covar=True)
         assert out == obs
