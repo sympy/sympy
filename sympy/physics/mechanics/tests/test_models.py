896858acb1ddb4337262d16d5ecf4a02b1f6a6af
James Brandon Milam <jmilam343@gmail.com>
2016-05-30 09:32:16 -0400

    Changed the comparisons of the sympy test_models
    The tests for test_models now compare the mass_matrix and forcing
    vectors of the results instead of calling rhs(). This has significantly
    simplified the test code and should result in major speed improvements.

--- sympy/physics/mechanics/tests/test_models.py
+++ sympy/physics/mechanics/tests/test_models.py
@@ -1,5 +1,5 @@
 import sympy.physics.mechanics.models as models
-from sympy import (cos, sin, Matrix, symbols, simplify)
+from sympy import (cos, sin, Matrix, symbols, simplify, zeros)
 from sympy.physics.mechanics import (dynamicsymbols)
 
 
@@ -10,20 +10,28 @@ def test_multi_mass_spring_damper_inputs():
     v0, x0, f0 = dynamicsymbols("v0 x0 f0")
 
     kane1 = models.multi_mass_spring_damper(1)
-    symexpr1 = Matrix([[v0], [(-c0*v0 - k0*x0)/m0]])
-    assert simplify(symexpr1 - kane1.rhs()) == Matrix([0, 0])
+    massmatrix1 = Matrix([[m0]])
+    forcing1 = Matrix([[-c0*v0 - k0*x0]])
+    assert simplify(massmatrix1 - kane1.mass_matrix) == Matrix([0])
+    assert simplify(forcing1 - kane1.forcing) == Matrix([0])
 
     kane2 = models.multi_mass_spring_damper(1, True)
-    symexpr2 = Matrix([[v0], [(-c0*v0 + g*m0 - k0*x0)/m0]])
-    assert simplify(symexpr2 - kane2.rhs()) == Matrix([0, 0])
+    massmatrix2 = Matrix([[m0]])
+    forcing2 = Matrix([[-c0*v0 + g*m0 - k0*x0]])
+    assert simplify(massmatrix2 - kane2.mass_matrix) == Matrix([0])
+    assert simplify(forcing2 - kane2.forcing) == Matrix([0])
 
     kane3 = models.multi_mass_spring_damper(1, True, True)
-    symexpr3 = Matrix([[v0], [(-c0*v0 + g*m0 - k0*x0 + f0)/m0]])
-    assert simplify(symexpr3 - kane3.rhs()) == Matrix([0, 0])
+    massmatrix3 = Matrix([[m0]])
+    forcing3 = Matrix([[-c0*v0 + g*m0 - k0*x0 + f0]])
+    assert simplify(massmatrix3 - kane3.mass_matrix) == Matrix([0])
+    assert simplify(forcing3 - kane3.forcing) == Matrix([0])
 
     kane4 = models.multi_mass_spring_damper(1, False, True)
-    symexpr4 = Matrix([[v0], [(-c0*v0 - k0*x0 + f0)/m0]])
-    assert simplify(symexpr4 - kane4.rhs()) == Matrix([0, 0])
+    massmatrix4 = Matrix([[m0]])
+    forcing4 = Matrix([[-c0*v0 - k0*x0 + f0]])
+    assert simplify(massmatrix4 - kane4.mass_matrix) == Matrix([0])
+    assert simplify(forcing4 - kane4.forcing) == Matrix([0])
 
 
 def test_multi_mass_spring_damper_higher_order():
@@ -35,35 +43,14 @@ def test_multi_mass_spring_damper_higher_order():
     v2, x2 = dynamicsymbols("v2 x2")
 
     kane1 = models.multi_mass_spring_damper(3)
-    symexpr = Matrix([[v0], [v1], [v2], [(-c0*v0 - k0*x0 - m2*(-c2*v2 - k2*x2 -
-                     m2*(-c0*v0 - k0*x0)/(m0 + m1 + m2) - (-m2*(m1 + m2)/(m0 +
-                     m1 + m2) + m2)*(-c1*v1 - k1*x1 - (m1 + m2)*(-c0*v0 -
-                     k0*x0)/(m0 + m1 + m2))/(m1 + m2 - (m1 + m2)**2/(m0 + m1 +
-                     m2)))/(-m2**2/(m0 + m1 + m2) + m2 - (-m2*(m1 + m2)/(m0 + m1
-                     + m2) + m2)**2/(m1 + m2 - (m1 + m2)**2/(m0 + m1 + m2))) -
-                     (m1 + m2)*(-c1*v1 - k1*x1 - (m1 + m2)*(-c0*v0 - k0*x0)/(m0
-                     + m1 + m2) - (-m2*(m1 + m2)/(m0 + m1 + m2) + m2)*(-c2*v2 -
-                     k2*x2 - m2*(-c0*v0 - k0*x0)/(m0 + m1 + m2) - (-m2*(m1 +
-                     m2)/(m0 + m1 + m2) + m2)*(-c1*v1 - k1*x1 - (m1 + m2)*
-                     (-c0*v0 - k0*x0)/(m0 + m1 + m2))/(m1 + m2 - (m1 + m2)**2/
-                     (m0 + m1 + m2)))/(-m2**2/(m0 + m1 + m2) + m2 - (-m2*(m1 +
-                     m2)/(m0 + m1 + m2) + m2)**2/(m1 + m2 - (m1 + m2)**2/(m0 +
-                     m1 + m2))))/(m1 + m2 - (m1 + m2)**2/(m0 + m1 + m2)))/(m0 +
-                     m1 + m2)], [(-c1*v1 - k1*x1 - (m1 + m2)*(-c0*v0 - k0*x0)/
-                     (m0 + m1 + m2) - (-m2*(m1 + m2)/(m0 + m1 + m2) + m2)*(-c2*
-                     v2 - k2*x2 - m2*(-c0*v0 - k0*x0)/(m0 + m1 + m2) - (-m2*(m1
-                     + m2)/(m0 + m1 + m2) + m2)*(-c1*v1 - k1*x1 - (m1 + m2)*
-                     (-c0*v0 - k0*x0)/(m0 + m1 + m2))/(m1 + m2 - (m1 + m2)**2/
-                     (m0 + m1 + m2)))/(-m2**2/(m0 + m1 + m2) + m2 - (-m2*(m1 +
-                     m2)/(m0 + m1 + m2) + m2)**2/(m1 + m2 - (m1 + m2)**2/(m0 +
-                     m1 + m2))))/(m1 + m2 - (m1 + m2)**2/(m0 + m1 + m2))],
-                     [(-c2*v2 - k2*x2 - m2*(-c0*v0 - k0*x0)/(m0 + m1 + m2) -
-                     (-m2*(m1 + m2)/(m0 + m1 + m2) + m2)*(-c1*v1 - k1*x1 - (m1 +
-                     m2)*(-c0*v0 - k0*x0)/(m0 + m1 + m2))/(m1 + m2 - (m1 +
-                     m2)**2/(m0 + m1 + m2)))/(-m2**2/(m0 + m1 + m2) + m2 - (-m2*
-                     (m1 + m2)/(m0 + m1 + m2) + m2)**2/(m1 + m2 - (m1 + m2)**2/
-                     (m0 + m1 + m2)))]])
-    assert simplify(symexpr - kane1.rhs()) == Matrix([0, 0, 0, 0, 0, 0])
+    massmatrix1 = Matrix([[m0 + m1 + m2, m1 + m2, m2],
+                          [m1 + m2, m1 + m2, m2],
+                          [m2, m2, m2]])
+    forcing1 = Matrix([[-c0*v0 - k0*x0],
+                       [-c1*v1 - k1*x1],
+                       [-c2*v2 - k2*x2]])
+    assert simplify(massmatrix1 - kane1.mass_matrix) == zeros(3)
+    assert simplify(forcing1 - kane1.forcing) == Matrix([0, 0, 0])
 
 
 def test_n_link_pendulum_on_cart_inputs():
@@ -74,41 +61,32 @@ def test_n_link_pendulum_on_cart_inputs():
     u0, u1 = dynamicsymbols("u0 u1")
 
     kane1 = models.n_link_pendulum_on_cart(1)
-    symexpr1 = Matrix([[u0], [u1], [(l0*m1*(g*l0*m1*sin(q1) + l0*m1*
-                      (-l0*m1*u1**2*sin(q1) + F)*cos(q1)/(m0 + m1))*cos(q1)/
-                      (-l0**2*m1**2*cos(q1)**2/(m0 + m1) + l0**2*m1) -
-                      l0*m1*u1**2*sin(q1) + F)/(m0 + m1)], [(g*l0*m1*sin(q1) +
-                      l0*m1*(-l0*m1*u1**2*sin(q1) + F)*cos(q1)/(m0 + m1))/
-                      (-l0**2*m1**2*cos(q1)**2/(m0 + m1) + l0**2*m1)]])
-    assert simplify(symexpr1 - kane1.rhs()) == Matrix([0, 0, 0, 0])
+    massmatrix1 = Matrix([[m0 + m1, -l0*m1*cos(q1)],
+                          [-l0*m1*cos(q1), l0**2*m1]])
+    forcing1 = Matrix([[-l0*m1*u1**2*sin(q1) + F], [g*l0*m1*sin(q1)]])
+    assert simplify(massmatrix1 - kane1.mass_matrix) == zeros(2)
+    assert simplify(forcing1 - kane1.forcing) == Matrix([0, 0])
 
     kane2 = models.n_link_pendulum_on_cart(1, False)
-    symexpr2 = Matrix([[u0], [u1], [(l0*m1*(g*l0*m1*sin(q1) - l0**2*m1**2*u1**2*
-                      sin(q1)*cos(q1)/(m0 + m1))*cos(q1)/(-l0**2*m1**2*
-                      cos(q1)**2/(m0 + m1) + l0**2*m1) - l0*m1*u1**2*sin(q1))/
-                      (m0 + m1)], [(g*l0*m1*sin(q1) - l0**2*m1**2*u1**2*sin(q1)*
-                      cos(q1)/(m0 + m1))/(-l0**2*m1**2*cos(q1)**2/(m0 + m1) +
-                      l0**2*m1)]])
-    assert simplify(symexpr2 - kane2.rhs()) == Matrix([0, 0, 0, 0])
+    massmatrix2 = Matrix([[m0 + m1, -l0*m1*cos(q1)],
+                          [-l0*m1*cos(q1), l0**2*m1]])
+    forcing2 = Matrix([[-l0*m1*u1**2*sin(q1)], [g*l0*m1*sin(q1)]])
+    assert simplify(massmatrix2 - kane2.mass_matrix) == zeros(2)
+    assert simplify(forcing2 - kane2.forcing) == Matrix([0, 0])
 
     kane3 = models.n_link_pendulum_on_cart(1, False, True)
-    symexpr3 = Matrix([[u0], [u1], [(-l0*m1*u1**2*sin(q1) + l0*m1*(g*l0*m1*
-                      sin(q1) - l0**2*m1**2*u1**2*sin(q1)*cos(q1)/(m0 + m1) +
-                      T1)*cos(q1)/(-l0**2*m1**2*cos(q1)**2/(m0 + m1) + l0**2*
-                      m1))/(m0 + m1)], [(g*l0*m1*sin(q1) - l0**2*m1**2*u1**2*
-                      sin(q1)*cos(q1)/(m0 + m1) + T1)/(-l0**2*m1**2*cos(q1)**2/
-                      (m0 + m1) + l0**2*m1)]])
-    assert simplify(symexpr3 - kane3.rhs()) == Matrix([0, 0, 0, 0])
-
+    massmatrix3 = Matrix([[m0 + m1, -l0*m1*cos(q1)],
+                          [-l0*m1*cos(q1), l0**2*m1]])
+    forcing3 = Matrix([[-l0*m1*u1**2*sin(q1)], [g*l0*m1*sin(q1) + T1]])
+    assert simplify(massmatrix3 - kane3.mass_matrix) == zeros(2)
+    assert simplify(forcing3 - kane3.forcing) == Matrix([0, 0])
 
     kane4 = models.n_link_pendulum_on_cart(1, True, False)
-    symexpr4 = Matrix([[u0], [u1], [(l0*m1*(g*l0*m1*sin(q1) + l0*m1*(-l0*m1*
-                      u1**2*sin(q1) + F)*cos(q1)/(m0 + m1))*cos(q1)/(-l0**2*
-                      m1**2*cos(q1)**2/(m0 + m1) + l0**2*m1) - l0*m1*u1**2*
-                      sin(q1) + F)/(m0 + m1)], [(g*l0*m1*sin(q1) + l0*m1*(-l0*m1
-                      *u1**2*sin(q1) + F)*cos(q1)/(m0 + m1))/(-l0**2*m1**2*
-                      cos(q1)**2/(m0 + m1) + l0**2*m1)]])
-    assert simplify(symexpr4 - kane4.rhs()) == Matrix([0, 0, 0, 0])
+    massmatrix4 = Matrix([[m0 + m1, -l0*m1*cos(q1)],
+                          [-l0*m1*cos(q1), l0**2*m1]])
+    forcing4 = Matrix([[-l0*m1*u1**2*sin(q1) + F], [g*l0*m1*sin(q1)]])
+    assert simplify(massmatrix4 - kane4.mass_matrix) == zeros(2)
+    assert simplify(forcing4 - kane4.forcing) == Matrix([0, 0])
 
 
 def test_n_link_pendulum_on_cart_higher_order():
@@ -121,80 +99,18 @@ def test_n_link_pendulum_on_cart_higher_order():
     F, T1 = dynamicsymbols("F T1")
 
     kane1 = models.n_link_pendulum_on_cart(2)
-    symexpr1 = Matrix([[u0], [u1], [u2], [(-l0*m1*u1**2*sin(q1) - l0*m2*u1**2*
-                      sin(q1) - l1*m2*u2**2*sin(q2) + l1*m2*(g*l1*m2*sin(q2) -
-                      l0*l1*m2*(-sin(q1)*cos(q2) + sin(q2)*cos(q1))*u1**2 + l1*
-                      m2*(-l0*m1*u1**2*sin(q1) - l0*m2*u1**2*sin(q1) - l1*m2*
-                      u2**2*sin(q2) + F)*cos(q2)/(m0 + m1 + m2) - (l0*l1*m2*
-                      (sin(q1)*sin(q2) + cos(q1)*cos(q2)) + l1*m2*(-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 + m2))*(g*l0*m1*
-                      sin(q1) + g*l0*m2*sin(q1) - l0*l1*m2*(sin(q1)*cos(q2) -
-                      sin(q2)*cos(q1))*u2**2 - (-l0*m1*cos(q1) - l0*m2*cos(q1))*
-                      (-l0*m1*u1**2*sin(q1) - l0*m2*u1**2*sin(q1) - l1*m2*u2**2*
-                      sin(q2) + F)/(m0 + m1 + m2))/(l0**2*m1 + l0**2*m2 - (-l0*
-                      m1*cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 + m2)))*cos(q2)/
-                      (-l1**2*m2**2*cos(q2)**2/(m0 + m1 + m2) + l1**2*m2 - (l0*
-                      l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)) + l1*m2*(-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 + m2))**2/(l0**2*
-                      m1 + l0**2*m2 - (-l0*m1*cos(q1) - l0*m2*cos(q1))**2/(m0 +
-                      m1 + m2))) - (-l0*m1*cos(q1) - l0*m2*cos(q1))*(g*l0*m1*
-                      sin(q1) + g*l0*m2*sin(q1) - l0*l1*m2*(sin(q1)*cos(q2) -
-                      sin(q2)*cos(q1))*u2**2 - (-l0*m1*cos(q1) - l0*m2*cos(q1))*
-                      (-l0*m1*u1**2*sin(q1) - l0*m2*u1**2*sin(q1) - l1*m2*u2**2*
-                      sin(q2) + F)/(m0 + m1 + m2) - (l0*l1*m2*(sin(q1)*sin(q2) +
-                      cos(q1)*cos(q2)) + l1*m2*(-l0*m1*cos(q1) - l0*m2*cos(q1))*
-                      cos(q2)/(m0 + m1 + m2))*(g*l1*m2*sin(q2) - l0*l1*m2*
-                      (-sin(q1)*cos(q2) + sin(q2)*cos(q1))*u1**2 + l1*m2*(-l0*m1
-                      *u1**2*sin(q1) - l0*m2*u1**2*sin(q1) - l1*m2*u2**2*
-                      sin(q2) + F)*cos(q2)/(m0 + m1 + m2) - (l0*l1*m2*(sin(q1)*
-                      sin(q2) + cos(q1)*cos(q2)) + l1*m2*(-l0*m1*cos(q1) - l0*
-                      m2*cos(q1))*cos(q2)/(m0 + m1 + m2))*(g*l0*m1*sin(q1) + g*
-                      l0*m2*sin(q1) - l0*l1*m2*(sin(q1)*cos(q2) - sin(q2)*
-                      cos(q1))*u2**2 - (-l0*m1*cos(q1) - l0*m2*cos(q1))*(-l0*m1*
-                      u1**2*sin(q1) - l0*m2*u1**2*sin(q1) - l1*m2*u2**2*
-                      sin(q2) + F)/(m0 + m1 + m2))/(l0**2*m1 + l0**2*m2 -
-                      (-l0*m1*cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 + m2)))/
-                      (-l1**2*m2**2*cos(q2)**2/(m0 + m1 + m2) + l1**2*m2 -
-                      (l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)) + l1*m2*
-                      (-l0*m1*cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 +
-                      m2))**2/(l0**2*m1 + l0**2*m2 - (-l0*m1*cos(q1) - l0*m2*
-                      cos(q1))**2/(m0 + m1 + m2))))/(l0**2*m1 + l0**2*m2 - (-l0*
-                      m1*cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 + m2)) + F)/(m0 +
-                      m1 + m2)], [(g*l0*m1*sin(q1) + g*l0*m2*sin(q1) - l0*l1*m2*
-                      (sin(q1)*cos(q2) - sin(q2)*cos(q1))*u2**2 - (-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))*(-l0*m1*u1**2*sin(q1) - l0*m2*
-                      u1**2*sin(q1) - l1*m2*u2**2*sin(q2) + F)/(m0 + m1 + m2) -
-                      (l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)) + l1*m2*
-                      (-l0*m1*cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 + m2))*
-                      (g*l1*m2*sin(q2) - l0*l1*m2*(-sin(q1)*cos(q2) + sin(q2)*
-                      cos(q1))*u1**2 + l1*m2*(-l0*m1*u1**2*sin(q1) - l0*m2*
-                      u1**2*sin(q1) - l1*m2*u2**2*sin(q2) + F)*cos(q2)/(m0 +
-                      m1 + m2) - (l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)) +
-                      l1*m2*(-l0*m1*cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 +
-                      m2))*(g*l0*m1*sin(q1) + g*l0*m2*sin(q1) - l0*l1*m2*
-                      (sin(q1)*cos(q2) - sin(q2)*cos(q1))*u2**2 - (-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))*(-l0*m1*u1**2*sin(q1) - l0*m2*
-                      u1**2*sin(q1) - l1*m2*u2**2*sin(q2) + F)/(m0 + m1 + m2))/
-                      (l0**2*m1 + l0**2*m2 - (-l0*m1*cos(q1) - l0*m2*
-                      cos(q1))**2/(m0 + m1 + m2)))/(-l1**2*m2**2*cos(q2)**2/
-                      (m0 + m1 + m2) + l1**2*m2 - (l0*l1*m2*(sin(q1)*sin(q2) +
-                      cos(q1)*cos(q2)) + l1*m2*(-l0*m1*cos(q1) - l0*m2*cos(q1))*
-                      cos(q2)/(m0 + m1 + m2))**2/(l0**2*m1 + l0**2*m2 - (-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 + m2))))/(l0**2*m1 +
-                      l0**2*m2 - (-l0*m1*cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 +
-                      m2))], [(g*l1*m2*sin(q2) - l0*l1*m2*(-sin(q1)*cos(q2) +
-                      sin(q2)*cos(q1))*u1**2 + l1*m2*(-l0*m1*u1**2*sin(q1) - l0*
-                      m2*u1**2*sin(q1) - l1*m2*u2**2*sin(q2) + F)*cos(q2)/(m0 +
-                      m1 + m2) - (l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)) +
-                      l1*m2*(-l0*m1*cos(q1) - l0*m2*cos(q1))*cos(q2)/(m0 + m1 +
-                      m2))*(g*l0*m1*sin(q1) + g*l0*m2*sin(q1) - l0*l1*m2*
-                      (sin(q1)*cos(q2) - sin(q2)*cos(q1))*u2**2 - (-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))*(-l0*m1*u1**2*sin(q1) - l0*m2*
-                      u1**2*sin(q1) - l1*m2*u2**2*sin(q2) + F)/(m0 + m1 + m2))/
-                      (l0**2*m1 + l0**2*m2 - (-l0*m1*cos(q1) - l0*m2*
-                      cos(q1))**2/(m0 + m1 + m2)))/(-l1**2*m2**2*cos(q2)**2/
-                      (m0 + m1 + m2) + l1**2*m2 - (l0*l1*m2*(sin(q1)*sin(q2) +
-                      cos(q1)*cos(q2)) + l1*m2*(-l0*m1*cos(q1) - l0*m2*cos(q1))*
-                      cos(q2)/(m0 + m1 + m2))**2/(l0**2*m1 + l0**2*m2 - (-l0*m1*
-                      cos(q1) - l0*m2*cos(q1))**2/(m0 + m1 + m2)))]])
-    assert simplify(symexpr1 - kane1.rhs()) == Matrix([0, 0, 0, 0, 0, 0])
+    massmatrix1 = Matrix([[m0 + m1 + m2, -l0*m1*cos(q1) - l0*m2*cos(q1),
+                           -l1*m2*cos(q2)],
+                          [-l0*m1*cos(q1) - l0*m2*cos(q1), l0**2*m1 + l0**2*m2,
+                           l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2))],
+                          [-l1*m2*cos(q2),
+                           l0*l1*m2*(sin(q1)*sin(q2) + cos(q1)*cos(q2)),
+                           l1**2*m2]])
+    forcing1 = Matrix([[-l0*m1*u1**2*sin(q1) - l0*m2*u1**2*sin(q1) -
+                        l1*m2*u2**2*sin(q2) + F],
+                       [g*l0*m1*sin(q1) + g*l0*m2*sin(q1) -
+                        l0*l1*m2*(sin(q1)*cos(q2) - sin(q2)*cos(q1))*u2**2],
+                       [g*l1*m2*sin(q2) - l0*l1*m2*(-sin(q1)*cos(q2) +
+                                                    sin(q2)*cos(q1))*u1**2]])
+    assert simplify(massmatrix1 - kane1.mass_matrix) == zeros(3)
+    assert simplify(forcing1 - kane1.forcing) == Matrix([0, 0, 0])
