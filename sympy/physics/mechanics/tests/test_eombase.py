37e969e29994774f4292f35e9241fcc77933b4fe
James Brandon Milam <jmilam343@gmail.com>
2016-06-03 14:51:43 -0400

    Added real test code for EOM
    I have changed the pseudo code to real test code. Currently the code is
    still messy and has extraneous bits (such as the formation of a
    LagrangesMethod instance).

--- sympy/physics/mechanics/tests/test_eombase.py
+++ sympy/physics/mechanics/tests/test_eombase.py
@@ -1,30 +1,85 @@
+from sympy import symbols, Matrix, Derivative, simplify, zeros
+import sympy.physics.mechanics as me
 import sympy.physics.mechanics.eombase as eombase
+from sympy.utilities.pytest import raises
 
-# Determine what inputs should be used
-EOM = eombase.EOMBase(frame=None)
 
 # Should contain access to all the property attributes contained in existing EOM
 # generators
 
-Thing = "Place holder until a correct output is chosen"
 
-# assert EOM.auxiliary_eqs == Thing
-assert EOM.bodies == Thing
-assert EOM.mass_matrix == Thing
-assert EOM.mass_matrix_full == Thing
-assert EOM.loads == Thing
-assert EOM.forcing == Thing
-assert EOM.forcing_full == Thing
-assert EOM.coordinates == Thing
-assert EOM.speeds == Thing
+# System state variables
+q = me.dynamicsymbols('q')
+qd = me.dynamicsymbols('q', 1)
 
-# The properties should not be able to be altered
-# EOM.auxiliary_eqs = Thing  # Raises error
-EOM.bodies = Thing  # Raises error
-EOM.mass_matrix = Thing  # Raises error
-EOM.mass_matrix_full = Thing  # Raises error
-EOM.loads = Thing  # Raises error
-EOM.forcing = Thing  # Raises error
-EOM.forcing_full = Thing  # Raises error
-EOM.coordinates = Thing  # Raises error
-EOM.speeds = Thing  # Raises error
+# Other system variables
+m, k, b, t = symbols('m k b t')
+
+# Set up the reference frame
+N = me.ReferenceFrame('N')
+
+# Set up the points and particles
+P = me.Point('P')
+P.set_vel(N, qd * N.x)
+
+Pa = me.Particle('Pa', P, m)
+
+# Define the potential energy and create the Lagrangian
+Pa.potential_energy = k * q**2 / 2.0
+L = me.Lagrangian(N, Pa)
+
+# Create the list of forces acting on the system
+fl = [(P, -b * qd * N.x)]
+
+# Create an instance of Lagranges Method
+l = me.LagrangesMethod(L, [q], forcelist=fl, frame=N)
+
+# Create the equations of motion using lagranges method
+l.form_lagranges_equations()
+
+# Set up a full mass matrix and forcing vector for a mass spring damper system
+mm = Matrix([[m]])
+f = Matrix([[-b*Derivative(q, t) - 1.0*k*q]])
+
+# Set up the kinematical diff eqs
+k_kqdot = Matrix([1])
+k_ku = Matrix([1])
+f_k = Matrix([0])
+
+# Determine what inputs should be used
+EOM = eombase.EOM([q], [qd], f, mm, k_kqdot, k_ku, f_k,  loads=fl,
+                  bodies=[Pa])
+
+def test_eom_properties():
+    # assert EOM.auxiliary_eqs == Change
+    assert EOM.bodies == [Pa]
+    assert simplify(EOM.mass_matrix - Matrix([[m]])) == Matrix([[0]])
+    assert simplify(EOM.mass_matrix_full - Matrix([[1, 0],
+                                                   [0, m]])) == zeros(2, 2)
+    assert EOM.loads == [(P, -b * qd * N.x)]
+    assert simplify(EOM.forcing -
+                    Matrix([[-b*Derivative(q, t) - 1.0*k*q]])) == zeros(1)
+    assert simplify(EOM.forcing_full -
+                    Matrix([[Derivative(q, t)],
+                            [-b*Derivative(q, t) - 1.0*k*q]])) == zeros(2, 1)
+    assert EOM.coordinates == [q]
+    assert EOM.speeds == [qd]
+
+    # The properties should not be able to be altered
+    Change = "Attempt to set values for the properties"
+    with raises(AttributeError):
+        EOM.bodies = Change
+    with raises(AttributeError):
+        EOM.mass_matrix = Change
+    with raises(AttributeError):
+        EOM.mass_matrix_full = Change
+    with raises(AttributeError):
+        EOM.loads = Change
+    with raises(AttributeError):
+        EOM.forcing = Change
+    with raises(AttributeError):
+        EOM.forcing_full = Change
+    with raises(AttributeError):
+        EOM.coordinates = Change
+    with raises(AttributeError):
+        EOM.speeds = Change
