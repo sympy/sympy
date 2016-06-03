ba2d0f9be976e49d510d877425c854d76d21df5f
James Brandon Milam <jmilam343@gmail.com>
2016-06-03 14:53:09 -0400

    Created a prototype of the EOM class
    Filled in the methods such that the test code now completely passes.

--- sympy/physics/mechanics/eombase.py
+++ sympy/physics/mechanics/eombase.py
@@ -1,11 +1,23 @@
 # Mock up of class to hold the equations of motion (not a parent class)
+from sympy import Matrix, zeros
 
 
 class EOM(object):
     def __init__(self, coordinates, speeds, forcing_vector, mass_matrix,
-                 loads=(), bodies=()):
+                 k_kqdot, k_ku, f_k, loads=(), bodies=()):
+
+        # Dynamical differential equation
         self._k_d = mass_matrix
         self._f_d = forcing_vector
+
+        # Kinematical differential equation
+        # Using the equation form defined in Kane's method documentation
+        # k_kqdot * qdot + k_ku * u + f_k = 0
+        self._k_kqdot = k_kqdot
+        self._k_ku = k_ku
+        self._f_k = f_k
+
+        # Coordinates and speeds
         self._coordinates = coordinates
         self._speeds = speeds
 
@@ -27,7 +39,8 @@ class EOM(object):
 
     @property
     def forcing_full(self):
-        pass
+        f1 = self._k_ku * Matrix(self.speeds) + self._f_k
+        return Matrix([f1, self.forcing])
 
     @property
     def mass_matrix(self):
@@ -35,7 +48,10 @@ class EOM(object):
 
     @property
     def mass_matrix_full(self):
-        pass
+        o = len(self.speeds)
+        n = len(self.coordinates)
+        return ((self._k_kqdot).row_join(zeros(n, o))).col_join((zeros(o,
+                n)).row_join(self.mass_matrix))
 
     @property
     def coordinates(self):
