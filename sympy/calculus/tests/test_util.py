7c5adad2cd901b97ffb1e93d76d1d161d88c585a
kshitij10496 <KshitijSaraogi@gmail.com>
2016-06-10 20:15:01 +0530

    added tests for method continuous_in

--- sympy/calculus/tests/test_util.py
+++ sympy/calculus/tests/test_util.py
@@ -23,6 +23,19 @@ def test_function_range():
     assert function_range(sqrt(3*x - 1), x, Interval(0, 2)) == Interval(0, sqrt(5))
 
 
+def test_continuous_in():
+    x = Symbol('x')
+    assert continuous_in(sin(x), x, Interval(0, 2*pi)) == Interval(0, 2*pi)
+    assert continuous_in(tan(x), x, Interval(0, 2*pi)) == \
+        Union(Interval(0, pi/2, False, True), Interval(pi/2, 3*pi/2, True, True),
+              Interval(3*pi/2, 2*pi, True, False))
+    assert continuous_in((x - 1)/((x - 1)**2), x, S.Reals) == \
+        Union(Interval(-oo, 1, True, True), Interval(1, oo, True, True))
+    assert continuous_in(log(x) + log(4*x - 1), x, S.Reals) == \
+        Interval(1/4, oo, True, True)
+    assert continuous_in(1/sqrt(x - 3), x, S.Reals) == Interval(3, oo, True, True)
+
+
 def test_not_empty_in():
     assert not_empty_in(FiniteSet(x, 2*x).intersect(Interval(1, 2, True, False)), x) == \
         Interval(S(1)/2, 2, True, False)
