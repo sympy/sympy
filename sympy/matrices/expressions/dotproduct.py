d3013d01203ac165485187b0a0d9218f21509c4f
Aaron Meurer <asmeurer@gmail.com>
2016-06-13 20:28:26 -0400

    Remove requirement for shape to match in DotProduct
    Only the length of the vector should match.
    
    Also add tests for all the exceptions raised in __new__.

--- sympy/matrices/expressions/dotproduct.py
+++ sympy/matrices/expressions/dotproduct.py
@@ -23,16 +23,21 @@ class DotProduct(MatrixExpr):
         if not (1 in arg2.shape):
             raise TypeError("Argument 2 of DotProduct is not a vector")
 
-        if arg1.shape != arg2.shape:
-            raise TypeError("Input to Dot Product, %s and %s, are not of same dimensions" % (str(arg1), str(arg2)))
+        if set(arg1.shape) != set(arg2.shape):
+            raise TypeError("DotProduct arguments are not the same length" % (str(arg1), str(arg2)))
 
         return Basic.__new__(cls, arg1, arg2)
 
     def doit(self, expand=False):
-        try:
+        if self.args[0].shape == self.args[1].shape:
             if self.args[0].shape[0] == 1:
-                return (self.args[0]*transpose(self.args[1])).doit()[0]
+                mul = self.args[0]*transpose(self.args[1])
             else:
-                return (transpose(self.args[0])*self.args[1]).doit()[0]
-        except (AttributeError, NotImplementedError):
-            return self
+                mul = transpose(self.args[0])*self.args[1]
+        else:
+            if self.args[0].shape[0] == 1:
+                mul = self.args[0]*self.args[1]
+            else:
+                mul = transpose(self.args[0])*transpose(self.args[1])
+
+        return mul[0]
