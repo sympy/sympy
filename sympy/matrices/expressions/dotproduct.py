15a9d42db743df06823e01873aa3b35bc407486c
Aaron Meurer <asmeurer@gmail.com>
2016-06-13 20:28:58 -0400

    Expand the docstring of DotProduct, and add doctests

--- sympy/matrices/expressions/dotproduct.py
+++ sympy/matrices/expressions/dotproduct.py
@@ -8,7 +8,22 @@ from sympy.matrices.expressions.matexpr import MatrixExpr
 
 class DotProduct(MatrixExpr):
     """
-        Dot Product of vector matrices
+    Dot product of vector matrices
+
+    The input should be two 1 x n or n x 1 matrices. The output represents the
+    scalar dotproduct.
+
+    This is similar to using MatrixElement and MatMul, except DotProduct does
+    not require that one vector to be a row vector and the other vector to be
+    a column vector.
+
+    >>> from sympy import MatrixSymbol, DotProduct
+    >>> A = MatrixSymbol('A', 1, 3)
+    >>> B = MatrixSymbol('B', 1, 3)
+    >>> DotProduct(A, B)
+    DotProduct(A, B)
+    >>> DotProduct(A, B).doit()
+    A[0, 0]*B[0, 0] + A[0, 1]*B[0, 1] + A[0, 2]*B[0, 2]
     """
 
     def __new__(cls, arg1, arg2):
