e175eb6102224d58dd24ab4a6dbf0ceb0cdae5da
Sampad Kumar Saha <sampadsaha5@gmail.com>
2016-06-15 11:33:42 +0530

    Adding the singularityintegrate.

---
+++ sympy/integrals/singularityfunctions.py
@@ -0,0 +1,30 @@
+from __future__ import print_function, division
+
+from sympy.functions import SingularityFunction, DiracDelta
+from sympy.core import sympify
+
+
+def singularityintegrate(f, x):
+    if not f.has(SingularityFunction):
+        return None
+
+    if f.func == SingularityFunction:
+        # I dont know how to handle the case for x*SingularityFunction(x, a, n)
+        # or ever more complex one where f.is_Mul or f.is_Pow is True. This way
+        # we can handle only the most simple form of the Singularity Functions.
+        x = sympify(f.args[0])
+        a = sympify(f.args[1])
+        n = sympify(f.args[2])
+        if n.is_positive or n.is_zero:
+            return SingularityFunction(x, a, n + 1)/(n + 1)
+        elif n == -1:
+            # For this case I am using the inbuilt DiracDelta integration of Sympy.
+            from sympy.integrals import integrate
+
+            expr = f.rewrite(DiracDelta)
+            expr = integrate(expr, x)
+            return expr
+
+        else:
+            return SingularityFunction(x, a, n + 1)
+    return None
