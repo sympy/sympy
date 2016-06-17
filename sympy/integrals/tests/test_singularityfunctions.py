9691dc69dcbd6320c715d111be11c41d5dfe0286
Sampad Kumar Saha <sampadsaha5@gmail.com>
2016-06-17 12:38:35 +0530

    Modified the singularityintegrate.

--- sympy/integrals/tests/test_singularityfunctions.py
+++ sympy/integrals/tests/test_singularityfunctions.py
@@ -5,5 +5,8 @@ x, a, n = symbols('x a n')
 
 
 def test_singularityintegrate():
-    assert singularityintegrate(SingularityFunction(x, a, 3), x) == SingularityFunction(x, a, 4)/4
-    assert singularityintegrate(SingularityFunction(x, a, -1), x) == Heaviside(x - a)
+    assert singularityintegrate(x, x) is None
+
+    assert 4*singularityintegrate(SingularityFunction(x, a, 3), x) == 4*SingularityFunction(x, a, 4)/4
+    assert 5*singularityintegrate(SingularityFunction(x, 5, -2), x) == 5*SingularityFunction(x, 5, -1)
+    assert 6*singularityintegrate(SingularityFunction(x, 5, -1), x) == 6*SingularityFunction(x, 5, 0)
