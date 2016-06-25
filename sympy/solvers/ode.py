85bf6903caac35c471a1c3ee8c593fe7d3485e88
arihant parsoya <parsoyaarihant@gmail.com>
2016-06-25 17:38:22 +0530

    solve fix for 1st_exact_Integral

--- sympy/solvers/ode.py
+++ sympy/solvers/ode.py
@@ -2810,7 +2810,6 @@ def _handle_Integral(expr, func, order, hint):
         sol = (expr.doit()).subs(y, f(x))
         del y
     elif hint == "1st_exact_Integral":
-        sol = expr.subs(y, f(x))
         del y
     elif hint == "nth_linear_constant_coeff_homogeneous":
         sol = expr
