aaa4491423d81a5deb5f79da234d8d30693773b3
Gaurav Dhingra <axyd0000@gmail.com>
2016-06-26 18:50:35 +0530

    Add "is_cyclic_conjugate" method in free group

--- sympy/combinatorics/free_group.py
+++ sympy/combinatorics/free_group.py
@@ -841,6 +841,39 @@ class FreeGroupElement(CantSympify, DefaultPrinting, tuple):
         """
         return set([self.cyclic_subword(i, i+len(self)) for i in range(len(self))])
 
+    def is_cyclic_conjugate(self, w):
+        """
+        Checks whether words ``self``, ``w`` are cyclic conjugates.
+
+        Examples
+        ========
+
+        >>> from sympy.combinatorics.free_group import free_group
+        >>> F, x, y = free_group("x, y")
+        >>> w1 = x**2*y**5
+        >>> w2 = x*y**5*x
+        >>> w1.is_cyclic_conjugate(w2)
+        True
+        >>> w3 = x**-1*y**5*x**-1
+        >>> w3.is_cyclic_conjugate(w2)
+        False
+
+        """
+        l1 = len(self)
+        l2 = len(w)
+        if l1 != l2:
+            return False
+        w1 = self.identity_cyclic_reduction()
+        w2 = w.identity_cyclic_reduction()
+        letter1 = w1.letter_form
+        letter2 = w2.letter_form
+        str1 = ' '.join(map(str, letter1))
+        str2 = ' '.join(map(str, letter2))
+        if len(str1) != len(str2):
+            return False
+
+        return str1 in str2 + ' ' + str2
+
     def number_syllables(self):
         """Returns the number of syllables of the associative word `self`.
 
