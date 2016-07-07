a407fd3c6b389c1a033a6e1cf38c65e2294db9b6
Juha Remes <jremes@outlook.com>
2016-07-07 14:39:33 +0100

    Specify the descriptions

--- sympy/codegen/ast.py
+++ sympy/codegen/ast.py
@@ -377,6 +377,7 @@ class For(Basic):
     def target(self):
         """
         Return the symbol (target) from the for-loop representation.
+        This object changes each iteration.
         Target must be a symbol.
         """
         return self._args[0]
@@ -385,6 +386,7 @@ class For(Basic):
     def iterable(self):
         """
         Return the iterable from the for-loop representation.
+        This is the object that target takes values from.
         Must be an iterable object.
         """
         return self._args[1]
@@ -393,6 +395,7 @@ class For(Basic):
     def body(self):
         """
         Return the sympy expression (body) from the for-loop representation.
+        This is run for each value of target.
         Must be an iterable object or CodeBlock.
         """
         return self._args[2]
