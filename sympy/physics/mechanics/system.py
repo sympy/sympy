f6ee05ea18551ebd6a0a5f7b6775bc35a3625929
James Brandon Milam <jmilam343@gmail.com>
2016-07-26 14:58:51 -0400

    Finished coding system.py
    A first draft of the code for SymbolicSystem in
    sympy/physics/mechanics/system.py is completed.

--- sympy/physics/mechanics/system.py
+++ sympy/physics/mechanics/system.py
@@ -1,3 +1,10 @@
+from sympy import eye, Matrix, zeros
+from sympy.physics.mechanics import dynamicsymbols
+from sympy.physics.mechanics.functions import find_dynamicsymbols
+
+__all__ = ['SymbolicSystem']
+
+
 class SymbolicSystem(object):
     """SymbolicSystem is a class that contains all the information about a
     system in a symbolic format such as the equations of motions and the bodies
@@ -51,7 +58,10 @@ class SymbolicSystem(object):
     alg_con : List
         This list contains the indices of the algebraic constraints in the
         combined equations of motion. The presence of these constraints requires
-        that a DAE solver be used instead of an ODE solver
+        that a DAE solver be used instead of an ODE solver. If the system is
+        given in form [3] the alg_con variable will be adjusted such that it is
+        a representation of the combined kinematics and dynamics thus make sure
+        it always matches the mass matrix entered.
 
     dyn_implicit_mat : Matrix
         This is the M matrix in form [3] of the equations of motion (the mass
@@ -169,80 +179,233 @@ class SymbolicSystem(object):
 
     """
 
-    def __init__(self):
+    def __init__(self, coord_states, right_hand_side, speeds=None,
+                 mass_matrix=None, coordinate_derivatives=None, alg_con=None,
+                 output_eqns={}, coord_idxs=None, speed_idxs=None, bodies=None,
+                 loads=None):
         """init method"""
+
+        # Extract information on speeds, coordinates and states
+        if speeds is None:
+            self._states = Matrix(coord_states)
+
+            if coord_idxs is None:
+                self._coordinates = None
+            else:
+                coords = [coord_states[i] for i in coord_idxs]
+                self._coordinates = Matrix(coords)
+
+            if speed_idxs is None:
+                self._speeds = None
+            else:
+                speeds_inter = [coord_states[i] for i in speed_idxs]
+                self._speeds = Matrix(speeds_inter)
+        else:
+            self._coordinates = Matrix(coord_states)
+            self._speeds = Matrix(speeds)
+            self._states = self._coordinates.col_join(self._speeds)
+
+        # Extract equations of motion form
+        if coordinate_derivatives is not None:
+            self._kin_explicit_rhs = coordinate_derivatives
+            self._dyn_implicit_rhs = right_hand_side
+            self._dyn_implicit_mat = mass_matrix
+            self._comb_implicit_rhs = None
+            self._comb_implicit_mat = None
+            self._comb_explicit_rhs = None
+        elif mass_matrix is not None:
+            self._kin_explicit_rhs = None
+            self._dyn_implicit_rhs = None
+            self._dyn_implicit_mat = None
+            self._comb_implicit_rhs = right_hand_side
+            self._comb_implicit_mat = mass_matrix
+            self._comb_explicit_rhs = None
+        else:
+            self._kin_explicit_rhs = None
+            self._dyn_implicit_rhs = None
+            self._dyn_implicit_mat = None
+            self._comb_implicit_rhs = None
+            self._comb_implicit_mat = None
+            self._comb_explicit_rhs = right_hand_side
+
+        # Set the remainder of the inputs as instance attributes
+        if alg_con is not None and coordinate_derivatives is not None:
+            alg_con = [i + len(coordinate_derivatives) for i in alg_con]
+        self._alg_con = alg_con
+        self._output_eqns = output_eqns
+        self._bodies = bodies
+        self._loads = loads
 
     @property
     def coordinates(self):
         """Returns the column matrix of the generalized coordinates"""
+        if self._coordinates is None:
+            raise AttributeError("The coordinates were not specified")
+        else:
+            return self._coordinates
 
     @property
     def speeds(self):
         """Returns the column matrix of generalized speeds"""
+        if self._speeds is None:
+            raise AttributeError("The speeds were not specified")
+        else:
+            return self._speeds
 
     @property
     def states(self):
         """Returns the column matrix of the state variables"""
+        return self._states
 
     @property
     def alg_con(self):
         """Returns a list with the indices of the rows containing algebraic
         constraints in the combined form of the equations of motion"""
+        return self._alg_con
 
     @property
     def dyn_implicit_mat(self):
         """Returns the matrix, M, corresponding to the dynamic equations in
         implicit form, M x' = F, where the kinematical equations are not
         included"""
+        if self._dyn_implicit_mat is None:
+            raise AttributeError("dyn_implicit_mat is not specified for "
+                                 "equations of motion form [1] or [2]")
+        else:
+            return self._dyn_implicit_mat
 
     @property
     def dyn_implicit_rhs(self):
         """Returns the column matrix, F, corresponding to the dynamic equations
         in implicit form, M x' = F, where the kinematical equations are not
         included"""
+        if self._dyn_implicit_rhs is None:
+            raise AttributeError("dyn_implicit_rhs is not specified for "
+                                 "equations of motion form [1] or [2]")
+        else:
+            return self._dyn_implicit_rhs
 
     @property
     def comb_implicit_mat(self):
         """Returns the matrix, M, corresponding to the equations of motion in
         implicit form (form [2]), M x' = F, where the kinematical equations are
         included"""
+        if self._comb_implicit_mat is None:
+            if self._dyn_implicit_mat is not None:
+                num_kin_eqns = len(self._kin_explicit_rhs)
+                num_dyn_eqns = len(self._dyn_implicit_rhs)
+                zeros1 = zeros(num_kin_eqns, num_dyn_eqns)
+                zeros2 = zeros(num_dyn_eqns, num_kin_eqns)
+                inter1 = eye(num_kin_eqns).row_join(zeros1)
+                inter2 = zeros2.row_join(self._dyn_implicit_mat)
+                self._comb_implicit_mat = inter1.col_join(inter2)
+                return self._comb_implicit_mat
+            else:
+                raise AttributeError("comb_implicit_mat is not specified for "
+                                     "equations of motion form [1]")
+        else:
+            return self._comb_implicit_mat
 
     @property
     def comb_implicit_rhs(self):
         """Returns the column matrix, F, corresponding to the equations of
         motion in implicit form (form [2]), M x' = F, where the kinematical
         equations are included"""
+        if self._comb_implicit_rhs is None:
+            if self._dyn_implicit_rhs is not None:
+                kin_inter = self._kin_explicit_rhs
+                dyn_inter = self._dyn_implicit_rhs
+                self._comb_implicit_rhs = kin_inter.col_join(dyn_inter)
+                return self._comb_implicit_rhs
+            else:
+                raise AttributeError("comb_implicit_mat is not specified for "
+                                     "equations of motion in form [1]")
+        else:
+            return self._comb_implicit_rhs
+
+    def compute_explicit_form(self):
+        """If the explicit right hand side of the combined equations of motion
+        is to provided upon initialization, this method will calculate it. This
+        calculation can potentially take awhile to compute."""
+        inter = self._comb_implicit_mat.LUsolve(self._comb_implicit_rhs)
+        self._comb_explicit_rhs = inter
 
     @property
     def comb_explicit_rhs(self):
         """Returns the right hand side of the equations of motion in explicit
         form, x' = F, where the kinematical equations are included"""
+        if self._comb_explicit_rhs is None:
+            raise AttributeError("Please run .combute_explicit_form before "
+                                 "attempting to access comb_explicit_rhs")
+        else:
+            return self._comb_explicit_rhs
 
     @property
     def kin_explicit_rhs(self):
         """Returns the right hand side of the kinematical equations in explicit
         form, q' = G"""
+        if self._kin_explicit_rhs is None:
+            raise AttributeError("kin_explicit_rhs is not specified for "
+                                 "equations of motion form [1] or [2]")
+        else:
+            return self._kin_explicit_rhs
 
     @property
     def dynamic_symbols(self):
         """Returns a column matrix containing all of the symbols in the system
         that depend on time"""
+        # Create a list of all of the expressions in the equations of motion
+        if self._comb_explicit_rhs is None:
+            eom_expressions = (self.comb_implicit_mat[:] +
+                               self.comb_implicit_rhs[:])
+        else:
+            eom_expressions = (self._comb_explicit_rhs[:])
+
+        functions_of_time = set()
+        for expr in eom_expressions:
+            functions_of_time = functions_of_time.union(
+                find_dynamicsymbols(expr))
+        functions_of_time = functions_of_time.union(self._states)
+
+        return functions_of_time
 
     @property
     def constant_symbols(self):
         """Returns a column matrix containing all of the symbols in the system
         that do not depend on time"""
+        # Create a list of all of the expressions in the equations of motion
+        if self._comb_explicit_rhs is None:
+            eom_expressions = (self.comb_implicit_mat[:] +
+                               self.comb_implicit_rhs[:])
+        else:
+            eom_expressions = (self._comb_explicit_rhs[:])
+
+        constants = set()
+        for expr in eom_expressions:
+            constants = constants.union(expr.free_symbols)
+        constants.remove(dynamicsymbols._t)
+
+        return constants
 
     @property
     def output_eqns(self):
         """Returns a dictionary of the given output equations where the key's
         are user defined names for each equation and the values are the symbolic
         equations themselves"""
+        return self._output_eqns
 
     @property
     def bodies(self):
         """Returns the bodies in the system"""
+        if self._bodies is None:
+            raise AttributeError("bodies were not specified for the system")
+        else:
+            return self._bodies
 
     @property
     def loads(self):
         """Returns the loads in the system"""
+        if self._loads is None:
+            raise AttributeError("bodies were not specified for the system")
+        else:
+            return self._loads
