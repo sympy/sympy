18173ffaa56ec11d1cedf43d70ac8e90ba7a56a1
Peter Stangl <peter.stangl@ph.tum.de>
2016-08-18 17:47:20 +0200

    [tensor] add test for fixed issue #11525 not depending on numpy

--- sympy/tensor/tests/test_tensor.py
+++ sympy/tensor/tests/test_tensor.py
@@ -639,6 +639,25 @@ def test_add2():
     t = A(m, -m, n) + A(n, p, -p)
     assert t == 0
 
+def test_add3():
+    Lorentz = TensorIndexType('Lorentz', dummy_fmt='L')
+    i0, i1 = tensor_indices('i0:2', Lorentz)
+    E, px, py, pz = symbols('E px py pz')
+    A = tensorhead('A', [Lorentz], [[1]])
+    B = tensorhead('B', [Lorentz], [[1]])
+
+    expr1 = A(i0)*A(-i0) - (E**2 - px**2 - py**2 - pz**2)
+    assert expr1.args == (-E**2 + px**2 + py**2 + pz**2, A(i0)*A(-i0))
+
+    expr2 = E**2 - px**2 - py**2 - pz**2 - A(i0)*A(-i0)
+    assert expr2.args == (E**2 - px**2 - py**2 - pz**2, -A(i0)*A(-i0))
+
+    expr3 = A(i0)*A(-i0) - E**2 + px**2 + py**2 + pz**2
+    assert expr3.args == (-E**2 + px**2 + py**2 + pz**2, A(i0)*A(-i0))
+
+    expr4 = B(i1)*B(-i1) + 2*E**2 - 2*px**2 - 2*py**2 - 2*pz**2 - A(i0)*A(-i0)
+    assert expr4.args == (2*E**2 - 2*px**2 - 2*py**2 - 2*pz**2, -A(i0)*A(-i0), B(i1)*B(-i1))
+
 def test_mul():
     from sympy.abc import x
     Lorentz = TensorIndexType('Lorentz', dummy_fmt='L')
