2f9f6d0cd42c18d4b0fb5936102473174dd43e1e
Kalevi Suominen <jksuom@gmail.com>
2016-09-14 09:45:01 +0300

    Create poly_linear_constraints
    Simplified version of linear_constraints for polynomials.

--- sympy/integrals/prde.py
+++ sympy/integrals/prde.py
@@ -193,6 +193,24 @@ def prde_linear_constraints(a, b, G, DE):
     qs, _ = list(zip(*Q))
     return (qs, M)
 
+def poly_linear_constraints(p, d):
+    """
+    Given p = [p1, ..., pm] in k[t]^m and d in k[t], return
+    q = [q1, ..., qm] in k[t]^m and a matrix M with entries in k such
+    that Sum(ci*pi, (i, 1, m)), for c1, ..., cm in k, is divisible
+    by d if and only if (c1, ..., cm) is a solution of Mx = 0, in
+    which case the quotient is Sum(ci*qi, (i, 1, m)).
+    """
+    m = len(p)
+    q, r = zip(*[pi.div(d) for pi in p])
+
+    if not all([ri.is_zero for ri in r]):
+        n = max([ri.degree() for ri in r])
+        M = Matrix(n + 1, m, lambda i, j: r[j].nth(i))
+    else:
+        M = Matrix()  # No constraints.
+
+    return q, M
 
 def constant_system(A, u, DE):
     """
