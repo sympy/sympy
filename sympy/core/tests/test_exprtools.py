b3d2100c661e8bfa095512cc51d5fb4350d63b87
arghdos <arghdos@gmail.com>
2016-09-14 20:44:42 -0400

    add sum tests to factor tests

--- sympy/core/tests/test_exprtools.py
+++ sympy/core/tests/test_exprtools.py
@@ -288,6 +288,10 @@ def test_factor_terms():
     assert factor_terms(e, sign=False) == e
     assert factor_terms(exp(-4*x - 2) - x) == -x + exp(Mul(-2, 2*x + 1, evaluate=False))
 
+    # sum tests
+    assert factor_terms(Sum(x, (y, 1, 10))) == x * Sum(1, (y, 1, 10))
+    assert factor_terms(Sum(x, (y, 1, 10)) + x) == x * (1 + Sum(1, (y, 1, 10)))
+
 
 def test_xreplace():
     e = Mul(2, 1 + x, evaluate=False)
