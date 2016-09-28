ec8f691392f33378f04a622d97632e1e920a08cd
Micky <mickydroch@gmail.com>
2016-09-29 01:31:51 +0530

    changes

--- sympy/combinatorics/perm_groups.py
+++ sympy/combinatorics/perm_groups.py
@@ -2667,7 +2667,7 @@ class PermutationGroup(Basic):
         >>> S = SymmetricGroup(5)
         >>> base, strong_gens = S.schreier_sims_random(consec_succ=5)
         >>> _verify_bsgs(S, base, strong_gens)
-        False
+        True
 
         Notes
         =====
