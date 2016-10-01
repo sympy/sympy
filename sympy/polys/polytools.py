cb78a3ded9fb6fafbaa677eb666396cbfab04b4d
shruti <shrutishrm512@gmail.com>
2016-10-01 11:38:26 +0530

    Polys: Improved the docstrings of LM(), LT() and revert()(#11427)
    Added more description about the method in LM(), LT() and gave the examples for revert(f, n) to clear about its functionality in docstrings. These changes have been made in the polytools.py file in polys.

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -1435,19 +1435,19 @@ class Poly(Expr):
         Polynomial pseudo-remainder of ``f`` by ``g``.
 
         Caveat: The function prem(f, g, x) can be safely used to compute
-        in Z[x] _only_ subresultant polynomial remainder sequences (prs's).
+          in Z[x] _only_ subresultant polynomial remainder sequences (prs's).
 
-        To safely compute Euclidean and Sturmian prs's in Z[x]
-        employ anyone of the corresponding functions found in
-        the module sympy.polys.subresultants_qq_zz. The functions
-        in the module with suffix _pg compute prs's in Z[x] employing
-        rem(f, g, x), whereas the functions with suffix _amv
-        compute prs's in Z[x] employing rem_z(f, g, x).
+          To safely compute Euclidean and Sturmian prs's in Z[x]
+          employ anyone of the corresponding functions found in
+          the module sympy.polys.subresultants_qq_zz. The functions
+          in the module with suffix _pg compute prs's in Z[x] employing
+          rem(f, g, x), whereas the functions with suffix _amv
+          compute prs's in Z[x] employing rem_z(f, g, x).
 
-        The function rem_z(f, g, x) differs from prem(f, g, x) in that
-        to compute the remainder polynomials in Z[x] it premultiplies
-        the divident times the absolute value of the leading coefficient
-        of the divisor raised to the power degree(f, x) - degree(g, x) + 1.
+          The function rem_z(f, g, x) differs from prem(f, g, x) in that
+          to compute the remainder polynomials in Z[x] it premultiplies
+          the divident times the absolute value of the leading coefficient
+          of the divisor raised to the power degree(f, x) - degree(g, x) + 1.
 
 
         Examples
