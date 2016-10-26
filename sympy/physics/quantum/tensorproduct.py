7ab946e73980c91cd1aacb381d5e5a3f5e43995f
Johan Blåbäck <johan.blaback@cea.fr>
2016-10-26 17:11:06 +0200

    `Trace` of `TensorProduct`
    Added properties `is_Matrix` and `is_square` to `TensorProduct` that are
    true when all arguments for `TensorProduct` has `is_Matrix` and `is_square`,
    respectively. This gives the desired output for the trace over the
    TensorProduct`:
    
    ```
    In [1]: import sys
    
    In [2]: sys.path.insert(0, '/local/repo/')
    
    In [3]: from sympy import *
    
    In [4]: from sympy.physics.quantum import TensorProduct as tp
    
    In [5]: n = Symbol('n')
    
    In [6]: X = MatrixSymbol('\\mathbb{X}', n, n)
    
    In [7]: Y = MatrixSymbol('\\mathbb{Y}', n, n)
    
    In [8]: trace(tp(X,Y))
    Out[8]: Tr(\mathbb{X})*Tr(\mathbb{Y})
    ```
    
    Furthermore, the following now works on `TensorProduct`:
    
    ```
    In [9]: tp(X,Y).is_Matrix
    Out[9]: True
    
    In [10]: tp(X,Y).is_square
    Out[10]: True
    ```

--- sympy/physics/quantum/tensorproduct.py
+++ sympy/physics/quantum/tensorproduct.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function, division
 
-from sympy import Expr, Add, Mul, Matrix, Pow, sympify
+from sympy import Expr, Add, Mul, Matrix, Pow, sympify, MatrixSymbol
 from sympy.core.compatibility import u, range
 from sympy.core.trace import Tr
 from sympy.printing.pretty.stringpict import prettyForm
@@ -115,6 +115,17 @@ class TensorProduct(Expr):
         AxC + BxC
     """
     is_commutative = False
+
+    @property
+    def is_Matrix(self):
+        return all(arg.is_Matrix for arg in self.args)
+
+    @property
+    def is_square(self):
+        if self.is_Matrix:
+            return all(arg.is_square for arg in self.args)
+        else:
+            return False
 
     def __new__(cls, *args):
         if isinstance(args[0], (Matrix, numpy_ndarray, scipy_sparse_matrix)):
