f9d7b6c60990f01f4938cd770e93e8415ea9ea96
maurogaravello <mauro.garavello@unimib.it>
2016-12-04 17:53:23 +0100

    added the test for integral

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -33,6 +33,7 @@ def test_improper_integral():
     assert integrate(log(x), (x, 0, 1)) == -1
     assert integrate(x**(-2), (x, 1, oo)) == 1
     assert integrate(1/(1 + exp(x)), (x, 0, oo)) == log(2)
+    assert integrate(log(Rational(1,2) - x), (x, 0, Rational(1,2))) == Rational(1,2) * log(Rational(1,2)) - Rational(1,2)
 
 
 def test_constructor():
