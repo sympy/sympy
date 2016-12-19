d31403a393bc6e4b97bbed28488ced106a67f960
Dhruv Bhanushali <dhruv_b@live.com>
2016-12-20 01:15:43 +0530

    Add a second condition to the if statement to ensure that x is also an integer because the if condition only checked if the value of y was an integer

--- sympy/solvers/diophantine.py
+++ sympy/solvers/diophantine.py
@@ -992,9 +992,8 @@ def _diop_quadratic(var, coeff, t):
                     + (sqa*g*u**2 + D*u + sqa*F) // _c
 
                 for z0 in range(0, abs(_c)):
-                    if divisible(
-                            sqa*g*z0**2 + D*z0 + sqa*F,
-                            _c):
+                    if (divisible(sqa*g*z0**2 + D*z0 + sqa*F, _c) and
+                            divisible(((sqa*g*z0**2 + D*z0 + sqa*F)*(e*sqc) - (z0*_c)), a*E - sqa*e*sqc*D)):
                         sol.add((solve_x(z0), solve_y(z0)))
 
     # (3) Method used when B**2 - 4*A*C is a square, is described in p. 6 of the below paper
