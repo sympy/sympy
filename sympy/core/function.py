3161e371b212dcfa17a616ddda3a7a090c99e4be
ArifAhmed1995 <arif.ahmed.5.10.1995@gmail.com>
2016-12-22 23:19:35 +0530

    Use chop=True to discard insignificant terms

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -2615,8 +2615,9 @@ def nfloat(expr, n=15, exponent=False):
     if rv.is_Number:
         return Float(rv, n)
     elif rv.is_number:
+        from sympy.core.evalf import N
         # evalf doesn't always set the precision
-        rv = rv.n(n)
+        rv = N(rv, n, chop=True)
         if rv.is_Number:
             rv = Float(rv.n(n), n)
         else:
