f09665b5f14a365d1ea7a269331578d4dd1e83c2
ArifAhmed1995 <arif.ahmed.5.10.1995@gmail.com>
2016-12-27 16:12:48 +0530

    Rectify return cases of get_abs(), add test

--- sympy/core/evalf.py
+++ sympy/core/evalf.py
@@ -210,14 +210,21 @@ def complex_accuracy(result):
 def get_abs(expr, prec, options):
     from sympy.functions.elementary.complexes import Abs
 
-    real, imag, re_acc, im_acc = evalf(expr, prec + 2, options)
-    if real or imag:
+    re, im, re_acc, im_acc = evalf(expr, prec + 2, options)
+
+    if not re:
+        re, re_acc, im, im_acc = im, im_acc, re, re_acc
+    if im:
         if expr.is_number:
             abs_expr, _, acc, _ = evalf(Abs(N(expr, prec + 2)),
                                         prec + 2, options)
             return abs_expr, None, acc, None
         else:
+            if 'subs' in options:
+                return libmp.mpc_abs((re, im), prec), None, re_acc, None
             return Abs(expr), None, prec, None
+    elif re:
+        return mpf_abs(re), None, re_acc, None
     else:
         return None, None, None, None
 
