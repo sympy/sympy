13c74d2a42759027530d47cfe9a2d471959c48a2
Jason K. Moore <moorepants@gmail.com>
2016-12-28 12:25:16 -0800

    Added a regression test for issue 11434.

--- sympy/matrices/tests/test_matrices.py
+++ sympy/matrices/tests/test_matrices.py
@@ -2446,6 +2446,16 @@ def test_rank():
     p = zeros(3)
     assert p.rank() == 0
 
+def test_issue_11434():
+    ax, ay, bx, by, cx, cy, dx, dy, ex, ey, t0, t1 = \
+        symbols('a_x a_y b_x b_y c_x c_y d_x d_y e_x e_y t_0 t_1')
+    M = Matrix([[ax, ay, ax*t0, ay*t0, 0],
+                [bx, by, bx*t0, by*t0, 0],
+                [cx, cy, cx*t0, cy*t0, 1],
+                [dx, dy, dx*t0, dy*t0, 1],
+                [ex, ey, 2*ex*t1 - ex*t0, 2*ey*t1 - ey*t0, 0]])
+    assert M.rank() == 4
+
 def test_replace():
     from sympy import symbols, Function, Matrix
     F, G = symbols('F, G', cls=Function)
