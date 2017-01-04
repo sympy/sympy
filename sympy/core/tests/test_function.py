6e13114d8ccead22d33f56d25efb5edf133b47b8
Shibasis Patel <smartshibasish@gmail.com>
2017-01-04 16:01:54 +0530

    Corrected trailing whitespace error in test

--- sympy/core/tests/test_function.py
+++ sympy/core/tests/test_function.py
@@ -712,5 +712,5 @@ def test_should_evalf():
     # This should not take forever to run (see #8506)
     assert isinstance(sin((1.0 + 1.0*I)**10000 + 1), sin)
 def test_issue_7027():
-    raises(ValueError, lambda: diff(2*cos(x),cos(x)))
+    raises(ValueError, lambda: diff(2*cos(x), cos(x)))
     raises(ValueError, lambda: diff(x, re(x)))
