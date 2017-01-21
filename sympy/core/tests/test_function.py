30692b567a86acecfbada642afa463521759e964
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2017-01-21 19:13:56 +0530

    Replace derivative of expr with 0, when s is variable if s is one of vars, the derivative of expr with respect to s should not be included.

--- sympy/core/tests/test_function.py
+++ sympy/core/tests/test_function.py
@@ -816,8 +816,10 @@ def test_issue_11159():
 def test_issue_12005():
     e1 = Subs(Derivative(f(x), x), (x,), (x,))
     assert e1.diff(x) == Derivative(f(x), x, x)
-
     e2 = Subs(Derivative(f(x), x), (x,), (x**2 + 1,))
     assert e2.diff(x) == 2*x*Subs(Derivative(f(x), x, x), (x,), (x**2 + 1,))
-
+    e3 = Subs(Derivative(f(x) + y**2 - y, y), (y,), (y**2,))
+    assert e3.diff(y) == 4*y
+    e4 = Subs(Derivative(f(x + y), y), (y,), (x**2))
+    assert e4.diff(y) == S.Zero
     assert f(g(x)).diff(g(x), g(x)) == Subs(Derivative(f(y), y, y), (y,), (g(x),))
