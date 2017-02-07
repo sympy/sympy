018f942ae05e1066fcf0cd7a20bb256e1d686675
Shikhar Makhija <shikharmakhija2@gmail.com>
2017-02-07 16:51:24 +0530

    'Limits'

--- sympy/series/limits.py
+++ sympy/series/limits.py
@@ -7,7 +7,7 @@ from sympy.functions.combinatorial.factorials import factorial
 from sympy.functions.special.gamma_functions import gamma
 from sympy.series.order import Order
 from .gruntz import gruntz
-from sympy.functions.elementary.exponential import exp
+from sympy.core.exprtools import factor_terms
 
 def limit(e, z, z0, dir="+"):
     """
@@ -146,12 +146,12 @@ class Limit(Expr):
 
         if e.is_Mul:
             if abs(z0) is S.Infinity:
-                ok = lambda w: (any(a.is_polynomial(z) or
-                                    all((m.is_polynomial(z) or
-                                        (m.func is exp and m.args[0].is_polynomial(z)))
+                e = factor_terms(e)
+                ok = lambda w: (z in w.free_symbols and
+                                any(a.is_polynomial(z) or
+                                    any(z in m.free_symbols and m.is_polynomial(z)
                                         for m in Mul.make_args(a))
-                                    or (a.func is exp and a.args[0].is_polynomial(z))
-                                for a in Add.make_args(w)))
+                                    for a in Add.make_args(w)))
                 if all(ok(w) for w in e.as_numer_denom()):
                     u = Dummy(positive=(z0 is S.Infinity))
                     inve = e.subs(z, 1/u)
