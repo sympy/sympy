39a14a3011638ac1d97ebd0d411e1f40d2efd0ce
Filip Gokstorp <filip@gokstorp.se>
2017-02-08 17:48:54 +0000

    Simple fix for BSplines of higher than 1 degree with repeated knots.

--- sympy/functions/special/bsplines.py
+++ sympy/functions/special/bsplines.py
@@ -16,16 +16,21 @@ def _add_splines(c, b1, d, b2):
         new_args = []
         n_intervals = len(b1.args)
         if n_intervals != len(b2.args):
-            raise ValueError("Args of b1 and b2 are not equal")
-        new_args.append((c*b1.args[0].expr, b1.args[0].cond))
-        for i in range(1, n_intervals - 1):
-            new_args.append((
-                c*b1.args[i].expr + d*b2.args[i - 1].expr,
-                b1.args[i].cond
-            ))
-        new_args.append((d*b2.args[-2].expr, b2.args[-2].cond))
-        new_args.append(b2.args[-1])
-        rv = Piecewise(*new_args)
+            # Args of b1 and b2 are not equal. Just combining the Piecewise without any fancy optimisation
+            p1 = piecewise_fold(c*b1)
+            p2 = piecewise_fold(d*b2)
+
+            rv = p1 + p2
+        else:
+            new_args.append((c*b1.args[0].expr, b1.args[0].cond))
+            for i in range(1, n_intervals - 1):
+                new_args.append((
+                    c*b1.args[i].expr + d*b2.args[i - 1].expr,
+                    b1.args[i].cond
+                ))
+            new_args.append((d*b2.args[-2].expr, b2.args[-2].cond))
+            new_args.append(b2.args[-1])
+            rv = Piecewise(*new_args)
 
     return rv.expand()
 
