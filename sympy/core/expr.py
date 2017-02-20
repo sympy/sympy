620fe770b0e85541743c9ca593c3da5aaec3a9c7
arihant parsoya <parsoyaarihant@gmail.com>
2017-02-20 09:38:33 +0530

    updated expr.coeff() doctstring
    Fixes #12200 .
    
    Updated Docstring of Expr.coeff() since Expr.coeff doesn’t return
    `None`.

--- sympy/core/expr.py
+++ sympy/core/expr.py
@@ -1106,11 +1106,12 @@ class Expr(Basic, EvalfMixin):
 
     def coeff(self, x, n=1, right=False):
         """
-        Returns the coefficient from the term(s) containing ``x**n`` or None. If ``n``
+        Returns the coefficient from the term(s) containing ``x**n``. If ``n``
         is zero then all terms independent of ``x`` will be returned.
 
-        When x is noncommutative, the coeff to the left (default) or right of x
-        can be returned. The keyword 'right' is ignored when x is commutative.
+        When ``x`` is noncommutative, the coefficient to the left (default) or
+        right of ``x`` can be returned. The keyword 'right' is ignored when
+        ``x`` is commutative.
 
         See Also
         ========
