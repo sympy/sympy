91798bc667ac89a3bed06a4fa5f33ddcce96e60a
Ekansh Purohit <purohit.e15@gmail.com>
2017-03-07 20:40:54 +0530

    Runtime Warning changed to an error

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -361,10 +361,14 @@ def test_numpy_old_matrix():
     assert isinstance(f(1, 2, 3), numpy.matrix)
 
 def test_python_div_zero_issue_11306():
+    numpy.seterr(all='raise')
     if not numpy:
         skip("numpy not installed.")
     p = Piecewise((1 / x, y < -1), (x, y <= 1), (1 / x, True))
-    lambdify([x, y], p, modules='numpy')(0, 1)
+    try:
+        f = lambdify([x, y], p, modules='numpy')(0, 1)
+    except ZeroDivisionError:
+        pass
 
 def test_issue9474():
     mods = [None, 'math']
