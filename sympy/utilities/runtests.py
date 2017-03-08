28375c47403bca390d7498503ba6bbee577fffd8
saiharsh <saiharsh.t13@iiits.in>
2017-03-09 04:48:00 +0530

    --no-subprocess flag set true in test

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -191,8 +191,9 @@ def run_in_subprocess_with_hash_randomization(function, function_args=(),
     p.communicate()
     if p.returncode != 0:
         return False
-
+    
     hash_seed = os.getenv("PYTHONHASHSEED")
+    
     if not hash_seed:
         os.environ["PYTHONHASHSEED"] = str(random.randrange(2**32))
     else:
