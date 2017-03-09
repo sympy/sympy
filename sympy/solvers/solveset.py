405f408778897a82acca50fe131ba0f1ab6de491
K TARUNESHWAR <TinkuWinku@Ks-MacBook-Pro.local>
2017-03-09 18:16:41 +0530

    can solve for more than one variable in solveset

--- sympy/solvers/solveset.py
+++ sympy/solvers/solveset.py
@@ -1,7 +1,7 @@
 """
 This module contains functions to:
 
-    - solve a single equation for a single variable, in any domain either real or complex.
+    - solve a single equation for a one or more than one variable, in any domain either real or complex.
 
     - solve a system of linear equations with N variables and M equations.
 
@@ -812,6 +812,14 @@ def solveset(f, symbol=None, domain=S.Complexes):
     >>> pprint(solveset(exp(x) - 1, x), use_unicode=False)
     {2*n*I*pi | n in Integers()}
 
+    * If you want to solve equation or inequality for more than one variable
+
+    >>> x = Symbol('x')
+    >>> y = Symbol('y')
+    >>> z = Symbol('z')
+    >>> solveset(x*y*z,[x,y,z])
+    [{x: {0}}, {y: {0}}, {z: {0}}]
+
     * If you want to use `solveset` to solve the equation in the
       real domain, provide a real domain. (Using `solveset\_real`
       does this automatically.)
@@ -840,6 +848,17 @@ def solveset(f, symbol=None, domain=S.Complexes):
     (0, oo)
 
     """
+    if type(symbol)==list:
+        solution_list=[]
+        for i in symbol:
+            ans={i: solve_set(f,i,domain)}
+            solution_list.append(ans)
+        return(solution_list)
+    else:
+        solution=solve_set(f,symbol,domain)
+        return(solution)
+
+def solve_set(f, symbol=None, domain=S.Complexes):
     f = sympify(f)
 
     if f is S.true:
