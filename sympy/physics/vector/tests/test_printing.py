7a67d3e439b5d51c1a904472f6ec777f3b760778
maurogaravello <mauro.garavello@unimib.it>
2017-03-12 18:26:19 +0100

    vlatex: added brackets in multiplication
    According to issue 12078, vlatex did not work correctly
    in the case of multiplication, where one term is a derivative
    of a sum.
    The changes are in the files
    modified:   sympy/physics/vector/printing.py
    modified:   sympy/physics/vector/tests/test_printing.py
    
    The change is in _print_Derivative in printing.py
    A test is added in test_printing.py

--- sympy/physics/vector/tests/test_printing.py
+++ sympy/physics/vector/tests/test_printing.py
@@ -184,3 +184,19 @@ def test_dyadic_latex():
                 r'\alpha \beta\mathbf{\hat{n}_z}\otimes \mathbf{\hat{n}_x}')
 
     assert x._latex() == expected
+
+def test_vlatex(): # vlatex is broken #12078
+    from sympy.physics.vector import vlatex
+
+    x = symbols('x')
+    J = symbols('J')
+
+    f = Function('f')
+    g = Function('g')
+    h = Function('h')
+
+    expected = r'J \left(\frac{d}{d x} g{\left (x \right )} - \frac{d}{d x} h{\left (x \right )}\right)'
+
+    expr = J*f(x).diff(x).subs(f(x), g(x)-h(x))
+
+    assert vlatex(expr) == expected
