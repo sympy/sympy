0152e13d53ee5a2e92e47ba532d1bd238f48838c
Satya Prakash Dwibedi <akash581050@gmail.com>
2017-03-13 19:26:01 +0530

    Update fcode.py

--- sympy/printing/fcode.py
+++ sympy/printing/fcode.py
@@ -120,12 +120,13 @@ class FCodePrinter(CodePrinter):
     #issue 12267
     def _print_sign(self,func):
         if func.args[0].is_integer:
-            return "merge(0, isign(1,{0}), {0}==0)".format(self._print(func.args[0]))
+            return "merge(0, isign(1, {0}), {0} == 0)".format(self._print(func.args[0]))
         elif func.args[0].is_complex:
-            return "merge(cmplx(0d0, 0d0), {0}/abs({0}), abs({0}) == 0d0)".format(self._print(func.args[0]))
+            return "merge( cmplx(0d0, 0d0), {0}/abs({0}), abs({0}) == 0d0)".format(self._print(func.args[0]))
         else:
             return "merge(0d0, dsign(1d0, {0}), {0} == 0d0)".format(self._print(func.args[0]))
-        
+
+
     def _get_loop_opening_ending(self, indices):
         open_lines = []
         close_lines = []
