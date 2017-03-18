3d70b030454f8bd5daf1abd0c0eb2a8adc389ced
Satya Prakash Dwibedi <akash581050@gmail.com>
2017-03-19 00:40:52 +0530

    Update test_fcode.py

--- sympy/printing/tests/test_fcode.py
+++ sympy/printing/tests/test_fcode.py
@@ -25,9 +25,12 @@ def test_fcode_sign():
     x=symbols('x')
     y=symbols('y', integer=True)
     z=symbols('z', complex=True)
-    assert fcode(sign(x), source_format='free') == "merge(0d0, dsign(1d0, x), x == 0d0)"
-    assert fcode(sign(y), source_format='free') == "merge(0, isign(1, y), y == 0)"
-    assert fcode(sign(z), source_format='free') == "merge(cmplx(0d0, 0d0), z/abs(z), abs(z) == 0d0)"
+    assert fcode(sign(x), standard=95, source_format="free") == "merge(0d0, dsign(1d0, x), x == 0d0)"
+    assert fcode(sign(y), standard=95, source_format="free") == "merge(0, isign(1, y), y == 0)"
+    assert fcode(sign(z), standard=95, source_format="free") == "merge(cmplx(0d0, 0d0), z/abs(z), abs(z) == 0d0)"
+    assert fcode(sign(x), assign_to="var", source_format="free") == 'if (x == 0d0) then\n   var =0d0\nelse\n   var = dsign(1d0,x)\nend if'
+    assert fcode(sign(y), assign_to="var", source_format="free") == 'if (y == 0) then\n   var = 0\nelse\n   var = isign(1,y)\nend if'
+    assert fcode(sign(z), assign_to="var", source_format="free") == 'if (abs(z) == 0d0) then\n   var = cmplx(0d0,0d0)\nelse\n   var = z/abs(z)\nend if'
 
 
 def test_fcode_Pow():
