ad824f962aaebdef911920728dca2c3920109118
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-03-27 02:53:25 +0530

    "_entry returns KroneckerDelta in diagonal"

--- sympy/matrices/expressions/diagonal.py
+++ sympy/matrices/expressions/diagonal.py
@@ -13,6 +13,7 @@ class DiagonalMatrix(MatrixExpr):
             return S.Zero
         elif eq is S.true:
             return self.arg[i, 0]
+        return self.arg[i, 0]*KroneckerDelta(i,j)
 
 class DiagonalOf(MatrixExpr):
     arg = property(lambda self: self.args[0])
