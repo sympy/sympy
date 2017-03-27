6cd82a4200ce593acb0a0b51574510bdc663a1c9
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-03-27 15:24:14 +0530

    Added a test for diagonal.py and made a correction

--- sympy/matrices/expressions/diagonal.py
+++ sympy/matrices/expressions/diagonal.py
@@ -8,12 +8,12 @@ class DiagonalMatrix(MatrixExpr):
     shape = property(lambda self: (self.arg.shape[0], self.arg.shape[0]))
 
     def _entry(self, i, j):
-        eq=Eq(i,j)
+        eq = Eq(i, j)
         if eq is S.false:
             return S.Zero
         elif eq is S.true:
             return self.arg[i, 0]
-        return self.arg[i, 0]*KroneckerDelta(i,j)
+        return self.arg[i, j]*KroneckerDelta(i,j)
 
 class DiagonalOf(MatrixExpr):
     arg = property(lambda self: self.args[0])
--- sympy/matrices/expressions/tests/test_diagonal.py
+++ sympy/matrices/expressions/tests/test_diagonal.py
@@ -9,9 +9,18 @@ D = DiagonalMatrix(x)
 d = DiagonalOf(X)
 
 def test_DiagonalMatrix():
+    x = MatrixSymbol('x', n, 1)
     assert D.shape == (n, n)
     assert D[1, 2] == 0
     assert D[1, 1] == x[1, 0]
+    i=Symbol('i')
+    j=Symbol('j')
+    x = MatrixSymbol('x', 3, 3)
+    ij = DiagonalMatrix(x)[i, j]
+    assert ij != 0
+    assert ij.subs({i:0, j:0}) == x[0, 0]
+    assert ij.subs({i:0, j:1}) == 0
+    assert ij.subs({i:1, j:1}) == x[1, 1]
 
 def test_DiagonalMatrix_Assumptions():
     assert ask(Q.diagonal(D))
