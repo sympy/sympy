70bcf629a1284a9cfc92621cc1201a2347ab459a
Francesco Bonazzi <franz.bonazzi@gmail.com>
2017-04-10 21:44:14 +0200

    Adding tests with modification of the args

---
+++ sympy/core/tests/test_constructor_postprocessor.py
@@ -0,0 +1,45 @@
+from sympy import Symbol, Mul, symbols
+
+
+class SymbolInMulOnce(Symbol):
+    # Test class for a symbol that can only appear once in a `Mul` expression.
+
+    _constructor_postprocessor_dict = {
+        "Mul": [lambda x: x],
+        "Pow": [lambda x: x.base if isinstance(x.base, SymbolInMulOnce) else x],
+        "Add": [lambda x: x],
+    }
+
+
+class SymbolRemovesOtherSymbols(Symbol):
+    # Test class for a symbol that removes other symbols in `Mul`.
+
+    def postprocess(expr):
+        args = tuple(i for i in expr.args if not isinstance(i, Symbol) or isinstance(i, SymbolRemovesOtherSymbols))
+        if args == expr.args:
+            return expr
+        return Mul.fromiter(args)
+
+    _constructor_postprocessor_dict = {
+        "Mul": [postprocess],
+    }
+
+
+def test_constructor_postprocessors1():
+    a = symbols("a")
+    x = SymbolInMulOnce("x")
+    y = SymbolInMulOnce("y")
+    assert isinstance(3*x, Mul)
+    assert (3*x).args == (3, x)
+    assert x*x == x
+    assert 3*x*x == 3*x
+    assert 2*x*x + x == 3*x
+    assert x**3*y*y == x*y
+    assert x**5 + y*x**3 == x + x*y
+
+    w = SymbolRemovesOtherSymbols("w")
+    assert x*w == w
+    assert (3*w).args == (3, w)
+    assert 3*a*w**2 == 3*w**2
+    assert 3*a*x**3*w**2 == 3*w**2
+    assert (w + x).args == (x, w)
