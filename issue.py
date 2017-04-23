92bcff0aba968befabd18b56df4a68125b65ed1f
maurogaravello <mauro.garavello@unimib.it>
2017-04-23 13:42:54 +0200

    Fix issue_12555. limit((3**x + 2 * x**10) / (x**10 + E**x), x, -oo) gives 0 instead of 2
    The wrong behavior is due to the fact that leading terms of
    numerator and denominator was wrong calculated.
    At some point the substitution x = 1/u was performed and then
    the leading terms of numerator and denominator
    were calculated comparing the order of the addends. In the
    case of the numerator we have O(3**(1/u)) and O(u**(-10)).
    When comparing the previous order, sympy assumes that u tends to 0, but
    positive. Instead in this situation u should be negative.

--- issue.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division, print_function
-
-from sympy import *
-
-x = Symbol('x', real = True)
-
-expr = (3**x + 2* x**10) / (x**10 + E**x)
-l = limit(expr, x, -oo)
--- sympy/series/limits.py
+++ sympy/series/limits.py
@@ -162,8 +162,12 @@ class Limit(Expr):
                                         for m in Mul.make_args(a))
                                     for a in Add.make_args(w)))
                 if all(ok(w) for w in e.as_numer_denom()):
-                    u = Dummy(positive=(z0 is S.Infinity))
-                    inve = e.subs(z, 1/u)
+                    if z0 is S.NegativeInfinity:
+                        u = Dummy(positive=True)
+                        inve = e.subs(z, -1/u)
+                    else:
+                        u = Dummy(positive=(z0 is S.Infinity))
+                        inve = e.subs(z, 1/u)
                     r = limit(inve.as_leading_term(u), u,
                               S.Zero, "+" if z0 is S.Infinity else "-")
                     if isinstance(r, Limit):
