3eb43e667e81f4bb4ae9c236c2d48edb5960a3c5
Juha Remes <jremes@outlook.com>
2017-04-25 21:07:44 +0100

    Cleared out trailing whitespace

--- sympy/geometry/ellipse.py
+++ sympy/geometry/ellipse.py
@@ -942,38 +942,38 @@ class Ellipse(GeometrySet):
     @property
     def semilatus_rectum(self):
         """
-        Calculates the semi-latus rectum of the Ellipse. 
-        
-        Semi-latus rectum is defined as one half of the the chord through a 
+        Calculates the semi-latus rectum of the Ellipse.
+
+        Semi-latus rectum is defined as one half of the the chord through a
         focus parallel to the conic section directrix of a conic section.
-        
+
         Returns
         =======
-        
+
         semilatus_rectum : number
-        
+
         See Also
         ========
-        
+
         apoapsis : Returns greatest distance between focus and contour
-        
+
         periapsis : The shortest distance between the focus and the contour
-        
+
         Examples
         ========
-        
+
         >>> from sympy import Point, Ellipse
         >>> p1 = Point(0, 0)
         >>> e1 = Ellipse(p1, 3, 1)
         >>> e1.semilatus_rectum()
         1/3
-        
+
         References
         ==========
-        
+
         [1] http://mathworld.wolfram.com/SemilatusRectum.html
         [2] https://en.wikipedia.org/wiki/Ellipse#Semi-latus_rectum
-        
+
         """
         return self.major * (1 - self.eccentricity ** 2)
 
