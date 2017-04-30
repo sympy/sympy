080efe2faefc0be93164239cd5fd30df6c739d40
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-05-01 01:52:29 +0530

    fluid_prop.py : Added Function to determine the Density of a Fluid
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

--- sympy/physics/fluid/fluid_prop.py
+++ sympy/physics/fluid/fluid_prop.py
@@ -1,2 +1,24 @@
 import numpy as np
 from sympy.core import S, Symbol, diff
+
+	'''
+	Density: The density, or more precisely, the volumetric mass density, of a substance is its mass per unit volume
+	================================================================================================================
+	Input Paramters :
+	 m - Mass of the Fluid
+	 v - Volume of the Fluid
+
+	Output Parameter :
+	 d - Density of the Fluid
+
+	================================================================================================================
+	Example :
+
+	>>> from sympy.core import S, Symbol, symbols
+	>>> from sympy.physics.fluid import *
+	>>> m, v = symbols('m, v')
+	>>> d = density(m, v)
+	'''
+
+def density(mass, volume):
+	return mass/volume;
