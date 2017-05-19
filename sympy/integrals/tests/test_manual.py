8d177607f3951feb34e0930ae9177cd95ae08cb6
Theodore Han <tqhan317@gmail.com>
2017-05-19 18:45:21 -0400

    Delete last test case

--- sympy/integrals/tests/test_manual.py
+++ sympy/integrals/tests/test_manual.py
@@ -318,4 +318,3 @@ def test_issue_12641():
     assert manualintegrate(sin(2*x),x) == -cos(2*x)/2
     assert manualintegrate(cos(x)*sin(2*x),x) == -2*cos(x)**3/3
     assert manualintegrate((sin(2*x)*cos(x))/(1+cos(x)),x) == -2*log(cos(x) + 1) - cos(x)**2 + 2*cos(x)
-    assert integrate((sin(2*x)*cos(x))/(1+cos(x)), (x, 0, pi/2), manual=True) == -1 + 2*log(2)
