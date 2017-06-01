8737b8f00eec4a4d3b49a7dc52de9e732c651a3f
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2017-06-01 09:46:06 +0530

    classof -> self and add a doctest

--- sympy/polys/polymatrix.py
+++ sympy/polys/polymatrix.py
@@ -19,6 +19,11 @@ class PolyMatrix(DenseMatrix):
     [    Poly(x**2 + x, x, domain='ZZ'), Poly(0, x, domain='ZZ')],
     [Poly(x**3 - x + 1, x, domain='ZZ'), Poly(0, x, domain='ZZ')]])
 
+    >>> v1*pm1
+    Matrix([
+    [ Poly(x**2, x, domain='ZZ'),  Poly(-x, x, domain='ZZ')],
+    [-Poly(x**2, x, domain='ZZ'), -Poly(-x, x, domain='ZZ')]])
+
     >>> pm2 = PolyMatrix([[Poly(x**2, x, domain='QQ'), Poly(0, x, domain='QQ'), Poly(-x**2, x, domain='QQ'), \
             Poly(x**3, x, domain='QQ'), Poly(0, x, domain='QQ'), Poly(-x**3, x, domain='QQ')]])
     >>> v2 = Matrix([1, 0, 0, 0, 0, 0])
@@ -65,4 +70,4 @@ class PolyMatrix(DenseMatrix):
                 vec = (mat[a]*other_mat[b] for a,b in zip(row_indices, col_indices))
                 new_mat[i] = sum(vec)
 
-        return classof(self, other)._new(new_mat_rows, new_mat_cols, new_mat, copy=False)
+        return self._new(new_mat_rows, new_mat_cols, new_mat, copy=False)
