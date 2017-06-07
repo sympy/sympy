c24bb01ddf566fc73ebcb371fe9c17a156e9b2bd
arihant parsoya <parsoyaarihant@gmail.com>
2017-06-07 22:29:48 +0530

    updated ConstantSymbol()

--- sympy/rubi/symbol.py
+++ sympy/rubi/symbol.py
@@ -1,9 +1,18 @@
-from matchpy import Symbol
+from matchpy import Symbol, Wildcard
+import matchpy
 
 class VariableSymbol(Symbol):
     pass
 
+'''
 class ConstantSymbol(Symbol):
     def __init__(self, value):
-        super(self.__class__, self).__init__(str(value))
+        super(self.__class__, self).__init__(name=str(value))
         self.value = value
+'''
+
+class ConstantSymbol(Symbol):
+    def __init__(self, value, variable_name=None):
+        super(self.__class__, self).__init__(name=str(value), variable_name=variable_name)
+        self.value = value
+        
--- sympy/rubi/tests/test_rubi.py
+++ sympy/rubi/tests/test_rubi.py
@@ -4,5 +4,5 @@ from sympy.rubi.rubi import rubi_integrate
 a, b, c, d, e, f, x = symbols('a b c d e f x')
 
 def test_rubi_integrate():
-    expr = a
+    expr = x**a
     assert rubi_integrate(expr, x) == x**(a + 1)/(a + 1)
