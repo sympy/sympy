111795c5ea4fff1dd7de8ca5826b6cddb5d9ed8e
arihant parsoya <parsoyaarihant@gmail.com>
2017-06-07 14:16:58 +0530

    Updates with_renamed_vars() function in Constraint

--- sympy/rubi/constraint.py
+++ sympy/rubi/constraint.py
@@ -1,4 +1,4 @@
-from matchpy import Constraint, substitute, Operation, Arity
+from matchpy import Constraint, substitute
 from sympy import sympify
 
 class cons(Constraint):
@@ -7,10 +7,11 @@ class cons(Constraint):
         self.vars = frozenset(v.name for v in vars)
 
     def __call__(self, substitution):
+        sub = substitute(self.expr, substitution)
         try:
-            return sympify(str(substitute(self.expr, substitution)))
+            return sympify(str(sub))
         except:
-            print('Unable to sympify')
+            print(('Unable to sympify: {}').format(sub))
             return False
 
     @property
@@ -18,12 +19,12 @@ class cons(Constraint):
         return self.vars
 
     def with_renamed_vars(self, renaming):
-        copy = cons(self.expr, [])
+        copy = cons(self.expr.with_renamed_vars(renaming), [])
         copy.vars = frozenset(renaming.get(v, v) for v in self.vars)
         return copy
 
     def __eq__(self, other):
-        return isinstance(other, NonzeroQ) and other.vars == self.vars and other.expr == self.expr
+        return isinstance(other, cons) and other.vars == self.vars and other.expr == self.expr
 
     def __hash__(self):
-        return hash(self.vars)
+        return hash((self.vars, self.expr))
