e5023b6de062cc3ef48db8c86833135ceb07b283
Rupesh Harode <rupeshharode@gmail.com>
2017-06-18 12:59:36 +0530

    added function to handle deprecated warnings

--- sympy/matrices/tests/test_densesolve.py
+++ sympy/matrices/tests/test_densesolve.py
@@ -17,3 +17,15 @@ def test_rref_solve():
     x, y, z = Dummy('x'), Dummy('y'), Dummy('z')
 
     assert rref_solve([[QQ(25), QQ(15), QQ(-5)], [QQ(15), QQ(18), QQ(0)], [QQ(-5), QQ(0), QQ(11)]], [[x], [y], [z]], [[QQ(2)], [QQ(3)], [QQ(1)]], QQ) == [[QQ(-1, 225)], [QQ(23, 135)], [QQ(4, 45)]]
+
+
+def test_deprecated():
+    # Maintain tests for deprecated functions.  We must capture
+    # the deprecation warnings.  When the deprecated functionality is
+    # removed, the corresponding tests should be removed.
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=SymPyDeprecationWarning)
+        m = matlist(3, 3, [0, 1, 0, -4, 4, 0, -2, 1, 2])
+        P, Jcells = m.jordan_blocks()
+        assert Jcells[1] == matlist(1, 1, [2])
+        assert Jcells[0] == matlist(2, 2, [2, 1, 0, 2])
