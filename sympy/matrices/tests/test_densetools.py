d52596611d300449012be376f7e0b8569a9a8c5e
Rupesh Harode <rupeshharode@gmail.com>
2017-06-18 13:09:29 +0530

    added function to handle deprecation warnings

--- sympy/matrices/tests/test_densetools.py
+++ sympy/matrices/tests/test_densetools.py
@@ -16,3 +16,15 @@ def test_transpose():
 
     assert transpose(a, ZZ) == ([[ZZ(3), ZZ(2), ZZ(6)], [ZZ(7), ZZ(4), ZZ(2)], [ZZ(4), ZZ(5), ZZ(3)]])
     assert transpose(b, ZZ) == b
+
+
+def test_deprecated():
+    # Maintain tests for deprecated functions.  We must capture
+    # the deprecation warnings.  When the deprecated functionality is
+    # removed, the corresponding tests should be removed.
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=SymPyDeprecationWarning)
+        m = matlist(3, 3, [0, 1, 0, -4, 4, 0, -2, 1, 2])
+        P, Jcells = m.jordan_blocks()
+        assert Jcells[1] == matlist(1, 1, [2])
+        assert Jcells[0] == matlist(2, 2, [2, 1, 0, 2])
