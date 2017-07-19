16d46d9a4d9779707c1cf427e2157986d4481753
Aaron Meurer <asmeurer@gmail.com>
2017-07-19 15:34:38 -0400

    Run the docker container with -t (makes Ctrl-C work)

--- release/release.sh
+++ release/release.sh
@@ -19,4 +19,4 @@ if [[ -z $2 ]]; then
     $2=$1
 fi
 
-docker run -v "$parent_path/release-$2":/home/release sympy/sympy-release "$@"
+docker run -t -v "$parent_path/release-$2":/home/release sympy/sympy-release "$@"
