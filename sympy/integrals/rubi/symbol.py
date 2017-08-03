e990bec8dccad830ad1a8f76ce8bef79cb914a89
Abdullah Javed Nesar <abduljaved1994@gmail.com>
2017-08-03 23:07:18 +0530

    resolved conflicts

--- sympy/integrals/rubi/symbol.py
+++ sympy/integrals/rubi/symbol.py
@@ -1,10 +1,20 @@
-from matchpy import Symbol, Wildcard
-import matchpy
+from sympy.external import import_module
+matchpy = import_module("matchpy")
+
+if matchpy:
+    Symbol = matchpy.Symbol
+else:
+    class Symbol(object):
+        def __init__(self, name, variable_name=None):
+            self.name = name
+            self.head = self
+        @staticmethod
+        def dot(x):
+            pass
 
 class VariableSymbol(Symbol):
     pass
 
-
-class Integer(Symbol):
+class matchpyInteger(Symbol):
     def __init__(self, value, variable_name=None):
         super(self.__class__, self).__init__(name=str(value), variable_name=variable_name)
