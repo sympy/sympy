595c118823d4973ad3d1ee0415cdfa5e45fd271e
Francesco Bonazzi <franz.bonazzi@gmail.com>
2017-08-19 11:44:26 -0400

    fixes on LaTeX printer

--- sympy/printing/tests/test_latex.py
+++ sympy/printing/tests/test_latex.py
@@ -179,7 +179,7 @@ def test_latex_vector_expressions():
     assert latex(Cross(A.i, A.j*A.x*3+A.k)) == r"\mathbf{\hat{i}_{A}} \times \left((3 \mathbf{{x}_{A}})\mathbf{\hat{j}_{A}} + \mathbf{\hat{k}_{A}}\right)"
     assert latex(Cross(A.i, A.j)) == r"\mathbf{\hat{i}_{A}} \times \mathbf{\hat{j}_{A}}"
     assert latex(x*Cross(A.i, A.j)) == r"x \left(\mathbf{\hat{i}_{A}} \times \mathbf{\hat{j}_{A}}\right)"
-    assert latex(Cross(x*A.i, A.j)) == r'\left((x)\mathbf{\hat{i}_{A}}\right) \times \mathbf{\hat{j}_{A}}'
+    assert latex(Cross(x*A.i, A.j)) == r'- \mathbf{\hat{j}_{A}} \times \left((x)\mathbf{\hat{i}_{A}}\right)'
 
     assert latex(Curl(3*A.x*A.j)) == r"\nabla\times \left((3 \mathbf{{x}_{A}})\mathbf{\hat{j}_{A}}\right)"
     assert latex(Curl(3*A.x*A.j+A.i)) == r"\nabla\times \left(\mathbf{\hat{i}_{A}} + (3 \mathbf{{x}_{A}})\mathbf{\hat{j}_{A}}\right)"
@@ -192,7 +192,7 @@ def test_latex_vector_expressions():
 
     assert latex(Dot(A.i, A.j*A.x*3+A.k)) == r"\mathbf{\hat{i}_{A}} \cdot \left((3 \mathbf{{x}_{A}})\mathbf{\hat{j}_{A}} + \mathbf{\hat{k}_{A}}\right)"
     assert latex(Dot(A.i, A.j)) == r"\mathbf{\hat{i}_{A}} \cdot \mathbf{\hat{j}_{A}}"
-    assert latex(Dot(x*A.i, A.j)) == r"\left((x)\mathbf{\hat{i}_{A}}\right) \cdot \mathbf{\hat{j}_{A}}"
+    assert latex(Dot(x*A.i, A.j)) == r"\mathbf{\hat{j}_{A}} \cdot \left((x)\mathbf{\hat{i}_{A}}\right)"
     assert latex(x*Dot(A.i, A.j)) == r"x \left(\mathbf{\hat{i}_{A}} \cdot \mathbf{\hat{j}_{A}}\right)"
 
     assert latex(Gradient(A.x)) == r"\nabla\cdot \mathbf{{x}_{A}}"
