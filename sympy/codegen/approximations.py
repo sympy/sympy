ead7088345741e635ab2fbda734b425e68f1978e
Björn Dahlgren <bjodah@gmail.com>
2017-08-29 16:39:49 +0200

    Determine bounds of monotonic expressions

--- sympy/codegen/approximations.py
+++ sympy/codegen/approximations.py
@@ -2,7 +2,8 @@
 from __future__ import (absolute_import, division, print_function)
 
 from itertools import product
-from sympy import Add, Symbol, sin, Abs, oo
+from sympy import Add, Symbol, sin, Abs, oo, Interval
+from sympy.calculus.singularities import is_increasing, is_decreasing
 from sympy.codegen.rewriting import Optimization
 
 """
@@ -15,6 +16,9 @@ methods).
 class SumApprox(Optimization):
     """ Approximates sum by neglecting small terms
 
+    If terms are expressions which can be determined to be monotonic, then
+    bounds for those expressions are added.
+
     Parameters
     ----------
     bounds : dict
@@ -25,18 +29,19 @@ class SumApprox(Optimization):
 
     Examples
     --------
+    >>> from sympy import exp
     >>> from sympy.abc import x, y, z
     >>> from sympy.codegen.rewriting import optimize
     >>> from sympy.codegen.approximations import SumApprox
-    >>> bounds = {x: (-1, 1), y: (1000, 2000), z: (0, 20)}
+    >>> bounds = {x: (-1, 1), y: (1000, 2000), z: (-10, 3)}
     >>> sum_approx3 = SumApprox(bounds, reltol=1e-3)
     >>> sum_approx2 = SumApprox(bounds, reltol=1e-2)
     >>> sum_approx1 = SumApprox(bounds, reltol=1e-1)
-    >>> expr = 3*(x + y + z)
+    >>> expr = 3*(x + y + exp(z))
     >>> optimize(expr, [sum_approx3])
-    3*(x + y + z)
+    3*(x + y + exp(z))
     >>> optimize(expr, [sum_approx2])
-    3*y + 3*z
+    3*y + 3*exp(z)
     >>> optimize(expr, [sum_approx1])
     3*y
 
@@ -54,6 +59,26 @@ class SumApprox(Optimization):
         return expr.is_Add
 
     def value(self, add):
+        for term in add.args:
+            if term.is_number or term in self.bounds or len(term.free_symbols) != 1:
+                continue
+            fs, = term.free_symbols
+            if fs not in self.bounds:
+                continue
+            intrvl = Interval(*self.bounds[fs])
+            if is_increasing(term, intrvl, fs):
+                self.bounds[term] = (
+                    term.subs({fs: self.bounds[fs][0]}),
+                    term.subs({fs: self.bounds[fs][1]})
+                )
+            elif is_decreasing(term, intrvl, fs):
+                self.bounds[term] = (
+                    term.subs({fs: self.bounds[fs][1]}),
+                    term.subs({fs: self.bounds[fs][0]})
+                )
+            else:
+                return add
+
         if all(term.is_number or term in self.bounds for term in add.args):
             bounds = [(term, term) if term.is_number else self.bounds[term] for term in add.args]
             largest_abs_guarantee = 0
--- sympy/codegen/tests/test_approximations.py
+++ sympy/codegen/tests/test_approximations.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division, print_function)
 
-from sympy import symbols
+from sympy import symbols, exp
 from sympy.codegen.rewriting import optimize
 from sympy.codegen.approximations import SumApprox
 
 
-def test_sum_approx():
-    x, y = symbols('x y')
+def test_SumApprox_trivial():
+    x = symbols('x')
     expr1 = 1 + x
     sum_approx = SumApprox(bounds={x: (-1e-20, 1e-20)}, reltol=1e-16)
     apx1 = optimize(expr1, [sum_approx])
     assert apx1 - 1 == 0
+
+
+def test_SumApprox_monotone_terms():
+    x, y, z = symbols('x y z')
+    expr1 = exp(z)*(x**2 + y**2 + 1)
+    bnds1 = {x: (0, 1e-3), y: (100, 1000)}
+    sum_approx_m2 = SumApprox(bounds=bnds1, reltol=1e-2)
+    sum_approx_m5 = SumApprox(bounds=bnds1, reltol=1e-5)
+    sum_approx_m11 = SumApprox(bounds=bnds1, reltol=1e-11)
+    assert (optimize(expr1, [sum_approx_m2])/exp(z) - (y**2)).simplify() == 0
+    assert (optimize(expr1, [sum_approx_m5])/exp(z) - (y**2 + 1)).simplify() == 0
+    assert (optimize(expr1, [sum_approx_m11])/exp(z) - (y**2 + 1 + x**2)).simplify() == 0
