70d3b224df8e157827a332163c0055787cff8bd4
Arihant Parsoya <parsoyaarihant@gmail.com>
2017-09-02 11:29:52 +0530

    fixed bugs in PerfectSquareQ and Dist
    Fixes #13245 , #13244 and #13247

--- sympy/integrals/rubi/tests/test_rubi_integrate.py
+++ sympy/integrals/rubi/tests/test_rubi_integrate.py
@@ -11,10 +11,12 @@ if sys.version_info[:2] < (3, 6):
 
 from sympy.core.symbol import symbols, Symbol
 from sympy.functions import log
-from sympy import sqrt, simplify, S, atanh, hyper, I, atan
+from sympy import (sqrt, simplify, S, atanh, hyper, I, atan, pi, Sum, cos, sin,
+    log, atan)
+from sympy.integrals.rubi.utility_function import rubi_test
 from sympy.integrals.rubi.rubi import rubi_integrate
 
-a, b, c, d, e, f, x, m, n, p = symbols('a b c d e f x m n p', real=True, imaginary=False)
+a, b, c, d, e, f, x, m, n, p, k = symbols('a b c d e f x m n p k', real=True, imaginary=False)
 
 def test_rubi_integrate():
     assert rubi_integrate(x, x) == x**2/2
@@ -30,3 +32,4 @@ def test_rubi_integrate():
     assert rubi_integrate((a + b*x)**3/x**3, x) == -a**3/(2*x**2) - 3*a**2*b/x + 3*a*b**2*log(x) + b**3*x
     assert rubi_integrate(x**3*(a + b*x), x) == a*x**4/4 + b*x**5/5
     assert rubi_integrate((b*x)**m*(d*x + 2)**n, x) == 2**n*(b*x)**(m + 1)*hyper((-n, m + 1), (m + 2,), -d*x/2)/(b*(m + 1))
+    assert rubi_test(rubi_integrate(1/(1 + x**5), x), x, log(x + S(1))/S(5) + S(2)*Sum(-log((S(2)*x - S(2)*cos(pi*(S(2)*k/S(5) + S(-1)/5)))**S(2) - S(4)*sin(S(2)*pi*k/S(5) + S(3)*pi/S(10))**S(2) + S(4))*cos(pi*(S(2)*k/S(5) + S(-1)/5))/S(2) - (-S(2)*cos(pi*(S(2)*k/S(5) + S(-1)/5))**S(2) + S(2))*atan((-x/cos(pi*(S(2)*k/S(5) + S(-1)/5)) + S(1))/sqrt(-(cos(S(2)*pi*k/S(5) - pi/S(5)) + S(-1))*(cos(S(2)*pi*k/S(5) - pi/S(5)) + S(1))/cos(S(2)*pi*k/S(5) - pi/S(5))**S(2)))/(S(2)*sqrt(-(cos(S(2)*pi*k/S(5) - pi/S(5)) + S(-1))*(cos(S(2)*pi*k/S(5) - pi/S(5)) + S(1))/cos(S(2)*pi*k/S(5) - pi/S(5))**S(2))*cos(pi*(S(2)*k/S(5) + S(-1)/5))), (k, S(1), S(2)))/S(5), _numerical=True)
