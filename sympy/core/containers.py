0f6d47d5873e916848fd5d53b1eaae7b5b2afcc9
Isuru Fernando <isuruf@gmail.com>
2017-09-13 17:54:48 -0500

    Use OrderedDict for OrderedSet

--- sympy/core/containers.py
+++ sympy/core/containers.py
@@ -268,17 +268,9 @@ class Dict(Basic):
         return tuple(sorted(self.args, key=default_sort_key))
 
 
-# OrderedSet code is based on http://code.activestate.com/recipes/576694/ by
-# Raymond Hettinger. Licensed under MIT License
-
 class OrderedSet(collections.MutableSet):
-
     def __init__(self, iterable=None):
-        self.end = end = []
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.map = {}                   # key --> [key, prev, next]
-        if iterable is not None:
-            self |= iterable
+        self.map = collections.OrderedDict((item, None) for item in iterable)
 
     def __len__(self):
         return len(self.map)
@@ -287,62 +279,12 @@ class OrderedSet(collections.MutableSet):
         return key in self.map
 
     def add(self, key):
-        if key not in self.map:
-            end = self.end
-            curr = end[1]
-            curr[2] = end[1] = self.map[key] = [key, curr, end]
+        self.map[key] = None
 
     def discard(self, key):
-        if key in self.map:
-            key, prev, next = self.map.pop(key)
-            prev[2] = next
-            next[1] = prev
+        self.pop(key)
 
     def __iter__(self):
-        end = self.end
-        curr = end[2]
-        while curr is not end:
-            yield curr[0]
-            curr = curr[2]
+        for key in self.map.keys():
+            yield key
 
-    def __reversed__(self):
-        end = self.end
-        curr = end[1]
-        while curr is not end:
-            yield curr[0]
-            curr = curr[1]
-
-    def pop(self, last=True):
-        if not self:
-            raise KeyError('set is empty')
-        key = self.end[1][0] if last else self.end[2][0]
-        self.discard(key)
-        return key
-
-    def __repr__(self):
-        if not self:
-            return '%s()' % (self.__class__.__name__,)
-        return '%s(%r)' % (self.__class__.__name__, list(self))
-
-    def __eq__(self, other):
-        if isinstance(other, OrderedSet):
-            return len(self) == len(other) and list(self) == list(other)
-        return set(self) == set(other)
-
-    def intersection(self, other):
-        result = []
-        for val in self:
-            if val in other:
-                result.append(val)
-        return self.__class__(result)
-
-    def difference(self, other):
-        result = []
-        for val in self:
-            if val not in other:
-                result.append(val)
-        return self.__class__(result)
-
-    def update(self, iterable):
-        for val in iterable:
-            self.add(val)
