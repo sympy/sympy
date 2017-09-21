5aa677cae48d73c7cd2d5a109a33350e78bd14e8
ethankward <ethanward@email.arizona.edu>
2017-09-21 13:14:51 -0700

    Dimension is attribute and parameter.

--- sympy/geometry/curve.py
+++ sympy/geometry/curve.py
@@ -29,6 +29,8 @@ class Curve(GeometrySet):
     function : list of functions
     limits : 3-tuple
         Function parameter and lower and upper bounds.
+    dimension: integer
+        Dimension of the curve (corresponding to the number of functions used in its definition).
     length: number or sympy expression
         Arc length of the curve over the limits.
     tangent: Matrix
@@ -49,7 +51,7 @@ class Curve(GeometrySet):
     parameter
     limits
     dimension
-
+    
     Raises
     ======
 
