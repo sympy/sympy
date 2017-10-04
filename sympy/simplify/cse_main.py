5be44d69c6ffbfe6640cd07c358c9b16ce29d38b
Isuru Fernando <isuruf@gmail.com>
2017-10-03 22:03:10 -0500

    Get commutative args as list

--- sympy/simplify/cse_main.py
+++ sympy/simplify/cse_main.py
@@ -248,7 +248,7 @@ def opt_cse(exprs, order='canonical'):
     # split muls into commutative
     comutative_muls = set()
     for m in muls:
-        c, nc = m.args_cnc(cset=True, warn=False)
+        c, nc = m.args_cnc(cset=False)
         if c:
             c_mul = m.func(*c)
             if nc:
