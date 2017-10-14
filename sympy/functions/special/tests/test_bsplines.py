8ccc9943a11a499ab487c28d4c815c5d02906a80
Filip Gokstorp <filip@gokstorp.se>
2017-10-14 11:29:06 +0100

    Adds test for BSplines of degree > 1 with repeated knots

--- sympy/functions/special/tests/test_bsplines.py
+++ sympy/functions/special/tests/test_bsplines.py
@@ -1,6 +1,6 @@
 from sympy.functions import bspline_basis_set
 from sympy.core.compatibility import range
-from sympy import Piecewise, Interval
+from sympy import Piecewise, Interval, And
 from sympy import symbols, Rational
 
 x, y = symbols('x,y')
@@ -81,3 +81,33 @@ def test_repeated_degree_1():
         (4 - x, Interval(3, 4).contains(x)), (0, True))
     assert splines[5] == Piecewise((-3 + x, Interval(3, 4).contains(x)
                                    ), (0, True))
+
+
+def test_repeated_degree_2():
+    d = 2
+    knots = [0, 0, 1, 2, 2, 3, 4, 4]
+    splines = bspline_basis_set(d, knots, x)
+
+    assert splines[0] == (Piecewise((-x**2 + x, And(x < 1, x >= 0)),
+                                    (0, True))
+                          + Piecewise((-x**2/2 + x, And(x < 1, x >= 0)),
+                                      (x**2/2 - 2*x + 2, And(x <= 2, x >= 1)),
+                                      (0, True)))
+    assert splines[1] == (Piecewise((-x**2 + 3*x - 2, And(x <= 2, x >= 1)),
+                                    (0, True))
+                          + Piecewise((x**2/2, And(x < 1, x >= 0)),
+                                      (-x**2/2 + x, And(x < 2, x >= 1)),
+                                      (0, True)))
+    assert splines[2] == Piecewise((x**2 - 2*x + 1, And(x < 2, x >= 1)),
+                                   (x**2 - 6*x + 9, And(x <= 3, x >= 2)),
+                                   (0, True))
+    assert splines[3] == (Piecewise((-x**2 + 5*x - 6, And(x < 3, x >= 2)),
+                                    (0, True))
+                          + Piecewise((-x**2/2 + 3*x - 4, And(x < 3, x >= 2)),
+                                      (x**2/2 - 4*x + 8, And(x <= 4, x >= 3)),
+                                      (0, True)))
+    assert splines[4] == (Piecewise((-x**2 + 7*x - 12, And(x <= 4, x >= 3)),
+                                    (0, True))
+                          + Piecewise((x**2/2 - 2*x + 2, And(x < 3, x >= 2)),
+                                      (-x**2/2 + 3*x - 4, And(x < 4, x >= 3)),
+                                      (0, True)))
