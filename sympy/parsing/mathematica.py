c3073d8a5348fc01580aed3e201b9418c85b821f
Yuki Matsuda <yuki.matsuda.w@gmail.com>
2017-10-24 19:48:29 +0200

    Rewrite rules for regex in more readable way
    I rewrote patterns for regex in more readable way with re.VERBOSE
    option. the content did not change intrinsically.

--- sympy/parsing/mathematica.py
+++ sympy/parsing/mathematica.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division
 
-from re import match
+import re
 from sympy import sympify
 
 
@@ -14,40 +14,151 @@ def parse(s):
     # Begin rules
     rules = (
         # Arithmetic operation between a constant and a function
-        (r"\A(\d+)([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",
-        lambda m: m.group(
-            1) + translateFunction(m.group(2)) + parse(m.group(3))),
+        (r"""
+            \A          # Matches only at the start of the string
+            (           # group1 
+            [+-]?       # + or - sign with 0 or 1 repetition 
+            \d+         # any number with 1 or more repetition 
+            [*/+^-]     # either of arithmetic operands 
+            )
+            (           # group1
+            \w+         # alphanumeric character and the underscore
+            \[          # [ as a character
+            [^\]]+      # character except ] with 1 or more repetittion
+            [^[]*       # character except [ with 0 or more repetittion
+            \]          # ] as a character
+            ) 
+            \Z          # Matches only at the end of the string
+            """,
+        lambda m: m.group(1) + parse(m.group(2))),
 
         # Arithmetic operation between two functions
-        (r"\A(\w+\[[^\]]+[^\[]*\])([*/+-^])(\w+\[[^\]]+[^\[]*\])\Z",
-        lambda m: parse(m.group(1)) + translateFunction(
-            m.group(2)) + parse(m.group(3))),
+        (r"""
+            \A
+            (           # group1
+            \w+         
+            \[          
+            [^\]]+      
+            [^\[]*      
+            \]          
+            ) 
+            (           # group2
+            [*/+-^]     
+            ) 
+            (           # group3
+            \w+
+            \[
+            [^\]]+
+            [^\[]*
+            \]
+            )
+            \Z
+            """,
+        lambda m: parse(m.group(1)) + m.group(2) + parse(m.group(3))),
 
-        (r"\A([+-]?)(\w+)\[([^\]]+[^\[]*)\]\Z",  # Function call
+        # Function call
+        (r"""
+            \A
+            ([+-]?)     # group1, + or - sign with 0 or 1 repetition 
+            (\w+)       # group2 
+            \[          
+            (           # group3
+            [^\]]+      
+            [^[]*       
+            ) 
+            \]          
+            \Z
+            """,  
         lambda m: m.group(1) + translateFunction(
             m.group(2)) + "(" + parse(m.group(3)) + ")"),
 
-        (r"\((.+)\)\((.+)\)",  # Parenthesized implied multiplication
+        # Parenthesized implied multiplication
+        (r"""
+            \(          # ( as a character
+            (.+)        # group1, any character with 1 or more repetition
+            \)          # ) as a character
+            \(
+            (.+)        # group2
+            \)
+            """,  
         lambda m: "(" + parse(m.group(1)) + ")*(" + parse(m.group(2)) + ")"),
 
-        (r"\A\((.+)\)\Z",  # Parenthesized expression
-        lambda m: "(" + parse(m.group(1)) + ")"),
+        # Parenthesized expression
+        (r"""
+            \A
+            ([+-]?)     # group1, + or - sign with 0 or 1 repetition
+            \(      
+            (.+)        # group2
+            \)      
+            \Z
+            """,  
+        lambda m: m.group(1) + "(" + parse(m.group(2)) + ")"),
 
-        (r"\A(.*[\w\.])\((.+)\)\Z",  # Implied multiplication - a(b)
+        # Implied multiplication - a(b)
+        (r"""
+            \A
+            (           # group1
+            .*          
+            [\w.]       # either of alphanumeric character, the underscore or dot
+            )
+            \(          
+            (.+)        # group2
+            \)      
+            \Z
+            """, 
         lambda m: parse(m.group(1)) + "*(" + parse(m.group(2)) + ")"),
 
-        (r"\A\((.+)\)([\w\.].*)\Z",  # Implied multiplication - (a)b
+        # Implied multiplication - (a)b
+        (r"""
+            \A
+            \(      
+            (.+)        # group1
+            \)      
+            (           # group2
+            [\w.]
+            .*
+            )
+            \Z
+            """,  
         lambda m: "(" + parse(m.group(1)) + ")*" + parse(m.group(2))),
 
-        (r"\A(-? *[\d\.]+)([a-zA-Z].*)\Z",  # Implied multiplication - 2a
+        # Implied multiplication - 2a
+        (r"""
+            \A
+            (           # group1
+            -?          # - sign with 0 or 1 repetition
+            \ *         # space with 0 or more repetition
+            [\d.]+     # either of number or dot with 1 or more repetition
+            )
+            (           # group2
+            [a-zA-Z]    
+            .*
+            )
+            \Z
+            """,  
         lambda m: parse(m.group(1)) + "*" + parse(m.group(2))),
 
-        (r"\A([^=]+)([\^\-\*/\+=]=?)(.+)\Z",  # Infix operator
-        lambda m: parse(m.group(1)) + translateOperator(m.group(2)) + parse(m.group(3))))
+        # Infix operator
+        (r"""
+            \A
+            (           # group1
+            [^=]+       # any characters except = with 1 or more repetition
+            )
+            (           # group2
+            [*/+^=-]    # operands and = 
+            =?          # = with 0 or 1 repetition
+            )
+            (.+)        # group3
+            \Z
+            """,  
+        lambda m: parse(m.group(1)) + translateOperator(
+            m.group(2)) + parse(m.group(3))),
+        )
     # End rules
 
     for rule, action in rules:
-        m = match(rule, s)
+        rule = re.compile(rule, re.VERBOSE)
+        m = re.match(rule, s)
         if m:
             return action(m)
 
