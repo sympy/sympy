5b2022b257e846a8dcf4f5853e30a61641aa2fd9
ashishkg0022 <ashishkg0022@gmail.com>
2017-11-04 02:15:57 +0530

    added unit tests for #13536

--- sympy/integrals/tests/test_meijerint.py
+++ sympy/integrals/tests/test_meijerint.py
@@ -673,3 +673,8 @@ def test_issue_10681():
     g = (1.0/3)*R**1.0*r**3*hyper((-0.5, S(3)/2), (S(5)/2,),
                                   r**2*exp_polar(2*I*pi)/R**2)
     assert RR.almosteq((f/g).n(), 1.0, 1e-12)
+
+def test_issue_13536():
+    from sympy import Symbol
+    a = Symbol('a', real=True, positive=True)
+    assert integrate(1/x**2, (x, oo, a)) == -1/a
