d7ddf26bb2c9f303789814956048c6b0446ae8b2
Francesco Bonazzi <franz.bonazzi@gmail.com>
2017-11-04 01:01:09 -0400

    added parsing tests to generated examples

--- sympy/matrices/expressions/tests/test_indexing.py
+++ sympy/matrices/expressions/tests/test_indexing.py
@@ -97,18 +97,23 @@ def test_matrix_expression_to_indices():
     expr = W*X*Z
     assert replace_dummies(expr._entry(i, j)) == \
         Sum(W[i, i1]*X[i1, i2]*Z[i2, j], (i1, 0, l-1), (i2, 0, m-1))
+    assert MatrixExpr.from_index_summation(expr._entry(i, j)) == expr
+
     expr = Z.T*X.T*W.T
     assert replace_dummies(expr._entry(i, j)) == \
         Sum(W[j, i2]*X[i2, i1]*Z[i1, i], (i1, 0, m-1), (i2, 0, l-1))
+    assert MatrixExpr.from_index_summation(expr._entry(i, j), i) == expr
 
     expr = W*X*Z + W*Y*Z
     assert replace_dummies(expr._entry(i, j)) == \
         Sum(W[i, i1]*X[i1, i2]*Z[i2, j], (i1, 0, l-1), (i2, 0, m-1)) +\
         Sum(W[i, i1]*Y[i1, i2]*Z[i2, j], (i1, 0, l-1), (i2, 0, m-1))
+    assert MatrixExpr.from_index_summation(expr._entry(i, j)) == expr
 
     expr = W*(X + Y)*Z
     assert replace_dummies(expr._entry(i, j)) == \
             Sum(W[i, i1]*(X[i1, i2] + Y[i1, i2])*Z[i2, j], (i1, 0, l-1), (i2, 0, m-1))
+    assert MatrixExpr.from_index_summation(expr._entry(i, j)) == expr
 
     expr = A*B**2*A
     #assert replace_dummies(expr._entry(i, j)) == \
