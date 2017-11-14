9d344fdcc6ef18d36b43ce9275f59ec538e77484
Francesco Bonazzi <franz.bonazzi@gmail.com>
2017-11-14 09:43:34 -0500

    fixed trigsimp on matrices

--- sympy/simplify/tests/test_trigsimp.py
+++ sympy/simplify/tests/test_trigsimp.py
@@ -351,9 +351,8 @@ def test_issue_2827_trigsimp_methods():
     # all methods should work with Basic expressions even if they
     # aren't Expr
     M = Matrix.eye(1)
-    # NOTE: "groebner" is currently not tested as it raises an exception:
     assert all(trigsimp(M, method=m) == M for m in
-        'fu matching old'.split())
+        'fu matching groebner old'.split())
     # watch for E in exptrigsimp, not only exp()
     eq = 1/sqrt(E) + E
     assert exptrigsimp(eq) == eq
