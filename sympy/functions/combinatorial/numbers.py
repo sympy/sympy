66cc665e2e9deda8f03a75bcb5c9ff9bc78800d9
Peleg Michaeli <freepeleg@gmail.com>
2017-11-19 15:53:32 +0200

    Some assumptions on Catalan numbers + tests

--- sympy/functions/combinatorial/numbers.py
+++ sympy/functions/combinatorial/numbers.py
@@ -977,6 +977,20 @@ class catalan(Function):
         k = Dummy('k', integer=True, positive=True)
         return Product((n + k) / k, (k, 2, n))
 
+    def _eval_is_integer(self):
+        if self.args[0].is_integer:
+            if self.args[0].is_nonnegative:
+                return True
+
+    def _eval_is_positive(self):
+        if self.args[0].is_nonnegative:
+            return True
+
+    def _eval_is_composite(self):
+        if self.args[0].is_integer:
+            if (self.args[0] - 3).is_positive:
+                return True
+
     def _eval_evalf(self, prec):
         from sympy import gamma
         if self.args[0].is_number:
