3cca0bc1bfaab7e8a3397fe8fad6777c4ea5de27
Nicholas Bollweg <nbollweg@continuum.io>
2017-12-06 12:34:53 -0500

    port latex2sympy

---
+++ sympy/parsing/_latex/__init__.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class Antlr4BuildError(Exception):
+    pass
+
+
+class LaTeXParserNotAvailable(Exception):
+    pass
+
+
+class Antlr4RuntimeMissing(Exception):
+    pass
+
+
+class Antlr4Missing(Exception):
+    pass
+
+
+def build_with_antlr():
+    try:
+        subprocess.check_output(["antlr4", os.path.join(here, "PS.g4")])
+    except subprocess.CalledProcessError as err:
+        raise Antlr4Missing("Please install antlr4")
+
+    try:
+        subprocess.check_output(["antlr4", os.path.join(here, "PS.g4")])
+    except subprocess.CalledProcessError as err:
+        raise Antlr4BuildError("Couldn't build LaTeX parser")
+
+
+def get_parser(retry=True):
+    try:
+        from sympy.parsing._latex.PSParser import PSParser
+        from sympy.parsing._latex.PSLexer import PSLexer
+        return PSParser, PSLexer
+    except ModuleNotFoundError:
+        print("Generated Latex parser not found, attempting to build with antlr4")
+        try:
+            build_with_antlr()
+        except Antlr4BuildError:
+            return None, None
+
+        return get_parser(False)
