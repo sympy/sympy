4548b50245fc7286cb6871174d2ec0ec245acc90
rushyam <rushyamsonu@gmail.com>
2017-12-11 14:29:18 +0530

    added product moment of area

--- sympy/geometry/polygon.py
+++ sympy/geometry/polygon.py
@@ -404,18 +404,19 @@ class Polygon(GeometrySet):
         return Point(simplify(A*cx), simplify(A*cy))
 
     def second_moment_of_area(self,p=None):
-        """Second moment of area of polygon.
-
-        Notes
-        =====
-
-        about the axis passing through centroid if point is not given othervise about the axis passing through given point.
+        """Second and product moment of area of polygon.
 
         Parameters
         ==========
 
         p : Point, None
             Default value is None.
+
+        Notes
+        =====
+
+        p is the point about which second moment of area is to be find.function will return second moment of area about
+        the axis passing through centroid if point is not given othervise about the axis passing through the p point.
 
         Examples
         ========
@@ -425,12 +426,12 @@ class Polygon(GeometrySet):
         >>> p1,p2,p3,p4,p5=[(0,0),(a,0),(a,b),(0,b),(a/3,b/3)]
         >>> rectangle=Polygon(p1,p2,p3,p4)
         >>> rectangle.second_moment_of_area()
-        (a*b**3/12, a**3*b/12)
+        (a*b**3/12, a**3*b/12, 0)
         >>> rectangle.second_moment_of_area(p5)
-        (a*b**3/9, a**3*b/9)
+        (a*b**3/9, a**3*b/9, a**2*b**2/36)
 
         """
-        I_xx,I_yy=0,0
+        I_xx,I_yy,I_xy=0,0,0
         args= self.args
         for i in range(len(args)):
             x1,y1 = args[i-1].args
@@ -438,18 +439,21 @@ class Polygon(GeometrySet):
             v = x1*y2 - x2*y1
             I_xx += (y1**2+y1*y2+y2**2)*v
             I_yy += (x1**2+x1*x2+x2**2)*v
+            I_xy += (x1*y2+2*x1*y1+2*x2*y2+x2*y1)*v
         A=self.area
         c_x=self.centroid[0]
         c_y=self.centroid[1]
         #parallel axis theorem
         I_xx_c = (I_xx/12) - A*(c_y**2)
         I_yy_c = (I_yy/12) - A*(c_x**2)
+        I_xy_c = (I_xy/24) - A*(c_x*c_y)
         if(p == None):
-            return I_xx_c,I_yy_c
+            return I_xx_c,I_yy_c,I_xy_c
         I_xx = (I_xx_c + A*((p[1]-c_y)**2))
         I_yy = (I_yy_c + A*((p[0]-c_x)**2))
+        I_xy = (I_xy_c + A*((p[0]-c_x)*(p[1]-c_y)))
 
-        return I_xx,I_yy
+        return I_xx,I_yy,I_xy
 
     @property
     def sides(self):
