3951e6d13ceb5d9408a04720f379275f22624d54
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2017-12-14 13:44:13 +0530

    Removed trailing whitespace

--- sympy/physics/tests/test_secondquant.py
+++ sympy/physics/tests/test_secondquant.py
@@ -1225,6 +1225,6 @@ def test_canonical_ordering_AntiSymmetricTensor():
  
     c, d = symbols(('c','d'), above_fermi=True, cls=Dummy)
     k, l = symbols(('k','l'), below_fermi=True, cls=Dummy)
-    
+
     assert AntiSymmetricTensor(v, (k, l), (d, c)) == -AntiSymmetricTensor(v, (l, k), (d, c))
     # Gives AntiSymTensor(kl,dc) or -AntiSymTensor(lk,dc) randomly between runs.
