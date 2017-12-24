efd8c9c521d1519271a56438107c5e0a57e5b83a
Abhigyan Khaund <mail@abhigyan.xyz>
2017-12-24 10:08:55 +0530

    Add test for __rmatmul__

--- sympy/matrices/tests/test_commonmatrix.py
+++ sympy/matrices/tests/test_commonmatrix.py
@@ -677,12 +677,22 @@ def test_multiplication():
 def test_matmul():
     a = Matrix([[1, 2], [3, 4]])
 
-    #Check a@2 case
     assert a.__matmul__(2) == NotImplemented
 
+    assert a.__rmatmul__(2) == NotImplemented
+
+    #This is done this way because @ is only supported in Python 3.5+
     #To check 2@a case
     try:
         eval('2 @ a')
+    except SyntaxError:
+        pass
+    except TypeError:  #TypeError is raised in case of NotImplemented is returned
+        pass
+
+    #Check a@2 case
+    try:
+        eval('a @ 2')
     except SyntaxError:
         pass
     except TypeError:  #TypeError is raised in case of NotImplemented is returned
