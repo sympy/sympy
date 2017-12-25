a78d62f9b5b8e5a9597b76c8aaa9a11120769440
Ashish Kumar Gaurav <ashishkg0022@gmail.com>
2017-12-25 23:25:48 +0530

    added classes of ordinals in test_args

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -590,6 +590,22 @@ def test_sympy__calculus__util__AccumulationBounds():
     from sympy.calculus.util import AccumulationBounds
     assert _test_args(AccumulationBounds(0, 1))
 
+@XFAIL
+def test_sympy__sets__ordinals__OmegaPower():
+    from sympy.sets.ordinals import OmegaPower
+    assert _test_args(OmegaPower(2,))
+
+def test_sympy__sets__ordinals__Ordinal():
+    from sympy.sets.ordinals import Ordinal
+    assert _test_args(Ordinal())
+
+def test_sympy__sets__ordinals__OrdinalOmega():
+    from sympy.sets.ordinals import OrdinalOmega
+    assert _test_args(OrdinalOmega())
+
+def test_sympy__sets__ordinals__OrdinalZero():
+    from sympy.sets.ordinals import OrdinalZero
+    assert _test_args(OrdinalZero())
 
 def test_sympy__sets__sets__EmptySet():
     from sympy.sets.sets import EmptySet
