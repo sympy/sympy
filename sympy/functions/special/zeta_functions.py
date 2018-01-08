39e1eb8d22692f8a470cf8e26b4c7b1313f03cc6
Leonid Kovalev <normalhuman@users.noreply.github.com>
2018-01-08 14:36:23 +0000

    Evaluate polylog(s, z) for s in [-1, 0, 1], without exp_polar
    polylog(s, z) has simple formulas for s in -1, 0, 1. They should be
    used by eval, just as it uses the formulas for z in -1, 0, 1 without
    the user needing to expand_func. For example:
    ```
    >>> polylog(2, S(1)/2)
    -log(2)**2/2 + pi**2/12
    >>> polylog(1, S(1)/2)
    log(2)     # previously this returned unevaluated
    ```
    One has to draw the line somewhere, and I left s = -2 and the rest
    un-evaluated, subject to the user applying expand_func
    
    Per the discussion at #13853 the expansion for s = -1 is simply
    -log(1 - z), as it appears in Wikipedia, Wolfram, etc.

--- sympy/functions/special/zeta_functions.py
+++ sympy/functions/special/zeta_functions.py
@@ -253,7 +253,7 @@ class polylog(Function):
     >>> from sympy import expand_func
     >>> from sympy.abc import z
     >>> expand_func(polylog(1, z))
-    -log(z*exp_polar(-I*pi) + 1)
+    -log(1 - z)
     >>> expand_func(polylog(0, z))
     z/(-z + 1)
 
@@ -277,7 +277,7 @@ class polylog(Function):
             return -dirichlet_eta(s)
         elif z == 0:
             return S.Zero
-        if s == 2:
+        elif s == 2:
             if z == S.Half:
                 return pi**2/12 - log(2)**2/2
             elif z == 2:
@@ -290,6 +290,12 @@ class polylog(Function):
                 return pi**2/15 - log((sqrt(5)-1)/2)**2
             elif z == (sqrt(5) - 1)/2:
                 return pi**2/10 - log((sqrt(5)-1)/2)**2
+        elif s == 1:
+            return -log(1 - z)
+        elif s == 0:
+            return z/(1 - z)
+        elif s == -1:
+            return z/(1 - z)**2
 
     def fdiff(self, argindex=1):
         s, z = self.args
@@ -304,7 +310,7 @@ class polylog(Function):
         from sympy import log, expand_mul, Dummy, exp_polar, I
         s, z = self.args
         if s == 1:
-            return -log(1 + exp_polar(-I*pi)*z)
+            return -log(1 - z)
         if s.is_Integer and s <= 0:
             u = Dummy('u')
             start = u/(1 - u)
