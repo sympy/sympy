91d26262e318ae80b8d11ccfcaafda0686c9d693
jashan498 <jashansingh.4398@gmail.com>
2018-01-10 19:14:21 +0530

    Changed bj <= 0 to bj.is_nonpositive. In bj<=0, value of bj has to be known for valid '<=' operation, which is not always the case in symbolic algebra

--- sympy/simplify/hyperexpand.py
+++ sympy/simplify/hyperexpand.py
@@ -1341,7 +1341,7 @@ class ReduceOrder(Operator):
         n = ai - bj
         if not n.is_Integer or n < 0:
             return None
-        if bj.is_integer and bj <= 0:
+        if bj.is_integer and bj.is_nonpositive:
             return None
 
         expr = Operator.__new__(cls)
