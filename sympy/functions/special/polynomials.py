1bae4043090eeed4b3762ded7226bb4248de8455
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-01-14 10:07:10 +0530

    fix #13916, legendre polynomial for negative n
    Instead of giving exception for negative n in Legendre Polynomial, replacing n to -n-1. This is a convention(http://functions.wolfram.com/Polynomials/LegendreP/02/) with Legendre polynomials

--- sympy/functions/special/polynomials.py
+++ sympy/functions/special/polynomials.py
@@ -800,9 +800,8 @@ class legendre(OrthogonalPolynomial):
             # n is a given fixed integer, evaluate into polynomial
             if n.is_negative:
                 n = -n - S.One
-                return cls._eval_at_order(n, x)
-            else:
-                return cls._eval_at_order(n, x)
+            return cls._eval_at_order(n, x)
+
 
     def fdiff(self, argindex=2):
         if argindex == 1:
