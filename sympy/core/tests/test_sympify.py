fe02eb75d8ebb556823b29b04362ad3d62c5a47a
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-14 21:47:55 +0100

    skip test_issue_13924 if numpy is not installed

--- sympy/core/tests/test_sympify.py
+++ sympy/core/tests/test_sympify.py
@@ -605,6 +605,9 @@ def test_sympify_rational_numbers_set():
 
 
 def test_issue_13924():
+    if not numpy:
+        skip("numpy not installed.")
+
     a = sympify(numpy.array([1]))
     assert isinstance(a, ImmutableDenseNDimArray)
     assert a[0] == 1
