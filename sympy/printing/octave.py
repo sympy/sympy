7d0e43799524895706db2c59b9575f70cb99e039
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-01-16 18:50:06 +0530

    Added requested changes

--- sympy/printing/octave.py
+++ sympy/printing/octave.py
@@ -436,13 +436,11 @@ class OctaveCodePrinter(CodePrinter):
 
 
     def _nested_binary_math_func(self, expr):
-        args = self._print(expr.args[0])
-        args += ', %s' % self._print(expr.func(*expr.args[1:]))
-
-        return '{name}({args})'.format(
+        return '{name}({arg1}, {arg2})'.format(
             name=self.known_functions[expr.__class__.__name__],
-            args=args
-        )
+            arg1=self._print(expr.args[0]),
+            arg2=self._print(expr.func(*expr.args[1:]))
+            )
 
     _print_Max = _print_Min = _nested_binary_math_func
 
