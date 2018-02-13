2df7bdf764b2f46ad262e382204e70c98975d81d
Björn Dahlgren <bjodah@gmail.com>
2018-02-13 16:06:34 +0100

    Add a py.test blacklist (test_bicycle runs indefinitely)

--- .ci/generate_durations_log.sh
+++ .ci/generate_durations_log.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 ABS_REPO_PATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
-python3 -m pytest --durations 0 >$ABS_REPO_PATH/.ci/durations.log
+python3 -m pytest -ra -k "not test_bicycle" --durations 0 >$ABS_REPO_PATH/.ci/durations.log
+cat <<EOF >${ABS_REPO_PATH}/blacklisted.json
+{
+    "sympy/physics/mechanics/tests/test_kane3.py": [
+        "test_bicycle"
+    ],
+}
+EOF
