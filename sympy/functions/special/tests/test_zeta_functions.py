c0f1746a93fb4f515992b91d59939e829066f808
vishalg2235 <vishalg2235@gmail.com>
2018-02-14 16:02:39 +0530

    test case edited

--- sympy/functions/special/tests/test_zeta_functions.py
+++ sympy/functions/special/tests/test_zeta_functions.py
@@ -228,7 +228,7 @@ def test_issue_10475():
 def test_issue_14177():
     n = Symbol('n', positive=True, integer=True)
 
-    assert zeta(2*n) == 2**(2*n - 1)*pi**(2*n)*Abs(bernoulli(2*n))/factorial(2*n)
+    assert zeta(2*n) == (-1)**(n + 1)*2**(2*n - 1)*pi**(2*n)*bernoulli(2*n)/factorial(2*n)
     assert zeta(-n) == (-1)**(-n)*bernoulli(n + 1)/(n + 1)
 
     n = Symbol('n')
