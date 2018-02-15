dc03581edd3dba546e597e89a2fb29546d4cd9d4
rushyam <rushyamsonu@gmail.com>
2018-02-15 09:27:19 +0530

    add

--- sympy/geometry/tests/test_ellipse.py
+++ sympy/geometry/tests/test_ellipse.py
@@ -1,11 +1,12 @@
 from __future__ import division
 
-from sympy import Dummy, Rational, S, Symbol, pi, sqrt, oo
+from sympy import Dummy, Rational, S, Symbol, symbols, pi, sqrt, oo
 from sympy.core.compatibility import range
 from sympy.geometry import (Circle, Ellipse, GeometryError, Line, Point, Polygon, Ray, RegularPolygon, Segment,
                             Triangle, intersection)
 from sympy.integrals.integrals import Integral
 from sympy.utilities.pytest import raises, slow
+from sympy import integrate
 
 
 @slow
