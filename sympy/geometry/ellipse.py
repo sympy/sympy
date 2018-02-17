0a61cb52f2d5126fc199a07613556e9a258ea017
rushyam <rushyamsonu@gmail.com>
2018-02-17 19:45:30 +0530

    push

--- sympy/geometry/ellipse.py
+++ sympy/geometry/ellipse.py
@@ -1283,8 +1283,8 @@ class Ellipse(GeometrySet):
         =======
 
         I_xx, I_yy, I_xy : number or sympy expression
-                           I_xx, I_yy are second moment of area of an ellise.
-                           I_xy is product moment of area of an ellipse
+            I_xx, I_yy are second moment of area of an ellise.
+            I_xy is product moment of area of an ellipse.
 
         Examples
         ========
