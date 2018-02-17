521484b2bc0f405a384f371ceb5dca6131c0da1b
Yathartha Joshi <yathartha32@gmail.com>
2018-02-17 09:10:21 +0530

    removed import for `expand`

--- sympy/functions/combinatorial/factorials.py
+++ sympy/functions/combinatorial/factorials.py
@@ -862,7 +862,6 @@ class binomial(CombinatorialFunction):
             if n.is_integer and n.is_nonnegative and d.is_negative:
                 return S.Zero
             if n.is_number:
-                from sympy.core.function import expand
                 res = cls._eval(n, k)
                 return res.expand(basic=True) if res else res
         elif n.is_negative and n.is_integer and not k.is_integer:
