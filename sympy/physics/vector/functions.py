086b488ec1de5eb4e2ef2bdd5cf858dab102f113
Rahil Hastu <rahilhastu@gmail.com>
2018-02-19 20:23:18 +0530

    As per asked changes
    These are the changes as asked by @smichr.

--- sympy/physics/vector/functions.py
+++ sympy/physics/vector/functions.py
@@ -275,13 +275,13 @@ def kinematic_equations(speeds, coords, rot_type, rot_order=''):
         if len(coords) != 3:
             raise ValueError('Need 3 coordinates for body or space')
         # Actual hard-coded kinematic differential equations
+        w1, w2, w3 = speeds
+        if w1==w2==w3==0:
+            return [S.Zero]*3
         q1, q2, q3 = coords
         q1d, q2d, q3d = [diff(i, dynamicsymbols._t) for i in coords]
-        w1, w2, w3 = speeds
         s1, s2, s3 = [sin(q1), sin(q2), sin(q3)]
         c1, c2, c3 = [cos(q1), cos(q2), cos(q3)]
-        if w1==w2==w3==0:
-            return [0,0,0]
         if rot_type.lower() == 'body':
             if rot_order == '123':
                 return [q1d - (w1 * c3 - w2 * s3) / c2, q2d - w1 * s3 - w2 *
