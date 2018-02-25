7934ee5fad78f0959d727a716a633b722f5e8e7a
Adwait Baokar <adwaitbaokar18@gmail.com>
2018-02-25 21:30:43 +0530

    Update test_spin.py

--- sympy/physics/quantum/tests/test_spin.py
+++ sympy/physics/quantum/tests/test_spin.py
@@ -149,9 +149,9 @@ def test_represent_uncoupled_states():
         [(-sqrt(2)/2 - sqrt(2)*I/2)*(-sqrt(2)/2 + sqrt(2)*I/2)*(sqrt(2)/2 - sqrt(2)*I/2)**2/2],
         [(-sqrt(2)/2 - sqrt(2)*I/2)*(-sqrt(2)/2 + sqrt(2)*I/2)*(sqrt(2)/2 - sqrt(2)*I/2)**2/2],
         [(-sqrt(2)/2 + sqrt(2)*I/2)**2*(sqrt(2)/2 - sqrt(2)*I/2)**2/2]])
-    assert represent(TensorProduct(JzKet(S(1)/2, S(1)/2), JzKet(S(1)/2, -S(1)/2)), basis=Jy) == \
+    assert (represent(TensorProduct(JzKet(S(1)/2, S(1)/2), JzKet(S(1)/2, -S(1)/2)), basis=Jy)).expand() == \
         Matrix([[-I/2], [S(1)/2], [-S(1)/2], [-I/2]])
-    assert represent(TensorProduct(JzKet(S(1)/2, -S(1)/2), JzKet(S(1)/2, S(1)/2)), basis=Jy) == \
+    assert (represent(TensorProduct(JzKet(S(1)/2, -S(1)/2), JzKet(S(1)/2, S(1)/2)), basis=Jy)).expand == \
         Matrix([[-I/2], [-S(1)/2], [S(1)/2], [-I/2]])
     assert represent(TensorProduct(JzKet(S(1)/2, -S(1)/2), JzKet(S(1)/2, -S(1)/2)), basis=Jy) == \
         Matrix([[(-sqrt(2)/2 - sqrt(2)*I/2)**2*(sqrt(2)/2 + sqrt(2)*I/2)**2/2],
@@ -172,9 +172,9 @@ def test_represent_uncoupled_states():
         [-(-sqrt(2)/2 - sqrt(2)*I/2)*(-sqrt(2)/2 + sqrt(2)*I/2)*(sqrt(2)/2 - sqrt(2)*I/2)**2/2],
         [-(-sqrt(2)/2 - sqrt(2)*I/2)*(-sqrt(2)/2 + sqrt(2)*I/2)*(sqrt(2)/2 - sqrt(2)*I/2)**2/2],
         [(-sqrt(2)/2 + sqrt(2)*I/2)**2*(sqrt(2)/2 - sqrt(2)*I/2)**2/2]])
-    assert represent(TensorProduct(JyKet(S(1)/2, S(1)/2), JyKet(S(1)/2, -S(1)/2)), basis=Jz) == \
+    assert (represent(TensorProduct(JyKet(S(1)/2, S(1)/2), JyKet(S(1)/2, -S(1)/2)), basis=Jz)).expand() == \
         Matrix([[I/2], [S(1)/2], [-S(1)/2], [I/2]])
-    assert represent(TensorProduct(JyKet(S(1)/2, -S(1)/2), JyKet(S(1)/2, S(1)/2)), basis=Jz) == \
+    assert (represent(TensorProduct(JyKet(S(1)/2, -S(1)/2), JyKet(S(1)/2, S(1)/2)), basis=Jz)).expand() == \
         Matrix([[I/2], [-S(1)/2], [S(1)/2], [I/2]])
     assert represent(TensorProduct(JyKet(S(1)/2, -S(1)/2), JyKet(S(1)/2, -S(1)/2)), basis=Jz) == \
         Matrix([[(-sqrt(2)/2 - sqrt(2)*I/2)**2*(sqrt(2)/2 + sqrt(2)*I/2)**2/2],
