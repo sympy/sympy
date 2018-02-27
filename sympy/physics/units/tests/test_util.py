41f74fdff5183e8b7e0f9d9e88538d136aab5e2b
Adwait Baokar <adwaitbaokar18@gmail.com>
2018-02-28 00:54:17 +0530

    Update test_util.py

--- sympy/physics/units/tests/test_util.py
+++ sympy/physics/units/tests/test_util.py
@@ -125,7 +125,7 @@ def test_eval_simplify():
     assert ((km/m).simplify()) == 1000
     assert ((km/cm).simplify()) == 100000
     assert ((10*x*K*km**2/m/cm).simplify()) == 1000000000*x*kelvin
-    assert ((cm/km/m).simplify()) == 1/100000
+    assert ((cm/km/m).simplify()) == 1/(100*kilometer)
 
     q = Quantity("x7", abbrev="xyl")
     raises(AttributeError, lambda: (q/cm).simplify())
