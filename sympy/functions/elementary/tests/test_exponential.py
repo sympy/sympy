6f7637233f2bf2ffa5d7ae8e4c74f14519a57145
Adwait Baokar <adwaitbaokar18@gmail.com>
2018-03-01 19:37:17 +0530

    Update test_exponential.py

--- sympy/functions/elementary/tests/test_exponential.py
+++ sympy/functions/elementary/tests/test_exponential.py
@@ -115,6 +115,9 @@ def test_exp_rewrite():
     assert exp(x).rewrite(tanh) == (1 + tanh(x/2))/(1 - tanh(x/2))
     assert exp(pi*I/4).rewrite(sqrt) == sqrt(2)/2 + sqrt(2)*I/2
     assert exp(pi*I/3).rewrite(sqrt) == 1/2 + sqrt(3)*I/2
+    assert Sum((exp(pi*I/2)/2)**n, (n, 0, oo)).rewrite(sqrt).doit() == 4/5 + 2*I/5
+    assert Sum((exp(pi*I/4)/2)**n, (n, 0, oo)).rewrite(sqrt).doit() == 1/(1 - sqrt(2)*(1 + I)/4)
+    assert Sum((exp(pi*I/3)/2)**n, (n, 0, oo)).rewrite(sqrt).doit() == 1/(3/4 - sqrt(3)*I/4)
 
 
 def test_exp_leading_term():
