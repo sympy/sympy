0df412bd44a565b38a0b933a64983fa79caf803f
Subhash <subhashsaurabh419@gmail.com>
2018-03-06 19:20:14 +0530

    Added simplification of mul having args as units and prefixes
    Mul having Prefixes multiplied with units and other arguments were not being
    simplified correctly. Therefore if in a Mul expression Prefix occured along
    with units then converted the Prefix into its scale_factor.

--- sympy/physics/units/tests/test_util.py
+++ sympy/physics/units/tests/test_util.py
@@ -115,7 +115,7 @@ def test_convert_to_tuples_of_quantities():
 
 
 def test_eval_simplify():
-    from sympy.physics.units import cm, mm, km, m, K, Quantity
+    from sympy.physics.units import cm, mm, km, m, K, Quantity, kilo
     from sympy.simplify.simplify import simplify
     from sympy.core.symbol import symbols
     from sympy.utilities.pytest import raises
@@ -128,3 +128,7 @@ def test_eval_simplify():
     assert ((km/cm).simplify()) == 100000
     assert ((10*x*K*km**2/m/cm).simplify()) == 1000000000*x*kelvin
     assert ((cm/km/m).simplify()) == 1/(100*kilometer)
+
+    assert (3*kilo*meter).simplify() == 3000*meter
+    assert (4*kilo*meter/(2*kilometer)).simplify() == 2
+    assert (4*kilometer**2/(kilo*meter)**2).simplify() == 4
