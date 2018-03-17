154cca60b17f5acb2d8b2f99d1209816cb214528
Adwait Baokar <adwaitbaokar18@gmail.com>
2018-03-18 02:09:18 +0530

    Update conditionset.py

--- sympy/sets/conditionset.py
+++ sympy/sets/conditionset.py
@@ -58,6 +58,7 @@ class ConditionSet(Set):
         return And(Lambda(self.sym, self.condition)(
             other), self.base_set.contains(other))
 
+    @property
     def free_symbols(self):
         a = self.args[1].free_symbols
         b = self.args[2].free_symbols
