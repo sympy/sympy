e38f40340a63d336f615d0dc90d781d8072cf5d4
Chris Smith <smichr@gmail.com>
2018-03-19 16:44:50 -0500

    increase safety of ConditionSet

--- sympy/sets/tests/test_conditionset.py
+++ sympy/sets/tests/test_conditionset.py
@@ -1,7 +1,7 @@
 from sympy.sets import (ConditionSet, Intersection, FiniteSet,
     EmptySet, Union)
 from sympy import (Symbol, Eq, Lt, S, Abs, sin, pi, Lambda, Interval,
-    And, Mod)
+    And, Mod, oo, Function)
 from sympy.utilities.pytest import raises
 
 
@@ -10,6 +10,7 @@ x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
 L = Symbol('lambda')
+f = Function('f')
 
 
 def test_CondSet():
@@ -20,6 +21,16 @@ def test_CondSet():
     assert 3*pi not in sin_sols_principal
     assert 5 in ConditionSet(x, x**2 > 4, S.Reals)
     assert 1 not in ConditionSet(x, x**2 > 4, S.Reals)
+    # in this case, 0 is not part of the base set so
+    # it can't be in any subset selected by the condition
+    assert 0 not in ConditionSet(x, y > 5, Interval(1, 7))
+    # since 'in' requires a true/false, the following raises
+    # an error because the given value provides no information
+    # for the condition to evaluate (since the condition does
+    # not depend on the dummy symbol): the result is `y > 5`.
+    # In this case, ConditionSet is just acting like
+    # Piecewise((Interval(1, 7), y > 5), (S.EmptySet, True)).
+    raises(TypeError, lambda: 6 in ConditionSet(x, y > 5, Interval(1, 7)))
 
     assert isinstance(ConditionSet(x, x < 1, {x, y}).base_set, FiniteSet)
     raises(TypeError, lambda: ConditionSet(x, x + 1, {x, y}))
@@ -84,11 +95,35 @@ def test_subs_CondSet():
     # you can only replace sym with a symbol that is not in
     # the free symbols
     assert c.subs(x, 1) == c
-    assert c.subs(x, y) == c
+    assert c.subs(x, y) == ConditionSet(y, y < 2, s)
+    assert ConditionSet(y, y < 2, s).subs(
+        y, w) == ConditionSet(y, y < 2, {w, z})
     assert c.subs(x, w) == ConditionSet(w, w < 2, s)
     assert ConditionSet(x, x < y, s
         ).subs(y, w) == ConditionSet(x, x < w, s.subs(y, w))
+    # if the user uses assumptions that cause the condition
+    # to evaluate, that can't be helped from SymPy's end
+    n = Symbol('n', negative=True)
+    assert ConditionSet(n, 0 < n, S.Integers) is S.EmptySet
+    p = Symbol('p', positive=True)
+    assert ConditionSet(n, n < y, S.Integers
+        ).subs(n, x) == ConditionSet(x, x < y, S.Integers)
+    nc = Symbol('nc', commutative=False)
+    raises(ValueError, lambda: ConditionSet(
+        x, x < p, S.Integers).subs(x, nc))
+    raises(ValueError, lambda: ConditionSet(
+        x, x < p, S.Integers).subs(x, n))
+    raises(ValueError, lambda: ConditionSet(
+        x + 1, x < 1, S.Integers))
+    assert ConditionSet(
+        n, n < x, Interval(0, oo)).subs(x, p) == Interval(0, oo)
+    assert ConditionSet(
+        n, n < x, Interval(-oo, 0)).subs(x, p) == S.EmptySet
+    assert ConditionSet(f(x), f(x) < 1, {w, z}
+        ).subs(f(x), y) == ConditionSet(y, y < 1, {w, z})
 
+
+def test_subs_CondSet_tebr():
     # to eventually be removed
     c = ConditionSet((x, y), {x + 1, x + y}, S.Reals)
     assert c.subs(x, z) == c
@@ -111,3 +146,18 @@ def test_dummy_eq():
     assert c1.dummy_eq(c3) is False
     assert c.dummy_eq(c1) is False
     assert c1.dummy_eq(c) is False
+
+
+def test_contains():
+    assert 6 in ConditionSet(x, x > 5, Interval(1, 7))
+    assert (8 in ConditionSet(x, y > 5, Interval(1, 7))) is False
+    # `in` should give True or False; in this case there is not
+    # enough information for that result
+    raises(TypeError,
+        lambda: 6 in ConditionSet(x, y > 5, Interval(1, 7)))
+    assert ConditionSet(x, y > 5, Interval(1, 7)
+        ).contains(6) == (y > 5)
+    assert ConditionSet(x, y > 5, Interval(1, 7)
+        ).contains(8) is S.false
+    assert ConditionSet(x, y > 5, Interval(1, 7)
+        ).contains(w) == And(w >= 1, w <= 7, y > 5)
