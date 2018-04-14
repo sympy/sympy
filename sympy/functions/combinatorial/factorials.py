106d6ed8b680b15f87d6130313009896247cc61c
Sidhant Nagpal <sidhantnagpal97@gmail.com>
2018-04-14 15:18:59 +0530

    Handled symbolic base cases in binomial eval

--- sympy/functions/combinatorial/factorials.py
+++ sympy/functions/combinatorial/factorials.py
@@ -828,18 +828,18 @@ class binomial(CombinatorialFunction):
     @classmethod
     def eval(cls, n, k):
         n, k = map(sympify, (n, k))
-        if k.is_zero:
+        d, nonneg = n - k, n.is_nonnegative
+        if k.is_zero or (nonneg and d.is_zero):
             return S.One
-        if (k - 1).is_zero:
+        if (k - 1).is_zero or (nonneg and (d - 1).is_zero):
             return n
         if k.is_integer:
-            if k.is_negative or (n.is_integer and n.is_nonnegative
-                    and (n - k).is_negative):
+            if k.is_negative or (nonneg and n.is_integer and d.is_negative):
                 return S.Zero
             elif n.is_number:
                 res = cls._eval(n, k)
                 return res.expand(basic=True) if res else res
-        elif n.is_negative and n.is_integer:
+        elif nonneg == False and n.is_integer:
             # a special case when binomial evaluates to complex infinity
             return S.ComplexInfinity
         elif k.is_number:
