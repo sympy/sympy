c1040e11c81fa00d03acec8ec483b16e49a7be97
Caley Finn <caleyreuben@gmail.com>
2018-05-04 16:28:32 +1000

    lambdify: added test cases with nested arguments

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -45,6 +45,17 @@ def test_list_args():
     f = lambdify([x, y], x + y)
     assert f(1, 2) == 3
 
+def test_nested_args():
+    f1 = lambdify([[w, x]], [w, x])
+    assert f1([91, 2]) == [91, 2]
+    raises(TypeError, lambda: f1(1, 2))
+
+    f2 = lambdify([(w, x), (y, z)], [w, x, y, z])
+    assert f2((18, 12), (73, 4)) == [18, 12, 73, 4]
+    raises(TypeError, lambda: f2(3, 4))
+
+    f3 = lambdify([w, [[[x]], y], z], [w, x, y, z])
+    assert f3(10, [[[52]], 31], 44) == [10, 52, 31, 44]
 
 def test_str_args():
     f = lambdify('x,y,z', 'z,y,x')
