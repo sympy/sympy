b43dad628620fe61a661341fda62d05023d1c54a
Dmitry Savransky <dsavransky@gmail.com>
2018-05-15 09:35:58 -0400

    adding test for fix for issue #13354

--- sympy/physics/vector/tests/test_printing.py
+++ sympy/physics/vector/tests/test_printing.py
@@ -200,3 +200,25 @@ def test_vlatex(): # vlatex is broken #12078
     expr = J*f(x).diff(x).subs(f(x), g(x)-h(x))
 
     assert vlatex(expr) == expected
+
+
+def test_issue_13354():
+    """ 
+    Test for proper pretty printing of physics vectors with ADD 
+    instances in arguments.
+
+    Test is exactly the one suggested in the original bug report by 
+    @moorepants.
+    """
+
+    a, b, c = symbols('a, b, c')
+    A = ReferenceFrame('A')
+    v = a * A.x + b * A.y + c * A.z
+    w = b * A.x + c * A.y + a * A.z
+    z = w + v
+
+    expected = """(a + b) a_x + (b + c) a_y + (a + c) a_z"""
+
+    assert ascii_vpretty(z) == expected
+
+
