476eb33146cd6928bb1fd86789bfe08faf8d1238
Aaron Meurer <asmeurer@gmail.com>
2018-05-21 11:14:09 -0600

    Make sure the current SymPy is on the PYTHONPATH for the Sage doctests on Travis

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -24,7 +24,7 @@ if [[ "${TEST_SAGE}" == "true" ]]; then
     echo "Testing SAGE"
     sage -v
     sage -python bin/test sympy/external/tests/test_sage.py
-    sage -t sympy/external/tests/test_sage.py
+    PYTHONPATH=. sage -t sympy/external/tests/test_sage.py
 fi
 
 # We change directories to make sure that we test the installed version of
