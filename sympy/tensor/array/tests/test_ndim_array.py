204acd826645a926b90ef9633b25143e9079835d
Yeshwanth N <yeshwanth.nagaraj@aptean.com>
2018-05-27 14:18:20 +0530

    Tests for Issue #14678: Negative indexing for Array is inconsistent.

--- sympy/tensor/array/tests/test_ndim_array.py
+++ sympy/tensor/array/tests/test_ndim_array.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sympy as sp
 
-def array_boundry_checks():
+def test_array_boundry_checks():
 	test_array = sp.Array(np.array([[1,2,3,4,5],[6,7,8,9,10]]))
 	assert test_array[:,-1] == sp.Array([5,10])
 	assert test_array[:,-2] == sp.Array([4,9])
