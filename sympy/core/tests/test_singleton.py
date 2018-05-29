6ecfef01da00f4952405adb575d4416094f1302c
Aaron Meurer <asmeurer@gmail.com>
2018-05-28 23:00:54 -0600

    Add a comment about why the singleton names in namespace test is there

--- sympy/core/tests/test_singleton.py
+++ sympy/core/tests/test_singleton.py
@@ -34,6 +34,25 @@ def test_names_in_namespace():
     # Every singleton name should be accessible from the 'from sympy import *'
     # namespace in addition to the S object. However, it does not need to be
     # by the same name (e.g., oo instead of S.Infinity).
+
+    # As a general rule, things should only be added to the singleton registry
+    # if they are used often enough that code can benefit either from the
+    # performance benefit of being able to use 'is' (this only matters in very
+    # tight loops), or from the memory savings of having exactly one instance
+    # (this matters for the numbers singletons, but very little else). The
+    # singleton registry is already a bit overpopulated, and things cannot be
+    # removed from it without breaking backwards compatibility. So if you got
+    # here by adding something new to the singletons, ask yourself if it
+    # really needs to be singletonized. Note that SymPy classes compare to one
+    # another just fine, so Class() == Class() will give True even if each
+    # Class() returns a new instance. Having unique instances is only
+    # necessary for the above noted performance gains. It should not be needed
+    # for any behavioral purposes.
+
+    # If you determine that something really should be a singleton, it must be
+    # accessible to sympify() without using 'S' (hence this test). Also, its
+    # str printer should print a form that does not use S. This is because
+    # sympify() disables attribute lookups by default for safety purposes.
     d = {}
     exec_('from sympy import *', d)
 
