075cc1145d9370aa66d625e25058dc3f15cbc2fe
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-05-30 14:26:09 +0200

    Update test_ndim_array.py

--- sympy/tensor/array/tests/test_ndim_array.py
+++ sympy/tensor/array/tests/test_ndim_array.py
@@ -1,16 +1,16 @@
-import numpy as np
-import sympy as sp
+from sympy import Array
 
-def test_array_boundry_checks():
-	test_array = sp.Array(np.array([[1,2,3,4,5],[6,7,8,9,10]]))
-	assert test_array[:,-1] == sp.Array([5,10])
-	assert test_array[:,-2] == sp.Array([4,9])
-	assert test_array[:,-3] == sp.Array([3,8])
-	assert test_array[:,-4] == sp.Array([2,7])
-	assert test_array[:,-5] == sp.Array([1,6])
-	assert test_array[:,1] == sp.Array([1,6])
-	assert test_array[:,2] == sp.Array([2,7])
-	assert test_array[:,3] == sp.Array([3,8])
-	assert test_array[:,4] == sp.Array([4,9])
-	assert test_array[:,5] == sp.Array([5,10])
 
+def test_array_negative_indices():
+	test_array = Array([[1,2,3,4,5],
+                        [6,7,8,9,10]])
+	assert test_array[:, -1] == Array([5, 10])
+	assert test_array[:, -2] == Array([4, 9])
+	assert test_array[:, -3] == Array([3, 8])
+	assert test_array[:, -4] == Array([2, 7])
+	assert test_array[:, -5] == Array([1, 6])
+	assert test_array[:, 1] == Array([1, 6])
+	assert test_array[:, 2] == Array([2, 7])
+	assert test_array[:, 3] == Array([3, 8])
+	assert test_array[:, 4] == Array([4, 9])
+	assert test_array[:, 5] == Array([5, 10])
