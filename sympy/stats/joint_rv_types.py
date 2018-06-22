f93ba5d92137b3cc93c0b3ad90023c83d1c9e67e
Akash Vaish <akash.9712@gmail.com>
2018-06-22 13:52:01 +0530

    Added multivariate distribution types.
    Added multivariate Laplace, multivariate T, and normal-gamma
    disributions.

--- sympy/stats/joint_rv_types.py
+++ sympy/stats/joint_rv_types.py
@@ -4,32 +4,20 @@ from sympy.stats.joint_rv import JointDistribution, JointPSpace
 from sympy.matrices.dense import Matrix
 from sympy.matrices.expressions.determinant import det
 
+__all__ = ['MultivariateNormal',
+'MultivariateLaplace',
+'MultivariateT',
+'NormalGamma'
+]
+
 def rv(cls, syms, *args):
     args = list(map(sympify, args))
     dist = cls(*args)
     dist.check(*args)
     return JointPSpace(syms, dist)
 
-
-def MultivariateNormal(syms, mu, sigma):
-    """
-    Creates a joint random variable with multivariate noramal idstribution.
-
-    Parameters:
-    ==========
-
-    syms: list/tuple/set of symbols for identifying each component
-    k: An integer greater than one, number of components
-    mean_vector: A list/tuple/set consisting of k means,represents a k
-        dimensional mean vector
-    covariance_mat: The covariance matrix for the distribution
-
-    Returns:
-    =======
-
-    A random symbol
-    """
-    return rv(MultivariateNormalDistribution, syms, mu, sigma)
+#-------------------------------------------------------------------------------
+# Multivariate Normal distribution ---------------------------------------------------------
 
 class MultivariateNormalDistribution(JointDistribution):
     _argnames = ['mu', 'sigma']
@@ -49,22 +37,190 @@ class MultivariateNormalDistribution(JointDistribution):
             "The covariance matrix must be positive definite. ")
 
     def pdf(self, *args):
-        mu, sigma = Matrix([self.mu]), Matrix(self.sigma)
+        mu, sigma = Matrix(self.mu), Matrix(self.sigma)
         k = len(mu)
-        args = Matrix([args])
+        args = Matrix(args)
         return  S(1)/sqrt((2*pi)**(k)*det(sigma))*exp(
-            -S(1)/2*(mu- args)*(sigma**(-1)*\
-                (mu - args).transpose()))[0]
+            -S(1)/2*(mu - args).transpose()*(sigma**(-1)*\
+                (mu - args)))[0]
 
     def marginal_density(self, indices, *sym):
-        _mu, _sigma = Matrix([self.mu]), Matrix(self.sigma)
-        sym = Matrix([list(sym)])
+        _mu, _sigma = Matrix(self.mu), Matrix(self.sigma)
+        sym = Matrix(list(sym))
         k = len(self.mu)
         for i in range(k):
             if i not in indices:
-                _mu.col_del(i)
+                _mu.row_del(i)
                 _sigma.col_del(i)
                 _sigma.row_del(i)
         return Lambda(sym, S(1)/sqrt((2*pi)**(len(_mu))*det(_sigma))*exp(
-            -S(1)/2*(_mu - sym)*(_sigma**(-1)*\
-                (_mu - sym).transpose()))[0])
+            -S(1)/2*(_mu - sym).transpose()*(_sigma**(-1)*\
+                (_mu - sym)))[0])
+
+
+def MultivariateNormal(syms, mu, sigma):
+    """
+    Creates a joint random variable with multivariate noramal distribution.
+
+    Parameters:
+    ==========
+
+    syms: list/tuple/set of symbols for identifying each component
+    mu: A list/tuple/set consisting of k means,represents a k
+        dimensional location vector
+    covariance_mat: The covariance matrix for the distribution
+
+    Returns:
+    =======
+
+    A random symbol
+    """
+    return rv(MultivariateNormalDistribution, syms, mu, sigma)
+
+#-------------------------------------------------------------------------------
+# Multivariate laplace distribution ---------------------------------------------------------
+
+class MultivariateLaplaceDistribution(JointDistribution):
+
+    _argnames = ['mu', 'sigma']
+    is_Continuous=True
+
+    def set(mu):
+        k = len(mu)
+        return S.Reals**k
+
+    def check(self, mu, sigma):
+        mu, sigma = Matrix([mu]), Matrix(sigma)
+        _value_check(len(mu) == len(sigma.col(0)),
+            "Size of the mean vector and covariance matrix are incorrect.")
+        #check if covariance matrix is positive definite or not.
+        _value_check(all([i > 0 for i in sigma.eigenvals().keys()]),
+            "The covariance matrix must be positive definite. ")
+
+    def pdf(self, *args):
+        from sympy.functions.special.bessel import besselk
+        mu, sigma = Matrix(self.mu), Matrix(self.sigma)
+        k = len(mu)
+        sigma_inv = sigma**(-1)
+        args = Matrix(args)
+        v = 1 - S(k)/2
+        return S(2)/((2*pi)**(S(k)/2)*sqrt(det(sigma))) \
+        *((args.transpose()*sigma_inv*args)[0]/(2 + (mu.transpose()*sigma_inv*mu)[0])) \
+        **(S(v)/2)*besselk(v, sqrt((2 + (mu.transpose()*sigma_inv*mu)[0])
+            *((args.transpose()*sigma_inv*args)[0])))\
+        *exp((args.transpose()*sigma_inv*mu)[0])
+
+def MultivariateLaplace(syms, mu, sigma):
+    """
+    Creates a joint random variable with multivariate laplace distribution.
+
+    Parameters:
+    ==========
+
+    syms: list/tuple/set of symbols for identifying each component
+    mean_vector: A list/tuple/set consisting of k means,represents a k
+        dimensional mean vector
+    covariance_mat: The covariance matrix for the distribution
+
+    Returns:
+    =======
+
+    A random symbol
+    """
+    return rv(MultivariateLaplaceDistribution, syms, mu, sigma)
+
+#-------------------------------------------------------------------------------
+# Multivariate T-distribution ---------------------------------------------------------
+
+class MultivariateTDistribution(JointDistribution):
+
+    _argnames = ['mu', 'shape_mat', 'dof']
+    is_Continuous=True
+
+    def set(mu):
+        k = len(mu)
+        return S.Reals**k
+
+    def check(self, mu, sigma, v):
+        mu, sigma = Matrix([mu]), Matrix(sigma)
+        _value_check(len(mu) == len(sigma.col(0)),
+            "Size of the location vector and shape matrix are incorrect.")
+        #check if covariance matrix is positive definite or not.
+        _value_check(all([i > 0 for i in sigma.eigenvals().keys()]),
+            "The shape matrix must be positive definite. ")
+
+    def pdf(self, *args):
+        from sympy.functions.special.gamma_functions import gamma
+        mu, sigma = Matrix(self.mu), Matrix(self.shape_mat)
+        v = S(self.dof)
+        k = S(len(mu))
+        sigma_inv = sigma**(-1)
+        args = Matrix(args)
+        return gamma((k + v)/2)/(gamma(v/2)*(v*pi)**(k/2)*sqrt(det(sigma)))\
+        *(1 + 1/v*((args - mu).transpose()*sigma_inv*(args - mu))[0])**((-v - k)/2)
+
+def MultivariateT(syms, mu, sigma, v):
+    """
+    Creates a joint random variable with multivariate T-distribution.
+
+    Parameters:
+    ==========
+
+    syms: list/tuple/set of symbols for identifying each component
+    mu: A list/tuple/set consisting of k means,represents a k
+        dimensional location vector
+    sigma: The shape matrix for the distribution
+
+    Returns:
+    =======
+
+    A random symbol
+    """
+    return rv(MultivariateTDistribution, syms, mu, sigma, v)
+
+#-------------------------------------------------------------------------------
+# Multivariate Normal Gamma distribution ---------------------------------------------------------
+
+class NormalGammaDistribution(JointDistribution):
+
+    _argnames = ['mu', 'lamda', 'alpha', 'beta']
+    is_Continuous=True
+
+    def check(self, mu, lamda, alpha, beta):
+        _value_check(mu.is_real, "Location must be real.")
+        _value_check(lamda > 0, "Lambda must be positive")
+        _value_check(alpha > 0, "alpha must be positive")
+        _value_check(beta > 0, "beta must be positive")
+
+    def set(self):
+        return S.Reals**2
+
+    def pdf(self, x, tau):
+        from sympy.functions.special.gamma_functions import gamma
+        beta, alpha, lamda = self.beta, self.alpha, self.lamda
+        mu = self.mu
+
+        return beta**alpha*sqrt(lamda)/(gamma(alpha)*sqrt(2*pi))*\
+        tau**(alpha - S(1)/2)*exp(-1*beta*tau)*\
+        exp(-1*(lamda*tau*(x - mu)**2)/S(2))
+
+def NormalGamma(syms, mu, lamda, alpha, beta):
+    """
+    Creates a bivariate joint random variable with multivariate Normal gamma
+    distribution.
+
+    Parameters:
+    ==========
+
+    syms: list/tuple/set of two symbols for identifying each component
+    mu: A real number, as the mean of the normal distribution
+    alpha: a positive integer
+    beta: a positive integer
+    lamda: a positive integer
+
+    Returns:
+    =======
+
+    A random symbol
+    """
+    return rv(NormalGammaDistribution, syms, mu, lamda, alpha, beta)
--- sympy/stats/tests/test_joint_rv.py
+++ sympy/stats/tests/test_joint_rv.py
@@ -1,14 +1,33 @@
-from sympy import symbols, pi
+from sympy import symbols, pi, oo, sqrt, exp
 from sympy.stats import density
-from sympy.stats.joint_rv_types import MultivariateNormal
 from sympy.stats.joint_rv import marginal_density
 from sympy.utilities.pytest import raises
-x, y, n = symbols('x y z')
+from sympy.integrals.integrals import integrate
+x, y, z, a, b = symbols('x y z a b')
 
-def test_multivariate_normal():
-    m = MultivariateNormal(('m1', 'm2'), [1, 2], [[1, 0], [0, 1]])
+def test_MultivariateNormal():
+    from sympy.stats.joint_rv_types import MultivariateNormal
+    m = MultivariateNormal((x, y), [1, 2], [[1, 0], [0, 1]])
     assert density(m)(1, 2) == 1/(2*pi)
     raises (ValueError,\
         lambda: MultivariateNormal(('m1', 'm2'),[1, 2], [[0, 0], [0, 1]]))
     n = MultivariateNormal(('x', 'y', 'z'), [1, 2, 3], [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     assert marginal_density(n, x, y)(1, 2) == 1/(2*pi)
+    assert integrate(density(m)(x, y), (x, -oo, oo), (y, -oo, oo)).evalf() == 1
+    raises (ValueError, lambda: MultivariateNormal(
+        ('m1', 'm2'), [1, 2], [[1, 1], [1, -1]]))
+
+def test_MultivariateTDist():
+    from sympy.stats.joint_rv_types import MultivariateT
+    t1 = MultivariateT((x, y), [0, 0], [[1, 0], [0, 1]], 2)
+    assert(density(t1))(1, 1) == 1/(8*pi)
+    assert integrate(density(t1)(x, y), (x, -oo, oo), \
+        (y, -oo, oo)).evalf() == 1
+    raises(ValueError, lambda: MultivariateT(
+        ('t1', 't2'), [1, 2], [[1, 1], [1, -1]], 1))
+
+def test_NormalGamma():
+    from sympy.stats.joint_rv_types import NormalGamma
+    ng = NormalGamma(('x', 'y'), 1, 2, 3, 4)
+    assert density(ng)(1, 1) == 32*exp(-4)/sqrt(pi)
+    raises(ValueError, lambda:NormalGamma(('x', 'y'), 1, 2, 3, -1))
