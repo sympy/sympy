5f3068e07ed7e8c00f8a2ed93064757840109ee0
Björn Dahlgren <bjodah@gmail.com>
2018-07-01 13:37:17 +0200

    CI: Require compilers to work in specific Travis runs

--- sympy/utilities/_compilation/availability.py
+++ sympy/utilities/_compilation/availability.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division, print_function)
+import os
 from .compilation import compile_run_strings
 
 def has_fortran():
     if not hasattr(has_fortran, 'result'):
-        try:
-            (stdout, stderr), info = compile_run_strings(
-                [('main.f90', (
-                    'program foo\n'
-                    'print *, "hello world"\n'
-                    'end program'
-                ))], clean=True
-            )
-            assert 'hello world' in stdout
-            assert stderr == ''
-        except:
+        (stdout, stderr), info = compile_run_strings(
+            [('main.f90', (
+                'program foo\n'
+                'print *, "hello world"\n'
+                'end program'
+            ))], clean=True
+        )
+        if info['exit_status'] != os.EX_OK or 'hello world' not in stdout:
+            if os.environ.get('SYMPY_STRICT_COMPILER_CHECKS', '0') == '1':
+                raise ValueError("Failed to compile test program:\n%s\n%s\n" % (stdout, stderr))
             has_fortran.result = False
         else:
             has_fortran.result = True
@@ -23,18 +23,17 @@ def has_fortran():
 
 def has_c():
     if not hasattr(has_c, 'result'):
-        try:
-            (stdout, stderr), info = compile_run_strings(
-                [('main.c', (
-                    '#include <stdio.h>\n'
-                    'int main(){\n'
-                    'printf("hello world\\n");\n'
-                    '}'
-                ))], clean=True
-            )
-            assert 'hello world' in stdout
-            assert stderr == ''
-        except:
+        (stdout, stderr), info = compile_run_strings(
+            [('main.c', (
+                '#include <stdio.h>\n'
+                'int main(){\n'
+                'printf("hello world\\n");\n'
+                '}'
+            ))], clean=True
+        )
+        if info['exit_status'] != os.EX_OK or 'hello world' not in stdout:
+            if os.environ.get('SYMPY_STRICT_COMPILER_CHECKS', '0') == '1':
+                raise ValueError("Failed to compile test program:\n%s\n%s\n" % (stdout, stderr))
             has_c.result = False
         else:
             has_c.result = True
@@ -43,18 +42,17 @@ def has_c():
 
 def has_cxx():
     if not hasattr(has_cxx, 'result'):
-        try:
-            (stdout, stderr), info = compile_run_strings(
-                [('main.cxx', (
-                    '#include <iostream>\n'
-                    'int main(){\n'
-                    'std::cout << "hello world" << std::endl;\n'
-                    '}'
-                ))], clean=True
-            )
-            assert 'hello world' in stdout
-            assert stderr == ''
-        except:
+        (stdout, stderr), info = compile_run_strings(
+            [('main.cxx', (
+                '#include <iostream>\n'
+                'int main(){\n'
+                'std::cout << "hello world" << std::endl;\n'
+                '}'
+            ))], clean=True
+        )
+        if info['exit_status'] != os.EX_OK or 'hello world' not in stdout:
+            if os.environ.get('SYMPY_STRICT_COMPILER_CHECKS', '0') == '1':
+                raise ValueError("Failed to compile test program:\n%s\n%s\n" % (stdout, stderr))
             has_cxx.result = False
         else:
             has_cxx.result = True
