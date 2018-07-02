989634437c9a86e8bd9c2e1e5d3b22baff304cce
Björn Dahlgren <bjodah@gmail.com>
2018-07-03 00:13:08 +0200

    Use py_compile

--- bin/test_syntax.sh
+++ bin/test_syntax.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 REPOROOT=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
 for f in $(find $REPOROOT/sympy -name "*.py"); do
-    output=$(python3 -We:invalid -m compileall -f $f)
+    output=$(python3 -We:invalid -m py_compile $f)
     if [ $? -ne 0 ]; then
-       echo $output
+       >&2 echo $output
        break;
     fi
 done
