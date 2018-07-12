c83da435b04fabcb03e4ae9f43e3e7a992533af4
Kalevi Suominen <jksuom@gmail.com>
2018-07-12 08:35:20 +0300

    Fix integers returned by parametric_log_deriv_heu.
    There is a typo in the pseudocode on p.249 of Bronstein's book.
    The integers M and N are defined twice, in two different ways.
    Only the latter definition agrees with the theory.
    
    The returned values are also expected to be of integer type, not
    polynomials, as they are to be compared with other integers.
    
    The current version of is_log_deriv_k_t_radical_in_field returns
    only two parameters.

--- sympy/integrals/prde.py
+++ sympy/integrals/prde.py
@@ -858,8 +858,7 @@ def parametric_log_deriv_heu(fa, fd, wa, wd, DE, c1=None):
             # deg(q) > B, no solution for c.
             return None
 
-        N, M = s[c1].as_numer_denom()  # N and M are integers
-        N, M = Poly(N, DE.t), Poly(M, DE.t)
+        M, N = s[c1].as_numer_denom()
 
         nfmwa = N*fa*wd - M*wa*fd
         nfmwd = fd*wd
@@ -869,9 +868,7 @@ def parametric_log_deriv_heu(fa, fd, wa, wd, DE, c1=None):
             # (N*f - M*w) is not the logarithmic derivative of a k(t)-radical.
             return None
 
-        Q, e, v = Qv
-        if e != 1:
-            return None
+        Q, v = Qv
 
         if Q.is_zero or v.is_zero:
             return None
