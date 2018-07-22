15da8ec1d8918b4295bc0806a8e6e1aed6ab19c2
ramvenkat98 <ramvenkat98@gmail.com>
2018-07-22 02:12:01 -0400

    rising and falling factorials: fixed numerical evaluation
    Fixed issue 14822 where RisingFactorial and FallingFactorial could not do numerical evaluations by adding an _eval_evalf method to the RisingFactorial and FallingFactorial classes in sympy/functions/combinatorial/factorials.py. Added test cases in test_comb_factorials.py to test this.

--- sympy/functions/combinatorial/tests/test_comb_factorials.py
+++ sympy/functions/combinatorial/tests/test_comb_factorials.py
@@ -61,6 +61,16 @@ def test_rf_eval_apply():
     assert rf(n, k).rewrite(factorial) == \
         factorial(n + k - 1) / factorial(n - 1)
 
+    assert str(rf(18, x).subs({x: S(2)/3}).evalf()) == \
+        '6.82615401311255'
+
+    import random
+    from mpmath import rf as mpmath_rf
+    for i in range(100):
+        x = -500 + 500 * random.random()
+        k = -500 + 500 * random.random()
+        assert (abs(mpmath_rf(x, k) - rf(x, k)) < 10**(-15))
+
 
 def test_ff_eval_apply():
     x, y = symbols('x,y')
@@ -119,6 +129,15 @@ def test_ff_eval_apply():
     assert ff(n, k).rewrite(factorial) == factorial(n) / factorial(n - k)
     assert ff(x, k).rewrite(binomial) == factorial(k) * binomial(x, k)
 
+    assert str(ff(18, x).subs({x: S(2)/3}).evalf()) == \
+        '6.91094012922343'
+
+    import random
+    from mpmath import ff as mpmath_ff
+    for i in range(100):
+        x = -500 + 500 * random.random()
+        k = -500 + 500 * random.random()
+        assert (abs(mpmath_ff(x, k) - ff(x, k)) < 10**(-15))
 
 def test_factorial():
     x = Symbol('x')
