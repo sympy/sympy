521df2da226ec3b258c621d7fd762c7972500594
Akash Vaish <akash.9712@gmail.com>
2018-07-25 16:18:23 +0530

    Modifications in tests in stats
    Modified some tests, and added `evaluate` flag for marginal
    distributions.

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1052,6 +1052,13 @@ def test_sympy__stats__joint_rv__JointDistributionHandmade():
     x1, x2 = (Indexed('x', i) for i in (1, 2))
     assert _test_args(JointDistributionHandmade(x1 + x2, S.Reals**2))
 
+
+def test_sympy__stats__joint_rv__MarginalDistribution():
+    from sympy.stats.rv import RandomSymbol
+    from sympy.stats.joint_rv import MarginalDistribution
+    r = RandomSymbol(S('r'))
+    assert _test_args(MarginalDistribution(r, (r,)))
+
 @SKIP("abstract class")
 def test_sympy__stats__drv__SingleDiscreteDistribution():
     pass
