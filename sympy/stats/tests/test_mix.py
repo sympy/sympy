7da1ad23ccec718093ed1447c597ca897ba5c99b
Akash Vaish <akash.9712@gmail.com>
2018-07-25 16:44:29 +0530

    Added test for density of compound distribution

--- sympy/stats/tests/test_mix.py
+++ sympy/stats/tests/test_mix.py
@@ -1,6 +1,7 @@
-from sympy.stats import Poisson, Beta, Exponential, P
+from sympy.stats import Poisson, Beta, Exponential, P, simplify, sqrt, exp, pi
 from sympy.stats.rv import pspace, density
 from sympy.stats.drv_types import PoissonDistribution
+from sympy.stats.crv import Normal
 from sympy.stats.joint_rv import JointPSpace
 from sympy import Symbol, Eq, Ne
 
@@ -11,6 +12,10 @@ def test_density():
     X = Poisson(x, rate)
     assert isinstance(pspace(X), JointPSpace)
     assert density(X, Eq(rate, rate.symbol)) == PoissonDistribution(l)
+    N1 = Normal('N1', 0, 1)
+    N2 = Normal('N2', N1, 2)
+    assert simplify(density(N2, Eq(N1, 1))(x)) == \
+        sqrt(2)*exp(-(x - 1)**2/8)/(4*sqrt(pi))
 
 def test_mix_expression():
     Y, E = Poisson('Y', 1), Exponential('E', 1)
