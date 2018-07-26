37cd193190acf719514b71571a74af21cc736c21
jashan498 <jashansingh.4398@gmail.com>
2018-07-26 20:19:27 +0530

    Minor changes in the docstring

--- sympy/physics/continuum_mechanics/beam.py
+++ sympy/physics/continuum_mechanics/beam.py
@@ -1150,9 +1150,7 @@ class Beam(object):
         >>> b.solve_for_reaction_loads(R1, R2)
         >>> b.plot_shear_force()
         Plot object containing:
-            [0]: cartesian line: -13750*SingularityFunction(x, 0, 0) + 5000*SingularityFunction(x, 2, 0)
-            + 10000*SingularityFunction(x, 4, 1) - 31250*SingularityFunction(x, 8, 0)
-            - 10000*SingularityFunction(x, 8, 1) for x over (0.0, 8.0)
+        [0]: cartesian line: -13750*SingularityFunction(x, 0, 0) + 5000*SingularityFunction(x, 2, 0) + 10000*SingularityFunction(x, 4, 1) - 31250*SingularityFunction(x, 8, 0) - 10000*SingularityFunction(x, 8, 1) for x over (0.0, 8.0)
         """
         shear_force = self.shear_force()
         for sym in shear_force.atoms(Symbol):
@@ -1200,9 +1198,7 @@ class Beam(object):
         >>> b.solve_for_reaction_loads(R1, R2)
         >>> b.plot_bending_moment()
         Plot object containing:
-            [0]: cartesian line: -13750*SingularityFunction(x, 0, 1) + 5000*SingularityFunction(x, 2, 1)
-            + 5000*SingularityFunction(x, 4, 2) - 31250*SingularityFunction(x, 8, 1)
-            - 5000*SingularityFunction(x, 8, 2) for x over (0.0, 8.0)
+        [0]: cartesian line: -13750*SingularityFunction(x, 0, 1) + 5000*SingularityFunction(x, 2, 1) + 5000*SingularityFunction(x, 4, 2) - 31250*SingularityFunction(x, 8, 1) - 5000*SingularityFunction(x, 8, 2) for x over (0.0, 8.0)
         """
         bending_moment = self.bending_moment()
         for sym in bending_moment.atoms(Symbol):
@@ -1250,9 +1246,7 @@ class Beam(object):
         >>> b.solve_for_reaction_loads(R1, R2)
         >>> b.plot_slope()
         Plot object containing:
-            [0]: cartesian line: -8.59375e-5*SingularityFunction(x, 0, 2) + 3.125e-5*SingularityFunction(x, 2, 2)
-            + 2.08333333333333e-5*SingularityFunction(x, 4, 3) - 0.0001953125*SingularityFunction(x, 8, 2)
-            - 2.08333333333333e-5*SingularityFunction(x, 8, 3) + 0.00138541666666667 for x over (0.0, 8.0)
+        [0]: cartesian line: -8.59375e-5*SingularityFunction(x, 0, 2) + 3.125e-5*SingularityFunction(x, 2, 2) + 2.08333333333333e-5*SingularityFunction(x, 4, 3) - 0.0001953125*SingularityFunction(x, 8, 2) - 2.08333333333333e-5*SingularityFunction(x, 8, 3) + 0.00138541666666667 for x over (0.0, 8.0)
         """
         slope = self.slope()
         for sym in slope.atoms(Symbol):
@@ -1300,10 +1294,7 @@ class Beam(object):
         >>> b.solve_for_reaction_loads(R1, R2)
         >>> b.plot_deflection()
         Plot object containing:
-            [0]: cartesian line: 0.00138541666666667*x - 2.86458333333333e-5*SingularityFunction(x, 0, 3)
-            + 1.04166666666667e-5*SingularityFunction(x, 2, 3) + 5.20833333333333e-6*SingularityFunction(x, 4, 4)
-            - 6.51041666666667e-5*SingularityFunction(x, 8, 3) - 5.20833333333333e-6*SingularityFunction(x, 8, 4)
-            for x over (0.0, 8.0)
+        [0]: cartesian line: 0.00138541666666667*x - 2.86458333333333e-5*SingularityFunction(x, 0, 3) + 1.04166666666667e-5*SingularityFunction(x, 2, 3) + 5.20833333333333e-6*SingularityFunction(x, 4, 4) - 6.51041666666667e-5*SingularityFunction(x, 8, 3) - 5.20833333333333e-6*SingularityFunction(x, 8, 4) for x over (0.0, 8.0)
         """
         deflection = self.deflection()
         for sym in deflection.atoms(Symbol):
