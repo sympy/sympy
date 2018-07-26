7bc15628ab3115c9d146133fb5c00b7a396e5aa5
Akash Vaish <akash.9712@gmail.com>
2018-07-26 12:51:44 +0530

    Corrections in test_mix.py
    Corrections in import statements.

--- sympy/stats/tests/test_mix.py
+++ sympy/stats/tests/test_mix.py
@@ -1,9 +1,9 @@
-from sympy.stats import Poisson, Beta, Exponential, P, simplify, sqrt, exp, pi
+from sympy.stats import Poisson, Beta, Exponential, P
 from sympy.stats.rv import pspace, density
 from sympy.stats.drv_types import PoissonDistribution
-from sympy.stats.crv import Normal
+from sympy.stats.crv_types import Normal
 from sympy.stats.joint_rv import JointPSpace
-from sympy import Symbol, Eq, Ne
+from sympy import Symbol, Eq, Ne, simplify, sqrt, exp, pi
 
 def test_density():
     x = Symbol('x')
@@ -11,11 +11,11 @@ def test_density():
     rate = Beta(l, 2, 3)
     X = Poisson(x, rate)
     assert isinstance(pspace(X), JointPSpace)
-    assert density(X, Eq(rate, rate.symbol)) == PoissonDistribution(l)
     N1 = Normal('N1', 0, 1)
     N2 = Normal('N2', N1, 2)
     assert simplify(density(N2, Eq(N1, 1))(x)) == \
         sqrt(2)*exp(-(x - 1)**2/8)/(4*sqrt(pi))
+    assert density(X, Eq(rate, rate.symbol)) == PoissonDistribution(l)
 
 def test_mix_expression():
     Y, E = Poisson('Y', 1), Exponential('E', 1)
