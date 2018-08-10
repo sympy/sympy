diofant/diofant

Use Function's instead of Symbol's in some places

sympy/solvers/ode.py
@@ -563,7 +563,7 @@ def dsolve(eq, func=None, hint="default", simplify=True,
     >>> dsolve(eq, hint='almost_linear')
     [Eq(f(x), -acos(C1/sqrt(-cos(x)**2)) + 2*pi), Eq(f(x), acos(C1/sqrt(-cos(x)**2)))]
     >>> t = symbols('t')
-    >>> x, y = symbols('x, y', function=True)
+    >>> x, y = symbols('x, y', cls=Function)
     >>> eq = (Eq(Derivative(x(t),t), 12*t*x(t) + 8*y(t)), Eq(Derivative(y(t),t), 21*x(t) + 7*t*y(t)))
     >>> dsolve(eq)
     [Eq(x(t), C1*x0(t) + C2*x0(t)*Integral(8*E**Integral(7*t, t)*E**Integral(12*t, t)/x0(t)**2, t)),
@@ -1320,7 +1320,7 @@ def classify_sysode(eq, funcs=None, **kwargs):
     >>> from sympy import Function, Eq, symbols, diff, Derivative
     >>> from sympy.solvers.ode import classify_sysode
     >>> from sympy.abc import t
-    >>> f, x, y = symbols('f, x, y', function=True)
+    >>> f, x, y = symbols('f, x, y', cls=Function)
     >>> k, l, m, n = symbols('k, l, m, n', Integer=True)
     >>> x1 = diff(x(t), t) ; y1 = diff(y(t), t)
     >>> x2 = diff(x(t), t, t) ; y2 = diff(y(t), t, t)
@@ -1931,11 +1931,11 @@ def checksysodesol(eqs, sols, func=None):
     Examples
     ========
 
-    >>> from sympy import Eq, diff, symbols, sin, cos, exp, sqrt, Rational
+    >>> from sympy import Eq, diff, symbols, sin, cos, exp, sqrt, Rational, Function
     >>> from sympy.solvers.ode import checksysodesol
     >>> C1, C2 = symbols('C1:3')
     >>> t = symbols('t')
-    >>> x, y = symbols('x, y', function=True)
+    >>> x, y = symbols('x, y', cls=Function)
     >>> eq = (Eq(diff(x(t),t), x(t) + y(t) + 17), Eq(diff(y(t),t), -2*x(t) + y(t) + 12))
     >>> sol = [Eq(x(t), (C1*sin(sqrt(2)*t) + C2*cos(sqrt(2)*t))*exp(t) - Rational(5, 3)),
     ... Eq(y(t), (sqrt(2)*C1*cos(sqrt(2)*t) - sqrt(2)*C2*sin(sqrt(2)*t))*exp(t) - Rational(46, 3))]
@@ -5554,8 +5554,10 @@ def infinitesimals(eq, func=None, order=None, hint='default', match=None):
 
     >>> from sympy import Function, diff, exp
     >>> from sympy.solvers.ode import infinitesimals
-    >>> from sympy.abc import x, eta, xi
+    >>> from sympy.abc import x
     >>> f = Function('f')
+    >>> eta = Function('eta')
+    >>> xi = Function('xi')
     >>> eq = f(x).diff(x) - x**2*f(x)
     >>> infinitesimals(eq) == [{eta(x, f(x)): exp(x**3/3), xi(x, f(x)): 0}]
     True
@@ -6705,7 +6707,7 @@ def _linear_2eq_order1_type5(x, y, t, r):
 
     """
     C1, C2, C3, C4 = symbols('C1:5')
-    u, v = symbols('u, v', function=True)
+    u, v = symbols('u, v', cls=Function)
     T = Symbol('T')
     if not cancel(r['c']/r['b']).has(t):
         p = cancel(r['c']/r['b'])
@@ -7495,7 +7497,7 @@ def _linear_2eq_order2_type11(x, y, t, r):
 
     """
     C1, C2, C3, C4 = symbols('C1:5')
-    u, v = symbols('u, v', function=True)
+    u, v = symbols('u, v', cls=Function)
     f = -r['c1']
     g = -r['d1']
     h = -r['c2']
@@ -7712,7 +7714,7 @@ def _linear_3eq_order1_type4(x, y, z, t, r):
     `u, v` and `w` in transformed equation gives value of `x, y` and `z`.
 
     """
-    u, v, w = symbols('u, v, w', function=True)
+    u, v, w = symbols('u, v, w', cls=Function)
     a2, a3 = cancel(r['b1']/r['c1']).as_numer_denom()
     f = cancel(r['b1']/a2)
     b1 = cancel(r['a2']/f)
@@ -7992,16 +7994,17 @@ def _nonlinear_2eq_order1_type3(x, y, t, eq):
 
     """
     C1, C2, C3, C4 = symbols('C1:5')
-    u, v = symbols('u, v', function=True)
+    v = Function('v')
+    u = Symbol('u')
     f = Wild('f')
     g = Wild('g')
     r1 = eq[0].match(diff(x(t), t) - f)
     r2 = eq[1].match(diff(y(t), t) - g)
-    F = r1[f].subs(x(t), u).subs(y(t), v)
-    G = r2[g].subs(x(t), u).subs(y(t), v)
-    sol2r = dsolve(Eq(diff(v(u), u), G.subs(v, v(u))/F.subs(v, v(u))))
+    F = r1[f].subs(x(t), u).subs(y(t), v(u))
+    G = r2[g].subs(x(t), u).subs(y(t), v(u))
+    sol2r = dsolve(Eq(diff(v(u), u), G/F))
     for sol2s in sol2r:
-        sol1 = solve(Integral(1/F.subs(v, sol2s.rhs), u).doit() - t - C2, u)
+        sol1 = solve(Integral(1/F.subs(v(u), sol2s.rhs), u).doit() - t - C2, u)
     sol = []
     for sols in sol1:
         sol.append(Eq(x(t), sols))
@@ -8030,6 +8033,7 @@ def _nonlinear_2eq_order1_type4(x, y, t, eq):
     """
     C1, C2 = symbols('C1:3')
     u, v = symbols('u, v')
+    U, V = symbols('U, V', cls=Function)
     f = Wild('f')
     g = Wild('g')
     f1 = Wild('f1', exclude=[v, t])
@@ -8050,9 +8054,9 @@ def _nonlinear_2eq_order1_type4(x, y, t, eq):
     sol2r = solve(Integral(F2/F1, u).doit() - Integral(G1/G2, v).doit() - C1, v)
     sol = []
     for sols in sol1r:
-        sol.append(Eq(y(t), dsolve(diff(v(t), t) - F2.subs(u, sols).subs(v, v(t))*G2.subs(v, v(t))*phi.subs(u, sols).subs(v, v(t))).rhs))
+        sol.append(Eq(y(t), dsolve(diff(V(t), t) - F2.subs(u, sols).subs(v, V(t))*G2.subs(v, V(t))*phi.subs(u, sols).subs(v, V(t))).rhs))
     for sols in sol2r:
-        sol.append(Eq(x(t), dsolve(diff(u(t), t) - F1.subs(u, u(t))*G1.subs(v, sols).subs(u, u(t))*phi.subs(v, sols).subs(u, u(t))).rhs))
+        sol.append(Eq(x(t), dsolve(diff(U(t), t) - F1.subs(u, U(t))*G1.subs(v, sols).subs(u, U(t))*phi.subs(v, sols).subs(u, U(t))).rhs))
     return set(sol)
 
 

sympy/sympy

sympy/solvers/ode.py
@@ -571,7 +571,7 @@ def dsolve(eq, func=None, hint="default", simplify=True,
     >>> dsolve(eq, hint='almost_linear')
     [Eq(f(x), -acos(C1/cos(x)) + 2*pi), Eq(f(x), acos(C1/cos(x)))]
     >>> t = symbols('t')
-    >>> x, y = symbols('x, y', function=True)
+    >>> x, y = symbols('x, y', cls=Function)
     >>> eq = (Eq(Derivative(x(t),t), 12*t*x(t) + 8*y(t)), Eq(Derivative(y(t),t), 21*x(t) + 7*t*y(t)))
     >>> dsolve(eq)
     [Eq(x(t), C1*x0 + C2*x0*Integral(8*exp(Integral(7*t, t))*exp(Integral(12*t, t))/x0**2, t)),
@@ -1458,7 +1458,7 @@ def classify_sysode(eq, funcs=None, **kwargs):
     >>> from sympy import Function, Eq, symbols, diff
     >>> from sympy.solvers.ode import classify_sysode
     >>> from sympy.abc import t
-    >>> f, x, y = symbols('f, x, y', function=True)
+    >>> f, x, y = symbols('f, x, y', cls=Function)
     >>> k, l, m, n = symbols('k, l, m, n', Integer=True)
     >>> x1 = diff(x(t), t) ; y1 = diff(y(t), t)
     >>> x2 = diff(x(t), t, t) ; y2 = diff(y(t), t, t)
@@ -2050,11 +2050,11 @@ def checksysodesol(eqs, sols, func=None):
     Examples
     ========
 
-    >>> from sympy import Eq, diff, symbols, sin, cos, exp, sqrt, S
+    >>> from sympy import Eq, diff, symbols, sin, cos, exp, sqrt, S, Function
     >>> from sympy.solvers.ode import checksysodesol
     >>> C1, C2 = symbols('C1:3')
     >>> t = symbols('t')
-    >>> x, y = symbols('x, y', function=True)
+    >>> x, y = symbols('x, y', cls=Function)
     >>> eq = (Eq(diff(x(t),t), x(t) + y(t) + 17), Eq(diff(y(t),t), -2*x(t) + y(t) + 12))
     >>> sol = [Eq(x(t), (C1*sin(sqrt(2)*t) + C2*cos(sqrt(2)*t))*exp(t) - S(5)/3),
     ... Eq(y(t), (sqrt(2)*C1*cos(sqrt(2)*t) - sqrt(2)*C2*sin(sqrt(2)*t))*exp(t) - S(46)/3)]
@@ -6812,7 +6812,7 @@ def _linear_2eq_order1_type5(x, y, t, r, eq):
 
     """
     C1, C2, C3, C4 = get_numbered_constants(eq, num=4)
-    u, v = symbols('u, v', function=True)
+    u, v = symbols('u, v', cls=Function)
     T = Symbol('T')
     if not cancel(r['c']/r['b']).has(t):
         p = cancel(r['c']/r['b'])
@@ -7579,7 +7579,7 @@ def _linear_2eq_order2_type11(x, y, t, r, eq):
 
     """
     C1, C2, C3, C4 = get_numbered_constants(eq, num=4)
-    u, v = symbols('u, v', function=True)
+    u, v = symbols('u, v', cls=Function)
     f = -r['c1'] ; g = -r['d1']
     h = -r['c2'] ; p = -r['d2']
     [msol1, msol2] = dsolve([Eq(diff(u(t),t), t*f*u(t) + t*g*v(t)), Eq(diff(v(t),t), t*h*u(t) + t*p*v(t))])
@@ -7769,7 +7769,7 @@ def _linear_3eq_order1_type4(x, y, z, t, r, eq):
     `u, v` and `w` in transformed equation gives value of `x, y` and `z`.
 
     """
-    u, v, w = symbols('u, v, w', function=True)
+    u, v, w = symbols('u, v, w', cls=Function)
     a2, a3 = cancel(r['b1']/r['c1']).as_numer_denom()
     f = cancel(r['b1']/a2)
     b1 = cancel(r['a2']/f); b3 = cancel(r['c2']/f)
@@ -8038,16 +8038,17 @@ def _nonlinear_2eq_order1_type3(x, y, t, eq):
 
     """
     C1, C2, C3, C4 = get_numbered_constants(eq, num=4)
-    u, v = symbols('u, v', function=True)
+    v = Function('v')
+    u = Symbol('u')
     f = Wild('f')
     g = Wild('g')
     r1 = eq[0].match(diff(x(t),t) - f)
     r2 = eq[1].match(diff(y(t),t) - g)
-    F = r1[f].subs(x(t),u).subs(y(t),v)
-    G = r2[g].subs(x(t),u).subs(y(t),v)
-    sol2r = dsolve(Eq(diff(v(u),u), G.subs(v,v(u))/F.subs(v,v(u))))
+    F = r1[f].subs(x(t), u).subs(y(t), v(u))
+    G = r2[g].subs(x(t), u).subs(y(t), v(u))
+    sol2r = dsolve(Eq(diff(v(u), u), G/F))
     for sol2s in sol2r:
-        sol1 = solve(Integral(1/F.subs(v, sol2s.rhs), u).doit() - t - C2, u)
+        sol1 = solve(Integral(1/F.subs(v(u), sol2s.rhs), u).doit() - t - C2, u)
     sol = []
     for sols in sol1:
         sol.append(Eq(x(t), sols))
@@ -8075,6 +8076,7 @@ def _nonlinear_2eq_order1_type4(x, y, t, eq):
     """
     C1, C2 = get_numbered_constants(eq, num=2)
     u, v = symbols('u, v')
+    U, V = symbols('U, V', cls=Function)
     f = Wild('f')
     g = Wild('g')
     f1 = Wild('f1', exclude=[v,t])
@@ -8095,9 +8097,9 @@ def _nonlinear_2eq_order1_type4(x, y, t, eq):
     sol2r = solve(Integral(F2/F1, u).doit() - Integral(G1/G2,v).doit() - C1, v)
     sol = []
     for sols in sol1r:
-        sol.append(Eq(y(t), dsolve(diff(v(t),t) - F2.subs(u,sols).subs(v,v(t))*G2.subs(v,v(t))*phi.subs(u,sols).subs(v,v(t))).rhs))
+        sol.append(Eq(y(t), dsolve(diff(V(t),t) - F2.subs(u,sols).subs(v,V(t))*G2.subs(v,V(t))*phi.subs(u,sols).subs(v,V(t))).rhs))
     for sols in sol2r:
-        sol.append(Eq(x(t), dsolve(diff(u(t),t) - F1.subs(u,u(t))*G1.subs(v,sols).subs(u,u(t))*phi.subs(v,sols).subs(u,u(t))).rhs))
+        sol.append(Eq(x(t), dsolve(diff(U(t),t) - F1.subs(u,U(t))*G1.subs(v,sols).subs(u,U(t))*phi.subs(v,sols).subs(u,U(t))).rhs))
     return set(sol)
 
 def _nonlinear_2eq_order1_type5(func, t, eq):