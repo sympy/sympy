f007b7b78e6ab881a63b8df2fe42bf6c5f4c2209
Colin B. Macdonald <cbm@m.fsf.org>
2018-08-14 21:02:37 -0700

    UndefinedFunctions with _imp_ can evalf
    The lambdify machinary has `implemented_function` to add implementation
    to an UndefinedFunction: not quite sure why we need this but since its
    there let's not break it.  If an UndefinedFunction has an `._imp_`
    method, than we should let it evalf.  But we still shouldn't look it up
    in mpmath so add a test for an UndefinedFunction with a suspicious name
    and a `._imp_`.

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -509,11 +509,12 @@ class Function(Application, Expr):
             return False
 
     def _eval_evalf(self, prec):
-        if isinstance(self.func, UndefinedFunction):
-            return
         # Lookup mpmath function based on name
-        fname = self.func.__name__
         try:
+            if isinstance(self.func, UndefinedFunction):
+                # Shouldn't lookup in mpmath but might have ._imp_
+                raise AttributeError
+            fname = self.func.__name__
             if not hasattr(mpmath, fname):
                 from sympy.utilities.lambdify import MPMATH_TRANSLATIONS
                 fname = MPMATH_TRANSLATIONS[fname]
