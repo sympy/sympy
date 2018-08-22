2cf2358d89f33f6ca433f7461ac0e044d402ed7b
Aaron Meurer <asmeurer@gmail.com>
2018-08-21 19:10:29 -0600

    Fix some test failures from MatrixSymbol.args[0] being Symbol

--- sympy/simplify/tests/test_powsimp.py
+++ sympy/simplify/tests/test_powsimp.py
@@ -223,7 +223,7 @@ def test_issue_9324_powsimp_on_matrix_symbol():
     M = MatrixSymbol('M', 10, 10)
     expr = powsimp(M, deep=True)
     assert expr == M
-    assert expr.args[0] == 'M'
+    assert expr.args[0] == Symbol('M')
 
 
 def test_issue_6367():
