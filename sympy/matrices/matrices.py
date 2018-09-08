cd64737b4c3a945a873caf6925ec94e5add6fde2
S.Y. Lee <sylee957@gmail.com>
2018-09-08 23:46:33 +0900

    Revert "Unmask iszerofunc in det()"
    This reverts commit b90f9ed6efc1bcb625dd03f518d8b69fb3d9ff86.

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -383,7 +383,7 @@ class MatrixDeterminant(MatrixCommon):
         return self._new(self.rows, self.cols,
                          lambda i, j: self.cofactor(i, j, method))
 
-    def det(self, method="bareiss", iszerofunc=_iszero):
+    def det(self, method="bareiss"):
         """Computes the determinant of a matrix.  If the matrix
         is at most 3x3, a hard-coded formula is used.
         Otherwise, the determinant using the method `method`.
@@ -430,7 +430,7 @@ class MatrixDeterminant(MatrixCommon):
         elif method == "berkowitz":
             return self._eval_det_berkowitz()
         elif method == "lu":
-            return self._eval_det_lu(iszerofunc=iszerofunc)
+            return self._eval_det_lu()
 
     def minor(self, i, j, method="berkowitz"):
         """Return the (i,j) minor of `self`.  That is,
