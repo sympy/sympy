d7db778eb9d07ae69bc51293819375694ae26caa
Ayushman Koul <ayushmankoul4570@gmail.com>
2018-09-11 23:19:05 +0530

    Update ruletest8.py

--- sympy/parsing/autolev/test-examples/ruletest8.py
+++ sympy/parsing/autolev/test-examples/ruletest8.py
@@ -26,24 +26,24 @@ body_r.inertia = (_me.inertia(body_r_f, i1, i2, i3, 0, 0, 0), body_r_cm)
 point_o.set_pos(particle_p1.point, c1*frame_a.x)
 point_o.set_pos(particle_p2.point, c2*frame_a.y)
 point_o.set_pos(body_r_cm, c3*frame_a.z)
-a=_me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a)
-a=_me.inertia_of_point_mass(particle_p2.mass, particle_p2.point.pos_from(point_o), frame_a)
-a=body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
-a=_me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a) + _me.inertia_of_point_mass(particle_p2.mass, particle_p2.point.pos_from(point_o), frame_a) + body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
-a=_me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a) + body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
-a=body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
-a=body_r.inertia[0]
+a = _me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a)
+a = _me.inertia_of_point_mass(particle_p2.mass, particle_p2.point.pos_from(point_o), frame_a)
+a = body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
+a = _me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a) + _me.inertia_of_point_mass(particle_p2.mass, particle_p2.point.pos_from(point_o), frame_a) + body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
+a = _me.inertia_of_point_mass(particle_p1.mass, particle_p1.point.pos_from(point_o), frame_a) + body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
+a = body_r.inertia[0] + _me.inertia_of_point_mass(body_r.mass, body_r.masscenter.pos_from(point_o), frame_a)
+a = body_r.inertia[0]
 particle_p2.point.set_pos(particle_p1.point, c1*frame_a.x+c2*frame_a.y)
 body_r_cm.set_pos(particle_p1.point, c3*frame_a.x)
 body_r_cm.set_pos(particle_p2.point, c3*frame_a.y)
-b=_me.functions.center_of_mass(point_o,particle_p1, particle_p2, body_r)
-b=_me.functions.center_of_mass(point_o,particle_p1, body_r)
-b=_me.functions.center_of_mass(particle_p1.point,particle_p1, particle_p2, body_r)
+b = _me.functions.center_of_mass(point_o,particle_p1, particle_p2, body_r)
+b = _me.functions.center_of_mass(point_o,particle_p1, body_r)
+b = _me.functions.center_of_mass(particle_p1.point,particle_p1, particle_p2, body_r)
 u1, u2, u3 = _me.dynamicsymbols('u1 u2 u3')
-v=u1*frame_a.x+u2*frame_a.y+u3*frame_a.z
-u=(v+c1*frame_a.x).normalize()
+v = u1*frame_a.x+u2*frame_a.y+u3*frame_a.z
+u = (v+c1*frame_a.x).normalize()
 particle_p1.point.set_vel(frame_a, u1*frame_a.x)
-a=particle_p1.point.partial_velocity(frame_a, u1)
+a = particle_p1.point.partial_velocity(frame_a, u1)
 m = particle_p1.mass+body_r.mass
 m = particle_p2.mass
 m = particle_p1.mass+particle_p2.mass+body_r.mass
