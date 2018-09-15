e9880d066d2995161aaf054c4219f83af54d4625
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-09-15 18:58:58 +0200

    TensAdd is no longer flattened in TensMul; canonicalization on object construction disabled for TensAdd

--- sympy/tensor/tensor.py
+++ sympy/tensor/tensor.py
@@ -33,15 +33,16 @@ from __future__ import print_function, division
 
 from collections import defaultdict
 import itertools
-from sympy import Matrix, Rational, prod, Integer
+from sympy import Rational, prod, Integer
 from sympy.combinatorics.tensor_can import get_symmetric_group_sgs, \
     bsgs_direct_product, canonicalize, riemann_bsgs
-from sympy.core import Basic, sympify, Add, Mul, S
+from sympy.core import Basic, Expr, sympify, Add, Mul, S
 from sympy.core.compatibility import string_types, reduce, range, SYMPY_INTS
 from sympy.core.containers import Tuple
 from sympy.core.decorators import deprecated
 from sympy.core.symbol import Symbol, symbols
-from sympy.core.sympify import CantSympify
+from sympy.core.sympify import CantSympify, _sympify
+from sympy.core.operations import AssocOp
 from sympy.matrices import eye
 
 
@@ -550,17 +551,16 @@ class TIDS(CantSympify):
         Sign is either 1 or -1, to correct the sign after metric contraction
         (for spinor indices).
         """
-        components = self.components
+        expr = self.expand()
+        components = expr.components
         antisym = g.index_types[0].metric_antisym
-        #if not any(x == g for x in components):
-        #    return self
         # list of positions of the metric ``g``
         gpos = [i for i, x in enumerate(components) if x == g]
         if not gpos:
-            return self, 1
+            return expr, 1
         sign = 1
-        dum = self.dum[:]
-        free = self.free[:]
+        dum = expr.dum[:]
+        free = expr.free[:]
         elim = set()
         for gposx in gpos:
             if gposx in elim:
@@ -1054,7 +1054,8 @@ class _TensorDataLazyEvaluator(CantSympify):
                 srch = (tensmul_args[0].components[0],) + signature
                 if srch in self._substitutions_dict_tensmul:
                     return self._substitutions_dict_tensmul[srch]
-            data_list = [self.data_from_tensor(i) for i in tensmul_args if isinstance(i, TensExpr)]
+            #data_list = [self.data_from_tensor(i) for i in tensmul_args if isinstance(i, TensExpr)]
+            data_list = [self.data_from_tensor(i) if isinstance(i, Tensor) else i.data for i in tensmul_args if isinstance(i, TensExpr)]
             coeff = prod([i for i in tensmul_args if not isinstance(i, TensExpr)])
             if all([i is None for i in data_list]):
                 return None
@@ -2480,7 +2481,7 @@ def _get_argtree_pos(expr, pos):
     return expr
 
 
-class TensExpr(Basic):
+class TensExpr(Expr):
     """
     Abstract base class for tensor expressions
 
@@ -2581,15 +2582,11 @@ class TensExpr(Basic):
         >>> t.fun_eval((i, k),(-j, l))
         A(k, L_0)*B(-L_0, l)
         """
-        index_tuples = dict(index_tuples)
-        indices = self.get_indices()
-        free_ind_set = self._get_free_indices_set()
-        for i, ind in enumerate(indices):
-            if ind in index_tuples and ind in free_ind_set:
-                indices[i] = index_tuples[ind]
-
-        indstruc = _IndexStructure.from_indices(*indices)
-        return self._set_new_index_structure(indstruc)
+        expr = self.xreplace(dict(index_tuples))
+        expr = expr.replace(lambda x: isinstance(x, Tensor), lambda x: x.args[0](*x.args[1]))
+        # For some reason, `TensMul` gets replaced by `Mul`, correct it:
+        expr = expr.replace(lambda x: isinstance(x, (Mul, TensMul)), lambda x: TensMul(*x.args))
+        return expr
 
     def get_matrix(self):
         """
@@ -2633,6 +2630,7 @@ class TensExpr(Basic):
         [14],
         [18]])
         """
+        from sympy import Matrix
         if 0 < self.rank <= 2:
             rows = self.data.shape[0]
             columns = self.data.shape[1] if self.rank == 2 else 1
@@ -2715,7 +2713,7 @@ class TensExpr(Basic):
         return recursor(self, ())
 
 
-class TensAdd(TensExpr, Add):
+class TensAdd(TensExpr, AssocOp):
     """
     Sum of tensors
 
@@ -2784,14 +2782,7 @@ class TensAdd(TensExpr, Add):
         if len(args) == 1:  # and isinstance(args[0], TensMul):
             return args[0]
 
-        # TODO: do not or do canonicalize by default?
-        # Technically, one may wish to have additions of non-canonicalized
-        # tensors. This feature should be removed in the future.
-        # Unfortunately this would require to rewrite a lot of tests.
-        # canonicalize all TensMul
-        args = [canon_bp(x) for x in args if x]
-
-        # After canonicalization, remove zeros:
+        # Remove zeros:
         args = [x for x in args if x]
 
         # if there are no more args (i.e. have cancelled out),
@@ -2886,6 +2877,9 @@ class TensAdd(TensExpr, Add):
             new_args = [scalars] + new_args
         return new_args
 
+    def get_indices(self):
+        return {i for arg in self.args if isinstance(arg, TensExpr) for i in get_indices(arg)}
+
     @property
     def rank(self):
         return self.args[0].rank
@@ -2893,6 +2887,9 @@ class TensAdd(TensExpr, Add):
     @property
     def free_args(self):
         return self.args[0].free_args
+
+    def expand(self, **hints):
+        return TensAdd(*[i.expand() for i in self.args])
 
     def __call__(self, *indices):
         """Returns tensor with ordered free indices replaced by ``indices``
@@ -2915,7 +2912,8 @@ class TensAdd(TensExpr, Add):
         >>> t = p(i0)*p(i1) + g(i0,i1)*q(i2)*q(-i2)
         >>> t(i0,i2)
         metric(i0, i2)*q(L_0)*q(-L_0) + p(i0)*p(i2)
-        >>> t(i0,i1) - t(i1,i0)
+        >>> from sympy.tensor.tensor import canon_bp
+        >>> canon_bp(t(i0,i1) - t(i1,i0))
         0
         """
         free_args = self.free_args
@@ -2934,7 +2932,8 @@ class TensAdd(TensExpr, Add):
         canonicalize using the Butler-Portugal algorithm for canonicalization
         under monoterm symmetries.
         """
-        args = [canon_bp(x) for x in self.args]
+        expr = self.expand()
+        args = [canon_bp(x) for x in expr.args]
         res = TensAdd(*args)
         return res
 
@@ -3036,7 +3035,7 @@ class TensAdd(TensExpr, Add):
         >>> A, B = tensorhead('A,B', [Lorentz]*2, [[1]*2])
         >>> t = A(i, k)*B(-k, -j) + A(i, -j)
         >>> t.fun_eval((i, k),(-j, l))
-        A(k, L_0)*B(l, -L_0) + A(k, l)
+        A(k, L_0)*B(-L_0, l) + A(k, l)
         """
         args = self.args
         args1 = []
@@ -3085,7 +3084,7 @@ class TensAdd(TensExpr, Add):
 
     @property
     def data(self):
-        return _tensor_data_substitution_dict[self]
+        return _tensor_data_substitution_dict[self.expand()]
 
     @data.setter
     def data(self, data):
@@ -3232,8 +3231,9 @@ class Tensor(TensExpr):
     def canon_bp(self):
         if self._is_canon_bp:
             return self
-        g, dummies, msym = self._index_structure.indices_canon_args()
-        v = components_canon_args([self.component])
+        expr = self.expand()
+        g, dummies, msym = expr._index_structure.indices_canon_args()
+        v = components_canon_args([expr.component])
         can = canonicalize(g, dummies, msym, *v)
         if can == 0:
             return S.Zero
@@ -3343,10 +3343,7 @@ class Tensor(TensExpr):
             del _tensor_data_substitution_dict[self.metric]
 
     def __mul__(self, other):
-        if isinstance(other, TensAdd):
-            return TensAdd(*[self*arg for arg in other.args])
-        tmul = TensMul(self, other)
-        return tmul
+        return TensMul(self, other)
 
     def __rmul__(self, other):
         return TensMul(other, self)
@@ -3435,7 +3432,7 @@ class Tensor(TensExpr):
         return self.contract_metric(metric)
 
 
-class TensMul(TensExpr, Mul):
+class TensMul(TensExpr, AssocOp):
     """
     Product of tensors
 
@@ -3474,44 +3471,44 @@ class TensMul(TensExpr, Mul):
     in the ``ipos2`` slot position in the ``icomp2``-th component tensor.
 
     """
+    identity = S.One
 
     def __new__(cls, *args, **kw_args):
-        # make sure everything is sympified:
-        args = [sympify(arg) for arg in args]
-
-        # flatten:
-        args = TensMul._flatten(args)
 
         is_canon_bp = kw_args.get('is_canon_bp', False)
-        args, indices, free, dum = TensMul._tensMul_contract_indices(args)
+        args = list(map(_sympify, args))
 
-        index_types = []
-        for t in args:
-            if not isinstance(t, TensExpr):
-                continue
-            index_types.extend(t.index_types)
-        index_structure = _IndexStructure(free, dum, index_types, indices, canon_bp=is_canon_bp)
+        # Flatten:
+        args = [i for arg in args for i in (arg.args if isinstance(arg, (TensMul, Mul)) else [arg])]
 
-        if any([isinstance(arg, TensAdd) for arg in args]):
-            add_args = TensAdd._tensAdd_flatten(args)
-            return TensAdd(*add_args)
-        coeff = reduce(lambda a, b: a*b, [S.One] + [arg for arg in args if not isinstance(arg, TensExpr)])
+        args = [arg for arg in args if arg != cls.identity]
+
+        # Extract non-tensor coefficients:
+        coeff = reduce(lambda a, b: a*b, [arg for arg in args if not isinstance(arg, TensExpr)], S.One)
         args = [arg for arg in args if isinstance(arg, TensExpr)]
-        TensMul._rebuild_tensors_list(args, index_structure)
 
-        if coeff != 1:
+        if len(args) == 0:
+            return coeff
+
+        if coeff != cls.identity:
             args = [coeff] + args
+
         if len(args) == 1:
             return args[0]
 
-        obj = Basic.__new__(cls, *args)
+        args, indices, free, dum = TensMul._tensMul_contract_indices(args)
+
+        # Data for indices:
+        index_types = [i.tensor_index_type for i in indices]
+        index_structure = _IndexStructure(free, dum, index_types, indices, canon_bp=is_canon_bp)
+
+        obj = TensExpr.__new__(cls, *args)
+
         obj._index_types = index_types
         obj._index_structure = index_structure
         obj._ext_rank = len(obj._index_structure.free) + 2*len(obj._index_structure.dum)
         obj._coeff = coeff
         obj._is_canon_bp = is_canon_bp
-        obj._free_indices_set = set(obj._index_structure.get_free_indices())
-        obj._dummy_indices_set = set(obj._index_structure.dum)
         return obj
 
     @staticmethod
@@ -3600,6 +3597,8 @@ class TensMul(TensExpr, Mul):
         for arg in args:
             if not isinstance(arg, TensExpr):
                 continue
+            if isinstance(arg, TensAdd):
+                continue
             components.extend(arg.components)
         return components
 
@@ -3614,16 +3613,6 @@ class TensMul(TensExpr, Mul):
             prev_pos = ind_pos
             ind_pos += arg.ext_rank
             args[i] = Tensor(arg.component, indices[prev_pos:ind_pos])
-
-    @staticmethod
-    def _flatten(args):
-        a = []
-        for arg in args:
-            if isinstance(arg, TensMul):
-                a.extend(arg.args)
-            else:
-                a.append(arg)
-        return a
 
     # TODO: this method should be private
     # TODO: should this method be renamed _from_components_free_dum ?
@@ -3807,6 +3796,13 @@ class TensMul(TensExpr, Mul):
                 res *= arg
         return splitp
 
+    def expand(self, *args, **hints):
+        # TODO: temporary solution, in the future this should be linked to
+        # `Expr.expand`.
+        args = [arg.expand() for arg in self.args]
+        args1 = [arg.args if isinstance(arg, (Add, TensAdd)) else (arg,) for arg in args]
+        return TensAdd(*[TensMul(*i) for i in itertools.product(*args1)])
+
     def __add__(self, other):
         return TensAdd(self, other)
 
@@ -3839,18 +3835,10 @@ class TensMul(TensExpr, Mul):
         >>> t1*t2
         p(L_0)*q(-L_0)
         """
-        other = sympify(other)
-        if not isinstance(other, TensExpr):
-            return TensMul(*(self.args + (other,)), is_canon_bp=self._is_canon_bp)
-        if isinstance(other, TensAdd):
-            return TensAdd(*[self*x for x in other.args])
-        if isinstance(other, TensMul):
-            return TensMul(*(self.args + other.args))
-        return TensMul(*(self.args + (other,)))
+        return TensMul(self, other)
 
     def __rmul__(self, other):
-        other = sympify(other)
-        return TensMul(*((other,)+self.args), is_canon_bp=self._is_canon_bp)
+        return TensMul(other, self)
 
     def __div__(self, other):
         other = sympify(other)
@@ -3931,9 +3919,12 @@ class TensMul(TensExpr, Mul):
         """
         if self._is_canon_bp:
             return self
-        if not self.components:
-            return self
-        t = self.sorted_components()
+        expr = self.expand()
+        if isinstance(expr, TensAdd):
+            return expr.canon_bp()
+        if not expr.components:
+            return expr
+        t = expr.sorted_components()
         g, dummies, msym = t._index_structure.indices_canon_args()
         v = components_canon_args(t.components)
         can = canonicalize(g, dummies, msym, *v)
@@ -3989,6 +3980,10 @@ class TensMul(TensExpr, Mul):
         >>> t.contract_metric(g).canon_bp()
         p(L_0)*q(-L_0)
         """
+        expr = self.expand()
+        if self != expr:
+            expr = expr.canon_bp()
+            return expr.contract_metric(g)
         pos_map = self._get_indices_to_args_pos()
         args = list(self.args)
 
@@ -4177,24 +4172,9 @@ class TensMul(TensExpr, Mul):
             return t.func(*t.args)
         return t
 
-    def _print(self):
-        args = self.args
-        get_str = lambda arg: str(arg) if arg.is_Atom or isinstance(arg, TensExpr) else ("(%s)" % str(arg))
-
-        if not args:
-            # no arguments is equivalent to "1", i.e. TensMul().
-            # If tensors are constructed correctly, this should never occur.
-            return "1"
-        if self.coeff == S.NegativeOne:
-            # expressions like "-A(a)"
-            return "-"+"*".join([get_str(arg) for arg in args[1:]])
-
-        # prints expressions like "A(a)", "3*A(a)", "(1+x)*A(a)"
-        return "*".join([get_str(arg) for arg in self.args])
-
     @property
     def data(self):
-        dat = _tensor_data_substitution_dict[self]
+        dat = _tensor_data_substitution_dict[self.expand()]
         return dat
 
     @data.setter
@@ -4264,6 +4244,7 @@ def riemann_cyclic(t2):
     >>> riemann_cyclic(t)
     0
     """
+    t2 = t2.expand()
     if isinstance(t2, (TensMul, Tensor)):
         args = [t2]
     else:
