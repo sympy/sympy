66c17dc40d5b062390f7bbb38c0426b38a141d4b
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-09-22 20:03:18 +0200

    added replacement with arrays for PartialDerivative

--- sympy/tensor/operators.py
+++ sympy/tensor/operators.py
@@ -1,4 +1,5 @@
 from sympy.tensor.tensor import (TensExpr, TensMul)
+from sympy import S, diag
 
 
 class PartialDerivative(TensExpr):
@@ -9,6 +10,8 @@ class PartialDerivative(TensExpr):
         if isinstance(expr, PartialDerivative):
             variables = expr.variables + variables
             expr = expr.expr
+
+        # TODO: check that all variables have rank 1.
 
         args, indices, free, dum = TensMul._tensMul_contract_indices([expr] +
             list(variables), replace_indices=True)
@@ -39,3 +42,22 @@ class PartialDerivative(TensExpr):
     @property
     def variables(self):
         return self.args[1:]
+
+    def _extract_data(self, replacement_dict):
+        from .array import derive_by_array, tensorcontraction, tensorproduct
+        indices, array = self.expr._extract_data(replacement_dict)
+        #import pdb; pdb.set_trace()
+        for variable in self.variables:
+            var_indices, var_array = variable._extract_data(replacement_dict)
+            coeff_array, var_array = zip(*[i.as_coeff_Mul() for i in var_array])
+            array = derive_by_array(array, var_array)
+            varindex = var_indices[0]
+            # TODO: find a more efficient way:
+            array = tensorcontraction(tensorproduct(diag(*[S.One/i for i in coeff_array]), array), (1, 2))
+            if -varindex in indices:
+                pos = indices.index(-varindex)
+                array = tensorcontraction(array, (0, pos+1))
+                indices.pop(pos)
+            else:
+                indices.append(varindex)
+        return indices, array
--- sympy/tensor/tests/test_operators.py
+++ sympy/tensor/tests/test_operators.py
@@ -1,16 +1,20 @@
 from sympy.tensor.operators import PartialDerivative
 from sympy.tensor.tensor import TensorIndexType, tensor_indices, tensorhead
+from sympy import symbols, diag
+from sympy import Array
+
+
+L = TensorIndexType("L")
+i, j, k = tensor_indices("i j k", L)
+i0 = tensor_indices("i0", L)
+L_0 = tensor_indices("L_0", L)
+
+A, B, C, D = tensorhead("A B C D", [L], [[1]])
+
+H = tensorhead("H", [L, L], [[1], [1]])
+
 
 def test_tensor_partial_deriv():
-    L = TensorIndexType("L")
-    i, j, k = tensor_indices("i j k", L)
-    i0 = tensor_indices("i0", L)
-    L_0 = tensor_indices("L_0", L)
-
-    A, B, C, D = tensorhead("A B C D", [L], [[1]])
-
-    H = tensorhead("H", [L, L], [[1], [1]])
-
     # Test flatten:
     expr = PartialDerivative(PartialDerivative(A(i), A(j)), A(-i))
     assert expr.expr == A(L_0)
@@ -31,3 +35,18 @@ def test_tensor_partial_deriv():
 
     expr5 = (A(i) + B(i))*PartialDerivative(C(-i), D(j))
     assert expr5.get_indices() == [L_0, -L_0, j]
+
+
+def test_replace_arrays_partial_derivative():
+    x, y, z, t = symbols("x y z t")
+
+    expr = PartialDerivative(A(i), A(j))
+    assert expr.replace_with_arrays({A(i): [x, y]}, [i, j]) == Array([[1, 0], [0, 1]])
+
+    expr = PartialDerivative(A(i), A(-i))
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, []) == 2
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, -1)}, []) == 0
+
+    expr = PartialDerivative(A(-i), A(i))
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, []) == 2
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, -1)}, []) == 0
