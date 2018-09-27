0b52a88123dd5f90fa4e7a32a0e36ccca60cb29c
austin <ap4000@nyu.edu>
2018-09-27 18:36:05 -0400

    formatting clean-up

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -1389,7 +1389,8 @@ def test_issue_12081():
     f = x**(-S(3)/2)*exp(-x)
     assert integrate(f, [x, 0, oo]) == oo
 
+
 def test_issue_15285():
     y = 1/x - 1
-    f = (4 * y * exp(-2 * y)) / (x**2)
-    assert(integrate(f,[x,0,1]) == 1)
+    f = (4*y*exp(-2*y))/(x**2)
+    assert(integrate(f, [x, 0, 1]) == 1)
