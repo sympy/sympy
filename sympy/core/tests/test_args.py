5d927e41a2ad3621351fc56bc61b5477eb27b9fc
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-10-03 18:48:25 +0200

    Added TensorElement to test_args

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -3879,6 +3879,14 @@ def test_sympy__tensor__tensor__TensMul():
     assert _test_args(3*p(a)*q(b))
 
 
+def test_sympy__tensor__tensor__TensorElement():
+    from sympy.tensor.tensor import TensorIndexType, tensorhead, TensorElement
+    L = TensorIndexType("L")
+    A = tensorhead("A", [L, L], [[1], [1]])
+    telem = TensorElement(A(x, y), {x: 1})
+    assert _test_args(telem)
+
+
 def test_as_coeff_add():
     assert (7, (3*x, 4*x**2)) == (7 + 3*x + 4*x**2).as_coeff_add()
 
