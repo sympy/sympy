9036ea7fc4fe4628501dd0e66ae821844e906915
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2018-10-05 19:38:00 +0530

    Fixed a mistake
    Deleted some code by mistake. I've added those lines back.

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -235,6 +235,9 @@ class Plot(object):
         >>> p2 = plot(x)
         >>> p1.append(p2[0])
         >>> p1
+        Plot object containing:
+        [0]: cartesian line: x**2 for x over (-10.0, 10.0)
+        [1]: cartesian line: x for x over (-10.0, 10.0)
 	
         See Also
         ========
