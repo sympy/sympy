ed62d41f0c43d2bfa9661595288b59ae8315bf92
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2018-10-05 18:31:20 +0530

    Added the matplotlib plot directive in extensions

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -23,7 +23,7 @@ sys.path = ['ext'] + sys.path
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax',
-              'numpydoc', 'sympylive', 'sphinx.ext.graphviz', ]
+              'numpydoc', 'sympylive', 'sphinx.ext.graphviz', 'matplotlib.sphinxext.plot_directive']
 
 # Use this to use pngmath instead
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.pngmath', ]
