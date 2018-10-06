2dbd96afa7eb05bf5368301a0091c1bd264b31f4
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2018-10-06 19:46:41 +0530

    No doctests skipped now.

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -228,20 +228,20 @@ class Plot(object):
         ``append`` method, like so:
 
         .. plot::
-           :context:
            :format: doctest
            :include-source: True
 
            >>> from sympy import symbols
            >>> from sympy.plotting import plot
            >>> x = symbols('x')
-           >>> p1 = plot(x*x, show = False)  # doctest: +SKIP
-           >>> p2 = plot(x, show = False)  # doctest: +SKIP
-           >>> p1.append(p2[0])  # doctest: +SKIP
-           >>> p1.show()  # doctest: +SKIP
-           Plot object containing:  # doctest: +SKIP
-           [0]: cartesian line: x**2 for x over (-10.0, 10.0)  # doctest: +SKIP
-           [1]: cartesian line: x for x over (-10.0, 10.0)  # doctest: +SKIP
+           >>> p1 = plot(x*x, show=False)
+           >>> p2 = plot(x, show=False)
+           >>> p1.append(p2[0])
+           >>> p1
+           >>> p1.show()
+           Plot object containing:
+           [0]: cartesian line: x**2 for x over (-10.0, 10.0)
+           [1]: cartesian line: x for x over (-10.0, 10.0)
 
         See Also
         ========
