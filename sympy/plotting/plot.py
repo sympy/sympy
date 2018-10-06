1b68b7c925d478428886ccf96be1f4598549beee
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2018-10-06 20:07:14 +0530

    swapped positions to try to avoid errors

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -238,10 +238,10 @@ class Plot(object):
            >>> p2 = plot(x, show=False)
            >>> p1.append(p2[0])
            >>> p1
-           >>> p1.show()
            Plot object containing:
            [0]: cartesian line: x**2 for x over (-10.0, 10.0)
            [1]: cartesian line: x for x over (-10.0, 10.0)
+           >>> p1.show()
 
         See Also
         ========
