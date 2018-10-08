47e402d1ed9b8ca1fe5a5ac5a55b8ccfb55e7313
S.Y. Lee <sylee957@gmail.com>
2018-10-09 02:35:15 +0900

    Migrated TmpFileManager and cleanup_tmp_files

--- sympy/utilities/tests/test_autowrap.py
+++ sympy/utilities/tests/test_autowrap.py
@@ -17,45 +17,6 @@ from sympy.utilities.codegen import (
     CCodeGen, C99CodeGen, CodeGenArgumentListError, make_routine
 )
 
-class TmpFileManager:
-    """
-    A class to track record of every temporary files created by the tests.
-    """
-    tmp_files = set('')
-    tmp_folders = set('')
-
-    @classmethod
-    def tmp_file(cls, name=''):
-        cls.tmp_files.add(name)
-        return name
-
-    @classmethod
-    def tmp_folder(cls, name=''):
-        cls.tmp_folders.add(name)
-        return name
-
-    @classmethod
-    def cleanup(cls):
-        while cls.tmp_files:
-            file = cls.tmp_files.pop()
-            if os.path.isfile(file):
-                os.remove(file)
-        while cls.tmp_folders:
-            folder = cls.tmp_folders.pop()
-            shutil.rmtree(folder)
-
-def cleanup_tmp_files(test_func):
-    """
-    A decorator to help test codes remove temporary files after the tests.
-    """
-    def wrapper_function():
-        try:
-            test_func()
-        finally:
-            TmpFileManager.cleanup()
-
-    return wrapper_function
-
 
 def get_string(dump_fn, routines, prefix="file", **kwargs):
     """Wrapper for dump_fn. dump_fn writes its results to a stream object and
