eb22f4bf23d1e36f9035261d27403969a5892b5c
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-10-26 17:32:18 +0200

    support for sorting of contracted arrays

--- sympy/codegen/array_utils.py
+++ sympy/codegen/array_utils.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from sympy.core.basic import Basic
 from sympy.core.sympify import _sympify
 from sympy.core.mul import Mul
+from sympy.core.compatibility import accumulate, default_sort_key
 from sympy.matrices.expressions import MatMul
 from sympy.matrices.expressions.matexpr import MatrixExpr
 from sympy.tensor.array import NDimArray
@@ -12,10 +13,39 @@ class CodegenArrayContraction(Basic):
     This class is meant to represent contractions of arrays in a form easily
     processable by the code printers.
     """
-    def __new__(cls, expr, *indices):
-        indices = [_sympify(ind) for ind in indices]
+    def __new__(cls, expr, *contraction_indices):
+        contraction_indices = [_sympify(ind) for ind in contraction_indices]
         expr = _sympify(expr)
-        return Basic.__new__(cls, expr, *indices)
+        obj = Basic.__new__(cls, expr, *contraction_indices)
+        obj._mapping = cls._get_mapping_from_contraction_indices(expr, *contraction_indices)
+        return obj
+
+    @staticmethod
+    def _get_mapping_from_contraction_indices(expr, *contraction_indices):
+        if not isinstance(expr, CodegenArrayTensorProduct):
+            return {}
+        args = expr.args
+        ranks = expr.ranks
+        mapping = {}
+        counter = 0
+        for i, rank in enumerate(ranks):
+            for j in range(rank):
+                mapping[counter] = (i, j)
+                counter += 1
+        return mapping
+
+    def _contraction_indices_to_contraction_tuples(self):
+        mapping = self._mapping
+        if not mapping:
+            raise NotImplementedError
+        return [[mapping[j] for j in i] for i in self.contraction_indices]
+
+    @staticmethod
+    def _contraction_tuple_to_contraction_indices(expr, contraction_tuples):
+        # TODO: check that `expr` has `.ranks`:
+        ranks = expr.ranks
+        cumulative_ranks = [0] + list(accumulate(ranks))
+        return [tuple(cumulative_ranks[j]+k for j, k in i) for i in contraction_tuples]
 
     @property
     def expr(self):
@@ -24,6 +54,38 @@ class CodegenArrayContraction(Basic):
     @property
     def contraction_indices(self):
         return self.args[1:]
+
+    def _contraction_indices_to_components(self):
+        expr = self.expr
+        if not isinstance(expr, CodegenArrayTensorProduct):
+            raise NotImplementedError("only for contractions of tensor products")
+        contraction_indices = self.contraction_indices
+        args = expr.args
+        ranks = expr.ranks
+        mapping = {}
+        counter = 0
+        for i, rank in enumerate(ranks):
+            for j in range(rank):
+                mapping[counter] = (i, j)
+                counter += 1
+        return mapping
+
+    def sort_args_by_name(self):
+        expr = self.expr
+        if not isinstance(expr, CodegenArrayTensorProduct):
+            return self
+        args = expr.args
+        sorted_data = sorted(enumerate(args), key=lambda x: default_sort_key(x[1]))
+        pos_sorted, args_sorted = zip(*sorted_data)
+        reordering_map = {i: pos_sorted.index(i) for i, arg in enumerate(args)}
+        contraction_tuples = self._contraction_indices_to_contraction_tuples()
+        contraction_tuples = [[(reordering_map[j], k) for j, k in i] for i in contraction_tuples]
+        c_tp = CodegenArrayTensorProduct(*args_sorted)
+        new_contr_indices = self._contraction_tuple_to_contraction_indices(
+                c_tp,
+                contraction_tuples
+        )
+        return CodegenArrayContraction(c_tp, *new_contr_indices)
 
     @staticmethod
     def from_summation(summation):
