7138cf904f6ddef930bf0ca2fb6ea53bfb42f609
czgdp1807 <czgdp1807@gmail.com>
2018-11-15 23:22:43 +0530

    sympy.simplify.radsimp.collect rectified to give correct results
    collect method has been modified by adding a condition for expressions
    which have is_Add = True. A test has also been added in
    sympy.simplify.tests.test_radsimp

--- sympy/simplify/tests/test_radsimp.py
+++ sympy/simplify/tests/test_radsimp.py
@@ -227,7 +227,7 @@ def test_collect_D():
     x, a, b = symbols('x,a,b')
     fx = D(f(x), x)
     fxx = D(f(x), x, x)
-
+    
     assert collect(a*fx + b*fx, fx) == (a + b)*fx
     assert collect(a*D(fx, x) + b*D(fx, x), fx) == (a + b)*D(fx, x)
     assert collect(a*fxx + b*fxx, fx) == (a + b)*D(fx, x)
@@ -239,6 +239,8 @@ def test_collect_D():
         (x*f(x) + f(x))*D(f(x), x) + f(x)
     assert collect(1/f(x) + 1/f(x)*diff(f(x), x) + x*diff(f(x), x)/f(x), f(x).diff(x), exact=True) == \
         (1/f(x) + x/f(x))*D(f(x), x) + 1/f(x)
+    e = (1 + x*fx + fx)/f(x)
+    assert collect(e.expand(), fx) == fx*(x/f(x) + 1/f(x)) + 1/f(x)
 
 
 def test_collect_func():
@@ -289,13 +291,6 @@ def test_rcollect():
 
 
 @XFAIL
-def test_collect_issues():
-    D = Derivative
-    f = Function('f')
-    e = (1 + x*D(f(x), x) + D(f(x), x))/f(x)
-    assert collect(e.expand(), f(x).diff(x)) != e
-
-
 def test_collect_D_0():
     D = Derivative
     f = Function('f')
