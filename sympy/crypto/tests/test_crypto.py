a7ff3fd3f5e709c5f3bd3d32d85d0e5982a4834e
yogesh1997 <ymishra013@gmail.com>
2018-11-18 19:54:41 +0530

    updated test_crypto.py
    added test for rail fence cipher

--- sympy/crypto/tests/test_crypto.py
+++ sympy/crypto/tests/test_crypto.py
@@ -13,7 +13,7 @@ from sympy.crypto.crypto import (cycle_list,
       encipher_elgamal, decipher_elgamal, dh_private_key, dh_public_key,
       dh_shared_key, decipher_shift, decipher_affine, encipher_bifid,
       decipher_bifid, bifid_square, padded_key, uniq, decipher_gm,
-      encipher_gm, gm_public_key, gm_private_key)
+      encipher_gm, gm_public_key, gm_private_key,encipher_railfence,decipher_railfence)
 from sympy.matrices import Matrix
 from sympy.ntheory import isprime, is_primitive_root
 from sympy.polys.domains import FF
@@ -21,6 +21,14 @@ from sympy.polys.domains import FF
 from sympy.utilities.pytest import raises, slow
 
 from random import randrange
+
+def test_encipher_railfence():
+    assert encipher_railfence("hello world",2) == "hlowrdel ol"
+    assert encipher_railfence("hello world",3) == "horel ollwd"
+    
+def test_decipher_railfence():
+    assert decipher_railfence("hlowrdel ol",2) == "hello world"
+    assert decipher_railfence("horel ollwd",3) == "hello world"
 
 
 def test_cycle_list():
