47b9f94949090b72057ef0d3c70b6bb3f7276be4
Avi Shrivastava <shrivastavaavi123@gmail.com>
2018-11-22 13:58:45 +0530

    shortening import lines

--- sympy/solvers/tests/test_ode.py
+++ sympy/solvers/tests/test_ode.py
@@ -5,9 +5,10 @@ from sympy import (acos, acosh, asinh, atan, cos, Derivative, diff, dsolve,
     Dummy, Eq, Ne, erf, erfi, exp, Function, I, Integral, LambertW, log, O, pi,
     Rational, rootof, S, simplify, sin, sqrt, Subs, Symbol, tan, asin, sinh,
     Piecewise, symbols, Poly, sec, Ei)
-from sympy.solvers.ode import (_undetermined_coefficients_match, checkodesol,
-    classify_ode, classify_sysode, constant_renumber, constantsimp, homogeneous_order,
-    infinitesimals, checkinfsol, checksysodesol, solve_ics, dsolve, get_numbered_constants)
+from sympy.solvers.ode import (_undetermined_coefficients_match,
+    checkodesol, classify_ode, classify_sysode, constant_renumber,
+    constantsimp, homogeneous_order, infinitesimals, checkinfsol,
+    checksysodesol, solve_ics, dsolve, get_numbered_constants)
 from sympy.solvers.deutils import ode_order
 from sympy.utilities.pytest import XFAIL, skip, raises, slow, ON_TRAVIS
 
