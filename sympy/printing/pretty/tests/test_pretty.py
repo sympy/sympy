2f0f34f721689c3eb22d3ea9f78f51214096763e
RituRajSingh878 <RituRajSingh878@gmail.com>
2018-12-23 23:54:04 +0530

    update pretty printer for physics vector

--- sympy/printing/pretty/tests/test_pretty.py
+++ sympy/printing/pretty/tests/test_pretty.py
@@ -6483,3 +6483,11 @@ def test_issue_15560():
     e = pretty(a*(KroneckerProduct(a, a)))
     result = 'a*(a x a)'
     assert e == result
+
+def test_issue_15583():
+    from  sympy.physics import mechanics
+
+    N = mechanics.ReferenceFrame('N')
+    result = '(n_x, n_y, n_z)'
+    e = pretty((N.x, N.y, N.z))
+    assert e == result
