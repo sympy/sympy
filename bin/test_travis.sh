46fdeb740faab009c31864ae835bcc5c865307f3
S.Y. Lee <sylee957@gmail.com>
2018-12-31 22:04:20 +0900

    Add missing comma

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -133,7 +133,7 @@ test_list = [
     # codegen
     'sympy/codegen/',
     'sympy/utilities/tests/test_codegen',
-    'sympy/utilities/_compilation/tests/test_compilation'
+    'sympy/utilities/_compilation/tests/test_compilation',
 
     # cloudpickle
     'pickling',
