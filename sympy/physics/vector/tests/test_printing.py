98192c7312fc3bbdff65ba8b52796a8edc6607e5
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2019-01-26 15:47:33 +0100

    Fixed derivatives printing

--- sympy/physics/vector/tests/test_printing.py
+++ sympy/physics/vector/tests/test_printing.py
@@ -220,3 +220,34 @@ def test_issue_13354():
     expected = """(a + b) a_x + (b + c) a_y + (a + c) a_z"""
 
     assert ascii_vpretty(z) == expected
+
+def test_vector_derivative_printing():
+    # First order tested above
+
+    # Second order
+    v = omega.diff().diff() * N.x
+
+    assert v._latex() == r'\ddot{\omega}\mathbf{\hat{n}_x}'
+    assert unicode_vpretty(v) == 'ω̈ n_x'
+    assert ascii_vpretty(v) == 'omëga n_x'
+
+    # Third order
+    v = omega.diff().diff().diff() * N.x
+
+    assert v._latex() == r'\dddot{\omega}\mathbf{\hat{n}_x}'
+    assert unicode_vpretty(v) == 'ω⃛ n_x'
+    assert ascii_vpretty(v) == 'ome⃛ga n_x'
+
+    # Fourth order
+    v = omega.diff().diff().diff().diff() * N.x
+
+    assert v._latex() == r'\ddddot{\omega}\mathbf{\hat{n}_x}'
+    assert unicode_vpretty(v) == 'ω⃜ n_x'
+    assert ascii_vpretty(v) == 'ome⃜ga n_x'
+
+    # Fifth order
+    v = omega.diff().diff().diff().diff().diff() * N.x
+
+    assert v._latex() == r'\frac{d^{5}}{d t^{5}} \omega{\left (t \right )}\mathbf{\hat{n}_x}'
+    assert unicode_vpretty(v) == '  5\n d\n───(ω) n_x\n  5\ndt'
+    assert ascii_vpretty(v) == '  5\n d\n---(omega) n_x\n  5\ndt'
