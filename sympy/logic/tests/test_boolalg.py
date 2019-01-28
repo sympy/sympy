20d27c93108bc73c73238b9d278c6aeede925757
Michael Lange <Michael.Lange@ecmwf.int>
2019-01-28 10:39:34 +0000

    Logic: Add a small test for the existence of `Not.canonical`

--- sympy/logic/tests/test_boolalg.py
+++ sympy/logic/tests/test_boolalg.py
@@ -114,6 +114,9 @@ def test_Not():
     assert Not(0) is true
     assert Not(1) is false
     assert Not(2) is false
+    assert Not(A).canonical == Not(A)
+    e = A > 1
+    assert Not(e).canonical == Not(e)
 
 
 def test_Nand():
