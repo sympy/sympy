645d524e01960dab8137c26f1527f9a722a18fdd
RituRajSingh878 <RituRajSingh878@gmail.com>
2019-01-30 16:52:30 +0530

    edited test

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -1480,4 +1480,4 @@ def test_issue_15494():
     x = symbols('x', real=True, positive=True)
     integrand = (exp(x/2)-2*exp(1.6*x)+exp(x))*exp(x)
     sol = integrate(integrand, x)
-    assert str(sol) == str(0.666666666666667*exp(Rational(3,2)*x) + 0.5*exp(2*x) -  0.769230769230769*exp(2.6*x))
+    assert str(sol) == str(Rational(2, 3)*exp(Rational(3,2)*x) + Rational(1, 2)*exp(2*x) -  0.769230769230769*exp(2.6*x))
