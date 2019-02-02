acf85f60f9fcb37b652fdd69f6833bf42445c0de
jmig5776 <jmig5776@gmail.com>
2019-02-03 01:49:17 +0530

    Update

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -2002,7 +2002,7 @@ def test_issue_15731():
     assert solve((-x)**(-x + 4) - 4) == [2]
     assert solve((x**2 - 6)**(x**2 - 2) - 4) == [-2, 2]
     assert solve((x**2 - 2*x - 1)**(x**2 - 3) - 1/(1 - 2*sqrt(2))) == [sqrt(2)]
-    assert solve(x**(x + 1/2) - 4*sqrt(2)) == [S(2)]
+    assert solve(x**(x + S.Half) - 4*sqrt(2)) == [S(2)]
     assert solve((x**2 + 1)**x - 25) == [2]
     assert solve(x**(2/x) - 2) == [2, 4]
     assert solve((x/2)**(2/x) - sqrt(2)) == [4, 8]
