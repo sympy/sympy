743a053723355794850f16325af2fe10e7064781
Christopher Smith <smichr@gmail.com>
2019-02-02 06:28:36 -0600

    Update test_solvers.py

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -694,7 +694,7 @@ def test_checking():
 def test_issue_4671_4463_4467():
     assert solve((sqrt(x**2 - 1) - 2)) in ([sqrt(5), -sqrt(5)],
                                            [-sqrt(5), sqrt(5)])
-    # This is porbably better than the form below but equivalent:
+    # This is probably better than the form below but equivalent:
     #assert solve((2**exp(y**2/x) + 2)/(x**2 + 15), y) == [-sqrt(x*log(1 + I*pi/log(2)))
     #                                                    , sqrt(x*log(1 + I*pi/log(2)))]
     assert solve((2**exp(y**2/x) + 2)/(x**2 + 15), y) == [
@@ -1998,15 +1998,15 @@ def test_issue_15415():
 def test_issue_15731():
     # f(x)**g(x)=c
     assert solve(Eq((x**2 - 7*x + 11)**(x**2 - 13*x + 42), 1)) == [2, 3, 4, 5, 6, 7]
-    assert solve((x)**(x+4)-4) == [-2]
-    assert solve((-x)**(-x+4)-4) == [2]
-    assert solve((x**2-6)**(x**2-2)-4) == [-2, 2]
-    assert solve((x**2-2*x-1)**(x**2-3) - 1/(1-2*sqrt(2))) == [sqrt(2)]
-    assert solve(x**(x+1/2) - 4*sqrt(2)) == [S(2)]
+    assert solve((x)**(x + 4) - 4) == [-2]
+    assert solve((-x)**(-x + 4) - 4) == [2]
+    assert solve((x**2 - 6)**(x**2 - 2) - 4) == [-2, 2]
+    assert solve((x**2 - 2*x - 1)**(x**2 - 3) - 1/(1 - 2*sqrt(2))) == [sqrt(2)]
+    assert solve(x**(x + 1/2) - 4*sqrt(2)) == [S(2)]
     assert solve((x**2 + 1)**x - 25) == [2]
     assert solve(x**(2/x) - 2) == [2, 4]
     assert solve((x/2)**(2/x) - sqrt(2)) == [4, 8]
-    assert solve(x**(x+S(1)/2)-S(9)/4) == [S(3)/2]
+    assert solve(x**(x + S.Half) - S(9)/4) == [S(3)/2]
     # a**g(x)=c
     assert solve((-sqrt(sqrt(2)))**x - 2) == [4, log(2)/(log(2**(S(1)/4)) + I*pi)]
     assert solve((sqrt(2))**x - sqrt(sqrt(2))) == [S(1)/2]
@@ -2014,8 +2014,8 @@ def test_issue_15731():
             (3*log(2)**2 + 4*pi**2 - 4*I*pi*log(2))/(log(2)**2 + 4*pi**2)]
     assert solve((sqrt(2))**x - 2*(sqrt(2))) == [3]
     assert solve(I**x + 1) == [2]
-    assert solve((1+I)**x - 2*I) == [2]
-    assert solve((sqrt(2)+sqrt(3))**x - (2*sqrt(6)+5)**(S(1)/3)) == [S(2)/3]
+    assert solve((1 + I)**x - 2*I) == [2]
+    assert solve((sqrt(2) + sqrt(3))**x - (2*sqrt(6) + 5)**(S(1)/3)) == [S(2)/3]
     # bases of both sides are equal
     b = Symbol('b')
     assert solve(b**x - b**2, x) == [2]
