510b33892727b497d6f8a5b620f2a628bb505e70
adhoc-king <46354827+adhoc-king@users.noreply.github.com>
2019-02-05 18:48:56 +0530

    Update __init__.py
    Imported proper_divisors and proper_divisor_count

--- sympy/ntheory/__init__.py
+++ sympy/ntheory/__init__.py
@@ -5,9 +5,10 @@ Number theory module (primes, etc)
 from .generate import nextprime, prevprime, prime, primepi, primerange, \
     randprime, Sieve, sieve, primorial, cycle_length, composite, compositepi
 from .primetest import isprime
-from .factor_ import divisors, factorint, multiplicity, perfect_power, \
-    pollard_pm1, pollard_rho, primefactors, totient, trailing, divisor_count, \
-    divisor_sigma, factorrat, reduced_totient, primenu, primeomega
+from .factor_ import divisors, proper_divisors, factorint, multiplicity, \
+    perfect_power, pollard_pm1, pollard_rho, primefactors, totient, trailing, \
+    divisor_count, proper_divisor_count, divisor_sigma, factorrat, \
+    reduced_totient, primenu, primeomega
 from .partitions_ import npartitions
 from .residue_ntheory import is_primitive_root, is_quad_residue, \
     legendre_symbol, jacobi_symbol, n_order, sqrt_mod, quadratic_residues, \
