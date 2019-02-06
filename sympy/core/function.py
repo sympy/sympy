fc829d908f771dc92a105691f7439381b2db6676
jmig5776 <jmig5776@gmail.com>
2019-02-06 18:30:53 +0530

    Fix evalf for exponential function

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -551,6 +551,19 @@ class Function(Application, Expr):
             if any(bad(a) for a in args):
                 raise ValueError  # one or more args failed to compute with significance
         except ValueError:
+            from sympy.functions.elementary.exponential import exp
+            if isinstance(self, exp):
+                try:
+                    args = []
+                    for a in self.args:
+                        newa = a._eval_evalf(prec)
+                        if newa is None:
+                            args.append(a)
+                        else:
+                            args.append(newa)
+                    return self.func(*args)
+                except:
+                    pass
             return
 
         with mpmath.workprec(prec):
