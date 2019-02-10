4b8a3f698f7b8e060a5146b1e687222e5d15ee37
RituRajSingh878 <RituRajSingh878@gmail.com>
2019-02-10 21:25:01 +0530

    added test case

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -1130,6 +1130,16 @@ def test_issue_15827():
     if not numpy:
         skip("numpy not installed")
     A = MatrixSymbol("A", 3, 3)
-    f = lambdify(A, 2*A)
+    k=Symbol("k")
+    f = lambdify(A, (2*k)*A)
+    g = lambdify(A, (2+k)*A)
+    h = lambdify(A, 2*A)
     assert numpy.array_equal(f(numpy.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])), \
+    numpy.array([[2*k, 4*k, 6*k], [2*k, 4*k, 6*k], [2*k, 4*k, 6*k]], dtype=object)
+
+    assert numpy.array_equal(g(numpy.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])), \
+    numpy.array([[k + 2, 2*k + 4, 3*k + 6], [k + 2, 2*k + 4, 3*k + 6], \
+    [k + 2, 2*k + 4, 3*k + 6]], dtype=object)
+
+    assert numpy.array_equal(h(numpy.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])), \
     numpy.array([[2, 4, 6], [2, 4, 6], [2, 4, 6]]))
