8892b2d96763d1fd4c0fac431c600299b5c5de35
Aaron Meurer <asmeurer@gmail.com>
2019-02-15 16:19:50 -0700

    Remove Azure Pipelines
    See the discussion at
    https://groups.google.com/forum/#!msg/sympy/jyYrYz8ys9o/p7IY4MCSAwAJ.
    
    Closes #15857
    Closes #15526
    Closes #15385

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -22,18 +22,10 @@ fi
 
 if [[ "${TEST_SAGE}" == "true" ]]; then
     echo "Testing SAGE"
-    if [[ "${AZURE}" != "true" ]]; then
-        source deactivate
-        source activate sage
-    fi
     sage -v
     sage -python bin/test sympy/external/tests/test_sage.py
     PYTHONPATH=. sage -t sympy/external/tests/test_sage.py
     export MPMATH_NOSAGE=1
-    if [[ "${AZURE}" != "true" ]]; then
-        source deactivate
-        source activate test-environment
-    fi
 fi
 
 if [[ -n "${TEST_OPT_DEPENDENCY}" ]]; then
