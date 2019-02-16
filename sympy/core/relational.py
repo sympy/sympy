5e00b14015a677f6271c6b99c7cc1c7feec42cd0
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2019-02-16 23:45:38 +0100

    Cleanup of debug comments

--- sympy/core/relational.py
+++ sympy/core/relational.py
@@ -206,11 +206,9 @@ class Relational(Boolean, Expr, EvalfMixin):
         """
         args = self.args
         r = self
-#        if not r.rhs.free_symbols:
         if r.rhs.is_number:
             if r.rhs.is_Number and r.lhs.is_Number and r.lhs > r.rhs:
                 r = r.reversed
- #       elif not r.lhs.free_symbols:
         elif r.lhs.is_number:
             r = r.reversed
         elif tuple(ordered(args)) != args:
@@ -219,7 +217,6 @@ class Relational(Boolean, Expr, EvalfMixin):
         # Check if first value has negative sign
         if not isinstance(r.lhs, BooleanAtom) and r.lhs.could_extract_minus_sign():
             r = r.reversedsign
-#        elif (not isinstance(r.rhs, BooleanAtom)) and r.rhs.free_symbols and r.rhs.could_extract_minus_sign():
         elif not isinstance(r.rhs, BooleanAtom) and not r.rhs.is_number and r.rhs.could_extract_minus_sign():
             # Right hand side have a minus, but not lhs.
             # How does the expression with reversed signs behave?
