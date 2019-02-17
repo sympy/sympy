3cd05f4adf98e1a94d504379eaf248028421fac9
NILESH <37436956+nileshpatra@users.noreply.github.com>
2019-02-17 17:07:17 +0530

    Update test_limits.py

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -533,8 +533,10 @@ def test_issue_10102():
     assert limit(fresnels(x), x, -oo) == -S.Half
     assert limit(4*fresnelc(x), x, -oo) == -2   
 
+    
 def test_issue_14377():
     raises(NotImplementedError, lambda: limit(exp(I*x)*sin(pi*x), x, oo))
 
+    
 def test_issue_15984():
     assert limit((-x + log(exp(x) + 1))/x, x, oo) == 0
