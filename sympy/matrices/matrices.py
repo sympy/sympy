73f026512b391b634ae054118edc481a5244a73c
dandiez <47832466+dandiez@users.noreply.github.com>
2019-02-24 16:53:45 +0100

    replaced .shape[1] with .cols
    replaced .shape[1] with .cols as per PR suggestion

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -2939,7 +2939,7 @@ class MatrixBase(MatrixDeprecated,
         from sympy.matrices import Matrix, zeros
 
         aug = self.hstack(self.copy(), B.copy())
-        B_cols = B.shape[1]
+        B_cols = B.cols
         row, col = aug[:, :-B_cols].shape
 
         # solve by reduced row echelon form
@@ -2962,7 +2962,7 @@ class MatrixBase(MatrixDeprecated,
 
         # Get index of free symbols (free parameters)
         free_var_index = permutation[
-                            len(pivots):]  # non-pivots columns are free variables
+                        len(pivots):]  # non-pivots columns are free variables
 
         # Free parameters
         # what are current unnumbered free symbol names?
