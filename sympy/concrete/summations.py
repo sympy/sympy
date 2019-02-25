4a935bcb4b0f7ae573a9b7533f5f7c65519530e9
jmig5776 <jmig5776@gmail.com>
2019-02-25 23:11:04 +0530

    Update

--- sympy/concrete/summations.py
+++ sympy/concrete/summations.py
@@ -1175,7 +1175,7 @@ def eval_sum_hyper(f, i_a_b):
         res1, cond1 = res1
         res2, cond2 = res2
         cond = And(cond1, cond2)
-        if cond == False or cond._eval_as_set() == S.EmptySet:
+        if cond == False or cond.as_set() == S.EmptySet:
             return None
         return Piecewise((res1 + res2, cond), (old_sum, True))
 
