8d7353cb62cd11aa64f866af8afde35cc680351c
Christopher Smith <smichr@gmail.com>
2019-02-28 11:54:06 -0600

    remove import; add docstring to _factor_sum_int

--- sympy/core/exprtools.py
+++ sympy/core/exprtools.py
@@ -1099,7 +1099,37 @@ def gcd_terms(terms, isprimitive=False, clear=True, fraction=True):
 
 
 def _factor_sum_int(expr, **kwargs):
-    from sympy.core.exprtools import factor_terms
+    """Return Sum or Integral object with factors that are not
+    in the wrt variables removed. In cases where there are additive
+    terms in the function of the object that are independent, the
+    object will be separated into two objects.
+
+    Examples
+    ========
+
+    >>> from sympy import Sum, factor_terms
+    >>> from sympy.abc import x, y
+    >>> factor_terms(Sum(x + y, (x, 1, 3)))
+    y*Sum(1, (x, 1, 3)) + Sum(x, (x, 1, 3))
+    >>> factor_terms(Sum(x*y, (x, 1, 3)))
+    y*Sum(x, (x, 1, 3))
+
+    Notes
+    =====
+
+    If a function in the summand or integrand is replaced
+    with a symbol, then this simplification should not be
+    done or else an incorrect result will be obtained when
+    the symbol is replaced with an expression that depends
+    on the variables of summation/integration:
+
+    >>> from sympy import Function
+    >>> eq = Sum(y, (x, 1, 3))
+    >>> factor_terms(eq).subs(y, x).doit()
+    3*x
+    >>> eq.subs(y, x).doit()
+    6
+    """
     result = expr.function
     if result == 0:
         return S.Zero
