f5fb224cee14e56e0d3bd8bfb363e5f6d6b3b421
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-03-03 18:31:50 +0000

    Remove AttributeError from _eval_is_positive

--- sympy/core/expr.py
+++ sympy/core/expr.py
@@ -777,17 +777,16 @@ class Expr(Basic, EvalfMixin):
         if self.is_number:
             if self.is_real is False:
                 return False
-            try:
-                # check to see that we can get a value
-                n2 = self._eval_evalf(2)
-                if n2 is None:
-                    raise AttributeError
-                if n2._prec == 1:  # no significance
-                    raise AttributeError
-                if n2 == S.NaN:
-                    raise AttributeError
-            except (AttributeError, ValueError):
+
+            # check to see that we can get a value
+            n2 = self._eval_evalf(2)
+            if n2 is None:
                 return None
+            if getattr(n2, '_prec', 1) == 1:  # no significance
+                return None
+            if n2 == S.NaN:
+                return None
+
             n, i = self.evalf(2).as_real_imag()
             if not i.is_Number or not n.is_Number:
                 return False
