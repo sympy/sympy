a7c90156b31d19e2e08d9c3d5c7fe2f25086bad1
Shivani Kohli <shivanikohli.09@gmail.com>
2019-03-03 20:01:36 -0600

    Updated test_printing to test first order!

--- sympy/physics/vector/tests/test_printing.py
+++ sympy/physics/vector/tests/test_printing.py
@@ -222,7 +222,10 @@ def test_issue_13354():
     assert ascii_vpretty(z) == expected
 
 def test_vector_derivative_printing():
-    # First order tested above
+    # First order
+    v = omega.diff() * N.x 
+    assert unicode_vpretty(v) == u('ω̇ n_x')
+    assert ascii_vpretty(v) == u("omega'(t) n_x")
 
     # Second order
     v = omega.diff().diff() * N.x
