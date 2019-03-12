b849c4fd9cebcdfec39a7dff50285b9a9dd3a1b3
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-03-13 02:11:18 +0530

    fix module import

--- sympy/geometry/util.py
+++ sympy/geometry/util.py
@@ -68,6 +68,8 @@ def are_coplanar(*e):
     from sympy.geometry.line import LinearEntity3D
     from sympy.geometry.point import Point3D
     from sympy.geometry.plane import Plane
+    from sympy.geometry.entity import GeometryEntity
+
     # XXX update tests for coverage
 
     e = set(e)
