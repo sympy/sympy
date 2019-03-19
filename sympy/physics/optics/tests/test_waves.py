2fabb2f0bcec403d9df11653867023255fec4169
Ritesh Kumar <ritesh99rakesh@gmail.com>
2019-03-19 23:58:55 +0530

    add test for waves.py under optics

--- sympy/physics/optics/tests/test_waves.py
+++ sympy/physics/optics/tests/test_waves.py
@@ -23,20 +23,25 @@ def test_twave():
     assert w1.phase == phi1
     assert w1.wavelength == c/(f*n)
     assert w1.time_period == 1/f
+    assert w1.angular_velocity == 2*pi*f
+    assert w1.wavenumber == 2*pi*f*n/c
+    assert w1.speed == c/n
     w3 = w1 + w2
     assert w3.amplitude == sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2) + A2**2)
     assert w3.frequency == f
+    assert w3.phase == atan2(A1*cos(phi1) + A2*cos(phi2), A1*sin(phi1) + A2*sin(phi2))
     assert w3.wavelength == c/(f*n)
     assert w3.time_period == 1/f
     assert w3.angular_velocity == 2*pi*f
     assert w3.wavenumber == 2*pi*f*n/c
-    assert simplify(w3.rewrite('sin') - sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2)
+    assert w3.speed == c/n
+    assert simplify(w3.rewrite(sin) - sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2)
     + A2**2)*sin(pi*f*n*x*s/(149896229*m) - 2*pi*f*t + atan2(A1*cos(phi1)
     + A2*cos(phi2), A1*sin(phi1) + A2*sin(phi2)) + pi/2)) == 0
     assert w3.rewrite('pde') == epsilon*mu*Derivative(E(x, t), t, t) + Derivative(E(x, t), x, x)
     assert w3.rewrite(cos) == sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2)
     + A2**2)*cos(pi*f*n*x*s/(149896229*m) - 2*pi*f*t + atan2(A1*cos(phi1)
     + A2*cos(phi2), A1*sin(phi1) + A2*sin(phi2)))
-    assert w3.rewrite('exp') == sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2)
+    assert w3.rewrite(exp) == sqrt(A1**2 + 2*A1*A2*cos(phi1 - phi2)
     + A2**2)*exp(I*(pi*f*n*x*s/(149896229*m) - 2*pi*f*t
     + atan2(A1*cos(phi1) + A2*cos(phi2), A1*sin(phi1) + A2*sin(phi2))))
