17a4ee30e2a3d449a2e6ccbfb9600251762465a5
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-03-19 23:45:05 +0530

    added quaternion differentiation, integration and also the test cases for those functions.

--- sympy/algebras/quaternion.py
+++ sympy/algebras/quaternion.py
@@ -697,3 +697,19 @@ class Quaternion(Expr):
 
             return Matrix([[m00, m01, m02, m03], [m10, m11, m12, m13],
                           [m20, m21, m22, m23], [m30, m31, m32, m33]])
+
+    def differentiate(self, vector):
+        """
+        returns the differentiation of quaternion which represents 
+        the rotation of the quaternion at the rate of 3d vector -'vector'.
+    
+        """
+        vec = vector._args
+        quat = Quaternion(0, vec[0], vec[1], vec[2])
+        return 0.5 * self * quat
+
+    def integrate(self, wrt):
+        res = []
+        for term in self.args:
+            res.append(integrate(term, wrt))
+        return Quaternion(res[0], res[1], res[2], res[3])
