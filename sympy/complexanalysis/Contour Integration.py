c74009cd7c86813b0145580908e0f22fca96b9fd
Vishesh Mangla <40588378+XtremeGood@users.noreply.github.com>
2019-03-20 01:47:18 +0530

    Update includes my attempt to show the contour to the user.
    It is working for theta in (0, pi) but giving strange curve for pi/4. Also, it would be good if y-axis has like "7i" instead of "7" without the loss of generality and if that temporary "radius=1" can be replaced by radius=oo . For poles in denominator there's no problem but how do I calculate points where the numerator is oo since solve gives an empty list. One case can be when a number is not a pole due to cancellation which I would find a way but for now please help me.

--- sympy/complexanalysis/Contour Integration.py
+++ sympy/complexanalysis/Contour Integration.py
@@ -1,39 +1,41 @@
+# %load Contour Integration.py
 from sympy import *
 import matplotlib.pyplot as plt
+from numpy import degrees
 
 class ComplexIntegral:
 
-    def __init__(self, eq, params=dict(shape="semi-circle", origin=(0,0), radius=oo, angle=pi)):
+    def __init__(self, eq, shape="semi-circle", params=dict(z0=0+0*I, radius=oo, theta1=0, theta2=pi)):
 
         self.eq = eq
-        self.shape = params["shape"]
-        if "circle" in self.shape:
-            self.solveit = self.solveit_cir()
-            self.origin = params["origin"]
-            self.rad = params["radius"]
-            self.angle = params["angle"]
+        self.shape = shape
 
+        if self.shape == "semi-circle" :
+            self.solveit = self.solveit_semi_cir()
+            self.z0 = params["z0"]
+            self.radius = params["radius"]
+            self.theta1 = params["theta1"]
+            self.theta2 = params["theta2"]
+            
         else:
-            pass 
-            #Enter in anticlockwise direction
+            pass            #Enter in anticlockwise direction
             self.solveit = solveit_oth()
             self.points = points
 
-    def contour(self):
+    def contour_cir(self):
+        self.radius=1
+        plot(im(self.z0) + sqrt(self.radius**2 - (x - re(self.z0))**2),(x, re(self.z0) +  
+                     self.radius * cos(self.theta2), im(self.z0) + self.radius * cos(self.theta1) ),
+                     xlabel="Real", ylabel="Imaginary"  )
+    
 
-        #Plot the contour
-        pass   
+    def solveit_semi_cir(self):
 
-    def residue_calc(self, rooot, mult, x ):
-        
-        return Rational(1/factorial(mult-1)) *limit(diff(self.eq*(x - rooot)**mult, x, mult - 1), x, complex(rooot))
-    def solveit_cir(self):
-
-        eqn_residues = self.eq.as_numer_denom()[1]
-        residues = roots(eqn_residues)
+        eqn_residues = self.eq.as_numer_denom()
+        residues = list(roots(eqn_residues[1]).keys()) + list(roots((eqn_residues[0])**-1).keys())
         sum_residues = 0
         x = list(self.eq.atoms(Symbol))[0]
-        for i, j in residues.items():   
+        for i in residues:   
             if im(i) > 0:    
-                sum_residues += self.residue_calc(i, j, x)
+                sum_residues += residue(self.eq, x, i)
         return simplify(2*pi*I*sum_residues)  
