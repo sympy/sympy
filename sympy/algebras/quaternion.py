54e48390b894f1367caee8c3d6b5952d24c83aeb
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-03-20 01:39:40 +0530

    Update quaternion.py

--- sympy/algebras/quaternion.py
+++ sympy/algebras/quaternion.py
@@ -699,7 +699,7 @@ class Quaternion(Expr):
                           [m20, m21, m22, m23], [m30, m31, m32, m33]])
 
     def differentiate(self, vector):
-        """returns the differentiation of quaternion which represents 
+        """returns the differentiation of quaternion which represents
         the rotation of the quaternion at the rate of 3d vector -'vector'.
         """
         vec = vector._args
