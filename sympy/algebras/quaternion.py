aa835af5a56ceb3bfb7e57fedcee67122c691dc9
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-03-21 02:18:07 +0530

    Update quaternion.py

--- sympy/algebras/quaternion.py
+++ sympy/algebras/quaternion.py
@@ -698,9 +698,8 @@ class Quaternion(Expr):
             return Matrix([[m00, m01, m02, m03], [m10, m11, m12, m13],
                           [m20, m21, m22, m23], [m30, m31, m32, m33]])
 
-    def differentiate(self, vector):
-       """returns the differentiation of quaternion which represents
-        the rotation of the quaternion at the rate of W.
+    def differentiate(self, W):
+       """Returns the instantaneous quaternion derivative representing a quaternion rotating at the rate of W.
 
         Parameters
         ==========
