500a166308ffe8fb06cad9ef5903244543e3adcd
Yathartha Joshi <yathartha32@gmail.com>
2019-03-20 22:03:54 +0530

    used `fraction=False` in `factor()` in bivariate.py

--- sympy/solvers/bivariate.py
+++ sympy/solvers/bivariate.py
@@ -209,7 +209,7 @@ def _solve_lambert(f, symbol, gens):
         lhs = expand_log(log(lhs))
         rhs = log(rhs)
 
-    lhs = factor(lhs)
+    lhs = factor(lhs, deep=True, fraction=False)
     # make sure we have inverted as completely as possible
     r = Dummy()
     i, lhs = _invert(lhs - r, symbol)
