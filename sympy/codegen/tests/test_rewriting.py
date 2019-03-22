06b63e49200994b4e1a49ea5a8b4598ec03eb05a
S.Y. Lee <sylee957@gmail.com>
2019-03-23 00:59:30 +0900

    Delete coding utf8

--- sympy/codegen/tests/test_rewriting.py
+++ sympy/codegen/tests/test_rewriting.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function)
 
 from sympy import log, exp, Symbol, Pow, sin
--- sympy/physics/units/dimensions.py
+++ sympy/physics/units/dimensions.py
@@ -1,5 +1,3 @@
-# -*- coding:utf-8 -*-
-
 """
 Definition of physical dimensions.
 
