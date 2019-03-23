0a09bf9e0403b6640d9a9d49ff77fc72360560bd
Francesco Bonazzi <franz.bonazzi@gmail.com>
2019-03-23 21:53:58 +0100

    fixing matrix derivative bugs

--- sympy/matrices/expressions/tests/test_derivatives.py
+++ sympy/matrices/expressions/tests/test_derivatives.py
@@ -33,7 +33,8 @@ def test_matrix_derivative_non_matrix_result():
 
 def test_matrix_derivative_trivial_cases():
     # Cookbook example 33:
-    assert X.diff(A) == 0
+    # TODO: find a way to represent a four-dimensional zero-array:
+    assert X.diff(A) == Derivative(X, A)
 
 
 def test_matrix_derivative_with_inverse():
@@ -57,6 +58,9 @@ def test_matrix_derivative_with_inverse():
 
 
 def test_matrix_derivative_vectors_and_scalars():
+
+    assert x.diff(x) == Identity(k)
+    assert x.T.diff(x) == Identity(k)
 
     # Cookbook example 69:
     expr = x.T*a
@@ -246,9 +250,23 @@ def test_derivatives_of_complicated_matrix_expr():
 
 def test_mixed_deriv_mixed_expressions():
 
+    expr = 3*Trace(A)
+    assert expr.diff(A) == 3*Identity(k)
+
+    expr = k
+    deriv = expr.diff(A)
+    assert isinstance(deriv, ZeroMatrix)
+    assert deriv == ZeroMatrix(k, k)
+
+    expr = Trace(A)**2
+    assert expr.diff(A) == (2*Trace(A))*Identity(k)
+
     expr = Trace(A)*A
     # TODO: this is not yet supported:
     assert expr.diff(A) == Derivative(expr, A)
 
     expr = Trace(Trace(A)*A)
     assert expr.diff(A) == (2*Trace(A))*Identity(k)
+
+    expr = Trace(Trace(Trace(A)*A)*A)
+    assert expr.diff(A) == (3*Trace(A)**2)*Identity(k)
