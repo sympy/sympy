8af4d834e03dcb94e5fd3bf8b9c1ffd94493524e
S.Y. Lee <sylee957@gmail.com>
2019-03-29 22:41:18 +0900

    Use PRECEDENCE_VALUES

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -18,7 +18,8 @@ from sympy.logic.boolalg import true
 from sympy.printing.precedence import precedence_traditional
 from sympy.printing.printer import Printer
 from sympy.printing.conventions import split_super_sub, requires_partial
-from sympy.printing.precedence import precedence, PRECEDENCE
+from sympy.printing.precedence import (
+    precedence, PRECEDENCE, PRECEDENCE_VALUES)
 
 import mpmath.libmp as mlib
 from mpmath.libmp import prec_to_dps
@@ -1558,40 +1559,38 @@ class LatexPrinter(Printer):
                                  self._print(expr.args[1]))
 
     def _print_HadamardProduct(self, expr):
-        from sympy import Add, MatAdd, MatMul
+        args = expr.args
+        prec = PRECEDENCE_VALUES['HadamardProduct']
+        parens = self.parenthesize
 
-        def parens(x):
-            if isinstance(x, (Add, MatAdd, MatMul)):
-                return r"\left(%s\right)" % self._print(x)
-            return self._print(x)
-        return r' \circ '.join(map(parens, expr.args))
+        return r' \circ '.join(
+            map(lambda arg: parens(arg, prec, strict=True), args))
 
     def _print_HadamardPower(self, expr):
-        from sympy import Add, MatAdd, MatMul
+        base, exp = expr.base, expr.exp
+        prec = PRECEDENCE_VALUES['HadamardPower']
+        parens = self.parenthesize
 
-        def parens(x):
-            if isinstance(x, (Add, MatAdd, MatMul)):
-                return r"\left(%s\right)" % self._print(x)
-            return self._print(x)
-        return r'{}^{{\circ {{{}}}}}'.format(*map(parens, expr.args))
+        return r'{}^{{\circ {{{}}}}}'.format(
+            parens(base, prec, strict=True),
+            parens(exp, prec, strict=True))
 
     def _print_KroneckerProduct(self, expr):
-        from sympy import Add, MatAdd, MatMul
+        args = expr.args
+        prec = PRECEDENCE_VALUES['KroneckerProduct']
+        parens = self.parenthesize
 
-        def parens(x):
-            if isinstance(x, (Add, MatAdd, MatMul)):
-                return r"\left(%s\right)" % self._print(x)
-            return self._print(x)
-        return r' \otimes '.join(map(parens, expr.args))
+        return r' \otimes '.join(
+            map(lambda arg: parens(arg, prec, strict=True), args))
 
     def _print_MatPow(self, expr):
         base, exp = expr.base, expr.exp
-        from sympy.matrices import MatrixSymbol
-        if not isinstance(base, MatrixSymbol):
-            return r"\left(%s\right)^{%s}" % (self._print(base),
-                                              self._print(exp))
-        else:
-            return "%s^{%s}" % (self._print(base), self._print(exp))
+        prec = PRECEDENCE_VALUES['MatPow']
+        parens = self.parenthesize
+
+        return "%s^{%s}" % (
+            parens(base, prec, strict=True),
+            parens(exp, prec, strict=True))
 
     def _print_ZeroMatrix(self, Z):
         return r"\mathbb{0}"
