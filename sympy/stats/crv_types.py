96f5b4bc826f547d95f7836fce3f2d620354baf1
czgdp1807 <singh.23@iitj.ac.in>
2019-03-31 19:48:02 +0530

    Added docs example

--- sympy/stats/crv_types.py
+++ sympy/stats/crv_types.py
@@ -1484,6 +1484,8 @@ def Gumbel(name, beta, mu):
     >>> X = Gumbel("x", beta, mu)
     >>> density(X)(x)
     exp(exp(-(-mu + x)/beta) - (-mu + x)/beta)/beta
+    >>> cdf(X)(x)
+    exp(-exp((mu - x)/beta))
 
     References
     ==========
