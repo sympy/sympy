dc2f091240b43f0d643d443779b58bba4a36168b
stm <supreet11agrawal@gmail.com>
2019-04-04 22:58:24 +0530

    `isinstance` to `is_Symbol`

--- sympy/calculus/util.py
+++ sympy/calculus/util.py
@@ -789,7 +789,7 @@ def maximum(f, symbol, domain=S.Reals):
     1/2
 
     """
-    if isinstance(symbol, Symbol):
+    if symbol.is_Symbol:
         if isinstance(domain, EmptySet):
             raise ValueError("Maximum value not defined for empty domain.")
 
@@ -831,7 +831,7 @@ def minimum(f, symbol, domain=S.Reals):
     -1/2
 
     """
-    if isinstance(symbol, Symbol):
+    if symbol.is_Symbol:
         if isinstance(domain, EmptySet):
             raise ValueError("Minimum value not defined for empty domain.")
 
