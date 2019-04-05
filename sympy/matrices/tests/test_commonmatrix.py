c6a0c43937de8d15f8f64d5d0e158fa0bfda9943
Nabanita Dash <dashnabanita@gmail.com>
2019-04-05 07:31:55 +0530

    added xfail tests
    added tests for rs_atan

--- sympy/matrices/tests/test_commonmatrix.py
+++ sympy/matrices/tests/test_commonmatrix.py
@@ -194,7 +194,6 @@ def test_get_diag_blocks2():
     assert C.get_diag_blocks() == [a, c, b]
     assert D.get_diag_blocks() == [c, c, b]
 
-
 def test_shape():
     m = ShapingOnlyMatrix(1, 2, [0, 0])
     m.shape == (1, 2)
@@ -1519,5 +1518,3 @@ def test___eq__():
         [[0, 1, 1],
         [1, 0, 0],
         [1, 1, 1]]) == {}) is False
-
-
