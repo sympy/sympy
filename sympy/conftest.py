b28c16f92111783bfb8c57643a0595bcd9078855
Prionti Nasir <pdn3628@rit.edu>
2019-04-08 07:16:06 -0400

    removed unused parameter session from process_split function

--- sympy/conftest.py
+++ sympy/conftest.py
@@ -10,7 +10,7 @@ import re
 
 sp = re.compile(r'([0-9]+)/([1-9][0-9]*)')
 
-def process_split(session, config, items):
+def process_split(config, items):
     split = config.getoption("--split")
     if not split:
         return
