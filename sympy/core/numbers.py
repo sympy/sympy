c36b1f92bef6a334a83aa6a4a790b2dc9615bacc
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-04-09 00:58:46 +0100

    WIP ...

--- sympy/core/numbers.py
+++ sympy/core/numbers.py
@@ -2807,7 +2807,9 @@ class Infinity(with_metaclass(Singleton, Number)):
         except SympifyError:
             raise TypeError("Invalid comparison %s <= %s" % (self, other))
         if other.is_extended_real:
-            if other.is_finite or other is S.NegativeInfinity:
+            if other is S.Infinity:
+                return S.true
+            elif other.is_finite or other is S.NegativeInfinity:
                 return S.false
             elif other.is_nonpositive:
                 return S.false
@@ -2821,7 +2823,9 @@ class Infinity(with_metaclass(Singleton, Number)):
         except SympifyError:
             raise TypeError("Invalid comparison %s > %s" % (self, other))
         if other.is_extended_real:
-            if other.is_finite or other is S.NegativeInfinity:
+            if other is S.Infinity:
+                return S.false
+            elif other.is_finite or other is S.NegativeInfinity:
                 return S.true
             elif other.is_nonpositive:
                 return S.true
@@ -2992,7 +2996,9 @@ class NegativeInfinity(with_metaclass(Singleton, Number)):
         except SympifyError:
             raise TypeError("Invalid comparison %s < %s" % (self, other))
         if other.is_extended_real:
-            if other.is_finite or other is S.Infinity:
+            if other is S.NegativeInfinity:
+                return S.false
+            elif other.is_finite or other is S.Infinity:
                 return S.true
             elif other.is_nonnegative:
                 return S.true
@@ -3024,6 +3030,8 @@ class NegativeInfinity(with_metaclass(Singleton, Number)):
         except SympifyError:
             raise TypeError("Invalid comparison %s >= %s" % (self, other))
         if other.is_extended_real:
+            if other is S.NegativeInfinity:
+                return S.true
             if other.is_finite or other is S.Infinity:
                 return S.false
             elif other.is_nonnegative:
