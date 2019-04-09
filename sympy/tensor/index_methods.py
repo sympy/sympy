8082649952dc84c8f09fd92e5a55aa5d0dbb5bc1
Rahul prajapati <33652351+Raarav@users.noreply.github.com>
2019-04-09 20:28:40 +0530

    adding comment for __future__

--- sympy/tensor/index_methods.py
+++ sympy/tensor/index_methods.py
@@ -12,6 +12,18 @@
 
 from __future__ import print_function, division
 
+ """
+
+ __future__ is a real module, and serves three purposes:
+
+To avoid confusing existing tools that analyze import statements and expect to find the modules they’re importing.
+To ensure that future statements run under releases prior to 2.1 at least yield runtime exceptions (the import of __future__ will fail, because there was no module of that name prior to 2.1).
+To document when incompatible changes were introduced, and when they will be — or were — made mandatory. This is a form of executable documentation, and can be inspected programmatically via importing __future__ and examining its contents.
+Each statement in __future__.py is of the form:
+
+ """
+
+
 from sympy.core.compatibility import reduce
 from sympy.core.function import Function
 from sympy.functions import exp, Piecewise
