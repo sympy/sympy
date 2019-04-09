45fb224fd34814714d9cc2518b60e2d4ffd1d3be
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-04-09 01:06:11 +0100

    WIP ...

--- sympy/core/assumptions.py
+++ sympy/core/assumptions.py
@@ -200,7 +200,7 @@ _assume_rules = FactRules([
 
     'infinite       ->  !finite',
     'noninteger     ==  real & !integer',
-    'nonzero        ==  real & !zero',
+    'nonzero        ==  extended_real & !zero',
 ])
 
 _assume_defined = _assume_rules.defined_facts.copy()
