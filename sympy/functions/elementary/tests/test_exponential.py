8f9fc2a74a7ae0de10caf396557b45728f1664d6
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-04-16 16:34:47 +0100

    Fix tests for positive -> finite in exponential

--- sympy/functions/elementary/tests/test_exponential.py
+++ sympy/functions/elementary/tests/test_exponential.py
@@ -273,10 +273,10 @@ def test_log_assumptions():
     p = symbols('p', positive=True)
     n = symbols('n', negative=True)
     z = symbols('z', zero=True)
-    x = symbols('x', infinite=True, positive=True)
+    x = symbols('x', infinite=True, extended_positive=True)
 
     assert log(z).is_positive is False
-    assert log(x).is_positive is True
+    assert log(x).is_extended_positive is True
     assert log(2) > 0
     assert log(1, evaluate=False).is_zero
     assert log(1 + z).is_zero
