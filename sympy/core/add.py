6bd10fc98f9ec3f882e28df111bab088380a0505
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-04-17 11:38:48 +0100

    Re-add Add._eval_is_real

--- sympy/core/add.py
+++ sympy/core/add.py
@@ -491,6 +491,8 @@ class Add(Expr, AssocOp):
         return all(term._eval_is_algebraic_expr(syms) for term in self.args)
 
     # assumption methods
+    _eval_is_real = lambda self: _fuzzy_group(
+        (a.is_real for a in self.args), quick_exit=True)
     _eval_is_extended_real = lambda self: _fuzzy_group(
         (a.is_extended_real for a in self.args), quick_exit=True)
     _eval_is_complex = lambda self: _fuzzy_group(
