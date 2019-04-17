d6227a8fc35e87d4f7e722ee5533b9d1466e5b36
Supreet Agrawal <supreet11agrawal@gmail.com>
2019-04-17 21:27:10 +0530

    Update __init__.py

--- sympy/stats/__init__.py
+++ sympy/stats/__init__.py
@@ -43,7 +43,7 @@ from . import rv_interface
 from .rv_interface import (
     cdf, characteristic_function, covariance, density, dependent, E, given, independent, P, pspace,
     random_symbols, sample, sample_iter, skewness, std, variance, where,
-    correlation, moment, cmoment, smoment, sampling_density, moment_generating_function, entropy, H
+    correlation, moment, cmoment, smoment, sampling_density, moment_generating_function, entropy, H,
     quantile
 )
 __all__.extend(rv_interface.__all__)
