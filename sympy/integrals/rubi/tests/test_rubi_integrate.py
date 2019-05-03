d39ee84f4d9ff7502afe2a620ecbe4ff8dcc0514
Francesco Bonazzi <franz.bonazzi@gmail.com>
2019-05-03 13:58:05 +0200

    Update test_rubi_integrate.py

--- sympy/integrals/rubi/tests/test_rubi_integrate.py
+++ sympy/integrals/rubi/tests/test_rubi_integrate.py
@@ -15,9 +15,11 @@ from sympy import (sqrt, simplify, S, atanh, hyper, I, atan, pi, Sum, cos, sin,
     log, atan)
 from sympy.integrals.rubi.utility_function import rubi_test
 from sympy.integrals.rubi.rubi import rubi_integrate
+from sympy.utilities.pytest import SKIP
 
 a, b, c, d, e, f, x, m, n, p, k = symbols('a b c d e f x m n p k', real=True, imaginary=False)
 
+@SKIP
 def test_rubi_integrate():
     assert rubi_integrate(x, x) == x**2/2
     assert rubi_integrate(x**2,x) == x**3/3
