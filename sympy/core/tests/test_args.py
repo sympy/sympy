0adab1500f71f3eead54cd0ba8798e9977da5157
Ritesh Kumar <ritesh99rakesh@gmail.com>
2019-05-12 17:47:47 +0530

    Added test in test_args.py

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1382,6 +1382,10 @@ def test_sympy__stats__crv_types__NormalDistribution():
     from sympy.stats.crv_types import NormalDistribution
     assert _test_args(NormalDistribution(0, 1))
 
+def test_sympy__stats__crv_types__NormalInverseDistribution():
+    from sympy.stats.crv_types import NormalInverseDistribution
+    assert _test_args(NormalInverseDistribution(1, 1))
+
 
 def test_sympy__stats__crv_types__ParetoDistribution():
     from sympy.stats.crv_types import ParetoDistribution
