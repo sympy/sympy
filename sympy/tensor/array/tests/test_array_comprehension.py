c57cdfff4fb1c02a6f17004eddc0b8f891955374
kangzhia <709563092@qq.com>
2019-05-22 01:04:00 +0200

    Added more tests for unimplemented functionalities

--- sympy/tensor/array/tests/test_array_comprehension.py
+++ sympy/tensor/array/tests/test_array_comprehension.py
@@ -1,10 +1,14 @@
 from sympy.tensor.array.array_comprehension import ArrayComprehension
-from sympy.abc import i, j, k
+from sympy.abc import i, j
 
 
 def test_array_comprehension():
-    a = ArrayComprehension(i, (i, 1, 5))
+    a = ArrayComprehension(i*j, (i, 1, 3), (j, 2, 4))
     b = ArrayComprehension(i, (i, 1, j))
-    assert a.doit() == [1, 2, 3, 4, 5]
+    c = ArrayComprehension(i*j, (i, 1, 3))
+    assert a.doit() == [[2, 3, 4], [4, 6, 8], [6, 9, 12]]
     assert isinstance(b.doit(), ArrayComprehension)
-    assert not isinstance(a.doit(), ArrayComprehension)
+    assert b.subs(j, 3) == [1, 2, 3]
+    assert c.doit == [j, 2*j, 3*j]
+    c.add_bound((j, 2, 4))
+    assert c.doit() == [[2, 3, 4], [4, 6, 8], [6, 9, 12]]
