57c741be051bf365c7acda9c97cb31681122c98d
Ankit Pandey <pandeyan@grinnell.edu>
2019-05-30 15:53:58 -0500

    Modify the ASR conversion class to extend CodePrinter
    The ASR converter extends `CodePrinter`, and the conversion functions now follow
    the naming conventions of methods in the `CodePrinter` class. This will allow us
    to use some of the existing methods in the class in the future.

--- sympy/codegen/lfort.py
+++ sympy/codegen/lfort.py
@@ -1,8 +1,10 @@
+from sympy.printing.codeprinter import CodePrinter
+
 import lfortran.asr.asr as asr
 import lfortran.asr.builder as builder
 
 
-class ASRConverter():
+class ASRConverter(CodePrinter):
 
     def __init__(self):
 
@@ -15,43 +17,43 @@ class ASRConverter():
         # Variables that will be used in the body of a function declaration.
         self.variables = []
 
-    def _convert(self, expr, **kwargs):
-        convertmethod = '_convert_' + type(expr).__name__
+    def _print(self, expr, **kwargs):
+        convertmethod = '_print_' + type(expr).__name__
         if hasattr(self, convertmethod):
             return getattr(self, convertmethod)(expr, **kwargs)
         else:
             raise NotImplementedError("Conversion method %s not implemented." %
                                       convertmethod)
 
-    def _convert_arith(self, expr, op):
+    def _print_arith(self, expr, op):
         "A generalized converter for binary arithmetic expressions."
         terms = list(expr.args)
 
-        first_left = self._convert(terms[0])
-        first_right = self._convert(terms[1])
+        first_left = self._print(terms[0])
+        first_right = self._print(terms[1])
 
         lf_node = builder.make_binop(first_left, op, first_right)
 
         for term in terms[2:]:
-            converted_term = self._convert(term)
+            converted_term = self._print(term)
             lf_node = builder.make_binop(lf_node, op, converted_term)
 
         return lf_node
 
     # Only works with Add and Mul so far
-    def _convert_Add(self, expr):
-        return self._convert_arith(expr, asr.Add())
+    def _print_Add(self, expr):
+        return self._print_arith(expr, asr.Add())
 
-    def _convert_Mul(self, expr):
-        return self._convert_arith(expr, asr.Mul())
+    def _print_Mul(self, expr):
+        return self._print_arith(expr, asr.Mul())
 
-    def _convert_Integer(self, expr):
+    def _print_Integer(self, expr):
         return asr.Num(expr.p, type=self.type_integer)
 
-    _convert_One = _convert_Integer
-    _convert_Zero = _convert_Integer
+    _print_One = _print_Integer
+    _print_Zero = _print_Integer
 
-    def _convert_Symbol(self, expr):
+    def _print_Symbol(self, expr):
         sym_name = expr.name
 
         # TODO: Symbols can also be assignments, in which case the intent
@@ -63,7 +65,7 @@ class ASRConverter():
     def wrap_function_definition(self, expr):
         """Convert a sympy expression, wrapping it in a function definition."""
 
-        converted_expr = self._convert(expr)
+        converted_expr = self._print(expr)
         f = builder.scope_add_function(self.translation_unit.global_scope, "f",
                                        return_var="ret")
         f.args = self.variables
