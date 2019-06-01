78b0142479e3d8bfab0896eb1919511222241a4f
S.Y. Lee <sylee957@gmail.com>
2019-06-01 21:15:16 +0900

    Modify tests in lambdarepr

--- sympy/printing/tests/test_lambdarepr.py
+++ sympy/printing/tests/test_lambdarepr.py
@@ -192,9 +192,11 @@ def test_multiple_sums():
 
 
 def test_sqrt():
-    prntr = LambdaPrinter()
+    prntr = LambdaPrinter({'standard' : 'python2'})
     assert prntr._print_Pow(sqrt(x), rational=False) == 'sqrt(x)'
     assert prntr._print_Pow(sqrt(x), rational=True) == 'x**(1./2.)'
+    prntr = LambdaPrinter({'standard' : 'python3'})
+    assert prntr._print_Pow(sqrt(x), rational=True) == 'x**(1/2)'
 
 
 def test_settings():
