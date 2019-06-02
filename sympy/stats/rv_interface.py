6e83d8c70ab1eb6862e36069112e2ee1206ab3f8
Ritesh Kumar <ritesh99rakesh@gmail.com>
2019-06-02 15:58:37 +0530

    Added __future__ import line back

--- sympy/stats/rv_interface.py
+++ sympy/stats/rv_interface.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 from .rv import (probability, expectation, density, where, given, pspace, cdf,
         characteristic_function, sample, sample_iter, random_symbols, independent, dependent,
         sampling_density, moment_generating_function, _value_check, quantile)
