6abe58242e4322e2659cb1bd7176b726e69ca443
czgdp1807 <singh.23@iitj.ac.in>
2019-06-11 19:00:57 +0530

    updated test_args.py

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1550,17 +1550,17 @@ def test_sympy__stats__stochastic_process_types__DiscreteTimeStochasticProcess()
     from sympy.stats.stochastic_process_types import DiscreteTimeStochasticProcess
     assert _test_args(DiscreteTimeStochasticProcess("Y", [1, 2, 3]))
 
-def test_sympy__stats__stochastic_process_types__TransitionMatrix():
-    from sympy.stats.stochastic_process_types import TransitionMatrix, DiscreteMarkovChain
+def test_sympy__stats__stochastic_process_types__TransitionMatrixOf():
+    from sympy.stats.stochastic_process_types import TransitionMatrixOf, DiscreteMarkovChain
     from sympy import MatrixSymbol
     DMC = DiscreteMarkovChain("Y")
-    assert _test_args(TransitionMatrix(DMC, MatrixSymbol('T', 3, 3)))
+    assert _test_args(TransitionMatrixOf(DMC, MatrixSymbol('T', 3, 3)))
 
-def test_sympy__stats__stochastic_process_types__StateSpace():
-    from sympy.stats.stochastic_process_types import StateSpace, DiscreteMarkovChain
+def test_sympy__stats__stochastic_process_types__StochasticStateSpaceOf():
+    from sympy.stats.stochastic_process_types import StochasticStateSpaceOf, DiscreteMarkovChain
     from sympy import MatrixSymbol
     DMC = DiscreteMarkovChain("Y")
-    assert _test_args(StateSpace(DMC, [0, 1, 2]))
+    assert _test_args(StochasticStateSpaceOf(DMC, [0, 1, 2]))
 
 def test_sympy__stats__stochastic_process_types__DiscreteMarkovChain():
     from sympy.stats.stochastic_process_types import DiscreteMarkovChain
