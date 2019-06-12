a397814f549f08c8aefd245414bd7d011a689de9
kangzhiq <709563092@qq.com>
2019-06-12 23:55:11 +0200

    Modified tests in test_arrayop
    - Added loop to make sure test passed for sprase and dense arrays
    - Ameliorated tests in test_derive_by_array

--- sympy/tensor/array/tests/test_arrayop.py
+++ sympy/tensor/array/tests/test_arrayop.py
@@ -7,7 +7,7 @@ from sympy.combinatorics.permutations import _af_invert
 from sympy.utilities.pytest import raises
 
 from sympy import symbols, sin, exp, log, cos, transpose, adjoint, conjugate, diff
-from sympy.tensor.array import Array, NDimArray, ImmutableSparseNDimArray
+from sympy.tensor.array import Array, NDimArray, ImmutableDenseNDimArray, ImmutableSparseNDimArray, MutableSparseNDimArray
 
 from sympy.tensor.array import tensorproduct, tensorcontraction, derive_by_array, permutedims
 
@@ -26,28 +26,29 @@ def test_tensorproduct():
     assert tensorproduct(x, y, z) == x*y*z
     assert tensorproduct(x, y, z, t) == x*y*z*t
 
-    A = Array([x, y])
-    B = Array([1, 2, 3])
-    C = Array([a, b, c, d])
+    for ArrayType in [ImmutableDenseNDimArray, ImmutableSparseNDimArray]:
+        A = ArrayType([x, y])
+        B = ArrayType([1, 2, 3])
+        C = ArrayType([a, b, c, d])
 
-    assert tensorproduct(A, B, C) == Array([[[a*x, b*x, c*x, d*x], [2*a*x, 2*b*x, 2*c*x, 2*d*x], [3*a*x, 3*b*x, 3*c*x, 3*d*x]],
-                                            [[a*y, b*y, c*y, d*y], [2*a*y, 2*b*y, 2*c*y, 2*d*y], [3*a*y, 3*b*y, 3*c*y, 3*d*y]]])
+        assert tensorproduct(A, B, C) == ArrayType([[[a*x, b*x, c*x, d*x], [2*a*x, 2*b*x, 2*c*x, 2*d*x], [3*a*x, 3*b*x, 3*c*x, 3*d*x]],
+                                                    [[a*y, b*y, c*y, d*y], [2*a*y, 2*b*y, 2*c*y, 2*d*y], [3*a*y, 3*b*y, 3*c*y, 3*d*y]]])
 
-    assert tensorproduct([x, y], [1, 2, 3]) == tensorproduct(A, B)
+        assert tensorproduct([x, y], [1, 2, 3]) == tensorproduct(A, B)
 
-    assert tensorproduct(A, 2) == Array([2*x, 2*y])
-    assert tensorproduct(A, [2]) == Array([[2*x], [2*y]])
-    assert tensorproduct([2], A) == Array([[2*x, 2*y]])
-    assert tensorproduct(a, A) == Array([a*x, a*y])
-    assert tensorproduct(a, A, B) == Array([[a*x, 2*a*x, 3*a*x], [a*y, 2*a*y, 3*a*y]])
-    assert tensorproduct(A, B, a) == Array([[a*x, 2*a*x, 3*a*x], [a*y, 2*a*y, 3*a*y]])
-    assert tensorproduct(B, a, A) == Array([[a*x, a*y], [2*a*x, 2*a*y], [3*a*x, 3*a*y]])
+        assert tensorproduct(A, 2) == ArrayType([2*x, 2*y])
+        assert tensorproduct(A, [2]) == ArrayType([[2*x], [2*y]])
+        assert tensorproduct([2], A) == ArrayType([[2*x, 2*y]])
+        assert tensorproduct(a, A) == ArrayType([a*x, a*y])
+        assert tensorproduct(a, A, B) == ArrayType([[a*x, 2*a*x, 3*a*x], [a*y, 2*a*y, 3*a*y]])
+        assert tensorproduct(A, B, a) == ArrayType([[a*x, 2*a*x, 3*a*x], [a*y, 2*a*y, 3*a*y]])
+        assert tensorproduct(B, a, A) == ArrayType([[a*x, a*y], [2*a*x, 2*a*y], [3*a*x, 3*a*y]])
 
     # tests for large scale sparse array
-    a = ImmutableSparseNDimArray({1:2, 3:4},(1000, 2000))
-    b = ImmutableSparseNDimArray({1:2, 3:4},(1000, 2000))
-    assert tensorproduct(a, b) == ImmutableSparseNDimArray({2000001: 4, 2000003: 8, 6000001: 8, 6000003: 16}, (1000, 2000, 1000, 2000))
-
+    for SparseArrayType in [ImmutableSparseNDimArray, MutableSparseNDimArray]:
+        a = SparseArrayType({1:2, 3:4},(1000, 2000))
+        b = SparseArrayType({1:2, 3:4},(1000, 2000))
+        assert tensorproduct(a, b) == ImmutableSparseNDimArray({2000001: 4, 2000003: 8, 6000001: 8, 6000003: 16}, (1000, 2000, 1000, 2000))
 
 
 def test_tensorcontraction():
@@ -63,7 +64,6 @@ def test_tensorcontraction():
 
 def test_derivative_by_array():
     from sympy.abc import a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
-    from sympy.tensor.array import MutableSparseNDimArray, ImmutableSparseNDimArray
 
     bexpr = x*y**2*exp(z)*log(t)
     sexpr = sin(bexpr)
@@ -92,11 +92,10 @@ def test_derivative_by_array():
                                                                          [[[0, 0], [1, 0]], [[0, 0], [0, 1]]]])
 
     # test for large scale sparse array
-    b = MutableSparseNDimArray.zeros(10000, 20000)
-    b[0, 0] = i
-    b[0, 1] = j
-    assert derive_by_array(b, i) == ImmutableSparseNDimArray({0: 1}, (10000, 20000))
-    assert derive_by_array(b, (i, j)) == ImmutableSparseNDimArray({0: 1, 200000001: 1}, (2, 10000, 20000))
+    for SparseArrayType in [ImmutableSparseNDimArray, MutableSparseNDimArray]:
+        b = MutableSparseNDimArray({0:i, 1:j}, (10000, 20000))
+        assert derive_by_array(b, i) == ImmutableSparseNDimArray({0: 1}, (10000, 20000))
+        assert derive_by_array(b, (i, j)) == ImmutableSparseNDimArray({0: 1, 200000001: 1}, (2, 10000, 20000))
 
 
 def test_issue_emerged_while_discussing_10972():
