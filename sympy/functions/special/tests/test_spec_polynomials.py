15169e1b290605d5ec68f0300ee939e3c6a56b09
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2019-06-13 12:01:21 +0200

    Updated tests after merging

--- sympy/functions/special/tests/test_spec_polynomials.py
+++ sympy/functions/special/tests/test_spec_polynomials.py
@@ -312,15 +312,14 @@ def test_hermite():
 
 def test_laguerre():
     n = Symbol("n")
+    m = Symbol("m", negative=True)
 
     # Laguerre polynomials:
     assert laguerre(0, x) == 1
     assert laguerre(1, x) == -x + 1
     assert laguerre(2, x) == x**2/2 - 2*x + 1
     assert laguerre(3, x) == -x**3/6 + 3*x**2/2 - 3*x + 1
-
-    X = laguerre(Rational(5,2), x)
-    assert isinstance(X, laguerre)
+    assert laguerre(-2, x) == (x + 1)*exp(x)
 
     X = laguerre(n, x)
     assert isinstance(X, laguerre)
@@ -335,19 +334,18 @@ def test_laguerre():
 
     assert laguerre(n, x).rewrite("polynomial").dummy_eq(
         Sum(x**_k*RisingFactorial(-n, _k)/factorial(_k)**2, (_k, 0, n)))
+    assert laguerre(m, x).rewrite("polynomial").dummy_eq(
+        exp(x)*Sum((-x)**_k*RisingFactorial(m + 1, _k)/factorial(_k)**2,
+            (_k, 0, -m - 1)))
 
     assert diff(laguerre(n, x), x) == -assoc_laguerre(n - 1, 1, x)
 
-    raises(ValueError, lambda: laguerre(-2.1, x))
-    raises(ArgumentIndexError, lambda: laguerre(n, x).fdiff(1))
-    raises(ArgumentIndexError, lambda: laguerre(n, x).fdiff(3))
-
-
-def test_legendre_fail():
-    skip("Infinite recursion kills Travis")
-    n = Symbol("n")
     assert laguerre(-n, x) == exp(x)*laguerre(n-1, -x)
     assert laguerre(-3, x) == exp(x)*laguerre(2, -x)
+    raises(ValueError, lambda: laguerre(-2.1, x))
+    raises(ValueError, lambda: laguerre(Rational(5, 2), x))
+    raises(ArgumentIndexError, lambda: laguerre(n, x).fdiff(1))
+    raises(ArgumentIndexError, lambda: laguerre(n, x).fdiff(3))
 
 
 def test_assoc_laguerre():
