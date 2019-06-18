1f0cb2f8323a96f13c5db386791838a785db0195
eward <eward@sunbelt-medical.com>
2019-06-18 12:09:25 -0700

    update issue #17044 to show construction

--- sympy/ntheory/tests/test_primetest.py
+++ sympy/ntheory/tests/test_primetest.py
@@ -139,7 +139,8 @@ def test_is_square():
     assert [i for i in range(25) if is_square(i)] == [0, 1, 4, 9, 16]
 
     # issue #17044
-    assert not is_square(216000)
-    assert not is_square(592704)
-    assert not is_square(1157625)
-    assert not is_square(1728000)
+    assert not is_square(60 ** 3)
+    assert not is_square(60 ** 5)
+    assert not is_square(84 ** 7)
+    assert not is_square(105 ** 9)
+    assert not is_square(120 ** 3)
