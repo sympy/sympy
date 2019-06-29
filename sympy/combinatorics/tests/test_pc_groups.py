c82cd23ce04e3dab2127be68a513788698504bdd
Divyanshu <divyanshu@iiitmanipur.ac.in>
2019-06-29 11:24:59 +0530

    sympy/.

--- sympy/combinatorics/tests/test_pc_groups.py
+++ sympy/combinatorics/tests/test_pc_groups.py
@@ -39,21 +39,39 @@ def test_collected_word():
     group = F
     collector = Collector(pc_relators, relative_order, group)
 
+    a = Permutation(0, 1) # x0
+    b = Permutation(0, 1, 2) # x1
+
     word = x1*x0
     assert collector.collected_word(word) == x0*x1**2
+    assert b*a == a*b**2
+
     word = x1*x0**2
     assert collector.collected_word(word) == x1
+    assert b*a**2 == b
+
     word = x1**2*x0
     assert collector.collected_word(word) == x0*x1
+    assert b**2*a == a*b
+
     word = x1**4*x0**6
     assert collector.collected_word(word) == x1
+    assert b**4*a**6 == b
+
     word = x0*x1
     # The word is already collected
     assert collector.collected_word(word) == x0*x1
+    assert a*b == a*b
+
     word = x0**2*x1
     assert collector.collected_word(word) == x1
+    assert a**2*b == b
+
     word = x0**2*x1**3
     # Handle Identity case
     assert collector.collected_word(word) == F.identity
+    assert a**2*b**3 == Permutation(2)
+
     word = x1**-2*x0
     assert collector.collected_word(word) == x0*x1**-4
+    assert b**-2*a == a*b**-4
