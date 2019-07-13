bb018b71653c1f7482cb3a66f582c3ee91674a42
Christopher Smith <smichr@gmail.com>
2019-07-13 10:25:15 -0500

    condense sorted tuples by showing multiplicity

--- sympy/logic/boolalg.py
+++ sympy/logic/boolalg.py
@@ -2375,29 +2375,38 @@ def _finger(eq):
     # of times it appeared as a Not(symbol);
     # of times it appeared as a Symbol in an And or Or;
     # of times it appeared as a Not(Symbol) in an And or Or;
-    a sorted list of tuples (i, j) where i is the number of arguments
+    a sorted tuple of tuples, (i, j, k), where i is the number of arguments
     in an And or Or with which it appeared as a Symbol, and j is
-    the number of arguments that were Not(Symbol)
+    the number of arguments that were Not(Symbol); k is the number
+    of times that (i, j) was seen.
     ]
 
     Examples
     ========
 
     >>> from sympy.logic.boolalg import _finger as finger
-    >>> from sympy import And, Or, Not
+    >>> from sympy import And, Or, Not, to_cnf, symbols
     >>> from sympy.abc import a, b, x, y
     >>> eq = Or(And(Not(y), a), And(Not(y), b), And(x, y))
     >>> dict(finger(eq))
-    {(0, 0, 1, 0, ((2, 0),)): [x],
-    (0, 0, 1, 0, ((2, 1),)): [a, b],
-    (0, 0, 1, 2, ((2, 0),)): [y]}
+    {(0, 0, 1, 0, ((2, 0, 1),)): [x],
+    (0, 0, 1, 0, ((2, 1, 1),)): [a, b],
+    (0, 0, 1, 2, ((2, 0, 1),)): [y]}
     >>> dict(finger(x & ~y))
     {(0, 1, 0, 0, ()): [y], (1, 0, 0, 0, ()): [x]}
+
+    In the following, the (5, 2, 6) means that there were 6 Or
+    functions in which a symbol appeared as itself amongst 5 arguments in
+    which there were also 2 negated symbols, e.g. ``(a0 | a1 | a2 | ~a3 | ~a4)``
+    is counted once for a0, a1 and a2.
+
+    >>> dict(finger(to_cnf(Xor(*symbols('a:5')))))
+    {(0, 0, 8, 8, ((5, 0, 1), (5, 2, 6), (5, 4, 1))): [a0, a1, a2, a3, a4]}
 
     The equation must not have more than one level of nesting:
 
     >>> dict(finger(And(Or(x, y), y)))
-    {(0, 0, 1, 0, ((2, 0),)): [x], (1, 0, 1, 0, ((2, 0),)): [y]}
+    {(0, 0, 1, 0, ((2, 0, 1),)): [x], (1, 0, 1, 0, ((2, 0, 1),)): [y]}
     >>> dict(finger(And(Or(x, And(a, x)), y)))
     Traceback (most recent call last):
     ...
@@ -2406,7 +2415,7 @@ def _finger(eq):
     So y and x have unique fingerprints, but a and b do not.
     """
     f = eq.free_symbols
-    d = dict(list(zip(f, [[0]*4 + [[]] for fi in f])))
+    d = dict(list(zip(f, [[0]*4 + [defaultdict(int)] for fi in f])))
     for a in eq.args:
         if a.is_Symbol:
             d[a][0] += 1
@@ -2417,14 +2426,14 @@ def _finger(eq):
             for ai in a.args:
                 if ai.is_Symbol:
                     d[ai][2] += 1
-                    d[ai][-1].append(o)
+                    d[ai][-1][o] += 1
                 elif ai.is_Not:
                     d[ai.args[0]][3] += 1
                 else:
                     raise NotImplementedError('unexpected level of nesting')
     inv = defaultdict(list)
     for k, v in ordered(iter(d.items())):
-        v[-1] = tuple(sorted(v[-1]))
+        v[-1] = tuple(sorted([k + (va,) for k, va in v[-1].items()]))
         inv[tuple(v)].append(k)
     return inv
 
