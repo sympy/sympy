9ab752e07af3c826c2c7ff6d034eee2e307b0d90
Tomasz Pytel <tompytel@gmail.com>
2019-07-13 12:02:10 -0300

    matrices: modified fix to just call mat**exp when needed
    I had unnecessarily put the call to jordan_pow function in the body of
    MatPow.doit() when all I had to do was modify the conditional check for
    a call out to mat**exp to let that function handle it, it is now changed
    and more simple.

--- sympy/matrices/expressions/matpow.py
+++ sympy/matrices/expressions/matpow.py
@@ -71,7 +71,7 @@ class MatPow(MatrixExpr):
             raise ValueError("Matrix determinant is 0, not invertible.")
         elif isinstance(base, (Identity, ZeroMatrix)):
             return base
-        elif isinstance(base, MatrixBase) and exp.is_number:
+        elif isinstance(base, MatrixBase) and (exp.is_number or exp.is_negative is not None or base.det() != 0):
             if exp is S.One:
                 return base
             return base**exp
@@ -81,10 +81,6 @@ class MatPow(MatrixExpr):
             return Inverse(base).doit(**kwargs)
         elif exp is S.One:
             return base
-        elif exp.is_Number or exp.is_negative is not None or (isinstance(base, MatrixBase) and base.det() != 0):
-            jordan_pow = getattr(base, '_matrix_pow_by_jordan_blocks', None)
-            if jordan_pow is not None:
-                return jordan_pow (exp)
         return MatPow(base, exp)
 
     def _eval_transpose(self):
