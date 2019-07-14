07c88e64976e90edf876011c3b72191f3ca48be7
kangzhiq <709563092@qq.com>
2019-07-15 00:08:34 +0800

    Update implementation of format
    - Ameliorated implementation of lil
    - Added implementation of coo after testing
    - Added basic implementation of csr

--- sympy/tensor/array/sp_utils.py
+++ sympy/tensor/array/sp_utils.py
@@ -7,8 +7,15 @@ from sympy.core.compatibility import Iterable
 
 import functools
 
+# A set of functions that could be adde to the base class
+# in order to avoid code repetition 
+def check_bound(shape, index):
+    if len(shape) != len(index):
+        return False
+    return all([shape[i] > index[i] for i in range(len(shape))])
+
 # Row-based linked list sparse matrix(LIL)
-class lil(Basic, NDimArray):
+class lil(Basic, SparseNDimArray):
     """
     Create a sparse array with Row-based linked list sparse matrix(LIL) format.
     This data structure is efficient in incremental construction of sparse arrays.
@@ -36,7 +43,8 @@ class lil(Basic, NDimArray):
     """
     def __new__(cls, iterable=None, shape=None, **kwargs):
         shape, flat_list = cls._handle_ndarray_creation_inputs(iterable, shape, **kwargs)
-        shape = Tuple(*map(_sympify, shape))
+        #shape = Tuple(*map(_sympify, shape))
+        shape = Tuple.fromiter(_sympify(i) for i in shape)
         cls._check_special_bounds(flat_list, shape)
         loop_size = functools.reduce(lambda x,y: x*y, shape) if shape else 0
 
@@ -51,16 +59,15 @@ class lil(Basic, NDimArray):
         self._loop_size = loop_size
 
         new_dict = {}
+        # if it is a dense array, it would be cast to a default sparse array
+        # and then convert to a LIL format
         if isinstance(iterable, Iterable):
             iterable = SparseNDimArray(iterable)
 
         if isinstance(iterable, SparseNDimArray):
             new_dict = iterable._sparse_array
-        # if it is a dense array, it would be cast to a default sparse array
-        # and then convert to a LIL format
-        elif isinstance(iterable, DenseNDimArray):
-            sp = SparseNDimArray(iterable)
-            new_dict = sp._sparse_array
+
+        # TODO: Enable the initialization with (data, rows) as a tuple
         else:
             raise NotImplementedError("Data type not yet supported")
 
@@ -95,10 +102,18 @@ class lil(Basic, NDimArray):
             temp_iter = temp_iter[i]
         return temp_iter
 
+    def __iter__(self):
+        def iterator():
+            for i in range(self._loop_size):
+                yield self[i]
+        return iterator()
 
     def __getitem__(self, index):
         if not isinstance(index, (tuple, Tuple)):
             index = self._get_tuple_index(index)
+        if not check_bound(self._shape, index):
+            raise ValueError('Index ' + str(index) + ' out of border')
+
         row_values = self._get_row(self._data, index)
         row_indices = self._get_row(self._rows, index)
 
@@ -112,6 +127,9 @@ class lil(Basic, NDimArray):
     def __setitem__(self, index, value):
         if not isinstance(index, (tuple, Tuple)):
             index = self._get_tuple_index(index)
+        if not check_bound(self._shape, index):
+            raise ValueError('Index ' + str(index) + ' out of border')
+
         row_values = self._get_row(self._data, index)
         row_indices = self._get_row(self._rows, index)
 
@@ -121,3 +139,137 @@ class lil(Basic, NDimArray):
         else:
             row_values.append(value)
             row_indices.append(index[-1])
+
+    # TODO: convert to other formats
+
+
+class coo(Basic, SparseNDimArray):
+    '''
+    Coordinate list format. This format is not very efficient for calculation. But it
+    can be easily cast to csr/csc format. 
+    '''
+    def __new__(cls, iterable=None, shape=None, **kwargs):
+        shape, flat_list = cls._handle_ndarray_creation_inputs(iterable, shape, **kwargs)
+        shape = Tuple.fromiter(_sympify(i) for i in shape)
+        cls._check_special_bounds(flat_list, shape)
+        loop_size = functools.reduce(lambda x,y: x*y, shape) if shape else 0
+
+        data = []
+        coor = [[] for i in range(len(shape))]
+
+        self = Basic.__new__(cls, data, coor, shape, **kwargs)
+        self._data = data
+        self._coor = coor
+        self._shape = shape
+        self._rank = len(shape)
+        self._loop_size = loop_size
+
+        if isinstance(iterable, Iterable):
+            iterable = SparseNDimArray(iterable)
+
+        if isinstance(iterable, SparseNDimArray):
+            for k, v in sorted(iterable._sparse_array.items()):
+                data.append(v)
+                idx = self._get_tuple_index(k)
+                for i in range(len(shape)):
+                    coor[i].append(idx[i])
+        # TODO: Enable the initialization with (data, coor) as a tuple
+        else:
+            raise NotImplementedError("Data type not yet supported")
+
+        return self
+
+    def __getitem__(self, index):
+        if not isinstance(index, (tuple, Tuple)):
+            index = self._get_tuple_index(index)
+        if not check_bound(self._shape, index):
+            raise ValueError('Index ' + str(index) + ' out of border')
+
+        for i in range(len(self._data)):
+            if all([index[j] == self._coor[j][i] for j in range(self._rank)]):
+                return self._data[i]
+        return 0
+
+    def __iter__(self):
+        def iterator():
+            for i in range(self._loop_size):
+                yield self[i]
+        return iterator()
+
+    # TODO: convert to other formats
+    def tocsr(self):
+
+
+class csr(Basic, SparseNDimArray):
+    '''
+    Compressed Sparse Row. This format is widely used. It has the following advantages:
+    Efficient item access and slicing
+    Fast arithmetics
+    Fast vector products
+    '''
+    def __new__(cls, iterable=None, shape=None, **kwargs):
+        shape, flat_list = cls._handle_ndarray_creation_inputs(iterable, shape, **kwargs)
+        shape = Tuple.fromiter(_sympify(i) for i in shape)
+        cls._check_special_bounds(flat_list, shape)
+        loop_size = functools.reduce(lambda x,y: x*y, shape) if shape else 0
+
+        data = []
+        col_ind = []
+        row_ptr = cls._empty(shape)
+
+        self = Basic.__new__(cls, data, col_ind, row_ptr, shape, **kwargs)
+        self._data = data
+        self._col_ind = col_ind
+        self._row_ptr = row_ptr
+        self._shape = shape
+        self._rank = len(shape)
+        self._loop_size = loop_size
+
+        counter = 0
+        if isinstance(iterable, Iterable):
+            iterable = SparseNDimArray(iterable)
+
+        if isinstance(iterable, SparseNDimArray):
+            for k, v in sorted(iterable._sparse_array.items()):
+                data.append(v)
+                idx = self._get_tuple_index(k)
+                col_ind.append(idx[-1])
+                self._get_row(row_ptr, idx) = counter
+                counter += 1
+            
+
+        # TODO: Enable the initialization with (data, col_ind, row_ptr) as a tuple
+        else:
+            raise NotImplementedError("Data type not yet supported")
+
+    # Repetitous code, should be regrouped in the base class
+    @classmethod
+    def _empty(cls, shape):
+        def f(s):
+            if len(s) == 0:
+                return []
+            if len(s) == 1:
+                return [[] for i in range(s[0])]
+            arr = f(s[1:])
+            return [arr for i in range(s[0])]
+
+        if not shape:
+            raise ValueError("Shape must be defined")
+        return f(shape[:-1])
+
+    def _get_row(self, iterable, index):
+        temp_iter = iterable
+        for i in index[:-1]:
+            temp_iter = temp_iter[i]
+        return temp_iter
+
+    def __getitem__(self, index):
+        if not isinstance(index, (tuple, Tuple)):
+            index = self._get_tuple_index(index)
+        if not check_bound(self._shape, index):
+            raise ValueError('Index ' + str(index) + ' out of border')
+
+        row_start = self._row_ptr[index[:-1]]
+        if row_start == []:
+            return 0
+        
