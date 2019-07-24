af98f0ad36c4a704f441972456b64ed78d4b8575
Nikhil Maan <nikhilmaan22@gmail.com>
2019-07-24 20:44:15 +0530

    Improved tests for the parser

--- sympy/parsing/tests/test_fortran_parser.py
+++ sympy/parsing/tests/test_fortran_parser.py
@@ -1,4 +1,5 @@
 from sympy.parsing.sym_expr import SymPyExpression
+from sympy.codegen.ast import (Assignment, Declaration, FunctionDefinition, )
 import re
 
 expr1 = SymPyExpression()
@@ -19,10 +20,13 @@ def test_sym_expr():
     expr3 = SymPyExpression(src,'f')
     expr4 = SymPyExpression(src1,'f')
     for iter in expr3.return_expr():
+        assert isinstance(iter, Declaration)
         assert re.match(r'Declaration\(.*?\)', str(iter))
     ls = expr4.return_expr()
-    for i in range(0,7):
+    for i in range(0, 7):
+        assert isinstance(ls[i], Declaration)
         assert re.match(r'Declaration\(.*?\)', str(ls[i]))
+    assert isinstance(ls[8], Assignment)
     assert re.match(r'Assignment\(.*?\)', str(ls[8]))
 
 
@@ -40,8 +44,13 @@ def test_assignment():
     ls1 = expr1.return_expr()
     for iter in range(0, 12):
         if (iter < 8):
-            pass
+            assert isinstance(ls1[iter], Declaration)
+            assert re.match(
+                r'Declaration\(Variable\(\w+,\stype\=(integer|real),\svalue\=.*\)\)',
+                str(ls1[iter])
+            )
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\((?P<var>Variable)\(\w+\),\s(?P=var)\(\w+\)\)',
                 str(ls1[iter])
@@ -60,9 +69,10 @@ def test_binop_add():
     expr1.convert_to_expr(src1, 'f')
     ls1 = expr1.return_expr()
     for iter in range(0, 11):
-        if(iter<8):
+        if(iter < 8):
             pass
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\(Variable\(\w+\),\s\w+(\s\+\s\w+)+\)',
                 str(ls1[iter])
@@ -81,9 +91,10 @@ def test_binop_sub():
     expr1.convert_to_expr(src1, 'f')
     ls1 = expr1.return_expr()
     for iter in range(0, 11):
-        if(iter<8):
+        if(iter < 8):
             pass
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\(Variable\(\w+\),\s\w+(\s-\s\w+)+\)',
                 str(ls1[iter])
@@ -102,9 +113,10 @@ def test_binop_mul():
     expr1.convert_to_expr(src1, 'f')
     ls1 = expr1.return_expr()
     for iter in range(0, 11):
-        if(iter<8):
+        if(iter < 8):
             pass
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\(Variable\(\w+\),\s\w+(\*\w+)+\)',
                 str(ls1[iter])
@@ -124,9 +136,10 @@ def test_binop_div():
     expr1.convert_to_expr(src1, 'f')
     ls1 = expr1.return_expr()
     for iter in range(0, 12):
-        if(iter<8):
+        if(iter < 8):
             pass
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\(Variable\(\w+\),\s\w+(/\w+)+\)',
                 str(ls1[iter])
@@ -146,9 +159,10 @@ def test_mul_binop():
     expr1.convert_to_expr(src1, 'f')
     ls1 = expr1.return_expr()
     for iter in range(0, 12):
-        if(iter<8):
+        if(iter < 8):
             pass
         else:
+            assert isinstance(ls1[iter], Assignment)
             assert re.match(
                 r'Assignment\(Variable\(\w+\),\s\w+(\s?[*/+-]\s?\w+)+\)',
                 str(ls1[iter])
@@ -164,16 +178,14 @@ def test_function():
     """
     expr1.convert_to_expr(src1, 'f')
     for iter in expr1.return_expr():
+        assert isinstance(iter, FunctionDefinition)
         assert re.match(r'FunctionDefinition\(.*\)', str(iter))
-        #assert re.match(
-        #    r"FunctionDefinition\(.*,\sname\=String\(\'\w+\'\),\sparameters\=(.*?),\sbody\=CodeBlock\(.*\),\\n\s*Return\(\.*\)\)",
-        #    str(iter)
-        #)
 
 
 def test_var():
     expr1.convert_to_expr(src, 'f')
     for iter in expr1.return_expr():
+        assert isinstance(iter, Declaration)
         assert re.match(
             r'Declaration\(Variable\(\w+,\s*type\=(integer|real),\s*value\=.*\)\)',
             str(iter)
@@ -184,7 +196,7 @@ def test_convert_py():
     expr1.convert_to_expr(src, 'f')
     for iter in expr1.convert_to_python():
         assert re.match(
-            r'\w+\s\=\s\d',
+            r'\w+\s\=\s.*',
             str(iter)
         )
 
