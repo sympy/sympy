0023b06552fd4533261fb2c905f11909f1f906f5
S.Y. Lee <sylee957@gmail.com>
2019-07-27 15:33:33 +0900

    Add proper tests for dotproduct

--- sympy/matrices/expressions/tests/test_dotproduct.py
+++ sympy/matrices/expressions/tests/test_dotproduct.py
@@ -1,5 +1,7 @@
 from sympy.core.expr import unchanged
+from sympy.core.mul import Mul
 from sympy.matrices import Matrix
+from sympy.matrices.expressions.matexpr import MatrixSymbol
 from sympy.matrices.expressions.dotproduct import DotProduct
 from sympy.utilities.pytest import raises
 
@@ -27,6 +29,7 @@ def test_dotproduct_symbolic():
     B = MatrixSymbol('B', 3, 1)
 
     dot = DotProduct(A, B)
-    assert unchanged(Mul, dot, 2)
+    assert dot.is_scalar == True
+    assert unchanged(Mul, 2, dot)
     # XXX Fix forced evaluation for arithmetics with matrix expressions
     assert dot * A == (A[0, 0]*B[0, 0] + A[1, 0]*B[1, 0] + A[2, 0]*B[2, 0])*A
