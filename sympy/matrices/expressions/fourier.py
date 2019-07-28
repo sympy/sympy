5f4aa58bf6eec1bec199be3cb554a447a5b6b256
S.Y. Lee <sylee957@gmail.com>
2019-07-28 11:49:05 +0900

    Rebase to master

--- sympy/matrices/expressions/fourier.py
+++ sympy/matrices/expressions/fourier.py
@@ -1,25 +1,60 @@
 from __future__ import print_function, division
 
-from sympy.matrices.expressions import MatrixExpr
+from sympy.core.sympify import _sympify
+from sympy.core.decorators import deprecated
+
+from .matexpr import MatrixExpr
+from .inverse import Inverse
+
 from sympy import S, I, sqrt, exp
 
-class DFT(MatrixExpr):
-    """ Discrete Fourier Transform """
+class DFTMatrix(MatrixExpr):
+    """Discrete Fourier transformation matrix"""
     n = property(lambda self: self.args[0])
     shape = property(lambda self: (self.n, self.n))
 
-    def _entry(self, i, j, **kwargs):
-        w = exp(-2*S.Pi*I/self.n)
-        return w**(i*j) / sqrt(self.n)
+    def __new__(cls, n, fourier_params=(0, -1)):
+        n = _sympify(n)
+        if n.is_number and not (n.is_integer and n.is_positive):
+            raise ValueError(
+                'Matrix size {} should be specified as a positive integer.')
+
+        (a, b) = _sympify(fourier_params)
+
+        return MatrixExpr().__new__(cls, n, a, b)
+
+    def _entry(self, i, j):
+        n = self.rows
+        a, b = self.args[1], self.args[2]
+        w = exp(2*S.Pi*I*b / n)
+
+        return w**(i*j) / sqrt(n**(1-a))
 
     def _eval_inverse(self):
-        return IDFT(self.n)
+        n = self.rows
+        a, b = self.args[1], self.args[2]
 
-class IDFT(DFT):
-    """ Inverse Discrete Fourier Transform """
-    def _entry(self, i, j, **kwargs):
-        w = exp(-2*S.Pi*I/self.n)
-        return w**(-i*j) / sqrt(self.n)
+        if b.is_number and b.is_integer:
+            if b.gcd(n) != 1:
+                raise ValueError(
+                    'The DFT Matrix with the specification is not invertible')
+            if not b.is_integer:
+                return Inverse(self)
 
-    def _eval_inverse(self):
-        return DFT(self.n)
+            return DFTMatrix(n, fourier_params=(-a, -b))
+
+@deprecated(
+    issue=99999,
+    useinstead="DFTMatrix",
+    deprecated_since_version="1.5")
+def DFT(n):
+    """Discrete Fourier transformation matrix."""
+    return DFTMatrix(n)
+
+
+@deprecated(
+    issue=99999,
+    useinstead="DFTMatrix.inverse",
+    deprecated_since_version="1.5")
+def IDFT(n):
+    return DFTMatrix(n).inverse()
