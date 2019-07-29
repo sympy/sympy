2152124815835af32c72a0e8294e54044ebfa4e5
Steven Lee <stevenlee123@protonmail.com>
2019-07-29 11:49:47 +1000

    Remove the test case of jn_zero with scipy

--- sympy/functions/special/tests/test_bessel.py
+++ sympy/functions/special/tests/test_bessel.py
@@ -273,12 +273,6 @@ def test_jn_zeros():
     assert eq(jn_zeros(3, 4), [6.987932, 10.417118, 13.698023, 16.923621])
     assert eq(jn_zeros(4, 4), [8.182561, 11.704907, 15.039664, 18.301255])
 
-    assert eq(jn_zeros(0, 4, method='scipy'), [3.141592, 6.283185, 9.424777, 12.566370])
-    assert eq(jn_zeros(1, 4, method='scipy'), [4.493409, 7.725251, 10.904121, 14.066193])
-    assert eq(jn_zeros(2, 4, method='scipy'), [5.763459, 9.095011, 12.322940, 15.514603])
-    assert eq(jn_zeros(3, 4, method='scipy'), [6.987932, 10.417118, 13.698023, 16.923621])
-    assert eq(jn_zeros(4, 4, method='scipy'), [8.182561, 11.704907, 15.039664, 18.301255])
-
 
 def test_bessel_eval():
     from sympy import I, Symbol
