1d9351f72accd6f89fc6b79090d2716a187bd4c5
Steven Lee <stevenlee123@protonmail.com>
2019-07-30 15:53:01 +1000

    FIx a bug in physics/quantum/density.py fidelity

--- sympy/physics/quantum/density.py
+++ sympy/physics/quantum/density.py
@@ -314,4 +314,4 @@ def fidelity(state1, state2):
                          "matrix obtained should be a square matrix")
 
     sqrt_state1 = state1**Rational(1, 2)
-    return Tr((sqrt_state1*state2ssqrt_state1)**Rational(1, 2)).doit()
+    return Tr((sqrt_state1*state2*sqrt_state1)**Rational(1, 2)).doit()
