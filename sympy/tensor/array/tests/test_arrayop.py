53bd35fc76e3a6db7dc0348aba5bb3a31f04e2bc
kangzhiq <709563092@qq.com>
2019-08-08 17:20:16 +0800

    Bug fixed for py2.7

--- sympy/tensor/array/tests/test_arrayop.py
+++ sympy/tensor/array/tests/test_arrayop.py
@@ -290,7 +290,7 @@ def test_flatten():
     from sympy import Matrix
     for ArrayType in [ImmutableDenseNDimArray, ImmutableSparseNDimArray, Matrix]:
         A = ArrayType(range(24)).reshape(4, 6)
-        assert [*Flatten(A)] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+        assert [i for i in Flatten(A)] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
         for i, v in enumerate(Flatten(A)):
             i == v
