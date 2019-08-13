8afeb73f44e926f87fc060ddd3c512c402c5cf5d
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2019-08-13 22:15:58 +0200

    Fixed #17405

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -579,7 +579,6 @@ class LineOver1DRangeSeries(Line2DBaseSeries):
         self.depth = kwargs.get('depth', 12)
         self.line_color = kwargs.get('line_color', None)
         self.xscale = kwargs.get('xscale', 'linear')
-        self.flag = 0
 
     def __str__(self):
         return 'cartesian line: %s for %s over %s' % (
@@ -623,13 +622,8 @@ class LineOver1DRangeSeries(Line2DBaseSeries):
                 ynew = f(xnew)
                 new_point = np.array([xnew, ynew])
 
-                if self.flag == 1:
-                    return
                 # Maximum depth
                 if depth > self.depth:
-                    if p[1] is None or q[1] is None:
-                        self.flag = 1
-                        return
                     list_segments.append([p, q])
 
                 # Sample irrespective of whether the line is flat till the
