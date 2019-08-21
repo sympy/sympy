37900248b3088b386a1107fb321859cce2d4d246
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-08-21 12:37:14 +0100

    Convert Lambda argument list to tuple in polys

--- sympy/polys/partfrac.py
+++ sympy/polys/partfrac.py
@@ -488,7 +488,7 @@ def assemble_partfrac_list(partial_list):
             ad, de = df.variables, df.expr
             # Hack to make dummies equal because Lambda created new Dummies
             de = de.subs(ad[0], an[0])
-            func = Lambda(an, nu/de**ex)
+            func = Lambda(tuple(an), nu/de**ex)
             pfd += RootSum(r, func, auto=False, quadratic=False)
         else:
             # Assemble in case the roots are given explicitly by a list of algebraic numbers
