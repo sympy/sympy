36a0f810508ca40d9fe3643687d244c483d2f8f0
S.Y. Lee <sylee957@gmail.com>
2019-08-30 04:32:55 +0900

    Add printig methods and tests for set operations
    Delete test files

--- sympy/printing/mathml.py
+++ sympy/printing/mathml.py
@@ -185,6 +185,7 @@ class MathMLContentPrinter(MathMLPrinterBase):
             'StrictGreaterThan': 'gt',
             'StrictLessThan': 'lt',
             'Union': 'union',
+            'Intersection': 'intersect',
         }
 
         for cls in e.__class__.__mro__:
@@ -550,6 +551,22 @@ class MathMLContentPrinter(MathMLPrinterBase):
         for arg in e.args:
             x.appendChild(self._print(arg))
         return x
+
+    def _print_Complement(self, e):
+        x = self.dom.createElement('apply')
+        x.appendChild(self.dom.createElement('setdiff'))
+        for arg in e.args:
+            x.appendChild(self._print(arg))
+        return x
+
+    def _print_ProductSet(self, e):
+        x = self.dom.createElement('apply')
+        x.appendChild(self.dom.createElement('cartesianproduct'))
+        for arg in e.args:
+            x.appendChild(self._print(arg))
+        return x
+
+    # XXX Symmetric difference is not supported for MathML content printers.
 
 
 class MathMLPresentationPrinter(MathMLPrinterBase):
