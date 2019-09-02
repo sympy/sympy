c54e577c244190063b942e75d5d060037b3ad71e
S.Y. Lee <sylee957@gmail.com>
2019-09-02 12:44:47 +0900

    Fix parenthesis for mathml set operators printing

--- sympy/printing/tests/test_mathml.py
+++ sympy/printing/tests/test_mathml.py
@@ -28,7 +28,7 @@ from sympy.physics.quantum import ComplexSpace, HilbertSpace, FockSpace, hbar, D
 from sympy.printing.mathml import mathml, MathMLContentPrinter, \
     MathMLPresentationPrinter, MathMLPrinter
 from sympy.sets.sets import FiniteSet, Union, Intersection, Complement, \
-    SymmetricDifference, Interval, EmptySet
+    SymmetricDifference, Interval, EmptySet, ProductSet
 from sympy.stats.rv import RandomSymbol
 from sympy.utilities.pytest import raises
 from sympy.vector import CoordSys3D, Cross, Curl, Dot, Divergence, Gradient, Laplacian
@@ -1230,22 +1230,75 @@ def test_print_SetOp():
     f1 = FiniteSet(x, 1, 3)
     f2 = FiniteSet(y, 2, 4)
 
-    assert mpp.doprint(Union(f1, f2, evaluate=False)) == \
+    prntr = lambda x: mathml(x, printer='presentation')
+
+    assert prntr(Union(f1, f2, evaluate=False)) == \
     '<mrow><mfenced close="}" open="{"><mn>1</mn><mn>3</mn><mi>x</mi>'\
     '</mfenced><mo>&#x222A;</mo><mfenced close="}" open="{"><mn>2</mn>'\
     '<mn>4</mn><mi>y</mi></mfenced></mrow>'
-    assert mpp.doprint(Intersection(f1, f2, evaluate=False)) == \
+    assert prntr(Intersection(f1, f2, evaluate=False)) == \
     '<mrow><mfenced close="}" open="{"><mn>1</mn><mn>3</mn><mi>x</mi>'\
     '</mfenced><mo>&#x2229;</mo><mfenced close="}" open="{"><mn>2</mn>'\
     '<mn>4</mn><mi>y</mi></mfenced></mrow>'
-    assert mpp.doprint(Complement(f1, f2, evaluate=False)) == \
+    assert prntr(Complement(f1, f2, evaluate=False)) == \
     '<mrow><mfenced close="}" open="{"><mn>1</mn><mn>3</mn><mi>x</mi>'\
     '</mfenced><mo>&#x2216;</mo><mfenced close="}" open="{"><mn>2</mn>'\
     '<mn>4</mn><mi>y</mi></mfenced></mrow>'
-    assert mpp.doprint(SymmetricDifference(f1, f2, evaluate=False)) == \
+    assert prntr(SymmetricDifference(f1, f2, evaluate=False)) == \
     '<mrow><mfenced close="}" open="{"><mn>1</mn><mn>3</mn><mi>x</mi>'\
     '</mfenced><mo>&#x2206;</mo><mfenced close="}" open="{"><mn>2</mn>'\
     '<mn>4</mn><mi>y</mi></mfenced></mrow>'
+
+    A = FiniteSet(a)
+    B = FiniteSet(b)
+    C = FiniteSet(c)
+    D = FiniteSet(d)
+
+    U1 = Union(A, B, evaluate=False)
+    U2 = Union(C, D, evaluate=False)
+    I1 = Intersection(A, B, evaluate=False)
+    I2 = Intersection(C, D, evaluate=False)
+    C1 = Complement(A, B, evaluate=False)
+    C2 = Complement(C, D, evaluate=False)
+    D1 = SymmetricDifference(A, B, evaluate=False)
+    D2 = SymmetricDifference(C, D, evaluate=False)
+    # XXX ProductSet does not support evaluate keyword
+    P1 = ProductSet(A, B)
+    P2 = ProductSet(C, D)
+
+    assert prntr(Intersection(A, U2, evaluate=False)) == \
+        '<mrow><mfenced close="}" open="{"><mi>a</mi></mfenced>' \
+        '<mo>&#x2229;</mo><mfenced><mrow><mfenced close="}" open="{">' \
+        '<mi>c</mi></mfenced><mo>&#x222A;</mo><mfenced close="}" open="{">' \
+        '<mi>d</mi></mfenced></mrow></mfenced></mrow>'
+    assert prntr(Intersection(U1, U2, evaluate=False)) == \
+        '<mrow><mfenced><mrow><mfenced close="}" open="{"><mi>a</mi>' \
+        '</mfenced><mo>&#x222A;</mo><mfenced close="}" open="{"><mi>b</mi>' \
+        '</mfenced></mrow></mfenced><mo>&#x2229;</mo><mfenced><mrow>' \
+        '<mfenced close="}" open="{"><mi>c</mi></mfenced><mo>&#x222A;</mo>' \
+        '<mfenced close="}" open="{"><mi>d</mi></mfenced></mrow></mfenced>' \
+        '</mrow>'
+    assert prntr(Intersection(C1, C2, evaluate=False)) == \
+        '<mrow><mfenced><mrow><mfenced close="}" open="{"><mi>a</mi>' \
+        '</mfenced><mo>&#x2216;</mo><mfenced close="}" open="{"><mi>b</mi>' \
+        '</mfenced></mrow></mfenced><mo>&#x2229;</mo><mfenced><mrow>' \
+        '<mfenced close="}" open="{"><mi>c</mi></mfenced><mo>&#x2216;</mo>' \
+        '<mfenced close="}" open="{"><mi>d</mi></mfenced></mrow></mfenced>' \
+        '</mrow>'
+    assert prntr(Intersection(D1, D2, evaluate=False)) == \
+        '<mrow><mfenced><mrow><mfenced close="}" open="{"><mi>a</mi>' \
+        '</mfenced><mo>&#x2206;</mo><mfenced close="}" open="{"><mi>b</mi>' \
+        '</mfenced></mrow></mfenced><mo>&#x2229;</mo><mfenced><mrow>' \
+        '<mfenced close="}" open="{"><mi>c</mi></mfenced><mo>&#x2206;</mo>' \
+        '<mfenced close="}" open="{"><mi>d</mi></mfenced></mrow></mfenced>' \
+        '</mrow>'
+    assert prntr(Intersection(P1, P2, evaluate=False)) == \
+        '<mrow><mfenced><mrow><mfenced close="}" open="{"><mi>a</mi>' \
+        '</mfenced><mo>&#x00d7;</mo><mfenced close="}" open="{"><mi>b</mi>' \
+        '</mfenced></mrow></mfenced><mo>&#x2229;</mo><mfenced><mrow>' \
+        '<mfenced close="}" open="{"><mi>c</mi></mfenced><mo>&#x00d7;</mo>' \
+        '<mfenced close="}" open="{"><mi>d</mi></mfenced></mrow></mfenced>' \
+        '</mrow>'
 
 
 def test_print_logic():
