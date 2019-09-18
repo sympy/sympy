5fbab8cb05795b542a1c0f6ebfd6543c3e4db142
arun <ysarun1999@gmail.com>
2019-09-18 20:26:47 +0530

    modify tests

--- sympy/functions/special/tests/test_beta_functions.py
+++ sympy/functions/special/tests/test_beta_functions.py
@@ -13,8 +13,8 @@ def test_beta():
     assert expand_func(beta(x, y) - beta(y, x)) == 0  # Symmetric
     assert expand_func(beta(x, y)) == expand_func(beta(x, y + 1) + beta(x + 1, y)).simplify()
 
-    assert diff(beta(x, y), x) == beta(x, y)*(digamma(x) - digamma(x + y))
-    assert diff(beta(x, y), y) == beta(x, y)*(digamma(y) - digamma(x + y))
+    assert diff(beta(x, y), x) == beta(x, y)*(polygamma(0, x) - polygamma(0, x + y))
+    assert diff(beta(x, y), y) == beta(x, y)*(polygamma(0, y) - polygamma(0, x + y))
 
     assert conjugate(beta(x, y)) == beta(conjugate(x), conjugate(y))
 
