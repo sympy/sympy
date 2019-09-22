dec1e0fb05b394085ae5dd9cf5b3f34d6b356e68
S.Y. Lee <sylee957@gmail.com>
2019-09-22 23:03:23 +0900

    Add return type checking

--- sympy/core/tests/test_arit.py
+++ sympy/core/tests/test_arit.py
@@ -1663,6 +1663,7 @@ def test_Mod():
     # modular exponentiation
     assert Mod(Pow(4, 13, evaluate=False), 497) == Mod(Pow(4, 13), 497)
     assert Mod(Pow(2, 10000000000, evaluate=False), 3) == 1
+    assert isinstance(Mod(Pow(2, 10000000000, evaluate=False), 3), Integer)
     assert Mod(Pow(32131231232, 9**10**6, evaluate=False),10**12) == pow(32131231232,9**10**6,10**12)
     assert Mod(Pow(33284959323, 123**999, evaluate=False),11**13) == pow(33284959323,123**999,11**13)
     assert Mod(Pow(78789849597, 333**555, evaluate=False),12**9) == pow(78789849597,333**555,12**9)
