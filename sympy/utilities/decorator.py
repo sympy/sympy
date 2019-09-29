8012efe636d90104a50e19f9ae871b00b3c2790d
Denys Rybalka <rybalka.denis@gmail.com>
2019-09-29 13:28:49 +0200

    `memoize_property` now caches value locally

--- sympy/utilities/decorator.py
+++ sympy/utilities/decorator.py
@@ -226,13 +226,19 @@ def public(obj):
     return obj
 
 
-def memoize_property(storage):
-    """Create a property, where the lookup is stored in ``storage``"""
-    def decorator(method):
-        name = method.__name__
-        def wrapper(self):
-            if name not in storage:
-                storage[name] = method(self)
-            return storage[name]
-        return property(update_wrapper(wrapper, method))
-    return decorator
+def memoize_property(propfunc):
+    """Property decorator that caches the value of potentially expensive
+    `propfunc` after the first evaluation. The cached value is stored in
+    the corresponding property name with an attached underscore."""
+    attrname = '_' + propfunc.__name__
+    sentinel = object()
+
+    @wraps(propfunc)
+    def accessor(self):
+        val = getattr(self, attrname, sentinel)
+        if val is sentinel:
+            val = propfunc(self)
+            setattr(self, attrname, val)
+        return val
+
+    return property(accessor)
