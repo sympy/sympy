950843405a7309b03ba6ab5255759de6b891ec83
Kenneth Emeka Odoh <kenneth.odoh@gmail.com>
2019-09-29 08:25:20 -0700

    refactor code using reviewer's feedback

--- sympy/utilities/tests/diagnose_debug_logs.py
+++
@@ -1,3 +0,0 @@
-from sympy import symbols, cos, integrate
-x = symbols("x")
-y = integrate((1-cos(x))/x, x)
--- sympy/utilities/tests/test_debug_logs.py
+++ sympy/utilities/tests/test_debug_logs.py
@@ -1,41 +1,14 @@
-"""
-A way to check if debugging works.
-"""
-from __future__ import print_function
-import subprocess
 import sys
-import os
-import inspect
-from os.path import abspath, dirname, join, normpath
-from contextlib import contextmanager
+from subprocess import Popen, PIPE
 
-@contextmanager
-def environ(env):
-    """
-    set environment variables
-    :param env: environment variable to modify
-    """
-    original_environ = os.environ.copy()
-    os.environ.update(env)
-    yield
-    os.environ = original_environ # reset the environment variable to prevent side effect
-
-def test_debug_logs():
-    my_filename = abspath(inspect.getfile(inspect.currentframe()))
-    my_dirname = dirname(my_filename)
-    diagnose_debug_logs_filename = join(my_dirname, 'diagnose_debug_logs.py')
-    diagnose_debug_logs_filename = normpath(diagnose_debug_logs_filename)
-    with environ({'SYMPY_DEBUG': 'True'}):
-        output = subprocess.check_output(['python', diagnose_debug_logs_filename])
-    assert check_debug_content(output), "There are problems or debug flag is not set \n"
-
-def check_debug_content(log):
-    """
-    check for token that is representative of our log
-    :param log: delog log as an input
-    return: True if it is debug, False otherwise
-    """
-    lower_case_log = log.lower()
-    if b"debug" in lower_case_log or b"rule" in lower_case_log:
-        return True
-    return False
+def test_debug_output():
+    env = {'SYMPY_DEBUG':'True'}
+    cmd = 'from sympy import *; x = Symbol("x"); print(integrate((1-cos(x))/x, x))'
+    cmdline = [sys.executable, '-c', cmd]
+    proc = Popen(cmdline, env=env, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    out = out.decode('ascii') # utf-8?
+    err = err.decode('ascii')
+    print (err)
+    expected = 'substituted: -x*(cos(x) - 1), u: 1/x, u_var: _u'
+    assert expected in err
