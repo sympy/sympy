5e636187f18c8377ef2a6babf2a125566144ddfb
Aaron Meurer <asmeurer@gmail.com>
2019-10-04 17:06:24 -0600

    Add mathjax_config to the docs
    See https://github.com/sympy/sphinx-math-dollar/pull/12.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -44,6 +44,14 @@ autodoc_inherit_docstrings = False
 # though it says 2.7.5.
 mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-AMS_HTML-full'
 
+# See https://www.sympy.org/sphinx-math-dollar/
+mathjax_config = {
+    'tex2jax': {
+        'inlineMath': [ ["\\(","\\)"] ],
+        'displayMath': [["\\[","\\]"] ],
+    },
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
