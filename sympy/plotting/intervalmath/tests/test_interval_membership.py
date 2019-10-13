c9977f6706656670f2f4f0dc99514c63f8f27197
S.Y. Lee <sylee957@gmail.com>
2019-10-13 23:20:40 +0900

    Add tests for consistency of regional Not

--- sympy/plotting/intervalmath/tests/test_interval_membership.py
+++ sympy/plotting/intervalmath/tests/test_interval_membership.py
@@ -1,4 +1,7 @@
+from sympy.core.symbol import Symbol
+from sympy.plotting.intervalmath import interval
 from sympy.plotting.intervalmath.interval_membership import intervalMembership
+from sympy.plotting.experimental_lambdify import experimental_lambdify
 from sympy.utilities.pytest import raises
 
 
@@ -31,6 +34,42 @@ def test_equivalence():
     b = intervalMembership(True, False)
     assert (a == b) is True
     assert (a != b) is False
+
+
+def test_not():
+    x = Symbol('x')
+
+    r1 = x > -1
+    r2 = x <= -1
+
+    i = interval
+
+    f1 = experimental_lambdify((x,), r1)
+    f2 = experimental_lambdify((x,), r2)
+
+    tt = i(-0.1, 0.1, is_valid=True)
+    tn = i(-0.1, 0.1, is_valid=None)
+    tf = i(-0.1, 0.1, is_valid=False)
+
+    assert f1(tt) == ~f2(tt)
+    assert f1(tn) == ~f2(tn)
+    assert f1(tf) == ~f2(tf)
+
+    nt = i(0.9, 1.1, is_valid=True)
+    nn = i(0.9, 1.1, is_valid=None)
+    nf = i(0.9, 1.1, is_valid=False)
+
+    assert f1(nt) == ~f2(nt)
+    assert f1(nn) == ~f2(nn)
+    assert f1(nf) == ~f2(nf)
+
+    ft = i(1.9, 2.1, is_valid=True)
+    fn = i(1.9, 2.1, is_valid=None)
+    ff = i(1.9, 2.1, is_valid=False)
+
+    assert f1(ft) == ~f2(ft)
+    assert f1(fn) == ~f2(fn)
+    assert f1(ff) == ~f2(ff)
 
 
 def test_boolean():
