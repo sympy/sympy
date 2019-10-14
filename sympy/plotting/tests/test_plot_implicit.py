283ef3b51464a1df1a66f87654af10625e059e66
S.Y. Lee <sylee957@gmail.com>
2019-10-14 13:12:14 +0900

    Add image comparison tests

--- sympy/plotting/tests/test_plot_implicit.py
+++ sympy/plotting/tests/test_plot_implicit.py
@@ -7,6 +7,8 @@ from sympy.utilities.pytest import skip, warns
 from sympy.external import import_module
 from sympy.utilities.tmpfiles import TmpFileManager, cleanup_tmp_files
 
+import os
+
 #Set plots not to show
 unset_show()
 
@@ -80,3 +82,47 @@ def test_matplotlib():
             TmpFileManager.cleanup()
     else:
         skip("Matplotlib not the default backend")
+
+
+def test_region_and():
+    matplotlib = import_module('matplotlib', min_module_version='1.1.0', catch=(RuntimeError,))
+    if not matplotlib:
+        skip("Matplotlib not the default backend")
+
+    from matplotlib.testing.compare import compare_images
+    test_directory = os.path.dirname(os.path.abspath(__file__))
+
+    try:
+        temp_dir = mkdtemp()
+        TmpFileManager.tmp_folder(temp_dir)
+
+        x, y = symbols('x y')
+
+        r1 = (x - 1)**2 + y**2 < 2
+        r2 = (x + 1)**2 + y**2 < 2
+
+        test_filename = tmp_file(dir=temp_dir, name="test_region_and")
+        cmp_filename = os.path.join(test_directory, "test_region_and.png")
+        p = plot_implicit(r1 & r2, x, y)
+        p.save(test_filename)
+        compare_images(cmp_filename, test_filename, 0.005)
+
+        test_filename = tmp_file(dir=temp_dir, name="test_region_or")
+        cmp_filename = os.path.join(test_directory, "test_region_or.png")
+        p = plot_implicit(r1 | r2, x, y)
+        p.save(test_filename)
+        compare_images(cmp_filename, test_filename, 0.005)
+
+        test_filename = tmp_file(dir=temp_dir, name="test_region_not")
+        cmp_filename = os.path.join(test_directory, "test_region_not.png")
+        p = plot_implicit(~r1, x, y)
+        p.save(test_filename)
+        compare_images(cmp_filename, test_filename, 0.005)
+
+        test_filename = tmp_file(dir=temp_dir, name="test_region_xor")
+        cmp_filename = os.path.join(test_directory, "test_region_xor.png")
+        p = plot_implicit(r1 ^ r2, x, y)
+        p.save(test_filename)
+        compare_images(cmp_filename, test_filename, 0.005)
+    finally:
+        TmpFileManager.cleanup()
