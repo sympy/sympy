86908a27f42d3497b9e613b0ab03f527fd7fe5d6
Aaron Meurer <asmeurer@gmail.com>
2019-10-15 11:34:24 -0600

    Fix the name of the SymPyTestResults namedtuple in the test runner
    This fixes the Sphinx documentation not being able to find it.

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -957,8 +957,7 @@ def split_list(l, split, density=None):
     return l[int(lower_frac*len(l)) : int(higher_frac*len(l))]
 
 from collections import namedtuple
-SymPyTestResults = namedtuple('TestResults', 'failed attempted')
-
+SymPyTestResults = namedtuple('SymPyTestResults', 'failed attempted')
 
 def sympytestfile(filename, module_relative=True, name=None, package=None,
              globs=None, verbose=None, report=True, optionflags=0,
