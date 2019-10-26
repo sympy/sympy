31fde4fc725da301ed4e868cb883fa76e420555a
S.Y. Lee <sylee957@gmail.com>
2019-10-26 23:48:56 +0900

    Fix doctests

--- sympy/utilities/lambdify.py
+++ sympy/utilities/lambdify.py
@@ -583,16 +583,43 @@ def lambdify(args, expr, modules=None, printer=None, use_imps=True,
 
     >>> import tensorflow as tf
     >>> from sympy import Max, sin
+
     >>> f = Max(x, sin(x))
     >>> func = lambdify(x, f, 'tensorflow')
+
+    After tensorflow v2, eager execution is enabled by default.
+    This line is for compatibility.
+
+    >>> tf.compat.v1.enable_eager_execution()
+
+    A tf.EagerTensor representing the result of the calculation
+
     >>> result = func(tf.constant(1.0))
-    >>> print(result) # a tf.Tensor representing the result of the calculation
+    >>> print(result)
+    tf.Tensor(1.0, shape=(), dtype=float32)
+    >>> print(result.__class__)
+    <class 'tensorflow.python.framework.ops.EagerTensor'>
+
+    >>> result.numpy()
+    1.0
+
+    If you disable eager execution
+
+    >>> tf.compat.v1.disable_eager_execution()
+
+    A tf.Tensor representing the result of the calculation
+
+    >>> result = func(tf.constant(1.0))
+    >>> print(result)
     Tensor("Maximum:0", shape=(), dtype=float32)
+    >>> print(result.__class__)
+    <class 'tensorflow.python.framework.ops.Tensor'>
+
     >>> sess = tf.compat.v1.Session()
     >>> sess.run(result) # compute result
     1.0
     >>> var = tf.Variable(1.0)
-    >>> sess.run(tf.global_variables_initializer())
+    >>> sess.run(tf.compat.v1.global_variables_initializer())
     >>> sess.run(func(var)) # also works for tf.Variable and tf.Placeholder
     1.0
     >>> tensor = tf.constant([[1.0, 2.0], [3.0, 4.0]]) # works with any shape tensor
