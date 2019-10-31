87f3385b6f336e6e9ecf6bd7f206b741f1986f20
Jonathan Warner <warnerjon12@gmail.com>
2019-10-30 18:19:13 -0600

    Updated test for issue #10047

--- sympy/solvers/tests/test_inequalities.py
+++ sympy/solvers/tests/test_inequalities.py
@@ -391,6 +391,7 @@ def test_issue_10047():
     # issue 10047: this must remain an inequality, not True, since if x
     # is not real the inequality is invalid
     assert solve(sin(x) < 2) == (x <= oo)
+    assert solveset(sin(x) < 2, domain=S.Reals) == S.Reals
 
     # with PR 16956, (x <= oo) autoevaluates when x is extended_real
     # assert solve(sin(x) < 2) == True
