9b0da0a8c226d45b3c80a92daec6af3e35745477
Francesco Bonazzi <franz.bonazzi@gmail.com>
2019-11-04 13:53:01 +0100

    correct floating points

--- sympy/physics/units/systems/cgs.py
+++ sympy/physics/units/systems/cgs.py
@@ -60,10 +60,10 @@ cgs_gauss.set_quantity_scale_factor(ampere, speed_of_light*statcoulomb/second/10
 cgs_gauss.set_quantity_scale_factor(volt, speed_of_light*statvolt/10**6)
 cgs_gauss.set_quantity_scale_factor(weber, 10**8*maxwell)
 cgs_gauss.set_quantity_scale_factor(tesla, 10**4*gauss)
-cgs_gauss.set_quantity_scale_factor(debye, 10**(-18)*statcoulomb*centimeter)
+cgs_gauss.set_quantity_scale_factor(debye, One/10**18*statcoulomb*centimeter)
 cgs_gauss.set_quantity_scale_factor(oersted, sqrt(gram/centimeter)/second)
 cgs_gauss.set_quantity_scale_factor(ohm, 10**9/speed_of_light**2*second/centimeter)
-cgs_gauss.set_quantity_scale_factor(farad, 10**(-9)*speed_of_light**2*centimeter)
+cgs_gauss.set_quantity_scale_factor(farad, One/10**9*speed_of_light**2*centimeter)
 cgs_gauss.set_quantity_scale_factor(henry, 10**9/speed_of_light**2/centimeter*second**2)
 
 # Coulomb's constant:
