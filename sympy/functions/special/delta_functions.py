65ddabf1ebb81030caf392f88c95bd022d2fcf00
Lauren Glattly <laurenglattly@gmail.com>
2019-11-05 17:20:26 -0500

    copy edit functions/special/delta_functions

--- sympy/functions/special/delta_functions.py
+++ sympy/functions/special/delta_functions.py
@@ -20,23 +20,27 @@ class DiracDelta(Function):
     """
     The DiracDelta function and its derivatives.
 
-    DiracDelta is not an ordinary function. It can be rigorously defined either
-    as a distribution or as a measure.
+    Explanation
+    ===========
 
-    DiracDelta only makes sense in definite integrals, and in particular, integrals
-    of the form ``Integral(f(x)*DiracDelta(x - x0), (x, a, b))``, where it equals
-    ``f(x0)`` if ``a <= x0 <= b`` and ``0`` otherwise. Formally, DiracDelta acts
-    in some ways like a function that is ``0`` everywhere except at ``0``,
-    but in many ways it also does not. It can often be useful to treat DiracDelta
-    in formal ways, building up and manipulating expressions with delta functions
-    (which may eventually be integrated), but care must be taken to not treat it
-    as a real function.
-    SymPy's ``oo`` is similar. It only truly makes sense formally in certain contexts
-    (such as integration limits), but SymPy allows its use everywhere, and it tries to be
-    consistent with operations on it (like ``1/oo``), but it is easy to get into trouble
-    and get wrong results if ``oo`` is treated too much like a number.
-    Similarly, if DiracDelta is treated too much like a function, it is easy to get wrong
-    or nonsensical results.
+    `DiracDelta <http://mathworld.wolfram.com/DeltaFunction.html>`_ is not an
+    ordinary function. It can be rigorously defined either as a distribution or
+    as a measure.
+
+    DiracDelta only makes sense in definite integrals, and in particular,
+    integrals of the form ``Integral(f(x)*DiracDelta(x - x0), (x, a, b))``,
+    where it equals ``f(x0)`` if ``a <= x0 <= b`` and ``0`` otherwise. Formally,
+    DiracDelta acts in some ways like a function that is ``0`` everywhere except
+    at ``0``, but in many ways it also does not. It can often be useful to treat
+    DiracDelta in formal ways, building up and manipulating expressions with
+    delta functions (which may eventually be integrated), but care must be taken
+    to not treat it as a real function. SymPy's ``oo`` is similar. It only
+    truly makes sense formally in certain contexts (such as integration limits),
+    but SymPy allows its use everywhere, and it tries to be consistent with
+    operations on it (like ``1/oo``), but it is easy to get into trouble and get
+    wrong results if ``oo`` is treated too much like a number. Similarly, if
+    DiracDelta is treated too much like a function, it is easy to get wrong or
+    nonsensical results.
 
     DiracDelta function has the following properties:
 
@@ -48,7 +52,7 @@ class DiracDelta(Function):
        Where ``x_i``-s are the roots of ``g``
     5) ``DiracDelta(-x) = DiracDelta(x)``
 
-    Derivatives of ``k``-th order of DiracDelta have the following property:
+    Derivatives of ``k``-th order of DiracDelta have the following properties:
 
     6) ``DiracDelta(x, k) = 0``, for all ``x != 0``
     7) ``DiracDelta(-x, k) = -DiracDelta(x, k)`` for odd ``k``
@@ -91,10 +95,6 @@ class DiracDelta(Function):
     sympy.simplify.simplify.simplify, is_simple
     sympy.functions.special.tensor_functions.KroneckerDelta
 
-    References
-    ==========
-
-    .. [1] http://mathworld.wolfram.com/DeltaFunction.html
     """
 
     is_real = True
@@ -103,12 +103,15 @@ class DiracDelta(Function):
         """
         Returns the first derivative of a DiracDelta Function.
 
-        The difference between ``diff()`` and ``fdiff()`` is:-
-        ``diff()`` is the user-level function and ``fdiff()`` is an object method.
-        ``fdiff()`` is just a convenience method available in the ``Function`` class.
-        It returns the derivative of the function without considering the chain rule.
-        ``diff(function, x)`` calls ``Function._eval_derivative`` which in turn calls
-        ``fdiff()`` internally to compute the derivative of the function.
+        Explanation
+        ===========
+
+        The difference between ``diff()`` and ``fdiff()`` is: ``diff()`` is the
+        user-level function and ``fdiff()`` is an object method. ``fdiff()`` is
+        a convenience method available in the ``Function`` class. It returns
+        the derivative of the function without considering the chain rule.
+        ``diff(function, x)`` calls ``Function._eval_derivative`` which in turn
+        calls ``fdiff()`` internally to compute the derivative of the function.
 
         Examples
         ========
@@ -144,11 +147,14 @@ class DiracDelta(Function):
         Returns a simplified form or a value of DiracDelta depending on the
         argument passed by the DiracDelta object.
 
-        The ``eval()`` method is automatically called when the ``DiracDelta`` class
-        is about to be instantiated and it returns either some simplified instance
-        or the unevaluated instance depending on the argument passed. In other words,
-        ``eval()`` method is not needed to be called explicitly, it is being called
-        and evaluated once the object is called.
+        Explanation
+        ===========
+
+        The ``eval()`` method is automatically called when the ``DiracDelta``
+        class is about to be instantiated and it returns either some simplified
+        instance or the unevaluated instance depending on the argument passed.
+        In other words, ``eval()`` method is not needed to be called explicitly,
+        it is being called and evaluated once the object is called.
 
         Examples
         ========
@@ -215,33 +221,37 @@ class DiracDelta(Function):
         return self.expand(diracdelta=True, wrt=x)
 
     def _eval_expand_diracdelta(self, **hints):
-        """Compute a simplified representation of the function using
-           property number 4. Pass wrt as a hint to expand the expression
-           with respect to a particular variable.
+        """
+        Compute a simplified representation of the function using
+        property number 4. Pass ``wrt`` as a hint to expand the expression
+        with respect to a particular variable.
 
-           wrt is:
+        Explanation
+        ===========
 
-           - a variable with respect to which a DiracDelta expression will
-           get expanded.
+        ``wrt`` is:
 
-           Examples
-           ========
+        - a variable with respect to which a DiracDelta expression will
+        get expanded.
 
-           >>> from sympy import DiracDelta
-           >>> from sympy.abc import x, y
+        Examples
+        ========
 
-           >>> DiracDelta(x*y).expand(diracdelta=True, wrt=x)
-           DiracDelta(x)/Abs(y)
-           >>> DiracDelta(x*y).expand(diracdelta=True, wrt=y)
-           DiracDelta(y)/Abs(x)
+        >>> from sympy import DiracDelta
+        >>> from sympy.abc import x, y
 
-           >>> DiracDelta(x**2 + x - 2).expand(diracdelta=True, wrt=x)
-           DiracDelta(x - 1)/3 + DiracDelta(x + 2)/3
+        >>> DiracDelta(x*y).expand(diracdelta=True, wrt=x)
+        DiracDelta(x)/Abs(y)
+        >>> DiracDelta(x*y).expand(diracdelta=True, wrt=y)
+        DiracDelta(y)/Abs(x)
 
-           See Also
-           ========
+        >>> DiracDelta(x**2 + x - 2).expand(diracdelta=True, wrt=x)
+        DiracDelta(x - 1)/3 + DiracDelta(x + 2)/3
 
-           is_simple, Diracdelta
+        See Also
+        ========
+
+        is_simple, Diracdelta
 
         """
         from sympy.polys.polyroots import roots
@@ -280,36 +290,36 @@ class DiracDelta(Function):
         return self
 
     def is_simple(self, x):
-        """is_simple(self, x)
+        """
+        Tells whether the argument(args[0]) of DiracDelta is a linear
+        expression in *x*.
 
-           Tells whether the argument(args[0]) of DiracDelta is a linear
-           expression in x.
+        Examples
+        ========
 
-           x can be:
+        >>> from sympy import DiracDelta, cos
+        >>> from sympy.abc import x, y
 
-           - a symbol
+        >>> DiracDelta(x*y).is_simple(x)
+        True
+        >>> DiracDelta(x*y).is_simple(y)
+        True
 
-           Examples
-           ========
+        >>> DiracDelta(x**2 + x - 2).is_simple(x)
+        False
 
-           >>> from sympy import DiracDelta, cos
-           >>> from sympy.abc import x, y
+        >>> DiracDelta(cos(x)).is_simple(x)
+        False
 
-           >>> DiracDelta(x*y).is_simple(x)
-           True
-           >>> DiracDelta(x*y).is_simple(y)
-           True
+        Parameters
+        ==========
 
-           >>> DiracDelta(x**2 + x - 2).is_simple(x)
-           False
+        x : can be a symbol
 
-           >>> DiracDelta(cos(x)).is_simple(x)
-           False
+        See Also
+        ========
 
-           See Also
-           ========
-
-           sympy.simplify.simplify.simplify, DiracDelta
+        sympy.simplify.simplify.simplify, DiracDelta
 
         """
         p = self.args[0].as_poly(x)
@@ -318,25 +328,26 @@ class DiracDelta(Function):
         return False
 
     def _eval_rewrite_as_Piecewise(self, *args, **kwargs):
-        """Represents DiracDelta in a Piecewise form
+        """
+        Represents DiracDelta in a piecewise form.
 
-           Examples
-           ========
+        Examples
+        ========
 
-           >>> from sympy import DiracDelta, Piecewise, Symbol, SingularityFunction
-           >>> x = Symbol('x')
+        >>> from sympy import DiracDelta, Piecewise, Symbol, SingularityFunction
+        >>> x = Symbol('x')
 
-           >>> DiracDelta(x).rewrite(Piecewise)
-           Piecewise((DiracDelta(0), Eq(x, 0)), (0, True))
+        >>> DiracDelta(x).rewrite(Piecewise)
+        Piecewise((DiracDelta(0), Eq(x, 0)), (0, True))
 
-           >>> DiracDelta(x - 5).rewrite(Piecewise)
-           Piecewise((DiracDelta(0), Eq(x - 5, 0)), (0, True))
+        >>> DiracDelta(x - 5).rewrite(Piecewise)
+        Piecewise((DiracDelta(0), Eq(x - 5, 0)), (0, True))
 
-           >>> DiracDelta(x**2 - 5).rewrite(Piecewise)
+        >>> DiracDelta(x**2 - 5).rewrite(Piecewise)
            Piecewise((DiracDelta(0), Eq(x**2 - 5, 0)), (0, True))
 
-           >>> DiracDelta(x - 5, 4).rewrite(Piecewise)
-           DiracDelta(x - 5, 4)
+        >>> DiracDelta(x - 5, 4).rewrite(Piecewise)
+        DiracDelta(x - 5, 4)
 
         """
         if len(args) == 1:
@@ -344,7 +355,8 @@ class DiracDelta(Function):
 
     def _eval_rewrite_as_SingularityFunction(self, *args, **kwargs):
         """
-        Returns the DiracDelta expression written in the form of Singularity Functions.
+        Returns the DiracDelta expression written in the form of Singularity
+        Functions.
 
         """
         from sympy.solvers import solve
@@ -377,9 +389,14 @@ class DiracDelta(Function):
 
 
 class Heaviside(Function):
-    """Heaviside Piecewise function
+    """
+    Heaviside Piecewise function.
 
-    Heaviside function has the following properties [1]_:
+    Explanation
+    ===========
+
+    `Heaviside function <http://mathworld.wolfram.com/
+    HeavisideStepFunction.html>`_ has the following properties:
 
     1) ``diff(Heaviside(x),x) = DiracDelta(x)``
                         ``( 0, if x < 0``
@@ -387,14 +404,18 @@ class Heaviside(Function):
                         ``( 1, if x > 0``
     3) ``Max(0,x).diff(x) = Heaviside(x)``
 
-    .. [1] Regarding to the value at 0, Mathematica defines ``H(0) = 1``,
-           but Maple uses ``H(0) = undefined``.  Different application areas
-           may have specific conventions.  For example, in control theory, it
-           is common practice to assume ``H(0) == 0`` to match the Laplace
-           transform of a DiracDelta distribution.
+    Regarding to the value at 0, Mathematica defines ``H(0) = 1``, but Maple
+    uses ``H(0) = undefined``. Different application areas may have specific
+    conventions. For example, in control theory, it is common practice to
+    assume ``H(0) == 0`` to match the Laplace transform of a DiracDelta
+    distribution.
 
-    To specify the value of Heaviside at x=0, a second argument can be given.
-    Omit this 2nd argument or pass ``None`` to recover the default behavior.
+    To specify the value of Heaviside at ``x=0``, a second argument can be
+    given. Omit this 2nd argument or pass ``None`` to recover the default
+    behavior.
+
+    Examples
+    ========
 
     >>> from sympy import Heaviside, S
     >>> from sympy.abc import x
@@ -417,8 +438,7 @@ class Heaviside(Function):
     References
     ==========
 
-    .. [2] http://mathworld.wolfram.com/HeavisideStepFunction.html
-    .. [3] http://dlmf.nist.gov/1.16#iv
+    .. [1] http://dlmf.nist.gov/1.16#iv
 
     """
 
@@ -464,11 +484,14 @@ class Heaviside(Function):
         Returns a simplified form or a value of Heaviside depending on the
         argument passed by the Heaviside object.
 
-        The ``eval()`` method is automatically called when the ``Heaviside`` class
-        is about to be instantiated and it returns either some simplified instance
-        or the unevaluated instance depending on the argument passed. In other words,
-        ``eval()`` method is not needed to be called explicitly, it is being called
-        and evaluated once the object is called.
+        Explanation
+        ===========
+
+        The ``eval()`` method is automatically called when the ``Heaviside``
+        class is about to be instantiated and it returns either some simplified
+        instance or the unevaluated instance depending on the argument passed.
+        In other words, ``eval()`` method is not needed to be called explicitly,
+        it is being called and evaluated once the object is called.
 
         Examples
         ========
@@ -518,22 +541,23 @@ class Heaviside(Function):
             raise ValueError("Function defined only for Real Values. Complex part: %s  found in %s ." % (repr(im(arg)), repr(arg)) )
 
     def _eval_rewrite_as_Piecewise(self, arg, H0=None, **kwargs):
-        """Represents Heaviside in a Piecewise form
+        """
+        Represents Heaviside in a Piecewise form.
 
-           Examples
-           ========
+        Examples
+        ========
 
-           >>> from sympy import Heaviside, Piecewise, Symbol, pprint
-           >>> x = Symbol('x')
+        >>> from sympy import Heaviside, Piecewise, Symbol, pprint
+        >>> x = Symbol('x')
 
-           >>> Heaviside(x).rewrite(Piecewise)
-           Piecewise((0, x < 0), (Heaviside(0), Eq(x, 0)), (1, x > 0))
+        >>> Heaviside(x).rewrite(Piecewise)
+        Piecewise((0, x < 0), (Heaviside(0), Eq(x, 0)), (1, x > 0))
 
-           >>> Heaviside(x - 5).rewrite(Piecewise)
-           Piecewise((0, x - 5 < 0), (Heaviside(0), Eq(x - 5, 0)), (1, x - 5 > 0))
+        >>> Heaviside(x - 5).rewrite(Piecewise)
+        Piecewise((0, x - 5 < 0), (Heaviside(0), Eq(x - 5, 0)), (1, x - 5 > 0))
 
-           >>> Heaviside(x**2 - 1).rewrite(Piecewise)
-           Piecewise((0, x**2 - 1 < 0), (Heaviside(0), Eq(x**2 - 1, 0)), (1, x**2 - 1 > 0))
+        >>> Heaviside(x**2 - 1).rewrite(Piecewise)
+        Piecewise((0, x**2 - 1 < 0), (Heaviside(0), Eq(x**2 - 1, 0)), (1, x**2 - 1 > 0))
 
         """
         if H0 is None:
@@ -545,9 +569,14 @@ class Heaviside(Function):
         return Piecewise((0, arg < 0), (H0, Eq(arg, 0)), (1, arg > 0))
 
     def _eval_rewrite_as_sign(self, arg, H0=None, **kwargs):
-        """Represents the Heaviside function in the form of sign function.
+        """
+        Represents the Heaviside function in the form of sign function.
+
+        Explanation
+        ===========
+
         The value of the second argument of Heaviside must specify Heaviside(0)
-        = 1/2 for rewritting as sign to be strictly equivalent.  For easier
+        = 1/2 for rewritting as sign to be strictly equivalent. For easier
         usage, we also allow this rewriting when Heaviside(0) is undefined.
 
         Examples
@@ -593,7 +622,8 @@ class Heaviside(Function):
 
     def _eval_rewrite_as_SingularityFunction(self, args, **kwargs):
         """
-        Returns the Heaviside expression written in the form of Singularity Functions.
+        Returns the Heaviside expression written in the form of Singularity
+        Functions.
 
         """
         from sympy.solvers import solve
