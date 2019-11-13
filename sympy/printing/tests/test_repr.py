5b193ecabfdbcee2fde45ffde6091efff703f8c9
aditisingh2362 <aditisingh2362@gmail.com>
2019-11-14 00:16:32 +0530

    printing: Add Finally block in Permutation repr test
    Added Finally block so that even if the tests fail, Permutation.print_cyclic gets reset.

--- sympy/printing/tests/test_repr.py
+++ sympy/printing/tests/test_repr.py
@@ -304,5 +304,7 @@ def test_Permutation():
     import_stmt = "from sympy.combinatorics import Permutation"
     print_cyclic = Permutation.print_cyclic
     Permutation.print_cyclic = True
-    sT(Permutation(1, 2), "Permutation(1, 2)", import_stmt)
-    Permutation.print_cyclic = print_cyclic
+    try:
+    	sT(Permutation(1, 2), "Permutation(1, 2)", import_stmt)
+    finally:
+    	Permutation.print_cyclic = print_cyclic
