326c87e7b5bc4efaf1bda4e9e9136ce31bcb8631
czgdp1807 <singh.23@iitj.ac.in>
2019-11-16 14:38:38 +0530

    shifted controlsys to physics

--- sympy/physics/__init__.py
+++ sympy/physics/__init__.py
@@ -3,4 +3,6 @@ A module that helps solving problems in physics
 """
 
 from . import units
+from .controlsys import *
 from .matrices import mgamma, msigma, minkowski_tensor, mdft
+
--- sympy/physics/controlsys/__init__.py
+++ sympy/physics/controlsys/__init__.py
@@ -1,8 +1,8 @@
-"""controlsys is a package for control systems theory. It currently only
+"""Controlsys is a package for control systems theory. It currently only
 supports linear, time invariant systems via the module 'lti'. You can
 create linear control systems in state space or transfer function model
 representation, transform these representation into one another,
 interconnect systems, evaluate the systems etc.
 """
 
-from sympy.controlsys.lti import StateSpaceModel, TransferFunctionModel
+from .lti import StateSpaceModel, TransferFunctionModel
