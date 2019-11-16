ca3b3396aae56c58b5a42dad8a0acc796006635d
czgdp1807 <singh.23@iitj.ac.in>
2019-11-16 14:39:23 +0530

    imports modified

--- sympy/physics/__init__.py
+++ sympy/physics/__init__.py
@@ -3,6 +3,6 @@ A module that helps solving problems in physics
 """
 
 from . import units
-from .controlsys import *
+from . import controlsys
 from .matrices import mgamma, msigma, minkowski_tensor, mdft
 
