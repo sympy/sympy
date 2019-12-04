4d8b8ebdc2b2823ad068955114ca84836371289e
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-12-04 23:13:14 +0000

    Remove unicode in test_coset_table.py

--- sympy/utilities/quality_unicode.py
+++ sympy/utilities/quality_unicode.py
@@ -46,7 +46,6 @@ unicode_whitelist = [
     r'*/sympy/printing/pretty/tests/test_pretty.py',
     r'*/sympy/printing/tests/test_preview.py',
     r'*/combinatorics/free_groups.py',
-    r'*/combinatorics/tests/test_coset_table.py',
     # These 3 files in liealgebras have functions that return unicode strings
     # and tests for those functions.
     r'*/liealgebras/type_g.py',
