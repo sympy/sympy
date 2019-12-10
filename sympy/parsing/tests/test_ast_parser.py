b5f862d5de4f81680d5f89df27720d7164edcdbb
davefern <57910017+davefern@users.noreply.github.com>
2019-12-10 09:42:21 -0500

    New test for the bug reported #17913
    Wrote a simple test in a try block for issue #17913 and put some more details in comments for others to work on. Will keep trying to fix but wanted the test to be there for others who may be looking at working on it and for perpetuity.

--- sympy/parsing/tests/test_ast_parser.py
+++ sympy/parsing/tests/test_ast_parser.py
@@ -16,3 +16,28 @@ def test_parse_expr():
     # tests Transform.visit_Name
     parse_expr('Rational(1, 2)', {}) == S(1)/2
     parse_expr('a', {'a': a}) == a
+    
+    #tests Transform.visit_Lambda
+    #Currently still causes a bug
+    try:
+        parse_expr('lambda a: 2',{}) == a
+    except:
+        """Lambda AST Parsing still has issue with recognizing expression type in compile()
+        We can dump a, the transformed node expression and we see the expected:
+        
+        Expression(
+            body=Call(
+                func=Name(id='Lambda', ctx=Load()), 
+                args=[Tuple(elts=[arg(arg='a', annotation=None, type_comment=None)], ctx=Load()), 
+                      Call(func=Name(id='Integer', ctx=Load()), args=[Constant(value=2, kind=None)], keywords=[])
+                ]#Args
+                , keywords=[]
+            )#Body Call
+        )#Expression
+        
+        But when we pass it into compile we get a type error claiming this object is a AST arg object.
+        
+        expected some sort of expr, but got <_ast.arg object at 0x10075da00>
+        
+        May be an issue with fix_missing_locations?"""
+        print("Lambda AST Parsing Error)
