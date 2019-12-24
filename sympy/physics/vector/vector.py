e701b5e6d31dedb85dd04ba0d3d45f197f64c347
Smit-create <55887635+Smit-create@users.noreply.github.com>
2019-12-25 01:02:33 +0530

    Inherited vector

--- sympy/physics/vector/vector.py
+++ sympy/physics/vector/vector.py
@@ -1,13 +1,14 @@
 from sympy.core.backend import (S, sympify, expand, sqrt, Add, zeros,
     ImmutableMatrix as Matrix)
 from sympy import trigsimp
+from sympy.core.evalf import EvalfMixin
 from sympy.core.compatibility import unicode
 from sympy.utilities.misc import filldedent
 
 __all__ = ['Vector']
 
 
-class Vector(object):
+class Vector(EvalfMixin):
     """The class used to define vectors.
 
     It along with ReferenceFrame are the building blocks of describing a
