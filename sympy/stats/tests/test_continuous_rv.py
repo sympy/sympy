acdd13d641ab9a1f90a03bae4e68e541e9510bd2
Smit-create <55887635+Smit-create@users.noreply.github.com>
2019-12-28 16:54:20 +0530

    Added test_powerfunction

--- sympy/stats/tests/test_continuous_rv.py
+++ sympy/stats/tests/test_continuous_rv.py
@@ -900,6 +900,32 @@ def test_pareto_numeric():
     # Skewness tests too slow. Try shortcutting function?
 
 
+def test_PowerFunction():
+    alpha = Symbol("alpha", nonpositive=True)
+    a, b = symbols('a, b', real=True)
+    raises (ValueError, lambda: PowerFunction('x', alpha, a, b))
+
+    a, b = symbols('a, b', real=False)
+    raises (ValueError, lambda: PowerFunction('x', alpha, a, b))
+
+    alpha = Symbol("alpha", positive=True)
+    a, b = symbols('a, b', real=True)
+    raises (ValueError, lambda: PowerFunction('x', alpha, 5, 2))
+
+    X = PowerFunction('X', 2, a, b)
+    assert density(X)(z) == (-2*a + 2*z)/(-a + b)**2
+    assert cdf(X)(z) == Piecewise((a**2/(a**2 - 2*a*b + b**2) - 
+        2*a*z/(a**2 - 2*a*b + b**2) + z**2/(a**2 - 2*a*b + b**2), a <= z), (0, True))
+
+    X = PowerFunction('X', 2, 0, 1)
+    assert density(X)(z) == 2*z
+    assert cdf(X)(z) == Piecewise((z**2, z >= 0), (0,True))
+    assert E(X) == Rational(2,3)
+    assert P(X < 0) == 0
+    assert P(X < 1/2) == Rational(1,4)
+    assert P(X < 1) == 1
+
+
 def test_raised_cosine():
     mu = Symbol("mu", real=True)
     s = Symbol("s", positive=True)
