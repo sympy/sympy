42e63bbd5aa366be330c95f0a6a52b7517c5b0f1
Francesco Bonazzi <franz.bonazzi@gmail.com>
2019-12-29 12:37:39 +0100

    Fixing error in PartialDerivative operator for tensor valence

---
+++ doc/src/modules/tensor/toperators.rst
@@ -0,0 +1,5 @@
+
+Tensor Operators
+================
+
+.. automodule:: sympy.tensor.toperators
--- sympy/tensor/tests/test_tensor_operators.py
+++ sympy/tensor/tests/test_tensor_operators.py
@@ -1,3 +1,5 @@
+from sympy.utilities.pytest import raises
+
 from sympy.tensor.toperators import PartialDerivative
 from sympy.tensor.tensor import TensorIndexType, tensor_indices, TensorHead, \
     tensor_heads
@@ -15,39 +17,54 @@ A, B, C, D = tensor_heads("A B C D", [L])
 H = TensorHead("H", [L, L])
 
 
+def test_invalid_partial_derivative_valence():
+    raises(ValueError, lambda: PartialDerivative(C(j), D(-j)))
+    raises(ValueError, lambda: PartialDerivative(C(-j), D(j)))
+
+
 def test_tensor_partial_deriv():
     # Test flatten:
-    expr = PartialDerivative(PartialDerivative(A(i), A(j)), A(-i))
+    expr = PartialDerivative(PartialDerivative(A(i), A(j)), A(i))
     assert expr.expr == A(L_0)
-    assert expr.variables == (A(j), A(-L_0))
+    assert expr.variables == (A(j), A(L_0))
 
     expr1 = PartialDerivative(A(i), A(j))
     assert expr1.expr == A(i)
     assert expr1.variables == (A(j),)
 
     expr2 = A(i)*PartialDerivative(H(k, -i), A(j))
-    assert expr2.get_indices() == [L_0, k, -L_0, j]
+    assert expr2.get_indices() == [L_0, k, -L_0, -j]
+
+    expr2b = A(i)*PartialDerivative(H(k, -i), A(-j))
+    assert expr2b.get_indices() == [L_0, k, -L_0, j]
 
     expr3 = A(i)*PartialDerivative(B(k)*C(-i) + 3*H(k, -i), A(j))
-    assert expr3.get_indices() == [L_0, k, -L_0, j]
+    assert expr3.get_indices() == [L_0, k, -L_0, -j]
 
-    expr4 = (A(i) + B(i))*PartialDerivative(C(-j), D(j))
-    assert expr4.get_indices() == [i, -L_0, L_0]
+    expr4 = (A(i) + B(i))*PartialDerivative(C(j), D(j))
+    assert expr4.get_indices() == [i, L_0, -L_0]
+
+    expr4b = (A(i) + B(i))*PartialDerivative(C(-j), D(-j))
+    assert expr4b.get_indices() == [i, -L_0, L_0]
 
     expr5 = (A(i) + B(i))*PartialDerivative(C(-i), D(j))
-    assert expr5.get_indices() == [L_0, -L_0, j]
+    assert expr5.get_indices() == [L_0, -L_0, -j]
 
 
 def test_replace_arrays_partial_derivative():
     x, y, z, t = symbols("x y z t")
 
-    expr = PartialDerivative(A(i), A(j))
-    assert expr.replace_with_arrays({A(i): [x, y]}, [i, j]) == Array([[1, 0], [0, 1]])
+    expr = PartialDerivative(A(i), A(-j))
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, [i, j]) == Array([[1, 0], [0, 1]])
 
-    expr = PartialDerivative(A(i), A(-i))
+    expr = PartialDerivative(A(i), A(j))
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, [i, j]) == Array([[1, 0], [0, 1]])
+    assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, -1)}, [i, j]) == Array([[1, 0], [0, -1]])
+
+    expr = PartialDerivative(A(i), A(i))
     assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, []) == 2
     assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, -1)}, []) == 0
 
-    expr = PartialDerivative(A(-i), A(i))
+    expr = PartialDerivative(A(-i), A(-i))
     assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, 1)}, []) == 2
     assert expr.replace_with_arrays({A(i): [x, y], L: diag(1, -1)}, []) == 0
