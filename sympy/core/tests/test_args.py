914fa6d00271282066bfcbeea61f52f803021055
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2020-01-04 12:45:11 +0530

    Revert "Merge branch 'powerfunction' into master"
    This reverts commit d554941f5289e55baa195c27227a4c34354d70a9, reversing
    changes made to 8db290e3dc35e30fa3a1b1ca2bd04cc602f4a73f.

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -1442,9 +1442,6 @@ def test_sympy__stats__crv_types__ParetoDistribution():
     from sympy.stats.crv_types import ParetoDistribution
     assert _test_args(ParetoDistribution(1, 1))
 
-def test_sympy__stats__crv_types__PowerFunctionDistribution():
-    from sympy.stats.crv_types import PowerFunctionDistribution
-    assert _test_args(PowerFunctionDistribution(2,0,1))
 
 def test_sympy__stats__crv_types__QuadraticUDistribution():
     from sympy.stats.crv_types import QuadraticUDistribution
