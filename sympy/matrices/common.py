012209a97a4659b81c95f0ad35ae096f903b4c94
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-01-05 02:24:59 +0000

    Remove unneeded imports in matrices

--- sympy/matrices/common.py
+++ sympy/matrices/common.py
@@ -15,12 +15,10 @@ from sympy.core.basic import Atom
 from sympy.core.compatibility import (
     Iterable, as_int, is_sequence, range, reduce)
 from sympy.core.decorators import call_highest_priority
-from sympy.core.expr import Expr
 from sympy.core.singleton import S
 from sympy.core.symbol import Symbol
 from sympy.core.sympify import sympify
 from sympy.functions import Abs
-from sympy.polys import cancel, together
 from sympy.simplify import simplify as _simplify, dotprodsimp as _dotprodsimp
 from sympy.utilities.exceptions import SymPyDeprecationWarning
 from sympy.utilities.iterables import flatten
