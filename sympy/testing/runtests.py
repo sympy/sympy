163f910db350a5a35d4515e21cd4f31b2d3c8bbe
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-01-07 17:17:10 +0000

    Siloence mypy warnings in runtests.py

--- sympy/testing/runtests.py
+++ sympy/testing/runtests.py
@@ -94,7 +94,7 @@ def _indent(s, indent=4):
     return re.sub('(?m)^(?!$)', indent*' ', s)
 
 
-pdoctest._indent = _indent
+pdoctest._indent = _indent  # type: ignore
 
 # override reporter to maintain windows and python3
 
@@ -292,8 +292,8 @@ def run_all_tests(test_args=(), test_kwargs=None,
 
         # Examples
         print()
-        sys.path.append("examples")
-        from all import run_examples  # examples/all.py
+        sys.path.append("examples")   # examples/all.py
+        from all import run_examples  # type: ignore
         if not run_examples(*examples_args, **examples_kwargs):
             tests_successful = False
 
@@ -1838,12 +1838,17 @@ class SymPyDocTestRunner(DocTestRunner):
             if clear_globs:
                 test.globs.clear()
 
+
 # We have to override the name mangled methods.
-SymPyDocTestRunner._SymPyDocTestRunner__patched_linecache_getlines = \
-    DocTestRunner._DocTestRunner__patched_linecache_getlines
-SymPyDocTestRunner._SymPyDocTestRunner__run = DocTestRunner._DocTestRunner__run
-SymPyDocTestRunner._SymPyDocTestRunner__record_outcome = \
-    DocTestRunner._DocTestRunner__record_outcome
+monkeypatched_methods = [
+    'patched_linecache_getlines',
+    'run',
+    'record_outcome'
+]
+for method in monkeypatched_methods:
+    oldname = '_DocTestRunner__' + method
+    newname = '_SymPyDocTestRunner__' + method
+    setattr(SymPyDocTestRunner, newname, getattr(DocTestRunner, oldname))
 
 
 class SymPyOutputChecker(pdoctest.OutputChecker):
