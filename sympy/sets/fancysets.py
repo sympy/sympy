edf1a98018ff25979f907428d32fefd0e0953a1a
Namannimmo <namangera15@gmail.com>
2020-01-11 23:11:45 +0530

    Further changes in fancysets

--- sympy/sets/fancysets.py
+++ sympy/sets/fancysets.py
@@ -920,13 +920,7 @@ class Range(Set):
                 x >= self.inf if self.inf in self else x > self.inf,
                 x <= self.sup if self.sup in self else x < self.sup)
 
-
-# Using range from compatibility above.
-if PY3:
-    converter[range] = lambda r: Range(r.start, r.stop, r.step)
-else:
-    converter[range] = lambda r: Range(*r.__reduce__()[1])
-
+converter[range] = lambda r: Range(r.start, r.stop, r.step)
 
 def normalize_theta_set(theta):
     """
