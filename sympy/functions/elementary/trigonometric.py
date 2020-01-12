c893840d624429913f639eea65d89629524566f9
Sachin Agarwal <sachinagarwal0499@gmail.com>
2020-01-12 18:12:36 +0530

    Made minor changes

--- sympy/functions/elementary/trigonometric.py
+++ sympy/functions/elementary/trigonometric.py
@@ -118,7 +118,7 @@ def _peeloff_pi(arg):
             if p is S.Pi and K.is_Rational:
                 break
             K = a.coeff(S.Pi)
-            if K.is_rational:
+            if K and K.is_rational:
                 break
 
     else:
