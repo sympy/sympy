ee0abfd1e495676fac364616415d6167e359a6a3
Tomasz Pytel <tompytel@gmail.com>
2020-01-15 13:09:24 -0300

    ...

--- sympy/matrices/utilities.py
+++ sympy/matrices/utilities.py
@@ -17,13 +17,12 @@ def _is_zero_after_expand_mul(x):
 
 from sympy.simplify.simplify import simplify as _simplify, dotprodsimp as _dotprodsimp
 
-_noplamb          = lambda x: x
 _simpopts_default = {}
 _simpopts_none    = {'simplify': False, 'simpfunc': _noplamb, 'dotprodsimp': False}
 _simpopts_full    = {'simplify': True, 'simpfunc': _simplify, 'dotprodsimp': True}
 _simpopts_dotprod = {'dotprodsimp': True}
 
-_simpopts = {
+_simpopts_dicts   = {
   None:      _simpopts_default,
   False:     _simpopts_none,
   True:      _simpopts_full,
@@ -33,5 +32,23 @@ _simpopts = {
   'dotprod': _simpopts_dotprod,
 }
 
-def _get_simpopts(simpopts, *args, simplify_as_func=False, **kwargs):
-  pass
+def _get_simpopts(simpopts, *args, simplify_as_func=False, dotprodsimp_as_func=False, **kwargs):
+  simpopts = _simpopts_dicts.get(simpopts, simpopts) # if 'simpopts' is dict it is used
+  ret      = []
+
+  for opt, default in zip(args[::2], args[1::2]):
+    val = kwargs.get(opt, ret) # ret used as sentinel
+
+    if val is ret:
+      val = simpopts.get(opt, default)
+
+    if opt == 'simplify':
+      if simplify_as_func and not callable(_simplify):
+        val = (lambda x: x, _simplify)[bool(val)]
+    elif opt == 'dotprodsimp':
+      if dotprodsimp_as_func and not callable(_dotprodsimp):
+        val = (lambda x: x, _dotprodsimp)[bool(val)]
+
+    ret.append(val)
+
+  return ret
