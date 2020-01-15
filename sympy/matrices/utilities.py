da3a41e7af19d4bcf59eb6a0008123b70325c6ee
Tomasz Pytel <tompytel@gmail.com>
2020-01-15 12:54:10 -0300

    ...

--- sympy/matrices/utilities.py
+++ sympy/matrices/utilities.py
@@ -12,3 +12,26 @@ def _is_zero_after_expand_mul(x):
     """Tests by expand_mul only, suitable for polynomials and rational
     functions."""
     return expand_mul(x) == 0
+
+
+
+from sympy.simplify.simplify import simplify as _simplify, dotprodsimp as _dotprodsimp
+
+_noplamb          = lambda x: x
+_simpopts_default = {}
+_simpopts_none    = {'simplify': False, 'simpfunc': _noplamb, 'dotprodsimp': False}
+_simpopts_full    = {'simplify': True, 'simpfunc': _simplify, 'dotprodsimp': True}
+_simpopts_dotprod = {'dotprodsimp': True}
+
+_simpopts = {
+  None:      _simpopts_default,
+  False:     _simpopts_none,
+  True:      _simpopts_full,
+  'default': _simpopts_default,
+  'none':    _simpopts_none,
+  'full':    _simpopts_full,
+  'dotprod': _simpopts_dotprod,
+}
+
+def _get_simpopts(simpopts, *args, simplify_as_func=False, **kwargs):
+  pass
