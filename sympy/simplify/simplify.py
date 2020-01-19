7cd0b548a3004a2056baa17df3ecbff17c9141dd
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-01-20 03:01:55 +0900

    Allow user defined keyword arguments to simplify
    Before this commit, only 'ratio', 'measure', 'rational', 'inverse',
    and 'doit' were passed to '_eval_simplify' method of expression.
    This commit allows every parameters to be passed to it, making
    simplification of user-defined object more flexible.

--- sympy/simplify/simplify.py
+++ sympy/simplify/simplify.py
@@ -544,12 +544,13 @@ def simplify(expr, ratio=1.7, measure=count_ops, rational=False, inverse=False, 
         return shorter(rv, collect_abs(rv))
 
     expr = sympify(expr)
-    kwargs = dict(
+    _kwargs = dict(
         ratio=kwargs.get('ratio', ratio),
         measure=kwargs.get('measure', measure),
         rational=kwargs.get('rational', rational),
         inverse=kwargs.get('inverse', inverse),
         doit=kwargs.get('doit', doit))
+    kwargs.update(_kwargs)
     # no routine for Expr needs to check for is_zero
     if isinstance(expr, Expr) and expr.is_zero and expr*0 == S.Zero:
         return S.Zero
