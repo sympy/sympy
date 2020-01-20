3e66f99545d62a1e7af6a84254fb33f76935ebe2
eward <etkewa@gmail.com>
2020-01-20 16:42:50 -0700

    remove private functions from this file

--- sympy/solvers/diophantine/__init__.py
+++ sympy/solvers/diophantine/__init__.py
@@ -6,8 +6,7 @@ from .diophantine import diophantine, merge_solution, diop_solve, classify_diop,
     norm, holzer, \
     diop_general_pythagorean, diop_general_sum_of_squares, diop_general_sum_of_even_powers, partition, \
     prime_as_sum_of_two_squares, sum_of_three_squares, sum_of_four_squares, \
-    power_representation, sum_of_powers, pow_rep_recursive, sum_of_squares, _diop_ternary_quadratic_normal, \
-    _diop_general_sum_of_squares, _nint_or_floor, _odd, _even, _remove_gcd, _can_do_sum_of_squares
+    power_representation, sum_of_powers, pow_rep_recursive, sum_of_squares
 
 __all__ = [
     'diophantine', 'merge_solution', 'diop_solve', 'classify_diop', 'diop_linear', 'base_solution_linear',
@@ -20,6 +19,5 @@ __all__ = [
     'norm', 'holzer',
     'diop_general_pythagorean', 'diop_general_sum_of_squares', 'diop_general_sum_of_even_powers', 'partition',
     'prime_as_sum_of_two_squares', 'sum_of_three_squares', 'sum_of_four_squares',
-    'power_representation', 'sum_of_powers', 'pow_rep_recursive', 'sum_of_squares', '_diop_ternary_quadratic_normal',
-    '_diop_general_sum_of_squares', '_nint_or_floor', '_odd', '_even', '_remove_gcd', '_can_do_sum_of_squares'
+    'power_representation', 'sum_of_powers', 'pow_rep_recursive', 'sum_of_squares'
 ]
