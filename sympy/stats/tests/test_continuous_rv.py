2c3f339ee55e16e87b7bd4131a4949a624e9a65b
Smit Lunagariya <smitlunagariya.mat18@itbhu.ac.in>
2020-01-20 14:20:59 +0530

    conflict

--- sympy/stats/tests/test_continuous_rv.py
+++ sympy/stats/tests/test_continuous_rv.py
@@ -19,7 +19,7 @@ from sympy.stats import (P, E, where, density, variance, covariance, skewness, k
                          Pareto, PowerFunction, QuadraticU, RaisedCosine, Rayleigh, Reciprocal, ShiftedGompertz, StudentT,
                          Trapezoidal, Triangular, Uniform, UniformSum, VonMises, Weibull,
                          WignerSemicircle, Wald, correlation, moment, cmoment, smoment, quantile)
-from sympy.stats.crv_types import NormalDistribution, ContinuousDistributionHandmade
+from sympy.stats.crv_types import NormalDistribution, ExponentialDistribution, ContinuousDistributionHandmade
 from sympy.stats.crv import SingleContinuousPSpace
 from sympy.stats.joint_rv import JointPSpace
 from sympy.testing.pytest import raises, XFAIL, slow, skip
