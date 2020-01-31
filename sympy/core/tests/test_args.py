c47890eae9976e093d5a891b669e58dfb65c1834
Michal Grno <m93a.cz@gmail.com>
2020-02-01 00:36:34 +0100

    added orthogonal bra+ket to test_args

--- sympy/core/tests/test_args.py
+++ sympy/core/tests/test_args.py
@@ -3665,6 +3665,16 @@ def test_sympy__physics__quantum__state__StateBase():
     assert _test_args(StateBase(0))
 
 
+def test_sympy__physics__quantum__state__OrthogonalBra():
+    from sympy.physics.quantum.state import OrthogonalBra
+    assert _test_args(OrthogonalBra(0))
+
+
+def test_sympy__physics__quantum__state__OrthogonalKet():
+    from sympy.physics.quantum.state import OrthogonalKet
+    assert _test_args(OrthogonalKet(0))
+
+
 def test_sympy__physics__quantum__state__TimeDepBra():
     from sympy.physics.quantum.state import TimeDepBra
     assert _test_args(TimeDepBra('psi', 't'))
