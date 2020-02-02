dce80396f8610c16badc8a507c88a9ba5f175808
Mohit Balwani <mohitbalwani.ict17@gmail.com>
2020-02-02 14:10:09 +0530

    Added subscheck and test_subscheck

--- sympy/solvers/ode/__init__.py
+++ sympy/solvers/ode/__init__.py
@@ -1,6 +1,6 @@
-from .ode import (allhints, checkinfsol, checkodesol, classify_ode,
+from .ode import (allhints, checkinfsol, classify_ode,
         constantsimp, dsolve, homogeneous_order, infinitesimals)
-
+from .subscheck import checkodesol
 __all__ = [
     'allhints', 'checkinfsol', 'checkodesol', 'classify_ode', 'constantsimp',
     'dsolve', 'homogeneous_order', 'infinitesimals',
