01730253a846cda00212880498420382d6aaa384
Mohit Gupta <mohitgupta@gmail.com>
2020-02-05 16:16:29 +0530

    added spaces around '='

--- sympy/core/tests/test_function.py
+++ sympy/core/tests/test_function.py
@@ -1353,5 +1353,5 @@ def test_Derivative_free_symbols():
 
 
 def test_issue_10503():
-    f=exp(x**3)*cos(x**6)
+    f = exp(x**3)*cos(x**6)
     assert f.series(x, 0, 14) == 1 + x**3 + x**6/2 + x**9/6 - 11*x**12/24 + O(x**14)
