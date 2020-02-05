4af6c3178ccc324b869a5bca9e2ba0f373160564
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-02-05 17:02:03 +0000

    Add blank lines in ode/__init__.py

--- sympy/solvers/ode/__init__.py
+++ sympy/solvers/ode/__init__.py
@@ -2,6 +2,8 @@ from .ode import (allhints, checkinfsol, classify_ode,
         constantsimp, dsolve, homogeneous_order, infinitesimals)
 
 from .subscheck import checkodesol
+
+
 __all__ = [
     'allhints', 'checkinfsol', 'checkodesol', 'classify_ode', 'constantsimp',
     'dsolve', 'homogeneous_order', 'infinitesimals',
