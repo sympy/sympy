8f3ca2e907796d84c6015aef0cab05eb540ed693
Michal Grno <m93a.cz@gmail.com>
2020-02-12 23:50:40 +0100

    changed self.__class__ to self.func

--- sympy/core/relational.py
+++ sympy/core/relational.py
@@ -242,19 +242,19 @@ class Relational(Boolean, Expr, EvalfMixin):
         Calls the function for each side, passing the lhs/rhs as
         an argument, and then returns a new equation consisting of
         the returned values."""
-        return self.__class__(op(self.lhs), op(self.rhs))
+        return self.func(op(self.lhs), op(self.rhs))
 
     def applylhs(self, op):
         """Call the function on the lhs and return a new equation
         whose lhs is the result of that function and rhs is the same
         as in the original equation."""
-        return self.__class__(op(self.lhs), self.rhs)
+        return self.func(op(self.lhs), self.rhs)
 
     def applyrhs(self, op):
         """Call the function on the rhs and return a new equation
         whose rhs is the result of that function and lhs is the same
         as in the original equation."""
-        return self.__class__(self.lhs, op(self.rhs))
+        return self.func(self.lhs, op(self.rhs))
 
     def equals(self, other, failing_expression=False):
         """Return True if the sides of the relationship are mathematically
