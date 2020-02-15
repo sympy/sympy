fac964d9b02d3e6c6d6be7953c796e724529bef2
Maq_Owais <57854867+MaqOwais@users.noreply.github.com>
2020-02-15 20:36:13 +0530

    correction

--- sympy/series/tests/test_limitseq.py
+++ sympy/series/tests/test_limitseq.py
@@ -102,7 +102,7 @@ def test_accum_bounds():
     assert limit_seq((-1)**n, n) == AccumulationBounds(-1, 1)
     assert limit_seq(cos(pi*n), n) == AccumulationBounds(-1, 1)
     assert limit_seq(sin(pi*n/2)**2, n) == AccumulationBounds(0, 1)
-    assert limit_seq(sin(pi*n/2), n) == AccumulationBounds(-1, 1) 
+    assert limit_seq(sin(pi*n/2), n) == AccumulationBounds(-1, 1)
     assert limit_seq(2*(-3)**n/(n + 3**n), n) == AccumulationBounds(-2, 2)
     assert limit_seq(3*n/(n + 1) + 2*(-1)**n, n) == AccumulationBounds(1, 5)
     
