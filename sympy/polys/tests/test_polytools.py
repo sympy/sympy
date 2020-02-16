c8d0d87c2834e982d34f82d9831722f69cbc651c
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-02-16 00:33:03 +0000

    test(Poly): Fix poly tests for deprecation
    Fix Poly tests so that they test for deprecation warnings rather than
    TypeError. A previous commit made it so that mixing Poly with
    non-polynomial Expr would give TypeError but a subsequent commit
    introduced a deprecation warning instead. This commit fixes the tests to
    check for the deprecation warning.

--- sympy/polys/tests/test_polytools.py
+++ sympy/polys/tests/test_polytools.py
@@ -650,7 +650,8 @@ def test_Poly_add():
     assert Poly(0, x, y) + Poly(1, x, y) == Poly(1, x, y)
 
     assert Poly(1, x) + x == Poly(x + 1, x)
-    raises(TypeError, lambda: Poly(1, x) + sin(x))
+    with warns_deprecated_sympy():
+        Poly(1, x) + sin(x)
 
     assert Poly(x, x) + 1 == Poly(x + 1, x)
     assert 1 + Poly(x, x) == Poly(x + 1, x)
@@ -666,7 +667,8 @@ def test_Poly_sub():
     assert Poly(0, x, y) - Poly(1, x, y) == Poly(-1, x, y)
 
     assert Poly(1, x) - x == Poly(1 - x, x)
-    raises(TypeError, lambda: Poly(1, x) - sin(x))
+    with warns_deprecated_sympy():
+        Poly(1, x) - sin(x)
 
     assert Poly(x, x) - 1 == Poly(x - 1, x)
     assert 1 - Poly(x, x) == Poly(1 - x, x)
@@ -682,7 +684,8 @@ def test_Poly_mul():
     assert Poly(4, x, y) * Poly(2, x, y) == Poly(8, x, y)
 
     assert Poly(1, x) * x == Poly(x, x)
-    raises(TypeError, lambda: Poly(1, x) * sin(x))
+    with warns_deprecated_sympy():
+        Poly(1, x) * sin(x)
 
     assert Poly(x, x) * 2 == Poly(2*x, x)
     assert 2 * Poly(x, x) == Poly(2*x, x)
