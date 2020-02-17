d793be552a8ce7b87319656c878da6e7cf5b723c
Milan Jolly <milan.cs16@iitp.ac.in>
2020-02-17 21:56:28 +0530

    combinatorics: Added seed value for using random
    Added seed value for using random in test_schur_number.py and
    increased the number of tests to 5.

--- sympy/combinatorics/tests/test_schur_number.py
+++ sympy/combinatorics/tests/test_schur_number.py
@@ -12,18 +12,21 @@ def _sum_free_test(subset):
                 raise AssertionError("This subset is not sum free")
 
 def test_schur_number():
-    n = random.randint(1, 1000)
-    result = schur_partition(n)
-    t = 0
-    numbers = []
-    for item in result:
-        _sum_free_test(item)
-        """
-        Checks if the occurance of all numbers  is exactly one
-        """
-        t += len(item)
-        for l in item:
-            if l in numbers:
-                raise AssertionError("Every number must exist once")
-            numbers.append(l)
-    assert n == t
+    random.seed(1000)
+
+    for _ in range(5):
+        n = random.randint(1, 1000)
+        result = schur_partition(n)
+        t = 0
+        numbers = []
+        for item in result:
+            _sum_free_test(item)
+            """
+            Checks if the occurance of all numbers  is exactly one
+            """
+            t += len(item)
+            for l in item:
+                if l in numbers:
+                    raise AssertionError("Every number must exist once")
+                numbers.append(l)
+        assert n == t
