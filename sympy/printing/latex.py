22c1179e3001f66b43f8969c61986304e89c49c2
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-03-04 02:46:47 +0530

    Update latex.py

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -786,15 +786,8 @@ class LatexPrinter(Printer):
 
                 symbols.insert(0, r"\, d%s" % self._print(symbol))
 
-        if any(_coeff_isneg(i) for i in expr.args):
-            return r"%s %s%s" % (tex, self.parenthesize(expr.function,
-                                                    PRECEDENCE["Mul"],
-                                                    is_neg=True,
-                                                    strict=True),
-                             "".join(symbols))
         return r"%s %s%s" % (tex, self.parenthesize(expr.function,
                                                     PRECEDENCE["Mul"],
-                                                    is_neg=False,
                                                     strict=True),
                              "".join(symbols))
 
