f912814bc579eecaba68868d7e71ba901e5ff945
sbt4104 <sthorat661@gmail.com>
2020-03-09 20:54:09 +0530

    whitespaces removed.

--- sympy/solvers/solvers.py
+++ sympy/solvers/solvers.py
@@ -1000,7 +1000,7 @@ def solve(f, *symbols, **flags):
             f[i] = fi
 
         if fi.has(binomial):
-            f[i] = fi.replace(lambda exp: binomial, lambda exp: expand_func(exp))
+            f[i] = fi.replace(lambda expr: binomial, lambda expr: expand_func(expr))
 
         if fi.is_Relational:
             return reduce_inequalities(f, symbols=symbols)
