0a9a3c9093030f7cc0c60d04b823f982779dc039
Chris Smith <smichr@gmail.com>
2020-03-11 16:07:32 -0500

    raise error if seq changed detected in uniq

--- sympy/utilities/iterables.py
+++ sympy/utilities/iterables.py
@@ -2088,8 +2088,12 @@ def has_variety(seq):
 def uniq(seq, result=None):
     """
     Yield unique elements from ``seq`` as an iterator. The second
-    parameter ``result``  is used internally; it is not necessary to pass
-    anything for this.
+    parameter ``result``  is used internally; it is not necessary
+    to pass anything for this.
+
+    Note: changing the sequence during iteration will change the
+    output or raise a RuntimeError (when not dealing with an
+    iterator whose length is not known).
 
     Examples
     ========
@@ -2107,14 +2111,26 @@ def uniq(seq, result=None):
     [[1], [2, 1]]
     """
     try:
+        n = len(seq)
+    except TypeError:
+        n = None
+    def check():
+        # check that size of seq did not change during iteration;
+        # if n == None the object won't support size changing, e.g.
+        # an iterator can't be changed
+        if n is not None and len(seq) != n:
+            raise RuntimeError('sequence changed size during iteration')
+    try:
         seen = set()
         result = result or []
         for i, s in enumerate(seq):
             if not (s in seen or seen.add(s)):
                 yield s
+                check()
     except TypeError:
         if s not in result:
             yield s
+            check()
             result.append(s)
         if hasattr(seq, '__getitem__'):
             for s in uniq(seq[i + 1:], result):
