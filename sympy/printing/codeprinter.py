d07ce80933b919d30ebbde6459a2cecd511d19ca
kom-bu <54900948+kom-bu@users.noreply.github.com>
2020-03-12 15:37:45 +0900

    Update codeprinter.py
    abolishing 'pow_paren' system in codeprinter.py

--- sympy/printing/codeprinter.py
+++ sympy/printing/codeprinter.py
@@ -464,7 +464,7 @@ class CodePrinter(StrPrinter):
         a = []  # items in the numerator
         b = []  # items that are in the denominator (if any)
 
-        pow_paren = []  # Will collect all pow with more than one base element and exp = -1
+        #pow_paren = []  # Will collect all pow with more than one base element and exp = -1
 
         if self.order not in ('old', 'none'):
             args = expr.as_ordered_factors()
@@ -478,8 +478,8 @@ class CodePrinter(StrPrinter):
                 if item.exp != -1:
                     b.append(Pow(item.base, -item.exp, evaluate=False))
                 else:
-                    if len(item.args[0].args) != 1 and isinstance(item.base, Mul):   # To avoid situations like #14160
-                        pow_paren.append(item)
+                    #if len(item.args[0].args) != 1 and isinstance(item.base, Mul):   # To avoid situations like #14160
+                    #    pow_paren.append(item)
                     b.append(Pow(item.base, -item.exp))
             else:
                 a.append(item)
@@ -490,9 +490,9 @@ class CodePrinter(StrPrinter):
         b_str = [self.parenthesize(x, prec) for x in b]
 
         # To parenthesize Pow with exp = -1 and having more than one Symbol
-        for item in pow_paren:
-            if item.base in b:
-                b_str[b.index(item.base)] = "(%s)" % b_str[b.index(item.base)]
+        #for item in pow_paren:
+        #    if item.base in b:
+        #        b_str[b.index(item.base)] = "(%s)" % b_str[b.index(item.base)]
 
         if not b:
             return sign + '*'.join(a_str)
