e2a3a8c5c455d26bd8d8a8ac10b524d4f0c00826
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-03-12 19:22:37 +0530

    followed PEP 8 style guide

--- sympy/physics/continuum_mechanics/beam.py
+++ sympy/physics/continuum_mechanics/beam.py
@@ -388,7 +388,8 @@ class Beam(object):
         Parameters
         ==========
         value : Sympifyable
-            The magnitude of an applied load for orders greater than or equal to 0 and slope for orders greater than 0.
+            The magnitude of an applied load for orders greater than or
+            equal to 0 and slope for orders greater than 0.
         start : Sympifyable
             The starting point of the applied load. For point moments and
             point forces this is the location of application.
@@ -457,7 +458,8 @@ class Beam(object):
         Parameters
         ==========
         value : Sympifyable
-            The magnitude of an applied load for orders greater than or equal to 0 and slope for orders greater than 0.
+            The magnitude of an applied load for orders greater than or
+            equal to 0 and slope for orders greater than 0.
         start : Sympifyable
             The starting point of the applied load. For point moments and
             point forces this is the location of application.
