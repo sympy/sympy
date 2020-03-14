1f6ceb0ea8c871611cd979fd01261453438198ae
Milan Jolly <milan.cs16@iitp.ac.in>
2020-03-15 01:38:33 +0530

    solvers: Added test cases for new function

--- sympy/solvers/ode/systems.py
+++ sympy/solvers/ode/systems.py
@@ -9,8 +9,8 @@ def _get_func_order(eqs, funcs):
     for func in funcs:
         if not order.get(func, False):
             max_order = 0
-            for i, eqs_ in enumerate(eqs):
-                order_ = ode_order(eqs, func)
+            for i, eq in enumerate(eqs):
+                order_ = ode_order(eq, func)
                 if max_order < order_:
                     max_order = order_
                     eq_no = i
@@ -29,6 +29,63 @@ def _get_func_order(eqs, funcs):
 
 
 def neq_nth_linear_constant_coeff_match(eqs, funcs, t):
+    r"""
+    Returns a dictionary with details of the eqs if every equation is constant coefficient
+    and linear else returns None
+
+    Explanation
+    ===========
+
+    This function takes the eqs, converts it into a form Ax = b where x is a vector of terms
+    containing dependent variables and their derivatives till their maximum order. If it is
+    possible to convert eqs into Ax = b, then all the equations in eqs are linear otherwise
+    they are non-linear.
+
+    To check if the equations are constant coefficient, we need to check if all the terms in
+    A obtained above are constant or not.
+
+    To check if the equations are homogeneous or not, we need to check if b is a zero matrix
+    or not.
+
+    Parameters
+    ==========
+
+    eqs: List
+        List of ODEs
+    funcs: List
+        List of dependent variables
+    t: Symbol
+        Independent variable of the equations in eqs
+
+    Returns
+    =======
+
+    match = {
+        'no_of_equation': len(eqs),
+        'eq': eqs,
+        'func': funcs,
+        'order': order,
+        'is_linear': is_linear,
+        'is_constant': is_constant,
+        'is_homogeneous': is_homogeneous,
+    }
+
+    Dict or None
+        Dict with values for keys:
+            1. no_of_equation: Number of equations
+            2. eq: The set of equations
+            3. func: List of dependent variables
+            4. order: A dictionary that gives the order of the
+                      dependent variable in eqs
+            5. is_linear: Boolean value indicating if the set of
+                          equations are linear or not.
+            6. is_constant: Boolean value indicating if the set of
+                          equations have constant coefficients or not.
+            7. is_homogeneous: Boolean value indicating if the set of
+                          equations are homogeneous or not.
+        This Dict is the answer returned if the eqs are linear and constant
+        coefficient. Otherwise, None is returned.
+    """
     from sympy.solvers.solveset import linear_eq_to_matrix
 
     # Error for i == 0 can be added but isn't for now
---
+++ sympy/solvers/ode/tests/test_systems.py
@@ -0,0 +1,47 @@
+from sympy import (symbols, Symbol, diff, Function, Derivative)
+from sympy.solvers.ode.systems import neq_nth_linear_constant_coeff_match
+
+def test_neq_nth_linear_constant_coeff_match():
+    x, y = symbols('x, y', cls=Function)
+    t = Symbol('t')
+    x1 = diff(x(t), t)
+    y1 = diff(y(t), t)
+    x2 = diff(x(t), t, t)
+    funcs = [x(t), y(t)]
+
+    eqs_1 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * t * x(t) + 3 * y(t) + t))
+    answer_1 = {'no_of_equation': 2,
+     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t),
+      -11*t*x(t) + t + 3*y(t) + 2*Derivative(y(t), t)),
+     'func': [x(t), y(t)],
+     'order': {x(t): 1, y(t): 1},
+     'is_linear': True,
+     'is_constant': False,
+     'is_homogeneous': False}
+    assert neq_nth_linear_constant_coeff_match(eqs_1, funcs, t) == answer_1
+
+    eqs_2 = (5 * (x1**2) + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * t * x(t) + 3 * y(t) + t))
+    answer_2 = None
+    assert neq_nth_linear_constant_coeff_match(eqs_2, funcs, t) is answer_2
+
+    eqs_3 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * x(t) + 3 * y(t)))
+    answer_3 = {'no_of_equation': 2,
+     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t),
+      -11*x(t) + 3*y(t) + 2*Derivative(y(t), t)),
+     'func': [x(t), y(t)],
+     'order': {x(t): 1, y(t): 1},
+     'is_linear': True,
+     'is_constant': True,
+     'is_homogeneous': True}
+    assert neq_nth_linear_constant_coeff_match(eqs_3, funcs, t) == answer_3
+
+    eqs_4 = (5 * x1 + 12 * x(t) - 6 * (y(t)) + x2, (2 * y1 - 11 * x(t) + 3 * y(t)))
+    answer_4 = {'no_of_equation': 2,
+     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t) + Derivative(x(t), (t, 2)),
+      -11*x(t) + 3*y(t) + 2*Derivative(y(t), t)),
+     'func': [x(t), y(t)],
+     'order': {x(t): 2, y(t): 1},
+     'is_linear': True,
+     'is_constant': True,
+     'is_homogeneous': True}
+    assert neq_nth_linear_constant_coeff_match(eqs_4, funcs, t) == answer_4
