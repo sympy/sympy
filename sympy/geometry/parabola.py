4ccb4a76745286d711b2d41d44189854cd2a8946
AyushGit123 <ayush.shukla2999@gmail.com>
2020-03-19 00:42:42 +0530

    This Pull request Fixes #18871 and maybe Fixes 15774 If this PR is accepted I can further extend the program to find out other things related to curve
    This function finds out the tangent for a General Curve at a Particular point (x1,y1)
    The program also tells whether the point lies inside the curve, outside the curve or on the curve
    If the particular point point lies on the curve the it displays the curve along with the tangent drawn on the curve at the point only
    I think this is the simplest approach to find the tangent and can be further extended to find out chord, director circle, etc
    <!-- BEGIN RELEASE NOTES -->
    I have used a one of most simple and fastest approach of finding the tangent on a curve at a given particular point 
    <!-- END RELEASE NOTES -->

--- sympy/geometry/parabola.py
+++ sympy/geometry/parabola.py
@@ -15,6 +15,7 @@ from sympy.geometry.point import Point, Point2D
 from sympy.geometry.line import Line, Line2D, Ray2D, Segment2D, LinearEntity3D
 from sympy.geometry.ellipse import Ellipse
 from sympy.functions import sign
+from sympy import *
 
 class Parabola(GeometrySet):
     """A parabolic GeometryEntity.
@@ -406,3 +407,30 @@ class Parabola(GeometrySet):
             vertex = Point(focus.args[0], focus.args[1] - self.p_parameter)
 
         return vertex
+def tangent():
+     x,y=symbols('x y')
+    print('Enter the parameters a,b,g,f,h,c of equation ax^2+by^2+2gx+2fy+2hxy+c=0')
+    a=int(input())
+    b=int(input())
+    g=int(input())
+    f=int(input())
+    h=int(input())
+    c=int(input())
+    P=Poly((x**2)*a+(y**2)*b+2*g*x+2*h*x*y+2*f*y+c,(x,y))
+    S=Eq((x**2)*a+(y**2)*b+(2*g*x)+(2*h*x*y)+(2*f*y),-c)
+    print('Enter the point')
+    x1=int(input())
+    y1=int(input())
+    S1=P(x1,y1)
+    if S1==0:
+        print('Point lies on the curve')
+    elif S1<0:
+        print('Point lies inside the curve')
+    else:
+        print('Point lies outside the curve')
+    
+    T=Eq((a*x1+g+h*y1)*x+(b*y1+f+h*x1)*y,-(c+(g*x1)+(f*y1)))
+    p1=plot_implicit(S,show=False)
+    p2=plot_implicit(T,show=False)
+    p1.extend(p2)
+    p1.show()
