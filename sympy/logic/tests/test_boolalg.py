a3155947dae4521735678cfd48174cd7b36cd5e9
Christopher Smith <smichr@gmail.com>
2020-03-20 07:48:06 -0500

    Update test_boolalg.py

--- sympy/logic/tests/test_boolalg.py
+++ sympy/logic/tests/test_boolalg.py
@@ -683,6 +683,8 @@ def test_is_literal():
     assert is_literal(Not(Q.zero(A))) is True
     assert is_literal(Or(A, B)) is False
     assert is_literal(And(Q.zero(A), Q.zero(B))) is False
+    assert is_literal(x < 3)
+    assert not is_literal(x + y < 3)
 
 
 def test_operators():
