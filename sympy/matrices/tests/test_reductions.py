7236fb62b323b7b14dab0b709ffbe88f0dd614ee
Arpan612 <f20180319@pilani.bits-pilani.ac.in>
2020-03-24 17:21:13 +0530

    Travis errors corrected

--- sympy/matrices/tests/test_reductions.py
+++ sympy/matrices/tests/test_reductions.py
@@ -1,13 +1,13 @@
-from sympy import symbols, simplify
+from sympy import symbols, simplify, I
 from sympy.matrices.common import _MinimalMatrix, _CastableMatrix
-from sympy.matrices.matrices import MatrixReductions, _find_reasonable_pivot_naive
+from sympy.matrices.matrices import MatrixReductions
 from sympy.testing.pytest import raises
 from sympy.matrices import Matrix, zeros
 from sympy.core.symbol import Symbol
 from sympy.core.numbers import Rational
 from sympy.functions.elementary.miscellaneous import sqrt
 from sympy.simplify.simplify import simplify
-from sympy.abc import a, b, c, d, e, f, x
+from sympy.abc import a, b, c, d, e, x
 
 class ReductionsOnlyMatrix(_MinimalMatrix, _CastableMatrix, MatrixReductions):
     pass
