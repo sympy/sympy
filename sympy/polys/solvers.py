60d0b26695e0cd77e131f3bd60a2f83490a14661
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-03-25 00:02:34 +0000

    feat(polys): Add DomainMatrix based on poly elements
    Split linear systems into connected components in solve_lin_sys. This
    should lead to big performance improvements for large sparse linear
    systems of the kind that often appear in heurisch.

--- sympy/polys/solvers.py
+++ sympy/polys/solvers.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function, division
 
+from sympy.utilities.iterables import connected_components
+
 from sympy.matrices import MutableDenseMatrix
 from sympy.polys.rings import sring
 from sympy.polys.polymatrix import DomainMatrix
@@ -23,6 +25,20 @@ def eqs_to_matrix(eqs, ring):
 
     return M
 
+def eqs_to_matrix2(eqs_coeffs, eqs_rhs, gens, domain):
+    sym2index = {x: n for n, x in enumerate(gens)}
+    nrows = len(eqs_coeffs)
+    ncols = len(gens) + 1
+    rows = [[domain.zero] * ncols for _ in range(nrows)]
+    for row, eq_coeff, eq_rhs in zip(rows, eqs_coeffs, eqs_rhs):
+        for sym, coeff in eq_coeff.items():
+            row[sym2index[sym]] = coeff
+        row[-1] = -eq_rhs
+
+    M = DomainMatrix(rows, (nrows, ncols), domain)
+
+    return M
+
 def sympy_eqs_to_ring(eqs, symbols):
     K, eqs_K = sring(eqs, symbols, field=True, extension=True)
     return eqs_K, K.to_domain()
@@ -39,31 +55,90 @@ def solve_lin_sys(eqs, ring, _raw=True):
 
     assert ring.domain.is_Field
 
+    eqs_dict = [dict(eq) for eq in eqs]
+
+    one_monom = ring.one.monoms()[0]
+    zero = ring.zero
+
+    eqs_rhs = []
+    eqs_coeffs = []
+    for eq_dict in eqs_dict:
+        eq_rhs = eq_dict.pop(one_monom, zero)
+        eq_coeffs = {ring.gens[monom.index(1)]:coeff for monom, coeff in eq_dict.items()}
+        if not eq_coeffs:
+            if not eq_rhs:
+                continue
+            else:
+                return None
+        eqs_rhs.append(eq_rhs)
+        eqs_coeffs.append(eq_coeffs)
+
+    result = _solve_lin_sys(eqs_coeffs, eqs_rhs, ring)
+
+    if result is not None and as_expr:
+
+        def to_sympy(x):
+            as_expr = getattr(x, 'as_expr', None)
+            if as_expr:
+                return as_expr()
+            else:
+                return ring.domain.to_sympy(x)
+
+        result = {to_sympy(sym): to_sympy(val) for sym, val in result.items()}
+
+    return result
+
+def _solve_lin_sys(eqs_coeffs, eqs_rhs, ring):
+
+    V = ring.gens
+    E = []
+    for eq_coeffs in eqs_coeffs:
+        syms = list(eq_coeffs)
+        E.extend(zip(syms[:-1], syms[1:]))
+    G = V, E
+
+    components = connected_components(G)
+
+    sym2comp = {}
+    for n, component in enumerate(components):
+        for sym in component:
+            sym2comp[sym] = n
+
+    subsystems = [([], []) for _ in range(len(components))]
+    for eq_coeff, eq_rhs in zip(eqs_coeffs, eqs_rhs):
+        sym = next(iter(eq_coeff), None)
+        sub_coeff, sub_rhs = subsystems[sym2comp[sym]]
+        sub_coeff.append(eq_coeff)
+        sub_rhs.append(eq_rhs)
+
+    sol = {}
+    for subsystem in subsystems:
+        subsol = _solve_lin_sys_component(subsystem[0], subsystem[1], ring)
+        if subsol is None:
+            return None
+        sol.update(subsol)
+
+    return sol
+
+
+def _solve_lin_sys_component(eqs_coeffs, eqs_rhs, ring):
+
     # transform from equations to matrix form
-    matrix = eqs_to_matrix(eqs, ring)
+    matrix = eqs_to_matrix2(eqs_coeffs, eqs_rhs, ring.gens, ring.domain)
 
     # solve by row-reduction
     echelon, pivots = matrix.rref()
 
     # construct the returnable form of the solutions
-    keys = ring.symbols if as_expr else ring.gens
+    keys = ring.symbols
 
     if pivots and pivots[-1] == len(keys):
         return None
 
-    def to_sympy(x):
-        as_expr = getattr(x, 'as_expr', None)
-        if as_expr:
-            return as_expr()
-        else:
-            return ring.domain.to_sympy(x)
-
     if len(pivots) == len(keys):
         sol = []
         for s in [row[-1] for row in echelon.rows]:
-            a = (s + ring.zero)
-            if as_expr:
-                a = to_sympy(a)
+            a = s + ring.zero
             sol.append(a)
         sols = dict(zip(keys, sol))
     else:
@@ -72,9 +147,7 @@ def solve_lin_sys(eqs, ring, _raw=True):
         echelon = echelon.rows
         for i, p in enumerate(pivots):
             v = echelon[i][-1] - sum(echelon[i][j]*g[j] for j in range(p+1, len(g)))
-            v = (v + ring.zero)
-            if as_expr:
-                v = to_sympy(v)
+            v = v + ring.zero
             sols[keys[p]] = v
 
     return sols
