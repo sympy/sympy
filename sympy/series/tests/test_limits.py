2a8529335444894e1375825f63ce5b70eb347207
Sachin Agarwal <sachinagarwal0499@gmail.com>
2020-03-26 22:06:49 +0530

    Fixes incorrect limit evaluation

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -612,6 +612,11 @@ def test_issue_14590():
     assert limit((x**3*((x + 1)/x)**x)/((x + 1)*(x + 2)*(x + 3)), x, oo) == exp(1)
 
 
+def test_issue_14393():
+    a, b = symbols('a b')
+    assert limit((x**b - y**b)/(x**a - y**a), x, y) == b*y**(-a)*y**b/a
+
+
 def test_issue_17431():
     assert limit(((n + 1) + 1) / (((n + 1) + 2) * factorial(n + 1)) *
                  (n + 2) * factorial(n) / (n + 1), n, oo) == 0
