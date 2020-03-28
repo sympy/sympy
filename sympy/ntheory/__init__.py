53e881c266d51ac65fed4ddbc93d447e0f64a5bd
Abhinav Anand <abhinav.anand2807@gmail.com>
2020-03-28 22:52:35 +0530

    basic index calculas algorithm

--- sympy/ntheory/__init__.py
+++ sympy/ntheory/__init__.py
@@ -25,6 +25,7 @@ from .continued_fraction import continued_fraction_periodic, \
     continued_fraction_convergents, continued_fraction
 from .digits import count_digits, digits, is_palindromic
 from .egyptian_fraction import egyptian_fraction
+from .index_calculus import index_calculus
 
 __all__ = [
     'nextprime', 'prevprime', 'prime', 'primepi', 'primerange', 'randprime',
@@ -59,4 +60,6 @@ __all__ = [
     'is_palindromic',
 
     'egyptian_fraction',
+
+    'index_calculus',
 ]
---
+++ sympy/ntheory/tests/test_index_calculus.py
@@ -0,0 +1,2 @@
+from sympy.ntheory import index_calculus
+assert index_calculus(37, 211, 18443, B=5, c=10) == 8500
