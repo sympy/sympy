0abcefe39d00f3ff52e7649747934772f4e5caf3
Milan Jolly <milan.cs16@iitp.ac.in>
2020-03-28 21:30:30 +0530

    solvers: Updated test cases in test_systems.py
    Since the function was updated to match the new solver being used,
    the test cases were to be updated as well.

--- sympy/solvers/ode/tests/test_systems.py
+++ sympy/solvers/ode/tests/test_systems.py
@@ -2,49 +2,59 @@ from sympy import (symbols, Symbol, diff, Function, Derivative, Matrix, Rational
 from sympy.solvers.ode.systems import neq_nth_linear_constant_coeff_match
 
 def test_neq_nth_linear_constant_coeff_match():
-    x, y = symbols('x, y', cls=Function)
+    x, y, z, w = symbols('x, y, z, w', cls=Function)
     t = Symbol('t')
     x1 = diff(x(t), t)
     y1 = diff(y(t), t)
+    z1 = diff(z(t), t)
+    w1 = diff(w(t), t)
     x2 = diff(x(t), t, t)
     funcs = [x(t), y(t)]
+    funcs_2 = funcs + [z(t), w(t)]
 
     eqs_1 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * t * x(t) + 3 * y(t) + t))
-    answer_1 = {'no_of_equation': 2,
-     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t),
-      -11*t*x(t) + t + 3*y(t) + 2*Derivative(y(t), t)),
-     'func': [x(t), y(t)],
-     'order': {x(t): 1, y(t): 1},
-     'is_linear': True,
-     'is_constant': False,
-     'is_homogeneous': False}
-    assert neq_nth_linear_constant_coeff_match(eqs_1, funcs, t) == answer_1
+    assert neq_nth_linear_constant_coeff_match(eqs_1, funcs, t) is None
 
     eqs_2 = (5 * (x1**2) + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * t * x(t) + 3 * y(t) + t))
-    answer_2 = None
-    assert neq_nth_linear_constant_coeff_match(eqs_2, funcs, t) is answer_2
+    assert neq_nth_linear_constant_coeff_match(eqs_2, funcs, t) is None
 
-    eqs_3 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * x(t) + 3 * y(t)))
-    answer_3 = {'no_of_equation': 2,
-     'eq': (12 * x(t) - 6 * y(t) + 5 * Derivative(x(t), t),
-            -11 * x(t) + 3 * y(t) + 2 * Derivative(y(t), t)),
-     'func': [x(t), y(t)],
-     'order': {x(t): 1, y(t): 1},
+    eqs_3 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * x(t) + 3 * y(t)), (5 * w1 + z(t)), (z1 + w(t)))
+    answer_3 = {'no_of_equation': 4,
+     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t),
+      -11*x(t) + 3*y(t) + 2*Derivative(y(t), t),
+      z(t) + 5*Derivative(w(t), t),
+      w(t) + Derivative(z(t), t)),
+     'func': [x(t), y(t), z(t), w(t)],
+     'order': {x(t): 1, y(t): 1, z(t): 1, w(t): 1},
      'is_linear': True,
      'is_constant': True,
      'is_homogeneous': True,
      'func_coeff': Matrix([
-         [Rational(12, 5), Rational(-6, 5)],
-         [Rational(-11, 2), Rational(3, 2)]])}
-    assert neq_nth_linear_constant_coeff_match(eqs_3, funcs, t) == answer_3
+     [Rational(12, 5), Rational(-6, 5), 0, 0],
+     [Rational(-11, 2),  Rational(3, 2),   0, 0],
+     [0, 0, 0, 1],
+     [0, 0, Rational(1, 5), 0]]),
+     'type_of_equation': 'type1'}
+    assert neq_nth_linear_constant_coeff_match(eqs_3, funcs_2, t) == answer_3
 
-    eqs_4 = (5 * x1 + 12 * x(t) - 6 * (y(t)) + x2, (2 * y1 - 11 * x(t) + 3 * y(t)))
-    answer_4 = {'no_of_equation': 2,
-     'eq': (12*x(t) - 6*y(t) + 5*Derivative(x(t), t) + Derivative(x(t), (t, 2)),
-      -11*x(t) + 3*y(t) + 2*Derivative(y(t), t)),
-     'func': [x(t), y(t)],
-     'order': {x(t): 2, y(t): 1},
+    eqs_4 = (5 * x1 + 12 * x(t) - 6 * (y(t)), (2 * y1 - 11 * x(t) + 3 * y(t)), (z1 - w(t)), (w1 - z(t)))
+    answer_4 = {'no_of_equation': 4,
+     'eq': (12 * x(t) - 6 * y(t) + 5 * Derivative(x(t), t),
+            -11 * x(t) + 3 * y(t) + 2 * Derivative(y(t), t),
+            -w(t) + Derivative(z(t), t),
+            -z(t) + Derivative(w(t), t)),
+     'func': [x(t), y(t), z(t), w(t)],
+     'order': {x(t): 1, y(t): 1, z(t): 1, w(t): 1},
      'is_linear': True,
      'is_constant': True,
-     'is_homogeneous': True}
-    assert neq_nth_linear_constant_coeff_match(eqs_4, funcs, t) == answer_4
+     'is_homogeneous': True,
+     'func_coeff': Matrix([
+         [Rational(12, 5), Rational(-6, 5), 0, 0],
+         [Rational(-11, 2), Rational(3, 2), 0, 0],
+         [0, 0, 0, -1],
+         [0, 0, -1, 0]]),
+     'type_of_equation': 'type1'}
+    assert neq_nth_linear_constant_coeff_match(eqs_4, funcs_2, t) == answer_4
+
+    eqs_5 = (5 * x1 + 12 * x(t) - 6 * (y(t)) + x2, (2 * y1 - 11 * x(t) + 3 * y(t)), (z1 - w(t)), (w1 - z(t)))
+    assert neq_nth_linear_constant_coeff_match(eqs_5, funcs_2, t) is None
