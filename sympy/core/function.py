0b23e73212c848bf03322b33646bc744cc5b687a
Saanidhya <saanidhyavats@gmail.com>
2020-03-29 19:12:47 +0530

    New changes

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -1389,7 +1389,7 @@ class Derivative(Expr):
         if isinstance(expr, Derivative):
             variable_count = list(expr.variable_count) + variable_count
             expr = expr.expr
-            return cls.__new__(expr, *variable_count, **kwargs)
+            return cls(expr, *variable_count, **kwargs)
 
         # we return here if evaluate is False or if there is no
         # _eval_derivative method
