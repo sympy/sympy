6459e07192e9e74652fec55dbeb0667a9ae9e7be
Ritu Raj Singh <RituRajSingh878@gmail.com>
2020-04-15 16:18:28 -0400

    updated test

--- sympy/solvers/ode/tests/test_ode.py
+++ sympy/solvers/ode/tests/test_ode.py
@@ -2884,9 +2884,9 @@ def test_separable_reduced():
     assert checkodesol(eq, sol, order=1, solve_for_func=False)[0]
 
     eq = Eq(f(x).diff(x) + f(x)/x * (1 + (x**(S(2)/3)*f(x))**2), 0)
-    # FIXME: This one hangs
-    #sol = dsolve(eq, hint = 'seprable_reduced')
-    # assert checkodesol(eq, sol) = (True, 0)
+    sol = dsolve(eq, hint = 'separable_reduced', simplify=False)
+    assert sol == Eq(-3*log(x**(S(2)/3)*f(x)) + 3*log(3*x**(S(4)/3)*f(x)**2 + 1)/2, C1 + log(x))
+    assert checkodesol(eq, sol, solve_for_func=False) == (True, 0)
 
     eq = Eq(f(x).diff(x) + f(x)/x * (1 + (x*f(x))**2), 0)
     sol = dsolve(eq, hint = 'separable_reduced')
@@ -2905,9 +2905,9 @@ def test_separable_reduced():
                   Eq(f(x), sqrt(2)*sqrt(1/(C1 + log(x)))/2)]
 
     eq = Eq(f(x).diff(x) + (f(x)+3)*f(x)/(x*(f(x)+2)), 0)
-    # FIXME: This one hangs
-    #sol == dsolve(eq, hint = 'separable_reduced')
-    #assert checkodesol(eq, sol) == (True, 0)
+    sol = dsolve(eq, hint = 'separable_reduced', simplify=False)
+    assert sol == Eq(-log(f(x) + 3)/3 - 2*log(f(x))/3, C1 + log(x))
+    assert checkodesol(eq, sol, solve_for_func=False) == (True, 0)
 
     eq = Eq(f(x).diff(x) + (f(x)+3)*f(x)/x, 0)
     sol = dsolve(eq, hint = 'separable_reduced')
@@ -2915,10 +2915,9 @@ def test_separable_reduced():
     assert checkodesol(eq, sol) == (True, 0)
 
     eq = Eq(f(x).diff(x) + (f(x)**2+f(x))*f(x)/(x), 0)
-    sol = dsolve(eq, hint='separable_reduced')
-    assert sol == Eq(-log(x) - log(f(x) + 1) + log(f(x)) + 1/f(x), C1)
-    # FIXME: Raise NotImplementedError
-    # checkodesol(eq, sol) = (True, 0)
+    sol = dsolve(eq, hint='separable_reduced', simplify=False)
+    assert sol == Eq(-log(f(x) + 1) + log(f(x)) + 1/f(x), C1 + log(x))
+    assert checkodesol(eq, sol, solve_for_func=False) == (True, 0)
 
 
 def test_homogeneous_function():
