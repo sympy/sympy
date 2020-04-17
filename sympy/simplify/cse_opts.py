cf267c29adec340eaad0e628e03082a95002d883
Christopher Smith <smichr@gmail.com>
2020-04-17 08:46:59 -0500

    Update cse_opts.py

--- sympy/simplify/cse_opts.py
+++ sympy/simplify/cse_opts.py
@@ -14,7 +14,14 @@ def sub_pre(e):
     """
     # replacing Add, A, from which -1 can be extracted with -1*-A
     adds = [a for a in e.atoms(Add) if a.could_extract_minus_sign()]
-    reps = dict((a, Mul(S.NegativeOne, -a, evaluate=False)) for a in adds)
+    reps = {}
+    ignore = set()
+    for a in adds:
+        na = -a
+        if na.is_Mul:  # e.g. MatExpr
+            set.add(a)
+            continue
+        reps[a] = Mul._from_args([S.NegativeOne, na])
 
     e = e.xreplace(reps)
 
@@ -23,6 +30,8 @@ def sub_pre(e):
     if isinstance(e, Basic):
         negs = {}
         for a in sorted(e.atoms(Add), key=default_sort_key):
+            if a in ignore:
+                continue
             if a in reps:
                 negs[a] = reps[a]
             elif a.could_extract_minus_sign():
@@ -38,7 +47,7 @@ def sub_post(e):
     for node in preorder_traversal(e):
         if isinstance(node, Mul) and \
             node.args[0] is S.One and node.args[1] is S.NegativeOne:
-            replacements.append((node, -Mul(*node.args[2:], evaluate=False)))
+            replacements.append((node, -Mul._from_args(node.args[2:])))
     for node, replacement in replacements:
         e = e.xreplace({node: replacement})
 
