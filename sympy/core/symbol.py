6bd559145bca76b1b49348a694d263cf0a62ef34
Eric Wieser <wieser.eric@gmail.com>
2020-04-28 12:46:37 +0100

    Speed up Dummy.sort_key
    There's no need to go through `StrPrinter` here.

--- sympy/core/symbol.py
+++ sympy/core/symbol.py
@@ -360,7 +360,7 @@ class Dummy(Symbol):
     @cacheit
     def sort_key(self, order=None):
         return self.class_key(), (
-            2, (str(self), self.dummy_index)), S.One.sort_key(), S.One
+            2, (self.name, self.dummy_index)), S.One.sort_key(), S.One
 
     def _hashable_content(self):
         return Symbol._hashable_content(self) + (self.dummy_index,)
