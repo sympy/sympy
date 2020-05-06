2ca3f5393e6bff92d2ab77d2d172f4f9a3277a0a
Abhinav Anand <abhinav.anand2807@gmail.com>
2020-05-06 23:25:44 +0530

    code quality improvements

--- sympy/ntheory/ecm.py
+++ sympy/ntheory/ecm.py
@@ -1,3 +1,10 @@
+#----------------------------------------------------------------------------#
+#                                                                            #
+#                   Lenstra's Elliptic Curve Factorization                   #
+#                                                                            #
+#----------------------------------------------------------------------------#
+
+
 class point:
     """For denoting the point on the curve.
     For ECM we don't need the y-coordinate. All
@@ -11,10 +18,12 @@ class point:
 def _pt_add(P, Q, diff, n):
     """
     Add two points P and Q where diff = P - Q.
+
     Parameters:
     ===========
-    P : point on the cuvve in Montgomery form
-    Q : point on the cuvve in Montgomery form
+
+    P : point on the curve in Montgomery form
+    Q : point on the curve in Montgomery form
     diff = P - Q
     n = modulus
     """
@@ -29,7 +38,9 @@ def _pt_add(P, Q, diff, n):
 def _pt_double(P, n, a24):
     """
     Doubles a point in an elliptic curve in Montgomery form.
+
     Parameter:
+
     P : Point on the curve
     a24 : Parameter for doubling
     n : modulus
@@ -46,7 +57,10 @@ def _mont_ladder(k, P, n, a24):
     """
     Scalar multiblication of a point in
     Ellipic Curve in Montgomery form.
+
     Parameters:
+    ==========
+
     k : Integer
     P : Poin on the curve in Montgomery form
     n : modulus
@@ -64,12 +78,14 @@ def _mont_ladder(k, P, n, a24):
             Q = _pt_double(Q, n, a24)
     return Q
 
-def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
+def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
     """Returns one factor of n using
     Lenstra's 2 stage Elliptic curve Factorization
     with Suyama's Parameterization.
+
     References
     ==========
+
     .. [1]  Carl Pomerance and Richard Crandall "Prime Numbers:
         A Computational Perspective" (2nd Ed.), page 344
     """
@@ -79,12 +95,14 @@ def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
     from sympy.core.power import integer_log
     from sympy.core.compatibility import as_int
     import random
+
     n = as_int(n)
     if B1 % 2 != 0 or B2 % 2 != 0:
         raise ValueError("The Bounds should be an even integer")
     sieve.extend(B2)
     if isprime(n):
         return n
+
     curve = 0
     D = int(sqrt(B2))
     beta = [0]*(D + 1)
@@ -94,13 +112,15 @@ def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
         k *= pow(p, integer_log(B1, p)[0])
     g = 1
     while(curve <= max_curve):
-        curve+=1
+        curve += 1
+
         #Suyama's Paramatrization
-        sigma = random.randint(6,  n )
+        sigma = random.randint(6, n)
         u = (sigma*sigma - 5) % n
         v = (4*sigma) % n
         diff = v - u
         u_3 = pow(u, 3, n)
+
         try:
             A = (pow(diff, 3, n)*(3*u + v)*mod_inverse(4*u_3*v, n) - 2) % n
         except ValueError:
@@ -119,6 +139,7 @@ def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
         S[2] = _pt_double(S[1], n, a24)
         beta[1] = (S[1].x*S[1].z) % n
         beta[2] = (S[2].x*S[2].z) % n
+
         for d in range(3, D + 1):
             S[d] = _pt_add(S[d - 1], S[1], S[d - 2], n)
             beta[d] = (S[d].x*S[d].z) % n
@@ -126,6 +147,7 @@ def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
         B = B1 - 1
         R = _mont_ladder(B, Q, n, a24)
         T = _mont_ladder(B - 2*D, Q, n, a24)
+
         for r in  range(B, B2, 2*D):
             alpha = (R.x*R.z) % n
             for q in sieve.primerange(r + 2, r + 2*D + 1):
@@ -140,6 +162,7 @@ def ecm_one_factor(n, B1= 10000, B2= 100000, max_curve = 200):
         #Stage 2 Factor found
         if g != 1 and g != n:
             return g
+
     #ECM failed, Increase the bounds
     raise ValueError("Increase the bounds")
 
@@ -147,6 +170,7 @@ def ecm(n):
     """Performs factorization using
     Lenstra's Elliptic curve method
     """
+
     from sympy import sieve, isprime
     factor = set()
     for i in sieve.primerange(1, 100000):
