f67aa6a3ce6d293ee6b88330a28c8b1c33d681e5
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-05-14 17:35:10 +0900

    Improve code quality

--- sympy/diffgeom/diffgeom.py
+++ sympy/diffgeom/diffgeom.py
@@ -4,7 +4,7 @@ from itertools import permutations
 
 from sympy.combinatorics import Permutation
 from sympy.core import (
-    AtomicExpr, Basic, Expr, Dummy, Function,  diff,
+    Basic, Expr, Dummy, Function,  diff,
     Pow, Mul, Add, Atom
 )
 from sympy.core.compatibility import reduce
