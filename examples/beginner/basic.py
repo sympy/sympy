6db170552fc9c72a36b1b92a81a6abe63605a0e4
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-24 12:49:17 +0530

    added a method for creating a normal equation and representing it.

--- examples/beginner/basic.py
+++ examples/beginner/basic.py
@@ -9,7 +9,14 @@ from sympy import Symbol, pprint
 
 
 
-
+def Normal_Equation():
+    a=Symbol('a')
+    b=Symbol('b')
+    c=Symbol('c')
+    e=(a*b)+(a*c)+(b*c)
+    print('')
+    print(e)
+    print('')
 
 def for_Squared_Expression():
     a = Symbol('a')
@@ -24,3 +31,4 @@ def for_Squared_Expression():
 
 if __name__ == "__main__":
     for_Squared_Expression()
+    Normal_Equation()
