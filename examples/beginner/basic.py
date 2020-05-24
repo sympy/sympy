dec8f4962059160a68c3531ae0e67c0f2c84d5eb
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-24 12:47:10 +0530

    changed the name of the method from main expression to for_sqaured_expression as I wanted to add a function for having a example with which people can understand easily

--- examples/beginner/basic.py
+++ examples/beginner/basic.py
@@ -8,15 +8,19 @@ Demonstrates how to create symbols and print some algebra operations.
 from sympy import Symbol, pprint
 
 
-def main():
+
+
+
+def for_Squared_Expression():
     a = Symbol('a')
     b = Symbol('b')
     c = Symbol('c')
     e = ( a*b*b + 2*b*a*b )**c
+
 
     print('')
     pprint(e)
     print('')
 
 if __name__ == "__main__":
-    main()
+    for_Squared_Expression()
