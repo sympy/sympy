5676aabffa923cab56efe7ee31440d8e0722420c
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-29 12:56:25 +0530

    made the example of normal equation.

--- examples/beginner/basic.py
+++ examples/beginner/basic.py
@@ -7,6 +7,19 @@ Demonstrates how to create symbols and print some algebra operations.
 
 from sympy import Symbol, pprint
 
+def normal_equation():
+    "This example is for adding the example of normal equation"
+    a = Symbol('a')
+    b = Symbol('b')
+    c = Symbol('c')
+    e = (a * b +b*c+c*a)
+
+    print('')
+    pprint(e)
+    print('')
+
+
+
 
 def squared_equation_example():
     "This Function is for having the example of Squared Equation"
@@ -22,3 +35,4 @@ def squared_equation_example():
 
 if __name__ == "__main__":
     squared_equation_example()
+    normal_equation()
