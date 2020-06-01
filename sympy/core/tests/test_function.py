7c883b101c884194554755b479c2b12b0a7b9824
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-06-01 16:16:42 +0900

    Add more test

--- sympy/core/tests/test_function.py
+++ sympy/core/tests/test_function.py
@@ -554,9 +554,11 @@ def test_function__eval_nseries():
         sqrt(x) - x**Rational(3, 2)/6 + x**Rational(5, 2)/120 + O(x**3)
 
     # issue 19065:
+    s1 = f(x,y).series(y, n=2)
+    assert {i.name for i in s1.atoms(Symbol)} == {'x', 'xi', 'y'}
     xi = Symbol('xi')
-    s = f(xi, y).series(y, n=2)
-    assert {i.name for i in s.atoms(Symbol)} == {'xi', 'xi0', 'y'}
+    s2 = f(xi, y).series(y, n=2)
+    assert {i.name for i in s2.atoms(Symbol)} == {'xi', 'xi0', 'y'}
 
 def test_doit():
     n = Symbol('n', integer=True)
