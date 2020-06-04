c79f851ccab4d405268a00a34a0b1a299a73508d
gutow <gutow@uwosh.edu>
2020-06-04 15:16:48 -0500

    Update sanity checking to only occur on direct call to `Equation`. Also provide `check=True/False` keyword to force or suppress checking.

--- sympy/core/equation.py
+++ sympy/core/equation.py
@@ -94,12 +94,24 @@ class Equation(Basic):
     [b/a]
     """
 
-    def __new__(cls, lhs, rhs):
+    def __new__(cls, lhs, rhs, **kwargs):
+        check = kwargs.pop('check', True)
         lhs = _sympify(lhs)
         rhs = _sympify(rhs)
         if lhs.is_number and rhs.is_number and lhs != rhs:
             print('WARNING: did your really mean to define unequal numbers as '
                   'equal? ' + str(lhs) + '=' + str(rhs))
+        if check:
+            lsimp = lhs.simplify()
+            rsimp = rhs.simplify()
+            if lhs.is_number and rhs.is_number and lsimp != rsimp:
+                from warnings import warn
+                warn('Did your really mean to define unequal numbers ' + str(
+                    lsimp) + ' and ' + str(
+                    rsimp) +
+                     ' as equal? To suppress this warning include '
+                     '`check=False` in the equation definition: '
+                     '`Eqn(lhs,rhs,check=False)`.')
         return super().__new__(cls, lhs, rhs)
 
     @property
@@ -128,7 +140,7 @@ class Equation(Basic):
         """
         Swaps the lhs and the rhs.
         """
-        return Equation(self.rhs, self.lhs)
+        return Equation(self.rhs, self.lhs, check=False)
 
     def _applyfunc(self, func, *args, **kwargs):
         # Assume if the expression has an attribute of name `func` that
@@ -141,22 +153,26 @@ class Equation(Basic):
         if (side == 'both'):
             if hasattr(self.lhs, str(func)) or hasattr(self.rhs, str(func)):
                 return Equation(getattr(self.lhs, str(func))(*args, **kwargs),
-                                getattr(self.rhs, str(func))(*args, **kwargs))
+                                getattr(self.rhs, str(func))(*args, **kwargs),
+                                check=False)
             else:
                 return Equation(func(self.lhs, *args, **kwargs),
-                                func(self.rhs, *args, **kwargs))
+                                func(self.rhs, *args, **kwargs), check=False)
         elif (side == 'lhs'):
             if hasattr(self.lhs, str(func)):
                 return Equation(getattr(self.lhs, str(func))(*args, **kwargs),
-                                self.rhs)
+                                self.rhs, check=False)
             else:
-                return Equation(func(self.lhs, *args, **kwargs), self.rhs)
+                return Equation(func(self.lhs, *args, **kwargs), self.rhs,
+                                check=False)
         elif (side == 'rhs'):
             if hasattr(self.rhs, str(func)):
                 return Equation(self.lhs, getattr(self.rhs,
-                                                  str(func))(*args, **kwargs))
+                                                  str(func))(*args, **kwargs),
+                                check=False)
             else:
-                return Equation(self.lhs, func(self.rhs, *args, **kwargs))
+                return Equation(self.lhs, func(self.rhs, *args, **kwargs),
+                                check=False)
         else:
             raise ValueError('keyword `Eqn_apply_side` must be one of '
                              '"both", "lhs" or "rhs".')
@@ -192,11 +208,14 @@ class Equation(Basic):
     @classmethod
     def _binary_op(cls, a, b, opfunc_ab):
         if isinstance(a, Equation) and not isinstance(b, Equation):
-            return Equation(opfunc_ab(a.lhs, b), opfunc_ab(a.rhs, b))
+            return Equation(opfunc_ab(a.lhs, b), opfunc_ab(a.rhs, b),
+                            check=False)
         elif isinstance(b, Equation) and not isinstance(a, Equation):
-            return Equation(opfunc_ab(a, b.lhs), opfunc_ab(a, b.rhs))
+            return Equation(opfunc_ab(a, b.lhs), opfunc_ab(a, b.rhs),
+                            check=False)
         elif isinstance(a, Equation) and isinstance(b, Equation):
-            return Equation(opfunc_ab(a.lhs, b.lhs), opfunc_ab(a.rhs, b.rhs))
+            return Equation(opfunc_ab(a.lhs, b.lhs), opfunc_ab(a.rhs, b.rhs),
+                            check=False)
         else:
             raise TypeError('One of a or b should be an equation')
 
@@ -257,19 +276,19 @@ class Equation(Basic):
     #####
     def expand(self, *args, **kwargs):
         return Equation(self.lhs.expand(*args, **kwargs), self.rhs.expand(
-            *args, **kwargs))
+            *args, **kwargs), check=False)
 
     def simplify(self, *args, **kwargs):
         return self._eval_simplify(*args, **kwargs)
 
     def _eval_simplify(self, *args, **kwargs):
         return Equation(self.lhs.simplify(*args, **kwargs), self.rhs.simplify(
-            *args, **kwargs))
+            *args, **kwargs), check=False)
 
     def _eval_factor(self, *args, **kwargs):
         # TODO: cancel out factors common to both sides.
         return Equation(self.lhs.factor(*args, **kwargs), self.rhs.factor(
-            *args, **kwargs))
+            *args, **kwargs), check=False)
 
     def factor(self, *args, **kwargs):
         return self._eval_factor(*args, **kwargs)
@@ -277,14 +296,14 @@ class Equation(Basic):
     def _eval_collect(self, *args, **kwargs):
         from sympy.simplify.radsimp import collect
         return Equation(collect(self.lhs, *args, **kwargs),
-                        collect(self.rhs, *args, **kwargs))
+                        collect(self.rhs, *args, **kwargs), check=False)
 
     def collect(self, *args, **kwargs):
         return self._eval_collect(*args, **kwargs)
 
     def evalf(self, *args, **kwargs):
         return Equation(self.lhs.evalf(*args, **kwargs),
-                        self.rhs.evalf(*args, **kwargs))
+                        self.rhs.evalf(*args, **kwargs), check=False)
 
     def _eval_derivative(self, *args, **kwargs):
         # TODO Find why diff and Derivative do not appear to pass through
@@ -298,7 +317,7 @@ class Equation(Basic):
                 _sympify(sym).is_number):
                     eval_lhs = True
         return Equation(self.lhs.diff(*args, **kwargs, evaluate=eval_lhs),
-                        self.rhs.diff(*args, **kwargs))
+                        self.rhs.diff(*args, **kwargs), check=False)
 
     def integ(self, *args, **kwargs):
         """
@@ -308,7 +327,7 @@ class Equation(Basic):
         """
         from sympy.integrals.integrals import Integral
         return Equation(Integral(self.lhs, *args, **kwargs),
-                        self.rhs.integrate(*args, **kwargs))
+                        self.rhs.integrate(*args, **kwargs), check=False)
 
     def _eval_Integral(self, *args, **kwargs):
         side = kwargs.pop('side', None)  # Could not seem to pass values for
