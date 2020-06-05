a4426420768035eb72919f61b5d89fa8385f01c0
Abhinav Anand <abhinav.anand2807@gmail.com>
2020-06-05 18:12:32 +0530

    made suggested changes

--- sympy/ntheory/ecm.py
+++ sympy/ntheory/ecm.py
@@ -1,3 +1,9 @@
+from sympy.ntheory import sieve, isprime
+from sympy import gcd, mod_inverse, sqrt
+from sympy.core.power import integer_log
+from sympy.core.compatibility import as_int
+import random
+
 #----------------------------------------------------------------------------#
 #                                                                            #
 #                   Lenstra's Elliptic Curve Factorization                   #
@@ -6,16 +12,30 @@
 
 
 class Point:
-    """For denoting the point on the curve.
-    For ECM we don't need the y-coordinate. All
-    arithmetic operations use only the x and z coordinates.
+    """Montgomery form of Points in an elliptic curve.
+    In this form, the addition and doubling of points
+    does not need any y-coordinate information thus
+    decreasing the number of operation.
+
+    Parameters:
+    ===========
+
+    x_corr : X coordinate of the Point
+    z_corr : Z coordinate of the Point
+    a_24 : Parameter of the elliptic curve in Montgomery form
+    mod : modulus
+
+    References
+    ==========
+
+    .. [1]  http://www.hyperelliptic.org/tanja/SHARCS/talks06/Gaj.pdf
     """
 
-    def __init__(self, x, z, a24, modulus):
-        self.x = x
-        self.z = z
-        self.a24 = a24
-        self.n = modulus
+    def __init__(self, x_corr, z_corr, a_24, mod):
+        self.x_corr = x_corr
+        self.z_corr = z_corr
+        self.a_24 = a_24
+        self.mod = mod
 
     def add(self, Q, diff):
         """
@@ -24,71 +44,37 @@ class Point:
         Parameters:
         ===========
 
-        P : point on the curve in Montgomery form
         Q : point on the curve in Montgomery form
-        diff = P - Q
-        n = modulus
-
-        References
-        ==========
-
-        .. [1]  http://www.hyperelliptic.org/tanja/SHARCS/talks06/Gaj.pdf
-            Algorithm - 3
+        diff : P - Q
         """
-
-        u = (self.x - self.z)*(Q.x + Q.z)
-        v = (self.x + self.z)*(Q.x - Q.z)
+        u = (self.x_corr - self.z_corr)*(Q.x_corr + Q.z_corr)
+        v = (self.x_corr + self.z_corr)*(Q.x_corr - Q.z_corr)
         add, subt = u + v, u - v
-        x_cor = diff.z*add*add % self.n
-        z_cor = diff.x*subt*subt % self.n
-        return Point(x_cor, z_cor, self.a24, self.n)
-
+        x_corr = diff.z_corr*add*add % self.mod
+        z_corr = diff.x_corr*subt*subt % self.mod
+        return Point(x_corr, z_corr, self.a_24, self.mod)
 
     def double(self):
         """
         Doubles a point in an elliptic curve in Montgomery form.
-
-        Parameter:
-        ==========
-
-        P : Point on the curve
-        a24 : Parameter for doubling
-        n : modulus
-
-        References
-        ==========
-
-        .. [1]  http://www.hyperelliptic.org/tanja/SHARCS/talks06/Gaj.pdf
-            Algorithm - 3
         """
-
-        u, v = self.x + self.z, self.x - self.z
+        u, v = self.x_corr + self.z_corr, self.x_corr - self.z_corr
         u, v = u*u, v*v
         diff = u - v
-        x_cor = u*v % self.n
-        z_cor = diff*(v + self.a24*diff) % self.n
-        return Point(x_cor, z_cor, self.a24, self.n)
+        x_corr = u*v % self.mod
+        z_corr = diff*(v + self.a_24*diff) % self.mod
+        return Point(x_corr, z_corr, self.a_24, self.mod)
 
     def mont_ladder(self, k):
         """
-        Scalar multiblication of a point in
-        Ellipic Curve in Montgomery form.
+        Scalar multiplication of a point in Montgomery form
+        using Montgomery Ladder Algorithm.
 
         Parameters:
         ==========
 
         k : The positive integer multiplier
-        P : Point on the curve in Montgomery form
-        n : modulus
-        a24 : Property of the curve
-
-        References
-        ==========
-
-        .. [1]  http://www.hyperelliptic.org/tanja/SHARCS/talks06/Gaj.pdf
-            Algorithm - 3
         """
-
         Q = self
         R = self.double()
         for i in bin(k)[3:]:
@@ -100,10 +86,13 @@ class Point:
                 Q = Q.double()
         return Q
 
-def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
+
+def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200, seed=1234):
     """Returns one factor of n using
     Lenstra's 2 stage Elliptic curve Factorization
-    with Suyama's Parameterization.
+    with Suyama's Parameterization. Here Montgomery
+    arthematic is used for fast computation of addition
+    and doubling of points in elliptic curve.
 
     Parameters:
     ===========
@@ -119,13 +108,6 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
     .. [1]  Carl Pomerance and Richard Crandall "Prime Numbers:
         A Computational Perspective" (2nd Ed.), page 344
     """
-
-    from sympy.ntheory import sieve, isprime
-    from sympy import gcd, mod_inverse, sqrt
-    from sympy.core.power import integer_log
-    from sympy.core.compatibility import as_int
-    import random
-
     n = as_int(n)
     if B1 % 2 != 0 or B2 % 2 != 0:
         raise ValueError("The Bounds should be an even integer")
@@ -143,6 +125,7 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
         k *= pow(p, integer_log(B1, p)[0])
     g = 1
 
+    random.seed(seed)
     while(curve <= max_curve):
         curve += 1
 
@@ -162,7 +145,7 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
         a24 = (C + 2)*mod_inverse(4, n) % n
         Q = Point(u_3 , pow(v, 3, n), a24, n)
         Q = Q.mont_ladder(k)
-        g = gcd(Q.z, n)
+        g = gcd(Q.z_corr, n)
 
         #Stage 1 factor
         if g != 1 and g != n:
@@ -174,12 +157,12 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
         #Stage 2 - Improved Standard Continuation
         S[1] = Q.double()
         S[2] = S[1].double()
-        beta[1] = (S[1].x*S[1].z) % n
-        beta[2] = (S[2].x*S[2].z) % n
+        beta[1] = (S[1].x_corr*S[1].z_corr) % n
+        beta[2] = (S[2].x_corr*S[2].z_corr) % n
 
         for d in range(3, D + 1):
             S[d] = S[d - 1].add(S[1], S[d - 2])
-            beta[d] = (S[d].x*S[d].z) % n
+            beta[d] = (S[d].x_corr*S[d].z_corr) % n
 
         g = 1
         B = B1 - 1
@@ -187,10 +170,10 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
         R = Q.mont_ladder(B)
 
         for r in  range(B, B2, 2*D):
-            alpha = (R.x*R.z) % n
+            alpha = (R.x_corr*R.z_corr) % n
             for q in sieve.primerange(r + 2, r + 2*D + 1):
                 delta = (q - r) // 2
-                f = (R.x - S[d].x)*(R.z + S[d].z) - alpha + beta[delta]
+                f = (R.x_corr - S[d].x_corr)*(R.z_corr + S[d].z_corr) - alpha + beta[delta]
                 g = (g*f) % n
             #Swap
             TT = R
@@ -205,7 +188,8 @@ def ecm_one_factor(n, B1=10000, B2=100000, max_curve=200):
     #ECM failed, Increase the bounds
     raise ValueError("Increase the bounds")
 
-def ecm(n, B1=10000, B2=100000, max_curve=200, increase_bound=False):
+
+def ecm(n, B1=10000, B2=100000, max_curve=200, increase_bound=False, seed=1234):
     """Performs factorization using
     Lenstra's Elliptic curve method
 
@@ -219,8 +203,6 @@ def ecm(n, B1=10000, B2=100000, max_curve=200, increase_bound=False):
     increase_bound : If True, then the Stage 1 and 2 bounds increase
         by a factor of 10 incase of ECM failure
     """
-
-    from sympy import sieve, isprime
     factor = set()
     for i in sieve.primerange(1, 100000):
         if n % i == 0:
@@ -232,7 +214,7 @@ def ecm(n, B1=10000, B2=100000, max_curve=200, increase_bound=False):
         factor.add(2)
     while(n > 1):
         try:
-            a = ecm_one_factor(n, B1, B2, max_curve)
+            a = ecm_one_factor(n, B1, B2, max_curve, seed)
         except ValueError:
             if increase_bound:
                 B1 *= 10
