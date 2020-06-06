ee010b536e5915ff800d9e0b0d369b7075fccb04
mohit <42018918+mohitshah3111999@users.noreply.github.com>
2020-06-06 20:24:13 +0530

    Minor Change 1

--- sympy/geometry/line.py
+++ sympy/geometry/line.py
@@ -80,9 +80,7 @@ class LinearEntity(GeometrySet):
 
     def __contains__(self, other):
         """Return a definitive answer or else raise an error if it cannot
-        be determined that other is on the boundaries of self.
-
-        """
+        be determined that other is on the boundaries of self."""
         result = self.contains(other)
 
         if result is not None:
