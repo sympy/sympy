c9b9cc67265f482b0e1802e4dc5ea5e3378a638c
Naman Gera <namangera15@gmail.com>
2020-06-08 02:11:37 +0530

    add further implementation

--- sympy/physics/control/lti.py
+++ sympy/physics/control/lti.py
@@ -1,5 +1,6 @@
-from sympy import Basic, Mul, degree, Symbol, sympify, expand
-from sympy.core.numbers import Integer
+from sympy import Basic, Mul, degree, Symbol, expand, cancel, Expr
+from sympy.core.numbers import Integer, Float
+from sympy.core.sympify import sympify, _sympify
 
 __all__ = ['TransferFunction',]
 
@@ -7,19 +8,22 @@ __all__ = ['TransferFunction',]
 class TransferFunction(Basic):
 
     def __new__(cls, num, den, var):
+        num, den = _sympify(num), _sympify(den)
 
         if not isinstance(var, Symbol):
-            raise TypeError("Last argument must be a complex Symbol.")
+            raise TypeError("Variable input must be a Symbol.")
         if den == 0:
             raise ValueError("TransferFunction can't have a zero denominator.")
 
-        num, den = sympify(num), sympify(den)
-        obj = Basic.__new__(cls, num, den, var)
-        obj._num = num
-        obj._den = den
-        obj._var = var
-
-        return obj
+        if (isinstance(num, Expr) and num.has(Symbol)) or isinstance(num, Integer):
+            if (isinstance(num, Expr) and num.has(Symbol)) or isinstance(num, Integer):
+                obj = super(TransferFunction, cls).__new__(cls, num, den, var)
+                obj._num = num
+                obj._den = den
+                obj._var = var
+                return obj
+        else:
+            raise ValueError("Unsupported type for numerator or denominator of TransferFunction.")
 
     @property
     def num(self):
@@ -33,33 +37,106 @@ class TransferFunction(Basic):
     def var(self):
         return self._var
 
+    def simplify(self):
+        tf = cancel(Mul(self.num, 1/self.den)).as_numer_denom()
+        num_, den_ = tf[0], tf[1]
+        return TransferFunction(num_, den_, self.var)
+
     def __add__(self, other):
-        if not self.var == other.var:
-            raise ValueError("Last argument for both Transfer Functions must be same.")
-        p = self.num * other.den + other.num * self.den
-        q = self.den * other.den
-        return TransferFunction(expand(p), expand(q), self.var)
+        other = _sympify(other)
+        if isinstance(other, (Integer, Float)):
+            if isinstance(other, Float):
+                other = Float(other, 3)
+            return TransferFunction(self.num + self.den*other, self.den, self.var)
+
+        elif isinstance(other, TransferFunction):
+            if not self.var == other.var:
+                raise ValueError("Both the Transfer Functions should be anchored "
+                    "with the same variable.")
+            p = self.num * other.den + other.num * self.den
+            q = self.den * other.den
+            return TransferFunction(p, q, self.var)
+
+        elif isinstance(other, Expr) and other.has(Symbol):
+            # other input is a polynomial.
+            if not other.is_commutative:
+                raise ValueError("Only commutative expressions can be added "
+                    "with a TransferFunction.")
+            return TransferFunction(self.num + self.den*other, self.den, self.var)
+        else:
+            raise ValueError("TransferFunction cannot be added with {}.".
+                format(type(other)))
 
     def __sub__(self, other):
-        if not self.var == other.var:
-            raise ValueError("Last argument for both Transfer Functions must be same.")
-        p = self.num * other.den - other.num * self.den
-        q = self.den * other.den
-        return TransferFunction(expand(p), expand(q), self.var)
+        other = _sympify(other)
+        if isinstance(other, (Integer, Float)):
+            if isinstance(other, Float):
+                other = Float(other, 3)
+            return TransferFunction(self.num - self.den*other, self.den, self.var)
+
+        elif isinstance(other, TransferFunction):
+            if not self.var == other.var:
+                raise ValueError("Both the Transfer Functions should be anchored "
+                    "with the same variable.")
+            p = self.num * other.den - other.num * self.den
+            q = self.den * other.den
+            return TransferFunction(p, q, self.var)
+
+        elif isinstance(other, Expr) and other.has(Symbol):
+            return TransferFunction(self.num - self.den*other, self.den, self.var)
+        else:
+            raise ValueError("{} cannot be subtracted from TransferFunction."
+                .format(type(other)))
 
     def __mul__(self, other):
-        if not self.var == other.var:
-            raise ValueError("Last argument for both Transfer Functions must be same.")
-        p = self.num * other.num
-        q = self.den * other.den
-        return TransferFunction(expand(p), expand(q), self.var)
+        other = _sympify(other)
+        if isinstance(other, (Integer, Float)):
+            if isinstance(other, Float):
+                other = Float(other, 3)
+            return TransferFunction(self.num*other, self.den, self.var)
+
+        elif isinstance(other, TransferFunction):
+            if not self.var == other.var:
+                raise ValueError("Both the Transfer Functions should be anchored "
+                    "with the same variable.")
+            p = self.num * other.num
+            q = self.den * other.den
+            return TransferFunction(p, q, self.var)
+
+        elif isinstance(other, Expr) and other.has(Symbol):
+            # other input is a polynomial.
+            if not other.is_commutative:
+                raise ValueError("Only commutative expressions can be multiplied "
+                    "with a TransferFunction.")
+            return TransferFunction(self.num*other, self.den, self.var)
+        else:
+            raise ValueError("TransferFunction cannot be multiplied with {}."
+                .format(type(other)))
+
+    __rmul__ = __mul__
 
     def __div__(self, other):
-        if not self.var == other.var:
-            raise ValueError("Last argument for both Transfer Functions must be same.")
-        p = self.num * other.den
-        q = self.den * other.num
-        return TransferFunction(expand(p), expand(q), self.var)
+        other = _sympify(other)
+        if isinstance(other, (Integer, Float)):
+            if other == 0:
+                raise ValueError("TransferFunction cannot be divided by zero.")
+            if isinstance(other, Float):
+                other = Float(other, 3)
+            return TransferFunction(self.num, self.den*other, self.var)
+
+        elif isinstance(other, TransferFunction):
+            if not self.var == other.var:
+                raise ValueError("Both the Transfer Functions should be anchored "
+                    "with the same variable.")
+            p = self.num * other.den
+            q = self.den * other.num
+            return TransferFunction(p, q, self.var)
+
+        elif isinstance(other, Expr) and other.has(Symbol):
+            return TransferFunction(self.num, self.den*other, self.var)
+        else:
+            raise ValueError("TransferFunction cannot be divided by {}.".
+                format(type(other)))
 
     __truediv__ = __div__
 
@@ -69,13 +146,13 @@ class TransferFunction(Basic):
             raise ValueError("Exponent must be an Integer.")
         if p == 0:
             return TransferFunction(1, 1, self.var)
-        if p < 0:
+        elif p > 0:
+            num_, den_ = expand(self.num**p), expand(self.den**p)
+        else:
             p = abs(p)
             num_, den_ = expand(self.den**p), expand(self.num**p)
-            return TransferFunction(num_, den_, self.var)
-        if p > 0:
-            num_, den_ = expand(self.num**p), expand(self.den**p)
-            return TransferFunction(num_, den_, self.var)
+
+        return TransferFunction(num_, den_, self.var)
 
     def __neg__(self):
         return TransferFunction(-self.num, self.den, self.var)
--- sympy/physics/control/tests/test_lti.py
+++ sympy/physics/control/tests/test_lti.py
@@ -63,21 +63,20 @@ def test_TransferFunction_construction():
 
     raises(ValueError, lambda: TransferFunction(Matrix([1, 2, 3]), s, s))
 
-    x, y = symbols('x, y', real=True)
-    raises(TypeError, lambda: TransferFunction(x**2 + 2*x - 1, x + 3, x))
-    raises(TypeError, lambda: TransferFunction(y + 1, 5 - y, y))
-    raises(TypeError, lambda: TransferFunction(x - 1, 4 - y, x))
+    raises(TypeError, lambda: TransferFunction(s**2 + 2*s - 1, s + 3, 3))
+    raises(TypeError, lambda: TransferFunction(p + 1, 5 - p, 4))
+    raises(TypeError, lambda: TransferFunction(3, 4, 8))
 
 
 def test_TransferFunction_functions():
     # explicitly cancel poles and zeros.
     tf0 = TransferFunction(s**5 + s**3 + s, s - s**2, s)
     a = TransferFunction(-(s**4 + s**2 + 1), s - 1, s)
-    assert tf0.simplify() == simplify(tf0) == a
+    assert tf0.simplify() == a
 
     tf1 = TransferFunction((p + 3)*(p - 1), (p - 1)*(p + 5), p)
     b = TransferFunction(p + 3, p + 5, p)
-    assert tf1.simplify() == simplify(tf1) == b
+    assert tf1.simplify() == b
 
     # purely symbolic polynomials.
     p1 = a1*s + a0
@@ -135,8 +134,9 @@ def test_TransferFunction_addition_and_subtraction():
     tf4 = TransferFunction(p, 2 - p, p)
 
     # addition
-    assert tf1 + tf2 == TransferFunction(2*s**2 + 5*s - 9, s**2 - 4*s - 5, s)
-    assert tf1 + (s - 1) == TransferFunction(s**2 - 5*s + 11, s - 5, s)
+    expect1 = TransferFunction((s - 5)*(s + 3) + (s + 1)*(s + 6), (s - 5)*(s + 1), s)
+    assert tf1 + tf2 == expect1
+    assert tf1 + (s - 1) == TransferFunction(s + (s - 5)*(s - 1) + 6, s - 5, s)
     assert tf1 + 8 == TransferFunction(9*s - 34, s - 5, s)
 
     c = symbols("c", commutative=False)
@@ -146,19 +146,20 @@ def test_TransferFunction_addition_and_subtraction():
     raises(ValueError, lambda: tf3 + tf4)
 
     # subtraction
-    assert tf1 - tf2 == TransferFunction(9*s + 21, s**2 - 4*s - 5, s)
+    expect1 = TransferFunction((s + 1)*(s + 6) - (s - 5)*(s + 3), (s - 5)*(s + 1), s)
+    assert tf1 - tf2 == expect1
     assert tf3 - tf2 == \
-        TransferFunction(-s**3 - 3*s**2 - 2*s - 2, s**3 + 2*s**2 + 2*s + 1, s)
-    assert tf1 - (s - 1) == TransferFunction(-s**2 + 7*s + 1, s - 5, s)
+        TransferFunction((s + 1)**2 - (s + 3)*(s**2 + s + 1), (s + 1)*(s**2 + s + 1), s)
+    assert tf1 - (s - 1) == TransferFunction(s - (s - 5)*(s - 1) + 6, s - 5, s)
     assert tf1 - 8 == TransferFunction(-7*s + 46, s - 5, s)
 
     raises(ValueError, lambda: tf1 - Matrix([1, 2, 3]))
     raises(ValueError, lambda: tf4 - Quaternion(3, 4, 5, 6))
-    raises(ValueError, lambda: tf2 - c)
     raises(ValueError, lambda: tf3 - tf4)
 
-    assert tf1 - tf2 + tf3 == \
-        TransferFunction(10*s**3 + 27*s**2 + 21*s + 16, s**4 - 3*s**3 - 8*s**2 - 9*s - 5, s)
+    p1 = (s - 5)*(s + 1)**2 + ((s + 1)*(s + 6) - (s - 5)*(s + 3))*(s**2 + s + 1)
+    p2 = (s - 5)*(s + 1)*(s**2 + s + 1)
+    assert tf1 - tf2 + tf3 == TransferFunction(p1, p2, s)
 
 
 def test_TransferFunction_multiplication_and_division():
@@ -170,12 +171,12 @@ def test_TransferFunction_multiplication_and_division():
     G6 = TransferFunction(s + 3, s + 1, s)
 
     # multiplication
-    expect1 = TransferFunction(s**2 + 4*s + 3, -s**4 + 5*s**3 + 9*s - 45, s)
+    expect1 = TransferFunction((s + 1)*(s + 3), (9 - s**3)*(s - 5), s)
     assert G1*G2 == expect1
 
-    expect2 = TransferFunction(p**2 + 4*p, p**5 - 5*p**4 - 6*p + 30, p)
+    expect2 = TransferFunction(p*(p + 4), (p - 5)*(p**4 - 6), p)
     assert G3*G4 == expect2
-    assert G5*(s - 1) == TransferFunction(s**2 + 5*s - 6, s - 5, s)
+    assert G5*(s - 1) == TransferFunction((s - 1)*(s + 6), s - 5, s)
     assert 9*G5 == TransferFunction(9*s + 54, s - 5, s)
 
     c = symbols("c", commutative=False)
@@ -185,26 +186,26 @@ def test_TransferFunction_multiplication_and_division():
     raises(ValueError, lambda: G3 * G5)
 
     # division
-    assert G5/G6 == TransferFunction(s**2 + 7*s + 6, s**2 - 2*s - 15, s)
+    assert G5/G6 == TransferFunction((s + 1)*(s + 6), (s - 5)*(s + 3), s)
     assert G5/2 == TransferFunction(s + 6, 2*s - 10, s)
-    assert G5/(s**2) == TransferFunction(s + 6, s**3 - 5*s**2, s)
+    assert G5/(s**2) == TransferFunction(s + 6, s**2*(s - 5), s)
 
     raises(ValueError, lambda: G3 / Matrix([1, 2, 3]))
     raises(ValueError, lambda: G4 / Quaternion(1, 2, 3, 4))
     raises(ValueError, lambda: G6 / 0)
     raises(ValueError, lambda: G3 / G5)
 
-    p1 = -s**6 - 2*s**5 + 30*s**4 + 41*s**3 - 2*s**2 - 327*s - 315
-    p2 = -s**6 + 7*s**5 + 5*s**4 - 66*s**3 - 63*s**2 - 45*s + 675
+    p1 = (9 - s**3)*(s - 5)*(s + 1)*(s + 6) + (s - 5)*(s + 1)*(s + 3)**2
+    p2 = (9 - s**3)*(s - 5)**2*(s + 3)
     assert G1*G2 + G5/G6 == TransferFunction(p1, p2, s)
 
     G3_ = TransferFunction(s**4 - 2*s**2 + 1, s - 1, s)
-    p3 = s**3 - 3*s**2 - 13*s + 15
-    p4 = -s**8 - s**7 + 2*s**6 + 11*s**5 + 8*s**4 - 19*s**3 - 18*s**2 + 9*s + 9
+    p3 = (s - 5)*(s - 1)*(s + 3)
+    p4 = (9 - s**3)*(s + 1)*(s**4 - 2*s**2 + 1)
     assert G1/G2/G3_ == TransferFunction(p3, p4, s)
 
-    p5 = s**9 + 2*s**8 - s**7 - 13*s**6 - 19*s**5 + 12*s**4 + 37*s**3 - 13*s**2 - 42*s + 36
-    p6 = -s**6 - 3*s**5 + s**4 + 12*s**3 + 27*s**2 - 9*s - 27
+    p5 = (s - 5)*(s - 1)*(s + 3)**2 - (9 - s**3)*(s + 1)**2*(s**4 - 2*s**2 + 1)
+    p6 = (9 - s**3)*(s - 1)*(s + 1)*(s + 3)
     assert G1/G2 - G3_/G6 == TransferFunction(p5, p6, s)
 
 
