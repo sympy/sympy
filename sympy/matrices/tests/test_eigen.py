25d27e06c20bdd4d7934ffefa656a9bb37597e32
Wes Galbraith <galbwe92@gmail.com>
2020-06-15 06:57:01 -0600

    Remove messages from assertions

--- sympy/matrices/tests/test_eigen.py
+++ sympy/matrices/tests/test_eigen.py
@@ -579,9 +579,5 @@ def test_definite():
         [0, 1, 2],
         [0, 2, 1]
     ])
-    assert not m.is_positive_definite, (
-        'The matrix \n\tm = {m}\nIs not positive definite,'
-        ' but m.is_positive_definite == True'.format(m=m))
-    assert not m.is_positive_semidefinite, (
-        'The matrix \n\tm = {m}\nIs not positive semidefinite,'
-        ' but m.is_positive_semidefinite == True'.format(m=m))
+    assert not m.is_positive_definite
+    assert not m.is_positive_semidefinite
