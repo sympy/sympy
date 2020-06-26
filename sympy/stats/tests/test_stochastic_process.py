f3b6176feb97d59969e60fca1804e48593760ac4
Smit Lunagariya <smitlunagariya.mat18@itbhu.ac.in>
2020-06-26 19:44:37 +0530

    typo

--- sympy/stats/tests/test_stochastic_process.py
+++ sympy/stats/tests/test_stochastic_process.py
@@ -7,7 +7,7 @@ from sympy.stats import (DiscreteMarkovChain, P, TransitionMatrixOf, E,
                          BernoulliProcess, PoissonProcess, WienerProcess,
                          GammaProcess, sample_stochastic_process)
 from sympy.stats.joint_rv import JointDistribution
-from sympy.stats.joint_rv_types JointDistributionHandmade
+from sympy.stats.joint_rv_types import JointDistributionHandmade
 from sympy.stats.rv import RandomIndexedSymbol
 from sympy.stats.symbolic_probability import Probability, Expectation
 from sympy.testing.pytest import raises, skip
