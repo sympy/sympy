6fab2397e6cbd08c61be124de126eae2b30b541f
Akhil Rajput <akh1lrjput@gmail.com>
2020-06-29 14:28:39 +0530

    closes #16222

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -698,6 +698,10 @@ def test_issue_14811():
     assert limit(((1 + ((S(2)/3)**(x + 1)))**(2**x))/(2**((S(4)/3)**(x - 1))), x, oo) == oo
 
 
+def test_issue_16222():
+    assert limit(exp(x), x, 1000000000) == exp(1000000000)
+
+
 def test_issue_16722():
     z = symbols('z', positive=True)
     assert limit(binomial(n + z, n)*n**-z, n, oo) == 1/gamma(z + 1)
