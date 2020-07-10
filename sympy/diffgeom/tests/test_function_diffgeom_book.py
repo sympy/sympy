1144a571c43a0e43f0551809e1ab7efdf92182bd
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-07-10 17:51:57 +0900

    Fix other tests

--- sympy/diffgeom/tests/test_function_diffgeom_book.py
+++ sympy/diffgeom/tests/test_function_diffgeom_book.py
@@ -1,9 +1,10 @@
-from sympy.diffgeom.rn import R2, R2_p, R2_r, R3_r, transforms_2d
+from sympy.diffgeom.rn import R2, R2_p, R2_r, R3_r
 from sympy.diffgeom import intcurve_series, Differential, WedgeProduct
 from sympy.core import symbols, Function, Derivative
 from sympy.simplify import trigsimp, simplify
 from sympy.functions import sqrt, atan2, sin, cos
 from sympy.matrices import Matrix
+from sympy.testing.pytest import warns_deprecated_sympy
 
 # Most of the functionality is covered in the
 # test_functional_diffgeom_ch* tests which are based on the
@@ -14,31 +15,32 @@ from sympy.matrices import Matrix
 # From "Functional Differential Geometry" as of 2011
 # by Sussman and Wisdom.
 
+
 def test_functional_diffgeom_ch2():
     x0, y0, r0, theta0 = symbols('x0, y0, r0, theta0', real=True)
     x, y = symbols('x, y', real=True)
     f = Function('f')
 
-    p = R2_r.point([x0, y0], transforms_2d)
-    assert R2_p.point_to_coords(p) == \
-        Matrix([sqrt(x0**2 + y0**2), atan2(y0, x0)])
-    p = R2_p.point([r0, theta0], transforms_2d)
-    assert R2_r.point_to_coords(p) == \
-        Matrix([r0*cos(theta0), r0*sin(theta0)])
+    assert (R2_p.point_to_coords(R2_r.point([x0, y0])) ==
+           Matrix([sqrt(x0**2 + y0**2), atan2(y0, x0)]))
+    assert (R2_r.point_to_coords(R2_p.point([r0, theta0])) ==
+           Matrix([r0*cos(theta0), r0*sin(theta0)]))
 
-    assert R2_p.jacobian(R2_r, [r0, theta0], transforms_2d) == \
-        Matrix([
-            [cos(theta0), -r0*sin(theta0)],
-            [sin(theta0), r0*cos(theta0)]])
+    assert R2_p.jacobian_matrix(R2_r, [r0, theta0]) == Matrix(
+            [[cos(theta0), -r0*sin(theta0)], [sin(theta0), r0*cos(theta0)]])
+
+    with warns_deprecated_sympy():
+        assert R2_p.jacobian(R2_r, [r0, theta0]) == Matrix(
+            [[cos(theta0), -r0*sin(theta0)], [sin(theta0), r0*cos(theta0)]])
 
     field = f(R2.x, R2.y)
-    p1_in_rect = R2_r.point([x0, y0], transforms_2d)
-    p1_in_polar = R2_p.point([sqrt(x0**2 + y0**2), atan2(y0, x0)], transforms_2d)
+    p1_in_rect = R2_r.point([x0, y0])
+    p1_in_polar = R2_p.point([sqrt(x0**2 + y0**2), atan2(y0, x0)])
     assert field.rcall(p1_in_rect) == f(x0, y0)
     assert field.rcall(p1_in_polar) == f(x0, y0)
 
-    p_r = R2_r.point([x0, y0], transforms_2d)
-    p_p = R2_p.point([r0, theta0], transforms_2d)
+    p_r = R2_r.point([x0, y0])
+    p_p = R2_p.point([r0, theta0])
     assert R2.x(p_r) == x0
     assert R2.x(p_p) == r0*cos(theta0)
     assert R2.r(p_p) == r0
@@ -56,17 +58,17 @@ def test_functional_diffgeom_ch3():
     f = Function('f')
     b1 = Function('b1')
     b2 = Function('b2')
-    p_r = R2_r.point([x0, y0], transforms_2d)
+    p_r = R2_r.point([x0, y0])
 
     s_field = f(R2.x, R2.y)
     v_field = b1(R2.x)*R2.e_x + b2(R2.y)*R2.e_y
     assert v_field.rcall(s_field).rcall(p_r).doit() == b1(
         x0)*Derivative(f(x0, y0), x0) + b2(y0)*Derivative(f(x0, y0), y0)
 
-    v = R2.e_x(R2.r**2)
-    assert v.rcall(p_r) == 2*x0
-    v = (R2.e_x + 2*R2.e_y).rcall(R2.r**2 + 3*R2.x)
-    assert v.rcall(p_r).doit() == 2*x0 + 4*y0 + 3
+    assert R2.e_x(R2.r**2).rcall(p_r) == 2*x0
+    v = R2.e_x + 2*R2.e_y
+    s = R2.r**2 + 3*R2.x
+    assert v.rcall(s).rcall(p_r).doit() == 2*x0 + 4*y0 + 3
 
     circ = -R2.y*R2.e_x + R2.x*R2.e_y
     series = intcurve_series(circ, t, R2_r.point([1, 0]), coeffs=True)
@@ -84,8 +86,8 @@ def test_functional_diffgeom_ch4():
     f = Function('f')
     b1 = Function('b1')
     b2 = Function('b2')
-    p_r = R2_r.point([x0, y0], transforms_2d)
-    p_p = R2_p.point([r0, theta0], transforms_2d)
+    p_r = R2_r.point([x0, y0])
+    p_p = R2_p.point([r0, theta0])
 
     f_field = b1(R2.x, R2.y)*R2.dx + b2(R2.x, R2.y)*R2.dy
     assert f_field.rcall(R2.e_x).rcall(p_r) == b1(x0, y0)
@@ -98,12 +100,10 @@ def test_functional_diffgeom_ch4():
 
     s_field_p = f(R2.r, R2.theta)
     df = Differential(s_field_p)
-    df_e_x = df(R2.e_x)
-    assert trigsimp(df_e_x.rcall(p_p).doit()) == (
+    assert trigsimp(df(R2.e_x).rcall(p_p).doit()) == (
         cos(theta0)*Derivative(f(r0, theta0), r0) -
         sin(theta0)*Derivative(f(r0, theta0), theta0)/r0)
-    df_e_y = df(R2.e_y)
-    assert trigsimp(df_e_y.rcall(p_p).doit()) == (
+    assert trigsimp(df(R2.e_y).rcall(p_p).doit()) == (
         sin(theta0)*Derivative(f(r0, theta0), r0) +
         cos(theta0)*Derivative(f(r0, theta0), theta0)/r0)
 
@@ -113,17 +113,12 @@ def test_functional_diffgeom_ch4():
     assert R2.dx(R2.e_y) == 0
 
     circ = -R2.y*R2.e_x + R2.x*R2.e_y
-    dx_circ = R2.dx(circ)
-    assert dx_circ.rcall(p_r).doit() == -y0
-    dy_circ = R2.dy(circ)
-    assert dy_circ.rcall(p_r) == x0
-    dr_circ = R2.dr(circ)
-    assert dr_circ.rcall(p_r) == 0
-    dt_circ = R2.dtheta(circ)
-    assert simplify(dt_circ.rcall(p_r)) == 1
+    assert R2.dx(circ).rcall(p_r).doit() == -y0
+    assert R2.dy(circ).rcall(p_r) == x0
+    assert R2.dr(circ).rcall(p_r) == 0
+    assert simplify(R2.dtheta(circ).rcall(p_r)) == 1
 
-    new = (circ - R2.e_theta).rcall(s_field_r)
-    assert new.rcall(p_r) == 0
+    assert (circ - R2.e_theta).rcall(s_field_r).rcall(p_r) == 0
 
 
 def test_functional_diffgeom_ch6():
