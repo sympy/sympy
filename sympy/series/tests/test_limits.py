2cead643410142b0ad9f270501dc806529415344
Rudr Tiwari <rudrtiwari@gmail.com>
2020-07-10 18:18:35 +0530

    series : Added test for issue 14793

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -276,6 +276,12 @@ def test_issue_5164():
     assert limit(x**(-0.5), x, 4) == S(4)**(-0.5)
 
 
+def test_issue_14793():
+    expr = ((x + S(1)/2) * log(x) - x + log(2*pi)/2 - \
+        log(factorial(x)) + S(1)/(12*x))*x**3
+    assert simplify(limit(expr, x, oo) - 1/360) == 0
+
+
 def test_issue_5183():
     # using list(...) so py.test can recalculate values
     tests = list(cartes([x, -x],
