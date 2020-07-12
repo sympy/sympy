97f96be0cd39c90dd203d7aba466b8452e0aee15
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-07-12 14:53:06 +0900

    Define parameters and add test

--- sympy/map/map.py
+++ sympy/map/map.py
@@ -1,31 +1,48 @@
-from sympy.core import Expr, S
+from sympy.core import Expr, S, Tuple
 from sympy.core.sympify import _sympify
 from sympy.core.symbol import Str
 
 class Map(Expr):
     """
     A class for general mathematical mappings
+
+    Parameters
+    ==========
+
+    parameters
+        Parameters of the map
+
+    domain, codomain : Set
+
+    name : str
+        Name of the map
+
     """
     def __new__(
-        cls, *args, domain=S.UniversialSet, codomain=S.UniversialSet,
+        cls, parameters, domain=S.UniversalSet, codomain=S.UniversalSet,
         name='', **kwargs
     ):
+        parameters = Tuple(*[_sympify(a) for a in parameters])
         domain, codomain = _sympify(domain), _sympify(codomain)
-        args = (_sympify(a) for a in args)
-        name = Str(name)
-        return super().__new__(cls, *args, domain, codomain, name)
+        if not isinstance(name, Str):
+            name = Str(name)
+        return super().__new__(cls, parameters, domain, codomain, name)
+
+    @property
+    def parameters(self):
+        return self.args[0]
 
     @property
     def domain(self):
-        return self.args[-3]
+        return self.args[1]
 
     @property
     def codomain(self):
-        return self.args[-2]
+        return self.args[2]
 
     @property
     def name(self):
-        return self.args[-1]
+        return self.args[3]
 
     def eval(self, *args):
         return None
@@ -35,10 +52,54 @@ class Map(Expr):
             ret = self.eval(*args)
             if ret is not None:
                 return ret
-        return AppliedMap(self, *args, evalaute=False)
+        return AppliedMap(self, *args, evaluate=False)
 
 class AppliedMap(Expr):
-    def __new__(cls, func, *args, evalaute=False):
+    """
+    Unevaluated result of Map applied to arguments
+
+    Parameters
+    ==========
+
+    map : Map
+
+    args
+        arguments applied to *map*
+
+    evaluate : bool, optional
+        If True, returns evaluated application of *map* to *args*
+
+    """
+    def __new__(cls, map, *args, evaluate=False):
+        args = (_sympify(a) for a in args)
         if evaluate:
-            return func(*args, evalaute=True)
-        return super().__new__(cls, func, *args)
+            return map(*args, evaluate=True)
+        return super().__new__(cls, map, *args)
+
+    @property
+    def map(self):
+        return self.args[0]
+
+    @property
+    def arguments(self):
+        return self.args[1:]
+
+    @property
+    def parameters(self):
+        return self.map.parameters
+
+    @property
+    def free_symbols(self):
+        result = set()
+        result.update(self.parameters.free_symbols)
+        for a in self.arguments:
+            result.update(a.free_symbols)
+        return result
+
+    def doit(self, **hints):
+        deep = hints.get('deep', True)
+        if deep:
+            arguments = (a.doit(**hints) for a in self.arguments)
+            return self.func(self.map, *arguments, evaluate=True)
+        else:
+            return self.func(self.map, *self.arguments, evaluate=True)
---
+++ sympy/map/tests/test_map.py
@@ -0,0 +1,18 @@
+from sympy import symbols, Tuple
+from sympy.map import Map, AppliedMap
+
+a, b, c, x = symbols('a b c x')
+
+def test_parameters():
+
+    class QuadraticFunction(Map):
+        def eval(self, x):
+            a, b, c = self.parameters
+            return a*x**2 + b*x + c
+    f = QuadraticFunction((a, b, c))
+
+    assert f(x, evaluate=True) == f(x).doit() == a*x**2 + b*x + c
+    assert f(x).free_symbols == {a, b, c, x}
+
+    assert f.subs(a, 2)(x).doit() == f(x).subs(a, 2).doit() == 2*x**2 + b*x + c
+    assert f(x).subs(a, 2).free_symbols == {b, c, x}
