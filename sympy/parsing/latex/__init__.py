71a13859d5f3e495fa5fdf4d00f738d8921b4f68
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-07-22 19:52:06 -0400

    Fixing importing of grammar file is lark is installed

--- sympy/parsing/latex/__init__.py
+++ sympy/parsing/latex/__init__.py
@@ -1,3 +1,3 @@
 """Used for translating LaTeX source code into a SymPy expression. """
 
-from sympy.parsing.latex.latex_parser import latex_parser # noqa
+from sympy.parsing.latex.latex_parser import parse_latex  # noqa
