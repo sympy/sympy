bf34f3681c193b1ae4456cc3115641537d0d4537
mijo2 <milan.cs16@iitp.ac.in>
2020-07-23 13:49:06 +0530

    solvers.ode: Fixed some of the test cases
    Some of the test cases will have different ordering in different
    Python versions. This was fixed in this commit

--- sympy/solvers/ode/tests/test_systems.py
+++ sympy/solvers/ode/tests/test_systems.py
@@ -1224,17 +1224,17 @@ def test_component_division():
             Eq(g(x), C1*exp(2*x) + C2),
             Eq(h(x), C3*exp(x)),
             Eq(k(x), (C4 + Integral(C3**4*exp(3*x), x))*exp(x))]
-    components1 = [[[Eq(Derivative(f(x), x), 2*f(x))], [Eq(Derivative(g(x), x), f(x))]],
-                    [[Eq(Derivative(h(x), x), h(x))], [Eq(Derivative(k(x), x), h(x)**4 + k(x))]]]
+    components1 = {((Eq(Derivative(f(x), x), 2*f(x)),), (Eq(Derivative(g(x), x), f(x)),)),
+                   ((Eq(Derivative(h(x), x), h(x)),), (Eq(Derivative(k(x), x), h(x)**4 + k(x)),))}
     eqsdict1 = ({f(x): set(), g(x): {f(x)}, h(x): set(), k(x): {h(x)}},
                 {f(x): Eq(Derivative(f(x), x), 2*f(x)),
                 g(x): Eq(Derivative(g(x), x), f(x)),
                 h(x): Eq(Derivative(h(x), x), h(x)),
                 k(x): Eq(Derivative(k(x), x), h(x)**4 + k(x))})
-    graph1 = ([f(x), g(x), h(x), k(x)], [(g(x), f(x)), (k(x), h(x))])
-    assert _component_division(eqs1, funcs, x) == components1
+    graph1 = [{f(x), g(x), h(x), k(x)}, {(g(x), f(x)), (k(x), h(x))}]
+    assert {tuple(tuple(scc) for scc in wcc) for wcc in _component_division(eqs1, funcs, x)} == components1
     assert _eqs2dict(eqs1, funcs) == eqsdict1
-    assert _dict2graph(eqsdict1[0]) == graph1
+    assert [set(element) for element in _dict2graph(eqsdict1[0])] == graph1
     assert dsolve(eqs1) == sol1
     assert checksysodesol(eqs1, sol1) == (True, [0, 0, 0, 0])
 
@@ -1246,19 +1246,19 @@ def test_component_division():
             Eq(g(x), C2 + Integral(C1*exp(2*x), x)),
             Eq(h(x), C3*exp(x)),
             Eq(k(x), (C4 + Integral(C1**4*exp(7*x), x))*exp(x))]
-    components2 = [[[Eq(Derivative(f(x), x), 2*f(x))],
-                    [Eq(Derivative(g(x), x), f(x))],
-                    [Eq(Derivative(k(x), x), f(x)**4 + k(x))]],
-                    [[Eq(Derivative(h(x), x), h(x))]]]
+    components2 = {frozenset([(Eq(Derivative(f(x), x), 2*f(x)),),
+                    (Eq(Derivative(g(x), x), f(x)),),
+                    (Eq(Derivative(k(x), x), f(x)**4 + k(x)),)]),
+                   frozenset([(Eq(Derivative(h(x), x), h(x)),)])}
     eqsdict2 = ({f(x): set(), g(x): {f(x)}, h(x): set(), k(x): {f(x)}},
                  {f(x): Eq(Derivative(f(x), x), 2*f(x)),
                   g(x): Eq(Derivative(g(x), x), f(x)),
                   h(x): Eq(Derivative(h(x), x), h(x)),
                   k(x): Eq(Derivative(k(x), x), f(x)**4 + k(x))})
-    graph2 = ([f(x), g(x), h(x), k(x)], [(g(x), f(x)), (k(x), f(x))])
-    assert _component_division(eqs2, funcs, x) == components2
+    graph2 = [{f(x), g(x), h(x), k(x)}, {(g(x), f(x)), (k(x), f(x))}]
+    assert {frozenset(tuple(scc) for scc in wcc) for wcc in _component_division(eqs2, funcs, x)} == components2
     assert _eqs2dict(eqs2, funcs) == eqsdict2
-    assert _dict2graph(eqsdict2[0]) == graph2
+    assert [set(element) for element in _dict2graph(eqsdict2[0])] == graph2
     assert dsolve(eqs2) == sol2
     assert checksysodesol(eqs2, sol2) == (True, [0, 0, 0, 0])
 
@@ -1270,19 +1270,19 @@ def test_component_division():
             Eq(g(x), C2 + Integral(C1*exp(2*x) + x, x)),
             Eq(h(x), C3*exp(x)),
             Eq(k(x), (C4 + Integral(C1**4*exp(7*x), x))*exp(x))]
-    components3 = [[[Eq(Derivative(f(x), x), 2*f(x))],
-                    [Eq(Derivative(g(x), x), x + f(x))],
-                    [Eq(Derivative(k(x), x), f(x)**4 + k(x))]],
-                    [[Eq(Derivative(h(x), x), h(x))]]]
+    components3 = {frozenset([(Eq(Derivative(f(x), x), 2*f(x)),),
+                    (Eq(Derivative(g(x), x), x + f(x)),),
+                    (Eq(Derivative(k(x), x), f(x)**4 + k(x)),)]),
+                    frozenset([(Eq(Derivative(h(x), x), h(x)),),])}
     eqsdict3 = ({f(x): set(), g(x): {f(x)}, h(x): set(), k(x): {f(x)}},
                 {f(x): Eq(Derivative(f(x), x), 2*f(x)),
                 g(x): Eq(Derivative(g(x), x), x + f(x)),
                 h(x): Eq(Derivative(h(x), x), h(x)),
                 k(x): Eq(Derivative(k(x), x), f(x)**4 + k(x))})
-    graph3 = ([f(x), g(x), h(x), k(x)], [(g(x), f(x)), (k(x), f(x))])
-    assert _component_division(eqs3, funcs, x) == components3
+    graph3 = [{f(x), g(x), h(x), k(x)}, {(g(x), f(x)), (k(x), f(x))}]
+    assert {frozenset(tuple(scc) for scc in wcc) for wcc in _component_division(eqs3, funcs, x)} == components3
     assert _eqs2dict(eqs3, funcs) == eqsdict3
-    assert _dict2graph(eqsdict3[0]) == graph3
+    assert [set(l) for l in _dict2graph(eqsdict3[0])] == graph3
     assert dsolve(eqs3) == sol3
     assert checksysodesol(eqs3, sol3) == (True, [0, 0, 0, 0])
 
@@ -1290,37 +1290,37 @@ def test_component_division():
             Eq(Derivative(g(x), x), f(x) + x*g(x) + x),
             Eq(Derivative(h(x), x), h(x)),
             Eq(Derivative(k(x), x), f(x)**4 + k(x))]
-    sol4 = [Eq(f(x), (C1/2 - sqrt(2)*C2/2 - sqrt(2)*Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 +
-                sqrt(2)*x)/2, x)/2 + Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 +
-                sqrt(2)*x)/2, x)/2)*exp(x**2/2 - sqrt(2)*x) + (C1/2 + sqrt(2)*C2/2 + sqrt(2)*Integral(x*exp(-x**2/2 -
-                sqrt(2)*x)/2 + x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 + Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 -
-                sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2)*exp(x**2/2 + sqrt(2)*x)),
-            Eq(g(x), (-sqrt(2)*C1/4 + C2/2 + Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 -
-                sqrt(2)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/4)*
-                exp(x**2/2 - sqrt(2)*x) + (sqrt(2)*C1/4 + C2/2 + Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 +
-                sqrt(2)*x)/2, x)/2 + sqrt(2)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 +
-                sqrt(2)*x)/2, x)/4)*exp(x**2/2 + sqrt(2)*x)),
+    sol4 = [Eq(f(x), (C1/2 - sqrt(2)*C2/2 - sqrt(2)*Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 +\
+                sqrt(2)*x)/2, x)/2 + Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 +\
+                sqrt(2)*x)/2, x)/2)*exp(x**2/2 - sqrt(2)*x) + (C1/2 + sqrt(2)*C2/2 + sqrt(2)*Integral(x*exp(-x**2/2
+                - sqrt(2)*x)/2 + x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 + Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2
+                - sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2)*exp(x**2/2 + sqrt(2)*x)),
+            Eq(g(x), (-sqrt(2)*C1/4 + C2/2 + Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 -\
+                sqrt(2)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2,
+                x)/4)*exp(x**2/2 - sqrt(2)*x) + (sqrt(2)*C1/4 + C2/2 + Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 +
+                x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 + sqrt(2)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 -
+                sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/4)*exp(x**2/2 + sqrt(2)*x)),
             Eq(h(x), C3*exp(x)),
             Eq(k(x), C4*exp(x) + exp(x)*Integral((C1*exp(x**2/2 - sqrt(2)*x)/2 + C1*exp(x**2/2 + sqrt(2)*x)/2 -
                 sqrt(2)*C2*exp(x**2/2 - sqrt(2)*x)/2 + sqrt(2)*C2*exp(x**2/2 + sqrt(2)*x)/2 - sqrt(2)*exp(x**2/2 -
                 sqrt(2)*x)*Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 + exp(x**2/2 -
-                sqrt(2)*x)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2 +
-                sqrt(2)*exp(x**2/2 + sqrt(2)*x)*Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 +
+                sqrt(2)*x)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 - sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2,
+                x)/2 + sqrt(2)*exp(x**2/2 + sqrt(2)*x)*Integral(x*exp(-x**2/2 - sqrt(2)*x)/2 + x*exp(-x**2/2 +
                 sqrt(2)*x)/2, x)/2 + exp(x**2/2 + sqrt(2)*x)*Integral(sqrt(2)*x*exp(-x**2/2 - sqrt(2)*x)/2 -
                 sqrt(2)*x*exp(-x**2/2 + sqrt(2)*x)/2, x)/2)**4*exp(-x), x))]
-    components4 = [[[Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
-                    Eq(Derivative(g(x), x), x*g(x) + x + f(x))],
-                    [Eq(Derivative(k(x), x), f(x)**4 + k(x))]],
-                    [[Eq(Derivative(h(x), x), h(x))]]]
+    components4 = {(frozenset([Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
+                    Eq(Derivative(g(x), x), x*g(x) + x + f(x))]),
+                    frozenset([Eq(Derivative(k(x), x), f(x)**4 + k(x)),])),
+                    (frozenset([Eq(Derivative(h(x), x), h(x)),]),)}
     eqsdict4 = ({f(x): {g(x)}, g(x): {f(x)}, h(x): set(), k(x): {f(x)}},
                 {f(x): Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
                 g(x): Eq(Derivative(g(x), x), x*g(x) + x + f(x)),
                 h(x): Eq(Derivative(h(x), x), h(x)),
                 k(x): Eq(Derivative(k(x), x), f(x)**4 + k(x))})
-    graph4 = ([f(x), g(x), h(x), k(x)], [(f(x), g(x)), (g(x), f(x)), (k(x), f(x))])
-    assert _component_division(eqs4, funcs, x) == components4
+    graph4 = [{f(x), g(x), h(x), k(x)}, {(f(x), g(x)), (g(x), f(x)), (k(x), f(x))}]
+    assert {tuple(frozenset(scc) for scc in wcc) for wcc in _component_division(eqs4, funcs, x)} == components4
     assert _eqs2dict(eqs4, funcs) == eqsdict4
-    assert _dict2graph(eqsdict4[0]) == graph4
+    assert [set(element) for element in _dict2graph(eqsdict4[0])] == graph4
     assert dsolve(eqs4) == sol4
     assert checksysodesol(eqs4, sol4) == (True, [0, 0, 0, 0])
 
@@ -1333,19 +1333,19 @@ def test_component_division():
             Eq(h(x), C3*exp(x)),
             Eq(k(x), C4*exp(x) + exp(x)*Integral((C1*exp(x**2/2 - sqrt(2)*x)/2 + C1*exp(x**2/2 + sqrt(2)*x)/2 -
                 sqrt(2)*C2*exp(x**2/2 - sqrt(2)*x)/2 + sqrt(2)*C2*exp(x**2/2 + sqrt(2)*x)/2)**4*exp(-x), x))]
-    components5 = [[[Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
-                    Eq(Derivative(g(x), x), x*g(x) + f(x))],
-                    [Eq(Derivative(k(x), x), f(x)**4 + k(x))]],
-                    [[Eq(Derivative(h(x), x), h(x))]]]
+    components5 = {(frozenset([Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
+                    Eq(Derivative(g(x), x), x*g(x) + f(x))]),
+                    frozenset([Eq(Derivative(k(x), x), f(x)**4 + k(x)),])),
+                    (frozenset([Eq(Derivative(h(x), x), h(x)),]),)}
     eqsdict5 = ({f(x): {g(x)}, g(x): {f(x)}, h(x): set(), k(x): {f(x)}},
                 {f(x): Eq(Derivative(f(x), x), x*f(x) + 2*g(x)),
                 g(x): Eq(Derivative(g(x), x), x*g(x) + f(x)),
                 h(x): Eq(Derivative(h(x), x), h(x)),
                 k(x): Eq(Derivative(k(x), x), f(x)**4 + k(x))})
-    graph5 = ([f(x), g(x), h(x), k(x)], [(f(x), g(x)), (g(x), f(x)), (k(x), f(x))])
-    assert _component_division(eqs5, funcs, x) == components5
+    graph5 = [{f(x), g(x), h(x), k(x)}, {(f(x), g(x)), (g(x), f(x)), (k(x), f(x))}]
+    assert {tuple(frozenset(scc) for scc in wcc) for wcc in _component_division(eqs5, funcs, x)} == components5
     assert _eqs2dict(eqs5, funcs) == eqsdict5
-    assert _dict2graph(eqsdict5[0]) == graph5
+    assert [set(element) for element in _dict2graph(eqsdict5[0])] == graph5
     assert dsolve(eqs5) == sol5
     assert checksysodesol(eqs5, sol5) == (True, [0, 0, 0, 0])
 
