cc2e145d139820c663fe3231c95b1fe76a837713
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-07-23 17:53:12 +0900

    Implement identities to BinaryOperator

--- sympy/map/operator.py
+++ sympy/map/operator.py
@@ -7,33 +7,41 @@ __all__ = [
 
 class BinaryOperator(Map):
     """
-    An abstract class for general binary operator.
+    An abstract class for general algebraic binary operator.
 
     Explanation
     ===========
 
     In mathematics, a binary operation is a calculation that combines
     two elements (called operands) to produce another element [1].
-    Unlike ordinary mappings, binary operators are often written using
-    infix notation [1].
+
+    Binary operator can multiple left identities or right identities. However,
+    if it has both left and right identity, there is a single two-sided identity
+    and no other left or right identity. For this case, you must give ``identity``
+    attribute to the operator.
 
     Examples
     ========
 
     >>> from sympy.map import BinaryOperator
-    >>> from sympy.abc import x, y
+    >>> from sympy.abc import a, b, e
 
-    >>> class AddOp(BinaryOperator):
-    ...     name = '+'
-    >>> addop = AddOp()
+    >>> class Op(BinaryOperator):
+    ...     name = '.'
+    ...     identity = e
+    >>> op = Op()
 
-    >>> addop(x, y)
-    x + y
+    >>> op(a, b)
+    a . b
 
-    BinaryOperator does not flatten nested arguments.
+    Binary operation with identity is evaluated.
 
-    >>> addop(addop(x, y), y)
-    (x + y) + y
+    >>> op(a, e)
+    a . e
+    >>> op(a, e, evaluate=True)
+    a
+    >>> op(e, b, evaluate=True)
+    b
 
     References
     ==========
@@ -42,12 +50,152 @@ class BinaryOperator(Map):
 
     """
 
+    # to be overridden
+    @property
+    def identity(self):
+        return
+
+    def left_identity(self, element):
+        r"""
+        Returns ``True`` if *element* is left identity of *self*.
+
+        Explanation
+        ===========
+
+        For a set $S$ and binary operation $*:S \times S \rightarrow S$, $e \in S$ is
+        left identity of $*$ if $ e * x = x$ for any $x \in S$.
+        There can be multiple left identities. However, if left identity and right identity
+        both exist, there is only a single two-sided identity.
+
+        Examples
+        ========
+
+        >>> from sympy.map import BinaryOperator
+        >>> from sympy.abc import a, b, c
+
+        >>> class Op(BinaryOperator):
+        ...     name = '.'
+        ...     def _eval_left_identity(self, element):
+        ...         return element in (a, b)
+        >>> op = Op()
+
+        >>> op(c, a, evaluate=True)
+        c . a
+        >>> op(a, c, evaluate=True)
+        c
+        >>> op(b, c, evaluate=True)
+        c
+
+        """
+        if self.identity is not None:
+            if element == self.identity:
+                return True
+            return False
+        result = self._eval_left_identity(element)
+        if result is not None:
+            return result
+        return
+
+    def _eval_left_identity(self, element):
+        return
+
+    def right_identity(self, element):
+        r"""
+        Returns ``True`` *element* is right identity of *self*.
+
+        Explanation
+        ===========
+
+        For a set $S$ and binary operation $*:S \times S \rightarrow S$, $e \in S$ is
+        left identity of $*$ if $ x * e = x$ for any $x \in S$.
+        There can be multiple right identities. However, if left identity and right identity
+        both exist, there is only a single two-sided identity.
+
+        Examples
+        ========
+
+        >>> from sympy.map import BinaryOperator
+        >>> from sympy.abc import a, b, c
+
+        >>> class Op(BinaryOperator):
+        ...     name = '.'
+        ...     def _eval_right_identity(self, element):
+        ...         return element in (a, b)
+        >>> op = Op()
+
+        >>> op(a, c, evaluate=True)
+        a . c
+        >>> op(c, a, evaluate=True)
+        c
+        >>> op(c, a, evaluate=True)
+        c
+
+        """
+        if self.identity is not None:
+            if element == self.identity:
+                return True
+            return False
+        result = self._eval_right_identity(element)
+        if result is not None:
+            return result
+        return
+
+    def _eval_right_identity(self, element):
+        return
+
+    def remove_identity(self, seq):
+        # this method is deliberately designed to work for len(seq) > 2 case
+        # to be compatible if self is AssociativeOperator.
+        if self.identity is not None:
+            return [o for o in seq if o != self.identity]
+
+        newseq = []
+        for i,o in enumerate(seq):
+            if self.left_identity(o):
+                # a*e != a if e is left identity
+                if i != len(seq)-1:
+                    continue
+            elif self.right_identity(o):
+                # e*a != a if e is right identity
+                if i != 0:
+                    continue
+            newseq.append(o)
+        return newseq
+
     def __call__(self, *args, evaluate=False, **kwargs):
         return AppliedBinaryOperator(self, args, evaluate=evaluate)
 
+class AppliedBinaryOperator(AppliedMap):
+    """
+    Unevaluated result of BinaryOperator applied to arguments.
+
+    """
+
+    def __new__(cls, mapping, args, evaluate=False, **kwargs):
+
+        if len(args) < 2:
+            raise TypeError(
+        "%s argument given to binary operator." % len(args)
+        )
+        if not mapping.is_associative and len(args) > 2:
+            raise TypeError(
+        "Multiple aruments given to non-associative binary operator."
+        )
+        args = [_sympify(a) for a in args]
+
+        if evaluate:
+            args = mapping.remove_identity(args)
+
+            if not args:
+                return mapping.identity
+            if len(args) == 1:
+                return args[0]
+
+        return super().__new__(cls, mapping, args, evaluate=evaluate, **kwargs)
+
 class AssociativeOperator(BinaryOperator):
     r"""
-    An abstract class for associative binary operator
+    An abstract class for general algebraic associative binary operator
 
     Explanation
     ===========
@@ -64,72 +212,60 @@ class AssociativeOperator(BinaryOperator):
     ========
 
     >>> from sympy.map import AssociativeOperator
-    >>> from sympy.abc import x, y
+    >>> from sympy.abc import a, b, e
 
     >>> class AddOp(AssociativeOperator):
     ...     name = '+'
+    ...     identity = e
     >>> addop = AddOp()
 
     AssociativeOperator flattens nested arguments.
 
-    >>> addop(addop(x, y), y, evaluate=True)
-    x + y + y
+    >>> addop(addop(a, b), b, evaluate=True)
+    a + b + b
+
+    Deeply nested arguments are not flattened. Use ``doit`` method for recursive flattening.
+
+    >>> addop(addop(addop(a, b), b), b, evaluate=True)
+    (a + b) + b + b
+    >>> addop(addop(addop(a, b), b), b).doit()
+    a + b + b + b
+
+    Associative operation with identity is evaluated.
+
+    >>> addop(addop(a, e), b, evaluate=True)
+    a + b
+    >>> addop(addop(e, e), addop(e, e), evaluate=True)
+    e
 
     """
 
     is_associative = True
 
     def flatten(self, seq):
+        # new_seq = []
+        # while seq:
+        #     o = seq.pop()
+        #     if isinstance(o, AppliedMap) and o.map.is_restriction(self):
+        #         seq.extend(o.arguments)
+        #     else:
+        #         new_seq.append(o)
+        # new_seq.reverse()
+        # return new_seq
         new_seq = []
-        while seq:
-            o = seq.pop()
+        for o in seq:
             if isinstance(o, AppliedMap) and o.map.is_restriction(self):
-                seq.extend(o.arguments)
+                new_seq.extend(o.arguments)
             else:
                 new_seq.append(o)
-        new_seq.reverse()
         return new_seq
 
     def __call__(self, *args, evaluate=False, **kwargs):
         return AppliedAssociativeOperator(self, args, evaluate=evaluate)
 
-class AppliedBinaryOperator(AppliedMap):
-    r"""
-    Unevaluated result of BinaryOperator applied to arguments.
-
-    Examples
-    ========
-
-    >>> from sympy.map import BinaryOperator
-    >>> from sympy.abc import x, y
-
-    >>> class AddOp(BinaryOperator):
-    ...     name = '+'
-    >>> addop = AddOp()
-    >>> expr = addop(addop(x, y), y, evaluate=True)
-
-    >>> expr.arguments # Not flattened
-    (x + y, y)
-
-    """
-
 class AppliedAssociativeOperator(AppliedBinaryOperator):
     """
     Unevaluated result of AssociativeOperator applied to arguments.
-
-    Examples
-    ========
-
-    >>> from sympy.map import AssociativeOperator
-    >>> from sympy.abc import x, y
-
-    >>> class AddOp(AssociativeOperator):
-    ...     name = '+'
-    >>> addop = AddOp()
-    >>> expr = addop(addop(x, y), y, evaluate=True)
-
-    >>> expr.arguments  # Flattened
-    (x, y, y)
 
     """
     def __new__(cls, mapping, args, evaluate=False, **kwargs):
--- sympy/map/tests/test_operator.py
+++ sympy/map/tests/test_operator.py
@@ -3,11 +3,12 @@ from sympy.map import (
     BinaryOperator, AssociativeOperator,
     AppliedBinaryOperator, AppliedAssociativeOperator,
 )
+from sympy.testing.pytest import raises
 
 A = Set('A')
-a1, a2 = A.element('a1'), A.element('a2')
+a1, a2, a3 = [A.element('a%s' % i) for i in range(3)]
 B = Set('B', (A,))
-b1, b2 = B.element('b1'), B.element('b2')
+b1, b2, b3 = [B.element('b%s' % i) for i in range(3)]
 
 class F(BinaryOperator):
     def __new__(cls, domain, codomain, **kwargs):
@@ -15,21 +16,65 @@ class F(BinaryOperator):
         obj.domain = domain
         obj.codomain = codomain
         return obj
+
+    def _eval_left_identity(self, element):
+        # assume identity is in A but outside of B
+        if element == a3:
+            return True
 f = F(A**2, A)
 
-class G(F, AssociativeOperator):
-    pass
+class F2(F):
+    identity = a3
+f2 = F2(A**2, A)
+
+class G(AssociativeOperator):
+    def __new__(cls, domain, codomain, **kwargs):
+        obj = super().__new__(cls, domain, codomain, **kwargs)
+        obj.domain = domain
+        obj.codomain = codomain
+        return obj
+
+    def _eval_right_identity(self, element):
+        # assume identity is in B (thus in A as well)
+        if element == b3:
+            return True
+
 g_A = G(A**2, B)
 g_B = G(B**2, B)
+
+class G2(G):
+    identity = a3
+g2 = G2(A**2, A)
 
 def test_BinaryOperator():
     # calling binary operator returns AppliedBinaryOperator
     assert isinstance(f(a1, a2), AppliedBinaryOperator)
 
+    # 0 or 1 arguments to binary operator is not supported
+    raises(TypeError, lambda: f())
+    raises(TypeError, lambda: f(a1))
+
+    # 3 or more arguments to non-associative binary operator is not supported
+    raises(TypeError, lambda: f(a1, a2, a2))
+
+    # identity checking
+    assert f.left_identity(a3)
+    assert not f.right_identity(a3)
+
+    # two side identity is both left and right identity
+    assert f2.left_identity(a3)
+    assert f2.right_identity(a3)
+
 def test_AppliedBinaryOperator():
     # check nested application (not flattened)
-    expr = f(f(a1, a1), f(a2, a2), evaluate=True)
+    expr = f(f(a1, a1), f(a2, a2)).doit()
     assert expr.arguments == (f(a1, a1), f(a2, a2))
+
+    # identity removal
+    expr = f(f(a3, a2), f(a1, a3)).doit()
+    assert expr.arguments == (a2, f(a1, a3))
+    expr = f2(f2(a3, a2), f2(a1, a3)).doit()
+    assert expr.arguments == (a2, a1)
 
 def test_AssociativeOperator():
     # calling associative operator returns AppliedAssociativeOperator
@@ -48,3 +93,16 @@ def test_AppliedAssociativeOperator():
     # extended function in argument is not flattened
     expr = g_B(g_B(a1, b1), g_A(b2, b2), evaluate=True)
     assert expr.arguments == (a1, b1, g_A(b2, b2))
+
+    # identity removal with flattening
+    expr = g_A(g_A(b3, b2), g_A(b1, b2)).doit()
+    assert expr.arguments == (b3, b2, b1, b2)
+    expr = g_A(g_A(b2, b3), g_A(b1, b2)).doit()
+    assert expr.arguments == (b2, b1, b2)
+    expr = g_A(g_A(b3, b1), g_A(b3, b2)).doit()
+    assert expr.arguments == (b3, b1, b2)
+    expr = g_A(g_A(b2, b1), g_A(b2, g_A(b2, b3))).doit()
+    assert expr.arguments == (b2, b1, b2, b2)
+
+    expr = g2(g2(a3, a1), g2(a3, a2)).doit()
+    assert expr.arguments == (a1, a2)
