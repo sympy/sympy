bad574e128f5803b5d1a9fbce4507043450c2fe4
Martin Roelfs <u0114255@kuleuven.be>
2020-07-24 17:48:10 +0200

    Dont import Dual to see if checks pass

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -229,7 +229,7 @@ from .calculus import (euler_equations, singularities, is_increasing,
         differentiate_finite, periodicity, not_empty_in, AccumBounds,
         is_convex, stationary_points, minimum, maximum)
 
-from .algebras import Quaternion, Dual
+from .algebras import Quaternion
 
 from .printing import (pager_print, pretty, pretty_print, pprint,
         pprint_use_unicode, pprint_try_use_unicode, latex, print_latex,
@@ -472,7 +472,7 @@ __all__ = [
     'AccumBounds', 'is_convex', 'stationary_points', 'minimum', 'maximum',
 
     # sympy.algebras
-    'Quaternion', 'Dual',
+    'Quaternion',
 
     # sympy.printing
     'pager_print', 'pretty', 'pretty_print', 'pprint', 'pprint_use_unicode',
