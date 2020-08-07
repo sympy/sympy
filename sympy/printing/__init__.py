01563a4d26ae825c909a4668bd5fdba9685ed618
Aaron Meurer <asmeurer@gmail.com>
2020-08-07 14:20:22 -0600

    Organize some code lines a little better

--- sympy/printing/__init__.py
+++ sympy/printing/__init__.py
@@ -10,15 +10,15 @@ from .python import python, print_python
 
 from .pycode import pycode
 
+from .codeprinter import print_ccode, print_fcode
+
 # We have to import these now because otherwise if there are imported later,
-# they will override sympy.printing.ccode and sympy.printing.fcode with the
+# they will override sympy.printing.ccode and so on with the
 # modules. It would be nice to not have to do this as we could avoid importing
 # these files at top-level import time completely otherwise.
 from . import fcode
 from . import ccode
 from . import cxxcode
-
-from .codeprinter import print_ccode, print_fcode
 from .codeprinter import ccode, fcode, cxxcode # noqa:F811
 
 from .glsl import glsl_code, print_glsl
