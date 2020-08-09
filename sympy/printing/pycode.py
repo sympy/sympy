b4708504b0758ec6080bb9bcfe226f22cd2d24ed
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-08-09 19:08:43 +0530

    Fixed issue number #17411

--- sympy/printing/pycode.py
+++ sympy/printing/pycode.py
@@ -502,6 +502,7 @@ _known_functions_mpmath = dict(_in_mpmath, **{
     'fresnelc': 'fresnelc',
     'fresnels': 'fresnels',
     'sign': 'sign',
+    'loggamma': 'loggamma',
 })
 _known_constants_mpmath = {
     'Exp1': 'e',
