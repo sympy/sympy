faad68410f5d99115c1af5a8b553fc3ab8264f43
Namannimmo <namangera15@gmail.com>
2020-08-14 02:51:45 +0530

    Add more tests for construction of Parallel

--- sympy/physics/control/tests/test_lti.py
+++ sympy/physics/control/tests/test_lti.py
@@ -429,24 +429,30 @@ def test_Parallel_construction():
     tf2 = TransferFunction(a2*p - s, a2*s + p, s)
     tf3 = TransferFunction(a0*p + p**a1 - s, p, p)
     tf4 = TransferFunction(1, s**2 + 2*zeta*wn*s + wn**2, s)
+    tf5 = TransferFunction(p, a0 - p, p)
     inp = Function('X_d')(s)
     out = Function('X')(s)
 
+    # SISO transfer function in the arguments.
     p0 = Parallel(tf, tf2)
     assert p0.args == (tf, tf2)
     assert p0.var == s
+    assert p0.is_SISO
 
     p1 = Parallel(Series(tf, -tf2), tf2)
     assert p1.args == (Series(tf, -tf2), tf2)
     assert p1.var == s
+    assert p1.is_SISO
 
     tf3_ = TransferFunction(inp, 1, s)
     tf4_ = TransferFunction(-out, 1, s)
     p2 = Parallel(tf, Series(tf3_, -tf4_), tf2)
     assert p2.args == (tf, Series(tf3_, -tf4_), tf2)
+    assert p2.is_SISO
 
     p3 = Parallel(tf, tf2, tf4)
     assert p3.args == (tf, tf2, tf4)
+    assert p3.is_SISO
 
     p4 = Parallel(tf3_, tf4_)
     assert p4.args == (tf3_, tf4_)
@@ -461,6 +467,7 @@ def test_Parallel_construction():
 
     p7 = Parallel(tf2, tf4, Series(tf2, -tf), tf4)
     assert p7.args == (tf2, tf4, Series(tf2, -tf), tf4)
+    assert p7.is_SISO
 
     raises(ValueError, lambda: Parallel(tf, tf3))
     raises(ValueError, lambda: Parallel(tf, tf2, tf3, tf4))
@@ -468,6 +475,57 @@ def test_Parallel_construction():
     raises(TypeError, lambda: Parallel(2, tf, tf4))
     raises(TypeError, lambda: Parallel(s**2 + p*s, tf3, tf2))
     raises(TypeError, lambda: Parallel(tf3, Matrix([1, 2, 3, 4])))
+
+    # Transfer function matrix in the arguments.
+    tfm1 = TransferFunctionMatrix([tf, tf2, tf4], (3, 1), s)
+    tfm2 = TransferFunctionMatrix([-tf2, tf4, tf], (3, 1), s)
+    tfm3 = TransferFunctionMatrix([TF1])
+    tfm4 = TransferFunctionMatrix(Matrix([tf3_, tf4_, TF1]), (3, 1), s)
+    tfm5 = TransferFunctionMatrix([[tf, tf4], [tf2, tf]])
+    tfm6 = TransferFunctionMatrix([[TF1, TF2], [tf, tf2]])
+    tfm7 = TransferFunctionMatrix([tf3, tf5, -tf5], (3, 1), p)
+
+    p8 = Parallel(tfm1, tfm2)
+    assert p8.args == (tfm1, tfm2)
+    assert p8.var == s
+    assert not p8.is_SISO # .is_SISO gives either True or False, not None.
+    assert p8.shape == (p8.num_outputs, p8.num_inputs) == (3, 1)
+
+    p9 = Parallel(Series(tfm1, tfm3), tfm2)
+    assert p9.args == (Series(tfm1, tfm3), tfm2)
+    assert p9.var == s
+    assert not p9.is_SISO
+    assert p9.shape == (p9.num_outputs, p9.num_inputs) == (3, 1)
+
+    p10 = Parallel(tfm1, Series(tfm4, tfm3), tfm2)
+    assert p10.args == (tfm1, Series(tfm4, tfm3), tfm2)
+    assert p10.var == s
+    assert not p10.is_SISO
+    assert p10.shape == (p10.num_outputs, p10.num_inputs) == (3, 1)
+
+    p11 = Parallel(tfm2, tfm1, tfm4)
+    assert p11.args == (tfm2, tfm1, tfm4)
+    assert not p11.is_SISO
+    assert p11.shape == (p11.num_outputs, p11.num_inputs) == (3, 1)
+
+    p12 = Parallel(tfm6, tfm5)
+    assert p12.args == (tfm6, tfm5)
+    assert not p12.is_SISO
+    assert p12.shape == (p12.num_outputs, p12.num_inputs) == (2, 2)
+
+    p13 = Parallel(tfm2, tfm4, Series(tfm4, -tfm3), -tfm4)
+    assert p13.args == (tfm2, tfm4, Series(tfm4, -tfm3), -tfm4)
+    assert p13.shape == (p13.num_outputs, p13.num_inputs) == (3, 1)
+
+    # all TFMs must have same shapes.
+    raises(ShapeError, lambda: Parallel(tfm1, tfm3, tfm4))
+
+    # all TFMs must be using the same complex variable.
+    raises(ValueError, lambda: Parallel(tfm2, -tfm1, tfm7))
+
+    # Number or expression not allowed in the arguments.
+    raises(TypeError, lambda: Parallel(2, tf, tf4))
+    raises(TypeError, lambda: Parallel(s**2 + p*s, tf3, tf2))
 
 
 def test_Parallel_functions():
