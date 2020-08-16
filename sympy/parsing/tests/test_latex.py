475db3861b51c29da3fc4f5305dd67e23a62c8cf
Ben <ben.is.located@gmail.com>
2020-08-16 13:49:47 -0400

    Update sympy/parsing/tests/test_latex.py
    Co-authored-by: Moses Paul R <iammosespaulr@gmail.com>

--- sympy/parsing/tests/test_latex.py
+++ sympy/parsing/tests/test_latex.py
@@ -144,7 +144,7 @@ GOOD_PAIRS = [
      Integral(3*_Pow(theta, -1), theta)),
     ("\\int \\frac{1}{x} + 1 dx", Integral(_Add(_Pow(x, -1), 1), x)),
     ("x_0", Symbol('x_{0}')),
-    ("\langle x |", Bra('x')),
+    ("\\langle x |", Bra('x')),
     ("| x \rangle", Ket('x')),
     ("x_{1}", Symbol('x_{1}')),
     ("x_a", Symbol('x_{a}')),
