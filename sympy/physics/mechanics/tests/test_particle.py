cb99aba94a2f124b34d41e18f3ee782992d8db51
sidhu1012 <45732670+sidhu1012@users.noreply.github.com>
2020-08-30 17:30:27 +0530

    Update sympy/physics/mechanics/tests/test_particle.py
    assert
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- sympy/physics/mechanics/tests/test_particle.py
+++ sympy/physics/mechanics/tests/test_particle.py
@@ -42,7 +42,7 @@ def test_particle():
     assert p.kinetic_energy(
         N) in [m2*(v1**2 + v2**2 + v3**2)/2,
         m2 * v1**2 / 2 + m2 * v2**2 / 2 + m2 * v3**2 / 2]
-    assert(p.total_mechanical_energy(N)) == g*h*m + m2*v1**2/2 + m2*v2**2/2 + m2*v3**2/2
+    assert p.total_mechanical_energy(N) == g*h*m + m2*v1**2/2 + m2*v2**2/2 + m2*v3**2/2
 
 
 def test_parallel_axis():
