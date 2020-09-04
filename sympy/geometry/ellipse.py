2174ce43eb9ac7f43e9217273ca80e7d86cc3ff5
Shubham Agrawal <shubham.ag6845@gmail.com>
2020-09-04 12:38:59 +0530

    fixed error

--- sympy/geometry/ellipse.py
+++ sympy/geometry/ellipse.py
@@ -140,7 +140,7 @@ class Ellipse(GeometrySet):
                 "eccentricity" must not be None.'''))
 
         if eccentricity is not None:
-            if eccentricity > 1:
+            if eccentricity.is_real == True and eccentricity > 1:
                 raise GeometryError("eccentricity of ellipse/circle can only be less than 1")
             elif hradius is None:
                 hradius = vradius / sqrt(1 - eccentricity**2)
