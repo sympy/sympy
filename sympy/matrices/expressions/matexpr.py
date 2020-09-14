0625ef351ab05a33e662d7293798f2a9a1245cb9
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-09-15 01:37:13 +0900

    Fix flake8 error

--- sympy/matrices/expressions/matexpr.py
+++ sympy/matrices/expressions/matexpr.py
@@ -5,8 +5,6 @@ from functools import wraps, reduce
 import collections
 
 from sympy.core import S, Symbol, Tuple, Integer, Basic, Expr, Mul, Add
-from sympy.core.add import add
-from sympy.core.mul import mul
 from sympy.core.decorators import call_highest_priority
 from sympy.core.compatibility import SYMPY_INTS, default_sort_key
 from sympy.core.sympify import SympifyError, _sympify
