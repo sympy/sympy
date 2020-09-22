aded0956d8228f02efb28abe6be71841055dce0e
S.Y. Lee <sylee957@gmail.com>
2020-09-23 06:18:37 +0900

    Fix flake8

--- sympy/matrices/expressions/matexpr.py
+++ sympy/matrices/expressions/matexpr.py
@@ -3,7 +3,7 @@ from sympy.core.logic import FuzzyBool
 from functools import wraps, reduce
 import collections
 
-from sympy.core import S, Symbol, Tuple, Integer, Basic, Expr, Mul, Add
+from sympy.core import S, Symbol, Integer, Basic, Expr, Mul, Add
 from sympy.core.decorators import call_highest_priority
 from sympy.core.compatibility import SYMPY_INTS, default_sort_key
 from sympy.core.sympify import SympifyError, _sympify
