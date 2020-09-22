d4119a916ad4db703221add01454f5c08b286ba2
sidhu1012 <sudeepmanilsidhu@gmail.com>
2020-09-22 18:00:14 +0530

    warning for cyclic paths and other possible paths along with test cases

--- sympy/physics/vector/tests/test_point.py
+++ sympy/physics/vector/tests/test_point.py
@@ -1,6 +1,6 @@
 from sympy.physics.vector import dynamicsymbols, Point, ReferenceFrame
-from sympy.testing.pytest import raises
-
+from sympy.testing.pytest import raises, ignore_warnings
+import warnings
 
 def test_point_v1pt_theorys():
     q, q2 = dynamicsymbols('q q2')
@@ -216,7 +216,10 @@ def test_auto_point_vel_shortest_path():
     O1 = Point('O1')
     O1.set_pos(O, q2 * B.z)
     P4.set_pos(O1, q1 * B.x + q2 * B.z)
-    assert P4.vel(B) == q1.diff(t) * B.x + u2 * B.y + 2 * q2.diff(t) * B.z
+    with warnings.catch_warnings():
+        warnings.simplefilter('error')
+        with ignore_warnings(UserWarning):
+            assert P4.vel(B) == q1.diff(t) * B.x + u2 * B.y + 2 * q2.diff(t) * B.z
 
 def test_auto_point_vel_connected_frames():
     t = dynamicsymbols._t
@@ -230,3 +233,35 @@ def test_auto_point_vel_connected_frames():
     raises(ValueError, lambda: P.vel(N))
     N.orient(B, 'Axis', (q, B.x))
     assert P.vel(N) == (u + q1.diff(t)) * N.x + q2.diff(t) * B.y - q2 * q.diff(t) * B.z
+
+def test_auto_point_vel_multiple_short_paths():
+    q, u = dynamicsymbols('q u')
+    N = ReferenceFrame('N')
+    O = Point('O')
+    P = Point('P')
+    Q = Point('Q')
+    R = Point('R')
+    P.set_vel(N, u * N.x)
+    Q.set_vel(N, u *N.y)
+    R.set_vel(N, u * N.z)
+    O.set_pos(P, q * N.z)
+    O.set_pos(Q, q * N.y)
+    O.set_pos(R, q * N.x)
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        raises(UserWarning ,lambda: O.vel(N))
+
+def test_auto_vel_cyclic_warning():
+    P = Point('P')
+    P1 = Point('P1')
+    P2 = Point('P2')
+    P3 = Point('P3')
+    N = ReferenceFrame('N')
+    P.set_vel(N, N.x)
+    P1.set_pos(P, N.x)
+    P2.set_pos(P1, N.y)
+    P3.set_pos(P2, N.z)
+    P1.set_pos(P3, N.x + N.y)
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        raises(UserWarning ,lambda: P2.vel(N))
