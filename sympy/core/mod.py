6a9b1cda9ad3f2a5ce0f6b265814302919130fd9
soumi7 <soumibardhan10@gmail.com>
2020-09-29 15:42:38 +0530

    Added required section in mod.py and mul.py.

--- sympy/core/mod.py
+++ sympy/core/mod.py
@@ -5,7 +5,14 @@ from .function import Function
 class Mod(Function):
     """Represents a modulo operation on symbolic expressions.
 
-    Receives two arguments, dividend p and divisor q.
+    Parameters
+    ==========
+
+    p : Expr
+        Dividend.
+
+    q : Expr
+        Divisor.
 
     The convention used is the same as Python's: the remainder always has the
     same sign as the divisor.
