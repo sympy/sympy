8380f0e95367ac110199d41153b7d7b10139953f
soumi7 <soumibardhan10@gmail.com>
2020-10-07 16:06:51 +0530

    fixed errors.

--- sympy/combinatorics/tensor_can.py
+++ sympy/combinatorics/tensor_can.py
@@ -31,16 +31,16 @@ def dummy_sgs(dummies, sym, n):
     Parameters
     ==========
 
-    dummies : list of dummy indices
-        `dummies[2k], dummies[2k+1]` are paired indices
+    dummies : List of dummy indices.
+        `dummies[2k], dummies[2k+1]` are paired indices.
+        In base form, the dummy indices are always in
+        consecutive positions.
     sym : symmetry under interchange of contracted dummies::
         * None  no symmetry
         * 0     commuting
         * 1     anticommuting
 
     n : number of indices
-
-    In base form the dummy indices are always in consecutive positions.
 
     Examples
     ========
@@ -73,9 +73,9 @@ def dummy_sgs(dummies, sym, n):
 
 def _min_dummies(dummies, sym, indices):
     """
-    Return list of minima of the orbits of indices in group of dummies
-    see `double_coset_can_rep` for the description of ``dummies`` and ``sym``
-    ``indices`` is the initial list of dummy indices
+    Return list of minima of the orbits of indices in group of dummies.
+    See ``double_coset_can_rep`` for the description of ``dummies`` and ``sym``.
+    ``indices`` is the initial list of dummy indices.
 
     Examples
     ========
