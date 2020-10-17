ac7388f5c426edd64f2c340968df3c690fd46947
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2020-10-17 10:38:19 +0200

    Added determinant (and eigenvalue) computation to DFT and IDFT

--- sympy/matrices/expressions/fourier.py
+++ sympy/matrices/expressions/fourier.py
@@ -1,6 +1,8 @@
 from sympy.core.sympify import _sympify
 from sympy.matrices.expressions import MatrixExpr
+from sympy.matrices.expressions.determinant import Determinant
 from sympy import S, I, sqrt, exp
+
 
 class DFT(MatrixExpr):
     """ Discrete Fourier Transform """
@@ -21,6 +23,32 @@ class DFT(MatrixExpr):
     def _eval_inverse(self):
         return IDFT(self.n)
 
+    def _eval_determinant(self):
+        eigs = self._eval_eigenvals()
+        if eigs:
+            tmp = 1
+            for e, cnt in eigs.items():
+                tmp *= e**cnt
+            return tmp
+        return Determinant(self)
+
+    def _eval_eigenvals(self):
+        # Based on Dickinson and Steiglitz, "Eigenvectors and Functions of the
+        # Discrete Fourier Transform", 1982
+        if self.n.is_Integer:
+            m = self.n // 4
+            r = self.n % 4
+            if r == 0:
+                ret = {1: m+1, -1: m, I: m-1, -I: m}
+            elif r == 1:
+                ret = {1: m+1, -1: m, I: m, -I: m}
+            elif r == 2:
+                ret = {1: m+1, -1: m+1, I: m, -I: m}
+            elif r == 3:
+                ret = {1: m+1, -1: m+1, I: m, -I: m+1}
+            return {eig: ret[eig] for eig in ret if ret[eig] > 0}
+
+
 class IDFT(DFT):
     """ Inverse Discrete Fourier Transform """
     def _entry(self, i, j, **kwargs):
@@ -29,3 +57,19 @@ class IDFT(DFT):
 
     def _eval_inverse(self):
         return DFT(self.n)
+
+    def _eval_eigenvals(self):
+        # Based on Dickinson and Steiglitz, "Eigenvectors and Functions of the
+        # Discrete Fourier Transform", 1982
+        if self.n.is_Integer:
+            m = self.n // 4
+            r = self.n % 4
+            if r == 0:
+                ret = {S.One: m+1, -1: m, I: m, -I: m-1}
+            elif r == 1:
+                ret = {S.One: m+1, -1: m, I: m, -I: m}
+            elif r == 2:
+                ret = {S.One: m+1, -1: m+1, I: m, -I: m}
+            elif r == 3:
+                ret = {S.One: m+1, -1: m+1, I: m+1, -I: m}
+            return {eig: ret[eig] for eig in ret if ret[eig] > 0}
--- sympy/matrices/expressions/tests/test_fourier.py
+++ sympy/matrices/expressions/tests/test_fourier.py
@@ -1,7 +1,7 @@
 from sympy import S, I, ask, Q, Abs, simplify, exp, sqrt
 from sympy.core.symbol import symbols
 from sympy.matrices.expressions.fourier import DFT, IDFT
-from sympy.matrices import det, Matrix, Identity
+from sympy.matrices import det, Matrix, Identity, Determinant
 from sympy.testing.pytest import raises
 
 
@@ -27,3 +27,37 @@ def test_dft():
     assert Abs(simplify(det(Matrix(DFT(4))))) == 1
     assert DFT(n)*IDFT(n) == Identity(n)
     assert DFT(n)[i, j] == exp(-2*S.Pi*I/n)**(i*j) / sqrt(n)
+
+
+def test_dft_determinant():
+    assert det(DFT(4)) == I
+    assert det(DFT(5)) == -1
+    assert det(DFT(6)) == 1
+    assert det(DFT(7)) == -I
+    assert det(DFT(4)) == DFT(4).as_explicit().det()
+    assert det(DFT(27)) == I
+
+    assert det(IDFT(4)) == -I
+    assert det(IDFT(4)) == IDFT(4).as_explicit().det()
+
+    n = symbols('n')
+    assert det(DFT(n)) == Determinant(DFT(n))
+
+
+def test_dft_eval_eigenvals():
+    assert DFT(3)._eval_eigenvals() == {-1: 1, 1: 1, -I: 1}
+    assert DFT(44)._eval_eigenvals() == {-1: 11, 1: 12, -I: 11, I: 10}
+    assert DFT(37)._eval_eigenvals() == {-1: 9, 1: 10, -I: 9, I: 9}
+    assert DFT(26)._eval_eigenvals() == {-1: 7, 1: 7, -I: 6, I: 6}
+
+    DFT(4)._eval_eigenvals() == DFT(4).as_explicit().eigenvals()
+
+    assert IDFT(3)._eval_eigenvals() == {-1: 1, 1: 1, I: 1}
+    assert IDFT(44)._eval_eigenvals() == {-1: 11, 1: 12, -I: 10, I: 11}
+    assert IDFT(37)._eval_eigenvals() == {-1: 9, 1: 10, -I: 9, I: 9}
+    assert IDFT(26)._eval_eigenvals() == {-1: 7, 1: 7, -I: 6, I: 6}
+
+    IDFT(4)._eval_eigenvals() == IDFT(4).as_explicit().eigenvals()
+
+    n = symbols('n')
+    assert DFT(n)._eval_eigenvals() is None
