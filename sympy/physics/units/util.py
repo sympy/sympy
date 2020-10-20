ab6be6f86f0b3544d5e78bd78be59080ac833988
Stan Schymanski <stan.schymanski@env.ethz.ch>
2020-10-20 17:09:25 +0200

    Fix test and update check_dimensions()

--- sympy/physics/units/util.py
+++ sympy/physics/units/util.py
@@ -158,8 +158,19 @@ def check_dimensions(expr, unit_system="SI"):
     # Also, when doing substitutions, multiplicative constants
     # might be introduced, so remove those now
 
+    import functools
+    import operator
     from sympy.physics.units import UnitSystem
     unit_system = UnitSystem.get_unit_system(unit_system)
+    
+    def addDict(dict1, dict2):
+        """Merge dictionaries by adding values of common keys and
+        removing keys with value of 0."""
+        dict3 = {**dict1, **dict2}
+        for key, value in dict3.items():
+            if key in dict1 and key in dict2:
+                   dict3[key] = value + dict1[key]
+        return {key:val for key, val in dict3.items() if val != 0}
 
     adds = expr.atoms(Add)
     DIM_OF = unit_system.get_dimension_system().get_dimensional_dependencies
@@ -171,19 +182,21 @@ def check_dimensions(expr, unit_system="SI"):
                 continue
             dims = []
             skip = False
+            dimdict = {}
             for i in Mul.make_args(ai):
                 if i.has(Quantity):
                     i = Dimension(unit_system.get_dimensional_expr(i))
                 if i.has(Dimension):
-                    dims.extend(DIM_OF(i).items())
+                    dimdict = addDict(dimdict, DIM_OF(i))
                 elif i.free_symbols:
                     skip = True
                     break
+            dims.extend(dimdict.items())
             if not skip:
                 deset.add(tuple(sorted(dims)))
                 if len(deset) > 1:
                     raise ValueError(
-                        "addends have incompatible dimensions")
+                        "addends have incompatible dimensions: {}".format(deset))
 
     # clear multiplicative constants on Dimensions which may be
     # left after substitution
