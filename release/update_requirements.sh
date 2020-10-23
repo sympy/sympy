abe887b6e439e67602498478e79ea21822992d11
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-10-23 13:47:38 +0100

    maint(release): add scripts to setup venv
    Adds a requirements.txt and a script to update it to be used for setting
    up reproducible builds when running the release script.

---
+++ release/update_requirements.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Run as
+#
+#   $ ./update_requirements.sh python
+#
+# where python is the name of the (on PATH) interpreter to use. This will
+# create a virtual environment with venv, rerun the pip install command to get
+# latest versions meeting the constraints and then use pip list to recreate
+# requirements.txt with updated versions.
+#
+# This will overwrite the requirements.txt file and show the diff
+
+if [ -z $1 ];
+then
+  python=python
+else
+  python=$1
+fi
+
+tmp_dir=$(mktemp -d -t venv-XXXX)
+venv_dir=$tmp_dir/venv
+
+$python -m venv $venv_dir
+. $venv_dir/bin/activate
+
+pip install -U pip
+pip install\
+  mpmath\
+  'matplotlib>=2.2'\
+  'numpy==1.18.5'\
+  scipy\
+  theano\
+  ipython\
+  gmpy2\
+  symengine\
+  cython\
+  tensorflow\
+  llvmlite\
+  wurlitzer\
+  autowrap\
+  numexpr\
+  'antlr4-python3-runtime==4.7.*'\
+  #
+
+pip list > requirements.txt
+git diff requirements.txt
