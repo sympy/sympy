da39a8f77a980fb1037c0a5375a33dc469aba5d3
soumi7 <soumibardhan10@gmail.com>
2020-10-28 11:36:40 +0530

    Added required sections in rigidbody.py.

--- sympy/physics/mechanics/rigidbody.py
+++ sympy/physics/mechanics/rigidbody.py
@@ -12,6 +12,9 @@ __all__ = ['RigidBody']
 class RigidBody(object):
     """An idealized rigid body.
 
+    Explanation
+    ===========
+
     This is essentially a container which holds the various components which
     describe a rigid body: a name, mass, center of mass, reference frame, and
     inertia.
@@ -21,6 +24,7 @@ class RigidBody(object):
 
     Attributes
     ==========
+
     name : string
         The body's name.
     masscenter : Point
@@ -123,6 +127,9 @@ class RigidBody(object):
     def linear_momentum(self, frame):
         """ Linear momentum of the rigid body.
 
+        Explanation
+        ===========
+
         The linear momentum L, of a rigid body B, with respect to frame N is
         given by
 
@@ -162,6 +169,9 @@ class RigidBody(object):
         """Returns the angular momentum of the rigid body about a point in the
         given frame.
 
+        Explanation
+        ===========
+
         The angular momentum H of a rigid body B about some point O in a frame
         N is given by:
 
@@ -174,6 +184,7 @@ class RigidBody(object):
 
         Parameters
         ==========
+
         point : Point
             The point about which angular momentum is desired.
         frame : ReferenceFrame
@@ -207,7 +218,10 @@ class RigidBody(object):
         return I.dot(w) + r.cross(m * v)
 
     def kinetic_energy(self, frame):
-        """Kinetic energy of the rigid body
+        """Kinetic energy of the rigid body.
+
+        Explanation
+        ===========
 
         The kinetic energy, T, of a rigid body, B, is given by
 
@@ -321,11 +335,13 @@ class RigidBody(object):
 
         Parameters
         ==========
+
         point : sympy.physics.vector.Point
             The point to express the inertia dyadic about.
 
         Returns
         =======
+
         inertia : sympy.physics.vector.Dyadic
             The inertia dyadic of the rigid body expressed about the provided
             point.
