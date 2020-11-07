2c696e6b0a58ba00c297b36621e17fc4fe9d7318
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-11-07 21:05:52 +0000

    maint(release): check authors in release script

--- release/releasecheck.py
+++ release/releasecheck.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
+from subprocess import check_call
 
+def main(version, outdir):
+    check_call(['bin/mailmap_update.py'])
+    check_call(['bin/authors_update.py'])
+    check_call(['release/build_docs.py', version, outdir])
+
+if __name__ == "__main__":
+    import sys
+    main(*sys.argv[1:])
