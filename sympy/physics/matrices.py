737226e1440e564ecaae6cf38d601758213f0ab2
soumi7 <soumibardhan10@gmail.com>
2020-11-12 12:41:56 +0530

    Added required sections in matrices.py.

--- sympy/physics/matrices.py
+++ sympy/physics/matrices.py
@@ -7,7 +7,7 @@ from sympy.functions import exp
 
 
 def msigma(i):
-    r"""Returns a Pauli matrix `\sigma_i` with `i=1,2,3`
+    r"""Returns a Pauli matrix `\sigma_i` with ``i=1,2,3``.
 
     References
     ==========
@@ -76,6 +76,9 @@ def pat_matrix(m, dx, dy, dz):
 def mgamma(mu, lower=False):
     r"""Returns a Dirac gamma matrix `\gamma^\mu` in the standard
     (Dirac) representation.
+
+    Explanation
+    ===========
 
     If you want `\gamma_\mu`, use ``gamma(mu, True)``.
 
