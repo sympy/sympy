e4ebd44b5045a68496a18e3f80654475f666d101
Francesco Bonazzi <franz.bonazzi@gmail.com>
2020-11-22 22:44:55 +0100

    more documentation for tensordiagonal

--- sympy/tensor/array/arrayop.py
+++ sympy/tensor/array/arrayop.py
@@ -158,7 +158,7 @@ def tensorcontraction(array, *contraction_axes):
     #    Uncontracted indices are determined by the combinatorial product of
     #    the absolute positions of the remaining indices.
     # 2. internal loop on all contracted indices.
-    #    It sum the values of the absolute contracted index and the absolute
+    #    It sums the values of the absolute contracted index and the absolute
     #    uncontracted index for the external loop.
     contracted_array = []
     for icontrib in itertools.product(*remaining_indices):
@@ -189,31 +189,41 @@ def tensordiagonal(array, *diagonal_axes):
     Examples
     ========
 
+    ``tensordiagonal`` acting on a 2-dimensional array by axes 0 and 1 is
+    equivalent to the diagonal of the matrix:
+
     >>> from sympy import Array, tensordiagonal
     >>> from sympy import Matrix, eye
     >>> tensordiagonal(eye(3), (0, 1))
     [1, 1, 1]
-    >>> A = Array(range(18), (3, 2, 3))
-    >>> A
-    [[[0, 1, 2], [3, 4, 5]], [[6, 7, 8], [9, 10, 11]], [[12, 13, 14], [15, 16, 17]]]
-    >>> tensordiagonal(A, (0, 2))
-    [[0, 7, 14], [3, 10, 17]]
 
     >>> from sympy.abc import a,b,c,d
     >>> m1 = Matrix([[a, b], [c, d]])
     >>> tensordiagonal(m1, [0, 1])
     [a, d]
 
+    In case of higher dimensional arrays, the diagonalized out dimensions
+    are appended removed and appended as a single dimension at the end:
+
+    >>> A = Array(range(18), (3, 2, 3))
+    >>> A
+    [[[0, 1, 2], [3, 4, 5]], [[6, 7, 8], [9, 10, 11]], [[12, 13, 14], [15, 16, 17]]]
+    >>> tensordiagonal(A, (0, 2))
+    [[0, 7, 14], [3, 10, 17]]
+    >>> from sympy import permutedims
+    >>> tensordiagonal(A, (0, 2)) == permutedims(Array([A[0, :, 0], A[1, :, 1], A[2, :, 2]]), [1, 0])
+    True
+
     """
     array, remaining_indices, remaining_shape, diagonal_deltas = _util_contraction_diagonal(array, *diagonal_axes)
 
-    # Compute the contracted array:
+    # Compute the diagonalized array:
     #
     # 1. external for loops on all undiagonalized indices.
     #    Undiagonalized indices are determined by the combinatorial product of
     #    the absolute positions of the remaining indices.
     # 2. internal loop on all diagonal indices.
-    #    It sum the values of the absolute contracted index and the absolute
+    #    It appends the values of the absolute diagonalized index and the absolute
     #    undiagonalized index for the external loop.
     diagonalized_array = []
     diagonal_shape = [len(i) for i in diagonal_deltas]
