af9f7c2bdf115c7b5e66ef96293a6e5c55a1485f
Eva <eva.tiwari@gmail.com>
2020-12-10 21:36:31 +0530

    Removed whitespace

--- sympy/stats/tests/test_rv.py
+++ sympy/stats/tests/test_rv.py
@@ -394,4 +394,4 @@ def test_issue_20286():
     k = Dummy('k', integer = True)
     eq = Sum(Piecewise((-p**k*(1 - p)**(-k + n)*log(p**k*(1 - p)**(-k + n)*binomial(n, k))*binomial(n, k), (k >= 0) & (k <= n)), (nan, True)), (k, 0, n))
     assert eq.dummy_eq(H(B))
-    
+
