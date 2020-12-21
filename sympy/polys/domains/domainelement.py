b003953107e0f3ebdf92cf9a5dc7dde82d487b1d
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-12-21 14:32:51 +0000

    doc(polys): explain the domain system
    Add docstring for DomainElement.parent()

--- sympy/polys/domains/domainelement.py
+++ sympy/polys/domains/domainelement.py
@@ -14,4 +14,24 @@ class DomainElement:
     """
 
     def parent(self):
+        """Get the domain associated with ``self``
+
+        Examples
+        ========
+
+        >>> from sympy import ZZ, symbols
+        >>> x, y = symbols('x, y')
+        >>> K = ZZ[x,y]
+        >>> p = K(x)**2 + K(y)**2
+        >>> p
+        x**2 + y**2
+        >>> p.parent()
+        ZZ[x,y]
+
+        Notes
+        =====
+
+        This is used by :py:meth:`~.Domain.convert` to identify the domain
+        associated with a domain element.
+        """
         raise NotImplementedError("abstract method")
