3211f2fbdf46ac5d4819e517fe58604c9b60680d
Mohitbalwani26 <mohitbalwani.ict17@gmail.com>
2020-12-22 19:38:41 +0530

    fix imports

--- sympy/solvers/ode/tests/test_ode.py
+++ sympy/solvers/ode/tests/test_ode.py
@@ -15,7 +15,7 @@ from sympy.solvers.ode.ode import (_linear_coeff_match,
 from sympy.functions import airyai, airybi
 
 from sympy.solvers.deutils import ode_order
-from sympy.testing.pytest import XFAIL, skip, raises, slow, ON_TRAVIS, SKIP
+from sympy.testing.pytest import XFAIL, skip, raises, slow, ON_TRAVIS
 
 
 C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10 = symbols('C0:11')
