b0fc29a9fb5bcc56982ac21c0aaeafb42bdf2f13
Rishabh Jain <jain.rishabhche@gmail.com>
2020-12-27 21:39:50 +0530

    core_test_diff Added one more assertion in the nth derivative test

--- sympy/core/tests/test_diff.py
+++ sympy/core/tests/test_diff.py
@@ -135,8 +135,11 @@ def test_diff_nth_derivative():
         Eq(y, 0) & Eq(Max(0, -y + n), 0)),
         (2*factorial(n)/(factorial(y)*factorial(-y + n)), Eq(y, 0) & Eq(Max(0,
         -y + n), 1)), (0, True)), (y, 0, n)))
-    # TODO: assert diff(x**2, (x, n)) == x**(2-n)*ff(2, n)
-    exprm = x*sin(x)
+
+    #Check the nth derivative of x**2
+    assert diff(x**2, (x, n)) == Derivative(x*x, (x, n))
+
+    exprm = x*sin(x)            
     mul_diff = diff(exprm, (x, n))
     assert isinstance(mul_diff, Sum)
     for i in range(5):
