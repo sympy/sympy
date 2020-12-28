735cd480f4e1115a4f5d7f2a2fda6560d2a5c9ef
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-12-28 18:34:30 +0000

    feat(polys): make FiniteExtension a proper domain
    Add test of FiniteExtension for calculations with sin and cos.

--- sympy/polys/agca/tests/test_extensions.py
+++ sympy/polys/agca/tests/test_extensions.py
@@ -1,8 +1,12 @@
+from sympy import symbols, sin, cos
 from sympy.polys import QQ, ZZ
 from sympy.polys.polytools import Poly
 from sympy.polys.polyerrors import NotInvertible
 from sympy.polys.agca.extensions import FiniteExtension
+from sympy.polys.domainmatrix import DomainMatrix
+
 from sympy.testing.pytest import raises
+
 from sympy.abc import x, y, t
 
 
@@ -164,3 +168,28 @@ def test_FiniteExtension_Poly():
     assert K2.to_sympy(eK ** 2) == 4 + 2*x*t
     p = Poly(x + t, y, domain=K2)
     assert p**2 == Poly(4 + 2*x*t, y, domain=K2)
+
+
+def test_FiniteExtension_sincos_jacobian():
+    # Use FiniteExtensino to compute the Jacobian of a matrix involving sin
+    # and cos of different symbols.
+    r, p, t = symbols('rho, phi, theta')
+    elements = [
+        [sin(p)*cos(t), r*cos(p)*cos(t), -r*sin(p)*sin(t)],
+        [sin(p)*sin(t), r*cos(p)*sin(t),  r*sin(p)*cos(t)],
+        [       cos(p),       -r*sin(p),                0],
+    ]
+
+    def make_extension(K):
+        K = FiniteExtension(Poly(sin(p)**2+cos(p)**2-1, sin(p), domain=K[cos(p)]))
+        K = FiniteExtension(Poly(sin(t)**2+cos(t)**2-1, sin(t), domain=K[cos(t)]))
+        return K
+
+    Ksc1 = make_extension(ZZ[r])
+    Ksc2 = make_extension(ZZ)[r]
+
+    for K in [Ksc1, Ksc2]:
+        elements_K = [[K.convert(e) for e in row] for row in elements]
+        J = DomainMatrix(elements_K, (3, 3), K)
+        det = J.charpoly()[-1] * (-K.one)**3
+        assert det == K.convert(r**2*sin(p))
