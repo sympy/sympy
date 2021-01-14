3cc89072296a23987d3f277286bd26a3d9a20415
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2021-01-14 20:01:32 +0000

    fix(polys): fix FiniteExtension.exquo for gmpy
    The FiniteEtension.exquo method was using self.domain (e.g. QQ) rather
    than self.ring (QQ[x]) in order to compute division with the rep. This
    somehow worked when gmpy was not installed but fails as should be
    expected when gmpy is installed. This commit ensures the correct domain
    is used.

--- sympy/polys/agca/extensions.py
+++ sympy/polys/agca/extensions.py
@@ -325,11 +325,10 @@ class MonogenicFiniteExtension(Domain):
         return self.set_domain(K)
 
     def quo(self, f, g):
-        rep = self.domain.exquo(f.rep, g.rep)
-        return ExtElem(rep % self.mod, self)
+        return self.exquo(f, g)
 
     def exquo(self, f, g):
-        rep = self.domain.exquo(f.rep, g.rep)
+        rep = self.ring.exquo(f.rep, g.rep)
         return ExtElem(rep % self.mod, self)
 
     def is_negative(self, a):
