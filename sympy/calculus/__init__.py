5d49ca2b9bae5d8baae0eac14ab24b699fb20af7
che2712 <57262185+che2712@users.noreply.github.com>
2021-01-17 23:53:21 +0530

    Update __init__.py
    adding an inflection point when the function is neither decreasing nor increasing

--- sympy/calculus/__init__.py
+++ sympy/calculus/__init__.py
@@ -3,7 +3,7 @@
 from .euler import euler_equations
 from .singularities import (singularities, is_increasing,
                             is_strictly_increasing, is_decreasing,
-                            is_strictly_decreasing, is_monotonic)
+                            is_strictly_decreasing, is_monotonic, has_inflection_point)
 from .finite_diff import finite_diff_weights, apply_finite_diff, as_finite_diff, differentiate_finite
 from .util import (periodicity, not_empty_in, AccumBounds, is_convex,
                    stationary_points, minimum, maximum)
@@ -13,7 +13,7 @@ __all__ = [
 
 'singularities', 'is_increasing',
 'is_strictly_increasing', 'is_decreasing',
-'is_strictly_decreasing', 'is_monotonic',
+'is_strictly_decreasing', 'is_monotonic', has_inflection_point 
 
 'finite_diff_weights', 'apply_finite_diff', 'as_finite_diff', 'differentiate_finite',
 
