d2a557d46b7700640c14b219197ab9564107fe50
MaqOwais <quadirowais200@gmail.com>
2021-01-19 07:46:09 +0530

    Update solveset.py

--- sympy/solvers/solveset.py
+++ sympy/solvers/solveset.py
@@ -3213,9 +3213,9 @@ def substitution(system, symbols, result=[{}], known_symbols=[],
     new_result_complex, solve_call2, cnd_call2 = _solve_using_known_values(
         old_result, solveset_complex)
 
-    # when `total_solveset_call` equals to `total_conditionset`
-    # implies `solveset` failed to solve all the equations
-    # returning conditionset in this case
+    # If total_solveset_call is equal to total_conditionset
+    # then solveset failed to solve all of the equations.
+    # In this case we return a ConditionSet here.
     total_conditionset += (cnd_call1 + cnd_call2)
     total_solveset_call += (solve_call1 + solve_call2)
 
