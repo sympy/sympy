1856068f0563c79b7e01f35ecb81f097cf3d0734
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-01-24 04:00:34 +0900

    refactor(assump): fix AppliedPredicate args and func
    Fix get_relevant_facts

--- sympy/assumptions/satask.py
+++ sympy/assumptions/satask.py
@@ -92,7 +92,7 @@ def get_relevant_facts(proposition, assumptions=None,
             req_keys |= tmp_keys
         keys |= {l for l in lkeys if find_symbols(l) & req_keys != set()}
 
-        exprs = {key.args[0] if isinstance(key, AppliedPredicate) else key for key in keys}
+        exprs = {key.arguments[0] if isinstance(key, AppliedPredicate) else key for key in keys}
         return exprs, relevant_facts
 
     for expr in exprs:
