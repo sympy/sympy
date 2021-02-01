10445f0c72d63a190b1c99c3febd19703362979a
Soumi Bardhan <51290447+Soumi7@users.noreply.github.com>
2021-02-01 10:56:20 +0530

    Update sympy/utilities/codegen.py
    Co-authored-by: S.Y. Lee <sylee957@gmail.com>

--- sympy/utilities/codegen.py
+++ sympy/utilities/codegen.py
@@ -916,10 +916,10 @@ class CCodeGen(CodeGen):
         If the routine has multiple result objects, an CodeGenError is
         raised.
 
-        See also
-        ========
+        References
+        ==========
 
-        https://en.wikipedia.org/wiki/Function_prototype
+        .. [1] https://en.wikipedia.org/wiki/Function_prototype
 
         """
         if len(routine.results) > 1:
