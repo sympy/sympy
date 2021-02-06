ef876cd9b0b25988210c00e4ed7c06fb4cca7d53
Sudeep Sidhu <sudeepmanilsidhu@gmail.com>
2021-02-06 13:52:15 +0530

    Fix space and explicit orientation docstring

--- sympy/physics/vector/frame.py
+++ sympy/physics/vector/frame.py
@@ -596,7 +596,7 @@ class ReferenceFrame:
 
         Setup variables for the examples:
 
-        >>> from sympy import symbols
+        >>> from sympy import symbols, Matrix
         >>> from sympy.physics.vector import ReferenceFrame
         >>> q1 = symbols('q1')
         >>> A = ReferenceFrame('A')
@@ -831,9 +831,9 @@ class ReferenceFrame:
         >>> B.orient_axis(B2, N.y, q3)
         >>> B.dcm(N).simplify()
         Matrix([
-        [ sin(q1)*sin(q2)*sin(q3) + cos(q1)*cos(q3), sin(q1)*cos(q2), sin(q1)*sin(q2)*cos(q3) - sin(q3)*cos(q1)],
-        [-sin(q1)*cos(q3) + sin(q2)*sin(q3)*cos(q1), cos(q1)*cos(q2), sin(q1)*sin(q3) + sin(q2)*cos(q1)*cos(q3)],
-        [                           sin(q3)*cos(q2),        -sin(q2),                           cos(q2)*cos(q3)]])
+        [ sin(q1)*sin(q2)*sin(q3) + cos(q1)*cos(q3), sin(q1)*cos(q2),                                                                                sin(q1)*sin(q2)*cos(q3) - sin(q3)*cos(q1)],
+        [-sin(q1)*cos(q3) + sin(q2)*sin(q3)*cos(q1), cos(q1)*cos(q2), sin(-q1 + q2 + q3)/4 - sin(q1 - q2 + q3)/4 + sin(q1 + q2 - q3)/4 + sin(q1 + q2 + q3)/4 + cos(q1 - q3)/2 - cos(q1 + q3)/2],
+        [                           sin(q3)*cos(q2),        -sin(q2),                                                                                                          cos(q2)*cos(q3)]])
 
         It is worth noting that space-fixed and body-fixed rotations are
         related by the order of the rotations, i.e. the reverse order of body
