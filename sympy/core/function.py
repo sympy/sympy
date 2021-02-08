08f0ca72c829c8914beb458af06d90ba745b3680
naveensaigit <naveensaisreenivas@gmail.com>
2021-02-09 00:23:18 +0530

    Minor change in evalf

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -551,6 +551,7 @@ class Function(Application, Expr):
         _eval_mpmath = getattr(self, '_eval_mpmath', None)
         if _eval_mpmath is None:
             func = _get_mpmath_func(self.func.__name__)
+            args = self.args
         else:
             func, args = _eval_mpmath()
 
@@ -570,10 +571,7 @@ class Function(Application, Expr):
         # XXX + 5 is a guess, it is similar to what is used in evalf.py. Should
         #     we be more intelligent about it?
         try:
-            if _eval_mpmath is None:
-                args = [arg._to_mpmath(prec + 5) for arg in self.args]
-            else:
-                args = [arg._to_mpmath(prec + 5) for arg in args]
+            args = [arg._to_mpmath(prec + 5) for arg in args]
             def bad(m):
                 from mpmath import mpf, mpc
                 # the precision of an mpf value is the last element
