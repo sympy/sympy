cf8a148b80048c0a53af05ae075424b2240339c5
Constantin Mateescu <costica1234@me.com>
2021-02-13 09:54:34 +0200

    Replaced [0-9] with \d to cover Unicode digits.

--- sympy/printing/conventions.py
+++ sympy/printing/conventions.py
@@ -7,7 +7,7 @@ import re
 from collections.abc import Iterable
 from sympy import Derivative
 
-_name_with_digits_p = re.compile(r'^([^\W\d_]+)([0-9]+)$', re.U)
+_name_with_digits_p = re.compile(r'^([^\W\d_]+)(\d+)$', re.U)
 
 
 def split_super_sub(text):
