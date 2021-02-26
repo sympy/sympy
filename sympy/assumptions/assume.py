9f0329f45e83521f2834ee7f9c49055263c9e8ef
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-02-26 17:16:16 +0900

    Improve code quality

--- sympy/assumptions/assume.py
+++ sympy/assumptions/assume.py
@@ -6,9 +6,7 @@ from sympy.core.assumptions import ManagedProperties
 from sympy.core.symbol import Str
 from sympy.core.sympify import _sympify
 from sympy.logic.boolalg import Boolean, false, true
-from sympy.multipledispatch.dispatcher import (
-    Dispatcher, MDNotImplementedError, str_signature
-)
+from sympy.multipledispatch.dispatcher import Dispatcher, str_signature
 from sympy.utilities.exceptions import SymPyDeprecationWarning
 from sympy.utilities.iterables import is_sequence
 from sympy.utilities.source import get_class
