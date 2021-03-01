d8a5793e94b7f1c404b2f8b2da08512da43ebb98
Aaron Meurer <asmeurer@gmail.com>
2021-03-01 16:56:10 -0700

    Don't try to parse methods as implicit multiplications in parse_expr
    For example x.y(z) should not be parsed as x.y*z.
    
    Attributes of an object are almost never expressions that would be multiplied.
    Methods are far more likely. Unlike for top-level names, we can't easily guess
    what methods an object will have since we don't know its type, so we just
    disable all implicit multiplications if the first name is a method.
    
    Fixes #21020.

--- sympy/parsing/sympy_parser.py
+++ sympy/parsing/sympy_parser.py
@@ -208,8 +208,16 @@ def _implicit_multiplication(tokens, local_dict, global_dict):
 
     """
     result = []
+    skip = False
     for tok, nextTok in zip(tokens, tokens[1:]):
         result.append(tok)
+        if skip:
+            skip = False
+            continue
+        if tok[0] == OP and tok[1] == '.' and nextTok[0] == NAME:
+            # Dotted name. Do not do implicit multiplication
+            skip = True
+            continue
         if (isinstance(tok, AppliedFunction) and
               isinstance(nextTok, AppliedFunction)):
             result.append((OP, '*'))
