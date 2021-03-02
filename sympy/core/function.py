2d6a3278c50101e6631718d9ae7954ab92f92d63
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-03-02 23:15:42 +0530

    With reference to issue #16350
    added return self.func(expr, *variable_count, **kwargs) in last line so that inheritance doesn't break.

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -1427,7 +1427,7 @@ class Derivative(Expr):
         if isinstance(expr, Derivative):
             variable_count = list(expr.variable_count) + variable_count
             expr = expr.expr
-            return _derivative_dispatch(expr, *variable_count, **kwargs)
+            return self.func(expr, *variable_count, **kwargs)
 
         # we return here if evaluate is False or if there is no
         # _eval_derivative method
