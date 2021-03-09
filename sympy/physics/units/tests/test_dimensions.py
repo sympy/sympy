fcf46068513235effc7de8b2efbafe90e61c50e2
mgreminger <michael.greminger@gmail.com>
2021-03-09 14:14:10 -0600

    Remove extra newline at end of file

--- sympy/physics/units/tests/test_dimensions.py
+++ sympy/physics/units/tests/test_dimensions.py
@@ -129,4 +129,3 @@ def test_Dimension_functions():
 
     assert dimsys_SI.get_dimensional_dependencies(Abs(length)) == {"length": 1}
     assert dimsys_SI.get_dimensional_dependencies(Abs(length/length)) == {}
-
