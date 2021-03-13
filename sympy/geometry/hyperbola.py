d60f5ebdb7e9f596f61cc55df354ff431a2a5359
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-03-13 22:30:37 +0530

    Magic method new Defined .

--- sympy/geometry/hyperbola.py
+++ sympy/geometry/hyperbola.py
@@ -3,7 +3,8 @@ Hyperbolic Geometrical Entity.
 Contains
 * Hyperbola
 """
-from sympy.geometry.entity import GeometrySet
+from sympy import Point, Line
+from sympy.geometry.entity import GeometrySet,GeometryEntity
 class Hyperbola(GeometrySet):
     """
     A Hyperbolic GeometryEntity:
@@ -13,7 +14,30 @@ class Hyperbola(GeometrySet):
 
     Parameters
     ==========
+        focus=Point
+        directrix=Line
         eccentricity: Eccentricity of Hyperbola is always one(>1).
 
+    Attributes
+    ==========
+    focus
+    directrix
+    focal length
+    eccentricity
+    axis of symmetry(Tranverse and conjugate axis)
+    Centre
+    focal chord
     """
+
+    def __new__(cls, focus=None,directrix=None, **kwargs):
+        if focus:
+            focus=Point(focus,dim=2)
+        directrix=Line(directrix)
+        if directrix.contains(focus):
+            raise ValueError("The focus must not be on the directrix")
+        return GeometryEntity.__new__(cls,focus,directrix,**kwargs)
+
+
+
+
 
