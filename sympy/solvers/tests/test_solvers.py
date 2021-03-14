1e5fb820efbc7261b1c8ea3b102bd67d2de95b39
Ayush <bisht.ayush2001@gmail.com>
2021-03-14 18:53:38 +0530

    add test5

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -2326,7 +2326,5 @@ def test_issue_4886():
     c2 = ((-a*sqrt(ra + rb - c ** 2)) / m + (b * c) / m)
     c3 = b*(((a*sqrt(ra + rb - c ** 2))) / m + (b * c) / m) - c
     c4 = ((a*sqrt(ra + rb - c ** 2)) / m + (b * c) / m)
-    assert solve([x**2 + y**2 - R**2, a*x + b*y - c], x, y) == \
-        [(-(c1 / a), c2), (-(c3 / a), c4)]
+    assert solve([x**2 + y**2 - R**2, a*x + b*y - c], x, y) == [(-(c1 / a), c2), (-(c3 / a), c4)]
     
-        
