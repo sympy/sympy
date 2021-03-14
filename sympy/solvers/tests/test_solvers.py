23906a69896b299a0d86738d2e79df307b935e95
Ayush <bisht.ayush2001@gmail.com>
2021-03-14 13:06:04 +0530

    checks

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -5,7 +5,7 @@ from sympy import (
     erfcinv, exp, im, log, pi, re, sec, sin,
     sinh, solve, solve_linear, sqrt, sstr, symbols, sympify, tan, tanh,
     root, atan2, arg, Mul, SparseMatrix, ask, Tuple, nsolve, oo,
-    E, cbrt, denom, Add, Piecewise, GoldenRatio, TribonacciConstant, pow, var)
+    E, cbrt, denom, Add, Piecewise, GoldenRatio, TribonacciConstant, pow)
 
 from sympy.core.function import nfloat
 from sympy.solvers import solve_linear_system, solve_linear_system_LU, \
