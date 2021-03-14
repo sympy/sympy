70acdedb365c3edfa106ba7cd89da2fb3cd2a389
Ayush <bisht.ayush2001@gmail.com>
2021-03-14 18:26:15 +0530

    add test4

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -2321,9 +2321,11 @@ def test_issue_4886():
     ra = R**2*a**2
     rb = R**2*b**2
     m = a**2 + b**2
-    c1, c2, c3, c4 = symbols('c1 c2 c3 c4', positive=True)    
+    c1, c2, c3, c4 = symbols('c1 c2 c3 c4', positive=True)
     c1 = b*(((-a*sqrt(ra + rb - c ** 2))) / m + (b * c) / m) - c
     c2 = ((-a*sqrt(ra + rb - c ** 2)) / m + (b * c) / m)
     c3 = b*(((a*sqrt(ra + rb - c ** 2))) / m + (b * c) / m) - c
     c4 = ((a*sqrt(ra + rb - c ** 2)) / m + (b * c) / m)
-    assert solve([x**2 + y**2 - R**2, a*x + b*y - c], x, y) == [(-(c1 / a), c2), (-(c3 / a), c4)]
+    assert solve([x**2 + y**2 - R**2, a*x + b*y - c], x, y) == \
+        [(-(c1 / a), c2), (-(c3 / a), c4)]
+
