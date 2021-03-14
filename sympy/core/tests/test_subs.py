5c9ef7d43287a4974b57985bdcd6703aa8aa6935
Ayush Bisht <61404154+ayushbisht2001@users.noreply.github.com>
2021-03-15 02:59:39 +0530

    Update test_subs.py

--- sympy/core/tests/test_subs.py
+++ sympy/core/tests/test_subs.py
@@ -3,7 +3,7 @@ from sympy import (
     Integer, Eq, symbols, Add, I, Float, log, Rational,
     Lambda, atan2, cse, cot, tan, S, Tuple, Basic, Dict,
     Piecewise, oo, Mul, factor, nsimplify, zoo, Subs, RootOf,
-    AccumBounds, Matrix, zeros, ZeroMatrix)
+    AccumBounds, Matrix, zeros, ZeroMatrix, nan)
 from sympy.core.basic import _aresame
 from sympy.testing.pytest import XFAIL
 from sympy.abc import a, x, y, z, t
@@ -867,8 +867,8 @@ def test_subs_undefined_result():
     x, y = symbols("x, y")
     expr = x/y
     assert expr.subs([(x, 0), (y, 0)]) == 0
-    assert expr.subs([(x, 0), (y, 0)], simultaneous=True) == NaN
-    assert expr.subs([(x, 2), (y, 0)], simultaneous=True) == NaN
+    assert expr.subs([(x, 0), (y, 0)], simultaneous=True) == nan
+    assert expr.subs([(x, 2), (y, 0)], simultaneous=True) == nan
     expr2 = x**y
     assert expr2.subs([(x, 0), (y, 0)]) == 1
     assert expr2.subs([(x, 0), (y, 0)], simultaneous=True) == 1
