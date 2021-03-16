437b99004a6a0cdf4f1385b5a83a1f8560dd1641
Mohammad Anas <anasmd4u@gmail.com>
2021-03-17 05:00:18 +0530

    modified tests added to test_integrals

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -404,95 +404,98 @@ def test_issue_18133():
 
 def test_issue_21024():
     x = Symbol('x', real=True, nonzero=True)
-
+    cnt=0
     f = log(x)*log(4*x) + log(3*x + exp(2))
     F = x*log(x)**2 + x*(1 - 2*log(2)) + (-2*x + 2*x*log(2))*log(x) + \
         (x + exp(2)/6)*log(3*x + exp(2)) + exp(2)*log(3*x + exp(2))/6
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
+
 
     f = (x + exp(3))/x**2
     F = log(x) - exp(3)/x
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = (x**2 + exp(5))/x
     F = x**2/2 + exp(5)*log(x)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
-    #This test case fails as sinh is express in terms of E
     f = x/(2*x + tanh(1))
-    F = x/2 - log(2*x + tanh(1))*tanh(1)/4
-    assert F == integrate(f, x)
+    F = x/2 - (-1 + E)*(1 + E)*log(2*x + tanh(1))/(4*(1 + exp(2)))
+    assert simplify(F - integrate(f,x)).cancel() ==0
+
 
     f = x - sinh(4)/x
-    F = x**2/2 - log(x)*sinh(4)
-    assert F == integrate(f, x)
+    F = x**2/2 - (-1 + E)*(1 + E)*(1 + exp(2))*(1 + exp(4))*exp(-4)*log(x)/2
+    assert simplify(F - integrate(f,x)) == 0
 
     f = log(x + exp(5)/x)
     F = x*log(x + exp(5)/x) - x + 2*exp(5/2)*atan(x*exp(-5/2))
-    assert F == integrate(f, x)
+    assert simplify(f - integrate(f,x).diff(x)).cancel() == 0
 
     f = x**5/(x + E)
     F = x**5/5 - E*x**4/4 + x**3*exp(2)/3 - x**2*exp(3)/2 + x*exp(4) - exp(5)*log(x + E)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = 4*x/(x + sinh(5))
-    F = 4*x - 4*log(x + sinh(5))*sinh(5)
-    assert F == integrate(f, x)
+    F = 4*x - 2*(-1 + E)*(1 + E)*(1 + E + exp(2) + \
+        exp(3) + exp(4))*(-exp(3) - E + 1 + exp(2) + exp(4))*exp(-5)*log(x + sinh(5))
+    assert simplify(F - integrate(f,x)) == 0
 
     f = x**2/(2*x + sinh(2))
     F = x**2/4 - x*sinh(2)/4 + (-1 + E)**2*(1 + E)**2*(1 + exp(2))**2*exp(-4)*log(2*x + sinh(2))/32
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = -x**2/(x + E)
     F = -x**2/2 + E*x - exp(2)*log(x + E)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = (2*x + 3)*exp(5)/x
     F = 2*x*exp(5) + 3*exp(5)*log(x)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = x + 2 + cosh(3)/x
-    F = x**2/2 + 2*x + log(x)*cosh(3)
-    assert F == integrate(f, x)
+    F = x**2/2 + 2*x + (1 + exp(2))*(-exp(2) + 1 + exp(4))*exp(-3)*log(x)/2
+    assert simplify(F - integrate(f,x)) == 0
 
     f = x - tanh(1)/x**3
-    F = x**2/2 + tanh(1)/(2*x**2)
-    assert F == integrate(f, x)
+    F = x**2/2 + (-1 + exp(2))/(x**2*(2 + 2*exp(2)))
+    assert simplify(F - integrate(f,x)) == 0
 
     f = (3*x - exp(6))/x
     F = 3*x - exp(6)*log(x)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f =  x**4/(x + exp(5))**2 + x
     F =  x**3/3 + x**2*(1/2 - exp(5)) + 3*x*exp(10) - 4*exp(15)*log(x + exp(5)) - exp(20)/(x + exp(5))
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = x*(x + exp(10)/x**2) + x
     F =  x**3/3 + x**2/2 + exp(10)*log(x)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = x + x/(5*x + sinh(3))
     F = x**2/2 + x/5 - (-1 + E)*(1 + E)*(1 + E + exp(2))*(-E + 1 + exp(2))*exp(-3)*log(5*x + sinh(3))/50
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = (x + exp(3))/(2*x**2 + 2*x)
     F = exp(3)*log(x)/2 - (-1 + E)*(1 + E + exp(2))*log(x + ((-1 + E)*(1 + E + exp(2)) \
         + exp(3))/(-1 + 2*exp(3)))/2
-
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
     f = log(x + 4*sinh(4))
-    F = x*log(x + 4*sinh(4)) - x + 4*log(x + 4*sinh(4))*sinh(4)
-    assert F == integrate(f, x)
+    F = x*log(x + 4*sinh(4)) - x + 2*(-1 + E)*(1 + E)*(1 + \
+         exp(2))*(1 + exp(4))*exp(-4)*log(x + 4*sinh(4))
+    assert simplify(F - integrate(f,x)) == 0
 
     f = -x + 20*(exp(-5) - atan(4)/x)**3*sin(4)/x
     F = (-x**2*exp(15)/2 + 20*log(x)*sin(4) + (180*x**2*exp(5)*sin(4)*atan(4) - \
         90*x*exp(10)*sin(4)*atan(4)**2 + 20*exp(15)*sin(4)*atan(4)**3)/(3*x**3))*exp(-15)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
+
 
     f =  2*x**2*exp(-4) + 6/x
     F = (2*x**3/3 + 6*exp(4)*log(x))*exp(-4)
-    assert F == integrate(f, x)
+    assert simplify(F - integrate(f,x)) == 0
 
 
 def test_matrices():
