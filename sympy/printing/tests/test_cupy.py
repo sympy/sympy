11a4efdd9267e522d8418a059a806b13aaf00dad
Thomas Aarholt <thomasaarholt@gmail.com>
2021-03-23 23:30:43 +0100

    more tests

--- sympy/printing/tests/test_cupy.py
+++ sympy/printing/tests/test_cupy.py
@@ -1,7 +1,7 @@
 from sympy import lambdify, Sum, sqrt, log
 from sympy.abc import x, i, a, b
 from sympy.codegen.numpy_nodes import logaddexp
-from sympy.printing.numpy import CuPyPrinter
+from sympy.printing.numpy import CuPyPrinter, _cupy_known_constants, _cupy_known_functions
 
 from sympy.testing.pytest import skip
 from sympy.external import import_module
@@ -21,7 +21,7 @@ def test_not_cupy_print():
     prntr = CuPyPrinter()
     assert "Not supported" in prntr.doprint("abcd(x)")
 
-def test_sum():
+def test_cupy_sum():
     if not cp:
         skip("CuPy not installed")
 
@@ -38,3 +38,15 @@ def test_sum():
     a_, b_ = 0, 10
     x_ = cp.linspace(-1, +1, 10)
     assert cp.allclose(f(a_, b_, x_), sum(i_ * x_ for i_ in range(a_, b_ + 1)))
+
+def test_cupy_known_funcs_consts():
+    assert _cupy_known_constants['NaN'] == 'cupy.nan'
+    assert _cupy_known_constants['EulerGamma'] == 'cupy.euler_gamma'
+
+    assert _cupy_known_functions['acos'] == 'cupy.arccos'
+    assert _cupy_known_functions['log'] == 'cupy.log'
+
+def test_cupy_print_methods():
+    prntr = CuPyPrinter()
+    assert hasattr(prntr, '_print_acos')
+    assert hasattr(prntr, '_print_log')
