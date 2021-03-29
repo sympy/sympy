b089c4352b5bd983f12b6165cf5c16a9222b1d6a
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2021-03-29 18:19:37 +0530

    Update evalf.py

--- sympy/core/evalf.py
+++ sympy/core/evalf.py
@@ -1439,7 +1439,11 @@ class EvalfMixin:
 
         chop : bool or number, optional
             Specifies how to replace tiny real or imaginary parts in
-            subresults by exact zeros.
+            subresults by exact zeros. e.g.
+            ``a = Rational('37751345442791/1000000000000000000000000')
+             b = exp(-24)
+             (a-b).evalf(n=20, chop=True)  #incorrect method
+             a.expand().evalf(n=20, chop=True) - b.expand().evalf(n=20, chop=True) #correct method``
 
             When ``True`` the chop value defaults to standard precision.
 
