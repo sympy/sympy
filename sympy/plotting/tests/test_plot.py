c50fd0f8b41be1df62f4b3708a8d886cffd961c6
Davide_sd <sandona.davide@gmail.com>
2021-04-15 20:06:22 +0200

    Removed unused variables

--- sympy/plotting/tests/test_plot.py
+++ sympy/plotting/tests/test_plot.py
@@ -703,25 +703,25 @@ def test_issue_20113():
 def test_custom_coloring():
     x = Symbol('x')
     y = Symbol('y')
-    p = plot(cos(x), line_color=lambda a: a)
-    p = plot(cos(x), line_color=1)
-    p = plot(cos(x), line_color="r")
-    p = plot_parametric(cos(x), sin(x), line_color=lambda a: a)
-    p = plot_parametric(cos(x), sin(x), line_color=1)
-    p = plot_parametric(cos(x), sin(x), line_color="r")
-    p = plot3d_parametric_line(cos(x), sin(x), x, line_color=lambda a: a)
-    p = plot3d_parametric_line(cos(x), sin(x), x, line_color=1)
-    p = plot3d_parametric_line(cos(x), sin(x), x, line_color="r")
-    p = plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
+    plot(cos(x), line_color=lambda a: a)
+    plot(cos(x), line_color=1)
+    plot(cos(x), line_color="r")
+    plot_parametric(cos(x), sin(x), line_color=lambda a: a)
+    plot_parametric(cos(x), sin(x), line_color=1)
+    plot_parametric(cos(x), sin(x), line_color="r")
+    plot3d_parametric_line(cos(x), sin(x), x, line_color=lambda a: a)
+    plot3d_parametric_line(cos(x), sin(x), x, line_color=1)
+    plot3d_parametric_line(cos(x), sin(x), x, line_color="r")
+    plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
             (x, -5, 5), (y, -5, 5),
             surface_color=lambda a, b: a**2 + b**2)
-    p = plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
+    plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
             (x, -5, 5), (y, -5, 5),
             surface_color=1)
-    p = plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
+    plot3d_parametric_surface(cos(x + y), sin(x - y), x - y,
             (x, -5, 5), (y, -5, 5),
             surface_color="r")
-    p = plot3d(x*y, (x, -5, 5), (y, -5, 5),
+    plot3d(x*y, (x, -5, 5), (y, -5, 5),
             surface_color=lambda a, b: a**2 + b**2)
-    p = plot3d(x*y, (x, -5, 5), (y, -5, 5), surface_color=1)
-    p = plot3d(x*y, (x, -5, 5), (y, -5, 5), surface_color="r")
+    plot3d(x*y, (x, -5, 5), (y, -5, 5), surface_color=1)
+    plot3d(x*y, (x, -5, 5), (y, -5, 5), surface_color="r")
