0e23573e52a2a863af563ce5e8ab832e5a8813f2
Christopher Smith <smichr@gmail.com>
2021-04-16 06:43:31 -0500

    Update sympy/sets/handlers/issubset.py

--- sympy/sets/handlers/issubset.py
+++ sympy/sets/handlers/issubset.py
@@ -1,7 +1,7 @@
 from sympy import S, Symbol
 from sympy.core.logic import fuzzy_and, fuzzy_bool, fuzzy_not
 from sympy.core.relational import Eq
-from sympy.sets.sets import FiniteSet, Interval, Set, Union, ProductSet
+from sympy.sets.sets import FiniteSet, Interval, Set, Union, ProductSet, Complement
 from sympy.sets.fancysets import Complexes, Reals, Range, Rationals
 from sympy.multipledispatch import dispatch
 
