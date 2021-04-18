d3743f5ae2ac71353bfc792e97544f08d1789799
Davide_sd <sandona.davide@gmail.com>
2021-04-18 12:21:22 +0200

    Code Refactoring of ContourSeries
    Previously, the `ContourSeries` class was a copy of the `SurfaceOver2DRangeSeries` with
    a few missing parameters, which prevented the capability of setting the number of
    discretization points.
    
    Now, `ContourSeries` inherits from `SurfaceOver2DRangeSeries`, improving code
    readability and reusability, also fixing the capability to set the number of
    discretization points.

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -1139,29 +1139,11 @@ class ParametricSurfaceSeries(SurfaceBaseSeries):
 
 
 ### Contours
-class ContourSeries(BaseSeries):
+class ContourSeries(SurfaceOver2DRangeSeries):
     """Representation for a contour plot."""
-    # The code is mostly repetition of SurfaceOver2DRange.
-    # Presently used in contour_plot function
 
+    is_3Dsurface = False
     is_contour = True
-
-    def __init__(self, expr, var_start_end_x, var_start_end_y):
-        super().__init__()
-        self.nb_of_points_x = 50
-        self.nb_of_points_y = 50
-        self.expr = sympify(expr)
-        self.var_x = sympify(var_start_end_x[0])
-        self.start_x = float(var_start_end_x[1])
-        self.end_x = float(var_start_end_x[2])
-        self.var_y = sympify(var_start_end_y[0])
-        self.start_y = float(var_start_end_y[1])
-        self.end_y = float(var_start_end_y[2])
-
-        self.get_points = self.get_meshes
-
-        self._xlim = (self.start_x, self.end_x)
-        self._ylim = (self.start_y, self.end_y)
 
     def __str__(self):
         return ('contour: %s for '
@@ -1171,15 +1153,6 @@ class ContourSeries(BaseSeries):
                     str((self.start_x, self.end_x)),
                     str(self.var_y),
                     str((self.start_y, self.end_y)))
-
-    def get_meshes(self):
-        np = import_module('numpy')
-        mesh_x, mesh_y = np.meshgrid(np.linspace(self.start_x, self.end_x,
-                                                 num=self.nb_of_points_x),
-                                     np.linspace(self.start_y, self.end_y,
-                                                 num=self.nb_of_points_y))
-        f = vectorized_lambdify((self.var_x, self.var_y), self.expr)
-        return (mesh_x, mesh_y, f(mesh_x, mesh_y))
 
 
 ##############################################################################
@@ -2486,7 +2459,7 @@ def plot_contour(*args, show=True, **kwargs):
     args = list(map(sympify, args))
     kwargs = _set_discretization_points(kwargs, "pc")
     plot_expr = check_arguments(args, 1, 2)
-    series = [ContourSeries(*arg) for arg in plot_expr]
+    series = [ContourSeries(*arg, **kwargs) for arg in plot_expr]
     plot_contours = Plot(*series, **kwargs)
     if len(plot_expr[0].free_symbols) > 2:
         raise ValueError('Contour Plot cannot Plot for more than two variables.')
