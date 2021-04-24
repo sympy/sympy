35b717e173f8237e61d727296deb07b7e445200e
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2021-04-24 19:53:28 -0400

    Undo editor formatting

--- sympy/core/mod.py
+++ sympy/core/mod.py
@@ -57,7 +57,7 @@ class Mod(Function):
 
             if q.is_Number:
                 if p.is_Number:
-                    return p % q
+                    return p%q
                 if q == 2:
                     if p.is_even:
                         return S.Zero
@@ -70,7 +70,7 @@ class Mod(Function):
                     return rv
 
             # by ratio
-            r = p / q
+            r = p/q
             if r.is_integer:
                 return S.Zero
             try:
@@ -79,8 +79,8 @@ class Mod(Function):
                 pass
             else:
                 if isinstance(d, int):
-                    rv = p - d * q
-                    if (rv * q < 0) == True:
+                    rv = p - d*q
+                    if (rv*q < 0) == True:
                         rv += q
                     return rv
 
@@ -111,14 +111,14 @@ class Mod(Function):
             qinner = p.args[1]
             if qinner % q == 0:
                 return cls(p.args[0], q)
-            elif (qinner * (q - qinner)).is_nonnegative:
+            elif (qinner*(q - qinner)).is_nonnegative:
                 # |qinner| < |q| and have same sign
                 return p
         elif isinstance(-p, cls):
             qinner = (-p).args[1]
             if qinner % q == 0:
                 return cls(-(-p).args[0], q)
-            elif (qinner * (q + qinner)).is_nonpositive:
+            elif (qinner*(q + qinner)).is_nonpositive:
                 # |qinner| < |q| and have different sign
                 return p
         elif isinstance(p, Add):
@@ -150,14 +150,14 @@ class Mod(Function):
                 prod_mod = Mul(*mod)
                 prod_non_mod = Mul(*non_mod)
                 prod_mod1 = Mul(*[i.args[0] for i in mod_l])
-                net = prod_mod1 * prod_mod
-                return prod_non_mod * cls(net, q)
+                net = prod_mod1*prod_mod
+                return prod_non_mod*cls(net, q)
 
             if q.is_Integer and q is not S.One:
                 _ = []
                 for i in non_mod_l:
                     if i.is_Integer and (i % q is not S.Zero):
-                        _.append(i % q)
+                        _.append(i%q)
                     else:
                         _.append(i)
                 non_mod_l = _
@@ -170,9 +170,8 @@ class Mod(Function):
         try:
             G = gcd(p, q)
             if G != 1:
-                p, q = [
-                    gcd_terms(i / G, clear=False, fraction=False)
-                    for i in (p, q)]
+                p, q = [gcd_terms(i/G, clear=False, fraction=False)
+                        for i in (p, q)]
         except PolynomialError:  # issue 21373
             G = S.One
         pwas, qwas = p, q
@@ -201,11 +200,11 @@ class Mod(Function):
                 r = cp % cq
                 if r == 0:
                     G *= cq
-                    p *= int(cp / cq)
+                    p *= int(cp/cq)
                     ok = True
             if not ok:
-                p = cp * p
-                q = cq * q
+                p = cp*p
+                q = cq*q
 
         # simple -1 extraction
         if p.could_extract_minus_sign() and q.could_extract_minus_sign():
@@ -214,16 +213,16 @@ class Mod(Function):
         # check again to see if p and q can now be handled as numbers
         rv = doit(p, q)
         if rv is not None:
-            return rv * G
+            return rv*G
 
         # put 1.0 from G on inside
         if G.is_Float and G == 1:
             p *= G
             return cls(p, q, evaluate=False)
         elif G.is_Mul and G.args[0].is_Float and G.args[0] == 1:
-            p = G.args[0] * p
+            p = G.args[0]*p
             G = Mul._from_args(G.args[1:])
-        return G * cls(p, q, evaluate=(p, q) != (pwas, qwas))
+        return G*cls(p, q, evaluate=(p, q) != (pwas, qwas))
 
     def _eval_is_integer(self):
         from sympy.core.logic import fuzzy_and, fuzzy_not
@@ -241,4 +240,4 @@ class Mod(Function):
 
     def _eval_rewrite_as_floor(self, a, b, **kwargs):
         from sympy.functions.elementary.integers import floor
-        return a - b * floor(a / b)
+        return a - b*floor(a/b)
