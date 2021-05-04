54b0344ce717a4200efb223c52db75cb6f70f2c3
Ickaser <ickaser@gmail.com>
2021-05-04 17:07:59 -0600

    Heaviside printer setting, first attempt

--- sympy/printing/numpy.py
+++ sympy/printing/numpy.py
@@ -222,7 +222,9 @@ class NumPyPrinter(PythonCodePrinter):
 
     def _print_Heaviside(self, expr):
         if len(expr.args) == 1: # Use a default value for Heaviside(0)
-            heaviside0 = 1/2 # Default value of heaviside at x=0. Other options include 0, 1
+            heaviside0 = self._settings.get('Heaviside0', None)
+            if heaviside0 == None:
+                heaviside0 = 1/2 # Default value of heaviside at x=0. Other options include 0, 1
         else:
             heaviside0 = expr.args[1]
         return "%s(%s,%s)" % (self._module_format('numpy.heaviside'), self._print(expr.args[0]), self._print(heaviside0))
