1f4e4417bb123418eab52db48da11a61ed5983d9
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2021-05-27 13:57:41 +0100

    test(polys): make linsolve tests more robust

--- sympy/polys/matrices/tests/test_linsolve.py
+++ sympy/polys/matrices/tests/test_linsolve.py
@@ -24,41 +24,52 @@ def test__linsolve():
 
 
 def test__linsolve_float():
+
+    # This should give the exact answer:
     eqs = [
         y - x,
         y - 0.0216 * x
     ]
-    sol = {x:0, y:0}
+    sol = {x:0.0, y:0.0}
     assert _linsolve(eqs, (x, y)) == sol
+
+    # Other cases should be close to eps
+
+    def all_close(sol1, sol2, eps=1e-15):
+        close = lambda a, b: abs(a - b) < eps
+        assert sol1.keys() == sol2.keys()
+        return all(close(sol1[s], sol2[s]) for s in sol1)
 
     eqs = [
         0.8*x +         0.8*z + 0.2,
         0.9*x + 0.7*y + 0.2*z + 0.9,
         0.7*x + 0.2*y + 0.2*z + 0.5
     ]
-    #sol = {x:-0.69047619047619047, y:-0.52380952380952395, z:0.44047619047619047}
-    sol = {x:-0.69047619047619069, y:-0.52380952380952361, z:0.44047619047619069}
-    assert _linsolve(eqs, [x,y,z]) == sol
+    sol_exact = {x:-29/42, y:-11/21, z:37/84}
+    sol_linsolve = _linsolve(eqs, [x,y,z])
+    assert all_close(sol_exact, sol_linsolve)
 
     eqs = [
         0.9*x + 0.3*y + 0.4*z + 0.6,
         0.6*x + 0.9*y + 0.1*z + 0.7,
         0.4*x + 0.6*y + 0.9*z + 0.5
     ]
-    #sol = {x:-0.50285714285714289, y:-0.43809523809523804, z:-0.040000000000000063}
-    sol = {x:-0.50285714285714278, y:-0.43809523809523798, z:-0.040000000000000022}
-    assert _linsolve(eqs, [x,y,z]) == sol
+    sol_exact = {x:-88/175, y:-46/105, z:-1/25}
+    sol_linsolve = _linsolve(eqs, [x,y,z])
+    assert all_close(sol_exact, sol_linsolve)
 
     eqs = [
         x*(0.7 + 0.6*I) + y*(0.4 + 0.7*I) + z*(0.9 + 0.1*I) + 0.5,
         0.2*I*x + 0.2*I*y + z*(0.9 + 0.2*I) + 0.1,
         x*(0.9 + 0.7*I) + y*(0.9 + 0.7*I) + z*(0.9 + 0.4*I) + 0.4,
     ]
-    sol = {x: -2.5 + 1.0000000000000002*I, y: 2.25 - 0.5*I, z: -0.063789868667917513 + 0.066916823014383939*I}
-    sol = {x: -0.77010631644778016 - 0.077110694183864806*I,
-           y:0.53277048155096973 + 0.22313946216385216*I,
-           z:-0.063789868667917443 + 0.066916823014384022*I}
-    assert _linsolve(eqs, [x,y,z]) == sol
+    sol_exact = {
+        x:-6157/7995 - 411/5330*I,
+        y:8519/15990 + 1784/7995*I,
+        z:-34/533 + 107/1599*I,
+    }
+    sol_linsolve = _linsolve(eqs, [x,y,z])
+    assert all_close(sol_exact, sol_linsolve)
 
 
 def test__linsolve_deprecated():
