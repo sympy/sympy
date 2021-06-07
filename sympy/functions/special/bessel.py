e7612cbba75434b1ba86f078cfaa326dbb9ee7cf
Sidharth Mundhra <sidharthmundhra16@gmail.com>
2021-06-07 15:28:34 +0530

    Fixed besselj and bessely leading term methods

--- sympy/functions/special/bessel.py
+++ sympy/functions/special/bessel.py
@@ -218,11 +218,11 @@ class besselj(BesselBase):
 
     def _eval_as_leading_term(self, x, cdir=0):
         nu, z = self.args
-        arg = z.as_leading_term(x)
+        arg = (z/2).as_leading_term(x)
         if x in arg.free_symbols:
-            return z**nu
+            return arg**nu
         else:
-            return self.func(*self.args)
+            return self.func(nu, z.subs(x, 0).cancel())
 
     def _eval_is_extended_real(self):
         nu, z = self.args
@@ -337,11 +337,14 @@ class bessely(BesselBase):
 
     def _eval_as_leading_term(self, x, cdir=0):
         nu, z = self.args
-        arg = z.as_leading_term(x)
+        term_one = ((2/pi)*log(z/2)*besselj(nu, z))
+        term_two = (z/2)**(-nu)*factorial(nu - 1)/pi if (nu - 1).is_positive else S.Zero
+        term_three = (z/2)**nu/(pi*factorial(nu))*(digamma(nu + 1) - S.EulerGamma)
+        arg = Add(*[term_one, term_two, term_three]).as_leading_term(x)
         if x in arg.free_symbols:
-            return z**nu
+            return arg
         else:
-            return self.func(*self.args)
+            return self.func(nu, z.subs(x, 0).cancel())
 
     def _eval_is_extended_real(self):
         nu, z = self.args
