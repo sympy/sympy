b5ba56020cc32e57404f5acd93b1dc3424a9e3c5
Sudeep Sidhu <sudeepmanilsidhu@gmail.com>
2021-06-30 12:30:41 +0530

    Correct the test

--- sympy/physics/vector/tests/test_frame.py
+++ sympy/physics/vector/tests/test_frame.py
@@ -453,8 +453,14 @@ def test_orient_explicit():
 def test_orient_axis():
     A = ReferenceFrame('A')
     B = ReferenceFrame('B')
-    assert A.orient_axis(B,-B.x, 1) == A.orient_axis(B, B.x, -1)
-    assert A.orient_axis(B,-B.x, 1) == A.orient_axis(B, 1, -B.x)
+    A.orient_axis(B,-B.x, 1)
+    A1 = A.dcm(B)
+    A.orient_axis(B, B.x, -1)
+    A2 = A.dcm(B)
+    A.orient_axis(B, 1, -B.x)
+    A3 = A.dcm(B)
+    assert A1 == A2
+    assert A2 == A3
     raises(TypeError, lambda: A.orient_axis(B, 1, 1))
 
 def test_orient_body():
