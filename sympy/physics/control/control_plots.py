44819e766de1a2c5b53d46cb18bc1be5dc2a94bc
akshanshbhatt <53227127+akshanshbhatt@users.noreply.github.com>
2021-07-20 12:32:31 +0530

    Initial Commit. Add `pole_zero` plot.

---
+++ sympy/physics/control/control_plots.py
@@ -0,0 +1,36 @@
+from sympy.external import import_module
+
+matplotlib = import_module(
+        'matplotlib', import_kwargs={'fromlist': ['pyplot']},
+        catch=(RuntimeError,))
+
+if matplotlib:
+    plt = matplotlib.pyplot
+
+def pole_zero(transfer_function, **kwargs):
+    r"""
+    Returns the Pole-Zero plot (also known as PZ Plot or PZ Map) of a system.
+    """
+    poles = transfer_function.poles()
+    zeros = transfer_function.zeros()
+
+    pole_points = [pole.as_real_imag() for pole in poles]
+    zero_points = [zero.as_real_imag() for zero in zeros]
+
+    x_poles = list(map(lambda x: x[0], pole_points))
+    y_poles = list(map(lambda x: x[1], pole_points))
+
+    x_zeros = list(map(lambda x: x[0], zero_points))
+    y_zeros = list(map(lambda x: x[1], zero_points))
+
+    plt.plot(x_poles, y_poles, 'x', mfc='none', markersize=15)
+    plt.plot(x_zeros, y_zeros, 'o', markersize=10)
+    plt.xlabel('Real')
+    plt.ylabel('Imaginary')
+    plt.title('Poles and Zeros')
+    plt.grid()
+    plt.axhline(0, color='black')
+    plt.axvline(0, color='black')
+    plt.show()
+    return
+
