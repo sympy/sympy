0e9da06bef0421ccf8e7f25f6c93a7750d55d271
Sudeep Sidhu <sudeepmanilsidhu@gmail.com>
2021-07-23 13:24:28 +0530

    Add tests with sliding joint

--- sympy/physics/mechanics/tests/test_jointsmethod.py
+++ sympy/physics/mechanics/tests/test_jointsmethod.py
@@ -1,5 +1,5 @@
-from sympy import symbols, Matrix, cos, sin
-from sympy.physics.mechanics import PinJoint, JointsMethod, Body, KanesMethod
+from sympy import symbols, Matrix, cos, sin, expand
+from sympy.physics.mechanics import PinJoint, JointsMethod, Body, KanesMethod, PrismaticJoint
 from sympy.physics.vector import dynamicsymbols
 
 def test_jointsmethod():
@@ -41,3 +41,24 @@ def test_complete_double_pendulum():
     qudots = mm.inv() * fo
     qudots.simplify()
     qudots == Matrix([[o1], [o2], [(l2*(-(o1 + o2)**2 + o1**2) - (l1*cos(t2) + l2)*o1**2)*sin(t2)/(l1*(cos(t2)**2 - 2))], [(l2*(l1*cos(t2) + l2)*((o1 + o2)**2 - o1**2) + (2*l1**2 + 2*l1*l2*cos(t2) + l2**2)*o1**2)*sin(t2)/(l1*l2*(cos(t2)**2 - 2))]])
+
+def test_two_dof_joints():
+    q1, q2, u1, u2 = dynamicsymbols('q1 q2 u1 u2')
+    m, c1, c2, k1, k2 = symbols('m c1 c2 k1 k2')
+    W = Body('W')
+    B1 = Body('B1', mass=m)
+    B2 = Body('B2', mass=m)
+    J1 = PrismaticJoint('J1', W, B1, coordinates=q1, speeds=u1)
+    J2 = PrismaticJoint('J2', B1, B2, coordinates=q2, speeds=u2)
+    W.apply_force(k1*q1*W.x, reaction_body=B1)
+    W.apply_force(c1*u1*W.x, reaction_body=B1)
+    B1.apply_force(k2*q2*W.x, reaction_body=B2)
+    B1.apply_force(c2*u2*W.x, reaction_body=B2)
+    method = JointsMethod(W, J1, J2)
+    method.form_eoms()
+    MM = method.mass_matrix
+    forcing = method.forcing
+    rhs = MM.inv() * forcing
+    assert expand(rhs[0]) == expand((-k1 * q1 - c1 * u1 + k2 * q2 + c2 * u2)/m)
+    assert expand(rhs[1]) == expand((k1 * q1 + c1 * u1 - 2 * k2 * q2 - 2 *
+                                    c2 * u2) / m)
