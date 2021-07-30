5fb520ed577f3eca9aabe784041ea0caa050cf72
akshanshbhatt <53227127+akshanshbhatt@users.noreply.github.com>
2021-07-30 18:46:46 +0530

    Update __init__.py.

--- sympy/physics/control/__init__.py
+++ sympy/physics/control/__init__.py
@@ -1,3 +1,3 @@
 from .lti import TransferFunction, Series, Parallel, Feedback, TransferFunctionMatrix
 
-__all__ = ['TransferFunction', 'Series', 'Parallel', 'Feedback', 'TransferFunctionMatrix']
+__all__ = ['TransferFunction', 'Series', 'MIMOSeries', 'Parallel', 'MIMOParallel', 'Feedback', 'TransferFunctionMatrix']
