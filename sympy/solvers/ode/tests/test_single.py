b6052f25024ef483324759dc8e227033c9214211
Mohit Balwani <mohitbalwani.ict17@gmail.com>
2021-08-02 22:44:52 +0530

    change test

--- sympy/solvers/ode/tests/test_single.py
+++ sympy/solvers/ode/tests/test_single.py
@@ -1357,7 +1357,8 @@ def _get_examples_ode_sol_nth_order_reducible():
     # Needs to be a way to know how to combine derivatives in the expression
     'reducible_12': {
         'eq': Derivative(x*f(x), x, x, x) + Derivative(f(x), x, x, x),
-        'sol': [Eq(f(x), C1 + C2*x + C3/Mul(2, (x + 1), evaluate=False))], # 2-arg Mul!
+        'sol': [Eq(f(x), C1 + C3/Mul(2, (x**2 + 2*x + 1), evaluate=False) +
+        x*(C2 + C3/Mul(2, (x**2 + 2*x + 1), evaluate=False)))], # 2-arg Mul!
         'slow': True,
     },
     }
