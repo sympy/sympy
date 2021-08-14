30153ebeb5554e54073236ed1aff5cd6cb22e976
Akshansh <53227127+akshanshbhatt@users.noreply.github.com>
2021-08-14 18:47:23 +0530

    Refactor Control Plots. WIP.

--- sympy/physics/control/control_plots.py
+++ sympy/physics/control/control_plots.py
@@ -1,9 +1,12 @@
-from sympy import I, log, sqrt
+from sympy import (I, log, sqrt, symbols, apart, Wild,
+    RootSum, Lambda, together, exp, gamma)
 from sympy.core.symbol import Symbol
 from sympy.external import import_module
 from sympy.functions import arg
 from sympy.integrals.transforms import inverse_laplace_transform
+from sympy.physics.control.lti import SISOLinearTimeInvariant
 from sympy.plotting import PlotGrid, plot
+from sympy.plotting.plot import LineOver1DRangeSeries
 from sympy.polys.polytools import Poly
 
 matplotlib = import_module(
@@ -19,11 +22,17 @@ if numpy:
     np = numpy  # Matplotlib already has numpy as a compulsory dependency. No need to install it separately.
 
 
-def pole_zero(system, pole_colour='r', zero_colour='b', grid=True, show=True, **kwargs):
-    r"""
-    Returns the Pole-Zero plot (also known as PZ Plot or PZ Map) of a system.
-    """
+def pole_zero_numerical_data(system):
+    """Returns the numerical data of poles and zeros of the system."""
     system = system.doit()  # Get the equivalent TransferFunction object.
+    if not isinstance(system, SISOLinearTimeInvariant):
+        
+    sys = system.to_expr()
+    len_free_symbols = len(sys.free_symbols)
+    if len_free_symbols > 1:
+        raise ValueError("Extra degree of freedom found. Make sure"
+            "There are no free symbols in the dynamical system other"
+            " than the variable of Laplace transform.")
 
     num_poly = Poly(system.num, system.var).all_coeffs()
     den_poly = Poly(system.den, system.var).all_coeffs()
@@ -31,7 +40,17 @@ def pole_zero(system, pole_colour='r', zero_colour='b', grid=True, show=True, **
     num_poly = np.array(num_poly, dtype=np.float64)
     den_poly = np.array(den_poly, dtype=np.float64)
 
-    zeros, poles = _find_roots(num_poly, den_poly)
+    zeros = np.roots(num_poly)
+    poles = np.roots(den_poly)
+
+    return zeros, poles
+
+
+def pole_zero_plot(system, pole_colour='r', zero_colour='b', grid=True, show=True, **kwargs):
+    r"""
+    Returns the Pole-Zero plot (also known as PZ Plot or PZ Map) of a system.
+    """
+    zeros, poles = pole_zero_numerical_data(system)
 
     zero_real = np.real(zeros)
     zero_imag = np.imag(zeros)
@@ -54,7 +73,11 @@ def pole_zero(system, pole_colour='r', zero_colour='b', grid=True, show=True, **
     return plt
 
 
-def step_response(system, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
+def step_response_numerical_data(system):
+    pass
+
+
+def step_response_plot(system, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
     r"""
     Return the unit step response of a continuous-time system.
     """
@@ -65,7 +88,11 @@ def step_response(system, show_input=False, colour='r', show=True, grid=True, up
         xlabel="Time (s)", ylabel="Amplitude")
 
 
-def impulse_response(system, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
+def impulse_response_numerical_data(system):
+    pass
+
+
+def impulse_response_plot(system, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
     r"""
     Return the unit impulse response (Input is the Dirac-Delta Function) of a
     continuous-time system.
@@ -73,11 +100,15 @@ def impulse_response(system, show_input=False, colour='r', show=True, grid=True,
     x = Symbol("x")
     expr = system.to_expr()
     y = inverse_laplace_transform(expr, system.var, x)
-    return plot(y, (x, 0, 6), show=True, title="Impulse Response", \
+    return plot(y, (x, 0, 60), show=True, title="Impulse Response", \
         xlabel="Time (s)", ylabel="Amplitude")
 
 
-def ramp_response(system, slope=1, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
+def ramp_response_numerical_data(system):
+    pass
+
+
+def ramp_response_plot(system, slope=1, show_input=False, colour='r', show=True, grid=True, upper_limit=6, **kwargs):
     r"""
     Return the unit impulse response (Input is the Dirac-Delta Function) of a
     continuous-time system.
@@ -87,6 +118,10 @@ def ramp_response(system, slope=1, show_input=False, colour='r', show=True, grid
     y = inverse_laplace_transform(expr, system.var, x)
     return plot(y, (x, 0, upper_limit), show=show, title="Ramp Response", \
         xlabel="Time (s)", ylabel="Amplitude")
+
+
+def bode_numerical_data(system):
+    pass
 
 
 def bode_plot(system, initial_exp=-5, final_exp=5, show=True, **kwargs):
@@ -111,50 +146,47 @@ def bode_plot(system, initial_exp=-5, final_exp=5, show=True, **kwargs):
     return PlotGrid(2, 1, mag_plot, phase_plot, show=show)
 
 
-def _find_roots(num, den, k=None):
-    """Private method to find roots efficiently."""
-    if k is not None:
-        num_roots = max(len(np.roots(den)), len(np.roots(num)))
-        roots = []
-        for gain in k:
-            individual_roots = np.roots(np.polyadd(den, (gain*num)))
-            if len(individual_roots) == num_roots:
-                individual_roots.sort()
-                roots.append(individual_roots)
+def _fast_inverse_laplace(e, s, t):
+    """Fast inverse Laplace transform of rational function including RootSum"""
+    a, b, n = symbols('a, b, n', cls=Wild, exclude=[s])
 
-        return np.vstack(roots)
+    def _ilt(e):
+        if not e.has(s):
+            return e
+        elif e.is_Add:
+            return _ilt_add(e)
+        elif e.is_Mul:
+            return _ilt_mul(e)
+        elif e.is_Pow:
+            return _ilt_pow(e)
+        elif isinstance(e, RootSum):
+            return _ilt_rootsum(e)
+        else:
+            raise NotImplementedError
 
-    num_roots = np.roots(num)
-    den_roots = np.roots(den)
+    def _ilt_add(e):
+        return e.func(*map(_ilt, e.args))
 
-    return num_roots, den_roots
+    def _ilt_mul(e):
+        coeff, expr = e.as_independent(s)
+        if expr.is_Mul:
+            raise NotImplementedError
+        return coeff * _ilt(expr)
 
+    def _ilt_pow(e):
+        match = e.match((a*s + b)**n)
+        if match is not None:
+            nm, am, bm = match[n], match[a], match[b]
+            if nm.is_Integer and nm < 0:
+                if nm == 1:
+                    return exp(-(bm/am)*t) / am
+                else:
+                    return t**(-nm-1)*exp(-(bm/am)*t)/(am**-nm*gamma(-nm))
+        raise NotImplementedError
 
-def root_locus(system, k_max=400.0, num=10000, show=True, grid=False, **kwargs):
-    """
-    Generates a root locus plot of a continuous-time system.
-    """
-    system = system.doit()
+    def _ilt_rootsum(e):
+        expr = e.fun.expr
+        [variable] = e.fun.variables
+        return RootSum(e.poly, Lambda(variable, together(_ilt(expr))))
 
-    k = np.linspace(0.0, float(k_max), num=num)
-
-    num_poly = Poly(system.num, system.var).all_coeffs()
-    den_poly = Poly(system.den, system.var).all_coeffs()
-
-    num_poly = np.array(num_poly, dtype=np.float64)
-    den_poly = np.array(den_poly, dtype=np.float64)
-
-    roots = _find_roots(num_poly, den_poly, k)
-
-    real = np.real(roots)
-    imag = np.imag(roots)
-
-    pz = pole_zero(system, show=False, grid=grid)
-
-    pz.title('Root Loci')
-    pz.plot(real, imag)
-
-    if show:
-        pz.show()
-        return
-    return pz
+    return _ilt(e)
