b159250ef3008d2d140698b399b1663bb1a688b8
Psycho-Pirate <prakharsaxena.civ18@iitbhu.ac.in>
2021-08-26 17:37:43 +0530

    test case added

--- sympy/physics/continuum_mechanics/tests/test_beam.py
+++ sympy/physics/continuum_mechanics/tests/test_beam.py
@@ -749,7 +749,7 @@ def test_max_bending_moment_Beam3D():
     b.apply_load(15, start=0, order=0, dir="z")
     b.apply_load(12*x, start=0, order=0, dir="y")
     b.bc_deflection = [(0, [0, 0, 0]), (20, [0, 0, 0])]
-    assert b.max_bending_moment() == [(0, 0), (20, 3000), (20, 16000)]
+    assert b.max_bmoment() == [(0, 0), (20, 3000), (20, 16000)]
 
 def test_max_deflection_Beam3D():
     x = symbols('x')
