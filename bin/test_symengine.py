f7a3fe36d45e6b10c22eb9aa641d2e3eea88c9d4
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2021-08-31 16:49:53 +0100

    maint(ci): run symengine tests in CI (USE_SYMENGINE=1)

---
+++ bin/test_symengine.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Run tests involving symengine
+
+These are separate from the other optional dependency tests because they need
+to be run with the USE_SYMENGINE=1 environment variable set. This script does
+not set the environment variable by default so that the same tests can be run
+with and without symengine.
+
+Run this as:
+
+    $ USE_SYMENGINE=1 bin/test_symengine.py
+"""
+
+# Add the local sympy to sys.path (needed for CI)
+from get_sympy import path_hack
+path_hack()
+
+
+class TestsFailedError(Exception):
+    pass
+
+
+test_list = doctest_list = [
+    'sympy/physics/mechanics',
+    'sympy/liealgebras',
+]
+
+
+print('Testing optional dependencies')
+
+
+import sympy
+if not (sympy.test(*test_list, verbose=True) and sympy.doctest(*doctest_list)):
+    raise TestsFailedError('Tests failed')
