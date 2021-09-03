ad709f60420965419b8b2dcdf59715e13aad7578
Evandro Bernardes <15084103+evbernardes@users.noreply.github.com>
2021-09-03 09:51:29 +0200

    added quaternion matrix and vector properties
    Sometimes it is useful to get the imaginary part of a quaternion as a vector (3x1 Matrix), and sometimes it is useful to have the whole quaternion as a 4-vector.
    I propose adding two properties to get exactly this: 
    - Quaternion.v = returns the imaginary part as a 3x1 Matrix / vector
    - Quaternion.v4 = returns the real+imaginary parts as a 4x1 Matrix / 4-vector
    
    Moreover, treating the quaternion as a 4-vector, we can easily define the Hamilton product between two quaternions as the product between a 4x4 matrix and quaternion (more can be read about this on Wikipedia).
    I propose the addition of two more properties: one that gives a matrix representing the product by a quaternion on the left, and another one representing the product of the quaternion on the right:
    - Quaternion.LM = matrix representing the multiplication by the quaternion on the left
    - Quaternion.RM = matrix representing the multiplication by the quaternion on the right
    
    To check that the implementations are correct, we can easily compare these three expressions:
    q = Quaternion(qa,qb,qc,qd)
    p = Quaternion(pa,pb,pc,pd)
    expr1 = (p * q).v4
    expr2 = p.LM * q.v4
    expr3 = q.RM * p.v4

--- sympy/algebras/quaternion.py
+++ sympy/algebras/quaternion.py
@@ -74,6 +74,32 @@ class Quaternion(Expr):
     @property
     def d(self):
         return self._d
+    
+    # vector/imaginary part as 3x1 Matrix
+    @property
+    def v(self):
+        return Matrix([self._b,self._c,self._d])
+    
+    # whole quaternion as 4x1 Matrix
+    @property
+    def v4(self):
+        return Matrix([self._a,self._b,self._c,self._d])
+    
+    @property
+    def LM(self):
+        return Matrix([
+                [+self.a,-self.b,-self.c,-self.d],
+                [+self.b,+self.a,-self.d,+self.c],
+                [+self.c,+self.d,+self.a,-self.b],
+                [+self.d,-self.c,+self.b,+self.a]])
+    
+    @property
+    def RM(self):
+        return Matrix([
+                [+self.a,-self.b,-self.c,-self.d],
+                [+self.b,+self.a,+self.d,-self.c],
+                [+self.c,-self.d,+self.a,+self.b],
+                [+self.d,+self.c,-self.b,+self.a]])
 
     @property
     def real_field(self):
