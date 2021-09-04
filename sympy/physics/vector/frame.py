88b450ea7d6614d4659fd2fb5a504f1455ce29c7
Sudeep Sidhu <sudeepmanilsidhu@gmail.com>
2021-09-04 13:03:55 +0530

    Fix failing symengine part in Frame

--- sympy/physics/vector/frame.py
+++ sympy/physics/vector/frame.py
@@ -1,7 +1,6 @@
-from sympy.core.backend import (diff, expand, sin, cos, sympify, eye,
+from sympy.core.backend import (diff, expand, sin, cos, sympify, eye, symbols,
                                 ImmutableMatrix as Matrix, MatrixBase)
-from sympy.core.sympify import sympify as sympify2
-from sympy import (trigsimp, solve, Symbol)
+from sympy import (trigsimp, solve, Symbol, Dummy)
 from sympy.physics.vector.vector import Vector, _check_vector
 from sympy.utilities.misc import translate
 
@@ -879,17 +878,17 @@ class ReferenceFrame:
 
         try:
             from sympy.polys.polyerrors import CoercionFailed
-            from sympy.physics.vector.functions import kinematic_equations, dynamicsymbols
+            from sympy.physics.vector.functions import kinematic_equations
             q1, q2, q3 = amounts
-            u1, u2, u3 = dynamicsymbols('u1, u2, u3')
+            u1, u2, u3 = symbols('u1, u2, u3', cls=Dummy)
             templist = kinematic_equations([u1, u2, u3], [q1, q2, q3],
                                            'body', rot_order)
             templist = [expand(i) for i in templist]
-            td = solve(templist, [u1, u2, u3], dict = True)
-            u1, u2, u3 = map(sympify2, [u1, u2, u3])
-            u1 = expand(td[0][u1])
-            u2 = expand(td[0][u2])
-            u3 = expand(td[0][u3])
+            td = solve(templist, [u1, u2, u3])
+            u1, u2, u3 = map(sympify, [u1, u2, u3])
+            u1 = expand(td[u1])
+            u2 = expand(td[u2])
+            u3 = expand(td[u3])
             wvec = u1 * self.x + u2 * self.y + u3 * self.z
         except (CoercionFailed, AssertionError):
             wvec = self._w_diff_dcm(parent)
@@ -1002,17 +1001,17 @@ class ReferenceFrame:
 
         try:
             from sympy.polys.polyerrors import CoercionFailed
-            from sympy.physics.vector.functions import kinematic_equations, dynamicsymbols
+            from sympy.physics.vector.functions import kinematic_equations
             q1, q2, q3 = amounts
-            u1, u2, u3 = dynamicsymbols('u1, u2, u3')
+            u1, u2, u3 = symbols('u1, u2, u3', cls=Dummy)
             templist = kinematic_equations([u1, u2, u3], [q1, q2, q3],
                                            'space', rot_order)
             templist = [expand(i) for i in templist]
-            td = solve(templist, [u1, u2, u3], dict = True)
-            u1, u2, u3 = map(sympify2, [u1, u2, u3])
-            u1 = expand(td[0][u1])
-            u2 = expand(td[0][u2])
-            u3 = expand(td[0][u3])
+            td = solve(templist, [u1, u2, u3])
+            u1, u2, u3 = map(sympify, [u1, u2, u3])
+            u1 = expand(td[u1])
+            u2 = expand(td[u2])
+            u3 = expand(td[u3])
             wvec = u1 * self.x + u2 * self.y + u3 * self.z
         except (CoercionFailed, AssertionError):
             wvec = self._w_diff_dcm(parent)
