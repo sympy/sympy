7a870ce9b9d95369ce9d25ed113456582f5e23c7
Steve Kieffer <sk@skieffer.info>
2021-10-07 18:54:45 -0400

    Simplify imports in `polys.numberfields.__init__`.
    We only import things that were explicitly marked as `@public`.

--- sympy/polys/numberfields/__init__.py
+++ sympy/polys/numberfields/__init__.py
@@ -8,22 +8,8 @@ __all__ = [
     'to_number_field', 'isolate',
 ]
 
-# Before `sympy.polys.numberfields` was a package, it was a module, containing
-# what is now defined in the `minpoly`, `isomorphism` and `numbers` modules.
-# To maintain backwards compatibility, we have to import everything that was
-# available in the old `sympy.polys.numberfields` module.
-from .minpoly import (
-    _choose_factor, _separate_sq, _minimal_polynomial_sq,
-    _minpoly_op_algebraic_element, _invertx, _muly, _minpoly_pow,
-    _minpoly_add, _minpoly_mul, _minpoly_sin, _minpoly_cos, _minpoly_tan,
-    _minpoly_exp, _minpoly_rootof, _minpoly_compose,
-    minimal_polynomial, _minpoly_groebner, minpoly, _switch_domain,
-    _linsolve, primitive_element
-)
-from .isomorphism import (
-    is_isomorphism_possible, field_isomorphism_pslq, field_isomorphism_factor,
-    field_isomorphism,
-)
-from .numbers import (
-    to_number_field, IntervalPrinter, isolate
-)
+from .minpoly import minpoly, minimal_polynomial, primitive_element
+
+from .isomorphism import field_isomorphism
+
+from .numbers import to_number_field, isolate
