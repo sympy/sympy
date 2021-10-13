837d238e2776c49c21be79ba511b241a34a4f40a
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-10-13 14:54:34 +0200

    test: add unit test for ArraySymbol
    Pins down existing behaviour so we can safely refactor it

---
+++ sympy/tensor/array/expressions/tests/test_array_symbol.py
@@ -0,0 +1,38 @@
+import pytest
+
+from sympy import Symbol, symbols
+from sympy.tensor.array.expressions.array_expressions import ArrayElement, ArraySymbol
+
+
+class TestArraySymbol:
+    @pytest.mark.parametrize(
+        "shape",
+        [
+            (),
+            (3, 2, 4),
+            symbols("k m n"),
+        ],
+    )
+    def test_constructor(self, shape: tuple):
+        A = ArraySymbol("A", *shape)
+        assert A.name == Symbol("A")
+        assert A.shape == shape
+
+    def test_equality(self):
+        m, n = symbols("m n")
+        assert ArraySymbol("A") == ArraySymbol("A")
+        assert ArraySymbol("A") != ArraySymbol("B")
+        assert ArraySymbol("A", 2, 3) != ArraySymbol("A")
+        assert ArraySymbol("A", m) != ArraySymbol("A", n)
+        assert ArraySymbol("A", n) == ArraySymbol("A", n)
+
+    def test_getitem(self):
+        A = ArraySymbol("A")
+        assert A[9, 7] == ArrayElement(A, indices=(9, 7))
+        A = ArraySymbol("A", 3, 2, 4)
+        assert A[2, 1] == ArrayElement(A, indices=(2, 1))
+        assert A[2, 1, 3] == ArrayElement(A, indices=(2, 1, 3))
+        with pytest.raises(ValueError, match="shape is out of bounds"):
+            A[4, 1, 3]
+        with pytest.raises(ValueError, match="shape contains negative values"):
+            A[0, -1]
