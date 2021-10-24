0c48951f4ca1eaa27760e73949c585b94c5258bf
Chris Smith <smichr@gmail.com>
2021-10-24 03:17:39 -0500

    solvers

--- sympy/solvers/tests/test_recurr.py
+++ sympy/solvers/tests/test_recurr.py
@@ -1,5 +1,13 @@
-from sympy import Eq, factor, factorial, Function, Lambda, rf, S, sqrt, symbols, I, \
-    expand, binomial, Rational, Symbol, cos, sin, Abs
+from sympy.core.function import (Function, Lambda, expand)
+from sympy.core.numbers import (I, Rational)
+from sympy.core.relational import Eq
+from sympy.core.singleton import S
+from sympy.core.symbol import (Symbol, symbols)
+from sympy.functions.combinatorial.factorials import (rf, binomial, factorial)
+from sympy.functions.elementary.complexes import Abs
+from sympy.functions.elementary.miscellaneous import sqrt
+from sympy.functions.elementary.trigonometric import (cos, sin)
+from sympy.polys.polytools import factor
 from sympy.solvers.recurr import rsolve, rsolve_hyper, rsolve_poly, rsolve_ratio
 from sympy.testing.pytest import raises, slow
 from sympy.abc import a, b
