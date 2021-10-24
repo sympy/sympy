84dd8f5cfa7dde7dbb89937ebffa46d2c08fd245
Chris Smith <smichr@gmail.com>
2021-10-24 03:17:01 -0500

    categories

--- sympy/categories/diagram_drawing.py
+++ sympy/categories/diagram_drawing.py
@@ -85,7 +85,7 @@ References
 from sympy.categories import (CompositeMorphism, IdentityMorphism,
                               NamedMorphism, Diagram)
 from sympy.core import Dict, Symbol
-from sympy.printing import latex
+from sympy.printing.latex import latex
 from sympy.sets import FiniteSet
 from sympy.utilities import default_sort_key
 from sympy.utilities.iterables import iterable
