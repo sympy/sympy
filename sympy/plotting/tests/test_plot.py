311a0d167e72a15b1b3114eadd141043b6d49611
Chris Smith <smichr@gmail.com>
2021-10-24 03:17:30 -0500

    plotting

--- sympy/plotting/tests/test_plot.py
+++ sympy/plotting/tests/test_plot.py
@@ -1,9 +1,17 @@
 import os
 from tempfile import TemporaryDirectory
 
-from sympy import (
-    pi, sin, cos, Symbol, Integral, Sum, sqrt, log, exp, Ne, oo, LambertW, I,
-    meijerg, exp_polar, Piecewise, And, real_root)
+from sympy.concrete.summations import Sum
+from sympy.core.numbers import (I, oo, pi)
+from sympy.core.relational import Ne
+from sympy.core.symbol import Symbol
+from sympy.functions.elementary.exponential import (LambertW, exp, exp_polar, log)
+from sympy.functions.elementary.miscellaneous import (real_root, sqrt)
+from sympy.functions.elementary.piecewise import Piecewise
+from sympy.functions.elementary.trigonometric import (cos, sin)
+from sympy.functions.special.hyper import meijerg
+from sympy.integrals.integrals import Integral
+from sympy.logic.boolalg import And
 from sympy.core.singleton import S
 from sympy.core.sympify import sympify
 from sympy.external import import_module
