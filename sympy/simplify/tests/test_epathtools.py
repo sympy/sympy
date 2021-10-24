43d48a2199d3634fc76ad487af91978ed9303baa
Chris Smith <smichr@gmail.com>
2021-10-24 03:17:38 -0500

    simplify

--- sympy/simplify/tests/test_epathtools.py
+++ sympy/simplify/tests/test_epathtools.py
@@ -3,7 +3,8 @@
 from sympy.simplify.epathtools import epath, EPath
 from sympy.testing.pytest import raises
 
-from sympy import sin, cos, E
+from sympy.core.numbers import E
+from sympy.functions.elementary.trigonometric import (cos, sin)
 from sympy.abc import x, y, z, t
 
 
