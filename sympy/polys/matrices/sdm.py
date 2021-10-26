fa50ff0b60319a7030c33593223e4baaacc428ab
Daniel Alcalde <daniel.alcaldepuente@rub.de>
2021-10-26 10:31:05 +0200

    also change for sparse matrices

--- sympy/polys/matrices/sdm.py
+++ sympy/polys/matrices/sdm.py
@@ -915,6 +915,8 @@ def sdm_transpose(M):
     MT = {}
     for i, Mi in M.items():
         for j, Mij in Mi.items():
+            if hasattr(Mij, "transpose"):
+                Mij =  Mij.transpose()
             try:
                 MT[j][i] = Mij
             except KeyError:
