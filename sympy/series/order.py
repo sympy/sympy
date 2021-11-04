c26a9ee06860222f28ca778f455f14c9cfcfc8c1
anutosh491 <87052487+anutosh491@users.noreply.github.com>
2021-11-04 18:07:51 +0530

    Update order.py

--- sympy/series/order.py
+++ sympy/series/order.py
@@ -1,7 +1,7 @@
 from sympy.core import S, sympify, Expr, Dummy, Add, Mul
 from sympy.core.cache import cacheit
 from sympy.core.containers import Tuple
-from sympy.core.function import Function, PoleError, expand_power_base, expand_log
+from sympy.core.function import expand_power_base, expand_log
 from sympy.core.sorting import default_sort_key
 from sympy.sets.sets import Complement
 from sympy.utilities.iterables import uniq, is_sequence
