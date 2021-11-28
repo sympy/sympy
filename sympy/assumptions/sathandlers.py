e05463ebd497074e92d6d9c94fe51369d801356d
Paul Spiering <paul@spiering.org>
2021-11-28 19:12:22 +0100

    basic: use type instead of func for dictionary keys Currently SymPy uses `obj.func` to obtain the classes of expressions to use as keys in dictionaries. This might not be safe to do when `obj.func` does not return `obj.__class__`. Instead, `type(obj)` can be used safely. It is also more efficient.

--- sympy/assumptions/sathandlers.py
+++ sympy/assumptions/sathandlers.py
@@ -182,7 +182,7 @@ class ClassFactRegistry:
     def __call__(self, expr):
         ret = set()
 
-        handlers1, handlers2 = self[expr.func]
+        handlers1, handlers2 = self[type(expr)]
 
         for h in handlers1:
             ret.add(h(expr))
