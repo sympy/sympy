07d54e0c745aa4ac163e17c5811e722ba55311af
ThePauliPrinciple <35560762+ThePauliPrinciple@users.noreply.github.com>
2021-12-03 01:47:14 +0100

    Update sympy/core/evalf.py
    Co-authored-by: Christopher Smith <smichr@gmail.com>

--- sympy/core/evalf.py
+++ sympy/core/evalf.py
@@ -600,8 +600,7 @@ def evalf_add(v: 'Add', prec: int, options: OPT_DICT) -> TMP_RES:
         options['maxprec'] = min(oldmaxprec, 2*prec)
 
         terms = [evalf(arg, prec + 10, options) for arg in v.args]
-        # remove any None so they won't be sympified during comparison
-        n = [term for term in terms if not isinstance(term, tuple)].count(S.ComplexInfinity)
+        n = sum(1 for i in terms if i is S.ComplexInfinity)
         if n >= 2:
             return fnan, None, prec, None
         re, re_acc = add_terms(
