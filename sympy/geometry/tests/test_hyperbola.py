fda28d70b8975bd207b52a89551cef1eaf9f783c
anutosh491 <andersonbhat491@gmail.com>
2021-12-30 11:12:16 +0530

    added basic layout for test_hyperbola.py

---
+++ sympy/geometry/tests/test_hyperbola.py
@@ -0,0 +1,50 @@
+from sympy.core.numbers import Rational
+from sympy.core.symbol import symbols
+from sympy.functions.elementary.miscellaneous import sqrt
+from sympy.geometry.point import Point2D
+from sympy.geometry import (Circle, Hyperbola, Point)
+from sympy.functions.elementary.miscellaneous import Max
+
+
+def test_hyperbola_equation_using_slope():
+    from sympy.abc import x, y
+
+    h1 = Hyperbola(Point(1, 0), 3, 2)
+    assert str(h1.equation(_slope=1)) == str(-(-x + y + 1)**2/8 + (x + y - 1)**2/18 - 1)
+
+    h2 = Hyperbola(Point(0, 0), 4, 1)
+    assert str(h2.equation(_slope=1)) == str(-(-x + y)**2/2 + (x + y)**2/32 - 1)
+
+    h3 = Hyperbola(Point(1, 5), 6, 2)
+    assert str(h3.equation(_slope=2)) == str(-(-2*x + y - 3)**2/20 + (x + 2*y - 11)**2/180 - 1)
+
+
+def test_auxiliary_circle():
+    x, y, a, b = symbols('x y a b')
+    h = Hyperbola((x, y), a, b)
+    # the general result
+    assert h.auxiliary_circle() == Circle((x, y), Max(a, b))
+
+
+def test_director_circle():
+    x, y, a, b = symbols('x y a b')
+    h = Hyperbola((x, y), a, b)
+    # the general result
+    assert h.director_circle() == Circle(Point2D(x, y), sqrt(a**2 - b**2))
+
+
+def test_evolute():
+    #Hyperbola centered at h, k
+    x, y, h, k = symbols('x y h k',real = True)
+    a, b = symbols('a b')
+    h1 = Hyperbola(Point(h, k), a, b)
+    t1 = (h1.hradius*(x - h1.center.x))**Rational(2, 3)
+    t2 = (h1.vradius*(y - h1.center.y))**Rational(2, 3)
+    H = t1 - t2 - (h1.hradius**2 + h1.vradius**2)**Rational(2, 3)
+    assert h1.evolute() == H
+    #Numerical Example
+    h1 = Hyperbola(Point(1, 1), 6, 3)
+    t1 = (6*(x - 1))**Rational(2, 3)
+    t2 = (3*(y - 1))**Rational(2, 3)
+    H = t1 - t2 - (45)**Rational(2, 3)
+    assert h1.evolute() == H
