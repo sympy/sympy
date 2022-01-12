86d13ad378a99a134b271a5b1793a55cb1af478a
Christopher Smith <smichr@gmail.com>
2022-01-12 07:35:53 -0600

    Update sympy/simplify/cse_main.py

--- sympy/simplify/cse_main.py
+++ sympy/simplify/cse_main.py
@@ -590,7 +590,7 @@ def tree_cse(exprs, symbols, opt_subs=None, order='canonical', ignore=()):
         if isinstance(expr, Basic) and (
                 expr.is_Atom or
                 expr.is_Order or
-                isinstance(expr, (MatrixSymbol, MatrixElement)):
+                isinstance(expr, (MatrixSymbol, MatrixElement))):
             if expr.is_Symbol:
                 excluded_symbols.add(expr)
             return
