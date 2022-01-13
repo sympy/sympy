7711fc0ab13e07932622de308d43188ca7d7913f
Jaitej <cjaitej@gmail.com>
2022-01-13 21:54:38 +0530

    changes made

--- sympy/core/sympify.py
+++ sympy/core/sympify.py
@@ -144,8 +144,8 @@ def sympify(a, locals=None, convert_xor=True, strict=False, rational=False,
     ...
     SympifyError: SympifyError: "could not parse '2x' "
 
-    "sympify" function parse the input, if it follows the python-syntax.
-    In order to parse a non python syntax use "parse_exp": 
+    sympify function parse the input if it follows the python-syntax.
+    To parse a non-python syntax use "parse_exp": 
     >>> parse_expr("2x", transformations="all")
     2*x
 
