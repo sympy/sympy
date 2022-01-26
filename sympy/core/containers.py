c9e4d0f4c3f4eee727e2e541c603e07fe33e3b3e
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2022-01-26 22:02:43 +0000

    remove unneeded import

--- sympy/core/containers.py
+++ sympy/core/containers.py
@@ -13,7 +13,7 @@ from typing import Any, Callable
 from .basic import Basic
 from .sorting import default_sort_key, ordered
 from .sympify import _sympify, sympify, _sympy_converter, SympifyError
-from sympy.core.kind import Kind, NumberKind
+from sympy.core.kind import Kind
 from sympy.utilities.iterables import iterable
 from sympy.utilities.misc import as_int
 
