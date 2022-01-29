3396355ec5a9bddb871bd4fddfd25d777bb34d68
Paul Spiering <paul@spiering.org>
2022-01-29 18:54:32 +0100

    test for assignments with indexed

--- sympy/printing/tests/test_pycode.py
+++ sympy/printing/tests/test_pycode.py
@@ -15,7 +15,7 @@ from sympy.printing.pycode import (
 from sympy.printing.tensorflow import TensorflowPrinter
 from sympy.printing.numpy import NumPyPrinter, SciPyPrinter
 from sympy.testing.pytest import raises, skip
-from sympy.tensor import IndexedBase
+from sympy.tensor import IndexedBase, Idx
 from sympy.tensor.array.expressions.array_expressions import ArraySymbol, ArrayDiagonal, ArrayContraction, ZeroArray, OneArray
 from sympy.external import import_module
 from sympy.functions.special.gamma_functions import loggamma
@@ -397,6 +397,7 @@ def test_numerical_accuracy_functions():
 def test_array_printer():
     A = ArraySymbol('A', (4,4,6,6,6))
     I = IndexedBase('I')
+    i,j,k = Idx('i', (0,1)), Idx('j', (2,3)), Idx('k', (4,5))
 
     prntr = NumPyPrinter()
     assert prntr.doprint(ZeroArray(5)) == 'numpy.zeros((5,))'
@@ -406,6 +407,7 @@ def test_array_printer():
     assert prntr.doprint(ArrayDiagonal(A, [2,3,4])) == 'numpy.einsum("abccc->abc", A)'
     assert prntr.doprint(ArrayDiagonal(A, [0,1], [2,3])) == 'numpy.einsum("aabbc->cab", A)'
     assert prntr.doprint(ArrayContraction(A, [2], [3])) == 'numpy.einsum("abcde->abe", A)'
+    assert prntr.doprint(Assignment(I[i,j,k], I[i,j,k])) == 'I = I'
 
     prntr = TensorflowPrinter()
     assert prntr.doprint(ZeroArray(5)) == 'tensorflow.zeros((5,))'
@@ -415,3 +417,4 @@ def test_array_printer():
     assert prntr.doprint(ArrayDiagonal(A, [2,3,4])) == 'tensorflow.linalg.einsum("abccc->abc", A)'
     assert prntr.doprint(ArrayDiagonal(A, [0,1], [2,3])) == 'tensorflow.linalg.einsum("aabbc->cab", A)'
     assert prntr.doprint(ArrayContraction(A, [2], [3])) == 'tensorflow.linalg.einsum("abcde->abe", A)'
+    assert prntr.doprint(Assignment(I[i,j,k], I[i,j,k])) == 'I = I'
