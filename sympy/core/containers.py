017d3c7ca2f952bc2fbfad70bf5e477274e1354a
me-t1me <siddhantashoknagar@gmail.com>
2022-01-30 21:39:42 +0530

    added _helper_kind, _union_setkind

--- sympy/core/containers.py
+++ sympy/core/containers.py
@@ -396,16 +396,16 @@ class TupleKind(Kind):
     >>> from sympy import Tuple
     >>> Tuple(1, 2).kind
     TupleKind(NumberKind, NumberKind)
+    >>> Tuple(1, 2).kind.element_kind
+    (NumberKind, NumberKind)
 
     See Also
     ========
 
     sympy.core.kind.NumberKind
-
     sympy.matrices.common.MatrixKind
-
     sympy.sets.sets.SetKind
-        """
+    """
     def __new__(cls, *args):
         obj = super().__new__(cls, *args)
         obj.element_kind = args
