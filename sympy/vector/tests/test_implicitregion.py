aa8e26f3331940ebb1f45f9f8a6e24d90b6291b1
NikhilSDate <nikhil.s.date@gmail.com>
2022-01-30 16:07:27 +0530

    removed unnecessary import

--- sympy/vector/tests/test_implicitregion.py
+++ sympy/vector/tests/test_implicitregion.py
@@ -1,6 +1,5 @@
 from sympy.core.relational import Eq
 from sympy.core.singleton import S
-from sympy.functions.elementary.miscellaneous import sqrt
 from sympy.abc import x, y, z, s, t
 from sympy.sets import FiniteSet, EmptySet
 from sympy.geometry import Point
