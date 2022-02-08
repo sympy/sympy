3f565f3dc66e2c263b719119eda12835a350e3c8
anutosh491 <andersonbhat491@gmail.com>
2022-02-08 19:59:23 +0530

    Revert "Added tests"
    This reverts commit dbd20246d937a7322651af18e992b66707d921d3.

--- sympy/series/fourier.py
+++ sympy/series/fourier.py
@@ -170,23 +170,25 @@ class FourierSeries(SeriesBase):
     def an(self):
         p = Wild("p")
         q = Wild("q")
+        n = Dummy('n')
         try:
             gen_term = self.cos_sequence.formula.match(p*cos(q))[p]
         except TypeError:
             return self.cos_sequence
         else:
-            return SeqFormula(gen_term, (*self.cos_sequence.variables, self.cos_sequence.start, self.cos_sequence.stop))
+            return SeqFormula(gen_term, (n, 1, oo))
 
     @property
     def bn(self):
         p = Wild("p")
         q = Wild("q")
+        n = Dummy('n')
         try:
             gen_term = self.sin_sequence.formula.match(p*sin(q))[p]
         except TypeError:
             return self.sin_sequence
         else:
-            return SeqFormula(gen_term, (*self.sin_sequence.variables, self.sin_sequence.start, self.cos_sequence.stop))
+            return SeqFormula(gen_term, (n, 1, oo))
 
     @property
     def interval(self):
--- sympy/series/tests/test_fourier.py
+++ sympy/series/tests/test_fourier.py
@@ -78,15 +78,6 @@ def test_FourierSeries_2():
                             4*cos(pi*x / 2) / pi**2 + S.Half)
 
 
-def test_Fourier_coefficients():
-    f = fourier_series(Piecewise((1, x < 0), (x, True)), (x, -1, 1))
-
-    assert f.an[0:5] == [-2/pi**2, 0, -2/(9*pi**2), 0, -2/(25*pi**2)]
-    assert f.bn[0:5] == [-1/pi, -1/(2*pi), -1/(3*pi), -1/(4*pi), -1/(5*pi)]
-    assert f.cos_sequence[0:5] == [-2*cos(pi*x)/pi**2, 0, -2*cos(3*pi*x)/(9*pi**2), 0, -2*cos(5*pi*x)/(25*pi**2)]
-    assert f.sin_sequence[0:5] == [-sin(pi*x)/pi, -sin(2*pi*x)/(2*pi), -sin(3*pi*x)/(3*pi), -sin(4*pi*x)/(4*pi), -sin(5*pi*x)/(5*pi)]
-
-
 def test_square_wave():
     """Test if fourier_series approximates discontinuous function correctly."""
     square_wave = Piecewise((1, x < pi), (-1, True))
