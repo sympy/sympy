9440d443707aa1b24e371c01bd0ee508da279fff
anutosh491 <andersonbhat491@gmail.com>
2022-02-16 17:48:10 +0530

    Fixed failing test

--- sympy/series/tests/test_formal.py
+++ sympy/series/tests/test_formal.py
@@ -283,9 +283,7 @@ def test_fps__hyper():
         x - x**2/2 + x**3/3 - x**4/4 + x**5/5 + O(x**6)
 
     f = airyai(x**2)
-    assert fps(f, x).truncate() == \
-        (3**Rational(5, 6)*gamma(Rational(1, 3))/(6*pi) -
-         3**Rational(2, 3)*x**2/(3*gamma(Rational(1, 3))) + O(x**6))
+    assert str(fps(f, x).truncate()) == '3**(1/3)/(3*gamma(2/3)) - 3**(2/3)*x**2/(3*gamma(1/3)) + O(x**6)'
 
     f = exp(x)*sin(x)
     assert fps(f, x).truncate() == x + x**2 + x**3/3 - x**5/30 + O(x**6)
