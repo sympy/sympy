b21fda59d6022eed694d40a0482d0529ddbf5772
Paul Spiering <paul@spiering.org>
2022-02-17 17:37:18 +0100

    format

--- sympy/physics/quantum/tests/test_grover.py
+++ sympy/physics/quantum/tests/test_grover.py
@@ -10,8 +10,10 @@ from sympy.physics.quantum.grover import (apply_grover, superposition_basis,
 def return_one_on_two(qubits):
     return qubits == IntQubit(2, qubits.nqubits)
 
+
 def return_one_on_one(qubits):
     return qubits == IntQubit(1, nqubits=qubits.nqubits)
+
 
 def test_superposition_basis():
     nbits = 2
