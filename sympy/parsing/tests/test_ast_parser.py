a99a9469b085ac0737f3ce85d4b41698d1ee8895
wermos <63574588+wermos@users.noreply.github.com>
2022-02-21 19:28:51 +0530

    Fixed the incorrrect test for `visit_Num`, and added an explanatory comment.

--- sympy/parsing/tests/test_ast_parser.py
+++ sympy/parsing/tests/test_ast_parser.py
@@ -1,3 +1,4 @@
+from sympy.core.numbers import Rational
 from sympy.core.singleton import S
 from sympy.core.symbol import symbols
 from sympy.parsing.ast_parser import parse_expr
@@ -23,4 +24,10 @@ def test_parse_expr():
     with warnings.catch_warnings():
         warnings.simplefilter('error')
         assert parse_expr('6 * 7', {}) == S(42)
-        assert parse_expr('1 / 2', {}) == S(1)/2
+        # Note: The test below can be removed when support for Python 3.7 is
+        # dropped. This test exists to ensure that the visit_Num function
+        # exists for Python 3.7, because in 3.7, Python didn't use the
+        # visit_Constant to create AST Nodes yet.
+        test_expr = parse_expr('1 / 3', {})
+        assert test_expr == S(1)/3 # sanity check
+        assert isinstance(test_expr, Rational)
