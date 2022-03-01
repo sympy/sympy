da5b7c04d3628921e5f3432d0d406d0f863f5fa5
wermos <63574588+wermos@users.noreply.github.com>
2022-03-01 22:01:44 +0530

    Fixed the bug.

--- sympy/printing/pretty/pretty.py
+++ sympy/printing/pretty/pretty.py
@@ -1147,7 +1147,9 @@ class PrettyPrinter(Printer):
                 if '\N{right parenthesis extension}' in tempstr:   # If scalar is a fraction
                     for paren in range(len(tempstr)):
                         flag[i] = 1
-                        if tempstr[paren] == '\N{right parenthesis extension}':
+                        if tempstr[paren] == '\N{right parenthesis extension}' and tempstr[paren + 1] == '\n':
+                            # We want to place the vector string after all the right parentheses, because
+                            # otherwise, the vector will be in the middle of the string
                             tempstr = tempstr[:paren] + '\N{right parenthesis extension}'\
                                          + ' '  + vectstrs[i] + tempstr[paren + 1:]
                             break
