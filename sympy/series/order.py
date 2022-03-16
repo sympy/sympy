7e161a6d3bc6186f1acdb17ca7e844adfb316472
Anutosh Bhat <87052487+anutosh491@users.noreply.github.com>
2022-03-16 18:37:03 +0530

    updated code for expr.is_Pow check

--- sympy/series/order.py
+++ sympy/series/order.py
@@ -3,6 +3,7 @@ from sympy.core.cache import cacheit
 from sympy.core.containers import Tuple
 from sympy.core.function import Function, PoleError, expand_power_base, expand_log
 from sympy.core.sorting import default_sort_key
+from sympy.functions.elementary.exponential import exp, log
 from sympy.sets.sets import Complement
 from sympy.utilities.iterables import uniq, is_sequence
 
@@ -254,6 +255,10 @@ class Order(Expr):
                                 expr = new_expr.expr
                             elif expr.is_Mul:
                                 expr = Mul(*[a.expr for a in orders])
+                            elif expr.is_Pow:
+                                e = expr.exp
+                                b = expr.base
+                                expr = exp(e * log(b))
                     expr = expr.as_independent(*args, as_Add=False)[1]
 
                     expr = expand_power_base(expr)
