03979c2ec0f68d6d337b8db2678fa775062030e2
faze-geek <bhat.1@iitj.ac.in>
2022-03-19 15:22:03 +0530

    Updated code

--- sympy/series/limits.py
+++ sympy/series/limits.py
@@ -320,20 +320,12 @@ class Limit(Expr):
                 elif ex == 0:
                     return coeff
                 elif ex.is_negative:
-                    if ex.is_integer and isinstance(ex,Mul) is False:
-                        if cdir == 1 or ex.is_even:
-                            return S.Infinity*sign(coeff)
-                        elif cdir == -1:
-                            return S.NegativeInfinity*sign(coeff)
-                        else:
-                            return S.ComplexInfinity
+                    if cdir == 1:
+                        return S.Infinity*sign(coeff)
+                    elif cdir == -1:
+                        return S.Infinity*sign(coeff)*S.NegativeOne**ex
                     else:
-                        if cdir == 1:
-                            return S.Infinity*sign(coeff)
-                        elif cdir == -1:
-                            return S.Infinity*sign(coeff)*S.NegativeOne**ex
-                        else:
-                            return S.ComplexInfinity
+                        return S.ComplexInfinity
 
         # gruntz fails on factorials but works with the gamma function
         # If no factorial term is present, e should remain unchanged.
