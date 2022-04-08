9413e4bb3f94b1748912b52747fb18a1460aceb2
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2022-04-08 12:49:01 -0400

    Update test_dimensionless.py

--- sympy/physics/units/tests/test_dimensionless.py
+++ sympy/physics/units/tests/test_dimensionless.py
@@ -1,16 +1,13 @@
 from sympy.physics.units import Dimension
 from sympy.physics.units import DimensionSystem
-
 """ 
 	We need to define the base quantities globally to work with 
 	and define the derived quantities from there on
 """
-
 M = Dimension('mass')
 L = Dimension('length')
 T = Dimension('time')
 I = Dimension('current')
-
 def test_navier_stokes_case():
 	""" In this case my function will be able to generate one set
 	of dimensionless quantities in Navier-Stokes equations ......"""
@@ -28,19 +25,16 @@ def test_navier_stokes_case():
 		'pc' : p_c,
 		'Fbc' : F_bc
 	}
-	set_of_dimless_quant, set_of_dimless_nums =
-					DimensionSystem.get_dimensionless_numbers(list_of_quantities)
+	set_of_dimless_quant, set_of_dimless_nums = DimensionSystem.get_dimensionless_numbers(list_of_quantities)
 	flag = True
 	for dimension in set_of_dimless_nums:
 		flag = flag and dimension == Dimension(1)
 	try:
 		assert flag
-		print('\n Set of dimensionless quantities with Navier-Stokes equations '+\
-			'-> \n')
+		print('\n Set of dimensionless quantities with Navier-Stokes equations -> \n')
 		for element in set_of_dimless_quant: print(element)
 	except Exception as exception: print(exception)
-	""" You should be able to see the Reynolds number, Euler's
-	number and Froude number in the output """
+	""" You should be able to see the Reynolds number, Euler's number and Froude number in the output """
 def test_mhd_case():
 	""" In this case my function will be able to generate one set of
 	dimensionless quantities in equations governing magnetohydrodynamics"""
@@ -60,8 +54,7 @@ def test_mhd_case():
 		'sigma_0' : sigma_0,
 		'B0' : B0
 	}
-	set_of_dimless_quant, set_of_dimless_nums =
-					DimensionSystem.get_dimensionless_numbers(list_of_quantities)
+	set_of_dimless_quant, set_of_dimless_nums = DimensionSystem.get_dimensionless_numbers(list_of_quantities)
 	flag = True
 	for dimension in set_of_dimless_nums:
 		flag = flag and dimension == Dimension(1)
@@ -70,8 +63,7 @@ def test_mhd_case():
 		print('\n Set of dimensionless quantities with MHD equations -> \n')
 		for element in set_of_dimless_quant: print(element)
 	except Exception as exception: print(exception)
-	""" You should be able to see the Reynolds number and the other two numbers
-		contain information of the Hartmann number, Chandrasekhar number """
+	""" You should be able to see the Reynolds number and the other two numbers contain information of the Hartmann number, Chandrasekhar number """
 def test_qed_case():
 	""" In this case my function will be able to generate one set
 	of dimensionless quantities in circuit quantum electrodynamics ......"""
@@ -89,8 +81,7 @@ def test_qed_case():
 		'Vm' : Vm,
 		'Qe' : Qe
 	}
-	set_of_dimless_quant, set_of_dimless_nums =
-					DimensionSystem.get_dimensionless_numbers(list_of_quantities)
+	set_of_dimless_quant, set_of_dimless_nums = DimensionSystem.get_dimensionless_numbers(list_of_quantities)
 	flag = True
 	for dimension in set_of_dimless_nums:
 		flag = flag and dimension == Dimension(1)
@@ -99,5 +90,4 @@ def test_qed_case():
 		print('\n Set of dimensionless quantities in circuit QED -> \n')
 		for element in set_of_dimless_quant: print(element)
 	except Exception as exception: print(exception)
-	""" One of the values in the output shows the
-	 fine-structure interaction """
+	""" One of the values in the output shows the fine-structure interaction """
