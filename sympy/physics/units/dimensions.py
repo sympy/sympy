ace1a9e0f3a3851e35e936963205d2fc4651d2f8
Yashvardhan Prasad <78342519+yasphy@users.noreply.github.com>
2022-04-11 23:16:03 +0530

    Update dimensions.py

--- sympy/physics/units/dimensions.py
+++ sympy/physics/units/dimensions.py
@@ -492,7 +492,7 @@ class DimensionSystem(Basic, _QuantityMapper):
 
         #TODO: the inversion will fail if the system is inconsistent, for
         #      example if the matrix is not a square
- 
+
         with warns_deprecated_sympy():
             sympy_deprecation_warning("use base and derive dims instead",
                  deprecated_since_version="1.10",
@@ -507,7 +507,7 @@ class DimensionSystem(Basic, _QuantityMapper):
         DO NOT USE.
         Dimensional representation in terms of the canonical base dimensions.
         """
-        
+
         with warns_deprecated_sympy():
             sympy_deprecation_warning("use base and derive dims instead",
                  deprecated_since_version="1.10",
@@ -545,7 +545,7 @@ class DimensionSystem(Basic, _QuantityMapper):
         Give the dimension of the system.
         That is return the number of dimensions forming the basis.
         """
-        
+ 
         with warns_deprecated_sympy():
             sympy_deprecation_warning("use base and derive dims instead",
                  deprecated_since_version="1.10",
