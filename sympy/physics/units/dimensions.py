938e5dbdf47b88221952d455daa8cf3fba6e9dc5
Yashvardhan Prasad <78342519+yasphy@users.noreply.github.com>
2022-04-11 22:47:03 +0530

    Update dimensions.py

--- sympy/physics/units/dimensions.py
+++ sympy/physics/units/dimensions.py
@@ -455,7 +455,7 @@ class DimensionSystem(Basic, _QuantityMapper):
         dimset = set()
         for i in self.base_dims:
             dimset.update(set(self.get_dimensional_dependencies(i).keys()))
-        return tuple(sorted(dimset, key=str))
+        return tuple(sorted(dimset, key=str)) 
 
     @property 
     def inv_can_transf_matrix(self):
