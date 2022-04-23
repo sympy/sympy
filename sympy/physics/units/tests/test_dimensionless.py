4cc5b2fb63ec3c71093fe7835f36a484458ba2ec
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2022-04-23 08:23:29 -0400

    Update test_dimensionless.py

--- sympy/physics/units/tests/test_dimensionless.py
+++ sympy/physics/units/tests/test_dimensionless.py
@@ -4,12 +4,15 @@ from sympy.physics.units import DimensionSystem
 We need to define the base quantities globally to work with and
 define the derived quantities from there on
 """
+
 M = Dimension('mass')
 L = Dimension('length')
 T = Dimension('time')
 I = Dimension('current')
-dummy_system = DimensionSystem((L, M, T, I))
-def test_navier_stokes_case():
+
+dummy_si_system = DimensionSystem((L, M, T, I))
+
+def test_navier_stokes_case_si():
     """ In this case my function will be able to generate one set of
     dimensionless quantities in Navier-Stokes equations ......"""
     Lc = L # Characteristic length scale
@@ -18,14 +21,15 @@ def test_navier_stokes_case():
     mu = M/L/T # Dynamic viscosity
     p_c = M/L/T**2 # Characteristic pressure scale
     F_bc = M*L/T**2 # Characteristic body forces scale
-    list_of_quantities = {'Lc': Lc, 'Uc' : Uc, 'rho' : rho_c, 'mu' : mu, 'pc' : p_c, 'Fbc' : F_bc}
-    set_of_dimless_quant, set_of_dimless_nums = dummy_system.get_dimensionless_numbers(list_of_quantities)
+    list_of_quantities = {'Lc': Lc, 'Uc': Uc, 'rho': rho_c, 'mu': mu, 'pc': p_c, 'Fbc': F_bc}
+    set_of_dimless_nums = dummy_si_system.get_dimensionless_numbers(list_of_quantities)
     flag = True
     for dimension in set_of_dimless_nums:
-        flag = flag and dimension == Dimension(1)
+        flag = flag and dummy_si_system.get_dimensional_dependencies(dimension) == {}
     assert flag
-    """ You should be able to see the Reynolds number, Euler's number and Froude number in the output """
-def test_mhd_case():
+    # You should be able to see the Reynolds number, Euler's number and Froude number in the output
+
+def test_mhd_case_si():
     """ In this case my function will be able to generate one set of
     dimensionless quantities in equations governing magnetohydrodynamics"""
     Lc = L # Characteristic length scale
@@ -35,14 +39,15 @@ def test_mhd_case():
     j0 = I/L**2 # Characteristic range value of current density
     sigma_0 = M*L**3*T**(-3)*I**(-2) # Characteristic conductivity scale
     B0 = M*T**(-2)*I**(-1) # Magnetic field characteristic scale
-    list_of_quantities = {'Lc': Lc, 'Uc' : Uc, 'rho' : rho_c, 'mu' : mu, 'j0' : j0, 'sigma_0' : sigma_0, 'B0' : B0}
-    set_of_dimless_quant, set_of_dimless_nums = dummy_system.get_dimensionless_numbers(list_of_quantities)
+    list_of_quantities = {'Lc': Lc, 'Uc': Uc, 'rho': rho_c, 'mu': mu, 'j0': j0, 'sigma_0': sigma_0, 'B0': B0}
+    set_of_dimless_nums = dummy_si_system.get_dimensionless_numbers(list_of_quantities)
     flag = True
     for dimension in set_of_dimless_nums:
-        flag = flag and dimension == Dimension(1)
+        flag = flag and dummy_si_system.get_dimensional_dependencies(dimension) == {}
     assert flag
-    """ You should be able to see the Reynolds number and the other two numbers contain information of the Hartmann number, Chandrasekhar number """
-def test_qed_case():
+    # You should be able to see the Reynolds number and the other two numbers contain information of the Hartmann number, Chandrasekhar number
+
+def test_qed_case_si():
     """ In this case my function will be able to generate one set
     of dimensionless quantities in circuit quantum electrodynamics ......"""
     hbar = M*L**2/T # Dimensions of the reduced Planck's constant
@@ -52,12 +57,12 @@ def test_qed_case():
     Qe = I*T # Dimensions of the electronic charge
     Uc = L/T # Velocity dimensions
     list_of_quantities = {'hbar': hbar, 'Uc' : Uc, 'omega' : omega, 'epsilon' : epsilon, 'Vm' : Vm, 'Qe' : Qe}
-    set_of_dimless_quant, set_of_dimless_nums = dummy_system.get_dimensionless_numbers(list_of_quantities)
+    set_of_dimless_nums = dummy_si_system.get_dimensionless_numbers(list_of_quantities)
     flag = True
     for dimension in set_of_dimless_nums:
-        flag = flag and dimension == Dimension(1)
+        flag = flag and dummy_si_system.get_dimensional_dependencies(dimension) == {}
     assert flag
-    """ One of the values in the output shows the fine-structure interaction """
+    # One of the values in the output shows the fine-structure interaction
 
 M = Dimension('mass')
 L = Dimension('length')
@@ -65,6 +70,7 @@ T = Dimension('time')
 dummy_gauss_system = DimensionSystem((L, M, T))
 
 def test_qed_case_gauss():
+    """ Testing circuit QED case with Gaussian units """
     hbar = M*L**2/T # Dimensions of the reduced Planck's constant
     omega = 1/T # Characteristic angular resonant frequency
     epsilon = 1 # Vaccuum permittivity is unitless in Gaussian (not in SI)
@@ -72,11 +78,12 @@ def test_qed_case_gauss():
     Qe = L**(3/2)*M**(1/2)/T # Dimensions of the electronic charge (in Gaussian)
     Uc = L/T # Velocity dimensions
     list_of_quantities = {'hbar': hbar, 'Uc' : Uc, 'omega' : omega, 'epsilon' : epsilon, 'Vm' : Vm, 'Qe' : Qe}
-    set_of_dimless_quant, set_of_dimless_nums = dummy_gauss_system.get_dimensionless_numbers(list_of_quantities)
+    set_of_dimless_nums = dummy_gauss_system.get_dimensionless_numbers(list_of_quantities)
     flag = True
     for dimension in set_of_dimless_nums:
-        flag = flag and dimension == Dimension(1)
+        flag = flag and dummy_gauss_system.get_dimensional_dependencies(dimension) == {}
     assert flag
+    # One of the values in the output shows the fine-structure interaction
 
 def test_mhd_case_gauss():
     """ The Gaussian system of units are different but dimensionally I am
@@ -85,12 +92,13 @@ def test_mhd_case_gauss():
     Uc = L/T # Characteristic velocity scale
     rho_c = M/L**3 # Characteristic density scale
     mu = M/L/T # Dynamic viscosity
-    j0 = M**(1/2)/L**(1/2)/T**2 # Characteristic range value of current density (in Gaussian)
-    sigma_0 = T**(-1) # Characteristic conductivity scale (in Gaussian)
-    B0 = L**(-1/2)*M**(1/2)/T # Magnetic field characteristic scale (in Gaussian)
+    j0 = M**(1/2)/L**(1/2)/T**2 # Characteristic range value of current density
+    sigma_0 = T**(-1) # Characteristic conductivity scale
+    B0 = L**(-1/2)*M**(1/2)/T # Magnetic field characteristic scale
     list_of_quantities = {'Lc': Lc, 'Uc' : Uc, 'rho': rho_c, 'mu': mu, 'j0': j0, 'sigma_0': sigma_0, 'B0': B0}
-    set_of_dimless_quant, set_of_dimless_nums = dummy_gauss_system.get_dimensionless_numbers(list_of_quantities)
+    set_of_dimless_nums = dummy_gauss_system.get_dimensionless_numbers(list_of_quantities)
     flag = True
     for dimension in set_of_dimless_nums:
-        flag = flag and dimension == Dimension(1)
+        flag = flag and dummy_gauss_system.get_dimensional_dependencies(dimension) == {}
     assert flag
+    # You should be able to see the Reynolds number and the other two numbers contain information of the Hartmann number, Chandrasekhar number
