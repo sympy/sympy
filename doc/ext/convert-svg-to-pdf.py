63f3b3809b672d624338884cb099ea5039830ca8
me-t1me <siddhantashoknagar@gmail.com>
2022-05-26 13:42:39 +0530

    added chromium options

--- doc/ext/convert-svg-to-pdf.py
+++ doc/ext/convert-svg-to-pdf.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 """
     Converts SVG images to PDF using chrome in case the builder does not
     support SVG images natively (e.g. LaTeX).
@@ -14,11 +14,8 @@ from errno import ENOENT, EPIPE, EINVAL
 import os
 import platform
 import tempfile
-
-if False:
-    # For type annotation
-    from typing import Any, Dict  # NOQA
-    from sphinx.application import Sphinx  # NOQA
+from typing import Any  # NOQA
+from sphinx.application import Sphinx  # NOQA
 
 
 logger = logging.getLogger(__name__)
@@ -29,13 +26,11 @@ class Converter(ImageConverter):
         ('image/svg+xml', 'application/pdf'),
     ]
 
-    def is_available(self):
-        # type: () -> bool
+    def is_available(self) -> bool:
         """Confirms if converter is available or not."""
         return True
 
-    def chrome_command(self):
-         # type: () -> str
+    def chrome_command(self) -> str:
         if platform.win32_ver()[0]:
             return os.path.join(os.environ['PROGRAMW6432'],
              "Google\Chrome\Application\chrome.exe")
@@ -44,28 +39,45 @@ class Converter(ImageConverter):
         elif platform.libc_ver()[0]:
             return "google-chrome"
         else:
-            logger.warning("Not able to find suitable chrome command for this os")
+            logger.error("Not able to find suitable chromium or chrome command for this os")
 
-    def convert(self, _from, _to):
-        # type: (unicode, unicode) -> bool
+    def chromium_command(self) -> str:
+        if platform.win32_ver()[0]:
+            return os.path.join(os.environ['PROGRAMW6432'],
+             "chrome-win/chrome.exe")
+        elif platform.mac_ver()[0]:
+            return "/Applications/Chromium.app/Contents/MacOS/Chromium"
+        elif platform.libc_ver()[0]:
+            return "chromium-browser"
+
+    def command_runner(self, chrome, _to, temp_name):
+        args = [chrome, '--headless', '--disable-gpu', '--disable-software-rasterizer', '--print-to-pdf=' + _to, temp_name]
+        logger.debug('Invoking %r ...', args)
+        p = subprocess.run(
+            args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        return p
+
+    def convert(self, _from: str, _to: str) -> bool:
         """Converts the image from SVG to PDF using chrome."""
 
         HTML = "<html ><head ><style >body {margin: 0; }</style ><script >function init() {const element = document.getElementById('targetsvg');const positionInfo = element.getBoundingClientRect();const height = positionInfo.height;const width = positionInfo.width;const style = document.createElement('style');style.innerHTML = `@page {margin: 0; size: ${width}px ${height}px}`;document.head.appendChild(style); }window.onload = init;</script ></head><body><img id=\"targetsvg\" src=\"%s\"></body></html>" % (_from)
-        chrome = self.chrome_command()
         try:
             temp = tempfile.NamedTemporaryFile(suffix=".html", mode='w', delete=False)
             with open(temp.name, 'w+') as f:
                 f.write(HTML)
                 f.seek(0)
-            args = [chrome, '--headless', '--disable-gpu', '--disable-software-rasterizer', '--print-to-pdf=' + _to, temp.name]
-            logger.debug('Invoking %r ...', args)
-            p = subprocess.run(
-                args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            chromium = self.chromium_command()
+            p = self.command_runner(chromium, _to, temp.name)
+
         except OSError as err:
             if err.errno != ENOENT:  # No such file or directory
                 raise
-            logger.warning('converter command cannot be run. chrome is not a internal or external command')
-            return False
+            try:
+                chrome = self.chrome_command()
+                p = self.command_runner(chrome, _to, temp.name)
+            except OSError as err:
+                logger.error('converter command cannot be run. chrome is not a internal or external command')
+                return False
 
         try:
             stdout, stderr = p.stdout, p.stderr
@@ -83,8 +95,7 @@ class Converter(ImageConverter):
         return True
 
 
-def setup(app):
-    # type: (Sphinx) -> Dict[unicode, Any]
+def setup(app: Sphinx) -> dict[str, Any]:
     app.add_post_transform(Converter)
 
     return {
