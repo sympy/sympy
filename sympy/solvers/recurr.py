diofant/diofant

rsolve_hyper: take into account degenerate solutions
This fixes sympy/sympy#8697:

    In [2]: rsolve(a(n + 3) - a(n + 2) - a(n + 1) + a(n), a(n))
    Out[2]:
        n
    (-1) ⋅C₁ + C₀ + C₂⋅n

Added also test from issue thread, which is not related
to the problem.  And from PR request diofant/diofant#442.

Test for sympy/sympy#6844 was adapted.

diofant/solvers/recurr.py
@@ -621,11 +621,12 @@ def rsolve_hyper(coeffs, f, n, **hints):
             if z.is_zero:
                 continue
 
-            (C, s) = rsolve_poly([ polys[i]*z**i for i in range(r + 1) ], 0, n, symbols=True)
+            sol, syms = rsolve_poly([polys[i]*z**i for i in range(r + 1)],
+                                     0, n, symbols=True)
+            sol = sol.collect(syms)
+            sol = [sol.coeff(_) for _ in syms]
 
-            if C is not None and C is not S.Zero:
-                symbols |= set(s)
-
+            for C in sol:
                 ratio = z * A * C.subs(n, n + 1) / B / C
                 ratio = simplify(ratio)
 

sympy/sympy

sympy/solvers/recurr.py
@@ -622,14 +622,13 @@ def rsolve_hyper(coeffs, f, n, **hints):
             if d == 0 and 0 != Add(*[recurr_coeffs[j]*j for j in range(1, r + 1)]):
                 # faster inline check (than calling rsolve_poly) for a
                 # constant solution to a constant coefficient recurrence.
-                C = Symbol("C" + str(len(symbols)))
-                s = [C]
+                sol = [Symbol("C" + str(len(symbols)))]
             else:
-                C, s = rsolve_poly(recurr_coeffs, 0, n, len(symbols), symbols=True)
+                sol, syms = rsolve_poly(recurr_coeffs, 0, n, len(symbols), symbols=True)
+                sol = sol.collect(syms)
+                sol = [sol.coeff(s) for s in syms]
 
-            if C is not None and C is not S.Zero:
-                symbols |= set(s)
-
+            for C in sol:
                 ratio = z * A * C.subs(n, n + 1) / B / C
                 ratio = simplify(ratio)
                 # If there is a nonnegative root in the denominator of the ratio,