584be62e1d90567827772854a55625725bb43957
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-06-06 08:53:49 +0200

    add benchmarks for evaluation of float arguments to functions

---
+++ sympy/functions/elementary/benchmarks/bench_trigoniometric.py
@@ -0,0 +1,9 @@
+from random import random
+from sympy.functions.elementary.trigonometric import atan, cos
+
+
+def timeit_atan_evalulate_float():
+    atan(random())
+
+def timeit_cos_evalulate_float():
+    cos(random())
--- sympy/functions/special/benchmarks/bench_special.py
+++ sympy/functions/special/benchmarks/bench_special.py
@@ -1,8 +1,13 @@
 from sympy.core.symbol import symbols
 from sympy.functions.special.spherical_harmonics import Ynm
+from sympy.functions.special.beta_functions import beta
 
 x, y = symbols('x,y')
 
 
 def timeit_Ynm_xy():
     Ynm(1, 1, x, y)
+
+def timeit_beta_evalulate_float():
+    beta(3.1)
+    beta(3.1, 2.1)
