7cfa553ac811fc7c7554ab482970d634055cc456
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-06-06 09:24:08 +0200

    add test for atan of sqrt3

--- sympy/functions/elementary/benchmarks/bench_trigoniometric.py
+++ sympy/functions/elementary/benchmarks/bench_trigoniometric.py
@@ -1,9 +1,16 @@
 from random import random
 from sympy.functions.elementary.trigonometric import atan, cos
+from sympy.functions.elementary.miscellaneous import sqrt
+
+sqrt3 = sqrt(3)
+
+def timeit_atan_evaluate_sqrt_3():
+    atan(sqrt3)
 
 
 def timeit_atan_evaluate_float():
     atan(random())
+
 
 def timeit_cos_evaluate_float():
     cos(random())
