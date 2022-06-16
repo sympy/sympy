f8343fd2ef727e2bc1dc11b88c92970e525052a2
Sangyub Lee <sylee957@gmail.com>
2022-06-16 18:11:17 +0900

    Move __new__ to __init__

--- sympy/polys/matrices/domainscalar.py
+++ sympy/polys/matrices/domainscalar.py
@@ -19,19 +19,18 @@ class DomainScalar:
     docstring
     """
 
-    def __new__(cls, element, domain):
+    def __init__(self, element, domain):
         if not isinstance(domain, Domain):
             raise TypeError("domain should be of type Domain")
         if not domain.of_type(element):
             raise TypeError("element %s should be in domain %s" % (element, domain))
-        return cls.new(element, domain)
+
+        self.element = element
+        self.domain = domain
 
     @classmethod
     def new(cls, element, domain):
-        obj = super().__new__(cls)
-        obj.element = element
-        obj.domain = domain
-        return obj
+        return DomainScalar(element, domain)
 
     def __repr__(self):
         return repr(self.element)
