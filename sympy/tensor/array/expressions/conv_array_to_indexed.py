103ab262169e84bec7e0cc022a0b4dee6f2defbe
Francesco Bonazzi <franz.bonazzi@gmail.com>
2022-06-21 23:56:28 +0200

    using SymPy's deprecated

--- sympy/tensor/array/expressions/conv_array_to_indexed.py
+++ sympy/tensor/array/expressions/conv_array_to_indexed.py
@@ -1,23 +1,12 @@
-from functools import wraps
-
 from sympy.tensor.array.expressions import from_array_to_indexed
-from sympy.utilities.exceptions import sympy_deprecation_warning
+from sympy.utilities.decorator import deprecated
 
 
-def _conv_to_from_warning():
-    sympy_deprecation_warning(
-        "module has been renamed by replacing 'conv_' with 'from_' in its name",
-        deprecated_since_version="1.11",
-        active_deprecations_target="deprecated-conv-array-expr-module-names",
-    )
-
-
-def _conv_to_from_decorator(f):
-    @wraps(f)
-    def new_f(*args, **kwargs):
-        _conv_to_from_warning()
-        return f(*args, **kwargs)
-    return new_f
+_conv_to_from_decorator = deprecated(
+    "module has been renamed by replacing 'conv_' with 'from_' in its name",
+    deprecated_since_version="1.11",
+    active_deprecations_target="deprecated-conv-array-expr-module-names",
+)
 
 
 convert_array_to_indexed = _conv_to_from_decorator(from_array_to_indexed.convert_array_to_indexed)
