3878c5422a85b6671977fcfdd92f25a896857a4a
Christopher Smith <smichr@gmail.com>
2022-06-28 10:37:33 -0500

    Update sympy/solvers/ode/riccati.py

--- sympy/solvers/ode/riccati.py
+++ sympy/solvers/ode/riccati.py
@@ -664,13 +664,11 @@ def compute_m_ybar(x, poles, choice, N):
     # Calculate the first (nested) summation for ybar
     # as given in Step 9 of the Thesis (Pg 82)
     dybar = []
-    dm = []
     for i, polei in enumerate(poles):
         for j, cij in enumerate(choice[i]):
             dybar.append(cij/(x - polei)**(j + 1))
-        dm.append(Poly(choice[i][0], x, extension=True))
+        m -=Poly(choice[i][0], x, extension=True)  # can't accumulate Poly and use with Add
     ybar += Add(*dybar)
-    m -= Add(*dm)
 
     # Calculate the second summation for ybar
     for i in range(N+1):
