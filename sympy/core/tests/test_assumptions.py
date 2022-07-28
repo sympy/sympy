06ba8358d34d383bca561376f7850cf4f421124d
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-07-28 22:15:41 +0200

    fix tests

--- sympy/core/tests/test_assumptions.py
+++ sympy/core/tests/test_assumptions.py
@@ -7,7 +7,7 @@ from sympy.functions.elementary.trigonometric import (asin, sin)
 from sympy.simplify.simplify import simplify
 from sympy.core import Symbol, S, Rational, Integer, Dummy, Wild, Pow
 from sympy.core.assumptions import (assumptions, check_assumptions,
-    failing_assumptions, common_assumptions, _generate_assumption_rules, 
+    failing_assumptions, common_assumptions, _generate_assumption_rules,
     _load_pre_generated_assumption_rules)
 from sympy.core.facts import InconsistentAssumptions
 
@@ -1300,4 +1300,4 @@ def test_pre_generated_assumption_rules_are_valid():
     # see sympy.core.assumptions._generate_assumption_rules
     pre_generated_assumptions =_load_pre_generated_assumption_rules()
     generated_assumptions =_generate_assumption_rules()
-    assert pre_generated_assumptions.to_python() == generated_assumptions.to_python(), "pre-generated assumptions are invalid, see sympy.core.assumptions._generate_assumption_rules"
+    assert pre_generated_assumptions._to_python() == generated_assumptions._to_python(), "pre-generated assumptions are invalid, see sympy.core.assumptions._generate_assumption_rules"
