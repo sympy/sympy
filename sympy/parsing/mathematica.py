e59d2a9788a0572c7a610d2cefd4e95440763793
Yves Tumushimire <yvestumushimire@gmail.com>
2022-10-01 20:25:29 +0200

    fix #24055 Cannot parse Greek characters (and possibly others) in parse_mathematica

--- sympy/parsing/mathematica.py
+++ sympy/parsing/mathematica.py
@@ -654,7 +654,7 @@ class MathematicaParser:
             code_splits[i] = code_split
 
         # Tokenize the input strings with a regular expression:
-        token_lists = [tokenizer.findall(i) if isinstance(i, str) else [i] for i in code_splits]
+        token_lists = [tokenizer.findall(i) if isinstance(i, str) and i.isascii() else [i] for i in code_splits]
         tokens = [j for i in token_lists for j in i]
 
         # Remove newlines at the beginning
@@ -663,7 +663,7 @@ class MathematicaParser:
         # Remove newlines at the end
         while tokens and tokens[-1] == "\n":
             tokens.pop(-1)
-
+            
         return tokens
 
     def _is_op(self, token: tUnion[str, list]) -> bool:
