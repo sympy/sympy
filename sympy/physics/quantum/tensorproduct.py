bb7b3b26c097bea0aeb4fcaff216a6fa8b75671c
Costor <pcs2009@web.de>
2022-10-23 15:35:59 +0200

    Update sympy/physics/quantum/tensorproduct.py
    Co-authored-by: (Lazard) S.Y. Lee  <sylee957@gmail.com>

--- sympy/physics/quantum/tensorproduct.py
+++ sympy/physics/quantum/tensorproduct.py
@@ -248,7 +248,7 @@ class TensorProduct(Expr):
                     tp = TensorProduct(*args[:i] + (aa,) + args[i + 1:])
                     c_part, nc_part = tp.args_cnc()
                     # Check for TensorProduct object: is the one object in nc_part, if any:
-                    #(Note: any other object type to be expanded must be added here)
+                    # (Note: any other object type to be expanded must be added here)
                     if len(nc_part)==1 and isinstance(nc_part[0], TensorProduct):
                         nc_part = (nc_part[0]._eval_expand_tensorproduct(), )
                     add_args.append(Mul(*c_part)*Mul(*nc_part))
