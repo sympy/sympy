9eaaa7d475b21312595c53c15a35fc8982746dcf
Ishan Pandhare <ishan9096137017@gmail.com>
2022-11-05 01:44:21 +0530

    added tests for the proposed fix

--- sympy/functions/elementary/tests/test_trigonometric.py
+++ sympy/functions/elementary/tests/test_trigonometric.py
@@ -388,7 +388,9 @@ def test_cos():
 
     assert cos(k*pi) == (-1)**k
     assert cos(2*k*pi) == 1
-
+    assert cos(0, evaluate=False).simplify() == 1
+    assert cos(Rational(1, 2)).is_zero is False
+    assert cos(asin(-1, evaluate=False), evaluate=False).simplify() == 0
     for d in list(range(1, 22)) + [60, 85]:
         for n in range(2*d + 1):
             x = n*pi/d
