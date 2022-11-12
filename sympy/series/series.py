63b2d89225336bf5a55196589ffaaf03e052432d
Maciej Skorski <31315784+maciejskorski@users.noreply.github.com>
2022-11-12 17:21:32 +0100

    Update sympy/series/series.py
    Co-authored-by: (Lazard) S.Y. Lee  <sylee957@gmail.com>

--- sympy/series/series.py
+++ sympy/series/series.py
@@ -121,6 +121,7 @@ def inversion_series(f, x, a=0, n=3):
     x + (x - 1)**3/3 - (x - 1)**2/2 - 1
 
     Functions with vanishing derivative cannot be inverted with this method
+
     >>> f = x**2
     >>> try:
     ...    inversion_series(f,x,0,2)
