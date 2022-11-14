26f3801cd898765a7025d15527bab358724c23bf
maciejskorski <maciej.skorski@gmail.com>
2022-11-14 17:40:06 +0000

    ascii coding

--- sympy/polys/ring_series.py
+++ sympy/polys/ring_series.py
@@ -643,7 +643,7 @@ def rs_fast_series_reversion(p,x,n):
     References
     ==========
 
-    .. [1] Brent, Richard P., and Hsiang T. Kung. "Fast algorithms for manipulating formal power series.â Journal of the ACM (JACM) 25, no. 4 (1978): 581-595.
+    .. [1] Brent, Richard P., and Hsiang T. Kung. "Fast algorithms for manipulating formal power series." Journal of the ACM (JACM) 25, no. 4 (1978): 581-595.
     .. [2] Weisstein, Eric W. "Series Reversion." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/SeriesReversion.html
     """
 
