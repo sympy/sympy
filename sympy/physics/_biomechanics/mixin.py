b9db7ce7e3174da6f0dc18c577298bd80964127e
Sam Brockie <sambrockie@icloud.com>
2022-11-15 10:22:01 +0100

    Create mixin module with _NamedMixin mixin class

---
+++ sympy/physics/_biomechanics/mixin.py
@@ -0,0 +1,40 @@
+"""Mixin classes for sharing functionality between unrelated classes."""
+
+
+__all__ = ['_NamedMixin']
+
+
+class _NamedMixin:
+    """Mixin class for adding `name` properties.
+
+    Valid names, as will typically be used by subclasses as a suffix when naming
+    automatically-instantiated symbol attributes, must be nonzero length
+    strings.
+
+    Attributes
+    ==========
+    name : str
+        The name identifier associated with the instance.
+
+    """
+
+    @property
+    def name(self) -> str:
+        """The name associated with the class instance."""
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        if not isinstance(name, str):
+            msg = (
+                f'Name {repr(name)} passed to `name` was of type '
+                f'{type(name)}, must be {str}.'
+            )
+            raise TypeError(msg)
+        if name in {''}:
+            msg = (
+                f'Name {repr(name)} is invalid, must be a nonzero length '
+                f'{type(str)}.'
+            )
+            raise ValueError(msg)
+        self._name = name
