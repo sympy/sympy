0a784545270394e7f8c2fbfcb522f8ffda472492
Vaani <vaanipathariya@gmail.com>
2022-11-26 00:16:11 +0530

    adding newlines

--- sympy/physics/units/tests/test_unit_system_cgs_gauss.py
+++ sympy/physics/units/tests/test_unit_system_cgs_gauss.py
@@ -46,9 +46,14 @@ def test_cgs_gauss_convert_constants():
     assert convert_to(elementary_charge, statcoulomb, cgs_gauss)
     assert convert_to(gravitational_constant, dyne*centimeter**2/gram**2, cgs_gauss)
     assert NS(convert_to(planck, erg*second, cgs_gauss)) == '6.62607015e-27*erg*second'
+
+
 def test_cgs_gauss_ohm_cgs():
+
     assert convert_to(ohm,second/centimeter,cgs_gauss) == 25000*second/(22468879468420441*centimeter)
     assert convert_to(1*ohm,second/centimeter,cgs_gauss) == 25000*second/(22468879468420441*centimeter)
     assert convert_to(2*ohm,second/centimeter,cgs_gauss) == 50000*second/(22468879468420441*centimeter)
     assert NS(convert_to(ohm,second/centimeter,cgs_gauss)) == '1.11265005605362e-12*second/centimeter'
     assert NS(convert_to(2*ohm,second/centimeter,cgs_gauss)) == '2.22530011210724e-12*second/centimeter'
+
+    
