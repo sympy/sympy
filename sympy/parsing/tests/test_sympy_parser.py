cee998ff856a807e56a9b4baa76e561ad99b96ad
1e9abhi1e10 <1e9abhi1e10@gmail.com>
2022-11-28 15:09:12 +0530

    fix #24288

--- sympy/parsing/tests/test_sympy_parser.py
+++ sympy/parsing/tests/test_sympy_parser.py
@@ -7,7 +7,7 @@ import types
 
 from sympy.assumptions import Q
 from sympy.core import Symbol, Function, Float, Rational, Integer, I, Mul, Pow, Eq
-from sympy.core.relational import Eq, Lt, Le, Gt, Ge, Ne
+from sympy.core.relational import  Lt, Le, Gt, Ge, Ne
 from sympy.functions import exp, factorial, factorial2, sin, Min, Max
 from sympy.logic import And
 from sympy.series import Limit
