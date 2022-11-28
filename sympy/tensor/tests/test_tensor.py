b785e443f06bf31ab193cafda22576d4c4ab0a76
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-11-28 14:10:08 +0530

    test_TensMul_matching: remove unnecessary newlines

--- sympy/tensor/tests/test_tensor.py
+++ sympy/tensor/tests/test_tensor.py
@@ -2056,10 +2056,7 @@ def test_TensMul_matching():
     W = WildTensorHead('W', unordered_indices=True)
     U = WildTensorHead('U')
 
-    assert(
-        ( wi*K(p) ).matches( K(p) )
-        == {wi: 1}
-        )
+    assert ( wi*K(p) ).matches( K(p) ) == {wi: 1}
     check_tens_eq(
         (K(p) * V(-p)).replace( W(a) * V(-a), 1),
         1
