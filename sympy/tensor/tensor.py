7a8e7955bbc96f7f57853c0ea9088ce5e89ca523
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-11-29 14:20:02 +0530

    TensAdd.doit: check and drop args which are zero
    Earlier, if an arg were to become zero due to doit,
    TensAdd._tensAdd_check would complain about mismatching indices. Now, if
    an arg turns out to be zero due to doit, it will be dropped.

--- sympy/tensor/tensor.py
+++ sympy/tensor/tensor.py
@@ -2487,6 +2487,9 @@ class TensAdd(TensExpr, AssocOp):
         if len(args) == 1 and not isinstance(args[0], TensExpr):
             return args[0]
 
+        # if any of the args are zero (after doit), drop them. Otherwise, the next step will complain about non-matching indices, even though the TensAdd is correctly formed.
+        args = [arg for arg in args if arg != 0]
+
         # now check that all addends have the same indices:
         TensAdd._tensAdd_check(args)
 
