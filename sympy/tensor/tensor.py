522734a0fc81b508747617fd554868501231bb24
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-12-05 10:08:36 +0530

    TensMul._matches_commutative: correctly pass rpl_dict during recursion.

--- sympy/tensor/tensor.py
+++ sympy/tensor/tensor.py
@@ -4310,14 +4310,18 @@ class TensMul(TensExpr, AssocOp):
             for e,m in matched_this:
                 rem_query = self.func(*[a for a in self.args if a != q_tensor]).doit()
                 rem_expr = expr.func(*[a for a in expr.args if a != e]).doit()
-                rem_m = rem_query.matches(rem_expr, repl_dict=m)
+                tmp_repl = dict()
+                tmp_repl.update(repl_dict)
+                tmp_repl.update(m)
+                rem_m = rem_query.matches(rem_expr, repl_dict=tmp_repl)
                 if rem_m is not None:
                     #Check that contracted indices are not mapped to different indices.
                     internally_consistent = True
                     for k in rem_m.keys():
-                        if isinstance(k,TensorIndex) and -k in rem_m.keys() and rem_m[-k] != -rem_m[k]:
-                            internally_consistent = False
-                            break
+                        if isinstance(k,TensorIndex):
+                            if -k in rem_m.keys() and rem_m[-k] != -rem_m[k]:
+                                internally_consistent = False
+                                break
                     if internally_consistent:
                         repl_dict.update(rem_m)
                         return repl_dict
