5e676000de96867c81abf4aecb62ac8e8dcf654a
Christopher Smith <smichr@gmail.com>
2022-12-17 02:18:36 -0600

    remove redundant tests

--- sympy/physics/units/tests/test_unit_system_cgs_gauss.py
+++ sympy/physics/units/tests/test_unit_system_cgs_gauss.py
@@ -32,34 +32,6 @@ def test_conversion_to_from_si():
     assert convert_to(newton, dyne, cgs_gauss) == 10**5*dyne
 
 
-def test_ohm_cgs_gauss():
-
-    assert convert_to(ohm,second/centimeter,cgs_gauss) == 25000*second/(22468879468420441*centimeter)
-    assert convert_to(1*ohm,second/centimeter,cgs_gauss) == 25000*second/(22468879468420441*centimeter)
-    assert convert_to(2*ohm,second/centimeter,cgs_gauss) == 50000*second/(22468879468420441*centimeter)
-    assert NS(convert_to(ohm,second/centimeter,cgs_gauss)) == '1.11265005605362e-12*second/centimeter'
-    assert NS(convert_to(2*ohm,second/centimeter,cgs_gauss)) == '2.22530011210724e-12*second/centimeter'
-
-def test_henry_cgs_gauss():
-    assert convert_to(henry,second**2/centimeter,cgs_gauss) == 25000*second**2/(22468879468420441*centimeter)
-    assert convert_to(1*henry,second**2/centimeter,cgs_gauss) == 25000*second**2/(22468879468420441*centimeter)
-    assert convert_to(2*henry,second**2/centimeter,cgs_gauss) == 50000*second**2/(22468879468420441*centimeter)
-    assert NS(convert_to(henry,second**2/centimeter,cgs_gauss)) == '1.11265005605362e-12*second**2/centimeter'
-    assert NS(convert_to(2*henry,second**2/centimeter,cgs_gauss)) == '2.22530011210724e-12*second**2/centimeter'
-
-def test_volt_cgs_gauss():
-    assert convert_to(volt,statvolt,cgs_gauss) == 10**6*statvolt/299792458
-    assert convert_to(1*volt,statvolt,cgs_gauss) == 10**6*statvolt/299792458
-    assert convert_to(2*volt,statvolt,cgs_gauss) == 2*10**6*statvolt/299792458
-
-def test_farad_cgs_gauss():
-
-   assert convert_to(farad,centimeter,cgs_gauss) == 299792458**2*centimeter/10**5
-   assert convert_to(1*farad,centimeter,cgs_gauss) == 299792458**2*centimeter/10**5
-   assert convert_to(2*farad,centimeter,cgs_gauss) == 2*299792458**2*centimeter/10**5
-
-
-
 def test_cgs_gauss_convert_constants():
 
     assert convert_to(speed_of_light, centimeter/second, cgs_gauss) == 29979245800*centimeter/second
@@ -74,3 +46,9 @@ def test_cgs_gauss_convert_constants():
     assert convert_to(elementary_charge, statcoulomb, cgs_gauss)
     assert convert_to(gravitational_constant, dyne*centimeter**2/gram**2, cgs_gauss)
     assert NS(convert_to(planck, erg*second, cgs_gauss)) == '6.62607015e-27*erg*second'
+
+    spc = 25000*second/(22468879468420441*centimeter)
+    assert convert_to(ohm, second/centimeter, cgs_gauss) == spc
+    assert convert_to(henry, second**2/centimeter, cgs_gauss) == spc*second
+    assert convert_to(volt, statvolt, cgs_gauss) == 10**6*statvolt/299792458
+    assert convert_to(farad, centimeter, cgs_gauss) == 299792458**2*centimeter/10**5
