483ecb429c614e2d1b32b3ebeae83d62fc3b76d0
evbernardes <s.evandro@hotmail.com>
2022-12-19 09:30:11 +0100

    minor edit

--- sympy/algebras/quaternion.py
+++ sympy/algebras/quaternion.py
@@ -784,7 +784,7 @@ class Quaternion(Expr):
     def _eval_conjugate(self):
         """Returns the conjugate of the quaternion."""
         q = self
-        return Quaternion(q.a, -q.b, -q.c, -q.d, norm=self._norm)
+        return Quaternion(q.a, -q.b, -q.c, -q.d, norm=q._norm)
 
     def norm(self):
         """Returns the norm of the quaternion."""
