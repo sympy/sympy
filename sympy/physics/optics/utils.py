f214be1dbe67c2a073c0bab0e1f641924e852c9c
Swastik Gour <sswastik959@gmail.com>
2022-12-30 17:29:01 +0530

    updated the name of function for lateral magnificaion of mirror and added function for lateral magnification of lens

--- sympy/physics/optics/utils.py
+++ sympy/physics/optics/utils.py
@@ -668,10 +668,13 @@ def hyperfocal_distance(f, N, c):
 
     return (1/(N * c))*(f**2)
 
-def transverse_magnification(si, so):
+
+
+
+def transverse_magnification_mirror(si, so):
     """
 
-    Calculates the transverse magnification, which is the ratio of the
+    Calculates the transverse magnification due to mirror, which is the ratio of the
     image size to the object size.
 
     Parameters
@@ -696,3 +699,35 @@ def transverse_magnification(si, so):
     so = sympify(so)
 
     return (-(si/so))
+
+
+
+
+def transverse_magnification_lens(si,so):
+    """
+
+    Calculates the transverse magnification due to mirror, which is the ratio of the
+    image size to the object size.
+
+    Parameters
+    ==========
+
+    so: sympifiable
+        Lens-object distance.
+
+    si: sympifiable
+        Lens-image distance.
+
+    Example
+    =======
+
+    >>> from sympy.physics.optics import transverse_magnification
+    >>> transverse_magnification(30, 15)
+    -2
+
+    """
+
+    si = sympify(si)
+    so = sympify(so)
+
+    return (si/so)
