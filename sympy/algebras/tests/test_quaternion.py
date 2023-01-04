69dd52354a401077be35eecc2fb87c93c2a41a29
evbernardes <s.evandro@hotmail.com>
2023-01-04 15:04:06 +0100

    fixed homogeneous test

--- sympy/algebras/tests/test_quaternion.py
+++ sympy/algebras/tests/test_quaternion.py
@@ -266,7 +266,7 @@ def test_quaternion_conversions():
 def test_rotation_matrix_homogeneous():
     q = Quaternion(w, x, y, z)
     R1 = q.to_rotation_matrix(homogeneous=True) * q.norm()**2
-    R2 = simplify(q.to_rotation_matrix() * q.norm()**2)
+    R2 = simplify(q.to_rotation_matrix(homogeneous=False) * q.norm()**2)
     assert R1 == R2
 
 
