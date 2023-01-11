35f08bfa795d7a3b658c990c75b6864b8a50daa6
Sangyub Lee <sylee957@gmail.com>
2023-01-11 06:42:34 +0200

    Add docstrings for new functions

--- sympy/matrices/expressions/shape.py
+++ sympy/matrices/expressions/shape.py
@@ -6,6 +6,27 @@ from typing import Union
 
 
 def is_matadd_valid(*args: MatrixExpr) -> Boolean:
+    """Return the symbolic condition how ``MatAdd``, ``HadamardProduct``
+    makes sense.
+
+    Parameters
+    ==========
+
+    args
+        The list of arguments of matrices to be tested for.
+
+    Examples
+    ========
+
+    >>> from sympy import MatrixSymbol, symbols
+    >>> from sympy.matrices.expressions.shape import is_matadd_valid
+
+    >>> m, n, p, q = symbols('m n p q')
+    >>> A = MatrixSymbol('A', m, n)
+    >>> B = MatrixSymbol('B', p, q)
+    >>> is_matadd_valid(A, B)
+    Eq(m, p) & Eq(n, q)
+    """
     rows, cols = zip(*(arg.shape for arg in args))
     return And(
         *(Eq(i, j) for i, j in zip(rows[:-1], rows[1:])),
@@ -14,9 +35,49 @@ def is_matadd_valid(*args: MatrixExpr) -> Boolean:
 
 
 def is_matmul_valid(*args: Union[MatrixExpr, Expr]) -> Boolean:
+    """Return the symbolic condition how ``MatMul`` makes sense
+
+    Parameters
+    ==========
+
+    args
+        The list of arguments of matrices and scalar expressions to be tested
+        for.
+
+    Examples
+    ========
+
+    >>> from sympy import MatrixSymbol, symbols
+    >>> from sympy.matrices.expressions.shape import is_matmul_valid
+
+    >>> m, n, p, q = symbols('m n p q')
+    >>> A = MatrixSymbol('A', m, n)
+    >>> B = MatrixSymbol('B', p, q)
+    >>> is_matmul_valid(A, B)
+    Eq(n, p)
+    """
     rows, cols = zip(*(arg.shape for arg in args if isinstance(arg, MatrixExpr)))
     return And(*(Eq(i, j) for i, j in zip(cols[:-1], rows[1:])))
 
 
-def is_square(arg: MatrixExpr) -> Boolean:
+def is_square(arg: MatrixExpr, /) -> Boolean:
+    """Return the symbolic condition how the matrix is assumed to be square
+
+    Parameters
+    ==========
+
+    arg
+        The matrix to be tested for.
+
+    Examples
+    ========
+
+    >>> from sympy import MatrixSymbol, symbols
+    >>> from sympy.matrices.expressions.shape import is_square
+
+    >>> m, n = symbols('m n')
+    >>> A = MatrixSymbol('A', m, n)
+    >>> is_square(A)
+    Eq(m, n)
+    """
     return Eq(arg.rows, arg.cols)
