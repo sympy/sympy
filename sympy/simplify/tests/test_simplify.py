841bc9ecbce18be1988f960ec7eb3f8557a1a9c2
Costor <pcs2009@web.de>
2023-01-15 22:02:29 +0100

    Replace < 0 by .is_negative

--- sympy/simplify/tests/test_simplify.py
+++ sympy/simplify/tests/test_simplify.py
@@ -1046,3 +1046,12 @@ def test_issue_22210():
     d = Symbol('d', integer=True)
     expr = 2*Derivative(sin(x), (x, d))
     assert expr.simplify() == expr
+
+def test_reduce_inverses_nc_pow():
+    x, y = symbols("x y", commutative=True)
+    Z = symbols("Z", commutative=False)
+    assert simplify(2**Z * y**Z) == 2**Z * y**Z
+    assert simplify(x**Z * y**Z) == x**Z * y**Z
+    x, y = symbols("x y", positive=True)
+    assert expand((x*y)**Z) == x**Z * y**Z
+    assert simplify(x**Z * y**Z) == expand((x*y)**Z)
