9c040e26695c1218ea0b4c78243dec9350114088
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-01-25 19:07:25 +0530

    resolve error

--- sympy/vector/tests/test_printing.py
+++ sympy/vector/tests/test_printing.py
@@ -198,9 +198,8 @@ def test_issue_23058():
     assert upretty(vecB) == vecB_str
     assert upretty(vecE) == vecE_str
 
-    ten = UnevaluatedExpr(10)
+    ten = symbols('10', commutative=False)
     eps, mu = 4*pi*ten**(-11), ten**(-5)
-
     Bx = 2 * ten**(-4) * cos(ten**5 * t) * sin(ten**(-3) * y)
     vecB = Bx * xhat
 
