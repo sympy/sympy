a6990e35039dcad054016480ddda2614773f4b3e
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-01-25 15:35:27 +0530

    add commit

--- sympy/physics/units/tests/test_unit_system_cgs_gauss.py
+++ sympy/physics/units/tests/test_unit_system_cgs_gauss.py
@@ -51,5 +51,6 @@ def test_cgs_gauss_convert_constants():
 
     spc = 25000*second/(22468879468420441*centimeter)
     assert convert_to(ohm, second/centimeter, cgs_gauss) == spc
+    assert convert_to(farad, centimeter, cgs_gauss) == 299792458**2*centimeter/10**5
     assert convert_to(henry, second**2/centimeter, cgs_gauss) == spc*second
     assert convert_to(volt, statvolt, cgs_gauss) == 10**6*statvolt/299792458
