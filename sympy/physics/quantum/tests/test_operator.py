dce701171a2c05c2e93d9e173087be7fe46cbd03
Costor <pcs2009@web.de>
2023-01-26 20:18:57 +0100

    Pow for Quantum HermitianOperator incorrect if also unitary and exponent not an explicit integer

--- sympy/physics/quantum/tests/test_operator.py
+++ sympy/physics/quantum/tests/test_operator.py
@@ -231,3 +231,25 @@ def test_differential_operator():
     assert diff(d, th) == \
         DifferentialOperator(Derivative(d.expr, th), f(r, th))
     assert qapply(d*w) == Wavefunction(3*sin(th), r, (th, 0, pi))
+
+
+def test_eval_power():
+    O = Operator('O')
+    U = UnitaryOperator('U')
+    H = HermitianOperator('H')
+    assert O**-1 == O.inv() # same as doc test
+    assert U**-1 == U.inv()
+    assert H**-1 == H.inv()
+    x = symbols("x", commutative = True)
+    assert H**x == Pow(H, x, evaluate=False)
+    from sympy.physics.quantum.gate import XGate
+    X = XGate(0) # is hermitian and unitary
+    assert X**x == Pow(X, x, evaluate=False)
+    n = symbols("n", integer=True, even=True)
+    assert X**n == 1
+    n = symbols("n", integer=True, odd=True)
+    assert X**n == X
+    n = symbols("n", integer=True)
+    assert X**n == Pow(X, n, evaluate=False)
+    assert X**4 == 1
+    assert X**7 == X
