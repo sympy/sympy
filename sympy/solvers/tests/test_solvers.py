48bdad829f6087736394037c0651fedfb8b0cf9b
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-01-27 22:10:39 +0000

    test(solvers): update solve inequality test

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -1898,12 +1898,11 @@ def test_issues_6819_6820_6821_6248_8692_25777_25779():
     x = symbols('x')
     assert solve(2**x + 4**x) == [I*pi/log(2)]
 
-def test_issue_17638():
 
+def test_issue_17638():
     assert solve(((2-exp(2*x))*exp(x))/(exp(2*x)+2)**2 > 0, x) == (-oo < x) & (x < log(2)/2)
     assert solve(((2-exp(2*x)+2)*exp(x+2))/(exp(x)+2)**2 > 0, x) == (-oo < x) & (x < log(4)/2)
-    assert solve((exp(x)+2+x**2)*exp(2*x+2)/(exp(x)+2)**2 > 0, x) == (-oo < x) & (x < oo)
-
+    assert solve((exp(x)+2+x**2)*exp(2*x+2)/(exp(x)+2)**2 > 0, x) == True
 
 
 def test_issue_14607():
