34ad338c0fcfd607835e54bf973ed39bd947953e
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2023-02-05 22:45:32 +0530

    Added 2 files in compare-tar whitelisted files list

--- release/compare_tar_against_git.py
+++ release/compare_tar_against_git.py
@@ -40,6 +40,8 @@ git_whitelist = {
     '.github/workflows/release.yml',
     '.github/workflows/docs-preview.yml',
     '.github/workflows/checkconflict.yml',
+    '.github/workflows/checkBrokenLinks.yml',
+    '.github/workflows/links-fail-fast.yml',
     '.ci/durations.json',
     '.ci/generate_durations_log.sh',
     '.ci/parse_durations_log.py',
