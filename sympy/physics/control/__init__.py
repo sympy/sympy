444e59c2d7aecc46ec4beccb2522bad5c2712888
zolabar <raszoufine@gmail.com>
2023-02-10 12:12:51 +0100

    Update __init__.py

--- sympy/physics/control/__init__.py
+++ sympy/physics/control/__init__.py
@@ -1,5 +1,5 @@
 from .lti import (TransferFunction, Series, MIMOSeries, Parallel, MIMOParallel,
-    Feedback, MIMOFeedback, TransferFunctionMatrix, bilinear)
+    Feedback, MIMOFeedback, TransferFunctionMatrix, bilinear, backward_diff)
 from .control_plots import (pole_zero_numerical_data, pole_zero_plot, step_response_numerical_data,
     step_response_plot, impulse_response_numerical_data, impulse_response_plot, ramp_response_numerical_data,
     ramp_response_plot, bode_magnitude_numerical_data, bode_phase_numerical_data, bode_magnitude_plot,
