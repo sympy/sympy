0fcb480554e85cd7f10c09eb09fa2982e33126c3
Steve Kieffer <sk@skieffer.info>
2023-02-21 15:47:46 -0500

    Make Round 2 alg accept more polys
    Can accept any irreducible poly over ZZ or QQ.
    Need not be monic, need not be over ZZ.

--- sympy/polys/numberfields/basis.py
+++ sympy/polys/numberfields/basis.py
@@ -100,8 +100,8 @@ def round_two(T, radicals=None):
     Explanation
     ===========
 
-    Carry out Zassenhaus's "Round 2" algorithm on a monic irreducible
-    polynomial *T* over :ref:`ZZ`. This computes an integral basis and the
+    Carry out Zassenhaus's "Round 2" algorithm on an irreducible polynomial
+    *T* over :ref:`ZZ` or :ref:`QQ`. This computes an integral basis and the
     discriminant for the field $K = \mathbb{Q}[x]/(T(x))$.
 
     Alternatively, you may pass an :py:class:`~.AlgebraicField` instance, in
@@ -153,9 +153,9 @@ def round_two(T, radicals=None):
     ==========
 
     T : :py:class:`~.Poly`, :py:class:`~.AlgebraicField`
-        Either (1) the irreducible monic polynomial over :ref:`ZZ` defining the
-        number field, or (2) an :py:class:`~.AlgebraicField` representing the
-        number field itself.
+        Either (1) the irreducible polynomial over :ref:`ZZ` or :ref:`QQ`
+        defining the number field, or (2) an :py:class:`~.AlgebraicField`
+        representing the number field itself.
 
     radicals : dict, optional
         This is a way for any $p$-radicals (if computed) to be returned by
@@ -191,16 +191,11 @@ def round_two(T, radicals=None):
     K = None
     if isinstance(T, AlgebraicField):
         K, T = T, T.ext.minpoly_of_element()
-    if T.domain == QQ:
-        try:
-            T = Poly(T, domain=ZZ)
-        except CoercionFailed:
-            pass  # Let the error be raised by the next clause.
     if (   not T.is_univariate
         or not T.is_irreducible
-        or not T.is_monic
-        or not T.domain == ZZ):
-        raise ValueError('Round 2 requires a monic irreducible univariate polynomial over ZZ.')
+        or T.domain not in [ZZ, QQ]):
+        raise ValueError('Round 2 requires an irreducible univariate polynomial over ZZ or QQ.')
+    T, _ = T.make_monic_over_integers_by_scaling_roots()
     n = T.degree()
     D = T.discriminant()
     D_modulus = ZZ.from_sympy(abs(D))
