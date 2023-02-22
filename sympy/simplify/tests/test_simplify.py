f7683c3bc44160de24f287d8a008ed3929e71d21
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-02-22 17:34:21 +0530

    fixed formatting

--- sympy/simplify/tests/test_simplify.py
+++ sympy/simplify/tests/test_simplify.py
@@ -1037,18 +1037,20 @@ def test_issue_23543():
     assert (x*(y + z/2)).simplify() == x*(2*y + z)/2
 
 def test_issue_11004():
-    N, k = symbols('N k')
     def f(n):
-        return sqrt(2*pi*n)*(n/E)**n
-    def m(n, k):
-        return  f(n)/ (f(n/k)**k)
-    def p(n,k):
-        return m(n, k)/(k**n)
+        return sqrt(2*pi*n) * (n/E)**n
 
+    def m(n, k):
+        return  f(n) / (f(n/k)**k)
+
+    def p(n,k):
+        return m(n, k) / (k**n)
+
+    N, k = symbols('N k')
     half = Float('0.5', 4)
-    z = log(p(n, k)/p(n, k+1)).expand(force=True)
+    z = log(p(n, k) / p(n, k + 1)).expand(force=True)
     r = simplify(z.subs(n, N).n(4))
-    assert r == half*k*log(k) - half*k*log(k + 1) + half*log(N) - half*log(k + 1) + Float(0.9189224, 4)
+    assert r == (half*k*log(k) - half*k*log(k + 1) + half*log(N) - half*log(k + 1) + Float(0.9189224, 4))
 
 
 def test_issue_19161():
