7dbf7630e7eaa16fc060725201c7b650742a78e5
Jay Patankar <patankarjays@gmail.com>
2023-02-24 19:32:12 +0530

    again modified __new__

--- sympy/sets/sets.py
+++ sympy/sets/sets.py
@@ -1298,10 +1298,10 @@ class Union(Set, LatticeOp):
     def zero(self):
         return S.UniversalSet
 
-    def __new__(cls, *args , evaluate=global_parameters.evaluate):
-        
-
+    def __new__(cls, *args , evaluate=None):     
         # flatten inputs to merge intersections and iterables
+        if evaluate==None:
+            evaluate=global_parameters.evaluate
         args = _sympify(args)
 
         # Reduce sets using known rules
