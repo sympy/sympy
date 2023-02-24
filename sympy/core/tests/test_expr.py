0089949d080dc66bb97622f6a52286655020acbe
Megan Ly <megan.ly@learnosity.com>
2023-02-23 17:36:11 -0800

    Fix as_coeff_Mul for Float(0.0)

--- sympy/core/tests/test_expr.py
+++ sympy/core/tests/test_expr.py
@@ -1660,6 +1660,7 @@ def test_as_coeff_Mul():
     assert Integer(3).as_coeff_Mul() == (Integer(3), Integer(1))
     assert Rational(3, 4).as_coeff_Mul() == (Rational(3, 4), Integer(1))
     assert Float(5.0).as_coeff_Mul() == (Float(5.0), Integer(1))
+    assert Float(0.0).as_coeff_Mul() == (Float(0.0), Integer(1))
 
     assert (Integer(3)*x).as_coeff_Mul() == (Integer(3), x)
     assert (Rational(3, 4)*x).as_coeff_Mul() == (Rational(3, 4), x)
