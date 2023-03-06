9997fc4a556b71689c683309718e5684213872c1
TJStienstra <timostienstra00@gmail.com>
2023-03-06 15:29:07 +0100

    Fix MatrixSolve not sympifying the vector argument

---
+++ sympy/codegen/tests/test_matrix_nodes.py
@@ -0,0 +1,9 @@
+from sympy.core.symbol import symbols
+from sympy.matrices.dense import Matrix
+from sympy.codegen.matrix_nodes import MatrixSolve
+
+
+def test_matrix_solve_24862():
+    A = Matrix(3, 3, symbols('a:9'))
+    b = Matrix(3, 1, symbols('b:3'))
+    hash(MatrixSolve(A, b))
