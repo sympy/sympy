a39936b3de110eb5fab1f3f1e5f6c9f0e8b865ab
evbernardes <s.evandro@hotmail.com>
2023-03-18 17:42:58 +0100

    Minor edit

--- sympy/matrices/matrices.py
+++ sympy/matrices/matrices.py
@@ -1268,7 +1268,7 @@ class MatrixBase(MatrixDeprecated,
         Examples
         ========
 
-        Calling ``hat`` creates a skew-symmetric Matrix:
+        Calling ``hat`` creates a skew-symmetric matrix:
 
         >>> from sympy import Matrix
         >>> a = Matrix([1, 2, 3])
@@ -1278,7 +1278,7 @@ class MatrixBase(MatrixDeprecated,
         [ 3,  0, -1],
         [-2,  1,  0]])
 
-        Multiplying it with another Matrix calculates the cross product:
+        Multiplying it with another 3x1 matrix calculates the cross product:
 
         >>> b = Matrix([3, 2, 1])
         >>> a.hat() * b
