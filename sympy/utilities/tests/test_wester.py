fe7b3ba5d48fceddad4f5345f9cfcfddf8ca8d03
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-03-19 16:30:28 +0000

    test: use a tooslow marker for tests to be skipped
    Replace the ON_CI variable used for skipping extremely slow tests with a
    tooslow decorator. The decorator means that pytest can understan to skip
    these tests by default.
    
    Some too slow tests were found not to be too slow any more and others
    that had been XFAIL were found to be working.

--- sympy/utilities/tests/test_wester.py
+++ sympy/utilities/tests/test_wester.py
@@ -76,8 +76,7 @@ from sympy.functions.combinatorial.numbers import stirling
 from sympy.functions.special.delta_functions import Heaviside
 from sympy.functions.special.error_functions import Ci, Si, erf
 from sympy.functions.special.zeta_functions import zeta
-from sympy.testing.pytest import (XFAIL, slow, SKIP, skip, ON_CI,
-    raises)
+from sympy.testing.pytest import (XFAIL, slow, SKIP, tooslow, raises)
 from sympy.utilities.iterables import partitions
 from mpmath import mpi, mpc
 from sympy.matrices import Matrix, GramSchmidt, eye
@@ -2607,10 +2606,8 @@ def test_W23b():
 
 
 @XFAIL
-@slow
+@tooslow
 def test_W24():
-    if ON_CI:
-        skip("Too slow for CI.")
     # Not that slow, but does not fully evaluate so simplify is slow.
     # Maybe also require doit()
     x, y = symbols('x y', real=True)
@@ -2619,10 +2616,8 @@ def test_W24():
 
 
 @XFAIL
-@slow
+@tooslow
 def test_W25():
-    if ON_CI:
-        skip("Too slow for CI.")
     a, x, y = symbols('a x y', real=True)
     i1 = integrate(
         sin(a)*sin(y)/sqrt(1 - sin(a)**2*sin(x)**2*sin(y)**2),
