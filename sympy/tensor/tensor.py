a979f13ce92f5fdd7079edb3c6d98c992c597c01
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-03-21 12:20:27 +0530

    tensor.TensMul: add _eval_expand_mul method
    Note that we don't call doit on the result. This allows something like
    (f(x) + 1)*K(p) (with f being a Function and K being a TensorHead)
    to actually be expanded.

--- sympy/tensor/tensor.py
+++ sympy/tensor/tensor.py
@@ -3853,6 +3853,12 @@ class TensMul(TensExpr, AssocOp):
             TensMul(*i) for i in itertools.product(*args1)]
         )
 
+    def _eval_expand_mul(self, **hints):
+        args1 = [arg.args if isinstance(arg, (Add, TensAdd)) else (arg,) for arg in self.args]
+        return TensAdd(*[
+            TensMul(*i).doit(deep=False) for i in itertools.product(*args1)]
+        )
+
     def __neg__(self):
         return TensMul(S.NegativeOne, self, is_canon_bp=self._is_canon_bp).doit(deep=False)
 
