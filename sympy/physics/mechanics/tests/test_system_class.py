e5d0c74510a93d4ce8e5fcda2154b1898b2bddad
TJStienstra <timostienstra00@gmail.com>
2023-03-22 15:19:29 +0100

    Refactor tests checking if an object can be resetted

--- sympy/physics/mechanics/tests/test_system_class.py
+++ sympy/physics/mechanics/tests/test_system_class.py
@@ -3,7 +3,7 @@ import pytest
 from sympy.core.backend import ImmutableMatrix, symbols
 from sympy.physics.mechanics import (
     System, ReferenceFrame, Point, RigidBody, Particle, dynamicsymbols,
-    PinJoint, PrismaticJoint)
+    PinJoint, PrismaticJoint, Force)
 
 t = dynamicsymbols._t  # type: ignore
 q = dynamicsymbols('q:6')  # type: ignore
@@ -50,22 +50,22 @@ class TestSystem:
         self.system.u_dep = u[2:4]
 
     def _filled_system_check(self, exclude=()):
-        assert 'q_ind' not in exclude or self.system.q_ind[:] == q[:3]
-        assert 'q_dep' not in exclude or self.system.q_dep[:] == [q[3]]
-        assert 'q' not in exclude or self.system.q[:] == q[:4]
-        assert 'u_ind' not in exclude or self.system.u_ind[:] == u[:2]
-        assert 'u_dep' not in exclude or self.system.u_dep[:] == u[2:4]
-        assert 'u' not in exclude or self.system.u[:] == u[:4]
-        assert 'kdes' not in exclude or self.system.kdes[:] == [
+        assert 'q_ind' in exclude or self.system.q_ind[:] == q[:3]
+        assert 'q_dep' in exclude or self.system.q_dep[:] == [q[3]]
+        assert 'q' in exclude or self.system.q[:] == q[:4]
+        assert 'u_ind' in exclude or self.system.u_ind[:] == u[:2]
+        assert 'u_dep' in exclude or self.system.u_dep[:] == u[2:4]
+        assert 'u' in exclude or self.system.u[:] == u[:4]
+        assert 'kdes' in exclude or self.system.kdes[:] == [
             ui - qdi for ui, qdi in zip(u[:4], qd[:4])]
-        assert ('holonomic_constraints' not in exclude or
+        assert ('holonomic_constraints' in exclude or
                 self.system.holonomic_constraints[:] == [q[2] - q[0] + q[1]])
-        assert ('nonholonomic_constraints' not in exclude or
+        assert ('nonholonomic_constraints' in exclude or
                 self.system.nonholonomic_constraints[:] == [u[3] - qd[1] + u[2]]
                 )
-        assert ('bodies' not in exclude or
+        assert ('bodies' in exclude or
                 self.system.bodies == tuple(self.bodies))
-        assert ('joints' not in exclude or
+        assert ('joints' in exclude or
                 self.system.joints == tuple(self.joints))
 
     def test_filled_system(self, _filled_system_setup):
@@ -134,30 +134,6 @@ class TestSystem:
         assert self.system.q[:] == exp_q
         self._empty_system_check(exclude=('q_ind', 'q_dep', 'q'))
 
-    def test_coordinate_additional(self, _filled_system_setup):
-        self.system.add_coordinates(q[4], independent=False)
-        assert self.system.q_ind[:] == q[:3]
-        assert self.system.q_dep[:] == q[3:5]
-        assert self.system.q[:] == q[:5]
-        # Test resetting the coordinates
-        self.system.q_ind = []
-        assert self.system.q_ind[:] == []
-        assert self.system.q_dep[:] == q[3:5]
-        assert self.system.q[:] == q[3:5]
-        self.system.q_dep = []
-        assert self.system.q_ind[:] == []
-        assert self.system.q_dep[:] == []
-        assert self.system.q[:] == []
-        # Test if filling the coordinates after reset works
-        self.system.add_coordinates(q[0])
-        assert self.system.q_ind[:] == [q[0]]
-        assert self.system.q_dep[:] == []
-        assert self.system.q[:] == [q[0]]
-        self.system.add_coordinates(q[1], independent=False)
-        assert self.system.q_ind[:] == [q[0]]
-        assert self.system.q_dep[:] == [q[1]]
-        assert self.system.q[:] == q[:2]
-
     @pytest.mark.parametrize('func', ['add_coordinates', 'add_speeds'])
     @pytest.mark.parametrize('args, kwargs', [
         ((q[0], q[5]), {}),
@@ -196,26 +172,24 @@ class TestSystem:
         assert self.system.u[:] == exp_u
         self._empty_system_check(exclude=('u_ind', 'u_dep', 'u'))
 
-    def test_speed_additional(self, _filled_system_setup):
-        self.system.add_speeds(u[4], independent=False)
-        assert self.system.u_ind[:] == u[:2]
-        assert self.system.u_dep[:] == u[2:5]
-        assert self.system.u[:] == u[:5]
-        # Test resetting the coordinates
-        self.system.u_ind = []
-        assert self.system.u_ind[:] == []
-        assert self.system.u_dep[:] == u[2:5]
-        assert self.system.u[:] == u[2:5]
-        self.system.u_dep = []
-        assert self.system.u_ind[:] == []
-        assert self.system.u_dep[:] == []
-        assert self.system.u[:] == []
-        # Test if filling the coordinates after reset works
-        self.system.add_speeds(u[0])
-        assert self.system.u_ind[:] == [u[0]]
-        assert self.system.u_dep[:] == []
-        assert self.system.u[:] == [u[0]]
-        self.system.add_speeds(u[1], independent=False)
-        assert self.system.u_ind[:] == [u[0]]
-        assert self.system.u_dep[:] == [u[1]]
-        assert self.system.u[:] == u[:2]
+    @pytest.mark.parametrize('prop, add_func, args, kwargs', [
+        ('q_ind', 'add_coordinates', (q[0],), {}),
+        ('q_dep', 'add_coordinates', (q[3],), {'independent': False}),
+        ('u_ind', 'add_speeds', (u[0],), {}),
+        ('u_dep', 'add_speeds', (u[3],), {'independent': False}),
+        ('kdes', 'add_kdes', (qd[0] - u[0],), {}),
+        ('holonomic_constraints', 'add_holonomic_constraints',
+         (q[0] - q[1],), {}),
+        ('nonholonomic_constraints', 'add_nonholonomic_constraints',
+         (u[0] - u[1],), {}),
+        ('bodies', 'add_bodies', (RigidBody('body'),), {}),
+        ('loads', 'add_loads', (Force(Point('P'), ReferenceFrame('N').x),), {}),
+    ])
+    def test_add_after_reset(self, _filled_system_setup, prop, add_func, args,
+                             kwargs):
+        setattr(self.system, prop, ())
+        self._filled_system_check(exclude=(prop, 'q', 'u'))
+        assert list(getattr(self.system, prop)[:]) == []
+        getattr(self.system, add_func)(*args, **kwargs)
+        assert list(getattr(self.system, prop)[:]) == list(args)
+
