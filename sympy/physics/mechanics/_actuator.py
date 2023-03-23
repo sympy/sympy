73fd93917e4e4021a1d1371bcb28aefb2042041d
Sam Brockie <sambrockie@icloud.com>
2023-03-23 13:58:33 +0000

    Create (private) actuator module in sympy.physics.mechanics

---
+++ sympy/physics/mechanics/_actuator.py
@@ -0,0 +1,10 @@
+"""Implementations of actuators for linked force and torque application.
+
+Notes
+=====
+
+This module is experimental and so is named with a leading underscore to
+indicate that the API is not yet stabilized and could be subject to breaking
+changes.
+
+"""
