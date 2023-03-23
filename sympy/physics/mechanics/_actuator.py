cb1515e48dc7be0d6ed106b5458cc0061fc051c3
Sam Brockie <sambrockie@icloud.com>
2023-03-23 13:59:59 +0000

    Implement ActuatorBase abstract base class for all actuators

--- sympy/physics/mechanics/_actuator.py
+++ sympy/physics/mechanics/_actuator.py
@@ -8,3 +8,52 @@ indicate that the API is not yet stabilized and could be subject to breaking
 changes.
 
 """
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+from sympy.core.expr import Expr
+from sympy.physics.mechanics import Point, Vector
+
+
+__all__ = []
+
+
+class ActuatorBase(ABC):
+    """Abstract base class for all actuator classes to inherit from.
+
+    Notes
+    =====
+
+    Instances of this class cannot be directly instantiated by users. However,
+    it can be used to created custom actuator types through subclassing.
+
+    """
+
+    def __init__(self) -> None:
+        """Initializer for ``ActuatorBase``."""
+        pass
+
+    @abstractmethod
+    def compute_loads(self, force: Expr) -> list[tuple[Point, Vector]]:
+        """Loads required by the equations of motion method classes.
+
+        Explanation
+        ===========
+
+        ``KanesMethod`` requires a list of ``Point``-``Vector`` tuples to be
+        passed to the ``loads`` parameters of its ``kanes_equations`` method
+        when constructing the equations of motion. This method acts as a
+        utility to produce the correctly-structred pairs of points and vectors
+        required so that these can be easily concatenated with other items in
+        the list of loads and passed to ``KanesMethod.kanes_equations``. These
+        loads are also in the correct form to also be passed to the other
+        equations of motion method classes, e.g. ``LagrangesMethod``.
+
+        """
+        pass
+
+    def __repr__(self) -> str:
+        """Default representation of an actuator."""
+        return f'{self.__class__.__name__}()'
