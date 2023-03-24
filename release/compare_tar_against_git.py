f7abc88c7aac4061bc035fec0cc3eac2de7a24c3
Lukas Molleman <Lukas.Molleman@gmail.com>
2023-03-24 11:40:51 +0100

    Added comma

--- release/compare_tar_against_git.py
+++ release/compare_tar_against_git.py
@@ -55,7 +55,7 @@ git_whitelist = {
     'codecov.yml',
     'MANIFEST.in',
     'banner.svg',
-    '.pre-commit-config.yaml'
+    '.pre-commit-config.yaml',
     # Code of conduct
     'CODE_OF_CONDUCT.md',
     # Contributing guide
