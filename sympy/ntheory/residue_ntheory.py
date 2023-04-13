bdd2790735140e96e09171f4acdf66742b197a2c
haru-44 <36563693+haru-44@users.noreply.github.com>
2023-04-13 21:18:11 +0900

    No longer need to sort

--- sympy/ntheory/residue_ntheory.py
+++ sympy/ntheory/residue_ntheory.py
@@ -108,7 +108,7 @@ def _primitive_root_prime_iter(p):
             # 2 is the smallest primitive root of p = 5,11,13,19,29,37
             g = 2
     else:
-        v = [(p - 1) // i for i in sorted(factorint(p - 1))]
+        v = [(p - 1) // i for i in factorint(p - 1).keys()]
         for g in range(2, p):
             if all(pow(g, pw, p) != 1 for pw in v):
                 break
