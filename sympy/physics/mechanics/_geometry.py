83fb707c91927a61378ae087a0ec610b018731ee
Sam Brockie <sambrockie@icloud.com>
2023-04-19 18:12:26 +0200

    Create (private) geometry module in sympy.physics.mechanics

---
+++ sympy/physics/mechanics/_geometry.py
@@ -0,0 +1,10 @@
+"""Implementations of geometry objects for use by wrapping pathways.
+
+Notes
+=====
+
+This module is experimental and so is named with a leading underscore to
+indicate that the API is not yet stabilized and could be subject to breaking
+changes.
+
+"""
