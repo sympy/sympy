2a0161ab561534e86eef1730edf57e15aacb7780
Sam Brockie <sambrockie@icloud.com>
2023-04-19 17:49:28 +0200

    Add tests for Sphere construction and attributes

--- sympy/physics/mechanics/tests/test_geometry.py
+++ sympy/physics/mechanics/tests/test_geometry.py
@@ -1,1 +1,21 @@
 """Tests for the ``sympy.physics.mechanics._geometry.py`` module."""
+
+from __future__ import annotations
+
+from sympy.core.backend import Symbol
+from sympy.physics.mechanics import Point
+from sympy.physics.mechanics._geometry import Sphere
+
+
+class TestSphere:
+
+    @staticmethod
+    def test_valid_constructor() -> None:
+        r = Symbol('r')
+        pO = Point('pO')
+        sphere = Sphere(r, pO)
+        assert isinstance(sphere, Sphere)
+        assert hasattr(sphere, 'radius')
+        assert sphere.radius == r
+        assert hasattr(sphere, 'point')
+        assert sphere.point == pO
