6bf5bda04623d5fbac4c3275e17e1bfe98e3dbb7
Sam Brockie <sambrockie@icloud.com>
2023-04-20 13:42:04 +0200

    Refactor tests to treat expansile actuator force as positive

--- sympy/physics/mechanics/tests/test_pathway.py
+++ sympy/physics/mechanics/tests/test_pathway.py
@@ -82,8 +82,8 @@ class TestLinearPathway:
     def test_static_pathway_compute_loads(self) -> None:
         self.pB.set_pos(self.pA, 2 * self.N.x)
         expected = [
-            (self.pA, self.F * self.N.x),
-            (self.pB, - self.F * self.N.x),
+            (self.pA, - self.F * self.N.x),
+            (self.pB, self.F * self.N.x),
         ]
         assert self.pathway.compute_loads(self.F) == expected
 
@@ -94,14 +94,14 @@ class TestLinearPathway:
 
     def test_2D_pathway_extension_velocity(self) -> None:
         self.pB.set_pos(self.pA, 2 * self.q1 * self.N.x)
-        expected = -2 * self.q1 * self.q1d / sqrt(self.q1**2)
+        expected = 2 * self.q1 * self.q1d / sqrt(self.q1**2)
         assert self.pathway.extension_velocity == expected
 
     def test_2D_pathway_compute_loads(self) -> None:
         self.pB.set_pos(self.pA, 2 * self.q1 * self.N.x)
         expected = [
-            (self.pA, self.F * (self.q1 / sqrt(self.q1**2)) * self.N.x),
-            (self.pB, - self.F * (self.q1 / sqrt(self.q1**2)) * self.N.x),
+            (self.pA, - self.F * (self.q1 / sqrt(self.q1**2)) * self.N.x),
+            (self.pB, self.F * (self.q1 / sqrt(self.q1**2)) * self.N.x),
         ]
         assert self.pathway.compute_loads(self.F) == expected
 
@@ -120,9 +120,9 @@ class TestLinearPathway:
         )
         length = sqrt(self.q1**2 + self.q2**2 + 4*self.q3**2)
         expected = (
-            - self.q1 * self.q1d / length
-            - self.q2 * self.q2d / length
-            - 4 * self.q3 * self.q3d / length
+            self.q1 * self.q1d / length
+            + self.q2 * self.q2d / length
+            + 4 * self.q3 * self.q3d / length
         )
         assert simplify(self.pathway.extension_velocity - expected) == 0
 
@@ -133,14 +133,14 @@ class TestLinearPathway:
         )
         length = sqrt(self.q1**2 + self.q2**2 + 4*self.q3**2)
         pO_force = (
-            self.F * self.q1 * self.N.x / length
-            - self.F * self.q2 * self.N.y / length
-            + 2 * self.F * self.q3 * self.N.z / length
-        )
-        pI_force = (
             - self.F * self.q1 * self.N.x / length
             + self.F * self.q2 * self.N.y / length
             - 2 * self.F * self.q3 * self.N.z / length
+        )
+        pI_force = (
+            self.F * self.q1 * self.N.x / length
+            - self.F * self.q2 * self.N.y / length
+            + 2 * self.F * self.q3 * self.N.z / length
         )
         expected = [
             (self.pA, pO_force),
