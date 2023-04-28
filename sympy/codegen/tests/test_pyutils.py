9c992d03a5aabf3b869000e9fb812ef4f7193a6d
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-04-28 16:21:46 +0200

    Adapt docstrings/tests to bugfix of Print-printing

--- sympy/codegen/tests/test_pyutils.py
+++ sympy/codegen/tests/test_pyutils.py
@@ -4,4 +4,4 @@ from sympy.codegen.pyutils import render_as_module
 def test_standard():
     ast = Print('x y'.split(), "coordinate: %12.5g %12.5g")
     assert render_as_module(ast, standard='python3') == \
-        '\n\nprint("coordinate: %12.5g %12.5g" % (x, y))'
+        '\n\nprint("coordinate: %12.5g %12.5g" % (x, y), end="")'
