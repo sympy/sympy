f2e252c3b446f081d26ad14869db033013c7205c
kujuo <61703000+kujuo@users.noreply.github.com>
2023-05-04 22:34:50 -0400

    Documentation for special points

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -134,6 +134,10 @@ class Plot:
     - margin : float in [0, 1]
     - backend : {'default', 'matplotlib', 'text'} or a subclass of BaseBackend
     - size : optional tuple of two floats, (width, height); default: None
+    - special_points : list of tuples of either two or three floats
+        Special points are only considered for ``MatplotlibBackend`` at the 
+        moment. Special points must also describe points in all axes available, 
+        depending on whether the plot is 3D or 2D.
 
     The per data series options and aesthetics are:
     There are none in the base series. See below for options for subclasses.
@@ -1529,12 +1533,17 @@ class MatplotlibBackend(BaseBackend):
         if parent.special_points:
             xticks = ax.get_xticks()
             yticks = ax.get_yticks()
+            if isinstance(ax, Axes3D): zticks = ax.get_zticks()
             for point in parent.special_points: 
                 xticks = np.append(xticks, point[0])
                 yticks = np.append(yticks, point[1])
+                if isinstance(ax, Axes3D):
+                    zticks = np.append(yticks, point[2])
+                    self.plt.plot(point[0], point[1], point[2], marker="o", markersize=5, markeredgecolor="blue", markerfacecolor="blue")
                 self.plt.plot(point[0], point[1], marker="o", markersize=5, markeredgecolor="blue", markerfacecolor="blue")
             ax.set_xticks(xticks)
             ax.set_yticks(yticks)
+            if isinstance(ax, Axes3D): ax.set_yticks(zticks)
 
     def process_series(self):
         """
