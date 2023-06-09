d7f687389b9089ba8935515b6aa7845475de2e87
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-06-08 17:16:19 -0700

    new assumptions: use Z3 solver library to handle relational assumptions
    Use Z3 solver to add functionality so that the ask function can handle
    relational assumptions which it couldn’t handle at all for the most part.
    Queries to ask that involve a mix of relational assumptions and
    non-relational assumptions aren’t handled very well at the moment.
    
    Example
    
    >>> from sympy.abc import x, y, z
    >>> ​​z3ask(x > z, (x > y) & (y > z))
    True

--- sympy/assumptions/ask.py
+++ sympy/assumptions/ask.py
@@ -446,6 +446,7 @@ def ask(proposition, assumptions=True, context=global_assumptions):
         be determined.
     """
     from sympy.assumptions.satask import satask
+    from sympy.assumptions.smtask import smtask
 
     proposition = sympify(proposition)
     assumptions = sympify(assumptions)
@@ -493,6 +494,10 @@ def ask(proposition, assumptions=True, context=global_assumptions):
 
     # using satask (still costly)
     res = satask(proposition, assumptions=assumptions, context=context)
+    if res is not None:
+        return res
+
+    res = smtask(proposition, assumptions=assumptions, context=context)
     return res
 
 
