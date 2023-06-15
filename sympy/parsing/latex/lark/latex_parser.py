49909cadcdeb22bdf90048804aa9d1ffa383921f
wermos <63574588+wermos@users.noreply.github.com>
2023-06-15 21:08:07 +0530

    Refactored the Lark-based parser so that Lark is imported only once.
    Instead of being imported every time the parsing function is called, it will be imported just once when the file is run for the first time.

--- sympy/parsing/latex/lark/latex_parser.py
+++ sympy/parsing/latex/lark/latex_parser.py
@@ -1,8 +1,5 @@
-import operator
 import os
-import functools
 
-import sympy
 from sympy.external import import_module
 
 
@@ -10,21 +7,15 @@ class LaTeXParsingError(Exception):
     pass
 
 
+# TODO: if lark isn't found, just define a dummy transformer class, and don't do
+# any type of parsing.
+_lark = import_module('lark')
+
+if _lark is None:
+    raise ImportError("Could not load 'lark'")
+
+
 def parse_latex_lark(s: str):
-    # default behavior is to regenerate grammar from `LaTeX.lark` if
-    # `lark` is not importable it falls back to using the standalone
-    # precompiled lark latex grammar if it exists
-
-    # TODO: move the import_module call outside of the function so that all that
-    # code isn't run every time the function is called.
-
-    # TODO: if lark isn't found, just define a dummy transformer class, and don't do
-    # any type of parsing.
-    _lark = import_module('lark')
-    if _lark is None:
-        # TODO: Emit appropriate error message if Lark module not found.
-        raise ImportError("Could not load 'lark'")
-
     # TODO: should we use pkg_resource to get grammar file?  I
     # think this would make sympy depend on setuptools which we
     # would not like
@@ -39,16 +30,11 @@ def parse_latex_lark(s: str):
                         maybe_placeholders=False,
                         keep_all_tokens=True)
 
+    print("expression =", s)
     string = parser.parse(s)
     print(string)
     print(string.pretty())
 
+
 if __name__ == "__main__":
     parse_latex_lark(r"a \;\thickspace * b")
-
-# with open('latex.lark', 'r') as f:
-#     latex_parser = lark.Lark(f, start='string')
-#
-#     string = latex_parser.parse("a * b")
-#     print(string)
-#     print(string.pretty())
