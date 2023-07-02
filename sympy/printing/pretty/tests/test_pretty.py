855771b7cde9f87319d141ce149aca18bf97f585
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-02 08:17:15 -0700

    Link XFAIL to Issue number

--- sympy/printing/pretty/tests/test_pretty.py
+++ sympy/printing/pretty/tests/test_pretty.py
@@ -7508,7 +7508,7 @@ def test_print_polylog():
     assert upretty(polylog(2, 3)) == uresult
 
 
-@XFAIL
+@XFAIL  # Issue #25312
 def test_print_polylog_symbolic_order():
     s, z = symbols("s, z")
     uresult = 'Liₛ(z)'
