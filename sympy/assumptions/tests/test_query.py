d45860db80f5e46905df45c5061c89baf1cf245d
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-07 18:34:49 -0400

    fixed test with invalid predicate call

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -2409,6 +2409,6 @@ def test_relational():
 
 
 def test_issue_25221():
-    assert ask(Q.transcendental(x), Q.algebraic(x) | Q.positive(y,y)) is None
+    assert ask(Q.transcendental(x), Q.algebraic(x) | Q.positive(y)) is None
     assert ask(Q.transcendental(x), Q.algebraic(x) | (0 > y)) is None
     assert ask(Q.transcendental(x), Q.algebraic(x) | Q.gt(0,y)) is None
