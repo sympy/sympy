8e4f159c6f4ad1cedd7ff71eaa967b11ae899baf
arunkrao <arunrao@hotmail.com>
2023-07-23 06:43:03 +0530

    Update test_sets.py

--- sympy/sets/tests/test_sets.py
+++ sympy/sets/tests/test_sets.py
@@ -1361,7 +1361,7 @@ def test_issue_Symbol_inter():
 
 def test_issue_11827():
     assert S.Naturals0**4
-    
+
 
 def test_issue_14336():
     try:
@@ -1371,7 +1371,7 @@ def test_issue_14336():
         U -= U.intersect(S.true.as_set())
     except RecursionError as ex:
         assert False
-        
+
 
 def test_issue_10113():
     f = x**2/(x**2 - 4)
