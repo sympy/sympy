9493703f51d32cd9ac2929af9cac1c1732891bae
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-07-25 12:29:11 -0700

    return variable assignment when SAT

--- sympy/solvers/tests/test_inequalities.py
+++ sympy/solvers/tests/test_inequalities.py
@@ -548,7 +548,7 @@ def test_LRA_solver():
     m = Matrix()
     lra = LRASolver(m, [], [x, y])
     assert lra.assert_con(Q.ge(x, 0)) == "OK"
-    assert lra.assert_con(Q.ge(x, -1)) == "SAT"
+    assert lra.assert_con(Q.ge(x, -1)) == ('SAT', {x: 0, y: 0})
     assert lra.assert_con(Q.le(x, -1)) == ('UNSAT', {x <= -1, x >= 0})
 
     m = Matrix()
@@ -565,19 +565,19 @@ def test_LRA_solver():
     A, _ = linear_eq_to_matrix(equations, [x, y, s1, s2])
     A = -A # the identity matrix should be negative
     lra = LRASolver(A, [s1, s2], [x, y])
-    assert lra.check()[0] == "SAT"
+    assert lra.check() == ('SAT', {x: 0, y: 0, s1: 0, s2: 0})
     assert {v: lra.assign[v] for v in lra.all_var} == {x:0, y:0, s1:0, s2:0}
 
     assert lra.assert_con(x <= -4) == "OK"
-    assert lra.check()[0] == "SAT"
+    assert lra.check() == ('SAT', {x: -4, y: 0, s1: 4, s2: -4})
     assert {v: lra.assign[v] for v in lra.all_var} == {x:-4, y:0, s1: 4, s2:-4}
 
     assert lra.assert_con(x >= -8) == "OK"
-    assert lra.check()[0] == "SAT"
+    assert lra.check() == ('SAT', {x: -4, y: 0, s1: 4, s2: -4})
     assert {v: lra.assign[v] for v in lra.all_var} == {x:-4, y:0, s1:4, s2:-4}
 
     assert lra.assert_con(s1 <= 1) == "OK"
-    assert lra.check()[0] == "SAT"
+    assert lra.check() == ('SAT', {x: -4, y: -3, s1: 1, s2: -7})
     assert {v: lra.assign[v] for v in lra.all_var} == {x:-4, y:-3, s1:1, s2:-7}
 
     assert lra.assert_con(s2 >= -3) == "OK"
