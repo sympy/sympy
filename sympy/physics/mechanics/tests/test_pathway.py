fc9d415c5605d2d1b01fd193f5655204178ebec0
Sam Brockie <sambrockie@icloud.com>
2023-07-26 11:32:12 +0200

    Add test case for 2D pathway extension_velocity

--- sympy/physics/mechanics/tests/test_pathway.py
+++ sympy/physics/mechanics/tests/test_pathway.py
@@ -327,6 +327,18 @@ class TestObstacleSetPathway:
         expected = 2 * sqrt(2) + sqrt(2 + 2*cos(self.q))
         assert (pathway.length - expected).simplify() == 0
 
+    def test_2D_pathway_extension_velocity(self) -> None:
+        self.pA.set_pos(self.pO, -(self.N.x + self.N.y))
+        self.pB.set_pos(
+            self.pO, cos(self.q) * self.N.x - (sin(self.q) + 1) * self.N.y
+        )
+        self.pI.set_pos(
+            self.pO, sin(self.q) * self.N.x + (cos(self.q) - 1) * self.N.y
+        )
+        pathway = ObstacleSetPathway(self.pO, self.pA, self.pB, self.pI)
+        expected = - (sqrt(2) * sin(self.q) * self.qd) / (2 * sqrt(cos(self.q) + 1))
+        assert (pathway.extension_velocity - expected).simplify() == 0
+
 
 class TestWrappingPathway:
 
