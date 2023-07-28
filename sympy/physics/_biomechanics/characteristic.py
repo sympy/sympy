b4d2ae9f7f901d3746a866f07325b21582e2533e
Sam Brockie <sambrockie@icloud.com>
2023-07-28 16:27:22 +0200

    Add support for custom printing of Python code

--- sympy/physics/_biomechanics/characteristic.py
+++ sympy/physics/_biomechanics/characteristic.py
@@ -48,6 +48,7 @@ class CharacteristicCurveFunction(Function):
     _cxxcode = _print_code
     _fcode = _print_code
     _octave = _print_code
+    _pythoncode = _print_code
 
 
 class fl_T_de_groote_2016(CharacteristicCurveFunction):
