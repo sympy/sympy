565b0b7baecbbb7089a12bf5d637f271f5a19cf8
Sam Brockie <sambrockie@icloud.com>
2023-07-28 15:52:20 +0200

    Add test class for fl_T_de_groote_2016, add test case for class inheritance

--- sympy/physics/_biomechanics/tests/test_characteristic.py
+++ sympy/physics/_biomechanics/tests/test_characteristic.py
@@ -1,1 +1,16 @@
 """Tests for the ``sympy.physics._biomechanics.characteristic.py`` module."""
+
+from sympy.core.function import Function
+from sympy.physics._biomechanics.characteristic import (
+    CharacteristicCurveFunction,
+    fl_T_de_groote_2016,
+)
+
+
+class TestTendonForceLengthDeGroote2016:
+
+    @staticmethod
+    def test_class() -> None:
+        assert issubclass(fl_T_de_groote_2016, Function)
+        assert issubclass(fl_T_de_groote_2016, CharacteristicCurveFunction)
+        assert fl_T_de_groote_2016.__name__ == 'fl_T_de_groote_2016'
