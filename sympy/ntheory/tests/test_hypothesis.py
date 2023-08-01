d3bd0fff89f79e9695da53f6c3af9c9d549ea5d0
dianetc <dtchuindjo@gmail.com>
2023-08-01 14:22:48 -0400

    merge and doc addition

--- sympy/ntheory/tests/test_hypothesis.py
+++ sympy/ntheory/tests/test_hypothesis.py
@@ -16,3 +16,11 @@ def test_tau_hypothesis(n):
     totients = [totient(n // i) for i in div]
     mul = [a * b for a, b in zip(sigmas, totients)]
     assert n * tau_n == sum(mul)
+
+
+@given(n=st.integers(1, 10**10))
+def test_totient_hypothesis(n):
+    assert totient(n) <= n
+    div = divisors(n)
+    totients = [totient(i) for i in div]
+    assert n == sum(totients)
