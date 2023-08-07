8c882564ecc8c013a4b22b063545a5b575021902
C.A.P. Linssen <c.linssen@fz-juelich.de>
2023-08-07 06:11:05 -0700

    add tie-breaker rule

--- sympy/core/basic.py
+++ sympy/core/basic.py
@@ -2091,7 +2091,7 @@ class Atom(Basic):
 
     @cacheit
     def sort_key(self, order=None):
-        return self.class_key(), (1, (str(self),)), S.One.sort_key(), S.One
+        return self.class_key(), (1, (str(self),)), S.One.sort_key(), S.One.sort_key()
 
     def _eval_simplify(self, **kwargs):
         return self
--- sympy/core/symbol.py
+++ sympy/core/symbol.py
@@ -467,7 +467,7 @@ class Dummy(Symbol):
     @cacheit
     def sort_key(self, order=None):
         return self.class_key(), (
-            2, (self.name, self.dummy_index)), S.One.sort_key(), S.One
+            2, (self.name, self.dummy_index)), S.One.sort_key(), S.One.sort_key()
 
     def _hashable_content(self):
         return Symbol._hashable_content(self) + (self.dummy_index,)
