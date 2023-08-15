e28367dee8034dfda52e17781e825cefcc73a8d3
Sam Brockie <sambrockie@icloud.com>
2023-08-15 11:00:39 +0200

    Reformat imports

--- sympy/physics/mechanics/tests/test_pathway.py
+++ sympy/physics/mechanics/tests/test_pathway.py
@@ -21,8 +21,11 @@ from sympy.physics.mechanics import (
     ReferenceFrame,
     dynamicsymbols,
 )
-from sympy.physics.mechanics.wrapping_geometry import (Cylinder, GeometryBase,
-                                                       Sphere)
+from sympy.physics.mechanics.wrapping_geometry import (
+    Cylinder,
+    GeometryBase,
+    Sphere,
+)
 from sympy.physics.mechanics._pathway import (
     LinearPathway,
     PathwayBase,
