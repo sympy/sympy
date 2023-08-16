defb37be0af438bbf6713d66bac15c8169101434
Sam Brockie <sambrockie@icloud.com>
2023-08-16 17:23:14 +0200

    Remove type hints, type hint-related imports, type hint formatting

--- sympy/physics/mechanics/actuator.py
+++ sympy/physics/mechanics/actuator.py
@@ -1,7 +1,5 @@
 """Implementations of actuators for linked force and torque application."""
 
-from __future__ import annotations
-
 from abc import ABC, abstractmethod
 
 from sympy.core.backend import S, sympify
