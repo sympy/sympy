f652d4c10d20ce3837034d89ac924eec9f0e5593
Davide Sandonà <sandona.davide@gmail.com>
2023-08-17 00:06:59 +0200

    remove import from sympy.physics.mechanics

--- sympy/plotting/tests/test_utils.py
+++ sympy/plotting/tests/test_utils.py
@@ -3,6 +3,8 @@ from sympy import (
     symbols, Expr, Tuple, Integer, cos, solveset, FiniteSet, ImageSet)
 from sympy.plotting.utils import (
     _create_ranges, _plot_sympify, extract_solution)
+from sympy.physics.mechanics import ReferenceFrame, Vector as MechVector
+from sympy.vector import CoordSys3D, Vector
 
 
 def test_plot_sympify():
@@ -42,6 +44,19 @@ def test_plot_sympify():
     assert isinstance(r[1][1], Tuple)
     assert isinstance(r[1][1][1], Integer)
     assert isinstance(r[1][2], str)
+
+    # vectors from sympy.physics.vectors module are not sympified
+    # vectors from sympy.vectors are sympified
+    # in both cases, no error should be raised
+    R = ReferenceFrame("R")
+    v1 = 2 * R.x + R.y
+    C = CoordSys3D("C")
+    v2 = 2 * C.i + C.j
+    args = (v1, v2)
+    r = _plot_sympify(args)
+    assert isinstance(r, (list, tuple, Tuple)) and len(r) == 2
+    assert isinstance(v1, MechVector)
+    assert isinstance(v2, Vector)
 
 
 def test_create_ranges():
