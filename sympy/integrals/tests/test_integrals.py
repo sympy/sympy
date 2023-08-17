7570ee4e979c8a830b2d13707e098d2fc2786378
Nico Schlömer <nico.schloemer@gmail.com>
2023-08-17 18:36:06 +0200

    uncomment more tests

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -2105,9 +2105,9 @@ def test_issue_19427():
     x = Symbol("x")
 
     # Have always been okay:
-    # assert integrate((x ** 4) * sqrt(1 - x ** 2), (x, -1, 1)) == pi / 16
-    # assert integrate((-2 * x ** 2) * sqrt(1 - x ** 2), (x, -1, 1)) == -pi / 4
-    # assert integrate((1) * sqrt(1 - x ** 2), (x, -1, 1)) == pi / 2
+    assert integrate((x ** 4) * sqrt(1 - x ** 2), (x, -1, 1)) == pi / 16
+    assert integrate((-2 * x ** 2) * sqrt(1 - x ** 2), (x, -1, 1)) == -pi / 4
+    assert integrate((1) * sqrt(1 - x ** 2), (x, -1, 1)) == pi / 2
 
     # Sum of the above, used to incorrectly return 0 for a while:
     assert integrate((x ** 4 - 2 * x ** 2 + 1) * sqrt(1 - x ** 2), (x, -1, 1)) == 5 * pi / 16
