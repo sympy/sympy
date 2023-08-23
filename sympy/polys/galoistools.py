7670e0e9ab6490dc1e5a02fcdde22cdf4baf75ad
haru-44 <36563693+haru-44@users.noreply.github.com>
2023-08-23 21:42:52 +0900

    Set the initial value of seed to `None`

--- sympy/polys/galoistools.py
+++ sympy/polys/galoistools.py
@@ -2,7 +2,7 @@
 
 from math import ceil as _ceil, sqrt as _sqrt, prod
 
-from sympy.core.random import uniform, randint, seed as seed_func
+from sympy.core.random import uniform, _randint
 from sympy.external.gmpy import SYMPY_INTS, MPZ, invert
 from sympy.polys.polyconfig import query
 from sympy.polys.polyerrors import ExactQuotientFailed
@@ -2328,7 +2328,7 @@ def _raise_mod_power(x, s, p, f):
     return linear_congruence(alpha, beta, p)
 
 
-def _csolve_prime_las_vegas(f, p, seed=1234):
+def _csolve_prime_las_vegas(f, p, seed=None):
     r""" Solutions of `f(x) \equiv 0 \pmod{p}`, `f(0) \not\equiv 0 \pmod{p}`.
 
     Explanation
@@ -2386,7 +2386,7 @@ def _csolve_prime_las_vegas(f, p, seed=1234):
     """
     from sympy.polys.domains import ZZ
     from sympy.ntheory import sqrt_mod
-    seed_func(seed)
+    randint = _randint(seed)
     root = set()
     g = gf_pow_mod([1, 0], p - 1, f, p, ZZ)
     g = gf_sub_ground(g, 1, p, ZZ)
