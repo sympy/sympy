b3d03b2880c3a9453d4f08c81e91450daa3f95fc
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-08-24 14:00:59 -0700

    clean up code so it's ready to merge

--- sympy/logic/algorithms/z3_wrapper.py
+++ sympy/logic/algorithms/z3_wrapper.py
@@ -3,6 +3,15 @@ from sympy.assumptions.assume import AppliedPredicate
 from sympy.assumptions.cnf import EncodedCNF
 from sympy.assumptions.ask import Q
 
+from sympy.core import Add, Mul
+from sympy.core.relational import Equality, LessThan, GreaterThan, StrictLessThan, StrictGreaterThan
+from sympy.functions.elementary.complexes import Abs
+from sympy.functions.elementary.exponential import Pow
+from sympy.functions.elementary.miscellaneous import Min, Max
+from sympy.logic.boolalg import And, Or, Xor, Implies
+from sympy.logic.boolalg import Not, ITE
+from sympy.assumptions.relation.equality import StrictGreaterThanPredicate, StrictLessThanPredicate, GreaterThanPredicate, LessThanPredicate, EqualityPredicate
+
 def z3_satisfiable(expr, all_models=False):
     if not isinstance(expr, EncodedCNF):
         exprs = EncodedCNF()
@@ -27,7 +36,6 @@ def clause_to_assertion(clause):
     return "(assert (or " + " ".join(clause_strings) + "))"
 
 
-
 def encoded_cnf_to_z3_solver(enc_cnf, z3):
     def dummify_bool(pred):
         return False
@@ -43,9 +51,6 @@ def encoded_cnf_to_z3_solver(enc_cnf, z3):
     declarations = [f"(declare-const d{var} Bool)" for var in enc_cnf.variables]
     assertions = [clause_to_assertion(clause) for clause in enc_cnf.data]
 
-    #declarations = [f"(declare-const d{var} Bool)" for var in enc_cnf.variables]
-    #assertions = [clause_to_assertion(clause) for clause in enc_cnf.data]
-
     symbols = set()
     for pred, enc in enc_cnf.encoding.items():
         if not isinstance(pred, AppliedPredicate):
@@ -53,17 +58,7 @@ def encoded_cnf_to_z3_solver(enc_cnf, z3):
         if pred.function not in (Q.gt, Q.lt, Q.ge, Q.le, Q.ne, Q.eq, Q.positive, Q.negative, Q.extended_negative, Q.extended_positive, Q.zero, Q.nonzero, Q.nonnegative, Q.nonpositive, Q.extended_nonzero, Q.extended_nonnegative, Q.extended_nonpositive):
             continue
 
-
-        try:
-            pred_str = smtlib_code(pred, auto_declare=False, auto_assert=False, known_functions=known_functions)
-        except KeyError:
-            # Sometimes pred can contain a matrix or something else the smtlib printer
-            # doesn't know how to print.
-            continue
-        except TypeError:
-            # assert ask(Q.finite(log(x))) is None
-            continue
-
+        pred_str = smtlib_code(pred, auto_declare=False, auto_assert=False, known_functions=known_functions)
 
         symbols |= pred.free_symbols
         pred = pred_str
@@ -74,27 +69,12 @@ def encoded_cnf_to_z3_solver(enc_cnf, z3):
     for sym in symbols:
         declarations.append(f"(declare-const {sym} Real)")
 
-
     declarations = "\n".join(declarations)
     assertions = "\n".join(assertions)
-    #print(declarations + "\n" + assertions)
     s.from_string(declarations)
     s.from_string(assertions)
-    #s.from_string(declarations + assertions)
 
     return s
-
-
-from sympy.core import Add, Mul
-from sympy.core.relational import Equality, LessThan, GreaterThan, StrictLessThan, StrictGreaterThan
-from sympy.functions.elementary.complexes import Abs
-from sympy.functions.elementary.exponential import Pow
-from sympy.functions.elementary.miscellaneous import Min, Max
-from sympy.logic.boolalg import And, Or, Xor, Implies
-from sympy.logic.boolalg import Not, ITE
-from sympy.assumptions.assume import AppliedPredicate
-from sympy.assumptions.ask import Q
-from sympy.assumptions.relation.equality import StrictGreaterThanPredicate, StrictLessThanPredicate, GreaterThanPredicate, LessThanPredicate, EqualityPredicate
 
 
 known_functions = {
