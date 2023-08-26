22c33ec9078921fd86f251d5fa8299101c375ed0
Christopher Smith <smichr@gmail.com>
2023-08-26 07:20:59 -0500

    remove _aresame and _ne (now unused)

--- sympy/core/basic.py
+++ sympy/core/basic.py
@@ -2107,59 +2107,6 @@ class Atom(Basic):
         ' to make a check for Atoms in the calling code.')
 
 
-def _aresame(a, b):
-    """Return True if a and b are structurally the same, else False.
-
-    Examples
-    ========
-
-    In SymPy (unlike Python) two numbers do not compare the same if they are
-    not of the same type. In particular an Integer or Rational will always
-    compare unequal with a Float. Also Floats of differing precision will
-    compare unequal.
-
-    >>> from sympy import S
-    >>> 2.0 == S(2)
-    False
-    >>> 0.5 == S.Half
-    False
-
-    Before SymPy 1.7 the two comparisons above would have given True rather
-    than False. This routine was written to provide a query that could
-    distinguish those cases but is no longer needed after the changes to Float
-    comparison introduced in SymPy 1.7. Its usage is
-
-    >>> from sympy.core.basic import _aresame
-    >>> _aresame(S(2.0), S(2))
-    False
-
-    """
-    from .numbers import Number
-    from .function import AppliedUndef, UndefinedFunction as UndefFunc
-    if isinstance(a, Number) and isinstance(b, Number):
-        return a == b and a.__class__ == b.__class__
-    for i, j in zip_longest(_preorder_traversal(a), _preorder_traversal(b)):
-        if i != j or type(i) != type(j):
-            if ((isinstance(i, UndefFunc) and isinstance(j, UndefFunc)) or
-                (isinstance(i, AppliedUndef) and isinstance(j, AppliedUndef))):
-                if i.class_key() != j.class_key():
-                    return False
-            else:
-                return False
-    return True
-
-
-def _ne(a, b):
-    # use this as a second test after `a != b` if you want to make
-    # sure that things are truly equal, e.g.
-    # a, b = 0.5, S.Half
-    # a !=b or _ne(a, b) -> True
-    from .numbers import Number
-    # 0.5 == S.Half
-    if isinstance(a, Number) and isinstance(b, Number):
-        return a.__class__ != b.__class__
-
-
 def _atomic(e, recursive=False):
     """Return atom-like quantities as far as substitution is
     concerned: Derivatives, Functions and Symbols. Do not
