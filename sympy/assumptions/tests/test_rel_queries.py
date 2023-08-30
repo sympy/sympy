800182dab84b61631e306db82da334f94d50b6f2
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-08-29 19:33:16 -0700

    ask: added tests involving inequalities

--- sympy/assumptions/tests/test_rel_queries.py
+++ sympy/assumptions/tests/test_rel_queries.py
@@ -1,5 +1,5 @@
 from sympy.assumptions.lra_satask import lra_satask
-from sympy.assumptions.ask import Q
+from sympy.assumptions.ask import Q, ask
 
 from sympy.core import symbols
 
@@ -14,3 +14,9 @@ def test_lra_satask():
     assert lra_satask(Q.ne(x, 0), Q.eq(x, 0)) is False
 
     assert lra_satask(Q.gt(x, 0), Q.gt(x, 1)) is True
+
+
+def test_rel_queries():
+    x, y, z = symbols("x y z", real=True)
+    assert ask(x + y > 2, (x < 0) & (y <0)) is False
+    assert ask(x > z, (x > y) & (y > z)) is True
