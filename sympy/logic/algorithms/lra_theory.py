ed6ff9a486d78c78015f3088eff53f6d31f4fb18
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-08-29 21:28:15 -0700

    move helper methods and classes to bottom of file

--- sympy/logic/algorithms/lra_theory.py
+++ sympy/logic/algorithms/lra_theory.py
@@ -15,223 +15,22 @@ from sympy.matrices.dense import Matrix
 from sympy.matrices.common import MatrixKind
 from sympy.core.kind import NumberKind
 
-
-def sep_const_coeff(expr):
-    if isinstance(expr, Add):
-        return expr, sympify(1)
-
-    if isinstance(expr, Mul):
-        coeffs = expr.args
-    else:
-        coeffs = [expr]
-
-    var, const = [], []
-    for c in coeffs:
-        c = sympify(c)
-        if len(c.free_symbols)==0:
-            const.append(c)
-        else:
-            var.append(c)
-    return Mul(*var), Mul(*const)
-
-
-def list_terms(expr):
-    if not isinstance(expr, Add):
-        return [expr]
-
-    return expr.args
-
-
-def sep_const_terms(expr):
-    if isinstance(expr, Add):
-        terms = expr.args
-    else:
-        terms = [expr]
-
-    var, const = [], []
-    for t in terms:
-        if len(t.free_symbols) == 0:
-            const.append(t)
-        else:
-            var.append(t)
-    return sum(var), sum(const)
-
-
-def standardize_binrel(prop):
-    assert prop.function in [Q.ge, Q.gt, Q.le, Q.lt, Q.eq]
-
-    expr = prop.lhs - prop.rhs
-    if prop.function in [Q.ge, Q.gt]:
-        expr = -expr
-    var, const = sep_const_terms(expr)
-
-    if prop.function == Q.eq:
-        return Q.eq(var, const)
-    if prop.function in [Q.gt, Q.lt]:
-        return Q.lt(var, const)
-    else:
-        return Q.le(var, const)
-
-def _eval_binrel(binrel):
-    if not (len(binrel.lhs.free_symbols) == 0 and len(binrel.rhs.free_symbols) == 0):
-        return binrel
-    if binrel.function == Q.lt:
-        res = binrel.lhs < binrel.rhs
-    if binrel.function == Q.gt:
-        res = binrel.lhs > binrel.rhs
-    if binrel.function == Q.le:
-        res = binrel.lhs <= binrel.rhs
-    if binrel.function == Q.ge:
-        res = binrel.lhs >= binrel.rhs
-    if binrel.function == Q.eq:
-        res = Eq(binrel.lhs, binrel.rhs)
-    if binrel.function == Q.ne:
-        res = Ne(binrel.lhs, binrel.rhs)
-
-    if res == True or res == False:
-        return res
-    else:
-        return None
-
-
-class Boundry:
-    """
-    Represents an upper or lower bound or an equality between a symbol
-    and some constant.
-    """
-    def __init__(self, var, const, upper, equality, strict=None):
-        if not equality in [True, False]:
-            assert equality in [True, False]
-
-
-        self.var = var
-        if isinstance(const, tuple):
-            s = const[1] != 0
-            if strict:
-                assert s == strict
-            self.bound = const[0]
-            self.strict = s
-        else:
-            self.bound = const
-            self.strict = strict
-        self.upper = upper if not equality else None
-        self.equality = equality
-        self.strict = strict
-        assert self.strict is not None
-
-    @staticmethod
-    def from_upper(var):
-        neg = -1 if var.upper_from_neg else 1
-        b = Boundry(var, var.upper[0], True, var.upper_from_eq, var.upper[1] != 0)
-        if neg < 0:
-            b = b.get_negated()
-        return b, neg
-
-    @staticmethod
-    def from_lower(var):
-        neg = -1 if var.lower_from_neg else 1
-        b = Boundry(var, var.lower[0], False, var.lower_from_eq, var.lower[1] != 0)
-        if neg < 0:
-            b = b.get_negated()
-        return b, neg
-
-    def get_negated(self):
-        return Boundry(self.var, self.bound, not self.upper, self.equality, not self.strict)
-
-    def get_inequality(self):
-        if self.equality:
-            return Eq(self.var.var, self.bound)
-        elif self.upper and self.strict:
-            return self.var.var < self.bound
-        elif not self.upper and self.strict:
-            return self.var.var > self.bound
-        elif self.upper:
-            return self.var.var <= self.bound
-        else:
-            return self.var.var >= self.bound
-
-    def __repr__(self):
-        return repr("Boundry(" + repr(self.get_inequality()) + ")")
-
-    def __eq__(self, other):
-        other = (other.var, other.bound, other.strict, other.upper, other.equality)
-        return (self.var, self.bound, self.strict, self.upper, self.equality) == other
-
-    def __hash__(self):
-        return hash((self.var, self.bound, self.strict, self.upper, self.equality))
-
-
-class LRARational():
-    """
-    Represents a rational plus or minus some amount
-    of arbitrary small deltas.
-    """
-    def __init__(self, rational, delta):
-        self.value = (rational, delta)
-
-    def __lt__(self, other):
-        return self.value < other.value
-
-    def __le__(self, other):
-        return self.value <= other.value
-
-    def __eq__(self, other):
-        return self.value == other.value
-
-    def __add__(self, other):
-        return LRARational(self.value[0] + other.value[0], self.value[1] + other.value[1])
-
-    def __sub__(self, other):
-        return LRARational(self.value[0] - other.value[0], self.value[1] - other.value[1])
-
-    def __mul__(self, other):
-        assert not isinstance(other, LRARational)
-        return LRARational(self.value[0] * other, self.value[1] * other)
-
-    def __getitem__(self, index):
-        return self.value[index]
-
-    def __repr__(self):
-        return repr(self.value)
-
-
-class LRAVariable():
-    def __init__(self, var):
-        self.upper = LRARational(float("inf"), 0)
-        self.upper_from_eq = False
-        self.upper_from_neg = False
-        self.lower = LRARational(-float("inf"), 0)
-        self.lower_from_eq = False
-        self.lower_from_neg = False
-        self.assign = LRARational(0,0)
-        self.var = var
-        self.col_idx = None
-
-    def __repr__(self):
-        return repr(self.var)
-
-    def __eq__(self, other):
-        return other.var == self.var
-
-    def __hash__(self):
-        return hash(self.var)
-
 class UnhandledNumber(Exception):
     pass
-
 
 ALLOWED_PRED = {Q.eq, Q.gt, Q.lt, Q.le, Q.ge}
 HANDLE_NEGATION = True
 
 class LRASolver():
     """
-    Linear Arithmatic Solver for DPLL(T) implemented with algorithm based on
-    the Dual Simplex method and Bland's pivoting rule.
+    Linear Arithmatic Solver for DPLL(T) implemented with an algorithm based on
+    the Dual Simplex method. Uses Bland's pivoting rule to avoid cycling.
 
     TODO:
      - Implement and utilize backtracking
      - Handle non-rational real numbers
      - Handle positive and negative infinity
+     - Simplify matrix by removing unused variables using Gaussian elimination
 
     References
     ==========
@@ -245,12 +44,14 @@ class LRASolver():
         """
         Use the "from_encoded_cnf" method to create a new LRASolver.
         """
+        self.run_checks = testing_mode
+        self.x_subs = x_subs
+        self.s_subs = s_subs
+
         if any(not isinstance(a, Rational) for a in A):
             raise UnhandledNumber
         if any(not isinstance(b.bound, Rational) and (b.bound != oo) and (b.bound != -oo) for b in enc_to_boundry.values()):
             raise UnhandledNumber
-
-        self.run_checks = testing_mode # set to True to turn on assert statements
         m, n = len(slack_variables), len(slack_variables)+len(nonslack_variables)
         if m != 0:
             assert A.shape == (m, n)
@@ -260,56 +61,15 @@ class LRASolver():
 
         self.enc_to_boundry = enc_to_boundry
         self.boundry_to_enc = {value: key for key, value in enc_to_boundry.items()}
-
-        self.A = A # TODO: Row reduce A
+        self.A = A
         self.slack = slack_variables
         self.nonslack = nonslack_variables
         self.all_var = nonslack_variables + slack_variables
 
-        self.x_subs = x_subs
-        self.s_subs = s_subs
+        self.slack_set = set(slack_variables)
 
-        # if previously we knew this was sat
-        # if no assertions about slack variables have been made since
-        # and changing bounds on nonslack hasn't lead to unsat
         self.is_sat = True
-        self.slack_set = set(slack_variables) # used for checking if var is slack
-
-
-        # always one of: (True, Assignment), (False, ConflictClause), None
-        self.result = None
-
-    # @staticmethod
-    # def _remove_uneeded_variables(A, nonbasic, num_unused):
-    #     """
-    #     WIP speed up. See the following cs stack exchange for more information.
-    #     cs.stackexchange.com/questions/161709/a-fast-linear-arithmetic-solver-how-can-gaussian-elimination-be-used-to-simplif/161713#161713
-    #     """
-    #     M = A.copy()
-    #     if num_unused == 0:
-    #         return M, nonbasic
-    #
-    #     rref, pivs = M.rref()
-    #
-    #     removed = set()
-    #     for col in pivs:
-    #         if col >= num_unused:
-    #             break
-    #
-    #         # col in terms of other variables
-    #         sol = rref[col, :]
-    #         if sol*M[col, col] == M[col, :]:
-    #             continue
-    #
-    #         removed.add(col)
-    #         for row in range(M.shape[0]):
-    #             M[row, :] = M[row, :]- sol*M[row, col]
-    #
-    #     not_removed = [col for col in range(M.shape[1]) if col not in removed]
-    #     M = M[:,not_removed]
-    #     nonbasic = [nonbasic[nr] for nr in not_removed if nr < len(nonbasic)]
-    #
-    #     return M, nonbasic
+        self.result = None  # always one of: (True, assignment), (False, conflict clause), None
 
     @staticmethod
     def from_encoded_cnf(encoded_cnf, testing_mode=False):
@@ -502,7 +262,7 @@ class LRASolver():
         sym, c, negated = boundry.var, boundry.bound, enc_constraint < 0
 
         if boundry.equality and negated:
-            return None # negated equality is not handled and should only appear as conflict clause
+            return None # negated equality is not handled and should only appear in conflict clauses
 
         upper = boundry.upper != negated
         if boundry.strict != negated:
@@ -801,3 +561,205 @@ class LRASolver():
                 A[row, :] = A[row, :] + A[row, j] * A[i, :]
 
         return A
+
+
+def sep_const_coeff(expr):
+    if isinstance(expr, Add):
+        return expr, sympify(1)
+
+    if isinstance(expr, Mul):
+        coeffs = expr.args
+    else:
+        coeffs = [expr]
+
+    var, const = [], []
+    for c in coeffs:
+        c = sympify(c)
+        if len(c.free_symbols)==0:
+            const.append(c)
+        else:
+            var.append(c)
+    return Mul(*var), Mul(*const)
+
+
+def list_terms(expr):
+    if not isinstance(expr, Add):
+        return [expr]
+
+    return expr.args
+
+
+def sep_const_terms(expr):
+    if isinstance(expr, Add):
+        terms = expr.args
+    else:
+        terms = [expr]
+
+    var, const = [], []
+    for t in terms:
+        if len(t.free_symbols) == 0:
+            const.append(t)
+        else:
+            var.append(t)
+    return sum(var), sum(const)
+
+
+def standardize_binrel(prop):
+    assert prop.function in [Q.ge, Q.gt, Q.le, Q.lt, Q.eq]
+
+    expr = prop.lhs - prop.rhs
+    if prop.function in [Q.ge, Q.gt]:
+        expr = -expr
+    var, const = sep_const_terms(expr)
+
+    if prop.function == Q.eq:
+        return Q.eq(var, const)
+    if prop.function in [Q.gt, Q.lt]:
+        return Q.lt(var, const)
+    else:
+        return Q.le(var, const)
+
+
+def _eval_binrel(binrel):
+    if not (len(binrel.lhs.free_symbols) == 0 and len(binrel.rhs.free_symbols) == 0):
+        return binrel
+    if binrel.function == Q.lt:
+        res = binrel.lhs < binrel.rhs
+    if binrel.function == Q.gt:
+        res = binrel.lhs > binrel.rhs
+    if binrel.function == Q.le:
+        res = binrel.lhs <= binrel.rhs
+    if binrel.function == Q.ge:
+        res = binrel.lhs >= binrel.rhs
+    if binrel.function == Q.eq:
+        res = Eq(binrel.lhs, binrel.rhs)
+    if binrel.function == Q.ne:
+        res = Ne(binrel.lhs, binrel.rhs)
+
+    if res == True or res == False:
+        return res
+    else:
+        return None
+
+
+class Boundry:
+    """
+    Represents an upper or lower bound or an equality between a symbol
+    and some constant.
+    """
+    def __init__(self, var, const, upper, equality, strict=None):
+        if not equality in [True, False]:
+            assert equality in [True, False]
+
+
+        self.var = var
+        if isinstance(const, tuple):
+            s = const[1] != 0
+            if strict:
+                assert s == strict
+            self.bound = const[0]
+            self.strict = s
+        else:
+            self.bound = const
+            self.strict = strict
+        self.upper = upper if not equality else None
+        self.equality = equality
+        self.strict = strict
+        assert self.strict is not None
+
+    @staticmethod
+    def from_upper(var):
+        neg = -1 if var.upper_from_neg else 1
+        b = Boundry(var, var.upper[0], True, var.upper_from_eq, var.upper[1] != 0)
+        if neg < 0:
+            b = b.get_negated()
+        return b, neg
+
+    @staticmethod
+    def from_lower(var):
+        neg = -1 if var.lower_from_neg else 1
+        b = Boundry(var, var.lower[0], False, var.lower_from_eq, var.lower[1] != 0)
+        if neg < 0:
+            b = b.get_negated()
+        return b, neg
+
+    def get_negated(self):
+        return Boundry(self.var, self.bound, not self.upper, self.equality, not self.strict)
+
+    def get_inequality(self):
+        if self.equality:
+            return Eq(self.var.var, self.bound)
+        elif self.upper and self.strict:
+            return self.var.var < self.bound
+        elif not self.upper and self.strict:
+            return self.var.var > self.bound
+        elif self.upper:
+            return self.var.var <= self.bound
+        else:
+            return self.var.var >= self.bound
+
+    def __repr__(self):
+        return repr("Boundry(" + repr(self.get_inequality()) + ")")
+
+    def __eq__(self, other):
+        other = (other.var, other.bound, other.strict, other.upper, other.equality)
+        return (self.var, self.bound, self.strict, self.upper, self.equality) == other
+
+    def __hash__(self):
+        return hash((self.var, self.bound, self.strict, self.upper, self.equality))
+
+
+class LRARational():
+    """
+    Represents a rational plus or minus some amount
+    of arbitrary small deltas.
+    """
+    def __init__(self, rational, delta):
+        self.value = (rational, delta)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __add__(self, other):
+        return LRARational(self.value[0] + other.value[0], self.value[1] + other.value[1])
+
+    def __sub__(self, other):
+        return LRARational(self.value[0] - other.value[0], self.value[1] - other.value[1])
+
+    def __mul__(self, other):
+        assert not isinstance(other, LRARational)
+        return LRARational(self.value[0] * other, self.value[1] * other)
+
+    def __getitem__(self, index):
+        return self.value[index]
+
+    def __repr__(self):
+        return repr(self.value)
+
+
+class LRAVariable():
+    def __init__(self, var):
+        self.upper = LRARational(float("inf"), 0)
+        self.upper_from_eq = False
+        self.upper_from_neg = False
+        self.lower = LRARational(-float("inf"), 0)
+        self.lower_from_eq = False
+        self.lower_from_neg = False
+        self.assign = LRARational(0,0)
+        self.var = var
+        self.col_idx = None
+
+    def __repr__(self):
+        return repr(self.var)
+
+    def __eq__(self, other):
+        return other.var == self.var
+
+    def __hash__(self):
+        return hash(self.var)
