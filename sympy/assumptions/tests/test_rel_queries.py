183d8d42deea4c71092c30e641ba5418064407ae
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-08-30 12:38:11 -0700

    fixed an edge case

--- sympy/assumptions/tests/test_rel_queries.py
+++ sympy/assumptions/tests/test_rel_queries.py
@@ -1,10 +1,12 @@
 from sympy.assumptions.lra_satask import lra_satask
 from sympy.assumptions.ask import Q, ask
 
-from sympy.core import symbols
+from sympy.core import symbols, Symbol
+from sympy.core.numbers import I
 
 def test_lra_satask():
     x = symbols("x", real=True)
+    im = Symbol('im', imaginary=True)
 
     # test preprocessing of unequalities is working correctly
     assert lra_satask(Q.eq(x, 1), ~Q.ne(x, 0)) is False
@@ -19,10 +21,12 @@ def test_lra_satask():
     assert lra_satask(Q.gt(x, 0), True) is None
     assert lra_satask(Q.gt(x, 0), False) is None
 
+    # check imaginary numbers are correctly handled
+    assert lra_satask(Q.gt(im * I, 0), Q.gt(im * I, 0)) is None  # (im * I).is_real returns True so this is an edge case
+
 
 def test_rel_queries():
     x, y, z = symbols("x y z", real=True)
     assert ask(Q.positive(x - z), (x > y) & (y > z)) is True
-
     assert ask(x + y > 2, (x < 0) & (y <0)) is False
     assert ask(x > z, (x > y) & (y > z)) is True
