3c97c4c0163989ca5c4c524a398501c3c859e571
haru-44 <36563693+haru-44@users.noreply.github.com>
2023-09-04 22:19:34 +0900

    Fix `is_euler_pseudoprime`
    There are several things called Euler pseudoprime. However, the current
    implementation is different from the docstring description and wikipedia.
    So we modified the implementation to be in line with the docstring definition.

--- sympy/ntheory/tests/test_primetest.py
+++ sympy/ntheory/tests/test_primetest.py
@@ -1,30 +1,44 @@
+from math import gcd
+
 from sympy.ntheory.generate import Sieve, sieve
 from sympy.ntheory.primetest import (mr, _lucas_sequence, _lucas_selfridge_params, _lucas_extrastrong_params,
                                      is_lucas_prp, is_square,
                                      is_strong_lucas_prp, is_extra_strong_lucas_prp, isprime, is_euler_pseudoprime,
                                      is_gaussian_prime)
 
-from sympy.testing.pytest import slow
+from sympy.testing.pytest import slow, raises
 from sympy.core.numbers import I
 
 def test_euler_pseudoprimes():
-    assert is_euler_pseudoprime(9, 1) == True
-    assert is_euler_pseudoprime(341, 2) == False
-    assert is_euler_pseudoprime(121, 3) == True
-    assert is_euler_pseudoprime(341, 4) == True
-    assert is_euler_pseudoprime(217, 5) == False
-    assert is_euler_pseudoprime(185, 6) == False
-    assert is_euler_pseudoprime(55, 111) == True
-    assert is_euler_pseudoprime(115, 114) == True
-    assert is_euler_pseudoprime(49, 117) == True
-    assert is_euler_pseudoprime(85, 84) == True
-    assert is_euler_pseudoprime(87, 88) == True
-    assert is_euler_pseudoprime(49, 128) == True
-    assert is_euler_pseudoprime(39, 77) == True
-    assert is_euler_pseudoprime(9881, 30) == True
-    assert is_euler_pseudoprime(8841, 29) == False
-    assert is_euler_pseudoprime(8421, 29) == False
-    assert is_euler_pseudoprime(9997, 19) == True
+    assert is_euler_pseudoprime(17, 6)
+    assert is_euler_pseudoprime(101, 7)
+    assert is_euler_pseudoprime(1009, 10)
+    assert is_euler_pseudoprime(11287, 41)
+
+    raises(ValueError, lambda: is_euler_pseudoprime(0, 4))
+    raises(ValueError, lambda: is_euler_pseudoprime(3, 1))
+    raises(ValueError, lambda: is_euler_pseudoprime(15, 6))
+
+    # A006970
+    euler_prp = [341, 561, 1105, 1729, 1905, 2047, 2465, 3277,
+                 4033, 4681, 5461, 6601, 8321, 8481, 10261, 10585]
+    for p in euler_prp:
+        assert is_euler_pseudoprime(p, 2)
+
+    # A048950
+    euler_prp = [121, 703, 1729, 1891, 2821, 3281, 7381, 8401, 8911, 10585,
+                 12403, 15457, 15841, 16531, 18721, 19345, 23521, 24661, 28009]
+    for p in euler_prp:
+        assert is_euler_pseudoprime(p, 3)
+
+    # A033181
+    absolute_euler_prp = [1729, 2465, 15841, 41041, 46657, 75361,
+                          162401, 172081, 399001, 449065, 488881]
+    for p in absolute_euler_prp:
+        for a in range(2, p):
+            if gcd(a, p) != 1:
+                continue
+            assert is_euler_pseudoprime(p, a)
 
 
 def test_lucas_sequence():
