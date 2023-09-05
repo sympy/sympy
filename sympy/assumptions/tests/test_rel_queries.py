8eea156a8ba13c5e30f2a72ba1c86830423594fa
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-09-04 17:31:17 -0700

    expand allowed pred to include ignorable pred

--- sympy/assumptions/tests/test_rel_queries.py
+++ sympy/assumptions/tests/test_rel_queries.py
@@ -6,9 +6,9 @@ from sympy.matrices.expressions.matexpr import MatrixSymbol
 from sympy.core.numbers import I
 
 from sympy.testing.pytest import raises
+x, y, z = symbols("x y z", real=True)
 
 def test_lra_satask():
-    x = symbols("x", real=True)
     im = Symbol('im', imaginary=True)
 
     # test preprocessing of unequalities is working correctly
@@ -33,7 +33,6 @@ def test_lra_satask():
 
 
 def test_rel_queries():
-    x, y, z = symbols("x y z", real=True)
     assert ask(Q.lt(x, 2) & Q.gt(x, 3)) is False
     assert ask(Q.positive(x - z), (x > y) & (y > z)) is True
     assert ask(x + y > 2, (x < 0) & (y <0)) is False
@@ -43,3 +42,27 @@ def test_rel_queries():
 def test_unhandled_queries():
     X = MatrixSymbol("X", 2, 2)
     assert ask(Q.lt(X, 2) & Q.gt(X, 3)) is None
+
+
+def test_all_pred():
+    # test usable pred
+    assert lra_satask(Q.extended_positive(x), (x > 2)) is True
+    assert lra_satask(Q.positive_infinite(x)) is False
+    assert lra_satask(Q.negative_infinite(x)) is False
+
+    # test benign pred
+    assert lra_satask((x > 0), (x > 2) & Q.complex(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.rational(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.irrational(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.algebraic(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.finite(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.transcendental(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.real(x)) is True
+    assert lra_satask((x > 0), (x > 2) & Q.extended_real(x)) is True
+
+    # test disallowed pred
+    assert lra_satask((x > 0), (x > 2) & Q.prime(x)) is None
+    assert lra_satask((x > 0), (x > 2) & Q.composite(x)) is None
+    assert lra_satask((x > 0), (x > 2) & Q.odd(x)) is None
+    assert lra_satask((x > 0), (x > 2) & Q.even(x)) is None
+    assert lra_satask((x > 0), (x > 2) & Q.integer(x)) is None
