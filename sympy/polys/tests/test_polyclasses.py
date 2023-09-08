49501e028715e7d8851e4a35d88d641609216146
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-09-08 12:52:24 +0100

    test: fix tests for DMP to use domain elements

--- sympy/polys/tests/test_polyclasses.py
+++ sympy/polys/tests/test_polyclasses.py
@@ -11,19 +11,19 @@ from sympy.testing.pytest import raises
 f_0, f_1, f_2, f_3, f_4, f_5, f_6 = [ f.to_dense() for f in f_polys() ]
 
 def test_DMP___init__():
-    f = DMP([[0], [], [0, 1, 2], [3]], ZZ)
+    f = DMP([[ZZ(0)], [], [ZZ(0), ZZ(1), ZZ(2)], [ZZ(3)]], ZZ)
 
     assert f.rep == [[1, 2], [3]]
     assert f.dom == ZZ
     assert f.lev == 1
 
-    f = DMP([[1, 2], [3]], ZZ, 1)
+    f = DMP([[ZZ(1), ZZ(2)], [ZZ(3)]], ZZ, 1)
 
     assert f.rep == [[1, 2], [3]]
     assert f.dom == ZZ
     assert f.lev == 1
 
-    f = DMP({(1, 1): 1, (0, 0): 2}, ZZ, 1)
+    f = DMP({(1, 1): ZZ(1), (0, 0): ZZ(2)}, ZZ, 1)
 
     assert f.rep == [[1, 0], [2]]
     assert f.dom == ZZ
@@ -45,11 +45,11 @@ def test_DMP___eq__():
 
 def test_DMP___bool__():
     assert bool(DMP([[]], ZZ)) is False
-    assert bool(DMP([[1]], ZZ)) is True
+    assert bool(DMP([[ZZ(1)]], ZZ)) is True
 
 
 def test_DMP_to_dict():
-    f = DMP([[3], [], [2], [], [8]], ZZ)
+    f = DMP([[ZZ(3)], [], [ZZ(2)], [], [ZZ(8)]], ZZ)
 
     assert f.to_dict() == \
         {(4, 0): 3, (2, 0): 2, (0, 0): 8}
@@ -60,34 +60,34 @@ def test_DMP_to_dict():
 
 def test_DMP_properties():
     assert DMP([[]], ZZ).is_zero is True
-    assert DMP([[1]], ZZ).is_zero is False
+    assert DMP([[ZZ(1)]], ZZ).is_zero is False
 
-    assert DMP([[1]], ZZ).is_one is True
-    assert DMP([[2]], ZZ).is_one is False
+    assert DMP([[ZZ(1)]], ZZ).is_one is True
+    assert DMP([[ZZ(2)]], ZZ).is_one is False
 
-    assert DMP([[1]], ZZ).is_ground is True
-    assert DMP([[1], [2], [1]], ZZ).is_ground is False
+    assert DMP([[ZZ(1)]], ZZ).is_ground is True
+    assert DMP([[ZZ(1)], [ZZ(2)], [ZZ(1)]], ZZ).is_ground is False
 
-    assert DMP([[1], [2, 0], [1, 0]], ZZ).is_sqf is True
-    assert DMP([[1], [2, 0], [1, 0, 0]], ZZ).is_sqf is False
+    assert DMP([[ZZ(1)], [ZZ(2), ZZ(0)], [ZZ(1), ZZ(0)]], ZZ).is_sqf is True
+    assert DMP([[ZZ(1)], [ZZ(2), ZZ(0)], [ZZ(1), ZZ(0), ZZ(0)]], ZZ).is_sqf is False
 
-    assert DMP([[1, 2], [3]], ZZ).is_monic is True
-    assert DMP([[2, 2], [3]], ZZ).is_monic is False
+    assert DMP([[ZZ(1), ZZ(2)], [ZZ(3)]], ZZ).is_monic is True
+    assert DMP([[ZZ(2), ZZ(2)], [ZZ(3)]], ZZ).is_monic is False
 
-    assert DMP([[1, 2], [3]], ZZ).is_primitive is True
-    assert DMP([[2, 4], [6]], ZZ).is_primitive is False
+    assert DMP([[ZZ(1), ZZ(2)], [ZZ(3)]], ZZ).is_primitive is True
+    assert DMP([[ZZ(2), ZZ(4)], [ZZ(6)]], ZZ).is_primitive is False
 
 
 def test_DMP_arithmetics():
-    f = DMP([[2], [2, 0]], ZZ)
+    f = DMP([[ZZ(2)], [ZZ(2), ZZ(0)]], ZZ)
 
-    assert f.mul_ground(2) == DMP([[4], [4, 0]], ZZ)
-    assert f.quo_ground(2) == DMP([[1], [1, 0]], ZZ)
+    assert f.mul_ground(2) == DMP([[ZZ(4)], [ZZ(4), ZZ(0)]], ZZ)
+    assert f.quo_ground(2) == DMP([[ZZ(1)], [ZZ(1), ZZ(0)]], ZZ)
 
     raises(ExactQuotientFailed, lambda: f.exquo_ground(3))
 
-    f = DMP([[-5]], ZZ)
-    g = DMP([[5]], ZZ)
+    f = DMP([[ZZ(-5)]], ZZ)
+    g = DMP([[ZZ(5)]], ZZ)
 
     assert f.abs() == g
     assert abs(f) == g
@@ -103,7 +103,7 @@ def test_DMP_arithmetics():
     assert f + 5 == h
     assert 5 + f == h
 
-    h = DMP([[-10]], ZZ)
+    h = DMP([[ZZ(-10)]], ZZ)
 
     assert f.sub(g) == h
     assert f - g == h
@@ -111,7 +111,7 @@ def test_DMP_arithmetics():
     assert f - 5 == h
     assert 5 - f == -h
 
-    h = DMP([[-25]], ZZ)
+    h = DMP([[ZZ(-25)]], ZZ)
 
     assert f.mul(g) == h
     assert f * g == h
@@ -119,7 +119,7 @@ def test_DMP_arithmetics():
     assert f * 5 == h
     assert 5 * f == h
 
-    h = DMP([[25]], ZZ)
+    h = DMP([[ZZ(25)]], ZZ)
 
     assert f.sqr() == h
     assert f.pow(2) == h
@@ -127,11 +127,11 @@ def test_DMP_arithmetics():
 
     raises(TypeError, lambda: f.pow('x'))
 
-    f = DMP([[1], [], [1, 0, 0]], ZZ)
-    g = DMP([[2], [-2, 0]], ZZ)
+    f = DMP([[ZZ(1)], [], [ZZ(1), ZZ(0), ZZ(0)]], ZZ)
+    g = DMP([[ZZ(2)], [ZZ(-2), ZZ(0)]], ZZ)
 
-    q = DMP([[2], [2, 0]], ZZ)
-    r = DMP([[8, 0, 0]], ZZ)
+    q = DMP([[ZZ(2)], [ZZ(2), ZZ(0)]], ZZ)
+    r = DMP([[ZZ(8), ZZ(0), ZZ(0)]], ZZ)
 
     assert f.pdiv(g) == (q, r)
     assert f.pquo(g) == q
@@ -139,11 +139,11 @@ def test_DMP_arithmetics():
 
     raises(ExactQuotientFailed, lambda: f.pexquo(g))
 
-    f = DMP([[1], [], [1, 0, 0]], ZZ)
-    g = DMP([[1], [-1, 0]], ZZ)
+    f = DMP([[ZZ(1)], [], [ZZ(1), ZZ(0), ZZ(0)]], ZZ)
+    g = DMP([[ZZ(1)], [ZZ(-1), ZZ(0)]], ZZ)
 
-    q = DMP([[1], [1, 0]], ZZ)
-    r = DMP([[2, 0, 0]], ZZ)
+    q = DMP([[ZZ(1)], [ZZ(1), ZZ(0)]], ZZ)
+    r = DMP([[ZZ(2), ZZ(0), ZZ(0)]], ZZ)
 
     assert f.div(g) == (q, r)
     assert f.quo(g) == q
@@ -157,9 +157,9 @@ def test_DMP_arithmetics():
 
 
 def test_DMP_functionality():
-    f = DMP([[1], [2, 0], [1, 0, 0]], ZZ)
-    g = DMP([[1], [1, 0]], ZZ)
-    h = DMP([[1]], ZZ)
+    f = DMP([[ZZ(1)], [ZZ(2), ZZ(0)], [ZZ(1), ZZ(0), ZZ(0)]], ZZ)
+    g = DMP([[ZZ(1)], [ZZ(1), ZZ(0)]], ZZ)
+    h = DMP([[ZZ(1)]], ZZ)
 
     assert f.degree() == 2
     assert f.degree_list() == (2, 2)
@@ -174,15 +174,15 @@ def test_DMP_functionality():
     assert f.max_norm() == 2
     assert f.l1_norm() == 4
 
-    u = DMP([[2], [2, 0]], ZZ)
+    u = DMP([[ZZ(2)], [ZZ(2), ZZ(0)]], ZZ)
 
     assert f.diff(m=1, j=0) == u
     assert f.diff(m=1, j=1) == u
 
     raises(TypeError, lambda: f.diff(m='x', j=0))
 
-    u = DMP([1, 2, 1], ZZ)
-    v = DMP([1, 2, 1], ZZ)
+    u = DMP([ZZ(1), ZZ(2), ZZ(1)], ZZ)
+    v = DMP([ZZ(1), ZZ(2), ZZ(1)], ZZ)
 
     assert f.eval(a=1, j=0) == u
     assert f.eval(a=1, j=1) == v
@@ -201,25 +201,25 @@ def test_DMP_functionality():
     assert (4*f).content() == ZZ(4)
     assert (4*f).primitive() == (ZZ(4), f)
 
-    f = DMP([[1], [2], [3], [4], [5], [6]], ZZ)
+    f = DMP([[ZZ(1)], [ZZ(2)], [ZZ(3)], [ZZ(4)], [ZZ(5)], [ZZ(6)]], ZZ)
 
-    assert f.trunc(3) == DMP([[1], [-1], [], [1], [-1], []], ZZ)
+    assert f.trunc(3) == DMP([[ZZ(1)], [ZZ(-1)], [], [ZZ(1)], [ZZ(-1)], []], ZZ)
 
     f = DMP(f_4, ZZ)
 
     assert f.sqf_part() == -f
     assert f.sqf_list() == (ZZ(-1), [(-f, 1)])
 
-    f = DMP([[-1], [], [], [5]], ZZ)
-    g = DMP([[3, 1], [], []], ZZ)
-    h = DMP([[45, 30, 5]], ZZ)
+    f = DMP([[ZZ(-1)], [], [], [ZZ(5)]], ZZ)
+    g = DMP([[ZZ(3), ZZ(1)], [], []], ZZ)
+    h = DMP([[ZZ(45), ZZ(30), ZZ(5)]], ZZ)
 
-    r = DMP([675, 675, 225, 25], ZZ)
+    r = DMP([ZZ(675), ZZ(675), ZZ(225), ZZ(25)], ZZ)
 
     assert f.subresultants(g) == [f, g, h]
     assert f.resultant(g) == r
 
-    f = DMP([1, 3, 9, -13], ZZ)
+    f = DMP([ZZ(1), ZZ(3), ZZ(9), ZZ(-13)], ZZ)
 
     assert f.discriminant() == -11664
 
@@ -235,33 +235,33 @@ def test_DMP_functionality():
 
     assert f.invert(g) == s
 
-    f = DMP([[1], [2], [3]], QQ)
+    f = DMP([[QQ(1)], [QQ(2)], [QQ(3)]], QQ)
 
     raises(ValueError, lambda: f.half_gcdex(f))
     raises(ValueError, lambda: f.gcdex(f))
 
     raises(ValueError, lambda: f.invert(f))
 
-    f = DMP([1, 0, 20, 0, 150, 0, 500, 0, 625, -2, 0, -10, 9], ZZ)
-    g = DMP([1, 0, 0, -2, 9], ZZ)
-    h = DMP([1, 0, 5, 0], ZZ)
+    f = DMP([ZZ(1), ZZ(0), ZZ(20), ZZ(0), ZZ(150), ZZ(0), ZZ(500), ZZ(0), ZZ(625), ZZ(-2), ZZ(0), ZZ(-10), ZZ(9)], ZZ)
+    g = DMP([ZZ(1), ZZ(0), ZZ(0), ZZ(-2), ZZ(9)], ZZ)
+    h = DMP([ZZ(1), ZZ(0), ZZ(5), ZZ(0)], ZZ)
 
     assert g.compose(h) == f
     assert f.decompose() == [g, h]
 
-    f = DMP([[1], [2], [3]], QQ)
+    f = DMP([[QQ(1)], [QQ(2)], [QQ(3)]], QQ)
 
     raises(ValueError, lambda: f.decompose())
     raises(ValueError, lambda: f.sturm())
 
 
 def test_DMP_exclude():
-    f = [[[[[[[[[[[[[[[[[[[[[[[[[[1]], [[]]]]]]]]]]]]]]]]]]]]]]]]]]
+    f = [[[[[[[[[[[[[[[[[[[[[[[[[[ZZ(1)]], [[]]]]]]]]]]]]]]]]]]]]]]]]]]
     J = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
         18, 19, 20, 21, 22, 24, 25]
 
-    assert DMP(f, ZZ).exclude() == (J, DMP([1, 0], ZZ))
-    assert DMP([[1], [1, 0]], ZZ).exclude() == ([], DMP([[1], [1, 0]], ZZ))
+    assert DMP(f, ZZ).exclude() == (J, DMP([ZZ(1), ZZ(0)], ZZ))
+    assert DMP([[ZZ(1)], [ZZ(1), ZZ(0)]], ZZ).exclude() == ([], DMP([[ZZ(1)], [ZZ(1), ZZ(0)]], ZZ))
 
 
 def test_DMF__init__():
@@ -534,17 +534,3 @@ def test_ANP_unify():
     assert b.unify(a)[0] == QQ
     assert a.unify(a)[0] == QQ
     assert b.unify(b)[0] == ZZ
-
-
-def test___hash__():
-    # issue 5571
-    # Make sure int vs. long doesn't affect hashing with Python ground types
-    assert DMP([[1, 2], [3]], ZZ) == DMP([[int(1), int(2)], [int(3)]], ZZ)
-    assert hash(DMP([[1, 2], [3]], ZZ)) == hash(DMP([[int(1), int(2)], [int(3)]], ZZ))
-    assert DMF(
-        ([[1, 2], [3]], [[1]]), ZZ) == DMF(([[int(1), int(2)], [int(3)]], [[int(1)]]), ZZ)
-    assert hash(DMF(([[1, 2], [3]], [[1]]), ZZ)) == hash(DMF(([[int(1),
-                int(2)], [int(3)]], [[int(1)]]), ZZ))
-    assert ANP([1, 1], [1, 0, 1], ZZ) == ANP([int(1), int(1)], [int(1), int(0), int(1)], ZZ)
-    assert hash(
-        ANP([1, 1], [1, 0, 1], ZZ)) == hash(ANP([int(1), int(1)], [int(1), int(0), int(1)], ZZ))
