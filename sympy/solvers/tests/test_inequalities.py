1b1ef237b80ba055b92d7b8a2fb5613adea104b9
Christopher Smith <smichr@gmail.com>
2023-09-29 16:16:43 -0500

    Update sympy/solvers/tests/test_inequalities.py

--- sympy/solvers/tests/test_inequalities.py
+++ sympy/solvers/tests/test_inequalities.py
@@ -447,8 +447,8 @@ def test_issue_10671_12466():
 
 
 def test_issue_25697():
-    assert reduce_inequalities([log(x, 3) <= 2], x) == True
-    assert reduce_inequalities([log(x, 3) < 2], x) == False
+    assert reduce_inequalities([log(x, 3) <= 2], x) == (x <= 9) & (0 < x)
+    assert reduce_inequalities([log(x, 3) < 2], x) == (x < 9) & (0 < x)
 
 
 def test__solve_inequality():
