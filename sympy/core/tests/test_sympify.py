70da1839ff08daef5b6b5c717350f508961c9564
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-10-05 00:05:35 +0100

    Remove redundant imports

--- sympy/core/tests/test_sympify.py
+++ sympy/core/tests/test_sympify.py
@@ -22,7 +22,7 @@ from sympy.core.sympify import (sympify, _sympify, SympifyError, kernS,
     CantSympify, converter)
 from sympy.core.decorators import _sympifyit
 from sympy.external import import_module
-from sympy.testing.pytest import raises, XFAIL, skip, warns_deprecated_sympy
+from sympy.testing.pytest import raises, XFAIL, skip
 from sympy.utilities.decorator import conserve_mpmath_dps
 from sympy.geometry import Point, Line
 from sympy.functions.combinatorial.factorials import factorial, factorial2
