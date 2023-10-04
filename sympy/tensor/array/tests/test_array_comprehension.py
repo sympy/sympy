2bdae01173288b38941ce52cc44ad2f2b306aa11
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-10-05 00:48:05 +0100

    Remove unneeded imports

--- sympy/tensor/array/tests/test_array_comprehension.py
+++ sympy/tensor/array/tests/test_array_comprehension.py
@@ -1,8 +1,7 @@
 from sympy.tensor.array.array_comprehension import ArrayComprehension, ArrayComprehensionMap
 from sympy.tensor.array import ImmutableDenseNDimArray
 from sympy.abc import i, j, k, l
-from sympy.testing.pytest import raises, warns
-from sympy.utilities.exceptions import SymPyDeprecationWarning
+from sympy.testing.pytest import raises
 from sympy.matrices import Matrix
 
 
