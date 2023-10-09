daa487da763bbe0eb04d2cdf146d539ba33a5364
vignesh vetrivel <vigneshvetrivel88@gmail.com>
2023-10-09 12:23:46 +0530

    modified mul.py

--- sympy/core/mul.py
+++ sympy/core/mul.py
@@ -430,8 +430,27 @@ class Mul(Expr, AssocOp):
                     #                             b    c       b+c
                     # try to combine last terms: a  * a   ->  a
                     o1 = nc_part.pop()
+                    b1, e1 = o1.as_base_exp()
+                    b2, e2 = o.as_base_exp()
+                    new_exp = e1 + e2
+                    if hasattr(o, 'diff'):
+                        nc_part.extend([o1, o])
+                    # Only allow powers to combine if the new exponent is
+                    # not an Add. This allow things like a**2*b**3 == a**5
+                    # if a.is_commutative == False, but prohibits
+                    # a**x*a**y and x**a*x**b from combining (x,y commute).
+                    elif b1 == b2 and (not new_exp.is_Add):
+                        o12 = b1 ** new_exp
 
-                    nc_part.extend([o1, o])
+                        # now o12 could be a commutative object
+                        if o12.is_commutative:
+                            seq.append(o12)
+                            continue
+                        else:
+                            nc_seq.insert(0, o12)
+
+                    else:
+                        nc_part.extend([o1, o])
 
         # We do want a combined exponent if it would not be an Add, such as
         #  y    2y     3y
