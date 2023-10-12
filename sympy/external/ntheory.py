f75bd5d759bd7c9249258ceff9b789a890998e22
haru-44 <36563693+haru-44@users.noreply.github.com>
2023-10-12 22:12:17 +0900

    Add `gcdext` to `external`

--- sympy/external/ntheory.py
+++ sympy/external/ntheory.py
@@ -116,6 +116,33 @@ else:
         return reduce(lambda x, y: x*y//math.gcd(x, y), args, 1)
 
 
+def _sign(n):
+    if n < 0:
+        return -1, -n
+    return 1, n
+
+
+def gcdext(a, b):
+    if not a or not b:
+        g = abs(a) or abs(b)
+        if not g:
+            return (0, 0, 0)
+        return (g, a // g, b // g)
+
+    x_sign, a = _sign(a)
+    y_sign, b = _sign(b)
+    x, r = 1, 0
+    y, s = 0, 1
+
+    while b:
+        q, c = divmod(a, b)
+        a, b = b, c
+        x, r = r, x - q*r
+        y, s = s, y - q*s
+
+    return (a, x * x_sign, y * y_sign)
+
+
 def is_square(x):
     """Return True if x is a square number."""
     if x < 0:
