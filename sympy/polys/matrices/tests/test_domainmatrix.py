87c302d5fef644fbdad5f4513ff7f21c1e80d4f8
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-11-15 21:13:43 +0000

    polys: fix conversion to GF(p) for large p

--- sympy/polys/matrices/tests/test_domainmatrix.py
+++ sympy/polys/matrices/tests/test_domainmatrix.py
@@ -99,6 +99,16 @@ def test_DomainMatrix_from_list():
     assert A.shape == (2, 2)
     assert A.domain == dom
 
+    dom = FF(2**127-1)
+    ddm = DDM([[dom(1), dom(2)], [dom(3), dom(4)]], (2, 2), dom)
+    A = DomainMatrix.from_list([[1, 2], [3, 4]], dom)
+    if GROUND_TYPES != 'flint':
+        assert A.rep == ddm
+    else:
+        assert A.rep == ddm.to_dfm()
+    assert A.shape == (2, 2)
+    assert A.domain == dom
+
     ddm = DDM([[QQ(1, 2), QQ(3, 1)], [QQ(1, 4), QQ(5, 1)]], (2, 2), QQ)
     A = DomainMatrix.from_list([[(1, 2), (3, 1)], [(1, 4), (5, 1)]], QQ)
     if GROUND_TYPES != 'flint':
