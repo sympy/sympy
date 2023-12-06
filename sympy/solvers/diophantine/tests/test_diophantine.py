ae96bcff5cf4b21c34ca7a37d16e543736cfb628
Christopher Smith <smichr@gmail.com>
2023-12-06 08:32:07 -0600

    Update test_diophantine.py

--- sympy/solvers/diophantine/tests/test_diophantine.py
+++ sympy/solvers/diophantine/tests/test_diophantine.py
@@ -668,7 +668,10 @@ def test_sum_of_three_squares():
 
     assert sum_of_three_squares(7) is None
     assert sum_of_three_squares((4**5)*15) is None
+    # if there are two zeros, there might be a solution
+    # with only one zero, e.g. 25 = (0, 3, 4)
     assert sum_of_three_squares(25) == (0, 0, 5)
+    assert sum_of_three_squares(49) == (2, 3, 6)
     assert sum_of_three_squares(4) == (0, 0, 2)
 
 
@@ -689,9 +692,8 @@ def test_sum_of_four_squares():
         assert all(r >= 0 for r in result)
         assert sum(r**2 for r in result) == n
         assert list(result) == sorted(result)
-        if 0 not in result:
-            nonzero += 1
-    assert nonzero == 450
+        nonzero += int(0 not in result)
+    assert nonzero == 742
 
 
 def test_power_representation():
