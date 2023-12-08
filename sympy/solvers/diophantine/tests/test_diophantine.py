8a24130fcdfcc379c59386377750ea6426947662
Christopher Smith <smichr@gmail.com>
2023-12-08 11:22:07 -0600

    Apply suggestions from code review

--- sympy/solvers/diophantine/tests/test_diophantine.py
+++ sympy/solvers/diophantine/tests/test_diophantine.py
@@ -669,9 +669,9 @@ def test_sum_of_three_squares():
     assert sum_of_three_squares(7) is None
     assert sum_of_three_squares((4**5)*15) is None
     # if there are two zeros, there might be a solution
-    # with only one zero, e.g. 25 = (0, 3, 4)
+    # with only one zero, e.g. 25 => (0, 3, 4) or
+    # with no zeros, e.g. 49 => (2, 3, 6)
     assert sum_of_three_squares(25) == (0, 0, 5)
-    assert sum_of_three_squares(49) == (2, 3, 6)
     assert sum_of_three_squares(4) == (0, 0, 2)
 
 
