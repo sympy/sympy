c8ed0f301b0a1bda2778cdac6526ce1be6b0acbf
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-12-13 13:51:40 +0000

    remove unneeded import

--- sympy/polys/domains/tests/test_polynomialring.py
+++ sympy/polys/domains/tests/test_polynomialring.py
@@ -1,6 +1,6 @@
 """Tests for the PolynomialRing classes. """
 
-from sympy.polys.domains import RR, QQ, ZZ
+from sympy.polys.domains import QQ, ZZ
 from sympy.polys.polyerrors import ExactQuotientFailed, CoercionFailed, NotReversible
 
 from sympy.abc import x, y
