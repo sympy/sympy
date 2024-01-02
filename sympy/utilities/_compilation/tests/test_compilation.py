7b16e72c5a30d00ba2fe17a0881d0159a62d7400
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-01-02 15:21:41 +0100

    utilities._compilation: allow passing extra objects

--- sympy/utilities/_compilation/tests/test_compilation.py
+++ sympy/utilities/_compilation/tests/test_compilation.py
@@ -1,3 +1,4 @@
+import atexit
 import shutil
 import os
 import subprocess
@@ -5,7 +6,7 @@ import tempfile
 from sympy.external import import_module
 from sympy.testing.pytest import skip
 
-from sympy.utilities._compilation.compilation import compile_link_import_strings, compile_sources, get_abspath
+from sympy.utilities._compilation.compilation import compile_link_import_py_ext, compile_link_import_strings, compile_sources, get_abspath
 
 numpy = import_module('numpy')
 cython = import_module('cython')
@@ -64,13 +65,30 @@ def test_compile_link_import_strings():
         if info and info['build_dir']:
             shutil.rmtree(info['build_dir'])
 
+class TemporaryDirectory:
+    """This is a variant of tempfile.TemporaryDirectory that works under pytest.
+    Running 'pytest --pdb' will cause tempfile.TemporaryDirectory to be deleted,
+    even when an assertion fails inside the with-block.
+    """
+    def __init__(self, suffix: str):
+        self.suffix = suffix
+
+    def __enter__(self):
+        self.temp_dir = tempfile.mkdtemp(self.suffix)
+        return self.temp_dir
+
+    def __exit__(self, *exc):
+        def _cleanup():
+            shutil.rmtree(self.temp_dir)
+        atexit.register(_cleanup)
+
 
 def test_compile_sources():
     from sympy.utilities._compilation import has_c
     if not has_c():
         skip("No C compiler found.")
 
-    with tempfile.TemporaryDirectory("sympy_test_compilation") as build_dir:
+    with TemporaryDirectory("sympy_test_compilation") as build_dir:
         _handle, file_path = tempfile.mkstemp('.c', dir=build_dir)
         with open(file_path, 'wt') as ofh:
             ofh.write("""
@@ -82,9 +100,20 @@ def test_compile_sources():
         obj_path = get_abspath(obj, cwd=build_dir)
         assert os.path.exists(obj_path)
         try:
-            subprocess.check_call(["nm", "--help"])
+            _ = subprocess.check_output(["nm", "--help"])
         except subprocess.CalledProcessError:
             pass  # we cannot test contents of object file
         else:
             nm_out = subprocess.check_output(["nm", obj_path])
             assert 'foo' in nm_out.decode('utf-8')
+
+        if not cython:
+            return  # the final (optional) part of the test below requires Cython.
+
+        _handle, pyx_path = tempfile.mkstemp('.pyx', dir=build_dir)
+        with open(pyx_path, 'wt') as ofh:
+            ofh.write(("cdef extern int foo(int)\n"
+                       "def _foo(arg):\n"
+                       "    return foo(arg)"))
+        mod = compile_link_import_py_ext([pyx_path], extra_objs=[obj_path], build_dir=build_dir)
+        assert mod._foo(21) == 42
