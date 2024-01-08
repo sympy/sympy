e047c1ffc99307f324f14a93298bd3195ec1f153
Mohit Kumar <mohitkumar020291@gmail.com>
2024-01-08 22:07:20 +0530

    modified vector.py

--- sympy/vector/vector.py
+++ sympy/vector/vector.py
@@ -393,16 +393,16 @@ class Vector(BasisDependent):
         try:
             # checking if the self and other are in same coordinate system or not
             if V_S == V_O:
-                try:
-                    result = [i.equal(j) for i, j in zip(S_S, S_O)]
-                    return all(result)
-                except Exception as e:
-                    for i in range(3):
-                        return all(radsimp(simplify(S_S[i])) == radsimp(simplify(S_O[i])) for i in range(3))
+                result = [i.equal(j) for i, j in zip(S_S, S_O)]
+                return all(result)
             else:
                 return False
         except Exception as e:
-            raise RuntimeError(f"An error occurred: {str(e)}") from e
+            try:
+                for i in range(3):
+                    return all(radsimp(simplify(S_S[i])) == radsimp(simplify(S_O[i])) for i in range(3))
+            except:
+                raise RuntimeError(f"An error occurred: {str(e)}") from e
 
 
 
