4eadeaa22f4b5db06ddea8ef2fd3cf40e9fca0f3
sachinSingh16-09 <sachinishu02@gmail.com>
2024-01-16 11:10:40 +0530

    test

--- sympy/solvers/tests/test_solveset.py
+++ sympy/solvers/tests/test_solveset.py
@@ -3541,4 +3541,4 @@ def test_issue_26077():
     result = stationary_points(function, x, S.Reals)
     expected_result = ConditionSet(x, Eq(x*(-5*cot(5*x)**2 - 5) + cot(5*x), 0), Complement(Reals, Union(ImageSet(Lambda(_n, simplify(2*_n*pi/5)), Integers), ImageSet(Lambda(_n, simplify(2*_n*pi/5 + pi/5)), Integers))))
 
-    assert result.args == (expected_result).args
+    assert result == expected_result
