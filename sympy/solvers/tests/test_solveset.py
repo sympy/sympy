bcf3572ab194ddc6857ba66ad3331ad5cc567a3a
sachinSingh16-09 <sachinishu02@gmail.com>
2024-01-16 12:08:44 +0530

    test

--- sympy/solvers/tests/test_solveset.py
+++ sympy/solvers/tests/test_solveset.py
@@ -3543,7 +3543,7 @@ def test_issue_26077():
     expected_result = ConditionSet(x, Eq(x*(-5*cot(5*x)**2 - 5) + cot(5*x), 0),
                                Complement(Reals,
                                           Union(ImageSet(Lambda(_n, 2*_n*pi/5), Integers),
-                                                ImageSet(Lambda(_n, 2*_n*pi/5 + pi/5), Integers))))
+                                                ImageSet(Lambda(_n, simplify(2*_n*pi/5 + pi/5)), Integers))))
     
-    modified_result = (result.args[0], result.args[1], simplify(result.args[2]))
-    assert modified_result.args == expected_result.args
+#    modified_result = (result.args[0], result.args[1], simplify(result.args[2]))
+    assert result.args == expected_result.args
