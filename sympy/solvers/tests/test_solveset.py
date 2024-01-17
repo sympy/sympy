7f54567c71d390427c35a3b2503cc3936205058d
Ravindu-Hirimuthugoda <ravindu.18@cse.mrt.ac.lk>
2024-01-17 14:14:37 +0530

    Fix type error in stationary_points

--- sympy/solvers/tests/test_solveset.py
+++ sympy/solvers/tests/test_solveset.py
@@ -3530,3 +3530,11 @@ def test_issue_22628():
 
 def test_issue_25781():
     assert solve(sqrt(x/2) - x) == [0, S.Half]
+
+def test_issue_26077():
+    _n = Symbol('_n')
+    function = x*cot(5*x)
+    critical_points = stationary_points(function, x, S.Reals)
+    excluded_points = Union(ImageSet(Lambda(_n, 2*_n*pi/5), S.Integers), ImageSet(Lambda(_n, 2*_n*pi/5 + pi/5), S.Integers))
+    solution = ConditionSet(x, Eq(x*(-5*cot(5*x)**2 - 5) + cot(5*x), 0), Complement(S.Reals, excluded_points))
+    assert solution.as_dummy() == critical_points.as_dummy()
