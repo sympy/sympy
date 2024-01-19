0943a39fff54515ba2fa162c6d9cdf580cf65529
Ankit Kumar Singh <ankitdiswar10@gmail.com>
2024-01-19 12:44:02 +0530

    Raised Value Error and Test is added.

--- sympy/stats/crv.py
+++ sympy/stats/crv.py
@@ -531,11 +531,16 @@ class SingleContinuousPSpace(ContinuousPSpace, SinglePSpace):
 
         gs = solveset(expr - y, self.value, S.Reals)
 
-        if isinstance(gs, Intersection) and S.Reals in gs.args:
-            gs = list(gs.args[1])
-
-        if not gs:
-            raise ValueError("Can not solve %s for %s"%(expr, self.value))
+        if isinstance(gs, Intersection):
+            if len(gs.args) == 2:
+                if gs.args[0] is S.Reals:
+                    gs = gs.args[1]
+                else:
+                    raise NotImplementedError("Anticipating Intersection(Reals, Set)")
+            else:
+                raise NotImplementedError("Intersection length is not 2")
+        if not gs.is_FiniteSet:
+            raise ValueError("Can not solve %s for %s" % (expr, self.value))
         fx = self.compute_density(self.value)
         fy = sum(fx(g) * abs(g.diff(y)) for g in gs)
         return Lambda(y, fy)
