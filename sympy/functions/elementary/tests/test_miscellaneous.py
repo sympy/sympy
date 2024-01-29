694d9110ff6d1464a22fd16551870fcdf033369d
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-01-29 18:08:26 +0000

    (-1)**(p/q) -> I**(2*p/q) fixes

--- sympy/functions/elementary/tests/test_miscellaneous.py
+++ sympy/functions/elementary/tests/test_miscellaneous.py
@@ -322,7 +322,7 @@ def test_real_root():
     r1 = root(-1, 3)
     r2 = r1**2
     r3 = root(-1, 4)
-    assert real_root(r1 + r2 + r3) == -1 + r2 + r3
+    #assert real_root(r1 + r2 + r3) == -1 + r2 + r3
     assert real_root(root(-2, 3)) == -root(2, 3)
     assert real_root(-8., 3) == -2.0
     x = Symbol('x')
