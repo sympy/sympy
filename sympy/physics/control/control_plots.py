b9c3c39a54f8d92497ee19c120a61b355ca08190
Soham-xT <136966898+Soham-xT@users.noreply.github.com>
2024-04-01 11:45:09 +0530

    Update control_plots.py

--- sympy/physics/control/control_plots.py
+++ sympy/physics/control/control_plots.py
@@ -1023,7 +1023,6 @@ def nyquist_numerical_data(system, initial_omega=0.01, final_omega=100, nb_of_po
 
     """
     _check_system(system)
-    
     s = system.var
     w = Dummy('w', real=True)
     
