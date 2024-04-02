cce1277201b6b3ac5c034de78613a9670beb3de8
Sam Lubelsky <sammy56lt@gmail.com>
2024-04-02 01:15:30 -0500

    Added test cases for the new __array__ methods(Copy=true/false/None)
    Currently copy=False is commented out due to a bug
    in numpy. see:
    https://github.com/numpy/numpy/issues/25941#issuecomment-2010343170

--- sympy/matrices/expressions/tests/test_matexpr.py
+++ sympy/matrices/expressions/tests/test_matexpr.py
@@ -370,6 +370,18 @@ def test_factor_expand():
     # Ideally we get the first, but we at least don't want a wrong answer
     assert factor(expr) in [I - C, B**-1*(A**-1*(I - C)*B**-1)**-1*A**-1]
 
+def test_numpy_conversion():
+    try:
+        from numpy import array, array_equal
+    except ImportError:
+        skip('NumPy must be available to test creating matrices from ndarrays')
+    A = MatrixSymbol('A', 2, 2)
+    np_array = array([[MatrixElement(A, 0, 0), MatrixElement(A, 1, 0)], 
+                     [MatrixElement(A, 0, 1), MatrixElement(A, 1, 1)]])
+    assert array_equal(array(A), np_array)
+    assert array_equal(array(A, copy=True), np_array)
+    #raises(TypeError, lambda: array(A, copy=False)) TODO: Uncomment this whenever copy variable properly passes to __array__
+
 
 def test_issue_2749():
     A = MatrixSymbol("A", 5, 2)
