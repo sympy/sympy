4301071c190adee66cac4e4a40a14bbd6787b8dc
Soham-xT <136966898+Soham-xT@users.noreply.github.com>
2024-04-05 09:54:21 +0530

    Update control_plots.py

--- sympy/physics/control/control_plots.py
+++ sympy/physics/control/control_plots.py
@@ -948,12 +948,13 @@ def bode_plot(system, initial_exp=-5, final_exp=5,
         :context: close-figs
         :format: doctest
         :include-source: True
-
+        
         >>> from sympy.abc import s
         >>> from sympy.physics.control.lti import TransferFunction
-        >>> from sympy.physics.control.control_plots import bode_plot
-        >>> tf1 = TransferFunction(1*s**2 + 0.1*s + 7.5, 1*s**4 + 0.12*s**3 + 9*s**2, s)
-        >>> bode_plot(tf1, initial_exp=0.2, final_exp=0.7)   # doctest: +SKIP
+        >>> from sympy.physics.control.control_plots import nyquist_plot
+        >>> tf1 = TransferFunction(2*s**2 + 5*s + 1,s**2 + 2*s + 3, s)
+        >>> nyquist_plot(tf1)   # doctest: +SKIP
+
 
     See Also
     ========
