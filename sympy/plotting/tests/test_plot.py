cf57b4f3631eba99cc379a458076fa0e992dcc61
Sam Lubelsky <sammy56lt@gmail.com>
2024-04-09 16:59:05 -0500

    added regression tests

--- sympy/plotting/tests/test_plot.py
+++ sympy/plotting/tests/test_plot.py
@@ -60,6 +60,13 @@ class DummyBackendOk(Plot):
     def close(self):
         pass
 
+def test_basic_plotting_backend():
+    x = Symbol('x')
+    try:
+        plot(x, (x, 0, 3), backend='text')
+        plot(x**2 + 1, (x, 0, 3), backend='text')
+    except TypeError as exc:
+        assert False, f"{exc}"
 
 @pytest.mark.parametrize("adaptive", [True, False])
 def test_plot_and_save_1(adaptive):
