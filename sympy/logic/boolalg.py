4be51569c8597b2878855557842eedcadd8588bb
Sam Lubelsky <sammy56lt@gmail.com>
2024-04-15 00:59:40 -0500

    fixes #25827.  Made call to superclass in Xor's _eval_simplify
    This change makes it so Xor will properly Simplify
    when simplify is called on it, in the same way that
    and and or currently do

--- sympy/logic/boolalg.py
+++ sympy/logic/boolalg.py
@@ -1045,6 +1045,7 @@ class Xor(BooleanFunction):
         # as standard simplify uses simplify_logic which writes things as
         # And and Or, we only simplify the partial expressions before using
         # patterns
+        rv = super()._eval_simplify(**kwargs)
         rv = self.func(*[a.simplify(**kwargs) for a in self.args])
         if not isinstance(rv, Xor):  # This shouldn't really happen here
             return rv
