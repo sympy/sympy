39b7da16f106ec9ebf2b661729055f8f138d6f9f
Christopher Smith <smichr@gmail.com>
2024-04-25 10:20:25 -0500

    Update sympy/logic/tests/test_boolalg.py

--- sympy/logic/tests/test_boolalg.py
+++ sympy/logic/tests/test_boolalg.py
@@ -1180,7 +1180,7 @@ def test_relational_simplification_patterns_numerically():
                     [Xor, _simplify_patterns_xor()]]
     valuelist = list(set(combinations(list(range(-2, 3))*3, 3)))
     # Skip combinations of +/-2 and 0, except for all 0
-    valuelist = [v for v in valuelist if any([w % 2 for w in v]) or not any(v)]
+    valuelist = [v for v in valuelist if any(w % 2 for w in v) or not any(v)]
     for func, patternlist in patternlists:
         for pattern in patternlist:
             original = func(*pattern[0].args)
