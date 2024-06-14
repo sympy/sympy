e8cb0a8a1fed3f152df4dec430d0c14d042e7bd8
Aaron Meurer <asmeurer@gmail.com>
2024-06-14 08:42:32 -0600

    Update git files whitelist

--- release/compare_tar_against_git.py
+++ release/compare_tar_against_git.py
@@ -34,6 +34,8 @@ git_whitelist = {
     '.mailmap',
     # CI
     '.github/PULL_REQUEST_TEMPLATE.md',
+    '.github/dependabot.yml',
+    '.github/workflows/dependabot-auto-merge.yml'
     '.github/workflows/runtests.yml',
     '.github/workflows/ci-sage.yml',
     '.github/workflows/comment-on-pr.yml',
