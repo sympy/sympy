1440f0eced8585ae5ff61bf30dad0a33fa1afebc
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-06-22 12:45:13 +0530

    apply load method added

--- sympy/physics/continuum_mechanics/arch.py
+++ sympy/physics/continuum_mechanics/arch.py
@@ -12,6 +12,9 @@ class Arches:
         if 'crown_y' in kwargs:
             self._crown_y = sympify(kwargs['crown_y'])
         self.get_parabola_eqn
+        self._loads = {}
+        self._conc_loads = {}
+        self._distributed_loads = {}
         # self._crown = (sympify(crown[0]),sympify(crown[1]))
 
     @property
@@ -41,7 +44,29 @@ class Arches:
             if len(solution) <2:
                 raise ValueError("parabolic arch cannot be constructed with the provided coordinates")
             parabola_eqn = solution[a]*(x-x0)**2+ solution[c]
+            self._crown_y = solution[c]
             self._shape_eqn = parabola_eqn
 
         else:
             raise KeyError("please provide crown_x to contruct arch")
+
+    def apply_load(self,order,label,coord1,coord2,mag,*args):
+        if label in self._loads:
+            raise ValueError("load with the given label already exists")
+
+        self._loads[label] = mag
+        if order == 0:
+            x0 = min(coord1,coord2)
+            x1 = max(coord1,coord2)
+            if x0>self._right_support[0] or x1<self._left_support[0]:
+                raise ValueError(f"loads must be applied between {self._left_support[0]} and {self._right_support[0]}")
+            self._distributed_loads[label] = (x0,x1)
+        if order == 1:
+            y0 = min(self._right_support[1],self._left_support[1])
+            y1 = max(self._right_support[1],self._left_support[1])
+
+            if coord1>self._right_support[0] or coord1<self._left_support[0]:
+                raise ValueError(f"loads must be applied between x = {self._left_support[0]} and x = {self._right_support[0]}")
+            if coord2>y1 or coord2<y0:
+                raise ValueError(f"loads must be applied between y = {y0} and y = {y1}")
+            self._conc_loads[label] = (coord1,coord2,args[0])
