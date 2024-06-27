d5905498ec512225cbe2219448f95a9fc0432c31
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-06-27 12:17:14 +0100

    test(matrices): also test against naive matmul

--- sympy/matrices/tests/test_matrices.py
+++ sympy/matrices/tests/test_matrices.py
@@ -280,7 +280,9 @@ def test_multiplication_inf_zero():
     [                                      pi*b*x**4/8 + pi*a*x**4/8 + O(x**5),   O(x**6), pi*b**2*x**6/32 + pi*a*b*x**6/48 + pi*a**2*x**6/32 + O(x**7),                                           pi*b*x**6/12 + pi*a*x**6/12 + O(x**7)],
     [                                                               -pi*x**2/2,         0,                       -pi*b*x**4/16 - pi*a*x**4/16 + O(x**5),       -pi*x**4/4 - pi*b**2*x**6/64 - pi*a*b*x**6/96 - pi*a**2*x**6/64 + O(x**7)]])
 
-    assert A*B == C
+    C2 = Matrix(4, 4, lambda i, j: Add(*(A[i,k]*B[k,j] for k in range(4))))
+
+    assert A*B == C == C2
 
 
 def test_power():
