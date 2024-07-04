a6e31dd564dc49a8a1634a760220640a47980350
arnabnandikgp <arnabnandi2002@gmail.com>
2024-07-04 13:46:55 +0530

    Added support for piecewise for limit calculations
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

--- sympy/functions/elementary/piecewise.py
+++ sympy/functions/elementary/piecewise.py
@@ -222,9 +222,40 @@ class Piecewise(Function):
         return piecewise_simplify(self, **kwargs)
 
     def _eval_as_leading_term(self, x, logx=None, cdir=0):
-        for e, c in self.args:
-            if c == True or c.subs(x, 0) == True:
-                return e.as_leading_term(x)
+        if cdir == S.Zero:
+            l = self._eval_as_leading_term(x, logx=None, cdir=-1)
+            r = self._eval_as_leading_term(x, logx=None, cdir=1)
+            if l != r:
+                raise ValueError("Two sided limit of %s around 0 does not exist" % self)
+            return r
+
+        expr_pairs = self.as_expr_set_pairs()
+
+        for pair in expr_pairs:
+            point = pair[1].sup if cdir == -1 else pair[1].inf
+            if point is S.Zero:
+                return pair[0].as_leading_term(x)
+
+        for bound in expr_pairs:
+            if S.Zero in bound[1] and not S.Zero in bound[1].boundary:
+                return bound[0].as_leading_term(x)
+
+    def _at_infinity(self, x):
+        at_infinity = []
+
+        for i, pair in enumerate(self.as_expr_set_pairs()):
+            if pair[1].inf == S.NegativeInfinity:
+                at_infinity.append((self.args[i][0].subs(x, 1/x), 1/x < 0))
+                break
+        for i, pair in enumerate(self.as_expr_set_pairs()):
+            if pair[1].sup == S.Infinity:
+                at_infinity.append((self.args[i][0].subs(x, 1/x), 1/x > 0))
+                break
+
+        if at_infinity[0][0] == at_infinity[1][0]:
+            at_infinity = [(at_infinity[0][0], True)]
+
+        return Piecewise(*tuple(at_infinity))
 
     def _eval_adjoint(self):
         return self.func(*[(e.adjoint(), c) for e, c in self.args])
