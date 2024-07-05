7d42213fb2436195c585fcf2ee14341b3965c018
Riccardo Di Girolamo <riccardodigirolamo01@gmail.com>
2024-07-05 15:50:36 +0200

    Refactor forward_jacobian() function
    This commit implements the following modifications:
    - Make the forward_jacobian function private.
    - Enhance forward_jacobian to handle expressions containing dynamicsymbols.
    - Optimize the transformation process from DAG to canonical expression tree.
    - Remove the option to output as CseExpr object.
    - Update tests to include expressions with dynamicsymbols.

--- sympy/simplify/tests/test_cse_diff.py
+++ sympy/simplify/tests/test_cse_diff.py
@@ -7,20 +7,21 @@ from sympy.core.numbers import Integer
 from sympy.functions.elementary.exponential import exp
 from sympy.functions.elementary.trigonometric import sin, tan
 from sympy.matrices.immutable import ImmutableDenseMatrix
-from sympy.simplify.cse_diff import forward_jacobian
+from sympy.physics.mechanics import dynamicsymbols
+from sympy.simplify.cse_diff import _forward_jacobian
 from sympy.simplify.simplify import simplify
-
 
 w = Symbol('w')
 x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
 
+q1, q2, q3 = dynamicsymbols('q1 q2 q3')
+
 zero = Integer(0)
 one = Integer(1)
 two = Integer(2)
 neg_one = Integer(-1)
-
 
 @pytest.mark.parametrize(
     'expr, wrt',
@@ -40,14 +41,14 @@ neg_one = Integer(-1)
         ([tan(x)], [x]),
         ([zero, one, x, y, x*y, x + y], [x, y]),
         ([((x/y) + sin(x/y) - exp(y))*((x/y) - exp(y))], [x, y]),
-        ([w*tan(y*z)/(x - tan(y*z)), w*x*tan(y*z)/(x - tan(y*z))], [w, x, y, z])
+        ([w*tan(y*z)/(x - tan(y*z)), w*x*tan(y*z)/(x - tan(y*z))], [w, x, y, z]),
+        ([q1**2 + q2, q2**2 + q3, q3**2 + q1], [q1, q2, q3])
     ]
 )
 def test_forward_jacobian(expr, wrt):
     expr = ImmutableDenseMatrix([expr]).T
     wrt = ImmutableDenseMatrix([wrt]).T
-    cse_jacobian = forward_jacobian(expr, wrt, as_cse_expr=True)
-    replacements = reversed(cse_jacobian.replacements)
-    jacobian = cse_jacobian.reduced_exprs.subs(replacements)
+    jacobian = _forward_jacobian(expr, wrt, as_cse_expr=True)
     zeros = ImmutableDenseMatrix.zeros(*jacobian.shape)
     assert simplify(jacobian - expr.jacobian(wrt)) == zeros
+
