88de09dc460a40e8655cf0005a7f33a0daaf42e3
Muhammad Maaz <mmaaz6004@gmail.com>
2024-07-05 03:30:26 -0400

    Initial addition of CAD to polysys
    Added functions for reducta and principal subresultant coefficients. Implemented Hong projection operator. Implemented cylindrical algebraic decomposition, which makes a CAD adapted to a set of polynomials. Lastly, made solve_poly_system_cad, which makes the CAD, then evalutes the truth using sample cells.

--- sympy/solvers/polysys.py
+++ sympy/solvers/polysys.py
@@ -1,10 +1,13 @@
 """Solvers of systems of polynomial equations. """
 import itertools
 
-from sympy.core import S
+from sympy.core import S, N
 from sympy.core.sorting import default_sort_key
-from sympy.polys import Poly, groebner, roots
+from sympy.core.function import diff
+from sympy.polys import Poly, groebner, roots, real_roots, nroots
 from sympy.polys.polytools import parallel_poly_from_expr
+from sympy.polys.polytools import LT, LC, degree, subresultants
+from sympy.polys.rootoftools import ComplexRootOf
 from sympy.polys.polyerrors import (ComputationFailed,
     PolificationFailed, CoercionFailed)
 from sympy.simplify import rcollect
@@ -429,3 +432,386 @@ def solve_triangulated(polys, *gens, **args):
 
         solutions = _solutions
     return sorted((s for s, _ in solutions), key=default_sort_key)
+
+
+def solve_poly_system_cad(seq, gens, return_one_sample=True):
+    """
+    Solves a system of polynomial inequalities/equalities via
+    cylindrical algebraic decomposition. Returns a sample point
+    in one (if return_all_samples=False) or all cells over which
+    the system holds. If the system is invalid over all cells, then
+    we return None.
+
+    Parameters
+    ==========
+
+    seq: a list/tuple/set
+        Listing all the (in)equalities that are needed to be solved
+    gens: generators
+        generators of the (in)equalities in seq for which we want the
+        solutions
+    strict: a boolean (default is False)
+        if strict is True, NotImplementedError will be raised if
+        the solution is known to be incomplete (which can occur if
+        not all solutions are expressible in radicals)
+    args: Keyword arguments
+        Special options for solving the (in)equalities.
+
+
+    Returns
+    =======
+
+    List[Tuple]
+        a list of tuples with elements being solutions for the
+        symbols in the order they were passed as gens
+    None
+        None is returned if no solutions exist.
+
+    Examples
+    ========
+
+    ADD EXAMPLES HERE
+    """
+    # prepare the atoms
+    atoms = [Poly(p.lhs - p.rhs, gens) for p in seq]
+
+    sample_points = cylindrical_algebraic_decomposition(atoms, gens)
+    valid_samples = []
+
+    for sample in sample_points:
+        if all([expr.subs(sample) for expr in seq]):
+            valid_samples.append(sample)
+            if return_one_sample:
+                break
+    
+    return valid_samples
+
+
+
+
+
+
+# HONG PROJECTOR OPERATOR AND OPERATIONS USED FOR IT
+
+"""
+Reducta
+
+The reducta of a function f, treated as a univariate function of
+main variable (mvar) x, is red(f) = f - lt(f), where lt is the
+leading term.
+
+The ith level reducta of f, red^i(f), is defined recursively.
+red^0(f) = f
+red^i(f) = red(red^{i-1}(f))
+
+The reducta set RED(f) is defined as: {red^i(f) | 0 <= i <= deg(f)}
+We exclude 0 from the set by definition as well.
+
+"""
+
+def red(f, mvar):
+    return f - LT(f, mvar)
+
+def red_level(f, mvar, level):
+    if level == 0:
+        return f
+    else:
+        red_curr = f
+        for _ in range(level):
+            red_curr = red(red_curr, mvar)
+        return red_curr
+    
+def red_set(f, mvar):
+    reds = []
+
+    # handle this case here because otherwise sympy says deg= -infty
+    if not f.free_symbols:
+        return []
+    
+    for i in range(degree(f, mvar) + 1):
+        if i == 0:
+            red_curr = f
+        else:
+            red_curr = red(reds[i-1], mvar)
+        
+        if red_curr == 0:
+            break
+        else:
+            reds.append(red_curr)
+    
+    return reds
+
+
+"""
+Principal subresultant coefficients
+
+In Basu, Pollack, and Roy (2006) this is defined as a subdeterminant
+of the Sylvester-Habicht matrix. However, here we can just use the
+subresultants already built in to SymPy, and take coefficients.
+"""
+
+def psc_set(f, g, mvar):
+    subres_polys = subresultants(f, g, mvar)[::-1]
+    
+    # subresultants returns it in decreasing order of degree
+    # so we reverse it so that the ith subres is at most degree i
+    # to match with the definition in Basu book
+    # then the coefficient of ith subres of mvar^{i} is the ith psc
+
+    final_psc = []
+
+    for i in range(len(subres_polys)):
+        p = subres_polys[i]
+        if p == 0:
+            continue
+        
+        curr_coeff = Poly(p, mvar).nth(i)
+        if curr_coeff == 0:
+            continue
+        
+        final_psc.append(curr_coeff)
+    
+    return final_psc
+
+
+# gets real roots, numeric if not algebraic
+def get_nice_roots(poly):
+    # its a constant
+    if not poly.free_symbols:
+        return []
+    
+    try:
+        roots = real_roots(poly)
+    except NotImplementedError:
+        return [root for root in nroots(poly) if root.is_real]
+
+    
+    roots_return = []
+    for root in roots:
+        """
+        Want to avoid CRootOf (for now?)
+
+        Note that we can't simply check for isinstance(ComplexRootOf)
+        Sometimes, expressions can involve ComplexRootOf, eg:
+        >>> Poly(-15*z**4 + 120*z**2 + 16).real_roots()[0]
+        2*CRootOf(15*x**4 - 30*x**2 - 1, 0)
+
+        To get around this, we use .has().
+        """
+
+        if root.has(ComplexRootOf):
+            roots_return.append(root.evalf()) # not sure how precise?
+        else:
+            roots_return.append(root)
+
+    return roots_return
+
+
+# HONG PROJECTION OPERATOR (1990)
+# input: set F of k-variate polynomials
+# output: set F' of (k-1)-variate polynomials such that a CAD of R^{k-1} can be lifted to R^k
+
+def projone(F, mvar):
+    """
+    Computes the PROJ1 operator as defined in Hong 1990.
+
+    Let F be a set of polynomials with a given mvar. Then,
+
+    PROJ1 = \cup_{f \in F, g \in RED(f)} ( ldcf(g) \cup PSC(g, D(g)))
+
+    where RED is the reducta set, ldcf is the leading coefficient,
+    PSC is the principal subresultant coefficient set, and D is the
+    derivative operator.
+
+    Parameters
+    ==========
+
+    F: a list/tuple/set
+        A list of polyomials
+    mvar: a generator
+        The "main variable".
+        Polynomials are treated as univariate in the mvar.
+
+
+    Returns
+    =======
+
+    Set
+        A set of projection factors.
+
+    Examples
+    ========
+
+    ADD EXAMPLES HERE
+    """
+    proj_set = set()
+    for f in F:
+        for g in red_set(f, mvar):
+            proj_set.add(LC(g, mvar))
+            proj_set.update(psc_set(g, diff(g, mvar), mvar))    
+    
+    return proj_set
+            
+
+def projtwo(F, mvar):
+    """
+    Computes the PROJ2* operator as defined in Hong (1990).
+    This is an updated version of the PROJ2 operator from Collins.
+    We will just call it PROJ2 here.
+
+    Let F be a set of polynomials with a given mvar. Then,
+
+    PROJ2 = \cup_{f,g \in F, f < g} \cup_{f' \in RED(f)} PSC(f', g)
+
+    where RED is the reducta set, < indicates an arbitray "linear 
+    ordering" to not loop over redundant pairs, and PSC is the 
+    principal subresultant coefficient set.
+
+    Parameters
+    ==========
+
+    F: a list/tuple/set
+        A list of polyomials
+    mvar: a generator
+        The "main variable".
+        Polynomials are treated as univariate in the mvar.
+
+    Returns
+    =======
+
+    Set
+        A set of projection factors.
+
+    Examples
+    ========
+
+    ADD EXAMPLES HERE
+    """
+
+    proj_set = set()
+    for i in range(len(F)):
+        # impose linear ordering
+        for j in range(i, len(F)):
+            f, g = F[i], F[j]
+            for f_ in red_set(f, mvar):
+                proj_set.update(psc_set(f_, g, mvar))
+    
+    return proj_set
+
+
+def hongproj(F, mvar):
+    """
+    The Hong projection operator, as defined in Hong(1990).
+    PROJH takes a set of k-variate polynomials F, with an mvar, and  
+    returns a set of (k-1)-variate polynomials F, with the mvar
+    eliminated. These projection factors satisfy the property that a
+    CAD of R^{k-1} can be lifted to a CAD of R^k.
+
+    The Hong projector, PROJH, is defined as:
+
+    PROJH(F) = PROJ1(F) \cup PROJ2(F)
+
+    PROJ1 = \cup_{f \in F, g \in RED(f)} (ldcf(g) \cup PSC(g, D(g)))
+
+    PROJ2 = \cup_{f,g \in F, f < g} \cup_{f' \in RED(f)} PSC(f', g)
+
+    where RED is the reducta set, ldcf is the leading coefficient,
+    PSC is the principal subresultant coefficient set, < indicates
+    an arbitray "linear ordering" to not loop over redundant pairs,
+    and D is the derivative operator.
+
+    Parameters
+    ==========
+
+    F: a list/tuple/set
+        A list of polyomials
+    mvar: a generator
+        The "main variable".
+        Polynomials are treated as univariate in the mvar.
+
+    Returns
+    =======
+
+    List
+        The set of projection factors.
+    """
+
+    return list(projone(F, mvar).union(projtwo(F, mvar)))
+
+
+def cylindrical_algebraic_decomposition(F, gens):
+    """
+    Calculates a cylindrical algebraic decomposition adapted to F.
+    Uses the Hong projection operator. Returns sample points which
+    represent cells over which each f \in F is sign-invariant. It
+    projects iteratively down to lower-dimension spaces according to
+    the list of generators given in gens.
+
+    Parameters
+    ==========
+
+    F: a list/tuple/set
+        A list of polyomials
+    gens: a list of generators
+
+    Returns
+    =======
+
+    ??: sample points
+        
+    """
+
+    # Compute the projection sets
+    projs_set = [F]
+    for i in range(len(gens) - 1):
+        projs_set.append(hongproj(projs_set[-1], gens[i]))
+
+    # Lifting
+    sample_points = [{}]
+
+    for i in reversed(range(len(gens))):
+        projs = projs_set[i]
+        gen = gens[i]
+
+        new_sample_points = []
+
+        for point in sample_points:
+            roots = set()
+            for proj in projs:
+                roots.update(get_nice_roots(proj.subs(point)))
+            roots = sorted(list(roots), reverse=False)
+
+            # Calculate sample points
+            if not roots:
+                samples = [0]
+            elif len(roots) == 1:
+                samples = [roots[0] - 1, roots[0], roots[0] + 1]
+            else:
+                samples = [roots[0] - 1]  # Point below the smallest root
+                for r1, r2 in zip(roots, roots[1:]):
+                    samples.extend([r1, (r1 + r2) / 2])
+                samples.extend([roots[-1], roots[-1] + 1])  # Last root and point above it
+            
+            for value in samples:
+                new_point = point.copy()
+                new_point[gen] = value
+                new_sample_points.append(new_point)
+        
+        sample_points = new_sample_points
+    
+
+    return sample_points
+            
+
+
+        
+
+
+
+
+
+
+
+
+        
+
