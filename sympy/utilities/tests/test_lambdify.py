86df1179995ad0cb5e4f5eec9441a3e6a7046252
Andrew Mosson <amosson@yahoo.com>
2024-07-12 20:43:58 +0100

    Fixes typo

--- sympy/utilities/tests/test_lambdify.py
+++ sympy/utilities/tests/test_lambdify.py
@@ -529,7 +529,7 @@ def test_numpy_old_matrix():
     sol_arr = numpy.array([[1, 2], [numpy.sin(3) + 4, 1]])
     f = lambdify((x, y, z), A, [{'ImmutableDenseMatrix': numpy.matrix}, 'numpy'])
     with ignore_warnings(PendingDeprecationWarning):
-        umpy.testing.assert_allclose(f(1, 2, 3), sol_arr)
+        numpy.testing.assert_allclose(f(1, 2, 3), sol_arr)
         assert isinstance(f(1, 2, 3), numpy.matrix)
 
 
