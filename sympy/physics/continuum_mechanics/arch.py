4c594db1984bbac069e9f7b8917f86b28bc4bb01
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-07-29 14:12:10 +0530

    Add roller case for arch
    Signed-off-by: Shishir Kushwaha <kushwahashishir1112@gmail.com>

--- sympy/physics/continuum_mechanics/arch.py
+++ sympy/physics/continuum_mechanics/arch.py
@@ -332,7 +332,20 @@ class Arch:
                     solution = solve((eq1,eq2,eq3,eq4,eq5),(R_A_x,R_A_y,R_B_x,R_B_y,T))
 
             elif self._rope[2]>self._right_support[0]:
-                pass
+                if net_x<0:
+                    eq1 = Eq(R_A_x,0)
+                    eq2 = Eq(T,0)
+                    eq3 = Eq(R_A_y+R_B_y+net_y,0)
+                    eq4 = Eq(R_B_x+net_x,0)
+                    eq5 = Eq(R_B_y*(self._right_support[0]-self._left_support[0])-R_B_x*(self._right_support[1]-self._left_support[1])+moment_A,0)
+                    solution = solve((eq1,eq2,eq3,eq4,eq5),(R_A_x,R_A_y,R_B_x,R_B_y,T))
+                else:
+                    eq1 = Eq(R_A_x,0)
+                    eq2 = Eq(R_B_x- T +net_x,0)
+                    eq3 = Eq(R_A_y + R_B_y + net_y,0)
+                    eq4 = Eq(moment_hinge_left+R_A_y*(self._left_support[0]-self._crown_x),0)
+                    eq5 = Eq(moment_A+R_B_y*(self._right_support[0]-self._left_support[0])-R_B_x*(self._right_support[1]-self._left_support[1])+T*(self._rope[2]-self._left_support[1]),0)
+                    solution = solve((eq1,eq2,eq3,eq4,eq5),(R_A_x,R_A_y,R_B_x,R_B_y,T))
 
         elif self._supports['right'] == 'roller':
             pass
