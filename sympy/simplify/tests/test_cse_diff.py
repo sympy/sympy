9fa008bb4278dd7b093e643409ed1ede9a9149d4
Riccardo Di Girolamo <riccardodigirolamo01@gmail.com>
2024-07-31 02:54:31 +0200

    Updated helper function for matrix multiplication
    - The helper function now check for possible NaN results
    instead of completely skipping multiplicaiton by zero
    
    - Made forward_jacobian private
    
    - Added tests from classic jacobian tests in test_cse_diff.py

--- sympy/simplify/tests/test_cse_diff.py
+++ sympy/simplify/tests/test_cse_diff.py
@@ -2,16 +2,21 @@
 
 import pytest
 
-from sympy.core.symbol import Symbol
+from sympy.core.symbol import (Symbol, symbols)
 from sympy.core.numbers import Integer
 from sympy.core.function import Function
 from sympy.core import Derivative
 from sympy.functions.elementary.exponential import exp
-from sympy.functions.elementary.trigonometric import sin, tan
 from sympy.matrices.immutable import ImmutableDenseMatrix
 from sympy.physics.mechanics import dynamicsymbols
-from sympy.simplify._cse_diff import forward_jacobian
+from sympy.simplify._cse_diff import _forward_jacobian
 from sympy.simplify.simplify import simplify
+from sympy.matrices import Matrix, eye
+
+from sympy.testing.pytest import raises
+from sympy.functions.elementary.trigonometric import (cos, sin, tan)
+from sympy.simplify.trigsimp import trigsimp
+
 
 w = Symbol('w')
 x = Symbol('x')
@@ -28,6 +33,7 @@ zero = Integer(0)
 one = Integer(1)
 two = Integer(2)
 neg_one = Integer(-1)
+
 
 @pytest.mark.parametrize(
     'expr, wrt',
@@ -56,6 +62,65 @@ neg_one = Integer(-1)
 def test_forward_jacobian(expr, wrt):
     expr = ImmutableDenseMatrix([expr]).T
     wrt = ImmutableDenseMatrix([wrt]).T
-    jacobian = forward_jacobian(expr, wrt)
+    jacobian = _forward_jacobian(expr, wrt)
     zeros = ImmutableDenseMatrix.zeros(*jacobian.shape)
     assert simplify(jacobian - expr.jacobian(wrt)) == zeros
+
+
+def test_jacobian_hessian():
+    L = Matrix(1, 2, [x**2*y, 2*y**2 + x*y])
+    syms = [x, y]
+    assert _forward_jacobian(L, syms) == Matrix([[2*x*y, x**2], [y, 4*y + x]])
+
+    L = Matrix(1, 2, [x, x**2*y**3])
+    assert _forward_jacobian(L, syms) == Matrix([[1, 0], [2*x*y**3, x**2*3*y**2]])
+
+
+def test_jacobian_metrics():
+    rho, phi = symbols("rho,phi")
+    X = Matrix([rho * cos(phi), rho * sin(phi)])
+    Y = Matrix([rho, phi])
+    J = _forward_jacobian(X, Y)
+    assert J == X.jacobian(Y.T)
+    assert J == (X.T).jacobian(Y)
+    assert J == (X.T).jacobian(Y.T)
+    g = J.T * eye(J.shape[0]) * J
+    g = g.applyfunc(trigsimp)
+    assert g == Matrix([[1, 0], [0, rho ** 2]])
+
+
+def test_jacobian2():
+    rho, phi = symbols("rho,phi")
+    X = Matrix([rho * cos(phi), rho * sin(phi), rho ** 2])
+    Y = Matrix([rho, phi])
+    J = Matrix([
+        [cos(phi), -rho * sin(phi)],
+        [sin(phi), rho * cos(phi)],
+        [2 * rho, 0],
+    ])
+    assert _forward_jacobian(X, Y) == J
+
+
+def test_issue_4564():
+    X = Matrix([exp(x + y + z), exp(x + y + z), exp(x + y + z)])
+    Y = Matrix([x, y, z])
+    for i in range(1, 3):
+        for j in range(1, 3):
+            X_slice = X[:i, :]
+            Y_slice = Y[:j, :]
+            J = _forward_jacobian(X_slice, Y_slice)
+            assert J.rows == i
+            assert J.cols == j
+            for k in range(j):
+                assert J[:, k] == X_slice
+
+
+def test_nonvectorJacobian():
+    X = Matrix([[exp(x + y + z), exp(x + y + z)],
+                [exp(x + y + z), exp(x + y + z)]])
+    raises(TypeError, lambda: _forward_jacobian(X, Matrix([x, y, z])))
+    X = X[0, :]
+    Y = Matrix([[x, y], [x, z]])
+    raises(TypeError, lambda: _forward_jacobian(X, Y))
+    raises(TypeError, lambda: _forward_jacobian(X, Matrix([[x, y], [x, z]])))
+
