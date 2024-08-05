21c8d3ad98a42ca92c8a04720a67179cbe476b5a
arnabnandikgp <arnabnandi2002@gmail.com>
2024-08-05 20:50:49 +0530

    refactored code
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

--- sympy/functions/special/error_functions.py
+++ sympy/functions/special/error_functions.py
@@ -1436,10 +1436,10 @@ class expint(Function):
 
     def _eval_as_leading_term(self, x, logx=None, cdir=0):
         nu, z = self.args
-        if nu.is_real and nu >= 1:
+        if nu == 1:
+            arg = -log(z) - EulerGamma
+        if nu.is_real and nu > 1:
             arg = 1/(nu - 1)
-            if nu == 1:
-                arg = -log(z) - EulerGamma
             return arg.as_leading_term(x, logx=logx)
         return super()._eval_as_leading_term(x, logx, cdir)
 
