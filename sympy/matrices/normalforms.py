24ca68940c46f44d5f149db71ef532a2b68af03e
Isuru Fernando <isuruf@gmail.com>
2024-08-18 21:29:27 -0500

    fix doctest

--- sympy/matrices/normalforms.py
+++ sympy/matrices/normalforms.py
@@ -55,7 +55,7 @@ def smith_normal_decomp(m, domain=None):
     >>> from sympy import Matrix, ZZ
     >>> from sympy.matrices.normalforms import smith_normal_form
     >>> m = Matrix([[12, 6, 4], [3, 9, 6], [2, 16, 14]])
-    >>> a, s, t = smith_normal_form(m, domain=ZZ)
+    >>> a, s, t = smith_normal_decomp(m, domain=ZZ)
     >>> assert a == m * s * t
     '''
     dM = _to_domain(m, domain)
