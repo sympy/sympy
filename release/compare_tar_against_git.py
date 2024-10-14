3f9514092822eb93012e96ae546f12be3fb6f3f3
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-10-15 01:50:37 +0530

    Re-add workflow for Pyodide testing

--- release/compare_tar_against_git.py
+++ release/compare_tar_against_git.py
@@ -36,6 +36,7 @@ git_whitelist = {
     '.github/PULL_REQUEST_TEMPLATE.md',
     '.github/dependabot.yml',
     '.github/workflows/runtests.yml',
+    '.github/workflows/emscripten.yml',
     '.github/workflows/ci-sage.yml',
     '.github/workflows/comment-on-pr.yml',
     '.github/workflows/release.yml',
