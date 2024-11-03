71e387e83073bfe54878ce30a251c887b34cee58
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-11-03 23:04:25 +0000

    fix(docs): add workaround for sphinx TypeVar bug
    https://github.com/sphinx-doc/sphinx/issues/10974

--- sympy/utilities/decorator.py
+++ sympy/utilities/decorator.py
@@ -9,7 +9,8 @@ from functools import wraps, update_wrapper
 from sympy.utilities.exceptions import sympy_deprecation_warning
 
 
-_T = TypeVar('_T')
+T = TypeVar('T')
+"""A generic type"""
 
 
 def threaded_factory(func, use_add):
@@ -180,7 +181,7 @@ def doctest_depends_on(exe=None, modules=None, disable_viewers=None,
     return depends_on_deco
 
 
-def public(obj: _T) -> _T:
+def public(obj: T) -> T:
     """
     Append ``obj``'s name to global ``__all__`` variable (call site).
 
