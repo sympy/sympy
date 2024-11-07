8cad674a1dcf1b47b5dc8182a2e06a8832a7eef5
Rushabh Mehta <mehtarushabh2005@gmail.com>
2024-11-07 20:13:19 +0530

    Add tests

--- sympy/physics/tests/test_clebsch_gordan.py
+++ sympy/physics/tests/test_clebsch_gordan.py
@@ -61,6 +61,9 @@ def test_clebsch_gordan_numpy():
     except ImportError:
         skip("numpy not installed")
     assert clebsch_gordan(*np.zeros(6).astype(np.int64)) == 1
+    assert wigner_3j(2, np.float64(6.0), 4.0, 0, 0, 0) == sqrt(715)/143
+    assert wigner_3j(0, 0.5, 0.5, 0, 0.5, -0.5) == sqrt(2)/2
+    raises(ValueError, lambda: wigner_3j(2.1, 6, 4, 0, 0, 0))
 
 
 def test_wigner():
