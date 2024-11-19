fdb939f2e73ba6f98e4c221873b810d5365c2d39
Stan U. <89152624+StanFromIreland@users.noreply.github.com>
2024-11-19 21:18:22 +0000

    Update sympy/ntheory/primetest.py
    Co-authored-by: Christopher Smith <smichr@gmail.com>

--- sympy/ntheory/primetest.py
+++ sympy/ntheory/primetest.py
@@ -824,4 +824,12 @@ def is_fermat_prime(num):
     if num in FERMAT_PRIMES:
         return True
 
-    return False
+    e = num - 1
+    for do in range(2):
+        be = perfect_power(e)
+        if not be:
+            return False
+        b, e = be
+        if b != 2:
+            return False
+    return False if (e < 65537) else None  # it seems unlikely that any more will be found, but not proven
