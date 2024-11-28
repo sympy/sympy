03ef4ca455618d2fb62ced4742e1d779726f8c80
Memória de Cálculo <memoriadcalculo@gmail.com>
2024-11-28 19:57:33 -0300

    Trailing spaces deleted

--- sympy/external/django/validators.py
+++ sympy/external/django/validators.py
@@ -14,7 +14,7 @@ class SympyValidationError(ValidationError):
     Sympy Validation Error.
     """
     message = _('Enter a Sympy Object.')
-    
+
     def __init__(self, message=message, code=None, params=None):
         super().__init__(message, code, params)
 
@@ -23,14 +23,14 @@ class SympyValidator:
     """
     Validate if the input is an instance of Sympy, otherwise raise SympyValidationError.
     """
-    
+
     def __init__(self):
         pass
-    
+
     def __call__(self, value):
         if not isinstance(value, Basic):
             raise SympyValidationError()
-    
+
     def __eq__(self, other):
         return (isinstance(other, self.__class__))
 
