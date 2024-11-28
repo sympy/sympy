4c8e1708dc967b87e46338ac77fe8b2b61ccd5dc
Memória de Cálculo <memoriadcalculo@gmail.com>
2024-11-28 19:57:05 -0300

    Trailing spaces deleted

--- sympy/external/django/models.py
+++ sympy/external/django/models.py
@@ -11,7 +11,7 @@ from sympy.external.django.validators import SympyValidator
 
 class SympyCharField(CharField):
     description = _("Sympy Char Field")
-    
+
     def __init__(self, *args, db_collation=None, **kwargs):
         Field.__init__(self, *args, **kwargs)
         self.db_collation = db_collation
@@ -20,13 +20,13 @@ class SympyCharField(CharField):
         # needs a string and SympyValidator a Sympy object
         # Sympy object doesn't len() function
         self.validators.append(SympyValidator())
-    
+
     def from_db_value(self, value, expression, connection, context=None):
         return str2sympy(value)
-    
+
     def to_python(self, value):
         return str2sympy(super().to_python(value))
-    
+
     def get_prep_value(self, value):
         value = super().get_prep_value(value)
         return str(value)
