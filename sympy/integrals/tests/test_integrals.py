1cc32d71c8969a865bdce0843f283f3446f7b083
vprayag2005 <v.prayag2005@gamil.com>
2024-12-16 08:44:57 +0530

    added a test for issue- 27374

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -2176,3 +2176,24 @@ def test_definite_integral_with_floats_issue_27231():
 
     # Verify that the result is approximately equal within a larger tolerance
     assert abs(result_numeric - expected_result.evalf()) < 1e-8
+
+def test_issue_27374():
+    #https://github.com/sympy/sympy/issues/27374
+    
+    # Define the symbols
+    x, z, R, a = symbols('x z R a')
+    
+    # Define expressions
+    r = sqrt(x**2 + z**2)
+    u = erf(a*r/sqrt(2))/r
+    Ec = diff(u, z, z).subs([(x, sqrt(R*R-z*z))])
+    
+    # Perform the integration
+    numeric_result=simplify(integrate(Ec, (z, -R, R)))
+    
+    # Define expected result
+    expected_result=-2*sqrt(2)*R*a**3*exp(-R**2*a**2/2)/(3*sqrt(pi))
+    
+    # Assert that the difference between the expected and numeric result is zero
+    assert (expected_result-numeric_result)==0
+
