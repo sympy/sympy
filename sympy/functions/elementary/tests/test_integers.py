1992b5655968e9757b2072ed64484428117f377b
Kris Katterjohn <katterjohn@gmail.com>
2024-12-21 13:24:40 -0500

    Fix floor/ceiling inequality simplifications for real args

--- sympy/functions/elementary/tests/test_integers.py
+++ sympy/functions/elementary/tests/test_integers.py
@@ -121,10 +121,10 @@ def test_floor():
     assert floor(factorial(50)/exp(1)) == \
         11188719610782480504630258070757734324011354208865721592720336800
 
-    assert (floor(y) < y) == False
+    assert (floor(y) < y).is_Relational
     assert (floor(y) <= y) == True
     assert (floor(y) > y) == False
-    assert (floor(y) >= y) == False
+    assert (floor(y) >= y).is_Relational
     assert (floor(x) <= x).is_Relational  # x could be non-real
     assert (floor(x) > x).is_Relational
     assert (floor(x) <= y).is_Relational  # arg is not same as rhs
@@ -311,9 +311,9 @@ def test_ceiling():
         11188719610782480504630258070757734324011354208865721592720336801
 
     assert (ceiling(y) >= y) == True
-    assert (ceiling(y) > y) == False
+    assert (ceiling(y) > y).is_Relational
     assert (ceiling(y) < y) == False
-    assert (ceiling(y) <= y) == False
+    assert (ceiling(y) <= y).is_Relational
     assert (ceiling(x) >= x).is_Relational  # x could be non-real
     assert (ceiling(x) < x).is_Relational
     assert (ceiling(x) >= y).is_Relational  # arg is not same as rhs
