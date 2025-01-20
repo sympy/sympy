fb53bffac80d5296933962cfc5559b72771e8e2f
Steven Lee <41497963+stevenleeS0ht@users.noreply.github.com>
2025-01-21 04:08:10 +0800

    Update sympy/integrals/tests/test_integrals.py
    Co-authored-by: S.Y. Lee  <sylee957@gmail.com>

--- sympy/integrals/tests/test_integrals.py
+++ sympy/integrals/tests/test_integrals.py
@@ -2176,12 +2176,3 @@ def test_definite_integral_with_floats_issue_27231():
 
     # Verify that the result is approximately equal within a larger tolerance
     assert abs(result_numeric - expected_result.evalf()) < 1e-8
-
-
-def test_issue_27374():
-    #https://github.com/sympy/sympy/issues/27374
-    r = sqrt(x**2 + z**2)
-    u = erf(a*r/sqrt(2))/r
-    Ec = diff(u, z, z).subs([(x, sqrt(b*b-z*z))])
-    expected_result = -2*sqrt(2)*b*a**3*exp(-b**2*a**2/2)/(3*sqrt(pi))
-    assert simplify(integrate(Ec, (z, -b, b))) == expected_result
