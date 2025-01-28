4ce9d43223b56d65af5e93a8069d541036c7151d
Pg999999 <pratykshgupta9999@gmail.com>
2025-01-28 21:44:00 +0530

    added the custom precedence function and the test case

--- sympy/printing/tests/test_precedence.py
+++ sympy/printing/tests/test_precedence.py
@@ -90,16 +90,43 @@ def test_And_Or():
     assert precedence(~y) == PRECEDENCE["Not"]
 
 
-def test_custom_function_mul_precedence():
-    class F(sympy.Function):
-        precedence = 45
+def test_custom_function_precedence_comparison():
+    """
+    Test cases for custom functions with different precedence values,
+    specifically handling:
+    1. Functions with precedence < PRECEDENCE["Mul"] (50)
+    2. Functions with precedence = Func (70)
 
+    Key distinction:
+    - Lower precedence functions (45) need parentheses: -2*(x F y)
+    - Higher precedence functions (70) don't: -2*x F y
+    """
+    class LowPrecedenceF(sympy.Function):
+        precedence = PRECEDENCE["Mul"] - 5
         def _sympystr(self, printer):
             return f"{printer._print(self.args[0])} F {printer._print(self.args[1])}"
 
-    f_instance = F(x, y)
+    class HighPrecedenceF(sympy.Function):
+        precedence = PRECEDENCE["Func"]
+        def _sympystr(self, printer):
+            return f"{printer._print(self.args[0])} F {printer._print(self.args[1])}"
 
-    positive_mul = 2 * f_instance
-    negative_mul = -2 * f_instance
-    assert str(positive_mul) == "2*(x F y)"
-    assert str(negative_mul) == "-2*(x F y)"
+    def test_low_precedence():
+        expr1 = 2 * LowPrecedenceF(x, y)
+        assert str(expr1) == "2*(x F y)"
+
+        expr2 = -2 * LowPrecedenceF(x, y)
+        assert str(expr2) == "-2*(x F y)"
+
+    def test_high_precedence():
+        expr1 = 2 * HighPrecedenceF(x, y)
+        assert str(expr1) == "2*x F y"
+
+        expr2 = -2 * HighPrecedenceF(x, y)
+        assert str(expr2) == "-2*x F y"
+
+    test_low_precedence()
+    test_high_precedence()
+
+if __name__ == "__main__":
+    test_custom_function_precedence_comparison()
