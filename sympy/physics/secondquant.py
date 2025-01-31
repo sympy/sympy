841fc0ceb63f0df1622747dc96fbac976ff66e39
Pg999999 <pratykshgupta9999@gmail.com>
2025-01-31 12:05:58 +0530

    fix(stats): correct interpretation of parameter `p` in NegativeBinomial distribution
    Updated the implementation to treat `p` as the probability of success.
    Adjusted the probability mass function (PMF) and related formulas to reflect the correct interpretation of `p`.
    Updated the documentation to clearly state that `p` represents the probability of success.
    
    Fixes #27203

--- sympy/physics/secondquant.py
+++ sympy/physics/secondquant.py
@@ -19,6 +19,7 @@ from sympy.core.singleton import S
 from sympy.core.sorting import default_sort_key
 from sympy.core.symbol import Dummy, Symbol
 from sympy.core.sympify import sympify
+from sympy.functions.elementary.complexes import conjugate
 from sympy.functions.elementary.miscellaneous import sqrt
 from sympy.functions.special.tensor_functions import KroneckerDelta
 from sympy.matrices.dense import zeros
@@ -139,17 +140,22 @@ class Dagger(Expr):
         dagger = getattr(arg, '_dagger_', None)
         if dagger is not None:
             return dagger()
+        if isinstance(arg, Symbol) and arg.is_commutative:
+            return conjugate(arg)
         if isinstance(arg, Basic):
             if arg.is_Add:
                 return Add(*tuple(map(Dagger, arg.args)))
             if arg.is_Mul:
                 return Mul(*tuple(map(Dagger, reversed(arg.args))))
-            if arg.is_Number:
-                return arg
+            if arg.is_Number or arg.is_scalar:
+                return conjugate(arg)
             if arg.is_Pow:
                 return Pow(Dagger(arg.args[0]), arg.args[1])
             if arg == I:
                 return -arg
+        if isinstance(arg, Function):
+            if all(a.is_commutative for a in arg.args):
+                return arg.func(*[Dagger(a) for a in arg.args])
         else:
             return None
 
