422273e7bae0f39971608bbefffef63d35e10bc8
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-02-06 18:10:15 +0530

    Corrected tests in test_query.py

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -1384,8 +1384,8 @@ def test_rational():
     assert ask(Q.rational(x**y),Q.integer(x) & Q.prime(x) & Q.rational(y)) is False
     assert ask(Q.rational(x**y),Q.integer(x) & Q.integer(y)) is None
     assert ask(Q.rational(x**y),Q.integer(x) & Q.eq(x,0) & Q.integer(y)) is None
-    assert ask(Q.rational(x**y),Q.eq(x,1) & Q.rational(y)) is True
-    assert ask(Q.rational(x**y),Q.eq(x,-1) & Q.rational(y)) is True
+    assert ask(Q.rational(x**y),Q.eq(x,1) & Q.rational(y)) is None
+    assert ask(Q.rational(x**y),Q.eq(x,-1) & Q.rational(y)) is None
 
 
 def test_hermitian():
