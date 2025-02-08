f2500d78da0058520765636c90fae624a0382979
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-02-08 13:38:50 +0530

    Corrected conditions in sets.py

--- sympy/assumptions/handlers/sets.py
+++ sympy/assumptions/handlers/sets.py
@@ -147,17 +147,17 @@ def _(expr, assumptions):
         if ask(Q.rational(x), assumptions):
             return ask(~Q.nonzero(x), assumptions)
         return
-    if ask(Q.integer(expr.exp), assumptions):
+    is_exp_integer = ask(Q.integer(expr.exp), assumptions)
+    if is_exp_integer:
         is_base_rational = ask(Q.rational(expr.base),assumptions)
         if is_base_rational is True:
             if ask(Q.ne(expr.base,0),assumptions):
                 return True
             if ask(Q.eq(expr.base,0),assumptions) and ask(Q.positive(expr.exp)) is True:
                 return True
-            return
-        return is_base_rational
+        return
     elif ask(Q.rational(expr.exp), assumptions):
-        if ask(Q.prime(expr.base), assumptions):
+        if ask(Q.prime(expr.base), assumptions) and is_exp_integer is False:
             return False
         if ask(Q.rational(expr.base),assumptions) is False:
             return False
