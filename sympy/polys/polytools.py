50b8f5d0811d29fe216195a8db30a4598f69defe
Pg999999 <pratykshgupta9999@gmail.com>
2025-02-13 17:08:26 +0530

    Added a change

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -324,7 +324,7 @@ class Poly(Basic):
         >>> from sympy import Poly, symbols
         >>> x = symbols('x')
         >>> Poly.from_roots([1, 2, 3], x)
-        Poly(x**3 - 6*x**2 + 11*x - 6, x, domain='QQ')
+        Poly(x**3 - 6*x**2 + 11*x - 6, x)
         >>> Poly.from_roots([-1, -2, -3], x)
         Poly(x**3 + 6*x**2 + 11*x + 6, x)
         """
