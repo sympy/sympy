880069123bac89a9189b3455d51fa77a07aa18ee
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-02-25 12:39:52 +0530

    Removed redundancy and replaced ~Q.nonzero with Q.zero

--- sympy/assumptions/handlers/sets.py
+++ sympy/assumptions/handlers/sets.py
@@ -145,25 +145,25 @@ def _(expr, assumptions):
     if expr.base == E:
         x = expr.exp
         if ask(Q.rational(x), assumptions):
-            return ask(~Q.nonzero(x), assumptions)
+            return ask(Q.zero(x), assumptions)
         return
 
     is_exp_integer = ask(Q.integer(expr.exp), assumptions)
     if is_exp_integer:
         is_base_rational = ask(Q.rational(expr.base),assumptions)
-        if is_base_rational is True:
+        if is_base_rational:
             is_base_zero = ask(Q.zero(expr.base),assumptions)
             if is_base_zero is False:
                 return True
-            if is_base_zero is True and ask(Q.positive(expr.exp)) is True:
+            if is_base_zero and ask(Q.positive(expr.exp)):
                 return True
         if ask(Q.algebraic(expr.base),assumptions) is False:
-            return ask(~Q.nonzero(expr.exp), assumptions)
+            return ask(Q.zero(expr.exp), assumptions)
         return
     elif ask(Q.rational(expr.exp), assumptions):
         if ask(Q.prime(expr.base), assumptions) and is_exp_integer is False:
             return False
-        if ask(Q.zero(expr.base)) and ask(Q.positive(expr.exp)) is True:
+        if ask(Q.zero(expr.base)) and ask(Q.positive(expr.exp)):
             return True
         if ask(Q.eq(expr.base,1)):
             return True
