97592380af808da845bbc8abfd3b9606ad230771
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-03-03 22:44:44 +0530

    Added tests

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -1642,6 +1642,9 @@ def test_integer():
     assert ask(Q.integer(x/3), Q.odd(x)) is None
     assert ask(Q.integer(x/3), Q.even(x)) is None
 
+    # https://github.com/sympy/sympy/issues/7286
+    assert ask(Q.integer(Abs(x)), ~Q.integer(x)) is None
+
 
 def test_negative():
     assert ask(Q.negative(x), Q.negative(x)) is True
