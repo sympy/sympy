5133b86a766ab672c2fa198e06e4ab30bcc5d02e
Daiki Takahashi <haru.td@gmail.com>
2025-03-09 19:35:01 +0900

    Enable complex number support in `eval_zeta_function`
    In `eval_zeta_function`, conversion from `Sum` to `zeta` is performed whenever
    possible. Currently, the conditions `s > 1` and `q > 0` are set for `s` and
    `q`, but these conditions do not handle cases where `s` or `q` are complex
    numbers correctly. According to Wikipedia, the Hurwitz zeta function is defined
    under the conditions `Re(s) > 1` and `q != 0, -1, -2, ...`. Therefore, we have
    modified the conditions accordingly.

--- sympy/concrete/summations.py
+++ sympy/concrete/summations.py
@@ -17,6 +17,7 @@ from sympy.core.sorting import ordered
 from sympy.core.symbol import Dummy, Wild, Symbol, symbols
 from sympy.functions.combinatorial.factorials import factorial
 from sympy.functions.combinatorial.numbers import bernoulli, harmonic
+from sympy.functions.elementary.complexes import re
 from sympy.functions.elementary.exponential import exp, log
 from sympy.functions.elementary.piecewise import Piecewise
 from sympy.functions.elementary.trigonometric import cot, csc
@@ -24,7 +25,7 @@ from sympy.functions.special.hyper import hyper
 from sympy.functions.special.tensor_functions import KroneckerDelta
 from sympy.functions.special.zeta_functions import zeta
 from sympy.integrals.integrals import Integral
-from sympy.logic.boolalg import And
+from sympy.logic.boolalg import And, Not
 from sympy.polys.partfrac import apart
 from sympy.polys.polyerrors import PolynomialError, PolificationFailed
 from sympy.polys.polytools import parallel_poly_from_expr, Poly, factor
@@ -32,6 +33,7 @@ from sympy.polys.rationaltools import together
 from sympy.series.limitseq import limit_seq
 from sympy.series.order import O
 from sympy.series.residues import residue
+from sympy.sets.contains import Contains
 from sympy.sets.sets import FiniteSet, Interval
 from sympy.utilities.iterables import sift
 import itertools
@@ -277,13 +279,16 @@ class Sum(AddWithLimits, ExprWithIntLimits):
         zeta function does not converge unless `s > 1` and `q > 0`
         """
         i, a, b = limits
+        if b is not S.Infinity:
+            return
         w, y, z = Wild('w', exclude=[i]), Wild('y', exclude=[i]), Wild('z', exclude=[i])
-        result = f.match((w * i + y) ** (-z))
-        if result is not None and b is S.Infinity:
+        if result := f.match((w * i + y) ** (-z)):
             coeff = 1 / result[w] ** result[z]
             s = result[z]
             q = result[y] / result[w] + a
-            return Piecewise((coeff * zeta(s, q), And(q > 0, s > 1)), (self, True))
+            return Piecewise((coeff * zeta(s, q),
+                              And(Not(Contains(-q, S.Naturals0)), re(s) > S.One)),
+                             (self, True))
 
     def _eval_derivative(self, x):
         """
