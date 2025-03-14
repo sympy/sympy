430807d986451ff6b2fe8da9088ac2635a981a49
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-03-14 08:16:27 +0530

    Updated conditions and corrected formatting issues

--- sympy/assumptions/handlers/sets.py
+++ sympy/assumptions/handlers/sets.py
@@ -68,16 +68,16 @@ def _(expr, assumptions):
 def _(expr,assumptions):
     if expr.is_number:
         return _IntegerPredicate_number(expr, assumptions)
-    if ask(Q.ne(expr.base,0),assumptions) and ask(Q.zero(expr.exp),assumptions):
+    if ask(Q.ne(expr.base,0), assumptions) and ask(Q.zero(expr.exp), assumptions):
         return True
-    if ask(Q.integer(expr.base),assumptions):
-        if ask(Q.integer(expr.exp),assumptions):
-            if ask(Q.nonnegative(expr.exp),assumptions):
+    if ask(Q.integer(expr.base), assumptions):
+        if ask(Q.integer(expr.exp), assumptions):
+            if ask(Q.positive(expr.exp), assumptions):
                 return True
-            if ask(Q.eq(expr.base,1),assumptions) or ask(Q.eq(expr.base,-1),assumptions):
+            if ask(Q.eq(expr.base,1), assumptions) or ask(Q.eq(expr.base,-1), assumptions):
                 return True
-            if ask(Q.zero(expr.base),assumptions) is not False:
-                if ask(Q.positive(expr.exp),assumptions):
+            if ask(Q.zero(expr.base), assumptions) is not False:
+                if ask(Q.positive(expr.exp), assumptions):
                     return True
             return
 
