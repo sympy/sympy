f872e0ab87d821e256b0d0d7d19916fcd6e5f5b2
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-29 13:01:05 -0700

    fix comment typo

--- sympy/assumptions/handlers/sets.py
+++ sympy/assumptions/handlers/sets.py
@@ -751,8 +751,8 @@ def _(mat, assumptions):
 def _isAlgebraic(expr, assumptions):
     # To check if the expression is a rational polynomial evaluated at pi or E, we
     # must check that the expression contains either pi or E but not both (hence the XOR).
-    # Furthermore Sympy simplifies expressions like E**2 + E te exp(2) + E; thus, polynomials
-    # containing E can have either E or E.
+    # Furthermore, Sympy simplifies expressions like E**2 + E te exp(2) + E; thus, polynomials
+    # containing E can have either E or exp.
     if not (expr.has(E) | expr.has(exp)) ^ expr.has(pi):
         return None
     new_expr = expr.subs(E, x).subs(pi, x)
