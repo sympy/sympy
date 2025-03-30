4687816aa67d421f87047bc7cdc171eff45dac66
yuliia-horyn <79104694+yuliia-horyn@users.noreply.github.com>
2025-03-30 19:56:11 +0300

    Update laplace.py

--- sympy/integrals/laplace.py
+++ sympy/integrals/laplace.py
@@ -77,8 +77,6 @@ def DEBUG_WRAP(func):
 
 def _debug(text):
     from sympy import SYMPY_DEBUG
-    global _LT_level
-
     if SYMPY_DEBUG:
         print('-LT- %s%s' % ('  '*_LT_level, text), file=sys.stderr)
 
