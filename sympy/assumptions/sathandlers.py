d2f3aec96dea9dfa1732ec45ca96fabce1492f28
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2025-04-06 16:23:16 -0700

    Use Q.complex instead of Q.finite

--- sympy/assumptions/sathandlers.py
+++ sympy/assumptions/sathandlers.py
@@ -233,8 +233,7 @@ def _(expr):
 
 @class_fact_registry.multiregister(Mul)
 def _(expr):
-    return [Equivalent(Q.zero(expr), anyarg(x, Q.zero(x), expr) & allargs(x, Q.finite(x), expr)),
-            allargs(x, Q.finite(expr) | Q.infinite(expr), expr) >> (Q.finite(expr) >> allargs(x, Q.finite(x), expr)),
+    return [Q.complex(expr)  >> Equivalent(Q.zero(expr), anyarg(x, Q.zero(x), expr)),
             allargs(x, Q.positive(x), expr) >> Q.positive(expr),
             allargs(x, Q.real(x), expr) >> Q.real(expr),
             allargs(x, Q.rational(x), expr) >> Q.rational(expr),
