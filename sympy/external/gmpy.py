d80df999a926a28c380bc06a480d079a7882b9a2
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2025-04-13 15:27:26 +0100

    fix(core): don't sympify elements of GF(p)

--- sympy/external/gmpy.py
+++ sympy/external/gmpy.py
@@ -202,6 +202,7 @@ if _SYMPY_GROUND_TYPES == 'gmpy':
     HAS_GMPY = 2
     GROUND_TYPES = 'gmpy'
     SYMPY_INTS = (int, type(gmpy.mpz(0)))
+    SYMPY_INTS_MOD = ()
     MPZ = gmpy.mpz
     MPQ = gmpy.mpq
 
@@ -248,6 +249,7 @@ elif _SYMPY_GROUND_TYPES == 'flint':
     HAS_GMPY = 0
     GROUND_TYPES = 'flint'
     SYMPY_INTS = (int, flint.fmpz) # type: ignore
+    SYMPY_INTS_MOD = (flint.nmod, flint.fmpz_mod) # type: ignore
     MPZ = flint.fmpz # type: ignore
     MPQ = flint.fmpq # type: ignore
 
@@ -309,6 +311,7 @@ elif _SYMPY_GROUND_TYPES == 'python':
     HAS_GMPY = 0
     GROUND_TYPES = 'python'
     SYMPY_INTS = (int,)
+    SYMPY_INTS_MOD = ()
     MPZ = int
     MPQ = PythonMPQ
 
