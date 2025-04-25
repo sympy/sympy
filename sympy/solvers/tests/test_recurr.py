6882f0d43ba383be3389ce5f42128756177c60bd
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-04-25 18:03:35 +0530

    Removed unused imports

--- sympy/solvers/tests/test_recurr.py
+++ sympy/solvers/tests/test_recurr.py
@@ -8,7 +8,6 @@ from sympy.functions.combinatorial.numbers import (fibonacci, euler, harmonic)
 from sympy.functions.elementary.complexes import Abs
 from sympy.functions.elementary.miscellaneous import sqrt
 from sympy.functions.elementary.trigonometric import (cos, sin)
-from sympy.functions.special.gamma_functions import gamma
 from sympy.polys.polytools import factor
 from sympy.solvers.recurr import rsolve, rsolve_hyper, rsolve_poly, rsolve_ratio
 from sympy.testing.pytest import raises, slow, XFAIL
