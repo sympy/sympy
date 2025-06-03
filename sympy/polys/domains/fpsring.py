f7fecfef109fa7890cdd96fed8cce192559fd19a
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-06-03 13:29:09 +0530

    swith to use PEP 604 & 585

--- sympy/polys/domains/fpsring.py
+++ sympy/polys/domains/fpsring.py
@@ -2,39 +2,39 @@ from sympy.core.expr import Expr
 from sympy.polys.domains import Domain
 from sympy.polys.domains.ring import Ring
 from sympy.polys.fps_ring import PowerSeriesElement, PowerSeriesPolyRing
-from sympy.polys.orderings import MonomialOrder, lex
+from sympy.polys.orderings import LexOrder
 from sympy.utilities import public
-
-from typing import Any, Union, List, Tuple, Optional, Type
 
 
 @public
 class PowerSeriesRing:
     """A class for representing univariate power series ring."""
 
-    is_PowerSeriesRing = is_PowerSeries = True
-    has_assoc_Ring  = True
-    has_assoc_Field = False
-    DEFAULT_PRECISION = 6
+    is_PowerSeriesRing: bool = True
+    is_PowerSeries: bool = True
+    has_assoc_Ring: bool = True
+    has_assoc_Field: bool = False
+    DEFAULT_PRECISION: int = 6
 
     domain: Domain
-    symbols: Union[str, Tuple[Expr, ...]]
+    symbols: str | tuple[Expr, ...]
     ring: PowerSeriesPolyRing
-    gens: Tuple[PowerSeriesElement, ...]
+    gens: tuple[PowerSeriesElement, ...]
     prec: int
-    order: Optional[Union[MonomialOrder, str]]
-    dtype: Type[PowerSeriesElement]
+    order: LexOrder | str | None
+    dtype: type[PowerSeriesElement]
 
-    def __init__(self, domain: Union[Domain, str],
-                 symbols: Union[str, List[Expr], Tuple[Expr, ...]],
-                 prec: Optional[int] = None,
-                 order: Optional[Union[MonomialOrder, str]] = None) -> None:
+    def __init__(
+        self,
+        domain: Domain | str,
+        symbols: str | list[Expr] | tuple[Expr, ...],
+        prec: int | None = None,
+        order: LexOrder | str | None = None
+        ) -> None:
 
         if prec is None:
             prec = self.DEFAULT_PRECISION
-        if order is None:
-            from sympy.polys.orderings import lex
-            order = lex
+
         ring = PowerSeriesPolyRing(symbols, domain, prec, order)
 
         self.ring = ring
@@ -62,10 +62,8 @@ class PowerSeriesRing:
     def __hash__(self) -> int:
         return hash((self.__class__.__name__, self.ring, self.domain, self.symbols, self.prec, self.order))
 
-    def __eq__(self, other: Any) -> bool:
-        """Returns `True` if two domain are equivalent. """
-        return (isinstance(other, PowerSeriesRing) and
-                self.domain == other.domain and
-                self.symbols == other.symbols and
-                self.prec == other.prec and
-                self.order == other.order)
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, PowerSeriesRing)
+            and self.ring == other.ring
+        )
