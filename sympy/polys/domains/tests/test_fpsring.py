e8c784607b1227069660010a984d4ca36333270e
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-06-03 14:03:16 +0530

    use _parse_symbols from the rings.py
    .

--- sympy/polys/domains/tests/test_fpsring.py
+++ sympy/polys/domains/tests/test_fpsring.py
@@ -1,17 +1,16 @@
-from sympy.core.symbol import symbols
-from sympy.polys.domains import QQ, ZZ, RR
+from sympy.polys.domains import QQ, RR
 from sympy.polys.domains.fpsring import PowerSeriesRing
-from sympy.polys.fps_ring import PowerSeriesElement, PowerSeriesPolyRing
+from sympy.polys.fps_ring import PowerSeriesElement
 
 
 def test_fpsring():
-    R = PowerSeriesRing('QQ', 'x', 5)
+    R = PowerSeriesRing(QQ, 'x', 5)
     assert R.dtype is PowerSeriesElement
     assert type(R.gens[0]) == R.dtype
     assert str(R) == 'QQ[[x], 5]'
     assert str(R.gens[0]) == 'x + O(x**5)'
 
-    R = PowerSeriesRing('RR', 'x')
+    R = PowerSeriesRing(RR, 'x')
     assert R.dtype is PowerSeriesElement
     assert type(R.gens[0]) == R.dtype
     assert str(R) == 'RR[[x], 6]'
