f7409e38d44ddb3bbe1ca7c44f4f95b3c71edd10
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2025-06-04 18:27:10 +0100

    Add an explanation comment for sympy.external.mpmath

--- sympy/external/mpmath.py
+++ sympy/external/mpmath.py
@@ -1,3 +1,13 @@
+#
+# This module is to centralize sympy's direct access of mpmath functions
+# so that there are not many places in the codebase that reach into what are
+# arguably mpmath's internal interfaces. The functions referenced here are all
+# quite small and so could just be copied from mpmath into sympy if it is a
+# problem to depend on being able to import them from mpmath.
+#
+# More functions could be added here if needed but these are the ones that were
+# widely used throughout the sympy codebase beforew this module was added.
+#
 from mpmath.libmp.libmpf import (
     prec_to_dps,
     dps_to_prec,
