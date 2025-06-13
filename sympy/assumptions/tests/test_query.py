1d96f9ab147d3d01e21bf3f5ca42c3780b5193b1
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-06-13 12:42:20 +0530

    Remove unused imports

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -21,7 +21,7 @@ from sympy.functions.elementary.trigonometric import (
     acos, acot, asin, atan, cos, cot, sin, tan)
 from sympy.logic.boolalg import Equivalent, Implies, Xor, And, to_cnf
 from sympy.matrices import Matrix, SparseMatrix
-from sympy.testing.pytest import (XFAIL, slow, raises, warns, warns_deprecated_sympy,
+from sympy.testing.pytest import (XFAIL, slow, raises, warns,
     _both_exp_pow)
 from sympy.utilities.exceptions import SymPyDeprecationWarning
 import math
