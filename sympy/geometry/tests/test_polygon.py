2d9c15d2f07003a57501112b48362abb8a6150fb
TrungPQ <trungpqhe171493@gmail.com>
2025-07-24 06:45:00 +0700

    Update test_polygon.py

--- sympy/geometry/tests/test_polygon.py
+++ sympy/geometry/tests/test_polygon.py
@@ -269,7 +269,7 @@ def test_polygon():
     assert t1.exradii[t1.sides[2]] == 5*sqrt(2)/2
 
     # Excenters
-    assert t1.excenters[t1.sides[2]] == Point2D(25*sqrt(2), -5*sqrt(2)/2)
+    assert t1.excenters[t1.sides[2]] == Point2D(5*sqrt(2)/2, -5*sqrt(2)/2)
 
     # Circumcircle
     assert t1.circumcircle.center == Point(2.5, 2.5)
