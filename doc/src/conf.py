c903f8b1200d26a4e61907b4bd6a8bb32c2419da
Aaron Meurer <asmeurer@gmail.com>
2025-08-03 13:35:52 -0600

    Don't include the sources in the HTML docs any more
    They were required for the "show source" button but that has been changed to
    point to GitHub.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -342,7 +342,7 @@ html_css_files = ['custom.css']
 html_domain_indices = ['py-modindex']
 
 # If true, the reST sources are included in the HTML build as _sources/<name>.
-# html_copy_source = True
+html_copy_source = False
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'SymPydoc'
