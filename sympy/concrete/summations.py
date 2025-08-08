99dca37e9f7e0285c88f2c5801d3dbe22e87392a
Christopher Smith <smichr@gmail.com>
2025-08-08 09:15:49 -0500

    allow (-oo,oo) to continue with non-integer shift

--- sympy/concrete/summations.py
+++ sympy/concrete/summations.py
@@ -1529,28 +1529,39 @@ def eval_sum_residue(f, i_a_b):
         return None
 
     # handle non-even function with limits (-oo, oo), (-oo, k) or (k, oo)
-    
+
+    both_inf = (a,b) == (S.NegativeInfinity, S.Infinity)
+
     if not is_even_function(numer, denom):
         # Try shifting summation and check if the summand can be made
         # even wrt the origin.
         # Sum(f(n), (n, a, b)) => Sum(f(n + s), (n, a - s, b - s))
         shift = get_shift(denom)
-        if not (shift and shift.is_Integer):
+        if not shift:
             return None
-
-        numer = numer.shift(shift)
-        denom = denom.shift(shift)
+        if not shift.is_Integer:
+            if not both_inf:
+                return None
+            # we can handle a non-integer shift if limits are infinite
+            numer, denom = match_rational((numer.as_expr()/denom.as_expr()
+                ).subs(i, i + shift), i)
+        else:
+            numer = numer.shift(shift)
+            denom = denom.shift(shift)
 
         if not is_even_function(numer, denom):
             return None
 
-        if alternating:
-            f = S.NegativeOne**i * (S.NegativeOne**shift * numer.as_expr() / denom.as_expr())
-        else:
-            f = numer.as_expr() / denom.as_expr()
-        return eval_sum_residue(f, (i, a-shift, b-shift))
+        if not both_inf:
+            if alternating:
+                f = S.NegativeOne**i * (S.NegativeOne**shift * numer.as_expr() / denom.as_expr())
+            else:
+                f = numer.as_expr() / denom.as_expr()
+            return eval_sum_residue(f, (i, a-shift, b-shift))
 
-    if (a, b) == (S.NegativeInfinity, S.Infinity):
+    # handle even function
+
+    if both_inf:
         poles = get_poles(denom)
         if poles is None:
             return None
@@ -1559,8 +1570,6 @@ def eval_sum_residue(f, i_a_b):
             return None
 
         return -S.Pi * sum(residues(numer, denom, alternating, nonint_roots))
-
-    # handle even function
 
     if a is S.NegativeInfinity:
         # this is ok for an even function and will be necessary
