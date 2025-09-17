sympy/sympy

commit c6bd1553f2eeafbdddffbac13cdde3b8ce09444d

fix: _op_priority mechanism for TensExpr
TensExpr set _op_priority but failed to decorate its special methods
with call_highest_priority.

sympy/tensor/tests/test_tensor.py
@@ -4,8 +4,9 @@ from sympy.core.numbers import Integer
 from sympy.matrices.dense import (Matrix, eye)
 from sympy.tensor.indexed import Indexed
 from sympy.combinatorics import Permutation
-from sympy.core import S, Rational, Symbol, Basic, Add, Wild, Function
+from sympy.core import S, Rational, Symbol, Basic, Add, Mul, Wild, Function, Expr
 from sympy.core.containers import Tuple
+from sympy.core.decorators import call_highest_priority
 from sympy.core.symbol import symbols
 from sympy.functions.elementary.miscellaneous import sqrt
 from sympy.integrals import integrate
@@ -1254,6 +1255,69 @@ def test_hash():
     assert tsymmetry.func(*tsymmetry.args) == tsymmetry
     assert hash(tsymmetry.func(*tsymmetry.args)) == hash(tsymmetry)
     assert check_all(tsymmetry)
+
+def test_op_priority():
+    class NewExpr(Expr):
+        _op_priority = 100
+        is_commutative = False
+
+        def __neg__(self):
+            return self*S.NegativeOne
+
+        @call_highest_priority('__radd__')
+        def __add__(self, other):
+            return NewAdd(self, other)
+
+        @call_highest_priority('__add__')
+        def __radd__(self, other):
+            return NewAdd(other, self)
+
+        @call_highest_priority('__rsub__')
+        def __sub__(self, other):
+            return NewAdd(self, -other)
+
+        @call_highest_priority('__sub__')
+        def __rsub__(self, other):
+            return NewAdd(other, -self)
+
+        @call_highest_priority('__rmul__')
+        def __mul__(self, other):
+            return NewMul(self, other)
+
+        @call_highest_priority('__mul__')
+        def __rmul__(self, other):
+            return NewMul(other, self)
+
+        @call_highest_priority('__rtruediv__')
+        def __truediv__(self, other):
+            return NewMul(self, other)
+
+        @call_highest_priority('__truediv__')
+        def __rtruediv__(self, other):
+            return NewMul(other, self)
+
+    class NewAdd(NewExpr, Add):
+        pass
+
+    class NewMul(NewExpr, Mul):
+        pass
+
+    class NewSymbol(NewExpr, Symbol):
+        def __init__(self, name):
+            self.name = name
+
+    Lorentz = TensorIndexType('Lorentz', dim=4, dummy_name='L')
+    a = tensor_indices('a', Lorentz)
+    p = tensor_heads('p', [Lorentz])
+    n = NewSymbol('n')
+
+    assert isinstance(n + p(a), NewAdd)
+    assert isinstance(p(a) + n, NewAdd)
+    assert isinstance(n - p(a), NewAdd)
+    assert isinstance(p(a) - n, NewAdd)
+    assert isinstance(n * p(a), NewMul)
+    assert isinstance(p(a) * n, NewMul)
+    assert isinstance(p(a) / n, NewMul)
 
 
 ### TEST VALUED TENSORS ###

sympy/tensor/tensor.py
@@ -46,6 +46,7 @@ from sympy.combinatorics.tensor_can import get_symmetric_group_sgs, \
 from sympy.core import Basic, Expr, sympify, Add, Mul, S
 from sympy.core.cache import clear_cache
 from sympy.core.containers import Tuple, Dict
+from sympy.core.decorators import call_highest_priority
 from sympy.core.function import WildFunction
 from sympy.core.sorting import default_sort_key
 from sympy.core.symbol import Symbol, symbols, Wild
@@ -2014,18 +2015,23 @@ class TensExpr(Expr, ABC):
     def __abs__(self):
         raise NotImplementedError
 
+    @call_highest_priority('__radd__')
     def __add__(self, other):
         return TensAdd(self, other).doit(deep=False)
 
+    @call_highest_priority('__add__')
     def __radd__(self, other):
         return TensAdd(other, self).doit(deep=False)
 
+    @call_highest_priority('__rsub__')
     def __sub__(self, other):
         return TensAdd(self, -other).doit(deep=False)
 
+    @call_highest_priority('__sub__')
     def __rsub__(self, other):
         return TensAdd(other, -self).doit(deep=False)
 
+    @call_highest_priority('__rmul__')
     def __mul__(self, other):
         """
         Multiply two tensors using Einstein summation convention.
@@ -2051,9 +2057,11 @@ class TensExpr(Expr, ABC):
         """
         return TensMul(self, other).doit(deep=False)
 
+    @call_highest_priority('__mul__')
     def __rmul__(self, other):
         return TensMul(other, self).doit(deep=False)
 
+    @call_highest_priority('__rtruediv__')
     def __truediv__(self, other):
         other = _sympify(other)
         if isinstance(other, TensExpr):

commit f5ac2c0d388ed96d0bd0376a63112e8c9de6cf57

fix: mypy complaining about incompatible inherited methods
This problem was introduced in commit
c6bd1553f2eeafbdddffbac13cdde3b8ce09444d.

sympy/tensor/tests/test_tensor.py
@@ -1306,6 +1306,14 @@ def test_op_priority():
         def __init__(self, name):
             self.name = name
 
+        # The following definitions are required to satisfy mypy.
+
+        def subs():
+            pass
+
+        def simplify():
+            pass
+
     Lorentz = TensorIndexType('Lorentz', dim=4, dummy_name='L')
     a = tensor_indices('a', Lorentz)
     p = tensor_heads('p', [Lorentz])