c85361e7670d832a6ad98fe781df3b3675014082
jiawei <jiawei2012220@gmail.com>
2025-10-18 06:16:58 +0000

    adds a default (0, True) case to Piecewise functions

--- sympy/functions/elementary/piecewise.py
+++ sympy/functions/elementary/piecewise.py
@@ -1,6 +1,5 @@
-from sympy.core import S, diff, Tuple, Dummy, Mul
+from sympy.core import S, Function, diff, Tuple, Dummy, Mul
 from sympy.core.basic import Basic, as_Basic
-from sympy.core.function import DefinedFunction
 from sympy.core.numbers import Rational, NumberSymbol, _illegal
 from sympy.core.parameters import global_parameters
 from sympy.core.relational import (Lt, Gt, Eq, Ne, Relational,
@@ -62,7 +61,7 @@ class ExprCondPair(Tuple):
         return self.func(*[a.simplify(**kwargs) for a in self.args])
 
 
-class Piecewise(DefinedFunction):
+class Piecewise(Function):
     """
     Represents a piecewise function.
 
@@ -222,7 +221,7 @@ class Piecewise(DefinedFunction):
     def _eval_simplify(self, **kwargs):
         return piecewise_simplify(self, **kwargs)
 
-    def _eval_as_leading_term(self, x, logx, cdir):
+    def _eval_as_leading_term(self, x, logx=None, cdir=0):
         for e, c in self.args:
             if c == True or c.subs(x, 0) == True:
                 return e.as_leading_term(x)
@@ -541,6 +540,12 @@ class Piecewise(DefinedFunction):
                     rv = piecewise_fold(rv)
                 return rv
 
+        # Ensure there's a default case to avoid undefined regions
+        # If the last condition is not True, add (0, True) as default
+        # This fixes issue #28469 where nested integrations return nan
+        if not self.args or self.args[-1].cond != true:
+            self = self.func(*self.args, (0, true))
+
         # handle a Piecewise with lo <= hi and no x-independent relationals
         # -----------------------------------------------------------------
         ok, abei = self._intervals(x)
@@ -735,7 +740,7 @@ class Piecewise(DefinedFunction):
             else:
                 return False, 'unrecognized condition: %s' % cond
 
-            upper = Max(lower, upper)
+            lower, upper = lower, Max(lower, upper)
             if err_on_Eq and lower == upper:
                 return False, 'encountered Eq condition'
             if (lower >= upper) is not S.true:
@@ -810,9 +815,9 @@ class Piecewise(DefinedFunction):
     _eval_is_positive = lambda self: self._eval_template_is_attr('is_positive')
     _eval_is_extended_real = lambda self: self._eval_template_is_attr(
             'is_extended_real')
-    _eval_is_extended_positive = lambda self: self._eval_template_is_attr( # type: ignore
+    _eval_is_extended_positive = lambda self: self._eval_template_is_attr(
             'is_extended_positive')
-    _eval_is_extended_negative = lambda self: self._eval_template_is_attr( # type: ignore
+    _eval_is_extended_negative = lambda self: self._eval_template_is_attr(
             'is_extended_negative')
     _eval_is_extended_nonzero = lambda self: self._eval_template_is_attr(
             'is_extended_nonzero')
@@ -1122,7 +1127,7 @@ def _clip(A, B, k):
     a, b = B
     c, d = A
     c, d = Min(Max(c, a), b), Min(Max(d, a), b)
-    a = Min(a, b)
+    a, b = Min(a, b), b
     p = []
     if a != c:
         p.append((a, c, -1))
