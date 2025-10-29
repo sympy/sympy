f66f82a58722d55503161f5abe1c0609dcaee12f
edson duarte <eduarte.uatach@gmail.com>
2025-10-29 17:07:21 -0300

    Initial implementation for _eval_subs in MatAdd

--- sympy/matrices/expressions/matadd.py
+++ sympy/matrices/expressions/matadd.py
@@ -104,6 +104,53 @@ class MatAdd(MatrixExpr, Add):
         add_lines = [arg._eval_derivative_matrix_lines(x) for arg in self.args]
         return [j for i in add_lines for j in i]
 
+    def _eval_subs(self, old, new):
+        if not old.is_Add:
+            if old is S.Infinity and -old in self.args:
+                # foo - oo is foo + (-oo) internally
+                return self.xreplace({-old: -new})
+            return None
+
+        coeff_self, terms_self = self.as_coeff_Add()
+        coeff_old, terms_old = old.as_coeff_Add()
+
+        if coeff_self.is_Rational and coeff_old.is_Rational:
+            if terms_self == terms_old:   # (2 + a).subs( 3 + a, y) -> -1 + y
+                return self.func(new, coeff_self, -coeff_old)
+            if terms_self == -terms_old:  # (2 + a).subs(-3 - a, y) -> -1 - y
+                return self.func(-new, coeff_self, coeff_old)
+
+        if coeff_self.is_Rational and coeff_old.is_Rational \
+                or coeff_self == coeff_old:
+            args_old, args_self = self.func.make_args(
+                terms_old), self.func.make_args(terms_self)
+            if len(args_old) < len(args_self):  # (a+b+c).subs(b+c,x) -> a+x
+                self_set = set(args_self)
+                old_set = set(args_old)
+
+                if old_set < self_set:
+                    ret_set = self_set - old_set
+                    if isinstance(coeff_old, MatrixExpr) \
+                            and isinstance(coeff_self, MatrixExpr):
+                        return self.func(new, coeff_self, -coeff_old,
+                                   *[s._subs(old, new) for s in ret_set])
+                    else:
+                        return self.func(new,
+                                   *[s._subs(old, new) for s in ret_set])
+
+                args_old = self.func.make_args(
+                    -terms_old)     # (a+b+c+d).subs(-b-c,x) -> a-x+d
+                old_set = set(args_old)
+                if old_set < self_set:
+                    ret_set = self_set - old_set
+                    if isinstance(coeff_old, MatrixExpr) \
+                            and isinstance(coeff_self, MatrixExpr):
+                        return self.func(new, coeff_self, -coeff_old,
+                                   *[s._subs(old, new) for s in ret_set])
+                    else:
+                        return self.func(new,
+                                   *[s._subs(old, new) for s in ret_set])
+
 add.register_handlerclass((Add, MatAdd), MatAdd)
 
 
