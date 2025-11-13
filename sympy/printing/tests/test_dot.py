8c799e94d9f1fae9159aba4e8f29d20714584710
OliverAh <oliverah.github@gmail.com>
2025-11-13 13:12:30 +0100

    dotprint: added switch to use short node names
    closes #28580.
    sympy.printing.dot.dotprint(...) creates a string representation
    of expressions, to be rendered into graph images by other packages,
    e.g. graphviz. This requires unique identifiers of the nodes.
    These unique identifiers are by default just the expression's srepr,
    which grows with the expression. If this string representation is
    too long, graphviz raises an error, and the graph is not rendered.
    This commit introduces a switch to create short node names of
    the form 'node_0', 'node_1', 'node_2', ...
    
    Example:
    In [1]: import sympy
    In [2]: from sympy.abc import x
    In [3]: import graphviz
    
    In [4]: expr = sympy.sin(x)
    In [5]: for i in range(8):
    ........... expr = sympy.Derivative(expr, x) + sympy.sin(expr)
    
    In [6]: string_dotprint = sympy.printing.dot.dotprint(expr,
    shortnames=True)
    In [7]: graph_source = graphviz.Source(string_dotprint)
    In [8]: graph_source.render(filename='mwe_breaking_sympy_graphviz_large', format='svg')
    
    Without 'shortnames=True' in line 6, graphviz would raise an error in
    line 8 and not render the graph. The default is 'False', which is the
    same behaviour as before this commit.

--- sympy/printing/tests/test_dot.py
+++ sympy/printing/tests/test_dot.py
@@ -1,4 +1,4 @@
-from sympy.printing.dot import (purestr, styleof, attrprint, dotnode,
+from sympy.printing.dot import (purestr, get_purestr_short, styleof, attrprint, dotnode,
         dotedges, dotprint)
 from sympy.core.basic import Basic
 from sympy.core.expr import Expr
@@ -19,6 +19,16 @@ def test_purestr():
             ('Basic(Integer(1), Integer(2))', ('Integer(1)', 'Integer(2)'))
     assert purestr(Float(2), with_args=True) == \
         ("Float('2.0', precision=53)", ())
+
+def test_get_purestr_short():
+    purestr_short = get_purestr_short()
+    assert purestr_short(Symbol('x')) == "Symbol('x')"
+    assert purestr_short(Basic(S(1), S(2))) == "node_0"
+    assert purestr_short(Float(2)) == "Float('2.0', precision=53)"
+
+    assert purestr_short(Symbol('x'), with_args=True) == ("Symbol('x')", ())
+    assert purestr_short(Basic(S(1), S(2)), with_args=True) == ('node_0', ('Integer(1)', 'Integer(2)'))
+    assert purestr_short(Float(2), with_args=True) == ("Float('2.0', precision=53)", ())
 
 
 def test_styleof():
@@ -49,6 +59,20 @@ def test_dotnode():
         '"Add(Symbol(\'x\'), Pow(Symbol(\'x\'), Integer(2)))_()" ' \
         '["color"="black", "label"="Add", "shape"="ellipse"];'
 
+def test_dotnode_shortnames():
+    purestr_short = get_purestr_short()
+
+    assert dotnode(x, repeat=False, nodenamefunc=purestr_short) == \
+        '"Symbol(\'x\')" ["color"="black", "label"="x", "shape"="ellipse"];'
+    assert dotnode(x+2, repeat=False, nodenamefunc=purestr_short) == \
+        '"node_0" ["color"="black", "label"="Add", "shape"="ellipse"];', \
+        dotnode(x+2,repeat=0, nodenamefunc=purestr_short)
+
+    assert dotnode(x + x**2, repeat=False, nodenamefunc=purestr_short) == \
+        '"node_1" ["color"="black", "label"="Add", "shape"="ellipse"];'
+    assert dotnode(x + x**2, repeat=True, nodenamefunc=purestr_short) == \
+        '"node_1_()" ["color"="black", "label"="Add", "shape"="ellipse"];'
+
 def test_dotedges():
     assert sorted(dotedges(x+2, repeat=False)) == [
         '"Add(Integer(2), Symbol(\'x\'))" -> "Integer(2)";',
@@ -57,6 +81,18 @@ def test_dotedges():
     assert sorted(dotedges(x + 2, repeat=True)) == [
         '"Add(Integer(2), Symbol(\'x\'))_()" -> "Integer(2)_(0,)";',
         '"Add(Integer(2), Symbol(\'x\'))_()" -> "Symbol(\'x\')_(1,)";'
+    ]
+
+def test_dotedges_shortnames():
+    purestr_short = get_purestr_short()
+
+    assert sorted(dotedges(x+2, repeat=False, nodenamefunc=purestr_short)) == [
+        '"node_0" -> "Integer(2)";',
+        '"node_0" -> "Symbol(\'x\')";'
+    ]
+    assert sorted(dotedges(x + 2, repeat=True, nodenamefunc=purestr_short)) == [
+        '"node_0_()" -> "Integer(2)_(0,)";',
+        '"node_0_()" -> "Symbol(\'x\')_(1,)";'
     ]
 
 def test_dotprint():
@@ -86,11 +122,51 @@ def test_dotprint():
         n in text for n in [dotnode(x, pos=(0,)), dotnode(x, pos=(1,))])
     assert 'digraph' in text
 
+def test_dotprint_shortnames():
+    purestr_short = get_purestr_short()
+
+    text = dotprint(x+2, repeat=False, shortnodenames=True)
+    assert all(e in text for e in dotedges(x+2, repeat=False, nodenamefunc=purestr_short))
+    assert all(
+        n in text for n in [dotnode(expr, repeat=False, nodenamefunc=purestr_short)
+        for expr in (x, Integer(2), x+2)])
+    assert 'digraph' in text
+
+    purestr_short = get_purestr_short()
+    text = dotprint(x+x**2, repeat=False, shortnodenames=True)
+    assert all(e in text for e in dotedges(x+x**2, repeat=False, nodenamefunc=purestr_short))
+    assert all(
+        n in text for n in [dotnode(expr, repeat=False, nodenamefunc=purestr_short)
+        for expr in (x, Integer(2), Integer(1)+Integer(1), x**2)]) #would also pass without int+int, because node_0 would then be x**2
+    assert 'digraph' in text
+
+    purestr_short = get_purestr_short()
+    text = dotprint(x+x**2, repeat=True, shortnodenames=True)
+    assert all(e in text for e in dotedges(x+x**2, repeat=True, nodenamefunc=purestr_short))
+    assert all(
+        n in text for n in [dotnode(expr, pos=(), nodenamefunc=purestr_short)
+        for expr in [x + x**2]])
+
+    purestr_short = get_purestr_short()
+    text = dotprint(x**x, repeat=True, shortnodenames=True)
+    assert all(e in text for e in dotedges(x**x, repeat=True, nodenamefunc=purestr_short))
+    assert all(
+        n in text for n in [dotnode(x, pos=(0,), nodenamefunc=purestr_short),
+                           dotnode(x, pos=(1,), nodenamefunc=purestr_short)])
+    assert 'digraph' in text
+
 def test_dotprint_depth():
     text = dotprint(3*x+2, depth=1)
     assert dotnode(3*x+2) in text
     assert dotnode(x) not in text
     text = dotprint(3*x+2)
+    assert "depth" not in text
+
+def test_dotprint_depth_shortnames():
+    text = dotprint(3*x+2, depth=1, shortnodenames=True)
+    assert "node_0" in text
+    assert dotnode(x) not in text
+    text = dotprint(3*x+2, shortnodenames=True)
     assert "depth" not in text
 
 def test_Matrix_and_non_basics():
@@ -121,6 +197,33 @@ def test_Matrix_and_non_basics():
 "MatrixSymbol(Str('X'), Symbol('n'), Symbol('n'))_()" -> "Symbol('n')_(2,)";
 }"""
 
+def test_Matrix_and_non_basics_shortnames():
+    from sympy.matrices.expressions.matexpr import MatrixSymbol
+    n = Symbol('n')
+    assert dotprint(MatrixSymbol('X', n, n), shortnodenames=True) == \
+"""digraph{
+
+# Graph style
+"ordering"="out"
+"rankdir"="TD"
+
+#########
+# Nodes #
+#########
+
+"node_0_()" ["color"="black", "label"="MatrixSymbol", "shape"="ellipse"];
+"Str('X')_(0,)" ["color"="blue", "label"="X", "shape"="ellipse"];
+"Symbol('n')_(1,)" ["color"="black", "label"="n", "shape"="ellipse"];
+"Symbol('n')_(2,)" ["color"="black", "label"="n", "shape"="ellipse"];
+
+#########
+# Edges #
+#########
+
+"node_0_()" -> "Str('X')_(0,)";
+"node_0_()" -> "Symbol('n')_(1,)";
+"node_0_()" -> "Symbol('n')_(2,)";
+}"""
 
 def test_labelfunc():
     text = dotprint(x + 2, labelfunc=srepr)
