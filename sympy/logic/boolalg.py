dc7c4220b8eec99448e4564c075af960522ac2dd
Christopher Smith <smichr@gmail.com>
2025-11-18 16:50:23 -0600

    Update boolalg.py

--- sympy/logic/boolalg.py
+++ sympy/logic/boolalg.py
@@ -5,7 +5,6 @@ Boolean algebra module for SymPy
 from __future__ import annotations
 from typing import TYPE_CHECKING, overload, Any
 from collections.abc import Iterable, Mapping
-import sys
 
 from collections import defaultdict
 from itertools import chain, combinations, product, permutations
