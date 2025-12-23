34a8a4679a297deaf94e2226b9e9b4eda5508e32
Anshul Sharma <anshulsharma4605@gmail.com>
2025-12-23 20:13:23 +0530

    Revert local suppression of parseString deprecation warning

--- sympy/utilities/memoization.py
+++ sympy/utilities/memoization.py
@@ -7,32 +7,24 @@ def recurrence_memo(
     initial: List[Any],
 ) -> Callable[..., Any]:
     """
-    Memo decorator for sequences defined by recurrence.
-
-    Parameters
-    ==========
-    initial : list
-        The initial values of the sequence.
-
-    Returns
-    =======
-    function
-        The decorated recurrence function.
+    Memo decorator for sequences defined by recurrence
 
     Examples
     ========
+
     >>> from sympy.utilities.memoization import recurrence_memo
-    >>> @recurrence_memo([1])
+    >>> @recurrence_memo([1]) # 0! = 1
     ... def factorial(n, prev):
     ...     return n * prev[-1]
     >>> factorial(4)
     24
-    >>> factorial(3)
+    >>> factorial(3) # use cache values
     6
-    >>> factorial.cache_length()
+    >>> factorial.cache_length() # cache length can be obtained
     5
     >>> factorial.fetch_item(slice(2, 4))
     [2, 6]
+
     """
     cache: List[Any] = initial
 
@@ -57,22 +49,12 @@ def assoc_recurrence_memo(
     base_seq: Callable[[int], Any],
 ) -> Callable[..., Any]:
     """
-    Memo decorator for associated sequences defined by recurrence starting from base.
+    Memo decorator for associated sequences defined by recurrence starting from base
 
-    Parameters
-    ==========
-    base_seq : callable
-        Callable to get base sequence elements.
+    base_seq(n) -- callable to get base sequence elements
 
-    Returns
-    =======
-    function
-        The decorated associated recurrence function.
-
-    Notes
-    =====
-    - Works only for Pn0 = base_seq(0) cases
-    - Works only for m <= n cases
+    XXX works only for Pn0 = base_seq(0) cases
+    XXX works only for m <= n cases
     """
     cache: List[List[Any]] = []
 
@@ -84,10 +66,12 @@ def assoc_recurrence_memo(
                 return cache[n][m]
 
             for i in range(L, n + 1):
+                # get base sequence
                 F_i0 = base_seq(i)
                 F_i_cache = [F_i0]
                 cache.append(F_i_cache)
-
+                # XXX only works for m <= n cases
+                # generate assoc sequence
                 for j in range(1, i + 1):
                     F_ij = f(i, j, cache)
                     F_i_cache.append(F_ij)
