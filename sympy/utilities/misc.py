c0d53a8b005e56b5245716c35b2dce20b219c7f3
vedantdusane <dusanev4@gmail.com>
2026-01-01 07:35:17 -0800

    Simplify func_name: convert typ to string immediately as suggested by mentor

--- sympy/utilities/misc.py
+++ sympy/utilities/misc.py
@@ -330,12 +330,12 @@ def func_name(x: Any, short: bool = False) -> str:
     'Equality': 'Eq',
     'Unequality': 'Ne',
     }
-    typ: type | str = type(x)
+    typ = str(type(x))
     if str(typ).startswith("<type '"):
         typ = str(typ).split("'")[1].split("'")[0]
     elif str(typ).startswith("<class '"):
         typ = str(typ).split("'")[1].split("'")[0]
-    rv: str = getattr(getattr(x, 'func', x), '__name__', str(typ))
+    rv = getattr(getattr(x, 'func', x), '__name__', typ)
     if '.' in rv:
         rv = rv.split('.')[-1]
     if short:
