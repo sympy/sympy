e931c19451b0b4427ceb21745d4acee73f452f07
vedantdusane <dusanev4@gmail.com>
2026-01-18 11:25:59 -0800

    Fix typing issues in recurrence_memo protocol

--- sympy/utilities/memoization.py
+++ sympy/utilities/memoization.py
@@ -1,7 +1,20 @@
 from functools import wraps
+from typing import TypeVar, Callable, List, Protocol, cast, Union
+
+T = TypeVar("T") # Elements of the main sequence
+U = TypeVar("U") # Elements of associated sequences
 
 
-def recurrence_memo(initial):
+class RecurrenceFunc(Protocol[T]):
+    def __call__(self, n: int) -> T: ...
+    cache_length: Callable[[], int]
+    fetch_item: Callable[[Union[int, slice]], Union[T, List[T]]]
+
+
+def recurrence_memo(initial: List[T]) -> Callable[
+    [Callable[[int, List[T]], T]],
+    RecurrenceFunc[T],
+]:
     """
     Memo decorator for sequences defined by recurrence
 
@@ -22,38 +35,43 @@ def recurrence_memo(initial):
     [2, 6]
 
     """
-    cache = initial
+    cache: List[T] = initial.copy()
 
-    def decorator(f):
+    def decorator(f: Callable[[int, List[T]], T]) -> RecurrenceFunc[T]:
         @wraps(f)
-        def g(n):
+        def g(n: int) -> T:
             L = len(cache)
             if n < L:
                 return cache[n]
             for i in range(L, n + 1):
                 cache.append(f(i, cache))
             return cache[-1]
-        g.cache_length = lambda: len(cache)
-        g.fetch_item = lambda x: cache[x]
-        return g
+        rf = cast(RecurrenceFunc[T], g)
+        rf.cache_length = lambda: len(cache)
+        rf.fetch_item = lambda x: cache[x]
+        return rf
+    
     return decorator
 
 
-def assoc_recurrence_memo(base_seq):
+def assoc_recurrence_memo(base_seq: Callable[[int], U]) -> Callable[
+    [Callable[[int, int, List[List[U]]], U]],
+    Callable[[int, int], U],
+]:
     """
     Memo decorator for associated sequences defined by recurrence starting from base
 
-    base_seq(n) -- callable to get base sequence elements
+    base_seq(n) -- Callable to get base sequence elements
 
     XXX works only for Pn0 = base_seq(0) cases
     XXX works only for m <= n cases
     """
 
-    cache = []
+    cache: List[List[U]] = []
 
-    def decorator(f):
+    def decorator(f: Callable[[int, int, List[List[U]]], U]) -> Callable[[int, int], U]:
         @wraps(f)
-        def g(n, m):
+        def g(n: int, m: int) -> U:
             L = len(cache)
             if n < L:
                 return cache[n][m]
@@ -61,7 +79,7 @@ def assoc_recurrence_memo(base_seq):
             for i in range(L, n + 1):
                 # get base sequence
                 F_i0 = base_seq(i)
-                F_i_cache = [F_i0]
+                F_i_cache: List[U] = [F_i0]
                 cache.append(F_i_cache)
 
                 # XXX only works for m <= n cases
