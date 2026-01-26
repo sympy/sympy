15d9c2f1b44ae3c79f794297a508b1f0944d3f9f
vedantdusane <dusanev4@gmail.com>
2026-01-26 06:21:06 -0800

    Add type annotations to timed()

--- sympy/utilities/timeutils.py
+++ sympy/utilities/timeutils.py
@@ -10,11 +10,7 @@ _scales = [1e0, 1e3, 1e6, 1e9]
 _units = ['s', 'ms', '\N{GREEK SMALL LETTER MU}s', 'ns']
 
 
-def timed(
-        func: Callable[[], object],
-          setup: str = "pass",
-            limit: int | None = None,
-        ) -> tuple[int, float, float, str]:
+def timed(func: Callable[[], object], setup: str = "pass", limit: int | None = None) -> tuple[int, float, float, str]:
     """Adaptively measure execution time of a function. """
     timer = timeit.Timer(func, setup=setup)
     repeat, number = 3, 1
