c0c0d5587962af2b62ef1f305b036de8b5b1f658
Atharva <atharvaparandeytl@gmail.com>
2026-01-28 08:12:56 +0530

    Simplify _eval_evalf method in FiniteSet

--- sympy/sets/sets.py
+++ sympy/sets/sets.py
@@ -2144,18 +2144,9 @@ class FiniteSet(Set):
         return Or(*[Eq(symbol, elem) for elem in self])
 
     def _eval_evalf(self, prec):
-        from sympy.core.containers import Tuple
-        dps = prec_to_dps(prec)
-
-        new_elems = []
-        for elem in self:
-            if isinstance(elem, Tuple):
-                new_elems.append(Tuple(*(e.evalf(n=dps) for e in elem)))
-            else:
-                new_elems.append(elem.evalf(n=dps))
-
-        return FiniteSet(*new_elems)
-
+      dps = prec_to_dps(prec)
+    return FiniteSet(*(elem.evalf(n=dps) for elem in self))
+    
     def _eval_simplify(self, **kwargs):
         from sympy.simplify import simplify
         return FiniteSet(*[simplify(elem, **kwargs) for elem in self])
