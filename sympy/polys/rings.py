68ef0846d676e18a0dc376794dde6908a3e19c9c
ayush-bothra <ayush9bothra@gmail.com>
2026-01-30 13:02:49 +0000

    POLYS: refactor sring to have cleaner try-except block

--- sympy/polys/rings.py
+++ sympy/polys/rings.py
@@ -174,6 +174,40 @@ def vring(symbols, domain, order=lex):
     return _ring
 
 
+def fast_sring(exprs, opt_fast):
+    if not opt_fast.gens:
+        symbols = set()
+        for expr in exprs:
+            symbols.update({s for s in expr.free_symbols if s.is_commutative})
+        opt_fast.gens = tuple(sorted(symbols, key=default_sort_key))
+
+    if opt_fast.domain is None:
+        numbers = []
+        for expr in exprs:
+            numbers.extend(expr.atoms(Number))
+        opt_fast.domain, coeffs_dom = construct_domain(numbers, opt=opt_fast)
+
+    _ring = PolyRing(opt_fast.gens, opt_fast.domain, opt_fast.order)
+    polys = list(map(_ring.from_expr, exprs))
+    return _ring, polys
+
+
+def slow_sring(exprs, opt):
+    reps, opt = _parallel_dict_from_expr(exprs, opt)
+
+    if opt.domain is None:
+        coeffs = sum([list(rep.values()) for rep in reps], [])
+
+        opt.domain, coeffs_dom = construct_domain(coeffs, opt=opt)
+
+        coeff_map = dict(zip(coeffs, coeffs_dom))
+        reps = [{m: coeff_map[c] for m, c in rep.items()} for rep in reps]
+   
+    _ring = PolyRing(opt.gens, opt.domain, opt.order)
+    polys = list(map(_ring.from_dict, reps))
+    return _ring, polys
+
+
 @public
 def sring(exprs, *symbols, **options):
     """Construct a ring deriving generators and domain from options and input expressions.
@@ -207,35 +241,10 @@ def sring(exprs, *symbols, **options):
 
     exprs = list(map(sympify, exprs))
     opt = build_options(symbols, options)
-    opt_fast = opt.clone()
     try:
-        if not opt_fast.gens:
-            symbols = set()
-            for expr in exprs:
-                symbols.update({s for s in expr.free_symbols if s.is_commutative})
-            opt_fast.gens = tuple(sorted(symbols, key=default_sort_key))
-
-        if opt_fast.domain is None:
-            numbers = []
-            for expr in exprs:
-                numbers.extend(expr.atoms(Number))
-            opt_fast.domain, coeffs_dom = construct_domain(numbers, opt=opt_fast)
-
-        _ring = PolyRing(opt_fast.gens, opt_fast.domain, opt_fast.order)
-        polys = [_ring.from_expr(expr) for expr in exprs]
-    except (ValueError, BasePolynomialError, NotImplementedError):
-        reps, opt = _parallel_dict_from_expr(exprs, opt)
-
-        if opt.domain is None:
-            coeffs = sum([list(rep.values()) for rep in reps], [])
-
-            opt.domain, coeffs_dom = construct_domain(coeffs, opt=opt)
-
-            coeff_map = dict(zip(coeffs, coeffs_dom))
-            reps = [{m: coeff_map[c] for m, c in rep.items()} for rep in reps]
-
-        _ring = PolyRing(opt.gens, opt.domain, opt.order)
-        polys = list(map(_ring.from_dict, reps))
+        _ring, polys = fast_sring(exprs, opt.clone())
+    except (ValueError, BasePolynomialError):
+        _ring, polys = slow_sring(exprs, opt)
 
     if single:
         return (_ring, polys[0])
