46e0a9c5fe62b270c5da84494ee391a3bba6a1b4
IronwallxR5 <rathipadamr5@gmail.com>
2026-02-01 13:15:57 +0530

    Fix missing comma in test_direction_cosine

--- sympy/geometry/tests/test_point.py
+++ sympy/geometry/tests/test_point.py
@@ -474,7 +474,7 @@ def test_direction_cosine():
 
     assert p1.direction_cosine(Point3D(2.4, 2.4, 0)) == [sqrt(2)/2, sqrt(2)/2, 0]
     assert p1.direction_cosine(Point3D(1, 1, 1)) == [sqrt(3) / 3, sqrt(3) / 3, sqrt(3) / 3]
-    assert p1.direction_cosine(Point3D(-12, 0 -15)) == [-4*sqrt(41)/41, -5*sqrt(41)/41, 0]
+    assert p1.direction_cosine(Point3D(-12, 0, -15)) == [-4*sqrt(41)/41, 0, -5*sqrt(41)/41]
 
     assert p2.direction_cosine(Point3D(0, 0, 0)) == [-sqrt(3) / 3, -sqrt(3) / 3, -sqrt(3) / 3]
     assert p2.direction_cosine(Point3D(1, 1, 12)) == [0, 0, 1]
