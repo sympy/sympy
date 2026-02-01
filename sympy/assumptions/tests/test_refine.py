117a21f157b3e29488fbde6148b18d8a9e4ce337
Sujal Kumar <sujaljayantkumar06@gmail.com>
2026-02-01 12:15:00 +0530

    Added more coverage of tests.

--- sympy/assumptions/tests/test_refine.py
+++ sympy/assumptions/tests/test_refine.py
@@ -229,6 +229,7 @@ def test_matrixelement():
 
 def test_sin_cos():
     n = Symbol('n')
+    m = Symbol('m')
     assert refine(cos(n*pi/2), Q.odd(n)) == 0
     assert refine(cos(n*pi), Q.even(n)) == 1
     assert refine(cos(n*pi), Q.odd(n)) == -1
@@ -252,3 +253,5 @@ def test_sin_cos():
     assert refine(cos(x + n*pi/2), Q.odd(n)) == ((-1)**((n + 1)/2)) * sin(x)
     assert refine(sin(x + y + 2*n*pi), Q.integer(n)) == sin(x + y)
     assert refine(cos(x + y + 2*n*pi), Q.integer(n)) == cos(x + y)
+    assert refine(cos(x + n * pi + m * pi / 2), Q.integer(n) & Q.even(m)) == (-1) ** (n + m / 2) * cos(x)
+    assert refine(cos(x + n * pi + m * pi / 2), Q.integer(n) & Q.odd(m)) == (-1) ** (n + (m + 1) / 2) * sin(x)
