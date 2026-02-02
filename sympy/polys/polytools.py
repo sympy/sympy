2c489cb68b7028121b453e49a3ab8fadd5e7c2b0
vedantdusane <dusanev4@gmail.com>
2026-02-02 09:19:39 -0800

    Change return type Poly to Self

--- sympy/polys/polytools.py
+++ sympy/polys/polytools.py
@@ -203,7 +203,7 @@ class Poly(Basic):
     # avoiding creating a Basic instance just to be hashable.
 
     @classmethod
-    def new(cls, rep: DMP, *gens: Expr) -> Poly:
+    def new(cls, rep: DMP, *gens: Expr) -> Self:
         """Construct :class:`Poly` instance from raw representation. """
         if not isinstance(rep, DMP):
             raise PolynomialError(
