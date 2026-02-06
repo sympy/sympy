7ab9fd6178b5f0a3f94db1dc7ecb9ab095e6e8a0
Ayush Kumar Jha <jha44481@gmail.com>
2026-02-07 03:09:09 +0530

    Remove nC import

--- sympy/functions/combinatorial/tests/test_comb_numbers.py
+++ sympy/functions/combinatorial/tests/test_comb_numbers.py
@@ -20,7 +20,7 @@ from sympy.functions import (
     primenu, primeomega, totient, reduced_totient, primepi,
     motzkin, binomial, gamma, sqrt, cbrt, hyper, log, digamma,
     trigamma, polygamma, factorial, sin, cos, cot, polylog, zeta, dirichlet_eta)
-from sympy.functions.combinatorial.numbers import _nT, nP,nC
+from sympy.functions.combinatorial.numbers import _nT, nP
 from sympy.ntheory.factor_ import factorint
 
 from sympy.core.expr import unchanged
