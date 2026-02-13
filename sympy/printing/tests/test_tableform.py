731e1c4fe2f50a9c959e13de0b8e7db9eec5ba96
Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>
2026-02-13 19:05:31 +0530

    Use array instead of tabular in TableForm LaTeX output
    The _latex method's contract requires returning math-mode LaTeX,
    but TableForm was using \begin{tabular} which is a text-mode
    environment. This caused invalid LaTeX when TableForm was nested
    inside other expressions (e.g. latex([t])) because math-mode
    commands like \left[ and \right] cannot wrap text-mode content.
    
    Switch to \begin{array} which is the math-mode equivalent, and
    drop the $...$ wrapping around cell values since array is already
    in math mode.
    
    Fixes #20063

--- sympy/printing/tests/test_tableform.py
+++ sympy/printing/tests/test_tableform.py
@@ -104,57 +104,57 @@ def test_TableForm_latex():
     s = latex(TableForm([[0, x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]],
             wipe_zeros=True, headings=("automatic", "automatic")))
     assert s == (
-        '\\begin{tabular}{r l l}\n'
+        '\\begin{array}{r l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
-        '1 &   & $x^{3}$ \\\\\n'
-        '2 & $c$ & $\\frac{1}{4}$ \\\\\n'
-        '3 & $\\sqrt{x}$ & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '1 &   & x^{3} \\\\\n'
+        '2 & c & \\frac{1}{4} \\\\\n'
+        '3 & \\sqrt{x} & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
     s = latex(TableForm([[0, x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]],
             wipe_zeros=True, headings=("automatic", "automatic"), alignments='l'))
     assert s == (
-        '\\begin{tabular}{r l l}\n'
+        '\\begin{array}{r l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
-        '1 &   & $x^{3}$ \\\\\n'
-        '2 & $c$ & $\\frac{1}{4}$ \\\\\n'
-        '3 & $\\sqrt{x}$ & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '1 &   & x^{3} \\\\\n'
+        '2 & c & \\frac{1}{4} \\\\\n'
+        '3 & \\sqrt{x} & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
     s = latex(TableForm([[0, x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]],
             wipe_zeros=True, headings=("automatic", "automatic"), alignments='l'*3))
     assert s == (
-        '\\begin{tabular}{l l l}\n'
+        '\\begin{array}{l l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
-        '1 &   & $x^{3}$ \\\\\n'
-        '2 & $c$ & $\\frac{1}{4}$ \\\\\n'
-        '3 & $\\sqrt{x}$ & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '1 &   & x^{3} \\\\\n'
+        '2 & c & \\frac{1}{4} \\\\\n'
+        '3 & \\sqrt{x} & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
     s = latex(TableForm([["a", x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]],
             headings=("automatic", "automatic")))
     assert s == (
-        '\\begin{tabular}{r l l}\n'
+        '\\begin{array}{r l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
-        '1 & $a$ & $x^{3}$ \\\\\n'
-        '2 & $c$ & $\\frac{1}{4}$ \\\\\n'
-        '3 & $\\sqrt{x}$ & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '1 & a & x^{3} \\\\\n'
+        '2 & c & \\frac{1}{4} \\\\\n'
+        '3 & \\sqrt{x} & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
     s = latex(TableForm([["a", x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]],
             formats=['(%s)', None], headings=("automatic", "automatic")))
     assert s == (
-        '\\begin{tabular}{r l l}\n'
+        '\\begin{array}{r l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
-        '1 & (a) & $x^{3}$ \\\\\n'
-        '2 & (c) & $\\frac{1}{4}$ \\\\\n'
-        '3 & (sqrt(x)) & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '1 & (a) & x^{3} \\\\\n'
+        '2 & (c) & \\frac{1}{4} \\\\\n'
+        '3 & (sqrt(x)) & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
 
     def neg_in_paren(x, i, j):
@@ -165,18 +165,18 @@ def test_TableForm_latex():
     s = latex(TableForm([[-1, 2], [-3, 4]],
             formats=[neg_in_paren]*2, headings=("automatic", "automatic")))
     assert s == (
-        '\\begin{tabular}{r l l}\n'
+        '\\begin{array}{r l l}\n'
         ' & 1 & 2 \\\\\n'
         '\\hline\n'
         '1 & -1 & 2 \\\\\n'
         '2 & (-3) & 4 \\\\\n'
-        '\\end{tabular}'
+        '\\end{array}'
     )
     s = latex(TableForm([["a", x**3], ["c", S.One/4], [sqrt(x), sin(x**2)]]))
     assert s == (
-        '\\begin{tabular}{l l}\n'
-        '$a$ & $x^{3}$ \\\\\n'
-        '$c$ & $\\frac{1}{4}$ \\\\\n'
-        '$\\sqrt{x}$ & $\\sin{\\left(x^{2} \\right)}$ \\\\\n'
-        '\\end{tabular}'
+        '\\begin{array}{l l}\n'
+        'a & x^{3} \\\\\n'
+        'c & \\frac{1}{4} \\\\\n'
+        '\\sqrt{x} & \\sin{\\left(x^{2} \\right)} \\\\\n'
+        '\\end{array}'
     )
