5ab4cc8f3ef93f4e0827165a91afe91c2c7c440d
graphpilled <graphpilled@protonmail.com>
2026-02-14 02:16:06 -0600

    printing: Move underscore escaping before translate() for Greek letter support

--- sympy/printing/tests/test_latex.py
+++ sympy/printing/tests/test_latex.py
@@ -433,7 +433,7 @@ def test_latex_symbols():
     assert latex(Symbol('_x')) == r"\_x"
     assert latex(Symbol('__x')) == r"\_\_x"
     assert latex(2 * Symbol('_x')) == r"2 \_x"
-    assert latex(Symbol('_alpha')) == r"\_alpha"
+    assert latex(Symbol('_alpha')) == r"\_\alpha"
     assert latex(Symbol('_x_1')) == r"\_x_{1}"
     assert latex(Symbol('_')) == r"\_"
     assert latex(Symbol('__')) == r"\_\_"
