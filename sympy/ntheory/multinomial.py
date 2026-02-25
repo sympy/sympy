5f450ec603e6a069fd9c96ae7f74e7aa64431b37
Andrew Phimister <andyph1@hotmail.com>
2026-02-25 13:08:37 +0000

    Changed formatting in line with sympy conventions

--- sympy/ntheory/multinomial.py
+++ sympy/ntheory/multinomial.py
@@ -132,7 +132,7 @@ def multinomial_coefficients(m: SupportsIndex, n: SupportsIndex) -> dict[tuple[i
 
 
 def multinomial_coefficients_iterator(
-        m: SupportsIndex, n: SupportsIndex, _tuple: Type[tuple[int, ...]] = tuple
+    m: SupportsIndex, n: SupportsIndex, _tuple: Type[tuple[int, ...]] = tuple
 ) -> Iterable[tuple[tuple[int, ...], int]]:
     """multinomial coefficient iterator
 
