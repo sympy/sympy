b43e778da50b50b98423c9aa038abc8e96b4421c
Musab Kasbati <musab16000@gmail.com>
2026-03-04 06:39:23 +0500

    remove redeclaration of variable

--- sympy/core/tests/test_power.py
+++ sympy/core/tests/test_power.py
@@ -614,8 +614,6 @@ def test_issue_14704():
 
 
 def test_issue_29000():
-    x = symbols('x')
-
     assert re(sqrt(x)).subs(x, 0) == 0
     assert im(sqrt(x)).subs(x, 0) == 0
     assert sqrt(x).as_real_imag()[0].subs(x, 0) == 0
