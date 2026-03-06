0cbafca346efa0d38246836a4c1a2d19a4560c0e
httpsVishu <vishusaini687@gmail.com>
2026-03-06 16:33:47 +0000

    added test for quantity substitution behaviour

--- sympy/physics/units/quantities.py
+++ sympy/physics/units/quantities.py
@@ -9,7 +9,6 @@ from sympy.core.sympify import sympify
 from sympy.physics.units.dimensions import _QuantityMapper
 from sympy.physics.units.prefixes import Prefix
 
-
 class Quantity(AtomicExpr):
     """
     Physical quantity: can be a unit of measure, a constant or a generic quantity.
@@ -151,3 +150,5 @@ class PhysicalConstant(Quantity):
     """Represents a physical constant, eg. `speed_of_light` or `avogadro_constant`."""
 
     is_physical_constant = True
+
+
