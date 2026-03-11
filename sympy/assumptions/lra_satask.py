34760714a3fac96e76a082084271b22d4624ac30
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2026-03-10 23:39:56 -0700

    lra_satask: Use unit clauses and refactor
    Use predicates from unit clauses in `assumptions_encoded_cnf` to
    determine whether expression are real. Previously, only assumuptions
    stored using the old assumption system were checked to determine if an
    expression was real.

--- sympy/assumptions/lra_satask.py
+++ sympy/assumptions/lra_satask.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from sympy.assumptions.assume import global_assumptions
 from sympy.assumptions.cnf import CNF, EncodedCNF
 from sympy.assumptions.ask import Q
+from sympy.core.numbers import I
 from sympy.logic.inference import satisfiable
 from sympy.logic.algorithms.lra_theory import UnhandledInput, ALLOWED_PRED
 from sympy.matrices.kind import MatrixKind
@@ -10,6 +11,17 @@ from sympy.assumptions.assume import AppliedPredicate
 from sympy.core.mul import Mul
 from sympy.core.singleton import S
 
+
+# Some predicates such as Q.prime can't be handled by lra_satask. For example,
+# (x > 0) & (x < 1) & Q.prime(x) is unsat but lra_satask would think it was sat.
+# WHITE_LIST is a list of predicates that can always be handled.
+WHITE_LIST = ALLOWED_PRED | {Q.positive, Q.negative, Q.zero, Q.nonzero, Q.nonpositive, Q.nonnegative,
+    Q.negative_infinite, Q.positive_infinite}
+
+REAL_IMPLYING_PREDICATES = WHITE_LIST | {Q.real}
+
+EXTENED_REAL_IMPLYING_PREDICATES = REAL_IMPLYING_PREDICATES | {Q.extended_real, Q.extended_positive, Q.extended_negative, Q.extended_nonpositive,
+    Q.extended_negative, Q.extended_nonzero}
 
 def lra_satask(proposition, assumptions=True, context=global_assumptions):
     """
@@ -24,36 +36,75 @@ def lra_satask(proposition, assumptions=True, context=global_assumptions):
     _props = CNF.from_prop(~proposition)
 
     cnf = CNF.from_prop(assumptions)
-    assumptions = EncodedCNF()
-    assumptions.from_cnf(cnf)
+    assumptions_encoded_cnf = EncodedCNF()
+    assumptions_encoded_cnf.from_cnf(cnf)
 
     context_cnf = CNF()
     if context:
         context_cnf = context_cnf.extend(context)
 
-    assumptions.add_from_cnf(context_cnf)
-
-    return check_satisfiability(props, _props, assumptions)
-
-# Some predicates such as Q.prime can't be handled by lra_satask.
-# For example, (x > 0) & (x < 1) & Q.prime(x) is unsat but lra_satask would think it was sat.
-# WHITE_LIST is a list of predicates that can always be handled.
-WHITE_LIST = ALLOWED_PRED | {Q.positive, Q.negative, Q.zero, Q.nonzero, Q.nonpositive, Q.nonnegative,
-                                            Q.extended_positive, Q.extended_negative, Q.extended_nonpositive,
-                                            Q.extended_negative, Q.extended_nonzero, Q.negative_infinite,
-                                            Q.positive_infinite}
+    assumptions_encoded_cnf.add_from_cnf(context_cnf)
 
 
-def check_satisfiability(prop, _prop, factbase):
+    # Use unit clauses from `assumptions_encoded_cnf` to
+    # deduce some expressions are extened real.
+    reverse_encoding = {value: key for key, value in assumptions_encoded_cnf.encoding.items()}
+    real_exprs = set()
+    for unit_clause in assumptions_encoded_cnf.data:
+        # Check if `unit_clause` is a unit clause.
+        if len(unit_clause) != 1:
+            continue
+
+        # Continue if negative because negative literals
+        # cannot establish that an expression is entended real.
+        (positive_literal,) = unit_clause
+        if not (positive_literal > 0):
+            continue
+
+        applied_predicate = reverse_encoding[positive_literal]
+        if not isinstance(applied_predicate, AppliedPredicate):
+            continue
+
+        if applied_predicate.function in REAL_IMPLYING_PREDICATES:
+            real_exprs.update(applied_predicate.arguments)
+
+    # SATIFSIABLE STARTED HERE
+
+    factbase = assumptions_encoded_cnf
     sat_true = factbase.copy()
     sat_false = factbase.copy()
-    sat_true.add_from_cnf(prop)
-    sat_false.add_from_cnf(_prop)
+    sat_true.add_from_cnf(props)
+    sat_false.add_from_cnf(_props)
 
-    all_pred, all_exprs = get_all_pred_and_expr_from_enc_cnf(sat_true)
+
+    # Check old assumptions to:
+    #   1. Deduce some expressions are extened real.
+    #   2. Add relevant inequality assumptions as unit clauses
+    #      to `assumptions_encoded_cnf`.
+    all_pred, all_exprs = get_all_pred_and_expr_from_enc_cnf([sat_true])
+    for expr in all_exprs:
+        convertable_to_inequality_applied_predicate, is_real  = extract_assumption_from_old_assumption(expr)
+        if is_real:
+            real_exprs.add(expr)
+        if convertable_to_inequality_applied_predicate is None:
+            continue
+
+
+        if convertable_to_inequality_applied_predicate not in assumptions_encoded_cnf.encoding:
+            applied_predicate_encoding = len(assumptions_encoded_cnf.encoding) + 1
+            assumptions_encoded_cnf.encoding[convertable_to_inequality_applied_predicate] = applied_predicate_encoding
+
+        # Add relevant old assumptions to `assumptions`.
+        applied_predicate_encoding = assumptions_encoded_cnf.encoding[convertable_to_inequality_applied_predicate]
+        assumptions_encoded_cnf.data.append([applied_predicate_encoding])
+
+    for expr in all_exprs:
+        if expr not in real_exprs:
+            raise UnhandledInput(f"LRASolver: {expr} must be real")
+
 
     for pred in all_pred:
-        if pred.function not in WHITE_LIST and pred.function != Q.ne:
+        if pred.function not in EXTENED_REAL_IMPLYING_PREDICATES and pred.function != Q.ne:
             raise UnhandledInput(f"LRASolver: {pred} is an unhandled predicate")
     for expr in all_exprs:
         if expr.kind == MatrixKind(NumberKind):
@@ -61,18 +112,13 @@ def check_satisfiability(prop, _prop, factbase):
         if expr == S.NaN:
             raise UnhandledInput("LRASolver: nan")
 
-    # convert old assumptions into predicates and add them to sat_true and sat_false
-    # also check for unhandled predicates
-    for assm in extract_pred_from_old_assum(all_exprs):
-        n = len(sat_true.encoding)
-        if assm not in sat_true.encoding:
-            sat_true.encoding[assm] = n+1
-        sat_true.data.append([sat_true.encoding[assm]])
+    # check satisfiable
 
-        n = len(sat_false.encoding)
-        if assm not in sat_false.encoding:
-            sat_false.encoding[assm] = n+1
-        sat_false.data.append([sat_false.encoding[assm]])
+    # factbase = assumptions_encoded_cnf
+    # sat_true = factbase.copy()
+    # sat_false = factbase.copy()
+    # sat_true.add_from_cnf(props)
+    # sat_false.add_from_cnf(_props)
 
 
     sat_true = _preprocess(sat_true)
@@ -97,7 +143,7 @@ def check_satisfiability(prop, _prop, factbase):
 def _preprocess(enc_cnf):
     """
     Returns an encoded cnf with only Q.eq, Q.gt, Q.lt,
-    Q.ge, and Q.le predicate.
+    Q.ge, Q.le, Q.real, Q.extended_real predicate.
 
     Converts every unequality into a disjunction of strict
     inequalities. For example, x != 3 would become
@@ -204,6 +250,7 @@ def _pred_to_binrel(pred):
 
     return pred
 
+
 pred_to_pos_neg_zero = {
     Q.extended_positive: Q.positive,
     Q.extended_negative: Q.negative,
@@ -214,15 +261,76 @@ pred_to_pos_neg_zero = {
     Q.positive_infinite: False
 }
 
-def get_all_pred_and_expr_from_enc_cnf(enc_cnf):
+
+def get_all_pred_and_expr_from_enc_cnf(enc_cnfs):
     all_exprs = set()
     all_pred = set()
-    for pred in enc_cnf.encoding.keys():
-        if isinstance(pred, AppliedPredicate):
-            all_pred.add(pred)
-            all_exprs.update(pred.arguments)
+    for enc_cnf in enc_cnfs:
+        for pred in enc_cnf.encoding.keys():
+            if isinstance(pred, AppliedPredicate):
+                all_pred.add(pred)
+                all_exprs.update(pred.arguments)
 
     return all_pred, all_exprs
+
+
+def extract_assumption_from_old_assumption(expr):
+    """
+    Extracts a single relevant assumption from the old assumption system
+    if there are any. Otherwise, it either returns None or raises an exception
+    if a dissallowed assumption is present.
+
+    Gives a
+
+    Example
+    =======
+    >>> from sympy.assumptions.lra_satask import extract_assumption_from_old_assumption
+    >>> from sympy import symbols
+    >>> x = symbols("x")
+    >>> extract_assumption_from_old_assumption(x) is None
+    True
+    >>> y = symbols("y", positive=True)
+    >>> extract_assumption_from_old_assumption(y)
+    Q.positive(y)
+    >>> extract_assumption_from_old_assumption(-y)
+    Q.negative(-y)
+    >>> y = symbols("y", integer=True)
+    >>> extract_assumption_from_old_assumption(y) # raises exception
+    """
+    if not hasattr(expr, "free_symbols") or len(expr.free_symbols) == 0:
+        if expr.is_real:
+            return None, True
+        return None, None
+
+    # Test for I times imaginary variable. Such expressions are considered
+    # real but aren't handled.
+    if expr.has(I):
+        raise UnhandledInput(f"LRASolver: {expr} must not contain I")
+
+    if expr.is_integer == True and expr.is_zero != True:
+        raise UnhandledInput(f"LRASolver: {expr} is an integer")
+    if expr.is_integer == False:
+        raise UnhandledInput(f"LRASolver: {expr} can't be an integer")
+    if expr.is_rational == False:
+        raise UnhandledInput(f"LRASolver: {expr} is irational")
+
+    if expr.is_zero:
+        return Q.zero(expr), True
+    if expr.is_positive:
+        return Q.positive(expr), True
+    if expr.is_negative:
+        return Q.negative(expr), True
+    if expr.is_nonzero:
+        return Q.nonzero(expr), True
+    if expr.is_nonpositive:
+        return Q.nonpositive(expr), True
+    if expr.is_nonnegative:
+        return Q.nonnegative(expr), True
+    if expr.is_real:
+        return None, True
+
+    return None, None
+
 
 def extract_pred_from_old_assum(all_exprs):
     """
