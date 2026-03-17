fd3234de76489a10f2e5d5af87a54bd6e156c0c1
Palak Kumari <palak.kumari1025@gmail.com>
2026-03-17 22:09:13 +0530

    Remove outdated XFAIL and fix inconsistent expectations in test_eigen_vects

--- sympy/matrices/tests/test_eigen.py
+++ sympy/matrices/tests/test_eigen.py
@@ -162,12 +162,10 @@ def test_float_eigenvals():
 @XFAIL
 def test_eigen_vects():
     m = Matrix(2, 2, [1, 0, 0, I])
-    raises(NotImplementedError, lambda: m.is_diagonalizable(True))
     # !!! bug because of eigenvects() or roots(x**2 + (-1 - I)*x + I, x)
     # see issue 5292
-    assert not m.is_diagonalizable(True)
-    raises(MatrixError, lambda: m.diagonalize(True))
-    (P, D) = m.diagonalize(True)
+    assert not m.is_diagonalizable(reals_only=True)
+    raises(MatrixError, lambda: m.diagonalize(reals_only=True))
 
 def test_issue_8240():
     # Eigenvalues of large triangular matrices
