66e04e7ea8e5e88ee4f90aa922e337708a7ef8ac
Sania Thapa <saniathapa1970@gmail.com>
2026-03-18 18:34:26 +0530

    Remove trailing whitespace

--- sympy/series/tests/test_series.py
+++ sympy/series/tests/test_series.py
@@ -425,6 +425,5 @@ def test_issue_26856():
 def test_issue_27109():
     x = symbols('x', positive=True)
     s= series(2*erf(x), x, oo, 3)
-    
     #ensure the exponential asymptotic term is presevered
     assert s.has(exp(-x**2)/x)
