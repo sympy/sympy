b461d8b0840fbb0d51f3afa9007727a77ccc35eb
ruxhme <golu993857@gmail.com>
2026-03-21 12:25:59 +0530

    Fix trailing whitespace in autolev parser and init

--- sympy/parsing/autolev/_listener_autolev_antlr.py
+++ sympy/parsing/autolev/_listener_autolev_antlr.py
@@ -441,7 +441,6 @@ if AutolevListener:
             # Write code to import common dependencies.
             self.output_code.append("import sympy.physics.mechanics as _me\n")
             self.output_code.append("import sympy as _sm\n")
-            
             self.output_code.append("\n")
 
             # Just a store for the max degree variable in a line.
