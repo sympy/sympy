0b8c2c0a7f84c606c7c396e3aa83b65adcad7db9
Palak Kumari <palak.kumari1025@gmail.com>
2026-03-22 14:58:23 +0530

    physics.units: prevent subs from modifying Quantity abbrev

--- sympy/physics/units/tests/test_quantities.py
+++ sympy/physics/units/tests/test_quantities.py
@@ -574,3 +574,13 @@ def test_physics_constant():
 
     assert not meter.is_physical_constant
     assert not joule.is_physical_constant
+
+def subs_does_not_modify_abbrev():
+    from sympy import Symbol
+    from sympy.physics.units import meter, kilogram
+
+    m = Symbol("m")
+    q = meter.subs({m: 950 * kilogram})
+
+    assert q == meter
+    assert q.abbrev == m
