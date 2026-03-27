231c2499103b2ddcdaa711468a80a481ef0844d7
gokul <gokulkomath@gmail.com>
2026-03-27 07:34:28 +0530

    removed unused import

--- sympy/plotting/pygletplot/plot_interval.py
+++ sympy/plotting/pygletplot/plot_interval.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, Iterator, Callable, TYPE_CHECKING
+from typing import Any, Iterator, Callable
 from sympy.core.singleton import S
 from sympy.core.symbol import Symbol
 from sympy.core.sympify import sympify
