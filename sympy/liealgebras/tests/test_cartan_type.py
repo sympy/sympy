51c92ed351b53c639d0d977e8f05790641569442
Shubhankar-Joshi2489 <shubhankarjoshi2489@gmail.com>
2026-03-27 21:59:37 +0530

    Fix CartanType invalid handling and WeylGroup series() usage

--- sympy/liealgebras/tests/test_cartan_type.py
+++ sympy/liealgebras/tests/test_cartan_type.py
@@ -4,10 +4,10 @@ from sympy.liealgebras.cartan_type import CartanType, Standard_Cartan
 def test_Standard_Cartan():
     c = CartanType("A4")
     assert c.rank() == 4
-    assert c.series == "A"
+    assert c.series() == "A"
     m = Standard_Cartan("A", 2)
     assert m.rank() == 2
-    assert m.series == "A"
+    assert m.series() == "A"
     b = CartanType("B12")
     assert b.rank() == 12
-    assert b.series == "B"
+    assert b.series() == "B"
