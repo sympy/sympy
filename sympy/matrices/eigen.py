8b97062195d1b9e2fc820310604f18f9223c3116
vprayag2005 <v.prayag2005@gamil.com>
2026-04-01 10:51:51 +0530

    Fix Matrix.eigenvals(rational=True) returning floats for numeric matrices

--- sympy/matrices/eigen.py
+++ sympy/matrices/eigen.py
@@ -207,15 +207,15 @@ def _eigenvals(M,
     if not M.is_square:
         raise NonSquareMatrixError("{} must be a square matrix.".format(M))
 
-    if M._rep.domain not in (ZZ, QQ):
-        # Skip this check for ZZ/QQ because it can be slow
-        if all(x.is_number for x in M) and M.has(Float):
-            return _eigenvals_mpmath(M, multiple=multiple)
-
     if rational:
         from sympy.simplify import nsimplify
         M = M.applyfunc(
             lambda x: nsimplify(x, rational=True) if x.has(Float) else x)
+
+    if M._rep.domain not in (ZZ, QQ):
+        # Skip this check for ZZ/QQ because it can be slow
+        if all(x.is_number for x in M) and M.has(Float):
+            return _eigenvals_mpmath(M, multiple=multiple)
 
     if multiple:
         return _eigenvals_list(
