715feca971cf8156aa9adc30a8cc4beb1a42aa15
Kishore G <kishore96@gmail.com>
2026-04-16 15:37:13 +0530

    test_tensor_can.py: change _array_form -> array_form
    The underscored one now returns a Tuple, not a list.

--- sympy/combinatorics/tests/test_tensor_can.py
+++ sympy/combinatorics/tests/test_tensor_can.py
@@ -17,11 +17,11 @@ def test_dummy_sgs():
     a = dummy_sgs([1,2], 0, 4)
     assert a == [[0,2,1,3,4,5]]
     a = dummy_sgs([2,3,4,5], 0, 8)
-    assert a == [x._array_form for x in [Perm(9)(2,3), Perm(9)(4,5),
+    assert a == [x.array_form for x in [Perm(9)(2,3), Perm(9)(4,5),
         Perm(9)(2,4)(3,5)]]
 
     a = dummy_sgs([2,3,4,5], 1, 8)
-    assert a == [x._array_form for x in [Perm(2,3)(8,9), Perm(4,5)(8,9),
+    assert a == [x.array_form for x in [Perm(2,3)(8,9), Perm(4,5)(8,9),
         Perm(9)(2,4)(3,5)]]
 
 def test_get_symmetric_group_sgs():
