072e6a97e214f516d43939099931417229d6a66f
Joseph Mehdiyev <yusifmehdiyev55@gmail.com>
2026-06-21 23:12:40 +0100

    TST: add tests for ask(Q.eq(a, a+1)) for extended reals.

--- sympy/core/tests/test_relational.py
+++ sympy/core/tests/test_relational.py
@@ -1200,6 +1200,11 @@ def test_is_eq():
     assert is_neq(x, S(0), assumptions=~Q.zero(x))
     assert is_neq(x, S(0), assumptions=Q.nonzero(x))
 
+    # issue 29861
+    assert is_eq(x, x + 1) is None
+    assert is_eq(x, x + 1, Q.finite(x)) is False
+    assert is_eq(oo, oo + 1)
+
     # test registration
     class PowTest(Expr):
         def __new__(cls, base, exp):
