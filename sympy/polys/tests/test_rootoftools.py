3c8d147086222c4a919647df4f42d72e2d5f999f
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2026-07-19 02:42:18 +0100

    fix(polys): add multithreaded test fro CRootOf

--- sympy/polys/tests/test_rootoftools.py
+++ sympy/polys/tests/test_rootoftools.py
@@ -1,6 +1,9 @@
 """Tests for the implementation of RootOf class and related tools. """
 from __future__ import annotations
 
+from concurrent.futures import ThreadPoolExecutor
+from threading import Barrier
+
 import pytest
 
 from sympy.polys.polytools import Poly
@@ -26,7 +29,7 @@ from sympy.polys.orthopolys import legendre_poly
 from sympy.solvers.solvers import solve
 
 
-from sympy.testing.pytest import raises, slow
+from sympy.testing.pytest import raises, skip_under_pyodide, slow
 from sympy.core.expr import unchanged
 
 from sympy.abc import a, b, x, y, z, r
@@ -622,6 +625,27 @@ def test_is_disjoint():
     ii = rootof(eq, 1)._get_interval()
     assert ir.is_disjoint(ii)
     assert ii.is_disjoint(ir)
+
+
+@skip_under_pyodide("Cannot create threads under pyodide.")
+def test_CRootOf_concurrent_cache():
+    poly = Poly((x**3 + x + 3)*(x**3 + 5*x + 1), x)
+    expected = [str(root.evalf(12)) for root in poly.all_roots()]
+    barrier = Barrier(4)
+
+    def get_roots(_):
+        results = []
+        for _ in range(10):
+            barrier.wait()
+            roots = poly.all_roots()
+            results.append([str(root.evalf(12)) for root in roots])
+        return results
+
+    CRootOf.clear_cache()
+    with ThreadPoolExecutor(max_workers=4) as pool:
+        results = pool.map(get_roots, range(4))
+
+    assert all(roots == expected for result in results for roots in result)
 
 
 def test_pure_key_dict():
