8c4315375ff6b24e844278cdeb0f4dd33e3a9b24
Joseph Mehdiyev <yusifmehdiyev55@gmail.com>
2026-07-25 08:27:25 +0100

    ENH: small refactoring

--- sympy/logic/algorithms/theory_solver.py
+++ sympy/logic/algorithms/theory_solver.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Literal, Protocol, TYPE_CHECKING, runtime_checkable
+from typing import Any, Literal, Protocol, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from sympy.assumptions.cnf import EncodedCNF
@@ -9,7 +9,6 @@ ConflictClause = list[int]
 Model = Any
 
 
-@runtime_checkable
 class TheorySolver(Protocol):
     """
     This class implements parameter X of a DPLL(X) engine.
@@ -20,15 +19,15 @@ class TheorySolver(Protocol):
     def from_encoded_cnf(
         cls, encoded_cnf: EncodedCNF
     ) -> tuple[TheorySolver, list[ConflictClause]]:
-        ...
+        raise NotImplementedError
 
     def assert_lit(
         self, literal: int
     ) -> tuple[Literal[False], ConflictClause] | None:
-        ...
+        raise NotImplementedError
 
     def check(self) -> tuple[Literal[True], Model] | tuple[Literal[False], ConflictClause]:
-        ...
+        raise NotImplementedError
 
     def reset(self) -> None:
-        ...
+        raise NotImplementedError
