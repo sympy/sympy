19299ecd10f754ef8751f286b54359cbdc002e6e
grdi1 <filip.pozar1@gmail.com>
2026-07-25 17:09:19 +0200

    Unicode whitelisted str.py because printing of AlgebraicTensors, which is defined in str.py, requires the unicode tensor product symbol

--- sympy/testing/quality_unicode.py
+++ sympy/testing/quality_unicode.py
@@ -68,6 +68,8 @@ unicode_whitelist = [
     r'*/sympy/tensor/algebraic/tests/test_algebraic_tensor.py',
     r'*/sympy/tensor/algebraic/tests/test_algebraic_zero_tensor.py',
     r'*/sympy/tensor/algebraic/tests/test_simplify.py',
+    #The printing functionality of algebraic tensors is in str.py
+    r'*/sympy/printing/str.py',
 ]
 
 unicode_strict_whitelist = [
