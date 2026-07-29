c7ebf5846b035c118fc996f6d5d574f8a81c829b
salastro <salah2112004@gmail.com>
2026-07-29 12:09:53 +0300

    test_manual: update test cases involoving special functions piecewise degenerate steps

--- sympy/integrals/tests/test_manual.py
+++ sympy/integrals/tests/test_manual.py
@@ -779,15 +779,22 @@ def test_issue_23566():
 def test_issue_25093():
     ap = Symbol('ap', positive=True)
     an = Symbol('an', negative=True)
-    assert manualintegrate(exp(a*x**2 + b), x) == sqrt(pi)*exp(b)*erfi(sqrt(a)*x)/(2*sqrt(a))
+    assert manualintegrate(exp(a*x**2 + b), x) == Piecewise(
+        (sqrt(pi)*exp(b)*erfi(sqrt(a)*x)/(2*sqrt(a)), Ne(a, 0)),
+        (x*exp(b), True))
     assert manualintegrate(exp(ap*x**2 + b), x) == sqrt(pi)*exp(b)*erfi(sqrt(ap)*x)/(2*sqrt(ap))
     assert manualintegrate(exp(an*x**2 + b), x) == -sqrt(pi)*exp(b)*erf(an*x/sqrt(-an))/(2*sqrt(-an))
-    assert manualintegrate(sin(a*x**2 + b), x) == (
-        sqrt(2)*sqrt(pi)*(sin(b)*fresnelc(sqrt(2)*sqrt(a)*x/sqrt(pi))
-        + cos(b)*fresnels(sqrt(2)*sqrt(a)*x/sqrt(pi)))/(2*sqrt(a)))
-    assert manualintegrate(cos(a*x**2 + b), x) == (
-        sqrt(2)*sqrt(pi)*(-sin(b)*fresnels(sqrt(2)*sqrt(a)*x/sqrt(pi))
-        + cos(b)*fresnelc(sqrt(2)*sqrt(a)*x/sqrt(pi)))/(2*sqrt(a)))
+    assert manualintegrate(sin(a*x**2 + b), x) == Piecewise(
+        (sqrt(2)*sqrt(pi)*(sin(b)*fresnelc(sqrt(2)*sqrt(a)*x/sqrt(pi)) +
+            cos(b)*fresnels(sqrt(2)*sqrt(a)*x/sqrt(pi)))/(2*sqrt(a)),
+         Ne(a, 0)),
+        (x*sin(b), True))
+    assert manualintegrate(cos(a*x**2 + b), x) == Piecewise(
+        (sqrt(2)*sqrt(pi)*(-sin(b)*fresnels(sqrt(2)*sqrt(a)*x/sqrt(pi)) +
+            cos(b)*fresnelc(sqrt(2)*sqrt(a)*x/sqrt(pi)))/(2*sqrt(a)),
+         Ne(a, 0)),
+        (x*cos(b), True))
+
 
 
 def test_nested_pow():
