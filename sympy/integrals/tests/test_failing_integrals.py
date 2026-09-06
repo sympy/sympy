73e3f47a0101735840244e97e6344c7adee1af82
greyf <258340642+jonathanwxh-cell@users.noreply.github.com>
2026-09-06 20:51:57 +0800

    Fix whole-period reciprocal trigonometric integrals with real parameters

--- sympy/integrals/tests/test_failing_integrals.py
+++ sympy/integrals/tests/test_failing_integrals.py
@@ -238,12 +238,6 @@ def test_issue_4311_slow():
     assert not integrate(x*abs(9-x**2), x).has(Integral)
 
 @XFAIL
-def test_issue_20370():
-    a = symbols('a', positive=True)
-    assert integrate((1 + a * cos(x))**-1, (x, 0, 2 * pi)) == (2 * pi / sqrt(1 - a**2))
-
-
-@XFAIL
 def test_polylog():
     # log(1/x)*log(x+1)-polylog(2, -x)
     assert not integrate(log(1/x)/(x + 1), x).has(Integral)
