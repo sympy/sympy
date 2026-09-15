02bdb6e7e8fed509ffe022e3a1ba3f49e5f7e107
Utsab Dahal <utsabdahal34@gmail.com>
2026-09-15 12:22:07 +0545

    fix: parenthesize folded Fortran function bases

--- sympy/printing/fortran.py
+++ sympy/printing/fortran.py
@@ -296,15 +296,23 @@ class FCodePrinter(CodePrinter):
         else:
             return CodePrinter._print_Add(self, expr)
 
+    def _eval_function(self, expr):
+        prec = self._settings['precision']
+        args = [N(a, prec) for a in expr.args]
+        return expr.func(*args)
+
+    def parenthesize(self, item, level, strict=False):
+        if isinstance(item, Function):
+            item = self._eval_function(item)
+        return super().parenthesize(item, level, strict)
+
     def _print_Function(self, expr):
         # All constant function args are evaluated as floats
-        prec =  self._settings['precision']
-        args = [N(a, prec) for a in expr.args]
-        eval_expr = expr.func(*args)
+        eval_expr = self._eval_function(expr)
         if not isinstance(eval_expr, Function):
             return self._print(eval_expr)
         else:
-            return CodePrinter._print_Function(self, expr.func(*args))
+            return CodePrinter._print_Function(self, eval_expr)
 
     def _print_Mod(self, expr):
         # NOTE : Fortran has the functions mod() and modulo(). modulo() behaves
