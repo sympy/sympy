bccd2602b36332ebadb44f1d1998a51b4424e202
Christopher Smith <smichr@gmail.com>
2026-09-22 13:21:08 -0500

    tests: expect Poly for named charpoly

--- sympy/matrices/tests/test_matrixbase.py
+++ sympy/matrices/tests/test_matrixbase.py
@@ -1205,7 +1205,7 @@ def test_issue_17247_expression_blowup_4():
 def test_issue_17247_expression_blowup_5():
     M = Matrix(6, 6, lambda i, j: 1 + (-1)**(i+j)*I)
     with dotprodsimp(True):
-        assert M.charpoly('x') == PurePoly(x**6 + (-6 - 6*I)*x**5 + 36*I*x**4, x, domain='ZZ_I')
+        assert M.charpoly('x') == Poly(x**6 + (-6 - 6*I)*x**5 + 36*I*x**4, x, domain='ZZ_I')
 
 
 def test_issue_17247_expression_blowup_6():
@@ -2545,13 +2545,14 @@ def test_Matrix_berkowitz_charpoly():
     charpoly = A.charpoly(x)
 
     assert charpoly == \
-        PurePoly(x**2 + (K_i*UA + K_w*UA + 2*K_i*K_w)/(K_i + K_w)*x +
+        Poly(x**2 + (K_i*UA + K_w*UA + 2*K_i*K_w)/(K_i + K_w)*x +
         K_i*K_w*UA/(K_i + K_w), x, domain='ZZ(K_i,K_w,UA)')
 
-    assert type(charpoly) is PurePoly
+    assert type(charpoly) is Poly
 
     A = Matrix([[1, 3], [2, 0]])
-    assert A.charpoly() == A.charpoly(x) == PurePoly(x**2 - x - 6)
+    assert A.charpoly() == PurePoly(x**2 - x - 6)
+    assert A.charpoly(x) == Poly(x**2 - x - 6, x)
 
     A = Matrix([[1, 2], [x, 0]])
     p = A.charpoly(x)
