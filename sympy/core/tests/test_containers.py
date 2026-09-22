14e4f10107efb3c83bf3ca0eb8592468dc00af68
Ishaan <ishaanbansal1412@gmail.com>
2026-09-22 19:12:12 +0530

    Update test_issue_5788 to compare against reversed pair

--- sympy/core/tests/test_containers.py
+++ sympy/core/tests/test_containers.py
@@ -214,5 +214,5 @@ def test_issue_5788():
         if o != Tuple:
             assert o(*args) == o(*reversed(args))
         pair = [o(*args), o(*reversed(args))]
-        sorted(pair)  # doesn't fail
+        assert sorted(pair) == sorted(pair[::-1])
         assert set(o(*args))  # doesn't fail
