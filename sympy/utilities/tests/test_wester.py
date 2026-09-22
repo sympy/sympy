1f91537c7ebf669ed41457d6e8310edd8cc56dad
Christopher Smith <smichr@gmail.com>
2026-09-22 14:16:28 -0500

    tests: update PurePoly instantiation expectations

--- sympy/utilities/tests/test_wester.py
+++ sympy/utilities/tests/test_wester.py
@@ -1565,7 +1565,7 @@ def test_P21():
     M = Matrix([[5, -3, -7],
                 [-2, 1,  2],
                 [2, -3, -4]])
-    assert M.charpoly(x).as_expr() == x**3 - 2*x**2 - 5*x + 6
+    assert M.charpoly()(x) == x**3 - 2*x**2 - 5*x + 6
 
 
 def test_P22():
